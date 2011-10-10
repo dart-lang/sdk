c123e91ddde0e745265ae02a80990a422c8f71a5
ager@google.com <ager@google.com>
2011-10-10 15:06:28 +0000

    Add simple directory listing test.
    For now, just test that we list something and that no errors occur.
    
    R=sgjesse@google.com
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org//8222006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@299 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ runtime/tests/dart/src/DirectoryTest.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+// Directory listing test.
+
+class DirectoryTest {
+  static void testListing() {
+    bool listedSomething = false;
+    Directory directory = new Directory.open(".");
+
+    directory.setDirHandler((dir) {
+      listedSomething = true;
+    });
+
+    directory.setFileHandler((f) {
+      listedSomething = true;
+    });
+
+    directory.setDoneHandler((completed) {
+      Expect.isTrue(completed, "directory listing did not complete");
+      Expect.isTrue(listedSomething, "empty directory");
+      directory.close();
+    });
+
+    directory.setDirErrorHandler((dir) {
+      Expect.fail("error listing directory");
+    });
+
+    directory.list();
+  }
+
+  static void testMain() {
+    testListing();
+  }
+}
+
+main() {
+  DirectoryTest.testMain();
+}
