76f932ce14870912b4b090595101a49fe86cd20e
srdjan@google.com <srdjan@google.com>
2011-10-17 19:43:42 +0000

    Add test for blacklisting extension/implementation of classes and interfaces. Added a few tests, please comment which additional ones need to be added. Review URL: http://codereview.chromium.org//8320002
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@481 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/BlackListedTest.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Dart test checking that static/instance field shadowing do not conflict.
+
+// Test that certain interfaces/classes are blacklisted from being
+// implemented or extended.
+
+// bool.
+class MyBool implements bool {}                 /// 01: compile-time error
+interface MyBoolInterface extends bool {}       /// 02: compile-time error
+
+// num.
+class MyNum implements num {}                   /// 03: compile-time error
+interface MyNumInterface extends num {}         /// 04: compile-time error
+
+// int.
+class MyInt implements int {}                   /// 05: compile-time error
+interface MyIntInterface extends int {}         /// 06: compile-time error
+
+// double.
+class MyDouble implements double {}            /// 07: compile-time error
+interface MyDoubleInterface extends double {}  /// 08: compile-time error
+
+// String.
+class MyString implements String {}            /// 09: compile-time error
+interface MyStringInterface extends String {}  /// 10: compile-time error
+
+main() {
+}
