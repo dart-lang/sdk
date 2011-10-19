73d6d0de5437e64e7ce52cf07b4a81de1b4d84cc
iposva@google.com <iposva@google.com>
2011-10-19 21:34:28 +0000

    - Make sure to test all combinations of getters. Review URL: http://codereview.chromium.org//8355014
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@573 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/src/GetterClosureExecutionOrderTest.dart
+++ tests/language/src/GetterClosureExecutionOrderTest.dart
@@ -7,19 +7,81 @@
 
 var counter = 0;
 
+class Test1 {
+  get a() {
+    Expect.equals(1, counter);
+    counter++;
+    return (c) { };
+  }
+
+  b() {
+    Expect.equals(0, counter);
+    counter++;
+    return 1;
+  }
+}
+
+class Test2 {
+  static get a() {
+    Expect.equals(0, counter);
+    counter++;
+    return (c) { };
+  }
+
+  static b() {
+    Expect.equals(1, counter);
+    counter++;
+    return 1;
+  }
+}
+
 get a() {
-  Expect.equals(1, counter);
+  Expect.equals(0, counter);
   counter++;
   return (c) { };
 }
 
 b() {
-  Expect.equals(0, counter);
+  Expect.equals(1, counter);
   counter++;
   return 1;
 }
 
 main() {
-  a(b());
-  Expect.equals(2, counter);
+  var failures = [];
+  try {
+    // Check instance getters.
+    counter = 0;
+    var o = new Test1();
+    o.a(o.b());
+    Expect.equals(2, counter);
+  } catch (var exc, var stack) {
+    failures.add(exc);
+    failures.add(stack);
+  }
+  try {
+    // Check static getters.
+    counter = 0;
+    Test2.a(Test2.b());
+    Expect.equals(2, counter);
+  } catch (var exc, var stack) {
+    failures.add(exc);
+    failures.add(stack);
+  }
+  try {
+    // Check top-level getters.
+    counter = 0;
+    a(b());
+    Expect.equals(2, counter);
+  } catch (var exc, var stack) {
+    failures.add(exc);
+    failures.add(stack);
+  }
+  // If any of the tests failed print out the details and fail the test.
+  if (failures.length != 0) {
+    for (var msg in failures) {
+      print(msg.toString());
+    }
+    throw "${failures.length ~/ 2} tests failed.";
+  }
 }
