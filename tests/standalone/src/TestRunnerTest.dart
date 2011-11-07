f7582845ef24b33647a01e974029fe753706d6fb
whesse@google.com <whesse@google.com>
2011-11-07 12:37:10 +0000

    Dart test script dart rewrite - find the correct executable file for "process_test".
    BUG=
    TEST=standalone/TestRunnerTest
    
    Review URL: http://codereview.chromium.org//8493004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@1247 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/standalone/src/TestRunnerTest.dart
+++ tests/standalone/src/TestRunnerTest.dart
@@ -46,13 +46,27 @@ TestCase MakeTestCase(String testName, List<String> expectations) {
                       new Set<String>.from(expectations));
 }
 
+String getProcessTestFileName() {
+  var names = ['out/Release_ia32/process_test',
+               'out/Debug_ia32/process_test',
+               'xcodebuild/Release_ia32/process_test',
+               'xcodebuild/Debug_ia32/process_test',
+               'Release_ia32/process_test.exe',
+               'Debug_ia32/process_test.exe'];
+  for (var name in names) {
+    if (new File(name).existsSync()) {
+      return name;
+    }
+  }
+  Expect.fail('Could not find the process_test program.');
+}
+
 void main() {
   new RunningProcess(MakeTestCase("PassTest", [PASS]), 30).start();
   new RunningProcess(MakeTestCase("FailTest", [FAIL]), 30).start();
   new RunningProcess(MakeTestCase("TimeoutTest", [TIMEOUT]), 30).start();
 
-  // TODO(whesse): Replace process_test with getProcessTestFileName().
-  new RunningProcess(new TestCase("out/Debug_ia32/process_test",
+  new RunningProcess(new TestCase(getProcessTestFileName(),
                                   const ["0", "0", "1", "1"],
                                   TestController.processCompletedTest,
                                   new Set<String>.from([CRASH])),
