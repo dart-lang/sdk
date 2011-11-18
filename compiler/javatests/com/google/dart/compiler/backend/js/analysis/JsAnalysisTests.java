46882b7fb0d969b236478a68f039c22bd6c5221c
mmendez@google.com <mmendez@google.com>
2011-11-18 15:59:17 +0000

    Added a suite for JS analysis so we run as part of test.py.
    TBR=codefu,fabiomfv,zundel
    
    Review URL: http://codereview.chromium.org//8549023
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@1659 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ compiler/javatests/com/google/dart/compiler/backend/js/analysis/JsAnalysisTests.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package com.google.dart.compiler.backend.js.analysis;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class JsAnalysisTests extends TestSetup {
+
+  public JsAnalysisTests(TestSuite test) {
+    super(test);
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Dart Javascript analysis test suite.");
+
+    suite.addTestSuite(TopLevelElementIndexerTest.class);
+    
+    return new JsAnalysisTests(suite);
+  }
+}
