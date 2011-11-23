1d21a8fa7d910304cd1b5174c80d2251e7550fcc
srdjan@google.com <srdjan@google.com>
2011-11-23 20:09:11 +0000

    Do not type checks at returns of implicit getter. Review URL: http://codereview.chromium.org//8678020
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@1813 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/code_generator_ia32.cc
+++ runtime/vm/code_generator_ia32.cc
@@ -797,11 +797,16 @@ void CodeGenerator::VisitReturnNode(ReturnNode* node) {
 
   // Generate type check.
   if (FLAG_enable_type_checks) {
-    GenerateAssertAssignable(
-        node->id(),
-        node->value()->token_index(),
-        Type::ZoneHandle(parsed_function().function().result_type()),
-        String::ZoneHandle(String::NewSymbol("function result")));
+    const RawFunction::Kind  kind = parsed_function().function().kind();
+    // Implicit getters do not need a type check at return.
+    if ((kind != RawFunction::kImplicitGetter) &&
+        (kind != RawFunction::kConstImplicitGetter)) {
+      GenerateAssertAssignable(
+          node->id(),
+          node->value()->token_index(),
+          Type::ZoneHandle(parsed_function().function().result_type()),
+          String::ZoneHandle(String::NewSymbol("function result")));
+    }
   }
   // Unchain the context(s) up to context level 0.
   int context_level = state()->context_level();
