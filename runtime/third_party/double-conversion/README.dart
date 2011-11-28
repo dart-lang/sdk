326417a3ba2a61ac7b5d2c871403534fa46a2c47
floitsch@google.com <floitsch@google.com>
2011-11-28 18:38:38 +0000

    double-conversion drop.
    Not yet integrated into build process.
    
    Review URL: http://codereview.chromium.org//8632010
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@1870 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ runtime/third_party/double-conversion/README.dart
@@ -0,0 +1,11 @@
+URL: http://code.google.com/p/double-conversion/
+Version: d7a1ee5a309c ('Merge in "Reduce static" branch.').
+License: BSD
+License File: LICENSE
+
+Description:
+This is the double->string and string->double library that has been developed
+for v8.
+
+Local Modifications:
+Removed the test directory, the Makefile and the scons-files.
