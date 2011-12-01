3bbfbdb214b781d6893f7741cd7160f27f1016b8
jmesserly@google.com <jmesserly@google.com>
2011-12-01 23:16:43 +0000

    Fixed escaping of const values http://code.google.com/p/dart/issues/detail?id=323
    Review URL: http://codereview.chromium.org//8773021
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@2001 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/ConstEscapeFrogTest.dart
@@ -0,0 +1,11 @@
+class Foo {
+  final Bar<Foo> bar = const Bar/* comment here use to trigger bug 323 */();
+}
+
+class Bar<T extends Foo> {
+  const Bar();
+}
+
+main() {
+  Expect.equals(new Foo().bar, const Bar());
+}
