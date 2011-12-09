9aa346b921433549a6712616251d90b033b820be
regis@google.com <regis@google.com>
2011-12-09 22:46:44 +0000

    Port jump and call instruction decoding and patching for x64. Review URL: http://codereview.chromium.org//8896017
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@2327 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ runtime/vm/instructions_x64.cc
@@ -0,0 +1,58 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include "vm/globals.h"  // Needed here to get TARGET_ARCH_X64.
+#if defined(TARGET_ARCH_X64)
+
+#include "vm/instructions.h"
+#include "vm/object.h"
+
+namespace dart {
+
+bool Instruction::TestBytesWith(const int* data, int num_bytes) const {
+  ASSERT(data != NULL);
+  const uint8_t* byte_array = reinterpret_cast<const uint8_t*>(start_);
+  for (int i = 0; i < num_bytes; i++) {
+    // Skip comparison for data[i] < 0.
+    if ((data[i] >= 0) && (byte_array[i] != (0xFF & data[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+uword CallOrJump::TargetAddress() const {
+  ASSERT(IsValid());
+  return *reinterpret_cast<uword*>(start() + 2);
+}
+
+
+void CallOrJump::SetTargetAddress(uword target) const {
+  ASSERT(IsValid());
+  *reinterpret_cast<uword*>(start() + 2) = target;
+}
+
+
+const int* Call::pattern() const {
+  // movq $target, TMP
+  // callq *TMP
+  static const int kCallPattern[kLengthInBytes] =
+      {0x49, 0xBB, -1, -1, -1, -1, -1, -1, -1, -1, 0x41, 0xFF, 0xE3};
+  return kCallPattern;
+}
+
+
+const int* Jump::pattern() const {
+  // movq $target, TMP
+  // jmpq TMP
+  static const int kJumpPattern[kLengthInBytes] =
+      {0x49, 0xBB, -1, -1, -1, -1, -1, -1, -1, -1, 0x41, 0xFF, 0xE3};
+  return kJumpPattern;
+}
+
+
+}  // namespace dart
+
+#endif  // defined TARGET_ARCH_X64
---
+++ runtime/vm/instructions_x64.h
@@ -0,0 +1,99 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Classes that describe assembly patterns as used by inline caches.
+
+#ifndef VM_INSTRUCTIONS_X64_H_
+#define VM_INSTRUCTIONS_X64_H_
+
+#ifndef VM_INSTRUCTIONS_H_
+#error Do not include instructions_ia32.h directly; use instructions.h instead.
+#endif
+
+#include "vm/allocation.h"
+
+namespace dart {
+
+// Forward declarations.
+class RawClass;
+class Immediate;
+class RawObject;
+
+// Abstract class for all instruction pattern classes.
+class Instruction : public ValueObject {
+ public:
+  explicit Instruction(uword pc) : start_(pc) {
+    ASSERT(pc != 0);
+  }
+  virtual ~Instruction() {}
+
+  // Call to check if the instruction pattern at 'pc' match the instruction.
+  virtual bool IsValid() const {
+    return TestBytesWith(pattern(), pattern_length_in_bytes());
+  }
+
+  // 'pattern' returns the expected byte pattern in form of an integer array
+  // with length of 'pattern_length_in_bytes'. A '-1' element means 'any byte'.
+  virtual const int* pattern() const = 0;
+  virtual int pattern_length_in_bytes() const = 0;
+
+ protected:
+  uword start() const { return start_; }
+
+ private:
+  // Returns true if the 'num_bytes' bytes at 'start_' correspond to
+  // array of integers 'data'. 'data' elements are either a byte or -1, which
+  // represents any byte.
+  bool TestBytesWith(const int* data, int num_bytes) const;
+
+  const uword start_;
+
+  DISALLOW_COPY_AND_ASSIGN(Instruction);
+};
+
+
+class CallOrJump : public Instruction {
+ public:
+  virtual int pattern_length_in_bytes() const {
+    return kLengthInBytes;
+  }
+  uword TargetAddress() const;
+  void SetTargetAddress(uword new_target) const;
+
+ protected:
+  explicit CallOrJump(uword pc) : Instruction(pc) {}
+  static const int kLengthInBytes = 13;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CallOrJump);
+};
+
+
+class Call : public CallOrJump {
+ public:
+  explicit Call(uword pc) : CallOrJump(pc) {}
+  static int InstructionLength() {
+    return kLengthInBytes;
+  }
+
+ private:
+  virtual const int* pattern() const;
+
+  DISALLOW_COPY_AND_ASSIGN(Call);
+};
+
+
+class Jump : public CallOrJump {
+ public:
+  explicit Jump(uword pc) : CallOrJump(pc) {}
+
+ private:
+  virtual const int* pattern() const;
+
+  DISALLOW_COPY_AND_ASSIGN(Jump);
+};
+
+
+}  // namespace dart
+
+#endif  // VM_INSTRUCTIONS_X64_H_
