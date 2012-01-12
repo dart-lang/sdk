c2564f534d4e3b37053b4e3e2667691a2ba23f73
jimhug@google.com <jimhug@google.com>
2012-01-12 14:01:26 +0000

    fix issue 356 by making test match spec
    Review URL: http://codereview.chromium.org//9147006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@3234 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/src/PseudoKWTest.dart
+++ tests/language/src/PseudoKWTest.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 // Check that we can use pseudo keywords as names in function level code.
@@ -7,23 +7,34 @@
 class PseudoKWTest {
   static testMain() {
 
-    // This list is taken from the 'identifier' production
-    // of the Dart grammar. It lists all the pseudo-keywords
-    // that are legal identifiers at the function level.
+    // This is a list of built-in identifiers from the Dart spec.
+    // It sanity checks that these pseudo-keywords are legal identifiers.
 
     var abstract = 0;
+    var assert = 0;
+    var call = 0;
+    var Dynamic = 0;
     var factory = 0;
     var get = 0;
     var implements = 0;
     var import = 0;
     var interface = 0;
     var library = 0;
-    var native = 0;
     var negate = 0;
     var operator = 0;
     var set = 0;
     var source = 0;
     var static = 0;
+    var typedef = 0;
+
+    // "native" is a per-implementation extension that is not a part of the
+    // Dart language.  While it is not an official built-in identifier, it
+    // is useful to ensure that it remains a legal identifier.
+    var native = 0;
+
+
+    // The code below adds a few additional variants of usage without any
+    // attempt at complete coverage.
     {
       void factory(set) {
         return 0;
