5a5677b37c49ee5c388a0ba93020e294b581dca1
efortuna@google.com <efortuna@google.com>
2012-01-23 23:39:04 +0000

    Missed file in previous checkin. TBR=sigmund@google.com Review URL: https://chromiumcodereview.appspot.com//9160013
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@3523 260f80e4-7a28-3924-810f-c04153c831b5

--- client/tools/buildbot_annotated_steps.py
+++ client/tools/buildbot_annotated_steps.py
@@ -63,14 +63,11 @@ def GetBuildInfo():
   if not name:
     name = socket.gethostname().split('.')[0]
   if not version:
-    has_shell = False
-    if platform == 'windows':
-      # In Windows we need to run in the shell, so that we have all the
-      # environment variables available.
-      has_shell = True
+    # In Windows we need to run in the shell, so that we have all the
+    # environment variables available.
     pipe = subprocess.Popen(
         ['svnversion', '-n'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        shell=has_shell)
+        shell=True)
     output = pipe.communicate()
     if pipe.returncode == 0:
       version = output[0]
@@ -185,17 +182,14 @@ def ProcessFrog(name):
   build and test experimental frog build
   '''
   print 'ProcessFrog'
-  has_shell = False
   if 'windows' in name:
     os.environ['PATH'] = (os.path.join('C:', 'Program Files (x86)', 'nodejs') +
                          os.pathsep + os.environ['PATH'])
-    # In Windows we need to run in the shell, so that we have all the
-    # environment variables available.
-    has_shell = True
-
+  # In Windows we need to run in the shell, so that we have all the
+  # environment variables available.
   return subprocess.call([sys.executable + ' ' +
       os.path.join('frog', 'scripts', 'buildbot_annotated_steps.py')],
-      env=os.environ, shell=has_shell)
+      env=os.environ, shell=True)
 
 def main():
   print 'main'
