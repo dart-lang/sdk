f8368275bbac9dc83b9e26e5663c07b89a2e8e77
turnidge@google.com <turnidge@google.com>
2012-02-02 18:38:15 +0000

    Make IllegalArgumentException more forgiving about its arguments. Review URL: https://chromiumcodereview.appspot.com//9307067
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@3858 260f80e4-7a28-3924-810f-c04153c831b5

--- corelib/src/exceptions.dart
+++ corelib/src/exceptions.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -71,9 +71,9 @@ class ObjectNotClosureException implements Exception {
 
 
 class IllegalArgumentException implements Exception {
-  const IllegalArgumentException([args = ""]) : _args = args;
-  String toString() => "Illegal argument(s): $_args";
-  final String _args;
+  const IllegalArgumentException([arg = ""]) : _arg = arg;
+  String toString() => "Illegal argument(s): $_arg";
+  final _arg;
 }
 
 
