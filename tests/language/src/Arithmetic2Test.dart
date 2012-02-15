e554ddf8a0d1456b37439cc9c77829c9d6fd6365
floitsch@google.com <floitsch@google.com>
2012-02-15 13:25:33 +0000

    Throw NoSuchMethod or IllegalArgument on bad operators or bad arguments to binary expressions.
    Fixes Issue 1588
    
    Review URL: https://chromiumcodereview.appspot.com//9408001
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@4260 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/Arithmetic2Test.dart
@@ -0,0 +1,70 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Dart test program to test arithmetic operations.
+
+class A {
+  static foo() => 499;
+}
+
+bool throwsNoSuchMethod(f) {
+  try {
+    f();
+    return false;
+  } catch(NoSuchMethodException e) {
+    return true;
+  }
+  return false;
+}
+
+bool throwsBecauseOfBadArgument(f) {
+  try {
+    f();
+    return false;
+  } catch(NoSuchMethodException e) {
+    return true;
+  } catch(IllegalArgumentException e) {
+    return true;
+  } catch(NullPointerException e) {
+    return true;
+  }
+  return false;
+}
+
+numberOpBadSecondArgument(f) {
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(true)));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(new A())));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f("foo")));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f("5")));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(() => 499)));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(null)));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(false)));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f([])));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f({})));
+  Expect.isTrue(throwsBecauseOfBadArgument(() => f(A.foo)));
+}
+
+badOperations(b) {
+  Expect.isTrue(throwsNoSuchMethod(() => b - 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b * 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b ~/ 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b / 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b % 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b + 3));
+  Expect.isTrue(throwsNoSuchMethod(() => b[3]));
+  Expect.isTrue(throwsNoSuchMethod(() => ~b));
+  Expect.isTrue(throwsNoSuchMethod(() => -b));
+}
+
+main() {
+  numberOpBadSecondArgument((x) => 3 + x);
+  numberOpBadSecondArgument((x) => 3 - x);
+  numberOpBadSecondArgument((x) => 3 * x);
+  numberOpBadSecondArgument((x) => 3 / x);
+  numberOpBadSecondArgument((x) => 3 ~/ x);
+  numberOpBadSecondArgument((x) => 3 % x);
+  badOperations(true);
+  badOperations(false);
+  badOperations(() => 499);
+  badOperations(A.foo);
+}
