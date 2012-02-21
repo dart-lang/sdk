87089f37f1aa0188685c0cf908ab34636403a028
srdjan@google.com <srdjan@google.com>
2012-02-21 22:07:59 +0000

    Fix cacthing exception object: a null exception is always caught. Review URL: https://chromiumcodereview.appspot.com//9416067
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@4411 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/src/ExceptionTest.dart
+++ tests/language/src/ExceptionTest.dart
@@ -18,6 +18,17 @@ class ExceptionTest {
       i += 10;
     }
     Expect.equals(20, i);
+
+    bool correctCatch = false;
+    try {
+      throw null;
+    } catch (String s) {
+      // null exception object is caught here.
+      correctCatch = true;
+    } catch (var x) {
+      correctCatch = false;
+    }
+    Expect.isTrue(correctCatch);
   }
 }
 
