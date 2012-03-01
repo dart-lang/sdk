f4942882286a61e5651795689957628411b217b7
ahe@google.com <ahe@google.com>
2012-03-01 15:16:52 +0000

    Add test inspired by Leg bug.
    Review URL: https://chromiumcodereview.appspot.com//9570002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@4799 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/Closure3Test.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test that a NullPointerException is thrown even when an expression
+// seems to be free of side-effects.
+
+test(x, y) {
+  (() { x - y; })();
+}
+
+main() {
+  try {
+    test(null, 2);
+    Expect.fail('Expected NullPointerException');
+  } catch (NullPointerException ex) {
+    return;
+  }
+  Expect.fail('Expected NullPointerException');
+}
