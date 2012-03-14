c96189c6a9a5da30c801e2a57b924fe40dabe12f
ager@google.com <ager@google.com>
2012-03-14 16:50:16 +0000

    Wrap all arguments passed to processes in "" on Windows.
    I'm not 100% sure that this is what we want. I'll dig into
    what other systems are doing tomorrow.
    
    This could make the test scripts work on Windows for Dartium.
    
    Updated windows test binary as well.
    
    R=antonm@google.com,sgjesse@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//9699032
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@5472 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/process_win.cc
+++ runtime/bin/process_win.cc
@@ -560,9 +560,9 @@ int Process::Start(const char* path,
   for (int i = 0; i < arguments_length; i++) {
     command_line_length += strlen(arguments[i]);
   }
-  // Account for two occurrences of '"' around the command, one
-  // space per argument and a terminating '\0'.
-  command_line_length += 2 + arguments_length + 1;
+  // Account for two occurrences of '"' around the command, one space
+  // and two occurrences of '"' per argument and a terminating '\0'.
+  command_line_length += 2 + (3 * arguments_length) + 1;
   static const int kMaxCommandLineLength = 32768;
   if (command_line_length > kMaxCommandLineLength) {
     int error_code = SetOsErrorMessage(os_error_message, os_error_message_len);
@@ -580,7 +580,7 @@ int Process::Start(const char* path,
   remaining -= written;
   ASSERT(remaining >= 0);
   for (int i = 0; i < arguments_length; i++) {
-    written = snprintf(command_line + len, remaining, " %s", arguments[i]);
+    written = snprintf(command_line + len, remaining, " \"%s\"", arguments[i]);
     len += written;
     remaining -= written;
     ASSERT(remaining >= 0);
