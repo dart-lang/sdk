a1a7f30d0168bc9181f0aa300cbc9bfd7694572a
podivilov@google.com <podivilov@google.com>
2012-03-30 13:39:27 +0000

    Adjust native field wrapper field count.
    See corresponding change in WebKit: http://codereview.chromium.org/9837116/.
    
    R=antonm@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com//9968004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6034 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/dom/src/native_DOMWrapperBase.dart
+++ lib/dom/src/native_DOMWrapperBase.dart
@@ -2,6 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-class DOMWrapperBase extends NativeFieldWrapperClass2 implements DOMType {
+class DOMWrapperBase extends NativeFieldWrapperClass1 implements DOMType {
   var dartObjectLocalStorage;
 }
