ec60610969001e91572dc6c44500fbe7336eaa72
floitsch@google.com <floitsch@google.com>
2012-03-30 17:54:41 +0000

    During bailout construction add condition instructions to list of live variables.
    Review URL: https://chromiumcodereview.appspot.com//9958007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6054 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/ConditionBailoutTest.dart
@@ -0,0 +1,45 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Dart test program testing closures.
+
+class A {
+  operator negate() => this;
+
+  foo(x) {
+    -a;
+    if(x) return true;
+    return false;
+  }
+
+  loop1(x) {
+    -a;
+    while(x) return true;
+    return false;
+  }
+
+  loop2(x) {
+    -a;
+    for(;x;) return true;
+    return false;
+  }
+
+  loop3(x) {
+    -a;
+    var i = 0;
+    do {
+      if (i++ == 1) return false;
+    } while(!x);
+    return true;
+  }
+}
+
+var a;
+
+main() {
+  a = new A();
+  Expect.isTrue(a.foo(true));
+  Expect.isTrue(a.loop1(true));
+  Expect.isTrue(a.loop2(true));
+  Expect.isTrue(a.loop3(true));
+}
