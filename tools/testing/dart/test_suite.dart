139b211f7df6b41824893a6391b0efb9363f2473
rnystrom@google.com <rnystrom@google.com>
2012-03-30 20:48:32 +0000

    First check in for pub package manager.
    It doesn't do anything more than show usage and version, but the test
    infrastructure is there.
    
    Review URL: https://chromiumcodereview.appspot.com//9950005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6066 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/testing/dart/test_suite.dart
+++ tools/testing/dart/test_suite.dart
@@ -984,6 +984,52 @@ class DartcCompilationTestSuite extends StandardTestSuite {
 }
 
 
+/**
+ * A standard test suite whose file organization matches an expected structure.
+ * To use this, your suite should look like:
+ *
+ *     dart/
+ *       path/
+ *         to/
+ *           mytestsuite/
+ *             mytestsuite.status
+ *             example1_tests.dart
+ *             example2_tests.dart
+ *             example3_tests.dart
+ *
+ * The important parts:
+ *
+ * * The leaf directory name is the name of your test suite.
+ * * The status file uses the same name.
+ * * Test files are directly in that directory and end in "_tests.dart".
+ *
+ * If you follow that convention, then you can construct one of these like:
+ *
+ *     new DirectoryTestSuite(configuration, 'path/to/mytestsuite');
+ *
+ * instead of having to create a custom [StandardTestSuite] subclass. In
+ * particular, if you add 'path/to/mytestsuite' to [TEST_SUITE_DIRECTORIES] in
+ * test.dart, this will all be set up for you.
+ */
+class DirectoryTestSuite extends StandardTestSuite {
+  factory DirectoryTestSuite(Map configuration, String directory) {
+    final name = directory.substring(directory.lastIndexOf('/') + 1);
+    print(name);
+
+    return new DirectoryTestSuite._internal(configuration,
+      name, directory, ['$directory/$name.status']);
+  }
+
+  DirectoryTestSuite._internal(Map configuration,
+                               String suiteName,
+                               String directoryPath,
+                               List<String> statusFilePaths)
+    : super(configuration, suiteName, directoryPath, statusFilePaths);
+
+  bool isTestFile(String filename) => filename.endsWith('_tests.dart');
+}
+
+
 class JUnitTestSuite implements TestSuite {
   Map configuration;
   String suiteName;
--- utils/tests/pub/pub_tests.dart
+++ utils/tests/pub/pub_tests.dart
@@ -4,11 +4,40 @@
 
 #library('pub_tests');
 
-// TODO(rnystrom): Better path to unittest.
+#import('dart:io');
+
+#import('test_pub.dart');
 #import('../../../lib/unittest/unittest_vm.dart');
 
 main() {
-  test('should pass', () {
-    expect(1).equals(1);
+  group('running pub with no command', () {
+    testOutput('displays usage',
+      [],
+      '''
+      Pub is a package manager for Dart.
+
+      Usage:
+
+        pub command [arguments]
+
+      The commands are:
+
+        version   print Pub version
+
+      Use "pub help [command]" for more information about a command.''');
+  });
+
+  group('the version command', () {
+    testOutput('displays the current version',
+      ['version'], 'Pub 0.0.0');
+  });
+
+  group('an unknown command', () {
+    testOutput('displays an error message',
+      ['quylthulg'],
+      '''
+      Unknown command "quylthulg".
+      Run "pub help" to see available commands.''',
+      exitCode: 64);
   });
 }
