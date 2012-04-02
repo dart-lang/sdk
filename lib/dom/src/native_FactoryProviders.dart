3cc89e4a5eb705ae8a6349a6de55460b173825ca
antonm@google.com <antonm@google.com>
2012-04-02 19:18:29 +0000

    Support 3-arg versions of .fromBuffer.
    BUG=2362
    TEST=TypedArrays2Test
    
    Review URL: https://chromiumcodereview.appspot.com//9969062
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6104 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/dom/src/native_FactoryProviders.dart
+++ lib/dom/src/native_FactoryProviders.dart
@@ -13,35 +13,43 @@ class _AudioContextFactoryProvider {
 class _TypedArrayFactoryProvider {
   factory Float32Array(int length) => FactoryProviderImplementation.F32(length);
   factory Float32Array.fromList(List<num> list) => FactoryProviderImplementation.F32(list);
-  factory Float32Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.F32(buffer);
+  factory Float32Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length]) =>
+      FactoryProviderImplementation.F32(buffer, byteOffset, length);
 
   factory Float64Array(int length) => FactoryProviderImplementation.F64(length);
   factory Float64Array.fromList(List<num> list) => FactoryProviderImplementation.F64(list);
-  factory Float64Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.F64(buffer);
+  factory Float64Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.F64(buffer, byteOffset, length);
 
   factory Int8Array(int length) => FactoryProviderImplementation.I8(length);
   factory Int8Array.fromList(List<num> list) => FactoryProviderImplementation.I8(list);
-  factory Int8Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.I8(buffer);
+  factory Int8Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.I8(buffer, byteOffset, length);
 
   factory Int16Array(int length) => FactoryProviderImplementation.I16(length);
   factory Int16Array.fromList(List<num> list) => FactoryProviderImplementation.I16(list);
-  factory Int16Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.I16(buffer);
+  factory Int16Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.I16(buffer, byteOffset, length);
 
   factory Int32Array(int length) => FactoryProviderImplementation.I32(length);
   factory Int32Array.fromList(List<num> list) => FactoryProviderImplementation.I32(list);
-  factory Int32Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.I32(buffer);
+  factory Int32Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.I32(buffer, byteOffset, length);
 
   factory Uint8Array(int length) => FactoryProviderImplementation.U8(length);
   factory Uint8Array.fromList(List<num> list) => FactoryProviderImplementation.U8(list);
-  factory Uint8Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.U8(buffer);
+  factory Uint8Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.U8(buffer, byteOffset, length);
 
   factory Uint16Array(int length) => FactoryProviderImplementation.U16(length);
   factory Uint16Array.fromList(List<num> list) => FactoryProviderImplementation.U16(list);
-  factory Uint16Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.U16(buffer);
+  factory Uint16Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.U16(buffer, byteOffset, length);
 
   factory Uint32Array(int length) => FactoryProviderImplementation.U32(length);
   factory Uint32Array.fromList(List<num> list) => FactoryProviderImplementation.U32(list);
-  factory Uint32Array.fromBuffer(ArrayBuffer buffer) => FactoryProviderImplementation.U32(buffer);
+  factory Uint32Array.fromBuffer(ArrayBuffer buffer, [int byteOffset, int length])
+      => FactoryProviderImplementation.U32(buffer, byteOffset, length);
 }
 
 class _WebKitPointFactoryProvider {
