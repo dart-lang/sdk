84d10e056112ec299ea042c4af6e0d9a37050a5b
hausner@google.com <hausner@google.com>
2012-04-10 15:28:07 +0000

    Basic support for reading and writing JSON objects.
    This is a minimal implementation that we can grow as needed
    for debugger protocols and isolate communication.
    Review URL: https://chromiumcodereview.appspot.com//9949054
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6362 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ runtime/vm/json.h
@@ -0,0 +1,119 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#ifndef VM_JSON_H_
+#define VM_JSON_H_
+
+#include "vm/allocation.h"
+#include "vm/globals.h"
+
+namespace dart {
+
+
+// A low level interface to tokenize JSON strings.
+class JSONScanner : ValueObject {
+ public:
+  enum Token {
+    TokenIllegal = 0,
+    TokenLBrace,
+    TokenRBrace,
+    TokenLBrack,
+    TokenRBrack,
+    TokenColon,
+    TokenComma,
+    TokenString,
+    TokenInteger,
+    TokenTrue,
+    TokenFalse,
+    TokenNull,
+    TokenEOM
+  };
+  explicit JSONScanner(const char* json_text);
+
+  void SetText(const char* json_text);
+  void Scan();
+  Token CurrentToken() const { return token_; }
+  bool EOM() const { return token_ == TokenEOM; }
+  const char* TokenChars() const { return token_start_; }
+  int TokenLen() const { return token_length_; }
+  bool IsStringLiteral(const char* literal) const;
+  void Skip(Token matching_token);
+
+ private:
+  bool IsLetter(char ch) const;
+  bool IsDigit(char ch) const;
+  bool IsLiteral(const char* literal);
+  void ScanNumber();
+  void ScanString();
+  void Recognize(Token t);
+
+  const char* current_pos_;
+  const char* token_start_;
+  int token_length_;
+  Token token_;
+};
+
+
+// JSONReader is a higher level interface that allows for lookup of
+// name-value pairs in JSON objects.
+class JSONReader : ValueObject {
+ public:
+  enum JSONType {
+    kString,
+    kInteger,
+    kObject,
+    kArray,
+    kLiteral,
+    kNone
+  };
+
+  explicit JSONReader(const char* json_object);
+  void Set(const char* json_object);
+
+  // Returns true if a pair with the given name was found.
+  bool Seek(const char* name);
+
+  // Returns true if a syntax error was found.
+  bool Error() const { return error_; }
+
+  JSONType Type() const;
+  const char* ValueChars() const {
+    return (Type() != kNone) ? scanner_.TokenChars() : NULL;
+  }
+  int ValueLen() const {
+    return (Type() != kNone) ? scanner_.TokenLen() : 0;
+  }
+  bool IsStringLiteral(const char* literal) const {
+    return scanner_.IsStringLiteral(literal);
+  }
+
+ private:
+  JSONScanner scanner_;
+  const char* json_object_;
+  bool error_;
+};
+
+
+// TextBuffer maintains a dynamic character buffer with a printf-style way to
+// append text.
+class TextBuffer : ValueObject {
+ public:
+  explicit TextBuffer(intptr_t buf_size);
+  ~TextBuffer();
+
+  intptr_t Printf(const char* format, ...);
+  void Clear();
+
+  char* buf() { return buf_; }
+
+ private:
+  void GrowBuffer(intptr_t len);
+  char* buf_;
+  intptr_t buf_size_;
+  intptr_t msg_len_;
+};
+
+}  // namespace dart
+
+#endif  // VM_JSON_H_
---
+++ runtime/vm/json_test.cc
@@ -0,0 +1,119 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include "platform/assert.h"
+#include "vm/json.h"
+#include "vm/unit_test.h"
+
+namespace dart {
+
+
+TEST_CASE(JSON_ScanJSON) {
+  const char* msg = "{ \"id\": 5, \"command\" : \"Debugger.pause\" }";
+
+  JSONScanner scanner(msg);
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenIllegal);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenLBrace);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenString);
+  EXPECT(scanner.IsStringLiteral("id"));
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenColon);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenInteger);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenComma);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenString);
+  EXPECT(scanner.IsStringLiteral("command"));
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenColon);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenString);
+  EXPECT(scanner.IsStringLiteral("Debugger.pause"));
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenRBrace);
+  scanner.Scan();
+  EXPECT_EQ(scanner.CurrentToken(), JSONScanner::TokenEOM);
+}
+
+
+TEST_CASE(JSON_SyntaxError) {
+    const char* jobj = "{ \"id\": 5, "
+                     "  \"command\" : \"Debugger.stop\""  // Missing comma.
+                     "  \"params\" : { "
+                     "    \"url\" : \"blah.dart\", "  // Missing comma.
+                     "    \"line\": 111, "
+                     "  },"
+                     "  \"foo\": \"outer foo\", "
+                     "}";
+  JSONReader reader(jobj);
+  bool found;
+
+  found = reader.Seek("id");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kInteger);
+  found = reader.Seek("params");
+  EXPECT(!found);
+  EXPECT(reader.Error());
+  EXPECT_EQ(reader.Type(), JSONReader::kNone);
+  EXPECT_EQ(0, reader.ValueLen());
+  EXPECT(reader.ValueChars() == NULL);
+}
+
+
+TEST_CASE(JSON_JSONReader) {
+  const char* jobj = "{ \"id\": 5, "
+                     "  \"command\" : \"Debugger.setBreakpoint\","
+                     "  \"params\" : { "
+                     "    \"url\" : \"blah.dart\", "
+                     "    \"foo\" : [null, 1, { }, \"bar\", true, false],"
+                     "    \"line\": 111, "
+                     "  },"
+                     "  \"foo\": \"outer foo\", "
+                     "}";
+
+  JSONReader reader(jobj);
+  bool found;
+
+  found = reader.Seek("id");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kInteger);
+  found = reader.Seek("foo");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kString);
+  EXPECT(reader.IsStringLiteral("outer foo"));
+  found = reader.Seek("line");
+  EXPECT(!found);
+  found = reader.Seek("params");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kObject);
+  reader.Set(reader.ValueChars());
+  found = reader.Seek("foo");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kArray);
+  found = reader.Seek("non-existing");
+  EXPECT(!found);
+  found = reader.Seek("line");
+  EXPECT(found);
+  EXPECT_EQ(reader.Type(), JSONReader::kInteger);
+}
+
+
+TEST_CASE(JSON_TextBuffer) {
+  TextBuffer w(5);  // Small enough to make buffer grow at least once.
+  w.Printf("{ \"%s\" : %d", "length", 175);
+  EXPECT_STREQ("{ \"length\" : 175", w.buf());
+  w.Printf(", \"%s\" : \"%s\" }", "command", "stopIt");
+  EXPECT_STREQ("{ \"length\" : 175, \"command\" : \"stopIt\" }", w.buf());
+
+  JSONReader r(w.buf());
+  bool found = r.Seek("command");
+  EXPECT(found);
+  EXPECT_EQ(r.Type(), JSONReader::kString);
+  EXPECT(r.IsStringLiteral("stopIt"));
+}
+
+}  // namespace dart
