eb7648dd3790f99429d5b7b4519f2302eb3d066b
floitsch@google.com <floitsch@google.com>
2012-04-13 13:14:35 +0000

    Add failing test.
    Review URL: https://chromiumcodereview.appspot.com//10082004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6518 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/src/Operator3Test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class A {
+  operator negate() => this;
+  toString() => "5";
+  abs() => "correct";
+}
+
+// This triggered a bug in Dart2Js: the speculative type optimization assigned
+// type "number" to 'a' and then to '-a'. In the bailout version the type for
+// 'a' was removed, but the type for '-a' was kept.
+foo(a) => -(-a);
+
+main() {
+  Expect.equals("correct", foo(new A()).abs());
+}
