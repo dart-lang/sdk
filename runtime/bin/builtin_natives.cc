ee81b6f0a147985df5ed39da5e3942b5e5235aaf
ager@google.com <ager@google.com>
2012-04-20 12:38:57 +0000

    Support passing an environment variable map to child processes.
    Fix minor type warning in test.
    
    R=sgjesse@google.com
    BUG=dartbug.com/2193
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10127008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6794 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/builtin_natives.cc
+++ runtime/bin/builtin_natives.cc
@@ -51,7 +51,7 @@
   V(Platform_PathSeparator, 0)                                                 \
   V(Platform_LocalHostname, 0)                                                 \
   V(Platform_Environment, 0)                                                   \
-  V(Process_Start, 9)                                                          \
+  V(Process_Start, 10)                                                         \
   V(Process_Kill, 2)                                                           \
   V(ServerSocket_CreateBindListen, 4)                                          \
   V(ServerSocket_Accept, 2)                                                    \
