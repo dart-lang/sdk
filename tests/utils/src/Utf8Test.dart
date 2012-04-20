b777033656a44707008b32988c79ec8464e6df55
antonm@google.com <antonm@google.com>
2012-04-20 09:58:12 +0000

    Add #library to make test run properly under DRT.
    TBR=ahe@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//10146006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6786 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/utils/src/Utf8Test.dart
+++ tests/utils/src/Utf8Test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#library('Utf8Test');
 #import('dart:utf');
 
 String decode(List<int> bytes) => decodeUtf8(bytes);
