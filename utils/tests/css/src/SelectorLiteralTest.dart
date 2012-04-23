cedda7fc3690ac0a9e768e4e684e3ce43d96e6b0
sigmund@google.com <sigmund@google.com>
2012-04-23 18:39:11 +0000

    fixes css tests, now they can also run in browsers too.
    Review URL: https://chromiumcodereview.appspot.com//10170008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6857 260f80e4-7a28-3924-810f-c04153c831b5

--- utils/tests/css/src/SelectorLiteralTest.dart
+++ utils/tests/css/src/SelectorLiteralTest.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#library("SelectorLiteralTest");
 #import("../../../css/css.dart");
 
 class SelectorLiteralTest {
