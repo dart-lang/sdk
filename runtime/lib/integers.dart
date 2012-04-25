1e61443c1487d2f077172588bc32c21d3e151949
floitsch@google.com <floitsch@google.com>
2012-04-25 15:50:36 +0000

    toRadixString returns lower case characters.
    Fixes issue 2670.
    
    Review URL: https://chromiumcodereview.appspot.com//10225003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6955 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/lib/integers.dart
+++ runtime/lib/integers.dart
@@ -137,7 +137,7 @@ class IntegerImplementation {
   }
   String toRadixString(int radix) {
     final table = const ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                         "A", "B", "C", "D", "E", "F"];
+                         "a", "b", "c", "d", "e", "f"];
     if ((radix <= 1) || (radix > 16)) {
       throw "Bad radix: $radix";
     }
