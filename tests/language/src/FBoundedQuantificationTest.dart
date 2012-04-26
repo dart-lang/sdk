c2f823d92b90b65abdfc6d9654280f9adb8bd50e
regis@google.com <regis@google.com>
2012-04-26 22:39:27 +0000

    Support cyclic types and F-bounded quantification (issue 439). Added tests for F-bounded quantification. Review URL: https://chromiumcodereview.appspot.com//10243001
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@7047 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/src/FBoundedQuantificationTest.dart
+++ tests/language/src/FBoundedQuantificationTest.dart
@@ -10,7 +10,42 @@ class Bar extends FBound<Bar> {}
 
 class SubBar extends Bar {}
 
+class Baz<T> extends FBound<Baz<T>> {}
+
+class SubBaz<T> extends Baz<T> {}
+
+
+isCheckedMode() {
+  try {
+    var i = 1;
+    String s = i;
+    return false;
+  } catch(var e) {
+    return true;
+  }
+}
+
 main() {
   FBound<Bar> fb = new FBound<Bar>();
-  FBound<SubBar> fsb = new FBound<SubBar>();  /// 01: static type warning
+  {
+    bool got_type_error = false;
+    try {
+      FBound<SubBar> fsb = new FBound<SubBar>();  /// 01: static type warning
+    } catch (TypeError error) {
+      got_type_error = true;
+    }
+    // Type error in checked mode only.
+    Expect.isTrue(got_type_error == isCheckedMode());  /// 01: continued
+  }
+  FBound<Baz<Bar>> fbb = new FBound<Baz<Bar>>();
+  {
+    bool got_type_error = false;
+    try {
+      FBound<SubBaz<Bar>> fsb = new FBound<SubBaz<Bar>>();  /// 02: static type warning
+    } catch (TypeError error) {
+      got_type_error = true;
+    }
+    // Type error in checked mode only.
+    Expect.isTrue(got_type_error == isCheckedMode());  /// 02: continued
+  }
 }
