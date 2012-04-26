642f2b1c8eaae71bd2865c507b0309929d5d325c
ager@google.com <ager@google.com>
2012-04-26 08:56:27 +0000

    Fix exception throwing in connection with process starting.
    For the run constructor, verify the arguments before starting the
    underlying process.
    
    R=sgjesse@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10202014
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@6995 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/process_impl.dart
+++ runtime/bin/process_impl.dart
@@ -318,20 +318,11 @@ class _InteractiveProcess implements Process {
 // buffers output so it can be delivered to the callback when the
 // process exits.
 class _NonInteractiveProcess implements Process {
-
   _NonInteractiveProcess.start(String path,
                                List<String> arguments,
                                ProcessOptions options,
                                Function this._callback) {
-    _process = new _InteractiveProcess.start(path, arguments, options);
-
-    // Setup process exit handling.
-    _process.onExit = (exitCode) {
-      _exitCode = exitCode;
-      _checkDone();
-    };
-
-    // Extract output encoding options.
+    // Extract output encoding options and verify arguments.
     var stdoutEncoding = Encoding.UTF_8;
     var stderrEncoding = Encoding.UTF_8;
     if (options !== null) {
@@ -350,6 +341,15 @@ class _NonInteractiveProcess implements Process {
         }
       }
     }
+
+    // Start the underlying process.
+    _process = new _InteractiveProcess.start(path, arguments, options);
+
+    // Setup process exit handling.
+    _process.onExit = (exitCode) {
+      _exitCode = exitCode;
+      _checkDone();
+    };
 
     // Setup stdout handling.
     _stdoutBuffer = new StringBuffer();
