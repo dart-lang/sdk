e4402161e413d7861bed5857e63434630c81814e
antonm@google.com <antonm@google.com>
2012-04-27 13:43:13 +0000

    Add #library decls to enable tests in Dartium.
    R=iposva@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//10170028
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@7079 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/lib/src/crypto/sha1Test.dart
+++ tests/lib/src/crypto/sha1Test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#library('sha1Test');
 // TODO(ager): Replace with "dart:crypto" when ready.
 #import("../../../../lib/crypto/crypto.dart");
 
