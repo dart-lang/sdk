eb256510f97589518b56d8a0b9c6f067de82967f
antonm@google.com <antonm@google.com>
2012-05-02 18:00:58 +0000

    Revert last CL.
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@7249 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/dom/typed_arrays_1_test.dart
+++ tests/dom/typed_arrays_1_test.dart
@@ -33,26 +33,6 @@ main() {
 
   });
 
-  test('aliasClampedTest', () {
-      var a1 = new Uint8ClampedArray.fromList([0,0,1,0x45]);
-      var a2 = new Float32Array.fromBuffer(a1.buffer);
-
-      Expect.equals(1, a2.length);
-
-      // 0x45010000 = 2048+16
-      Expect.equals(2048 + 16, a2[0]);
-
-      a1[2] = 0;
-      // 0x45000000 = 2048
-      Expect.equals(2048, a2[0]);
-
-      a1[3]--;
-      a1[2] += 128;
-      // 0x44800000 = 1024
-      Expect.equals(1024, a2[0]);
-
-  });
-
   test('typeTests', () {
       var a = new Float32Array(10);
       Expect.isTrue(a is List);
