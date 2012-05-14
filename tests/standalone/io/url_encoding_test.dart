00863120066fd9f54d27dbacd996951058bcaedb
sgjesse@google.com <sgjesse@google.com>
2012-05-14 09:58:27 +0000

    Fix URL decoding in HTTP library
    Now URL decoding works for non-ASCII URLs and will be UTF-8 decoded.
    
    R=ager@google.com
    
    BUG=dart:2902
    TEST=tests/standalone/io/url_encoding_test.dart
    
    Review URL: https://chromiumcodereview.appspot.com//10387082
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@7599 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/standalone/io/url_encoding_test.dart
+++ tests/standalone/io/url_encoding_test.dart
@@ -2,6 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#import("dart:utf");
+#source("../../../runtime/bin/input_stream.dart");
+#source("../../../runtime/bin/output_stream.dart");
+#source("../../../runtime/bin/chunked_stream.dart");
+#source("../../../runtime/bin/string_stream.dart");
+#source("../../../runtime/bin/stream_util.dart");
+#source("../../../runtime/bin/http.dart");
+#source("../../../runtime/bin/http_impl.dart");
+#source("../../../runtime/bin/http_parser.dart");
 #source("../../../runtime/bin/http_utils.dart");
 
 void testParseEncodedString() {
@@ -13,14 +22,17 @@ void testParseEncodedString() {
 void testParseQueryString() {
   // The query string includes escaped "?"s, "&"s, "%"s and "="s.
   // These should not affect the splitting of the string.
-  String queryString = '%3F=%3D&foo=bar&%26=%25';
+  String queryString =
+      '%3F=%3D&foo=bar&%26=%25&sqrt2=%E2%88%9A2&name=Franti%C5%A1ek';
   Map<String, String> map = _HttpUtils.splitQueryString(queryString);
   for (String key in map.getKeys()) {
-    Expect.equals(map[key], { '&'   : '%',
-                              'foo' : 'bar',
-                              '?'   : '='}[key]);
+    Expect.equals(map[key], { '&'     : '%',
+                              'foo'   : 'bar',
+                              '?'     : '=',
+                              'sqrt2' : '\u221A2',
+                              'name'  : 'Franti\u0161ek'}[key]);
   }
-  Expect.setEquals(map.getKeys(), ['&', '?', 'foo']);
+  Expect.setEquals(map.getKeys(), ['&', '?', 'foo', 'sqrt2', 'name']);
 }
 
 void main() {
