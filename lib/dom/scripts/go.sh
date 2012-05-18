89241b6fbf78d1443c80a4d3d6e50fc81c26f008
sra@google.com <sra@google.com>
2012-05-18 22:26:32 +0000

    Script to generate some or all systems
    I find it essential to generate all the systems in one command.
    I have several CLs in flight where I am changing dart:dom and dart:html
    together.
    
    My habit is to run:
    
      ./go.sh | tee Q
    
    I can inspect file Q if needed.
    
    If I know the IDL has not changed, it is 5x faster to run
    
      ./go.sh --cached
    
    Systems can be picked:
    
      ./go.sh native,frog
    
    The missing piece here is that files are generated under two directories
    lib/dom/generated/
    lib/html/generated/
    
    I propose that we move all the generated files under one directory, so
    that the following process gives a *complete* picture of the changes:
    
      git checkout master
      ./go.sh
      mv ../generated ../generated0
      git checkout work
      ./go.sh
      meld ../generated0 ../generated
    
    Review URL: https://chromiumcodereview.appspot.com//10383252
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@7773 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ lib/dom/scripts/go.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -x
+#
+#   go.sh [--cached] [systems]
+#
+# Convenience script to generate systems.  Do not call from build steps or tests
+# - call fremontcutbuilder and dartdomgenerator instead. Do not add 'real'
+# functionality here - change the python code instead.
+#
+# I find it essential to generate all the systems so I know if I am breaking
+# other systems.  My habit is to run:
+#
+#   ./go.sh | tee Q
+#
+# I can inspect file Q if needed.
+#
+# If I know the IDL has not changed since the last run, it is faster to run
+#
+#   ./go.sh --cached
+#
+# To generate a subset of systems:
+#
+#   ./go.sh frog,htmlfrog
+#
+# The following gives a picture of the changes due to 'work'
+#
+#   git checkout master               # select client without changes
+#   ./go.sh
+#   mv ../generated ../generated0     # save generated files
+#   git checkout work                 # select client with changes
+#   ./go.sh
+#   meld ../generated0 ../generated   # compare directories with too
+
+CACHED=
+if [[ "$1" == "--cached" ]] ; then
+  CACHED=1
+  shift
+fi
+
+ALLSYSTEMS="frog,dummy,native,htmlfrog,htmldartium,native"
+SYSTEMS="$ALLSYSTEMS"
+
+if [[ "$1" != "" ]] ; then
+  SYSTEMS="$1"
+fi
+
+if [[ $CACHED ]] ; then
+  reset &&
+  ./dartdomgenerator.py --use-database-cache --systems="$SYSTEMS"
+else
+  reset &&
+  ./fremontcutbuilder.py &&
+  ./dartdomgenerator.py --systems="$SYSTEMS"
+fi
