4f036454cbae33c2a215e704cedaaab9b67580dd
cshapiro@google.com <cshapiro@google.com>
2012-06-14 01:33:31 +0000

    Fix includes to unbreak the Mac build.
    Review URL: https://chromiumcodereview.appspot.com//10543154
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@8639 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/heap_profiler_test.cc
+++ runtime/vm/heap_profiler_test.cc
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#include "platform/assert.h"
 #include "vm/heap_profiler.h"
 #include "vm/growable_array.h"
 #include "vm/unit_test.h"
