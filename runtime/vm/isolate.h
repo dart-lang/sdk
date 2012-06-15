b11999d58cd51ca9d1d213bcaf75c6c8a47241c1
srdjan@google.com <srdjan@google.com>
2012-06-15 00:56:25 +0000

    Fix stack sizes (kDefaultStackSize to 64kB, and kStackSizeBuffer to 4kb) to be able to catch stack overflows. Review URL: https://chromiumcodereview.appspot.com//10539174
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@8701 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/isolate.h
+++ runtime/vm/isolate.h
@@ -198,8 +198,8 @@ class Isolate : public BaseIsolate {
 
   static uword GetSpecifiedStackSize();
 
-  static const uword kStackSizeBuffer = (128 * KB);
-  static const uword kDefaultStackSize = (1 * MB);
+  static const uword kStackSizeBuffer = (4 * KB);
+  static const uword kDefaultStackSize = (128 * KB);
 
   static ThreadLocalKey isolate_key;
   StoreBufferBlock store_buffer_;
