831422d53dded7c371d1c7c9e95201b7281848e8
iposva@google.com <iposva@google.com>
2012-06-18 18:19:08 +0000

    - Remove passing of obsolete parameter in dart2js. Review URL: https://chromiumcodereview.appspot.com//10565036
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@8812 260f80e4-7a28-3924-810f-c04153c831b5

--- utils/compiler/build_helper.dart
+++ utils/compiler/build_helper.dart
@@ -73,10 +73,7 @@ List<String> buildScript(Uri dartUri, Uri dartVmLocation, String options) {
   print('dart2jsPath = $dart2jsPath');
   print('dart2jsPathWin = $dart2jsPathWin');
 
-  // Disable String operator+. This should only be necessary
-  // temporarily as the operator will go away soon.
-  // TODO(ahe): Remove this option.
-  options = ' --allow_string_plus=false$options';
+  options = ' $options';
 
   // Tell the VM to grow the heap more aggressively. This should only
   // be necessary temporarily until the VM is better at detecting how
