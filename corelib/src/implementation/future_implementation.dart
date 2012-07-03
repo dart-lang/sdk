448fc589b22c19551820d2dc3a4ebba94069ea18
nweiz@google.com <nweiz@google.com>
2012-07-03 21:39:38 +0000

    Properly capture and pass the stack trace in Future exceptions.
    Review URL: https://chromiumcodereview.appspot.com//10701084
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@9378 260f80e4-7a28-3924-810f-c04153c831b5

--- corelib/src/implementation/future_implementation.dart
+++ corelib/src/implementation/future_implementation.dart
@@ -172,14 +172,14 @@ class FutureImpl<T> implements Future<T> {
   Future transform(Function transformation) {
     final completer = new Completer();
     handleException((e) {
-      completer.completeException(e);
+      completer.completeException(e, this.stackTrace);
       return true;
     });
     then((v) {
       var transformed = null;
       try {
         transformed = transformation(v);
-      } catch (final ex, final stackTrace) {
+      } catch (Exception ex, final stackTrace) {
         completer.completeException(ex, stackTrace);
         return;
       }
@@ -191,19 +191,19 @@ class FutureImpl<T> implements Future<T> {
   Future chain(Function transformation) {
     final completer = new Completer();
     handleException((e) {
-      completer.completeException(e);
+      completer.completeException(e, this.stackTrace);
       return true;
     });
     then((v) {
       var future = null;
       try {
         future = transformation(v);
-      } catch (final ex, final stackTrace) {
+      } catch (Exception ex, final stackTrace) {
         completer.completeException(ex, stackTrace);
         return;
       }
       future.handleException((e) {
-        completer.completeException(e);
+        completer.completeException(e, this.stackTrace);
         return true;
       });
       future.then((b) => completer.complete(b));
