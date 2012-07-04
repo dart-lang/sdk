4fbf112cc95a9ab3d3809fe085d1d2905287f5a2
podivilov@google.com <podivilov@google.com>
2012-07-04 11:53:25 +0000

    Associate partial source map with each code block in Universe.generatedCode.
    That way we can create partial source maps for individual functions in codegen,
    and join them into a single map at the end.
    
    Currently partial source maps contain only begin and end positions of all functions.
    
    R=ahe@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//10668029
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@9400 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/compiler/implementation/dart_backend/backend.dart
+++ lib/compiler/implementation/dart_backend/backend.dart
@@ -26,7 +26,7 @@ class DartBackend extends Backend {
     // TODO(antonm): Implement this method, if needed.
   }
 
-  String codegen(WorkItem work) { }
+  CodeBlock codegen(WorkItem work) { return new CodeBlock(null, null); }
 
   void processNativeClasses(Enqueuer world,
                             Collection<LibraryElement> libraries) {
