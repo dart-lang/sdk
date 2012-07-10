b462d8dbd46b947c733d7e5060dd240be409c266
antonm@google.com <antonm@google.com>
2012-07-10 15:06:13 +0000

    Support timers in DOM isolates.
    R=podivilov@chromium.org
    BUG=4013
    
    Review URL: https://chromiumcodereview.appspot.com//10756006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@9511 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/html/dartium/html_dartium.dart
+++ lib/html/dartium/html_dartium.dart
@@ -40565,6 +40565,35 @@ interface ReadyState {
    */
   static final String COMPLETE = "complete";
 }
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// TODO(antonm): support not DOM isolates too.
+class _Timer implements Timer {
+  final canceller;
+
+  _Timer(this.canceller);
+
+  void cancel() { canceller(); }
+}
+
+_getTimerFactoryClosure() =>
+  (int milliSeconds, void callback(Timer timer), bool repeating) {
+    var maker;
+    var canceller;
+    if (repeating) {
+      maker = window.setInterval;
+      canceller = window.clearInterval;
+    } else {
+      maker = window.setTimeout;
+      canceller = window.clearTimeout;
+    }
+    Timer timer;
+    final int id = maker(() { callback(timer); }, milliSeconds);
+    timer = new _Timer(() { canceller(id); });
+    return timer;
+  };
 // Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
