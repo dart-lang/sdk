e9b7aaeba3e06ca8fdf18ecc6ad6df4fbacb429e
floitsch@google.com <floitsch@google.com>
2012-07-13 08:12:31 +0000

    Add test for bug in dart2js.
    Review URL: https://chromiumcodereview.appspot.com//10695179
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@9622 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js/is_inference2_test.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#import("compiler_helper.dart");
+
+final String TEST_IF_BOOL_FIRST_INSTRUCTION = @"""
+negate(x) {
+  if (x is bool) return !x;
+  return x;
+}
+""";
+
+main() {
+  String generated = compile(TEST_IF_BOOL_FIRST_INSTRUCTION, 'negate');
+  Expect.isTrue(generated.contains("!"));  // We want to see !x.
+  Expect.isFalse(generated.contains("!="));  // And not !== true.
+  Expect.isFalse(generated.contains("true"));
+  Expect.isFalse(generated.contains("false"));
+}
