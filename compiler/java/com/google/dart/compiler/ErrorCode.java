53c1cc23a561dfa641c90b5193c066791f1d55c5
scheglov@google.com <scheglov@google.com>
2012-07-24 21:48:39 +0000

    Basic 'Quick Fix' support and one fix as example/test
    Also use ErrorCdoe instead of "problemId" integer.
    
    R=messick@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10816034
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@9865 260f80e4-7a28-3924-810f-c04153c831b5

--- compiler/java/com/google/dart/compiler/ErrorCode.java
+++ compiler/java/com/google/dart/compiler/ErrorCode.java
@@ -3,6 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.google.dart.compiler;
 
+import com.google.dart.compiler.util.apache.StringUtils;
+
 /**
  * The behavior common to objects representing error codes associated with
  * {@link DartCompilationError Dart compilation errors}.
@@ -28,4 +30,29 @@ public interface ErrorCode {
    *         during next incremental compilation.
    */
   boolean needsRecompilation();
+  
+  public class Helper {
+    /**
+     * @return the "qualified name" of the given {@link ErrorCode} enumeration, good for passing it
+     *         to {@link #forQualifiedName(String)}.
+     */
+    public static String toQualifiedName(ErrorCode errorCode) {
+      return errorCode.getClass().getCanonicalName() + "." + ((Enum<?>) errorCode).name();
+    }
+
+    /**
+     * @return the {@link ErrorCode} enumeration constant for string from
+     *         {@link #toQualifiedName(ErrorCode)}.
+     */
+    public static ErrorCode forQualifiedName(String qualifiedName) {
+      try {
+        String className = StringUtils.substringBeforeLast(qualifiedName, ".");
+        String fieldName = StringUtils.substringAfterLast(qualifiedName, ".");
+        Class<?> errorCodeClass = Class.forName(className);
+        return (ErrorCode) errorCodeClass.getField(fieldName).get(null);
+      } catch (Throwable e) {
+        return null;
+      }
+    }
+  }
 }
