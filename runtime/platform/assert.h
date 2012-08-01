40b0d11c1f569b452abeaf8d80dab33fcf1afd2c
turnidge@google.com <turnidge@google.com>
2012-08-01 18:29:55 +0000

    Add missing files from last commit! Review URL: https://chromiumcodereview.appspot.com//10829127
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10133 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/platform/assert.h
+++ runtime/platform/assert.h
@@ -268,6 +268,29 @@ void DynamicAssertionHelper::NotNull(const T p) {
 
 #define DEBUG_ASSERT(cond)
 
+// The COMPILE_ASSERT macro can be used to verify that a compile time
+// expression is true. For example, you could use it to verify the
+// size of a static array:
+//
+//   COMPILE_ASSERT(ARRAYSIZE(content_type_names) == CONTENT_NUM_TYPES,
+//                  content_type_names_incorrect_size);
+//
+// or to make sure a struct is smaller than a certain size:
+//
+//   COMPILE_ASSERT(sizeof(foo) < 128, foo_too_large);
+//
+// The second argument to the macro is the name of the variable. If
+// the expression is false, most compilers will issue a warning/error
+// containing the name of the variable.
+
+template <bool>
+struct CompileAssert {
+};
+
+#define COMPILE_ASSERT(expr, msg)                       \
+  typedef CompileAssert<(static_cast<bool>(expr))>      \
+  msg[static_cast<bool>(expr) ? 1 : -1]
+
 #endif  // if defined(DEBUG)
 
 
