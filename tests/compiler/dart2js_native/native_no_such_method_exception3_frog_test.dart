0397557c717576dc74956fabeb3a3c36a34b0a87
kasperl@google.com <kasperl@google.com>
2012-08-07 13:00:15 +0000

    Make the NSM handling code even smaller and make it work on native classes.
    R=floitsch@google.com
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com//10855018
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10335 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js_native/native_no_such_method_exception3_frog_test.dart
@@ -0,0 +1,39 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class A native "*A" {
+  bar() => 42;
+}
+
+class B native "*B" {
+  foo() => 42;
+}
+
+class C {
+  static create() => new C();
+  noSuchMethod(x, y) => "$x:$y";
+}
+
+makeA() native;
+
+setup() native """
+  function A() {}
+  makeA = function() { return new A; }
+""";
+
+main() {
+  setup();
+  var a = makeA();
+  a.bar();
+  var exception;
+  try {
+    a.foo();
+  } catch (NoSuchMethodException e) {
+    exception = e;
+  }
+  Expect.isNotNull(exception);
+  var c = C.create();
+  Expect.equals("foo:[1, 2]", c.foo(1, 2));
+  Expect.equals("foo:[3, 4, 5]", c.foo(3, 4, 5));
+}
