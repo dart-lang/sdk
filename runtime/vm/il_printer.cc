f1bdaf504aa6ace8c8dcb79774497ceb1d026737
regis@google.com <regis@google.com>
2012-08-10 01:26:13 +0000

    Retrying r10475 (now with null check): Continue work on type propagation in optimizing compiler (still WIP). Propagated types are cached in ssa definition nodes. Type propagation iterates until fix point is reached. Type checks are marked as eliminated when possible. Review URL: https://chromiumcodereview.appspot.com//10854084
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10478 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/il_printer.cc
+++ runtime/vm/il_printer.cc
@@ -91,16 +91,18 @@ void FlowGraphPrinter::PrintTypeCheck(const ParsedFunction& parsed_function,
                                       bool eliminated) {
     const Class& cls = Class::Handle(parsed_function.function().owner());
     const Script& script = Script::Handle(cls.script());
-    const char* static_type_name = "unknown";
+    const char* compile_type_name = "unknown";
     if (value != NULL) {
-      const AbstractType& type = AbstractType::Handle(value->StaticType());
-      static_type_name = String::Handle(type.UserVisibleName()).ToCString();
+      const AbstractType& type = AbstractType::Handle(value->CompileType());
+      if (!type.IsNull()) {
+        compile_type_name = String::Handle(type.UserVisibleName()).ToCString();
+      }
     }
     Parser::PrintMessage(script, token_pos, "",
-                         "%s type check: static type '%s' is %s specific than "
+                         "%s type check: compile type '%s' is %s specific than "
                          "type '%s' of '%s'.",
                          eliminated ? "Eliminated" : "Generated",
-                         static_type_name,
+                         compile_type_name,
                          eliminated ? "more" : "not more",
                          String::Handle(dst_type.UserVisibleName()).ToCString(),
                          dst_name.ToCString());
@@ -163,9 +165,10 @@ void ConstantVal::PrintTo(BufferFormatter* f) const {
 
 void AssertAssignableComp::PrintOperandsTo(BufferFormatter* f) const {
   value()->PrintTo(f);
-  f->Print(", %s, '%s'",
-            String::Handle(dst_type().Name()).ToCString(),
-            dst_name().ToCString());
+  f->Print(", %s, '%s'%s",
+           String::Handle(dst_type().Name()).ToCString(),
+           dst_name().ToCString(),
+           IsEliminated() ? " eliminated" : "");
   f->Print(" instantiator(");
   instantiator()->PrintTo(f);
   f->Print(")");
