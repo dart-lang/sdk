6a7b1733d662437eb9147aab30146e552dd4081e
srdjan@google.com <srdjan@google.com>
2012-08-10 20:48:18 +0000

    Enable SSA by default for IA32 as well. Review URL: https://chromiumcodereview.appspot.com//10825301
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10529 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/compiler.cc
+++ runtime/vm/compiler.cc
@@ -33,11 +33,7 @@ DEFINE_FLAG(bool, disassemble, false, "Disassemble dart code.");
 DEFINE_FLAG(bool, disassemble_optimized, false, "Disassemble optimized code.");
 DEFINE_FLAG(bool, trace_bailout, false, "Print bailout from ssa compiler.");
 DEFINE_FLAG(bool, trace_compiler, false, "Trace compiler operations.");
-#if defined(TARGET_ARCH_X64)
 DEFINE_FLAG(bool, use_ssa, true, "Use SSA form");
-#else
-DEFINE_FLAG(bool, use_ssa, false, "Use SSA form");
-#endif
 DEFINE_FLAG(int, deoptimization_counter_threshold, 5,
     "How many times we allow deoptimization before we disallow"
     " certain optimizations");
