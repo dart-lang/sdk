9a34c8f5bd58a9f5362569f49e59f526cce679d4
brianwilkerson@google.com <brianwilkerson@google.com>
2012-08-13 20:20:10 +0000

    (TBR) Fix the build Review URL: https://chromiumcodereview.appspot.com//10855130
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10601 260f80e4-7a28-3924-810f-c04153c831b5

--- compiler/javatests/com/google/dart/compiler/common/LibrarySourceFileTest.dart
+++ compiler/javatests/com/google/dart/compiler/common/LibrarySourceFileTest.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library "LibrarySourceFileTest";
+library LibrarySourceFileTest;
 part "OneSourceFile.dart";
 part "Source2.dart";
 part "subdir/Source3.dart";
