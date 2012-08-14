5f0160badcd25eabb8fef303948e7c5dc85ec1b3
ricow@google.com <ricow@google.com>
2012-08-14 10:59:29 +0000

    Add python script for clobbering the output directory of a build on any platform given the mode and architecture. Review URL: https://chromiumcodereview.appspot.com//10830302
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10629 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tools/clean_output_directory.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+#
+
+import optparse
+import shutil
+import sys
+import utils
+
+HOST_OS = utils.GuessOS()
+
+def BuildOptions():
+  result = optparse.OptionParser()
+  result.add_option("-m", "--mode",
+      help='Build variants (comma-separated).',
+      metavar='[all,debug,release]',
+      default='debug')
+  result.add_option("--arch",
+      help='Target architectures (comma-separated).',
+      metavar='[all,ia32,x64,simarm,arm]',
+      default=utils.GuessArchitecture())
+  return result
+
+def ProcessOptions(options):
+  if options.arch == 'all':
+    options.arch = 'ia32,x64'
+  if options.mode == 'all':
+    options.mode = 'release,debug'
+  options.mode = options.mode.split(',')
+  options.arch = options.arch.split(',')
+  for mode in options.mode:
+    if not mode in ['debug', 'release']:
+      print "Unknown mode %s" % mode
+      return False
+  for arch in options.arch:
+    if not arch in ['ia32', 'x64', 'simarm', 'arm']:
+      print "Unknown arch %s" % arch
+      return False
+  return True
+
+def Main():
+  parser = BuildOptions()
+  (options, args) = parser.parse_args()
+  if not ProcessOptions(options):
+    parser.print_help()
+    return 1
+
+  # Delete the output for the targets for each requested configuration.
+  for mode in options.mode:
+    for arch in options.arch:
+      build_root = utils.GetBuildRoot(HOST_OS, mode=mode, arch=arch)
+      print "Deleting %s" % (build_root)
+      shutil.rmtree(build_root, ignore_errors=True)
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(Main())
