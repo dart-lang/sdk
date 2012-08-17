dart-lang/sdk

commit 404f888f832cc6219cffb43127a0ac70d6b922ac

Refactor Library/CompilationUnit and how we define local Scope.
This also collapses members and constructors of a class into the same
map.

BUG=

Original URL: https://chromiumcodereview.appspot.com//10832203

Review URL: https://chromiumcodereview.appspot.com//10830303

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10632 260f80e4-7a28-3924-810f-c04153c831b5

lib/compiler/implementation/elements/elements.dart
@@ -4,6 +4,8 @@
 
 #library('elements');
 
+#import('dart:uri');
+
 #import('../tree/tree.dart');
 #import('../scanner/scannerlib.dart');
 #import('../leg.dart');  // TODO(karlklose): we only need type.
@@ -202,11 +204,8 @@ class Element implements Hashable {
   }
 
   CompilationUnitElement getCompilationUnit() {
-    Element element = this;
-    while (element !== null && !element.isCompilationUnit()) {
-      element = element.enclosingElement;
-    }
-    return element.asCompilationUnit();
+    if (isCompilationUnit()) return this;
+    return enclosingElement.getCompilationUnit();
   }
 
   LibraryElement getLibrary() {
@@ -278,10 +277,44 @@ class Element implements Hashable {
 }
 
 class ContainerElement extends Element {
-  ContainerElement(name, kind, enclosingElement) :
-    super(name, kind, enclosingElement);
+  Link<Element> localMembers = const EmptyLink<Element>();
 
-  abstract void addMember(Element element, DiagnosticListener listener);
+  ContainerElement(name, kind, enclosingElement)
+    : super(name, kind, enclosingElement);
+
+  void addMember(Element element, DiagnosticListener listener) {
+    localMembers = localMembers.prepend(element);
+  }
+}
+
+class ScopeContainerElement extends ContainerElement {
+  final Map<SourceString, Element> localScope;
+
+  ScopeContainerElement(name, kind, enclosingElement)
+    : super(name, kind, enclosingElement),
+      localScope = new Map<SourceString, Element>();
+
+  void addMember(Element element, DiagnosticListener listener) {
+    super.addMember(element, listener);
+    addToScope(element, listener);
+  }
+
+  void addToScope(Element element, DiagnosticListener listener) {
+    if (element.isAccessor()) {
+      addGetterOrSetter(element, localScope[element.name], listener);
+    } else {
+      Element existing = localScope.putIfAbsent(element.name, () => element);
+      if (existing !== element) {
+        // TODO(ahe): Do something similar to Resolver.reportErrorWithContext.
+        listener.cancel('duplicate definition', token: element.position());
+        listener.cancel('existing definition', token: existing.position());
+      }
+    }
+  }
+
+  Element localLookup(SourceString elementName) {
+    return localScope[elementName];
+  }
 
   void addGetterOrSetter(Element element,
                          Element existing,
@@ -324,7 +357,6 @@ class ContainerElement extends Element {
 
 class CompilationUnitElement extends ContainerElement {
   final Script script;
-  Link<Element> topLevelElements = const EmptyLink<Element>();
 
   CompilationUnitElement(Script script, Element enclosing)
     : this.script = script,
@@ -332,25 +364,11 @@ class CompilationUnitElement extends ContainerElement {
             ElementKind.COMPILATION_UNIT,
             enclosing);
 
-  CompilationUnitElement.library(Script script)
-    : this.script = script,
-      super(new SourceString(script.name), ElementKind.LIBRARY, null);
-
-  CompilationUnitElement asCompilationUnit() => this;
-
   void addMember(Element element, DiagnosticListener listener) {
-    LibraryElement library = enclosingElement;
-    library.addMember(element, listener);
-    topLevelElements = topLevelElements.prepend(element);
-  }
-
-  void define(Element element, DiagnosticListener listener) {
-    LibraryElement library = enclosingElement;
-    library.define(element, listener);
-  }
-
-  void addTag(ScriptTag tag, DiagnosticListener listener) {
-    listener.cancel("script tags not allowed here", node: tag);
+    // Keep a list of top level members.
+    super.addMember(element, listener);
+    // Provide the member to the library to build scope.
+    getLibrary().addMember(element, listener);
   }
 }
 
@@ -367,21 +385,22 @@ class CompilationUnitOverrideElement extends Element {
   CompilationUnitElement asCompilationUnit() => compilationUnit;
 }
 
-class LibraryElement extends CompilationUnitElement {
-  // TODO(ahe): Library element should not be a subclass of
-  // CompilationUnitElement.
-
+class LibraryElement extends ScopeContainerElement {
+  CompilationUnitElement entryCompilationUnit;
   Link<CompilationUnitElement> compilationUnits =
-    const EmptyLink<CompilationUnitElement>();
+      const EmptyLink<CompilationUnitElement>();
+
   Link<ScriptTag> tags = const EmptyLink<ScriptTag>();
   ScriptTag libraryTag;
-  Map<SourceString, Element> elements;
   bool canUseNative = false;
   LibraryElement patch = null;
 
   LibraryElement(Script script)
-      : elements = new Map<SourceString, Element>(),
-        super.library(script);
+    : super(new SourceString(script.name), ElementKind.LIBRARY, null) {
+    entryCompilationUnit = new CompilationUnitElement(script, this);
+  }
+
+  Uri get uri() => entryCompilationUnit.script.uri;
 
   bool get isPatched() => patch !== null;
 
@@ -393,46 +412,28 @@ class LibraryElement extends CompilationUnitElement {
     tags = tags.prepend(tag);
   }
 
-  void addMember(Element element, DiagnosticListener listener) {
-    topLevelElements = topLevelElements.prepend(element);
-    define(element, listener);
-  }
-
-  void define(Element element, DiagnosticListener listener) {
-    if (element.kind == ElementKind.GETTER
-        || element.kind == ElementKind.SETTER) {
-      addGetterOrSetter(element, elements[element.name], listener);
-    } else {
-      Element existing = elements.putIfAbsent(element.name, () => element);
-      if (existing !== element) {
-        // TODO(ahe): Do something similar to Resolver.reportErrorWithContext.
-        listener.cancel('duplicate definition', token: element.position());
-        listener.cancel('existing definition', token: existing.position());
-      }
-    }
-  }
-
   /** Look up a top-level element in this library. The element could
     * potentially have been imported from another library. Returns
     * null if no such element exist. */
   Element find(SourceString elementName) {
-    return elements[elementName];
+    return localScope[elementName];
   }
 
   /** Look up a top-level element in this library, but only look for
     * non-imported elements. Returns null if no such element exist. */
   Element findLocal(SourceString elementName) {
-    Element result = elements[elementName];
+    Element result = localScope[elementName];
     if (result === null || result.getLibrary() != this) return null;
     return result;
   }
 
   void forEachExport(f(Element element)) {
-    elements.forEach((SourceString _, Element e) {
+    localScope.forEach((_, Element e) {
       if (this === e.getLibrary()
           && e.kind !== ElementKind.PREFIX
-          && e.kind !== ElementKind.FOREIGN) {
-        if (!e.name.isPrivate()) f(e);
+          && e.kind !== ElementKind.FOREIGN
+          && !e.name.isPrivate()) {
+        f(e);
       }
     });
   }
@@ -449,10 +450,12 @@ class LibraryElement extends CompilationUnitElement {
       return libraryTag.argument.dartString.slowToString();
     } else {
       // Use the file name as script name.
-      var path = script.uri.path;
+      String path = uri.path;
       return path.substring(path.lastIndexOf('/') + 1);
     }
   }
+
+  CompilationUnitElement getCompilationUnit() => entryCompilationUnit;
 
   Scope buildEnclosingScope() => new TopScope(this);
 }
@@ -693,7 +696,8 @@ class AbstractFieldElement extends Element {
     //
     // We need to make sure that the position returned is relative to
     // the compilation unit of the abstract element.
-    if (getter !== null && getter.enclosingElement === enclosingElement) {
+    if (getter !== null 
+        && getter.getCompilationUnit() === getCompilationUnit()) {
       return getter.position();
     } else {
       return setter.position();
@@ -980,7 +984,7 @@ abstract class TypeDeclarationElement implements Element {
   }
 }
 
-class ClassElement extends ContainerElement
+class ClassElement extends ScopeContainerElement
     implements TypeDeclarationElement {
   static final int STATE_NOT_STARTED = 0;
   static final int STATE_STARTED = 1;
@@ -990,9 +994,6 @@ class ClassElement extends ContainerElement
   InterfaceType type;
   Type supertype;
   Type defaultClass;
-  Link<Element> members = const EmptyLink<Element>();
-  Map<SourceString, Element> localMembers;
-  Map<SourceString, Element> constructors;
   Link<Type> interfaces;
   SourceString nativeName;
 
@@ -1019,27 +1020,7 @@ class ClassElement extends ContainerElement
   Link<Type> allSupertypes;
 
   ClassElement(SourceString name, Element enclosing, this.id)
-    : localMembers = new Map<SourceString, Element>(),
-      constructors = new Map<SourceString, Element>(),
-      super(name, ElementKind.CLASS, enclosing);
-
-  void addMember(Element element, DiagnosticListener listener) {
-    members = members.prepend(element);
-    if (element.kind == ElementKind.GENERATIVE_CONSTRUCTOR ||
-        element.modifiers.isFactory()) {
-      constructors[element.name] = element;
-    } else if (element.kind == ElementKind.GETTER
-               || element.kind == ElementKind.SETTER) {
-      addGetterOrSetter(element, localMembers[element.name], listener);
-    } else {
-      Element existing = localMembers.putIfAbsent(element.name, () => element);
-      if (existing !== element) {
-        // TODO(ahe): Do something similar to Resolver.reportErrorWithContext.
-        listener.cancel('duplicate definition', token: element.position());
-        listener.cancel('existing definition', token: existing.position());
-      }
-    }
-  }
+    : super(name, ElementKind.CLASS, enclosing);
 
   InterfaceType computeType(compiler) {
     if (type == null) {
@@ -1060,10 +1041,18 @@ class ClassElement extends ContainerElement
     return this;
   }
 
+  /**
+   * Lookup local members in the class. This will ignore constructors.
+   */
   Element lookupLocalMember(SourceString memberName) {
-    return localMembers[memberName];
+    var result = localLookup(memberName);
+    if (result !== null && result.isConstructor()) return null;
+    return result;
   }
 
+  /**
+  * Lookup super members for the class. This will ignore constructors.
+   */
   Element lookupSuperMember(SourceString memberName) {
     bool isPrivate = memberName.isPrivate();
     for (ClassElement s = superclass; s != null; s = s.superclass) {
@@ -1085,7 +1074,7 @@ class ClassElement extends ContainerElement
    * rules, where library scope comes before superclass scope.
    */
   Element lookupMember(SourceString memberName) {
-    Element localMember = localMembers[memberName];
+    Element localMember = lookupLocalMember(memberName);
     return localMember === null ? lookupSuperMember(memberName) : localMember;
   }
 
@@ -1130,9 +1119,26 @@ class ClassElement extends ContainerElement
     } else {
       normalizedName = className;
     }
-    Element result = constructors[normalizedName];
-    if (result === null && noMatch !== null) {
-      result = noMatch(lookupLocalMember(constructorName));
+    Element result = localLookup(normalizedName);
+    if (result === null || !result.isConstructor()) {
+      result = noMatch !== null ? noMatch(result) : null;
+    }
+    return result;
+  }
+
+  bool get hasConstructor() {
+    // Search in scope to be sure we search patched constructors.
+    for (var element in localScope.getValues()) {
+      if (element.isConstructor()) return true;
+    }
+    return false;
+  }
+
+  Link<Element> get constructors() {
+    // TODO(ajohnsen): See if we can avoid this method at some point.
+    Link<Element> result = const EmptyLink<Element>();
+    for (Element member in localMembers) {
+      if (member.isConstructor()) result = result.prepend(member);
     }
     return result;
   }
@@ -1161,7 +1167,7 @@ class ClassElement extends ContainerElement
     do {
       if (seen.contains(classElement)) return;
       seen.add(classElement);
-      for (Element element in classElement.members) {
+      for (Element element in classElement.localMembers) {
         f(classElement, element);
       }
       if (includeBackendMembers) {

lib/compiler/implementation/js_backend/backend.dart
@@ -146,7 +146,8 @@ class JavaScriptBackend extends Backend {
       // If there are field setters but there is only constructor then the type
       // of the field is determined by the assignments in the constructor
       // body.
-      if (classElement.constructors.length == 1) {
+      var constructors = classElement.constructors;
+      if (constructors.head !== null && constructors.tail === null) {
         return fieldConstructorSetters[classElement][field];
       } else {
         return HType.UNKNOWN;

lib/compiler/implementation/compiler.dart
@@ -295,7 +295,7 @@ class Compiler implements DiagnosticListener {
       // take advantage of this as core and coreimpl import js_helper
       // and see Dynamic this way.
       withCurrentElement(dynamicClass, () {
-        library.define(dynamicClass, this);
+        library.addToScope(dynamicClass, this);
       });
     }
   }
@@ -374,19 +374,20 @@ class Compiler implements DiagnosticListener {
   }
 
   void applyLibraryPatch(LibraryElement original, LibraryElement patch) {
-    Link<Element> patches = patch.topLevelElements;
-    applyContainerPatch(original, patches, original.findLocal);
+    Link<Element> patches = patch.localMembers;
+    applyContainerPatch(original, patches);
 
     // Copy imports from patch to original library.
     Map<String, LibraryElement> delayedPatches = <LibraryElement>{};
-    Uri patchBase = patch.script.uri;
+    Uri patchBase = patch.uri;
     for (ScriptTag tag in patch.tags.reverse()) {
       if (tag.isImport()) {
         StringNode argument = tag.argument;
         Uri resolved = patchBase.resolve(argument.dartString.slowToString());
         LibraryElement importedLibrary =
             scanner.loadLibrary(resolved, argument);
-        scanner.importLibrary(original, importedLibrary, tag, patch);
+        scanner.importLibrary(original, importedLibrary, tag,
+                              patch.entryCompilationUnit);
         if (resolved.scheme == "dart") {
           delayedPatches[resolved.path] = importedLibrary;
         }
@@ -403,11 +404,11 @@ class Compiler implements DiagnosticListener {
     });
   }
 
-  void applyContainerPatch(ContainerElement original, Link<Element> patches,
-                           Element lookup(SourceString name)) {
+  void applyContainerPatch(ScopeContainerElement original,
+                           Link<Element> patches) {
     while (!patches.isEmpty()) {
       Element patchElement = patches.head;
-      Element originalElement = lookup(patchElement.name);
+      Element originalElement = original.localLookup(patchElement.name);
       if (patchElement.isAccessor()) {
         // TODO(lrn): When we change to always add accessors to members, and
         // not add abstract fields, the logic here should be reversed.
@@ -429,7 +430,7 @@ class Compiler implements DiagnosticListener {
                                        this);
             if (originalField === null && patchField.setter !== null) {
               // It exists now, so find it for the setter patching.
-              originalField = lookup(patchElement.name);
+              originalField = original.localLookup(patchElement.name);
             }
           } else {
             patchMember(originalField.getter, patchField.getter);
@@ -541,13 +542,8 @@ class Compiler implements DiagnosticListener {
     ClassNode node = original.parseNode(this);
     // Parse patch class with "patch" parser.
     ClassNode patchNode = patchParser.parsePatchClassNode(patch);
-    Link<Element> patches = patch.members;
-    Element lookupMemberOrConstructor(SourceString name) {
-      Element result = original.lookupLocalMember(name);
-      if (result !== null) return result;
-      return original.lookupConstructor(name);
-    }
-    applyContainerPatch(original, patches, lookupMemberOrConstructor);
+    Link<Element> patches = patch.localMembers;
+    applyContainerPatch(original, patches);
   }
 
   /**
@@ -558,17 +554,17 @@ class Compiler implements DiagnosticListener {
 
   /** Define the JS helper functions in the given library. */
   void addForeignFunctions(LibraryElement library) {
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('JS'), library), this);
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('UNINTERCEPTED'), library), this);
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('JS_HAS_EQUALS'), library), this);
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('JS_CURRENT_ISOLATE'), library), this);
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('JS_CALL_IN_ISOLATE'), library), this);
-    library.define(new ForeignElement(
+    library.addToScope(new ForeignElement(
         const SourceString('DART_CLOSURE_TO_JS'), library), this);
   }
 

lib/compiler/implementation/enqueue.dart
@@ -140,7 +140,7 @@ class Enqueuer {
   }
 
   void processInstantiatedClass(ClassElement cls) {
-    cls.members.forEach(processInstantiatedClassMember);
+    cls.localMembers.forEach(processInstantiatedClassMember);
   }
 
   void registerFieldClosureInvocations() {
@@ -218,7 +218,7 @@ class Enqueuer {
         if (seenClasses.contains(cls)) return;
         seenClasses.add(cls);
         cls.ensureResolved(compiler);
-        cls.members.forEach(processInstantiatedClassMember);
+        cls.localMembers.forEach(processInstantiatedClassMember);
         cls = cls.superclass;
       }
     });

lib/compiler/implementation/js_backend/native_emitter.dart
@@ -112,7 +112,7 @@ function(cls, fields, methods) {
       nativeBuffer.add("$className.$name = $value;\n");
     }
 
-    for (Element member in classElement.members) {
+    for (Element member in classElement.localMembers) {
       if (member.isInstanceMember()) {
         emitter.addInstanceMember(member, defineInstanceMember);
       }

lib/compiler/implementation/native_handler.dart
@@ -40,7 +40,7 @@ void processNativeClassesInLibrary(Enqueuer world,
                                    LibraryElement library) {
   bool hasNativeClass = false;
   final compiler = emitter.compiler;
-  for (Link<Element> link = library.topLevelElements;
+  for (Link<Element> link = library.localMembers;
        !link.isEmpty(); link = link.tail) {
     Element element = link.head;
     if (element.kind == ElementKind.CLASS) {
@@ -77,14 +77,15 @@ void maybeEnableNative(Compiler compiler,
                        LibraryElement library,
                        Uri uri) {
   String libraryName = uri.toString();
-  if (library.script.name.contains('dart/tests/compiler/dart2js_native')
+  if (library.entryCompilationUnit.script.name.contains(
+          'dart/tests/compiler/dart2js_native')
       || libraryName == 'dart:dom_deprecated'
       || libraryName == 'dart:isolate'
       || libraryName == 'dart:html') {
     library.canUseNative = true;
-    library.define(compiler.findHelper(const SourceString('JS')), compiler);
+    library.addToScope(compiler.findHelper(const SourceString('JS')), compiler);
     if (compiler.jsIndexingBehaviorInterface !== null) {
-      library.define(compiler.jsIndexingBehaviorInterface, compiler);
+      library.addToScope(compiler.jsIndexingBehaviorInterface, compiler);
     }
   }
 }

lib/compiler/implementation/patch_parser.dart
@@ -39,11 +39,13 @@ class PatchParserTask extends leg.CompilerTask {
   void scanLibraryElements(LibraryElement library) {
     measure(() {
       // TODO(lrn): Possibly recursively handle #source directives in patch.
-      leg.Script script = library.script;
+      leg.Script script = library.entryCompilationUnit.script;
       Token tokens = new StringScanner(script.text).tokenize();
       Function idGenerator = compiler.getNextFreeClassId;
       PatchListener patchListener =
-          new PatchElementListener(compiler, library, idGenerator);
+          new PatchElementListener(compiler,
+                                   library.entryCompilationUnit,
+                                   idGenerator);
       new PatchParser(patchListener).parseUnit(tokens);
     });
   }

lib/compiler/implementation/resolver.dart
@@ -1905,10 +1905,10 @@ class ClassResolverVisitor extends TypeDefinitionVisitor {
    * constructors.
    */
   void addDefaultConstructorIfNeeded(ClassElement element) {
-    if (element.constructors.length != 0) return;
+    if (element.hasConstructor) return;
     SynthesizedConstructorElement constructor =
       new SynthesizedConstructorElement(element);
-    element.constructors[element.name] = constructor;
+    element.addToScope(constructor, compiler);
     Type returnType = compiler.types.voidType;
     constructor.type = new FunctionType(returnType, const EmptyLink<Type>(),
                                         constructor);

commit 3109b142a33e24050ac563ad9d918c599702920b

Fix a performance regressions introduced by r10632.
This gives the compiler the exact same behaviour as before.

BUG=

Review URL: https://chromiumcodereview.appspot.com//10827353

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10891 260f80e4-7a28-3924-810f-c04153c831b5

lib/compiler/implementation/elements/elements.dart
@@ -852,8 +852,7 @@ class FunctionElement extends Element {
 
   bool isInstanceMember() {
     return isMember()
-           && kind != ElementKind.GENERATIVE_CONSTRUCTOR
-           && !modifiers.isFactory()
+           && !isConstructor()
            && !modifiers.isStatic();
   }
 

lib/compiler/implementation/js_backend/backend.dart
@@ -238,7 +238,7 @@ class JavaScriptBackend extends Backend {
       // of the field is determined by the assignments in the constructor
       // body.
       var constructors = classElement.constructors;
-      if (constructors.head !== null && constructors.tail === null) {
+      if (constructors.head !== null && constructors.tail.isEmpty()) {
         return fieldConstructorSetters[classElement][field];
       } else {
         return HType.UNKNOWN;