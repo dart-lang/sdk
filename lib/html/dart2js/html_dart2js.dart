5f8ff75236feb072cd117c89b4f0b96c14cd7da1
antonm@google.com <antonm@google.com>
2012-08-20 13:43:23 +0000

    Rebuild dart:html.
    TBR=sra@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//10836338
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@10961 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/html/dart2js/html_dart2js.dart
+++ lib/html/dart2js/html_dart2js.dart
@@ -37703,33 +37703,6 @@ class _IDBKeyRangeFactoryProvider {
       '''return cls.bound(lower, upper, lowerOpen, upperOpen);''';
 
 }
-// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-// IDBOpenDBRequest.  IDBFactory.open returns a plain IDBRequest on Chrome and
-// Dartium but an IDBOpenDBRequest on Firefox.  Chrome/Dartium is expected to
-// change at some point as IDBOpenDBRequest is more standard.  When it appears
-// in the WebKit IDL we can remove this hand-written file.
-//
-// See:
-// http://www.w3.org/TR/IndexedDB/#request-api
-// http://dvcs.w3.org/hg/IndexedDB/raw-file/tip/Overview.html#request-api
-
-class _IDBOpenDBRequestImpl extends _IDBRequestImpl implements IDBOpenDBRequest
-    native "*IDBOpenDBRequest" {
-
-  _IDBOpenDBRequestEventsImpl get on() =>
-    new _IDBOpenDBRequestEventsImpl(this);
-}
-
-class _IDBOpenDBRequestEventsImpl extends _IDBRequestEventsImpl implements IDBOpenDBRequestEvents {
-  _IDBOpenDBRequestEventsImpl(_ptr) : super(_ptr);
-
-  EventListenerList get blocked() => _get('blocked');
-
-  EventListenerList get upgradeneeded() => _get('upgradeneeded');
-}
 // Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
