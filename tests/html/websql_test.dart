4355e96fb4be3a5296e40ff8825aac90dc8b46a6
blois@google.com <blois@google.com>
2012-08-23 22:57:15 +0000

    Fixing up some APIs to allow Web DB to work from dart and adding tests.
    Basically the SQLTransaction.executeSQL was being ommitted because the compiler didn't recognize the ObjectArray type. Adding this in makes this work in dart2js sscenarios (where the browser supports it).
    
    I've opened bug #4645 that the APIs need to be implemented in Dartium.
    
    BUG=1888
    
    Review URL: https://chromiumcodereview.appspot.com//10876002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@11272 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/html/websql_test.dart
@@ -0,0 +1,116 @@
+#library('WebDBTest');
+#import('../../pkg/unittest/unittest.dart');
+#import('../../pkg/unittest/html_config.dart');
+#import('dart:html');
+
+void fail(message) {
+  guardAsync(() {
+      Expect.fail(message);
+    });
+}
+
+Future<SQLTransaction> createTransaction(Database db) {
+  final completer = new Completer<SQLTransaction>();
+
+  db.transaction((SQLTransaction transaction) {
+    completer.complete(transaction);
+  });
+
+  return completer.future;
+}
+
+createTable(tableName, columnName) => (SQLTransaction transaction) {
+  final completer = new Completer<SQLTransaction>();
+
+  final sql = 'CREATE TABLE $tableName ($columnName)';
+  transaction.executeSql(sql, [],
+    (SQLTransaction tx, SQLResultSet rs) {
+      completer.complete(transaction);
+    },
+    (SQLTransaction tx, SQLError error) {
+      fail(error.message);
+    });
+
+  return completer.future;
+};
+
+insert(tableName, columnName, value) => (SQLTransaction transaction) {
+  final completer = new Completer<SQLTransaction>();
+
+  final sql = 'INSERT INTO $tableName ($columnName) VALUES (?)';
+  transaction.executeSql(sql, [value],
+    (SQLTransaction tx, SQLResultSet rs) {
+      completer.complete(tx);
+    },
+    (SQLTransaction tx, SQLError error) {
+      fail(error.message);
+    });
+
+  return completer.future;
+};
+
+queryTable(tableName, callback) => (SQLTransaction transaction) {
+  final completer = new Completer<SQLTransaction>();
+
+  final sql = 'SELECT * FROM $tableName';
+  transaction.executeSql(sql, [],
+    (SQLTransaction tx, SQLResultSet rs) {
+      callback(rs);
+      completer.complete(tx);
+    },
+    (SQLTransaction tx, SQLError error) {
+      fail(error.message);
+    });
+
+  return completer.future;
+};
+
+dropTable(tableName, [bool ignoreFailure = false]) =>
+    (SQLTransaction transaction) {
+  final completer = new Completer<SQLTransaction>();
+
+  final sql = 'DROP TABLE $tableName';
+  transaction.executeSql(sql, [],
+    (SQLTransaction tx, SQLResultSet rs) {
+      completer.complete(tx);
+    },
+    (SQLTransaction tx, SQLError error) {
+      if (ignoreFailure) {
+        completer.complete(tx);
+      } else {
+        fail(error.message);
+      }
+    });
+
+  return completer.future;
+};
+
+main() {
+  useHtmlConfiguration();
+
+  test('Web Database', () {
+    final tableName = 'test_table';
+    final columnName = 'test_data';
+
+    final db = window.openDatabase('test_db', '1.0', 'test_db', 1024 * 1024);
+
+    Expect.isNotNull(db, 'Unable to open database');
+
+    createTransaction(db)
+      // Attempt to clear out any tables which may be lurking from previous
+      // runs.
+      .chain(dropTable(tableName, true))
+      .chain(createTable(tableName, columnName))
+      .chain(insert(tableName, columnName, 'Some text data'))
+      .chain(queryTable(tableName, (resultSet) {
+        guardAsync(() {
+          Expect.equals(1, resultSet.rows.length);
+
+          // Should validate the rowData contents- need to be able to map the JS
+          // object back to regular types.
+        });
+      }))
+      .chain(dropTable(tableName))
+      .then(expectAsync1((tx) {}));
+  });
+}
