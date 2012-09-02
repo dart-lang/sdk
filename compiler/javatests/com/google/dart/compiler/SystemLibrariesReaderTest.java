fff9065cacb9a740d8c93bc38f767e7ed32f5779
danrubel@google.com <danrubel@google.com>
2012-09-02 13:19:45 +0000

    improve test failure message Review URL: https://chromiumcodereview.appspot.com//10918040
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@11744 260f80e4-7a28-3924-810f-c04153c831b5

--- compiler/javatests/com/google/dart/compiler/SystemLibrariesReaderTest.java
+++ compiler/javatests/com/google/dart/compiler/SystemLibrariesReaderTest.java
@@ -32,11 +32,14 @@ public class SystemLibrariesReaderTest extends TestCase {
     Map<String, DartLibrary> librariesMap = reader.getLibrariesMap();
     assertTrue(!librariesMap.isEmpty());
     for (Entry<String, DartLibrary> entry : librariesMap.entrySet()) {
-      String path = entry.getValue().getPath(); 
+      String path = entry.getValue().getPath();
       File file = new File(base.resolve(new URI(null, null, path, null, null)).normalize());
-      assertTrue(file.exists());
-   }
-   
+      if (!file.exists()) {
+        fail("Expected " + entry.getKey() + " path in libraries.dart to exist in SDK"
+            + "\n  could not find " + file);
+      }
+    }
+
     // check content
     //   "coreimpl": const LibraryInfo(
     //       "coreimpl/coreimpl_runtime.dart",
