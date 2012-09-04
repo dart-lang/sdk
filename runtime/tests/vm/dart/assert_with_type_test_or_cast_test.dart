1713727944c8aef81a67c2091eb9f971b3dbaa8d
tball@google.com <tball@google.com>
2012-09-04 22:10:57 +0000

    Fixed test in checked mode. Review URL: https://chromiumcodereview.appspot.com//10918061
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@11846 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/tests/vm/dart/assert_with_type_test_or_cast_test.dart
+++ runtime/tests/vm/dart/assert_with_type_test_or_cast_test.dart
@@ -11,17 +11,17 @@
 // The cause was incomplete generic type skipping, so each of the assert
 // statements below would fail.
 main() {
- var names = new List<String>();
+ var names = new List<int>();
  
  // Generic type test.
- assert(names is List<String>);
+ assert(names is List<int>);
  
  // Negated generic type test.
- assert(names is !List<int>);
+ assert(names is !List<String>);
  
  // Generic type cast.
- assert((names as List<int>).length == 0);
+ assert((names as List<num>).length == 0);
  
  // Generic type test inside expression.
- assert((names is List<String>));
+ assert((names is List<int>));
 }
