dart-lang/sdk

commit c805e88ea6f28460342596bcfc66e3b509d5beb0

Make final fields GVN-able.
Fixes issue 4002.

Review URL: https://chromiumcodereview.appspot.com//10916083

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@11797 260f80e4-7a28-3924-810f-c04153c831b5

lib/compiler/implementation/ssa/nodes.dart
@@ -1400,11 +1400,11 @@ abstract class HFieldAccess extends HInstruction {
 }
 
 class HFieldGet extends HFieldAccess {
-  final bool isFinalOrConst;
+  final bool isAssignable;
 
-  HFieldGet(Element element, HInstruction receiver,
-            [this.isFinalOrConst = false])
-      : super(element, <HInstruction>[receiver]);
+  HFieldGet(Element element, HInstruction receiver)
+      : this.isAssignable = (element == null || element.isAssignable()),
+        super(element, <HInstruction>[receiver]);
 
   HInstruction get receiver => inputs[0];
 
@@ -1413,7 +1413,7 @@ class HFieldGet extends HFieldAccess {
   void prepareGvn(HTypeMap types) {
     setUseGvn();
     clearAllSideEffects();
-    if (!isFinalOrConst) setDependsOnSomething();
+    if (isAssignable) setDependsOnSomething();
   }
 
   int typeCode() => HInstruction.FIELD_GET_TYPECODE;

lib/compiler/implementation/ssa/optimize.dart
@@ -610,8 +610,7 @@ class SsaConstantFolder extends HBaseVisitor implements OptimizationPhase {
           break;
       }
     }
-    return new HFieldGet(
-        field, node.inputs[0], isFinalOrConst: isFinalOrConst);
+    return new HFieldGet(field, node.inputs[0]);
   }
 
   HInstruction visitInvokeDynamicSetter(HInvokeDynamicSetter node) {

lib/compiler/implementation/ssa/tracer.dart
@@ -267,7 +267,7 @@ class HInstructionStringifier implements HVisitor<String> {
 
   String visitFieldGet(HFieldGet node) {
     String fieldName = node.element.name.slowToString();
-    return 'get ${temporaryId(node.receiver)}.$fieldName}';
+    return 'get ${temporaryId(node.receiver)}.$fieldName';
   }
 
   String visitFieldSet(HFieldSet node) {

commit 62491680cc0307773cd9e8190b0af0736df32766

Fix performance regression introduced in r11797.
R=floitsch@google.com
BUG=

Review URL: https://chromiumcodereview.appspot.com//10915118

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@11935 260f80e4-7a28-3924-810f-c04153c831b5

lib/compiler/implementation/ssa/nodes.dart
@@ -1407,8 +1407,10 @@ abstract class HFieldAccess extends HInstruction {
 class HFieldGet extends HFieldAccess {
   final bool isAssignable;
 
-  HFieldGet(Element element, HInstruction receiver)
-      : this.isAssignable = (element == null || element.isAssignable()),
+  HFieldGet(Element element, HInstruction receiver, [bool isAssignable])
+      : this.isAssignable = (isAssignable !== null)
+            ? isAssignable
+            : element.isAssignable(),
         super(element, <HInstruction>[receiver]);
 
   HInstruction get receiver => inputs[0];

lib/compiler/implementation/ssa/optimize.dart
@@ -620,7 +620,7 @@ class SsaConstantFolder extends HBaseVisitor implements OptimizationPhase {
           break;
       }
     }
-    return new HFieldGet(field, node.inputs[0]);
+    return new HFieldGet(field, node.inputs[0], isAssignable: !isFinalOrConst);
   }
 
   HInstruction visitInvokeDynamicSetter(HInvokeDynamicSetter node) {