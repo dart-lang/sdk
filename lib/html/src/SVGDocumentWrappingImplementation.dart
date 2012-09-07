136809aeda91eb23cd0c9b82fd4796623ce5dd89
podivilov@google.com <podivilov@google.com>
2012-09-07 13:13:52 +0000

    Remove stale files from lib/html/src.
    R=antonm@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//10910129
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12032 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/html/src/SVGDocumentWrappingImplementation.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-// WARNING: Do not edit - generated code.
-
-class SVGDocumentWrappingImplementation extends DocumentWrappingImplementation implements SVGDocument {
-  SVGDocumentWrappingImplementation._wrap(dom.SVGDocument ptr) : super._wrap(ptr, ptr.rootElement);
-}
