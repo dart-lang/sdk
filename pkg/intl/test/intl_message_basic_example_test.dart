2748b083f035133f4239b1d65f1cd505a3d7c41a
alanknight@google.com <alanknight@google.com>
2012-09-13 19:58:16 +0000

    Add test that uses the basic example code Review URL: https://codereview.chromium.org//10914272
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12349 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ pkg/intl/test/intl_message_basic_example_test.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/**
+ * Tests internationalization of messages using the basic example as a template.
+ */
+#library('intl_message_test_2');
+
+#import('../date_format.dart');
+#import('../date_symbol_data_local.dart');
+#import('../intl.dart');
+#import('../message_lookup_local.dart');
+#import('../example/basic/basic_example.dart');
+#import('../../unittest/unittest.dart');
+
+List list;
+
+main() {
+  list = [];
+  setup(runAllTests, addToList);
+}
+
+addToList(x) {list.add(x);}
+
+runAllTests(_) {
+  setup(runProgram, addToList);
+  test('Verify basic example printing localized messages', () {
+  expect(list[0], "Ran at 00:00:00 on Thursday, January 1, 1970");
+  expect(list[1], "Ausgedruckt am 00:00:00 am Donnerstag, 1. Januar 1970.");
+  expect(list[2], "วิ่ง 0:00:00 on วันพฤหัสบดี 1 มกราคม 1970.");
+  });
+}
