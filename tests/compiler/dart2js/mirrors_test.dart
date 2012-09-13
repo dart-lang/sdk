528e201672b8ad82c6d14ec9581b7d8041c219c7
rnystrom@google.com <rnystrom@google.com>
2012-09-13 18:08:37 +0000

    Fix mirrors_test after dartdoc file moves.
    Review URL: https://codereview.chromium.org//10918222
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12344 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js/mirrors_test.dart
+++ tests/compiler/dart2js/mirrors_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#import("../../../pkg/dartdoc/mirrors/mirrors.dart");
-#import("../../../pkg/dartdoc/mirrors/mirrors_util.dart");
+#import("../../../pkg/dartdoc/lib/mirrors.dart");
+#import("../../../pkg/dartdoc/lib/mirrors_util.dart");
 
 #import('dart:io');
 
