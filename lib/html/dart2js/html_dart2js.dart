9b250cab27e1972400916c1b39b635347ba3acc6
blois@google.com <blois@google.com>
2012-09-18 20:09:50 +0000

    Revert "Changing return value of request animation frame."
    This reverts commit 0df59d6575544308571b7c9c57d8e6f30aa3078b.
    
    BUG=
    
    Review URL: https://codereview.chromium.org//10950012
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12509 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/html/dart2js/html_dart2js.dart
+++ lib/html/dart2js/html_dart2js.dart
@@ -22481,7 +22481,7 @@ class _RectImpl implements Rect native "*Rect" {
 
 // WARNING: Do not edit - generated code.
 
-typedef void RequestAnimationFrameCallback(int time);
+typedef bool RequestAnimationFrameCallback(int time);
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
@@ -35925,9 +35925,7 @@ class _WindowImpl extends _EventTargetImpl implements Window native "@*DOMWindow
    }
    if (this.requestAnimationFrame && this.cancelAnimationFrame) return;
    this.requestAnimationFrame = function(callback) {
-      return window.setTimeout(function() {
-        callback(Date.now());
-      }, 16 /* 16ms ~= 60fps */);
+       return window.setTimeout(callback, 16 /* 16ms ~= 60fps */);
    };
    this.cancelAnimationFrame = function(id) { clearTimeout(id); }
 ''';
--- lib/html/dartium/html_dartium.dart
+++ lib/html/dartium/html_dartium.dart
@@ -24738,7 +24738,7 @@ class _RectImpl extends NativeFieldWrapperClass1 implements Rect {
 
 // WARNING: Do not edit - generated code.
 
-typedef void RequestAnimationFrameCallback(int time);
+typedef bool RequestAnimationFrameCallback(int time);
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
