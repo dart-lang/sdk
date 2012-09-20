13e6976997eb128e057c3d5054bfd9fa03d229c7
ricow@google.com <ricow@google.com>
2012-09-20 11:11:50 +0000

    Fix isnot_malformed_type_test by putting the multitest comment on the right line. Review URL: https://codereview.chromium.org//10966006
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12629 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/isnot_malformed_type_test.dart
+++ tests/language/isnot_malformed_type_test.dart
@@ -1,12 +1,13 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 // Parser throws an error when parsing x is! T with malformed type T.
 
 f(obj) {
-  return (obj is !Baz);  // 'Baz' is not loaded error.
+  // 'Baz' is not loaded error.
+  return (obj is !Baz);  /// 01: runtime error
 }
 
 main () {
-  f(null);  /// 01: runtime error
+  f(null);  
 }
