5d53ea6166d706da994532fa3134a52ec7e9d508
antonm@google.com <antonm@google.com>
2012-09-25 19:07:47 +0000

    Temporarily expose createObjectUrl/revokeObjectUrl on window.
    TBR=blois@google.com,podivilov@chromium.org,sra@google.com
    
    Review URL: https://codereview.chromium.org//10989022
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12871 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/url_test.dart
+++ tests/html/url_test.dart
@@ -36,7 +36,7 @@ main() {
   group('blob', () {
     test('createObjectUrl', () {
       var blob = createImageBlob();
-      var url = DOMURL.createObjectURL(blob);
+      var url = window.createObjectUrl(blob);
       expect(url.length, greaterThan(0));
       expect(url.startsWith('blob:'));
 
@@ -52,9 +52,9 @@ main() {
       img.src = url;
     });
 
-    test('revokeObjectURL', () {
+    test('revokeObjectUrl', () {
       var blob = createImageBlob();
-      var url = DOMURL.createObjectURL(blob);
+      var url = window.createObjectUrl(blob);
       expect(url.startsWith('blob:'));
       DOMURL.revokeObjectURL(url);
 
