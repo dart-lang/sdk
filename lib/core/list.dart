38e5f4a7caa8e9b2296a077c93edda85aad6c301
ager@google.com <ager@google.com>
2012-09-26 10:50:24 +0000

    Add documentation of List constructor.
    R=lrn@google.com
    BUG=dartbug.com/948
    
    Review URL: https://codereview.chromium.org//10985039
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@12900 260f80e4-7a28-3924-810f-c04153c831b5

--- lib/core/list.dart
+++ lib/core/list.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -10,6 +10,12 @@ interface List<E> extends Collection<E> default ListImplementation<E> {
 
   /**
    * Creates a list of the given [length].
+   *
+   * If no [length] argument is supplied an extendable list of
+   * length 0 is created.
+   *
+   * If a [length] argument is supplied, a fixed size list of that
+   * length is created.
    */
   List([int length]);
 
