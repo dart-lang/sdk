ba55e7aa7133c22f73782ffd00b652610a5a1def
dgrove@google.com <dgrove@google.com>
2012-09-28 16:18:33 +0000

    Fix issue 3402 . The initial suggestion of using readlink doesn't work on MacOS, so I went this slightly more obtuse solution, which does work on MacOS and Linux. Review URL: https://codereview.chromium.org//10985068
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13025 260f80e4-7a28-3924-810f-c04153c831b5

--- utils/compiler/build_helper.dart
+++ utils/compiler/build_helper.dart
@@ -98,12 +98,19 @@ List<String> buildScript(String name,
 
   return [
 '''
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-BIN_DIR=`dirname \$0`
+# The following line MUST be the first command in the script.
+ABS_SCRIPT_PATH="\$_"
+
+# Setting BIN_DIR this way is ugly, but is needed to handle the case where
+# dart-sdk/bin has been symlinked to. On MacOS, readlink doesn't work
+# with this case.
+BIN_DIR="\$(cd "\${ABS_SCRIPT_PATH%/*}" ; pwd -P)"
+
 unset COLORS
 if test -t 1; then
   # Stdout is a terminal.
@@ -112,7 +119,7 @@ if test -t 1; then
     COLORS="--enable-diagnostic-colors"
   fi
 fi
-exec \$BIN_DIR/dart$options \$BIN_DIR/$path \$COLORS "\$@"
+exec "\$BIN_DIR"/dart$options "\$BIN_DIR/$path" \$COLORS "\$@"
 ''',
 '''
 @echo off
