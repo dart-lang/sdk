5b8e8c045944d9c681a6beb2c39a96e1c4b89965
ahe@google.com <ahe@google.com>
2012-10-05 11:23:23 +0000

    Revert "Make test.dart executable."
    This reverts r13282.
    
    Review URL: https://codereview.chromium.org//11038061
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13284 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/test.dart
+++ tools/test.dart
@@ -1,4 +1,4 @@
-#!/bin/bash -c exec$IFS"${0%/*}"/testing/bin/$OSTYPE/dart$IFS"$0"$IFS"$@"
+#!/usr/bin/env dart
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
