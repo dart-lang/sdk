2d77e48462854dc14aeb469f653def594830e255
ahe@google.com <ahe@google.com>
2012-10-05 10:59:26 +0000

    Make test.dart executable.
    Review URL: https://codereview.chromium.org//11065036
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13282 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/test.dart
+++ tools/test.dart
@@ -1,4 +1,4 @@
-#!/usr/bin/env dart
+#!/bin/bash -c exec$IFS"${0%/*}"/testing/bin/$OSTYPE/dart$IFS"$0"$IFS"$@"
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
