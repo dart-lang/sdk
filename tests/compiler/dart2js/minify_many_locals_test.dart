3297a9fce5c1786bb58f0cf6755c24075838d2cd
erikcorry@google.com <erikcorry@google.com>
2012-10-11 07:40:40 +0000

    Rename locals to a, b, c... instead of z0, z1... BUG=
    Review URL: https://codereview.chromium.org//11103006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13518 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js/minify_many_locals_test.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Test that parameters keep their names in the output.
+
+#import("compiler_helper.dart");
+
+main() {
+  var buffer = new StringBuffer();
+  buffer.add("var foo(");
+  for (int i = 0; i < 2000; i++) {
+    buffer.add("x$i, ");
+  }
+  buffer.add("x) { int i = ");
+  for (int i = 0; i < 2000; i++) {
+    buffer.add("x$i+");
+  }
+  buffer.add("2000; return i; }");
+  var generated = compile(buffer.toString(), 'foo', minify: true);
+  RegExp re = const RegExp(r"\(a,b,c");
+  Expect.isTrue(re.hasMatch(generated));
+
+  re = const RegExp(r"x,y,z,A,B,C");
+  Expect.isTrue(re.hasMatch(generated));
+  
+  re = const RegExp(r"Y,Z,a0,a1,a2,a3,a4,a5,a6");
+  Expect.isTrue(re.hasMatch(generated));
+
+  re = const RegExp(r"g8,g9,h0,h1");
+  Expect.isTrue(re.hasMatch(generated));
+
+  re = const RegExp(r"Z8,Z9,aa0,aa1,aa2");
+  Expect.isTrue(re.hasMatch(generated));
+
+  re = const RegExp(r"aa9,ab0,ab1");
+  Expect.isTrue(re.hasMatch(generated));
+
+  re = const RegExp(r"aZ9,ba0,ba1");
+  Expect.isTrue(re.hasMatch(generated));
+}
