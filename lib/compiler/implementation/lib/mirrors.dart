7b171e398cc72ec5cf50176b9d2a326aa8a1eb46
johnniwinther@google.com <johnniwinther@google.com>
2012-10-12 13:16:50 +0000

    Mirrors stub added to dart2js.
    BUG=4812
    
    Review URL: https://codereview.chromium.org//11114017
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13619 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ lib/compiler/implementation/lib/mirrors.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library("dart:mirrors");
+
+#import("dart:isolate");
+
+#source("../../../mirrors/mirrors.dart");
+
+/**
+ * Stub class for the mirror system.
+ */
+class _Mirrors {
+  static MirrorSystem currentMirrorSystem() {
+    throw new UnsupportedOperationException("MirrorSystem not implemented");
+  }
+
+  static Future<MirrorSystem> mirrorSystemOf(SendPort port) {
+    throw new UnsupportedOperationException("MirrorSystem not implemented");
+  }
+
+  static InstanceMirror reflect(Object reflectee) {
+    throw new UnsupportedOperationException("MirrorSystem not implemented");
+  }
+}
