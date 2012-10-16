4d6fc3b482e1ccbf6c897c158e5ba8285ddb7f7d
efortuna@google.com <efortuna@google.com>
2012-10-16 21:28:18 +0000

    Change Windows build default to build with VS 2010. Review URL: https://codereview.chromium.org//11184008
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13702 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/gyp_dart.py
+++ tools/gyp_dart.py
@@ -22,9 +22,9 @@ def main():
           '-Idart/tools/gyp/all.gypi', 'dart/dart.gyp']
 
   if sys.platform == 'win32':
-    # Generate Visual Studio 2008 compatible files by default.
+    # Generate Visual Studio 2010 compatible files by default.
     if not os.environ.get('GYP_MSVS_VERSION'):
-      args.extend(['-G', 'msvs_version=2008'])
+      args.extend(['-G', 'msvs_version=2010'])
 
   sys.exit(execute(args))
 
