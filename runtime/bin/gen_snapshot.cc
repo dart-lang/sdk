d4a7aedea47702edef24713e7c5289f1100b5f08
asiva@google.com <asiva@google.com>
2012-10-17 17:47:04 +0000

    The snapshot buffer was being released too early resulting in weird crashes. Review URL: https://codereview.chromium.org//11193024
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@13749 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/gen_snapshot.cc
+++ runtime/bin/gen_snapshot.cc
@@ -357,7 +357,6 @@ int main(int argc, char** argv) {
                                                 snapshot_buffer,
                                                 NULL,
                                                 &error);
-      free(snapshot_buffer);
       if (isolate == NULL) {
         fprintf(stderr, "%s", error);
         free(error);
@@ -384,6 +383,8 @@ int main(int argc, char** argv) {
 
       // Now create and write snapshot of script.
       CreateAndWriteSnapshot(true);
+
+      free(snapshot_buffer);
     }
   } else {
     SetupForGenericSnapshotCreation();
