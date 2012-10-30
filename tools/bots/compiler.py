c606bbda994f5c68f986db484f62b5108c26bb1e
rnystrom@google.com <rnystrom@google.com>
2012-10-30 01:17:38 +0000

    Fix copyright for bot scripts.
    Review URL: https://codereview.chromium.org//11260052
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14238 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/bots/compiler.py
+++ tools/bots/compiler.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
 
 """
 Dart2js buildbot steps
