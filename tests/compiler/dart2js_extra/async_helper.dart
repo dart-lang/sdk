bff4aee22a5e5b475181a7753ae75b7da5b5e462
ahe@google.com <ahe@google.com>
2012-11-02 07:24:57 +0000

    Lightweight asynchronous test helper.
    Review URL: https://codereview.chromium.org//11358039
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14452 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js_extra/async_helper.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library async_helper;
+
+/**
+ * Asynchronous test runner.
+ *
+ * [test] is a one argument function which must accept a one argument
+ * function (onDone).  The test function may start asynchronous tasks,
+ * and must call onDone exactly once when all asynchronous tasks have
+ * completed.  The argument to onDone is a bool which indicates
+ * success of the complete test.
+ */
+void asyncTest(void test(void onDone(bool success))) {
+  onDone(bool success) {
+    if (!success) throw 'test failed';
+    print('unittest-suite-success');
+  }
+  test(onDone);
+  print('unittest-suite-wait-for-done');
+}
---
+++ tests/compiler/dart2js_extra/timer_negative_test.dart
@@ -0,0 +1,24 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:isolate';
+
+import 'async_helper.dart';
+
+void test(void onDone(bool success)) {
+  int expected = 4;
+
+  void timerCallback(timer) {
+    if (--expected == 0) onDone(false);
+  }
+
+  new Timer(0, timerCallback);
+  new Timer(10, timerCallback);
+  new Timer(100, timerCallback);
+  new Timer(1000, timerCallback);
+}
+
+main() {
+  asyncTest(test);
+}
