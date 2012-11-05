9d29e43da96de6ff28f25508a4c69229fd495fb4
ngeoffray@google.com <ngeoffray@google.com>
2012-11-05 11:00:41 +0000

    Fix one more checked failure. Review URL: https://codereview.chromium.org//11369073
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14521 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js_extra/3_test.dart
+++ tests/compiler/dart2js_extra/3_test.dart
@@ -2,6 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-void main() {
+int main() {
   return 3;
 }
