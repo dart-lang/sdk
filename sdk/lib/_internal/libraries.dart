80ae89cff6421b4468ff21bcd5b02b645e07c6da
gram@google.com <gram@google.com>
2012-11-08 01:28:34 +0000

    Update library syntax. Review URL: https://codereview.chromium.org//11377035
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14671 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/libraries.dart
+++ sdk/lib/_internal/libraries.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#library('libraries');
+library libraries;
 
 /**
  * A bit flag used by [LibraryInfo] indicating that a library is used by dart2js
