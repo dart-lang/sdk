e4e50abc57f5aee5c2b91eed0407e915cb7e9ebf
johnniwinther@google.com <johnniwinther@google.com>
2012-11-09 13:15:00 +0000

    Library name added to test.
    Review URL: https://codereview.chromium.org//11363165
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14739 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/duplicate_export_negative_test.dart
+++ tests/language/duplicate_export_negative_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library duplicate_export_negative_test;
+
 import 'duplicate_import_libd.dart';
 
 void main() {
