f5ae52c7e79eb9f694e30b6a17f54b442f314790
asiva@google.com <asiva@google.com>
2012-11-15 22:16:46 +0000

    Update test to be compatible for dartium testing. Review URL: https://codereview.chromium.org//11316030
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@14982 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/isolate/illegal_msg_test.dart
+++ tests/isolate/illegal_msg_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library illegal_msg_tests;
 import 'dart:isolate';
 
 funcFoo(x) => x + 2;
