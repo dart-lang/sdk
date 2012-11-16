99127ef9e4a412d65a2fbb5f76b6561a0996327d
amouravski@google.com <amouravski@google.com>
2012-11-16 18:06:24 +0000

    Added top level definition to Window.
    Review URL: https://codereview.chromium.org//11416028
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@15022 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/html/src/CrossFrameTypes.dart
+++ sdk/lib/html/src/CrossFrameTypes.dart
@@ -4,6 +4,22 @@
 
 part of html;
 
+/**
+ * An object representing the top-level context object for web scripting.
+ *
+ * In a web browser, a [Window] object represents the actual browser window.
+ * In a multi-tabbed browser, each tab has its own [Window] object. A [Window]
+ * is the container that displays a [Document]'s content. All web scripting
+ * happens within the context of a [Window] object.
+ *
+ * **Note:** This class represents any window, whereas [LocalWindow] is
+ * used to access the properties and content of the current window.
+ *
+ * See also:
+ *
+ * * [DOM Window](https://developer.mozilla.org/en-US/docs/DOM/window) from MDN.
+ * * [Window](http://www.w3.org/TR/Window/) from the W3C.
+ */
 abstract class Window {
   // Fields.
   Location get location;
