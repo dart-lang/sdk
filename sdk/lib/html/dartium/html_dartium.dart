e019728497cd3d3d332a7d11bdb3029f1a0b9c58
amouravski@google.com <amouravski@google.com>
2012-11-20 06:22:22 +0000

    Removed Window.focus and Window.blur as they are no longer recommended.
    See: dev.w3.org/html5/spec/editing.html#document-level-focus-apis
    
    Review URL: https://codereview.chromium.org//11280077
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@15131 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/html/dartium/html_dartium.dart
+++ sdk/lib/html/dartium/html_dartium.dart
@@ -15636,10 +15636,6 @@ class LocalWindow extends EventTarget implements Window {
   String atob(String string) native "DOMWindow_atob_Callback";
 
 
-  /** @domName DOMWindow.blur */
-  void blur() native "DOMWindow_blur_Callback";
-
-
   /** @domName DOMWindow.btoa */
   String btoa(String string) native "DOMWindow_btoa_Callback";
 
@@ -15674,10 +15670,6 @@ class LocalWindow extends EventTarget implements Window {
 
   /** @domName DOMWindow.find */
   bool find(String string, bool caseSensitive, bool backwards, bool wrap, bool wholeWord, bool searchInFrames, bool showDialog) native "DOMWindow_find_Callback";
-
-
-  /** @domName DOMWindow.focus */
-  void focus() native "DOMWindow_focus_Callback";
 
 
   /** @domName DOMWindow.getComputedStyle */
@@ -28614,8 +28606,6 @@ abstract class Window {
   Window get top;
 
   // Methods.
-  void focus();
-  void blur();
   void close();
   void postMessage(var message, String targetOrigin, [List messagePorts = null]);
 }
