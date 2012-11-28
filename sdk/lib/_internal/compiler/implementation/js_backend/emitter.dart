6b297e5cdb068c6928d3341ff9b0f58b6996e82e
sra@google.com <sra@google.com>
2012-11-28 02:31:54 +0000

    Emit constants using ASTs
    Cleans up inconsistency reported in Issue 6081.
    
    The main observable difference other than Issue 6081 is that properties in object literals are quoted more intelligently and minify slightly more.
    
    Review URL: https://codereview.chromium.org//11308175
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@15435 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/js_backend/emitter.dart
+++ sdk/lib/_internal/compiler/implementation/js_backend/emitter.dart
@@ -81,13 +81,17 @@ class CodeEmitterTask extends CompilerTask {
     });
   }
 
-  void writeConstantToBuffer(Constant value, CodeBuffer buffer,
-                             {emitCanonicalVersion: true}) {
-    if (emitCanonicalVersion) {
-      constantEmitter.emitCanonicalVersionOfConstant(value, buffer);
-    } else {
-      constantEmitter.emitJavaScriptCodeForConstant(value, buffer);
-    }
+  js.Expression constantReference(Constant value) {
+    return constantEmitter.reference(value);
+  }
+
+  js.Expression constantInitializerExpression(Constant value) {
+    return constantEmitter.initializationExpression(value);
+  }
+
+  // Deprecated.  Remove after last use is converted to JS ASTs.
+  void writeConstantToBuffer(Constant value, CodeBuffer buffer) {
+    buffer.add(js.prettyPrint(constantReference(value), compiler));
   }
 
   String get name => 'CodeEmitter';
@@ -1319,12 +1323,17 @@ $classesCollector.$mangledName = {'':
     List<VariableElement> staticNonFinalFields =
         handler.getStaticNonFinalFieldsForEmission();
     for (Element element in staticNonFinalFields) {
-      buffer.add('$isolateProperties.${namer.getName(element)} = ');
       compiler.withCurrentElement(element, () {
-          Constant initialValue = handler.getInitialValueFor(element);
-          writeConstantToBuffer(initialValue, buffer);
-        });
-      buffer.add(';\n');
+        Constant initialValue = handler.getInitialValueFor(element);
+        js.Expression init =
+            new js.Assignment(
+                new js.PropertyAccess.field(
+                    new js.VariableUse(isolateProperties),
+                    namer.getName(element)),
+                constantEmitter.referenceInInitializationContext(initialValue));
+        buffer.add(js.prettyPrint(init, compiler));
+        buffer.add(';\n');
+      });
     }
   }
 
@@ -1382,8 +1391,13 @@ $classesCollector.$mangledName = {'':
         addedMakeConstantList = true;
         emitMakeConstantList(buffer);
       }
-      buffer.add('$isolateProperties.$name = ');
-      writeConstantToBuffer(constant, buffer, emitCanonicalVersion: false);
+      js.Expression init =
+          new js.Assignment(
+              new js.PropertyAccess.field(
+                  new js.VariableUse(isolateProperties),
+                  name),
+              constantInitializerExpression(constant));
+      buffer.add(js.prettyPrint(init, compiler));
       buffer.add(';\n');
     }
   }
