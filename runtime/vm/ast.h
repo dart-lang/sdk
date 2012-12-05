361af1463a035adc7cda31dbcb251beac395cbfd
vegorov@google.com <vegorov@google.com>
2012-12-05 14:29:41 +0000

    Remove unused field AstNode::ic_data.
    R=fschneider@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//11438018
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@15733 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/ast.h
+++ runtime/vm/ast.h
@@ -92,17 +92,11 @@ NODE_LIST(DEFINE_VISITOR_FUNCTION)
 class AstNode : public ZoneAllocated {
  public:
   explicit AstNode(intptr_t token_pos)
-      : token_pos_(token_pos),
-        ic_data_(ICData::ZoneHandle()) {
+      : token_pos_(token_pos) {
     ASSERT(token_pos_ >= 0);
   }
 
   intptr_t token_pos() const { return token_pos_; }
-
-  const ICData& ic_data() const { return ic_data_; }
-  void set_ic_data(const ICData& value) {
-    ic_data_ = value.raw();
-  }
 
 #define AST_TYPE_CHECK(type, name)                                             \
   virtual bool Is##type() const { return false; }                              \
@@ -148,8 +142,6 @@ NODE_LIST(AST_TYPE_CHECK)
 
  private:
   const intptr_t token_pos_;
-  // IC data collected for this node.
-  ICData& ic_data_;
   DISALLOW_COPY_AND_ASSIGN(AstNode);
 };
 
