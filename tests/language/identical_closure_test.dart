d3a2700275b4eac4f1660a82da7e635b3e756162
ngeoffray@google.com <ngeoffray@google.com>
2012-12-13 14:39:56 +0000

    Update comment suggested by Kasper. Review URL: https://codereview.chromium.org//11574005
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16113 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/identical_closure_test.dart
+++ tests/language/identical_closure_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// We assign the closure to a global to avoid potential optimizations.
 var myIdentical = identical;
 
 main() {
