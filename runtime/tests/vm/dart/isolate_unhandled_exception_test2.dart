1e665120cca722072b8fc20488ba69ddc7b6d741
asiva@google.com <asiva@google.com>
2012-12-15 18:43:17 +0000

    Fix new isolate_unhandled_exception tests to make them compatible for dartium testing. Review URL: https://codereview.chromium.org//11595002
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16197 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/tests/vm/dart/isolate_unhandled_exception_test2.dart
+++ runtime/tests/vm/dart/isolate_unhandled_exception_test2.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library isolate_unhandled_exception_test2;
+
 import 'dart:isolate';
 import 'dart:io';
 
--- runtime/tests/vm/dart/isolate_unhandled_exception_uri_helper.dart
+++ runtime/tests/vm/dart/isolate_unhandled_exception_uri_helper.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library isolate_unhandled_exception_uri_helper;
+
 import 'dart:isolate';
 
 // Isolate script that throws an uncaught exception, which is caught by an
