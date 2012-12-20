84f41caf0deae98116cd15d6457ffe33a56ccbef
blois@google.com <blois@google.com>
2012-12-20 21:20:52 +0000

    Fixing core exception tests after Chrome update.
    Related to https://codereview.chromium.org/11645050/, additional core tests were broken as well.
    
    Basically Dart is relying on the JS exception's message to unwrap into a Dart exception, but this appears to be quite unstable.
    
    BUG=
    
    Review URL: https://codereview.chromium.org//11644053
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16399 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/lib/js_helper.dart
+++ sdk/lib/_internal/compiler/implementation/lib/js_helper.dart
@@ -1016,7 +1016,8 @@ unwrapException(ex) {
     if (message is String) {
       if (message.endsWith('is null') ||
           message.endsWith('is undefined') ||
-          message.endsWith('is null or undefined')) {
+          message.endsWith('is null or undefined') ||
+          message.endsWith('of null')) {
         return new NoSuchMethodError(null, '<unknown>', [], {});
       } else if (contains(message, ' has no method ') ||
                  contains(message, ' is not a function') ||
