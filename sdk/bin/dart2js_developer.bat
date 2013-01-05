3803e4e5c622cc82814e9994035e38aa3794801c
aprelev@gmail.com <aprelev@gmail.com>
2013-01-05 02:12:18 +0000

    Fixed dart2js.bat so it looks for dart2js snapshot in right place.
    BUG=dartbug.com/7663
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//11740040
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16669 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/bin/dart2js_developer.bat
+++ sdk/bin/dart2js_developer.bat
@@ -3,13 +3,7 @@ REM Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 REM for details. All rights reserved. Use of this source code is governed by a
 REM BSD-style license that can be found in the LICENSE file.
 
-set SCRIPTPATH=%~dp0
-
-REM Does the path have a trailing slash? If so, remove it.
-if %SCRIPTPATH:~-1%== set SCRIPTPATH=%SCRIPTPATH:~0,-1%
-
-set arguments=%*
-set SNAPSHOTNAME=%SCRIPTPATH%dart2js.snapshot
-if exist %SNAPSHOTNAME% set SNAPSHOT=--use_script_snapshot=%SNAPSHOTNAME%
-
-"%SCRIPTPATH%dart" --checked --no_use_inlining --heap_growth_rate=512 %SNAPSHOT% "%SCRIPTPATH%..\lib\_internal\compiler\implementation\dart2js.dart" %arguments%
+setlocal
+set DART2JS_DEVELOPER_MODE=1
+call "%~dp0dart2js.bat" %*
+endlocal
