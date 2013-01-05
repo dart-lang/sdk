d9115f9740281c87943beeb7cdb20a2addca2e68
aprelev@gmail.com <aprelev@gmail.com>
2013-01-05 02:42:50 +0000

    Revert "Fixed dart2js.bat so it looks for dart2js snapshot in right place." due to dart2js chrome build failures.
    This reverts r16669.
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//11773009
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16670 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/bin/dart2js.bat
+++ sdk/bin/dart2js.bat
@@ -3,55 +3,13 @@ REM Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 REM for details. All rights reserved. Use of this source code is governed by a
 REM BSD-style license that can be found in the LICENSE file.
 
-setlocal
+set SCRIPTPATH=%~dp0
 
-rem Handle the case where dart-sdk/bin has been symlinked to.
-set DIR_NAME_WITH_SLASH=%~dp0
-set DIR_NAME=%DIR_NAME_WITH_SLASH:~0,-1%%
-call :follow_links "%DIR_NAME%", RETURNED_BIN_DIR
+REM Does the path have a trailing slash? If so, remove it.
+if %SCRIPTPATH:~-1%== set SCRIPTPATH=%SCRIPTPATH:~0,-1%
 
-rem Get rid of surrounding quotes.
-for %%i in ("%RETURNED_BIN_DIR%") do set BIN_DIR=%%~fi
+set arguments=%*
+set SNAPSHOTNAME="%SCRIPTPATH%dart2js.snapshot"
+if exist %SNAPSHOTNAME% set SNAPSHOT=--use_script_snapshot=%SNAPSHOTNAME%
 
-rem Get absolute full name for SDK_DIR.
-for %%i in ("%BIN_DIR%\..\") do set SDK_DIR=%%~fi
-
-set DART2JS=%SDK_DIR%lib\_internal\compiler\implementation\dart2js.dart
-set DART=%BIN_DIR%\dart
-set SNAPSHOT=%DART2JS%.snapshot
-
-set EXTRA_OPTIONS=
-set EXTRA_VM_OPTIONS=
-
-if _%DART2JS_DEVELOPER_MODE%_ == _1_ (
-  set EXTRA_VM_OPTIONS=%EXTRA_VM_OPTIONS% --checked
-)
-
-if exist "%SNAPSHOT%" (
-  echo Using snapshot "%SNAPSHOT%" >&2
-  set EXTRA_VM_OPTIONS=%EXTRA_VM_OPTIONS% --use_script_snapshot="%SNAPSHOT%"
-)
-
-rem See comments regarding options below in dart2js shell script.
-set EXTRA_VM_OPTIONS=%EXTRA_VM_OPTIONS% --heap_growth_rate=512
-set EXTRA_VM_OPTIONS=%EXTRA_VM_OPTIONS% --no_use_inlining
-
-"%DART%" %EXTRA_VM_OPTIONS% "%DART2JS%" %EXTRA_OPTIONS% %*
-
-endlocal
-
-goto end
-
-:follow_links
-setlocal
-for %%i in (%1) do set result=%%~fi
-set current=
-for /f "tokens=2 delims=[]" %%i in ('dir /a:l ^"%~dp1^" 2^>nul ^
-                                     ^| find ">     %~n1 ["') do (
-  set current=%%i
-)
-if not "%current%"=="" call :follow_links "%current%", result
-endlocal & set %~2=%result%
-goto :eof
-
-:end
+"%SCRIPTPATH%dart" --no_use_inlining --heap_growth_rate=512 %SNAPSHOT% "%SCRIPTPATH%..\lib\_internal\compiler\implementation\dart2js.dart" %arguments%
