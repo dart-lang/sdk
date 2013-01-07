23312c2400360c7fb7916baa41d99e8ae0be6865
sgjesse@google.com <sgjesse@google.com>
2013-01-07 14:55:31 +0000

    Add missing import in test.
    R=ager@google.com
    
    BUG=
    
    Review URL: https://codereview.chromium.org//11788017
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16704 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/websql_test.dart
+++ tests/html/websql_test.dart
@@ -1,6 +1,7 @@
 library WebDBTest;
 import '../../pkg/unittest/lib/unittest.dart';
 import '../../pkg/unittest/lib/html_config.dart';
+import 'dart:async';
 import 'dart:html';
 
 void fail(message) {
