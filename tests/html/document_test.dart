204bacf94bfa53e505c67503916598ce881ae017
blois@google.com <blois@google.com>
2013-01-10 23:33:48 +0000

    Removing outdated xml tests.
    XMLDocument and XMLElement are no longer exposed and we have no plans to do so, so these tests are no longer being used.
    
    BUG=
    
    Review URL: https://codereview.chromium.org//11778071
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16943 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/document_test.dart
+++ tests/html/document_test.dart
@@ -13,20 +13,34 @@ main() {
   var isUnknownElement =
       predicate((x) => x is UnknownElement, 'is UnknownElement');
 
-  test('CreateElement', () {
-    // FIXME: nifty way crashes, do it boring way.
-    expect(new Element.tag('span'), isElement);
-    expect(new Element.tag('div'), isDivElement);
-    expect(new Element.tag('a'), isAnchorElement);
-    expect(new Element.tag('bad_name'), isUnknownElement);
-  });
-
   group('getCssCanvasContext', () {
     test('getCssCanvasContext 2d', () {
       var context = document.getCssCanvasContext('2d', 'testContext', 10, 20);
       expect(context is CanvasRenderingContext2D, true);
       expect(context.canvas.width, 10);
       expect(context.canvas.height, 20);
+    });
+  });
+
+  group('document', () {
+    test('Document.query', () {
+      Document doc = new DomParser().parseFromString(
+      '''<ResultSet>
+           <Row>A</Row>
+           <Row>B</Row>
+           <Row>C</Row>
+         </ResultSet>''','text/xml');
+
+      var rs = doc.query('ResultSet');
+      expect(rs, isNotNull);
+    });
+
+    test('CreateElement', () {
+      // FIXME: nifty way crashes, do it boring way.
+      expect(new Element.tag('span'), isElement);
+      expect(new Element.tag('div'), isDivElement);
+      expect(new Element.tag('a'), isAnchorElement);
+      expect(new Element.tag('bad_name'), isUnknownElement);
     });
   });
 }
