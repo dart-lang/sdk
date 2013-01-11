4071075fdbd65c40562283c8df41436a3c3fbaaa
sgjesse@google.com <sgjesse@google.com>
2013-01-11 13:18:38 +0000

    Reapply "Add onClosed callbacks to HTTP output streams""
    The test have been rewritten to expect any order of the onClosed
    events.
    
    R=ager@google.com
    
    BUG=dart:7761
    
    Review URL: https://codereview.chromium.org//11853011
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16969 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/standalone/io/http_stream_close_test.dart
@@ -0,0 +1,56 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+
+import "dart:io";
+import "dart:uri";
+
+main() {
+  bool serverOnClosed = false;
+  bool clientOnClosed = false;
+  bool requestOnClosed = false;
+
+  var server = new HttpServer();
+  var client = new HttpClient();
+
+  checkDone() {
+    if (serverOnClosed && clientOnClosed && requestOnClosed) {
+      server.close();
+      client.shutdown();
+    }
+  }
+
+  server.listen("127.0.0.1", 0);
+  server.defaultRequestHandler = (request, response) {
+    request.inputStream.onData = request.inputStream.read;
+    request.inputStream.onClosed = () {
+      response.outputStream.onClosed = () {
+        serverOnClosed = true;
+        checkDone();
+      };
+      response.outputStream.writeString("hello!");
+      response.outputStream.close();
+    };
+  };
+
+  var connection = client.postUrl(new Uri.fromString("http://127.0.0.1:${server.port}"));
+  connection.onError = (e) => throw e;
+  connection.onRequest = (request) {
+    request.contentLength = "hello!".length;
+    request.outputStream.onError = (e) => throw e;
+    request.outputStream.onClosed = () {
+      clientOnClosed = true;
+      checkDone();
+    };
+    request.outputStream.writeString("hello!");
+    request.outputStream.close();
+  };
+  connection.onResponse = (response) {
+    response.inputStream.onData = response.inputStream.read;
+    response.inputStream.onClosed = () {
+      requestOnClosed = true;
+      checkDone();
+    };
+  };
+}
