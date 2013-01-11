ebeba7bc13e6f81d1ff0e0d17db89e5bb40448db
kasperl@google.com <kasperl@google.com>
2013-01-11 07:16:04 +0000

    Fix http://dartbug.com/7800 by avoiding the use of 'with'.
    R=johnniwinther@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//11854007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@16951 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/typechecker.dart
+++ sdk/lib/_internal/compiler/implementation/typechecker.dart
@@ -611,17 +611,15 @@ class Types {
   final VoidType voidType;
   final DynamicType dynamicType;
 
-  Types(Compiler compiler, ClassElement dynamicElement)
-    : this.with(compiler, dynamicElement,
-                new LibraryElement(new Script(null, null)));
-
-  Types.with(Compiler this.compiler,
-             ClassElement dynamicElement,
-             LibraryElement library)
-    : voidType = new VoidType(new VoidElement(library)),
-      dynamicType = new DynamicType(dynamicElement) {
-      dynamicElement.rawType = dynamicElement.thisType = dynamicType;
+  factory Types(Compiler compiler, ClassElement dynamicElement) {
+    LibraryElement library = new LibraryElement(new Script(null, null));
+    VoidType voidType = new VoidType(new VoidElement(library));
+    DynamicType dynamicType = new DynamicType(dynamicElement);
+    dynamicElement.rawType = dynamicElement.thisType = dynamicType;
+    return new Types.internal(compiler, voidType, dynamicType);
   }
+
+  Types.internal(this.compiler, this.voidType, this.dynamicType);
 
   /** Returns true if t is a subtype of s */
   bool isSubtype(DartType t, DartType s) {
