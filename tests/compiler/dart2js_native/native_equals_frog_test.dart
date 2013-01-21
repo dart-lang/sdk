c1afbd6afd414434513c89ad3e7646222cdfe69f
ngeoffray@google.com <ngeoffray@google.com>
2013-01-21 13:06:35 +0000

    Add a test that native classes work with operator==. Review URL: https://codereview.chromium.org//12036009
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@17350 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js_native/native_equals_frog_test.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class A native "*A" {}
+makeA() native;
+
+void setup() native """
+function A() {}
+makeA = function(){return new A;};
+""";
+
+
+main() {
+  setup();
+  var a = makeA();
+  Expect.isTrue(a == a);
+  Expect.isTrue(identical(a, a));
+
+  Expect.isFalse(a == makeA());
+  Expect.isFalse(identical(a, makeA()));
+}
