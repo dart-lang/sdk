2e7da75aab27458cad9b485c961a3d3b4d73adda
kasperl@google.com <kasperl@google.com>
2013-01-28 09:58:35 +0000

    Extends native mixin tests.
    R=ngeoffray@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//12079020
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@17700 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js_native/native_mixin_multiple_test.dart
@@ -0,0 +1,72 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test that native classes can use ordinary Dart classes as mixins.
+
+class A native "*A" {
+  foo() => "A-foo";
+  baz() => "A-baz";
+}
+
+class B extends A with M1, M2 native "*B" {
+  bar() => baz();
+}
+
+class M1 {
+  foo() => "M1-foo";
+  baz() => "M1-baz";
+}
+
+class M2 {
+  foo() => "M2-foo";
+}
+
+A makeA() native;
+B makeB() native;
+
+void setup() native """
+function A() {}
+function B() {}
+makeA = function(){return new A;};
+makeB = function(){return new B;};
+""";
+
+main() {
+  setup();
+  A a = makeA();
+  Expect.equals("A-foo", a.foo());
+  Expect.throws(() => a.bar(), (error) => error is NoSuchMethodError);
+  Expect.equals("A-baz", a.baz());
+  Expect.isTrue(a is A);
+  Expect.isFalse(a is B);
+  Expect.isFalse(a is M1);
+  Expect.isFalse(a is M2);
+
+  B b = makeB();
+  Expect.equals("M2-foo", b.foo());
+  Expect.equals("M1-baz", b.bar());
+  Expect.equals("M1-baz", b.baz());
+  Expect.isTrue(b is A);
+  Expect.isTrue(b is B);
+  Expect.isTrue(b is M1);
+  Expect.isTrue(b is M2);
+
+  M1 m1 = new M1();
+  Expect.equals("M1-foo", m1.foo());
+  Expect.throws(() => m1.bar(), (error) => error is NoSuchMethodError);
+  Expect.equals("M1-baz", m1.baz());
+  Expect.isFalse(m1 is A);
+  Expect.isFalse(m1 is B);
+  Expect.isTrue(m1 is M1);
+  Expect.isFalse(m1 is M2);
+
+  M2 m2 = new M2();
+  Expect.equals("M2-foo", m2.foo());
+  Expect.throws(() => m2.bar(), (error) => error is NoSuchMethodError);
+  Expect.throws(() => m2.baz(), (error) => error is NoSuchMethodError);
+  Expect.isFalse(m2 is A);
+  Expect.isFalse(m2 is B);
+  Expect.isFalse(m2 is M1);
+  Expect.isTrue(m2 is M2);
+}
--- tests/compiler/dart2js_native/native_mixin_test.dart
+++ tests/compiler/dart2js_native/native_mixin_test.dart
@@ -5,8 +5,8 @@
 // Test that native classes can use ordinary Dart classes as mixins.
 
 class A native "*A" {
-  foo() => 42;
-  baz() => 99;
+  foo() => "A-foo";
+  baz() => "A-baz";
 }
 
 class B extends A with M native "*B" {
@@ -14,8 +14,8 @@ class B extends A with M native "*B" {
 }
 
 class M {
-  foo() => 87;
-  bar() => 101;
+  foo() => "M-foo";
+  bar() => "M-bar";
 }
 
 A makeA() native;
@@ -31,24 +31,24 @@ makeB = function(){return new B;};
 main() {
   setup();
   A a = makeA();
-  Expect.equals(42, a.foo());
+  Expect.equals("A-foo", a.foo());
   Expect.throws(() => a.bar(), (error) => error is NoSuchMethodError);
-  Expect.equals(99, a.baz());
+  Expect.equals("A-baz", a.baz());
   Expect.isTrue(a is A);
   Expect.isFalse(a is B);
   Expect.isFalse(a is M);
 
   B b = makeB();
-  Expect.equals(87, b.foo());
-  Expect.equals(99, b.bar());
-  Expect.equals(99, b.baz());
+  Expect.equals("M-foo", b.foo());
+  Expect.equals("A-baz", b.bar());
+  Expect.equals("A-baz", b.baz());
   Expect.isTrue(b is A);
   Expect.isTrue(b is B);
   Expect.isTrue(b is M);
 
   M m = new M();
-  Expect.equals(87, m.foo());
-  Expect.equals(101, m.bar());
+  Expect.equals("M-foo", m.foo());
+  Expect.equals("M-bar", m.bar());
   Expect.throws(() => m.baz(), (error) => error is NoSuchMethodError);
   Expect.isFalse(m is A);
   Expect.isFalse(m is B);
