1a704b41f4c9d4fbc9011c5202b7bf9c760ec82d
efortuna@google.com <efortuna@google.com>
2013-02-11 23:28:46 +0000

    Deflake notifications test.
    BUG=
    
    Review URL: https://codereview.chromium.org//12210124
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@18355 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/notifications_test.dart
+++ tests/html/notifications_test.dart
@@ -29,11 +29,14 @@ main() {
         }
       });
 
+      /*
+      // Sporadically flaky on Mac Chrome. Uncomment when Issue 8482 is fixed.
       test('construct notification', () {
         var note = new Notification('this is a notification');
         var note2 = new Notification('another notificiation',
           {'option1': 'foo'});
       });
+      */
     }
   });
 }
