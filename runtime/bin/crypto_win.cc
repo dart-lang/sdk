b1c855488b52c9487bbd1f8652df027b350f3cbc
iposva@google.com <iposva@google.com>
2013-02-20 21:27:07 +0000

    - Attempt to fix build breakage on Windows. Review URL: https://codereview.chromium.org//12319030
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@18788 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/crypto_win.cc
+++ runtime/bin/crypto_win.cc
@@ -2,10 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#define _CRT_RAND_S
+
 #include "platform/globals.h"
 #if defined(TARGET_OS_WINDOWS)
 
-#define _CRT_RAND_S
 #include "bin/crypto.h"
 
 
