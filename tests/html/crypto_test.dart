1180a61916730b7e764ff71ac48477e3a01666e1
ager@google.com <ager@google.com>
2013-02-22 09:41:02 +0000

    Add test of crypto.getRandomValues.
    Implemented this in Dartium and would like a regression test.
    
    I'll update status files with information on the browsers where
    this passes when the test looks good to you.
    
    R=blois@google.com,antonm@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//12314030
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@18872 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/html/crypto_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library crypto_test;
+import '../../pkg/unittest/lib/unittest.dart';
+import '../../pkg/unittest/lib/html_config.dart';
+import 'dart:html';
+
+main() {
+  useHtmlConfiguration();
+
+  test('exists', () {
+    var crypto = window.crypto;
+    expect(crypto is Crypto, isTrue);
+  });
+
+  test('successful call', () {
+    var crypto = window.crypto;
+    var data = new Uint8Array(100);
+    expect(data.every((e) => e == 0), isTrue);
+    crypto.getRandomValues(data);
+    // In theory this is flaky. However, in practice you will get 100 zeroes
+    // in a row from a cryptographically secure random number generator so
+    // rarely that we don't have to worry about it.
+    expect(data.any((e) => e != 0), isTrue);
+  });
+
+  test('type mismatch', () {
+    var crypto = window.crypto;
+    var data = new Float32Array(100);
+    expect(() {
+      crypto.getRandomValues(data);
+    }, throws, reason: 'Only typed array views with integer types allowed');
+  });
+}
