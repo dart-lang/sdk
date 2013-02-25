1d62e0a0777d141a87e11aa370aeee4798719077
rnystrom@google.com <rnystrom@google.com>
2013-02-25 23:20:47 +0000

    Disable test on Windows for now.
    Review URL: https://codereview.chromium.org//12313114
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19016 260f80e4-7a28-3924-810f-c04153c831b5

--- utils/tests/pub/install/path/shared_dependency_test.dart
+++ utils/tests/pub/install/path/shared_dependency_test.dart
@@ -127,47 +127,4 @@ main() {
       dir("shared", [file("shared.dart", 'main() => "shared";')])
     ]).scheduleValidate();
   });
-
-  integration("shared dependency with symlink", () {
-    dir("shared", [
-      libDir("shared"),
-      libPubspec("shared", "0.0.1")
-    ]).scheduleCreate();
-
-    dir("foo", [
-      libDir("foo"),
-      libPubspec("foo", "0.0.1", deps: [
-        {"path": "../shared"}
-      ])
-    ]).scheduleCreate();
-
-    dir("bar", [
-      libDir("bar"),
-      libPubspec("bar", "0.0.1", deps: [
-        {"path": "../link/shared"}
-      ])
-    ]).scheduleCreate();
-
-    dir(appPath, [
-      pubspec({
-        "name": "myapp",
-        "dependencies": {
-          "foo": {"path": "../foo"},
-          "bar": {"path": "../bar"}
-        }
-      })
-    ]).scheduleCreate();
-
-    dir("link").scheduleCreate();
-    scheduleSymlink("shared", path.join("link", "shared"));
-
-    schedulePub(args: ["install"],
-        output: new RegExp(r"Dependencies installed!$"));
-
-    dir(packagesPath, [
-      dir("foo", [file("foo.dart", 'main() => "foo";')]),
-      dir("bar", [file("bar.dart", 'main() => "bar";')]),
-      dir("shared", [file("shared.dart", 'main() => "shared";')])
-    ]).scheduleValidate();
-  });
 }
