4f8f682f82b4e67be959d825e9b488e5bd141811
johnmccutchan@google.com <johnmccutchan@google.com>
2013-02-26 03:14:40 +0000

    Fix dartium build by fixing SIMD tests.
    Review URL: https://codereview.chromium.org//12347002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19027 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/tests/vm/dart/simd128float32_test.dart
+++ runtime/tests/vm/dart/simd128float32_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Library tag to be able to run in html test framework.
+library simd128_float32_test;
+
 import 'dart:scalarlist';
 
 testAdd() {
