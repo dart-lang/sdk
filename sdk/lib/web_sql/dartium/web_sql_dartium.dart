89b1a828db72d2ea79d88851c89a2c98c92efd05
amouravski@google.com <amouravski@google.com>
2013-02-26 22:36:55 +0000

    Added library comment and warning message to web_sql.
    Review URL: https://codereview.chromium.org//12334098
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19092 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/web_sql/dartium/web_sql_dartium.dart
+++ sdk/lib/web_sql/dartium/web_sql_dartium.dart
@@ -1,3 +1,13 @@
+/**
+ * An API for storing data in the browser that can be queried with SQL.
+ *
+ * **Caution:** this specification is no longer actively maintained by the Web
+ * Applications Working Group and may be removed at any time.
+ * See [the W3C Web SQL Database specification](http://www.w3.org/TR/webdatabase/)
+ * for more information.
+ *
+ * The [dart:indexed_db] APIs is a recommended alternatives.
+ */
 library dart.dom.web_sql;
 
 import 'dart:async';
