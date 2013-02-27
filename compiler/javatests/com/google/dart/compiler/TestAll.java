0020cd93e36c0c8dabd58a00cc156031aac763bd
devoncarew@google.com <devoncarew@google.com>
2013-02-27 21:43:26 +0000

    Add the services_test plugin to the buildbot_test suite.
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19174 260f80e4-7a28-3924-810f-c04153c831b5

--- compiler/javatests/com/google/dart/compiler/TestAll.java
+++ compiler/javatests/com/google/dart/compiler/TestAll.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2013, the Dart project authors.
- * 
+ *
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -25,10 +25,8 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class TestAll {
-  
+
   public static Test suite() {
-    initSdk();
-    
     TestSuite suite = new TestSuite("Tests in " + TestAll.class.getPackage().getName());
 
     suite.addTest(CompilerTests.suite());
@@ -42,20 +40,5 @@ public class TestAll {
 
     return suite;
   }
-  
-  private static final String DART_SDK_PROPERTY_NAME = "com.google.dart.sdk";
-  private static final String DART_SDK_ENV_VARIABLE = "DART_SDK";
-  
-  private static void initSdk() {
-    String sdkProperty = System.getProperty(DART_SDK_PROPERTY_NAME);
-    
-    if (sdkProperty == null) {
-      sdkProperty = System.getenv(DART_SDK_ENV_VARIABLE);
-      
-      if (sdkProperty != null) {
-        System.setProperty(DART_SDK_PROPERTY_NAME, sdkProperty);
-      }
-    }
-  }
-  
+
 }
