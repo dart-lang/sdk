06b9059cd0ef27c7feb75d41452c1de46f5c2004
kasperl@google.com <kasperl@google.com>
2013-03-05 13:26:19 +0000

    Re-land "Implement subclass union and intersection on type mask."
    R=ngeoffray@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//12437004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19477 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/ssa/types.dart
+++ sdk/lib/_internal/compiler/implementation/ssa/types.dart
@@ -978,7 +978,7 @@ class HBoundedType extends HType {
 
     TypeMask otherMask = other.computeMask(compiler);
     if (otherMask != null) {
-      TypeMask intersection = mask.intersection(otherMask, compiler.types);
+      TypeMask intersection = mask.intersection(otherMask, compiler);
       if (intersection != null) {
         if (intersection == mask) return this;
         return new HType.fromMask(intersection, compiler);
@@ -1002,7 +1002,7 @@ class HBoundedType extends HType {
 
     TypeMask otherMask = other.computeMask(compiler);
     if (otherMask != null) {
-      TypeMask union = mask.union(otherMask, compiler.types);
+      TypeMask union = mask.union(otherMask, compiler);
       if (union == null) return HType.UNKNOWN;
       if (union == mask) return this;
       return new HType.fromMask(union, compiler);
