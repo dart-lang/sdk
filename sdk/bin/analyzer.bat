bf56eebc8687854a87350c50dd8b9d356e512ece
ricow@google.com <ricow@google.com>
2013-03-12 17:42:15 +0000

    Create "binaries" for calling the new analyzer.
    This make the testing script look much nicer, and it allows for easier calling the analyzer instead of using java -jar
    Review URL: https://codereview.chromium.org//12335133
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@19882 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ sdk/bin/analyzer.bat
@@ -0,0 +1,25 @@
+@echo off
+rem Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+rem for details. All rights reserved. Use of this source code is governed by a
+rem BSD-style license that can be found in the LICENSE file.
+rem 
+
+rem This file is used to execute the analyzer by running the jar file.
+rem It is a simple wrapper enabling us to have simpler command lines in
+rem the testing infrastructure.
+
+set SCRIPTPATH=%~dp0
+
+rem Does the path have a trailing slash? If so, remove it.
+if %SCRIPTPATH:~-1%==\ set SCRIPTPATH=%SCRIPTPATH:~0,-1%
+
+rem DART_CONFIGURATION defaults to ReleaseIA32
+if "%DART_CONFIGURATION%"=="" set DART_CONFIGURATION=ReleaseIA32
+
+set arguments=%*
+
+set "JAR_DIR=%SCRIPTPATH%\..\..\build\%DART_CONFIGURATION%\new_analyzer"
+
+set "JAR_FILE=%JAR_DIR%\new_analyzer.jar"
+
+java -jar %JAR_FILE% %arguments%"
