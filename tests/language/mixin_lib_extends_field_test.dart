1548e37c82e0b67cd13d409c39137ee1116400fd
hausner@google.com <hausner@google.com>
2013-03-22 18:06:26 +0000

    Fix dartium tests Review URL: https://codereview.chromium.org//12729031
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@20392 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/mixin_lib_extends_field_test.dart
+++ tests/language/mixin_lib_extends_field_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library mixin_lib_extends_field_test;
+
 import "mixin_lib_extends_field_lib.dart" as L;
 
 class S {
