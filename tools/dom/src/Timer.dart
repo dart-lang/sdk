7a50271d5b948abdca0b3a01a360594fdf9765d8
antonm@google.com <antonm@google.com>
2013-03-28 12:15:33 +0000

    Support pure isolate timers.
    R=vsm@google.com
    BUG=4997
    
    Review URL: https://codereview.chromium.org//12594027
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@20608 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/dom/src/Timer.dart
+++ tools/dom/src/Timer.dart
@@ -28,3 +28,59 @@ get _timerFactoryClosure => (int milliSeconds, void callback(Timer timer), bool 
   timer = new _Timer(() { canceller(id); });
   return timer;
 };
+
+const _NEW_TIMER = 'NEW_TIMER';
+const _CANCEL_TIMER = 'CANCEL_TIMER';
+const _TIMER_PING = 'TIMER_PING';
+
+class _PureIsolateTimer implements Timer {
+  final ReceivePort _port = new ReceivePort();
+  SendPort _sendPort; // Effectively final.
+
+  _PureIsolateTimer(int milliSeconds, callback, repeating) {
+    _sendPort = _port.toSendPort();
+    _port.receive((msg, replyTo) {
+      assert(msg == _TIMER_PING);
+      assert(replyTo == _HELPER_ISOLATE_PORT);
+      callback(this);
+      if (!repeating) _cancel();
+    });
+    _HELPER_ISOLATE_PORT.then((port) {
+      port.send([_NEW_TIMER, milliSeconds, repeating], _sendPort);
+    });
+  }
+
+  void cancel() {
+    _cancel();
+    _HELPER_ISOLATE_PORT.send([_CANCEL_TIMER], _sendPort);
+  }
+
+  void _cancel() {
+    _port.close();
+  }
+
+  static final Future<SendPort> _HELPER_ISOLATE_PORT =
+      spawnDomFunction(_helperIsolateMain);
+}
+
+final _TIMER_REGISTRY = new Map<SendPort, Timer>();
+
+_helperIsolateMain() {
+  port.receive((msg, replyTo) {
+    final cmd = msg[0];
+    if (cmd == _NEW_TIMER) {
+      final duration = new Duration(milliseconds: msg[1]);
+      bool periodic = msg[2];
+      final callback = () { replyTo.send(_TIMER_PING); };
+      _TIMER_REGISTRY[replyTo] = periodic ?
+          new Timer.periodic(duration, callback) :
+          new Timer(duration, callback);
+    } else if (cmd == _CANCEL_TIMER) {
+      _TIMER_REGISTRY.remove(replyTo).cancel();
+    }
+  });
+}
+
+get _pureIsolateTimerFactoryClosure =>
+    ((int milliSeconds, void callback(Timer time), bool repeating) =>
+        new _PureIsolateTimer(milliSeconds, callback, repeating));
