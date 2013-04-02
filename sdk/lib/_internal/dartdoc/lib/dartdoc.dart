06c75abac2ef4780a03f814c05a00fc020cbb50b
amouravski@google.com <amouravski@google.com>
2013-04-02 00:08:03 +0000

    Document dynamic returns.
    This change fixes the bot breaks from last CL.
    
    Review URL: https://codereview.chromium.org//13386012
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@20762 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/dartdoc/lib/dartdoc.dart
+++ sdk/lib/_internal/dartdoc/lib/dartdoc.dart
@@ -1397,12 +1397,12 @@ class Dartdoc {
       }
 
       if (!member.isConstructor) {
-        annotateType(host, member.returnType);
+        annotateDynamicType(host, member.returnType);
       }
     } else {
       assert(member is VariableMirror);
       if (asGetter) {
-        annotateType(host, member.type);
+        annotateDynamicType(host, member.type);
       } else {
         write('void ');
       }
@@ -1698,6 +1698,14 @@ class Dartdoc {
     final rel = isAbsolute(href) ? ' ref="external"' : '';
     final cssClass = css == null ? '' : ' class="$css"';
     return '<a href="${relativePath(href)}"$cssClass$rel>$contents</a>';
+  }
+
+  /**
+   * Writes a type annotation, preferring to print dynamic.
+   */
+  annotateDynamicType(ContainerMirror enclosingType,
+                      TypeMirror type) {
+    annotateType(enclosingType, type, type.isDynamic ? 'dynamic' : null);
   }
 
   /**
