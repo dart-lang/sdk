42413781d3d95292d0136d0b474541ba885c8e40
iposva@google.com <iposva@google.com>
2013-04-11 23:34:20 +0000

    - Make the definition of strndup static to avoid conflicting in case it is available. Review URL: https://codereview.chromium.org//14198007
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@21322 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/platform/globals.h
+++ runtime/platform/globals.h
@@ -410,7 +410,7 @@ inline D bit_copy(const S& source) {
 
 #if defined(NEEDS_STRNDUP)
 // size_t used to match function signature on other platforms.
-inline char* strndup(const char* s, size_t n) {
+static inline char* strndup(const char* s, size_t n) {
   size_t len = strlen(s);
   if (n < len) {
     len = n;
