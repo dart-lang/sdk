5880d92241418e9203b66a3dc37f345d02425e99
ricow@google.com <ricow@google.com>
2013-04-12 11:01:00 +0000

    Merge revision 21306 to trunk. This fixes an issue with xcode output parsing on 10.6 Review URL: https://codereview.chromium.org//13945021
    git-svn-id: http://dart.googlecode.com/svn/trunk@21350 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/build.py
+++ tools/build.py
@@ -273,6 +273,8 @@ PhaseScriptExecution "Action \"upload_sdk_py\"" xcodebuild/dart.build/...
         is_fancy_tty = True
     except subprocess.CalledProcessError:
       is_fancy_tty = False
+    except AttributeError:
+      is_fancy_tty = False
   for line in unbuffered(process.stdout.readline):
     line = line.rstrip()
     if line.startswith('=== BUILD ') or line.startswith('** BUILD '):
