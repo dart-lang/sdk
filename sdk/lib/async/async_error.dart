dd60c5d7a8ea47afd6001dc5d3e4536c46cc1017
ajohnsen@google.com <ajohnsen@google.com>
2013-04-15 20:58:10 +0000

    Comment getAttachedStackTrace.
    BUG=
    
    Review URL: https://codereview.chromium.org//14168012
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@21513 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/async/async_error.dart
+++ sdk/lib/async/async_error.dart
@@ -11,6 +11,16 @@ void _attachStackTrace(o, st) {
   _stackTraceExpando[o] = st;
 }
 
+/**
+ * *This is an experimental API.*
+ *
+ * Get the [StackTrace] attached to [o].
+ *
+ * If object [o] was thrown and caught in a dart:async method, a [StackTrace]
+ * object was attached to it. Use [getAttachedStackTrace] to get that object.
+ *
+ * Returns [null] if no [StackTrace] was attached.
+ */
 getAttachedStackTrace(o) {
   if (o == null || o is bool || o is num || o is String) return null;
   return _stackTraceExpando[o];
