dfd51cba6f5e875ddee57de1639edefcb9a58c77
kevmoo@j832.com <kevmoo@j832.com>
2013-04-18 16:20:07 +0000

    svg and json tests: fixing nested test calls
    Review URL: https://codereview.chromium.org//14258009
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@21678 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/json/json_test.dart
+++ tests/json/json_test.dart
@@ -71,6 +71,7 @@ main() {
                    '"z":"hi","w":{"c":null,"d":true}, "v":null}'),
         equals({"x": {"a":3, "b": -4.5}, "y":[{}],
                    "z":"hi","w":{"c":null,"d":true}, "v":null}));
+  });
 
   test('stringify', () {
     // Scalars.
@@ -134,7 +135,6 @@ main() {
      * we try to stringify something that cannot be converted to json.
      */
     expect(() => json.stringify(new TestClass()), throws);
-    });
   });
 }
 
