63c98181b0211389746844a0a732f2b6afaa697d
ager@google.com <ager@google.com>
2013-04-23 12:06:00 +0000

    Fix wrong typed_data change.
    R=asiva@google.com, floitsch@google.com, srdjan@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//14091015
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@21873 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/class_finalizer.cc
+++ runtime/vm/class_finalizer.cc
@@ -1964,7 +1964,7 @@ void ClassFinalizer::VerifyImplicitFieldOffsets() {
     field ^= fields_array.At(0);
     ASSERT(field.Offset() == TypedDataView::data_offset());
     name ^= field.name();
-    expected_name ^= String::New("_typed_data");
+    expected_name ^= String::New("_typedData");
     ASSERT(String::EqualsIgnoringPrivateKey(name, expected_name));
     field ^= fields_array.At(1);
     ASSERT(field.Offset() == TypedDataView::offset_in_bytes_offset());
@@ -1983,7 +1983,7 @@ void ClassFinalizer::VerifyImplicitFieldOffsets() {
   field ^= fields_array.At(0);
   ASSERT(field.Offset() == TypedDataView::data_offset());
   name ^= field.name();
-  expected_name ^= String::New("_typed_data");
+  expected_name ^= String::New("_typedData");
   ASSERT(String::EqualsIgnoringPrivateKey(name, expected_name));
   field ^= fields_array.At(1);
   ASSERT(field.Offset() == TypedDataView::offset_in_bytes_offset());
