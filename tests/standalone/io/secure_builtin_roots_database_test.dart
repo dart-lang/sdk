5d83ba3daceab35c0defded23aa7c8838c71ff21
whesse@google.com <whesse@google.com>
2013-04-29 11:47:16 +0000

    dart:io | Add two more tests for SecureSocket builtin certificates.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//13956011
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@22113 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/standalone/io/secure_builtin_roots_database_test.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+import "dart:io";
+import "dart:uri";
+import "dart:isolate";
+import "dart:async";
+
+void testGoogleUrl() {
+  ReceivePort keepAlive = new ReceivePort();
+  HttpClient client = new HttpClient();
+  client.getUrl(Uri.parse('https://www.google.com'))
+      .then((request) => request.close())
+      .then((response)=> response.last)
+      .then((_) {
+        client.close();
+        keepAlive.close();
+      });
+}
+
+void InitializeSSL() {
+  // If the built-in root certificates aren't loaded, the connection
+  // should signal an error.  Even when an external database is loaded,
+  // they should not be loaded.
+  Path scriptDir = new Path(new Options().script).directoryPath;
+  Path certificateDatabase = scriptDir.append('pkcert');
+  SecureSocket.initialize(database: certificateDatabase.toNativePath(),
+                          password: 'dartdart',
+                          useBuiltinRoots: true);
+}
+
+void main() {
+  InitializeSSL();
+  testGoogleUrl();
+}
