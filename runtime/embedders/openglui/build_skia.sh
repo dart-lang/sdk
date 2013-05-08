3cf4ca80ce7d01ae4d8a8e1fb2bf9bcca6996965
ricow@google.com <ricow@google.com>
2013-05-08 06:22:24 +0000

    Speculative fix for android build.
    It is really hard to follow the directory logic, with DART_DIR being just ../../.. and a few pushd and cd commands thrown in.
    
    I don't particularly like this solution either, this is basically to verify that I am assuming correctly in that this is caused by the relative paths being messed up.
    
    Review URL: https://codereview.chromium.org//14961005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@22494 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/embedders/openglui/build_skia.sh
+++ runtime/embedders/openglui/build_skia.sh
@@ -65,6 +65,7 @@ if [ ${DO_ANDROID} != 0 ] ; then
   gclient sync
 
   export ANDROID_SDK_ROOT=`readlink -f ../android_tools/sdk`
+  export GSUTIL_LOCATION=`readlink -f ../gsutil`
 
   cd trunk
 
@@ -72,7 +73,7 @@ if [ ${DO_ANDROID} != 0 ] ; then
   if [ ${CLEAN} != 0 ] ; then
     ../android/bin/android_make -d $TARGET_ARCH -j clean
   else
-    env -i BUILDTYPE=$BUILD ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT}" PATH="${PATH}:${DART_DIR}/third_party/gsutil" ../android/bin/android_make BUILDTYPE=$BUILD -d $TARGET_ARCH -j --debug=j
+    env -i BUILDTYPE=$BUILD ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT}" PATH="${PATH}:${GSUTIL_LOCATION}" ../android/bin/android_make BUILDTYPE=$BUILD -d $TARGET_ARCH -j --debug=j
   fi
 
 else
