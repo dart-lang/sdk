a7d1dd46f78f55079bdffb89798479fe69d639e7
kasperl@google.com <kasperl@google.com>
2013-05-22 11:50:39 +0000

    Give dart2js.dart and dart2jslib.dart different library names.
    R=ahe@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//15728004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@23009 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/dart2js.dart
+++ sdk/lib/_internal/compiler/implementation/dart2js.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library dart2js;
+library dart2js.cmdline;
 
 import 'dart:async';
 import 'dart:collection' show Queue, LinkedHashMap;
