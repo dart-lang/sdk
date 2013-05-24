c11289d8dce3a27ef22051ce7bd0f94ad2c9c34f
nweiz@google.com <nweiz@google.com>
2013-05-24 22:36:08 +0000

    Throw failure to upload as an ApplicationException.
    R=rnystrom@google.com
    
    Review URL: https://codereview.chromium.org//16023007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@23186 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/pub/lib/src/command_lish.dart
+++ sdk/lib/_internal/pub/lib/src/command_lish.dart
@@ -87,7 +87,7 @@ class LishCommand extends PubCommand {
         // TODO(nweiz): the response may have XML-formatted information about
         // the error. Try to parse that out once we have an easily-accessible
         // XML parser.
-        throw new Exception('Failed to upload the package.');
+        throw new ApplicationException('Failed to upload the package.');
       } else if (urisEqual(Uri.parse(url.origin), Uri.parse(server.origin))) {
         handleJsonError(error.response);
       } else {
