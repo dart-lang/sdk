66e4fdd37f11f24bc65b39cd211d8e3a80cf972e
sra@google.com <sra@google.com>
2013-05-31 01:28:20 +0000

    Revert "Fix bug with super setter and interceptor calling convention."
    Review URL: https://codereview.chromium.org//15812009
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@23452 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/ssa/nodes.dart
+++ sdk/lib/_internal/compiler/implementation/ssa/nodes.dart
@@ -1418,8 +1418,8 @@ class HInvokeSuper extends HInvokeStatic {
 
   HInstruction get value {
     assert(isSetter);
-    // The 'inputs' are [receiver, value] or [interceptor, receiver, value].
-    return inputs.last;
+    // Index 0: 'this'.
+    return inputs[1];
   }
 }
 
