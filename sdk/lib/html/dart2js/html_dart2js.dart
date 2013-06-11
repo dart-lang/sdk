648084af6f8ca337599fc4a640796389af10570d
blois@google.com <blois@google.com>
2013-06-11 15:40:30 +0000

    Bringing dart:html's spawnDomFunction into sync between dart2js and dartium.
    BUG=10221
    
    Review URL: https://codereview.chromium.org//16757005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@23857 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/html/dart2js/html_dart2js.dart
+++ sdk/lib/html/dart2js/html_dart2js.dart
@@ -65,7 +65,8 @@ _callPortSync(int id, message) {
   return JS('var', r'ReceivePortSync.dispatchCall(#, #)', id, message);
 }
 
-spawnDomFunction(Function f) => IsolateNatives.spawnDomFunction(f);
+Future<SendPort> spawnDomFunction(Function f) =>
+  new Future.value(IsolateNatives.spawnDomFunction(f));
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
