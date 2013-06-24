9f9d785362f829134569d7fb2bab6362331acedd
ajohnsen@google.com <ajohnsen@google.com>
2013-06-24 13:14:00 +0000

    Fix 'part of' for mime package.
    BUG=
    R=kasperl@google.com
    
    Review URL: https://codereview.chromium.org//17583010
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24331 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/mime/lib/src/mime_type.dart
+++ pkg/mime/lib/src/mime_type.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-part of mime_type;
+part of mime;
 
 
 MimeTypeResolver _globalResolver = new MimeTypeResolver();
