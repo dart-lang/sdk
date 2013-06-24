54bd4df5de760a865844fb03b494191d5747e4a0
lrn@google.com <lrn@google.com>
2013-06-24 10:31:13 +0000

    Make unmodifiable_collection test import of self use package:-uri.
    R=ajohnsen@google.com
    
    Review URL: https://codereview.chromium.org//17591006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24320 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/unmodifiable_collection/test/unmodifiable_collection_test.dart
+++ pkg/unmodifiable_collection/test/unmodifiable_collection_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "../lib/unmodifiable_collection.dart";
+import "package:unmodifiable_collection/unmodifiable_collection.dart";
 import "package:unittest/unittest.dart";
 
 // Test unmodifiable collection views.
