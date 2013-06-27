f08c9d860a4cb108fc22cc8e802bae12f788d25f
jmesserly@google.com <jmesserly@google.com>
2013-06-27 00:45:10 +0000

    remove warning that is no longer in dart:html TBR to fix dart2js bot.
    Review URL: https://codereview.chromium.org//17982002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24503 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js/analyze_api_test.dart
+++ tests/compiler/dart2js/analyze_api_test.dart
@@ -20,9 +20,6 @@ import 'analyze_helper.dart';
 // TODO(johnniwinther): Support canonical URIs as keys and message kinds as
 // values.
 const Map<String, List<String>> WHITE_LIST = const {
-  'path_observer.dart':
-      const ['Hint: Using "new Symbol"', // Issue 10565.
-            ],
   'sdk/lib/html/dart2js/html_dart2js.dart':
       const ['Hint: The class "Rect" overrides "operator==", '
              'but not "get hashCode".',
