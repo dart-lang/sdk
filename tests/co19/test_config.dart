6c6b88967f719c587b95a3f4f1451f8c8f40b680
scheglov@google.com <scheglov@google.com>
2013-07-03 17:50:34 +0000

    Remove dartc status sections.
    R=brianwilkerson@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//18566003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24735 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/co19/test_config.dart
+++ tests/co19/test_config.dart
@@ -16,7 +16,6 @@ class Co19TestSuite extends StandardTestSuite {
               new Path("tests/co19/src"),
               ["tests/co19/co19-analyzer.status",
                "tests/co19/co19-analyzer2.status",
-               "tests/co19/co19-compiler.status",
                "tests/co19/co19-runtime.status",
                "tests/co19/co19-dart2dart.status",
                "tests/co19/co19-dart2js.status"]);
