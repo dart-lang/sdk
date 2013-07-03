937d3f14833f42a2e6bab2022524169ed8a896ac
sra@google.com <sra@google.com>
2013-07-03 04:20:51 +0000

    Revert "Use ListMixin for typed_data arrays"
    Review URL: https://codereview.chromium.org//18272013
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24714 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/typed_arrays_dataview_test.dart
+++ tests/html/typed_arrays_dataview_test.dart
@@ -16,77 +16,57 @@ main() {
     return;
   }
 
-  test('create', () {
-    var bd = new ByteData(100);
-    expect(bd.lengthInBytes, 100);
-    expect(bd.offsetInBytes, 0);
-
-    var a1 = new Uint8List.fromList([1,2,3,4,5,6,7,8]);
-
-    var bd2 = new ByteData.view(a1.buffer);
-    expect(bd2.lengthInBytes, 8);
-    expect(bd2.offsetInBytes, 0);
-
-    var bd3 = new ByteData.view(a1.buffer, 2);
-    expect(bd3.lengthInBytes, 6);
-    expect(bd3.offsetInBytes, 2);
-
-    var bd4 = new ByteData.view(a1.buffer, 3, 4);
-    expect(bd4.lengthInBytes, 4);
-    expect(bd4.offsetInBytes, 3);
-  });
-
   test('access8', () {
-    var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
+      var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
 
-    var bd = new ByteData.view(a1.buffer, 2, 6);
+      var dv = new ByteData.view(a1.buffer, 2, 6);
 
-    expect(bd.getInt8(0), equals(3));
-    expect(bd.getInt8(1), equals(-1));
-    expect(bd.getUint8(0), equals(3));
-    expect(bd.getUint8(1), equals(255));
+      expect(dv.getInt8(0), equals(3));
+      expect(dv.getInt8(1), equals(-1));
+      expect(dv.getUint8(0), equals(3));
+      expect(dv.getUint8(1), equals(255));
 
-    bd.setInt8(2, -56);
-    expect(bd.getInt8(2), equals(-56));
-    expect(bd.getUint8(2), equals(200));
+      dv.setInt8(2, -56);
+      expect(dv.getInt8(2), equals(-56));
+      expect(dv.getUint8(2), equals(200));
 
-    bd.setUint8(3, 200);
-    expect(bd.getInt8(3), equals(-56));
-    expect(bd.getUint8(3), equals(200));
+      dv.setUint8(3, 200);
+      expect(dv.getInt8(3), equals(-56));
+      expect(dv.getUint8(3), equals(200));
   });
 
   test('access16', () {
-    var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
+      var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
 
-    var bd = new ByteData.view(a1.buffer, 2);
+      var dv = new ByteData.view(a1.buffer, 2);
 
-    expect(bd.lengthInBytes, equals(10 - 2));
+      expect(dv.lengthInBytes, equals(10 - 2));
 
-    expect(bd.getInt16(0), equals(1023));
-    expect(bd.getInt16(0, Endianness.BIG_ENDIAN), equals(1023));
-    expect(bd.getInt16(0, Endianness.LITTLE_ENDIAN), equals(-253));
+      expect(dv.getInt16(0), equals(1023));
+      expect(dv.getInt16(0, Endianness.BIG_ENDIAN), equals(1023));
+      expect(dv.getInt16(0, Endianness.LITTLE_ENDIAN), equals(-253));
 
-    expect(bd.getUint16(0), equals(1023));
-    expect(bd.getUint16(0, Endianness.BIG_ENDIAN), equals(1023));
-    expect(bd.getUint16(0, Endianness.LITTLE_ENDIAN), equals(0xFF03));
+      expect(dv.getUint16(0), equals(1023));
+      expect(dv.getUint16(0, Endianness.BIG_ENDIAN), equals(1023));
+      expect(dv.getUint16(0, Endianness.LITTLE_ENDIAN), equals(0xFF03));
 
-    bd.setInt16(2, -1);
-    expect(bd.getInt16(2), equals(-1));
-    expect(bd.getUint16(2), equals(0xFFFF));
+      dv.setInt16(2, -1);
+      expect(dv.getInt16(2), equals(-1));
+      expect(dv.getUint16(2), equals(0xFFFF));
   });
 
   test('access32', () {
-    var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
+      var a1 = new Uint8List.fromList([0,0,3,255,0,0,0,0,0,0]);
 
-    var bd = new ByteData.view(a1.buffer);
+      var dv = new ByteData.view(a1.buffer);
 
-    expect(bd.getInt32(0), equals(1023));
-    expect(bd.getInt32(0, Endianness.BIG_ENDIAN), equals(1023));
-    expect(bd.getInt32(0, Endianness.LITTLE_ENDIAN), equals(-0xFD0000));
+      expect(dv.getInt32(0), equals(1023));
+      expect(dv.getInt32(0, Endianness.BIG_ENDIAN), equals(1023));
+      expect(dv.getInt32(0, Endianness.LITTLE_ENDIAN), equals(-0xFD0000));
 
-    expect(bd.getUint32(0), equals(1023));
-    expect(bd.getUint32(0, Endianness.BIG_ENDIAN), equals(1023));
-    expect(bd.getUint32(0, Endianness.LITTLE_ENDIAN), equals(0xFF030000));
+      expect(dv.getUint32(0), equals(1023));
+      expect(dv.getUint32(0, Endianness.BIG_ENDIAN), equals(1023));
+      expect(dv.getUint32(0, Endianness.LITTLE_ENDIAN), equals(0xFF030000));
   });
 
 }
