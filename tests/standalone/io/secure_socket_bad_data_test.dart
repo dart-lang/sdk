d3b6076f86a7089099b2f639837f40ceceb80374
whesse@google.com <whesse@google.com>
2013-07-04 12:42:17 +0000

    Refactor standalone/io/secure_socket_bad_data_test.
    BUG=
    R=ajohnsen@google.com
    
    Review URL: https://codereview.chromium.org//18390003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24775 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/standalone/io/secure_socket_bad_data_test.dart
+++ tests/standalone/io/secure_socket_bad_data_test.dart
@@ -29,130 +29,131 @@ const CERTIFICATE = "localhost_cert";
 // this causes an error in the NSS code.  When the NSS code is in debug
 // mode, bad data on the connection can cause an assertion failure, rather
 // than an exception.
-void test(bool hostnameInConnect) {
-  ReceivePort port = new ReceivePort();
 
-  const messageSize = 1000;
+const messageSize = 1000;
 
-  List<int> createTestData() {
-    List<int> data = new List<int>(messageSize);
-    for (int i = 0; i < messageSize; i++) {
-      data[i] = i & 0xff;
-    }
-    return data;
+
+List<int> createTestData() {
+  List<int> data = new List<int>(messageSize);
+  for (int i = 0; i < messageSize; i++) {
+    data[i] = i & 0xff;
   }
-
-  void verifyTestData(List<int> data) {
-    Expect.equals(messageSize, data.length);
-    List<int> expected = createTestData();
-    for (int i = 0; i < messageSize; i++) {
-      Expect.equals(expected[i], data[i]);
-    }
-  }
-
-  Future runServer(RawSocket client) {
-    final completer = new Completer();
-    final dataReceived = new List<int>(messageSize);
-    int bytesRead = 0;
-    client.writeEventsEnabled = false;
-    client.listen((event) {
-      switch (event) {
-        case RawSocketEvent.READ:
-          Expect.isTrue(client.available() > 0);
-          var buffer = client.read(200);
-          dataReceived.setRange(bytesRead, bytesRead + buffer.length, buffer);
-          bytesRead += buffer.length;
-          if (bytesRead == dataReceived.length) {
-            verifyTestData(dataReceived);
-          }
-          break;
-        case RawSocketEvent.WRITE:
-          Expect.fail('WRITE event received');
-          break;
-        case RawSocketEvent.READ_CLOSED:
-          Expect.fail('READ_CLOSED event received');
-          client.shutdown(SocketDirection.SEND);
-          completer.complete(null);
-          break;
-        default: throw "Unexpected event $event";
-      }
-    },
-    onError: (e) {
-      Expect.isTrue(e is TlsException);
-      Expect.isTrue(e.toString().contains(
-          'received a record with an incorrect Message Authentication Code'));
-      completer.complete(null);
-    });
-    return completer.future;
-  }
-
-  Future<RawSocket> runClient(List sockets) {
-    RawSocket baseSocket = sockets[0];
-    RawSecureSocket socket = sockets[1];
-    final completer = new Completer();
-    bool completed = false;
-    void tryComplete() {
-      if (!completed) {
-        completed = true;
-        completer.complete(null);
-      }
-    }
-    final data = createTestData();
-    int bytesWritten = 0;
-    socket.listen((event) {
-      switch (event) {
-        case RawSocketEvent.READ:
-          Expect.fail('READ event received');
-          break;
-        case RawSocketEvent.WRITE:
-          if (bytesWritten < data.length) {
-            bytesWritten +=  socket.write(data, bytesWritten);
-          }
-          if (bytesWritten < data.length) {
-            socket.writeEventsEnabled = true;
-          }
-          if (bytesWritten == data.length) {
-            baseSocket.write(data, 0, 300);
-            socket.shutdown(SocketDirection.SEND);
-          }
-          break;
-        case RawSocketEvent.READ_CLOSED:
-          tryComplete();
-          break;
-        default: throw "Unexpected event $event";
-      }
-    },
-    onError: (e) {
-      Expect.isTrue(e is IOException);
-      tryComplete();
-    });
-    return completer.future;
-  }
-
-
-  Future<List> connectClient(int port) =>
-    RawSocket.connect(HOST_NAME, port)
-    .then((socket) =>
-      (hostnameInConnect ? RawSecureSocket.secure(socket)
-                         : RawSecureSocket.secure(socket, host: HOST_NAME))
-      .then((secureSocket) => [socket, secureSocket]));
-
-
-  serverReady(server) {
-    server.listen((client) {
-      RawSecureSocket.secureServer(client, CERTIFICATE).then((secureClient) {
-        Expect.throws(() => client.add([0]));
-        runServer(secureClient).then((_) => server.close());
-      });
-    });
-
-    connectClient(server.port)
-    .then(runClient)
-    .then((_) => port.close());
-  }
-
-  RawServerSocket.bind(HOST_NAME, 0).then(serverReady);
+  return data;
 }
+
+
+void verifyTestData(List<int> data) {
+  Expect.equals(messageSize, data.length);
+  List<int> expected = createTestData();
+  for (int i = 0; i < messageSize; i++) {
+    Expect.equals(expected[i], data[i]);
+  }
+}
+
+
+Future runServer(RawSocket client) {
+  final completer = new Completer();
+  final dataReceived = new List<int>(messageSize);
+  int bytesRead = 0;
+  client.writeEventsEnabled = false;
+  client.listen((event) {
+    switch (event) {
+      case RawSocketEvent.READ:
+        Expect.isTrue(client.available() > 0);
+        var buffer = client.read(200);
+        dataReceived.setRange(bytesRead, bytesRead + buffer.length, buffer);
+        bytesRead += buffer.length;
+        if (bytesRead == dataReceived.length) {
+          verifyTestData(dataReceived);
+        }
+        break;
+      case RawSocketEvent.WRITE:
+        Expect.fail('WRITE event received');
+        break;
+      case RawSocketEvent.READ_CLOSED:
+        Expect.fail('READ_CLOSED event received');
+        client.shutdown(SocketDirection.SEND);
+        completer.complete(null);
+        break;
+    }
+  },
+  onError: (e) {
+    Expect.isTrue(e is TlsException);
+    Expect.isTrue(e.toString().contains(
+        'received a record with an incorrect Message Authentication Code'));
+    completer.complete(null);
+  });
+  return completer.future;
+}
+
+
+Future<RawSocket> runClient(List sockets) {
+  final RawSocket baseSocket = sockets[0];
+  final RawSecureSocket socket = sockets[1];
+  final data = createTestData();
+  var completer = new Completer();
+  void tryComplete() {
+    if (completer != null) {
+      completer.complete(null);
+      completer = null;
+    }
+  }
+
+  int bytesWritten = 0;
+  socket.listen((event) {
+    switch (event) {
+      case RawSocketEvent.READ:
+        Expect.fail('READ event received');
+        break;
+      case RawSocketEvent.WRITE:
+        if (bytesWritten < data.length) {
+          bytesWritten +=  socket.write(data, bytesWritten);
+        }
+        if (bytesWritten < data.length) {
+          socket.writeEventsEnabled = true;
+        }
+        if (bytesWritten == data.length) {
+          baseSocket.write(data, 0, 300);
+          socket.shutdown(SocketDirection.SEND);
+        }
+        break;
+      case RawSocketEvent.READ_CLOSED:
+        tryComplete();
+        break;
+    }
+  },
+  onError: (e) {
+    Expect.isTrue(e is IOException);
+    tryComplete();
+  });
+  return completer.future;
+}
+
+
+Future<List> connectClient(int port, bool hostnameInConnect) =>
+  RawSocket.connect(HOST_NAME, port)
+  .then((socket) =>
+    (hostnameInConnect ? RawSecureSocket.secure(socket)
+                       : RawSecureSocket.secure(socket, host: HOST_NAME))
+    .then((secureSocket) => [socket, secureSocket]));
+
+
+Future test(bool hostnameInConnect) {
+  return RawServerSocket.bind(HOST_NAME, 0)
+  .then((server) {
+    server.listen((client) {
+      RawSecureSocket.secureServer(client, CERTIFICATE)
+      .then((secureClient) {
+        Expect.throws(() => client.add([0]));
+        return runServer(secureClient);
+      })
+      .then((_) => server.close());
+    });
+
+    return connectClient(server.port, hostnameInConnect).then(runClient);
+  });
+}
+
 
 main() {
   Path scriptDir = new Path(Platform.script).directoryPath;
@@ -160,6 +161,7 @@ main() {
   SecureSocket.initialize(database: certificateDatabase.toNativePath(),
                           password: 'dartdart',
                           useBuiltinRoots: false);
-  test(false);
-  test(true);
+  var keepAlive = new ReceivePort();
+  Future.wait([test(false), test(true)])
+  .then((_) => keepAlive.close());
 }
