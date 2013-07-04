ad95ee86e239d782d47cc311ed5c54f94daa1b03
kustermann@google.com <kustermann@google.com>
2013-07-04 11:55:19 +0000

    Make multiple attempts to write binding files to disc
    Our dartium-win builders are currently flaky on the compile step. Sometimes
    we're not able to write the generated dart bindings to disc (we get an IOError:
    Permission denied).
    
    This change makes sure we do multiple attempts for writing out the generated
    files before we give up. Hopefully we can eliminate this hack as soon as we
    found the root cause.
    
    TBR=efortuna@google.com
    
    Review URL: https://codereview.chromium.org//18676003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@24772 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/dom/scripts/multiemitter.py
+++ tools/dom/scripts/multiemitter.py
@@ -6,9 +6,9 @@
 """Templating to help generate structured text."""
 
 import os
-import re
 import sys
 import subprocess
+import time
 import emitter
 import logging
 
@@ -85,24 +85,33 @@ def _WriteFile(path, lines):
       _logger.info('Warning: File still exists- %s' % path)
 
   # Write the file.
-  try:
-    _logger.info('Writing - %s' % path)
-    f = open(path, 'w')
-    f.writelines(lines)
-    f.close()
-  except IOError as error:
-    # FIXME(kustermann): Remove this later on.
-    # We try to get more debugging information to figure out why we sometimes
-    # get a "Permission denied" error when opening the file for writing.
-    # (hypothesis: Another process has already opened the file.)
-    _logger.info('Got exception (%s) ' % error)
-
-    if sys.platform  == 'win32':
-      handle_file = r'E:\handle.exe'
-      if os.path.exists(handle_file):
-        _logger.info('Running handle.exe for debugging purposes')
-        subprocess.call([handle_file, '-a', r'E:\b\build\slave'])
+  num_attempts = 4
+  for i in range(num_attempts):
+    try:
+      _logger.info('Writing (attempt %d) - %s' % (i + 1, path))
+      f = open(path, 'w')
+      f.writelines(lines)
+      f.close()
+      return
+    except IOError as error:
+      last_attempt = (i == (num_attempts - 1))
+      if not last_attempt:
+        # Sleep for 50 ms and try again
+        time.sleep(0.05)
       else:
-        _logger.info("Couldn't find %s. Not printing open handles."
-                     % handle_file)
-    raise error
+        # FIXME(kustermann): Remove this later on.
+        # We try to get more debugging information to figure out why we
+        # sometimes get a "Permission denied" error when opening the file for
+        # writing. (hypothesis: Another process has already opened the file.)
+        _logger.info('Got exception (%s) ' % error)
+
+        if sys.platform  == 'win32':
+          handle_file = r'E:\handle.exe'
+          if os.path.exists(handle_file):
+            _logger.info('Running handle.exe for debugging purposes')
+            subprocess.call([handle_file, '-a', r'E:\b\build\slave'])
+          else:
+            _logger.info("Couldn't find %s. Not printing open handles."
+                         % handle_file)
+        raise error
+
