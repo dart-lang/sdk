25292ecbf078fe7513836a09b4afe4a61b9a95b7
floitsch@google.com <floitsch@google.com>
2013-07-23 12:28:20 +0000

    Improve toStringX documentations.
    Addresses comments from https://codereview.chromium.org/17567003.
    
    R=lrn@google.com
    
    Review URL: https://codereview.chromium.org//17574005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@25340 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/core/num.dart
+++ sdk/lib/core/num.dart
@@ -165,39 +165,77 @@ abstract class num implements Comparable<num> {
   double toDouble();
 
   /**
-   * Converts `this` to a [double] and returns its string representation with
-   * [fractionDigits] digits after the decimal point.
+   * Returns a decimal-point string-representation of `this`.
+   *
+   * Converts `this` to a [double] before computing the string representation.
+   *
+   * If the absolute value of `this` is greater or equal to `10^21` then this
+   * methods returns an exponential representation computed by
+   * `this.toStringAsExponential()`. Otherwise the result
+   * is the closest string representation with exactly [fractionDigits] digits
+   * after the decimal point. If [fractionDigits] equals 0 then the decimal
+   * point is omitted.
    *
    * The parameter [fractionDigits] must be an integer satisfying:
    * [:0 <= fractionDigits <= 20:].
+   *
+   * Examples:
+   *
+   *     1.toStringAsFixed(3);  // 1.000
+   *     (4321.12345678).toStringAsFixed(3);  // 4321.123
+   *     (4321.12345678).toStringAsFixed(5);  // 4321.12346
+   *     123456789012345678901.toStringAsFixed(3);  // 123456789012345683968.000
+   *     1000000000000000000000.toStringAsFixed(3); // 1e+21
+   *     5.25.toStringAsFixed(0); // 5
    */
   String toStringAsFixed(int fractionDigits);
 
   /**
-   * Converts `this` to a [double] and returns its string representation in
-   * decimal exponential notation with [fractionDigits] digits after the decimal
-   * point.
+   * Returns an exponential string-representation of `this`.
+   *
+   * Converts `this` to a [double] before computing the string representation.
    *
    * If [fractionDigits] is given then it must be an integer satisfying:
-   * [:0 <= fractionDigits <= 20:]. Without the parameter the returned string
-   * uses the shortest number of digits that accurately represent [this].
+   * [:0 <= fractionDigits <= 20:]. In this case the string contains exactly
+   * [fractionDigits] after the decimal point. Otherwise, without the parameter,
+   * the returned string uses the shortest number of digits that accurately
+   * represent [this].
+   *
+   * If [fractionDigits] equals 0 then the decimal point is omitted.
+   * Examples:
+   *
+   *     1.toStringAsExponential();       // 1e+0
+   *     1.toStringAsExponential(3);      // 1.000e+0
+   *     123456.toStringAsExponential();  // 1.23456e+5
+   *     123456.toStringAsExponential(3); // 1.235e+5
+   *     123.toStringAsExponential(0);    // 1e+2
    */
   String toStringAsExponential([int fractionDigits]);
 
   /**
-   * Converts `this` to a double and returns its string representation with
-   * [precision] significant digits.
+   * Converts `this` to a double and returns a string representation with
+   * exactly [precision] significant digits.
    *
    * The parameter [precision] must be an integer satisfying:
    * [:1 <= precision <= 21:].
+   *
+   * Examples:
+   *
+   *     1.toStringAsPrecision(2);       // 1.0
+   *     1e15.toStringAsPrecision(3);    // 1.00+15
+   *     1234567.toStringAsPrecision(3); // 1.23e+6
+   *     1234567.toStringAsPrecision(9); // 1234567.00
+   *     12345678901234567890.toStringAsPrecision(20); // 12345678901234567168
+   *     12345678901234567890.toStringAsPrecision(14); // 1.2345678901235e+19
+   *     0.00000012345.toPrecision(15); // 1.23450000000000e-7
+   *     0.0000012345.toPrecision(15);  // 0.00000123450000000000
    */
   String toStringAsPrecision(int precision);
 
   /**
-   * Computes the shortest string of digits that correctly represent the input
-   * number.
+   * Returns the shortest string that correctly represent the input number.
    *
-   * [double]s in the range `10^-6` (inclusive) to `10^21` (exclusive)
+   * All [double]s in the range `10^-6` (inclusive) to `10^21` (exclusive)
    * are converted to their decimal representation with at least one digit
    * after the decimal point. For all other doubles,
    * except for special values like `NaN` or `Infinity`, this method returns an
@@ -206,27 +244,30 @@ abstract class num implements Comparable<num> {
    * Returns `"NaN"` for [double.NAN], `"Infinity"` for [double.INFINITY], and
    * `"-Infinity"` for [double.MINUS_INFINITY].
    *
-   * [int]s are always converted to their decimal representation.
+   * An [int] is converted to a decimal representation with no decimal point.
    *
    * Examples:
    *
-   *     (0.000001).toString(); // "0.000001"
+   *     (0.000001).toString();  // "0.000001"
    *     (0.0000001).toString(); // "1e-7"
-   *     (111111111111111111111.0).toString(); // "111111111111111110000.0"
-   *     (100000000000000000000.0).toString(); // "100000000000000000000.0"
+   *     (111111111111111111111.0).toString();  // "111111111111111110000.0"
+   *     (100000000000000000000.0).toString();  // "100000000000000000000.0"
    *     (1000000000000000000000.0).toString(); // "1e+21"
    *     (1111111111111111111111.0).toString(); // "1.1111111111111111e+21"
    *     1.toString(); // "1"
-   *     111111111111111111111.toString(); // "111111111111111110000"
-   *     100000000000000000000.toString(); // "100000000000000000000"
+   *     111111111111111111111.toString();  // "111111111111111110000"
+   *     100000000000000000000.toString();  // "100000000000000000000"
    *     1000000000000000000000.toString(); // "1000000000000000000000"
    *     1111111111111111111111.toString(); // "1111111111111111111111"
+   *     1.234e5.toString();   // 123400
+   *     1234.5e6.toString();  // 1234500000
+   *     12.345e67.toString(); // 1.2345e+68
    *
    * Note: the conversion may round the output if the returned string
    * is accurate enough to uniquely identify the input-number.
    * For example the most precise representation of the [double] `9e59` equals
    * `"899999999999999918767229449717619953810131273674690656206848"`, but
-   * this method returns the shorter (but still correct) `"9e59"`.
+   * this method returns the shorter (but still uniquely identifying) `"9e59"`.
    *
    */
   String toString();
