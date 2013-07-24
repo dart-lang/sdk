eca9e4cf6441212d6b983e078a9df3525a1ae68c
ajohnsen@google.com <ajohnsen@google.com>
2013-07-24 12:51:53 +0000

    Add ability to send a ping interval on a WebSocket.
    By enabling it, ping messages can be used to ensure if a websocket
    connection is open. However, as it depends on the remote peer to support
    ping/pong, it's disabled by default.
    
    BUG=https://code.google.com/p/dart/issues/detail?id=7366
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//19970004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@25410 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/standalone/io/web_socket_ping_test.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+// VMOptions=
+// VMOptions=--short_socket_read
+// VMOptions=--short_socket_write
+// VMOptions=--short_socket_read --short_socket_write
+
+import "package:expect/expect.dart";
+import "dart:async";
+import "dart:io";
+
+
+void testPing(int totalConnections) {
+  HttpServer.bind('localhost', 0).then((server) {
+    server.transform(new WebSocketTransformer()).listen((webSocket) {
+      webSocket.pingInterval = const Duration(milliseconds: 100);
+      webSocket.drain();
+    });
+
+    var futures = [];
+    for (int i = 0; i < totalConnections; i++) {
+      futures.add(
+          WebSocket.connect('ws://localhost:${server.port}').then((webSocket) {
+        webSocket.pingInterval = const Duration(milliseconds: 100);
+        webSocket.drain();
+        new Timer(const Duration(seconds: 1), () {
+          // Should not be closed yet.
+          Expect.equals(null, webSocket.closeCode);
+          webSocket.close();
+        });
+        return webSocket.done;
+      }));
+    }
+    Future.wait(futures).then((_) => server.close());
+  });
+}
+
+
+void main() {
+  testPing(10);
+}
