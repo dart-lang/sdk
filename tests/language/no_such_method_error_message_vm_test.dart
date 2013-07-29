d0858b91c870d39237c99f0a2be0f86931e170ce
fschneider@google.com <fschneider@google.com>
2013-07-29 13:44:21 +0000

    Move VM-specific part of a test into a separate vm_test file.
    This way dart2js runs the rest of the tests included in the original file.
    
    BUG=https://code.google.com/p/dart/issues/detail?id=11937
    R=ahe@google.com
    
    Review URL: https://codereview.chromium.org//21038003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@25560 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/language/no_such_method_error_message_vm_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// VMOptions=--optimization-counter-threshold=10 --no-use-osr
+
+import "package:expect/expect.dart";
+
+// Test error message with noSuchMethodError: non-existent names
+// should result in a "method not found" message.
+
+call_bar(x) => x.bar();
+
+testMessage() {
+  try {
+    call_bar(5);
+  } catch (e) {
+    Expect.isTrue(e.toString().indexOf("method not found") != -1);
+  }
+}
+
+
+main() {
+  for (var i = 0; i < 20; i++) testMessage();
+}
+
