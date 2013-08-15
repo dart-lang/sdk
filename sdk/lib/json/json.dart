163a75a47078cf82b998dbef5e2e56d59203c2c4
mcampione@google.com <mcampione@google.com>
2013-08-15 21:01:28 +0000

    Added a one-line description for the library
    BUG=
    R=kathyw@google.com
    
    Review URL: https://codereview.chromium.org//22805007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26218 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/json/json.dart
+++ sdk/lib/json/json.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/**
+ * Utilities for encoding and decoding JSON (JavaScript Object Notation) data.
+ */
+
 library dart.json;
 
 // JSON parsing and serialization.
