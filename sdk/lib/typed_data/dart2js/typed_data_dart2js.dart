b429a53e958d25ffde9ca2f7445ab15940c1f0e9
mcampione@google.com <mcampione@google.com>
2013-08-15 22:29:09 +0000

    Added one-line library description for typed_data library.
    BUG=
    R=kathyw@google.com
    
    Review URL: https://codereview.chromium.org//23263002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26234 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/typed_data/dart2js/typed_data_dart2js.dart
+++ sdk/lib/typed_data/dart2js/typed_data_dart2js.dart
@@ -2,7 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// The Dart TypedData library.
+/**
+ * Specialized integers and floating point numbers,
+ * with SIMD support and efficient lists.
+ */
 library dart.typed_data;
 
 import 'dart:collection';
