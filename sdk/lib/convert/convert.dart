2f3a001e00cde8ea00a2a2d72b5af272ddeaff71
financeCoding@gmail.com <financeCoding@gmail.com>
2013-08-15 21:21:59 +0000

    doc fix for dart:convert library
    BUG=
    R=kathyw@google.com
    
    Review URL: https://codereview.chromium.org//23238004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26223 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/convert/convert.dart
+++ sdk/lib/convert/convert.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/**
+ * Converters for JSON and UTF-8, as well as support for creating additional 
+ * converters.
+ */
 library dart.convert;
 
 import 'dart:async';
