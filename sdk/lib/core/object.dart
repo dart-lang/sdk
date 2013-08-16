c25b2fc08604f40378b12fa70520147c76b5899f
lrn@google.com <lrn@google.com>
2013-08-16 10:55:57 +0000

    Add documentation to Object.== that specifies the requirements.
    Any method overriding == should be an equivalence relation (Reflexive,
    Symmetric, Transitive).
    
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//23089004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26266 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/core/object.dart
+++ sdk/lib/core/object.dart
@@ -23,6 +23,25 @@ class Object {
    * The default behavior for all [Object]s is to return true if and
    * only if [:this:] and [other] are the same object.
    *
+   * Override this method to specify a different equality relation on
+   * a class. The overriding method must still be an equivalence relation.
+   * That is, it must be:
+   *
+   *  * Total: It must return a boolean for all arguments. It should never throw
+   *    or return `null`.
+   *
+   *  * Reflexive: For all objects `o`, `o == o` must be true.
+   *
+   *  * Symmetric: For all objects `o1` and `o2`, `o1 == o2` and `o2 == o1` must
+   *    either both be true, or both be false.
+   *
+   *  * Transitive: For all objects `o1`, `o2`, and `o3`, if `o1 == o2` and
+   *    `o2 == o3` are true, then `o1 == o3` must be true.
+   *
+   * The method should also be consistent over time, so equality of two objects
+   * should not change over time, or at least only change if one of the objects
+   * was modified.
+   *
    * If a subclass overrides the equality operator it should override
    * the [hashCode] method as well to maintain consistency.
    */
