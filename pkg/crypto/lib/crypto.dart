954ae820f4c479cfac949ed076964804e1ea89cc
kathyw@google.com <kathyw@google.com>
2013-08-21 21:32:14 +0000

    add one-line description for crypto library
    R=mcampione@google.com
    
    Review URL: https://codereview.chromium.org//23383007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26475 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/crypto/lib/crypto.dart
+++ pkg/crypto/lib/crypto.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/**
+ * Cryptographic algorithms, with support for hash functions such as
+ * SHA-1, SHA-256, HMAC, and MD5.
+ */
 library crypto;
 
 import 'dart:math';
