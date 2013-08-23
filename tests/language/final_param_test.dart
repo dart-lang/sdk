cf33b82c6cb39bb149fec1ab226a2cf6a30b6f96
sgjesse@google.com <sgjesse@google.com>
2013-08-23 08:14:57 +0000

    Update more test for assignment to final variables
    This is a follow-up to
    https://code.google.com/p/dart/source/detail?r=26519, as there where
    more tests checking assignments to final variables. Changed the
    negative tests to multi-tests for running with the analyzer(s).
    
    The latest spec (0.51) says:
    
    Attempting to assign to a final variable anywhere except in its
    declaration or in a constructor header will cause a runtime error to
    be thrown as discussed below. The assignment will also give rise to a
    static warning.
    
    R=ricow@google.com
    BUG=7062, 11591, 12184
    
    Review URL: https://codereview.chromium.org//23112017
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26583 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/final_param_test.dart
+++ tests/language/final_param_test.dart
@@ -1,12 +1,14 @@
 // Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// Disallow re-assignment of a final static variable.
+// Disallow assignment of parameters marked as final.
 
 class A {
-  static const x = 1;
+  static void test(final x) {
+    x = 2;  /// 01: static type warning, runtime error
+  }
 }
 
 main() {
-  A.x = 2;  // <- reassignment not allowed.
+  A.test(1);
 }
