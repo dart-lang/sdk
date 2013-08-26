c23446c37cb142d143f194dde9072e7ac1aaf236
pquitslund@google.com <pquitslund@google.com>
2013-08-26 16:55:31 +0000

    Updated reference to missing UTF8 codec.
    R=devoncarew@google.com
    
    Review URL: https://codereview.chromium.org//23416005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26657 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/analyzer_experimental/bin/formatter.dart
+++ pkg/analyzer_experimental/bin/formatter.dart
@@ -4,6 +4,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
