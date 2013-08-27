fd1a8cf947b24278365a9bd4f6b0805e89ad504e
whesse@google.com <whesse@google.com>
2013-08-27 12:22:17 +0000

    Make dart:io SecureSocket builtin root certificate test more robust, merge test files.
    BUG=dartbug.com/12770
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//23583002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26709 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/standalone/io/secure_builtin_roots_test.dart
+++ tests/standalone/io/secure_builtin_roots_test.dart
@@ -3,29 +3,87 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import "package:expect/expect.dart";
+import "package:path/path.dart";
 import "dart:io";
 import "dart:isolate";
 import "dart:async";
 
-void testGoogleUrl() {
-  ReceivePort keepAlive = new ReceivePort();
-  HttpClient client = new HttpClient();
-  client.getUrl(Uri.parse('https://www.google.com'))
-      .then((request) => request.close())
-      .then((response) => response.last)
-      .then((_) {
-        client.close();
-        keepAlive.close();
-      });
-}
-
-void InitializeSSL() {
-  // If the built-in root certificates aren't loaded, the connection
-  // should signal an error.
-  SecureSocket.initialize(useBuiltinRoots: true);
-}
-
 void main() {
-  InitializeSSL();
-  testGoogleUrl();
+  var args = new Options().arguments;
+  if (!args.contains('--child')) {
+    runAllTestsInChildProcesses();
+  } else {
+    InitializeSSL(useDatabase: args.contains('--database'),
+                  useBuiltinRoots: args.contains('--builtin-roots'));
+    testGoogleUrl(args.contains('--builtin-roots'));
+  }
 }
+
+void InitializeSSL({bool useDatabase, bool useBuiltinRoots}) {
+  // If the built-in root certificates aren't loaded, the connection
+  // should signal an error.  Even when an external database is loaded,
+  // they should not be loaded.
+  if (useDatabase) {
+    var certificateDatabase = join(dirname(Platform.script), 'pkcert');
+    SecureSocket.initialize(database: certificateDatabase,
+                            password: 'dartdart',
+                            useBuiltinRoots: useBuiltinRoots);
+  } else {
+    SecureSocket.initialize(useBuiltinRoots: useBuiltinRoots);
+  }
+}
+
+void testGoogleUrl(bool expectSuccess) {
+  // We need to use an external server that is backed by a
+  // built-in root certificate authority.
+
+  // First, check if the lookup fails.  If not then run the test.
+  InternetAddress.lookup('www.google.com').then((_) {
+    HttpClient client = new HttpClient();
+    client.getUrl(Uri.parse('https://www.google.com'))
+      .then((request) => request.close())
+      .then((response) {
+        Expect.isTrue(expectSuccess, "Unexpected successful connection");
+        print('SUCCESS');
+        return response.last;
+      })
+      .catchError((error) {
+        // Allow SocketExceptions if www.google.com is unreachable or down.
+        Expect.isTrue((!expectSuccess && error is HandshakeException) ||
+                      error is SocketException);
+        print('SUCCESS');
+      })
+      .whenComplete(client.close);
+  },
+  onError: (e) {
+    // Lookup failed.
+    Expect.isTrue(e is SocketException);
+    print('SUCCESS');
+  });
+}
+
+void runAllTestsInChildProcesses() {
+  Future runChild(List<String> scriptArguments) {
+    return Process.run(Platform.executable,
+                       []..addAll(Platform.executableArguments)
+                         ..add(Platform.script)
+                         ..addAll(scriptArguments))
+    .then((ProcessResult result) {
+      if (result.exitCode != 0 || !result.stdout.contains('SUCCESS')) {
+        print("Client failed");
+        print("  stdout:");
+        print(result.stdout);
+        print("  stderr:");
+        print(result.stderr);
+        Expect.fail('Client subprocess exit code: ${result.exitCode}');
+      }
+    });
+  }
+
+  ReceivePort keepAlive = new ReceivePort();
+  Future.wait([runChild(['--child']),
+               runChild(['--child', '--database']),
+               runChild(['--child', '--builtin-roots']),
+               runChild(['--child', '--builtin-roots', '--database'])])
+      .then((_) => keepAlive.close());
+  }
