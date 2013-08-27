9095fafcce56132bf55204997c57329dd7a5bbd1
kathyw@google.com <kathyw@google.com>
2013-08-27 08:00:09 +0000

    add library descriptions for sequence_zip and the libraries it exports
    R=lrn@google.com
    
    Review URL: https://codereview.chromium.org//22877044
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26691 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/sequence_zip/lib/iterable_zip.dart
+++ pkg/sequence_zip/lib/iterable_zip.dart
@@ -2,6 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/**
+ * Help for combining multiple Iterables into a single Iterable.
+ *
+ * This API is also available as part of the
+ * [sequence_zip](#sequence_zip) library.
+ */
 library iterable_zip;
 
 import "dart:collection";
