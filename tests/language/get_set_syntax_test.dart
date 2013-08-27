0c1248f7310d2dd619c19d41a0d32c26d98ab6f4
iposva@google.com <iposva@google.com>
2013-08-27 17:09:22 +0000

    - Tests should not rely on --compile_all.
    Review URL: https://codereview.chromium.org//23578003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26723 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/get_set_syntax_test.dart
+++ tests/language/get_set_syntax_test.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// VMOptions=--compile_all
 
 var get;
 var get a;            /// 00: compile-time error
@@ -42,5 +41,7 @@ class C2 {
 }
 
 main() {
+  new C0();
   new C1();
+  new C2();
 }
