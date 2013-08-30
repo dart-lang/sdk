fede338905816725dacce164f9543a19ff83ba36
sgjesse@google.com <sgjesse@google.com>
2013-08-30 13:44:48 +0000

    Update language.status and rewrite two tests
    Added issue numbers
    
    Changed two negative tests into mulit-tests.
    
    R=kustermann@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//23761003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@26914 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/constructor9_test.dart
+++ tests/language/constructor9_test.dart
@@ -1,22 +1,18 @@
 // Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// Check that all final instance fields of a class are initialized by constructors.
+
+// Check that all final instance fields of a class are initialized by
+// constructors.
 
 
 class Klass {
   Klass(var v): field_ = v { }
-  final uninitializedFinalField_;
+  final uninitializedFinalField_;  /// 01: compile-time error
   var field_;
 }
 
 
-class Constructor2NegativeTest {
-  static testMain() {
-    var o = new Klass(5);
-  }
-}
-
 main() {
-  Constructor2NegativeTest.testMain();
+  new Klass(5);
 }
