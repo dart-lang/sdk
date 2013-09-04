fe2adfdb62c72e48698a8e718e495318fe8721ad
mcampione@google.com <mcampione@google.com>
2013-09-04 20:32:53 +0000

    Edited one-liner for SVG. Also regenerated indexeddb doc and httprequest.
    BUG=
    R=sethladd@google.com
    
    Review URL: https://codereview.chromium.org//23866005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@27150 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/svg/dart2js/svg_dart2js.dart
+++ sdk/lib/svg/dart2js/svg_dart2js.dart
@@ -1,3 +1,11 @@
+/**
+ * Scalable Vector Graphics:
+ * Two-dimensional vector graphics with support for events and animation.
+ *
+ * For details about the features and syntax of SVG, a W3C standard,
+ * refer to the
+ * [Scalable Vector Graphics Specification](http://www.w3.org/TR/SVG/).
+ */
 library dart.dom.svg;
 
 import 'dart:async';
