87d38da24b54c7148e96c52f573964e3bcf053d5
bak@google.com <bak@google.com>
2013-09-04 11:40:15 +0000

    Annotated the SDK libraries with level of maturity.
    R=kasperl@google.com
    
    Review URL: https://codereview.chromium.org//23583034
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@27114 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/libraries.dart
+++ sdk/lib/_internal/libraries.dart
@@ -24,32 +24,39 @@ const Map<String, LibraryInfo> LIBRARIES = const {
 
   "async": const LibraryInfo(
       "async/async.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/async_patch.dart"),
 
   "chrome": const LibraryInfo(
       "chrome/dart2js/chrome_dart2js.dart",
+      maturity: Maturity.DEPRECATED,
       category: "Client"),
 
   "collection": const LibraryInfo(
       "collection/collection.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/collection_patch.dart"),
 
   "convert": const LibraryInfo(
       "convert/convert.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/convert_patch.dart"),
 
   "core": const LibraryInfo(
       "core/core.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/core_patch.dart"),
 
   "html": const LibraryInfo(
       "html/dartium/html_dartium.dart",
       category: "Client",
+      maturity: Maturity.WEB_STABLE,
       dart2jsPath: "html/dart2js/html_dart2js.dart"),
 
   "html_common": const LibraryInfo(
       "html/html_common/html_common.dart",
       category: "Client",
+      maturity: Maturity.WEB_STABLE,
       dart2jsPath: "html/html_common/html_common_dart2js.dart",
       documented: false,
       implementation: true),
@@ -57,31 +64,38 @@ const Map<String, LibraryInfo> LIBRARIES = const {
   "indexed_db": const LibraryInfo(
       "indexed_db/dartium/indexed_db_dartium.dart",
       category: "Client",
+      maturity: Maturity.WEB_STABLE,
       dart2jsPath: "indexed_db/dart2js/indexed_db_dart2js.dart"),
 
   "io": const LibraryInfo(
       "io/io.dart",
       category: "Server",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/io_patch.dart"),
 
   "isolate": const LibraryInfo(
       "isolate/isolate.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/isolate_patch.dart"),
 
   "js": const LibraryInfo(
       "js/dartium/js_dartium.dart",
       category: "Client",
+      maturity: Maturity.STABLE,
       dart2jsPath: "js/dart2js/js_dart2js.dart"),
 
   "json": const LibraryInfo(
-      "json/json.dart"),
+      "json/json.dart",
+      maturity: Maturity.DEPRECATED),
 
   "math": const LibraryInfo(
       "math/math.dart",
+      maturity: Maturity.STABLE,
       dart2jsPatchPath: "_internal/lib/math_patch.dart"),
 
   "mirrors": const LibraryInfo(
       "mirrors/mirrors.dart",
+      maturity: Maturity.UNSTABLE, 
       dart2jsPatchPath: "_internal/lib/mirrors_patch.dart"),
 
   "nativewrappers": const LibraryInfo(
@@ -93,30 +107,36 @@ const Map<String, LibraryInfo> LIBRARIES = const {
 
   "typed_data": const LibraryInfo(
       "typed_data/typed_data.dart",
+      maturity: Maturity.STABLE,
       dart2jsPath: "typed_data/dart2js/typed_data_dart2js.dart"),
 
   "svg": const LibraryInfo(
-        "svg/dartium/svg_dartium.dart",
-        category: "Client",
-        dart2jsPath: "svg/dart2js/svg_dart2js.dart"),
+      "svg/dartium/svg_dartium.dart",
+      category: "Client",
+      maturity: Maturity.WEB_STABLE,
+      dart2jsPath: "svg/dart2js/svg_dart2js.dart"),
 
   "utf": const LibraryInfo(
-      "utf/utf.dart"),
+      "utf/utf.dart",
+      maturity: Maturity.DEPRECATED),
 
   "web_audio": const LibraryInfo(
-        "web_audio/dartium/web_audio_dartium.dart",
-        category: "Client",
-        dart2jsPath: "web_audio/dart2js/web_audio_dart2js.dart"),
+      "web_audio/dartium/web_audio_dartium.dart",
+      category: "Client",
+      maturity: Maturity.WEB_STABLE,
+      dart2jsPath: "web_audio/dart2js/web_audio_dart2js.dart"),
 
   "web_gl": const LibraryInfo(
-        "web_gl/dartium/web_gl_dartium.dart",
-        category: "Client",
-        dart2jsPath: "web_gl/dart2js/web_gl_dart2js.dart"),
+      "web_gl/dartium/web_gl_dartium.dart",
+      category: "Client",
+      maturity: Maturity.WEB_STABLE,
+      dart2jsPath: "web_gl/dart2js/web_gl_dart2js.dart"),
 
   "web_sql": const LibraryInfo(
-        "web_sql/dartium/web_sql_dartium.dart",
-        category: "Client",
-        dart2jsPath: "web_sql/dart2js/web_sql_dart2js.dart"),
+      "web_sql/dartium/web_sql_dartium.dart",
+      category: "Client",
+      maturity: Maturity.WEB_STABLE,
+      dart2jsPath: "web_sql/dart2js/web_sql_dart2js.dart"),
 
   "_collection-dev": const LibraryInfo(
       "_collection_dev/collection_dev.dart",
@@ -219,14 +239,62 @@ class LibraryInfo {
    */
   final bool implementation;
 
+  /**
+   * States the current maturity of this library.
+   */
+  final Maturity maturity;
+
   const LibraryInfo(this.path, {
                     this.category: "Shared",
                     this.dart2jsPath,
                     this.dart2jsPatchPath,
                     this.implementation: false,
                     this.documented: true,
+                    this.maturity: Maturity.UNSPECIFIED,
                     this.platforms: DART2JS_PLATFORM | VM_PLATFORM});
 
   bool get isDart2jsLibrary => (platforms & DART2JS_PLATFORM) != 0;
   bool get isVmLibrary => (platforms & VM_PLATFORM) != 0;
 }
+
+
+
+/**
+ * Abstraction to capture the maturity of a library.
+ */
+class Maturity {
+  final int level;
+  final String name;
+  final String description;
+  
+  const Maturity(this.level, this.name, this.description);
+  
+  String toString() => "$name: $level\n$description\n";
+
+  static const Maturity DEPRECATED = const Maturity(0, "Deprecated",
+    "This library will be remove before next major release.");
+  
+  static const Maturity EXPERIMENTAL = const Maturity(1, "Experimental",
+    "This library is experimental and will likely change or be removed\n"
+    "in future versions.");
+  
+  static const Maturity UNSTABLE = const Maturity(2, "Unstable",
+    "This library is in still changing and have not yet endured\n"
+    "sufficient real-world testing.\n"
+    "Backwards-compatibility is NOT guaranteed.");
+  
+  static const Maturity WEB_STABLE = const Maturity(3, "Web Stable",
+    "This library is tracking the DOM evolution as defined by WC3.\n"
+    "Backwards-compatibility is NOT guaranteed.");
+
+  static const Maturity STABLE = const Maturity(4, "Stable",
+    "The library is stable. API backwards-compatibility is guaranteed.\n"
+    "However implementation details might change.");
+  
+  static const Maturity LOCKED = const Maturity(5, "Locked",
+    "This library will not change except when serious bugs are encountered.");
+
+  static const Maturity UNSPECIFIED = const Maturity(-1, "Unspecified",
+    "The maturity for this library has not been specified.");
+}
+
