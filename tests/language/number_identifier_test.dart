0b6479cdaf8de3a9a019f626aa3a70e57b62de3c
lrn@google.com <lrn@google.com>
2013-09-05 10:28:29 +0000

    Change language/number_identifier_test to be correct.
    Also make it more comprehensive.
    
    BUG: http://dartbug.com/13014 http://dartbug.com/13015
    
    R=ahe@google.com
    
    Review URL: https://codereview.chromium.org//23868006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@27187 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/number_identifier_test.dart
+++ tests/language/number_identifier_test.dart
@@ -2,7 +2,36 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import "package:expect/expect.dart";
+
 main() {
-  1is int;  /// 01: compile-time error
-  1as int;  /// 02: compile-time error
+  // Integer literals.
+  Expect.isTrue(2is int);
+  Expect.equals(2, 2as int);
+  Expect.isTrue(-2is int);
+  Expect.equals(-2, -2as int);
+  Expect.isTrue(0x10is int);
+  Expect.isTrue(-0x10is int);
+  // "a" will be part of hex literal, the following "s" is an error.
+  0x10as int;  /// 01: compile-time error
+  0x;  /// 04: compile-time error
+
+  // Double literals.
+  Expect.isTrue(2.0is double);
+  Expect.equals(2.0, 2.0as double);
+  Expect.isTrue(-2.0is double);
+  Expect.equals(-2.0, -2.0as double);
+  Expect.isTrue(.2is double);
+  Expect.equals(0.2, .2as double);
+  Expect.isTrue(1e2is double);
+  Expect.equals(1e2, 1e2as double);
+  Expect.isTrue(1e-2is double);
+  Expect.equals(1e-2, 1e-2as double);
+  Expect.isTrue(1e+2is double);
+  Expect.equals(1e+2, 1e+2as double);
+  Expect.throws(() => 1.e+2,                                 /// 05: ok
+                (e) => e is NoSuchMethodError &&             /// 05: continued
+                       e.memberName == const Symbol("e"));   /// 05: continued
+  1e;  /// 02: compile-time error
+  1x;  /// 03: compile-time error
 }
