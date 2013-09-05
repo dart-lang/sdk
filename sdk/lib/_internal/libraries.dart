7e6f6f847f547c80d09d568ca29e00dcbe964832
bak@google.com <bak@google.com>
2013-09-05 11:12:00 +0000

    Changed the maturity of the Isolate library to UNSTABLE (requested by Dan).
    R=kasperl@google.com
    
    Review URL: https://codereview.chromium.org//23548016
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@27191 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/libraries.dart
+++ sdk/lib/_internal/libraries.dart
@@ -75,7 +75,7 @@ const Map<String, LibraryInfo> LIBRARIES = const {
 
   "isolate": const LibraryInfo(
       "isolate/isolate.dart",
-      maturity: Maturity.STABLE,
+      maturity: Maturity.UNSTABLE,
       dart2jsPatchPath: "_internal/lib/isolate_patch.dart"),
 
   "js": const LibraryInfo(
