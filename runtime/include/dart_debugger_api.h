cac607707a1ade7bdbcdab9ff6c4b5d11c806808
jacobr@google.com <jacobr@google.com>
2013-09-17 20:52:22 +0000

    Make Dart_ScriptGetTokenInfo account for line_offset() and col_offset()
    Cleanup bugs in wrapExpressionAsClosure Fix bug in how setters were handled and filter out 'this' from local variables list.
    
    BUG=
    R=hausner@google.com
    
    Review URL: https://codereview.chromium.org//23526056
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@27581 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/include/dart_debugger_api.h
+++ runtime/include/dart_debugger_api.h
@@ -147,8 +147,8 @@ DART_EXPORT Dart_Handle Dart_ScriptGetSource(
  * Returns an array of numbers. Null values indicate the beginning of
  * a new line. The first number after null is the line number.
  * The line number is followed by pairs of numbers, with the first value
- * being the "token offset" and the second value being the character offset
- * of the token relative to the beginning of the script.
+ * being the "token offset" and the second value being the column number
+ * of the token.
  * The "token offset" is a value that is used to indicate a location
  * in code, similarly to a "PC" address.
  * Source lines with no tokens are omitted.
