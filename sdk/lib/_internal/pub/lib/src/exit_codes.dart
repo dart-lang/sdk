17afd679a997039cde25a67696ebd1f2813df39f
kevmoo@j832.com <kevmoo@j832.com>
2013-10-01 17:41:19 +0000

    pub exit codes should be const
    R=rnystrom@google.com
    
    Review URL: https://codereview.chromium.org//25531003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@28105 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/pub/lib/src/exit_codes.dart
+++ sdk/lib/_internal/pub/lib/src/exit_codes.dart
@@ -10,46 +10,46 @@
 library pub.exit_codes;
 
 /// The command was used incorrectly.
-final USAGE = 64;
+const USAGE = 64;
 
 /// The input data was incorrect.
-final DATA = 65;
+const DATA = 65;
 
 /// An input file did not exist or was unreadable.
-final NO_INPUT = 66;
+const NO_INPUT = 66;
 
 /// The user specified did not exist.
-final NO_USER = 67;
+const NO_USER = 67;
 
 /// The host specified did not exist.
-final NO_HOST = 68;
+const NO_HOST = 68;
 
 /// A service is unavailable.
-final UNAVAILABLE = 69;
+const UNAVAILABLE = 69;
 
 /// An internal software error has been detected.
-final SOFTWARE = 70;
+const SOFTWARE = 70;
 
 /// An operating system error has been detected.
-final OS = 71;
+const OS = 71;
 
 /// Some system file did not exist or was unreadable.
-final OS_FILE = 72;
+const OS_FILE = 72;
 
 /// A user-specified output file cannot be created.
-final CANT_CREATE = 73;
+const CANT_CREATE = 73;
 
 /// An error occurred while doing I/O on some file.
-final IO = 74;
+const IO = 74;
 
 /// Temporary failure, indicating something that is not really an error.
-final TEMP_FAIL = 75;
+const TEMP_FAIL = 75;
 
 /// The remote system returned something invalid during a protocol exchange.
-final PROTOCOL = 76;
+const PROTOCOL = 76;
 
 /// The user did not have sufficient permissions.
-final NO_PERM = 77;
+const NO_PERM = 77;
 
 /// Something was unconfigured or mis-configured.
-final CONFIG = 78;
+const CONFIG = 78;
