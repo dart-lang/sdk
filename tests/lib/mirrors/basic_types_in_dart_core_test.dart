d278f9f3c4b33c7de312ac725e2ef860cb553a8f
rmacnak@google.com <rmacnak@google.com>
2013-10-14 17:07:32 +0000

    New mirror tests: basic types found in dart:core and member collections are immutable.
    R=ahe@google.com, gbracha@google.com
    
    Review URL: https://codereview.chromium.org//27069003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@28591 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/lib/mirrors/basic_types_in_dart_core_test.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library test.basic_types_in_dart_core;
+
+import 'dart:mirrors';
+import 'package:expect/expect.dart';
+
+main() {
+  LibraryMirror dartcore = currentMirrorSystem().findLibrary(#dart.core).single;
+  ClassMirror cm;
+
+  cm = dartcore.classes[#int];
+  Expect.equals(reflectClass(int), cm);
+  Expect.equals(#int, cm.simpleName);
+
+  cm = dartcore.classes[#double];
+  Expect.equals(reflectClass(double), cm);
+  Expect.equals(#double, cm.simpleName);
+
+  cm = dartcore.classes[#num];  /// 01: ok
+  Expect.equals(reflectClass(num), cm);  /// 01: ok
+  Expect.equals(#num, cm.simpleName);  /// 01: ok
+
+  cm = dartcore.classes[#bool];
+  Expect.equals(reflectClass(bool), cm);
+  Expect.equals(#bool, cm.simpleName);
+
+  cm = dartcore.classes[#String];
+  Expect.equals(reflectClass(String), cm);
+  Expect.equals(#String, cm.simpleName);
+
+  cm = dartcore.classes[#List];
+  Expect.equals(reflectClass(List), cm);
+  Expect.equals(#List, cm.simpleName);
+
+  cm = dartcore.classes[#Null];
+  Expect.equals(reflectClass(Null), cm);
+  Expect.equals(#Null, cm.simpleName);
+
+  cm = dartcore.classes[#Object];
+  Expect.equals(reflectClass(Object), cm);
+  Expect.equals(#Object, cm.simpleName);
+
+  cm = dartcore.classes[#dynamic];
+  Expect.isNull(cm);
+
+  cm = dartcore.classes[const Symbol('void')];
+  Expect.isNull(cm);
+}
