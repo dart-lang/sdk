faba61a26137b5af6e5e091a2b88880e8988943d
ajohnsen@google.com <ajohnsen@google.com>
2013-10-24 08:11:27 +0000

    Copy 'dart:platform' library in to sdk.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//39633002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@29158 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/create_sdk.py
+++ tools/create_sdk.py
@@ -204,7 +204,7 @@ def Main(argv):
                   join('html', 'dart2js'), join('html', 'dartium'),
                   join('html', 'html_common'),
                   join('indexed_db', 'dart2js'), join('indexed_db', 'dartium'),
-                  'js', 'json', 'math', 'mirrors', 'typed_data',
+                  'js', 'json', 'math', 'mirrors', 'platform', 'typed_data',
                   join('svg', 'dart2js'), join('svg', 'dartium'),
                   'utf',
                   join('web_audio', 'dart2js'), join('web_audio', 'dartium'),
