4f580969d81c7ea20be13f60b37115594778fbe0
rmacnak@google.com <rmacnak@google.com>
2013-10-30 22:09:54 +0000

    Tests and docs for instanceMembers, staticMembers, toplevelMembers.
    BUG=http://dartbug.com/14632
    BUG=http://dartbug.com/14633
    R=ahe@google.com, gbracha@google.com
    
    Review URL: https://codereview.chromium.org//44813004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@29593 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/mirrors/mirrors.dart
+++ sdk/lib/mirrors/mirrors.dart
@@ -578,6 +578,18 @@ abstract class LibraryMirror implements DeclarationMirror, ObjectMirror {
   Map<Symbol, DeclarationMirror> get declarations;
 
   /**
+   * Returns a map of the top-level methods, getters and setters of the library.
+   *
+   * The intent is to capture those members that constitute the API of a
+   * library. Hence fields are not included, but the getters and setters 
+   * implicitly introduced by fields are included. Synthetic getters for the
+   * types exported by the library are also included.
+   *
+   * The map is keyed by the simple names of the members.
+   */
+  Map<Symbol, MethodMirror> get topLevelMembers;
+
+  /**
    * Returns [:true:] if this mirror is equal to [other].
    * Otherwise returns [:false:].
    *
@@ -709,6 +721,32 @@ abstract class ClassMirror implements TypeMirror, ObjectMirror {
    * This does not include inherited members.
    */
   Map<Symbol, DeclarationMirror> get declarations;
+
+  /**
+   * Returns a map of the methods, getters and setters of an instance of the
+   * class.
+   *
+   * The intent is to capture those members that constitute the API of an
+   * instance. Hence fields are not included, but the getters and setters
+   * implicitly introduced by fields are included. The map includes methods,
+   * getters and setters that are inherited as well as those introduced by the
+   * class itself.
+   *
+   * The map is keyed by the simple names of the members.
+   */
+  Map<Symbol, MethodMirror> get instanceMembers;
+
+  /**
+   * Returns a map of the static methods, getters and setters of the class.
+   *
+   * The intent is to capture those members that constitute the API of a class.
+   * Hence fields are not included, but the getters and setters implicitly
+   * introduced by fields are included.
+   *
+   * The map is keyed by the simple names of the members.
+   */
+  Map<Symbol, MethodMirror> get staticMembers;
+
 
   /**
    * The mixin of this class.
---
+++ tests/lib/mirrors/instance_members_test.dart
@@ -0,0 +1,54 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library test.instance_members;
+
+import 'dart:mirrors';
+import 'package:expect/expect.dart';
+
+import 'declarations_model.dart' as declarations_model;
+
+selectKeys(map, predicate) {
+  return map.keys.where((key) => predicate(map[key]));
+}
+
+main() {
+  ClassMirror cm = reflectClass(declarations_model.Class);
+
+  Expect.setEquals(
+    [#+,
+     #instanceVariable,
+     const Symbol('instanceVariable='),
+     #instanceGetter,
+     const Symbol('instanceSetter='),
+     #instanceMethod,
+     #-,
+     #inheritedInstanceVariable,
+     const Symbol('inheritedInstanceVariable='),
+     #inheritedInstanceGetter,
+     const Symbol('inheritedInstanceSetter='),
+     #inheritedInstanceMethod,
+     #*,
+     #mixinInstanceVariable,
+     const Symbol('mixinInstanceVariable='),
+     #mixinInstanceGetter,
+     const Symbol('mixinInstanceSetter='),
+     #mixinInstanceMethod,
+     #hashCode,
+     #runtimeType,
+     #==,
+     #noSuchMethod,
+     #toString],
+    selectKeys(cm.instanceMembers, (dm) => !dm.isPrivate));
+  // Filter out private to avoid implementation-specific members of Object.
+
+  Expect.setEquals(
+    [#instanceVariable,
+     const Symbol('instanceVariable='),
+     #inheritedInstanceVariable,
+     const Symbol('inheritedInstanceVariable='),
+     #mixinInstanceVariable,
+     const Symbol('mixinInstanceVariable=')],
+    selectKeys(cm.instanceMembers, (dm) => dm.isSynthetic));
+}
