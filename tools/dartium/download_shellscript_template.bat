75005b1d31a818e64d80eb0a3941e200feef0f11
kustermann@google.com <kustermann@google.com>
2013-11-05 14:42:04 +0000

    editor/build/build.py: Use 32-bit version of dartium/content_shell on macos/windows + bat script fixes
    R=ricow@google.com
    
    Review URL: https://codereview.chromium.org//59623003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@29909 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/dartium/download_shellscript_template.bat
+++ tools/dartium/download_shellscript_template.bat
@@ -1,14 +1,16 @@
-REM Copyright (c) 2013, the Dart project authors. Please see the AUTHORS file
-REM for details. All rights reserved. Use of this source code is governed by a
-REM BSD-style license that can be found in the LICENSE file.
+@REM Copyright (c) 2013, the Dart project authors. Please see the AUTHORS file
+@REM for details. All rights reserved. Use of this source code is governed by a
+@REM BSD-style license that can be found in the LICENSE file.
 
-REM This script will download VAR_DOWNLOAD_URL to VAR_DESTINATION in the current
-REM working directory.
+@REM This script will download VAR_DOWNLOAD_URL to VAR_DESTINATION in the
+@REM current working directory.
 
-CHROMIUM_DIR="%~dp0"
-SDK_BIN="%CHROMIUM_DIR%\..\dart-sdk\bin"
+@echo off
 
-DART="%SDK_BIN%\dart.exe"
-DOWNLOAD_SCRIPT="%CHROMIUM_DIR%\download_file.dart"
+set "CHROMIUM_DIR=%~dp0"
+set "SDK_BIN=%CHROMIUM_DIR%\..\dart-sdk\bin"
+
+set "DART=%SDK_BIN%\dart.exe"
+set "DOWNLOAD_SCRIPT=%CHROMIUM_DIR%\download_file.dart"
 
 "%DART%" "%DOWNLOAD_SCRIPT%" "VAR_DOWNLOAD_URL" "VAR_DESTINATION"
