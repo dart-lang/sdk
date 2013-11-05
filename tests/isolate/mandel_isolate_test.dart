56c59b638c823a06c041a8fcb66e1fdaaabdf993
floitsch@google.com <floitsch@google.com>
2013-11-05 23:24:50 +0000

    Fix warning in mandel_isolate_test.
    BUG= http://dartbug.com/14782
    R=kustermann@google.com
    
    Review URL: https://codereview.chromium.org//61233002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@29944 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/isolate/mandel_isolate_test.dart
+++ tests/isolate/mandel_isolate_test.dart
@@ -14,7 +14,8 @@ const ISOLATES = 20;
 
 main() {
   // Test is really slow in debug builds of the VM.
-  unittestConfiguration.timeout = const Duration(seconds: 480);
+  SimpleConfiguration configuration = unittestConfiguration;
+  configuration.timeout = const Duration(seconds: 480);
   test("Render Mandelbrot in parallel", () {
     final state = new MandelbrotState();
     state._validated.future.then(expectAsync1((result) {
