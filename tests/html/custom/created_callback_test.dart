43d64b38f8d13103f51085048527eb0f6e8f1abe
regis@google.com <regis@google.com>
2013-11-05 21:08:25 +0000

    Fix typo in html test.
    Review URL: https://codereview.chromium.org//59843006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@29936 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/html/custom/created_callback_test.dart
+++ tests/html/custom/created_callback_test.dart
@@ -215,7 +215,7 @@ class AccessWhileUpgradingElement extends HtmlElement {
     var child = upgradingContext.firstChild;
 
     expect(child.foo, 666);
-    expect(upgradingContextChild is HTMLElement, isTrue);
+    expect(upgradingContextChild is HtmlElement, isTrue);
     expect(upgradingContextChild is AccessWhileUpgradingElement, isFalse,
         reason: 'Elements accessed while upgrading should not be upgraded.');
   }
