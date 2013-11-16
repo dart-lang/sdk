69f1243709818092165b4f11199b05665142a116
nweiz@google.com <nweiz@google.com>
2013-11-16 01:10:23 +0000

    Add a missing import to mac_os_watcher.
    TBR
    
    Review URL: https://codereview.chromium.org//68523020
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@30330 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/watcher/test/directory_watcher/mac_os_test.dart
+++ pkg/watcher/test/directory_watcher/mac_os_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:scheduled_test/scheduled_test.dart';
 import 'package:watcher/src/directory_watcher/mac_os.dart';
 import 'package:watcher/watcher.dart';
