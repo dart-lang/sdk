8d3c15767b3613efd61fb230e53d0e32df607b22
lrn@google.com <lrn@google.com>
2013-11-19 11:32:35 +0000

    Growable typed data buffers.
    Like growable lists, but with typed data.
    
    Added to package:collection_helpers.
    
    R=floitsch@google.com
    
    Review URL: https://codereview.chromium.org//26695002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@30396 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/collection_helpers/lib/all.dart
+++ pkg/collection_helpers/lib/all.dart
@@ -6,13 +6,16 @@
  * Exports all the individual parts of the collection-helper library.
  *
  * The sub-libraries of this package are:
+ *
  * - `algorithms.dart`: Algorithms that work on lists (shuffle, binary search
  *   and various sorting algorithms).
  * - `wrappers.dart`: Wrapper classes that delegate to a collection object.
  * - `equality.dart`: Different notions of equality of collections.
+ * - `typed_buffers.dart`: Growable typed data lists.
  */
 library dart.collection_helper;
 
 export "algorithms.dart";
-export "wrappers.dart";
 export "equality.dart";
+export "typed_buffers.dart";
+export "wrappers.dart";
