b149db19e3b1a4b6991cae3d9e1fe9df1f9ea0cf
rmacnak@google.com <rmacnak@google.com>
2013-12-11 00:21:46 +0000

    Changes to generated code for templatized DartDOMWrapper::lookupWrapper.
    R=asiva@google.com
    
    Review URL: https://codereview.chromium.org//100923004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@31050 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/dom/scripts/systemnative.py
+++ tools/dom/scripts/systemnative.py
@@ -423,7 +423,7 @@ class DartiumBackend(HtmlDartGenerator):
         '            return Dart_Null();\n'
         '        DartDOMData* domData = DartDOMData::current();\n'
         '        Dart_WeakPersistentHandle result =\n'
-        '            DartDOMWrapper::lookupWrapper(domData, isNode, value);\n'
+        '            DartDOMWrapper::lookupWrapper<Dart$(INTERFACE)>(domData, value);\n'
         '        if (result)\n'
         '            return Dart_HandleFromWeakPersistent(result);\n'
         '        return createWrapper(domData, value);\n'
@@ -435,14 +435,14 @@ class DartiumBackend(HtmlDartGenerator):
         '            DartDOMData* domData = static_cast<DartDOMData*>(\n'
         '                Dart_GetNativeIsolateData(args));\n'
         '            Dart_WeakPersistentHandle result =\n'
-        '                DartDOMWrapper::lookupWrapper(domData, isNode, value);\n'
+        '                DartDOMWrapper::lookupWrapper<Dart$(INTERFACE)>(domData, value);\n'
         '            if (result)\n'
         '                Dart_SetWeakHandleReturnValue(args, result);\n'
         '            else\n'
         '                Dart_SetReturnValue(args, createWrapper(domData, value));\n'
         '        }\n'
         '    }\n',
-        )
+        INTERFACE=self._interface.id)
 
     if ('CustomToV8' in ext_attrs or
         'PureInterface' in ext_attrs or
