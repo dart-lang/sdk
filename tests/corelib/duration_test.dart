1fa76c85b5da3202dcc2192b75d5272215f8e009
lrn@google.com <lrn@google.com>
2013-12-20 07:18:21 +0000

    Fix bug in Duration.toString.
    BUG= http://dartbug.com/15678
    
    Review URL: https://codereview.chromium.org//119263002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@31318 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/corelib/duration_test.dart
+++ tests/corelib/duration_test.dart
@@ -225,4 +225,46 @@ main() {
 
   d = const Duration(hours: -1999, minutes: -17, seconds: -42);
   Expect.equals("-1999:17:42.000000", d.toString());
+
+  // Edge conditions for toString of microseconds.
+  // Regression test for http://dartbug.com/15678
+
+  d = const Duration(microseconds: 1);
+  Expect.equals("0:00:00.000001", d.toString());
+
+  d = const Duration(microseconds: 9);
+  Expect.equals("0:00:00.000009", d.toString());
+
+  d = const Duration(microseconds: 10);
+  Expect.equals("0:00:00.000010", d.toString());
+
+  d = const Duration(microseconds: 99);
+  Expect.equals("0:00:00.000099", d.toString());
+
+  d = const Duration(microseconds: 100);
+  Expect.equals("0:00:00.000100", d.toString());
+
+  d = const Duration(microseconds: 999);
+  Expect.equals("0:00:00.000999", d.toString());
+
+  d = const Duration(microseconds: 1000);
+  Expect.equals("0:00:00.001000", d.toString());
+
+  d = const Duration(microseconds: 9999);
+  Expect.equals("0:00:00.009999", d.toString());
+
+  d = const Duration(microseconds: 10000);
+  Expect.equals("0:00:00.010000", d.toString());
+
+  d = const Duration(microseconds: 99999);
+  Expect.equals("0:00:00.099999", d.toString());
+
+  d = const Duration(microseconds: 100000);
+  Expect.equals("0:00:00.100000", d.toString());
+
+  d = const Duration(microseconds: 999999);
+  Expect.equals("0:00:00.999999", d.toString());
+
+  d = const Duration(microseconds: 1000000);
+  Expect.equals("0:00:01.000000", d.toString());
 }
