d31141e045c8d6da6e5be8378cf3acfef6ac3ce2
kevmoo@google.com <kevmoo@google.com>
2013-12-20 19:47:33 +0000

    pkg/polymer: remove css_lib dependency
    Subsumes https://codereview.chromium.org/63693002/
    
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org//103783005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@31346 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/polymer/lib/src/build/runner.dart
+++ pkg/polymer/lib/src/build/runner.dart
@@ -110,9 +110,8 @@ Map<String, String> _readPackageDirsFromPub(String currentPackage) {
 // TODO(sigmund): consider computing this list by recursively parsing
 // pubspec.yaml files in the `Options.packageDirs`.
 final Set<String> _polymerPackageDependencies = [
-    'analyzer', 'args', 'barback', 'browser', 'csslib',
-    'custom_element', 'fancy_syntax', 'html5lib', 'html_import', 'js',
-    'logging', 'meta', 'mutation_observer', 'observe', 'path'
+    'analyzer', 'args', 'barback', 'browser', 'custom_element', 'html5lib',
+    'html_import', 'js', 'logging', 'mutation_observer', 'observe', 'path'
     'polymer_expressions', 'serialization', 'shadow_dom', 'source_maps',
     'stack_trace', 'template_binding', 'unittest', 'unmodifiable_collection',
     'yaml'].toSet();
