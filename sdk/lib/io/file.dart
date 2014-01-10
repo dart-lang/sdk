2b8da1ade83a5d7f6e71ee7d7150014e0846c070
sgjesse@google.com <sgjesse@google.com>
2014-01-10 12:30:29 +0000

    Add documentation to the FileSystemException class
    Also update toString slightly.
    
    R=ajohnsen@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//131583004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@31692 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/io/file.dart
+++ sdk/lib/io/file.dart
@@ -561,10 +561,34 @@ abstract class RandomAccessFile {
 }
 
 
+/**
+ * Exception thrown when a file operation fails.
+ */
 class FileSystemException implements IOException {
+  /**
+   * Message describing the error. This does not include any detailed
+   * information form the underlying OS error. Check [osError] for
+   * that information.
+   */
   final String message;
+
+  /**
+   * The file system path on which the error occurred. Can be `null`
+   * if the exception does not relate directly to a file system path.
+   */
   final String path;
+
+  /**
+   * The underlying OS error. Can be `null` if the exception is not
+   * raised due to an OS error.
+   */
   final OSError osError;
+
+  /**
+   * Creates a new FileSystemException with an optional error message
+   * [message], optional file system path [path] and optional OS error
+   * [osError].
+   */
   const FileSystemException([this.message = "", this.path = "", this.osError]);
 
   String toString() {
@@ -573,16 +597,18 @@ class FileSystemException implements IOException {
     if (!message.isEmpty) {
       sb.write(": $message");
       if (path != null) {
-        sb.write(", path = $path");
+        sb.write(", path = '$path'");
       }
       if (osError != null) {
         sb.write(" ($osError)");
       }
     } else if (osError != null) {
-      sb.write(": osError");
+      sb.write(": $osError");
       if (path != null) {
-        sb.write(", path = $path");
+        sb.write(", path = '$path'");
       }
+    } else if (path != null) {
+      sb.write(": $path");
     }
     return sb.toString();
   }
