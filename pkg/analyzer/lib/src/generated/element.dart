7a418f273fbefd45ed4fb3c63a5b10fe2a411046
scheglov@google.com <scheglov@google.com>
2014-01-13 16:01:36 +0000

    Add copyright header to generated analyzer files.
    R=brianwilkerson@google.com, danrubel@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//136143002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@31743 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/analyzer/lib/src/generated/element.dart
+++ pkg/analyzer/lib/src/generated/element.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // This code was auto-generated, is not intended to be edited, and is subject to
 // significant change. Please see the README file for more information.
 
