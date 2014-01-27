43c9a6c78ce231ef00c3a11bb45ec964de3f1a25
floitsch@google.com <floitsch@google.com>
2014-01-27 16:29:55 +0000

    Revert "Fix closures in fields that could be intercepted."
    This reverts commit r32018.
    
    Review URL: https://codereview.chromium.org//131623003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32021 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/js_emitter/container_builder.dart
+++ sdk/lib/_internal/compiler/implementation/js_emitter/container_builder.dart
@@ -233,23 +233,19 @@ class ContainerBuilder extends CodeEmitterHelper {
     // If the method is intercepted, the stub gets the
     // receiver explicitely and we need to pass it to the getter call.
     bool isInterceptedMethod = backend.isInterceptedMethod(member);
-    bool isInterceptorClass =
-        backend.isInterceptorClass(member.getEnclosingClass());
 
     const String receiverArgumentName = r'$receiver';
 
     jsAst.Expression buildGetter() {
-      jsAst.Expression receiver =
-          js(isInterceptorClass ? receiverArgumentName : 'this');
       if (member.isGetter()) {
         String getterName = namer.getterName(member);
-        return receiver[getterName](
-            isInterceptedMethod ?
-            <jsAst.Expression>[receiver] :
-            <jsAst.Expression>[]);
+        return js('this')[getterName](
+            isInterceptedMethod
+                ? <jsAst.Expression>[js(receiverArgumentName)]
+                : <jsAst.Expression>[]);
       } else {
         String fieldName = namer.instanceFieldPropertyName(member);
-        return receiver[fieldName];
+        return js('this')[fieldName];
       }
     }
 
