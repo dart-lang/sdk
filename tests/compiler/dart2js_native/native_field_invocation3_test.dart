8e5378dcf59164894bca65e67c6ee790b113acc2
floitsch@google.com <floitsch@google.com>
2014-01-28 17:56:42 +0000

    Fix closures in fields that could be intercepted.
    BUG= http://dartbug.com/16068
    R=sra@google.com
    
    Committed: https://code.google.com/p/dart/source/detail?r=32018
    Reverted: https://code.google.com/p/dart/source/detail?r=32021
    
    Review URL: https://codereview.chromium.org//140833007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32073 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js_native/native_field_invocation3_test.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+makeCC() native;
+
+void setup() native """
+function CC() {}
+makeCC = function() { return new CC; }
+""";
+
+
+class ClickCounter native "CC" {
+  var status;
+
+  var foo;
+
+  init() {
+    foo = wrap(g);
+  }
+
+  g(val) => "### $val ###";
+}
+
+wrap(cb) {
+  return (val) {
+    return cb("!!! $val !!!");
+  };
+}
+
+main() {
+  setup();
+  var c = makeCC();
+  c.init();
+  // `foo` contains a closure. Make sure that invoking foo through an
+  // interceptor works.
+  Expect.equals("### !!! 499 !!! ###", c.foo(499));
+}
---
+++ tests/compiler/dart2js_native/native_field_invocation6_test.dart
@@ -0,0 +1,53 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+makeA() native;
+nativeFirst(x, y) native;
+
+void setup() native """
+nativeFirst = function(x, y) { return x; }
+
+function A() {}
+makeA = function() { return new A; }
+""";
+
+
+class A native "A" {
+  var _foo;
+
+  get foo => _foo;
+
+  init() {
+    _foo = () => 42;
+  }
+}
+
+class B {
+  var foo = 499;
+}
+
+class C {
+  foo() => 499;
+}
+
+class D {
+  var foo = () => 499;
+}
+
+main() {
+  setup();
+  var a = makeA();
+  a.init();
+  var b = new B();
+  var c = new C();
+  var d = new D();
+  a = nativeFirst(a, b);
+  a = nativeFirst(a, c);
+  a = nativeFirst(a, d);
+  // The variable a is still an instance of class A, but dart2js can't infer
+  // that anymore.
+  Expect.equals(42, a.foo());
+}
