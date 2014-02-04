ee2258d2e2a0a91cc6b9a4a0cb2707930dfe39ab
floitsch@google.com <floitsch@google.com>
2014-02-04 12:40:13 +0000

    Infer types for compile-time constant closures.
    BUG= http://dartbug.com/16448
    R=johnniwinther@google.com
    
    Committed: https://code.google.com/p/dart/source/detail?r=32276
    Reverted: https://code.google.com/p/dart/source/detail?r=32278
    
    Review URL: https://codereview.chromium.org//147353010
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32279 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js/simple_inferrer_global_field_closure2_test.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:expect/expect.dart';
+import "package:async_helper/async_helper.dart";
+import 'compiler_helper.dart';
+
+const String TEST = """
+
+method(a) {  // Called only via [foo] with a small integer.
+  return a;
+}
+
+var foo = method;
+
+returnInt() {
+  return foo(54);
+}
+
+main() {
+  returnInt();
+}
+""";
+
+
+void main() {
+  Uri uri = new Uri(scheme: 'source');
+  var compiler = compilerFor(TEST, uri);
+  asyncTest(() => compiler.runCompiler(uri).then((_) {
+    var typesInferrer = compiler.typesTask.typesInferrer;
+
+    checkArgument(String functionName, type) {
+      var functionElement = findElement(compiler, functionName);
+      var signature = functionElement.functionSignature;
+      var element = signature.requiredParameters.first;
+      Expect.equals(type,
+          typesInferrer.getTypeOfElement(element).simplify(compiler),
+          functionName);
+    }
+
+    checkArgument('method', compiler.typesTask.uint31Type);
+  }));
+}
