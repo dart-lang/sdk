1c761a45a1a563c92a32c11b74c615dfc29b42bd
rnystrom@google.com <rnystrom@google.com>
2014-02-11 18:38:15 +0000

    Test that .dart files are copied to packages/ in debug mode.
    BUG=https://code.google.com/p/dart/issues/detail?id=16567
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//149213009
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32566 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ sdk/lib/_internal/pub/test/build/includes_dart_files_from_dependencies_in_debug_test.dart
@@ -0,0 +1,64 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:scheduled_test/scheduled_test.dart';
+
+import '../descriptor.dart' as d;
+import '../test_pub.dart';
+
+main() {
+  initConfig();
+
+  integration("includes .dart files from dependencies in debug mode", () {
+    // Dart2js can take a long time to compile dart code, so we increase the
+    // timeout to cope with that.
+    currentSchedule.timeout *= 3;
+
+    d.dir("foo", [
+      d.libPubspec("foo", "0.0.1"),
+      d.dir("lib", [
+        d.file('foo.dart', 'foo() => print("hello");'),
+        d.dir("sub", [
+          d.file('bar.dart', 'bar() => print("hello");'),
+        ])
+      ])
+    ]).create();
+
+    d.dir(appPath, [
+      d.appPubspec({
+        "foo": {"path": "../foo"}
+      }),
+      d.dir("example", [
+        d.file("main.dart", 'myapp() => print("not entrypoint");'),
+        d.dir("sub", [
+          d.file("main.dart", 'myapp() => print("not entrypoint");')
+        ])
+      ])
+    ]).create();
+
+    schedulePub(args: ["build", "--mode", "debug", "example"],
+        output: new RegExp(r"Built 4 files!"));
+
+    d.dir(appPath, [
+      d.dir('build', [
+        d.dir('example', [
+          d.file("main.dart", 'myapp() => print("not entrypoint");'),
+          d.dir('packages', [
+            d.dir('foo', [
+              d.file('foo.dart', 'foo() => print("hello");'),
+              d.dir("sub", [
+                d.file('bar.dart', 'bar() => print("hello");'),
+              ])
+            ])
+          ]),
+          d.dir("sub", [
+            d.file("main.dart", 'myapp() => print("not entrypoint");'),
+            // Does *not* copy packages into subdirectories.
+            d.nothing("packages")
+          ])
+        ])
+      ])
+    ]).validate();
+  });
+}
