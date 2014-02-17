4acd74a94c892301ef626d35ad2e66655e6367d6
karlklose@google.com <karlklose@google.com>
2014-02-17 10:05:34 +0000

    Unify resolution of metadata and store them in the right order.
    R=johnniwinther@google.com
    
    Review URL: https://codereview.chromium.org//163183010
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32714 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/implementation/resolution/members.dart
+++ sdk/lib/_internal/compiler/implementation/resolution/members.dart
@@ -1231,6 +1231,18 @@ class ResolverTask extends CompilerTask {
     // TODO(ahe): Make non-fatal.
     compiler.reportFatalError(node, kind, arguments);
   }
+
+  resolveMetadata(VariableListElementX variables, VariableDefinitions node) {
+    LinkBuilder<MetadataAnnotation> metadata =
+        new LinkBuilder<MetadataAnnotation>();
+    for (Metadata annotation in node.metadata.nodes) {
+      ParameterMetadataAnnotation metadataAnnotation =
+          new ParameterMetadataAnnotation(annotation);
+      metadataAnnotation.annotatedElement = variables;
+      metadata.addLast(metadataAnnotation.ensureResolved(compiler));
+    }
+    variables.metadata = metadata.toLink();
+  }
 }
 
 class ConstantMapper extends Visitor {
@@ -3010,15 +3022,7 @@ class ResolverVisitor extends MappingVisitor<Element> {
       }
     }
     if (node.metadata != null) {
-      // TODO(johnniwinther): Unify handling of metadata on locals/formals.
-      for (Link<Node> link = node.metadata.nodes;
-           !link.isEmpty;
-           link = link.tail) {
-        ParameterMetadataAnnotation metadata =
-            new ParameterMetadataAnnotation(link.head);
-        variables.addMetadata(metadata);
-        metadata.ensureResolved(compiler);
-      }
+      compiler.resolver.resolveMetadata(variables, node);
     }
     visitor.visit(node.definitions);
   }
