1de976703cdf6c604ae698bece2e3b7f9bd67c2f
floitsch@google.com <floitsch@google.com>
2014-02-24 10:29:11 +0000

    Don't use `console` unconditionally (because of IE9).
    R=ricow@google.com
    
    Review URL: https://codereview.chromium.org//177403003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@32957 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/browser/lib/interop.js
+++ pkg/browser/lib/interop.js
@@ -3,5 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // TODO(jmesserly): remove this script after a deprecation period.
-console.warn('<script src="packages/browser/interop.js"> is no longer ' +
-    'needed for dart:js. See http://pub.dartlang.org/packages/browser.');
+if (typeof console == "object" && typeof console.warn == "function") {
+  console.warn('<script src="packages/browser/interop.js"> is no longer ' +
+      'needed for dart:js. See http://pub.dartlang.org/packages/browser.');
+}
