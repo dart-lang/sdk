7e55a59ce4ccbfe4f64ae01bd3a2706ce3bdf594
ajohnsen@google.com <ajohnsen@google.com>
2014-03-04 10:26:17 +0000

    Fix fromEnvironment when called from isolates.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//186393004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@33264 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/corelib/bool_from_environment_default_value_test.dart
+++ tests/corelib/bool_from_environment_default_value_test.dart
@@ -2,11 +2,21 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import "dart:isolate";
+
 import "package:expect/expect.dart";
 
-main() {
+void test(port) {
   Expect.isFalse(const bool.fromEnvironment('NOT_FOUND'));
   Expect.isTrue(const bool.fromEnvironment('NOT_FOUND', defaultValue: true));
   Expect.isFalse(const bool.fromEnvironment('NOT_FOUND', defaultValue: false));
   Expect.isNull(const bool.fromEnvironment('NOT_FOUND', defaultValue: null));
+  if (port != null) port.send(null);
+}
+
+main() {
+  test(null);
+  var port = new ReceivePort();
+  Isolate.spawn(test, port.sendPort);
+  port.listen((_) => port.close());
 }
