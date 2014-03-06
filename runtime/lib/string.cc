b26561d41de826b0aa1ebdc1fcf1df536dfa22f4
ajohnsen@google.com <ajohnsen@google.com>
2014-03-06 09:24:54 +0000

    Fix last commit.
    BUG=
    
    Review URL: https://codereview.chromium.org//187813011
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@33364 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/lib/string.cc
+++ runtime/lib/string.cc
@@ -19,7 +19,7 @@ DEFINE_NATIVE_ENTRY(String_fromEnvironment, 3) {
   GET_NON_NULL_NATIVE_ARGUMENT(String, name, arguments->NativeArgAt(1));
   GET_NATIVE_ARGUMENT(String, default_value, arguments->NativeArgAt(2));
   // Call the embedder to supply us with the environment.
-  Api::Scope api_scope();
+  Api::Scope api_scope(isolate);
   Dart_EnvironmentCallback callback = isolate->environment_callback();
   if (callback != NULL) {
     Dart_Handle result = callback(Api::NewHandle(isolate, name.raw()));
