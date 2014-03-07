f4a2d2ae9b5021e6062699a253f5cc957eec7fab
jwren@google.com <jwren@google.com>
2014-03-07 20:55:25 +0000

    Change to dart command line analyzer- Remove the rich toString in AnalyzerError to prevent .contents from being called on a source.
    R=brianwilkerson@google.com, scheglov@google.com
    
    Review URL: https://codereview.chromium.org//189913004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@33452 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/analyzer/lib/src/error.dart
+++ pkg/analyzer/lib/src/error.dart
@@ -44,45 +44,50 @@ class AnalyzerError implements Exception {
 
   String toString() {
     var builder = new StringBuffer();
-    var content = error.source.contents.data;
-    var beforeError = content.substring(0, error.offset);
-    var lineNumber = "\n".allMatches(beforeError).length + 1;
-    builder.writeln("Error on line $lineNumber of ${error.source.fullName}: "
-        "${error.message}");
 
-    var errorLineIndex = beforeError.lastIndexOf("\n") + 1;
-    var errorEndOfLineIndex = content.indexOf("\n", error.offset);
-    if (errorEndOfLineIndex == -1) errorEndOfLineIndex = content.length;
-    var errorLine = content.substring(
-        errorLineIndex, errorEndOfLineIndex);
-    var errorColumn = error.offset - errorLineIndex;
-    var errorLength = error.length;
+    // Print a less friendly string representation to ensure that
+    // error.source.contents is not executed, as .contents it isn't async
+    builder.writeln("Error in ${error.source.fullName}: ${error.message}");
 
-    // Ensure that the error line we display isn't too long.
-    if (errorLine.length > _MAX_ERROR_LINE_LENGTH) {
-      var leftLength = errorColumn;
-      var rightLength = errorLine.length - leftLength;
-      if (leftLength > _MAX_ERROR_LINE_LENGTH ~/ 2 &&
-          rightLength > _MAX_ERROR_LINE_LENGTH ~/ 2) {
-        errorLine = "..." + errorLine.substring(
-            errorColumn - _MAX_ERROR_LINE_LENGTH ~/ 2 + 3,
-            errorColumn + _MAX_ERROR_LINE_LENGTH ~/ 2 - 3)
-            + "...";
-        errorColumn = _MAX_ERROR_LINE_LENGTH ~/ 2;
-      } else if (rightLength > _MAX_ERROR_LINE_LENGTH ~/ 2) {
-        errorLine = errorLine.substring(0, _MAX_ERROR_LINE_LENGTH - 3) + "...";
-      } else {
-        assert(leftLength > _MAX_ERROR_LINE_LENGTH ~/ 2);
-        errorColumn -= errorLine.length - _MAX_ERROR_LINE_LENGTH;
-        errorLine = "..." + errorLine.substring(
-            errorLine.length - _MAX_ERROR_LINE_LENGTH + 3, errorLine.length);
-      }
-      errorLength = math.min(errorLength, _MAX_ERROR_LINE_LENGTH - errorColumn);
-    }
-    builder.writeln(errorLine);
+//    var content = error.source.contents.data;
+//    var beforeError = content.substring(0, error.offset);
+//    var lineNumber = "\n".allMatches(beforeError).length + 1;
+//    builder.writeln("Error on line $lineNumber of ${error.source.fullName}: "
+//        "${error.message}");
 
-    for (var i = 0; i < errorColumn; i++) builder.write(" ");
-    for (var i = 0; i < errorLength; i++) builder.write("^");
+//    var errorLineIndex = beforeError.lastIndexOf("\n") + 1;
+//    var errorEndOfLineIndex = content.indexOf("\n", error.offset);
+//    if (errorEndOfLineIndex == -1) errorEndOfLineIndex = content.length;
+//    var errorLine = content.substring(
+//        errorLineIndex, errorEndOfLineIndex);
+//    var errorColumn = error.offset - errorLineIndex;
+//    var errorLength = error.length;
+//
+//    // Ensure that the error line we display isn't too long.
+//    if (errorLine.length > _MAX_ERROR_LINE_LENGTH) {
+//      var leftLength = errorColumn;
+//      var rightLength = errorLine.length - leftLength;
+//      if (leftLength > _MAX_ERROR_LINE_LENGTH ~/ 2 &&
+//          rightLength > _MAX_ERROR_LINE_LENGTH ~/ 2) {
+//        errorLine = "..." + errorLine.substring(
+//            errorColumn - _MAX_ERROR_LINE_LENGTH ~/ 2 + 3,
+//            errorColumn + _MAX_ERROR_LINE_LENGTH ~/ 2 - 3)
+//            + "...";
+//        errorColumn = _MAX_ERROR_LINE_LENGTH ~/ 2;
+//      } else if (rightLength > _MAX_ERROR_LINE_LENGTH ~/ 2) {
+//        errorLine = errorLine.substring(0, _MAX_ERROR_LINE_LENGTH - 3) + "...";
+//      } else {
+//        assert(leftLength > _MAX_ERROR_LINE_LENGTH ~/ 2);
+//        errorColumn -= errorLine.length - _MAX_ERROR_LINE_LENGTH;
+//        errorLine = "..." + errorLine.substring(
+//            errorLine.length - _MAX_ERROR_LINE_LENGTH + 3, errorLine.length);
+//      }
+//      errorLength = math.min(errorLength, _MAX_ERROR_LINE_LENGTH - errorColumn);
+//    }
+//    builder.writeln(errorLine);
+//
+//    for (var i = 0; i < errorColumn; i++) builder.write(" ");
+//    for (var i = 0; i < errorLength; i++) builder.write("^");
     builder.writeln();
 
     return builder.toString();
