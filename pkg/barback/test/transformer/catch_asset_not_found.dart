a39fb8d58577ac5a29c1839df76d3836bbb2cf00
nweiz@google.com <nweiz@google.com>
2014-03-13 23:50:32 +0000

    Fix an analyzer warning in barback.
    R=rnystrom@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//196573013
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@33678 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/barback/test/transformer/catch_asset_not_found.dart
+++ pkg/barback/test/transformer/catch_asset_not_found.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library barback.test.transformer.bad;
+library barback.test.transformer.catch_asset_not_found;
 
 import 'dart:async';
 
