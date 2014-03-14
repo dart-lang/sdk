c4b6289cda1d3e971a56dadf312eccd2f8391d3b
nweiz@google.com <nweiz@google.com>
2014-03-14 20:24:15 +0000

    Improve the documentation for [TransformLogger.error].
    R=rnystrom@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//199723002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@33716 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/barback/lib/src/transform_logger.dart
+++ pkg/barback/lib/src/transform_logger.dart
@@ -56,7 +56,15 @@ class TransformLogger {
   /// Otherwise it's associated with the primary input of [transformer].
   /// If present, [span] indicates the location in the input asset that caused
   /// the error.
-  // TODO(sigmund,nweiz): clarify when an error should be logged or thrown.
+  ///
+  /// Logging any errors will cause Barback to consider the transformation to
+  /// have failed, much like throwing an exception. This means that neither the
+  /// primary input nor any outputs emitted by the transformer will be passed on
+  /// to the following phase, and the build will be reported as having failed.
+  ///
+  /// Unlike throwing an exception, this doesn't cause a transformer to stop
+  /// running. This makes it useful in cases where a single input may have
+  /// multiple errors that the user wants to know about.
   void error(String message, {AssetId asset, Span span}) {
     _logFunction(asset, LogLevel.ERROR, message, span);
   }
