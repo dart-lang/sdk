ac09e08ac8a015529d8a8168becc8806972bfbec
jmesserly@google.com <jmesserly@google.com>
2014-03-18 21:38:24 +0000

    [browser] restore previous version with tighter SDK lower bound follow up to http://code.google.com/p/dart/source/detail?r=34062
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org//203973002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34066 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/browser/lib/interop.js
+++ pkg/browser/lib/interop.js
@@ -2,9 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Type for remote proxies to Dart objects with dart2js.
-// WARNING: do not call this constructor or rely on it being
-// in the global namespace, as it may be removed.
-function DartObject(o) {
-  this.o = o;
+// TODO(jmesserly): remove this script after a deprecation period.
+if (typeof console == "object" && typeof console.warn == "function") {
+  console.warn('<script src="packages/browser/interop.js"> is no longer ' +
+      'needed for dart:js. See http://pub.dartlang.org/packages/browser.');
 }
