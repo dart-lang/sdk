19a5147493db11d2029b02ca20e47dfbeb972f77
ahe@google.com <ahe@google.com>
2014-03-25 14:08:36 +0000

    Add a test suite for Try Dart!
    R=kasperl@google.com, lukechurch@google.com
    
    Review URL: https://codereview.chromium.org//210873002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34360 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/try/paste_content_rewriting_test.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--package-root=sdk/lib/_internal/
+
+library trydart.paste_test;
+
+import 'dart:html';
+import 'dart:async';
+
+import '../../site/try/src/interaction_manager.dart' show
+    InteractionManager;
+
+import '../../site/try/src/ui.dart' show
+    inputPre,
+    observer;
+
+import '../../site/try/src/user_option.dart' show
+    UserOption;
+
+import '../../pkg/expect/lib/expect.dart';
+import '../../pkg/async_helper/lib/async_helper.dart';
+
+main() {
+  UserOption.storage = {};
+
+  var interaction = new InteractionManager();
+  inputPre = new DivElement();
+  document.body.append(inputPre);
+  observer = new MutationObserver(interaction.onMutation)
+      ..observe(inputPre, childList: true, characterData: true, subtree: true);
+
+  inputPre.innerHtml = "<span><p>//...</p>}</span>";
+
+  asyncTest(() => new Future(() {
+    print('Welcome to the future');
+    Expect.stringEquals('//...\n}\n', inputPre.text);
+  }));
+}
