5fbca19b7df35ac8c36c3326996dabe8278a725d
efortuna@google.com <efortuna@google.com>
2014-03-28 19:30:11 +0000

    Adding debug statement for IE 11 crash testing.
    BUG=
    R=blois@google.com
    
    Review URL: https://codereview.chromium.org//217283002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34517 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/async/schedule_microtask.dart
+++ sdk/lib/async/schedule_microtask.dart
@@ -31,6 +31,7 @@ void _asyncRunCallback() {
   try {
     _asyncRunCallbackLoop();
   } catch (e) {
+    print('microtask error ${e.stackTrace}'); // TODO(efortuna): Remove this.
     _AsyncRun._scheduleImmediate(_asyncRunCallback);
     _nextCallback = _nextCallback.next;
     rethrow;
