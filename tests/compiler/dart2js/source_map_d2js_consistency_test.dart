751c19f6551daf7bd75c341640fe018049214038
zarah@google.com <zarah@google.com>
2014-04-02 09:30:26 +0000

    Add end-to-end test for consistency in source map file and js file.
    R=johnniwinther@google.com
    
    Review URL: https://codereview.chromium.org//219263003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34642 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/compiler/dart2js/source_map_d2js_consistency_test.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'package:async_helper/async_helper.dart';
+
+import 'source_map_consistency_helper.dart';
+import '../../../sdk/lib/_internal/compiler/implementation/dart2js.dart' as entry;
+
+void main() {
+  asyncTest(() => createTempDir().then((Directory tmpDir) {
+    print('Compiling tests/compiler/dart2js/exit_code_helper.dart');
+    Future result =
+          entry.internalMain(['tests/compiler/dart2js/exit_code_helper.dart',
+                              '-o${tmpDir.path}/out.js',
+                              '--library-root=sdk']);
+      return result.then((_) {
+        Uri uri =
+            new Uri.file('${tmpDir.path}/out.js', windows: Platform.isWindows);
+        checkConsistency(uri);
+
+        print("Deleting '${tmpDir.path}'.");
+        tmpDir.deleteSync(recursive: true);
+      });
+  }));
+}
