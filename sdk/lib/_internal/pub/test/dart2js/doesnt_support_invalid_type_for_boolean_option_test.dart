a8966cb9d7886ef47548fd4070ae5052e38ec93c
rnystrom@google.com <rnystrom@google.com>
2014-04-02 21:43:06 +0000

    Reorganize the dart2js transformer tests.
    Aside from tweaking a bit of whitespace and consolidating a couple
    of tests, there are no substantive changes here.
    
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//221413007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34673 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ sdk/lib/_internal/pub/test/dart2js/doesnt_support_invalid_type_for_boolean_option_test.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS d.file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../descriptor.dart' as d;
+import '../test_pub.dart';
+import '../serve/utils.dart';
+
+main() {
+  initConfig();
+  integration("doesn't support invalid type for boolean option", () {
+    d.dir(appPath, [
+      d.pubspec({
+        "name": "myapp",
+        "transformers": [{
+          "\$dart2js": {
+            "checked": "foo",
+          }
+        }]
+      }),
+      d.dir("lib", [d.dir("src", [
+        d.file("transformer.dart", REWRITE_TRANSFORMER)
+      ])]),
+      d.dir("web", [d.file("main.dart", "void main() {}")])
+    ]).create();
+
+    createLockFile('myapp', pkg: ['barback']);
+
+    var server = pubServe();
+    requestShould404("main.dart.js");
+    server.stderr.expect(emitsLines(
+        'Build error:\n'
+        'Transform Dart2JS on myapp|web/main.dart threw error: '
+            'FormatException: Invalid value for \$dart2js.checked: "foo" '
+            '(expected true or false).'));
+    endPubServe();
+  });
+}
