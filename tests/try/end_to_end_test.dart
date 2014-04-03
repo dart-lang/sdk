9b640e9e8c2ca924fa98f78c611a43779b7c59b5
ahe@google.com <ahe@google.com>
2014-04-03 11:07:05 +0000

    end_to_end_test detects errors instead of timing out.
    R=kustermann@google.com
    
    Review URL: https://codereview.chromium.org//220453006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34690 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/try/end_to_end_test.dart
+++ tests/try/end_to_end_test.dart
@@ -14,7 +14,36 @@ library trydart.end_to_end_test;
 import 'dart:html';
 import 'dart:async';
 
+// TODO(ahe): Remove this import if issue 17936 is fixed.
+import 'dart:js' as hack;
+
 import 'package:async_helper/async_helper.dart';
+
+void installErrorHandlerOn(IFrameElement iframe) {
+  // This method uses dart:js to install an error event handler on the content
+  // window of [iframe]. This is a workaround for http://dartbug.com/17936.
+  var iframeProxy = new hack.JsObject.fromBrowserObject(iframe);
+  var contentWindowProxy = iframeProxy['contentWindow'];
+  if (contentWindowProxy == null) {
+    print('No contentWindow in iframe');
+    throw 'No contentWindow in iframe';
+  }
+  contentWindowProxy.callMethod('addEventListener', ['error', (eventProxy) {
+    String filename = eventProxy['filename'];
+    int lineno = eventProxy['lineno'];
+    String message = eventProxy['message'];
+    print("Error occurred in iframe: $message");
+    new Future(() {
+      // Chrome seems to not call window.onerror when you throw in response to
+      // an error event. So we throw the error in a future.
+      throw 'Error from iframe: $filename:$lineno: $message';
+    });
+  }]);
+}
+
+void onIframeLoaded(ErrorEvent event) {
+  installErrorHandlerOn(event.target);
+}
 
 void main() {
   asyncStart();
@@ -36,8 +65,14 @@ void main() {
   // time.
   window.localStorage.clear();
 
-  document.body.append(new IFrameElement()
+  IFrameElement iframe = new IFrameElement()
       ..src = '/root_build/try_dartlang_org/index.html'
       ..style.width = '90vw'
-      ..style.height = '90vh');
+      ..style.height = '90vh'
+      ..onLoad.listen(onIframeLoaded);
+  document.body.append(iframe);
+  // Install an error handler both on the new iframe element, and when it has
+  // fired the load event.  That seems to matter according to some sources on
+  // stackoverflow.
+  installErrorHandlerOn(iframe);
 }
