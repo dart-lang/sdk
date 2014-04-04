6995d6a5b08f1b0fc2de2cc7494531e6860e8425
karlklose@google.com <karlklose@google.com>
2014-04-04 07:50:07 +0000

    Fix dart2js_extra/type_error_message_test work in minified mode.
    R=sigurdm@google.com
    
    Review URL: https://codereview.chromium.org//225423002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@34724 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js_extra/type_error_message_test.dart
+++ tests/compiler/dart2js_extra/type_error_message_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Test that the error message for a failing subtype test includes type
+// arguments.
+
 import 'package:expect/expect.dart';
 
 class C<T, S> {}
@@ -21,7 +24,10 @@ main() {
     try {
       C<String, String> x = new C<C<int, String>, String>();
     } catch (e) {
-      String expected = 'C<C<int, String>, String>';
+      String nameOfC = (C).toString();
+      String nameOfInt = (int).toString();
+      String nameOfString = (String).toString();
+      String expected = '$nameOfC<$nameOfC<$nameOfInt, $nameOfString>, $nameOfString>';
       Expect.isTrue(e.toString().contains(expected),
                     'Expected "$expected" in the message');
       caught = true;
