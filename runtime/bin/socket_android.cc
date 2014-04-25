47d03a04a08fd695a97d182a4801ec412e56c6c8
zra@google.com <zra@google.com>
2014-04-25 14:45:34 +0000

    Adds AI_ADDRCONFIG back to address lookup on Android.
    R=ajohnsen@google.com
    
    Review URL: https://codereview.chromium.org//257613002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@35425 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/socket_android.cc
+++ runtime/bin/socket_android.cc
@@ -221,14 +221,14 @@ AddressList<SocketAddress>* Socket::LookupAddress(const char* host,
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = SocketAddress::FromType(type);
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = 0;
+  hints.ai_flags = AI_ADDRCONFIG;
   hints.ai_protocol = IPPROTO_TCP;
   struct addrinfo* info = NULL;
   int status = getaddrinfo(host, 0, &hints, &info);
   if (status != 0) {
     // We failed, try without AI_ADDRCONFIG. This can happen when looking up
     // e.g. '::1', when there are no IPv6 addresses.
-    hints.ai_flags = AI_V4MAPPED;
+    hints.ai_flags = 0;
     status = getaddrinfo(host, 0, &hints, &info);
     if (status != 0) {
       ASSERT(*os_error == NULL);
