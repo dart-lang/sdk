d1003c11f617b26bbad5a86d3ffd10092c665bfc
vsm@google.com <vsm@google.com>
2014-05-14 11:07:02 +0000

    Show web_audio type map
    This was throwing as the symbol wasn't available in _getType.
    
    TBR=leafp@google.com
    
    Review URL: https://codereview.chromium.org//285033002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@36155 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_blink/dartium/_blink_dartium.dart
+++ sdk/lib/_blink/dartium/_blink_dartium.dart
@@ -23,7 +23,7 @@ import 'dart:svg' as svg;
 import 'dart:svg' show Matrix;
 import 'dart:svg' show SvgSvgElement;
 import 'dart:svg' show svgBlinkMap;
-import 'dart:web_audio' show AudioNode, AudioParam;
+import 'dart:web_audio' show AudioNode, AudioParam, web_audioBlinkMap;
 // Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
