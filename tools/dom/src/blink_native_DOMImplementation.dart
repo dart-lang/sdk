1828e5c6932ad3e5de8b6fcf1af2f55106b74f6f
leafp@google.com <leafp@google.com>
2014-05-19 20:58:06 +0000

    Roll back explicit cross frame object until M35 lands
    BUG=
    R=jacobr@google.com
    
    Review URL: https://codereview.chromium.org//294643006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@36323 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/dom/src/blink_native_DOMImplementation.dart
+++ tools/dom/src/blink_native_DOMImplementation.dart
@@ -19,32 +19,32 @@ Native_Utils_initializeCustomElement(element) native "Utils_initializeCustomElem
 
 Native_Utils_changeElementWrapper(element, type) native "Utils_changeElementWrapper";
 
-// _DOMWindowCrossFrame native entry points
-Native_DOMWindowCrossFrame_get_history(_DOMWindowCrossFrame) native "WindowCrossFrame_history_Getter";
+// FIXME: Return to using explicit cross frame entry points after roll to M35
+Native_DOMWindowCrossFrame_get_history(_DOMWindowCrossFrame) native "Window_history_cross_frame_Getter";
 
-Native_DOMWindowCrossFrame_get_location(_DOMWindowCrossFrame) native "WindowCrossFrame_location_Getter";
+Native_DOMWindowCrossFrame_get_location(_DOMWindowCrossFrame) native "Window_location_cross_frame_Getter";
 
-Native_DOMWindowCrossFrame_get_closed(_DOMWindowCrossFrame) native "WindowCrossFrame_closed_Getter";
+Native_DOMWindowCrossFrame_get_closed(_DOMWindowCrossFrame) native "Window_closed_Getter";
 
-Native_DOMWindowCrossFrame_get_opener(_DOMWindowCrossFrame) native "WindowCrossFrame_opener_Getter";
+Native_DOMWindowCrossFrame_get_opener(_DOMWindowCrossFrame) native "Window_opener_Getter";
 
-Native_DOMWindowCrossFrame_get_parent(_DOMWindowCrossFrame) native "WindowCrossFrame_parent_Getter";
+Native_DOMWindowCrossFrame_get_parent(_DOMWindowCrossFrame) native "Window_parent_Getter";
 
-Native_DOMWindowCrossFrame_get_top(_DOMWindowCrossFrame) native "WindowCrossFrame_top_Getter";
+Native_DOMWindowCrossFrame_get_top(_DOMWindowCrossFrame) native "Window_top_Getter";
 
-Native_DOMWindowCrossFrame_close(_DOMWindowCrossFrame) native "WindowCrossFrame_close_Callback";
+Native_DOMWindowCrossFrame_close(_DOMWindowCrossFrame) native "Window_close_Callback";
 
-Native_DOMWindowCrossFrame_postMessage(_DOMWindowCrossFrame, message, targetOrigin, [messagePorts]) native "WindowCrossFrame_postMessage_Callback";
+Native_DOMWindowCrossFrame_postMessage(_DOMWindowCrossFrame, message, targetOrigin, [messagePorts]) native "Window_postMessage_Callback";
 
 // _HistoryCrossFrame native entry points
-Native_HistoryCrossFrame_back(_HistoryCrossFrame) native "HistoryCrossFrame_back_Callback";
+Native_HistoryCrossFrame_back(_HistoryCrossFrame) native "History_back_Callback";
 
-Native_HistoryCrossFrame_forward(_HistoryCrossFrame) native "HistoryCrossFrame_forward_Callback";
+Native_HistoryCrossFrame_forward(_HistoryCrossFrame) native "History_forward_Callback";
 
-Native_HistoryCrossFrame_go(_HistoryCrossFrame, distance) native "HistoryCrossFrame_go_Callback";
+Native_HistoryCrossFrame_go(_HistoryCrossFrame, distance) native "History_go_Callback";
 
 // _LocationCrossFrame native entry points
-Native_LocationCrossFrame_set_href(_LocationCrossFrame, h) native "LocationCrossFrame_href_Setter";
+Native_LocationCrossFrame_set_href(_LocationCrossFrame, h) native "Location_href_Setter";
 
 // _DOMStringMap native entry  points
 Native_DOMStringMap_containsKey(_DOMStringMap, key) native "DOMStringMap_containsKey_Callback";
