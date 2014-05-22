28f18c580ddffb8f2ed3aeede6ecbff02bbcf3a7
srdjan@google.com <srdjan@google.com>
2014-05-22 16:59:00 +0000

    Increase inlining depth threshold, no negative performance impact observed.
    R=asiva@google.com
    
    Review URL: https://codereview.chromium.org//296893006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@36512 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/flow_graph_inliner.cc
+++ runtime/vm/flow_graph_inliner.cc
@@ -28,7 +28,7 @@ DEFINE_FLAG(charp, inlining_filter, NULL, "Inline only in named function");
 // Flags for inlining heuristics.
 DEFINE_FLAG(int, inline_getters_setters_smaller_than, 10,
     "Always inline getters and setters that have fewer instructions");
-DEFINE_FLAG(int, inlining_depth_threshold, 3,
+DEFINE_FLAG(int, inlining_depth_threshold, 5,
     "Inline function calls up to threshold nesting depth");
 DEFINE_FLAG(int, inlining_size_threshold, 25,
     "Always inline functions that have threshold or fewer instructions");
