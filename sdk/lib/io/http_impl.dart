dbb592ed0adaf83057dfcd454bec6003f638d3b1
ajohnsen@google.com <ajohnsen@google.com>
2014-05-27 09:17:57 +0000

    Put delivering of parsed http-requests at the end of the message-queue.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//298143003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@36655 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/io/http_impl.dart
+++ sdk/lib/io/http_impl.dart
@@ -2190,7 +2190,17 @@ class _HttpServer
     _sessionManager.sessionTimeout = timeout;
   }
 
-  void _handleRequest(HttpRequest request) => _controller.add(request);
+  void _handleRequest(_HttpRequest request) {
+    // Delay the request until the isolate's message-queue is handled.
+    // This greatly improves scheduling when a lot of requests are active.
+    Timer.run(() {
+      if (!closed) {
+        _controller.add(request);
+      } else {
+        request._httpConnection.destroy();
+      }
+    });
+  }
 
   void _handleError(error) {
     if (!closed) _controller.addError(error);
