a0e024b273bcdad825b84e2527fc93d24ed9d098
ricow@google.com <ricow@google.com>
2014-06-02 17:40:08 +0000

    Make the functional testing annotated steps script call the eggplant starting script
    R=messick@google.com
    
    Review URL: https://codereview.chromium.org//310633003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@36883 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/bots/functional_testing.py
+++ tools/bots/functional_testing.py
@@ -16,8 +16,12 @@ import sys
 import bot
 import bot_utils
 
+utils = bot_utils.GetUtils()
+
 FT_BUILDER = r'ft-slave-(linux|mac)'
 FT_MASTER = r'ft-master'
+
+HOST_OS = utils.GuessOS()
 
 EDITOR_LOCATION='/home/chrome-bot/Desktop'
 
@@ -45,27 +49,46 @@ def Run(args):
   sys.stdout.flush()
   bot.RunProcess(args)
 
+def FTSlave(config):
+  with bot.BuildStep('Fetching editor'):
+    revision = int(os.environ['BUILDBOT_GOT_REVISION'])
+    bot_name, _ = bot.GetBotName()
+    print bot_name
+    channel = bot_utils.GetChannelFromName(bot_name)
+    namer = bot_utils.GCSNamer(channel=channel)
+    system = config.system
+    if system == 'mac':
+      system = 'macos'
+    editor_path = namer.editor_zipfilepath(revision, system, 'x64')
+    gsutils = bot_utils.GSUtil()
+    local_path = os.path.join(EDITOR_LOCATION, 'editor.zip')
+    if os.path.exists(local_path):
+      os.remove(local_path)
+    local_extracted = os.path.join(EDITOR_LOCATION, 'dart')
+    shutil.rmtree(local_extracted, ignore_errors=True)
+    gsutils.execute(['cp', editor_path, local_path])
+    Run(['unzip', local_path, '-d', EDITOR_LOCATION])
+
+def FTMaster(config):
+  run = int(os.environ['BUILDBOT_ANNOTATED_STEPS_RUN'])
+  with bot.BuildStep('Master run %s' % run):
+    if run == 1:
+      print 'Not doing anything on master before the triggers'
+      return
+    else:
+      builddir = os.path.join(bot_utils.DART_DIR,
+                              utils.GetBuildDir(HOST_OS, HOST_OS),
+                              'functional_testing')
+      shutil.rmtree(builddir, ignore_errors=True)
+      os.makedirs(builddir)
+      script_locations = os.path.join(bot_utils.DART_DIR, 'editor', 'ft')
+      Run(['/home/chrome-bot/func-test/bot-run', builddir, script_locations])
+
 def FTSteps(config):
   if config.builder_tag == 'master':
-    print 'Not doing anything on master for now'
-    return
-  revision = int(os.environ['BUILDBOT_GOT_REVISION'])
-  bot_name, _ = bot.GetBotName()
-  print bot_name
-  channel = bot_utils.GetChannelFromName(bot_name)
-  namer = bot_utils.GCSNamer(channel=channel)
-  system = config.system
-  if system == 'mac':
-    system = 'macos'
-  editor_path = namer.editor_zipfilepath(revision, system, 'x64')
-  gsutils = bot_utils.GSUtil()
-  local_path = os.path.join(EDITOR_LOCATION, 'editor.zip')
-  if os.path.exists(local_path):
-    os.remove(local_path)
-  local_extracted = os.path.join(EDITOR_LOCATION, 'dart')
-  shutil.rmtree(local_extracted, ignore_errors=True)
-  gsutils.execute(['cp', editor_path, local_path])
-  Run(['unzip', local_path, '-d', EDITOR_LOCATION])
-
+    FTMaster(config)
+  else:
+    FTSlave(config)
+  
 if __name__ == '__main__':
-  bot.RunBot(SrcConfig, FTSteps)
+  bot.RunBot(SrcConfig, FTSteps, build_step=None)
