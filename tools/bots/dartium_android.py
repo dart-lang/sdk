2ceae2693eafc69187406f096c8ca896a6773a34
ricow@google.com <ricow@google.com>
2014-06-12 12:54:51 +0000

    Fix getting the buildername on the dartium android bot
    R=whesse@google.com
    
    Review URL: https://codereview.chromium.org//337483002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@37267 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/bots/dartium_android.py
+++ tools/bots/dartium_android.py
@@ -40,7 +40,7 @@ def UploadSetACL(gsutil, local, remote):
 def UploadAPKs(options):
   with bot.BuildStep('Upload apk'):
     revision = utils.GetSVNRevision()
-    bot_name, _ = bot.GetBotName()
+    bot_name = os.environ.get("BUILDBOT_BUILDERNAME")
     channel = bot_utils.GetChannelFromName(bot_name)
     namer = bot_utils.GCSNamer(channel=channel)
     gsutil = bot_utils.GSUtil()
