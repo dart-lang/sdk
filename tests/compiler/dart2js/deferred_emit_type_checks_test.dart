4c21d2b7428c066bb6a50663098e84f0eb08a71e
sra@google.com <sra@google.com>
2014-06-20 01:36:54 +0000

    Redo "Move code emitted by emitRuntimeTypeSupport into a function to so that local variables can be used to avoid repeated subexpressions."
    BUG=
    
    Review URL: https://codereview.chromium.org//346953002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@37529 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js/deferred_emit_type_checks_test.dart
+++ tests/compiler/dart2js/deferred_emit_type_checks_test.dart
@@ -52,7 +52,7 @@ void main() {
   asyncTest(() => compiler.run(Uri.parse('memory:main.dart')).then((_) {
     String mainOutput = outputs['main.js'].mem[0];
     String deferredOutput = outputs['out_deferred.part.js'].mem[0];
-    RegExp re = new RegExp(r"\n.\.A\.\$isA = true;");
+    RegExp re = new RegExp(r"\n  _ = .\.A;\n  _.\$isA = TRUE;");
     print(deferredOutput);
     Expect.isTrue(re.hasMatch(deferredOutput));
     Expect.isFalse(re.hasMatch(mainOutput));
