cd2b96afd8d4fc99505c29b73ff4457de5e5d9fa
whesse@google.com <whesse@google.com>
2014-06-24 09:40:48 +0000

    Add _owner setter to RawSecureSocket.
    BUG=
    R=ajohnsen@google.com
    
    Review URL: https://codereview.chromium.org//354593002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@37647 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/io/secure_socket.dart
+++ sdk/lib/io/secure_socket.dart
@@ -609,6 +609,8 @@ class _RawSecureSocket extends Stream<RawSocketEvent>
 
   int get remotePort => _socket.remotePort;
 
+  void set _owner(owner) { _socket._owner = owner; }
+
   int available() {
     return _status != CONNECTED ? 0
                                 : _secureFilter.buffers[READ_PLAINTEXT].length;
