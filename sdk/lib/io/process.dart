9e8dcea24405b9741384fd24c959700ba1c1c245
ricow@google.com <ricow@google.com>
2014-06-27 07:06:24 +0000

    Fix typo in dart:io process documentation
    BUG: http://dartbug.com/19590
    
    R=ajohnsen@google.com, ricow@google.com
    
    Review URL: https://codereview.chromium.org//349753002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@37775 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/io/process.dart
+++ sdk/lib/io/process.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -387,7 +387,7 @@ abstract class ProcessResult {
 
   /**
    * Standard output from the process. The value used for the
-   * `stdoutEncoding` argument to `Process.run` determins the type. If
+   * `stdoutEncoding` argument to `Process.run` determines the type. If
    * `null` was used this value is of type `List<int> otherwise it is
    * of type `String`.
    */
@@ -395,7 +395,7 @@ abstract class ProcessResult {
 
   /**
    * Standard error from the process. The value used for the
-   * `stderrEncoding` argument to `Process.run` determins the type. If
+   * `stderrEncoding` argument to `Process.run` determines the type. If
    * `null` was used this value is of type `List<int>
    * otherwise it is of type `String`.
    */
