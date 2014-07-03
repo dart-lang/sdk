139a3eff6c2b4174dcfcfa4bd6263d26d4d35846
asgerf@google.com <asgerf@google.com>
2014-07-03 08:39:31 +0000

    Fixed static type warning annotation in rewrite_implicit_this_test.dart.
    BUG=
    
    Review URL: https://codereview.chromium.org//367263003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@37976 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/rewrite_implicit_this_test.dart
+++ tests/language/rewrite_implicit_this_test.dart
@@ -10,30 +10,30 @@ class Foo {
     String x = 'x';
 
     easy(z) {
-        return x + y + z;
+        return x + y + z; /// 01: static type warning
     }
 
     // Shadow the 'y' field in various ways
     shadow_y_parameter(y) {
-        return x + this.y + y;
+        return x + this.y + y; /// 01: continued
     }
 
     shadow_y_local(z) {
         var y = z;
-        return x + this.y + y;
+        return x + this.y + y; /// 01: continued
     }
 
     shadow_y_capturedLocal(z) {
         var y = z;
         foo() {
-            return x + this.y + y;
+            return x + this.y + y; /// 01: continued
         }
         return foo();
     }
 
     shadow_y_closureParam(z) {
         foo(y) {
-            return x + this.y + y;
+            return x + this.y + y; /// 01: continued
         }
         return foo(z);
     }
@@ -41,32 +41,32 @@ class Foo {
     shadow_y_localInsideClosure(z) {
         foo() {
             var y = z;
-            return x + this.y + y;
+            return x + this.y + y; /// 01: continued
         }
         return foo();
     }
 
     // Shadow the 'x' field in various ways
     shadow_x_parameter(x) {
-        return this.x + y + x;
+        return this.x + y + x; /// 01: continued
     }
 
     shadow_x_local(z) {
         var x = z;
-        return this.x + y + x;
+        return this.x + y + x; /// 01: continued
     }
 
     shadow_x_capturedLocal(z) {
         var x = z;
         foo() {
-            return this.x + y + x;
+            return this.x + y + x; /// 01: continued
         }
         return foo();
     }
 
     shadow_x_closureParam(z) {
         foo(x) {
-            return this.x + y + x;
+            return this.x + y + x; /// 01: continued
         }
         return foo(z);
     }
@@ -74,13 +74,13 @@ class Foo {
     shadow_x_localInsideClosure(z) {
         foo() {
             var x = z;
-            return this.x + y + x;
+            return this.x + y + x; /// 01: continued
         }
         return foo();
     }
 
     shadow_x_toplevel() {
-        return x + this.y + toplevel + this.toplevel;
+        return x + this.y + toplevel + this.toplevel; /// 01: continued
     }
     
 }
@@ -91,17 +91,17 @@ class Sub extends Foo {
 }
 
 main() {
-    Expect.equals('xyz', new Sub().easy('z'));
-    Expect.equals('xyz', new Sub().shadow_y_parameter('z'));
-    Expect.equals('xyz', new Sub().shadow_y_local('z'));
-    Expect.equals('xyz', new Sub().shadow_y_capturedLocal('z'));
-    Expect.equals('xyz', new Sub().shadow_y_closureParam('z'));
-    Expect.equals('xyz', new Sub().shadow_y_localInsideClosure('z'));
-    Expect.equals('xyz', new Sub().shadow_x_parameter('z'));
-    Expect.equals('xyz', new Sub().shadow_x_local('z'));
-    Expect.equals('xyz', new Sub().shadow_x_capturedLocal('z'));
-    Expect.equals('xyz', new Sub().shadow_x_closureParam('z'));
-    Expect.equals('xyz', new Sub().shadow_x_localInsideClosure('z'));
+    Expect.equals('xyz', new Sub().easy('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_y_parameter('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_y_local('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_y_capturedLocal('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_y_closureParam('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_y_localInsideClosure('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_x_parameter('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_x_local('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_x_capturedLocal('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_x_closureParam('z')); /// 01: continued
+    Expect.equals('xyz', new Sub().shadow_x_localInsideClosure('z')); /// 01: continued
 
-    Expect.equals('xyAB', new Sub().shadow_x_toplevel());
+    Expect.equals('xyAB', new Sub().shadow_x_toplevel()); /// 01: continued
 }
