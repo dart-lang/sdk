e921f8c41648076fe1fe40a72c9f2bac3c9ee593
asiva@google.com <asiva@google.com>
2014-07-22 22:45:38 +0000

    Fix dartium build break.
    R=hausner@google.com
    
    Review URL: https://codereview.chromium.org//407073005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@38479 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/bin/gen_snapshot.cc
+++ runtime/bin/gen_snapshot.cc
@@ -540,6 +540,10 @@ int main(int argc, char** argv) {
         Builtin::LoadAndCheckLibrary(Builtin::kBuiltinLibrary);
     CHECK_RESULT(builtin_lib);
 
+    // Ensure that we mark all libraries as loaded.
+    result = Dart_FinalizeLoading(false);
+    CHECK_RESULT(result);
+
     // Prepare for script loading by setting up the 'print' and 'timer'
     // closures and setting up 'package root' for URI resolution.
     result = DartUtils::PrepareForScriptLoading(package_root, builtin_lib);
@@ -564,6 +568,9 @@ int main(int argc, char** argv) {
     // Load the specified script.
     library = LoadSnapshotCreationScript(app_script_name);
     VerifyLoaded(library);
+    // Ensure that we mark all libraries as loaded.
+    result = Dart_FinalizeLoading(false);
+    CHECK_RESULT(result);
     CreateAndWriteSnapshot();
 
     Dart_EnterIsolate(UriResolverIsolateScope::isolate);
