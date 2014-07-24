6cd21978f69865b392f3d1726c438da9e9811266
srdjan@google.com <srdjan@google.com>
2014-07-24 21:28:47 +0000

    Add test for issue 20190.
    R=hausner@google.com
    
    Review URL: https://codereview.chromium.org//420503003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@38554 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/standalone/int_list_test.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+// Dart Mint representations and type propagation issue.
+// Testing Int32List and Uint32List loads.
+//
+// VMOptions=--optimization-counter-threshold=5 --no-use-osr
+
+import 'dart:typed_data';
+import "package:expect/expect.dart";
+
+main() {
+  var a = new Uint32List(100);
+  a[2] = 3;
+  var res = sumIt1(a, 2);
+  Expect.equals(3 * 10, res);
+  res = sumIt1(a, 2);
+  Expect.equals(3 * 10, res);
+  a = new Int32List(100);
+  a[2] = 3;
+  res = sumIt2(a, 2);
+  Expect.equals(3 * 10, res);
+  res = sumIt2(a, 2);
+  Expect.equals(3 * 10, res);
+}
+
+sumIt1(Uint32List a, int n) {
+  var sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += a[n];
+  }
+  return sum;
+}
+
+sumIt2(Int32List a, int n) {
+  var sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += a[n];
+  }
+  return sum;
+}
