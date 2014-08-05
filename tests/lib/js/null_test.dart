43b31f88a8b9438b6f764afa9bdaa92e72360b40
sigmund@google.com <sigmund@google.com>
2014-08-05 21:46:43 +0000

    Fix js/null_test to run also in CSP mode
    R=sra@google.com
    
    Review URL: https://codereview.chromium.org//439513002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@38909 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/lib/js/null_test.dart
+++ tests/lib/js/null_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
---
+++ tests/lib/js/null_test.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This code is not inlined in the test to support testing in CSP mode.
+function isUndefined(o) { return o === undefined; }
+function isNull(o) { return o === null; }
