d7c294b4e28342db723377009dead758b01f03af
ricow@google.com <ricow@google.com>
2014-08-07 12:59:35 +0000

    Remove observatory symlink from the repository - they have unforseen issues when copying around stuff for testing.
    Long storry short: for testing the ability to do pub get and pub build we copy in the packages and test them if they have a pubspec yaml file.
    
    If you copy a symlink, like the one added in r38788, without -L to cp you will just get the relative symlink copied in, which will, of course, not work.
    
    I generally think it is a bad idea to add symlinks to the repo, we have had enough issues with the ones for packages/pub
    
    Instead, to keep the testing, add the runtime/bin/vmservice to the location of where we look for packages
    
    R=koda@google.com
    
    Review URL: https://codereview.chromium.org//434883002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@38983 260f80e4-7a28-3924-810f-c04153c831b5

--- tools/testing/dart/test_suite.dart
+++ tools/testing/dart/test_suite.dart
@@ -400,6 +400,7 @@ abstract class TestSuite {
      *      pkg/PACKAGE_NAME
      *      pkg/third_party/PACKAGE_NAME
      *      third_party/pkg/PACKAGE_NAME
+     *      runtime/bin/vmservice/PACKAGE_NAME
      */
 
     // Directories containing "-" are not valid pub packages and we therefore
@@ -412,6 +413,8 @@ abstract class TestSuite {
       listDir(dartDir.append('pkg'), isValid),
       listDir(dartDir.append('pkg').append('third_party'), isValid),
       listDir(dartDir.append('third_party').append('pkg'), isValid),
+      listDir(dartDir.append('runtime').append('bin').append('vmservice'),
+              isValid),
     ];
     return Future.wait(futures).then((results) {
       var packageDirectories = {};
