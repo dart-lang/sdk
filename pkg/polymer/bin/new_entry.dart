f0c215d05d927ccc7cecf93af4b0be3df1136392
sigmund@google.com <sigmund@google.com>
2014-08-08 00:14:27 +0000

    Some fixes for new_entry.dart that I run into.
    R=dgrove@google.com
    
    Review URL: https://codereview.chromium.org//453673002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@39015 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/polymer/bin/new_entry.dart
+++ pkg/polymer/bin/new_entry.dart
@@ -1,29 +1,29 @@
-/// 
+///
 /// Script to create boilerplate for a Polymer element.
 /// Produces new .html entry point for a polymer app and updates the
 /// pubspec.yaml to reflect it.
-/// 
+///
 /// Run this script with pub run:
-/// 
+///
 ///     pub run polymer:new_entry <html_file>
-/// 
+///
 import 'dart:io';
 import 'package:path/path.dart' as path;
 import 'package:yaml/yaml.dart';
 import 'package:source_span/source_span.dart';
 
 void main(List<String> args) {
-  var entryPoint = args[0];  
+  var entryPoint = args[0];
   var outputDir = path.dirname(entryPoint);
   var outputDirLocation = new Directory(outputDir);
-  
+
   if (!outputDirLocation.existsSync()) {
     outputDirLocation.createSync(recursive: true);
   }
-  
+
   outputDir = outputDirLocation.resolveSymbolicLinksSync();
   var pubspecDir = _findDirWithFile(outputDir, 'pubspec.yaml');
-  
+
   if (pubspecDir == null) {
     print('Could not find pubspec.yaml when walking up from $outputDir');
     exitCode = 1;
@@ -32,9 +32,9 @@ void main(List<String> args) {
 
   var relativeEntryPoint = path.relative(
       path.join(outputDir, path.basename(entryPoint)), from: pubspecDir);
-  
+
   try {
-    if (_createBoilerPlate(relativeEntryPoint, pubspecDir) == false) {
+    if (_createBoilerPlate(relativeEntryPoint, pubspecDir)) {
       print('Added $entryPoint to ${path.join(pubspecDir, "pubspec.yaml")}');
     }
     print('Successfully created:');
@@ -44,7 +44,7 @@ void main(List<String> args) {
     print('Error creating files in $outputDir');
     exitCode = 1;
   }
- 
+
   return;
 }
 
@@ -58,10 +58,11 @@ String _findDirWithFile(String dir, String filename) {
   return dir;
 }
 
-// Returns true if there was a monolithic polymer transformer in the pubspec,
-// in which case the pubspec wasn't modified. Returns false otherwise.
+// Returns true if the pubspec file was modified. It might not be modified if
+// there was a monolithic polymer transformer in the pubspec, or if the entry
+// point for some reason already existed in the pubspec.
 bool _createBoilerPlate(String entryPoint, String pubspecDir) {
- 
+
 String html = '''
 <!doctype html>
 <html>
@@ -79,11 +80,11 @@ String html = '''
 ''';
 
   new File(path.join(pubspecDir, entryPoint)).writeAsStringSync(html);
-  
+
   var pubspecPath = path.join(pubspecDir, 'pubspec.yaml');
   var pubspecText = new File(pubspecPath).readAsStringSync();
   var transformers = loadYaml(pubspecText)['transformers'];
-  var entryPoints = [entryPoint];
+  var entryPoints;
 
   var insertionPoint;
   var textToInsert = '';
@@ -93,33 +94,32 @@ String html = '''
     // transformers, get the entry points, and delete the old entry points.
     var transformersSourceSpan = transformers.span;
     SourceSpan sourceSpan;
-    bool monolithicPolymerTransformer = false;
-    
-    transformers.forEach((e) {
-      if (e is YamlMap && e['polymer'] != null) {
-        entryPoints.addAll(e['polymer']['entry_points']);
-        if (e['polymer'].keys.length > 1) {
-          // TODO(dgrove): handle the case where there are additional sections
-          // in the polymer transformer.
-          throw new UnimplementedError('Cannot handle non-entry_point entries '
-              'for polymer transformer');
-        }
-        sourceSpan = e.span;
-      } else if (e == 'polymer') {
-        if (path.split(entryPoint)[0] == 'web') {
-          // In this case, there's no need to add the entry point.
-          monolithicPolymerTransformer = true;
-        } else {
+
+    for (var e in transformers) {
+      if (e != 'polymer' && (e is! YamlMap || e['polymer'] == null)) continue;
+      if (e == 'polymer' || !e['polymer'].containsKey('entry_points')) {
+        if (path.split(entryPoint)[0] != 'web') {
           print('WARNING: Did not add entry_point $entryPoint to pubspec.yaml'
               ' because of already-existing transformer|polymer section');
-          monolithicPolymerTransformer = true;
         }
-      } 
-    });
-  
-    if (monolithicPolymerTransformer) {
-      return true;
+        return false;
+      } else if (e['polymer'].keys.length > 1) {
+        // TODO(dgrove): handle the case where there are additional sections
+        // in the polymer transformer.
+        throw new UnimplementedError('Cannot handle non-entry_point entries '
+            'for polymer transformer');
+      } else {
+        var existing = e['polymer']['entry_points'];
+        entryPoints = existing == null ? [] :
+            (existing is String ? [existing] : existing.toList());
+
+        if (entryPoints.contains(entryPoint)) return false;
+        entryPoints.add(entryPoint);
+
+        sourceSpan = e.span;
+      }
     }
+
     if (sourceSpan == null) {
       // There were no polymer transformers.
       insertionPoint = transformersSourceSpan.start.offset;
@@ -128,31 +128,31 @@ String html = '''
       insertionPoint = sourceSpan.start.offset;
       pubspecText = '${pubspecText.substring(0, insertionPoint)}'
           '${pubspecText.substring(sourceSpan.end.offset)}';
-    } 
+    }
   } else {
     // There were no transformers at all.
     insertionPoint = pubspecText.length;
     textToInsert = 'transformers:\n- ';
   }
-  
+
   // TODO(dgrove): Once dartbug.com/20409 is addressed, use that here.
   var entryPointsText = entryPoints.map((e) => '    - $e').join('\n');
-  
+
   textToInsert =
 '''${textToInsert}polymer:
     entry_points:
 $entryPointsText''';
 
-  
+
   if (insertionPoint == pubspecText.length) {
     pubspecText = '${pubspecText}${textToInsert}';
   } else {
     pubspecText = '${pubspecText.substring(0, insertionPoint)}'
         '${textToInsert}\n${pubspecText.substring(insertionPoint)}';
   }
- 
+
   _writePubspec(pubspecPath, pubspecText);
-  return false;
+  return true;
 }
 
 _writePubspec(String pubspecPath, String text) {
