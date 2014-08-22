bbb1fdd4af060d7a6988d8cf25f50228d0f76bbf
lrn@google.com <lrn@google.com>
2014-08-22 11:02:20 +0000

    Update pub barback dependency to allow 0.15.1-dev.
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//497703002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@39486 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/pub/lib/src/barback.dart
+++ sdk/lib/_internal/pub/lib/src/barback.dart
@@ -38,7 +38,7 @@ import 'version.dart';
 ///
 /// [compat]: https://gist.github.com/nex3/10942218
 final pubConstraints = {
-  "barback": new VersionConstraint.parse(">=0.13.0 <0.15.1"),
+  "barback": new VersionConstraint.parse(">=0.13.0 <0.15.2"),
   "source_span": new VersionConstraint.parse(">=1.0.0 <2.0.0"),
   "stack_trace": new VersionConstraint.parse(">=0.9.1 <2.0.0")
 };
