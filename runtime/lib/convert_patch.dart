109c16e2048ce8c1dd1e82369f336984196c3163
lrn@google.com <lrn@google.com>
2014-08-27 08:53:50 +0000

    Add import of dart:typed_data in dart:convert.
    Was previously handled by patch file for VM, and was now missing
    for dart2js.
    
    Review URL: https://codereview.chromium.org//506273006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@39585 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/lib/convert_patch.dart
+++ runtime/lib/convert_patch.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "dart:typed_data";
-
 // JSON conversion.
 
 patch _parseJson(String json, reviver(var key, var value)) {
