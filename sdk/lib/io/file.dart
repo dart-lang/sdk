7770efc4a9a54e170c355a0f93aa16b2f2aaf0a2
sgjesse@google.com <sgjesse@google.com>
2014-11-06 12:05:19 +0000

    Update documentation for RandomAccessFile
    R=kustermann@google.com, lrn@google.com
    BUG=http://dartbug.com/21516
    
    Review URL: https://codereview.chromium.org//702363002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@41548 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/io/file.dart
+++ sdk/lib/io/file.dart
@@ -496,9 +496,23 @@ abstract class File extends FileSystemEntity {
 
 
 /**
- * [RandomAccessFile] provides random access to the data in a
- * file. [RandomAccessFile] objects are obtained by calling the
+ * `RandomAccessFile` provides random access to the data in a
+ * file.
+ *
+ * `RandomAccessFile` objects are obtained by calling the
  * [:open:] method on a [File] object.
+ *
+ * A `RandomAccessFile` have both asynchronous and synchronous
+ * methods. The asynchronous methods all return a `Future`
+ * whereas the synchronous methods will return the result directly,
+ * and block the current isolate until the result is ready.
+ *
+ * At most one asynchronous method can be pending on a given `RandomAccessFile`
+ * instance at the time. If an asynchronous method is called when one is
+ * already in progress a [FileSystemException] is thrown.
+ *
+ * If an asynchronous method is pending it is also not possible to call any
+ * synchronous methods. This will also throw a [FileSystemException].
  */
 abstract class RandomAccessFile {
   /**
