f189792444ee2930459d605c5e384c61e0f938e1
wibling@google.com <wibling@google.com>
2014-11-17 14:14:45 +0000

    Make the unittest's breath test a bit more resilient against timing issue.
    BUG=
    R=kustermann@google.com, sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//734023002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@41774 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/unittest/test/breath_test.dart
+++ pkg/unittest/test/breath_test.dart
@@ -11,9 +11,16 @@ import 'package:unittest/unittest.dart';
 void main() {
   // Test the sync test 'breath' feature of unittest.
 
+  // We use the testStartStopwatch to determine if the 'starve'
+  // test was executed within a small enough time interval from
+  // the first test that we are guaranteed the second test is
+  // running in a microtask. If the second test is running as a
+  // microtask we are guaranteed the timer scheduled in the
+  // first test has not been run yet.
+  var testStartStopwatch = new Stopwatch()..start();
+
   group('breath', () {
     var sentinel = 0;
-    var start;
 
     test('initial', () {
       Timer.run(() {
@@ -22,12 +29,19 @@ void main() {
     });
 
     test('starve', () {
-      start = new DateTime.now().millisecondsSinceEpoch;
-      var now;
-      do {
+      // If less than BREATH_INTERVAL time has passed since before
+      // we started the test group then the previous test's timer
+      // has not been run (at least this is what we are testing).
+      if (testStartStopwatch.elapsed.inMilliseconds <= BREATH_INTERVAL) {
         expect(sentinel, 0);
-        now = new DateTime.now().millisecondsSinceEpoch;
-      } while (now - start <= BREATH_INTERVAL);
+      }
+
+      // Next we wait for at least BREATH_INTERVAL to guaranteed the
+      // next (third) test is run using a timer which means it will
+      // run after the timer scheduled in the first test and hence
+      // the sentinel should have been set to 1.
+      var sw = new Stopwatch()..start();
+      while (sw.elapsed.inMilliseconds < BREATH_INTERVAL);
     });
 
     test('breathed', () {
