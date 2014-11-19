c41cc7df73d23a30360c18fc360d3b26dfd9831e
karlklose@google.com <karlklose@google.com>
2014-11-19 14:37:53 +0000

    Allow to compare generated code in the cps-ir test.
    R=sigurdm@google.com
    
    Review URL: https://codereview.chromium.org//735313002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@41835 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/compiler/dart2js/js_backend_cps_ir_test.dart
+++ tests/compiler/dart2js/js_backend_cps_ir_test.dart
@@ -11,11 +11,21 @@ import 'package:expect/expect.dart';
 import 'package:compiler/src/apiimpl.dart'
        show Compiler;
 import 'memory_compiler.dart';
+import 'package:compiler/src/js/js.dart' as js;
+import 'package:compiler/src/common.dart' show Element;
+
 
 const String TEST_MAIN_FILE = 'test.dart';
 
+class TestEntry {
+  final String source;
+  final String expectation;
+  const TestEntry(this.source, [this.expectation]);
+}
+
 /// The list of tests to run.
-const List<String> RAW_TESTS = const [
+const List<TestEntry> tests = const [
+  const TestEntry(
   """
 foo(a) {
   return a;
@@ -34,32 +44,56 @@ main() {
 }
   """,
   """
+function() {
+  var a, b;
+  a = 10;
+  b = 1;
+  P.print(b);
+  P.print(a);
+  P.print(a);
+  P.print(V.foo(b));
+  return null;
+}"""),
+  const TestEntry(
+  """
 foo() { return 42; }
 main() { return foo(); }
   """,
-  "main() {}",
-  "main() { return 42; }",
+  """function() {
+  return V.foo();
+}"""),
+  const TestEntry("main() {}"),
+  const TestEntry("main() { return 42; }"),
 ];
 
 String formatTest(Map test) {
   return test[TEST_MAIN_FILE];
 }
 
+String getCodeForMain(Compiler compiler) {
+  Element mainFunction = compiler.mainFunction;
+  js.Node ast = compiler.enqueuer.codegen.generatedCode[mainFunction];
+  return js.prettyPrint(ast, compiler).getText();
+}
+
 main() {
   Expect.isTrue(const bool.fromEnvironment("USE_CPS_IR"));
 
-  Iterable<Map> tests =
-    RAW_TESTS.map((String text) => {TEST_MAIN_FILE: text});
-
-  for (Map test in tests) {
+  for (TestEntry test in tests) {
+    Map files = {TEST_MAIN_FILE: test.source};
     asyncTest(() {
-      Compiler compiler = compilerFor(test);
+      Compiler compiler = compilerFor(files);
       Uri uri = Uri.parse('memory:$TEST_MAIN_FILE');
       return compiler.run(uri).then((_) {
         Expect.isNotNull(compiler.assembledCode);
+        String expectation = test.expectation;
+        if (expectation != null) {
+          Expect.equals(test.expectation, getCodeForMain(compiler));
+        }
       }).catchError((e) {
+        print(e);
         Expect.fail('The following test failed to compile:\n'
-                    '${formatTest(test)}');
+                    '${formatTest(files)}');
       });
     });
   }
