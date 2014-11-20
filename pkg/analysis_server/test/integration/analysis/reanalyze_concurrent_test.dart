651d589c2fe02f33af2109a2ea79267cc1ec2136
paulberry@google.com <paulberry@google.com>
2014-11-20 22:52:30 +0000

    Remove flaky check from reanalyze_concurrent_test.
    This should avoid sporadic false failures with this test.  If in the
    future we genuinely regress bug 21448, there is a danger that this
    test won't catch it reliably, but that is mitigated by the fact that
    (a) even flaky failures do get noticed, and (b) the bug was severe
    enough that it probably would be noticed in manual testing.  The extra
    risk seems worth it to avoid the sporadic false failures we've been
    seeing.
    
    BUG=dartbug.com/21638
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//724403003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@41870 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/analysis_server/test/integration/analysis/reanalyze_concurrent_test.dart
+++ pkg/analysis_server/test/integration/analysis/reanalyze_concurrent_test.dart
@@ -12,9 +12,6 @@ library test.integration.analysis.reanalyze_concurrent;
 
 import 'dart:async';
 
-import 'package:analysis_server/src/protocol.dart';
-import 'package:unittest/unittest.dart';
-
 import '../../reflective_tests.dart';
 import '../integration_tests.dart';
 
@@ -41,23 +38,8 @@ main() {}''';
       return onServerStatus.first.then((_) {
         sendAnalysisReanalyze();
         return analysisFinished.then((_) {
-          // Now that reanalysis has finished, no further analysis should occur.
-          onServerStatus.listen((ServerStatusParams data) {
-            if (data.analysis != null) {
-              if (data.analysis.isAnalyzing) {
-                // Most likely what happened is that the first reanalyze
-                // completed before the second reanalyze had a chance to start,
-                // and we are just now starting the second reanalyze.  If this
-                // is the case, then the test isn't testing what it's meant to
-                // test (because we are trying to test what happens when one
-                // reanalyze starts while another is in progress).  In which
-                // case we will need to give the analyzer more work to do.
-                fail('First reanalyze finished before second started.');
-              }
-            }
-          });
-          // Give the server an extra second to make sure it doesn't take any
-          // more actions.
+          // Now that reanalysis has finished, give the server an extra second
+          // to make sure it doesn't crash.
           return new Future.delayed(new Duration(seconds: 1));
         });
       });
