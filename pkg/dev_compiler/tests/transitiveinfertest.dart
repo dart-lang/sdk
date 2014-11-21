2116aa559b2d54db4b1c6ce62080910ab443d9e6
Vijay Menon <vsm@google.com>
2014-11-21 13:48:40 -0800

    transitive inference test
    We're currently getting the first var, but somehow not transitively.  If d1 is explicit typed, we don't see these dynamic invokes:
    
    > dart ./bin/checker.dart -l info -o out tests/transitiveinfertest.dart
    No issues found
    #1: line 21, column 9 of tests/transitiveinfertest.dart: d1.c.b.a.x requires dynamic invoke
      print(d1.c.b.a.x);
            ^^^^^^^^^^
    #2: line 21, column 9 of tests/transitiveinfertest.dart: d1.c.b.a requires dynamic invoke
      print(d1.c.b.a.x);
            ^^^^^^^^
    #3: line 21, column 9 of tests/transitiveinfertest.dart: d1.c.b requires dynamic invoke
      print(d1.c.b.a.x);
            ^^^^^^
    
    R=sigmund@google.com
    
    Review URL: https://chromereviews.googleplex.com/120937013

---
+++ pkg/dev_compiler/tests/transitiveinfertest.dart
@@ -0,0 +1,26 @@
+library transitiveinfertest;
+
+class A {
+  int x = 42;
+}
+
+class B {
+  A a = new A();
+}
+
+class C {
+  B b = new B();
+}
+
+class D {
+  C c = new C();
+}
+
+void main() {
+  var d1 = new D();
+  print(d1.c.b.a.x);
+
+  D d2 = new D();
+  print(d2.c.b.a.x);  
+}
+
