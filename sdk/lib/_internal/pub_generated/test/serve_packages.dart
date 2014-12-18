0935b930eaed5d2697f7d8b86c9655884feec774
nweiz@google.com <nweiz@google.com>
2014-12-18 19:02:43 +0000

    Remove barback from the repo.
    R=rnystrom@google.com, sigmund@google.com
    
    Review URL: https://codereview.chromium.org//808713003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@42468 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/pub_generated/test/serve_packages.dart
+++ sdk/lib/_internal/pub_generated/test/serve_packages.dart
@@ -148,8 +148,9 @@ class PackageServerBuilder {
       if (_packages.containsKey(name)) return;
       _packages[name] = [];
 
-      var pubspec = new Map.from(
-          loadYaml(readTextFile(p.join(repoRoot, 'pkg', name, 'pubspec.yaml'))));
+      var root = packagePath(name);
+      var pubspec =
+          new Map.from(loadYaml(readTextFile(p.join(root, 'pubspec.yaml'))));
 
       // Remove any SDK constraints since we don't have a valid SDK version
       // while testing.
@@ -160,9 +161,7 @@ class PackageServerBuilder {
               pubspec,
               [
                   d.file('pubspec.yaml', yaml(pubspec)),
-                  new d.DirectoryDescriptor.fromFilesystem(
-                      'lib',
-                      p.join(repoRoot, 'pkg', name, 'lib'))]));
+                  new d.DirectoryDescriptor.fromFilesystem('lib', p.join(root, 'lib'))]));
 
       if (pubspec.containsKey('dependencies')) {
         pubspec['dependencies'].keys.forEach(_addPackage);
