7a398b9e31de1227b5c10e3855182507a51cd93b
pquitslund <pquitslund@gmail.com>
2015-02-02 09:58:54 -0800

    Basic lint driver jumping off point.

---
+++ lib/src/linter.dart
@@ -0,0 +1,187 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library linter_impl;
+
+import 'dart:io';
+
+import 'package:analyzer/analyzer.dart';
+import 'package:analyzer/src/generated/engine.dart';
+import 'package:analyzer/src/generated/java_engine.dart';
+import 'package:analyzer/src/generated/parser.dart';
+import 'package:analyzer/src/services/lint.dart';
+import 'package:dart_lint/src/analysis.dart';
+import 'package:dart_lint/src/rules.dart';
+
+
+/// Dart source linter.
+abstract class DartLinter {
+
+  /// Creates a new linter for dart source.
+  factory DartLinter([LinterOptions options]) => new SourceLinter(options);
+
+  disableRule(String ruleName);
+
+  enableRule(String ruleName);
+
+  Iterable<AnalysisErrorInfo> lintFile(File sourceFile);
+
+  Iterable<AnalysisErrorInfo> lintPath(String sourcePath);
+
+  registerRule(String ruleName, Linter lintRule);
+}
+
+class LintDriver extends AnalysisDriver {
+
+  static LinterOptions get standardOptions => new LinterOptions();
+
+  factory LintDriver.forFile(File file, Iterable<Linter> linters,
+      [LinterOptions options]) {
+    _registerLinters(linters);
+    return new LintDriver._forFile(
+        file,
+        options != null ? options : standardOptions);
+  }
+
+  factory LintDriver.forPath(String path, Iterable<Linter> linters,
+      [LinterOptions options]) {
+    _registerLinters(linters);
+    return new LintDriver._forPath(
+        path,
+        options != null ? options : standardOptions);
+  }
+
+  LintDriver._forFile(File file, [LinterOptions options])
+      : super.forFile(file, options);
+
+  LintDriver._forPath(String path, [LinterOptions options])
+      : super.forPath(path, options);
+
+  bool isDesiredError(AnalysisError error) =>
+      error.errorCode.type == ErrorType.LINT;
+
+  Iterable<AnalysisErrorInfo> lint() {
+    analyzeSync();
+    return errorInfos;
+  }
+
+  static void _registerLinters(Iterable<Linter> linters) {
+    LintGenerator.LINTERS.clear();
+    linters.forEach((l) => LintGenerator.LINTERS.add(l));
+  }
+}
+
+/// Thrown when an error occurs in linting.
+class LinterException implements Exception {
+
+  /// A message describing the error.
+  final String message;
+
+  /// Creates a new LinterException with an optional error [message].
+  const LinterException([this.message = 'LinterException']);
+
+  LinterException.forError(List<AnalysisError> errors)
+      : message = _createMessage(errors);
+
+  String toString() => '$message';
+
+  //TODO: revisit
+  static String _createMessage(errors) {
+    var errorCode = errors[0].errorCode;
+    var phase = errorCode is ParserErrorCode ? 'parsing' : 'scanning';
+    return 'An error occured while $phase (${errorCode.name}).';
+  }
+}
+
+/// Linter options.
+class LinterOptions extends DriverOptions {
+  bool get enableLints => true;
+}
+
+abstract class Reporter {
+  void exception(LinterException exception);
+  void warn(String message);
+}
+
+/// Linter implementation
+class SourceLinter implements DartLinter, AnalysisErrorListener {
+
+  final errors = <AnalysisError>[];
+  LinterOptions options;
+  final Reporter reporter;
+  RuleRegistry registry;
+
+  SourceLinter(this.options, [this.reporter = const StdIoReporter(),
+      this.registry]) {
+    if (options == null) {
+      options = new LinterOptions();
+    }
+    if (registry == null) {
+      registry = new RuleRegistry(reporter);
+    }
+  }
+
+  @override
+  disableRule(String ruleName) {
+    registry.disable(ruleName);
+  }
+
+  @override
+  enableRule(String ruleName) {
+    registry.enable(ruleName);
+  }
+
+  @override
+  Iterable<AnalysisErrorInfo> lintFile(File sourceFile) =>
+      new LintDriver.forFile(sourceFile, registry.enabledLints, options).lint();
+
+  @override
+  Iterable<AnalysisErrorInfo> lintPath(String sourcePath) =>
+      new LintDriver.forPath(sourcePath, registry.enabledLints, options).lint();
+
+  @override
+  onError(AnalysisError error) {
+    errors.add(error);
+  }
+
+  @override
+  registerRule(String ruleName, Linter lintRule) {
+    registry.registerLinter(ruleName, lintRule);
+  }
+}
+
+class StdIoReporter implements Reporter, Logger {
+
+  const StdIoReporter();
+
+  @override
+  void exception(LinterException exception) {
+    print('EXCEPTION: $exception');
+  }
+
+  @override
+  void logError(String message, [CaughtException exception]) {
+    print('ERROR: $message');
+  }
+
+  @override
+  void logError2(String message, Object exception) {
+    print('ERROR: $message');
+  }
+
+  @override
+  void logInformation(String message, [CaughtException exception]) {
+    print('INFO: $message');
+  }
+
+  @override
+  void logInformation2(String message, Object exception) {
+    print('INFO: $message');
+  }
+
+  @override
+  void warn(String message) {
+    print('WARN: $message');
+  }
+}
---
+++ lib/src/rules.dart
@@ -0,0 +1,66 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library rules;
+
+import 'package:analyzer/src/services/lint.dart';
+import 'package:dart_lint/src/linter.dart';
+//import 'package:dart_lint/src/rules/super_goes_last.dart';
+
+/// Map of contributed lint rules.
+final Map<String, Linter> _rules = {
+  //'super_goes_last': new SuperGoesLastLint()
+};
+
+class Rule {
+
+  /// Whether this rule is enabled in the current rule set.
+  bool enabled;
+  /// A uniquely identifying name for this rule.
+  final String ruleName;
+  /// The associated linter.
+  final Linter linter;
+
+  Rule(this.ruleName, this.linter, {this.enabled: false});
+}
+
+class RuleRegistry {
+
+  final Reporter reporter;
+  final Map<String, Rule> _ruleMap = <String, Rule>{};
+
+  RuleRegistry(this.reporter) {
+    // Register rules
+    _rules.forEach((n, l) => registerLinter(n, l));
+  }
+
+  /// An empty registry for testing
+  RuleRegistry.empty(this.reporter);
+
+  Iterable<Linter> get enabledLints =>
+      _ruleMap.values.where((Rule r) => r.enabled).map((Rule r) => r.linter);
+
+  void disable(String ruleName) {
+    if (_ruleMap[ruleName] == null) {
+      reporter.warn("No rule registered to '$ruleName', cannot disable");
+    } else {
+      _ruleMap[ruleName].enabled = false;
+    }
+  }
+
+  void enable(String ruleName) {
+    if (_ruleMap[ruleName] == null) {
+      reporter.warn("No rule registered to '$ruleName', cannot enable");
+    } else {
+      _ruleMap[ruleName].enabled = true;
+    }
+  }
+
+  void registerLinter(String name, Linter linter) {
+    if (_ruleMap[name] != null) {
+      reporter.warn("Multiple linter rules registered to name '$name'");
+    }
+    _ruleMap[name] = new Rule(name, linter);
+  }
+}
