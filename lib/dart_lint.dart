4a9cbd001387d4fb139606eb73e756746a6d10f5
pquitslund <pquitslund@gmail.com>
2015-02-02 11:12:15 -0800

    Entry point.

---
+++ lib/dart_lint.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library dart_lint;
+
+export 'package:dart_lint/src/linter.dart' show DartLinter, LinterOptions,
+    LinterException;
