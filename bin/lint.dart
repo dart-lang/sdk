bf69cf264028e427f233b401fa92eb8d49cdf836
pquitslund <pquitslund@gmail.com>
2015-02-06 14:49:42 -0800

    Simple command line driver.

---
+++ bin/lint.dart
@@ -0,0 +1,74 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:dart_lint/src/io.dart';
+
+
+const processFileFailedExitCode = 65;
+const unableToProcessExitCode = 64;
+
+isLinterErrorCode(int code) =>
+    code == unableToProcessExitCode || code == processFileFailedExitCode;
+
+
+void main(List<String> args) {
+  var parser = new ArgParser(allowTrailingOptions: true);
+
+  parser.addFlag(
+      "help",
+      abbr: "h",
+      negatable: false,
+      help: "Shows usage information.");
+
+  var options;
+  try {
+    options = parser.parse(args);
+  } on FormatException catch (err) {
+    printUsage(parser, err.message);
+    exitCode = unableToProcessExitCode;
+    return;
+  }
+
+  if (options["help"]) {
+    printUsage(parser);
+    return;
+  }
+
+  if (options.rest.isEmpty) {
+    printUsage(parser, "Please provide at least one library file to lint.");
+    exitCode = unableToProcessExitCode;
+    return;
+  }
+
+  for (var path in options.rest) {
+    var file = new File(path);
+    if (file.existsSync()) {
+      print("Linting $path...");
+      if (!processFile(file)) {
+        exitCode = processFileFailedExitCode;
+      }
+    } else {
+      stderr.writeln('No file found at "$path".');
+      exitCode = unableToProcessExitCode;
+    }
+  }
+}
+
+void printUsage(ArgParser parser, [String error]) {
+
+  var message = "Lints Dart source files.";
+  if (error != null) {
+    message = error;
+  }
+
+  stdout.write('''$message
+Usage: dartlint <files>
+${parser.usage}
+  
+For more information, see https://github.com/dart-lang/dart_lint
+''');
+}
