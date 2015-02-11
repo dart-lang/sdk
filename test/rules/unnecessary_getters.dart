7e84573b74d995ecc44fb0bd80aad60aea4e89c0
pquitslund <pquitslund@gmail.com>
2015-02-10 16:07:09 -0800

    Lint for unnecessary getters. Fix dart-lang/linter#23.

---
+++ test/rules/unnecessary_getters.dart
@@ -0,0 +1,35 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class BoxBad {
+  var _contents;
+  get contents => _contents; //LINT
+}
+
+class BoxGood {
+  final contents = [];
+}
+
+class Bad {
+  var _x;
+  get x { //LINT
+    return _x;
+  }
+}
+
+class Ok {
+  var _stuff;
+  get stuff => _stuff;
+  set stuff(stuff) {
+    _stuff = stuff;
+  }
+}
+
+class OkToo {
+  var _x;
+  get x {
+    print(x);
+    return _x;
+  }
+}
