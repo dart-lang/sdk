d767611e013fa03a9d42427dd846cbd4c9299bad
paulberry@google.com <paulberry@google.com>
2015-03-03 13:42:05 +0000

    Fix warning in deferred_shared_and_unshared_classes_test.
    TBR=sigurdm@google.com
    
    Review URL: https://codereview.chromium.org//971353002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@44169 260f80e4-7a28-3924-810f-c04153c831b5

--- tests/language/deferred_shared_and_unshared_classes_lib2.dart
+++ tests/language/deferred_shared_and_unshared_classes_lib2.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library lib2;
+
 import "deferred_shared_and_unshared_classes_lib_shared.dart";
 
 foo() {
