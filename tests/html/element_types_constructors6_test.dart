f2b2c64fbc01a573728611837b8490a63356f1fe
alanknight@google.com <alanknight@google.com>
2015-03-09 16:42:00 +0000

    Split the constructors group in element_types_test into multiple files to narrow down timeouts
    BUG=
    R=whesse@google.com
    
    Review URL: https://codereview.chromium.org//984133004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@44327 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/html/element_types_constructors6_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library element_types_constructors6_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:unittest/html_individual_config.dart';
+import 'dart:html';
+
+main() {
+  useHtmlIndividualConfiguration();
+
+  group('ul', () {
+    check('ul', () => new UListElement() is UListElement);
+
+    test('accepts li', () {
+      var ul = new UListElement();
+      var li = new LIElement();
+      ul.append(li);
+    });
+  });
+  check('video', () => new VideoElement() is VideoElement);
+  check('unknown', () => new Element.tag('someunknown') is UnknownElement);
+}
