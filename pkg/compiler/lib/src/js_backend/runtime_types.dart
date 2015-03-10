d62e45b914f1ea07a6a322776ad6a4da0e36deeb
zarah@google.com <zarah@google.com>
2015-03-10 11:41:12 +0000

    dart2js: remove name string of function type encoding in metadata.
    R=herhut@google.com
    
    Review URL: https://codereview.chromium.org//974793002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@44365 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/compiler/lib/src/js_backend/runtime_types.dart
+++ pkg/compiler/lib/src/js_backend/runtime_types.dart
@@ -680,8 +680,7 @@ class TypeRepresentationGenerator extends DartTypeVisitor {
       properties.add(new jsAst.Property(js.string(name), value));
     }
 
-    jsAst.LiteralString name = js.string(namer.getFunctionTypeName(type));
-    addProperty(namer.functionTypeTag, name);
+    addProperty(namer.functionTypeTag, js.string(''));
     if (type.returnType.isVoid) {
       addProperty(namer.functionTypeVoidReturnTag, js('true'));
     } else if (!type.returnType.treatAsDynamic) {
