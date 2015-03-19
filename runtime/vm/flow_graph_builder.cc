cddac39a650298c184ed17a63c1dbfee623f2efc
hausner@google.com <hausner@google.com>
2015-03-19 16:54:44 +0000

    Eliminate some DebugStep calls in async code
    There is no reason to check debugger stepping when assigning
    to internal helper variables.
    
    Review URL: https://codereview.chromium.org//1019683002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@44577 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/vm/flow_graph_builder.cc
+++ runtime/vm/flow_graph_builder.cc
@@ -3436,10 +3436,13 @@ void ValueGraphVisitor::VisitLoadLocalNode(LoadLocalNode* node) {
 void EffectGraphVisitor::VisitStoreLocalNode(StoreLocalNode* node) {
   // If the right hand side is an expression that does not contain
   // a safe point for the debugger to stop, add an explicit stub
-  // call.
-  if (node->value()->IsLiteralNode() ||
+  // call. Exception: don't do this when assigning to internal variables,
+  // or for generated code that has no source position.
+  if ((node->value()->IsLiteralNode() ||
       node->value()->IsLoadLocalNode() ||
-      node->value()->IsClosureNode()) {
+      node->value()->IsClosureNode()) &&
+      node->local().IsInternal() &&
+      (node->token_pos() != Scanner::kNoSourcePos)) {
     AddInstruction(new(Z) DebugStepCheckInstr(
         node->token_pos(), RawPcDescriptors::kRuntimeCall));
   }
