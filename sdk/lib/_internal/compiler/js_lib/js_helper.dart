cbc7d0a0c7bfc8d3e057676e0db0b5f6b9984d46
herhut@google.com <herhut@google.com>
2015-03-20 09:41:23 +0000

    Check for required number of arguments in Function.apply.
    BUG= http://dartbug.com/22917
    R=karlklose@google.com
    
    Review URL: https://codereview.chromium.org//1021203002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@44598 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/compiler/js_lib/js_helper.dart
+++ sdk/lib/_internal/compiler/js_lib/js_helper.dart
@@ -1171,9 +1171,12 @@ class Primitives {
         return functionNoSuchMethod(function, positionalArguments, null);
       }
       ReflectionInfo info = new ReflectionInfo(jsFunction);
-      int maxArgumentCount = info.requiredParameterCount +
+      int requiredArgumentCount = info.requiredParameterCount;
+      int maxArgumentCount = requiredArgumentCount +
           info.optionalParameterCount;
-      if (info.areOptionalParametersNamed || maxArgumentCount < argumentCount) {
+      if (info.areOptionalParametersNamed ||
+          requiredArgumentCount > argumentCount ||
+          maxArgumentCount < argumentCount) {
         return functionNoSuchMethod(function, positionalArguments, null);
       }
       arguments = new List.from(arguments);
