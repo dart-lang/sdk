ea8aef3c4b13c54eaad45887c90f8fd2534dc8a1
Kevin Moore <kevmoo@google.com>
2015-03-26 12:20:11 +0100

    test both stable and dev branches of SDK, but coverage only on stable

--- pkg/dev_compiler/tool/travis.sh
+++ pkg/dev_compiler/tool/travis.sh
@@ -3,22 +3,10 @@
 # Fast fail the script on failures.
 set -e
 
-function clean {
-  # This is a much simpler clean script, assuming git is available
-  pushd test
-  git clean -fdx
-  popd
-  pub install
-}
-
-clean
-
 dart --checked test/all_tests.dart
 
 # Install dart_coveralls; gather and send coverage data.
-if [ "$COVERALLS_TOKEN" ]; then
-  clean
-
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
   pub global run dart_coveralls report \
     --token $COVERALLS_TOKEN \
     --retry 2 \
