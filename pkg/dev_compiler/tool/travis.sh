2de38f1bf7c53e6ced67239e7090d1b333750be2
John Messerly <jmesserly@google.com>
2015-03-30 12:22:02 -0700

    Revert "Reuse test.sh under Travis and fixe failing test"

--- pkg/dev_compiler/tool/travis.sh
+++ pkg/dev_compiler/tool/travis.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-set -e # bail on error
 
-# Prerequisite: ./tool/build_sdk.sh has been run.
+# Fast fail the script on failures.
+set -e
+
+# Build first
+./tool/build_sdk.sh
+
+dart --checked test/all_tests.dart
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
