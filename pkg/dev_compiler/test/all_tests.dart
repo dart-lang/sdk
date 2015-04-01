316a99b719a05c9a13a0f731e38c0bece1138ab5
Vijay Menon <vsm@google.com>
2015-04-01 10:15:11 -0700

    Revert "Re-enable logging test"
    We're getting an error on dev runs with this test.
    
    TBR=jmesserly@google.com
    
    Review URL: https://codereview.chromium.org/1052523003

--- pkg/dev_compiler/test/all_tests.dart
+++ pkg/dev_compiler/test/all_tests.dart
@@ -13,7 +13,6 @@ import 'codegen_test.dart' as codegen_test;
 import 'end_to_end_test.dart' as e2e;
 import 'report_test.dart' as report_test;
 import 'runtime/dart_runtime_test.dart' as runtime_test;
-import 'runtime/dart_logging_runtime_test.dart' as logging_runtime_test;
 import 'dependency_graph_test.dart' as dependency_graph_test;
 
 import 'test_util.dart';
@@ -26,7 +25,6 @@ void main(args) {
   group('checker', checker_test.main);
   group('report', report_test.main);
   group('runtime', runtime_test.main);
-  group('logging_runtime', logging_runtime_test.main);
   group('dependency_graph', dependency_graph_test.main);
   group('codegen', () => codegen_test.main(args));
   var args2 = new List.from((args == null) ? [] : args, growable: true);
