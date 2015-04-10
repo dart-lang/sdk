c4403ec25068a903c5e8ddcf3b14f4007799ffe8
rmacnak@google.com <rmacnak@google.com>
2015-04-10 17:12:06 +0000

    Ensure call_site_data_test runs with unoptimized code so invocation counts are accurate.
    BUG=http://dartbug.com/23160
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org//1078893004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45073 260f80e4-7a28-3924-810f-c04153c831b5

--- runtime/observatory/tests/service/call_site_data_test.dart
+++ runtime/observatory/tests/service/call_site_data_test.dart
@@ -1,7 +1,8 @@
 // Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// VMOptions=--compile-all --error_on_bad_type --error_on_bad_override --checked
+// VMOptions=--optimization_filter=doesNotExist
+// ^Force code to be unoptimized so the invocation counts are accurate.
 
 library call_site_data_test;
 
