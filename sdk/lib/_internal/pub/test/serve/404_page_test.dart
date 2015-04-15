3c39ad2ec40604026696cebc2677da39193ed5e7
dgrove@google.com <dgrove@google.com>
2015-04-15 23:13:40 +0000

    Fix off-by-one error in https://code.google.com/p/dart/source/detail?r=45153 and https://code.google.com/p/dart/source/detail?r=45154 .
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org//1055283003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45189 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/_internal/pub/test/serve/404_page_test.dart
+++ sdk/lib/_internal/pub/test/serve/404_page_test.dart
@@ -39,7 +39,7 @@ main() {
       expect(response.statusCode, equals(404));
 
       // Should mention the asset that can't be found.
-      expect(response.body, contains('&quot;&#46;packages&quot;'));
+      expect(response.body, contains('&quot;&#47;packages&quot;'));
     });
 
     endPubServe();
