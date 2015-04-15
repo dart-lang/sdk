e375dac7051d66ee36e5570136b0744a0323e1c9
kmillikin@google.com <kmillikin@google.com>
2015-04-15 15:13:22 +0000

    Remove an unused field from one of the CPS translation classes.
    BUG=
    R=asgerf@google.com
    
    Review URL: https://codereview.chromium.org//1087243002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45168 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/compiler/lib/src/cps_ir/cps_ir_builder.dart
+++ pkg/compiler/lib/src/cps_ir/cps_ir_builder.dart
@@ -479,8 +479,6 @@ abstract class IrBuilder {
   // TODO(johnniwinther): Make these field final and remove the default values
   // when [IrBuilder] is a property of [IrBuilderVisitor] instead of a mixin.
 
-  final List<ir.Parameter> _parameters = <ir.Parameter>[];
-
   IrBuilderDelimitedState state;
 
   /// A map from variable indexes to their values.
@@ -562,20 +560,18 @@ abstract class IrBuilder {
     _enterScope(closureScope);
   }
 
-  List<ir.Primitive> buildFunctionHeader(Iterable<Local> parameters,
-                                        {ClosureScope closureScope,
-                                         ClosureEnvironment env}) {
+  void buildFunctionHeader(Iterable<Local> parameters,
+                           {ClosureScope closureScope,
+                            ClosureEnvironment env}) {
     _createThisParameter();
     _enterClosureEnvironment(env);
     _enterScope(closureScope);
     parameters.forEach(_createFunctionParameter);
-    return _parameters;
   }
 
   /// Creates a parameter for [local] and adds it to the current environment.
   ir.Parameter createLocalParameter(Local local) {
     ir.Parameter parameter = new ir.Parameter(local);
-    _parameters.add(parameter);
     environment.extend(local, parameter);
     return parameter;
   }
@@ -2190,7 +2186,6 @@ class DartIrBuilder extends IrBuilder {
 
   void _createFunctionParameter(Local parameterElement) {
     ir.Parameter parameter = new ir.Parameter(parameterElement);
-    _parameters.add(parameter);
     if (isInMutableVariable(parameterElement)) {
       state.functionParameters.add(getMutableVariable(parameterElement));
     } else {
@@ -2399,7 +2394,6 @@ class JsIrBuilder extends IrBuilder {
 
   void _createFunctionParameter(Local parameterElement) {
     ir.Parameter parameter = new ir.Parameter(parameterElement);
-    _parameters.add(parameter);
     state.functionParameters.add(parameter);
     ClosureLocation location = jsState.boxedVariables[parameterElement];
     if (location != null) {
