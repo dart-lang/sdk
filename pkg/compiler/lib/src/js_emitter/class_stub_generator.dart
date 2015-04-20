3210ee4967c420eedcda755603286ab9e05ee8f7
zarah@google.com <zarah@google.com>
2015-04-20 12:32:49 +0000

    Revert "dart2js: add type arguments to constructor in case the class needs rti."
    This reverts commit r45277.
    
    BUG=
    
    Review URL: https://codereview.chromium.org//1094083002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45281 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/compiler/lib/src/js_emitter/class_stub_generator.dart
+++ pkg/compiler/lib/src/js_emitter/class_stub_generator.dart
@@ -20,14 +20,10 @@ class ClassStubGenerator {
     //        [ constructorName, fields,
     //            fields.map(
     //                (name) => js('this.# = #', [name, name]))]));
-    return js('''function(#, typeInfo) {
-                   #;
-                   if (typeInfo) this.\$builtinTypeInfo = typeInfo;
-                   this.#();
-                 }''',
-              [fields,
-               fields.map((name) => js('this.# = #', [name, name])),
-               namer.deferredAction]);
+    return js('function(#) { #; this.#();}',
+        [fields,
+         fields.map((name) => js('this.# = #', [name, name])),
+         namer.deferredAction]);
   }
 
   jsAst.Expression generateGetter(Element member, String fieldName) {
