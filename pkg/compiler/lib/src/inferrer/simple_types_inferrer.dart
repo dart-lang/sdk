667173800be08998302e592763a421307858034b
sigmund@google.com <sigmund@google.com>
2015-04-21 15:36:22 +0000

    Visit arguments of invalid TypeLiteralSends during inference
    BUG= http://dartbug.com/23264
    R=johnniwinther@google.com
    
    Review URL: https://codereview.chromium.org//1081873005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45315 260f80e4-7a28-3924-810f-c04153c831b5

--- pkg/compiler/lib/src/inferrer/simple_types_inferrer.dart
+++ pkg/compiler/lib/src/inferrer/simple_types_inferrer.dart
@@ -1031,6 +1031,18 @@ class SimpleTypeInferrerVisitor<T>
         node, new Selector.fromElement(element), element, arguments);
   }
 
+  @override
+  T visitTypeLiteralSend(ast.Send node) {
+    if (node.isCall) {
+      // This is reached when users forget to put a `new` in front of a type
+      // literal. The emitter will generate an actual call (even though it is
+      // likely invalid), and for that it needs to have the arguments processed
+      // as well.
+      analyzeArguments(node.arguments);
+    }
+    return super.visitTypeLiteralSend(node);
+  }
+
   T visitStaticSend(ast.Send node) {
     Element element = elements[node];
     assert(!elements.isAssert(node));
