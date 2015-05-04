34aa76f66659d3896448e38ffb382ba670e21798
sethladd@google.com <sethladd@google.com>
2015-05-04 23:17:02 +0000

    add a one-line comment for dart:profiler
    BUG=
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org//1125713005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45514 260f80e4-7a28-3924-810f-c04153c831b5

--- sdk/lib/profiler/profiler.dart
+++ sdk/lib/profiler/profiler.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// Instrument your code with counters, gauges, and more.
 library dart.profiler;
 
 import 'dart:convert';
