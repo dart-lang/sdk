6e7983c67441d633bff5e65b576c30c9d32e5b50
floitsch@google.com <floitsch@google.com>
2015-05-11 19:30:50 +0000

    Fix status file and add accidentally removed test.
    Review URL: https://codereview.chromium.org//1139793002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge/dart@45712 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ tests/isolate/deferred_in_isolate2_test.dart
@@ -0,0 +1,21 @@
+import 'dart:isolate';
+import 'dart:async';
+import 'package:unittest/unittest.dart';
+
+import 'deferred_in_isolate2_lib.dart' deferred as lib;
+
+loadDeferred(port) {
+  lib.loadLibrary().then((_) {
+    port.send(lib.f());
+  });
+}
+
+main() {
+  test("Deferred loading in isolate", () {
+    ReceivePort port = new ReceivePort();
+    port.first.then(expectAsync((msg) {
+       expect(msg, equals("hi"));
+    }));
+    Isolate.spawn(loadDeferred, port.sendPort);
+  });
+}
