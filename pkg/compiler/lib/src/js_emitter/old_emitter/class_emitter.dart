a1b630641e86eb862bbec1d4b84ce8c2eb9d6f65
Stephan Herhut <herhut@google.com>
2015-06-08 12:53:54 +0200

    dart2js: Do not emit type metadata for fields declared in non-reflectable classes.
    BUG=
    R=karlklose@google.com, zarah@google.com
    
    Committed: https://github.com/dart-lang/sdk/commit/67537ffbd7657bf8e679f8d677d658d096e5c7af
    
    Reverted: https://github.com/dart-lang/sdk/commit/6cc3bd11240dae61efdbfae2bdc6ceffc0314c34
    
    Review URL: https://codereview.chromium.org//1169453005

--- pkg/compiler/lib/src/js_emitter/old_emitter/class_emitter.dart
+++ pkg/compiler/lib/src/js_emitter/old_emitter/class_emitter.dart
@@ -161,7 +161,14 @@ class ClassEmitter extends CodeEmitterHelper {
             fieldCode = FIELD_CODE_CHARACTERS[code - FIRST_FIELD_CODE];
           }
         }
-        if (backend.isAccessibleByReflection(fieldElement)) {
+        // Fields can only be reflected if their declaring class is reflectable
+        // (as they are only accessible via [ClassMirror.declarations]).
+        // However, set/get operations can be performed on them, so they are
+        // reflectable in some sense, which leads to [isAccessibleByReflection]
+        // reporting `true`.
+        if (backend.isAccessibleByReflection(fieldElement) &&
+            (fieldElement.isTopLevel ||
+             backend.isAccessibleByReflection(fieldElement.enclosingClass))) {
           DartType type = fieldElement.type;
           reflectionMarker = '-${task.metadataCollector.reifyType(type)}';
         }
