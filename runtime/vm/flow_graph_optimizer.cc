2a1f91914ee9127c980abaf83840c601f20288fe
Srdjan Mitrovic <srdjan@google.com>
2015-06-18 15:27:45 -0700

    Disable TryCreateICData until a crash in object.cc has been investigated.
    BUG=
    
    Review URL: https://codereview.chromium.org//1182323012.

--- runtime/vm/flow_graph_optimizer.cc
+++ runtime/vm/flow_graph_optimizer.cc
@@ -163,6 +163,7 @@ static bool IsNumberCid(intptr_t cid) {
 
 // Attempt to build ICData for call using propagated class-ids.
 bool FlowGraphOptimizer::TryCreateICData(InstanceCallInstr* call) {
+  return false;
   ASSERT(call->HasICData());
   if (call->ic_data()->NumberOfChecks() > 0) {
     // This occurs when an instance call has too many checks, will be converted
