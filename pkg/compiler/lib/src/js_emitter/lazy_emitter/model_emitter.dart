2b1e0999c422ebb84082ebfc37b8dfb70d925ec0
Florian Loitsch <floitsch@google.com>
2015-07-10 11:31:34 +0200

    dart2js: Update library names for the lazy emitter.
    R=herhut@google.com
    
    Review URL: https://codereview.chromium.org//1230793004 .

--- pkg/compiler/lib/src/js_emitter/lazy_emitter/model_emitter.dart
+++ pkg/compiler/lib/src/js_emitter/lazy_emitter/model_emitter.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library dart2js.new_js_emitter.model_emitter;
+library dart2js.js_emitter.lazy_emitter.model_emitter;
 
 import '../../constants/values.dart' show ConstantValue, FunctionConstantValue;
 import '../../dart2jslib.dart' show Compiler;
