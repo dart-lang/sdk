48af868f47cdae5493c97c944f2d7cb8c312f141
Terry L. Lucas <terry@google.com>
2015-07-13 13:24:08 -0700

    Fixed compilation error
    Review URL: https://codereview.chromium.org//1233923003 .

--- sdk/lib/html/dartium/html_dartium.dart
+++ sdk/lib/html/dartium/html_dartium.dart
@@ -13993,7 +13993,7 @@ abstract class Element extends Node implements GlobalEventHandlers, ParentNode, 
 
   @DomName('Element.offsetTop')
   @DocsEditable()
-  int get offsetTop => _blink.BlinkElement.offsetTop_Getter_(thi)).round();
+  int get offsetTop => _blink.BlinkElement.offsetTop_Getter_(this).round();
 
   @DomName('Element.offsetWidth')
   @DocsEditable()
@@ -36166,10 +36166,10 @@ class Url extends NativeFieldWrapperClass2 implements UrlUtils {
     if ((blob_OR_source_OR_stream is Blob || blob_OR_source_OR_stream == null)) {
       return _blink.BlinkURL.instance.createObjectURL_Callback_1_(unwrap_jso(blob_OR_source_OR_stream));
     }
-    if ((blob_OR_source_OR_stream is MediaSource)) {
+    if ((blob_OR_source_OR_stream is MediaStream)) {
       return _blink.BlinkURL.instance.createObjectURL_Callback_1_(unwrap_jso(blob_OR_source_OR_stream));
     }
-    if ((blob_OR_source_OR_stream is MediaStream)) {
+    if ((blob_OR_source_OR_stream is MediaSource)) {
       return _blink.BlinkURL.instance.createObjectURL_Callback_1_(unwrap_jso(blob_OR_source_OR_stream));
     }
     throw new ArgumentError("Incorrect number or type of arguments");
