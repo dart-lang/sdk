2eeb8ef94555b8e8ed79a8a8ff16c830590e919a
Todd Turnidge <turnidge@google.com>
2015-08-03 15:12:44 -0700

    Fix failing version test.
    BUG=
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//1267183002 .

--- runtime/observatory/tests/service/get_version_rpc_test.dart
+++ runtime/observatory/tests/service/get_version_rpc_test.dart
@@ -12,7 +12,7 @@ var tests = [
   (VM vm) async {
     var result = await vm.invokeRpcNoUpgrade('getVersion', {});
     expect(result['type'], equals('Version'));
-    expect(result['major'], equals(1));
+    expect(result['major'], equals(2));
     expect(result['minor'], equals(0));
     expect(result['_privateMajor'], equals(0));
     expect(result['_privateMinor'], equals(0));
