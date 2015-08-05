52e01ec34e52929752a00d405c3dfad5b8b80a84
Florian Loitsch <floitsch@google.com>
2015-08-05 14:24:36 +0200

    dart2js: use correct Compiler class.
    Review URL: https://codereview.chromium.org//1272613003 .

--- pkg/compiler/lib/src/js_emitter/headers.dart
+++ pkg/compiler/lib/src/js_emitter/headers.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 library dart2js.js_emitter.headers;
-import '../apiimpl.dart' show Compiler;
+import '../dart2jslib.dart' show Compiler;
 
 String generatedBy(Compiler compiler, {String flavor: ""}) {
   String suffix = '';
