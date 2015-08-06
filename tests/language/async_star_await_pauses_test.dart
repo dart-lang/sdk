60c7a3d0abb5df3e833e15b279a0c0ff0fd39083
William Hesse <whesse@google.com>
2015-08-06 14:56:30 +0200

    Factor failing tests out of language/async_star_test.dart
    Make async_star_test no longer a multitest, and stop using package:test in
    the factored tests.
    
    BUG=dartbug.com/2238, dartbug.com/22853
    R=lrn@google.com
    
    Review URL: https://codereview.chromium.org//1270413002 .

---
+++ tests/language/async_star_await_pauses_test.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "dart:async";
+import "package:expect/expect.dart";
+import "package:async_helper/async_helper.dart";
+
+main() {
+  var sc;
+  var i = 0;
+  void send() {
+    if (i == 5) {
+      sc.close();
+    } else {
+      sc.add(i++);
+    }
+  }
+  sc = new StreamController(onListen: send, onResume: send);
+
+  f(s) async {
+    var r = 0;
+    await for (var i in s) {
+      r += await new Future.delayed(new Duration(milliseconds: 10), () => i);
+    }
+    return r;
+  }
+
+  asyncStart();
+  f(sc.stream).then((v) {
+    Expect.equals(10, v);
+    asyncEnd();
+  });
+}
