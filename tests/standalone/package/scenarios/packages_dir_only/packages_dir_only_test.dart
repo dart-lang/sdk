f65c675c16be47138f2933fb94dba5ee5b4b437b
Harry Terkelsen <het@google.com>
2015-08-17 17:13:19 -0700

    Add tests for simple packages scenarios
    BUG=
    R=iposva@google.com, pquitslund@google.com
    
    Review URL: https://codereview.chromium.org//1290623002 .

---
+++ tests/standalone/package/scenarios/packages_dir_only/packages_dir_only_test.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// PackageRoot=none
+
+library packages_dir_only_test;
+
+import 'package:foo/foo.dart' as foo;
+
+main() {
+  if (foo.bar != 'hello') {
+    throw new Exception('package "foo" was not resolved correctly');
+  }
+}
