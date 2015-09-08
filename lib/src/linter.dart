5e931b299daa49bd34cd9a61a901d0a9c4621fab
pq <pquitslund@google.com>
2015-09-08 15:43:49 -0700

    Removed debug tracing.

--- lib/src/linter.dart
+++ lib/src/linter.dart
@@ -221,8 +221,6 @@ abstract class LintRule extends Linter implements Comparable<LintRule> {
   }
 
   void reportPubLint(PSNode node) {
-    stderr.writeln(node.span.sourceUrl);
-
     Source source = createSource(node.span.sourceUrl);
 
     // Cache error and location info for creating AnalysisErrorInfos
