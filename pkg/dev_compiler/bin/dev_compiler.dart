d3e6d3af2471e71dab6f83e4bd8d4b4978ee9236
Devon Carew <devoncarew@gmail.com>
2015-09-16 16:13:11 -0700

    support running the dev_compiler package directly

---
+++ pkg/dev_compiler/bin/dev_compiler.dart
@@ -0,0 +1,10 @@
+#!/usr/bin/env dart
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library dev_compiler.bin.dev_compiler;
+
+import 'dartdevc.dart' as dartdevc;
+
+main(List<String> args) => dartdevc.main(args);
