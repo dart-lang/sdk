5d615053ddff2b557266003d3a14dd52f45c0f8f
Alan Knight <alanknight@google.com>
2015-10-09 11:00:23 -0700

    Another attempt at getting NativeUint8 usage in html_common/conversions right
    BUG=
    
    Review URL: https://codereview.chromium.org/1391143003 .

--- sdk/lib/html/html_common/html_common_dart2js.dart
+++ sdk/lib/html/html_common/html_common_dart2js.dart
@@ -10,6 +10,7 @@ import 'dart:html';
 import 'dart:_internal' show WhereIterable;
 import 'dart:web_gl' as gl;
 import 'dart:typed_data';
+import 'dart:_native_typed_data';
 import 'dart:_js_helper' show Creates, Returns, convertDartClosureToJS;
 import 'dart:_foreign_helper' show JS;
 import 'dart:_interceptors' show Interceptor, JSExtendableArray;
