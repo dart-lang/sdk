8eb408914d1410a79af066e43c959fd6e6b51b05
Brian Wilkerson <brianwilkerson@google.com>
2015-10-19 07:46:32 -0700

    Fix library name
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org/1411523007 .

--- pkg/analysis_server/lib/starter.dart
+++ pkg/analysis_server/lib/starter.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library driver;
+library analysis_server.starter;
 
 import 'package:analysis_server/plugin/analysis/resolver_provider.dart';
 import 'package:analysis_server/src/server/driver.dart';
