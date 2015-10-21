fb8cc0aa39e911cb0119a434ba6ea9e6e9ad76af
pq <pquitslund@google.com>
2015-10-21 14:13:59 -0700

    Unsupported analysis option validation.
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org/1418533004 .

--- pkg/analyzer/test/src/task/options_test.dart
+++ pkg/analyzer/test/src/task/options_test.dart
@@ -5,6 +5,7 @@
 library test.src.task.options_test;
 
 import 'package:analyzer/analyzer.dart';
+import 'package:analyzer/source/analysis_options_provider.dart';
 import 'package:analyzer/src/generated/engine.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/task/options.dart';
@@ -18,6 +19,7 @@ import '../context/abstract_context.dart';
 main() {
   initializeTestEnvironment();
   runReflectiveTests(GenerateOptionsErrorsTaskTest);
+  runReflectiveTests(OptionsFileValidatorTest);
 }
 
 isInstanceOf isGenerateOptionsErrorsTask =
@@ -81,6 +83,21 @@ class GenerateOptionsErrorsTaskTest extends AbstractContextTest {
     expect(errors[0].errorCode, AnalysisOptionsErrorCode.PARSE_ERROR);
   }
 
+  test_perform_unsupported_analyzer_option() {
+    String code = r'''
+analyzer:
+  not_supported: true
+''';
+    AnalysisTarget target = newSource(optionsFilePath, code);
+    computeResult(target, ANALYSIS_OPTIONS_ERRORS);
+    expect(task, isGenerateOptionsErrorsTask);
+    List<AnalysisError> errors = outputs[ANALYSIS_OPTIONS_ERRORS];
+    expect(errors, hasLength(1));
+    expect(errors[0].errorCode, AnalysisOptionsWarningCode.UNSUPPORTED_OPTION);
+    expect(errors[0].message,
+        "The option 'not_supported' is not supported by analyzer");
+  }
+
   test_perform_OK() {
     String code = r'''
 analyzer:
@@ -90,5 +107,46 @@ analyzer:
     computeResult(target, ANALYSIS_OPTIONS_ERRORS);
     expect(task, isGenerateOptionsErrorsTask);
     expect(outputs[ANALYSIS_OPTIONS_ERRORS], isEmpty);
+  }
+}
+
+@reflectiveTest
+class OptionsFileValidatorTest {
+  final OptionsFileValidator validator = new OptionsFileValidator(null);
+  final AnalysisOptionsProvider optionsProvider = new AnalysisOptionsProvider();
+
+  test_analyzer_supported_exclude() {
+    validate(
+        '''
+analyzer:
+  exclude:
+    - test/_data/p4/lib/lib1.dart
+    ''',
+        []);
+  }
+
+  test_analyzer_supported_strong_mode() {
+    validate(
+        '''
+analyzer:
+  strong-mode: true
+    ''',
+        []);
+  }
+
+  test_analyzer_unsupported_option() {
+    validate(
+        '''
+analyzer:
+  not_supported: true
+    ''',
+        [AnalysisOptionsWarningCode.UNSUPPORTED_OPTION]);
+  }
+
+  void validate(String source, List<AnalysisOptionsErrorCode> expected) {
+    var options = optionsProvider.getOptionsFromString(source);
+    var errors = validator.validate(options);
+    expect(errors.map((AnalysisError e) => e.errorCode),
+        unorderedEquals(expected));
   }
 }
