9731bac3f65a3e78f7c435768958b3f6e9aeeaaf
Siva Annamalai <asiva@google.com>
2015-11-03 12:41:48 -0800

    Fix build break, ensure the simulator is created and setup before accessing it in LongJumpScope::IsSafeToJump
    Review URL: https://codereview.chromium.org/1415753004 .

--- runtime/vm/longjump.cc
+++ runtime/vm/longjump.cc
@@ -28,8 +28,8 @@ bool LongJumpScope::IsSafeToJump() {
   Thread* thread = Thread::Current();
   uword jumpbuf_addr = Isolate::GetCurrentStackPointer();
 #if defined(USING_SIMULATOR)
-  Isolate* isolate = thread->isolate();
-  uword top_exit_frame_info = isolate->simulator()->top_exit_frame_info();
+  Simulator* sim = Simulator::Current();
+  uword top_exit_frame_info = sim->top_exit_frame_info();
 #else
   uword top_exit_frame_info = thread->top_exit_frame_info();
 #endif
