bad39686a4197a587419712ee05320543aaaf141
Srdjan Mitrovic <srdjan@google.com>
2015-11-18 15:28:34 -0800

    Revert "Enable deferred deoptimization always."
    This reverts commit 94754bee2963edb3a4dc32d47cdf0b41fc6ab0cb.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1462703002 .

--- runtime/vm/flow_graph_optimizer.cc
+++ runtime/vm/flow_graph_optimizer.cc
@@ -4654,10 +4654,14 @@ void FlowGraphOptimizer::VisitStoreInstanceField(
         }
       }
       field.set_is_unboxing_candidate(false);
-      // Delay deoptimization of dependent code to the code installation time.
-      // The invalidation of the background compilation result occurs only
-      // when the deoptimization is triggered at code installation.
-      flow_graph()->deoptimize_dependent_code().Add(&field);
+      if (Compiler::IsBackgroundCompilation()) {
+        // Delay deoptimization of dependent code to the code installation time.
+        // The invalidation of the background compilation result occurs only
+        // when the deoptimization is triggered at code installation.
+        flow_graph()->deoptimize_dependent_code().Add(&field);
+      } else {
+        field.DeoptimizeDependentCode();
+      }
     } else {
       FlowGraph::AddToGuardedFields(flow_graph_->guarded_fields(), &field);
     }
