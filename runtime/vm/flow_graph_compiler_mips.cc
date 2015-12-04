688d5cfcc30a539a1956eba5c2b93c95b313b342
Ryan Macnak <rmacnak@google.com>
2015-12-04 12:40:25 -0800

    Precompilation/mips: Fix SwitchableCalls to use patchable object pool entries for the lookup stubs.
    R=srdjan@google.com
    
    Review URL: https://codereview.chromium.org/1500083002 .

--- runtime/vm/flow_graph_compiler_mips.cc
+++ runtime/vm/flow_graph_compiler_mips.cc
@@ -1312,7 +1312,7 @@ void FlowGraphCompiler::EmitSwitchableInstanceCall(
   __ lw(T0, Address(SP, (argument_count - 1) * kWordSize));
   if (ic_data.NumArgsTested() == 1) {
     __ LoadUniqueObject(S5, ic_data);
-    __ BranchLink(*StubCode::ICLookup_entry());
+    __ BranchLinkPatchable(*StubCode::ICLookup_entry());
   } else {
     const String& name = String::Handle(zone(), ic_data.target_name());
     const Array& arguments_descriptor =
@@ -1323,7 +1323,7 @@ void FlowGraphCompiler::EmitSwitchableInstanceCall(
         MegamorphicCacheTable::Lookup(isolate(), name, arguments_descriptor));
 
     __ LoadUniqueObject(S5, cache);
-    __ BranchLink(*StubCode::MegamorphicLookup_entry());
+    __ BranchLinkPatchable(*StubCode::MegamorphicLookup_entry());
   }
   __ jalr(T1);
 
