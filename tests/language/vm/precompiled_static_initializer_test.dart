2b7ff6dfaba9723ee1fa467554b8c2fd4b42ae29
Florian Schneider <fschneider@google.com>
2015-12-04 13:08:14 +0100

    Fix test by removing the --noopt flag from the test file.
    Not all platforms (ia32) support --noopt, and test.py --noopt will run the test
    with this option anyway.
    
    TBR=rmacnak@google.com
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1497213002 .

--- tests/language/vm/precompiled_static_initializer_test.dart
+++ tests/language/vm/precompiled_static_initializer_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 // Test optimizations with static fields with precompilation.
-// VMOptions=--noopt --inlining-hotness=0
+// VMOptions=--inlining-hotness=0
 
 import 'package:expect/expect.dart';
 
