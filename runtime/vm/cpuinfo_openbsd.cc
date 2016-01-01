c057343d0e165a9c5cdd48c7dd1acc74a0057d21
Adam Wolk <adam.wolk@tintagel.pl>
2016-01-01 18:42:36 +0100

    Implement cpuinfo for OpenBSD using cpuid path from linux

--- runtime/vm/cpuinfo_openbsd.cc
+++ runtime/vm/cpuinfo_openbsd.cc
@@ -16,52 +16,60 @@ CpuInfoMethod CpuInfo::method_ = kCpuInfoDefault;
 const char* CpuInfo::fields_[kCpuInfoMax] = {0};
 
 void CpuInfo::InitOnce() {
-  UNIMPLEMENTED();
-
+  fields_[kCpuInfoProcessor] = "vendor_id";
+  fields_[kCpuInfoModel] = "model name";
+  fields_[kCpuInfoHardware] = "model name";
+  fields_[kCpuInfoFeatures] = "flags";
+  method_ = kCpuInfoCpuId;
+  CpuId::InitOnce();
   return;
 }
 
 
 void CpuInfo::Cleanup() {
-  UNIMPLEMENTED();
-
+  CpuId::Cleanup();
   return;
 }
 
 
 bool CpuInfo::FieldContains(CpuInfoIndices idx, const char* search_string) {
-  UNIMPLEMENTED();
-
-  return false;
+  return strstr(CpuId::field(idx), search_string);
 }
 
 
 bool CpuInfo::FieldContainsByString(const char* field,
                                     const char* search_string) {
+  for (int i = 0; i < kCpuInfoMax; i++) {
+    if (strcmp(field, fields_[i]) == 0) {
+      return FieldContains(static_cast<CpuInfoIndices>(i), search_string);
+    }
+  }
   UNIMPLEMENTED();
-
   return false;
 }
 
 
 const char* CpuInfo::ExtractField(CpuInfoIndices idx) {
-  UNIMPLEMENTED();
-
-  return "";
+  return CpuId::field(idx);
 }
 
 
 const char* CpuInfo::ExtractFieldByString(const char* field) {
+  for (int i = 0; i < kCpuInfoMax; i++) {
+    if (strcmp(field, fields_[i]) == 0) {
+      return ExtractField(static_cast<CpuInfoIndices>(i));
+    }
+  }
   UNIMPLEMENTED();
-
-  return "";
+  return NULL;
 }
 
 
 bool CpuInfo::HasField(const char* field) {
-  UNIMPLEMENTED();
-
-  return false;
+  return (strcmp(field, fields_[kCpuInfoProcessor]) == 0) ||
+    (strcmp(field, fields_[kCpuInfoModel]) == 0) ||
+    (strcmp(field, fields_[kCpuInfoHardware]) == 0) ||
+    (strcmp(field, fields_[kCpuInfoFeatures]) == 0);
 }
 
 }  // namespace dart
