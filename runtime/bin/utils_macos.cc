dart-lang/sdk

commit dd4ef2909893c055f584e1e8aeecd4c093b077e2

Refactor monotonic clock interface and use raw tick values in stopwatch
- Split interface to provide direct access to raw clock value and clock frequency.
- Update Stopwatch to use raw clock value and clock frequency instead of microseconds.
- Fix possible initialization race on OSX

R=rmacnak@google.com

Review URL: https://codereview.chromium.org/1563473002 .

runtime/bin/utils_macos.cc
@@ -74,7 +74,11 @@ bool ShellUtils::GetUtf8Argv(int argc, char** argv) {
   return false;
 }
 
+static mach_timebase_info_data_t timebase_info;
+
 void TimerUtils::InitOnce() {
+  kern_return_t kr = mach_timebase_info(&timebase_info);
+  ASSERT(KERN_SUCCESS == kr);
 }
 
 int64_t TimerUtils::GetCurrentMonotonicMillis() {
@@ -97,17 +101,7 @@ int64_t TimerUtils::GetCurrentMonotonicMicros() {
   origin += boottime.tv_usec;
   return now - origin;
 #else
-  static mach_timebase_info_data_t timebase_info;
-  if (timebase_info.denom == 0) {
-    // Zero-initialization of statics guarantees that denom will be 0 before
-    // calling mach_timebase_info.  mach_timebase_info will never set denom to
-    // 0 as that would be invalid, so the zero-check can be used to determine
-    // whether mach_timebase_info has already been called.  This is
-    // recommended by Apple's QA1398.
-    kern_return_t kr = mach_timebase_info(&timebase_info);
-    ASSERT(KERN_SUCCESS == kr);
-  }
-
+  ASSERT(timebase_info.denom != 0);
   // timebase_info converts absolute time tick units into nanoseconds.  Convert
   // to microseconds.
   int64_t result = mach_absolute_time() / kNanosecondsPerMicrosecond;

runtime/lib/stopwatch.cc
@@ -10,14 +10,12 @@
 namespace dart {
 
 DEFINE_NATIVE_ENTRY(Stopwatch_now, 0) {
-  // TODO(iposva): investigate other hi-res time sources such as cycle count.
-  return Integer::New(OS::GetCurrentMonotonicMicros());
+  return Integer::New(OS::GetCurrentMonotonicTicks());
 }
 
 
 DEFINE_NATIVE_ENTRY(Stopwatch_frequency, 0) {
-  // TODO(iposva): investigate other hi-res time sources such as cycle count.
-  return Integer::New(1000000);
+  return Integer::New(OS::GetCurrentMonotonicFrequency());
 }
 
 }  // namespace dart

runtime/vm/os.h
@@ -50,6 +50,12 @@ class OS {
   // Returns the current time used by the tracing infrastructure.
   static int64_t GetCurrentMonotonicMicros();
 
+  // Returns the raw clock value from the monotonic clock.
+  static int64_t GetCurrentMonotonicTicks();
+
+  // Returns the frequency of the monotonic clock.
+  static int64_t GetCurrentMonotonicFrequency();
+
   // Returns a cleared aligned array of type T with n entries.
   // Alignment must be >= 16 and a power of two.
   template<typename T>

runtime/vm/os_android.cc
@@ -179,17 +179,29 @@ int64_t OS::GetCurrentTimeMicros() {
 }
 
 
-int64_t OS::GetCurrentMonotonicMicros() {
+int64_t OS::GetCurrentMonotonicTicks() {
   struct timespec ts;
   if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
     UNREACHABLE();
     return 0;
   }
-  // Convert to microseconds.
+  // Convert to nanoseconds.
   int64_t result = ts.tv_sec;
-  result *= kMicrosecondsPerSecond;
-  result += (ts.tv_nsec / kNanosecondsPerMicrosecond);
+  result *= kNanosecondsPerSecond;
+  result += ts.tv_nsec;
   return result;
+}
+
+
+int64_t OS::GetCurrentMonotonicFrequency() {
+  return kNanosecondsPerSecond;
+}
+
+
+int64_t OS::GetCurrentMonotonicMicros() {
+  int64_t ticks = GetCurrentMonotonicTicks();
+  ASSERT(GetCurrentMonotonicFrequency() == kNanosecondsPerSecond);
+  return ticks / kNanosecondsPerMicrosecond;
 }
 
 

runtime/vm/os_linux.cc
@@ -396,17 +396,29 @@ int64_t OS::GetCurrentTimeMicros() {
 }
 
 
-int64_t OS::GetCurrentMonotonicMicros() {
+int64_t OS::GetCurrentMonotonicTicks() {
   struct timespec ts;
   if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
     UNREACHABLE();
     return 0;
   }
-  // Convert to microseconds.
+  // Convert to nanoseconds.
   int64_t result = ts.tv_sec;
-  result *= kMicrosecondsPerSecond;
-  result += (ts.tv_nsec / kNanosecondsPerMicrosecond);
+  result *= kNanosecondsPerSecond;
+  result += ts.tv_nsec;
   return result;
+}
+
+
+int64_t OS::GetCurrentMonotonicFrequency() {
+  return kNanosecondsPerSecond;
+}
+
+
+int64_t OS::GetCurrentMonotonicMicros() {
+  int64_t ticks = GetCurrentMonotonicTicks();
+  ASSERT(GetCurrentMonotonicFrequency() == kNanosecondsPerSecond);
+  return ticks / kNanosecondsPerMicrosecond;
 }
 
 

runtime/vm/os_macos.cc
@@ -90,7 +90,10 @@ int64_t OS::GetCurrentTimeMicros() {
 }
 
 
-int64_t OS::GetCurrentMonotonicMicros() {
+static mach_timebase_info_data_t timebase_info;
+
+
+int64_t OS::GetCurrentMonotonicTicks() {
 #if TARGET_OS_IOS
   // On iOS mach_absolute_time stops while the device is sleeping. Instead use
   // now - KERN_BOOTTIME to get a time difference that is not impacted by clock
@@ -106,23 +109,32 @@ int64_t OS::GetCurrentMonotonicMicros() {
   origin += boottime.tv_usec;
   return now - origin;
 #else
-  static mach_timebase_info_data_t timebase_info;
-  if (timebase_info.denom == 0) {
-    // Zero-initialization of statics guarantees that denom will be 0 before
-    // calling mach_timebase_info.  mach_timebase_info will never set denom to
-    // 0 as that would be invalid, so the zero-check can be used to determine
-    // whether mach_timebase_info has already been called.  This is
-    // recommended by Apple's QA1398.
-    kern_return_t kr = mach_timebase_info(&timebase_info);
-    ASSERT(KERN_SUCCESS == kr);
-  }
-
-  // timebase_info converts absolute time tick units into nanoseconds.  Convert
-  // to microseconds.
-  int64_t result = mach_absolute_time() / kNanosecondsPerMicrosecond;
+  ASSERT(timebase_info.denom != 0);
+  // timebase_info converts absolute time tick units into nanoseconds.
+  int64_t result = mach_absolute_time();
   result *= timebase_info.numer;
   result /= timebase_info.denom;
   return result;
+#endif  // TARGET_OS_IOS
+}
+
+
+int64_t OS::GetCurrentMonotonicFrequency() {
+#if TARGET_OS_IOS
+  return kMicrosecondsPerSecond;
+#else
+  return kNanosecondsPerSecond;
+#endif  // TARGET_OS_IOS
+}
+
+
+int64_t OS::GetCurrentMonotonicMicros() {
+#if TARGET_OS_IOS
+  ASSERT(GetCurrentMonotonicFrequency() == kMicrosecondsPerSecond);
+  return GetCurrentMonotonicTicks();
+#else
+  ASSERT(GetCurrentMonotonicFrequency() == kNanosecondsPerSecond);
+  return GetCurrentMonotonicTicks() / kNanosecondsPerMicrosecond;
 #endif  // TARGET_OS_IOS
 }
 
@@ -361,6 +373,8 @@ void OS::InitOnce() {
   static bool init_once_called = false;
   ASSERT(init_once_called == false);
   init_once_called = true;
+  kern_return_t kr = mach_timebase_info(&timebase_info);
+  ASSERT(KERN_SUCCESS == kr);
 }
 
 

runtime/vm/os_win.cc
@@ -123,7 +123,8 @@ int64_t OS::GetCurrentTimeMicros() {
 
 static int64_t qpc_ticks_per_second = 0;
 
-int64_t OS::GetCurrentMonotonicMicros() {
+
+int64_t OS::GetCurrentMonotonicTicks() {
   if (qpc_ticks_per_second == 0) {
     // QueryPerformanceCounter not supported, fallback.
     return GetCurrentTimeMicros();
@@ -132,11 +133,27 @@ int64_t OS::GetCurrentMonotonicMicros() {
   LARGE_INTEGER now;
   QueryPerformanceCounter(&now);
   int64_t qpc_value = static_cast<int64_t>(now.QuadPart);
+}
+
+
+int64_t OS::GetCurrentMonotonicFrequency() {
+  if (qpc_ticks_per_second == 0) {
+    // QueryPerformanceCounter not supported, fallback.
+    return kMicrosecondsPerSecond;
+  }
+  return qpc_ticks_per_second;
+}
+
+
+int64_t OS::GetCurrentMonotonicMicros() {
+  int64_t ticks = GetCurrentMonotonicTicks();
+  int64_t frequency = GetCurrentMonotonicFrequency();
+
   // Convert to microseconds.
-  int64_t seconds = qpc_value / qpc_ticks_per_second;
-  int64_t leftover_ticks = qpc_value - (seconds * qpc_ticks_per_second);
+  int64_t seconds = ticks / frequency;
+  int64_t leftover_ticks = ticks - (seconds * frequency);
   int64_t result = seconds * kMicrosecondsPerSecond;
-  result += ((leftover_ticks * kMicrosecondsPerSecond) / qpc_ticks_per_second);
+  result += ((leftover_ticks * kMicrosecondsPerSecond) / frequency);
   return result;
 }
 

commit f85c015ccd3bc466edd0e7459af9bd13511b1a7c

Fix iOS implementation of |GetCurrentMonotonicMicros|. Fixes regression introduced in dd4ef2909893c055f584e1e8aeecd4c093b077e2
R=rmacnak@google.com

Review URL: https://codereview.chromium.org/1571343003 .

runtime/bin/utils_macos.cc
@@ -13,6 +13,10 @@
 #include <sys/time.h>  // NOLINT
 #include <time.h>  // NOLINT
 
+#if TARGET_OS_IOS
+#include <sys/sysctl.h>  // NOLINT
+#endif
+
 #include "bin/utils.h"
 #include "platform/assert.h"
 #include "platform/utils.h"
@@ -84,6 +88,20 @@ void TimerUtils::InitOnce() {
 int64_t TimerUtils::GetCurrentMonotonicMillis() {
   return GetCurrentMonotonicMicros() / 1000;
 }
+
+#if TARGET_OS_IOS
+
+static int64_t GetCurrentTimeMicros() {
+  // gettimeofday has microsecond resolution.
+  struct timeval tv;
+  if (gettimeofday(&tv, NULL) < 0) {
+    UNREACHABLE();
+    return 0;
+  }
+  return (static_cast<int64_t>(tv.tv_sec) * 1000000) + tv.tv_usec;
+}
+
+#endif  // TARGET_OS_IOS
 
 int64_t TimerUtils::GetCurrentMonotonicMicros() {
 #if TARGET_OS_IOS