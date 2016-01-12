dart-lang/sdk

commit a7ebeb00fbb09b774836239b31d2c7defcc12c9c

Add a CompareAndSwapUint32 variant to AtomicOperations so that it is possible to a CAS of a 32 bit value.
R=rmacnak@google.com

Review URL: https://codereview.chromium.org/1562253002 .

runtime/vm/simulator_arm.cc
@@ -1171,6 +1171,14 @@ uword Simulator::CompareExchange(uword* address,
 }
 
 
+uint32_t Simulator::CompareExchangeUint32(uint32_t* address,
+                                          uint32_t compare_value,
+                                          uint32_t new_value) {
+  ASSERT(sizeof(uword) == sizeof(uint32_t));
+  return CompareExchange(address, compare_value, new_value);
+}
+
+
 // Returns the top of the stack area to enable checking for stack pointer
 // validity.
 uword Simulator::StackTop() const {

runtime/vm/simulator_mips.cc
@@ -1191,6 +1191,14 @@ uword Simulator::CompareExchange(uword* address,
 }
 
 
+uint32_t Simulator::CompareExchangeUint32(uint32_t* address,
+                                          uint32_t compare_value,
+                                          uint32_t new_value) {
+  ASSERT(sizeof(uword) == sizeof(uint32_t));
+  return CompareExchange(address, compare_value, new_value);
+}
+
+
 // Calls into the Dart runtime are based on this interface.
 typedef void (*SimulatorRuntimeCall)(NativeArguments arguments);
 

runtime/vm/atomic.h
@@ -33,6 +33,8 @@ class AtomicOperations : public AllStatic {
   //
   // NOTE: OK to use with memory locations that are accessed by generated code
   static uword CompareAndSwapWord(uword* ptr, uword old_value, uword new_value);
+  static uint32_t CompareAndSwapUint32(
+      uint32_t* ptr, uint32_t old_value, uint32_t new_value);
 
   // Performs a load of a word from 'ptr', but without any guarantees about
   // memory order (i.e., no load barriers/fences).

runtime/vm/atomic_android.h
@@ -32,6 +32,13 @@ inline uword AtomicOperations::CompareAndSwapWord(uword* ptr,
                                                   uword new_value) {
   return __sync_val_compare_and_swap(ptr, old_value, new_value);
 }
+
+
+inline uint32_t AtomicOperations::CompareAndSwapUint32(uint32_t* ptr,
+                                                       uint32_t old_value,
+                                                       uint32_t new_value) {
+  return __sync_val_compare_and_swap(ptr, old_value, new_value);
+}
 #endif  // !defined(USING_SIMULATOR)
 
 }  // namespace dart

runtime/vm/atomic_linux.h
@@ -32,6 +32,13 @@ inline uword AtomicOperations::CompareAndSwapWord(uword* ptr,
                                                   uword new_value) {
   return __sync_val_compare_and_swap(ptr, old_value, new_value);
 }
+
+
+inline uint32_t AtomicOperations::CompareAndSwapUint32(uint32_t* ptr,
+                                                       uint32_t old_value,
+                                                       uint32_t new_value) {
+  return __sync_val_compare_and_swap(ptr, old_value, new_value);
+}
 #endif  // !defined(USING_SIMULATOR)
 
 }  // namespace dart

runtime/vm/atomic_macos.h
@@ -32,6 +32,13 @@ inline uword AtomicOperations::CompareAndSwapWord(uword* ptr,
                                                   uword new_value) {
   return __sync_val_compare_and_swap(ptr, old_value, new_value);
 }
+
+
+inline uint32_t AtomicOperations::CompareAndSwapUint32(uint32_t* ptr,
+                                                       uint32_t old_value,
+                                                       uint32_t new_value) {
+  return __sync_val_compare_and_swap(ptr, old_value, new_value);
+}
 #endif  // !defined(USING_SIMULATOR)
 
 }  // namespace dart

runtime/vm/atomic_simulator.h
@@ -18,6 +18,13 @@ inline uword AtomicOperations::CompareAndSwapWord(uword* ptr,
                                                   uword new_value) {
   return Simulator::CompareExchange(ptr, old_value, new_value);
 }
+
+
+inline uint32_t AtomicOperations::CompareAndSwapUint32(uint32_t* ptr,
+                                                       uint32_t old_value,
+                                                       uint32_t new_value) {
+  return Simulator::CompareExchangeUint32(ptr, old_value, new_value);
+}
 #endif  // defined(USING_SIMULATOR)
 
 }  // namespace dart

runtime/vm/atomic_test.cc
@@ -31,4 +31,22 @@ UNIT_TEST_CASE(LoadRelaxed) {
   EXPECT_EQ(static_cast<uword>(42), AtomicOperations::LoadRelaxed(&v));
 }
 
+
+UNIT_TEST_CASE(CompareAndSwapWord) {
+  uword old_value = 42;
+  uword new_value = 100;
+  uword result = AtomicOperations::CompareAndSwapWord(
+      &old_value, old_value, new_value);
+  EXPECT_EQ(static_cast<uword>(42), result);
+}
+
+
+UNIT_TEST_CASE(CompareAndSwapUint32) {
+  uint32_t old_value = 42;
+  uint32_t new_value = 100;
+  uint32_t result = AtomicOperations::CompareAndSwapUint32(
+      &old_value, old_value, new_value);
+  EXPECT_EQ(static_cast<uint32_t>(42), result);
+}
+
 }  // namespace dart

commit 87eae28239f285d9e363e0cd2075b07592a50a41

Fix simarm and simmips builds on Mac.
Broken by a7ebeb00fbb09b774836239b31d2c7defcc12c9c.

R=asiva@google.com

Review URL: https://codereview.chromium.org/1574393002 .

runtime/vm/simulator_arm.cc
@@ -1174,8 +1174,10 @@ uword Simulator::CompareExchange(uword* address,
 uint32_t Simulator::CompareExchangeUint32(uint32_t* address,
                                           uint32_t compare_value,
                                           uint32_t new_value) {
-  ASSERT(sizeof(uword) == sizeof(uint32_t));
-  return CompareExchange(address, compare_value, new_value);
+  COMPILE_ASSERT(sizeof(uword) == sizeof(uint32_t));
+  return CompareExchange(reinterpret_cast<uword*>(address),
+                         static_cast<uword>(compare_value),
+                         static_cast<uword>(new_value));
 }
 
 

runtime/vm/simulator_mips.cc
@@ -1194,8 +1194,10 @@ uword Simulator::CompareExchange(uword* address,
 uint32_t Simulator::CompareExchangeUint32(uint32_t* address,
                                           uint32_t compare_value,
                                           uint32_t new_value) {
-  ASSERT(sizeof(uword) == sizeof(uint32_t));
-  return CompareExchange(address, compare_value, new_value);
+  COMPILE_ASSERT(sizeof(uword) == sizeof(uint32_t));
+  return CompareExchange(reinterpret_cast<uword*>(address),
+                         static_cast<uword>(compare_value),
+                         static_cast<uword>(new_value));
 }
 
 