dart-lang/sdk

commit 1ecd06d39cbfa120709f1d986c25761e17f68e53

Add a count of total lines to status performance page
R=scheglov@google.com

Review URL: https://codereview.chromium.org/1622713002 .

pkg/analysis_server/lib/src/status/get_handler.dart
@@ -639,8 +639,16 @@ class GetHandler {
           Set<AnalysisTarget> countedTargets = new HashSet<AnalysisTarget>();
           Map<String, int> sourceTypeCounts = new HashMap<String, int>();
           Map<String, int> typeCounts = new HashMap<String, int>();
+          int explicitSourceCount = 0;
+          int explicitLineInfoCount = 0;
+          int explicitLineCount = 0;
+          int implicitSourceCount = 0;
+          int implicitLineInfoCount = 0;
+          int implicitLineCount = 0;
           analysisServer.folderMap
               .forEach((Folder folder, InternalAnalysisContext context) {
+            Set<Source> explicitSources = new HashSet<Source>();
+            Set<Source> implicitSources = new HashSet<Source>();
             AnalysisCache cache = context.analysisCache;
             MapIterator<AnalysisTarget, CacheEntry> iterator = cache.iterator();
             while (iterator.moveNext()) {
@@ -651,8 +659,10 @@ class GetHandler {
                   String sourceName;
                   if (AnalysisEngine.isDartFileName(name)) {
                     if (iterator.value.explicitlyAdded) {
+                      explicitSources.add(target);
                       sourceName = 'Dart file (explicit)';
                     } else {
+                      implicitSources.add(target);
                       sourceName = 'Dart file (implicit)';
                     }
                   } else if (AnalysisEngine.isHtmlFileName(name)) {
@@ -677,6 +687,25 @@ class GetHandler {
                 }
               }
             }
+
+            int lineCount(Set<Source> sources, bool explicit) {
+              return sources.fold(0, (int previousTotal, Source source) {
+                LineInfo lineInfo = context.getLineInfo(source);
+                if (lineInfo == null) {
+                  return previousTotal;
+                }
+                if (explicit) {
+                  explicitLineInfoCount++;
+                } else {
+                  implicitLineInfoCount++;
+                }
+                return previousTotal + lineInfo.lineCount;
+              });
+            }
+            explicitSourceCount += explicitSources.length;
+            explicitLineCount += lineCount(explicitSources, true);
+            implicitSourceCount += implicitSources.length;
+            implicitLineCount += lineCount(implicitSources, false);
           });
           List<String> sourceTypeNames = sourceTypeCounts.keys.toList();
           sourceTypeNames.sort();
@@ -696,6 +725,37 @@ class GetHandler {
             _writeRow(buffer, [typeName, typeCounts[typeName]],
                 classes: [null, "right"]);
           }
+          buffer.write('</table>');
+
+          buffer.write('<p><b>Line counts</b></p>');
+          buffer.write(
+              '<table style="border-collapse: separate; border-spacing: 10px 5px;">');
+          _writeRow(buffer, ['Kind', 'Lines of Code', 'Source Counts'],
+              header: true);
+          _writeRow(buffer, [
+            'Explicit',
+            explicitLineCount.toString(),
+            '$explicitLineInfoCount / $explicitSourceCount'
+          ], classes: [
+            null,
+            "right"
+          ]);
+          _writeRow(buffer, [
+            'Implicit',
+            implicitLineCount.toString(),
+            '$implicitLineInfoCount / $implicitSourceCount'
+          ], classes: [
+            null,
+            "right"
+          ]);
+          _writeRow(buffer, [
+            'Total',
+            (explicitLineCount + implicitLineCount).toString(),
+            '${explicitLineInfoCount + implicitLineInfoCount} / ${explicitSourceCount + implicitSourceCount}'
+          ], classes: [
+            null,
+            "right"
+          ]);
           buffer.write('</table>');
         }, (StringBuffer buffer) {
           //

pkg/analyzer/lib/src/generated/source.dart
@@ -178,12 +178,12 @@ class DartUriResolver extends UriResolver {
 }
 
 /**
- * Instances of the class `LineInfo` encapsulate information about line and column information
- * within a source file.
+ * Information about line and column information within a source file.
  */
 class LineInfo {
   /**
-   * An array containing the offsets of the first character of each line in the source code.
+   * A list containing the offsets of the first character of each line in the
+   * source code.
    */
   final List<int> _lineStarts;
 
@@ -194,10 +194,8 @@ class LineInfo {
   int _previousLine = 0;
 
   /**
-   * Initialize a newly created set of line information to represent the data encoded in the given
-   * array.
-   *
-   * @param lineStarts the offsets of the first character of each line in the source code
+   * Initialize a newly created set of line information to represent the data
+   * encoded in the given list of [_lineStarts].
    */
   LineInfo(this._lineStarts) {
     if (_lineStarts == null) {
@@ -208,10 +206,12 @@ class LineInfo {
   }
 
   /**
-   * Return the location information for the character at the given offset.
-   *
-   * @param offset the offset of the character for which location information is to be returned
-   * @return the location information for the character at the given offset
+   * Return the number of lines in the file.
+   */
+  int get lineCount => _lineStarts.length;
+
+  /**
+   * Return the location information for the character at the given [offset].
    */
   LineInfo_Location getLocation(int offset) {
     var min = 0;

commit 201e980fd29cf1777b3b74ece067fcd4d7534e0a

Rework recent changes to LineInfo to be backward compatible.
Commit 1ecd06d39cbfa120709f1d986c25761e17f68e53 introduced a new getter
to the LineInfo class, breaking clients such as linter that use
LineInfo in an `implements` clause.

This CL reworks the change to be backward compatible.  It may be
reverted once we are ready to roll to a new major version of analyzer.

R=brianwilkerson@google.com, scheglov@google.com

Review URL: https://codereview.chromium.org/1625783002 .

pkg/analysis_server/lib/src/status/get_handler.dart
@@ -691,15 +691,16 @@ class GetHandler {
             int lineCount(Set<Source> sources, bool explicit) {
               return sources.fold(0, (int previousTotal, Source source) {
                 LineInfo lineInfo = context.getLineInfo(source);
-                if (lineInfo == null) {
+                if (lineInfo is LineInfoWithCount) {
+                  if (explicit) {
+                    explicitLineInfoCount++;
+                  } else {
+                    implicitLineInfoCount++;
+                  }
+                  return previousTotal + lineInfo.lineCount;
+                } else {
                   return previousTotal;
                 }
-                if (explicit) {
-                  explicitLineInfoCount++;
-                } else {
-                  implicitLineInfoCount++;
-                }
-                return previousTotal + lineInfo.lineCount;
               });
             }
             explicitSourceCount += explicitSources.length;

pkg/analyzer/lib/src/generated/source.dart
@@ -197,18 +197,19 @@ class LineInfo {
    * Initialize a newly created set of line information to represent the data
    * encoded in the given list of [_lineStarts].
    */
-  LineInfo(this._lineStarts) {
+  factory LineInfo(List<int> _lineStarts) => new LineInfoWithCount(_lineStarts);
+
+  /**
+   * Initialize a newly created set of line information to represent the data
+   * encoded in the given list of [_lineStarts].
+   */
+  LineInfo._(this._lineStarts) {
     if (_lineStarts == null) {
       throw new IllegalArgumentException("lineStarts must be non-null");
     } else if (_lineStarts.length < 1) {
       throw new IllegalArgumentException("lineStarts must be non-empty");
     }
   }
-
-  /**
-   * Return the number of lines in the file.
-   */
-  int get lineCount => _lineStarts.length;
 
   /**
    * Return the location information for the character at the given [offset].
@@ -285,6 +286,26 @@ class LineInfo_Location {
 
   @override
   String toString() => '$lineNumber:$columnNumber';
+}
+
+/**
+ * Information about line and column information within a source file,
+ * including a count of the total number of lines.
+ *
+ * TODO(paulberry): in the next major version roll of analyzer, merge this
+ * class into [LineInfo].
+ */
+class LineInfoWithCount extends LineInfo {
+  /**
+   * Initialize a newly created set of line information to represent the data
+   * encoded in the given list of [_lineStarts].
+   */
+  LineInfoWithCount(List<int> _lineStarts) : super._(_lineStarts);
+
+  /**
+   * Return the number of lines in the file.
+   */
+  int get lineCount => _lineStarts.length;
 }
 
 /**