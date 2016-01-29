980c580483f9e1d65ecfcba092055a330e259aa8
vsmenon <vsmenon@gmail.com>
2016-01-29 10:16:19 -0800

    Update devc.dart

--- pkg/dev_compiler/lib/devc.dart
+++ pkg/dev_compiler/lib/devc.dart
@@ -10,4 +10,4 @@ export 'src/compiler.dart' show BatchCompiler, setupLogger, createErrorReporter;
 export 'src/server/server.dart' show DevServer;
 
 // When updating this version, also update the version in the pubspec.
-const devCompilerVersion = '0.1.16';
+const devCompilerVersion = '0.1.17';
