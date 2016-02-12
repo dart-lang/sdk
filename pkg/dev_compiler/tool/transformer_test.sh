7a373c198449a5033dbcb704b8afbf32e9a2b706
Olivier Chafik <ochafik@google.com>
2016-02-12 14:53:17 +0000

    e2e test for transformer (webdriver + pub serve)
    BUG=
    R=vsm@google.com
    
    Review URL: https://codereview.chromium.org/1672563002 .

--- pkg/dev_compiler/tool/transformer_test.sh
+++ pkg/dev_compiler/tool/transformer_test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -eu
-DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $( dirname "${BASH_SOURCE[0]}" )/..
 
-# TODO(ochafik): Run pub serve + launch a browser on
-# http://localhost:8080/hello_app/web/
+export CHROME_CANARY_BIN=`./tool/get_chrome_canary.sh`
+
+echo "*** Testing pub serve + DDC transformer"
+pub run test --timeout 120s test/transformer_e2e_test.dart
+
+echo "*** Testing pub build + DDC transformer"
 cd test/transformer/hello_app
 pub get
 pub build
 
 expected_files=(
+  build/web/hello_app/web/index.html
   build/web/hello_app/web/main.js
   build/web/hello_dep/simple.js
   build/web/hello_dep/utils.js
