66d4d7665c413880c818331e17e4b8ae0b87e3b5
Alan Knight <alanknight@google.com>
2016-02-17 10:26:54 -0800

    Remove apparent duplicate call to PrepareForScriptLoading in Dartium
    BUG=
    
    Review URL: https://codereview.chromium.org/1698323004 .

--- runtime/bin/vmservice_dartium.cc
+++ runtime/bin/vmservice_dartium.cc
@@ -64,11 +64,6 @@ Dart_Isolate VmServiceServer::CreateIsolate(const uint8_t* snapshot_buffer) {
 
   Dart_Handle result;
 
-  // Prepare for script loading by setting up the 'print' and 'timer'
-  // closures and setting up 'package root' for URI resolution.
-  result = DartUtils::PrepareForScriptLoading(true, false);
-  CHECK_RESULT(result);
-
   ASSERT(Dart_IsServiceIsolate(isolate));
   if (!VmService::Setup(DEFAULT_VM_SERVICE_SERVER_IP,
                         DEFAULT_VM_SERVICE_SERVER_PORT,
