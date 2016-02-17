6a242d046aaaf73d41bf437ac20758da8bba2187
pq <pquitslund@google.com>
2016-02-17 09:44:24 -0800

    Add `pkg/meta` (#25741).
    BUG=
    R=brianwilkerson@google.com
    
    Review URL: https://codereview.chromium.org/1705803002 .

---
+++ pkg/meta/lib/meta.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/**
+ * Constants for use in metadata annotations such as `@protected`.
+ *
+ * See also `@deprecated` and `@override` in the `dart:core` library.
+ *
+ * Annotations provide semantic information
+ * that tools can use to provide a better user experience.
+ * For example, an IDE might not autocomplete
+ * the name of a function that's been marked `@deprecated`,
+ * or it might display the function's name differently.
+ *
+ * For information on installing and importing this library, see the
+ * [meta package on pub.dartlang.org]
+ * (http://pub.dartlang.org/packages/meta).
+ * For examples of using annotations, see
+ * [Metadata](https://www.dartlang.org/docs/dart-up-and-running/ch02.html#metadata)
+ * in the language tour.
+ */
+library meta;
