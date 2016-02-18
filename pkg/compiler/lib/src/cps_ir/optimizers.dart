71fdc6687f224eb1937bd6538f2fcc7cbdfdf2a1
Asger Feldthaus <asgerf@google.com>
2016-02-18 10:44:28 +0100

    dart2js cps: Rename duplicate_branch.dart to match the new pass name.
    BUG=
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org/1709623002 .

--- pkg/compiler/lib/src/cps_ir/optimizers.dart
+++ pkg/compiler/lib/src/cps_ir/optimizers.dart
@@ -25,7 +25,7 @@ export 'gvn.dart' show GVN;
 export 'inline.dart' show Inliner;
 export 'eagerly_load_statics.dart' show EagerlyLoadStatics;
 export 'loop_invariant_branch.dart' show LoopInvariantBranchMotion;
-export 'duplicate_branch.dart' show PathBasedOptimizer;
+export 'path_based_optimizer.dart' show PathBasedOptimizer;
 export 'use_field_initializers.dart' show UseFieldInitializers;
 export 'parent_visitor.dart' show ParentVisitor;
 
--- pkg/compiler/lib/src/cps_ir/path_based_optimizer.dart
+++ pkg/compiler/lib/src/cps_ir/path_based_optimizer.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-library dart2js.cps_ir.duplicate_branch; // FIXME: Rename file.
+library dart2js.cps_ir.path_based_optimizer;
 
 import 'cps_ir_nodes.dart';
 import 'optimizers.dart';
