dart-lang/sdk

commit 851b4a995c72b484e1b4d13dabbc7e6dcfe7cc24

Compile-time error if mixin class has a factory
The VM now reports the location of the mixin application as well as
the location of the illegal constructor in the mixin class.

Update test language/mixin_illegal_constructor_test to follow the spec.

BUG=
R=rmacnak@google.com

Review URL: https://codereview.chromium.org/1638923002 .

runtime/vm/class_finalizer.cc
@@ -2210,12 +2210,23 @@ void ClassFinalizer::ApplyMixinMembers(const Class& cls) {
   const intptr_t num_functions = functions.Length();
   for (intptr_t i = 0; i < num_functions; i++) {
     func ^= functions.At(i);
-    if (func.IsGenerativeConstructor()) {
+    if (func.IsFactory() || func.IsGenerativeConstructor()) {
       // A mixin class must not have explicit constructors.
       if (!func.IsImplicitConstructor()) {
-        ReportError(cls, cls.token_pos(),
-                    "mixin class '%s' must not have constructors\n",
-                    String::Handle(zone, mixin_cls.Name()).ToCString());
+        const char* ctr_kind = func.IsFactory() ? "factory" : "constructor";
+        const Script& script = Script::Handle(cls.script());
+        const Error& error = Error::Handle(
+            LanguageError::NewFormatted(Error::Handle(),
+                script, func.token_pos(), Report::AtLocation,
+                Report::kError, Heap::kNew,
+                "%s '%s' is illegal in mixin class %s",
+                ctr_kind,
+                String::Handle(func.PrettyName()).ToCString(),
+                String::Handle(zone, mixin_cls.Name()).ToCString()));
+
+        ReportErrors(error, cls, cls.token_pos(),
+                     "mixin class '%s' must not have constructors",
+                     String::Handle(zone, mixin_cls.Name()).ToCString());
       }
       continue;  // Skip the implicit constructor.
     }

tests/language/language_analyzer2.status
@@ -13,6 +13,9 @@ deep_nesting2_negative_test: Crash # Issue 25558
 enum_syntax_test/05: Fail # 21649
 enum_syntax_test/06: Fail # 21649
 
+mixin_illegal_constructor_test/20: MissingCompileTimeError # 25588
+mixin_illegal_constructor_test/21: MissingCompileTimeError # 25588
+
 tearoff_basic_test: Skip # Tear-off not supported
 tearoff_constructor_basic_test: Skip # Tear-off not supported
 

tests/language/language_dart2js.status
@@ -14,6 +14,9 @@ tearoff_constructor_basic_test: Skip # Tear-off not supported
 try_catch_on_syntax_test/10: Fail # Issue 19823
 try_catch_on_syntax_test/11: Fail # Issue 19823
 
+mixin_illegal_constructor_test/20: MissingCompileTimeError # 25587
+mixin_illegal_constructor_test/21: MissingCompileTimeError # 25587
+
 deep_nesting1_negative_test: Crash # Issue 25557
 deep_nesting2_negative_test: Crash # Issue 25557
 

tests/language/mixin_illegal_constructor_test.dart
@@ -15,7 +15,7 @@ class M2 {
   M2.named();
 }
 
-class C0 = Object with M0;
+class C0 = Object with M0;      /// 20: compile-time error
 class C1 = Object with M1;      /// 01: compile-time error
 class C2 = Object with M2;      /// 02: compile-time error
 class C3 = Object with M0, M1;  /// 03: compile-time error
@@ -23,7 +23,7 @@ class C4 = Object with M1, M0;  /// 04: compile-time error
 class C5 = Object with M0, M2;  /// 05: compile-time error
 class C6 = Object with M2, M0;  /// 06: compile-time error
 
-class D0 extends Object with M0 { }
+class D0 extends Object with M0 { }      /// 21: compile-time error
 class D1 extends Object with M1 { }      /// 07: compile-time error
 class D2 extends Object with M2 { }      /// 08: compile-time error
 class D3 extends Object with M0, M1 { }  /// 09: compile-time error
@@ -32,7 +32,7 @@ class D5 extends Object with M0, M2 { }  /// 11: compile-time error
 class D6 extends Object with M2, M0 { }  /// 12: compile-time error
 
 main() {
-  new C0();
+  new C0();  /// 20: continued
   new C1();  /// 01: continued
   new C2();  /// 02: continued
   new C3();  /// 03: continued
@@ -40,16 +40,11 @@ main() {
   new C5();  /// 05: continued
   new C6();  /// 06: continued
 
-  new D0();
+  new D0();  /// 21: continued
   new D1();  /// 07: continued
   new D2();  /// 08: continued
   new D3();  /// 09: continued
   new D4();  /// 10: continued
   new D5();  /// 11: continued
   new D6();  /// 12: continued
-
-  new C0(1,2,3);   /// 13: static type warning, runtime error
-  new C0.named();  /// 14: static type warning, runtime error
-  new D0(1,2,3);   /// 15: static type warning, runtime error
-  new D0.named();  /// 16: static type warning, runtime error
 }

commit 6a0b3210265c5fbb74ed0d1b4b6c67910454e853

Allow factory constructors on mixin classes.
This mitigates a breaking change impacting real users caused by 851b4a995c72b484e1b4d13dabbc7e6dcfe7cc24.
The spec will be updated to allow factory constructors on mixins.

R=gbracha@google.com, iposva@google.com

Review URL: https://codereview.chromium.org/1699233002 .

runtime/vm/class_finalizer.cc
@@ -2251,17 +2251,15 @@ void ClassFinalizer::ApplyMixinMembers(const Class& cls) {
   const intptr_t num_functions = functions.Length();
   for (intptr_t i = 0; i < num_functions; i++) {
     func ^= functions.At(i);
-    if (func.IsFactory() || func.IsGenerativeConstructor()) {
+    if (func.IsGenerativeConstructor()) {
       // A mixin class must not have explicit constructors.
       if (!func.IsImplicitConstructor()) {
-        const char* ctr_kind = func.IsFactory() ? "factory" : "constructor";
         const Script& script = Script::Handle(cls.script());
         const Error& error = Error::Handle(
             LanguageError::NewFormatted(Error::Handle(),
                 script, func.token_pos(), Report::AtLocation,
                 Report::kError, Heap::kNew,
-                "%s '%s' is illegal in mixin class %s",
-                ctr_kind,
+                "constructor '%s' is illegal in mixin class %s",
                 String::Handle(func.PrettyName()).ToCString(),
                 String::Handle(zone, mixin_cls.Name()).ToCString()));
 

tests/language/language_analyzer2.status
@@ -13,9 +13,6 @@ deep_nesting2_negative_test: Crash # Issue 25558
 enum_syntax_test/05: Fail # 21649
 enum_syntax_test/06: Fail # 21649
 
-mixin_illegal_constructor_test/20: MissingCompileTimeError # 25588
-mixin_illegal_constructor_test/21: MissingCompileTimeError # 25588
-
 tearoff_basic_test: Skip # Tear-off not supported
 tearoff_constructor_basic_test: Skip # Tear-off not supported
 

tests/language/language_dart2js.status
@@ -14,9 +14,6 @@ tearoff_constructor_basic_test: Skip # Tear-off not supported
 try_catch_on_syntax_test/10: Fail # Issue 19823
 try_catch_on_syntax_test/11: Fail # Issue 19823
 
-mixin_illegal_constructor_test/20: MissingCompileTimeError # 25587
-mixin_illegal_constructor_test/21: MissingCompileTimeError # 25587
-
 deep_nesting1_negative_test: Crash # Issue 25557
 deep_nesting2_negative_test: Crash # Issue 25557
 

tests/language/mixin_illegal_constructor_test.dart
@@ -15,7 +15,7 @@ class M2 {
   M2.named();
 }
 
-class C0 = Object with M0;      /// 20: compile-time error
+class C0 = Object with M0;
 class C1 = Object with M1;      /// 01: compile-time error
 class C2 = Object with M2;      /// 02: compile-time error
 class C3 = Object with M0, M1;  /// 03: compile-time error
@@ -23,7 +23,7 @@ class C4 = Object with M1, M0;  /// 04: compile-time error
 class C5 = Object with M0, M2;  /// 05: compile-time error
 class C6 = Object with M2, M0;  /// 06: compile-time error
 
-class D0 extends Object with M0 { }      /// 21: compile-time error
+class D0 extends Object with M0 { }
 class D1 extends Object with M1 { }      /// 07: compile-time error
 class D2 extends Object with M2 { }      /// 08: compile-time error
 class D3 extends Object with M0, M1 { }  /// 09: compile-time error
@@ -32,7 +32,7 @@ class D5 extends Object with M0, M2 { }  /// 11: compile-time error
 class D6 extends Object with M2, M0 { }  /// 12: compile-time error
 
 main() {
-  new C0();  /// 20: continued
+  new C0();
   new C1();  /// 01: continued
   new C2();  /// 02: continued
   new C3();  /// 03: continued
@@ -40,11 +40,16 @@ main() {
   new C5();  /// 05: continued
   new C6();  /// 06: continued
 
-  new D0();  /// 21: continued
+  new D0();
   new D1();  /// 07: continued
   new D2();  /// 08: continued
   new D3();  /// 09: continued
   new D4();  /// 10: continued
   new D5();  /// 11: continued
   new D6();  /// 12: continued
+
+  new C0(1,2,3);   /// 13: static type warning, runtime error
+  new C0.named();  /// 14: static type warning, runtime error
+  new D0(1,2,3);   /// 15: static type warning, runtime error
+  new D0.named();  /// 16: static type warning, runtime error
 }