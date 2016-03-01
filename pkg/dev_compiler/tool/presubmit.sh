f20caad681394ca4260c3371f658408e60976f5d
Olivier Chafik <ochafik@google.com>
2016-02-29 22:00:43 -0800

    Split out transformer and node tests in their own env

--- pkg/dev_compiler/tool/presubmit.sh
+++ pkg/dev_compiler/tool/presubmit.sh
@@ -4,9 +4,10 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 $DIR/build_sdk.sh
 $DIR/test.sh
 $DIR/browser_test.sh
-$DIR/node_test.sh
 $DIR/analyze.sh
 $DIR/format.sh
-# TODO(vsm/ochafik): Re-enable when this is addressed:
-# https://github.com/dart-lang/dev_compiler/issues/458
-# $DIR/transformer_test.sh
+if [[ "$TRAVIS" != true ]]; then
+  $DIR/node_test.sh
+  $DIR/transformer_test.sh
+fi
+echo "*** Presubmit finished"
