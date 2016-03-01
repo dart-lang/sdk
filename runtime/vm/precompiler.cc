dart-lang/sdk

commit e1cd55dc8f5c7c1efbe79e11f71a9ae0fcfbfa90

Precompilation: also trace types of exception handlers.
Cf. 4b112c6d25fa714ad1fa4060a3d4bc4cc40c54eb

R=regis@google.com

Review URL: https://codereview.chromium.org/1746313002 .

runtime/vm/precompiler.cc
@@ -571,6 +571,23 @@ void Precompiler::AddTypesOf(const Function& function) {
     type = function.ParameterTypeAt(i);
     AddType(type);
   }
+  Code& code = Code::Handle(Z, function.CurrentCode());
+  if (code.IsNull()) {
+    ASSERT(function.kind() == RawFunction::kSignatureFunction);
+  } else {
+    const ExceptionHandlers& handlers =
+        ExceptionHandlers::Handle(Z, code.exception_handlers());
+    if (!handlers.IsNull()) {
+      Array& types = Array::Handle(Z);
+      for (intptr_t i = 0; i < handlers.num_entries(); i++) {
+        types = handlers.GetHandledTypes(i);
+        for (intptr_t j = 0; j < types.Length(); j++) {
+          type ^= types.At(j);
+          AddType(type);
+        }
+      }
+    }
+  }
 }
 
 

commit 7b6463b708be34acf4823980e0b5b42edec789f5

Exception handlers may have null type arrays.
Fixes crash introduced by e1cd55dc8f5c7c1efbe79e11f71a9ae0fcfbfa90.

R=regis@google.com

Review URL: https://codereview.chromium.org/1755713002 .

runtime/vm/precompiler.cc
@@ -581,9 +581,11 @@ void Precompiler::AddTypesOf(const Function& function) {
       Array& types = Array::Handle(Z);
       for (intptr_t i = 0; i < handlers.num_entries(); i++) {
         types = handlers.GetHandledTypes(i);
-        for (intptr_t j = 0; j < types.Length(); j++) {
-          type ^= types.At(j);
-          AddType(type);
+        if (!types.IsNull()) {
+          for (intptr_t j = 0; j < types.Length(); j++) {
+            type ^= types.At(j);
+            AddType(type);
+          }
         }
       }
     }