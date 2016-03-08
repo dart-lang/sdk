4e29500150fcc24b5e0ecffed907d56ea48c8665
John Messerly <jmesserly@google.com>
2016-03-08 11:03:33 -0800

    remove checked mode from build_sdk. Saves ~25% from build SDK time
    Review URL: https://codereview.chromium.org/1753453002 .

--- pkg/dev_compiler/tool/build_sdk.sh
+++ pkg/dev_compiler/tool/build_sdk.sh
@@ -10,7 +10,7 @@ echo "*** Compiling SDK to JavaScript"
 
 # TODO(ochafik): Re-enable named params destructuring when Atom supports it
 # (see https://github.com/dart-lang/dev_compiler/issues/396)
-dart -c bin/dartdevc.dart --force-compile --no-source-maps --sdk-check \
+dart bin/dartdevc.dart --force-compile --no-source-maps --sdk-check \
     -l warning --dart-sdk tool/generated_sdk -o lib/runtime/ \
     --no-destructure-named-params \
     "$@" \
