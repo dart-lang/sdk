47f0b848382b5536aeffafd247fdc340861d68f1
Srdjan Mitrovic <srdjan@google.com>
2016-03-17 09:35:27 -0700

    Update test's options to optimize deterministically (turn off background compilation)
    BUG=
    R=asiva@google.com
    
    Review URL: https://codereview.chromium.org/1807013003 .

--- tests/lib/convert/json_test.dart
+++ tests/lib/convert/json_test.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+// Disable background compilation so that the Issue 24908 can be reproduced.
+// VMOptions=--no-background-compilation
 
 library json_test;
 
