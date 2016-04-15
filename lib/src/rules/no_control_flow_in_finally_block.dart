5261ca6ffadd9ca73ea413088f16e927c994d584
Alexei Eleusis Díaz Vera <alexei.eleusis@gmail.com>
2016-04-15 13:35:29 -0700

    Lint rule: Avoid flow control in finally block.

---
+++ lib/src/rules/no_control_flow_in_finally_block.dart
@@ -0,0 +1,166 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library linter.src.rules.no_control_flow_in_finally_block;
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:linter/src/linter.dart';
+
+const desc = r'Avoid control flow in `finally block.';
+
+const details = r'''
+**AVOID** control flow leaving finally blocks. This will cause unexpected
+behavior that is hard to debug.
+
+**GOOD:**
+```
+class Ok {
+  double compliantMethod() {
+    var i = 5;
+    try {
+      i = 1 / 0;
+    } catch (e) {
+      print(e); // OK
+    }
+    return i;
+  }
+}
+```
+
+**BAD:**
+```
+class BadReturn {
+  double nonCompliantMethod() {
+    try {
+      return 1 / 0;
+    } catch (e) {
+      print(e);
+    } finally {
+      return 1.0; // LINT
+    }
+  }
+}
+```
+
+**BAD:**
+```
+class BadContinue {
+  double nonCompliantMethod() {
+    for (var o in [1, 2]) {
+      try {
+        print(o / 0);
+      } catch (e) {
+        print(e);
+      } finally {
+        continue; // LINT
+      }
+    }
+    return 1.0;
+  }
+}
+```
+
+**BAD:**
+```
+class BadBreak {
+  double nonCompliantMethod() {
+    for (var o in [1, 2]) {
+      try {
+        print(o / 0);
+      } catch (e) {
+        print(e);
+      } finally {
+        break; // LINT
+      }
+    }
+    return 1.0;
+  }
+}
+```
+''';
+
+class NoControlFlowInFinallyBlock extends LintRule {
+  _Visitor _visitor;
+
+  NoControlFlowInFinallyBlock()
+      : super(
+            name: 'no_control_flow_in_finally_block',
+            description: desc,
+            details: details,
+            group: Group.errors) {
+    _visitor = new _Visitor(this);
+  }
+
+  @override
+  AstVisitor getVisitor() => _visitor;
+}
+
+class _Visitor extends SimpleAstVisitor
+    with ControlFlowInFinallyBlockReporterMixin {
+  @override
+  final LintRule rule;
+
+  _Visitor(this.rule);
+
+  @override
+  visitReturnStatement(ReturnStatement node) {
+    reportIfFinallyAncestorExists(node);
+  }
+
+  @override
+  visitContinueStatement(ContinueStatement node) {
+    reportIfFinallyAncestorExists(node, ancestor: node.target);
+  }
+
+  @override
+  visitBreakStatement(BreakStatement node) {
+    reportIfFinallyAncestorExists(node, ancestor: node.target);
+  }
+}
+
+/// Do not extend this class, it is meant to be used from
+/// [NoThrowInFinallyBlock] which is in a separate rule to allow a more granular
+/// configurability given that reporting throw statements in a finally clause is
+/// controversial.
+abstract class ControlFlowInFinallyBlockReporterMixin {
+  LintRule get rule;
+
+  void reportIfFinallyAncestorExists(AstNode node, {AstNode ancestor}) {
+    final TryStatement tryStatement =
+        node.getAncestor((n) => n is TryStatement);
+    final finallyBlock = tryStatement?.finallyBlock;
+    final Function finallyBlockAncestorPredicate =
+        (AstNode n) => n == finallyBlock;
+    if (tryStatement == null ||
+        finallyBlock == null ||
+        node.getAncestor(finallyBlockAncestorPredicate) == null) {
+      return;
+    }
+
+    AstNode enablerNode = _findEnablerNode(
+        ancestor, finallyBlockAncestorPredicate, node, tryStatement);
+    if (enablerNode == null) {
+      rule.reportLint(node);
+    }
+  }
+
+  AstNode _findEnablerNode(
+      AstNode ancestor,
+      bool finallyBlockAncestorPredicate(AstNode n),
+      AstNode node,
+      TryStatement tryStatement) {
+    AstNode enablerNode;
+    if (ancestor == null) {
+      final functionBlockPredicate = (n) =>
+          n is FunctionBody &&
+          n.getAncestor(finallyBlockAncestorPredicate) != null;
+      enablerNode = node.getAncestor(functionBlockPredicate);
+    } else {
+      enablerNode = ancestor.getAncestor((n) => n == tryStatement);
+    }
+
+    return enablerNode;
+  }
+}
---
+++ test/rules/no_throw_in_finally_block.dart
@@ -0,0 +1,67 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// test w/ `dart test/util/solo_test.dart no_throw_in_finally_block`
+
+class Ok {
+  double compliantMethod() {
+    var i = 5;
+    try {
+      i = 1 / 0;
+    } catch (e) {
+      print(e);
+    } finally {
+      i = i * i; // OK
+    }
+    return i;
+  }
+}
+
+class BadThrow01 {
+  double nonCompliantMethod() {
+    try {
+      print('hello world! ${1 / 0}');
+    } catch (e) {
+      print(e);
+    } finally {
+      if (1 > 0) {
+        throw 'Find the hidden error :P'; // LINT
+      } else {
+        print('should catch nested throws!');
+      }
+    }
+    return 1.0;
+  }
+}
+
+class GoodThrow01 {
+  double compliantMethod() {
+    try {
+      print('hello world! ${1 / 0}');
+    } catch (e) {
+      print(e);
+    } finally {
+      try {
+        print(1 / 0);
+      } catch (e) {
+        throw new WeirdException(); // OK
+      }
+    }
+    return 1.0;
+  }
+}
+
+class WeirdException {}
+
+Function registrationGuard;
+
+void outer() {
+  try {
+    registrationGuard();
+  } finally {
+    registrationGuard = () {
+      throw new WeirdException(); // OK
+    };
+  }
+}
