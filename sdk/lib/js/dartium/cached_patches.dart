7c9319a4f64fe2b7387701d4d6067f1b0ce2bb17
Terry Lucas <terry@google.com>
2016-04-19 08:32:50 -0700

    The patch file cached_patches.dart generated from C++ with special dartium switch --enable-blink-features=dartGenCachedPatches
    TBR=jacobr@google.com,alanknight@google.com
    
    Review URL: https://codereview.chromium.org/1898163002 .

--- sdk/lib/js/dartium/cached_patches.dart
+++ sdk/lib/js/dartium/cached_patches.dart
@@ -7,7 +7,8 @@
 
 library cached_patches;
 
-var cached_patches = {"dart:html": ["dart:html", "dart:html_js_interop_patch.dart", """import 'dart:js' as js_library;
+var cached_patches = {
+"dart:html": ["dart:html", "dart:html_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -4057,7 +4058,8 @@ class _XMLHttpRequestProgressEventImpl extends _XMLHttpRequestProgressEvent impl
   toString() => super.toString();
 }
 
-"""],"dart:indexed_db": ["dart:indexed_db", "dart:indexed_db_js_interop_patch.dart", """import 'dart:js' as js_library;
+"""],
+"dart:indexed_db": ["dart:indexed_db", "dart:indexed_db_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -4165,7 +4167,8 @@ class VersionChangeEventImpl extends VersionChangeEvent implements js_library.JS
   toString() => super.toString();
 }
 
-"""],"dart:web_gl": ["dart:web_gl", "dart:web_gl_js_interop_patch.dart", """import 'dart:js' as js_library;
+"""],
+"dart:web_gl": ["dart:web_gl", "dart:web_gl_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -4561,7 +4564,8 @@ class _WebGLRenderingContextBaseImpl extends _WebGLRenderingContextBase implemen
   toString() => super.toString();
 }
 
-"""],"dart:web_sql": ["dart:web_sql", "dart:web_sql_js_interop_patch.dart", """import 'dart:js' as js_library;
+"""],
+"dart:web_sql": ["dart:web_sql", "dart:web_sql_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -4615,7 +4619,8 @@ class SqlTransactionImpl extends SqlTransaction implements js_library.JSObjectIn
   toString() => super.toString();
 }
 
-"""],"dart:svg": ["dart:svg", "dart:svg_js_interop_patch.dart", """import 'dart:js' as js_library;
+"""],
+"dart:svg": ["dart:svg", "dart:svg_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -5767,7 +5772,8 @@ class _SVGMPathElementImpl extends _SVGMPathElement implements js_library.JSObje
   toString() => super.toString();
 }
 
-"""],"dart:web_audio": ["dart:web_audio", "dart:web_audio_js_interop_patch.dart", """import 'dart:js' as js_library;
+"""],
+"dart:web_audio": ["dart:web_audio", "dart:web_audio_js_interop_patch.dart", """import 'dart:js' as js_library;
 
 /**
  * Placeholder object for cases where we need to determine exactly how many
@@ -6028,5 +6034,7 @@ class WaveShaperNodeImpl extends WaveShaperNode implements js_library.JSObjectIn
   toString() => super.toString();
 }
 
-"""],};
+"""],
+
+};
 // END_OF_CACHED_PATCHES
