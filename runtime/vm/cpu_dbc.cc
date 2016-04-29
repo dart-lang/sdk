1b058141f8ec242a40f8846052d500d4595d98ce
Zachary Anderson <zra@google.com>
2016-04-29 09:36:58 -0700

    DBC: Enable CpuInfo test
    R=iposva@google.com
    
    Review URL: https://codereview.chromium.org/1928473003 .

--- runtime/vm/cpu_dbc.cc
+++ runtime/vm/cpu_dbc.cc
@@ -6,10 +6,11 @@
 #if defined(TARGET_ARCH_DBC)
 
 #include "vm/cpu.h"
+#include "vm/cpu_dbc.h"
 
+#include "vm/cpuinfo.h"
 
 namespace dart {
-
 
 void CPU::FlushICache(uword start, uword size) {
   // Nothing to do.
@@ -20,6 +21,31 @@ const char* CPU::Id() {
   return "dbc";
 }
 
+
+const char* HostCPUFeatures::hardware_ = NULL;
+#if defined(DEBUG)
+bool HostCPUFeatures::initialized_ = false;
+#endif
+
+void HostCPUFeatures::InitOnce() {
+  CpuInfo::InitOnce();
+  hardware_ = CpuInfo::GetCpuModel();
+#if defined(DEBUG)
+  initialized_ = true;
+#endif
+}
+
+
+void HostCPUFeatures::Cleanup() {
+  DEBUG_ASSERT(initialized_);
+#if defined(DEBUG)
+  initialized_ = false;
+#endif
+  ASSERT(hardware_ != NULL);
+  free(const_cast<char*>(hardware_));
+  hardware_ = NULL;
+  CpuInfo::Cleanup();
+}
 
 }  // namespace dart
 
--- runtime/vm/cpu_dbc.h
+++ runtime/vm/cpu_dbc.h
@@ -12,13 +12,33 @@ namespace dart {
 
 class HostCPUFeatures: public AllStatic {
  public:
-  static const char* hardware() { return "simdbc"; }
+  static void InitOnce();
+  static void Cleanup();
+
+  static const char* hardware() {
+    DEBUG_ASSERT(initialized_);
+    return hardware_;
+  }
+
+ private:
+  static const char* hardware_;
+#if defined(DEBUG)
+  static bool initialized_;
+#endif
 };
 
 class TargetCPUFeatures : public AllStatic {
  public:
-  static void InitOnce() {}
-  static void Cleanup() {}
+  static void InitOnce() {
+    HostCPUFeatures::InitOnce();
+  }
+  static void Cleanup() {
+    HostCPUFeatures::Cleanup();
+  }
+
+  static const char* hardware() {
+    return CPU::Id();
+  }
 
   static bool double_truncate_round_supported() {
     return true;
