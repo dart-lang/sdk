62af0fc5d602ba907a88c8e740e0122a54f2c2d0
John McCutchan <johnmccutchan@google.com>
2016-06-08 07:34:55 -0700

    Fix checked mode error
    BUG=
    
    Review URL: https://codereview.chromium.org/2039813005 .

--- runtime/lib/lib_prefix.dart
+++ runtime/lib/lib_prefix.dart
@@ -8,7 +8,7 @@ import "dart:isolate";
 // This type corresponds to the VM-internal class LibraryPrefix.
 class _LibraryPrefix {
   bool _load() native "LibraryPrefix_load";
-  Error _loadError() native "LibraryPrefix_loadError";
+  Object _loadError() native "LibraryPrefix_loadError";
   bool isLoaded() native "LibraryPrefix_isLoaded";
   bool _invalidateDependentCode()
       native "LibraryPrefix_invalidateDependentCode";
