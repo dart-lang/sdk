962353674bc8666fc2c99dedcf05acd09170b3ea
Regis Crelier <regis@google.com>
2016-06-13 11:30:57 -0700

    Delete stray comment in tests.
    R=hausner@google.com
    
    Review URL: https://codereview.chromium.org/2067453003 .

--- tests/language/vm/string_polymorphic_test.dart
+++ tests/language/vm/string_polymorphic_test.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-// Tests that the VM does not crash on weird corner cases of class Math.
 // VMOptions=--optimization_counter_threshold=10 --no-background_compilation
 
 import 'package:expect/expect.dart';
