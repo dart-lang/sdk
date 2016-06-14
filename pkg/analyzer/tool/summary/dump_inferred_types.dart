ed4dc774366f436eb02b4d9520108ae5af6d5500
pq <pquitslund@google.com>
2016-06-14 12:34:08 -0700

    Switch statement cleanups.
    BUG=
    R=brianwilkerson@google.com, scheglov@google.com
    
    Review URL: https://codereview.chromium.org/2067973002 .

--- pkg/analyzer/tool/summary/dump_inferred_types.dart
+++ pkg/analyzer/tool/summary/dump_inferred_types.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
