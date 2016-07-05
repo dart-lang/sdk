3f62feab2d77b2ba173eab41c72c162a2015f37d
Johnni Winther <johnniwinther@google.com>
2016-07-05 15:26:31 +0200

    Rename library name in test.
    Review URL: https://codereview.chromium.org/2126573002 .

--- tests/compiler/dart2js/serialization/compilation0_test.dart
+++ tests/compiler/dart2js/serialization/compilation0_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library dart2js.serialization.compilation1_test;
+library dart2js.serialization.compilation0_test;
 
 import 'compilation_test_helper.dart' as test;
 import 'test_data.dart';
