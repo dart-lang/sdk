803b0e34547447c66858792c844ddc2743546714
Vijay Menon <vsm@google.com>
2016-08-02 14:40:46 -0700

    Test against dev version of analyzer on travis
    R=jmesserly@google.com
    
    Review URL: https://codereview.chromium.org/2203583004 .

--- pkg/dev_compiler/tool/override_analyzer_dependency.sh
+++ pkg/dev_compiler/tool/override_analyzer_dependency.sh
@@ -5,4 +5,4 @@ cd $( dirname "${BASH_SOURCE[0]}" )/..
 . ./tool/dependency_overrides.sh
 
 checkout_dependency_override_from_github \
-  analyzer dart-lang/sdk master /pkg/analyzer/
+  analyzer dart-lang/sdk $1 /pkg/analyzer/
