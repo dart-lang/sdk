34aed6a08b8547a985ac701cf3910a5f5450dd52
Florian Loitsch <floitsch@google.com>
2016-08-03 15:29:41 +0200

    Better error-message when bind fails.
    See #26790.
    
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/2205913003 .

--- runtime/bin/io_natives.cc
+++ runtime/bin/io_natives.cc
@@ -121,6 +121,7 @@ namespace bin {
   V(Socket_CreateConnect, 3)                                                   \
   V(Socket_CreateBindConnect, 4)                                               \
   V(Socket_CreateBindDatagram, 4)                                              \
+  V(Socket_IsBindError, 1)                                                     \
   V(Socket_Available, 1)                                                       \
   V(Socket_Read, 2)                                                            \
   V(Socket_RecvFrom, 1)                                                        \
