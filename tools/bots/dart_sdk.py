9d2ab4d0163ba031580e9315e02f84d7a1b0612b
William Hesse <whesse@google.com>
2016-08-10 18:06:05 +0200

    Change location of sysroot download so bot doesn't erase it.
    BUG=https://github.com/dart-lang/sdk/issues/27048
    TBR=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/2230333002 .

--- tools/bots/dart_sdk.py
+++ tools/bots/dart_sdk.py
@@ -23,21 +23,23 @@ CHANNEL = bot_utils.GetChannelFromName(bot_name)
 def BuildSDK():
   with bot.BuildStep('Build SDK'):
     sysroot_env =  dict(os.environ)
-    ia32root = os.path.join(bot_utils.DART_DIR, 'sysroots', 'build', 'linux',
-                            'debian_wheezy_i386-sysroot')
-    sysroot_env['CXXFLAGS'] = ("--sysroot=%s -I=/usr/include/c++/4.6 "
-             "-I=/usr/include/c++/4.6/i486-linux-gnu") % ia32root
-    sysroot_env['LDFLAGS'] = '--sysroot=%s' % ia32root
-    sysroot_env['CFLAGS'] = '--sysroot=%s' % ia32root
+    if BUILD_OS == 'linux':
+      ia32root = os.path.join(bot_utils.DART_DIR, '..', 'sysroots',
+                              'build', 'linux', 'debian_wheezy_i386-sysroot')
+      sysroot_env['CXXFLAGS'] = ("--sysroot=%s -I=/usr/include/c++/4.6 "
+               "-I=/usr/include/c++/4.6/i486-linux-gnu") % ia32root
+      sysroot_env['LDFLAGS'] = '--sysroot=%s' % ia32root
+      sysroot_env['CFLAGS'] = '--sysroot=%s' % ia32root
     Run([sys.executable, './tools/build.py', '--mode=release',
          '--arch=ia32', 'create_sdk'], env=sysroot_env)
 
-    x64root = os.path.join(bot_utils.DART_DIR, 'sysroots', 'build', 'linux',
-                            'debian_wheezy_amd64-sysroot')
-    sysroot_env['CXXFLAGS'] = ("--sysroot=%s -I=/usr/include/c++/4.6 "
-            "-I=/usr/include/c++/4.6/x86_64-linux-gnu") % x64root
-    sysroot_env['LDFLAGS'] = '--sysroot=%s' % x64root
-    sysroot_env['CFLAGS'] = '--sysroot=%s' % x64root
+    x64root = os.path.join(bot_utils.DART_DIR, '..', 'sysroots', 'build',
+                           'linux', 'debian_wheezy_amd64-sysroot')
+    if BUILD_OS == 'linux':
+      sysroot_env['CXXFLAGS'] = ("--sysroot=%s -I=/usr/include/c++/4.6 "
+              "-I=/usr/include/c++/4.6/x86_64-linux-gnu") % x64root
+      sysroot_env['LDFLAGS'] = '--sysroot=%s' % x64root
+      sysroot_env['CFLAGS'] = '--sysroot=%s' % x64root
     Run([sys.executable, './tools/build.py', '--mode=release',
          '--arch=x64', 'create_sdk'], env=sysroot_env)
 
@@ -233,6 +235,7 @@ def DartArchiveFile(local_path, remote_path, checksum_files=False):
 
 def Run(command, env=None):
   print "Running %s" % ' '.join(command)
+  print "Environment %s" % env
   return bot.RunProcess(command, env=env)
 
 if __name__ == '__main__':
