dart-lang/sdk

commit b29c6bf33000ca129987edffe9e5df0fde793f3b

Suppress follow-on errors when a file is imported with either a prefix or a show clause
R=scheglov@google.com

Review URL: https://codereview.chromium.org/2226613004 .

pkg/analyzer/lib/dart/element/element.dart
@@ -1156,6 +1156,11 @@ abstract class ExportElement implements Element, UriReferencedElement {
    * directive.
    */
   LibraryElement get exportedLibrary;
+
+  /**
+   * Return `true` if the file referenced by the import's URI exists.
+   */
+  bool get uriExists;
 }
 
 /**
@@ -1341,6 +1346,11 @@ abstract class ImportElement implements Element, UriReferencedElement {
    * prefix, or otherwise does not have an offset.
    */
   int get prefixOffset;
+
+  /**
+   * Return `true` if the file referenced by the import's URI exists.
+   */
+  bool get uriExists;
 }
 
 /**

commit 052a6c867de08e493127e5aaa4d02941ac31d2f8

Fix summary handling of unresolved imports, exports, and parts.
Previously this was working for a few use cases by dumb luck, but it
wasn't well tested and became broken by
b29c6bf33000ca129987edffe9e5df0fde793f3b.  This CL fixes it for real
and adds tests to verify the fix.

Note that there is a change to how we represent an unresolved
import/export: instead of storing a URI_EXISTS modifier in the element
model, we simply mark the imported library element as synthetic.
(This library element existed anyway; we just weren't marking it as
synthetic).  This makes it easy to tie in with summaries, since it is
easy for the resynthesizer to detect that the library doesn't exist at
the time the library handle is resolved.

R=brianwilkerson@google.com

Review URL: https://codereview.chromium.org/2235373003 .

pkg/analyzer/doc/tasks.html
@@ -194,6 +194,7 @@ digraph G {
   LibraryErrorsReadyTask -> LIBRARY_ERRORS_READY
   LibraryUnitErrorsTask -> LIBRARY_UNIT_ERRORS
   MODIFICATION_TIME -> BuildDirectiveElementsTask
+  MODIFICATION_TIME -> BuildLibraryElementTask
   MODIFICATION_TIME -> ParseDartTask
   MODIFICATION_TIME -> ScanDartTask
   MODIFICATION_TIME -> VerifyUnitTask

pkg/analyzer/lib/dart/element/element.dart
@@ -1156,11 +1156,6 @@ abstract class ExportElement implements Element, UriReferencedElement {
    * directive.
    */
   LibraryElement get exportedLibrary;
-
-  /**
-   * Return `true` if the file referenced by the import's URI exists.
-   */
-  bool get uriExists;
 }
 
 /**
@@ -1346,11 +1341,6 @@ abstract class ImportElement implements Element, UriReferencedElement {
    * prefix, or otherwise does not have an offset.
    */
   int get prefixOffset;
-
-  /**
-   * Return `true` if the file referenced by the import's URI exists.
-   */
-  bool get uriExists;
 }
 
 /**

pkg/analyzer/lib/src/dart/element/builder.dart
@@ -154,7 +154,6 @@ class DirectiveElementBuilder extends SimpleAstVisitor<Object> {
       ImportElementImpl importElement = new ImportElementImpl(-1);
       importElement.importedLibrary = importLibraryMap[coreLibrarySource];
       importElement.synthetic = true;
-      importElement.uriExists = true;
       imports.add(importElement);
     }
     //
@@ -183,7 +182,6 @@ class DirectiveElementBuilder extends SimpleAstVisitor<Object> {
         exportElement.uriEnd = uriLiteral.end;
       }
       exportElement.uri = node.uriContent;
-      exportElement.uriExists = exportedTime >= 0;
       exportElement.combinators = _buildCombinators(node);
       exportElement.exportedLibrary = exportedLibrary;
       setElementDocumentationComment(exportElement, node);
@@ -229,7 +227,6 @@ class DirectiveElementBuilder extends SimpleAstVisitor<Object> {
         importElement.uriEnd = uriLiteral.end;
       }
       importElement.uri = node.uriContent;
-      importElement.uriExists = importedTime >= 0;
       importElement.deferred = node.deferredKeyword != null;
       importElement.combinators = _buildCombinators(node);
       importElement.importedLibrary = importedLibrary;

pkg/analyzer/lib/src/dart/element/element.dart
@@ -4157,23 +4157,6 @@ class ExportElementImpl extends UriReferencedElementImpl
   }
 
   @override
-  bool get uriExists {
-    if (_unlinkedExportNonPublic != null) {
-      return true;
-    }
-    return hasModifier(Modifier.URI_EXISTS);
-  }
-
-  /**
-   * Set whether the file referenced by the import's URI exists to match the
-   * given flag.
-   */
-  void set uriExists(bool exists) {
-    assert(_unlinkedExportNonPublic == null);
-    setModifier(Modifier.URI_EXISTS, exists);
-  }
-
-  @override
   int get uriOffset {
     if (_unlinkedExportNonPublic != null) {
       return _unlinkedExportNonPublic.uriOffset;
@@ -5114,23 +5097,6 @@ class ImportElementImpl extends UriReferencedElementImpl
   void set uriEnd(int uriEnd) {
     assert(_unlinkedImport == null);
     super.uriEnd = uriEnd;
-  }
-
-  @override
-  bool get uriExists {
-    if (_unlinkedImport != null) {
-      return true;
-    }
-    return hasModifier(Modifier.URI_EXISTS);
-  }
-
-  /**
-   * Set whether the file referenced by the import's URI exists to match the
-   * given flag.
-   */
-  void set uriExists(bool exists) {
-    assert(_unlinkedImport == null);
-    setModifier(Modifier.URI_EXISTS, exists);
   }
 
   @override
@@ -6433,12 +6399,6 @@ class Modifier extends Enum<Modifier> {
    */
   static const Modifier SYNTHETIC = const Modifier('SYNTHETIC', 16);
 
-  /**
-   * A flag used for import and export elements that indicates whether the URI
-   * in the corresponding directive referenced a file that exists.
-   */
-  static const Modifier URI_EXISTS = const Modifier('URI_EXISTS', 17);
-
   static const List<Modifier> values = const [
     ABSTRACT,
     ASYNCHRONOUS,
@@ -6456,8 +6416,7 @@ class Modifier extends Enum<Modifier> {
     REFERENCES_SUPER,
     SETTER,
     STATIC,
-    SYNTHETIC,
-    URI_EXISTS
+    SYNTHETIC
   ];
 
   const Modifier(String name, int ordinal) : super(name, ordinal);

pkg/analyzer/lib/src/dart/element/handle.dart
@@ -562,9 +562,6 @@ class ExportElementHandle extends ElementHandle implements ExportElement {
   int get uriEnd => actualElement.uriEnd;
 
   @override
-  bool get uriExists => actualElement.uriExists;
-
-  @override
   int get uriOffset => actualElement.uriOffset;
 }
 
@@ -708,9 +705,6 @@ class ImportElementHandle extends ElementHandle implements ImportElement {
 
   @override
   int get uriEnd => actualElement.uriEnd;
-
-  @override
-  bool get uriExists => actualElement.uriExists;
 
   @override
   int get uriOffset => actualElement.uriOffset;

pkg/analyzer/lib/src/dart/resolver/scope.dart
@@ -452,7 +452,8 @@ class LibraryImportScope extends Scope {
       int count = imports.length;
       for (int i = 0; i < count; i++) {
         ImportElement importElement = imports[i];
-        if (importElement.prefix?.name == prefix && !importElement.uriExists) {
+        if (importElement.prefix?.name == prefix &&
+            importElement.importedLibrary?.isSynthetic != false) {
           Iterable<NamespaceCombinator> showCombinators =
               getShowCombinators(importElement);
           if (showCombinators.isEmpty) {
@@ -471,7 +472,8 @@ class LibraryImportScope extends Scope {
       int count = imports.length;
       for (int i = 0; i < count; i++) {
         ImportElement importElement = imports[i];
-        if (importElement.prefix == null && !importElement.uriExists) {
+        if (importElement.prefix == null &&
+            importElement.importedLibrary?.isSynthetic != false) {
           for (ShowElementCombinator combinator
               in getShowCombinators(importElement)) {
             if (combinator.shownNames.contains(name)) {

pkg/analyzer/lib/src/generated/resolver.dart
@@ -1918,10 +1918,10 @@ class DeadCodeVerifier extends RecursiveAstVisitor<Object> {
   @override
   Object visitExportDirective(ExportDirective node) {
     ExportElement exportElement = node.element;
-    if (exportElement != null && exportElement.uriExists) {
+    if (exportElement != null) {
       // The element is null when the URI is invalid
       LibraryElement library = exportElement.exportedLibrary;
-      if (library != null) {
+      if (library != null && !library.isSynthetic) {
         for (Combinator combinator in node.combinators) {
           _checkCombinator(exportElement.exportedLibrary, combinator);
         }
@@ -1962,11 +1962,11 @@ class DeadCodeVerifier extends RecursiveAstVisitor<Object> {
   @override
   Object visitImportDirective(ImportDirective node) {
     ImportElement importElement = node.element;
-    if (importElement != null && importElement.uriExists) {
+    if (importElement != null) {
       // The element is null when the URI is invalid, but not when the URI is
       // valid but refers to a non-existent file.
       LibraryElement library = importElement.importedLibrary;
-      if (library != null) {
+      if (library != null && !library.isSynthetic) {
         for (Combinator combinator in node.combinators) {
           _checkCombinator(library, combinator);
         }
@@ -4452,7 +4452,7 @@ class ImportsVerifier {
         LibraryElement libraryElement = importElement.importedLibrary;
         if (libraryElement == null ||
             libraryElement.isDartCore ||
-            !importElement.uriExists) {
+            libraryElement.isSynthetic) {
           continue;
         }
       }
@@ -4910,6 +4910,15 @@ class InferenceContext {
   }
 
   /**
+   * Look for contextual type information attached to [node].  Returns
+   * the type if found, otherwise null.
+   *
+   * If [node] has a contextual union type like `T | Future<T>` this will be
+   * returned. You can use [getType] if you prefer to only get the `T`.
+   */
+  static DartType getContext(AstNode node) => node?.getProperty(_typeProperty);
+
+  /**
    * Look for a single contextual type attached to [node], and returns the type
    * if found, otherwise null.
    *
@@ -4924,15 +4933,6 @@ class InferenceContext {
     }
     return t;
   }
-
-  /**
-   * Look for contextual type information attached to [node].  Returns
-   * the type if found, otherwise null.
-   *
-   * If [node] has a contextual union type like `T | Future<T>` this will be
-   * returned. You can use [getType] if you prefer to only get the `T`.
-   */
-  static DartType getContext(AstNode node) => node?.getProperty(_typeProperty);
 
   /**
    * Like [getContext] but expands a union type into a list of types.

pkg/analyzer/lib/src/summary/index_unit.dart
@@ -23,7 +23,9 @@ class IndexElementInfo {
 
   factory IndexElementInfo(Element element) {
     IndexSyntheticElementKind kind = IndexSyntheticElementKind.notSynthetic;
-    if (element.isSynthetic) {
+    if (element is LibraryElement || element is CompilationUnitElement) {
+      kind = IndexSyntheticElementKind.unit;
+    } else if (element.isSynthetic) {
       if (element is ConstructorElement) {
         kind = IndexSyntheticElementKind.constructor;
         element = element.enclosingElement;
@@ -60,8 +62,6 @@ class IndexElementInfo {
         throw new ArgumentError(
             'Unsupported synthetic element ${element.runtimeType}');
       }
-    } else if (element is LibraryElement || element is CompilationUnitElement) {
-      kind = IndexSyntheticElementKind.unit;
     }
     return new IndexElementInfo._(element, kind);
   }

pkg/analyzer/lib/src/summary/resynthesize.dart
@@ -19,6 +19,7 @@ import 'package:analyzer/src/generated/resolver.dart';
 import 'package:analyzer/src/generated/source_io.dart';
 import 'package:analyzer/src/generated/testing/ast_factory.dart';
 import 'package:analyzer/src/generated/testing/token_factory.dart';
+import 'package:analyzer/src/summary/format.dart';
 import 'package:analyzer/src/summary/idl.dart';
 
 /**
@@ -151,6 +152,7 @@ abstract class SummaryResynthesizer extends ElementResynthesizer {
                 e is PropertyAccessorElementImpl ? e.identifier : e.name;
             elementsInUnit[id] = e;
           }
+
           unitElement.accessors.forEach(putElement);
           unitElement.enums.forEach(putElement);
           unitElement.functions.forEach(putElement);
@@ -204,15 +206,29 @@ abstract class SummaryResynthesizer extends ElementResynthesizer {
       return parent.getLibraryElement(uri);
     }
     return _resynthesizedLibraries.putIfAbsent(uri, () {
-      LinkedLibrary serializedLibrary = _getLinkedSummaryOrThrow(uri);
-      List<UnlinkedUnit> serializedUnits = <UnlinkedUnit>[
-        _getUnlinkedSummaryOrThrow(uri)
-      ];
+      LinkedLibrary serializedLibrary = _getLinkedSummaryOrNull(uri);
       Source librarySource = _getSource(uri);
+      if (serializedLibrary == null) {
+        LibraryElementImpl libraryElement =
+            new LibraryElementImpl(context, '', -1, 0);
+        libraryElement.synthetic = true;
+        CompilationUnitElementImpl unitElement =
+            new CompilationUnitElementImpl(librarySource.shortName);
+        libraryElement.definingCompilationUnit = unitElement;
+        unitElement.source = librarySource;
+        unitElement.librarySource = librarySource;
+        return libraryElement..synthetic = true;
+      }
+      UnlinkedUnit unlinkedSummary = _getUnlinkedSummaryOrNull(uri);
+      if (unlinkedSummary == null) {
+        throw new StateError('Unable to find unlinked summary: $uri');
+      }
+      List<UnlinkedUnit> serializedUnits = <UnlinkedUnit>[unlinkedSummary];
       for (String part in serializedUnits[0].publicNamespace.parts) {
         Source partSource = sourceFactory.resolveUri(librarySource, part);
         String partAbsUri = partSource.uri.toString();
-        serializedUnits.add(_getUnlinkedSummaryOrThrow(partAbsUri));
+        serializedUnits.add(_getUnlinkedSummaryOrNull(partAbsUri) ??
+            new UnlinkedUnitBuilder(codeRange: new CodeRangeBuilder()));
       }
       _LibraryResynthesizer libraryResynthesizer = new _LibraryResynthesizer(
           this, serializedLibrary, serializedUnits, librarySource);
@@ -244,18 +260,14 @@ abstract class SummaryResynthesizer extends ElementResynthesizer {
   bool hasLibrarySummary(String uri);
 
   /**
-   * Return the [LinkedLibrary] for the given [uri] or throw [StateError] if it
+   * Return the [LinkedLibrary] for the given [uri] or return `null` if it
    * could not be found.
    */
-  LinkedLibrary _getLinkedSummaryOrThrow(String uri) {
+  LinkedLibrary _getLinkedSummaryOrNull(String uri) {
     if (parent != null && parent._hasLibrarySummary(uri)) {
-      return parent._getLinkedSummaryOrThrow(uri);
+      return parent._getLinkedSummaryOrNull(uri);
     }
-    LinkedLibrary summary = getLinkedSummary(uri);
-    if (summary != null) {
-      return summary;
-    }
-    throw new StateError('Unable to find linked summary: $uri');
+    return getLinkedSummary(uri);
   }
 
   /**
@@ -266,18 +278,14 @@ abstract class SummaryResynthesizer extends ElementResynthesizer {
   }
 
   /**
-   * Return the [UnlinkedUnit] for the given [uri] or throw [StateError] if it
+   * Return the [UnlinkedUnit] for the given [uri] or return `null` if it
    * could not be found.
    */
-  UnlinkedUnit _getUnlinkedSummaryOrThrow(String uri) {
+  UnlinkedUnit _getUnlinkedSummaryOrNull(String uri) {
     if (parent != null && parent._hasLibrarySummary(uri)) {
-      return parent._getUnlinkedSummaryOrThrow(uri);
+      return parent._getUnlinkedSummaryOrNull(uri);
     }
-    UnlinkedUnit summary = getUnlinkedSummary(uri);
-    if (summary != null) {
-      return summary;
-    }
-    throw new StateError('Unable to find unlinked summary: $uri');
+    return getUnlinkedSummary(uri);
   }
 
   /**
@@ -1280,8 +1288,10 @@ class _ReferenceInfo {
           typeArguments = element.typeParameters.map((typeParameter) {
             DartType bound = typeParameter.bound;
             return libraryResynthesizer.summaryResynthesizer.strongMode &&
-                instantiateToBoundsAllowed &&
-                bound != null ? bound : DynamicTypeImpl.instance;
+                    instantiateToBoundsAllowed &&
+                    bound != null
+                ? bound
+                : DynamicTypeImpl.instance;
           }).toList();
         }
         return typeArguments;
@@ -1573,6 +1583,7 @@ class _UnitResynthesizer {
           return DynamicTypeImpl.instance;
         }
       }
+
       _ReferenceInfo referenceInfo = getReferenceInfo(type.reference);
       return referenceInfo.buildType(
           instantiateToBoundsAllowed,

pkg/analyzer/lib/src/task/dart.dart
@@ -1492,6 +1492,11 @@ class BuildLibraryElementTask extends SourceBasedAnalysisTask {
   static const String PARTS_UNIT_INPUT = 'PARTS_UNIT_INPUT';
 
   /**
+   * The name of the input whose value is the modification time of the source.
+   */
+  static const String MODIFICATION_TIME_INPUT = 'MODIFICATION_TIME_INPUT';
+
+  /**
    * The task descriptor describing this kind of task.
    */
   static final TaskDescriptor DESCRIPTOR = new TaskDescriptor(
@@ -1527,6 +1532,7 @@ class BuildLibraryElementTask extends SourceBasedAnalysisTask {
     CompilationUnit definingCompilationUnit =
         getRequiredInput(DEFINING_UNIT_INPUT);
     List<CompilationUnit> partUnits = getRequiredInput(PARTS_UNIT_INPUT);
+    int modificationTime = getRequiredInput(MODIFICATION_TIME_INPUT);
     //
     // Process inputs.
     //
@@ -1647,6 +1653,7 @@ class BuildLibraryElementTask extends SourceBasedAnalysisTask {
     if (libraryElement == null) {
       libraryElement =
           new LibraryElementImpl.forNode(owningContext, libraryNameNode);
+      libraryElement.synthetic = modificationTime < 0;
       libraryElement.definingCompilationUnit = definingCompilationUnitElement;
       libraryElement.entryPoint = entryPoint;
       libraryElement.parts = sourcedCompilationUnits;
@@ -1745,7 +1752,8 @@ class BuildLibraryElementTask extends SourceBasedAnalysisTask {
           RESOLVED_UNIT1.of(new LibrarySpecificUnit(source, source)),
       PARTS_UNIT_INPUT: INCLUDED_PARTS.of(source).toList((Source unit) {
         return RESOLVED_UNIT1.of(new LibrarySpecificUnit(source, unit));
-      })
+      }),
+      MODIFICATION_TIME_INPUT: MODIFICATION_TIME.of(source)
     };
   }
 

pkg/analyzer/tool/task_dependency_graph/tasks.dot
@@ -189,6 +189,7 @@ digraph G {
   LibraryErrorsReadyTask -> LIBRARY_ERRORS_READY
   LibraryUnitErrorsTask -> LIBRARY_UNIT_ERRORS
   MODIFICATION_TIME -> BuildDirectiveElementsTask
+  MODIFICATION_TIME -> BuildLibraryElementTask
   MODIFICATION_TIME -> ParseDartTask
   MODIFICATION_TIME -> ScanDartTask
   MODIFICATION_TIME -> VerifyUnitTask

pkg/analyzer/test/src/summary/resynthesize_ast_test.dart
@@ -20,6 +20,7 @@ import 'package:analyzer/src/summary/summarize_ast.dart';
 import 'package:analyzer/src/summary/summarize_elements.dart'
     show PackageBundleAssembler;
 import 'package:analyzer/task/dart.dart' show PARSED_UNIT;
+import 'package:analyzer/task/general.dart';
 import 'package:unittest/unittest.dart';
 
 import '../../reflective_tests.dart';
@@ -714,7 +715,8 @@ class ResynthesizeAstTest extends ResynthesizeTest
 /**
  * Abstract mixin for serializing ASTs and resynthesizing elements from it.
  */
-abstract class _AstResynthesizeTestMixin {
+abstract class _AstResynthesizeTestMixin
+    implements _AstResynthesizeTestMixinInterface {
   final Set<Source> serializedSources = new Set<Source>();
   final PackageBundleAssembler bundleAssembler = new PackageBundleAssembler();
   final Map<String, UnlinkedUnitBuilder> uriToUnit =
@@ -743,7 +745,7 @@ abstract class _AstResynthesizeTestMixin {
       Map<String, LinkedLibrary> sdkLibraries =
           SerializedMockSdk.instance.uriToLinkedLibrary;
       LinkedLibrary linkedLibrary = sdkLibraries[absoluteUri];
-      if (linkedLibrary == null) {
+      if (linkedLibrary == null && !allowMissingFiles) {
         fail('Linker unexpectedly requested LinkedLibrary for "$absoluteUri".'
             '  Libraries available: ${sdkLibraries.keys}');
       }
@@ -753,7 +755,7 @@ abstract class _AstResynthesizeTestMixin {
     UnlinkedUnit getUnit(String absoluteUri) {
       UnlinkedUnit unit = uriToUnit[absoluteUri] ??
           SerializedMockSdk.instance.uriToUnlinkedUnit[absoluteUri];
-      if (unit == null) {
+      if (unit == null && !allowMissingFiles) {
         fail('Linker unexpectedly requested unit for "$absoluteUri".');
       }
       return unit;
@@ -777,7 +779,8 @@ abstract class _AstResynthesizeTestMixin {
           ..addAll(unlinkedSummaries),
         new Map<String, LinkedLibrary>()
           ..addAll(SerializedMockSdk.instance.uriToLinkedLibrary)
-          ..addAll(linkedSummaries));
+          ..addAll(linkedSummaries),
+        allowMissingFiles);
   }
 
   UnlinkedUnit _getUnlinkedUnit(Source source) {
@@ -790,6 +793,14 @@ abstract class _AstResynthesizeTestMixin {
       }
     }
     return uriToUnit.putIfAbsent(uriStr, () {
+      int modificationTime = context.computeResult(source, MODIFICATION_TIME);
+      if (modificationTime < 0) {
+        // Source does not exist.
+        if (!allowMissingFiles) {
+          fail('Unexpectedly tried to get unlinked summary for $source');
+        }
+        return null;
+      }
       CompilationUnit unit = context.computeResult(source, PARSED_UNIT);
       UnlinkedUnitBuilder unlinkedUnit = serializeAstUnlinked(unit);
       bundleAssembler.addUnlinkedUnit(source, unlinkedUnit);
@@ -820,14 +831,30 @@ abstract class _AstResynthesizeTestMixin {
     }
 
     UnlinkedPublicNamespace getImport(String relativeUri) {
-      return getPart(relativeUri).publicNamespace;
+      return getPart(relativeUri)?.publicNamespace;
     }
 
     UnlinkedUnit definingUnit = _getUnlinkedUnit(librarySource);
-    LinkedLibraryBuilder linkedLibrary =
-        prelink(definingUnit, getPart, getImport);
-    linkedLibrary.dependencies.skip(1).forEach((LinkedDependency d) {
-      _serializeLibrary(resolveRelativeUri(d.uri));
-    });
+    if (definingUnit != null) {
+      LinkedLibraryBuilder linkedLibrary =
+          prelink(definingUnit, getPart, getImport);
+      linkedLibrary.dependencies.skip(1).forEach((LinkedDependency d) {
+        _serializeLibrary(resolveRelativeUri(d.uri));
+      });
+    }
   }
+}
+
+/**
+ * Interface that [_AstResynthesizeTestMixin] requires of classes it's mixed
+ * into.  We can't place the getter below into [_AstResynthesizeTestMixin]
+ * directly, because then it would be overriding a field at the site where the
+ * mixin is instantiated.
+ */
+abstract class _AstResynthesizeTestMixinInterface {
+  /**
+   * A test should return `true` to indicate that a missing file at the time of
+   * summary resynthesis shouldn't trigger an error.
+   */
+  bool get allowMissingFiles;
 }

pkg/analyzer/test/src/summary/resynthesize_test.dart
@@ -49,6 +49,12 @@ abstract class AbstractResynthesizeTest extends AbstractSingleUnitTest {
    */
   Set<String> variablesWithNotConstInitializers = new Set<String>();
 
+  /**
+   * Tests may set this to `true` to indicate that a missing file at the time of
+   * summary resynthesis shouldn't trigger an error.
+   */
+  bool allowMissingFiles = false;
+
   bool get checkPropagatedTypes => true;
 
   void addLibrary(String uri) {
@@ -1184,7 +1190,7 @@ abstract class AbstractResynthesizeTest extends AbstractSingleUnitTest {
       });
     }
     return new TestSummaryResynthesizer(
-        null, context, unlinkedSummaries, linkedSummaries);
+        null, context, unlinkedSummaries, linkedSummaries, allowMissingFiles);
   }
 
   ElementImpl getActualElement(Element element, String desc) {
@@ -1331,13 +1337,6 @@ abstract class AbstractResynthesizeTest extends AbstractSingleUnitTest {
       return false;
     } else if (modifier == Modifier.SYNTHETIC) {
       return element.isSynthetic;
-    } else if (modifier == Modifier.URI_EXISTS) {
-      if (element is ExportElement) {
-        return element.uriExists;
-      } else if (element is ImportElement) {
-        return element.uriExists;
-      }
-      return false;
     }
     throw new UnimplementedError(
         'Modifier $modifier for ${element?.runtimeType}');
@@ -1422,7 +1421,7 @@ class ResynthesizeElementTest extends ResynthesizeTest {
       });
     }
     return new TestSummaryResynthesizer(
-        null, context, unlinkedSummaries, linkedSummaries);
+        null, context, unlinkedSummaries, linkedSummaries, allowMissingFiles);
   }
 
   /**
@@ -4476,6 +4475,21 @@ typedef F();''');
     checkLibrary('f() {} g() {}');
   }
 
+  test_unresolved_export() {
+    allowMissingFiles = true;
+    checkLibrary("export 'foo.dart';", allowErrors: true);
+  }
+
+  test_unresolved_import() {
+    allowMissingFiles = true;
+    checkLibrary("import 'foo.dart';", allowErrors: true);
+  }
+
+  test_unresolved_part() {
+    allowMissingFiles = true;
+    checkLibrary("part 'foo.dart';", allowErrors: true);
+  }
+
   test_unused_type_parameter() {
     checkLibrary('''
 class C<T> {
@@ -4588,6 +4602,7 @@ var x;''');
 class TestSummaryResynthesizer extends SummaryResynthesizer {
   final Map<String, UnlinkedUnit> unlinkedSummaries;
   final Map<String, LinkedLibrary> linkedSummaries;
+  final bool allowMissingFiles;
 
   /**
    * The set of uris for which unlinked summaries have been requested using
@@ -4602,7 +4617,7 @@ class TestSummaryResynthesizer extends SummaryResynthesizer {
   final Set<String> linkedSummariesRequested = new Set<String>();
 
   TestSummaryResynthesizer(SummaryResynthesizer parent, AnalysisContext context,
-      this.unlinkedSummaries, this.linkedSummaries)
+      this.unlinkedSummaries, this.linkedSummaries, this.allowMissingFiles)
       : super(parent, context, context.typeProvider, context.sourceFactory,
             context.analysisOptions.strongMode);
 
@@ -4610,7 +4625,7 @@ class TestSummaryResynthesizer extends SummaryResynthesizer {
   LinkedLibrary getLinkedSummary(String uri) {
     linkedSummariesRequested.add(uri);
     LinkedLibrary serializedLibrary = linkedSummaries[uri];
-    if (serializedLibrary == null) {
+    if (serializedLibrary == null && !allowMissingFiles) {
       fail('Unexpectedly tried to get linked summary for $uri');
     }
     return serializedLibrary;
@@ -4620,7 +4635,7 @@ class TestSummaryResynthesizer extends SummaryResynthesizer {
   UnlinkedUnit getUnlinkedSummary(String uri) {
     unlinkedSummariesRequested.add(uri);
     UnlinkedUnit serializedUnit = unlinkedSummaries[uri];
-    if (serializedUnit == null) {
+    if (serializedUnit == null && !allowMissingFiles) {
       fail('Unexpectedly tried to get unlinked summary for $uri');
     }
     return serializedUnit;

pkg/analyzer/test/src/summary/summary_common.dart
@@ -1309,6 +1309,35 @@ class E {}
     expect(cls.interfaces, isEmpty);
   }
 
+  test_unresolved_import() {
+    allowMissingFiles = true;
+    serializeLibraryText("import 'foo.dart';", allowErrors: true);
+    expect(unlinkedUnits[0].imports, hasLength(2));
+    expect(unlinkedUnits[0].imports[0].uri, 'foo.dart');
+    // Note: imports[1] is the implicit import of dart:core.
+    expect(unlinkedUnits[0].imports[1].isImplicit, true);
+    expect(linked.importDependencies, hasLength(2));
+    checkDependency(
+        linked.importDependencies[0], absUri('/foo.dart'), 'foo.dart');
+  }
+
+  test_unresolved_export() {
+    allowMissingFiles = true;
+    serializeLibraryText("export 'foo.dart';", allowErrors: true);
+    expect(unlinkedUnits[0].publicNamespace.exports, hasLength(1));
+    expect(unlinkedUnits[0].publicNamespace.exports[0].uri, 'foo.dart');
+    expect(linked.exportDependencies, hasLength(1));
+    checkDependency(
+        linked.exportDependencies[0], absUri('/foo.dart'), 'foo.dart');
+  }
+
+  test_unresolved_part() {
+    allowMissingFiles = true;
+    serializeLibraryText("part 'foo.dart';", allowErrors: true);
+    expect(unlinkedUnits[0].publicNamespace.parts, hasLength(1));
+    expect(unlinkedUnits[0].publicNamespace.parts[0], 'foo.dart');
+  }
+
   test_class_no_mixins() {
     UnlinkedClass cls = serializeClassText('class C {}');
     expect(cls.mixins, isEmpty);
@@ -2702,8 +2731,11 @@ const int v = p.a.length;
 
   test_constExpr_pushInt_max() {
     UnlinkedVariable variable = serializeVariableText('const v = 0xFFFFFFFF;');
-    _assertUnlinkedConst(variable.initializer.bodyExpr,
-        operators: [UnlinkedConstOperation.pushInt,], ints: [0xFFFFFFFF]);
+    _assertUnlinkedConst(variable.initializer.bodyExpr, operators: [
+      UnlinkedConstOperation.pushInt,
+    ], ints: [
+      0xFFFFFFFF
+    ]);
   }
 
   test_constExpr_pushInt_negative() {
@@ -2726,15 +2758,26 @@ const int v = p.a.length;
 
   test_constExpr_pushLongInt_min2() {
     UnlinkedVariable variable = serializeVariableText('const v = 0x100000000;');
-    _assertUnlinkedConst(variable.initializer.bodyExpr,
-        operators: [UnlinkedConstOperation.pushLongInt], ints: [2, 1, 0,]);
+    _assertUnlinkedConst(variable.initializer.bodyExpr, operators: [
+      UnlinkedConstOperation.pushLongInt
+    ], ints: [
+      2,
+      1,
+      0,
+    ]);
   }
 
   test_constExpr_pushLongInt_min3() {
     UnlinkedVariable variable =
         serializeVariableText('const v = 0x10000000000000000;');
-    _assertUnlinkedConst(variable.initializer.bodyExpr,
-        operators: [UnlinkedConstOperation.pushLongInt], ints: [3, 1, 0, 0,]);
+    _assertUnlinkedConst(variable.initializer.bodyExpr, operators: [
+      UnlinkedConstOperation.pushLongInt
+    ], ints: [
+      3,
+      1,
+      0,
+      0,
+    ]);
   }
 
   test_constExpr_pushNull() {