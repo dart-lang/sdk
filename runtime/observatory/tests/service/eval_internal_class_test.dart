8860a60af43508e66a73ffd1f4c5c4ad4f281db0
Carlo Bernaschina <cbernaschina@google.com>
2016-08-12 15:08:55 -0700

    Removing warnings from tests
    R=rmacnak@google.com
    
    Review URL: https://codereview.chromium.org/2243833003 .

--- runtime/observatory/tests/service/eval_internal_class_test.dart
+++ runtime/observatory/tests/service/eval_internal_class_test.dart
@@ -2,11 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:developer';
 import 'package:observatory/service_io.dart';
 import 'package:unittest/unittest.dart';
-import 'service_test_common.dart';
 import 'test_helper.dart';
 
 var tests = [
