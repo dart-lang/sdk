88383fb5e17ca552e301c83edf8597188729a8e9
Sigmund Cherem <sigmund@google.com>
2016-08-12 16:19:11 -0700

    Fix batch test as well.
    BUG=
    R=sra@google.com
    
    Review URL: https://codereview.chromium.org/2243073002 .

--- tests/compiler/dart2js/launch_helper.dart
+++ tests/compiler/dart2js/launch_helper.dart
@@ -2,26 +2,29 @@ import 'dart:async';
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
-Future launchDart2Js(args,
-    {bool noStdoutEncoding: false}) {
+List<String> dart2JsCommand(List<String> args) {
   String basePath = path.fromUri(Platform.script);
   while (path.basename(basePath) != 'sdk') {
     basePath = path.dirname(basePath);
   }
-  String dart2jsPath = path.normalize(
-      path.join(basePath, 'pkg/compiler/lib/src/dart2js.dart'));
-  List allArgs = [];
+  String dart2jsPath =
+      path.normalize(path.join(basePath, 'pkg/compiler/lib/src/dart2js.dart'));
+  List command = <String>[];
   if (Platform.packageRoot != null) {
-    allArgs.add('--package-root=${Platform.packageRoot}');
+    command.add('--package-root=${Platform.packageRoot}');
   } else if (Platform.packageConfig != null) {
-    allArgs.add('--packages=${Platform.packageConfig}');
+    command.add('--packages=${Platform.packageConfig}');
   }
-  allArgs.add(dart2jsPath);
-  allArgs.addAll(args);
-  if (noStdoutEncoding) {
-    return Process.run(Platform.executable, allArgs, stdoutEncoding: null);
-  } else {
-    return Process.run(Platform.executable, allArgs);
-  }
+  command.add(dart2jsPath);
+  command.addAll(args);
+  return command;
 }
 
+Future launchDart2Js(args, {bool noStdoutEncoding: false}) {
+  if (noStdoutEncoding) {
+    return Process.run(Platform.executable, dart2JsCommand(args),
+        stdoutEncoding: null);
+  } else {
+    return Process.run(Platform.executable, dart2JsCommand(args));
+  }
+}
