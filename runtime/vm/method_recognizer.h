cb36467568ed3af7da53c10447d17c05339422bf
Siva Annamalai <asiva@google.com>
2016-08-12 16:06:12 -0700

    - Fix for issue 27054 (leak when removing element from _GrowableList) - _GrowableList.removeLast was not shrinking the backing array
    BUG=27054
    R=johnmccutchan@google.com, rmacnak@google.com
    
    Review URL: https://codereview.chromium.org/2240313002 .

--- runtime/vm/method_recognizer.h
+++ runtime/vm/method_recognizer.h
@@ -361,7 +361,7 @@ namespace dart {
   V(_GrowableList, get:length, GrowableArrayLength, 0x18dc9df6)                \
   V(_GrowableList, get:_capacity, GrowableArrayCapacity, 0x02734d82)           \
   V(_GrowableList, add, GrowableListAdd, 0x0d1358ed)                           \
-  V(_GrowableList, removeLast, GrowableListRemoveLast, 0x135d7384)             \
+  V(_GrowableList, removeLast, GrowableListRemoveLast, 0x7add0363)             \
   V(_StringBase, get:length, StringBaseLength, 0x2a2c1b13)                     \
   V(ListIterator, moveNext, ListIteratorMoveNext, 0x3f892e71)                  \
   V(_FixedSizeArrayIterator, moveNext, FixedListIteratorMoveNext, 0x5681c902)  \
