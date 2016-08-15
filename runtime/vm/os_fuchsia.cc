fa1bbc98cf3094899799b8eb5bda0f4052f8863a
James Robinson <jamesr@google.com>
2016-08-15 15:21:36 -0700

    [fuchsia] Use sysconf to get number of processors
    We should be using standard interfaces to access information like
    the nunber of processors.
    
    R=zra@google.com
    
    Review URL: https://codereview.chromium.org/2244253004 .

--- runtime/vm/os_fuchsia.cc
+++ runtime/vm/os_fuchsia.cc
@@ -10,7 +10,6 @@
 #include <errno.h>
 #include <magenta/syscalls.h>
 #include <magenta/types.h>
-#include <runtime/sysinfo.h>
 
 #include "platform/assert.h"
 #include "vm/zone.h"
@@ -152,7 +151,7 @@ bool OS::AllowStackFrameIteratorFromAnotherThread() {
 
 
 int OS::NumberOfAvailableProcessors() {
-  return mxr_get_nprocs_conf();
+  return sysconf(_SC_NPROCESSORS_CONF);
 }
 
 
