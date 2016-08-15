dart-lang/sdk

commit c41427383b8dbe5fc49e0980bfb44c74f18d397b

Deprecate DirectoryBasedDartSdk and transition existing uses to the new API
R=scheglov@google.com

Review URL: https://codereview.chromium.org/2242853002 .

pkg/analyzer/lib/src/summary/summary_file_builder.dart
@@ -5,14 +5,15 @@
 library analyzer.src.summary.summary_file_builder;
 
 import 'dart:collection';
-import 'dart:io';
+import 'dart:io' as io;
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/java_io.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/summary/flat_buffers.dart' as fb;
 import 'package:analyzer/src/summary/index_unit.dart';
@@ -34,14 +35,14 @@ class BuilderOutput {
     // Write summary.
     {
       String outputPath = join(outputDirectoryPath, '$modeName.sum');
-      File file = new File(outputPath);
-      file.writeAsBytesSync(sum, mode: FileMode.WRITE_ONLY);
+      io.File file = new io.File(outputPath);
+      file.writeAsBytesSync(sum, mode: io.FileMode.WRITE_ONLY);
     }
     // Write index.
     {
       String outputPath = join(outputDirectoryPath, '$modeName.index');
-      File file = new File(outputPath);
-      file.writeAsBytesSync(index, mode: FileMode.WRITE_ONLY);
+      io.File file = new io.File(outputPath);
+      file.writeAsBytesSync(index, mode: io.FileMode.WRITE_ONLY);
     }
   }
 }
@@ -50,7 +51,6 @@ class BuilderOutput {
  * Summary build configuration.
  */
 class SummaryBuildConfig {
-
   /**
    * Whether to use exclude informative data from created summaries.
    */
@@ -115,8 +115,9 @@ class SummaryBuilder {
     //
     // Prepare SDK.
     //
-    DirectoryBasedDartSdk sdk =
-        new DirectoryBasedDartSdk(new JavaFile(sdkPath), strongMode);
+    ResourceProvider resourceProvider = PhysicalResourceProvider.INSTANCE;
+    FolderBasedDartSdk sdk = new FolderBasedDartSdk(resourceProvider,
+        FolderBasedDartSdk.defaultSdkDirectory(resourceProvider), strongMode);
     sdk.useSummary = false;
     sdk.analysisOptions = new AnalysisOptionsImpl()..strongMode = strongMode;
 
@@ -153,7 +154,7 @@ class SummaryOutput {
    * Write this summary output to the given [outputPath] and return the
    * created file.
    */
-  File write(String outputPath) {
+  io.File write(String outputPath) {
     fb.Builder builder = new fb.Builder();
     fb.Offset specSumOffset = builder.writeListUint8(spec.sum);
     fb.Offset specIndexOffset = builder.writeListUint8(spec.index);
@@ -165,8 +166,8 @@ class SummaryOutput {
     builder.addOffset(FIELD_STRONG_SUM, strongSumOffset);
     builder.addOffset(FIELD_STRONG_INDEX, strongIndexOffset);
     fb.Offset offset = builder.endTable();
-    return new File(outputPath)
-      ..writeAsBytesSync(builder.finish(offset), mode: FileMode.WRITE_ONLY);
+    return new io.File(outputPath)
+      ..writeAsBytesSync(builder.finish(offset), mode: io.FileMode.WRITE_ONLY);
   }
 }
 

pkg/analysis_server/lib/src/server/driver.dart
@@ -20,11 +20,10 @@ import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/instrumentation/file_instrumentation.dart';
 import 'package:analyzer/instrumentation/instrumentation.dart';
 import 'package:analyzer/plugin/resolver_provider.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/engine.dart';
 import 'package:analyzer/src/generated/incremental_logger.dart';
-import 'package:analyzer/src/generated/java_io.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:args/args.dart';
 import 'package:linter/src/plugin/linter_plugin.dart';
 import 'package:plugin/manager.dart';
@@ -405,18 +404,22 @@ class Driver implements ServerStarter {
     ExtensionManager manager = new ExtensionManager();
     manager.processPlugins(plugins);
 
-    JavaFile defaultSdkDirectory;
+    String defaultSdkPath;
     if (results[SDK_OPTION] != null) {
-      defaultSdkDirectory = new JavaFile(results[SDK_OPTION]);
+      defaultSdkPath = results[SDK_OPTION];
     } else {
       // No path to the SDK was provided.
       // Use DirectoryBasedDartSdk.defaultSdkDirectory, which will make a guess.
-      defaultSdkDirectory = DirectoryBasedDartSdk.defaultSdkDirectory;
+      defaultSdkPath = FolderBasedDartSdk
+          .defaultSdkDirectory(PhysicalResourceProvider.INSTANCE)
+          .path;
     }
     bool useSummaries = analysisServerOptions.fileReadMode == 'as-is';
     SdkCreator defaultSdkCreator = (AnalysisOptions options) {
-      DirectoryBasedDartSdk sdk =
-          new DirectoryBasedDartSdk(defaultSdkDirectory);
+      PhysicalResourceProvider resourceProvider =
+          PhysicalResourceProvider.INSTANCE;
+      FolderBasedDartSdk sdk = new FolderBasedDartSdk(resourceProvider,
+          FolderBasedDartSdk.defaultSdkDirectory(resourceProvider));
       sdk.analysisOptions = options;
       sdk.useSummary = useSummaries;
       return sdk;
@@ -424,7 +427,7 @@ class Driver implements ServerStarter {
     // TODO(brianwilkerson) It would be nice to avoid creating an SDK that
     // cannot be re-used, but the SDK is needed to create a package map provider
     // in the case where we need to run `pub` in order to get the package map.
-    DirectoryBasedDartSdk defaultSdk = defaultSdkCreator(null);
+    DartSdk defaultSdk = defaultSdkCreator(null);
     //
     // Initialize the instrumentation service.
     //
@@ -448,8 +451,7 @@ class Driver implements ServerStarter {
     //
     socketServer = new SocketServer(
         analysisServerOptions,
-        new DartSdkManager(defaultSdkDirectory.getAbsolutePath(), useSummaries,
-            defaultSdkCreator),
+        new DartSdkManager(defaultSdkPath, useSummaries, defaultSdkCreator),
         defaultSdk,
         service,
         serverPlugin,

pkg/analysis_server/lib/src/socket_server.dart
@@ -14,7 +14,6 @@ import 'package:analyzer/instrumentation/instrumentation.dart';
 import 'package:analyzer/plugin/resolver_provider.dart';
 import 'package:analyzer/source/pub_package_map_provider.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:plugin/plugin.dart';
 
 /**
@@ -31,7 +30,7 @@ class SocketServer {
    */
   final DartSdkManager sdkManager;
 
-  final DirectoryBasedDartSdk defaultSdk;
+  final DartSdk defaultSdk;
   final InstrumentationService instrumentationService;
   final ServerPlugin serverPlugin;
   final ResolverProvider fileResolverProvider;

pkg/analysis_server/lib/src/source/caching_pub_package_map_provider.dart
@@ -11,8 +11,8 @@ import 'dart:io' as io;
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/source/package_map_provider.dart';
 import 'package:analyzer/source/pub_package_map_provider.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:analyzer/src/generated/source.dart';
 
 /**
@@ -80,7 +80,7 @@ class CachingPubPackageMapProvider extends PubPackageMapProvider {
    * [RunPubList] and [WriteFile] implementations may be injected for testing
    */
   CachingPubPackageMapProvider(
-      ResourceProvider resourceProvider, DirectoryBasedDartSdk sdk,
+      ResourceProvider resourceProvider, FolderBasedDartSdk sdk,
       [RunPubList runPubList, this._writeFile])
       : super(resourceProvider, sdk, runPubList) {
     if (_writeFile == null) {

pkg/analysis_server/lib/src/status/get_handler.dart
@@ -39,7 +39,6 @@ import 'package:analyzer/src/generated/error.dart';
 import 'package:analyzer/src/generated/java_engine.dart';
 import 'package:analyzer/src/generated/resolver.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/generated/utilities_collection.dart';
 import 'package:analyzer/src/generated/utilities_general.dart';
@@ -1453,7 +1452,7 @@ class GetHandler {
             DartSdk sdk = context?.sourceFactory?.dartSdk;
             writeOptions(buffer, sdk?.context?.analysisOptions,
                 writeAdditionalOptions: (StringBuffer buffer) {
-              if (sdk is DirectoryBasedDartSdk) {
+              if (sdk is FolderBasedDartSdk) {
                 _writeOption(buffer, 'Use summaries', sdk.useSummary);
               }
             });
@@ -1490,9 +1489,9 @@ class GetHandler {
               DartSdk sdk = resolver.dartSdk;
               buffer.write(' (sdk = ');
               buffer.write(sdk.runtimeType);
-              if (sdk is DirectoryBasedDartSdk) {
+              if (sdk is FolderBasedDartSdk) {
                 buffer.write(' (path = ');
-                buffer.write(sdk.directory.getAbsolutePath());
+                buffer.write(sdk.directory.path);
                 buffer.write(')');
               } else if (sdk is EmbedderSdk) {
                 buffer.write(' (map = ');

pkg/analysis_server/test/socket_server_test.dart
@@ -11,9 +11,10 @@ import 'package:analysis_server/src/analysis_server.dart';
 import 'package:analysis_server/src/constants.dart';
 import 'package:analysis_server/src/plugin/server_plugin.dart';
 import 'package:analysis_server/src/socket_server.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/instrumentation/instrumentation.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:plugin/manager.dart';
 import 'package:unittest/unittest.dart';
 
@@ -109,11 +110,13 @@ class SocketServerTest {
   }
 
   static SocketServer _createSocketServer() {
+    PhysicalResourceProvider resourceProvider =
+        PhysicalResourceProvider.INSTANCE;
     ServerPlugin serverPlugin = new ServerPlugin();
     ExtensionManager manager = new ExtensionManager();
     manager.processPlugins([serverPlugin]);
-    SdkCreator sdkCreator = (_) =>
-        new DirectoryBasedDartSdk(DirectoryBasedDartSdk.defaultSdkDirectory);
+    SdkCreator sdkCreator = (_) => new FolderBasedDartSdk(resourceProvider,
+        FolderBasedDartSdk.defaultSdkDirectory(resourceProvider));
     return new SocketServer(
         new AnalysisServerOptions(),
         new DartSdkManager('', false, sdkCreator),

pkg/analysis_server/test/source/caching_put_package_map_provider_test.dart
@@ -12,8 +12,8 @@ import 'package:analysis_server/src/source/caching_pub_package_map_provider.dart
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/memory_file_system.dart';
 import 'package:analyzer/source/package_map_provider.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/sdk_io.dart';
 import 'package:unittest/unittest.dart';
 
 import '../utils.dart';
@@ -74,7 +74,8 @@ main() {
     CachingPubPackageMapProvider newPkgProvider() {
       return new CachingPubPackageMapProvider(
           resProvider,
-          DirectoryBasedDartSdk.defaultSdk,
+          new FolderBasedDartSdk(
+              resProvider, FolderBasedDartSdk.defaultSdkDirectory(resProvider)),
           mockRunner.runPubList,
           mockWriteFile);
     }

pkg/analyzer/benchmark/errors_in_all_libraries.dart
@@ -10,21 +10,17 @@
 import 'dart:io';
 
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/java_io.dart';
-import 'package:analyzer/src/generated/sdk_io.dart' show DirectoryBasedDartSdk;
-import 'package:analyzer/src/generated/source.dart';
-import 'package:analyzer/src/generated/source_io.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
+import 'package:analyzer/src/generated/engine.dart';
+import 'package:analyzer/src/generated/java_io.dart';
+import 'package:analyzer/src/generated/sdk.dart';
+import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/generated/source_io.dart';
 import 'package:path/path.dart' as p;
 
 void main(List<String> args) {
-  JavaSystemIO.setProperty(
-      "com.google.dart.sdk",
-      p.normalize(
-          p.join(p.dirname(p.fromUri(Platform.script)), "../../../sdk")));
-
   // Assumes you have run "pub get" in the analyzer directory itself and uses
   // that "packages" directory as its package root.
   var packageRoot =
@@ -35,10 +31,14 @@ void main(List<String> args) {
     var start = new DateTime.now();
     AnalysisEngine.instance.clearCaches();
 
-    var context = AnalysisEngine.instance.createAnalysisContext();
+    PhysicalResourceProvider resourceProvider =
+        PhysicalResourceProvider.INSTANCE;
+    DartSdk sdk = new FolderBasedDartSdk(
+        resourceProvider, resourceProvider.getFolder(args[0]));
+    AnalysisContext context = AnalysisEngine.instance.createAnalysisContext();
     context.sourceFactory = new SourceFactory([
-      new DartUriResolver(DirectoryBasedDartSdk.defaultSdk),
-      new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
+      new DartUriResolver(sdk),
+      new ResourceUriResolver(resourceProvider),
       new PackageUriResolver([new JavaFile(packageRoot)])
     ]);
 
@@ -86,6 +86,7 @@ List<LibraryElement> _reachableLibraries(LibraryElement start) {
     lib.importedLibraries.forEach(find);
     lib.exportedLibraries.forEach(find);
   }
+
   find(start);
   return results;
 }

commit ea1b511e236fcedd4eaf214ecfcffa52eb1334bb

Fix handling of sdk path when building SDK summaries.
This was inadvertently broken by c41427383b8dbe5fc49e0980bfb44c74f18d397b.

R=brianwilkerson@google.com

Review URL: https://codereview.chromium.org/2245073002 .

pkg/analyzer/lib/src/summary/summary_file_builder.dart
@@ -116,8 +116,8 @@ class SummaryBuilder {
     // Prepare SDK.
     //
     ResourceProvider resourceProvider = PhysicalResourceProvider.INSTANCE;
-    FolderBasedDartSdk sdk = new FolderBasedDartSdk(resourceProvider,
-        FolderBasedDartSdk.defaultSdkDirectory(resourceProvider), strongMode);
+    FolderBasedDartSdk sdk = new FolderBasedDartSdk(
+        resourceProvider, resourceProvider.getFolder(sdkPath), strongMode);
     sdk.useSummary = false;
     sdk.analysisOptions = new AnalysisOptionsImpl()..strongMode = strongMode;
 