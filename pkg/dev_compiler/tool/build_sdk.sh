822f65386fcf1505ecf4cd5d1754b95bc1481c01
John Messerly <jmesserly@google.com>
2016-08-22 13:01:09 -0700

    simplify build sdk
    the .dart script is left over from when we couldn't build SDK via normal compile path
    
    R=vsm@google.com
    
    Review URL: https://codereview.chromium.org/2265303002 .

--- pkg/dev_compiler/tool/build_sdk.sh
+++ pkg/dev_compiler/tool/build_sdk.sh
@@ -9,14 +9,36 @@ dart -c tool/patch_sdk.dart tool/input_sdk gen/patched_sdk
 echo "*** Compiling SDK to JavaScript"
 
 # TODO(jmesserly): break out dart:html & friends.
-#
-# Right now we can't summarize our SDK, so we can't treat it as a normal
-# explicit input (instead we're implicitly compiling against the user's SDK).
-#
-# Another possible approach is to hard code the dart:* library->module mapping
-# into the compiler itself, so it can emit the correct import.
-#
-dart -c tool/build_sdk.dart \
+dart -c bin/dartdevc.dart \
     --dart-sdk gen/patched_sdk \
     -o lib/runtime/dart_sdk.js \
+    --unsafe-force-compile --no-source-map --no-emit-metadata \
+    dart:_runtime \
+    dart:_debugger \
+    dart:_foreign_helper \
+    dart:_interceptors \
+    dart:_internal \
+    dart:_isolate_helper \
+    dart:_js_embedded_names \
+    dart:_js_helper \
+    dart:_js_mirrors \
+    dart:_js_primitives \
+    dart:_metadata \
+    dart:_native_typed_data \
+    dart:async \
+    dart:collection \
+    dart:convert \
+    dart:core \
+    dart:isolate \
+    dart:js \
+    dart:math \
+    dart:mirrors \
+    dart:typed_data \
+    dart:indexed_db \
+    dart:html \
+    dart:html_common \
+    dart:svg \
+    dart:web_audio \
+    dart:web_gl \
+    dart:web_sql \
     "$@" > tool/sdk_expected_errors.txt
