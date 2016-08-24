2b6a4b64d10624be18aa29cc7253c6cb34bd54ee
Harry Terkelsen <het@google.com>
2016-08-24 15:42:57 -0700

    small tweaks to kernel test helper to work in checked mode
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org/2271143004 .

--- tests/compiler/dart2js/kernel/helper.dart
+++ tests/compiler/dart2js/kernel/helper.dart
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
+import 'package:compiler/src/compiler.dart' show Compiler;
 import 'package:compiler/src/elements/elements.dart' show Element;
-import 'package:compiler/src/js_backend/backend.dart' show JavaScriptBackend;
+import 'package:compiler/src/js_backend/backend.dart' as js show JavaScriptBackend;
 import 'package:compiler/src/commandline_options.dart' show Flags;
 import 'package:test/test.dart';
 
@@ -30,7 +33,7 @@ Future<String> compile(String code, {String entry: 'main',
   return backend.getGeneratedCode(element);
 }
 
-void check(String code, {String entry: 'main'}) async {
+Future check(String code, {String entry: 'main'}) async {
   var original = await compile(code, entry: entry, useKernel: false);
   var kernel = await compile(code, entry: entry, useKernel: true);
   expect(original, kernel);
