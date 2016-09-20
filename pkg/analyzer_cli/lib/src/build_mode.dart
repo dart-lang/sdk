dart-lang/sdk

commit 1b10de1a86a7869e2177c3e98b5e061d7913b181

Generate only AST based summaries in build mode.
Should we also remove fallback mode?

R=paulberry@google.com
BUG=

Review URL: https://codereview.chromium.org/2355613003 .

pkg/analyzer_cli/lib/src/build_mode.dart
@@ -8,7 +8,6 @@ import 'dart:core';
 import 'dart:io' as io;
 
 import 'package:analyzer/dart/ast/ast.dart' show CompilationUnit;
-import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/src/dart/sdk/sdk.dart';
 import 'package:analyzer/src/generated/engine.dart';
@@ -137,7 +136,6 @@ class BuildMode {
   PackageBundleAssembler assembler;
   final Set<Source> processedSources = new Set<Source>();
   final Map<Uri, UnlinkedUnit> uriToUnit = <Uri, UnlinkedUnit>{};
-  PackageBundle sdkBundle;
 
   BuildMode(this.resourceProvider, this.options, this.stats);
 
@@ -194,29 +192,12 @@ class BuildMode {
         excludeHashes: options.buildSummaryExcludeInformative &&
             options.buildSummaryOutputSemantic == null);
     if (_shouldOutputSummary) {
-      if (options.buildSummaryOnlyAst && !options.buildSummaryFallback) {
+      if (!options.buildSummaryFallback) {
         _serializeAstBasedSummary(explicitSources);
       } else {
         for (Source source in explicitSources) {
-          if (context.computeKindOf(source) == SourceKind.LIBRARY) {
-            if (options.buildSummaryFallback) {
-              assembler.addFallbackLibrary(source);
-            } else {
-              LibraryElement libraryElement =
-                  context.computeLibraryElement(source);
-              assembler.serializeLibraryElement(libraryElement);
-            }
-          }
-          if (options.buildSummaryFallback) {
-            assembler.addFallbackUnit(source);
-          }
+          assembler.addFallbackUnit(source);
         }
-      }
-      if (!options.buildSummaryOnlyAst) {
-        // In non-AST mode, the SDK bundle wasn't added to the summaryDataStore
-        // because it is automatically loaded during analysis.  However we still
-        // want the SDK bundle to be noted as a dependency, so add it now.
-        summaryDataStore.addBundle(null, sdkBundle);
       }
       // Write the whole package bundle.
       assembler.recordDependencies(summaryDataStore);
@@ -267,6 +248,7 @@ class BuildMode {
         recordDependencyInfo: _shouldOutputSummary);
 
     DartSdk sdk;
+    PackageBundle sdkBundle;
     if (options.dartSdkSummaryPath != null) {
       SummaryBasedDartSdk summarySdk = new SummaryBasedDartSdk(
           options.dartSdkSummaryPath, options.strongMode);
@@ -277,15 +259,13 @@ class BuildMode {
           resourceProvider.getFolder(options.dartSdkPath), options.strongMode);
       dartSdk.analysisOptions =
           Driver.createAnalysisOptionsForCommandLineOptions(options);
-      dartSdk.useSummary = !options.buildSummaryOnlyAst;
+      dartSdk.useSummary = false;
       sdk = dartSdk;
       sdkBundle = dartSdk.getSummarySdkBundle(options.strongMode);
     }
 
-    // In AST mode include SDK bundle to avoid parsing SDK sources.
-    if (options.buildSummaryOnlyAst) {
-      summaryDataStore.addBundle(null, sdkBundle);
-    }
+    // Include SDK bundle to avoid parsing SDK sources.
+    summaryDataStore.addBundle(null, sdkBundle);
 
     // Create the context.
     context = AnalysisEngine.instance.createAnalysisContext();
@@ -302,13 +282,6 @@ class BuildMode {
         contextOptions.analyzeFunctionBodies = false;
       }
     });
-
-    if (!options.buildSummaryOnlyAst) {
-      // Configure using summaries.
-      context.typeProvider = sdk.context.typeProvider;
-      context.resultProvider =
-          new InputPackagesResultProvider(context, summaryDataStore);
-    }
   }
 
   /**

pkg/analyzer_cli/lib/src/options.dart
@@ -48,10 +48,6 @@ class CommandLineOptions {
   /// Whether to skip analysis when creating summaries in build mode.
   final bool buildSummaryOnly;
 
-  /// Whether to create summaries using only ASTs, i.e. don't perform
-  /// resolution.
-  final bool buildSummaryOnlyAst;
-
   /// Whether to use diet parsing, i.e. skip function bodies. We don't need to
   /// analyze function bodies to use summaries during future compilation steps.
   final bool buildSummaryOnlyDiet;
@@ -172,7 +168,6 @@ class CommandLineOptions {
         buildSummaryFallback = args['build-summary-fallback'],
         buildSummaryInputs = args['build-summary-input'] as List<String>,
         buildSummaryOnly = args['build-summary-only'],
-        buildSummaryOnlyAst = args['build-summary-only-ast'],
         buildSummaryOnlyDiet = args['build-summary-only-diet'],
         buildSummaryExcludeInformative =
             args['build-summary-exclude-informative'],
@@ -416,7 +411,7 @@ class CommandLineOptions {
           negatable: false,
           hide: true)
       ..addFlag('build-summary-only-ast',
-          help: 'Generate summaries using ASTs.',
+          help: 'deprecated -- Generate summaries using ASTs.',
           defaultsTo: false,
           negatable: false,
           hide: true)

commit 2839653161d6a05e1c27c9b36fcdbbcf05ad2294

Fix generation of analysis results in analyzer_cli "build mode".
This was inadvertently broken in 1b10de1a86a7869e2177c3e98b5e061d7913b181.

R=scheglov@google.com

Review URL: https://codereview.chromium.org/2350963005 .

pkg/analyzer_cli/lib/src/build_mode.dart
@@ -253,7 +253,7 @@ class BuildMode {
           resourceProvider.getFolder(options.dartSdkPath), options.strongMode);
       dartSdk.analysisOptions =
           Driver.createAnalysisOptionsForCommandLineOptions(options);
-      dartSdk.useSummary = false;
+      dartSdk.useSummary = !options.buildSummaryOnly;
       sdk = dartSdk;
       sdkBundle = dartSdk.getSummarySdkBundle(options.strongMode);
     }
@@ -276,6 +276,13 @@ class BuildMode {
         contextOptions.analyzeFunctionBodies = false;
       }
     });
+
+    if (!options.buildSummaryOnly) {
+      // Configure using summaries.
+      context.typeProvider = sdk.context.typeProvider;
+      context.resultProvider =
+          new InputPackagesResultProvider(context, summaryDataStore);
+    }
   }
 
   /**