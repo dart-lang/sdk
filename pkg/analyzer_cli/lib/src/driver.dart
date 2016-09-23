5d1dcde18c227177251eb953e1c65b4cdacd902c
Paul Berry <paulberry@google.com>
2016-09-23 05:12:42 -0700

    Revert "Issue 27375. Use ContextBuilder to find analysis options file in analyzer_cli."
    This broke the analyzer bots.  See for example: https://build.chromium.org/p/client.dart/builders/analyzer_experimental-linux-release-be/builds/9135/steps/tests%20--host-checked%20--checked/logs/stdio
    
    This reverts commit 75a4f968beb01b2023500e0e6a09c605d474064f.
    
    TBR=scheglov@google.com
    
    Review URL: https://codereview.chromium.org/2366913002 .

--- pkg/analyzer_cli/lib/src/driver.dart
+++ pkg/analyzer_cli/lib/src/driver.dart
@@ -744,8 +744,12 @@ class Driver implements CommandLineStarter {
             exitCode: ErrorSeverity.ERROR.ordinal);
       }
     } else {
-      return new ContextBuilder(resourceProvider, null, null)
-          .getOptionsFile(options.sourceFiles.first);
+      filePath = AnalysisEngine.ANALYSIS_OPTIONS_FILE;
+      file = resourceProvider.getFile(filePath);
+      if (!file.exists) {
+        filePath = AnalysisEngine.ANALYSIS_OPTIONS_YAML_FILE;
+        file = resourceProvider.getFile(filePath);
+      }
     }
     return file;
   }
