95ef8bb59ab875dbcbbc6866bc25684b6dcced57
Jennifer Messerly <jmesserly@google.com>
2016-10-20 16:31:06 -0700

    fix #27643, failed to virtualize fields if an abstract one is mixed in
    R=vsm@google.com
    
    Review URL: https://codereview.chromium.org/2441693003 .

--- pkg/dev_compiler/test/codegen/lib/math/implement_rectangle_test.dart
+++ pkg/dev_compiler/test/codegen/lib/math/implement_rectangle_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:math' hide Rectangle;
 import 'dart:math' as math show Point, Rectangle, MutableRectangle;
 import 'package:expect/expect.dart' show Expect;
