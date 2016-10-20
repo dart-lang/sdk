1f6e03883eec3f18ef09a0166344a5b6e96ce1fe
Vyacheslav Egorov <vegorov@google.com>
2016-10-20 20:09:07 +0200

    Teach analyzer parser to handle native const factories/const constructors.
    This is needed to parse VM patch files, which contain things like:
    
    @patch const factory String.fromEnvironment(String name,
                                                  {String defaultValue})
          native "String_fromEnvironment";
    
    Currently analyzer would throw "Only redirecting factory constructors can be
    declared to be 'const'." error when it sees this code.
    
    BUG=
    R=paulberry@google.com
    
    Review URL: https://codereview.chromium.org/2435013003 .

--- pkg/analyzer/lib/src/generated/parser.dart
+++ pkg/analyzer/lib/src/generated/parser.dart
@@ -6252,7 +6252,8 @@ class Parser {
           parseFunctionBody(true, ParserErrorCode.MISSING_FUNCTION_BODY, false);
       if (constKeyword != null &&
           factoryKeyword != null &&
-          externalKeyword == null) {
+          externalKeyword == null &&
+          body is! NativeFunctionBody) {
         _reportErrorForToken(ParserErrorCode.CONST_FACTORY, factoryKeyword);
       } else if (body is EmptyFunctionBody) {
         if (factoryKeyword != null &&
@@ -6262,7 +6263,7 @@ class Parser {
               ParserErrorCode.FACTORY_WITHOUT_BODY, factoryKeyword);
         }
       } else {
-        if (constKeyword != null) {
+        if (constKeyword != null && body is! NativeFunctionBody) {
           _reportErrorForNode(
               ParserErrorCode.CONST_CONSTRUCTOR_WITH_BODY, body);
         } else if (externalKeyword != null) {
