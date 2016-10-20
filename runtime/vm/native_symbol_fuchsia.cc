8200955ed005d82739209350e4819249dc577899
Zachary Anderson <zra@google.com>
2016-10-20 13:39:35 -0700

    Fuchsia: Add native symbol resolver. Small fixes.
    R=asiva@google.com
    
    Review URL: https://codereview.chromium.org/2438843002 .

--- runtime/vm/native_symbol_fuchsia.cc
+++ runtime/vm/native_symbol_fuchsia.cc
@@ -2,33 +2,41 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#include "vm/globals.h"
+#include "platform/globals.h"
 #if defined(TARGET_OS_FUCHSIA)
 
 #include "vm/native_symbol.h"
 
-#include "platform/assert.h"
+#include <dlfcn.h>  // NOLINT
 
 namespace dart {
 
 void NativeSymbolResolver::InitOnce() {
-  UNIMPLEMENTED();
 }
 
 
 void NativeSymbolResolver::ShutdownOnce() {
-  UNIMPLEMENTED();
 }
 
 
 char* NativeSymbolResolver::LookupSymbolName(uintptr_t pc, uintptr_t* start) {
-  UNIMPLEMENTED();
-  return NULL;
+  Dl_info info;
+  int r = dladdr(reinterpret_cast<void*>(pc), &info);
+  if (r == 0) {
+    return NULL;
+  }
+  if (info.dli_sname == NULL) {
+    return NULL;
+  }
+  if (start != NULL) {
+    *start = reinterpret_cast<uintptr_t>(info.dli_saddr);
+  }
+  return strdup(info.dli_sname);
 }
 
 
 void NativeSymbolResolver::FreeSymbolName(char* name) {
-  UNIMPLEMENTED();
+  free(name);
 }
 
 }  // namespace dart
