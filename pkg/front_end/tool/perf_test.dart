5964bdfa0b019f24a3a6b996ed1f4c275ddc598f
Sigmund Cherem <sigmund@google.com>
2016-10-21 09:07:48 -0700

    Copy perf.dart into front_end/tool.
    This keeps analyzer_cli/perf.dart for now: I need to make the change in two
    parts so I can update the benchmark runners after I submit this.
    
    BUG=
    R=paulberry@google.com
    
    Review URL: https://codereview.chromium.org/2432043008 .

---
+++ pkg/front_end/tool/perf_test.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// The only purpose of this file is to enable analyzer tests on `perf.dart`,
+/// the code here just has a dummy import to the rest of the code.
+library front_end.tool.perf_test;
+
+import 'perf.dart' as m;
+
+main() => print('done ${m.scanTotalChars}');
