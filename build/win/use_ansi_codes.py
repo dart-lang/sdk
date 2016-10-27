99bc0e2d131d29aa61de4730af658f6391e85198
Jan-Hendrik Dolling <git@dolling.it>
2016-10-27 09:10:44 +0200

    change "the the" to the

--- build/win/use_ansi_codes.py
+++ build/win/use_ansi_codes.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Prints if the the terminal is likely to understand ANSI codes."""
+"""Prints if the terminal is likely to understand ANSI codes."""
 
 import os
 
