90d3962ca3abd6270a33e9dfeb7a15ff8b475027
Zachary Anderson <zra@google.com>
2016-11-10 11:16:24 -0800

    Don't depend on dart_bootstrap if there is a usable prebuilt sdk
    Also replace invoke_dart() with compiled_action(), and some other
    cleanups.
    
    fixes #27781
    
    R=johnmccutchan@google.com
    
    Review URL: https://codereview.chromium.org/2493833002 .

--- build/gn_run_binary.py
+++ build/gn_run_binary.py
@@ -8,15 +8,28 @@ Run with:
   python gn_run_binary.py <binary_name> [args ...]
 """
 
+import os
 import sys
 import subprocess
 
-# This script is designed to run binaries produced by the current build. We
-# always prefix it with "./" to avoid picking up system versions that might
-# also be on the path.
-path = './' + sys.argv[1]
+# Run a command, swallowing the output unless there is an error.
+def run_command(command):
+  try:
+    subprocess.check_output(command, stderr=subprocess.STDOUT)
+    return 0
+  except subprocess.CalledProcessError as e:
+    return ("Command failed: " + ' '.join(command) + "\n" +
+            "output: " + e.output)
+
+# Unless the path is absolute, this script is designed to run binaries produced
+# by the current build. We always prefix it with "./" to avoid picking up system
+# versions that might also be on the path.
+if os.path.isabs(sys.argv[1]):
+  path = sys.argv[1]
+else:
+  path = './' + sys.argv[1]
 
 # The rest of the arguements are passed directly to the executable.
 args = [path] + sys.argv[2:]
 
-sys.exit(subprocess.call(args))
+sys.exit(run_command(args))
