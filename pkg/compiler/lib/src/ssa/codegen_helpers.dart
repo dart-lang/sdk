29ad2c2326fdb80daf473470cee2a55bf6aad11c
Stephen Adams <sra@google.com>
2016-11-16 12:25:26 -0800

    Fix generateAtUseSite for complex HIs checks
    This avoids a few unnecessary temporaries.
    
    R=sigmund@google.com
    
    Review URL: https://codereview.chromium.org/2439653002 .

--- pkg/compiler/lib/src/ssa/codegen_helpers.dart
+++ pkg/compiler/lib/src/ssa/codegen_helpers.dart
@@ -440,9 +440,16 @@ class SsaInstructionMerger extends HBaseVisitor {
 
   void visitIs(HIs instruction) {
     // In the general case the input might be used multple multiple times, so it
-    // must not be set generate at use site.  If the code will generate
-    // 'instanceof' then we can generate at use site.
+    // must not be set generate at use site.
+
+    // If the code will generate 'instanceof' then we can generate at use site.
     if (instruction.useInstanceOf) {
+      analyzeInputs(instruction, 0);
+    }
+
+    // Compound and variable checks use a separate instruction to compute the
+    // result.
+    if (instruction.isCompoundCheck || instruction.isVariableCheck) {
       analyzeInputs(instruction, 0);
     }
   }
