f45d5ad70eed503524f11c2970978bfd55e689e3
John McCutchan <johnmccutchan@google.com>
2016-11-17 15:15:24 -0800

    Fix flutter build
    BUG=
    
    Review URL: https://codereview.chromium.org/2511653003 .

--- runtime/vm/method_recognizer.h
+++ runtime/vm/method_recognizer.h
@@ -527,13 +527,13 @@ class MethodRecognizer : public AllStatic {
 };
 
 
-#if !defined(DART_PRECOMPILED_RUNTIME)
+#if defined(DART_NO_SNAPSHOT) && !defined(PRODUCT)
 #define CHECK_FINGERPRINT2(f, p0, p1, fp)                                      \
   ASSERT(f.CheckSourceFingerprint(#p0 ", " #p1, fp))
 
 #define CHECK_FINGERPRINT3(f, p0, p1, p2, fp)                                  \
   ASSERT(f.CheckSourceFingerprint(#p0 ", " #p1 ", " #p2, fp))
-#endif  // !defined(DART_PRECOMPILED_RUNTIME)
+#endif  // defined(DART_NO_SNAPSHOT) && !defined(PRODUCT)
 
 
 // clang-format off
