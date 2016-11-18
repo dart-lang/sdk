70fa0defe9b416669b6e01acf9f31d49aac3426f
Erik Ernst <eernst@google.com>
2016-11-18 09:56:17 +0100

    Enable syntax-only support for generic functions by default.
    This CL removes the 'DartOptions=--generic-method-syntax' directives in
    test cases concerned with generic functions and enables that feature by
    default. The option '--generic-method-syntax' is still allowed, but the
    feature is always enabled so the option makes no difference.
    
    R=johnniwinther@google.com, sigmund@google.com
    
    Review URL: https://codereview.chromium.org/2476423005 .

--- pkg/compiler/lib/src/diagnostics/messages.dart
+++ pkg/compiler/lib/src/diagnostics/messages.dart
@@ -1221,7 +1221,6 @@ void main() => new C().m(null);
       MessageKind.TYPE_VARIABLE_FROM_METHOD_NOT_REIFIED: const MessageTemplate(
           MessageKind.TYPE_VARIABLE_FROM_METHOD_NOT_REIFIED,
           "Method type variables do not have a runtime value.",
-          options: const ["--generic-method-syntax"],
           howToFix: "Try using the upper bound of the type variable, "
               "or refactor the code to avoid needing this runtime value.",
           examples: const [
@@ -1243,7 +1242,6 @@ main() => f<int>(42);
       const MessageTemplate(
           MessageKind.TYPE_VARIABLE_FROM_METHOD_CONSIDERED_DYNAMIC,
           "Method type variables are treated as `dynamic` in `as` expressions.",
-          options: const ["--generic-method-syntax"],
           howToFix: "Try using the upper bound of the type variable, or check "
               "that the blind success of the test does not introduce bugs.",
           examples: const [
--- tests/language/generic_methods_type_expression_test.dart
+++ tests/language/generic_methods_type_expression_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 //
-// DartOptions=--generic-method-syntax
 // VMOptions=--generic-method-syntax
 
 /// Dart test on the usage of method type arguments in type expressions. With
