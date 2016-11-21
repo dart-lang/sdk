2301e67340ec89d41b241960fa7e87c3735cdd15
Konstantin Shcheglov <scheglov@google.com>
2016-11-21 08:33:46 -0800

    Put 'finer grained invalidation' back behind the flag.
    Unfortunately we see exceptions in the wild, both internally and
    externally :-(
    
    For example https://github.com/dart-lang/sdk/issues/27770
    
    R=brianwilkerson@google.com, paulberry@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org/2518843002 .

--- pkg/analysis_server/lib/src/server/driver.dart
+++ pkg/analysis_server/lib/src/server/driver.dart
@@ -393,7 +393,7 @@ class Driver implements ServerStarter {
     analysisServerOptions.enablePubSummaryManager =
         results[ENABLE_PUB_SUMMARY_MANAGER];
     analysisServerOptions.finerGrainedInvalidation =
-        true /*results[FINER_GRAINED_INVALIDATION]*/;
+        results[FINER_GRAINED_INVALIDATION];
     analysisServerOptions.noErrorNotification = results[NO_ERROR_NOTIFICATION];
     analysisServerOptions.noIndex = results[NO_INDEX];
     analysisServerOptions.useAnalysisHighlight2 =
