6c45f24fa6c79823f4dcd43bb149d17b40e0718f
Ryan Macnak <rmacnak@google.com>
2016-11-22 09:19:08 -0800

    Report a peer address over IPv4 as IPv4.
    R=asiva@google.com
    
    Review URL: https://codereview.chromium.org/2518263002 .

--- tests/standalone/io/socket_info_ipv6_test.dart
+++ tests/standalone/io/socket_info_ipv6_test.dart
@@ -6,15 +6,19 @@ import "package:expect/expect.dart";
 import "dart:io";
 
 void testHostAndPort() {
-  ServerSocket.bind("127.0.0.1", 0).then((server) {
+  ServerSocket.bind("::1", 0).then((server) {
 
-    Socket.connect("127.0.0.1", server.port).then((clientSocket) {
+    Socket.connect("::1", server.port).then((clientSocket) {
       server.listen((socket) {
         Expect.equals(socket.port, server.port);
         Expect.equals(clientSocket.port, socket.remotePort);
         Expect.equals(clientSocket.remotePort, socket.port);
-        Expect.equals(socket.remoteAddress.address, "127.0.0.1");
-        Expect.equals(clientSocket.remoteAddress.address, "127.0.0.1");
+        Expect.equals(socket.remoteAddress.address, "::1");
+        Expect.equals(socket.remoteAddress.type,
+                      InternetAddressType.IP_V6);
+        Expect.equals(clientSocket.remoteAddress.address, "::1");
+        Expect.equals(clientSocket.remoteAddress.type,
+                      InternetAddressType.IP_V6);
         socket.destroy();
         clientSocket.destroy();
         server.close();
