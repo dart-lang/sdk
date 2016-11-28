d76809aea704861a6b78272cd29763b1dbe2ce22
Maxim Dikun <dikmax@gmail.com>
2016-11-28 14:59:27 +0100

    No adjacent strings in list. Fixes dart-lang/linter#225.

---
+++ lib/src/rules/no_adjacent_strings_in_list.dart
@@ -0,0 +1,63 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library linter.src.rules.no_adjacent_strings_in_list;
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:linter/src/linter.dart';
+
+const desc = 'Do not use adjacent strings in list';
+
+const details = r'''
+**DO NOT** use adjacent strings in list. This can be
+sign of forgotten comma.
+
+**GOOD:**
+
+```
+List<String> list = <String>[
+  'a' +
+  'b',
+  'c',
+];
+```
+
+**BAD:**
+
+```
+List<String> list = <String>[
+  'a'
+  'b',
+  'c',
+];
+```
+''';
+
+class NoAdjacentStringsInList extends LintRule {
+  NoAdjacentStringsInList()
+      : super(
+            name: 'no_adjacent_strings_in_list',
+            description: desc,
+            details: details,
+            group: Group.errors);
+
+  @override
+  AstVisitor getVisitor() => new Visitor(this);
+}
+
+class Visitor extends SimpleAstVisitor {
+  LintRule rule;
+
+  Visitor(this.rule);
+
+  @override
+  void visitListLiteral(ListLiteral node) {
+    node.elements.forEach((Expression e) {
+      if (e is AdjacentStrings) {
+        rule.reportLint(e);
+      }
+    });
+  }
+}
---
+++ test/rules/no_adjacent_strings_in_list.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// test w/ `pub run test -N no_adjacent_strings_in_list`
+
+void bad() {
+  List<String> list = <String>[
+    'a' // LINT
+    'b',
+    'c',
+  ];
+
+  List<String> list2 = <String>[
+    'a' // LINT
+    'b'
+    'c'
+  ];
+}
+
+void good() {
+  List<String> list = <String>[
+    'a' + // OK
+    'b',
+    'c',
+  ];
+}
