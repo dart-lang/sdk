ec81359fc0cfeebe4faf8e91db04ed4b4c7c9685
Vijay Menon <vsm@google.com>
2016-12-01 15:38:46 -0800

    Fixes #27960
    For "m.call(..)", the function is "m".
    
    R=jmesserly@google.com
    
    Review URL: https://codereview.chromium.org/2539033004 .

--- pkg/dev_compiler/lib/src/compiler/code_generator.dart
+++ pkg/dev_compiler/lib/src/compiler/code_generator.dart
@@ -1315,7 +1315,7 @@ class CodeGenerator extends GeneralizingAstVisitor
       jsMethods.add(new JS.Method(
           _propertyName('constructor'),
           js.call('function(...args) { return this.new.apply(this, args); }')
-              as JS.Fun));
+          as JS.Fun));
     } else if (ctors.isEmpty) {
       jsMethods.add(_emitImplicitConstructor(node, fields, virtualFields));
     }
@@ -3396,7 +3396,7 @@ class CodeGenerator extends GeneralizingAstVisitor
       if (targetType is FunctionType) {
         // Call methods on function types should be handled as regular function
         // invocations.
-        return _emitFunctionCall(node);
+        return _emitFunctionCall(node, node.target);
       }
       if (targetType.isDartCoreFunction || targetType.isDynamic) {
         // TODO(vsm): Can a call method take generic type parameters?
@@ -3543,10 +3543,14 @@ class CodeGenerator extends GeneralizingAstVisitor
 
   /// Emits a function call, to a top-level function, local function, or
   /// an expression.
-  JS.Expression _emitFunctionCall(InvocationExpression node) {
-    var fn = _visit(node.function);
+  JS.Expression _emitFunctionCall(InvocationExpression node,
+      [Expression function]) {
+    if (function == null) {
+      function = node.function;
+    }
+    var fn = _visit(function);
     var args = _visit(node.argumentList) as List<JS.Expression>;
-    if (isDynamicInvoke(node.function)) {
+    if (isDynamicInvoke(function)) {
       return _emitDynamicInvoke(node, fn, args);
     } else {
       return new JS.Call(_applyInvokeTypeArguments(fn, node), args);
@@ -3968,7 +3972,7 @@ class CodeGenerator extends GeneralizingAstVisitor
           new JS.Method(
               access,
               js.call('function() { return #; }', _visitInitializer(node))
-                  as JS.Fun,
+              as JS.Fun,
               isGetter: true),
           node,
           _findAccessor(element, getter: true)));
@@ -4528,7 +4532,7 @@ class CodeGenerator extends GeneralizingAstVisitor
       }
       result = astFactory.prefixedIdentifier(
           _bindValue(scope, 'o', ident.prefix, context: context)
-              as SimpleIdentifier,
+          as SimpleIdentifier,
           ident.period,
           ident.identifier);
     } else {
