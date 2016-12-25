dart-lang/sdk

commit be6a822daff2fae75a480d7da656ab0d9c86512e

Support + concatenation in constant expressions with strings
BUG=
R=paulberry@google.com, scheglov@google.com

Review-Url: https://codereview.chromium.org/2586363004 .

pkg/analyzer/lib/src/dart/ast/utilities.dart
@@ -2190,16 +2190,21 @@ class AstComparator implements AstVisitor<bool> {
  * >   expressions that evaluate to an integer value or to <b>null</b>.
  * >   </span>
  * > * <span>
- * >   An expression of one of the forms <i>-e</i>, <i>e<sub>1</sub> +
- * >   e<sub>2</sub></i>, <i>e<sub>1</sub> -e<sub>2</sub></i>,
- * >   <i>e<sub>1</sub> * e<sub>2</sub></i>, <i>e<sub>1</sub> /
- * >   e<sub>2</sub></i>, <i>e<sub>1</sub> ~/ e<sub>2</sub></i>,
- * >   <i>e<sub>1</sub> &gt; e<sub>2</sub></i>, <i>e<sub>1</sub> &lt;
- * >   e<sub>2</sub></i>, <i>e<sub>1</sub> &gt;= e<sub>2</sub></i>,
- * >   <i>e<sub>1</sub> &lt;= e<sub>2</sub></i> or <i>e<sub>1</sub> %
- * >   e<sub>2</sub></i>, where <i>e</i>, <i>e<sub>1</sub></i> and
- * >   <i>e<sub>2</sub></i> are constant expressions that evaluate to a numeric
- * >   value or to <b>null</b>.
+ * >   An expression of one of the forms <i>-e</i>, <i>e<sub>1</sub>
+ * >   -e<sub>2</sub></i>, <i>e<sub>1</sub> * e<sub>2</sub></i>,
+ * >   <i>e<sub>1</sub> / e<sub>2</sub></i>, <i>e<sub>1</sub> ~/
+ * >   e<sub>2</sub></i>, <i>e<sub>1</sub> &gt; e<sub>2</sub></i>,
+ * >   <i>e<sub>1</sub> &lt; e<sub>2</sub></i>, <i>e<sub>1</sub> &gt;=
+ * >   e<sub>2</sub></i>, <i>e<sub>1</sub> &lt;= e<sub>2</sub></i> or
+ * >   <i>e<sub>1</sub> % e<sub>2</sub></i>, where <i>e</i>,
+ * >   <i>e<sub>1</sub></i> and <i>e<sub>2</sub></i> are constant expressions
+ * >   that evaluate to a numeric value or to <b>null</b>.
+ * >   </span>
+ * > * <span>
+ * >   An expression of one the form <i>e<sub>1</sub> + e<sub>2</sub></i>,
+ * >   <i>e<sub>1</sub> -e<sub>2</sub></i> where <i>e<sub>1</sub> and
+ * >   e<sub>2</sub></i> are constant expressions that evaluate to a numeric or
+ * >   string value or to <b>null</b>.
  * >   </span>
  * > * <span>
  * >   An expression of the form <i>e<sub>1</sub> ? e<sub>2</sub> :
@@ -2207,6 +2212,8 @@ class AstComparator implements AstVisitor<bool> {
  * >   <i>e<sub>3</sub></i> are constant expressions, and <i>e<sub>1</sub></i>
  * >   evaluates to a boolean value.
  * >   </span>
+ *
+ * However, this comment is now at least a little bit out of sync with the spec.
  *
  * The values returned by instances of this class are therefore `null` and
  * instances of the classes `Boolean`, `BigInteger`, `Double`, `String`, and
@@ -2332,7 +2339,8 @@ class ConstantEvaluator extends GeneralizingAstVisitor<Object> {
         }
       } else if (node.operator.type == TokenType.PLUS) {
         // numeric or {@code null}
-        if (leftOperand is num && rightOperand is num) {
+        if ((leftOperand is num && rightOperand is num) ||
+            (leftOperand is String && rightOperand is String)) {
           return leftOperand + rightOperand;
         }
       } else if (node.operator.type == TokenType.STAR) {

pkg/analyzer/test/src/dart/ast/utilities_test.dart
@@ -196,6 +196,11 @@ class ConstantEvaluatorTest extends ParserTestCase {
     expect(value, true);
   }
 
+  void test_binary_plus_string() {
+    Object value = _getConstantValue("'hello ' + 'world'");
+    expect(value, 'hello world');
+  }
+
   void test_binary_plus_double() {
     Object value = _getConstantValue("2.3 + 3.2");
     expect(value, 2.3 + 3.2);
@@ -204,6 +209,26 @@ class ConstantEvaluatorTest extends ParserTestCase {
   void test_binary_plus_integer() {
     Object value = _getConstantValue("2 + 3");
     expect(value, 5);
+  }
+
+  void test_binary_plus_string_int() {
+    Object value = _getConstantValue("5 + 'world'");
+    expect(value, ConstantEvaluator.NOT_A_CONSTANT);
+  }
+
+  void test_binary_plus_int_string() {
+    Object value = _getConstantValue("'world' + 5");
+    expect(value, ConstantEvaluator.NOT_A_CONSTANT);
+  }
+
+  void test_binary_plus_double_string() {
+    Object value = _getConstantValue("'world' + 5.5");
+    expect(value, ConstantEvaluator.NOT_A_CONSTANT);
+  }
+
+  void test_binary_plus_string_double() {
+    Object value = _getConstantValue("5.5 + 'world'");
+    expect(value, ConstantEvaluator.NOT_A_CONSTANT);
   }
 
   void test_binary_remainder_double() {

commit 2805019038c52e33b15d6046a83ce3166dda94f3

Fix warnings in pkg/analyzer caused by be6a822daff2fae75a480d7da656ab0d9c86512e
Dart's type promotion rules aren't sophisticated enough to understand
that (a is num && b is num) || (a is String && b is String) means that
it is safe to evaluate (a + b), so we have to split it into two
separate cases.

TBR=mfairhurst@google.com,scheglov@google.com

Review-Url: https://codereview.chromium.org/2605583002 .

pkg/analyzer/lib/src/dart/ast/utilities.dart
@@ -2339,8 +2339,10 @@ class ConstantEvaluator extends GeneralizingAstVisitor<Object> {
         }
       } else if (node.operator.type == TokenType.PLUS) {
         // numeric or {@code null}
-        if ((leftOperand is num && rightOperand is num) ||
-            (leftOperand is String && rightOperand is String)) {
+        if (leftOperand is num && rightOperand is num) {
+          return leftOperand + rightOperand;
+        }
+        if (leftOperand is String && rightOperand is String) {
           return leftOperand + rightOperand;
         }
       } else if (node.operator.type == TokenType.STAR) {