f3f70fea5fd895a2813de7722d30ed3fd250a468
Vyacheslav Egorov <vegorov@google.com>
2017-01-05 15:32:39 +0100

    Gardening: improve debug output in standalone/io/socket_ipv6_test.dart.
    This test is flaky on the bot - we need to better understand why it times out.
    
    (See issue #28266)
    
    R=kustermann@google.com
    BUG=http://dartbug.com/28266
    
    Review-Url: https://codereview.chromium.org/2613893003 .

--- tests/standalone/io/test_utils.dart
+++ tests/standalone/io/test_utils.dart
@@ -13,15 +13,21 @@ Future<int> freeIPv4AndIPv6Port() async {
   return port;
 }
 
+int lastRetryId = 0;
+
 Future retry(Future fun(), {int maxCount: 10}) async {
+  final int id = lastRetryId++;
   for (int i = 0; i < maxCount; i++) {
     try {
       // If there is no exception this will simply return, otherwise we keep
       // trying.
       return await fun();
-    } catch (_) {}
-    print("Failed to execute test closure in attempt $i "
-          "(${maxCount - i} retries left).");
+    } catch (e, stack) {
+      print("Failed to execute test closure (retry id: ${id}) in attempt $i "
+            "(${maxCount - i} retries left).");
+      print("Exception: ${e}");
+      print("Stacktrace: ${stack}");
+    }
   }
   return await fun();
 }
