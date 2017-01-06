429379045b21f079ae659272bdf4142af462aa6c
Konstantin Shcheglov <scheglov@google.com>
2017-01-06 11:35:29 -0800

    Run rename-import tests with the new analysis driver.
    R=brianwilkerson@google.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/2617213002 .

--- pkg/analysis_server/test/services/refactoring/rename_import_test.dart
+++ pkg/analysis_server/test/services/refactoring/rename_import_test.dart
@@ -14,6 +14,7 @@ import 'abstract_rename.dart';
 main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(RenameImportTest);
+    defineReflectiveTests(RenameImportTest_Driver);
   });
 }
 
@@ -229,4 +230,10 @@ main() {
         findNodeAtString(search, (node) => node is ImportDirective);
     createRenameRefactoringForElement(directive.element);
   }
+}
+
+@reflectiveTest
+class RenameImportTest_Driver extends RenameImportTest {
+  @override
+  bool get enableNewAnalysisDriver => true;
 }
