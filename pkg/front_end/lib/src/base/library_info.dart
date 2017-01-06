848d0595816c446e9e91fa9060691340b08f50d3
Paul Berry <paulberry@google.com>
2017-01-06 10:03:35 -0800

    Add a missing copyright notice.
    R=sigmund@google.com
    
    Review-Url: https://codereview.chromium.org/2611183004 .

--- pkg/front_end/lib/src/base/library_info.dart
+++ pkg/front_end/lib/src/base/library_info.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 /// A bit flag used by [LibraryInfo] indicating that a library is used by
 /// dart2js.
 ///
