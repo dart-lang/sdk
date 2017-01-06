9ff7ee61326b856c4552634cf6808112c9a1b820
Ben Konyi <bkonyi@google.com>
2017-01-06 12:38:11 -0800

    Created placeholders for MallocHooks implementation and added define DART_USE_TCMALLOC to specifiy when tcmalloc is available.
    BUG=
    R=zra@google.com
    
    Review-Url: https://codereview.chromium.org/2619443002 .

---
+++ runtime/vm/malloc_hooks.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#if defined(DART_USE_TCMALLOC)
+
+#include "vm/malloc_hooks.h"
+
+namespace dart {
+
+void MallocHooks::Init() {
+  // TODO(bkonyi): Implement
+}
+
+}  // namespace dart
+
+#endif  // defined(DART_USE_TCMALLOC)
