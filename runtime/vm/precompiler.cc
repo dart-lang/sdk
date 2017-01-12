2dd8946fc4dbb7da44e80dd334cca75b29e50c05
Florian Schneider <fschneider@google.com>
2017-01-11 18:08:35 -0800

    Fix service isolate initialization in PRODUCT mode.
    My previous CL (removing noopt) incorrectly refactored this code and
    broke PRODUCT mode.
    
    R=rmacnak@google.com
    
    Review-Url: https://codereview.chromium.org/2627203002 .

--- runtime/vm/precompiler.cc
+++ runtime/vm/precompiler.cc
@@ -684,9 +684,9 @@ void Precompiler::AddRoots(Dart_QualifiedFunctionName embedder_entry_points[]) {
     {"dart:typed_data", "ByteData", "ByteData."},
     {"dart:typed_data", "ByteData", "ByteData._view"},
     {"dart:typed_data", "_ByteBuffer", "_ByteBuffer._New"},
+    {"dart:_vmservice", "::", "boot"},
 #if !defined(PRODUCT)
     {"dart:_vmservice", "::", "_registerIsolate"},
-    {"dart:_vmservice", "::", "boot"},
     {"dart:developer", "Metrics", "_printMetrics"},
     {"dart:developer", "::", "_runExtension"},
     {"dart:isolate", "::", "_runPendingImmediateCallback"},
