ee969164e2712c9c611934547332fec8d795ea63
Lex Berezhny <lex@damoti.com>
2017-01-13 03:03:52 -0500

    added Range.toString() to dart:html

--- sdk/lib/html/dart2js/html_dart2js.dart
+++ sdk/lib/html/dart2js/html_dart2js.dart
@@ -27863,6 +27863,10 @@ class Range extends Interceptor {
   @DocsEditable()
   void surroundContents(Node newParent) native;
 
+  @DomName('Range.toString')
+  @DocsEditable()
+  String toString() native;
+
 
   /**
    * Checks if createContextualFragment is supported.
