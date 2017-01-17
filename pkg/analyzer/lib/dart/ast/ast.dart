6a7139cf51f2afadec22a98350b15f6d1196fc8f
Brian Wilkerson <brianwilkerson@google.com>
2017-01-17 12:18:42 -0800

    Make TypeName a subtype of NamedType
    R=paulberry@google.com
    
    Review-Url: https://codereview.chromium.org/2635113004 .

--- pkg/analyzer/lib/dart/ast/ast.dart
+++ pkg/analyzer/lib/dart/ast/ast.dart
@@ -6615,54 +6615,7 @@ abstract class TypedLiteral extends Literal {
  *
  * Clients may not extend, implement or mix-in this class.
  */
-abstract class TypeName extends TypeAnnotation {
-  /**
-   * Return `true` if this type is a deferred type.
-   *
-   * 15.1 Static Types: A type <i>T</i> is deferred iff it is of the form
-   * </i>p.T</i> where <i>p</i> is a deferred prefix.
-   */
-  bool get isDeferred;
-
-  /**
-   * Return the name of the type.
-   */
-  Identifier get name;
-
-  /**
-   * Set the name of the type to the given [identifier].
-   */
-  void set name(Identifier identifier);
-
-  /**
-   * Return the question mark marking this as a nullable type, or `null` if
-   * the type is non-nullable.
-   */
-  Token get question;
-
-  /**
-   * Return the question mark marking this as a nullable type to the given
-   * [question].
-   */
-  void set question(Token question);
-
-  /**
-   * Set the type being named to the given [type].
-   */
-  void set type(DartType type);
-
-  /**
-   * Return the type arguments associated with the type, or `null` if there are
-   * no type arguments.
-   */
-  TypeArgumentList get typeArguments;
-
-  /**
-   * Set the type arguments associated with the type to the given
-   * [typeArguments].
-   */
-  void set typeArguments(TypeArgumentList typeArguments);
-}
+abstract class TypeName extends NamedType {}
 
 /**
  * A type parameter.
