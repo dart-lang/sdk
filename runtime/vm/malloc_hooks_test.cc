bcd7ba105645d24813333c4ca5bb55b202a6a563
Ben Konyi <bkonyi@google.com>
2017-01-19 12:09:07 -0800

    Implemented basic heap memory allocation tracking in MallocHooks using hooks registered with tcmalloc. Also updated BUILD.gn files to account for include paths within the tcmalloc project.
    BUG=
    R=zra@google.com
    
    Review-Url: https://codereview.chromium.org/2623613003 .

---
+++ runtime/vm/malloc_hooks_test.cc
@@ -0,0 +1,80 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include "platform/globals.h"
+
+#if defined(DART_USE_TCMALLOC) && !defined(PRODUCT)
+
+#include "platform/assert.h"
+#include "vm/class_finalizer.h"
+#include "vm/globals.h"
+#include "vm/malloc_hooks.h"
+#include "vm/symbols.h"
+#include "vm/unit_test.h"
+
+namespace dart {
+
+// Note: for these tests, there is no need to call MallocHooks::Init() or
+// MallocHooks::TearDown() as this is all done by the VM test framework.
+
+static void MallocHookTestBufferInitializer(volatile char* buffer,
+                                            uintptr_t size) {
+  // Run through the buffer and do something. If we don't do this and the memory
+  // in buffer isn't touched, the tcmalloc hooks won't be called.
+  for (uintptr_t i = 0; i < size; ++i) {
+    buffer[i] = i;
+  }
+}
+
+
+UNIT_TEST_CASE(BasicMallocHookTest) {
+  MallocHooks::ResetStats();
+  EXPECT_EQ(0L, MallocHooks::allocation_count());
+  EXPECT_EQ(0L, MallocHooks::heap_allocated_memory_in_bytes());
+
+  const intptr_t buffer_size = 10;
+  char* buffer = new char[buffer_size];
+  MallocHookTestBufferInitializer(buffer, buffer_size);
+
+  EXPECT_EQ(1L, MallocHooks::allocation_count());
+  EXPECT_EQ(static_cast<intptr_t>(sizeof(char) * buffer_size),
+            MallocHooks::heap_allocated_memory_in_bytes());
+
+  delete[] buffer;
+  EXPECT_EQ(0L, MallocHooks::allocation_count());
+  EXPECT_EQ(0L, MallocHooks::heap_allocated_memory_in_bytes());
+}
+
+
+UNIT_TEST_CASE(FreeUnseenMemoryMallocHookTest) {
+  const intptr_t pre_hook_buffer_size = 3;
+  char* pre_hook_buffer = new char[pre_hook_buffer_size];
+  MallocHookTestBufferInitializer(pre_hook_buffer, pre_hook_buffer_size);
+
+  MallocHooks::ResetStats();
+  EXPECT_EQ(0L, MallocHooks::allocation_count());
+  EXPECT_EQ(0L, MallocHooks::heap_allocated_memory_in_bytes());
+
+  const intptr_t buffer_size = 10;
+  volatile char* buffer = new char[buffer_size];
+  MallocHookTestBufferInitializer(buffer, buffer_size);
+
+  EXPECT_EQ(1L, MallocHooks::allocation_count());
+  EXPECT_EQ(static_cast<intptr_t>(sizeof(char) * buffer_size),
+            MallocHooks::heap_allocated_memory_in_bytes());
+
+  delete[] pre_hook_buffer;
+  EXPECT_EQ(1L, MallocHooks::allocation_count());
+  EXPECT_EQ(static_cast<intptr_t>(sizeof(char) * buffer_size),
+            MallocHooks::heap_allocated_memory_in_bytes());
+
+
+  delete[] buffer;
+  EXPECT_EQ(0L, MallocHooks::allocation_count());
+  EXPECT_EQ(0L, MallocHooks::heap_allocated_memory_in_bytes());
+}
+
+};  // namespace dart
+
+#endif  // defined(DART_USE_TCMALLOC) && !defined(PRODUCT)
