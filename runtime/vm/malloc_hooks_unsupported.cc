10686766cddb600319820aa31aff42ef82118aee
Ben Konyi <bkonyi@google.com>
2017-01-19 12:14:19 -0800

    Revert "Implemented basic heap memory allocation tracking in MallocHooks using hooks registered with tcmalloc. Also updated BUILD.gn files to account for include paths within the tcmalloc project."
    This reverts commit bcd7ba105645d24813333c4ca5bb55b202a6a563.
    
    TBR=zra@google.com
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2645973002 .

--- runtime/vm/malloc_hooks_unsupported.cc
+++ runtime/vm/malloc_hooks_unsupported.cc
@@ -2,38 +2,16 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#include "platform/globals.h"
-
-#if !defined(DART_USE_TCMALLOC) || defined(PRODUCT)
+#if defined(DART_USE_TCMALLOC)
 
 #include "vm/malloc_hooks.h"
 
 namespace dart {
 
 void MallocHooks::Init() {
-  // Do nothing.
-}
-
-
-void MallocHooks::TearDown() {
-  // Do nothing.
-}
-
-
-void MallocHooks::Reset() {
-  // Do nothing.
-}
-
-
-uintptr_t MallocHooks::allocation_count() {
-  return 0;
-}
-
-
-uintptr_t MallocHooks::heap_allocated_memory_in_bytes() {
-  return 0;
+  // TODO(bkonyi): Implement
 }
 
 }  // namespace dart
 
-#endif  // defined(DART_USE_TCMALLOC) || defined(PRODUCT)
+#endif  // defined(DART_USE_TCMALLOC)
