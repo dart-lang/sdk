b3c4f5806e64b4cabd74bdb0cc1b263d478c5bfb
Regis Crelier <regis@google.com>
2017-01-23 14:50:22 -0800

    Fix generic function tests to satisfy analyzer (fixes #27728).
    R=asiva@google.com
    
    Review-Url: https://codereview.chromium.org/2650823003 .

--- tests/language/generic_methods_generic_function_parameter_test.dart
+++ tests/language/generic_methods_generic_function_parameter_test.dart
@@ -7,11 +7,11 @@
 import "package:expect/expect.dart";
 
 class C<T> {
-  bar<V>(T t, int u, V v) => t + u + v;
-  foo<U>(bar<V>(T t, U u, V v)) => bar<int>(1, 2, 3);
+  bar<V>(T t, int u, V v) => t.toString() + u.toString() + v.toString();
+  foo<U>(bar<V>(T t, U u, V v)) => bar<int>(1 as T, 2 as U, 3);
 }
 
 main() {
   var c = new C<int>();
-  Expect.equals(6, c.foo<int>(c.bar));
+  Expect.equals("123", c.foo<int>(c.bar));
 }
