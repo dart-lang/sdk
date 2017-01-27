39a5e660cd2740f4f3e0289b8c011e61fee92627
Konstantin Shcheglov <scheglov@google.com>
2017-01-27 15:11:09 -0800

    Call addFile() before getErrors() to actually get errors.
    With this change we have only 7 failing shared tests.
    
    R=brianwilkerson@google.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/2662693002 .

--- pkg/analyzer_cli/lib/src/analyzer_impl.dart
+++ pkg/analyzer_cli/lib/src/analyzer_impl.dart
@@ -132,6 +132,7 @@ class AnalyzerImpl {
       for (Source source in sources) {
         if (analysisDriver != null) {
           String path = source.fullName;
+          analysisDriver.addFile(path);
           ErrorsResult errorsResult = await analysisDriver.getErrors(path);
           errorInfos.add(new AnalysisErrorInfoImpl(
               errorsResult.errors, errorsResult.lineInfo));
