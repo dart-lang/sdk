d8fc3630cff055376f915422842f3c1bb503cb7f
Paul Berry <paulberry@google.com>
2017-01-30 15:05:52 -0800

    Move resolveRelativeUri into front_end.
    Note: I removed the exception wrapping code as part of the migration.
    Rationale: this function performs a small task and has been operating
    successfully for a long time; removing the exception wrapping code
    avoids making front_end dependent upon AnalysisException and
    CaughtException at this time.  In the unlikely event that an exception
    occurs in this function, we still have many avenues available for
    tracking down the problem: examine the stack trace of the exception,
    isolate a repeatable test case (if the exception happened in
    e.g. command-line analyzer), or reproduce the exception from file
    state information recorded by the AnalysisDriver (if the exception
    happened in analysis server).
    
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2668553002 .

--- pkg/analyzer/lib/src/generated/utilities_dart.dart
+++ pkg/analyzer/lib/src/generated/utilities_dart.dart
@@ -6,40 +6,10 @@ library analyzer.src.generated.utilities_dart;
 
 import 'package:analyzer/dart/ast/ast.dart' show AnnotatedNode, Comment;
 import 'package:analyzer/dart/ast/token.dart' show Token;
-import 'package:analyzer/exception/exception.dart';
 import 'package:analyzer/src/dart/element/element.dart' show ElementImpl;
-import 'package:analyzer/src/generated/source.dart';
 
-/**
- * Resolve the [containedUri] against [baseUri] using Dart rules.
- *
- * This function behaves similarly to [Uri.resolveUri], except that it properly
- * handles situations like the following:
- *
- *     resolveRelativeUri(dart:core, bool.dart) -> dart:core/bool.dart
- *     resolveRelativeUri(package:a/b.dart, ../c.dart) -> package:a/c.dart
- */
-Uri resolveRelativeUri(Uri baseUri, Uri containedUri) {
-  if (containedUri.isAbsolute) {
-    return containedUri;
-  }
-  Uri origBaseUri = baseUri;
-  try {
-    String scheme = baseUri.scheme;
-    // dart:core => dart:core/core.dart
-    if (scheme == DartUriResolver.DART_SCHEME) {
-      String part = baseUri.path;
-      if (part.indexOf('/') < 0) {
-        baseUri = Uri.parse('$scheme:$part/$part.dart');
-      }
-    }
-    return baseUri.resolveUri(containedUri);
-  } catch (exception, stackTrace) {
-    throw new AnalysisException(
-        "Could not resolve URI ($containedUri) relative to source ($origBaseUri)",
-        new CaughtException(exception, stackTrace));
-  }
-}
+export 'package:front_end/src/base/resolve_relative_uri.dart'
+    show resolveRelativeUri;
 
 /**
  * If the given [node] has a documentation comment, remember its content
