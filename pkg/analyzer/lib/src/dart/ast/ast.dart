921f8664967fc76bdee94a74d717032a15a873a2
Brian Wilkerson <brianwilkerson@google.com>
2017-02-01 09:33:46 -0800

    First step toward resolving generic function types
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2667073004 .

--- pkg/analyzer/lib/src/dart/ast/ast.dart
+++ pkg/analyzer/lib/src/dart/ast/ast.dart
@@ -5700,6 +5700,9 @@ class GenericFunctionTypeImpl extends TypeAnnotationImpl
    */
   FormalParameterList _parameters;
 
+  @override
+  DartType type;
+
   /**
    * Initialize a newly created generic function type.
    */
@@ -5742,9 +5745,6 @@ class GenericFunctionTypeImpl extends TypeAnnotationImpl
   void set returnType(TypeAnnotation type) {
     _returnType = _becomeParentOf(type as AstNodeImpl);
   }
-
-  @override
-  DartType get type => null;
 
   /**
    * Return the type parameters for the function type, or `null` if the function
