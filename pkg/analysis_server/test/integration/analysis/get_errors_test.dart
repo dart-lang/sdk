06b3f474349ce5725765e29b1cb3ec88200e0f0d
Brian Wilkerson <brianwilkerson@google.com>
2017-02-06 10:09:03 -0800

    [Gardening] Remove an invalid (and flaky) test
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2681493002 .

--- pkg/analysis_server/test/integration/analysis/get_errors_test.dart
+++ pkg/analysis_server/test/integration/analysis/get_errors_test.dart
@@ -7,21 +7,23 @@ library test.integration.analysis.get.errors;
 import 'dart:async';
 
 import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import '../integration_tests.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(GetErrorsTest);
+    defineReflectiveTests(GetErrorsTest_Driver);
+  });
+}
 
 /**
  * Base class for testing the "analysis.getErrors" request.
  */
 class AnalysisDomainGetErrorsTest
     extends AbstractAnalysisServerIntegrationTest {
-  /**
-   * True if the "analysis.getErrors" request should be made after analysis is
-   * complete.
-   */
-  final bool afterAnalysis;
-
-  AnalysisDomainGetErrorsTest(this.afterAnalysis);
+  AnalysisDomainGetErrorsTest();
 
   test_getErrors() {
     String pathname = sourcePath('test.dart');
@@ -36,10 +38,16 @@ main() {
         expect(result.errors, equals(currentAnalysisErrors[pathname]));
       });
     }
-    if (afterAnalysis) {
-      return analysisFinished.then((_) => finishTest());
-    } else {
-      return finishTest();
-    }
+
+    return analysisFinished.then((_) => finishTest());
   }
+}
+
+@reflectiveTest
+class GetErrorsTest extends AnalysisDomainGetErrorsTest {}
+
+@reflectiveTest
+class GetErrorsTest_Driver extends AnalysisDomainGetErrorsTest {
+  @override
+  bool get enableNewAnalysisDriver => true;
 }
