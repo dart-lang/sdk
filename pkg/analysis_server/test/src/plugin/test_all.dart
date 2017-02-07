ccdf574d491d8dc9eb3b950ea3b05f7b83d521ac
Brian Wilkerson <brianwilkerson@google.com>
2017-02-07 08:06:59 -0800

    Add support for merging results from plugins
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2678673002 .

--- pkg/analysis_server/test/src/plugin/test_all.dart
+++ pkg/analysis_server/test/src/plugin/test_all.dart
@@ -6,10 +6,12 @@ import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'plugin_locator_test.dart' as plugin_locator_test;
 import 'result_collector_test.dart' as result_collector_test;
+import 'result_merger_test.dart' as result_merger_test;
 
 main() {
   defineReflectiveSuite(() {
     plugin_locator_test.main();
     result_collector_test.main();
+    result_merger_test.main();
   });
 }
