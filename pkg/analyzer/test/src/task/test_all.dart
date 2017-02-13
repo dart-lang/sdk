e3380d2f67db04ed3ce01682355f4c7f73eca50d
Konstantin Shcheglov <scheglov@google.com>
2017-02-13 13:31:31 -0800

    Run InstanceMemberInferrerTest with the new analysis driver.
    R=brianwilkerson@google.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/2697443003 .

--- pkg/analyzer/test/src/task/test_all.dart
+++ pkg/analyzer/test/src/task/test_all.dart
@@ -20,6 +20,7 @@ import 'model_test.dart' as model_test;
 import 'options_test.dart' as options_test;
 import 'options_work_manager_test.dart' as options_work_manager_test;
 import 'strong/test_all.dart' as strong_mode_test_all;
+import 'strong_mode_driver_test.dart' as strong_mode_driver_test;
 import 'strong_mode_test.dart' as strong_mode_test;
 import 'yaml_test.dart' as yaml_test;
 
@@ -39,6 +40,7 @@ main() {
     options_test.main();
     options_work_manager_test.main();
     strong_mode_test_all.main();
+    strong_mode_driver_test.main();
     strong_mode_test.main();
     yaml_test.main();
   }, name: 'task');
