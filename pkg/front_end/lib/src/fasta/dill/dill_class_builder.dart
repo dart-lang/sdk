dart-lang/sdk

commit b62a37a36b2d01c3c27979a2cf5eada24afce035

Implement type variables in mixin applications.
R=karlklose@google.com

Review-Url: https://codereview.chromium.org/2689303003 .

pkg/front_end/lib/src/fasta/dill/dill_class_builder.dart
@@ -11,9 +11,13 @@ import 'package:kernel/ast.dart' show
     Procedure,
     ProcedureKind;
 
+import '../errors.dart' show
+    internalError;
+
 import '../kernel/kernel_builder.dart' show
     Builder,
-    KernelClassBuilder;
+    KernelClassBuilder,
+    TypeBuilder;
 
 import '../modifier.dart' show
     abstractMask;
@@ -49,6 +53,12 @@ class DillClassBuilder extends KernelClassBuilder {
       }
     }
   }
+
+  /// Returns true if this class is the result of applying a mixin to its
+  /// superclass.
+  bool get isMixinApplication => cls.isMixinApplication;
+
+  TypeBuilder get mixedInType => internalError("Not implemented.");
 
   Builder findConstructorOrFactory(String name) => constructors[name];
 }

pkg/front_end/lib/src/fasta/kernel/kernel_enum_builder.dart
@@ -52,7 +52,8 @@ import 'kernel_builder.dart' show
     KernelTypeBuilder,
     LibraryBuilder,
     MemberBuilder,
-    MetadataBuilder;
+    MetadataBuilder,
+    TypeBuilder;
 
 class KernelEnumBuilder extends SourceClassBuilder
     implements EnumBuilder<KernelTypeBuilder, InterfaceType> {
@@ -139,6 +140,8 @@ class KernelEnumBuilder extends SourceClassBuilder
     selfType.builder = enumBuilder;
     return enumBuilder;
   }
+
+  TypeBuilder get mixedInType => null;
 
   InterfaceType buildType(List<KernelTypeBuilder> arguments) {
     return cls.rawType;

pkg/front_end/lib/src/fasta/kernel/kernel_named_mixin_application_builder.dart
@@ -16,6 +16,7 @@ import 'kernel_builder.dart' show
     LibraryBuilder,
     MetadataBuilder,
     NamedMixinApplicationBuilder,
+    TypeBuilder,
     TypeVariableBuilder;
 
 class KernelNamedMixinApplicationBuilder extends SourceClassBuilder
@@ -29,4 +30,8 @@ class KernelNamedMixinApplicationBuilder extends SourceClassBuilder
           interfaces, <String, Builder>{}, parent, null, charOffset);
 
   KernelTypeBuilder get mixinApplication => supertype;
+
+  // TODO(ahe): This is a bit odd, as it means this answers false to
+  // [isMixinApplication], but its superclass is the mixin application.
+  TypeBuilder get mixedInType => null;
 }

pkg/front_end/lib/src/fasta/builder/class_builder.dart
@@ -33,6 +33,12 @@ abstract class ClassBuilder<T extends TypeBuilder, R>
       this.members, LibraryBuilder parent, int charOffset)
       : super(metadata, modifiers, name, parent, charOffset);
 
+  /// Returns true if this class is the result of applying a mixin to its
+  /// superclass.
+  bool get isMixinApplication => mixedInType != null;
+
+  T get mixedInType;
+
   List<ConstructorReferenceBuilder> get constructorReferences => null;
 
   Map<String, Builder> get constructors;

pkg/front_end/lib/src/fasta/builder/mixin_application_builder.dart
@@ -9,7 +9,8 @@ import '../errors.dart' show
 
 import 'builder.dart' show
     TypeBuilder,
-    TypeDeclarationBuilder;
+    TypeDeclarationBuilder,
+    TypeVariableBuilder;
 
 import 'scope.dart' show
     Scope;
@@ -22,6 +23,16 @@ abstract class MixinApplicationBuilder<T extends TypeBuilder>
   MixinApplicationBuilder(this.supertype, this.mixins, int charOffset,
       Uri fileUri)
       : super(charOffset, fileUri);
+
+  void set typeVariables(List<TypeVariableBuilder> variables);
+
+  /// If this mixin application uses type variables, it needs a unique name
+  /// based on its subclass. If this name is provided, the name will be
+  /// `name^mixin`, otherwise it'll be `superclass&mixin`.
+  //
+  // TODO(ahe): This is to reduce diff against dartk. Consider if this is
+  // necessary.
+  void set subclassName(String value);
 
   String get name => null;
 

pkg/front_end/lib/src/fasta/kernel/kernel_library_builder.dart
@@ -92,7 +92,7 @@ class KernelLibraryBuilder
   KernelTypeBuilder addMixinApplication(KernelTypeBuilder supertype,
       List<KernelTypeBuilder> mixins, int charOffset) {
     KernelTypeBuilder type = new KernelMixinApplicationBuilder(
-        supertype, mixins, charOffset, fileUri);
+        supertype, mixins, this, charOffset, fileUri);
     return addType(type);
   }
 
@@ -225,23 +225,6 @@ class KernelLibraryBuilder
     if (builder is SourceClassBuilder) {
       Class cls = builder.build(this);
       library.addClass(cls);
-      Class superclass = cls.superclass;
-      if (superclass != null && superclass.isMixinApplication) {
-        List<Class> mixinApplications = <Class>[];
-        mixinApplicationClasses.add(cls);
-        while (superclass != null && superclass.isMixinApplication) {
-          if (superclass.parent == null) {
-            mixinApplications.add(superclass);
-          }
-          superclass = superclass.superclass;
-        }
-        for (Class cls in mixinApplications.reversed) {
-          // TODO(ahe): Should be able to move this into the above loop as long
-          // as we don't care about matching dartk perfectly.
-          library.addClass(cls);
-          mixinApplicationClasses.add(cls);
-        }
-      }
     } else if (builder is KernelFieldBuilder) {
       library.addMember(builder.build(library)..isStatic = true);
     } else if (builder is KernelProcedureBuilder) {

pkg/front_end/lib/src/fasta/kernel/kernel_mixin_application_builder.dart
@@ -5,16 +5,29 @@
 library fasta.kernel_mixin_application_builder;
 
 import 'package:kernel/ast.dart' show
-    Class,
     InterfaceType,
-    Supertype;
+    Supertype,
+    setParents;
+
+import '../modifier.dart' show
+    abstractMask;
 
 import 'kernel_builder.dart' show
+    Builder,
+    ConstructorReferenceBuilder,
+    KernelLibraryBuilder,
+    KernelNamedTypeBuilder,
     KernelTypeBuilder,
-    MixinApplicationBuilder;
+    KernelTypeVariableBuilder,
+    MixinApplicationBuilder,
+    TypeBuilder,
+    TypeVariableBuilder;
 
 import '../util/relativize.dart' show
     relativizeUri;
+
+import '../source/source_class_builder.dart' show
+    SourceClassBuilder;
 
 class KernelMixinApplicationBuilder
     extends MixinApplicationBuilder<KernelTypeBuilder>
@@ -23,10 +36,16 @@ class KernelMixinApplicationBuilder
 
   final String relativeFileUri;
 
+  final KernelLibraryBuilder library;
+
   Supertype builtType;
 
+  List<TypeVariableBuilder> typeVariables;
+
+  String subclassName;
+
   KernelMixinApplicationBuilder(KernelTypeBuilder supertype,
-      List<KernelTypeBuilder> mixins, int charOffset, Uri fileUri)
+      List<KernelTypeBuilder> mixins, this.library, int charOffset, Uri fileUri)
       : charOffset = charOffset,
         relativeFileUri = relativizeUri(fileUri),
         super(supertype, mixins, charOffset, fileUri);
@@ -35,28 +54,48 @@ class KernelMixinApplicationBuilder
 
   Supertype buildSupertype() {
     if (builtType != null) return builtType;
-    Supertype supertype =
-        this.supertype.buildSupertype()?.classNode?.asRawSupertype;
-    if (supertype == null) {
-      return null;
-    }
+    KernelTypeBuilder s = this.supertype;
     for (KernelTypeBuilder builder in mixins) {
-      Supertype mixin = builder.buildSupertype()?.classNode?.asRawSupertype;
-      if (mixin == null) {
-        return null;
-      }
-      Class application = new Class(
-          name: "${supertype.classNode.name}&${mixin.classNode.name}",
-          isAbstract: true,
-          supertype: supertype,
-          mixedInType: mixin,
-          typeParameters: null, // TODO(ahe): Compute these.
-          fileUri: relativeFileUri);
-      application.fileOffset = charOffset;
-      // TODO(ahe): Use asThisSupertype instead and translate type variables.
-      supertype = application.asRawSupertype;
+      s = applyMixin(s, builder);
     }
-    builtType = supertype;
+    builtType = s.buildSupertype();
     return builtType;
+  }
+
+  TypeBuilder applyMixin(TypeBuilder supertype, TypeBuilder mixin) {
+    KernelLibraryBuilder library = this.library.partOfLibrary ?? this.library;
+    List<TypeVariableBuilder> newTypeVariables;
+    List<KernelTypeBuilder> typeArguments;
+    if (typeVariables != null) {
+      newTypeVariables = library.copyTypeVariables(typeVariables);
+      Map<TypeVariableBuilder, TypeBuilder> substitution =
+          <TypeVariableBuilder, TypeBuilder>{};
+      typeArguments = <KernelTypeBuilder>[];
+      for (int i = 0; i < typeVariables.length; i++) {
+        substitution[typeVariables[i]] = newTypeVariables[i].asTypeBuilder();
+        typeArguments.add(typeVariables[i].asTypeBuilder());
+      }
+      supertype = supertype.subst(substitution);
+      mixin = mixin.subst(substitution);
+    }
+    // To reduce diff against dartk, we create a different name for mixin
+    // applications that have free type variables. We do this by setting
+    // [subclassName] when setting typeVariables.
+    String name = subclassName != null
+        ? "${subclassName}^${mixin.name}"
+        : "${supertype.name}&${mixin.name}";
+    SourceClassBuilder cls = new SourceClassBuilder(null, abstractMask, name,
+        newTypeVariables, supertype, null, <String, Builder>{}, library,
+        <ConstructorReferenceBuilder>[], charOffset, null, mixin);
+    library.addImplementationBuilder(name, cls, charOffset);
+    if (newTypeVariables != null) {
+      for (KernelTypeVariableBuilder t in newTypeVariables) {
+        cls.cls.typeParameters.add(t.parameter);
+      }
+      setParents(cls.cls.typeParameters, cls.cls);
+    }
+    return new KernelNamedTypeBuilder(name, typeArguments, charOffset,
+        library.fileUri)
+        ..builder = cls;
   }
 }

pkg/front_end/lib/src/fasta/kernel/kernel_target.dart
@@ -346,6 +346,9 @@ class KernelTarget extends TargetImplementation {
       if (cls != objectClass) {
         cls.supertype ??= objectClass.asRawSupertype;
       }
+      if (builder.isMixinApplication) {
+        cls.mixedInType = builder.mixedInType.buildSupertype();
+      }
     }
     ticker.logMs("Installed Object as implicit superclass");
   }
@@ -368,7 +371,7 @@ class KernelTarget extends TargetImplementation {
 
   /// If [builder] doesn't have a constructors, install the defaults.
   void installDefaultConstructor(SourceClassBuilder builder) {
-    if (!builder.constructors.isEmpty) return;
+    if (builder.isMixinApplication || builder.constructors.isNotEmpty) return;
     /// Quotes below are from [Dart Programming Language Specification, 4th
     /// Edition](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-408.pdf):
     if (builder is NamedMixinApplicationBuilder) {

commit 7aeba7513d72ea943819c45ada4fd11620d0a44d

Fix strong mode errors introduced by b62a37a36b2d01c3c27979a2cf5eada24afce035.
R=sigmund@google.com

Review-Url: https://codereview.chromium.org/2698013003 .

pkg/front_end/lib/src/fasta/dill/dill_class_builder.dart
@@ -17,7 +17,7 @@ import '../errors.dart' show
 import '../kernel/kernel_builder.dart' show
     Builder,
     KernelClassBuilder,
-    TypeBuilder;
+    KernelTypeBuilder;
 
 import '../modifier.dart' show
     abstractMask;
@@ -58,7 +58,7 @@ class DillClassBuilder extends KernelClassBuilder {
   /// superclass.
   bool get isMixinApplication => cls.isMixinApplication;
 
-  TypeBuilder get mixedInType => internalError("Not implemented.");
+  KernelTypeBuilder get mixedInType => internalError("Not implemented.");
 
   Builder findConstructorOrFactory(String name) => constructors[name];
 }

pkg/front_end/lib/src/fasta/kernel/kernel_enum_builder.dart
@@ -52,8 +52,7 @@ import 'kernel_builder.dart' show
     KernelTypeBuilder,
     LibraryBuilder,
     MemberBuilder,
-    MetadataBuilder,
-    TypeBuilder;
+    MetadataBuilder;
 
 class KernelEnumBuilder extends SourceClassBuilder
     implements EnumBuilder<KernelTypeBuilder, InterfaceType> {
@@ -141,7 +140,7 @@ class KernelEnumBuilder extends SourceClassBuilder
     return enumBuilder;
   }
 
-  TypeBuilder get mixedInType => null;
+  KernelTypeBuilder get mixedInType => null;
 
   InterfaceType buildType(List<KernelTypeBuilder> arguments) {
     return cls.rawType;

pkg/front_end/lib/src/fasta/kernel/kernel_named_mixin_application_builder.dart
@@ -16,7 +16,6 @@ import 'kernel_builder.dart' show
     LibraryBuilder,
     MetadataBuilder,
     NamedMixinApplicationBuilder,
-    TypeBuilder,
     TypeVariableBuilder;
 
 class KernelNamedMixinApplicationBuilder extends SourceClassBuilder
@@ -33,5 +32,5 @@ class KernelNamedMixinApplicationBuilder extends SourceClassBuilder
 
   // TODO(ahe): This is a bit odd, as it means this answers false to
   // [isMixinApplication], but its superclass is the mixin application.
-  TypeBuilder get mixedInType => null;
+  KernelTypeBuilder get mixedInType => null;
 }