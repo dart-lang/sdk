dart-lang/sdk

commit cfe5e5f075431641642a8e3be2fbd4414eaca1fd

Include metadata in AOT to expand inline frames in stack traces and provide line numbers.
Also fix stack trace collection to always include invisible frames. It can happen that a visible function is inlined into an invisible function, and we don't expand inlined frames until we print a stack trace.

dart2js product x64:
compile time: 12.459s
VMIsolate(CodeSize): 152292
Isolate(CodeSize): 3343117
ReadOnlyData(CodeSize): 3728928
Instructions(CodeSize): 8677600
Total(CodeSize): 15901937
->
compile time: 14.195s (+13%)
VMIsolate(CodeSize): 174034
Isolate(CodeSize): 3892418 (+16%)
ReadOnlyData(CodeSize): 5036320 (+35%)
Instructions(CodeSize): 8682624
Total(CodeSize): 17785396 (+12%)

R=asiva@google.com

Review-Url: https://codereview.chromium.org/2687143005 .

runtime/vm/raw_object.h
@@ -1139,12 +1139,12 @@ class RawCode : public RawObject {
   RawExceptionHandlers* exception_handlers_;
   RawPcDescriptors* pc_descriptors_;
   RawArray* stackmaps_;
+  RawArray* inlined_id_to_function_;
+  RawCodeSourceMap* code_source_map_;
   NOT_IN_PRECOMPILED(RawInstructions* active_instructions_);
   NOT_IN_PRECOMPILED(RawArray* deopt_info_array_);
   // (code-offset, function, code) triples.
   NOT_IN_PRECOMPILED(RawArray* static_calls_target_table_);
-  NOT_IN_PRECOMPILED(RawArray* inlined_id_to_function_);
-  NOT_IN_PRECOMPILED(RawCodeSourceMap* code_source_map_);
   // If return_address_metadata_ is a Smi, it is the offset to the prologue.
   // Else, return_address_metadata_ is null.
   NOT_IN_PRECOMPILED(RawObject* return_address_metadata_);

runtime/tests/vm/dart/optimized_stacktrace_line_and_column_test.dart
@@ -19,8 +19,9 @@ test1() {
     Expect.fail("Unreachable");
   } catch (e, stacktrace) {
     String s = stacktrace.toString();
-    Expect.isFalse(s.contains("-1:-1"));
-    Expect.isTrue(s.contains("11:18"));
+    print(s);
+    Expect.isFalse(s.contains("-1:-1"), "A");
+    Expect.isTrue(s.contains("optimized_stacktrace_line_and_column_test.dart:11:18"), "B");
   }
 
   // Optimized.
@@ -30,8 +31,9 @@ test1() {
     Expect.fail("Unreachable");
   } catch (e, stacktrace) {
     String s = stacktrace.toString();
-    Expect.isFalse(s.contains("-1:-1"));
-    Expect.isTrue(s.contains("11:18"));
+    print(s);
+    Expect.isFalse(s.contains("-1:-1"), "C");
+    Expect.isTrue(s.contains("optimized_stacktrace_line_and_column_test.dart:11:18"), "D");
   }
 }
 
@@ -56,9 +58,9 @@ test2() {
   } catch (e, stacktrace) {
     String s = stacktrace.toString();
     print(s);
-    Expect.isTrue(s.contains("maximus"));
-    Expect.isTrue(s.contains("moritz"));
-    Expect.isFalse(s.contains("-1:-1"));
+    Expect.isTrue(s.contains("maximus"), "E");
+    Expect.isTrue(s.contains("moritz"), "F");
+    Expect.isFalse(s.contains("-1:-1"), "G");
   }
 
   try {
@@ -66,9 +68,9 @@ test2() {
   } catch (e, stacktrace) {
     String s = stacktrace.toString();
     print(s);
-    Expect.isTrue(s.contains("maximus"));
-    Expect.isTrue(s.contains("moritz"));
-    Expect.isFalse(s.contains("-1:-1"));
+    Expect.isTrue(s.contains("maximus"), "H");
+    Expect.isTrue(s.contains("moritz"), "I");
+    Expect.isFalse(s.contains("-1:-1"), "J");
   }
 }
 

runtime/tests/vm/dart/optimized_stacktrace_line_test.dart
@@ -1,0 +1,80 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Test correct source positions in stack trace with optimized functions.
+import "package:expect/expect.dart";
+
+// (1) Test normal exception.
+foo(x) => bar(x);
+
+bar(x) {
+  if (x == null) throw 42;  // throw at position 11:18
+  return x + 1;
+}
+
+test1() {
+  // First unoptimized.
+  try {
+    foo(null);
+    Expect.fail("Unreachable");
+  } catch (e, stacktrace) {
+    String s = stacktrace.toString();
+    print(s);
+    Expect.isFalse(s.contains("-1:-1"), "A");
+    Expect.isTrue(s.contains("optimized_stacktrace_line_test.dart:11"), "B");
+  }
+
+  // Optimized.
+  for (var i=0; i<10000; i++) foo(42);
+  try {
+    foo(null);
+    Expect.fail("Unreachable");
+  } catch (e, stacktrace) {
+    String s = stacktrace.toString();
+    print(s);
+    Expect.isFalse(s.contains("-1:-1"), "C");
+    Expect.isTrue(s.contains("optimized_stacktrace_line_test.dart:11"), "D");
+  }
+}
+
+
+// (2) Test checked mode exceptions.
+maximus(x) => moritz(x);
+
+moritz(x) {
+  if (x == 333)  return 42 ? 0 : 1;  // Throws in checked mode.
+  if (x == 777)   {
+    bool b = x;  // Throws in checked mode.
+    return b;
+  }
+
+  return x + 1;
+}
+
+test2() {
+  for (var i=0; i<100000; i++) maximus(42);
+  try {
+    maximus(333);
+  } catch (e, stacktrace) {
+    String s = stacktrace.toString();
+    print(s);
+    Expect.isTrue(s.contains("maximus"), "E");
+    Expect.isTrue(s.contains("moritz"), "F");
+    Expect.isFalse(s.contains("-1:-1"), "G");
+  }
+
+  try {
+    maximus(777);
+  } catch (e, stacktrace) {
+    String s = stacktrace.toString();
+    print(s);
+    Expect.isTrue(s.contains("maximus"), "H");
+    Expect.isTrue(s.contains("moritz"), "I");
+    Expect.isFalse(s.contains("-1:-1"), "J");
+  }
+}
+
+main() {
+  test1();
+  test2();
+}

runtime/tests/vm/vm.status
@@ -149,7 +149,8 @@ dart/simd128float32_test: Skip # compilers not aware of Simd128
 
 [ $compiler == dart2js ]
 # The source positions do not match with dart2js.
-dart/optimized_stacktrace_test: RuntimeError
+dart/optimized_stacktrace_line_test: RuntimeError
+dart/optimized_stacktrace_line_and_column_test: RuntimeError
 
 # Methods can be missing in dart2js stack traces due to inlining.  Also when
 # minifying they can be renamed, which is issue 7953.
@@ -169,7 +170,8 @@ cc/Int8ListLengthMaxElements: Skip # Issue 23536, uses 1 GB memory.
 cc/FindCodeObject: SkipSlow # Takes more than 8 minutes. Issue 17440
 
 [ $compiler == dart2analyzer ]
-dart/optimized_stacktrace_test: StaticWarning
+dart/optimized_stacktrace_line_test: StaticWarning
+dart/optimized_stacktrace_line_and_column_test: StaticWarning
 
 [ $compiler == dart2analyzer && $builder_tag == strong ]
 *: Skip # Issue 28649
@@ -256,14 +258,12 @@ cc/CreateMirrorSystem: SkipByDesign # Imports dart:mirrors
 cc/CoreSnapshotSize: SkipByDesign # Imports dart:mirrors
 cc/StandaloneSnapshotSize: SkipByDesign # Imports dart:mirrors
 
-[ $runtime == dart_precompiled ]
-# StackTraces in precompilation omit inlined frames.
-dart/inline_stack_frame_test: Pass, RuntimeError
-dart/optimized_stacktrace_test: Pass, RuntimeError
-dart/data_uri_spawn_test: SkipByDesign # Isolate.spawnUri
+[ $compiler == app_jit ]
+dart/optimized_stacktrace_line_and_column_test: RuntimeError,OK # app-jit lacks column information
 
-[ $compiler == app_jit || $compiler == precompiler ]
-dart/optimized_stacktrace_test: SkipByDesign # Requires line numbers
+[ $runtime == dart_precompiled ]
+dart/optimized_stacktrace_line_and_column_test: RuntimeError,OK # AOT lacks column information
+dart/data_uri_spawn_test: SkipByDesign # Isolate.spawnUri
 
 [ $runtime == vm && $mode == product ]
 cc/IsolateSetCheckedMode: Fail,OK  # Expects exact type name.

runtime/vm/clustered_snapshot.cc
@@ -1567,12 +1567,12 @@ class CodeSerializationCluster : public SerializationCluster {
     s->Push(code->ptr()->exception_handlers_);
     s->Push(code->ptr()->pc_descriptors_);
     s->Push(code->ptr()->stackmaps_);
+    s->Push(code->ptr()->inlined_id_to_function_);
+    s->Push(code->ptr()->code_source_map_);
 
     if (s->kind() == Snapshot::kAppJIT) {
       s->Push(code->ptr()->deopt_info_array_);
       s->Push(code->ptr()->static_calls_target_table_);
-      s->Push(code->ptr()->inlined_id_to_function_);
-      s->Push(code->ptr()->code_source_map_);
       NOT_IN_PRODUCT(s->Push(code->ptr()->return_address_metadata_));
     }
   }
@@ -1621,12 +1621,12 @@ class CodeSerializationCluster : public SerializationCluster {
       s->WriteRef(code->ptr()->exception_handlers_);
       s->WriteRef(code->ptr()->pc_descriptors_);
       s->WriteRef(code->ptr()->stackmaps_);
+      s->WriteRef(code->ptr()->inlined_id_to_function_);
+      s->WriteRef(code->ptr()->code_source_map_);
 
       if (s->kind() == Snapshot::kAppJIT) {
         s->WriteRef(code->ptr()->deopt_info_array_);
         s->WriteRef(code->ptr()->static_calls_target_table_);
-        s->WriteRef(code->ptr()->inlined_id_to_function_);
-        s->WriteRef(code->ptr()->code_source_map_);
         NOT_IN_PRODUCT(s->WriteRef(code->ptr()->return_address_metadata_));
       }
 
@@ -1691,6 +1691,10 @@ class CodeDeserializationCluster : public DeserializationCluster {
       code->ptr()->pc_descriptors_ =
           reinterpret_cast<RawPcDescriptors*>(d->ReadRef());
       code->ptr()->stackmaps_ = reinterpret_cast<RawArray*>(d->ReadRef());
+      code->ptr()->inlined_id_to_function_ =
+          reinterpret_cast<RawArray*>(d->ReadRef());
+      code->ptr()->code_source_map_ =
+          reinterpret_cast<RawCodeSourceMap*>(d->ReadRef());
 
 #if !defined(DART_PRECOMPILED_RUNTIME)
       if (d->kind() == Snapshot::kAppJIT) {
@@ -1698,10 +1702,6 @@ class CodeDeserializationCluster : public DeserializationCluster {
             reinterpret_cast<RawArray*>(d->ReadRef());
         code->ptr()->static_calls_target_table_ =
             reinterpret_cast<RawArray*>(d->ReadRef());
-        code->ptr()->inlined_id_to_function_ =
-            reinterpret_cast<RawArray*>(d->ReadRef());
-        code->ptr()->code_source_map_ =
-            reinterpret_cast<RawCodeSourceMap*>(d->ReadRef());
 #if defined(PRODUCT)
         code->ptr()->return_address_metadata_ = Object::null();
 #else
@@ -1710,8 +1710,6 @@ class CodeDeserializationCluster : public DeserializationCluster {
       } else {
         code->ptr()->deopt_info_array_ = Array::null();
         code->ptr()->static_calls_target_table_ = Array::null();
-        code->ptr()->inlined_id_to_function_ = Array::null();
-        code->ptr()->code_source_map_ = CodeSourceMap::null();
         code->ptr()->return_address_metadata_ = Object::null();
       }
 

runtime/vm/code_descriptors.cc
@@ -315,6 +315,24 @@ RawCodeSourceMap* CodeSourceMapBuilder::Finalize() {
 }
 
 
+void CodeSourceMapBuilder::WriteChangePosition(TokenPosition pos) {
+  stream_.Write<uint8_t>(kChangePosition);
+  if (FLAG_precompiled_mode) {
+    intptr_t line = -1;
+    intptr_t inline_id = buffered_inline_id_stack_.Last();
+    if (inline_id < inline_id_to_function_.length()) {
+      const Function* function = inline_id_to_function_[inline_id];
+      Script& script = Script::Handle(function->script());
+      line = script.GetTokenLineUsingLineStarts(pos);
+    }
+    stream_.Write<int32_t>(static_cast<int32_t>(line));
+  } else {
+    stream_.Write<int32_t>(static_cast<int32_t>(pos.value()));
+  }
+  written_token_pos_stack_.Last() = pos;
+}
+
+
 void CodeSourceMapReader::GetInlinedFunctionsAt(
     int32_t pc_offset,
     GrowableArray<const Function*>* function_stack,

runtime/vm/code_descriptors.h
@@ -182,11 +182,7 @@ class CodeSourceMapBuilder : public ZoneAllocated {
   void BufferChangePosition(TokenPosition pos) {
     buffered_token_pos_stack_.Last() = pos;
   }
-  void WriteChangePosition(TokenPosition pos) {
-    stream_.Write<uint8_t>(kChangePosition);
-    stream_.Write<int32_t>(static_cast<int32_t>(pos.value()));
-    written_token_pos_stack_.Last() = pos;
-  }
+  void WriteChangePosition(TokenPosition pos);
   void BufferAdvancePC(int32_t distance) { buffered_pc_offset_ += distance; }
   void WriteAdvancePC(int32_t distance) {
     stream_.Write<uint8_t>(kAdvancePC);

runtime/vm/flow_graph_compiler.cc
@@ -1040,12 +1040,6 @@ void FlowGraphCompiler::FinalizeStaticCallTargetsTable(const Code& code) {
 
 
 void FlowGraphCompiler::FinalizeCodeSourceMap(const Code& code) {
-  if (FLAG_precompiled_mode) {
-    // TODO(rmacnak): Include a filtered verion of this to produce stack traces
-    // with inlined frames.
-    return;
-  }
-
   const Array& inlined_id_array =
       Array::Handle(zone(), code_source_map_builder_->InliningIdToFunction());
   INC_STAT(Thread::Current(), total_code_size,

commit 019529566e2566f1496f94cc5f724534d916fd2b

Fix RawCode::to() for the precompiled runtime.
Broken by cfe5e5f075431641642a8e3be2fbd4414eaca1fd.

R=asiva@google.com

Review-Url: https://codereview.chromium.org/2711193002 .

runtime/vm/raw_object.h
@@ -1152,7 +1152,7 @@ class RawCode : public RawObject {
   NOT_IN_PRECOMPILED(RawArray* comments_);
   RawObject** to() {
 #if defined(DART_PRECOMPILED_RUNTIME)
-    return reinterpret_cast<RawObject**>(&ptr()->stackmaps_);
+    return reinterpret_cast<RawObject**>(&ptr()->code_source_map_);
 #else
     return reinterpret_cast<RawObject**>(&ptr()->comments_);
 #endif