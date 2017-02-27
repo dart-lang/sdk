fe81f73188803f50770a2c633c9163e36e792fde
Ben Konyi <bkonyi@google.com>
2017-02-27 09:12:37 -0800

    Updated enable-malloc-hooks flag to be disabled by default in debug mode.
    BUG=
    R=zra@google.com
    
    Review-Url: https://codereview.chromium.org/2718963004 .

--- runtime/vm/flag_list.h
+++ runtime/vm/flag_list.h
@@ -25,12 +25,6 @@
 #define USING_MULTICORE false
 #endif
 
-#if defined(DEBUG)
-#define USING_DEBUG true
-#else
-#define USING_DEBUG false
-#endif
-
 // List of all flags in the VM.
 // Flags can be one of three categories:
 // * P roduct flags: Can be set in any of the deployment modes, including in
@@ -74,7 +68,7 @@
     "Dump megamorphic cache statistics")                                       \
   R(dump_symbol_stats, false, bool, false, "Dump symbol table statistics")     \
   R(enable_asserts, false, bool, false, "Enable assert statements.")           \
-  R(enable_malloc_hooks, false, bool, USING_DEBUG,                             \
+  R(enable_malloc_hooks, false, bool, false,                                   \
     "Enable native memory statistic collection. Enabled by default in Debug "  \
     "mode")                                                                    \
   C(enable_mirrors, false, false, bool, true,                                  \
