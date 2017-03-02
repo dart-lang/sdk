c5ad5ac72d168b4b454afc943e7f27fd8e812793
Brian Wilkerson <brianwilkerson@google.com>
2017-03-02 11:01:44 -0800

    Add warning and error markers to some tests
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2721203006 .

--- tests/language_strong/try_catch4_test.dart
+++ tests/language_strong/try_catch4_test.dart
@@ -13,11 +13,11 @@ foo1() {
   var b = false;
   var entered = false;
   while (true) {
-    if (entered) return b;
+    if (entered) return b; /// static warning
     b = 8 == a; // This expression should not be GVN'ed.
     try {
       a = 8;
-      return;
+      return; /// static warning
     } finally {
       b = 8 == a;
       entered = true;
@@ -32,12 +32,12 @@ foo2() {
   var b = false;
   var entered = false;
   while (true) {
-    if (entered) return b;
+    if (entered) return b; /// static warning
     b = 8 == a; // This expression should not be GVN'ed.
     try {
       a = 8;
       doThrow();
-      return;
+      return; /// static warning
     } catch(e) {
       b = 8 == a;
       entered = true;
@@ -50,14 +50,14 @@ foo3() {
   var b = false;
   var entered = false;
   while (true) {
-    if (entered) return b;
+    if (entered) return b; /// static warning
     b = 8 == a; // This expression should not be GVN'ed.
     try {
       doThrow();
     } catch(e) {
       a = 8;
       entered = true;
-      return;
+      return; /// static warning
     } finally {
       b = 8 == a;
       entered = true;
