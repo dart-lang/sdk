dart-lang/sdk

commit c8e275537dd9288a423c3a432d417379a49c6a4a

Surfaced native heap memory allocation information to the Observatory
BUG=
R=johnmccutchan@google.com

Review-Url: https://codereview.chromium.org/2667743004 .

runtime/observatory/lib/src/elements/vm_view.dart
@@ -192,6 +192,28 @@ class VMViewElement extends HtmlElement implements Renderable {
                     ..text = Utils.formatSize(_vm.nativeZoneMemoryUsage)
                     ..title = '${_vm.nativeZoneMemoryUsage} bytes'
                 ],
+              new DivElement()
+                ..classes = ['memberItem']
+                ..children = [
+                  new DivElement()
+                    ..classes = ['memberName']
+                    ..text = 'native heap memory',
+                  new DivElement()
+                    ..classes = ['memberValue']
+                    ..text = Utils.formatSize(_vm.heapAllocatedMemoryUsage)
+                    ..title = '${_vm.heapAllocatedMemoryUsage} bytes'
+                ],
+              new DivElement()
+                ..classes = ['memberItem']
+                ..children = [
+                  new DivElement()
+                    ..classes = ['memberName']
+                    ..text = 'native heap allocation count',
+                  new DivElement()
+                    ..classes = ['memberValue']
+                    ..text = _vm.heapAllocationCount
+                ],
+ 
               new BRElement(),
               new DivElement()
                 ..classes = ['memberItem']

runtime/observatory/lib/src/models/objects/vm.dart
@@ -32,6 +32,12 @@ abstract class VM implements VMRef {
   /// The process id for the VM.
   int get pid;
 
+  /// The current amount of native heap allocated memory within the VM.
+  int get heapAllocatedMemoryUsage;
+
+  /// The current number of allocations on the native heap within the VM.
+  int get heapAllocationCount;
+
   int get maxRSS;
 
   /// The time that the VM started in milliseconds since the epoch.

runtime/observatory/lib/src/service/object.dart
@@ -661,6 +661,8 @@ abstract class VM extends ServiceObjectOwner implements M.VM {
   bool typeChecksEnabled = false;
   int nativeZoneMemoryUsage = 0;
   int pid = 0;
+  int heapAllocatedMemoryUsage = 0;
+  int heapAllocationCount = 0;
   int maxRSS = 0;
   bool profileVM = false;
   DateTime startTime;
@@ -951,6 +953,12 @@ abstract class VM extends ServiceObjectOwner implements M.VM {
       nativeZoneMemoryUsage = map['_nativeZoneMemoryUsage'];
     }
     pid = map['pid'];
+    if (map['_heapAllocatedMemoryUsage'] != null) {
+      heapAllocatedMemoryUsage = map['_heapAllocatedMemoryUsage'];
+    }
+    if (map['_heapAllocationCount'] != null) {
+      heapAllocationCount = map['_heapAllocationCount'];
+    }
     maxRSS = map['_maxRSS'];
     profileVM = map['_profilerMode'] == 'VM';
     assertsEnabled = map['_assertsEnabled'];

runtime/vm/malloc_hooks.cc
@@ -223,6 +223,11 @@ void MallocHooks::ResetStats() {
 }
 
 
+bool MallocHooks::Initialized() {
+  return MallocHooksState::initialized();
+}
+
+
 intptr_t MallocHooks::allocation_count() {
   MutexLocker ml(MallocHooksState::malloc_hook_mutex());
   return MallocHooksState::allocation_count();

runtime/vm/malloc_hooks.h
@@ -14,6 +14,7 @@ class MallocHooks {
   static void InitOnce();
   static void TearDown();
   static void ResetStats();
+  static bool Initialized();
 
   static intptr_t allocation_count();
   static intptr_t heap_allocated_memory_in_bytes();

runtime/vm/malloc_hooks_unsupported.cc
@@ -25,6 +25,11 @@ void MallocHooks::ResetStats() {
 }
 
 
+bool MallocHooks::Initialized() {
+  return false;
+}
+
+
 intptr_t MallocHooks::allocation_count() {
   return 0;
 }

runtime/vm/service.cc
@@ -16,6 +16,7 @@
 #include "vm/debugger.h"
 #include "vm/isolate.h"
 #include "vm/lockers.h"
+#include "vm/malloc_hooks.h"
 #include "vm/message.h"
 #include "vm/message_handler.h"
 #include "vm/native_entry.h"
@@ -3801,6 +3802,11 @@ void Service::PrintJSONForVM(JSONStream* js, bool ref) {
   jsobj.AddProperty64("_maxRSS", OS::MaxRSS());
   jsobj.AddPropertyTimeMillis(
       "startTime", OS::GetCurrentTimeMillis() - Dart::UptimeMillis());
+  if (MallocHooks::Initialized()) {
+    jsobj.AddProperty("_heapAllocatedMemoryUsage",
+                      MallocHooks::heap_allocated_memory_in_bytes());
+    jsobj.AddProperty("_heapAllocationCount", MallocHooks::allocation_count());
+  }
   // Construct the isolate list.
   {
     JSONArray jsarr(&jsobj, "isolates");

commit 1ba39faa3f2c5b1c13774939a925018ef537efbe

Make observatory 'pub serve' work again in Dartium
pub serve in runtime/observatory with Dartium seems to have been broken
since c8e275537dd9288a423c3a432d417379a49c6a4a.

When I try to make observatory connect I just get a blank page.
If I open Dartiums console I get a *long* stacktrace saying
"Caught exception: String Expected".

BUG=
R=kmillikin@google.com

Review-Url: https://codereview.chromium.org/2729773004 .

runtime/observatory/lib/src/elements/vm_view.dart
@@ -211,7 +211,7 @@ class VMViewElement extends HtmlElement implements Renderable {
                     ..text = 'native heap allocation count',
                   new DivElement()
                     ..classes = ['memberValue']
-                    ..text = _vm.heapAllocationCount
+                    ..text = '${_vm.heapAllocationCount}'
                 ],
  
               new BRElement(),