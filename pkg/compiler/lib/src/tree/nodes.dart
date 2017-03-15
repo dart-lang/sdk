6c76f592a6125d51bda4826f28714145d0fe9f22
Stephen Adams <sra@google.com>
2017-03-15 09:56:28 -0700

    Move TreeElementMixin to avoid out-of-library private overrides
    This is one of two solutions for Issue 28809
    https://codereview.chromium.org/2722923002
    https://codereview.chromium.org/2722043002
    
    We should pick one.
    
    R=johnniwinther@google.com, sigmund@google.com
    
    Review-Url: https://codereview.chromium.org/2722923002 .

--- pkg/compiler/lib/src/tree/nodes.dart
+++ pkg/compiler/lib/src/tree/nodes.dart
@@ -6,8 +6,6 @@ import 'dart:collection' show IterableMixin;
 
 import '../common.dart';
 import '../elements/elements.dart' show MetadataAnnotation;
-import '../resolution/secret_tree_element.dart'
-    show NullTreeElementMixin, StoredTreeElementMixin;
 import 'package:front_end/src/fasta/scanner/precedence.dart' as Precedence
     show FUNCTION_INFO;
 import 'package:front_end/src/fasta/scanner.dart' show BeginGroupToken, Token;
@@ -3229,4 +3227,63 @@ class ErrorNode extends Node
   get typeParameters => null;
   get formals => null;
   get typedefKeyword => null;
+}
+
+/**
+ * Encapsulates the field [TreeElementMixin._element].
+ *
+ * This library is an implementation detail of dart2js, and should not
+ * be imported except by resolution and tree node libraries, or for
+ * testing.
+ *
+ * We have taken great care to ensure AST nodes can be cached between
+ * compiler instances.  Part of this requires that we always access
+ * resolution results through TreeElements.
+ *
+ * So please, do not add additional elements to this library, and do
+ * not import it.
+ */
+/// Interface for associating
+abstract class TreeElementMixin {
+  Object get _element;
+  void set _element(Object value);
+}
+
+/// Null implementation of [TreeElementMixin] which does not allow association
+/// of elements.
+///
+/// This class is the superclass of all AST nodes.
+abstract class NullTreeElementMixin implements TreeElementMixin, Spannable {
+  // Deliberately using [Object] here to thwart code completion.
+  // You're not really supposed to access this field anyways.
+  Object get _element => null;
+  set _element(_) {
+    assert(invariant(this, false,
+        message: "Elements cannot be associated with ${runtimeType}."));
+  }
+}
+
+/// Actual implementation of [TreeElementMixin] which stores the associated
+/// element in the private field [_element].
+///
+/// This class is mixed into the node classes that are actually associated with
+/// elements.
+abstract class StoredTreeElementMixin implements TreeElementMixin {
+  Object _element;
+}
+
+/**
+ * Do not call this method directly.  Instead, use an instance of
+ * TreeElements.
+ *
+ * Using [Object] as return type to thwart code completion.
+ */
+Object getTreeElement(TreeElementMixin node) => node._element;
+
+/**
+ * Do not call this method directly.  Instead, use an instance of
+ * TreeElements.
+ */
+void setTreeElement(TreeElementMixin node, Object value) {
+  node._element = value;
 }
