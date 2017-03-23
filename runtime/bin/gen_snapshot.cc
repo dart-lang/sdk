442d177d3d78d362ddc03264869ee1c261eeff05
Ryan Macnak <rmacnak@google.com>
2017-03-22 18:14:49 -0700

    Make gen_snapshot quiet for the Fuchsia build.
    R=zra@google.com
    
    Review-Url: https://codereview.chromium.org/2774523002 .

--- runtime/bin/gen_snapshot.cc
+++ runtime/bin/gen_snapshot.cc
@@ -1471,11 +1471,6 @@ int main(int argc, char** argv) {
 
   if (IsSnapshottingForPrecompilation()) {
     vm_options.AddArgument("--precompilation");
-    vm_options.AddArgument("--print_snapshot_sizes");
-#if TARGET_ARCH_ARM
-    // This is for the iPod Touch 5th Generation (and maybe other older devices)
-    vm_options.AddArgument("--no-use_integer_division");
-#endif
   }
 
   Dart_SetVMFlags(vm_options.count(), vm_options.arguments());
