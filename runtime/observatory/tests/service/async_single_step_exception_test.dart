83735546dbc6ee32909fbb2e86e20365c114822e
John McCutchan <johnmccutchan@google.com>
2017-03-31 13:18:02 -0700

    Fix test failure and add new awaiterFrames to service_dev.md
    BUG=
    
    Review-Url: https://codereview.chromium.org/2788993002 .

--- runtime/observatory/tests/service/async_single_step_exception_test.dart
+++ runtime/observatory/tests/service/async_single_step_exception_test.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 // VMOptions=--error_on_bad_type --error_on_bad_override  --verbose_debug
 
+import 'dart:async';
 import 'dart:developer';
 import 'package:observatory/models.dart' as M;
 import 'package:observatory/service_io.dart';
@@ -10,17 +11,18 @@ import 'package:unittest/unittest.dart';
 import 'service_test_common.dart';
 import 'test_helper.dart';
 
-const LINE_A = 20;
-const LINE_B = 21;
-const LINE_C = 26;
-const LINE_D = 27;
-const LINE_E = 28;
-const LINE_F = 35;
-const LINE_G = 38;
+const LINE_A = 23;
+const LINE_B = 24;
+const LINE_C = 30;
+const LINE_D = 32;
+const LINE_E = 35;
+const LINE_F = 38;
+const LINE_G = 40;
 
 helper() async {
   print('helper'); // LINE_A.
   throw 'a'; // LINE_B.
+  return null;
 }
 
 testMain() async {
@@ -52,7 +54,13 @@ var tests = [
   stoppedAtLine(LINE_B), // throw 'a'.
   stepInto, // exit helper via a throw.
   hasStoppedAtBreakpoint,
+  stepInto, // exit helper via a throw.
+  hasStoppedAtBreakpoint,
+  stepInto, // step once from entry to main.
+  hasStoppedAtBreakpoint,
   stoppedAtLine(LINE_E), // print(error)
+  stepOver,
+  hasStoppedAtBreakpoint,
   stepOver,
   hasStoppedAtBreakpoint,
   stoppedAtLine(LINE_F), // print(foo)
