fb84ff3fc42ee5885a66d9ab080bafbdaf67e9f8
John McCutchan <johnmccutchan@google.com>
2017-04-03 00:06:22 -0700

    Revert "Disable awaiter stepping by default until context level computation is fixed"
    This reverts commit 61e11c6808e3fd87c574f4277b3d9ff745dc85f0.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2791953002 .

--- runtime/vm/debugger.cc
+++ runtime/vm/debugger.cc
@@ -1974,7 +1974,7 @@ DebuggerStackTrace* Debugger::CollectAsyncCausalStackTrace() {
 
 
 DebuggerStackTrace* Debugger::CollectAwaiterReturnStackTrace() {
-  if (!FLAG_async_debugger_stepping) {
+  if (!FLAG_causal_async_stacks) {
     return NULL;
   }
   // Causal async stacks are not supported in the AOT runtime.
