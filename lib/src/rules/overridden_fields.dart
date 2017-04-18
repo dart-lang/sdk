19ce2f136b308951e984390beb665ed752a5d4de
Konstantin Shcheglov <scheglov@google.com>
2017-04-18 11:54:59 -0700

    Use Identifier.isPrivateName() to avoid exception on empty names.
    I don't know how to reproduce this, and the user has not sent the
    crash log yet, but I saw this in the wild.
    
    R=pquitslund@google.com
    BUG=
    
    Review-Url: https://codereview.chromium.org//2826723002 .

--- lib/src/rules/overridden_fields.dart
+++ lib/src/rules/overridden_fields.dart
@@ -120,7 +120,7 @@ class _Visitor extends SimpleAstVisitor {
     bool isOverriddenMember(PropertyAccessorElement a) {
       if (a.isSynthetic && a.name == memberName) {
         // Ensure that private members are overriding a member of the same library.
-        if (memberName[0] == '_') {
+        if (Identifier.isPrivateName(memberName)) {
           return library == a.library;
         }
         return true;
