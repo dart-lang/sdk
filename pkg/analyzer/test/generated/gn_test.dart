bfdf42a8e00c11f9567831b9b68d95cb9d1a45b5
P.Y. Laligand <py.laligand@gmail.com>
2017-04-18 10:41:29 -0700

    GN workspaces now use packages files. (#29364)
    The previous version would use a global cache of package locations which would hide missing dependencies.
    Also fixed an issue in file tracking with non-existing sources that resolvers such as PackageMapUriResolver sometimes return.

--- pkg/analyzer/test/generated/gn_test.dart
+++ pkg/analyzer/test/generated/gn_test.dart
@@ -12,92 +12,8 @@ import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 main() {
   defineReflectiveSuite(() {
-    defineReflectiveTests(GnPackageUriResolverTest);
     defineReflectiveTests(GnWorkspaceTest);
   });
-}
-
-@reflectiveTest
-class GnPackageUriResolverTest extends _BaseTest {
-  GnWorkspace workspace;
-  GnPackageUriResolver resolver;
-
-  void test_resolve() {
-    _addResources([
-      '/workspace/.jiri_root/',
-      '/workspace/out/debug-x87_128/gen/dart.sources/',
-      '/workspace/some/code/',
-      '/workspace/a/source/code.dart',
-    ]);
-    provider.newFile(
-        _p('/workspace/out/debug-x87_128/gen/dart.sources/flutter'),
-        _p('/workspace/a/source'));
-    _setUp();
-    _assertResolve(
-        'package:flutter/code.dart', _p('/workspace/a/source/code.dart'));
-  }
-
-  void test_resolveDoesNotExist() {
-    _addResources([
-      '/workspace/.jiri_root/',
-      '/workspace/out/debug-x87_128/gen/dart.sources/',
-      '/workspace/some/code/',
-      '/workspace/a/source/code.dart',
-    ]);
-    provider.newFile(
-        _p('/workspace/out/debug-x87_128/gen/dart.sources/flutter'),
-        _p('/workspace/a/source'));
-    _setUp();
-    expect(
-        resolver.resolveAbsolute(Uri.parse('package:bogus/code.dart')), null);
-  }
-
-  void test_resolveAddToCache() {
-    _addResources([
-      '/workspace/.jiri_root/',
-      '/workspace/out/debug-x87_128/gen/dart.sources/',
-      '/workspace/some/code/',
-      '/workspace/a/source/code.dart',
-    ]);
-    _setUp();
-    expect(
-        resolver.resolveAbsolute(Uri.parse('package:flutter/code.dart')), null);
-    provider.newFile(
-        _p('/workspace/out/debug-x87_128/gen/dart.sources/flutter'),
-        _p('/workspace/a/source'));
-    _assertResolve(
-        'package:flutter/code.dart', _p('/workspace/a/source/code.dart'));
-  }
-
-  void _addResources(List<String> paths) {
-    for (String path in paths) {
-      if (path.endsWith('/')) {
-        provider.newFolder(_p(path.substring(0, path.length - 1)));
-      } else {
-        provider.newFile(_p(path), '');
-      }
-    }
-  }
-
-  void _setUp() {
-    workspace = GnWorkspace.find(provider, _p('/workspace'));
-    resolver = new GnPackageUriResolver(workspace);
-  }
-
-  void _assertResolve(String uriStr, String posixPath,
-      {bool exists: true, bool restore: true}) {
-    Uri uri = Uri.parse(uriStr);
-    Source source = resolver.resolveAbsolute(uri);
-    expect(source, isNotNull);
-    expect(source.fullName, _p(posixPath));
-    expect(source.uri, uri);
-    expect(source.exists(), exists);
-    // If enabled, test also "restoreAbsolute".
-    if (restore) {
-      Uri uri = resolver.restoreAbsolute(source);
-      expect(uri.toString(), uriStr);
-    }
-  }
 }
 
 @reflectiveTest
@@ -115,27 +31,43 @@ class GnWorkspaceTest extends _BaseTest {
 
   void test_find_withRoot() {
     provider.newFolder(_p('/workspace/.jiri_root'));
-    provider.newFolder(_p('/workspace/out/debug-x87_128/gen/dart.sources'));
     provider.newFolder(_p('/workspace/some/code'));
+    provider.newFile(_p('/workspace/some/code/pubspec.yaml'), '');
+    provider.newFile(
+        _p('/workspace/out/debug-x87_128/gen/some/code/foo.packages'), '');
     GnWorkspace workspace =
         GnWorkspace.find(provider, _p('/workspace/some/code'));
     expect(workspace, isNotNull);
-    expect(workspace.root, _p('/workspace'));
+    expect(workspace.root, _p('/workspace/some/code'));
+  }
+
+  void test_find_inHost() {
+    provider.newFolder(_p('/workspace/.jiri_root'));
+    provider.newFolder(_p('/workspace/some/code'));
+    provider.newFile(_p('/workspace/some/code/pubspec.yaml'), '');
+    provider.newFile(
+        _p('/workspace/out/debug-x87_128/host_y32/gen/some/code/foo.packages'),
+        '');
+    GnWorkspace workspace =
+        GnWorkspace.find(provider, _p('/workspace/some/code'));
+    expect(workspace, isNotNull);
+    expect(workspace.root, _p('/workspace/some/code'));
   }
 
   void test_packages() {
     provider.newFolder(_p('/workspace/.jiri_root'));
-    provider.newFolder(_p('/workspace/out/debug-x87_128/gen/dart.sources'));
-    provider.newFile(
-        _p('/workspace/out/debug-x87_128/gen/dart.sources/flutter'),
-        _p('/path/to/source'));
     provider.newFolder(_p('/workspace/some/code'));
+    provider.newFile(_p('/workspace/some/code/pubspec.yaml'), '');
+    String packageLocation = _p('/workspace/this/is/the/package');
+    provider.newFile(
+        _p('/workspace/out/debug-x87_128/gen/some/code/foo.packages'),
+        'flutter:$packageLocation');
     GnWorkspace workspace =
         GnWorkspace.find(provider, _p('/workspace/some/code'));
     expect(workspace, isNotNull);
-    expect(workspace.root, _p('/workspace'));
+    expect(workspace.root, _p('/workspace/some/code'));
     expect(workspace.packageMap.length, 1);
-    expect(workspace.packageMap['flutter'][0].path, _p('/path/to/source'));
+    expect(workspace.packageMap['flutter'][0].path, packageLocation);
   }
 }
 
