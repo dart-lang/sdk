550b7ddcec74e390fe95cee022ed9867c8401b7c
Juan Pablo Paulsen <casanueva@google.com>
2017-04-26 12:49:59 -0700

    New rule: prefer_foreach_call

---
+++ lib/src/rules/prefer_foreach.dart
@@ -0,0 +1,129 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library linter.src.rules.prefer_foreach_call;
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:linter/src/analyzer.dart';
+import 'package:linter/src/util/dart_type_utilities.dart';
+
+const _desc = r'Use forEach call to only apply a function to all the elements.';
+
+const _details = r'''
+
+**DO** use forEach call if you are only going to apply a function or a method to all the elements of an iterable.
+
+**BAD:**
+```
+for (final key in map.keys.toList()) {
+  map.remove(key);
+}
+```
+
+**GOOD:**
+```
+map.keys.toList().forEach(map.remove);
+```
+
+**NOTE:** Replacing a for each statement with a forEach call may change the behavior in the case there are side-effects on the iterable itself.
+```
+for (final v in myList) {
+  foo().f(v); // This code invokes foo() many times.
+}
+
+myList.forEach(foo().f); // But this one invokes foo() just once.
+```
+
+''';
+
+class PreferForeach extends LintRule {
+  _Visitor _visitor;
+  PreferForeach()
+      : super(
+            name: 'prefer_foreach',
+            description: _desc,
+            details: _details,
+            group: Group.style,
+            maturity: Maturity.experimental) {
+    _visitor = new _Visitor(this);
+  }
+
+  @override
+  AstVisitor getVisitor() => _visitor;
+}
+
+class _PreferForEachVisitor extends SimpleAstVisitor {
+  final LintRule rule;
+  LocalVariableElement element;
+  ForEachStatement forEachStatement;
+
+  _PreferForEachVisitor(this.rule);
+
+  @override
+  visitBlock(Block node) {
+    if (node.statements.length == 1) {
+      node.statements.first.accept(this);
+    }
+  }
+
+  @override
+  visitExpressionStatement(ExpressionStatement node) {
+    node.expression.accept(this);
+  }
+
+  @override
+  visitForEachStatement(ForEachStatement node) {
+    final element = node.loopVariable?.element;
+    if (element != null) {
+      forEachStatement = node;
+      this.element = element;
+      node.body.accept(this);
+    }
+  }
+
+  @override
+  visitFunctionExpressionInvocation(FunctionExpressionInvocation node) {
+    final arguments = node.argumentList.arguments;
+    if (arguments.length == 1 &&
+        DartTypeUtilities.getCanonicalElementFromIdentifier(arguments.first) ==
+            element) {
+      rule.reportLint(forEachStatement);
+    }
+  }
+
+  @override
+  visitMethodInvocation(MethodInvocation node) {
+    final arguments = node.argumentList.arguments;
+    if (arguments.length == 1 &&
+        DartTypeUtilities.getCanonicalElementFromIdentifier(arguments.first) ==
+            element &&
+        (node.target == null ||
+            (DartTypeUtilities.getCanonicalElementFromIdentifier(node.target) !=
+                    element &&
+                !DartTypeUtilities
+                    .traverseNodesInDFS(node.target)
+                    .map(DartTypeUtilities.getCanonicalElementFromIdentifier)
+                    .contains(element)))) {
+      rule.reportLint(forEachStatement);
+    }
+  }
+
+  @override
+  visitParenthesizedExpression(ParenthesizedExpression node) {
+    node.unParenthesized.accept(this);
+  }
+}
+
+class _Visitor extends SimpleAstVisitor {
+  final LintRule rule;
+  _Visitor(this.rule);
+
+  @override
+  visitForEachStatement(ForEachStatement node) {
+    final visitor = new _PreferForEachVisitor(rule);
+    node.accept(visitor);
+  }
+}
---
+++ test/rules/prefer_foreach.dart
@@ -0,0 +1,63 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// test w/ `pub run test -N prefer_foreach`
+
+void f(Object o) {}
+
+void foo() {
+  final myList = [];
+  for (final a in myList) { // LINT
+    f(a);
+  }
+}
+
+class WithMethods {
+  void f(Object o) {}
+
+  void foo() {
+    final myList = [];
+    for (final a in myList) { // LINT
+      f(a);
+    }
+  }
+}
+
+class WithThirdPartyMethods {
+  WithMethods x;
+
+  void foo() {
+    final myList = [];
+    for (final a in myList) { // LINT
+      x.f(a);
+    }
+  }
+}
+
+class WithElementInTarget {
+  List<WithMethods> myList;
+
+  void good() {
+    for (final x in myList) { // OK because x is the target
+      x.f(x);
+    }
+  }
+
+  void good2() {
+    for (final x in myList) { // OK because x is in the target
+      myList[myList.indexOf(x)].f(x);
+    }
+  }
+}
+
+class WithStaticMethods {
+  static void f(Object o) {}
+
+  void foo() {
+    final myList = [];
+    for (final a in myList) { // LINT
+      WithStaticMethods.f(a);
+    }
+  }
+}
