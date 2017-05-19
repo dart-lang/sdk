80fd95bb7020418b0989864d23884006c15adb18
Brian Wilkerson <brianwilkerson@google.com>
2017-05-19 13:07:58 -0700

    Fix parser bug found while trying to reproduce a different bug
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2895743002 .

--- pkg/analyzer/lib/src/generated/parser.dart
+++ pkg/analyzer/lib/src/generated/parser.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library analyzer.src.generated.parser;
-
 import 'dart:collection';
 import "dart:math" as math;
 
@@ -5517,7 +5515,7 @@ class Parser {
     if (!_tokenMatches(startToken, TokenType.OPEN_PAREN)) {
       return null;
     }
-    return (startToken as BeginToken).endToken.next;
+    return (startToken as BeginToken).endToken?.next;
   }
 
   /**
