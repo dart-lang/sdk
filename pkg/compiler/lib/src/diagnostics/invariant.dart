641767c1da670788b04f1019ba627642b3f4e359
Stephen Adams <sra@google.com>
2017-05-23 08:25:51 -0700

    dart2js: replace 'invariant' with 'failedAt' to defer interpolation
    Speeds up dart2js_builds by ~5% (probably more with more replacements).
    
    BUG=
    R=johnniwinther@google.com
    
    Review-Url: https://codereview.chromium.org/2902633002 .

--- pkg/compiler/lib/src/diagnostics/invariant.dart
+++ pkg/compiler/lib/src/diagnostics/invariant.dart
@@ -44,7 +44,7 @@ bool invariant(Spannable spannable, var condition, {var message: null}) {
   // information on assertion errors.
   if (spannable == null) {
     throw new SpannableAssertionFailure(CURRENT_ELEMENT_SPANNABLE,
-        "Spannable was null for invariant. Use CURRENT_ELEMENT_SPANNABLE.");
+        'Spannable was null for invariant. Use CURRENT_ELEMENT_SPANNABLE.');
   }
   if (condition is Function) {
     condition = condition();
@@ -60,6 +60,30 @@ bool invariant(Spannable spannable, var condition, {var message: null}) {
     throw new SpannableAssertionFailure(spannable, message);
   }
   return true;
+}
+
+/// Throws a [SpannableAssertionFailure].
+///
+/// Use this method to provide better information for assertion by calling
+/// [failedAt] as the second argument to an `assert` statement:
+///
+///     assert(condition, failedAt(position, message));
+///
+/// [spannable] must be non-null and will be used to provide positional
+/// information in the generated error message.
+bool failedAt(Spannable spannable, [var message]) {
+  // TODO(johnniwinther): Use [spannable] and [message] to provide better
+  // information on assertion errors.
+  if (spannable == null) {
+    throw new SpannableAssertionFailure(
+        CURRENT_ELEMENT_SPANNABLE,
+        'Spannable was null for failedInvariant. '
+        'Use CURRENT_ELEMENT_SPANNABLE.');
+  }
+  if (message is Function) {
+    message = message();
+  }
+  throw new SpannableAssertionFailure(spannable, message);
 }
 
 typedef void InternalErrorFunction(Spannable location, String message);
