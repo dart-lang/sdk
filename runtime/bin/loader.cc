dart-lang/sdk

commit 99c6a18a1bc2a4f13d8dd027a597fdca0f2d6e0c

Attempt 2 -- Fix updating packages map on reload and add a regression test
- [x] Update the isolate's .packages path when requested.
- [x] Always refresh package map when loader is initialized.
- [x] Add regression test.

Review-Url: https://codereview.chromium.org/2810223003 .

runtime/bin/loader.cc
@@ -652,7 +652,15 @@ Dart_Handle Loader::LibraryTagHandler(Dart_LibraryTag tag,
   IsolateData* isolate_data =
       reinterpret_cast<IsolateData*>(Dart_CurrentIsolateData());
   ASSERT(isolate_data != NULL);
-
+  if ((tag == Dart_kScriptTag) && Dart_IsString(library)) {
+    // Update packages file for isolate.
+    const char* packages_file = NULL;
+    Dart_Handle result = Dart_StringToCString(library, &packages_file);
+    if (Dart_IsError(result)) {
+      return result;
+    }
+    isolate_data->UpdatePackagesFile(packages_file);
+  }
   // Grab this isolate's loader.
   Loader* loader = NULL;
 

runtime/bin/vmservice/loader.dart
@@ -90,9 +90,11 @@ class IsolateLoaderState extends IsolateEmbedderData {
     if (packagesConfigFlag != null) {
       _setPackagesConfig(packagesConfigFlag);
     }
-    if (_fileRequestQueue == null) {
-      _fileRequestQueue = new List<FileRequest>();
-    }
+  }
+
+  void updatePackageMap(String packagesConfigFlag) {
+    _packageMap = null;
+    _setPackagesConfig(packagesConfigFlag);
   }
 
   void cleanup() {
@@ -134,7 +136,7 @@ class IsolateLoaderState extends IsolateEmbedderData {
   // _fileRequestQueue and are processed when we can safely issue them.
   static const int _maxFileRequests = 16;
   int currentFileRequests = 0;
-  List<FileRequest> _fileRequestQueue;
+  final List<FileRequest> _fileRequestQueue = new List<FileRequest>();
 
   bool get shouldIssueFileRequest => currentFileRequests < _maxFileRequests;
   void enqueueFileRequest(FileRequest fr) {
@@ -1034,7 +1036,9 @@ _processLoadRequest(request) {
           loaderState = new IsolateLoaderState(isolateId);
           isolateEmbedderData[isolateId] = loaderState;
           loaderState.init(
-              packageRoot, packagesFile, workingDirectory, rootScript);
+            packageRoot, packagesFile, workingDirectory, rootScript);
+        } else {
+          loaderState.updatePackageMap(packagesFile);
         }
         loaderState.sp = sp;
         assert(isolateEmbedderData[isolateId] == loaderState);

runtime/bin/isolate_data.h
@@ -53,6 +53,14 @@ class IsolateData {
   char* packages_file;
   uint8_t* udp_receive_buffer;
 
+  void UpdatePackagesFile(const char* packages_file_) {
+    if (packages_file != NULL) {
+      free(packages_file);
+      packages_file = NULL;
+    }
+    packages_file = strdup(packages_file_);
+  }
+
   // While loading a loader is associated with the isolate.
   bool HasLoader() const { return loader_ != NULL; }
   Loader* loader() const {

runtime/observatory/lib/src/service/object.dart
@@ -1356,10 +1356,14 @@ class Isolate extends ServiceObjectOwner implements M.Isolate {
     return invokeRpc('getSourceReport', params);
   }
 
-  Future<ServiceMap> reloadSources({String rootLibUri, bool pause}) {
+  Future<ServiceMap> reloadSources(
+      {String rootLibUri, String packagesUri, bool pause}) {
     Map<String, dynamic> params = <String, dynamic>{};
     if (rootLibUri != null) {
       params['rootLibUri'] = rootLibUri;
+    }
+    if (packagesUri != null) {
+      params['packagesUri'] = packagesUri;
     }
     if (pause != null) {
       params['pause'] = pause;

runtime/observatory/tests/service/complex_reload/v2/.gitignore
@@ -1,0 +1,1 @@
+!packages

runtime/observatory/tests/service/complex_reload/v2/foobar_lib/foo.dart
@@ -1,0 +1,1 @@
+fooLib() => 'fooLib';

runtime/observatory/tests/service/complex_reload/v2/main.dart
@@ -3,4 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 // VMOptions=--error_on_bad_type --error_on_bad_override
 
-test() => 'banana';
+import 'package:foobar/foo.dart';
+
+test() => fooLib();

runtime/observatory/tests/service/complex_reload/v2/packages
@@ -1,0 +1,1 @@
+foobar:foobar_lib/

commit 13f5616ef6012d01a2fa1c008241ea95c05bb57f

VM: Fix snapshoting slow-down caused by 99c6a18a1bc2a4f13d8dd027a597fdca0f2d6e0c.
Only reload packages map when we are actually doing the isolate reload.

Snapshotting creates and destroys and loader object every time it loads a dart
file - which means kInitLoader is on the hot path and we should avoid rereading
packages map.

R=asiva@google.com, rmacnak@google.com

Review-Url: https://codereview.chromium.org/2901093002 .

runtime/bin/loader.cc
@@ -140,7 +140,7 @@ void Loader::Init(const char* package_root,
   // Keep in sync with loader.dart.
   const intptr_t _Dart_kInitLoader = 4;
 
-  Dart_Handle request = Dart_NewList(8);
+  Dart_Handle request = Dart_NewList(9);
   Dart_ListSetAt(request, 0, trace_loader ? Dart_True() : Dart_False());
   Dart_ListSetAt(request, 1, Dart_NewInteger(Dart_GetMainPortId()));
   Dart_ListSetAt(request, 2, Dart_NewInteger(_Dart_kInitLoader));
@@ -155,6 +155,8 @@ void Loader::Init(const char* package_root,
   Dart_ListSetAt(request, 7, (root_script_uri == NULL)
                                  ? Dart_Null()
                                  : Dart_NewStringFromCString(root_script_uri));
+  Dart_ListSetAt(request, 8, Dart_NewBoolean(Dart_IsReloading()));
+
 
   bool success = Dart_Post(loader_port, request);
   ASSERT(success);

runtime/bin/vmservice/loader.dart
@@ -1035,12 +1035,13 @@ _processLoadRequest(request) {
         String packagesFile = request[5];
         String workingDirectory = request[6];
         String rootScript = request[7];
+        bool isReloading = request[8];
         if (loaderState == null) {
           loaderState = new IsolateLoaderState(isolateId);
           isolateEmbedderData[isolateId] = loaderState;
           loaderState.init(
               packageRoot, packagesFile, workingDirectory, rootScript);
-        } else {
+        } else if (isReloading) {
           loaderState.updatePackageMap(packagesFile);
         }
         loaderState.sp = sp;

runtime/include/dart_tools_api.h
@@ -906,6 +906,11 @@ typedef bool (*Dart_FileModifiedCallback)(const char* url, int64_t since);
 DART_EXPORT Dart_Handle
 Dart_SetFileModifiedCallback(Dart_FileModifiedCallback file_modified_callback);
 
+/**
+ * Returns true if isolate is currently reloading.
+ */
+DART_EXPORT bool Dart_IsReloading();
+
 /*
  * ========
  * Timeline

runtime/vm/dart_api_impl.cc
@@ -6129,6 +6129,11 @@ Dart_SetFileModifiedCallback(Dart_FileModifiedCallback file_mod_callback) {
 }
 
 
+DART_EXPORT bool Dart_IsReloading() {
+  return false;
+}
+
+
 DART_EXPORT void Dart_GlobalTimelineSetRecordedStreams(int64_t stream_mask) {
   return;
 }
@@ -6261,6 +6266,14 @@ Dart_SetFileModifiedCallback(Dart_FileModifiedCallback file_modified_callback) {
   }
   IsolateReloadContext::SetFileModifiedCallback(file_modified_callback);
   return Api::Success();
+}
+
+
+DART_EXPORT bool Dart_IsReloading() {
+  Thread* thread = Thread::Current();
+  Isolate* isolate = thread->isolate();
+  CHECK_ISOLATE(isolate);
+  return isolate->IsReloading();
 }
 
 