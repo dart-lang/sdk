f1472ed8bb3d7f2cb5fb50e0f6b96b66afa877db
Johnni Winther <johnniwinther@google.com>
2017-06-01 11:32:55 +0200

    Fix gardening/bot help messages
    R=floitsch@google.com
    
    Review-Url: https://codereview.chromium.org/2919693003 .

--- tools/gardening/bin/bot.dart
+++ tools/gardening/bin/bot.dart
@@ -1,39 +1,47 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
 import 'dart:io';
 
 import 'compare_failures.dart' as compare_failures;
 import 'current_summary.dart' as current_summary;
 import 'status_summary.dart' as status_summary;
 
-void help(List<String> args) {
+typedef Future MainFunction(List<String> args);
+
+help(List<String> args) async {
   if (args.length == 1 && args[0] == "--help") {
     print("This help");
-    return;
+    return null;
   }
 
   print("A script that combines multiple commands:\n");
 
-  commands.forEach((command, fun) {
-    print("$command:");
-    fun(["--help"]);
+  for (String command in commands.keys) {
+    print(command);
+    print('-' * command.length);
+    await commands[command](["--help"]);
     print("");
-  });
+  }
 }
 
-const commands = const {
+const Map<String, MainFunction> commands = const <String, MainFunction>{
   "help": help,
   "compare-failures": compare_failures.main,
   "current-summary": current_summary.main,
   "status-summary": status_summary.main,
 };
 
-void main(List<String> args) {
+main(List<String> args) async {
   if (args.isEmpty) {
-    help([]);
+    await help([]);
     exit(-1);
   }
   var command = commands[args[0]];
   if (command == null) {
-    help([]);
+    await help([]);
     exit(-1);
   }
   command(args.sublist(1));
