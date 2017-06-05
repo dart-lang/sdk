db7d3d0fe73bd1c84c0040418447c92a6b56c901
Brian Wilkerson <brianwilkerson@google.com>
2017-06-05 10:10:55 -0700

    Move relevance constants for use in plugins
    R=danrubel@google.com
    
    Review-Url: https://codereview.chromium.org/2923573002 .

--- pkg/analysis_server/lib/src/provisional/completion/dart/completion_dart.dart
+++ pkg/analysis_server/lib/src/provisional/completion/dart/completion_dart.dart
@@ -15,24 +15,7 @@ import 'package:analyzer_plugin/protocol/protocol_common.dart';
 
 export 'package:analysis_server/src/provisional/completion/completion_core.dart'
     show EMPTY_LIST;
-
-const int DART_RELEVANCE_COMMON_USAGE = 1200;
-const int DART_RELEVANCE_DEFAULT = 1000;
-const int DART_RELEVANCE_HIGH = 2000;
-const int DART_RELEVANCE_INCREMENT = 100;
-const int DART_RELEVANCE_INHERITED_ACCESSOR = 1057;
-const int DART_RELEVANCE_INHERITED_FIELD = 1058;
-const int DART_RELEVANCE_INHERITED_METHOD = 1057;
-const int DART_RELEVANCE_KEYWORD = 1055;
-const int DART_RELEVANCE_LOCAL_ACCESSOR = 1057;
-const int DART_RELEVANCE_LOCAL_FIELD = 1058;
-const int DART_RELEVANCE_LOCAL_FUNCTION = 1056;
-const int DART_RELEVANCE_LOCAL_METHOD = 1057;
-const int DART_RELEVANCE_LOCAL_TOP_LEVEL_VARIABLE = 1056;
-const int DART_RELEVANCE_LOCAL_VARIABLE = 1059;
-const int DART_RELEVANCE_LOW = 500;
-const int DART_RELEVANCE_NAMED_PARAMETER = 1060;
-const int DART_RELEVANCE_PARAMETER = 1059;
+export 'package:analyzer_plugin/utilities/completion/relevance.dart';
 
 /**
  * An object used to instantiate a [DartCompletionContributor] instance
