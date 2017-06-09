a8c2d1adbab6189035402ea5ec0608cf2b7695fa
Florian Loitsch <floitsch@google.com>
2017-06-09 21:33:17 +0200

    Add library tag to common_usage_sorter.dart.
    Review-Url: https://codereview.chromium.org/2931163002 .

--- pkg/analysis_server/lib/src/services/completion/dart/common_usage_sorter.dart
+++ pkg/analysis_server/lib/src/services/completion/dart/common_usage_sorter.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library services.completion.dart.sorter.common;
+
 import 'dart:async';
 
 import 'package:analysis_server/src/protocol_server.dart' as protocol;
