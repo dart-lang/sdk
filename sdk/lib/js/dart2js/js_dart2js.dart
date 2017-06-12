dart-lang/sdk

commit 043c0096adeec45c0b7c544ce717956e1dd9942b

Complain about use before declaration.
R=johnniwinther@google.com

Review-Url: https://codereview.chromium.org/2924423003 .

sdk/lib/js/dart2js/js_dart2js.dart
@@ -500,7 +500,7 @@ class JsArray<E> extends JsObject with ListMixin<E> {
 
   void setRange(int start, int end, Iterable<E> iterable, [int skipCount = 0]) {
     _checkRange(start, end, length);
-    int length = end - start;
+    length = end - start;
     if (length == 0) return;
     if (skipCount < 0) throw new ArgumentError(skipCount);
     var args = [start, length]..addAll(iterable.skip(skipCount).take(length));

pkg/front_end/lib/src/fasta/scope.dart
@@ -31,6 +31,8 @@ class Scope extends MutableScope {
 
   Map<String, Builder> forwardDeclaredLabels;
 
+  Map<String, int> usedNames;
+
   Scope(Map<String, Builder> local, Map<String, Builder> setters, Scope parent,
       {this.isModifiable: true})
       : super(local, setters = setters ?? const <String, Builder>{}, parent);
@@ -89,6 +91,13 @@ class Scope extends MutableScope {
     return new Scope(local, setters, parent, isModifiable: true);
   }
 
+  void recordUse(String name, int charOffset, Uri fileUri) {
+    if (isModifiable) {
+      usedNames ??= <String, int>{};
+      usedNames.putIfAbsent(name, () => charOffset);
+    }
+  }
+
   Builder lookupIn(String name, int charOffset, Uri fileUri,
       Map<String, Builder> map, bool isInstanceScope) {
     Builder builder = map[name];
@@ -104,6 +113,7 @@ class Scope extends MutableScope {
 
   Builder lookup(String name, int charOffset, Uri fileUri,
       {bool isInstanceScope: true}) {
+    recordUse(name, charOffset, fileUri);
     Builder builder =
         lookupIn(name, charOffset, fileUri, local, isInstanceScope);
     if (builder != null) return builder;
@@ -120,6 +130,7 @@ class Scope extends MutableScope {
 
   Builder lookupSetter(String name, int charOffset, Uri fileUri,
       {bool isInstanceScope: true}) {
+    recordUse(name, charOffset, fileUri);
     Builder builder =
         lookupIn(name, charOffset, fileUri, setters, isInstanceScope);
     if (builder != null) return builder;
@@ -175,11 +186,15 @@ class Scope extends MutableScope {
   InputError declare(
       String name, Builder builder, int charOffset, Uri fileUri) {
     if (isModifiable) {
+      if (usedNames?.containsKey(name) ?? false) {
+        return new InputError(
+            fileUri, usedNames[name], "Previous use of '$name'.");
+      }
+      recordUse(name, charOffset, fileUri);
       local[name] = builder;
     } else {
       internalError("Can't extend an unmodifiable scope.");
     }
-    // TODO(ahe): Return an error if [name] was already used in this scope.
     return null;
   }
 

pkg/front_end/test/fasta/strong.status
@@ -6,6 +6,8 @@
 # Kernel ASTs directly, that is, code in pkg/fasta/lib/src/kernel/ with
 # strong-mode enabled.
 
+inference/infer_local_function_return_type: RuntimeError
+
 DeltaBlue: Fail
 accessors: Fail
 arithmetic: Fail

pkg/front_end/testcases/inference/infer_local_function_return_type.dart.strong.expect
@@ -1,115 +1,5 @@
-library test;
+library;
 import self as self;
-import "dart:async" as asy;
-import "dart:core" as core;
 
-static method main() → dynamic {
-  function f0() → dynamic
-    return 42;
-  function f1() → dynamic /* originally async */ {
-    final asy::Completer<asy::FutureOr<dynamic>> :completer = asy::Completer::sync<asy::FutureOr<dynamic>>();
-    asy::FutureOr<dynamic> :return_value;
-    dynamic :async_op_then;
-    dynamic :async_op_error;
-    dynamic :await_jump_var = 0;
-    dynamic :await_ctx_var;
-    function :async_op([dynamic :result, dynamic :exception, dynamic :stack_trace]) → dynamic yielding 
-      try {
-        #L1:
-        {
-          :return_value = 42;
-          break #L1;
-        }
-        :completer.complete(:return_value);
-        return;
-      }
-      on dynamic catch(dynamic :exception, dynamic :stack_trace) {
-        :completer.completeError(:exception, :stack_trace);
-      }
-    :async_op_then = asy::_asyncThenWrapperHelper(:async_op);
-    :async_op_error = asy::_asyncErrorWrapperHelper(:async_op);
-    asy::Future::microtask<dynamic>(:async_op);
-    return :completer.future;
-  }
-  function f2() → dynamic {
-    return 42;
-  }
-  function f3() → dynamic /* originally async */ {
-    final asy::Completer<asy::FutureOr<dynamic>> :completer = asy::Completer::sync<asy::FutureOr<dynamic>>();
-    asy::FutureOr<dynamic> :return_value;
-    dynamic :async_op_then;
-    dynamic :async_op_error;
-    dynamic :await_jump_var = 0;
-    dynamic :await_ctx_var;
-    function :async_op([dynamic :result, dynamic :exception, dynamic :stack_trace]) → dynamic yielding 
-      try {
-        #L2:
-        {
-          :return_value = 42;
-          break #L2;
-        }
-        :completer.complete(:return_value);
-        return;
-      }
-      on dynamic catch(dynamic :exception, dynamic :stack_trace) {
-        :completer.completeError(:exception, :stack_trace);
-      }
-    :async_op_then = asy::_asyncThenWrapperHelper(:async_op);
-    :async_op_error = asy::_asyncErrorWrapperHelper(:async_op);
-    asy::Future::microtask<dynamic>(:async_op);
-    return :completer.future;
-  }
-  function f4() → dynamic /* originally sync* */ {
-    dynamic :await_jump_var = 0;
-    dynamic :await_ctx_var;
-    function :sync_op(core::Iterator<dynamic> :iterator) → core::bool yielding {
-      {
-        {
-          :iterator._current = 42;
-          [yield] true;
-        }
-      }
-      return false;
-    }
-    return new core::_SyncIterable::•(:sync_op);
-  }
-  function f5() → dynamic /* originally async* */ {
-    dynamic :controller;
-    dynamic :async_op_then;
-    dynamic :async_op_error;
-    dynamic :await_jump_var = 0;
-    dynamic :await_ctx_var;
-    dynamic :saved_try_context_var0;
-    dynamic :saved_try_context_var1;
-    function :async_op([dynamic :result, dynamic :exception, dynamic :stack_trace]) → dynamic yielding 
-      try
-        try {
-          #L3:
-          {
-            if(:controller.add(42))
-              return null;
-            else
-              [yield] null;
-          }
-          return;
-        }
-        on dynamic catch(dynamic :exception, dynamic :stack_trace) {
-          :controller.addError(:exception, :stack_trace);
-        }
-      finally {
-        :controller.close();
-      }
-    :async_op_then = asy::_asyncThenWrapperHelper(:async_op);
-    :async_op_error = asy::_asyncErrorWrapperHelper(:async_op);
-    :controller = new asy::_AsyncStarStreamController::•(:async_op);
-    return :controller.stream;
-  }
-  function f6() → core::num
-    return 42;
-  function f7() → dynamic
-    return f7.call();
-  function f8() → dynamic
-    return throw new core::NoSuchMethodError::_withType(null, #f9, 32, <dynamic>[].toList(growable: false), <dynamic, dynamic>{}, null);
-  function f9() → dynamic
-    return f5.call();
-}
+static method main() → dynamic
+  throw "pkg/front_end/testcases/inference/infer_local_function_return_type.dart:32:3: Error: Can't declare 'f9' because it was already used in this scope.\n  f9() => f5();\n  ^\npkg/front_end/testcases/inference/infer_local_function_return_type.dart:31:51: Error: Previous use of 'f9'.\n  f8() => /*error:REFERENCED_BEFORE_DECLARATION*/ f9();\n                                                  ^";

tests/co19/co19-kernel.status
@@ -169,10 +169,6 @@ Language/Statements/For/syntax_t13: MissingCompileTimeError
 Language/Statements/For/syntax_t19: MissingCompileTimeError
 Language/Statements/For/syntax_t20: MissingCompileTimeError
 Language/Statements/Labels/scope_t05: MissingCompileTimeError
-Language/Statements/Local_Function_Declaration/reference_before_declaration_t01: MissingCompileTimeError
-Language/Statements/Local_Function_Declaration/reference_before_declaration_t03: MissingCompileTimeError
-Language/Statements/Local_Variable_Declaration/syntax_t11: MissingCompileTimeError
-Language/Statements/Local_Variable_Declaration/syntax_t20: MissingCompileTimeError
 Language/Statements/Switch/equal_operator_t01: MissingCompileTimeError
 Language/Statements/Switch/equal_operator_t02: MissingCompileTimeError
 Language/Statements/Switch/expressions_t01: MissingCompileTimeError
@@ -207,9 +203,6 @@ Language/Variables/final_t04: MissingCompileTimeError
 Language/Variables/final_t05: MissingCompileTimeError
 Language/Variables/final_t06: MissingCompileTimeError
 Language/Variables/final_t07: MissingCompileTimeError
-Language/Variables/local_variable_t01: MissingCompileTimeError
-Language/Variables/local_variable_t04: MissingCompileTimeError
-Language/Variables/local_variable_t05: MissingCompileTimeError
 LibTest/async/DeferredLibrary/DeferredLibrary_A01_t01: Skip # No support for deferred libraries.
 LibTest/isolate/Isolate/spawnUri_A01_t06: Skip
 

tests/language/language_kernel.status
@@ -232,28 +232,12 @@ vm/optimized_stacktrace_test: Skip # Issue 28788
 [ $compiler == dartk && $mode == debug ]
 deopt_inlined_function_lazy_test: Skip
 hello_dart_test: Crash  # error: expected: cls.is_type_finalized()
-ref_before_declaration_test/00: Crash
-ref_before_declaration_test/01: Crash
-ref_before_declaration_test/02: Crash
-ref_before_declaration_test/03: Crash
-ref_before_declaration_test/04: Crash
-ref_before_declaration_test/05: Crash
-ref_before_declaration_test/06: Crash
-ref_before_declaration_test/07: Crash
 vm/lazy_deopt_vm_test: Crash
 
 # dartk: precompilation failures
 [ $compiler == dartkp ]
 const_syntax_test/08: Crash
 final_syntax_test/09: MissingCompileTimeError
-ref_before_declaration_test/00: Crash
-ref_before_declaration_test/01: Crash
-ref_before_declaration_test/02: Crash
-ref_before_declaration_test/03: Crash
-ref_before_declaration_test/04: Crash
-ref_before_declaration_test/05: Crash
-ref_before_declaration_test/06: Crash
-ref_before_declaration_test/07: Crash
 ref_before_declaration_test/none: Crash
 setter_declaration2_negative_test: Crash
 setter_declaration_negative_test: Crash
@@ -398,16 +382,7 @@ function_type_alias9_test/00: MissingCompileTimeError # Fixed in ahe's dev branc
 metadata_test: CompileTimeError # Fixed in ahe's dev branch.
 named_parameters_aggregated_test/04: MissingCompileTimeError # Fixed in ahe's dev branch.
 named_parameters_aggregated_test/06: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/00: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/01: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/02: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/03: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/04: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/05: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/06: MissingCompileTimeError # Fixed in ahe's dev branch.
-ref_before_declaration_test/07: MissingCompileTimeError # Fixed in ahe's dev branch.
 regress_29349_test: CompileTimeError # Fixed in ahe's dev branch.
-scope_variable_test/01: MissingCompileTimeError # Fixed in ahe's dev branch.
 
 [ $unchecked && ($compiler == dartk || $compiler == dartkp) ]
 const_conditional_test/08: MissingCompileTimeError # KernelVM bug: Constant evaluation.

commit 1f294cbed079bf9cc6ba3ec46c21c557e3ad63b5

Fix problem introduced with 043c0096adeec45c0b7c544ce717956e1dd9942b.
R=johnniwinther@google.com

Review-Url: https://codereview.chromium.org/2931383002 .

sdk/lib/js/dart2js/js_dart2js.dart
@@ -499,8 +499,8 @@ class JsArray<E> extends JsObject with ListMixin<E> {
   }
 
   void setRange(int start, int end, Iterable<E> iterable, [int skipCount = 0]) {
-    _checkRange(start, end, length);
-    length = end - start;
+    _checkRange(start, end, this.length);
+    int length = end - start;
     if (length == 0) return;
     if (skipCount < 0) throw new ArgumentError(skipCount);
     var args = [start, length]..addAll(iterable.skip(skipCount).take(length));