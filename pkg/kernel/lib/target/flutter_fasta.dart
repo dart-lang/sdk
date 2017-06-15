f5a03eb0ef9195a2a20aa525fe47a662c79bb591
Sigmund Cherem <sigmund@google.com>
2017-06-14 21:34:11 -0700

    fix bot: remove analyzer error
    (I'm not sure why I didn't see this error before submitting)
    
    TBR=aam@google.com
    
    Review-Url: https://codereview.chromium.org/2940863004 .

--- pkg/kernel/lib/target/flutter_fasta.dart
+++ pkg/kernel/lib/target/flutter_fasta.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-library kernel.target.vm_fasta;
+library kernel.target.flutter_fasta;
 
 import '../ast.dart' show Program, Library;
 import '../core_types.dart' show CoreTypes;
