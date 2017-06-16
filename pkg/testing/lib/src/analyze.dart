e24970ef40f32014bdc729015fd6f98806cc7532
Peter von der Ahé <ahe@google.com>
2017-06-16 12:21:52 +0200

    dart2js isn't strong mode clean yet.
    R=johnniwinther@google.com
    
    Review-Url: https://codereview.chromium.org/2939313003 .

--- pkg/testing/lib/src/analyze.dart
+++ pkg/testing/lib/src/analyze.dart
@@ -237,10 +237,12 @@ Future<Null> analyzeUris(
     await for (AnalyzerDiagnostic diagnostic in diagnostics) {
       if (diagnostic.uri != null) {
         String path = toFilePath(diagnostic.uri);
-        if (diagnostic.code.startsWith("STRONG_MODE") &&
+        if (!(analysisOptions?.path.contains("/pkg/compiler/") ?? false) &&
+            diagnostic.code.startsWith("STRONG_MODE") &&
             (path.startsWith("pkg/compiler/") ||
                 path.startsWith("tests/compiler/dart2js/"))) {
-          // Hack to work around dart2js not being strong-mode clean.
+          // TODO(ahe): Remove this hack to work around dart2js not being
+          // strong-mode clean.
           continue;
         }
         if (!filesToAnalyze.contains(path)) continue;
