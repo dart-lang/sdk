0c1dab293364048bebe478343a46d7fb6974b118
Paul Berry <paulberry@google.com>
2017-06-23 14:26:51 -0700

    Re-introduce corrupted type inference tests.
    These two tests were added some time ago, but they became corrupted
    (presumably due to a formatter bug that has since been fixed).  Later,
    I moved the corrupted tests into the inference_new folder because they
    didn't infer correctly with analyzer.
    
    This CL re-introduces the original uncorrupted tests so that we don't
    lose test coverage.
    
    Note that these tests make use of a syntax that Fasta doesn't support
    yet, so they are marked as failing.
    
    R=sigmund@google.com
    
    Review-Url: https://codereview.chromium.org/2958623002 .

---
+++ pkg/front_end/testcases/inference/unsafe_block_closure_inference_function_call_explicit_type_param_via_expr2.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/*@testedFeatures=inference*/
+library test;
+
+List<T> f<T>(T g()) => <T>[g()];
+var /*@topType=dynamic*/ v =
+    (f)<int>(/*info:INFERRED_TYPE_CLOSURE*/ /*@returnType=int*/ () {
+  return 1;
+});
+
+main() {
+  v;
+}
