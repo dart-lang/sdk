56f07cf7709871514455ef1e74fc69aff7a92623
danrubel <danrubel@google.com>
2017-07-05 16:18:09 -0400

    Revert "enable fasta scanner in analyzer by default"
    This reverts commit d16163ca0bb27ca5664f5cc93cd3bb30717b1356.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2967253002 .

--- pkg/front_end/lib/src/scanner/scanner.dart
+++ pkg/front_end/lib/src/scanner/scanner.dart
@@ -129,8 +129,7 @@ abstract class Scanner {
    * A flag indicating whether the analyzer [Scanner] factory method
    * will return a fasta based scanner or an analyzer based scanner.
    */
-  static bool useFasta =
-      const bool.fromEnvironment("useFastaScanner", defaultValue: true);
+  static bool useFasta = const bool.fromEnvironment("useFastaScanner");
 
   /**
    * The reader used to access the characters in the source.
