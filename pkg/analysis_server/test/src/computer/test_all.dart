cc5ccf5dcf4a565fccfc4364963c711806c93b99
Brian Wilkerson <brianwilkerson@google.com>
2017-07-06 09:54:41 -0700

    Add unit tests for copy/paste support
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2965243002 .

---
+++ pkg/analysis_server/test/src/computer/test_all.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import 'import_elements_computer_test.dart' as import_elements_computer_test;
+import 'imported_elements_computer_test.dart'
+    as imported_elements_computer_test;
+
+main() {
+  defineReflectiveSuite(() {
+    import_elements_computer_test.main();
+    imported_elements_computer_test.main();
+  });
+}
