dart-lang/sdk

commit cec963f028198ec5871840491ba78b096ad0b819

The current growth strategy for growable arrays allocates a backing array of size 2 at (empty) creation and doubles the size whenever the capacity is insufficient while adding elements.
I collected statistics for the sizes and capacities of growable arrays which are promoted to old-space or survive an old-space gc when running dart2js and Fasta. For these applications, the vast majority of arrays stay empty. More than half of the total object size of promoted backing arrays is backing for empty growable arrays.

Furthermore, since the overhead for an array is 3 words (header, type parameters and length), and object sizes are rounded up to an even number of words, we waste one word for all even-sized arrays.

This CL changes the growth strategy so that empty growable arrays are created with a shared, zero-sized array as backing, avoiding the allocation of a backing array if no elements are added. When the array needs to grow, it starts out at 3 and grows to double size plus one each time: 7, 15, 31, ...

A few places in the VM code need to handle these shared, zero-sized arrays specially. In particular, the Array::MakeArray function needs to allocate a new, empty array if its result is to be returned to Dart code.

Benchmarks suggest that the change improves memory usage by a few percent overall and does not significantly affect run time.

BUG=
R=erikcorry@google.com

Review-Url: https://codereview.chromium.org/2949803002 .

runtime/lib/growable_array.dart
@@ -87,10 +87,8 @@ class _GrowableList<T> extends ListBase<T> {
     return result;
   }
 
-  static const int _kDefaultCapacity = 2;
-
   factory _GrowableList(int length) {
-    var data = new _List((length == 0) ? _kDefaultCapacity : length);
+    var data = _allocateData(length);
     var result = new _GrowableList<T>.withData(data);
     if (length > 0) {
       result._setLength(length);
@@ -99,7 +97,7 @@ class _GrowableList<T> extends ListBase<T> {
   }
 
   factory _GrowableList.withCapacity(int capacity) {
-    var data = new _List((capacity == 0) ? _kDefaultCapacity : capacity);
+    var data = _allocateData(capacity);
     return new _GrowableList<T>.withData(data);
   }
 
@@ -112,16 +110,6 @@ class _GrowableList<T> extends ListBase<T> {
   void set length(int new_length) {
     int old_capacity = _capacity;
     int new_capacity = new_length;
-    if (new_length == 0) {
-      // Ensure that we use _kDefaultCapacity only when the old_capacity
-      // is greater than _kDefaultCapacity otherwise we end up growing the
-      // the array.
-      if (old_capacity < _kDefaultCapacity) {
-        new_capacity = old_capacity;
-      } else {
-        new_capacity = _kDefaultCapacity;
-      }
-    }
     if (new_capacity > old_capacity) {
       _grow(new_capacity);
       _setLength(new_length);
@@ -152,14 +140,10 @@ class _GrowableList<T> extends ListBase<T> {
 
   void operator []=(int index, T value) native "GrowableList_setIndexed";
 
-  // The length of this growable array. It is always less than or equal to the
-  // length of the object array, which itself is always greater than 0, so that
-  // grow() does not have to check for a zero length object array before
-  // doubling its size.
   void add(T value) {
     var len = length;
     if (len == _capacity) {
-      _grow(len * 2);
+      _grow(_nextCapacity(len));
     }
     _setLength(len + 1);
     this[len] = value;
@@ -178,7 +162,7 @@ class _GrowableList<T> extends ListBase<T> {
       var newLen = len + iterLen;
       if (newLen > cap) {
         do {
-          cap *= 2;
+          cap = _nextCapacity(cap);
         } while (newLen > cap);
         _grow(cap);
       }
@@ -204,7 +188,7 @@ class _GrowableList<T> extends ListBase<T> {
         if (this.length != newLen) throw new ConcurrentModificationError(this);
         len = newLen;
       }
-      _grow(_capacity * 2);
+      _grow(_nextCapacity(_capacity));
     } while (true);
   }
 
@@ -232,20 +216,36 @@ class _GrowableList<T> extends ListBase<T> {
     ;
   }
 
-  void _grow(int new_capacity) {
-    var new_data = new _List(new_capacity);
-    for (int i = 0; i < length; i++) {
-      new_data[i] = this[i];
+  // Shared array used as backing for new empty growable arrays.
+  static final _List _emptyList = new _List(0);
+
+  static _List _allocateData(int capacity) {
+    if (capacity == 0) {
+      // Use shared empty list as backing.
+      return _emptyList;
     }
-    _setData(new_data);
+    // Round up size to the next odd number, since this is free
+    // because of alignment requirements of the GC.
+    return new _List(capacity | 1);
+  }
+
+  // Grow from 0 to 3, and then double + 1.
+  int _nextCapacity(int old_capacity) => (old_capacity * 2) | 3;
+
+  void _grow(int new_capacity) {
+    var newData = _allocateData(new_capacity);
+    for (int i = 0; i < length; i++) {
+      newData[i] = this[i];
+    }
+    _setData(newData);
   }
 
   void _shrink(int new_capacity, int new_length) {
-    var new_data = new _List(new_capacity);
+    var newData = _allocateData(new_capacity);
     for (int i = 0; i < new_length; i++) {
-      new_data[i] = this[i];
+      newData[i] = this[i];
     }
-    _setData(new_data);
+    _setData(newData);
   }
 
   // Iterable interface.

runtime/lib/array_patch.dart
@@ -47,10 +47,6 @@ class List<E> {
       list.add(e);
     }
     if (growable) return list;
-    if (list.length == 0) {
-      // Avoid getting an immutable list from makeListFixedLength.
-      return new _List<E>(0);
-    }
     return makeListFixedLength(list);
   }
 

runtime/lib/growable_array.cc
@@ -16,7 +16,7 @@ DEFINE_NATIVE_ENTRY(GrowableList_allocate, 2) {
   const TypeArguments& type_arguments =
       TypeArguments::CheckedHandle(arguments->NativeArgAt(0));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, data, arguments->NativeArgAt(1));
-  if (data.Length() <= 0) {
+  if (data.Length() < 0) {
     Exceptions::ThrowRangeError("length",
                                 Integer::Handle(Integer::New(data.Length())),
                                 0,  // This is the limit the user sees.
@@ -82,7 +82,7 @@ DEFINE_NATIVE_ENTRY(GrowableList_setData, 2) {
   const GrowableObjectArray& array =
       GrowableObjectArray::CheckedHandle(arguments->NativeArgAt(0));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, data, arguments->NativeArgAt(1));
-  ASSERT(data.Length() > 0);
+  ASSERT(data.Length() >= 0);
   array.SetData(data);
   return Object::null();
 }
@@ -91,7 +91,7 @@ DEFINE_NATIVE_ENTRY(GrowableList_setData, 2) {
 DEFINE_NATIVE_ENTRY(Internal_makeListFixedLength, 1) {
   GET_NON_NULL_NATIVE_ARGUMENT(GrowableObjectArray, array,
                                arguments->NativeArgAt(0));
-  return Array::MakeArray(array);
+  return Array::MakeFixedLength(array, /* unique = */ true);
 }
 
 

runtime/lib/stacktrace.cc
@@ -173,9 +173,9 @@ const StackTrace& GetCurrentStackTrace(int skip_frames) {
   const GrowableObjectArray& pc_offset_list =
       GrowableObjectArray::Handle(GrowableObjectArray::New());
   AppendFrames(code_list, pc_offset_list, skip_frames);
-  const Array& code_array = Array::Handle(Array::MakeArray(code_list));
+  const Array& code_array = Array::Handle(Array::MakeFixedLength(code_list));
   const Array& pc_offset_array =
-      Array::Handle(Array::MakeArray(pc_offset_list));
+      Array::Handle(Array::MakeFixedLength(pc_offset_list));
   const StackTrace& stacktrace =
       StackTrace::Handle(StackTrace::New(code_array, pc_offset_array));
   return stacktrace;

runtime/vm/class_finalizer.cc
@@ -2429,7 +2429,7 @@ void ClassFinalizer::ApplyMixinMembers(const Class& cls) {
       cloned_funcs.Add(func);
     }
   }
-  functions = Array::MakeArray(cloned_funcs);
+  functions = Array::MakeFixedLength(cloned_funcs);
   cls.SetFunctions(functions);
 
   // Now clone the fields from the mixin class. There should be no
@@ -2664,7 +2664,8 @@ void ClassFinalizer::FinalizeClass(const Class& cls) {
     const Class& mixin_cls = Class::Handle(mixin_type.type_class());
 
     CreateForwardingConstructors(cls, mixin_cls, cloned_funcs);
-    const Array& functions = Array::Handle(Array::MakeArray(cloned_funcs));
+    const Array& functions =
+        Array::Handle(Array::MakeFixedLength(cloned_funcs));
     cls.SetFunctions(functions);
   }
   // Every class should have at least a constructor, unless it is a top level

runtime/vm/clustered_snapshot.cc
@@ -3472,7 +3472,7 @@ class MintDeserializationCluster : public DeserializationCluster {
       }
     }
     const Array& constants_array =
-        Array::Handle(zone, Array::MakeArray(new_constants));
+        Array::Handle(zone, Array::MakeFixedLength(new_constants));
     const Class& mint_cls =
         Class::Handle(zone, Isolate::Current()->object_store()->mint_class());
     mint_cls.set_constants(constants_array);

runtime/vm/code_descriptors.cc
@@ -73,7 +73,7 @@ RawArray* StackMapTableBuilder::FinalizeStackMaps(const Code& code) {
   if (num_entries == 0) {
     return Object::empty_array().raw();
   }
-  return Array::MakeArray(list_);
+  return Array::MakeFixedLength(list_);
 }
 
 
@@ -406,7 +406,7 @@ RawArray* CodeSourceMapBuilder::InliningIdToFunction() {
   if (inlined_functions_.Length() == 0) {
     return Object::empty_array().raw();
   }
-  return Array::MakeArray(inlined_functions_);
+  return Array::MakeFixedLength(inlined_functions_);
 }
 
 

runtime/vm/dart_api_impl.cc
@@ -5786,7 +5786,7 @@ DART_EXPORT Dart_Handle Dart_GetImportsOfScheme(Dart_Handle scheme) {
     }
   }
 
-  return Api::NewHandle(T, Array::MakeArray(result));
+  return Api::NewHandle(T, Array::MakeFixedLength(result));
 }
 
 

commit d24040af02d13264c198ced6774c5ce32e75188a

VM(CoreLib): Work around JIT optimization issue revealed by cec963f028198ec5871840491ba78b096ad0b819
Array Bounds Check Generalization pass attempts to hoist CheckArrayBound
instructions out of loops by generalizing them: for example in the loop
like

  for (var i = 0; i < L; i++) {
    // a[i]
    CheckArrayBound(i, a.length)
    LoadIndexed(a, i)
  }

we hoist CheckArrayBound(i, a.length) by turning it into
CheckArrayBound(L - 1, a.length):

  CheckArrayBound(L - 1, a.length)
  for (var i = 0; i < L; i++) {
    // a[i]
    LoadIndexed(a, i)
  }

However this leads to deoptimizations if the loop never executes: e.g. if
L = 0 then L - 1 is -1 which does not pass generalized bounds check.

We prevent repeated deoptimizations by disabling this optimization if any
of the generalized bounds checks deoptimizes. However this does not work
as intended because we only check this flag on an outermost function, so
if function with a problematic bounds check get inlined into a lot of places
it ends up causing a lot of deoptimizations in different places.

This is why cec963f028198ec5871840491ba78b096ad0b819 caused performance issues
in dartdoc: _GrowableList._grow is now called with length == 0 whenever we
add an element to an empty list. This causes deoptimization in a lot of different
functions in dartdoc because _GrowableList._grow ends up inlined into a lot
of different places.

Fixes https://github.com/dart-lang/sdk/issues/30090

This CL will be reverted once a better solution for the underlying problem is in place (tracked by https://github.com/dart-lang/sdk/issues/30102)

BUG=
R=askesc@google.com, erikcorry@google.com

Review-Url: https://codereview.chromium.org/2971303002 .

runtime/lib/growable_array.dart
@@ -234,16 +234,27 @@ class _GrowableList<T> extends ListBase<T> {
 
   void _grow(int new_capacity) {
     var newData = _allocateData(new_capacity);
-    for (int i = 0; i < length; i++) {
-      newData[i] = this[i];
+    // This is a work-around for dartbug.com/30090: array-bound-check
+    // generalization causes excessive deoptimizations because it
+    // hoists CheckArrayBound(i, ...) out of the loop below and turns it
+    // into CheckArrayBound(length - 1, ...). Which deoptimizes
+    // if length == 0. However the loop itself does not execute
+    // if length == 0.
+    if (length > 0) {
+      for (int i = 0; i < length; i++) {
+        newData[i] = this[i];
+      }
     }
     _setData(newData);
   }
 
   void _shrink(int new_capacity, int new_length) {
     var newData = _allocateData(new_capacity);
-    for (int i = 0; i < new_length; i++) {
-      newData[i] = this[i];
+    // This is a work-around for dartbug.com/30090. See the comment in _grow.
+    if (new_length > 0) {
+      for (int i = 0; i < new_length; i++) {
+        newData[i] = this[i];
+      }
     }
     _setData(newData);
   }