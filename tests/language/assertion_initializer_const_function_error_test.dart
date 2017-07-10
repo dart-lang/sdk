609d26a2274ccde0f74725f4df7e081ebc8ea020
Lasse R.H. Nielsen <lrn@google.com>
2017-07-10 13:59:36 +0200

    Add assert-in-initializer-list to formal specification.
    Add tests for new syntax.
    Not dart-formatted yet since the formatter doesn't accept the new syntax.
    
    R=eernst@google.com, floitsch@google.com
    
    Review-Url: https://codereview.chromium.org/2962153002 .

---
+++ tests/language/assertion_initializer_const_function_error_test.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// VMOptions=--assert_initializer
+//
+// Dart test program testing assert statements.
+
+import "package:expect/expect.dart";
+
+class C {
+  static bool staticTrue() => true;
+  final int x;
+  const C(this.x);
+  // The expression *is* a compile-time constant, but not a bool value.
+  // Static warning, assertion throws which makes it a compile-time error.
+  const C.bc02(this.x, y)
+      : assert(staticTrue) //# 01: static type warning
+      ;
+}
+
+
+main() {
+  // Assertion fails, so in checked mode it's a compile-time error.
+  // Production mode will succeed because the assertion isn't evaluated.
+  var c = const C(1);
+  c = const C.bc02(1, 2);  //# 01: compile-time error
+  if (c.x != 1) throw "non-trivial use of c";
+  Expect.identical(const C(1), c);
+}
