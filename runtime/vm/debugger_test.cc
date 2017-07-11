dart-lang/sdk

commit 1eaa3dd84baf04db4ab7c437c734dceaa27785a3

Cleanup how a best fit function is found when setting a breakpoint.
1. Look for functions/closures only in the script where the breakpoint
is being set.
2. If an inner most closure is found, return that as the best fit.
3. When going over a class' functions, return immediately when a fit is
found as going over the other class functions will not yield a narrower
fit.

BUG=
R=asiva@google.com

Review-Url: https://codereview.chromium.org/2898153006 .

runtime/vm/debugger.cc
@@ -2649,23 +2649,38 @@ static void SelectBestFit(Function* best_fit, Function* func) {
 }
 
 
+static bool IsTokenPosWithinFunction(const Function& func, TokenPosition pos) {
+  return (func.token_pos() <= pos && pos <= func.end_token_pos());
+}
+
+
 RawFunction* Debugger::FindBestFit(const Script& script,
                                    TokenPosition token_pos) {
   Zone* zone = Thread::Current()->zone();
   Class& cls = Class::Handle(zone);
   Array& functions = Array::Handle(zone);
-  GrowableObjectArray& closures = GrowableObjectArray::Handle(zone);
+  const GrowableObjectArray& closures = GrowableObjectArray::Handle(
+      zone, isolate_->object_store()->closure_functions());
   Function& function = Function::Handle(zone);
   Function& best_fit = Function::Handle(zone);
   Error& error = Error::Handle(zone);
 
-  closures = isolate_->object_store()->closure_functions();
   const intptr_t num_closures = closures.Length();
   for (intptr_t i = 0; i < num_closures; i++) {
     function ^= closures.At(i);
-    if (FunctionContains(function, script, token_pos)) {
+    if (function.script() != script.raw()) {
+      continue;
+    }
+    if (IsTokenPosWithinFunction(function, token_pos)) {
+      // Select the inner most closure.
       SelectBestFit(&best_fit, &function);
     }
+  }
+  if (!best_fit.IsNull()) {
+    // The inner most closure found will be the best fit. Going
+    // over class functions below will not help in any further
+    // narrowing.
+    return best_fit.raw();
   }
 
   const ClassTable& class_table = *isolate_->class_table();
@@ -2673,13 +2688,7 @@ RawFunction* Debugger::FindBestFit(const Script& script,
   for (intptr_t i = 1; i < num_classes; i++) {
     if (class_table.HasValidClassAt(i)) {
       cls = class_table.At(i);
-      // Note: if this class has been parsed and finalized already,
-      // we need to check the functions of this class even if
-      // it is defined in a different 'script'. There could
-      // be mixin functions from the given script in this class.
-      // However, if this class is not parsed yet (not finalized),
-      // we can ignore it and avoid the side effect of parsing it.
-      if ((cls.script() != script.raw()) && !cls.is_finalized()) {
+      if (cls.script() != script.raw()) {
         continue;
       }
       // Parse class definition if not done yet.
@@ -2697,14 +2706,18 @@ RawFunction* Debugger::FindBestFit(const Script& script,
         for (intptr_t pos = 0; pos < num_functions; pos++) {
           function ^= functions.At(pos);
           ASSERT(!function.IsNull());
-          if (FunctionContains(function, script, token_pos)) {
-            SelectBestFit(&best_fit, &function);
+          if (IsTokenPosWithinFunction(function, token_pos)) {
+            // Closures and inner functions within a class method are not
+            // present in the functions of a class. Hence, we can return
+            // right away as looking through other functions of a class
+            // will not narrow down to any inner function/closure.
+            return function.raw();
           }
         }
       }
     }
   }
-  return best_fit.raw();
+  return Function::null();
 }
 
 

commit f0ef900fd3e20f0513be7fc13da36dbf89f2e1ac

Fix setting breakpoints in parts of a library.
This fixes the regression caused by
1eaa3dd84baf04db4ab7c437c734dceaa27785a3 and reported in #29988.

When picking a class to set a breakpoint, instead of rejecting classes
not defined in the script, we now reject classes which do not belong to
the library to which the script belongs.

R=asiva@google.com

Review-Url: https://codereview.chromium.org/2965673004 .

runtime/vm/debugger.cc
@@ -2710,6 +2710,8 @@ bool Debugger::FindBestFit(const Script& script,
                            Function* best_fit) {
   Zone* zone = Thread::Current()->zone();
   Class& cls = Class::Handle(zone);
+  Library& lib = Library::Handle(zone, script.FindLibrary());
+  ASSERT(!lib.IsNull());
   const GrowableObjectArray& closures = GrowableObjectArray::Handle(
       zone, isolate_->object_store()->closure_functions());
   Array& functions = Array::Handle(zone);
@@ -2740,7 +2742,9 @@ bool Debugger::FindBestFit(const Script& script,
       continue;
     }
     cls = class_table.At(i);
-    if (cls.script() != script.raw()) {
+    // This class is relevant to us only if it belongs to the
+    // library to which |script| belongs.
+    if (cls.library() != lib.raw()) {
       continue;
     }
     // Parse class definition if not done yet.
@@ -2786,6 +2790,11 @@ bool Debugger::FindBestFit(const Script& script,
         TokenPosition end;
         field ^= fields.At(pos);
         ASSERT(!field.IsNull());
+        if (field.Script() != script.raw()) {
+          // The field should be defined in the script we want to set
+          // the breakpoint in.
+          continue;
+        }
         if (Parser::FieldHasFunctionLiteralInitializer(field, &start, &end)) {
           if ((start <= token_pos && token_pos <= end) ||
               (token_pos <= start && start <= last_token_pos)) {

runtime/tests/vm/vm.status
@@ -273,6 +273,7 @@ cc/Debug_ScriptGetTokenInfo_MultiLineInterpolation: Fail
 cc/Debugger_PrintBreakpointsToJSONArray: Fail
 cc/Debugger_Rewind_Optimized: SkipSlow
 cc/Debugger_SetBreakpointInFunctionLiteralFieldInitializers: Crash
+cc/Debugger_SetBreakpointInPartOfLibrary: Crash
 cc/ErrorHandleBasics: Fail
 cc/EvalExpression: Crash
 cc/ExternalUint8ClampedArrayAccess: Fail

runtime/vm/debugger_test.cc
@@ -80,6 +80,37 @@ static void PausedInClosuresHandler(Dart_IsolateId isolate_id,
   closure_hit_count++;
 }
 
+TEST_CASE(Debugger_SetBreakpointInPartOfLibrary) {
+  const char* kMainScript = "main() {}\n";
+  const char* kLib = "library test_lib;\n";
+  const char* kLibPart =
+      "part of test_lib;\n"
+      "void func(int a, int b) {\n"
+      "  return a + b;\n"
+      "}\n";
+  SetFlagScope<bool> sfs(&FLAG_remove_script_timestamps_for_test, true);
+  Dart_Handle root_lib = TestCase::LoadTestScript(kMainScript, NULL);
+  EXPECT_VALID(root_lib);
+
+  Dart_Handle url = NewString("test_lib_url");
+  Dart_Handle lib_source = NewString(kLib);
+  Dart_Handle lib = Dart_LoadLibrary(url, Dart_Null(), lib_source, 0, 0);
+  EXPECT_VALID(lib);
+  EXPECT(Dart_IsLibrary(lib));
+
+  Dart_Handle part_url = NewString("part_url");
+  Dart_Handle part_source = NewString(kLibPart);
+  Dart_Handle result =
+      Dart_LoadSource(lib, part_url, Dart_Null(), part_source, 0, 0);
+  EXPECT_VALID(result);
+  EXPECT(Dart_IsLibrary(result));
+  EXPECT(Dart_IdentityEquals(lib, result));
+
+  result = Dart_SetBreakpoint(part_url, 3);
+  EXPECT_VALID(result);
+  EXPECT(Dart_IsInteger(result));
+}
+
 TEST_CASE(Debugger_SetBreakpointInFunctionLiteralFieldInitializers) {
   const char* kScriptChars =
       "main() {\n"