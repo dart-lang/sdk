283fa3a77dc26fcf664efad4c0207a6c0797ced7
Natalie Weizenbaum <nweiz@google.com>
2017-07-12 14:56:19 -0700

    Code review

--- pkg/dev_compiler/tool/input_sdk/patch/core_patch.dart
+++ pkg/dev_compiler/tool/input_sdk/patch/core_patch.dart
@@ -604,8 +604,11 @@ class _Uri {
   @patch
   static bool get _isWindows => _isWindowsCached;
 
-  static final bool _isWindowsCached =
-      JS('bool', 'process !== undefined && process.platform == "win32"');
+  static final bool _isWindowsCached = JS(
+      'bool',
+      'typeof process != "undefined" && '
+      'Object.prototype.toString.call(process) == "[object process]" && '
+      'process.platform == "win32"');
 
   // Matches a String that _uriEncodes to itself regardless of the kind of
   // component.  This corresponds to [_unreservedTable], i.e. characters that
