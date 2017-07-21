cc81f38519b530f8dba6e46d82bc3c9554f5ce3d
Régis Crelier <regis@google.com>
2017-07-21 15:07:56 -0700

    [VM generic functions] Disallow generic function types as type arguments or bounds. Adjust existing test and status file.
    R=asiva@google.com
    
    Review-Url: https://codereview.chromium.org/2989513002 .

--- tests/language_2/generic_methods_generic_function_result_test.dart
+++ tests/language_2/generic_methods_generic_function_result_test.dart
@@ -4,19 +4,25 @@
 //
 // VMOptions=--generic-method-syntax,--error-on-bad-type
 
-// Verify that function type parameter S can be resolved in bar.
+// Verify that function type parameter S can be resolved in bar's result type.
+// Verify that generic function types are not allowed as type arguments.
 
 import "package:expect/expect.dart";
 
-T foo<T extends num>(int i, T t) => i + t;
+int foo
+       <T>  //# 01: continued
+          (int i, int j) => i + j;
 
-List<T Function<T extends num>(S, T)> bar<S extends int>() {
-  return <T Function<T extends num>(S, T)>[foo, foo];
+List<int Function
+                 <T>  //# 01: compile-time error
+                    (S, int)> bar<S extends int>() {
+  return <int Function
+                      <T>  //# 01: continued
+                         (S, int)>[foo, foo];
 }
 
 void main() {
   var list = bar<int>();
-  print(list[0]
-      .runtimeType); // "<T extends num>(int, T) => T" when reifying generic functions.
+  print(list[0].runtimeType);
   Expect.equals(123, list[1]<int>(100, 23));
 }
