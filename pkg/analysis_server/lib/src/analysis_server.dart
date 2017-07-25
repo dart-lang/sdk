a040de8c083e885e7d19a80ffaa1cfc7a9de6bd0
Brian Wilkerson <brianwilkerson@google.com>
2017-07-25 12:45:15 -0700

    Update the version of the server API
    R=scheglov@google.com
    
    Review-Url: https://codereview.chromium.org/2985003002 .

--- pkg/analysis_server/lib/src/analysis_server.dart
+++ pkg/analysis_server/lib/src/analysis_server.dart
@@ -100,7 +100,7 @@ class AnalysisServer {
    * The version of the analysis server. The value should be replaced
    * automatically during the build.
    */
-  static final String VERSION = '1.18.2';
+  static final String VERSION = '1.18.3';
 
   /**
    * The options of this server instance.
