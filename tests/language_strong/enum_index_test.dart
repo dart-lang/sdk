63948f0ebeffd8a72dbadae8fa2562ebfcc8faf7
Bob Nystrom <robert@stuffwithstuff.com>
2017-07-25 15:31:31 -0700

    Re-triage the DDC test statuses. (#30234)
    - The other fromEnvironment() tests are passing now. (I must have
      missed those earlier.)
    - Couple of other tests are compiling.
    - Remove "--enable-enum" from the SharedOptions since those are being
      passed to DDC now and it doesn't know what that means. Also, uh,
      enums have been enabled for years...

--- tests/language_strong/enum_index_test.dart
+++ tests/language_strong/enum_index_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// SharedOptions=--enable-enum
-
 // Test index access for enums.
 
 library enum_index_test;
