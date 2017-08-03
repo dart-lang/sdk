9442c1cf442db21ce6e971c738f5d267f732e37a
Stephen Adams <sra@google.com>
2017-08-03 16:43:34 -0700

    Revert "Work-around for missing mixin-application charOffset"
    This reverts commit 2007487cf042628a8776e0d741a86e1d6c605a4f.
    
    Needs status file changes.
    
    TBR=sigmund@google.com
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2992993002 .

--- pkg/front_end/lib/src/fasta/kernel/kernel_library_builder.dart
+++ pkg/front_end/lib/src/fasta/kernel/kernel_library_builder.dart
@@ -290,10 +290,6 @@ class KernelLibraryBuilder
       int unresolvedCount = 0;
       Map<String, TypeBuilder> freeTypes = <String, TypeBuilder>{};
 
-      // TODO(30316): Use correct locations of mixin applications
-      // (e.g. identifiers for mixed-in classes).
-      if (charOffset == -1) charOffset = type.charOffset;
-
       if (name == null || type.mixins.length != 1) {
         TypeBuilder last = type.mixins.last;
 
@@ -423,8 +419,8 @@ class KernelLibraryBuilder
         checkArguments(mixin);
         supertype = applyMixin(supertype, mixin, signature,
             isSyntheticMixinImplementation: true,
-            typeVariables: new List<TypeVariableBuilder>.from(variables.values),
-            charOffset: charOffset);
+            typeVariables:
+                new List<TypeVariableBuilder>.from(variables.values));
       }
       KernelNamedTypeBuilder mixin = type.mixins.last;
 
