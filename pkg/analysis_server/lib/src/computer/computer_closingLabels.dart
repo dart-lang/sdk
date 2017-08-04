18b30f0c5f897b3c881ad63290c511e911639cd8
Danny Tuppeny <danny@tuppeny.com>
2017-08-04 18:54:21 +0100

    Add initial implementation and test for closing labels.
    See #30285

--- pkg/analysis_server/lib/src/computer/computer_closingLabels.dart
+++ pkg/analysis_server/lib/src/computer/computer_closingLabels.dart
@@ -34,4 +34,50 @@ class _DartUnitClosingLabelsComputerVisitor
   final DartUnitClosingLabelsComputer computer;
 
   _DartUnitClosingLabelsComputerVisitor(this.computer);
+
+  @override
+  Object visitInstanceCreationExpression(InstanceCreationExpression node) {
+    if (_spansManyLines(node)) {
+      final name = node.constructorName.type.name;
+      final label = name is PrefixedIdentifier ? name.prefix.name : name.name;
+      _addLabel(node, label);
+    }
+
+    return super.visitInstanceCreationExpression(node);
+  }
+
+  @override
+  Object visitMethodInvocation(MethodInvocation node) {
+    if (node.argumentList != null && _spansManyLines(node.argumentList)) {
+      _addLabel(node, node.methodName.name);
+    }
+
+    return super.visitMethodInvocation(node);
+  }
+
+  @override
+  visitListLiteral(ListLiteral node) {
+    if (_spansManyLines(node)) {
+      final typeName = node.typeArguments?.arguments[0]?.toString();
+
+      if (typeName != null) {
+        _addLabel(node, "List<$typeName>");
+      }
+    }
+
+    return super.visitListLiteral(node);
+  }
+
+  bool _spansManyLines(AstNode node) {
+    final start = computer._lineInfo.getLocation(node.offset);
+    // TODO(dantup) Is this valid? We want the last character of the node, not the start of the next?
+    final end = computer._lineInfo.getLocation(node.end - 1);
+
+    return (start.lineNumber - end.lineNumber).abs() > 1;
+  }
+
+  void _addLabel(AstNode node, String label) {
+    computer._closingLabels
+        .add(new ClosingLabel(node.offset, node.length, label));
+  }
 }
--- pkg/analysis_server/test/analysis/notification_closingLabels_test.dart
+++ pkg/analysis_server/test/analysis/notification_closingLabels_test.dart
@@ -36,7 +36,6 @@ class _AnalysisNotificationClosingLabelsTest extends AbstractAnalysisTest {
   }
 
   void processNotification(Notification notification) {
-    print(notification.toJson());
     if (notification.event == ANALYSIS_NOTIFICATION_CLOSING_LABELS) {
       var params =
           new AnalysisClosingLabelsParams.fromNotification(notification);
@@ -51,5 +50,106 @@ class _AnalysisNotificationClosingLabelsTest extends AbstractAnalysisTest {
   void setUp() {
     super.setUp();
     createProject();
+  }
+
+  static const sampleCode = '''
+Widget build(BuildContext context) {
+  return /*1*/new Row(
+    children: /*2*/<Widget>[
+      new Text('a'),
+      new Text('b'),
+    ]/*2:List<Widget>*/,
+  )/*1:Row*/;
+}
+''';
+
+  test_afterAnalysis() async {
+    addTestFile(sampleCode);
+    await waitForTasksFinished();
+    expect(lastLabels, isNull);
+
+    await waitForLabels(() => subscribeForLabels());
+    _compareLastResultsWithTestFileComments(2);
+  }
+
+  test_afterUpdate() async {
+    addTestFile('');
+    // TODO(dantup) currently required to get notifications on updates
+    setPriorityFiles([testFile]);
+
+    // Before subscribing, we shouldn't have had any labels.
+    await waitForTasksFinished();
+    expect(lastLabels, isNull);
+
+    // With no content, there should be zero labels.
+    await waitForLabels(() => subscribeForLabels());
+    expect(lastLabels, hasLength(0));
+
+    // With sample code there will be labels.
+    await waitForLabels(() => modifyTestFile(sampleCode));
+    _compareLastResultsWithTestFileComments(2);
+  }
+
+  test_multiple_nested() async {
+    addTestFile('''
+Widget build(BuildContext context) {
+  return /*1*/new Row(
+    children: /*2*/<Widget>[
+      /*3*/new RaisedButton(
+        onPressed: increment,
+        child: /*4*/new Text(
+          'Increment'
+        )/*4:Text*/,
+      )/*3:RaisedButton*/,
+      /*5*/_makeWidget(
+        'a',
+        'b'
+      )/*5:_makeWidget*/,
+      new Text('Count: \$counter'),
+    ]/*2:List<Widget>*/,
+  )/*1:Row*/;
+}
+''');
+    await waitForTasksFinished();
+    expect(lastLabels, isNull);
+
+    await waitForLabels(() {
+      subscribeForLabels();
+    });
+    _compareLastResultsWithTestFileComments(5);
+  }
+
+  /// Compares the latest received closing labels with expected
+  /// labels extracted from the comments in the test file.
+  _compareLastResultsWithTestFileComments(int expectedLabelCount) {
+    // Require the test pass us the expected count to guard
+    // against expected annotations being mistyped and not
+    // extracted by the regex.
+    expect(lastLabels, hasLength(expectedLabelCount));
+
+    // Find all numeric markers for label starts.
+    var regex = new RegExp("/\\*(\\d+)\\*/");
+    var expectedLabels = regex.allMatches(testCode);
+
+    // Check we didn't get more than expected, since the loop below only
+    // checks for the presence of matches, not absence.
+    expect(lastLabels, hasLength(expectedLabels.length));
+
+    // Go through each marker, find the expected label/end and
+    // ensure it's in the results.
+    expectedLabels.forEach((m) {
+      var i = m.group(1);
+      // Find the end marker.
+      var endMatch = new RegExp("/\\*$i:(.+)\\*/").firstMatch(testCode);
+
+      var expectedStart = m.end;
+      var expectedLength = endMatch.start - expectedStart;
+      var expectedLabel = endMatch.group(1);
+
+      expect(
+          lastLabels,
+          contains(
+              new ClosingLabel(expectedStart, expectedLength, expectedLabel)));
+    });
   }
 }
