fb01f51ff9b74073a5945714541d09790b3092ff
Jennifer Messerly <jmesserly@google.com>
2017-08-04 14:42:36 -0700

    disable batch mode on travis

--- pkg/dev_compiler/tool/browser_test.sh
+++ pkg/dev_compiler/tool/browser_test.sh
@@ -4,8 +4,16 @@ set -e # bail on error
 # Switch to the root of the SDK tree
 cd $( dirname "${BASH_SOURCE[0]}" )/../../..
 
+FLAGS=
+if [ "$TRAVIS" ]; then
+  # TODO(jmesserly): figure out how to get batch mode working on Travis
+  echo Disabling --batch mode on Travis
+  FLAGS=--noBatch
+fi
+
 # TODO(jmesserly): we need to figure out how to get Travis to extract the
 # checked in SDK used by test.py. I think this might be done by runhooks, which
 # we're skipping when we aren't in C++ build mode.
-dart -c tools/testing/dart/main.dart -m release -r chrome -c dartdevc --strong \
+dart -c tools/testing/dart/main.dart $FLAGS \
+    -m release -r chrome -c dartdevc --strong \
     corelib_2 language_2 lib_2 corelib_strong language_strong lib_strong || fail
