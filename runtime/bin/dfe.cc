dart-lang/sdk

commit c7faf2dc6b64b65e04d677fa3ad68c1e90e19bff

[kernel] Free memory for kernel data.
This should cleanup user script kernel data and service isolate kernel data,
but leave the platform kernel data alone as it is used several times,
and it is unclear to me when it is safe to free it.

BUG=
R=kmillikin@google.com

Review-Url: https://codereview.chromium.org/3000623002 .

runtime/bin/dfe.cc
@@ -42,6 +42,10 @@ DFE::~DFE() {
   }
 }
 
+void DFE::clear_kernel_vmservice_io() {
+  kernel_vmservice_io_ = NULL;
+}
+
 void DFE::SetKernelBinaries(const char* name) {
   intptr_t len = snprintf(NULL, 0, "%s%s%s", name, File::PathSeparator(),
                           kPlatformBinaryName) +

runtime/bin/dfe.h
@@ -34,6 +34,8 @@ class DFE {
 
   void* kernel_vmservice_io() const { return kernel_vmservice_io_; }
 
+  void clear_kernel_vmservice_io();
+
   bool kernel_file_specified() const { return kernel_file_specified_; }
   void set_kernel_file_specified(bool value) { kernel_file_specified_ = value; }
 

runtime/bin/main.cc
@@ -848,6 +848,7 @@ static Dart_Isolate IsolateSetupHelper(Dart_Isolate isolate,
   if (dfe.kernel_file_specified()) {
     ASSERT(kernel_program != NULL);
     result = Dart_LoadKernel(kernel_program);
+    isolate_data->kernel_program = NULL;  // Dart_LoadKernel takes ownership.
   } else {
     if (kernel_program != NULL) {
       Dart_Handle uri = Dart_NewStringFromCString(script_uri);
@@ -857,6 +858,7 @@ static Dart_Isolate IsolateSetupHelper(Dart_Isolate isolate,
       result =
           Dart_LoadScript(uri, resolved_script_uri,
                           reinterpret_cast<Dart_Handle>(kernel_program), 0, 0);
+      isolate_data->kernel_program = NULL;  // Dart_LoadScript takes ownership.
       CHECK_RESULT(result);
     }
   }
@@ -1047,6 +1049,7 @@ static Dart_Isolate CreateAndSetupServiceIsolate(const char* script_uri,
   if (dfe.UsePlatformBinary()) {
     Dart_Handle library = Dart_LoadKernel(dfe.kernel_vmservice_io());
     CHECK_RESULT_CLEANUP(library, isolate_data);
+    dfe.clear_kernel_vmservice_io();  // Dart_LoadKernel takes ownership.
     skip_library_load = true;
   }
 #endif  // !defined(DART_PRECOMPILED_RUNTIME)

runtime/vm/dart_api_impl.cc
@@ -5072,11 +5072,13 @@ static void CompileSource(Thread* thread,
 static Dart_Handle LoadKernelProgram(Thread* T,
                                      const String& url,
                                      void* kernel) {
-  // NOTE: Now the VM owns the [kernel_program] memory!  Currently we do not
-  // free it because (similar to the token stream) it will be used to repeatedly
-  // run the `kernel::FlowGraphBuilder()`.
-  kernel::KernelReader reader(reinterpret_cast<kernel::Program*>(kernel));
+  // NOTE: Now the VM owns the [kernel_program] memory!
+  // We will promptly delete it when done.
+  kernel::Program* program = reinterpret_cast<kernel::Program*>(kernel);
+  kernel::KernelReader reader(program);
   const Object& tmp = reader.ReadProgram();
+  delete program;
+
   if (tmp.IsError()) {
     return Api::NewHandle(T, tmp.raw());
   }
@@ -5254,12 +5256,13 @@ DART_EXPORT Dart_Handle Dart_LoadKernel(void* kernel_program) {
   CHECK_CALLBACK_STATE(T);
   CHECK_COMPILATION_ALLOWED(I);
 
-  // NOTE: Now the VM owns the [kernel_program] memory!  Currently we do not
-  // free it because (similar to the token stream) it will be used to repeatedly
-  // run the `kernel::FlowGraphBuilder()`.
-  kernel::KernelReader reader(
-      reinterpret_cast<kernel::Program*>(kernel_program));
+  // NOTE: Now the VM owns the [kernel_program] memory!
+  // We will promptly delete it when done.
+  kernel::Program* program = reinterpret_cast<kernel::Program*>(kernel_program);
+  kernel::KernelReader reader(program);
   const Object& tmp = reader.ReadProgram();
+  delete program;
+
   if (tmp.IsError()) {
     return Api::NewHandle(T, tmp.raw());
   }

runtime/vm/kernel.h
@@ -116,6 +116,11 @@ class LogicalExpression {
 
 class Program {
  public:
+  ~Program() {
+    free(const_cast<uint8_t*>(kernel_data_));
+    kernel_data_ = NULL;
+  }
+
   static Program* ReadFrom(Reader* reader);
 
   NameIndex main_method() { return main_method_reference_; }

commit d89b06ab096a4b574341b8873535be157e52321e

[vm,aot] Fix AOT snapshot creation with kernel front-end
The following problems are fixed in dart_bootstrap when it is used to
create AOT snapshot along with new kernel front-end:

* Data race between main isolate and service isolate when accessing
  vmservice_io kernel program stored in DFE::kernel_vmservice_io_.

* Double-deletion of vmservice_io kernel file program from
  VmService::LoadForGenPrecompiled / Dart_LoadLibrary /
  LoadKernelProgram and then from DFE::~DFE.

It looks like both of these bugs were introduced in
c7faf2dc6b64b65e04d677fa3ad68c1e90e19bff.

R=jensj@google.com, rmacnak@google.com

Review-Url: https://codereview.chromium.org/3002733002 .

runtime/bin/dfe.cc
@@ -20,7 +20,6 @@ DFE::DFE()
       platform_binary_filename_(NULL),
       vmservice_io_binary_filename_(NULL),
       kernel_platform_(NULL),
-      kernel_vmservice_io_(NULL),
       kernel_file_specified_(false) {}
 
 DFE::~DFE() {
@@ -35,15 +34,6 @@ DFE::~DFE() {
     delete reinterpret_cast<kernel::Program*>(kernel_platform_);
     kernel_platform_ = NULL;
   }
-
-  if (kernel_vmservice_io_ != NULL) {
-    delete reinterpret_cast<kernel::Program*>(kernel_vmservice_io_);
-    kernel_vmservice_io_ = NULL;
-  }
-}
-
-void DFE::clear_kernel_vmservice_io() {
-  kernel_vmservice_io_ = NULL;
 }
 
 void DFE::SetKernelBinaries(const char* name) {
@@ -118,15 +108,15 @@ void* DFE::CompileAndReadScript(const char* script_uri,
   return NULL;
 }
 
-void* DFE::ReadPlatform() {
-  return kernel_platform_ = ReadScript(platform_binary_filename_);
+void* DFE::ReadPlatform() const {
+  return ReadScript(platform_binary_filename_);
 }
 
-void* DFE::ReadVMServiceIO() {
-  return kernel_vmservice_io_ = ReadScript(vmservice_io_binary_filename_);
+void* DFE::ReadVMServiceIO() const {
+  return ReadScript(vmservice_io_binary_filename_);
 }
 
-void* DFE::ReadScript(const char* script_uri) {
+void* DFE::ReadScript(const char* script_uri) const {
   const uint8_t* buffer = NULL;
   intptr_t buffer_length = -1;
   bool result = TryReadKernelFile(script_uri, &buffer, &buffer_length);
@@ -138,7 +128,7 @@ void* DFE::ReadScript(const char* script_uri) {
 
 bool DFE::TryReadKernelFile(const char* script_uri,
                             const uint8_t** kernel_ir,
-                            intptr_t* kernel_ir_size) {
+                            intptr_t* kernel_ir_size) const {
   *kernel_ir = NULL;
   *kernel_ir_size = -1;
   void* script_file = DartUtils::OpenFile(script_uri, false);

runtime/bin/dfe.h
@@ -31,10 +31,9 @@ class DFE {
   bool UsePlatformBinary() const { return platform_binary_filename_ != NULL; }
 
   void* kernel_platform() const { return kernel_platform_; }
-
-  void* kernel_vmservice_io() const { return kernel_vmservice_io_; }
-
-  void clear_kernel_vmservice_io();
+  void set_kernel_platform(void* kernel_platform) {
+    kernel_platform_ = kernel_platform;
+  }
 
   bool kernel_file_specified() const { return kernel_file_specified_; }
   void set_kernel_file_specified(bool value) { kernel_file_specified_ = value; }
@@ -55,16 +54,16 @@ class DFE {
 
   // Reads the platform kernel file.
   // Returns an in memory kernel representation of the platform kernel file.
-  void* ReadPlatform();
+  void* ReadPlatform() const;
 
   // Reads the vmservice_io kernel file.
   // Returns the in memory representation of the vmservice_io kernel file.
-  void* ReadVMServiceIO();
+  void* ReadVMServiceIO() const;
 
   // Reads the script kernel file if specified 'script_uri' is a kernel file.
   // Returns an in memory kernel representation of the specified script is a
   // valid kernel file, false otherwise.
-  void* ReadScript(const char* script_uri);
+  void* ReadScript(const char* script_uri) const;
 
  private:
   // Tries to read [script_uri] as a Kernel IR file.
@@ -74,13 +73,12 @@ class DFE {
   // was returned.
   bool TryReadKernelFile(const char* script_uri,
                          const uint8_t** kernel_ir,
-                         intptr_t* kernel_ir_size);
+                         intptr_t* kernel_ir_size) const;
 
   const char* frontend_filename_;
   char* platform_binary_filename_;
   char* vmservice_io_binary_filename_;
   void* kernel_platform_;
-  void* kernel_vmservice_io_;
   bool kernel_file_specified_;  // Kernel file was specified on the cmd line.
 
   DISALLOW_COPY_AND_ASSIGN(DFE);

runtime/bin/main.cc
@@ -1047,9 +1047,16 @@ static Dart_Isolate CreateAndSetupServiceIsolate(const char* script_uri,
 
 #if !defined(DART_PRECOMPILED_RUNTIME)
   if (dfe.UsePlatformBinary()) {
-    Dart_Handle library = Dart_LoadKernel(dfe.kernel_vmservice_io());
+    // Read vmservice_io kernel file independently of main thread
+    // as Dart_LoadKernel takes ownership.
+    void* kernel_vmservice_io = dfe.ReadVMServiceIO();
+    if (kernel_vmservice_io == NULL) {
+      Log::PrintErr("Could not read dart:vmservice_io binary file.");
+      Platform::Exit(kErrorExitCode);
+    }
+    // Dart_LoadKernel takes ownership.
+    Dart_Handle library = Dart_LoadKernel(kernel_vmservice_io);
     CHECK_RESULT_CLEANUP(library, isolate_data);
-    dfe.clear_kernel_vmservice_io();  // Dart_LoadKernel takes ownership.
     skip_library_load = true;
   }
 #endif  // !defined(DART_PRECOMPILED_RUNTIME)
@@ -1454,16 +1461,24 @@ bool RunMainIsolate(const char* script_name, CommandLineOptions* dart_options) {
     result = Dart_LibraryImportLibrary(isolate_data->builtin_lib(), root_lib,
                                        Dart_Null());
     if ((gen_snapshot_kind == kAppAOT) || (gen_snapshot_kind == kAppJIT)) {
-// Load the embedder's portion of the VM service's Dart code so it will
-// be included in the app snapshot.
-#if defined(DART_PRECOMPILED_RUNTIME)
-      if (!VmService::LoadForGenPrecompiled(NULL)) {
-#else
-      if (!VmService::LoadForGenPrecompiled(dfe.kernel_vmservice_io())) {
+      // Load the embedder's portion of the VM service's Dart code so it will
+      // be included in the app snapshot.
+      void* kernel_vmservice_io = NULL;
+#if !defined(DART_PRECOMPILED_RUNTIME)
+      if (dfe.UsePlatformBinary()) {
+        // Do not cache vmservice_io kernel file as
+        // VmService::LoadForGenPrecompiled takes ownership.
+        kernel_vmservice_io = dfe.ReadVMServiceIO();
+        if (kernel_vmservice_io == NULL) {
+          Log::PrintErr("Could not read dart:vmservice_io binary file.");
+          Platform::Exit(kErrorExitCode);
+        }
+      }
 #endif  // defined(DART_PRECOMPILED_RUNTIME)
+      if (!VmService::LoadForGenPrecompiled(kernel_vmservice_io)) {
         Log::PrintErr("VM service loading failed: %s\n",
                       VmService::GetErrorMessage());
-        exit(kErrorExitCode);
+        Platform::Exit(kErrorExitCode);
       }
     }
 
@@ -1765,14 +1780,12 @@ void main(int argc, char** argv) {
   // step will become redundant once we have the snapshot version
   // of the kernel core/platform libraries.
   if (dfe.UsePlatformBinary()) {
-    if (dfe.ReadPlatform() == NULL) {
+    void* kernel_platform = dfe.ReadPlatform();
+    if (kernel_platform == NULL) {
       Log::PrintErr("The platform binary is not a valid Dart Kernel file.");
       Platform::Exit(kErrorExitCode);
     }
-    if (dfe.ReadVMServiceIO() == NULL) {
-      Log::PrintErr("Could not read dart:vmservice_io binary file.");
-      Platform::Exit(kErrorExitCode);
-    }
+    dfe.set_kernel_platform(kernel_platform);
   }
 #endif
 