53553e8f611a90d04e709b72d3695e509ba04d6e
Paul Berry <paulberry@google.com>
2017-08-22 16:01:11 -0700

    Beef up the covariant generic parameter tear off test case
    This reflects some discussion that happened during the review of how
    tear offs should be handled; it should help us remember to test a few
    additional corner cases.
    
    R=danrubel@google.com
    
    Review-Url: https://codereview.chromium.org/2995383002 .

--- pkg/front_end/testcases/runtime_checks/covariant_generic_parameter_tear_off.dart
+++ pkg/front_end/testcases/runtime_checks/covariant_generic_parameter_tear_off.dart
@@ -7,12 +7,15 @@ library test;
 
 typedef void F<T>(T x);
 
+typedef U G<T, U>(T x);
+
 class C<T> {
-  void f(T /*@checkFormal=semiSafe*/ x) {}
+  void f1(T /*@checkFormal=semiSafe*/ x) {}
+  T f2(List<T> /*@checkFormal=semiSafe*/ x) => x.first;
 }
 
-void g1(C<num> c) {
-  c.f;
+F<num> g1(C<num> c) {
+  return c.f1;
 }
 
 void g2(C<int> c, Object x) {
@@ -20,9 +23,14 @@ void g2(C<int> c, Object x) {
   f /*@checkCall=interface(semiTyped:0)*/ (x);
 }
 
+G<List<num>, num> g3(C<num> c) {
+  return c.f2;
+}
+
 void test() {
   var x = g1(new C<int>());
   x /*@checkCall=interface(semiTyped:0)*/ (1.5);
+  g3(new C<int>());
 }
 
 main() {}
