20066b625a723d0bca4a13afe408e9d6b1442e0e
Carlo Bernaschina <cbernaschina@google.com>
2017-08-29 11:14:54 -0700

    Avoid to run dart code during paused_on_exit
    When a MessageHandler gets to the paused_on_exist state it should just handle service events.
    
    If ports are open though, at the next message the isolate will consider itself still alive and handle any kind of request.
    
    The MessageHandler is now checking for its paused_on_exit state and avoid to handle normal messages in that state.
    
    Closes https://github.com/dart-lang/sdk/issues/30555
    
    R=asiva@google.com
    
    Review-Url: https://codereview.chromium.org/3010503002 .

--- runtime/vm/message_handler.cc
+++ runtime/vm/message_handler.cc
@@ -346,7 +346,18 @@ void MessageHandler::TaskCallback() {
         PausedOnStartLocked(&ml, false);
       }
     }
-#endif
+    if (is_paused_on_exit()) {
+      status = HandleMessages(&ml, false, false);
+      if (ShouldPauseOnExit(status)) {
+        // Still paused.
+        ASSERT(oob_queue_->IsEmpty());
+        task_ = NULL;  // No task in queue.
+        return;
+      } else {
+        PausedOnExitLocked(&ml, false);
+      }
+    }
+#endif  // !defined(PRODUCT)
 
     if (status == kOK) {
       if (start_callback_) {
@@ -373,17 +384,15 @@ void MessageHandler::TaskCallback() {
     if (status != kOK || !HasLivePorts()) {
 #if !defined(PRODUCT)
       if (ShouldPauseOnExit(status)) {
-        if (!is_paused_on_exit()) {
-          if (FLAG_trace_service_pause_events) {
-            OS::PrintErr(
-                "Isolate %s paused before exiting. "
-                "Use the Observatory to release it.\n",
-                name());
-          }
-          PausedOnExitLocked(&ml, true);
-          // More messages may have come in while we released the monitor.
-          status = HandleMessages(&ml, false, false);
+        if (FLAG_trace_service_pause_events) {
+          OS::PrintErr(
+              "Isolate %s paused before exiting. "
+              "Use the Observatory to release it.\n",
+              name());
         }
+        PausedOnExitLocked(&ml, true);
+        // More messages may have come in while we released the monitor.
+        status = HandleMessages(&ml, false, false);
         if (ShouldPauseOnExit(status)) {
           // Still paused.
           ASSERT(oob_queue_->IsEmpty());
