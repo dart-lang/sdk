893b4d4fe9580ccd89bad6886409b6339d6b4d1e
Peter von der Ahé <ahe@google.com>
2017-08-31 13:09:19 +0200

    Add missing copyright on test cases.
    Also rename void-methods.dart to void_methods.dart.
    
    R=johnniwinther@google.com
    
    Review-Url: https://codereview.chromium.org/3006963002 .

--- pkg/front_end/testcases/void_methods.dart
+++ pkg/front_end/testcases/void_methods.dart
@@ -2,10 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-class Foo {}
-
-set foo(x) {}
+class Foo {
+  List list = [1, 2, 3];
+  set first(x) => list[0] = x;
+  operator []=(x, y) => list[x] = y;
+  void clear() => list.clear();
+}
 
 main() {
-  foo = new Foo();
+  new Foo().first = 4;
+  new Foo()[3] = 4;
+  new Foo().clear();
 }
