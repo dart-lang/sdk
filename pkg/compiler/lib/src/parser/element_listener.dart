6cfee12257fb56d036b56ce650ba41658d858852
Dan Rubel <danrubel@google.com>
2017-09-07 08:21:01 -0400

    remove duplicate dart2js external method check
    This removes an error generated by the front-end that is already
    reported by dart2js and fixes a failing test.
    
    Change-Id: I8082cab9546b962a0b3d4011044fe224867e7613
    Reviewed-on: https://dart-review.googlesource.com/3960
    Reviewed-by: Dan Rubel <danrubel@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>
    Commit-Queue: Dan Rubel <danrubel@google.com>

--- pkg/compiler/lib/src/parser/element_listener.dart
+++ pkg/compiler/lib/src/parser/element_listener.dart
@@ -609,10 +609,6 @@ class ElementListener extends Listener {
         }
         return null;
 
-      case "EXTERNAL_WITH_BODY":
-        reportErrorFromToken(token, MessageKind.EXTERNAL_WITH_BODY);
-        return token;
-
       case "UNMATCHED_TOKEN":
         reportErrorFromToken(token, MessageKind.UNMATCHED_TOKEN,
             {"end": arguments["string"], "begin": arguments["token"]});
--- pkg/front_end/lib/src/fasta/fasta_codes_generated.dart
+++ pkg/front_end/lib/src/fasta/fasta_codes_generated.dart
@@ -1024,7 +1024,7 @@ const Code<Null> codeExternalMethodWithBody = messageExternalMethodWithBody;
 const MessageCode messageExternalMethodWithBody = const MessageCode(
     "ExternalMethodWithBody",
     analyzerCode: "EXTERNAL_METHOD_WITH_BODY",
-    dart2jsCode: "EXTERNAL_WITH_BODY",
+    dart2jsCode: "*ignored*",
     message: r"""An external or native method can't have a body.""");
 
 // DO NOT EDIT. THIS FILE IS GENERATED. SEE TOP OF FILE.
