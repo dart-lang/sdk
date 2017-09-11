e40219d4f85f016ae0c78350e6fa9e0c9ffcc66b
Jakob Roland Andersen <jakobr@google.com>
2017-09-11 13:50:51 +0000

    Migrated test batch 124 to Dart 2.0.
    Bug:
    Change-Id: Ic6c699caadddb59b6615490a9360fd3bd8891230
    Reviewed-on: https://dart-review.googlesource.com/4760
    Reviewed-by: Florian Loitsch <floitsch@google.com>
    Commit-Queue: Jakob Roland Andersen <jakobr@google.com>

--- tests/language_2/large_implicit_getter_test.dart
+++ tests/language_2/large_implicit_getter_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 // Dart test program for testing compilation of large implicit getters.
-// VMOptions=--optimization-counter-threshold=10
+// VMOptions=--optimization-counter-threshold=10 --no-background-compilation
 
 List<List> panels = [
   [6853.940039224797, 6050.837897021371],
