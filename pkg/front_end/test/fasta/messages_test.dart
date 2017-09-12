4153aa220a68445fac0e0fb62daed45a878071ae
Dan Rubel <danrubel@google.com>
2017-09-12 18:39:00 +0000

    test messages.yaml - first cut
    This is a first cut at a new test for messages.yaml.
    The long term goal is for this test to:
    - Assert each error code maps to analyzer error code.
    - Assert each error code has at least one example.
    - Analyze each associated example
        to assert that the error code is generated.
    
    Carried forward from https://codereview.chromium.org/3010843002/
    
    Change-Id: I24dea5556896011287ac81036641b547d1107a5a
    Reviewed-on: https://dart-review.googlesource.com/5285
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/front_end/test/fasta/messages_test.dart
@@ -0,0 +1,58 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:yaml/yaml.dart' show loadYaml;
+
+main(List<String> arguments) async {
+  File file = new File.fromUri(Uri.base.resolve('pkg/front_end/messages.yaml'));
+  if (!await file.exists()) {
+    file = new File.fromUri(Uri.base.resolve('messages.yaml'));
+  }
+  Map yaml = loadYaml(await file.readAsString());
+
+  int untestedExampleCount = 0;
+  int missingExamplesCount = 0;
+  int missingAnalyzerCode = 0;
+  List<String> keys = yaml.keys.toList()..sort();
+  for (String name in keys) {
+    var description = yaml[name];
+    while (description is String) {
+      description = yaml[description];
+    }
+    Map map = description;
+
+    int localUntestedExampleCount = countExamples(map, name, 'bytes');
+    localUntestedExampleCount += countExamples(map, name, 'declaration');
+    localUntestedExampleCount += countExamples(map, name, 'expression');
+    localUntestedExampleCount += countExamples(map, name, 'script');
+    localUntestedExampleCount += countExamples(map, name, 'statement');
+    if (localUntestedExampleCount == 0) ++missingExamplesCount;
+    untestedExampleCount += localUntestedExampleCount;
+
+    if (map['analyzerCode'] == null) ++missingAnalyzerCode;
+  }
+
+  print('$untestedExampleCount examples not tested');
+  print('$missingExamplesCount error codes missing examples');
+  print('$missingAnalyzerCode error codes missing analyzer code');
+
+  // TODO(danrubel): Update this to assert each count == 0 and stays zero.
+  return untestedExampleCount == 0 ||
+          missingExamplesCount == 0 ||
+          missingAnalyzerCode == 0
+      ? 1
+      : 0;
+}
+
+int countExamples(Map map, String name, String key) {
+  var example = map[key];
+  if (example == null) return 0;
+  if (example is String) return 1;
+  if (example is List) return example.length;
+  if (example is Map) return example.length;
+
+  throw 'Unknown value for $name $key --> ${example.runtimeType}\n  $example';
+}
