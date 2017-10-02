05ba807d4384bb7778c5e8501a16458ee869d396
Jennifer Messerly <jmesserly@google.com>
2017-10-02 20:45:29 +0000

    fix #30925, optimize `x is T` when x is null
    Change-Id: Id99a2ce821c49c4dcce098fe119aab63e916b3ab
    Reviewed-on: https://dart-review.googlesource.com/9792
    Reviewed-by: Vijay Menon <vsm@google.com>

--- pkg/dev_compiler/tool/input_sdk/private/ddc_runtime/classes.dart
+++ pkg/dev_compiler/tool/input_sdk/private/ddc_runtime/classes.dart
@@ -478,8 +478,7 @@ addTypeTests(ctor, isClass) {
   JS(
       '',
       '''#.is = function is_C(obj) {
-    if (obj != null && obj[#]) return true;
-    return #(obj, this);
+    return obj != null && (obj[#] || #(obj, this));
   }''',
       ctor,
       isClass,
