7bbea1e8db17350fed683c1d8590aacabfeeb5ab
Sigmund Cherem <sigmund@google.com>
2017-10-04 19:25:10 +0000

    change incremental_perf.dart to use a smaller example and only compile dart2js once. This should reduce the time spent on this test.
    Change-Id: I5d6b034887cd636f1d893788a59ed2ad62d9413a
    Reviewed-on: https://dart-review.googlesource.com/9793
    Commit-Queue: Sigmund Cherem <sigmund@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>

--- pkg/front_end/tool/incremental_perf_test.dart
+++ pkg/front_end/tool/incremental_perf_test.dart
@@ -4,33 +4,11 @@
 
 /// Test to ensure that incremental_perf.dart is running without errors.
 
-import 'dart:convert';
 import 'dart:io';
 import 'incremental_perf.dart' as m;
 
+/// Run the incremental compiler on a couple examples.
 main() async {
-  var entryUri = Platform.script.resolve('../../compiler/lib/src/dart2js.dart');
-  var tmp = Directory.systemTemp.createTempSync();
-  var jsonUri = tmp.uri.resolve('edits.json');
-  var editedFile =
-      Platform.script.resolve('../../compiler/lib/src/library_loader.dart');
-  new File.fromUri(jsonUri).writeAsStringSync(JSON.encode([
-    // iteration 1: no edits
-    [],
-    // iteration 2: a single edit
-    [
-      ['$editedFile', 'root=', 'root2=']
-    ],
-    // iteration 3: no edits
-    [],
-    // iteration 4: a single edit reverting to a known state from iteration 1,
-    // if the incremental compiler is caching old results, this should not
-    // require much work.
-    [
-      ['$editedFile', 'root2=', 'root=']
-    ],
-  ]));
-
   // Derive the outline.dill location from the vm.  Depending on whether tests
   // are run with --use-sdk, we resolve the outline file slightly differently.
   var dartVm = Uri.parse(Platform.resolvedExecutable);
@@ -42,6 +20,67 @@ main() async {
     // TODO(sigmund): switch to outline.dill (issue #29881)
     sdkOutline = dir.resolve('patched_sdk/platform.dill');
   }
-  await m.main(['--sdk-summary', '$sdkOutline', '$entryUri', '$jsonUri']);
+
+  var tmp = Directory.systemTemp.createTempSync();
+  await runSmallExample(sdkOutline, tmp.uri);
+  await runLargeExample(sdkOutline, tmp.uri);
   tmp.deleteSync(recursive: true);
+}
+
+/// Creates a small example with a few files in a temporary folder, and runs the
+/// benchmark on it. The example includes several edits in sequence.
+runSmallExample(Uri sdkOutline, Uri tmpUri) async {
+  Map<String, String> files = {
+    'a.dart': '''
+        import 'b.dart';
+        main() => b();
+    ''',
+    'b.dart': '''
+        import 'c.dart';
+        b() => new C().m1();
+    ''',
+    'c.dart': '''
+        class C {
+          m1() => print('hello1');
+          m2() => print('hello2');
+        }
+    ''',
+    //
+    // iteration 1: no edits
+    // iteration 2: edit b.dart (a.dart may recompile, c.dart unchanged)
+    // iteration 3: no edits
+    // iteration 4: edit c.dart (may recompile everything)
+    // iteration 4: edit b.dart and c.dart to revert to state of iteration 1
+    //              may skip recompile if compiler cached old results.
+    'edits.json': '''[
+      [],
+      [["${tmpUri.resolve('b.dart')}", "m1()", "m2()"]],
+      [],
+      [["${tmpUri.resolve('c.dart')}", "hello1", "hello3"]],
+      [
+        ["${tmpUri.resolve('b.dart')}", "m2()", "m1()"],
+        ["${tmpUri.resolve('c.dart')}", "hello3", "hello1"]
+      ]
+    ]''',
+  };
+
+  files.forEach((name, contents) {
+    new File.fromUri(tmpUri.resolve(name)).writeAsStringSync(contents);
+  });
+  var entryUri = tmpUri.resolve('a.dart');
+  var jsonUri = tmpUri.resolve('edits.json');
+  await m.main(['--sdk-summary', '$sdkOutline', '$entryUri', '$jsonUri']);
+}
+
+/// Create an example using the dart2js codebase. It can take a while to
+/// compile dart2js, so to avoid timeouts we only run a single iteration of the
+/// incremental compiler on this codebase.
+runLargeExample(Uri sdkOutline, Uri tmpUri) async {
+  var jsonUri = tmpUri.resolve('edits.json');
+  var loaderFile =
+      Platform.script.resolve('../../compiler/lib/src/library_loader.dart');
+  new File.fromUri(jsonUri)
+      .writeAsStringSync('[[["$loaderFile", "root=", "root2="]]]');
+  var entryUri = Platform.script.resolve('../../compiler/lib/src/dart2js.dart');
+  await m.main(['--sdk-summary', '$sdkOutline', '$entryUri', '$jsonUri']);
 }
