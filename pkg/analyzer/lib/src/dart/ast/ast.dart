2d30c30f714cb3f22676b69bc819de32f6cec146
Konstantin Shcheglov <scheglov@google.com>
2017-10-05 15:36:40 +0000

    Add EmptyStatement.isSynthetic getter.
    R=brianwilkerson@google.com
    
    Bug:
    Change-Id: I16b6457834321fb89c04feb4c50915161765ea6d
    Reviewed-on: https://dart-review.googlesource.com/11345
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/ast/ast.dart
+++ pkg/analyzer/lib/src/dart/ast/ast.dart
@@ -3767,6 +3767,9 @@ class EmptyStatementImpl extends StatementImpl implements EmptyStatement {
   Token get endToken => semicolon;
 
   @override
+  bool get isSynthetic => semicolon.isSynthetic;
+
+  @override
   E accept<E>(AstVisitor<E> visitor) => visitor.visitEmptyStatement(this);
 
   @override
@@ -7960,19 +7963,6 @@ class NodeListImpl<E extends AstNode> extends Object
     throw new UnsupportedError("Cannot resize NodeList.");
   }
 
-  /// This is non-API and may be changed or removed at any point.
-  ///
-  /// Changes the length of this list
-  /// If [newLength] is greater than the current length,
-  /// entries are initialized to `null`.
-  ///
-  /// This list should NOT contain any `null` elements,
-  /// so be sure to immediately follow a call to this method with calls
-  /// to replace all the `null` elements with non-`null` elements.
-  void setLength(int newLength) {
-    _elements.length = newLength;
-  }
-
   @override
   AstNode get owner => _owner;
 
@@ -8057,6 +8047,19 @@ class NodeListImpl<E extends AstNode> extends Object
     E removedNode = _elements[index];
     _elements.removeAt(index);
     return removedNode;
+  }
+
+  /// This is non-API and may be changed or removed at any point.
+  ///
+  /// Changes the length of this list
+  /// If [newLength] is greater than the current length,
+  /// entries are initialized to `null`.
+  ///
+  /// This list should NOT contain any `null` elements,
+  /// so be sure to immediately follow a call to this method with calls
+  /// to replace all the `null` elements with non-`null` elements.
+  void setLength(int newLength) {
+    _elements.length = newLength;
   }
 }
 
