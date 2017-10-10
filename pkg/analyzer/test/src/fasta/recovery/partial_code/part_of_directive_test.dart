7b4e3c3ba20f4b9815dabcd88c033cf1a500c881
Brian Wilkerson <brianwilkerson@google.com>
2017-10-10 13:20:11 +0000

    Last of partial code tests
    Change-Id: I0e451229fc9650e1e30b57bc0cbeae97b66496d3
    Reviewed-on: https://dart-review.googlesource.com/12342
    Reviewed-by: Dan Rubel <danrubel@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/analyzer/test/src/fasta/recovery/partial_code/part_of_directive_test.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/src/dart/error/syntactic_errors.dart';
+
+import 'partial_code_support.dart';
+
+main() {
+  new PartOfDirectivesTest().buildAll();
+}
+
+class PartOfDirectivesTest extends PartialCodeTest {
+  buildAll() {
+    buildTests(
+        'part_of_directive',
+        [
+          new TestDescriptor(
+              'keyword',
+              'part of',
+              [
+                ParserErrorCode.MISSING_NAME_IN_PART_OF_DIRECTIVE,
+                ParserErrorCode.EXPECTED_TOKEN
+              ],
+              'part of _s_;',
+              allFailing: true),
+          new TestDescriptor('name', 'part of lib',
+              [ParserErrorCode.EXPECTED_TOKEN], 'library lib;',
+              allFailing: true),
+          new TestDescriptor(
+              'nameDot',
+              'part of lib.',
+              [
+                ParserErrorCode.MISSING_IDENTIFIER,
+                ParserErrorCode.EXPECTED_TOKEN
+              ],
+              'part of lib._s_;',
+              allFailing: true),
+          new TestDescriptor('nameDotName', 'part of lib.a',
+              [ParserErrorCode.EXPECTED_TOKEN], 'part of lib.a;',
+              allFailing: true),
+          new TestDescriptor('emptyUri', "part of ''",
+              [ParserErrorCode.EXPECTED_TOKEN], "part of '';",
+              allFailing: true),
+          new TestDescriptor('uri', "part of 'a.dart'",
+              [ParserErrorCode.EXPECTED_TOKEN], "part of 'a.dart';",
+              allFailing: true),
+        ],
+        PartialCodeTest.declarationSuffixes);
+  }
+}
