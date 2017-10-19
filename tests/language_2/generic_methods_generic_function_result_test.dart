7a540d2f859b6b73906c6a60b7478e82c57f8038
Bob Nystrom <rnystrom@google.com>
2017-10-19 16:19:16 +0000

    Remove VMOptions in tests for options that are now on by default.
    Change-Id: Ic12edd3e76ba1f8b9fb096c96e310e7253cc6ac6
    Reviewed-on: https://dart-review.googlesource.com/14722
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Régis Crelier <regis@google.com>

--- tests/language_2/generic_methods_generic_function_result_test.dart
+++ tests/language_2/generic_methods_generic_function_result_test.dart
@@ -1,8 +1,8 @@
 // Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-//
-// VMOptions=--generic-method-syntax,--error-on-bad-type
+
+// VMOptions=--error-on-bad-type
 
 // Verify that function type parameter S can be resolved in bar's result type.
 // Verify that generic function types are not allowed as type arguments.
