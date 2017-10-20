dart-lang/sdk

commit abefb7b432ed2bea537ffe6f4f62d17ec18ac0f7

[kernel] Assign proper types and interface targets to nodes created by async transformer.
Bug: https://github.com/dart-lang/sdk/issues/31070
Change-Id: Iaedbd425fcb6d243c0bfa93495335658553a9301
Reviewed-on: https://dart-review.googlesource.com/14481
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
Reviewed-by: Kevin Millikin <kmillikin@google.com>

pkg/kernel/lib/core_types.dart
@@ -77,6 +77,8 @@ class CoreTypes {
   Class _completerClass;
   Class _futureOrClass;
   Procedure _completerSyncConstructor;
+  Procedure _completerComplete;
+  Procedure _completerCompleteError;
   Procedure _futureMicrotaskConstructor;
   Constructor _syncIterableDefaultConstructor;
   Constructor _streamIteratorDefaultConstructor;
@@ -100,9 +102,37 @@ class CoreTypes {
     return _asyncLibrary ??= _index.getLibrary('dart:async');
   }
 
+  Member get asyncStarStreamControllerAdd {
+    return _index.getMember('dart:async', '_AsyncStarStreamController', 'add');
+  }
+
+  Member get asyncStarStreamControllerAddError {
+    return _index.getMember(
+        'dart:async', '_AsyncStarStreamController', 'addError');
+  }
+
+  Member get asyncStarStreamControllerAddStream {
+    return _index.getMember(
+        'dart:async', '_AsyncStarStreamController', 'addStream');
+  }
+
+  Class get asyncStarStreamControllerClass {
+    return _index.getClass('dart:async', '_AsyncStarStreamController');
+  }
+
+  Member get asyncStarStreamControllerClose {
+    return _index.getMember(
+        'dart:async', '_AsyncStarStreamController', 'close');
+  }
+
   Constructor get asyncStarStreamControllerDefaultConstructor {
     return _asyncStarStreamControllerDefaultConstructor ??=
         _index.getMember('dart:async', '_AsyncStarStreamController', '');
+  }
+
+  Member get asyncStarStreamControllerStream {
+    return _index.getMember(
+        'dart:async', '_AsyncStarStreamController', 'get:stream');
   }
 
   Procedure get asyncStackTraceHelperProcedure {
@@ -131,6 +161,20 @@ class CoreTypes {
   Procedure get completerSyncConstructor {
     return _completerSyncConstructor ??=
         _index.getMember('dart:async', 'Completer', 'sync');
+  }
+
+  Procedure get completerComplete {
+    return _completerComplete ??=
+        _index.getMember('dart:async', 'Completer', 'complete');
+  }
+
+  Procedure get completerCompleteError {
+    return _completerCompleteError ??=
+        _index.getMember('dart:async', 'Completer', 'completeError');
+  }
+
+  Member get completerFuture {
+    return _index.getMember('dart:async', 'Completer', 'get:future');
   }
 
   Library get coreLibrary {
@@ -255,9 +299,25 @@ class CoreTypes {
     return _streamClass ??= _index.getClass('dart:async', 'Stream');
   }
 
+  Member get streamIteratorCancel {
+    return _index.getMember('dart:async', '_StreamIterator', 'cancel');
+  }
+
+  Class get streamIteratorClass {
+    return _index.getClass('dart:async', '_StreamIterator');
+  }
+
   Constructor get streamIteratorDefaultConstructor {
     return _streamIteratorDefaultConstructor ??=
         _index.getMember('dart:async', '_StreamIterator', '');
+  }
+
+  Member get streamIteratorMoveNext {
+    return _index.getMember('dart:async', '_StreamIterator', 'moveNext');
+  }
+
+  Member get streamIteratorCurrent {
+    return _index.getMember('dart:async', '_StreamIterator', 'get:current');
   }
 
   Class get stringClass {
@@ -271,6 +331,18 @@ class CoreTypes {
   Constructor get syncIterableDefaultConstructor {
     return _syncIterableDefaultConstructor ??=
         _index.getMember('dart:core', '_SyncIterable', '');
+  }
+
+  Class get syncIteratorClass {
+    return _index.getClass('dart:core', '_SyncIterator');
+  }
+
+  Member get syncIteratorCurrent {
+    return _index.getMember('dart:core', '_SyncIterator', '_current');
+  }
+
+  Member get syncIteratorIsYieldEach {
+    return _index.getMember('dart:core', '_SyncIterator', 'isYieldEach');
   }
 
   Class get typeClass {

commit 9cd7bc724dfb6ed13c51dc86272963d99abd3fd1

[VM, Kernel] Fix sync* implementation in checked mode
Change abefb7b432ed2bea537ffe6f4f62d17ec18ac0f7 introduced a typing issue
instead of fixing it: _SyncIterator<T>._current was actually used to return
either a value of type T (for yield) or value of type Iterable<T> (for yield*)
from the move callback.

This change refactors implementation of _SyncIterator in such a way that
_current is only used to return value for yield and a separate field
_yieldEachIterable is used to return Iterable<T> for yield*.

Change-Id: I3e3c832bbc8986d6976ddbb0856a1b5a127d845b
Reviewed-on: https://dart-review.googlesource.com/15542
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
Reviewed-by: Martin Kustermann <kustermann@google.com>

pkg/kernel/lib/core_types.dart
@@ -341,8 +341,8 @@ class CoreTypes {
     return _index.getMember('dart:core', '_SyncIterator', '_current');
   }
 
-  Member get syncIteratorIsYieldEach {
-    return _index.getMember('dart:core', '_SyncIterator', 'isYieldEach');
+  Member get syncIteratorYieldEachIterable {
+    return _index.getMember('dart:core', '_SyncIterator', '_yieldEachIterable');
   }
 
   Class get typeClass {

pkg/kernel/lib/transformations/continuation.dart
@@ -217,21 +217,19 @@ class SyncStarFunctionRewriter extends ContinuationRewriterBase {
 
     var statements = <Statement>[];
     if (node.isYieldStar) {
-      var markYieldEach = new ExpressionStatement(new PropertySet(
+      statements.add(new ExpressionStatement(new PropertySet(
           new VariableGet(iteratorVariable),
-          new Name("isYieldEach"),
-          new BoolLiteral(true),
-          helper.syncIteratorIsYieldEach));
-      statements.add(markYieldEach);
+          new Name("_yieldEachIterable", helper.coreLibrary),
+          transformedExpression,
+          helper.syncIteratorYieldEachIterable)));
+    } else {
+      statements.add(new ExpressionStatement(new PropertySet(
+          new VariableGet(iteratorVariable),
+          new Name("_current", helper.coreLibrary),
+          transformedExpression,
+          helper.syncIteratorCurrent)));
     }
 
-    var setCurrentIteratorValue = new ExpressionStatement(new PropertySet(
-        new VariableGet(iteratorVariable),
-        new Name("_current", helper.coreLibrary),
-        transformedExpression,
-        helper.syncIteratorCurrent));
-
-    statements.add(setCurrentIteratorValue);
     statements.add(createContinuationPoint(new BoolLiteral(true)));
     return new Block(statements);
   }
@@ -979,7 +977,7 @@ class HelperNodes {
   final Constructor syncIterableConstructor;
   final Class syncIteratorClass;
   final Member syncIteratorCurrent;
-  final Member syncIteratorIsYieldEach;
+  final Member syncIteratorYieldEachIterable;
 
   HelperNodes._(
       this.asyncErrorWrapper,
@@ -1016,7 +1014,7 @@ class HelperNodes {
       this.syncIterableConstructor,
       this.syncIteratorClass,
       this.syncIteratorCurrent,
-      this.syncIteratorIsYieldEach);
+      this.syncIteratorYieldEachIterable);
 
   factory HelperNodes.fromCoreTypes(CoreTypes coreTypes) {
     return new HelperNodes._(
@@ -1054,6 +1052,6 @@ class HelperNodes {
         coreTypes.syncIterableDefaultConstructor,
         coreTypes.syncIteratorClass,
         coreTypes.syncIteratorCurrent,
-        coreTypes.syncIteratorIsYieldEach);
+        coreTypes.syncIteratorYieldEachIterable);
   }
 }

runtime/lib/core_patch.dart
@@ -103,11 +103,11 @@ class _EnumHelper {
 // implement sync* generator functions. A sync* generator allocates
 // and returns a new _SyncIterable object.
 
-typedef bool _SyncGeneratorCallback(Iterator iterator);
+typedef bool _SyncGeneratorCallback<T>(_SyncIterator<T> iterator);
 
 class _SyncIterable<T> extends IterableBase<T> {
   // _moveNextFn is the closurized body of the generator function.
-  final _SyncGeneratorCallback _moveNextFn;
+  final _SyncGeneratorCallback<T> _moveNextFn;
 
   const _SyncIterable(this._moveNextFn);
 
@@ -115,18 +115,21 @@ class _SyncIterable<T> extends IterableBase<T> {
     // Note: _Closure._clone returns _Closure which is not related to
     // _SyncGeneratorCallback, which means we need explicit cast.
     return new _SyncIterator<T>(
-        (_moveNextFn as _Closure)._clone() as _SyncGeneratorCallback);
+        (_moveNextFn as _Closure)._clone() as _SyncGeneratorCallback<T>);
   }
 }
 
 class _SyncIterator<T> implements Iterator<T> {
-  bool isYieldEach; // Set by generated code for the yield* statement.
-  Iterator<T> yieldEachIterator;
-  T _current; // Set by generated code for the yield and yield* statement.
-  _SyncGeneratorCallback _moveNextFn;
+  _SyncGeneratorCallback<T> _moveNextFn;
+  Iterator<T> _yieldEachIterator;
+
+  // These two fields are set by generated code for the yield and yield*
+  // statement.
+  T _current;
+  Iterable<T> _yieldEachIterable;
 
   T get current =>
-      yieldEachIterator != null ? yieldEachIterator.current : _current;
+      _yieldEachIterator != null ? _yieldEachIterator.current : _current;
 
   _SyncIterator(this._moveNextFn);
 
@@ -135,23 +138,24 @@ class _SyncIterator<T> implements Iterator<T> {
       return false;
     }
     while (true) {
-      if (yieldEachIterator != null) {
-        if (yieldEachIterator.moveNext()) {
+      if (_yieldEachIterator != null) {
+        if (_yieldEachIterator.moveNext()) {
           return true;
         }
-        yieldEachIterator = null;
+        _yieldEachIterator = null;
       }
-      isYieldEach = false;
-      // _moveNextFn() will update the values of isYieldEach and _current.
+      // _moveNextFn() will update the values of _yieldEachIterable
+      //  and _current.
       if (!_moveNextFn(this)) {
         _moveNextFn = null;
         _current = null;
         return false;
       }
-      if (isYieldEach) {
+      if (_yieldEachIterable != null) {
         // Spec mandates: it is a dynamic error if the class of [the object
         // returned by yield*] does not implement Iterable.
-        yieldEachIterator = (_current as Iterable<T>).iterator;
+        _yieldEachIterator = _yieldEachIterable.iterator;
+        _yieldEachIterable = null;
         _current = null;
         continue;
       }

runtime/vm/parser.cc
@@ -10365,20 +10365,18 @@ AstNode* Parser::ParseYieldStatement() {
     LocalVariable* iterator_param =
         LookupLocalScope(Symbols::IteratorParameter());
     ASSERT(iterator_param != NULL);
-    // Generate :iterator.current = expr;
     AstNode* iterator =
         new (Z) LoadLocalNode(TokenPosition::kNoSource, iterator_param);
-    AstNode* store_current = new (Z) InstanceSetterNode(
-        TokenPosition::kNoSource, iterator,
-        Library::PrivateCoreLibName(Symbols::_current()), expr);
-    yield->AddNode(store_current);
     if (is_yield_each) {
-      // Generate :iterator.isYieldEach = true;
-      AstNode* set_is_yield_each = new (Z)
-          InstanceSetterNode(TokenPosition::kNoSource, iterator,
-                             String::ZoneHandle(Symbols::IsYieldEach().raw()),
-                             new (Z) LiteralNode(TokenPos(), Bool::True()));
-      yield->AddNode(set_is_yield_each);
+      // Generate :iterator._yieldEachIterable = expr;
+      yield->AddNode(new (Z) InstanceSetterNode(
+          TokenPosition::kNoSource, iterator,
+          Library::PrivateCoreLibName(Symbols::_yieldEachIterable()), expr));
+    } else {
+      // Generate :iterator._current = expr;
+      yield->AddNode(new (Z) InstanceSetterNode(
+          TokenPosition::kNoSource, iterator,
+          Library::PrivateCoreLibName(Symbols::_current()), expr));
     }
     AwaitMarkerNode* await_marker = new (Z) AwaitMarkerNode(
         async_temp_scope_, current_block_->scope, TokenPosition::kNoSource);

runtime/vm/symbols.h
@@ -37,7 +37,7 @@ class ObjectPointerVisitor;
   V(Current, "current")                                                        \
   V(_current, "_current")                                                      \
   V(MoveNext, "moveNext")                                                      \
-  V(IsYieldEach, "isYieldEach")                                                \
+  V(_yieldEachIterable, "_yieldEachIterable")                                  \
   V(Value, "value")                                                            \
   V(_EnumHelper, "_EnumHelper")                                                \
   V(_SyncIterable, "_SyncIterable")                                            \

pkg/front_end/test/src/incremental/mock_sdk.dart
@@ -241,8 +241,8 @@ void print(Object o) {}
 abstract class _SyncIterable<T> implements Iterable<T> {}
 
 class _SyncIterator<T> implements Iterator<T> {
-  bool isYieldEach;
   T _current;
+  Iterable<T> _yieldEachIterable;
 }
 
 class _InvocationMirror {}

pkg/front_end/testcases/shaker/empty_program.dart.outline.expect
@@ -193,10 +193,13 @@ import "dart:collection" as col;
 import "dart:_internal" as _in;
 import "dart:math" as math;
 
-typedef _SyncGeneratorCallback = (self::Iterator<dynamic>) → self::bool;
 class _SyncIterable<T extends self::Object> extends col::IterableBase<self::_SyncIterable::T> {
-  const constructor •((self::Iterator<dynamic>) → self::bool _moveNextFn) → void;
+  const constructor •((self::_SyncIterator<self::_SyncIterable::T>) → self::bool _moveNextFn) → void;
   get iterator() → self::Iterator<self::_SyncIterable::T>;
+}
+class _SyncIterator<T extends self::Object> extends self::Object implements self::Iterator<self::_SyncIterator::T> {
+  get current() → self::_SyncIterator::T;
+  method moveNext() → self::bool;
 }
 class _InvocationMirror extends self::Object implements self::Invocation {
   constructor •(self::String _functionName, self::List<dynamic> _argumentsDescriptor, self::List<dynamic> _arguments, self::bool _isSuperInvocation) → void;

pkg/front_end/testcases/shaker/empty_program.dart.shaker.expect
@@ -149,10 +149,12 @@ library dart:math:
 
 library dart:core:
   - member print
-  - typedef _SyncGeneratorCallback
   - class _SyncIterable
     - (default constructor)
     - iterator
+  - class _SyncIterator
+    - current
+    - moveNext
   - class _InvocationMirror
     - (default constructor)
     - memberName