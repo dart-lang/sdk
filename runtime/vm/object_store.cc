dart-lang/sdk

commit 4985165e76a5a5b530e9ed4fab71b352eeb5c3d9

[VM] Make "Hello, World!" run in strong mode (through DFE).
Includes the following changes:

- Disable type checking in the service isolate when strong mode is 
turned on. We don't yet have strong mode clean vmservice Kernel 
binary available (Issue #31203);
- Introduce new Dart VM API to allow creating List<T> (T in {String, int}).
Current API only allows to create List<dynamic> and in strong mode 
List<dynamic> is not assignable to List<T>. For now we limit this API to 
a fixed number of core types for performance considerations (type 
arguments can be canonicalized and cache ahead of time). We will
extend the API allowing creation of arbitrary List<T> if we will
discover the need for it later;
- Use this new API to fix CreateRuntimeOptions to create List<String>
and not List<dynamic>;
- Likewise fix OneByteString_splitWithCharCode to return List<String>
and not List<dynamic>;

Additionally this CL refactors how getters and setters are created
in object_store.h removing 500 lines of largely boilerplate code.

Bug: https://github.com/dart-lang/sdk/issues/31052
Change-Id: I3fdcd2d54ff3f307db0913c67a7c2f009ea9d7a7
Reviewed-on: https://dart-review.googlesource.com/15884
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
Reviewed-by: Siva Annamalai <asiva@google.com>

runtime/vm/object_store.cc
@@ -138,9 +138,9 @@ void ObjectStore::PrintToJSONObject(JSONObject* jsobj) {
     JSONObject fields(jsobj, "fields");
     Object& value = Object::Handle();
 #define PRINT_OBJECT_STORE_FIELD(type, name)                                   \
-  value = name;                                                                \
-  fields.AddProperty(#name, value);
-    OBJECT_STORE_FIELD_LIST(PRINT_OBJECT_STORE_FIELD);
+  value = name##_;                                                             \
+  fields.AddProperty(#name "_", value);
+    OBJECT_STORE_FIELD_LIST(PRINT_OBJECT_STORE_FIELD, PRINT_OBJECT_STORE_FIELD);
 #undef PRINT_OBJECT_STORE_FIELD
   }
 }

runtime/bin/main.cc
@@ -84,7 +84,8 @@ static Dart_Isolate main_isolate = NULL;
 
 static Dart_Handle CreateRuntimeOptions(CommandLineOptions* options) {
   int options_count = options->count();
-  Dart_Handle dart_arguments = Dart_NewList(options_count);
+  Dart_Handle dart_arguments =
+      Dart_NewListOf(Dart_CoreType_String, options_count);
   if (Dart_IsError(dart_arguments)) {
     return dart_arguments;
   }

runtime/include/dart_api.h
@@ -1829,7 +1829,7 @@ DART_EXPORT Dart_Handle Dart_StringGetProperties(Dart_Handle str,
  */
 
 /**
- * Returns a List of the desired length.
+ * Returns a List<dynamic> of the desired length.
  *
  * \param length The length of the list.
  *
@@ -1837,6 +1837,25 @@ DART_EXPORT Dart_Handle Dart_StringGetProperties(Dart_Handle str,
  *   an error handle.
  */
 DART_EXPORT Dart_Handle Dart_NewList(intptr_t length);
+
+typedef enum {
+  Dart_CoreType_Dynamic,
+  Dart_CoreType_Int,
+  Dart_CoreType_String,
+} Dart_CoreType_Id;
+
+/**
+ * Returns a List of the desired length with the desired element type.
+ *
+ * \param element_type_id The type of elements of the list.
+ *
+ * \param length The length of the list.
+ *
+ * \return The List object if no error occurs. Otherwise returns
+ *   an error handle.
+ */
+DART_EXPORT Dart_Handle Dart_NewListOf(Dart_CoreType_Id element_type_id,
+                                       intptr_t length);
 
 /**
  * Gets the length of a List.

runtime/lib/string.cc
@@ -10,6 +10,7 @@
 #include "vm/isolate.h"
 #include "vm/native_entry.h"
 #include "vm/object.h"
+#include "vm/object_store.h"
 #include "vm/symbols.h"
 #include "vm/unicode.h"
 
@@ -280,6 +281,8 @@ DEFINE_NATIVE_ENTRY(OneByteString_splitWithCharCode, 2) {
   str = OneByteString::SubStringUnchecked(receiver, start, (i - start),
                                           Heap::kNew);
   result.Add(str);
+  result.SetTypeArguments(TypeArguments::Handle(
+      zone, isolate->object_store()->type_argument_string()));
   return result.raw();
 }
 

runtime/vm/dart_api_impl.cc
@@ -2474,10 +2474,36 @@ DART_EXPORT Dart_Handle Dart_StringGetProperties(Dart_Handle object,
 // --- Lists ---
 
 DART_EXPORT Dart_Handle Dart_NewList(intptr_t length) {
+  return Dart_NewListOf(Dart_CoreType_Dynamic, length);
+}
+
+static RawTypeArguments* TypeArgumentsForElementType(
+    ObjectStore* store,
+    Dart_CoreType_Id element_type_id) {
+  switch (element_type_id) {
+    case Dart_CoreType_Dynamic:
+      return TypeArguments::null();
+    case Dart_CoreType_Int:
+      return store->type_argument_int();
+    case Dart_CoreType_String:
+      return store->type_argument_string();
+  }
+  UNREACHABLE();
+  return NULL;
+}
+
+DART_EXPORT Dart_Handle Dart_NewListOf(Dart_CoreType_Id element_type_id,
+                                       intptr_t length) {
   DARTSCOPE(Thread::Current());
   CHECK_LENGTH(length, Array::kMaxElements);
   CHECK_CALLBACK_STATE(T);
-  return Api::NewHandle(T, Array::New(length));
+  const Array& arr = Array::Handle(Z, Array::New(length));
+  if (element_type_id != Dart_CoreType_Dynamic) {
+    arr.SetTypeArguments(TypeArguments::Handle(
+        Z, TypeArgumentsForElementType(T->isolate()->object_store(),
+                                       element_type_id)));
+  }
+  return Api::NewHandle(T, arr.raw());
 }
 
 #define GET_LIST_LENGTH(zone, type, obj, len)                                  \

runtime/vm/object.cc
@@ -1258,6 +1258,7 @@ RawError* Object::Init(Isolate* isolate, kernel::Program* kernel_program) {
     Type& type = Type::Handle(zone);
     Array& array = Array::Handle(zone);
     Library& lib = Library::Handle(zone);
+    TypeArguments& type_args = TypeArguments::Handle(zone);
 
     // All RawArray fields will be initialized to an empty array, therefore
     // initialize array class first.
@@ -1733,6 +1734,19 @@ RawError* Object::Init(Isolate* isolate, kernel::Program* kernel_program) {
     // Consider removing when/if Null becomes an ordinary class.
     type = object_store->object_type();
     cls.set_super_type(type);
+
+    // Create and cache commonly used type arguments <int> and <String>
+    type_args = TypeArguments::New(1);
+    type = object_store->int_type();
+    type_args.SetTypeAt(0, type);
+    type_args.Canonicalize();
+    object_store->set_type_argument_int(type_args);
+
+    type_args = TypeArguments::New(1);
+    type = object_store->string_type();
+    type_args.SetTypeAt(0, type);
+    type_args.Canonicalize();
+    object_store->set_type_argument_string(type_args);
 
     // Finish the initialization by compiling the bootstrap scripts containing
     // the base interfaces and the implementation of the internal classes.

runtime/vm/service_isolate.cc
@@ -331,6 +331,12 @@ class RunServiceTask : public ThreadPool::Task {
 
     Dart_IsolateFlags api_flags;
     Isolate::FlagsInitialize(&api_flags);
+    if (FLAG_strong) {
+      // TODO(dartbug.com/31203) currently we don't have a strong version of
+      // vm service so disable type checking in the service completely.
+      api_flags.enable_type_checks = false;
+      api_flags.enable_asserts = false;
+    }
 
     isolate = reinterpret_cast<Isolate*>(create_callback(
         ServiceIsolate::kName, NULL, NULL, NULL, &api_flags, NULL, &error));

commit eaf97656e95819e5d8fe3fec83330bcb0fa9c7ed

[VM] Fix DEBUG mode builds broken by 4985165e76a5a5b530e9ed4fab71b352eeb5c3d9
Initialize ObjectStore fields using OBJECT_STORE_FIELD_LIST instead 
of enumerating them by hand.

Change-Id: Iaf13104380d0ea08d25a19d5f886eb56f3a3ff17
Reviewed-on: https://dart-review.googlesource.com/16260
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
Reviewed-by: Martin Kustermann <kustermann@google.com>

runtime/vm/object_store.cc
@@ -15,100 +15,11 @@
 
 namespace dart {
 
-ObjectStore::ObjectStore()
-    : object_class_(Class::null()),
-      object_type_(Type::null()),
-      null_class_(Class::null()),
-      null_type_(Type::null()),
-      function_type_(Type::null()),
-      type_type_(Type::null()),
-      closure_class_(Class::null()),
-      number_type_(Type::null()),
-      int_type_(Type::null()),
-      integer_implementation_class_(Class::null()),
-      int64_type_(Type::null()),
-      smi_class_(Class::null()),
-      smi_type_(Type::null()),
-      mint_class_(Class::null()),
-      mint_type_(Type::null()),
-      bigint_class_(Class::null()),
-      double_class_(Class::null()),
-      double_type_(Type::null()),
-      float32x4_type_(Type::null()),
-      int32x4_type_(Type::null()),
-      float64x2_type_(Type::null()),
-      string_type_(Type::null()),
-      compiletime_error_class_(Class::null()),
-      future_class_(Class::null()),
-      completer_class_(Class::null()),
-      stream_iterator_class_(Class::null()),
-      symbol_class_(Class::null()),
-      one_byte_string_class_(Class::null()),
-      two_byte_string_class_(Class::null()),
-      external_one_byte_string_class_(Class::null()),
-      external_two_byte_string_class_(Class::null()),
-      bool_type_(Type::null()),
-      bool_class_(Class::null()),
-      array_class_(Class::null()),
-      array_type_(Type::null()),
-      immutable_array_class_(Class::null()),
-      growable_object_array_class_(Class::null()),
-      linked_hash_map_class_(Class::null()),
-      float32x4_class_(Class::null()),
-      int32x4_class_(Class::null()),
-      float64x2_class_(Class::null()),
-      error_class_(Class::null()),
-      weak_property_class_(Class::null()),
-      symbol_table_(Array::null()),
-      canonical_types_(Array::null()),
-      canonical_type_arguments_(Array::null()),
-      async_library_(Library::null()),
-      builtin_library_(Library::null()),
-      core_library_(Library::null()),
-      collection_library_(Library::null()),
-      convert_library_(Library::null()),
-      developer_library_(Library::null()),
-      _internal_library_(Library::null()),
-      isolate_library_(Library::null()),
-      math_library_(Library::null()),
-      mirrors_library_(Library::null()),
-      native_wrappers_library_(Library::null()),
-      profiler_library_(Library::null()),
-      root_library_(Library::null()),
-      typed_data_library_(Library::null()),
-      _vmservice_library_(Library::null()),
-      libraries_(GrowableObjectArray::null()),
-      libraries_map_(Array::null()),
-      closure_functions_(GrowableObjectArray::null()),
-      pending_classes_(GrowableObjectArray::null()),
-      pending_deferred_loads_(GrowableObjectArray::null()),
-      resume_capabilities_(GrowableObjectArray::null()),
-      exit_listeners_(GrowableObjectArray::null()),
-      error_listeners_(GrowableObjectArray::null()),
-      stack_overflow_(Instance::null()),
-      out_of_memory_(Instance::null()),
-      preallocated_unhandled_exception_(UnhandledException::null()),
-      preallocated_stack_trace_(StackTrace::null()),
-      lookup_port_handler_(Function::null()),
-      empty_uint32_array_(TypedData::null()),
-      handle_message_function_(Function::null()),
-      simple_instance_of_function_(Function::null()),
-      simple_instance_of_true_function_(Function::null()),
-      simple_instance_of_false_function_(Function::null()),
-      async_clear_thread_stack_trace_(Function::null()),
-      async_set_thread_stack_trace_(Function::null()),
-      async_star_move_next_helper_(Function::null()),
-      complete_on_async_return_(Function::null()),
-      async_star_stream_controller_(Class::null()),
-      library_load_error_table_(Array::null()),
-      unique_dynamic_targets_(Array::null()),
-      token_objects_(GrowableObjectArray::null()),
-      token_objects_map_(Array::null()),
-      megamorphic_cache_table_(GrowableObjectArray::null()),
-      megamorphic_miss_code_(Code::null()),
-      megamorphic_miss_function_(Function::null()),
-      obfuscation_map_(Array::null()),
-      changed_in_last_reload_(GrowableObjectArray::null()) {
+ObjectStore::ObjectStore() {
+#define INIT_FIELD(Type, name) name##_ = Type::null();
+  OBJECT_STORE_FIELD_LIST(INIT_FIELD, INIT_FIELD)
+#undef INIT_FIELD
+
   for (RawObject** current = from(); current <= to(); current++) {
     ASSERT(*current == Object::null());
   }