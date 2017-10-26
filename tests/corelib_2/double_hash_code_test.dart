442e0e81922f802679b058bc2ebefcbefcdfefeb
Ryan Macnak <rmacnak@google.com>
2017-10-26 23:59:32 +0000

    Test double.hashCode with and without intrinsics.
    Change-Id: I34c317d3f11bef061cb9f2f6ad1123e948ef78f2
    Reviewed-on: https://dart-review.googlesource.com/16683
    Reviewed-by: Vyacheslav Egorov <vegorov@google.com>
    Reviewed-by: Kasper Lund <kasperl@google.com>

--- tests/corelib_2/double_hash_code_test.dart
+++ tests/corelib_2/double_hash_code_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// VMOptions=--intrinsify
+// VMOptions=--no_intrinsify
+
 import 'package:expect/expect.dart';
 
 main() {
