94d4543245e5af7ba2298fe7355cb96f57404fc3
Erik Ernst <eernst@google.com>
2017-11-09 14:04:08 +0000

    Moved a comma in a test over some white space.
    The multi-test language_2/assertion_initializer_const_error2_test used
    to have outcomes caused by a syntax error (arising for all other cases
    than `cc02` in this multi-test). This CL moves that comma such that all
    subtests have the intended shape, and the test actually tests the
    intended property. This causes a rather large number of status file
    entries to be adjusted.
    
    Change-Id: I2e176611813e5706e553fb51cda7553f17dfa300
    Reviewed-on: https://dart-review.googlesource.com/19191
    Commit-Queue: Erik Ernst <eernst@google.com>
    Reviewed-by: Florian Loitsch <floitsch@google.com>

--- tests/language_2/assertion_initializer_const_error2_test.dart
+++ tests/language_2/assertion_initializer_const_error2_test.dart
@@ -10,54 +10,51 @@ class C {
   final int x;
   // Const constructors.
   const C.cc01(this.x, y)
-      : assert(x < y)  //# cc01: compile-time error
-  ;
-  const C.cc02(x, y) : x = x,
-      assert(x < y)  //# cc02: compile-time error
+      : assert(x < y)  //# cc01: checked mode compile-time error
+      ;
+  const C.cc02(x, y) : x = x
+      , assert(x < y)  //# cc02: checked mode compile-time error
       ;
   const C.cc03(x, y) :
-      assert(x < y),  //# cc03: compile-time error
+      assert(x < y),  //# cc03: checked mode compile-time error
       x = x;
-  const C.cc04(this.x, y) : super()
-      , assert(x < y)  //# cc04: compile-time error
-      ;
   const C.cc05(this.x, y) :
-      assert(x < y),   //# cc05: compile-time error
+      assert(x < y),   //# cc05: checked mode compile-time error
       super();
-  const C.cc06(x, y) : x = x, super()
-      , assert(x < y)  //# cc06: compile-time error
+  const C.cc06(x, y) : x = x
+      , assert(x < y)  //# cc06: checked mode compile-time error
+      , super()
       ;
   const C.cc07(x, y) :
-      assert(x < y),  //# cc07: compile-time error
-      super(), x = x;
+      assert(x < y),  //# cc07: checked mode compile-time error
+      x = x, super();
   const C.cc08(x, y) :
-      assert(x < y),  //# cc08: compile-time error
-      super(), x = x
+      assert(x < y),  //# cc08: checked mode compile-time error
+      x = x
       , assert(y > x)  //# cc08: continued
+      , super()
       ;
   const C.cc09(this.x, y)
-      : assert(x < y, "$x < $y")  //# cc09: compile-time error
+      : assert(x < y, "$x < $y")  //# cc09: checked mode compile-time error
       ;
   const C.cc10(this.x, y)
-      : assert(x < y,)  //# cc10: compile-time error
+      : assert(x < y,)  //# cc10: checked mode compile-time error
       ;
   const C.cc11(this.x, y)
-      : assert(x < y, "$x < $y",)  //# cc11: compile-time error
+      : assert(x < y, "$x < $y",)  //# cc11: checked mode compile-time error
       ;
 }
 
-
 main() {
   // Failing assertions in const invociations are compile-time errors.
-  const C.cc01(2, 1);  //# cc01: compile-time error
-  const C.cc02(2, 1);  //# cc02: compile-time error
-  const C.cc03(2, 1);  //# cc03: compile-time error
-  const C.cc04(2, 1);  //# cc04: compile-time error
-  const C.cc05(2, 1);  //# cc05: compile-time error
-  const C.cc06(2, 1);  //# cc06: compile-time error
-  const C.cc07(2, 1);  //# cc07: compile-time error
-  const C.cc08(2, 1);  //# cc08: compile-time error
-  const C.cc09(2, 1);  //# cc09: compile-time error
-  const C.cc10(2, 1);  //# cc10: compile-time error
-  const C.cc11(2, 1);  //# cc11: compile-time error
+  const C.cc01(2, 1);  //# cc01: continued
+  const C.cc02(2, 1);  //# cc02: continued
+  const C.cc03(2, 1);  //# cc03: continued
+  const C.cc05(2, 1);  //# cc05: continued
+  const C.cc06(2, 1);  //# cc06: continued
+  const C.cc07(2, 1);  //# cc07: continued
+  const C.cc08(2, 1);  //# cc08: continued
+  const C.cc09(2, 1);  //# cc09: continued
+  const C.cc10(2, 1);  //# cc10: continued
+  const C.cc11(2, 1);  //# cc11: continued
 }
