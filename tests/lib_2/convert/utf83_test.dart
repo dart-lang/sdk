3836c70a8150bc33a695788deee346b940b40769
Stephen Adams <sra@google.com>
2017-11-16 04:59:26 +0000

    Redo "js_runtime: Use browser's TextDecoder for UTF8 decoding"
    Bug:
    Change-Id: I40903bcdc1502e15cbf84298b43ba04ae8a8ecaf
    Reviewed-on: https://dart-review.googlesource.com/21383
    Reviewed-by: Stephen Adams <sra@google.com>

--- tests/lib_2/convert/utf83_test.dart
+++ tests/lib_2/convert/utf83_test.dart
@@ -6,46 +6,68 @@ library utf8_test;
 
 import "package:expect/expect.dart";
 import 'dart:convert';
+import 'dart:typed_data' show Uint8List;
 
-main() {
-  // Test that UTF-8-decoder removes leading BOM.
-  Expect.equals("a", utf8.decode([0xEF, 0xBB, 0xBF, 0x61]));
-  Expect.equals("a", utf8.decoder.convert([0xEF, 0xBB, 0xBF, 0x61]));
-  Expect.equals("a", new Utf8Decoder().convert([0xEF, 0xBB, 0xBF, 0x61]));
+/// [bytes] transforms the input into some representation (eg. plain list,
+/// Uint8List).
+test(List<int> bytes(List<int> input)) {
+  // Test that UTF8-decoder removes leading BOM.
+  Expect.equals("a", utf8.decode(bytes([0xEF, 0xBB, 0xBF, 0x61])));
+  Expect.equals("a", utf8.decoder.convert(bytes([0xEF, 0xBB, 0xBF, 0x61])));
   Expect.equals(
-      "a", utf8.decode([0xEF, 0xBB, 0xBF, 0x61], allowMalformed: true));
-  Expect.equals("a",
-      new Utf8Codec(allowMalformed: true).decode([0xEF, 0xBB, 0xBF, 0x61]));
+      "a", new Utf8Decoder().convert(bytes([0xEF, 0xBB, 0xBF, 0x61])));
+  Expect.equals(
+      "a", utf8.decode(bytes([0xEF, 0xBB, 0xBF, 0x61]), allowMalformed: true));
+  Expect.equals(
+      "a",
+      new Utf8Codec(allowMalformed: true)
+          .decode(bytes([0xEF, 0xBB, 0xBF, 0x61])));
   Expect.equals(
       "a",
       new Utf8Codec(allowMalformed: true)
           .decoder
-          .convert([0xEF, 0xBB, 0xBF, 0x61]));
-  Expect.equals("a",
-      new Utf8Decoder(allowMalformed: true).convert([0xEF, 0xBB, 0xBF, 0x61]));
-  Expect.equals("", utf8.decode([0xEF, 0xBB, 0xBF]));
-  Expect.equals("", utf8.decoder.convert([0xEF, 0xBB, 0xBF]));
-  Expect.equals("", new Utf8Decoder().convert([0xEF, 0xBB, 0xBF]));
-  Expect.equals("", utf8.decode([0xEF, 0xBB, 0xBF], allowMalformed: true));
+          .convert(bytes([0xEF, 0xBB, 0xBF, 0x61])));
   Expect.equals(
-      "", new Utf8Codec(allowMalformed: true).decode([0xEF, 0xBB, 0xBF]));
+      "a",
+      new Utf8Decoder(allowMalformed: true)
+          .convert(bytes([0xEF, 0xBB, 0xBF, 0x61])));
+  Expect.equals("", utf8.decode(bytes([0xEF, 0xBB, 0xBF])));
+  Expect.equals("", utf8.decoder.convert(bytes([0xEF, 0xBB, 0xBF])));
+  Expect.equals("", new Utf8Decoder().convert(bytes([0xEF, 0xBB, 0xBF])));
+  Expect.equals(
+      "", utf8.decode(bytes([0xEF, 0xBB, 0xBF]), allowMalformed: true));
   Expect.equals("",
-      new Utf8Codec(allowMalformed: true).decoder.convert([0xEF, 0xBB, 0xBF]));
+      new Utf8Codec(allowMalformed: true).decode(bytes([0xEF, 0xBB, 0xBF])));
   Expect.equals(
-      "", new Utf8Decoder(allowMalformed: true).convert([0xEF, 0xBB, 0xBF]));
-  Expect.equals("a\u{FEFF}", utf8.decode([0x61, 0xEF, 0xBB, 0xBF]));
-  Expect.equals("a\u{FEFF}", utf8.decoder.convert([0x61, 0xEF, 0xBB, 0xBF]));
+      "",
+      new Utf8Codec(allowMalformed: true)
+          .decoder
+          .convert(bytes([0xEF, 0xBB, 0xBF])));
+  Expect.equals("",
+      new Utf8Decoder(allowMalformed: true).convert(bytes([0xEF, 0xBB, 0xBF])));
+  Expect.equals("a\u{FEFF}", utf8.decode(bytes([0x61, 0xEF, 0xBB, 0xBF])));
   Expect.equals(
-      "a\u{FEFF}", new Utf8Decoder().convert([0x61, 0xEF, 0xBB, 0xBF]));
+      "a\u{FEFF}", utf8.decoder.convert(bytes([0x61, 0xEF, 0xBB, 0xBF])));
   Expect.equals(
-      "a\u{FEFF}", utf8.decode([0x61, 0xEF, 0xBB, 0xBF], allowMalformed: true));
+      "a\u{FEFF}", new Utf8Decoder().convert(bytes([0x61, 0xEF, 0xBB, 0xBF])));
   Expect.equals("a\u{FEFF}",
-      new Utf8Codec(allowMalformed: true).decode([0x61, 0xEF, 0xBB, 0xBF]));
+      utf8.decode(bytes([0x61, 0xEF, 0xBB, 0xBF]), allowMalformed: true));
+  Expect.equals(
+      "a\u{FEFF}",
+      new Utf8Codec(allowMalformed: true)
+          .decode(bytes([0x61, 0xEF, 0xBB, 0xBF])));
   Expect.equals(
       "a\u{FEFF}",
       new Utf8Codec(allowMalformed: true)
           .decoder
-          .convert([0x61, 0xEF, 0xBB, 0xBF]));
-  Expect.equals("a\u{FEFF}",
-      new Utf8Decoder(allowMalformed: true).convert([0x61, 0xEF, 0xBB, 0xBF]));
+          .convert(bytes([0x61, 0xEF, 0xBB, 0xBF])));
+  Expect.equals(
+      "a\u{FEFF}",
+      new Utf8Decoder(allowMalformed: true)
+          .convert(bytes([0x61, 0xEF, 0xBB, 0xBF])));
+}
+
+main() {
+  test((list) => list);
+  test((list) => new Uint8List.fromList(list));
 }
--- tests/lib_2/convert/utf84_test.dart
+++ tests/lib_2/convert/utf84_test.dart
@@ -4,6 +4,7 @@
 
 import 'package:expect/expect.dart';
 import 'dart:convert';
+import 'dart:typed_data' show Uint8List;
 
 const String testEnglishPhrase = "The quick brown fox jumps over the lazy dog.";
 
@@ -621,19 +622,33 @@ const List<int> testKatakanaUtf8 = const <int>[
 ];
 
 void main() {
-  testUtf8bytesToCodepoints();
-  testUtf8BytesToString();
   testEncodeToUtf8();
-}
 
-List<int> encodeUtf8(String str) => utf8.encode(str);
-List<int> utf8ToRunes(List<int> codeUnits) {
-  return utf8.decode(codeUnits, allowMalformed: true).runes.toList();
-}
+  String decodeUtf8List(List<int> codeUnits) => utf8.decode(codeUnits);
+  String decodeUtf8Uint8List(List<int> codeUnits) =>
+      UTF8.decode(new Uint8List.fromList(codeUnits));
 
-String decodeUtf8(List<int> codeUnits) => utf8.decode(codeUnits);
+  testUtf8BytesToString(decodeUtf8List);
+  testUtf8BytesToString(decodeUtf8Uint8List);
+
+  List<int> utf8ToRunes1(List<int> codeUnits) {
+    return utf8.decode(codeUnits, allowMalformed: true).runes.toList();
+  }
+
+  List<int> utf8ToRunes2(List<int> codeUnits) {
+    return utf8
+        .decode(new Uint8List.fromList(codeUnits), allowMalformed: true)
+        .runes
+        .toList();
+  }
+
+  testUtf8bytesToCodepoints(utf8ToRunes1);
+  testUtf8bytesToCodepoints(utf8ToRunes2);
+}
 
 void testEncodeToUtf8() {
+  List<int> encodeUtf8(String str) => UTF8.encode(str);
+
   Expect.listEquals(
       testEnglishUtf8, encodeUtf8(testEnglishPhrase), "english to utf8");
 
@@ -653,7 +668,7 @@ void testEncodeToUtf8() {
       testKatakanaUtf8, encodeUtf8(testKatakanaPhrase), "Katakana to utf8");
 }
 
-void testUtf8bytesToCodepoints() {
+void testUtf8bytesToCodepoints(List<int> utf8ToRunes(List<int> utf8)) {
   Expect.listEquals(
       [954, 972, 963, 956, 949],
       utf8ToRunes([0xce, 0xba, 0xcf, 0x8c, 0xcf, 0x83, 0xce, 0xbc, 0xce, 0xb5]),
@@ -976,7 +991,7 @@ void testUtf8bytesToCodepoints() {
   Expect.listEquals([0xffff], utf8ToRunes([0xef, 0xbf, 0xbf]), "U+FFFF");
 }
 
-void testUtf8BytesToString() {
+void testUtf8BytesToString(String decodeUtf8(List<int> input)) {
   Expect.stringEquals(
       testEnglishPhrase, decodeUtf8(testEnglishUtf8), "English");
 
