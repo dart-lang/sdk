3616a6f341d4e6369d062a76e09fe18ca530f842
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2017-11-29 14:09:11 +0100

    handle more cases in prefer_bool_in_asserts

--- test/rules/prefer_bool_in_asserts.dart
+++ test/rules/prefer_bool_in_asserts.dart
@@ -15,4 +15,23 @@ main() {
   assert(() { return true; }, 'message'); // LINT
   assert(() { return true; }()); // OK
   assert(() { return true; }(), 'message'); // OK
+  assert(() { throw ""; }()); // OK
+}
+m1(p) {
+  assert(() { return p; }()); // OK
+}
+m2(Object p) {
+  assert(() { return p; }()); // OK
+}
+m3<T>(T p) {
+  assert(() { return p; }()); // OK
+}
+m4<T extends List>(T p) {
+  assert(() { return p; }()); // LINT
+}
+m5<S, T extends S>(T p) {
+  assert(() { return p; }()); // OK
+}
+m6<S extends List, T extends S>(T p) {
+  assert(() { return p; }()); // LINT
 }
