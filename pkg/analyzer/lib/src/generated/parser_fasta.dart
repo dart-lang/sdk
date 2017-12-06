f369d3caa1a9770de4f7419d2d55e1780f9ff9d5
Dan Rubel <danrubel@google.com>
2017-12-06 18:40:45 +0000

    Allow native clause in analyzer use of fasta parser
    Change-Id: Idb0224d448d8d5cee2043ee51058125f51b7fe33
    Reviewed-on: https://dart-review.googlesource.com/26681
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Dan Rubel <danrubel@google.com>

--- pkg/analyzer/lib/src/generated/parser_fasta.dart
+++ pkg/analyzer/lib/src/generated/parser_fasta.dart
@@ -352,17 +352,21 @@ class _Parser2 extends ParserAdapter {
   @override
   bool enableNnbd = false;
 
-  factory _Parser2(Source source, AnalysisErrorListener errorListener) {
+  factory _Parser2(Source source, AnalysisErrorListener errorListener,
+      {bool allowNativeClause: false}) {
     var errorReporter = new ErrorReporter(errorListener, source);
     var library = new _KernelLibraryBuilder(source.uri);
     var member = new _Builder();
     var scope = new Scope.top(isModifiable: true);
-    return new _Parser2._(source, errorReporter, library, member, scope);
+    return new _Parser2._(source, errorReporter, library, member, scope,
+        allowNativeClause: allowNativeClause);
   }
 
   _Parser2._(this._source, ErrorReporter errorReporter,
-      KernelLibraryBuilder library, Builder member, Scope scope)
-      : super(null, errorReporter, library, member, scope);
+      KernelLibraryBuilder library, Builder member, Scope scope,
+      {bool allowNativeClause: false})
+      : super(null, errorReporter, library, member, scope,
+            allowNativeClause: allowNativeClause);
 
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
