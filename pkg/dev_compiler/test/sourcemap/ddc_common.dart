9d8d2aec5757003dfd5a3c76c9244adda1cbe9f3
Jens Johansen <jensj@google.com>
2017-12-11 08:34:37 +0000

    [DDC-step-test] Use absolute paths
    Bug:
    Change-Id: I3637e6f6693f1629746090f56cc466cc3e91f12e
    Reviewed-on: https://dart-review.googlesource.com/27180
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Kevin Millikin <kmillikin@google.com>
    Reviewed-by: Alan Knight <alanknight@google.com>

--- pkg/dev_compiler/test/sourcemap/ddc_common.dart
+++ pkg/dev_compiler/test/sourcemap/ddc_common.dart
@@ -8,12 +8,12 @@ import 'package:path/path.dart' as path;
 import 'package:sourcemap_testing/src/annotated_code_helper.dart';
 import 'package:sourcemap_testing/src/stacktrace_helper.dart';
 import 'package:testing/testing.dart';
+import 'package:sourcemap_testing/src/stepping_helper.dart';
 
 import 'common.dart';
 
 abstract class DdcRunner {
-  ProcessResult runDDC(String ddcDir, String inputFile, String outputFile,
-      String outWrapperPath);
+  ProcessResult runDDC(Uri inputFile, Uri outputFile, Uri outWrapperPath);
 }
 
 class Compile extends Step<Data, Data, ChainContext> {
@@ -30,14 +30,14 @@ class Compile extends Step<Data, Data, ChainContext> {
     data.outDir = await Directory.systemTemp.createTemp("ddc_step_test");
     data.code = new AnnotatedCode.fromText(
         new File(inputFile).readAsStringSync(), commentStart, commentEnd);
-    var testFile = "${data.outDir.path}/test.dart";
-    new File(testFile).writeAsStringSync(data.code.sourceCode);
-    var outputPath = data.outDir.path;
+    var outDirUri = data.outDir.uri;
+    var testFile = outDirUri.resolve("test.dart");
+    new File.fromUri(testFile).writeAsStringSync(data.code.sourceCode);
     var outputFilename = "js.js";
-    var outputFile = path.join(outputPath, outputFilename);
-    var outWrapperPath = path.join(outputPath, "wrapper.js");
+    var outputFile = outDirUri.resolve(outputFilename);
+    var outWrapperPath = outDirUri.resolve("wrapper.js");
 
-    ddcRunner.runDDC(getDdcDir().path, testFile, outputFile, outWrapperPath);
+    ddcRunner.runDDC(testFile, outputFile, outWrapperPath);
 
     return pass(data);
   }
@@ -65,17 +65,21 @@ class TestStackTrace extends Step<Data, Data, ChainContext> {
   }
 
   Future<bool> _compile(String input, String output) async {
-    var outWrapperPath = _getWrapperPathFromDirectoryFile(input);
-    ddcRunner.runDDC(getDdcDir().path, input, output, outWrapperPath);
+    var outWrapperPath = _getWrapperPathFromDirectoryFile(new Uri.file(input));
+    ddcRunner.runDDC(new Uri.file(input), new Uri.file(output), outWrapperPath);
     return true;
   }
 
-  List<String> _getPreambles(input, output) {
-    return ['--module', _getWrapperPathFromDirectoryFile(input), '--'];
+  List<String> _getPreambles(String input, String output) {
+    return [
+      '--module',
+      _getWrapperPathFromDirectoryFile(new Uri.file(input)).toFilePath(),
+      '--'
+    ];
   }
 
-  String _getWrapperPathFromDirectoryFile(input) {
-    return new File.fromUri(new File(input).uri.resolve("wrapper.js")).path;
+  Uri _getWrapperPathFromDirectoryFile(Uri input) {
+    return input.resolve("wrapper.js");
   }
 
   String _convertName(String name) {
@@ -111,13 +115,13 @@ Directory getDdcDir() {
   return _cachedDdcDir ??= search();
 }
 
-String getWrapperContent(Uri jsSdkPath, String inputFileName, Uri outputFile) {
-  assert(!jsSdkPath.isAbsolute);
-  assert(!outputFile.isAbsolute);
+String getWrapperContent(
+    Uri jsSdkPath, String inputFileNameNoExt, String outputFilename) {
+  assert(jsSdkPath.isAbsolute);
   return """
-    import { dart, _isolate_helper } from '$jsSdkPath';
-    import { $inputFileName } from '$outputFile';
-    let main = $inputFileName.main;
+    import { dart, _isolate_helper } from '${uriPathForwardSlashed(jsSdkPath)}';
+    import { $inputFileNameNoExt } from '$outputFilename';
+    let main = $inputFileNameNoExt.main;
     dart.ignoreWhitelistedErrors(false);
     try {
       _isolate_helper.startRootIsolate(main, []);
@@ -127,24 +131,26 @@ String getWrapperContent(Uri jsSdkPath, String inputFileName, Uri outputFile) {
     """;
 }
 
-void createHtmlWrapper(String ddcDir, File sdkJsFile, String outputFile,
-    String jsContent, Uri outFileRelative, String outDir) {
+void createHtmlWrapper(File sdkJsFile, Uri outputFile, String jsContent,
+    String outputFilename, Uri outDir) {
   // For debugging via HTML, Chrome and ./tools/testing/dart/http_server.dart.
-  String sdkPath = new File(ddcDir).parent.parent.path;
+  Directory sdkPath = sdkRoot;
   String jsRootDart =
-      "/root_dart/${new File(path.relative(sdkJsFile.path, from: sdkPath))
+      "/root_dart/${new File(path.relative(sdkJsFile.path, from: sdkPath.path))
       .uri}";
-  new File(outputFile + ".html.js").writeAsStringSync(
-      jsContent.replaceFirst("from 'dart_sdk'", "from '$jsRootDart'"));
-  new File(outputFile + ".html.html").writeAsStringSync(getWrapperHtmlContent(
-      jsRootDart, "/root_build/$outFileRelative.html.js"));
+  new File.fromUri(outputFile.resolve("$outputFilename.html.js"))
+      .writeAsStringSync(
+          jsContent.replaceFirst("from 'dart_sdk'", "from '$jsRootDart'"));
+  new File.fromUri(outputFile.resolve("$outputFilename.html.html"))
+      .writeAsStringSync(getWrapperHtmlContent(
+          jsRootDart, "/root_build/$outputFilename.html.js"));
 
   print("You should now be able to run\n\n"
-      "dart $sdkPath/tools/testing/dart/http_server.dart -p 39550 "
-      "--network 127.0.0.1"
-      "--build-directory=$outDir"
+      "dart ${sdkRoot.path}/tools/testing/dart/http_server.dart -p 39550 "
+      "--network 127.0.0.1 "
+      "--build-directory=${outDir.toFilePath()}"
       "\n\nand go to\n\n"
-      "http://127.0.0.1:39550/root_build/$outFileRelative.html.html"
+      "http://127.0.0.1:39550/root_build/$outputFilename.html.html"
       "\n\nto step through via the browser.");
 }
 
