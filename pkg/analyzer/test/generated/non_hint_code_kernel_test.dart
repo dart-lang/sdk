fa265c892ef0cffca28feed97e1c41887758d2b0
Brian Wilkerson <brianwilkerson@google.com>
2017-12-12 23:17:01 +0000

    Triage some failing tests
    Change-Id: Ia6ef2b687b4eb5abc5d88a58b8416a6b4c9e2bb8
    Reviewed-on: https://dart-review.googlesource.com/28860
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/generated/non_hint_code_kernel_test.dart
+++ pkg/analyzer/test/generated/non_hint_code_kernel_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/src/dart/error/hint_codes.dart';
+import 'package:analyzer/src/generated/source.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'non_hint_code_driver_test.dart';
@@ -28,6 +30,7 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @override
   @potentialAnalyzerProblem
   test_deadCode_deadBlock_if_debugConst_propertyAccessor() async {
+    // Appears to be an issue with resolution of import prefixes.
     await super.test_deadCode_deadBlock_if_debugConst_propertyAccessor();
   }
 
@@ -35,6 +38,8 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @override
   @potentialAnalyzerProblem
   test_deprecatedMemberUse_inDeprecatedLibrary() async {
+    // LibraryAnalyzer is not applying resolution data to annotations on
+    // directives.
     await super.test_deprecatedMemberUse_inDeprecatedLibrary();
   }
 
@@ -43,6 +48,7 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @potentialAnalyzerProblem
   test_duplicateImport_as() async {
     // Expected 0 errors of type HintCode.UNUSED_IMPORT, found 1 (38)
+    // Appears to be an issue with resolution of import prefixes.
     await super.test_duplicateImport_as();
   }
 
@@ -50,15 +56,24 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @override
   @potentialAnalyzerProblem
   test_importDeferredLibraryWithLoadFunction() async {
+    // Appears to be an issue with resolution of import prefixes.
     await super.test_importDeferredLibraryWithLoadFunction();
   }
 
-  @failingTest
   @override
-  @potentialAnalyzerProblem
   test_unnecessaryCast_generics() async {
-    // Expected 0 errors of type HintCode.UNNECESSARY_CAST, found 1 (90)
-    await super.test_unnecessaryCast_generics();
+    // dartbug.com/18953
+    // Overridden because type inference now produces more information and there
+    // should now be a hint, where there wasn't one before.
+    Source source = addSource(r'''
+import 'dart:async';
+Future<int> f() => new Future.value(0);
+void g(bool c) {
+  (c ? f(): new Future.value(0) as Future<int>).then((int value) {});
+}''');
+    await computeAnalysisResult(source);
+    assertErrors(source, [HintCode.UNNECESSARY_CAST]);
+    verify([source]);
   }
 
   @failingTest
@@ -79,6 +94,7 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @override
   @potentialAnalyzerProblem
   test_unusedImport_prefix_topLevelFunction() async {
+    // Appears to be an issue with resolution of import prefixes.
     await super.test_unusedImport_prefix_topLevelFunction();
   }
 
@@ -86,6 +102,7 @@ class NonHintCodeTest_Kernel extends NonHintCodeTest_Driver {
   @override
   @potentialAnalyzerProblem
   test_unusedImport_prefix_topLevelFunction2() async {
+    // Appears to be an issue with resolution of import prefixes.
     await super.test_unusedImport_prefix_topLevelFunction2();
   }
 }
