24a510a91e9ee2105bc78da77fd34d867e7e619d
Brian Wilkerson <brianwilkerson@google.com>
2017-12-13 18:15:45 +0000

    Enable previewDart2 for more tests
    Change-Id: I1f2003040246b5b12f2866d513ff3502246ac39f
    Reviewed-on: https://dart-review.googlesource.com/29220
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/generated/hint_code_kernel_test.dart
+++ pkg/analyzer/test/generated/hint_code_kernel_test.dart
@@ -18,15 +18,183 @@ class HintCodeTest_Kernel extends HintCodeTest_Driver {
   bool get enableKernelDriver => true;
 
   @override
+  bool get previewDart2 => true;
+
+  @failingTest
+  @override
+  test_abstractSuperMemberReference_getter() async {
+    // Expected 1 errors of type HintCode.ABSTRACT_SUPER_MEMBER_REFERENCE, found 0
+    return super.test_abstractSuperMemberReference_getter();
+  }
+
+  @failingTest
+  @override
+  test_abstractSuperMemberReference_method_invocation() async {
+    // Expected 1 errors of type HintCode.ABSTRACT_SUPER_MEMBER_REFERENCE, found 0
+    return super.test_abstractSuperMemberReference_method_invocation();
+  }
+
+  @failingTest
+  @override
+  test_abstractSuperMemberReference_method_reference() async {
+    // Expected 1 errors of type HintCode.ABSTRACT_SUPER_MEMBER_REFERENCE, found 0
+    return super.test_abstractSuperMemberReference_method_reference();
+  }
+
+  @failingTest
+  @override
+  test_argumentTypeNotAssignable_functionType() async {
+    // Expected 1 errors of type HintCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, found 0
+    return super.test_argumentTypeNotAssignable_functionType();
+  }
+
+  @failingTest
+  @override
+  test_argumentTypeNotAssignable_type() async {
+    // Expected 1 errors of type HintCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, found 0
+    return super.test_argumentTypeNotAssignable_type();
+  }
+
+  @failingTest
+  @override
+  test_deadCode_deadBlock_else() async {
+    // Expected 1 errors of type HintCode.DEAD_CODE, found 0
+    return super.test_deadCode_deadBlock_else();
+  }
+
+  @failingTest
+  @override
+  test_deadCode_deadBlock_else_nested() async {
+    // Expected 1 errors of type HintCode.DEAD_CODE, found 0
+    return super.test_deadCode_deadBlock_else_nested();
+  }
+
+  @failingTest
+  @override
+  test_deadCode_deadBlock_if() async {
+    // Expected 1 errors of type HintCode.DEAD_CODE, found 0
+    return super.test_deadCode_deadBlock_if();
+  }
+
+  @failingTest
+  @override
+  test_deadCode_deadBlock_if_nested() async {
+    // Expected 1 errors of type HintCode.DEAD_CODE, found 0
+    return super.test_deadCode_deadBlock_if_nested();
+  }
+
+  @failingTest
+  @override
+  test_deadCode_deadFinalStatementInCase() async {
+    // Expected 1 errors of type StaticWarningCode.CASE_BLOCK_NOT_TERMINATED, found 0
+    return super.test_deadCode_deadFinalStatementInCase();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedAnnotationUse_Deprecated() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_MEMBER_USE, found 0
+    return super.test_deprecatedAnnotationUse_Deprecated();
+  }
+
+  @override
   @failingTest
   test_deprecatedAnnotationUse_named() async {
     return super.test_deprecatedAnnotationUse_named();
   }
 
-  @override
   @failingTest
+  @override
   test_deprecatedAnnotationUse_positional() async {
     return super.test_deprecatedAnnotationUse_positional();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedFunction_class() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_FUNCTION_CLASS_DECLARATION, found 0
+    return super.test_deprecatedFunction_class();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedFunction_extends() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_EXTENDS_FUNCTION, found 0;
+    //          1 errors of type StaticWarningCode.FUNCTION_WITHOUT_CALL, found 0
+    return super.test_deprecatedFunction_extends();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedFunction_extends2() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_FUNCTION_CLASS_DECLARATION, found 0;
+    //          1 errors of type HintCode.DEPRECATED_EXTENDS_FUNCTION, found 0
+    return super.test_deprecatedFunction_extends2();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedFunction_mixin() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_MIXIN_FUNCTION, found 0;
+    //          1 errors of type StaticWarningCode.FUNCTION_WITHOUT_CALL, found 0
+    return super.test_deprecatedFunction_mixin();
+  }
+
+  @failingTest
+  @override
+  test_deprecatedFunction_mixin2() async {
+    // Expected 1 errors of type HintCode.DEPRECATED_FUNCTION_CLASS_DECLARATION, found 0;
+    //          1 errors of type HintCode.DEPRECATED_MIXIN_FUNCTION, found 0
+    return super.test_deprecatedFunction_mixin2();
+  }
+
+  @failingTest
+  @override
+  test_divisionOptimization_propagatedType() async {
+    // Expected 1 errors of type HintCode.DIVISION_OPTIMIZATION, found 0
+    return super.test_divisionOptimization_propagatedType();
+  }
+
+  @failingTest
+  @override
+  test_duplicateImport3() async {
+    // Expected 0 errors of type HintCode.UNUSED_IMPORT, found 2 (18, 57)
+    return super.test_duplicateImport3();
+  }
+
+  @failingTest
+  @override
+  test_importDeferredLibraryWithLoadFunction() async {
+    // ad state: Expected element reference for analyzer offset 60; got one for kernel offset 65
+    return super.test_importDeferredLibraryWithLoadFunction();
+  }
+
+  @failingTest
+  @override
+  test_invalidAssignment_instanceVariable() async {
+    // Expected 1 errors of type HintCode.INVALID_ASSIGNMENT, found 0
+    return super.test_invalidAssignment_instanceVariable();
+  }
+
+  @failingTest
+  @override
+  test_invalidAssignment_localVariable() async {
+    // Expected 1 errors of type HintCode.INVALID_ASSIGNMENT, found 0
+    return super.test_invalidAssignment_localVariable();
+  }
+
+  @failingTest
+  @override
+  test_invalidAssignment_staticVariable() async {
+    // Expected 1 errors of type HintCode.INVALID_ASSIGNMENT, found 0
+    return super.test_invalidAssignment_staticVariable();
+  }
+
+  @failingTest
+  @override
+  test_invalidAssignment_variableDeclaration() async {
+    // UnimplementedError: Multiple field
+    return super.test_invalidAssignment_variableDeclaration();
   }
 
   @override
@@ -58,6 +226,69 @@ class HintCodeTest_Kernel extends HintCodeTest_Driver {
   @failingTest
   test_invalidRequiredParam_valid() async {
     return super.test_invalidRequiredParam_valid();
+  }
+
+  @failingTest
+  @override
+  test_js_lib_OK() async {
+    // Bad state: Expected element reference for analyzer offset 51; got one for kernel offset 1
+    return super.test_js_lib_OK();
+  }
+
+  @failingTest
+  @override
+  test_missingJsLibAnnotation_class() async {
+    // Expected 1 errors of type HintCode.MISSING_JS_LIB_ANNOTATION, found 0;
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (21)
+    return super.test_missingJsLibAnnotation_class();
+  }
+
+  @failingTest
+  @override
+  test_missingJsLibAnnotation_externalField() async {
+    // Expected 1 errors of type ParserErrorCode.EXTERNAL_FIELD, found 0;
+    //          1 errors of type HintCode.MISSING_JS_LIB_ANNOTATION, found 0;
+    //          0 errors of type ParserErrorCode.EXTRANEOUS_MODIFIER, found 1 (36);
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (7)
+    return super.test_missingJsLibAnnotation_externalField();
+  }
+
+  @failingTest
+  @override
+  test_missingJsLibAnnotation_function() async {
+    // Expected 1 errors of type HintCode.MISSING_JS_LIB_ANNOTATION, found 0;
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (21)
+    return super.test_missingJsLibAnnotation_function();
+  }
+
+  @failingTest
+  @override
+  test_missingJsLibAnnotation_method() async {
+    // Expected 1 errors of type HintCode.MISSING_JS_LIB_ANNOTATION, found 0;
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (21)
+    return super.test_missingJsLibAnnotation_method();
+  }
+
+  @failingTest
+  @override
+  test_missingJsLibAnnotation_variable() async {
+    // Expected 1 errors of type HintCode.MISSING_JS_LIB_ANNOTATION, found 0;
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (7)
+    return super.test_missingJsLibAnnotation_variable();
+  }
+
+  @failingTest
+  @override
+  test_mustCallSuper() async {
+    // Expected 1 errors of type HintCode.MUST_CALL_SUPER, found 0
+    return super.test_mustCallSuper();
+  }
+
+  @failingTest
+  @override
+  test_mustCallSuper_indirect() async {
+    // Expected 1 errors of type HintCode.MUST_CALL_SUPER, found 0
+    return super.test_mustCallSuper_indirect();
   }
 
   @override
@@ -118,5 +349,148 @@ class HintCodeTest_Kernel extends HintCodeTest_Driver {
   @failingTest
   test_required_typedef_function_param() async {
     return super.test_required_typedef_function_param();
+  }
+
+  @failingTest
+  @override
+  test_strongMode_downCastCompositeHint() async {
+    // Expected 1 errors of type StrongModeCode.STRONG_MODE_DOWN_CAST_COMPOSITE, found 0
+    return super.test_strongMode_downCastCompositeHint();
+  }
+
+  @failingTest
+  @override
+  test_strongMode_downCastCompositeWarn() async {
+    // Expected 1 errors of type StrongModeCode.STRONG_MODE_DOWN_CAST_COMPOSITE, found 0
+    return super.test_strongMode_downCastCompositeWarn();
+  }
+
+  @failingTest
+  @override
+  test_undefinedGetter() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_GETTER, found 0
+    return super.test_undefinedGetter();
+  }
+
+  @failingTest
+  @override
+  test_undefinedMethod() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_METHOD, found 0
+    return super.test_undefinedMethod();
+  }
+
+  @failingTest
+  @override
+  test_undefinedMethod_assignmentExpression() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_METHOD, found 0
+    return super.test_undefinedMethod_assignmentExpression();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_binaryExpression() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_binaryExpression();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_indexBoth() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_indexBoth();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_indexGetter() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_indexGetter();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_indexSetter() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_indexSetter();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_postfixExpression() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_postfixExpression();
+  }
+
+  @failingTest
+  @override
+  test_undefinedOperator_prefixExpression() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_OPERATOR, found 0
+    return super.test_undefinedOperator_prefixExpression();
+  }
+
+  @failingTest
+  @override
+  test_undefinedSetter() async {
+    // Expected 1 errors of type HintCode.UNDEFINED_SETTER, found 0
+    return super.test_undefinedSetter();
+  }
+
+  @failingTest
+  @override
+  test_unusedImport_as() async {
+    // Failed to resolve 2 nodes:
+    //   one (/test.dart : 53)
+    //   one (/test.dart : 58)
+    return super.test_unusedImport_as();
+  }
+
+  @failingTest
+  @override
+  test_unusedImport_inComment_libraryDirective() async {
+    // Expected 0 errors of type HintCode.UNUSED_IMPORT, found 1 (42)
+    return super.test_unusedImport_inComment_libraryDirective();
+  }
+
+  @failingTest
+  @override
+  test_unusedLocalVariable_isInvoked() async {
+    // Expected 0 errors of type HintCode.UNUSED_LOCAL_VARIABLE, found 1 (30)
+    return super.test_unusedLocalVariable_isInvoked();
+  }
+
+  @failingTest
+  @override
+  test_unusedShownName() async {
+    // Expected 1 errors of type HintCode.UNUSED_SHOWN_NAME, found 0
+    return super.test_unusedShownName();
+  }
+
+  @failingTest
+  @override
+  test_unusedShownName_as() async {
+    // Expected 1 errors of type HintCode.UNUSED_SHOWN_NAME, found 0;
+    //          0 errors of type HintCode.UNUSED_IMPORT, found 1 (18)
+    return super.test_unusedShownName_as();
+  }
+
+  @failingTest
+  @override
+  test_unusedShownName_duplicates() async {
+    // Expected 2 errors of type HintCode.UNUSED_SHOWN_NAME, found 0
+    return super.test_unusedShownName_duplicates();
+  }
+
+  @failingTest
+  @override
+  test_unusedShownName_topLevelVariable() async {
+    // Expected 1 errors of type HintCode.UNUSED_SHOWN_NAME, found 0
+    return super.test_unusedShownName_topLevelVariable();
+  }
+
+  @failingTest
+  @override
+  test_useOfVoidResult_variableDeclaration_method2() async {
+    // UnimplementedError: Multiple variables in one declaration
+    return super.test_useOfVoidResult_variableDeclaration_method2();
   }
 }
