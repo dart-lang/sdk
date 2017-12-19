2721f9087ca3663f6d024a8b11f9179e3be79b5a
Phil Quitslund <pq@users.noreply.github.com>
2017-12-19 10:12:16 -0800

    Code reference resolution docs for `comment_references` (dart-lang/linter#861)
    See: dart-lang/linter#857

--- lib/src/rules/comment_references.dart
+++ lib/src/rules/comment_references.dart
@@ -33,6 +33,16 @@ On the other hand, assuming `outOfScopeId` is out of scope:
 void f(int outOfScopeId) { ... }
 ```
 
+Note that the square bracket comment format is designed to allow 
+comments to refer to declarations using a fairly natural format 
+but does not allow *arbitrary expressions*.  In particular, code 
+references within square brackets can consist of either
+
+- a single identifier where the identifier is any identifier in scope for the comment (see the spec for what is in scope in doc comments),
+- two identifiers separated by a period where the first identifier is the name of a class that is in scope and the second is the name of a member declared in the class,
+- a single identifier followed by a pair of parentheses where the identifier is the name of a class that is in scope (used to refer to the unnamed constructor for the class), or
+- two identifiers separated by a period and followed by a pair of parentheses where the first identifier is the name of a class that is in scope and the second is the name of a named constructor (not strictly necessary, but allowed for consistency).
+
 ''';
 
 class CommentReferences extends LintRule {
