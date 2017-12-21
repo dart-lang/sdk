22298629e8ec0209842be892b1558c24f16056ec
Sigmund Cherem <sigmund@google.com>
2017-12-21 10:26:00 -0800

    Add unstable API for bazel summary worker and move the actual worker out of package:front-end
    Bug:
    Change-Id: I0a64d3de46ef55e8185e723d6111fa315ad286ca
    Reviewed-on: https://dart-review.googlesource.com/30621
    Commit-Queue: Sigmund Cherem <sigmund@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>
    Reviewed-by: Jake Macdonald <jakemac@google.com>

--- utils/bazel/kernel_summary_worker.dart
+++ utils/bazel/kernel_summary_worker.dart
@@ -1,16 +1,21 @@
 // Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
+/// A tool that invokes the CFE to compute kernel summary files.
+///
+/// This script can be used as a command-line command or a persistent server.
+/// The server is implemented using the bazel worker protocol, so it can be used
+/// within bazel as is. Other tools (like pub-build and package-build) also
+/// use this persistent worker via the same protocol.
+
 import 'dart:async';
 import 'dart:io';
 
 import 'package:args/args.dart';
 import 'package:bazel_worker/bazel_worker.dart';
-import 'package:front_end/src/api_prototype/front_end.dart'
-    hide FileSystemException;
-import 'package:front_end/src/fasta/command_line_reporting.dart';
+import 'package:front_end/src/api_unstable/summary_worker.dart' as fe;
 import 'package:front_end/src/multi_root_file_system.dart';
-import 'package:front_end/src/api_prototype/physical_file_system.dart';
 import 'package:kernel/target/targets.dart';
 
 main(List<String> args) async {
@@ -108,42 +113,38 @@ Future<bool> computeSummary(List<String> args,
   var fileSystem = new MultiRootFileSystem(
       'org-dartlang-multi-root',
       parsedArgs['multi-root'].map(Uri.parse).toList(),
-      PhysicalFileSystem.instance);
-  var options = new CompilerOptions()
-    ..packagesFileUri = Uri.parse(parsedArgs['packages-file'])
-    ..inputSummaries = parsedArgs['input-summary'].map(Uri.parse).toList()
-    ..sdkSummary = Uri.parse(parsedArgs['dart-sdk-summary'])
-    ..fileSystem = fileSystem
-    ..target = new NoneTarget(new TargetFlags());
+      fe.PhysicalFileSystem.instance);
 
-  options.onError = (CompilationMessage error) {
-    var message = new StringBuffer()
-      ..write(severityName(error.severity, capitalized: true))
-      ..write(': ');
-    if (error.span != null) {
-      message.writeln(error.span.message(error.message));
-    } else {
-      message.writeln(error.message);
-    }
-    if (error.tip != null) {
-      message.writeln(error.tip);
-    }
+  var state = await fe.initializeCompiler(
+      // TODO(sigmund): pass an old state once we can make use of it.
+      null,
+      Uri.parse(parsedArgs['dart-sdk-summary']),
+      Uri.parse(parsedArgs['packages-file']),
+      parsedArgs['input-summary'].map(Uri.parse).toList(),
+      new NoneTarget(new TargetFlags()),
+      fileSystem);
+
+  void onProblem(problem, severity, String formatted, line, column) {
     if (outputBuffer != null) {
-      outputBuffer.writeln(message);
+      outputBuffer.writeln(formatted);
     } else {
-      print(message);
+      stderr.writeln(formatted);
     }
-    if (error.severity != Severity.nit) {
+    if (severity != fe.Severity.nit) {
       succeeded = false;
     }
-  };
+  }
 
   var sources = parsedArgs['source'].map(Uri.parse).toList();
-  var program = await summaryFor(sources, options);
+  var summary = await fe.compile(state, sources, onProblem);
 
-  var outputFile = new File(parsedArgs['output']);
-  outputFile.createSync(recursive: true);
-  outputFile.writeAsBytesSync(program);
+  if (summary != null) {
+    var outputFile = new File(parsedArgs['output']);
+    outputFile.createSync(recursive: true);
+    outputFile.writeAsBytesSync(summary);
+  } else {
+    assert(!succeeded);
+  }
 
   return succeeded;
 }
