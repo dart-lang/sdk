68e39ed6dc24942621d65a9a6fb1bec04fc4406a
Johnni Winther <johnniwinther@google.com>
2017-12-29 22:30:11 +0100

    Split value_range_test and mark load_elimination_test as slow
    Change-Id: I0becd6ddc9edd758f119436ec5ed6cdbde3c6359
    Reviewed-on: https://dart-review.googlesource.com/31785
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- tests/compiler/dart2js/codegen/value_range_kernel_test.dart
+++ tests/compiler/dart2js/codegen/value_range_kernel_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -7,8 +7,8 @@ import 'value_range_test_helper.dart';
 
 main() {
   asyncTest(() async {
-    print('--test from ast---------------------------------------------------');
-    await runTests(useKernel: false);
-    // Kernel part tested in 'value_range_kernel_test.dart'.
+    // AST part tested in 'value_range_test.dart'.
+    print('--test from kernel------------------------------------------------');
+    await runTests(useKernel: true);
   });
 }
