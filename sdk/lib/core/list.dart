d1382f983cfb29c31c446b08965f3c1a2cc81174
Lasse R.H. Nielsen <lrn@google.com>
2018-01-03 11:29:18 +0100

    Initial steps to add cast operation.
    Start by adding static castTo method to the interfaces. This provides everybody with the
    desired implementation without exposing the classes.
    When implementing the cast method, the recommended way is to (extend the base class,
    but if you choose not to ...) use the castTo method.
    
    If we ever get a language feature, we can implement the castTo method using that,
    without being stuck with an unnecessary class.
    
    Change-Id: I53274e60c88a4de519bead85d2ca44c1065a9c1a
    Reviewed-on: https://dart-review.googlesource.com/26620
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Leaf Petersen <leafp@google.com>

--- sdk/lib/core/list.dart
+++ sdk/lib/core/list.dart
@@ -156,6 +156,22 @@ abstract class List<E> implements EfficientLengthIterable<E> {
   external factory List.unmodifiable(Iterable elements);
 
   /**
+   * Adapts [source] to be a `List<T>`.
+   *
+   * Any time the list would produce an element that is not a [T],
+   * the element access will throw.
+   *
+   * Any time a [T] value is attempted stored into the adapted list,
+   * the store will throw unless the value is also an instance of [S].
+   *
+   * If all accessed elements of [source] are actually instances of [T],
+   * and if all elements stored into the returned list are actually instance
+   * of [S],
+   * then the returned list can be used as a `List<T>`.
+   */
+  static List<T> castTo<S, T>(List<S> source) => new CastList<S, T>(source);
+
+  /**
    * Returns the object at the given [index] in the list
    * or throws a [RangeError] if [index] is out of bounds.
    */
