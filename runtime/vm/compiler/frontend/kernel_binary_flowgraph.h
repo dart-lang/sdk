784ee768d83e5d01d3d4fae2063215c323c87f20
Samir Jindel <sjindel@google.com>
2018-01-11 21:30:29 +0100

    [kernel] Unrevert "Support for the partial tearoff type application operator in VM."
    The original revision is available in the first patchset.
    
    Change-Id: I579b4b85a19ac17088eed050680a46df7ffc5c0a
    Reviewed-on: https://dart-review.googlesource.com/34102
    Commit-Queue: Samir Jindel <sjindel@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- runtime/vm/compiler/frontend/kernel_binary_flowgraph.h
+++ runtime/vm/compiler/frontend/kernel_binary_flowgraph.h
@@ -1230,6 +1230,7 @@ class StreamingFlowGraphBuilder {
   Fragment BuildVectorCopy(TokenPosition* position);
   Fragment BuildClosureCreation(TokenPosition* position);
   Fragment BuildConstantExpression(TokenPosition* position);
+  Fragment BuildPartialTearoffInstantiation(TokenPosition* position);
 
   Fragment BuildExpressionStatement();
   Fragment BuildBlock();
