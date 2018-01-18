513ac0ffd773724414441063de9ae7e3f6f46309
Johnni Winther <johnniwinther@google.com>
2018-01-18 12:57:45 +0000

    Implement runDiff for Windows
    Change-Id: Ic9191e3844e5bcd47c97867f4a7cec52c161fd7a
    Reviewed-on: https://dart-review.googlesource.com/35280
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>

--- pkg/testing/lib/src/stdio_process.dart
+++ pkg/testing/lib/src/stdio_process.dart
@@ -43,8 +43,10 @@ class StdioProcess {
   static Future<StdioProcess> run(String executable, List<String> arguments,
       {String input,
       Duration timeout: const Duration(seconds: 60),
-      bool suppressOutput: true}) async {
-    Process process = await Process.start(executable, arguments);
+      bool suppressOutput: true,
+      bool runInShell: false}) async {
+    Process process =
+        await Process.start(executable, arguments, runInShell: runInShell);
     Timer timer;
     StringBuffer sb = new StringBuffer();
     if (timeout != null) {
