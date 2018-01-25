17a00e63ff85f82fb785ee650dffc47f42f12485
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2018-01-25 08:47:50 +0100

    lint also local variables

--- test/rules/prefer_const_declarations.dart
+++ test/rules/prefer_const_declarations.dart
@@ -12,11 +12,15 @@ final o5 = 1; // LINT
 final o6 = 1.3; // LINT
 final o7 = null; // LINT
 final o8 = const {}; // LINT
+final o9 = {}; // OK
+final o10 = o1; // LINT
 
 // lint on final keyword
 final a = null; // LINT
 
 class A {
+  const A();
+
   static const o1 = const []; // OK
   static final o2 = []; // OK
   static final o3 = const []; // LINT
@@ -25,10 +29,33 @@ class A {
   static final o6 = 1.3; // LINT
   static final o7 = null; // LINT
   static final o8 = const {}; // LINT
+  static final o9 = {}; // OK
 
   // lint on final keyword
   static //
       final a = null; // LINT
 
   final i = const []; // OK
+
+  int m() => 0;
+}
+
+m() {
+  const o1 = const []; // OK
+  final o2 = []; // OK
+  final o3 = const []; // LINT
+  final o4 = ''; // LINT
+  final o5 = 1; // LINT
+  final o6 = 1.3; // LINT
+  final o7 = null; // LINT
+  final o8 = const {}; // LINT
+  final o9 = {}; // OK
+  final o10 = new A(); // OK
+  final o11 = const A(); // LINT
+  final o12 = A.o1; // LINT
+  final o13 = A.o2; // OK
+  final o14 = o11.m(); // OK
+
+  // lint on final keyword
+  final a = null; // LINT
 }
