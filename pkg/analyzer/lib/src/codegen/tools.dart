8fbcf977e51b61faeb7b76006c696f99d29e89eb
Konstantin Shcheglov <scheglov@google.com>
2018-01-27 03:18:18 +0000

    Replace license in generated files.
    R=devoncarew@google.com
    
    Change-Id: I7511c8da329eba760698a48abc0f8db16038b9ea
    Reviewed-on: https://dart-review.googlesource.com/37127
    Reviewed-by: Devon Carew <devoncarew@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/codegen/tools.dart
+++ pkg/analyzer/lib/src/codegen/tools.dart
@@ -145,17 +145,9 @@ class CodeGenerator {
     if (codeGeneratorSettings.languageName == 'java') {
       header = '''
 /*
- * Copyright (c) ${year ?? '2015'}, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright (c) ${year ?? '2018'}, the Dart project authors.  Please see the AUTHORS file
+ * for details. All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE file.
  *
  * This file has been automatically generated.  Please do not edit it manually.
  * To regenerate the file, use the script "pkg/analysis_server/tool/spec/generate_files".
