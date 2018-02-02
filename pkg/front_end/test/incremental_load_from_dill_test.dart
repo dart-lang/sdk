7d06a43afa7c65903a4223e2eef62121fbf32967
Jens Johansen <jensj@google.com>
2018-02-02 09:03:43 +0000

    Revert "Avoid file io in incremental_load_from_dill_test"
    This reverts commit 69412b2da76f97eaf8aeff69e3f6e80e952e3093.
    
    Reason for revert: Didn't work.
    
    Original change's description:
    > Avoid file io in incremental_load_from_dill_test
    > 
    > Attempt to avoid failure on Windows.
    > 
    > Change-Id: Ib86d36e377e655dc85f5a3e2da219e911fa3fa3c
    > Reviewed-on: https://dart-review.googlesource.com/37761
    > Reviewed-by: Jens Johansen <jensj@google.com>
    
    TBR=ahe@google.com,jensj@google.com,kmillikin@google.com
    
    Change-Id: I0f1957fd3a4f60dfcf86e907d2e36d243521b25e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/37745
    Reviewed-by: Jens Johansen <jensj@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

--- pkg/front_end/test/incremental_load_from_dill_test.dart
+++ pkg/front_end/test/incremental_load_from_dill_test.dart
@@ -3,74 +3,74 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async' show Future;
+import 'dart:io' show Directory, File;
 
 import 'package:expect/expect.dart' show Expect;
 import 'package:front_end/src/api_prototype/compiler_options.dart'
     show CompilerOptions;
-import 'package:front_end/src/api_prototype/memory_file_system.dart'
-    show MemoryFileSystem;
 import 'package:front_end/src/api_prototype/incremental_kernel_generator.dart'
     show IncrementalKernelGenerator;
 import 'package:front_end/src/compute_platform_binaries_location.dart'
     show computePlatformBinariesLocation;
 import 'package:front_end/src/fasta/incremental_compiler.dart'
     show IncrementalCompiler;
-import 'package:front_end/src/fasta/kernel/utils.dart' show serializeProgram;
-import 'package:front_end/src/testing/hybrid_file_system.dart'
-    show HybridFileSystem;
+import 'package:front_end/src/fasta/kernel/utils.dart' show writeProgramToFile;
 
 final Uri dart2jsUrl = Uri.base.resolve("pkg/compiler/bin/dart2js.dart");
 final Uri invalidateUri = Uri.parse("package:compiler/src/filenames.dart");
+Directory outDir;
 Uri normalDill;
 Uri bootstrappedDill;
-Uri root = Uri.parse('org-dartlang-test:///');
-MemoryFileSystem fs = new MemoryFileSystem(root);
 
 main() async {
-  fs.entityForUri(root).createDirectory();
-  normalDill = root.resolve("dart2js.full.dill");
-  bootstrappedDill = root.resolve("dart2js.bootstrap.dill");
-  Uri nonexisting = root.resolve("dart2js.nonexisting.dill");
-  Uri nonLoadable = root.resolve("dart2js.nonloadable.dill");
+  outDir =
+      Directory.systemTemp.createTempSync("incremental_load_from_dill_test");
+  normalDill = outDir.uri.resolve("dart2js.full.dill");
+  bootstrappedDill = outDir.uri.resolve("dart2js.bootstrap.dill");
+  Uri nonexisting = outDir.uri.resolve("dart2js.nonexisting.dill");
+  Uri nonLoadable = outDir.uri.resolve("dart2js.nonloadable.dill");
+  try {
+    // Compile dart2js without bootstrapping.
+    Stopwatch stopwatch = new Stopwatch()..start();
+    await normalCompile();
+    print("Normal compile took ${stopwatch.elapsedMilliseconds} ms");
 
-  // Compile dart2js without bootstrapping.
-  Stopwatch stopwatch = new Stopwatch()..start();
-  await normalCompile();
-  print("Normal compile took ${stopwatch.elapsedMilliseconds} ms");
-
-  // Create a file that cannot be (fully) loaded as a dill file.
-  List<int> corruptData = await fs.entityForUri(normalDill).readAsBytes();
-  for (int i = 10 * (corruptData.length ~/ 16);
-      i < 15 * (corruptData.length ~/ 16);
-      ++i) {
-    corruptData[i] = 42;
-  }
-  fs.entityForUri(nonLoadable).writeAsBytesSync(corruptData);
-
-  // Compile dart2js, bootstrapping from the just-compiled dill.
-  for (List<Object> bootstrapData in [
-    [normalDill, true],
-    [nonexisting, false],
-    [nonLoadable, false]
-  ]) {
-    Uri bootstrapWith = bootstrapData[0];
-    bool bootstrapExpect = bootstrapData[1];
-    stopwatch.reset();
-    bool bootstrapResult = await bootstrapCompile(bootstrapWith);
-    print("Bootstrapped compile(s) from ${bootstrapWith.pathSegments.last} "
-        "took ${stopwatch.elapsedMilliseconds} ms");
-
-    // Compare the two files.
-    List<int> normalDillData = await fs.entityForUri(normalDill).readAsBytes();
-    List<int> bootstrappedDillData =
-        await fs.entityForUri(bootstrappedDill).readAsBytes();
-    Expect.equals(normalDillData.length, bootstrappedDillData.length);
-    for (int i = 0; i < normalDillData.length; ++i) {
-      if (normalDillData[i] != bootstrappedDillData[i]) {
-        Expect.fail("Normally compiled and bootstrapped compile differs.");
-      }
+    // Create a file that cannot be (fully) loaded as a dill file.
+    List<int> corruptData = new File.fromUri(normalDill).readAsBytesSync();
+    for (int i = 10 * (corruptData.length ~/ 16);
+        i < 15 * (corruptData.length ~/ 16);
+        ++i) {
+      corruptData[i] = 42;
     }
-    Expect.equals(bootstrapExpect, bootstrapResult);
+    new File.fromUri(nonLoadable).writeAsBytesSync(corruptData);
+
+    // Compile dart2js, bootstrapping from the just-compiled dill.
+    for (List<Object> bootstrapData in [
+      [normalDill, true],
+      [nonexisting, false],
+      [nonLoadable, false]
+    ]) {
+      Uri bootstrapWith = bootstrapData[0];
+      bool bootstrapExpect = bootstrapData[1];
+      stopwatch.reset();
+      bool bootstrapResult = await bootstrapCompile(bootstrapWith);
+      print("Bootstrapped compile(s) from ${bootstrapWith.pathSegments.last} "
+          "took ${stopwatch.elapsedMilliseconds} ms");
+
+      // Compare the two files.
+      List<int> normalDillData = new File.fromUri(normalDill).readAsBytesSync();
+      List<int> bootstrappedDillData =
+          new File.fromUri(bootstrappedDill).readAsBytesSync();
+      Expect.equals(normalDillData.length, bootstrappedDillData.length);
+      for (int i = 0; i < normalDillData.length; ++i) {
+        if (normalDillData[i] != bootstrappedDillData[i]) {
+          Expect.fail("Normally compiled and bootstrapped compile differs.");
+        }
+      }
+      Expect.equals(bootstrapExpect, bootstrapResult);
+    }
+  } finally {
+    outDir.deleteSync(recursive: true);
   }
 }
 
@@ -79,8 +79,7 @@ CompilerOptions getOptions() {
   var options = new CompilerOptions()
     ..sdkRoot = sdkRoot
     ..librariesSpecificationUri = Uri.base.resolve("sdk/lib/libraries.json")
-    ..strongMode = false
-    ..fileSystem = new HybridFileSystem(fs);
+    ..strongMode = false;
   return options;
 }
 
@@ -88,9 +87,8 @@ Future<bool> normalCompile() async {
   CompilerOptions options = getOptions();
   IncrementalCompiler compiler =
       new IncrementalKernelGenerator(options, dart2jsUrl);
-  var program = await compiler.computeDelta();
-  List<int> data = serializeProgram(program);
-  fs.entityForUri(normalDill).writeAsBytesSync(data);
+  var y = await compiler.computeDelta();
+  await writeProgramToFile(y, normalDill);
   return compiler.bootstrapSuccess;
 }
 
@@ -101,8 +99,7 @@ Future<bool> bootstrapCompile(Uri bootstrapWith) async {
   compiler.invalidate(invalidateUri);
   var bootstrappedProgram = await compiler.computeDelta();
   bool result = compiler.bootstrapSuccess;
-  List<int> data = serializeProgram(bootstrappedProgram);
-  fs.entityForUri(bootstrappedDill).writeAsBytesSync(data);
+  await writeProgramToFile(bootstrappedProgram, bootstrappedDill);
   compiler.invalidate(invalidateUri);
 
   var partialProgram = await compiler.computeDelta();
