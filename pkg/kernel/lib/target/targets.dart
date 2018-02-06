fb6779d71d0e6a15f2fad661aabd732bd18dd863
Vyacheslav Egorov <vegorov@google.com>
2018-02-06 16:12:02 +0000

    Alter super call resolution rules for Flutter target.
    By default super method invocations look for dispatch target.
    
    However super-mixins semantics permits super invocations to
    target interface members too.
    
    Fixes https://github.com/dart-lang/sdk/issues/31994
    
    Change-Id: I950abb3f0fea4a9611e3dd3b90dd05415a6d2979
    Reviewed-on: https://dart-review.googlesource.com/38800
    Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>

--- pkg/kernel/lib/target/targets.dart
+++ pkg/kernel/lib/target/targets.dart
@@ -76,6 +76,22 @@ abstract class Target {
   /// promotion do not slow down compilation too much.
   bool get disableTypeInference => false;
 
+  /// A derived class may change this to `true` to enable Flutter specific
+  /// "super-mixins" semantics.
+  ///
+  /// This semantics relaxes a number of constraint previously imposed on
+  /// mixins. Importantly it imposes the following change:
+  ///
+  ///     An abstract class may contain a member with a super-invocation that
+  ///     corresponds to a member of the superclass interface, but where the
+  ///     actual superclass does not declare or inherit a matching method.
+  ///     Since no amount of overriding can change this property, such a class
+  ///     cannot be extended to a class that is not abstract, it can only be
+  ///     used to derive a mixin from.
+  ///
+  /// See dartbug.com/31542 for details of the semantics.
+  bool get enableSuperMixins => false;
+
   /// Perform target-specific transformations on the outlines stored in
   /// [Program] when generating summaries.
   ///
