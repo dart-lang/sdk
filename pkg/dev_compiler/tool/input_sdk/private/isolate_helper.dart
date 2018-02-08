bdc6e9c862d5870b9998470e5d856f42bada65cf
Lasse R.H. Nielsen <lrn@google.com>
2018-02-08 10:16:00 +0000

    Add ticks counter to Timer.
    Change-Id: Ie5ed28fa8b7ef9c6aea97331267cf3c717b17099
    Reviewed-on: https://dart-review.googlesource.com/21342
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>

--- pkg/dev_compiler/tool/input_sdk/private/isolate_helper.dart
+++ pkg/dev_compiler/tool/input_sdk/private/isolate_helper.dart
@@ -1352,6 +1352,7 @@ class TimerImpl implements Timer {
   final bool _once;
   bool _inEventLoop = false;
   int _handle;
+  int _tick = 0;
 
   TimerImpl(int milliseconds, void callback()) : _once = true {
     if (milliseconds == 0 && (!hasTimer() || _globalState.isWorker)) {
@@ -1377,6 +1378,7 @@ class TimerImpl implements Timer {
       void internalCallback() {
         _handle = null;
         leaveJsAsync();
+        _tick = 1;
         callback();
       }
 
@@ -1394,13 +1396,24 @@ class TimerImpl implements Timer {
       : _once = false {
     if (hasTimer()) {
       enterJsAsync();
+      int start = JS('int', 'Date.now()');
       _handle = JS('int', '#.setInterval(#, #)', global, () {
+        int tick = this._tick + 1;
+        if (milliseconds > 0) {
+          int duration = JS('int', 'Date.now()') - start;
+          if (duration > (tick + 1) * milliseconds) {
+            tick = duration ~/ milliseconds;
+          }
+        }
+        this._tick = tick;
         callback(this);
       }, milliseconds);
     } else {
       throw new UnsupportedError("Periodic timer.");
     }
   }
+
+  int get tick => _tick;
 
   void cancel() {
     if (hasTimer()) {
