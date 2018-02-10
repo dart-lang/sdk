dart-lang/sdk

commit c9700040c344133f496e6451dffe20de3121acb6

Restore console to original state on VM exit. Fixes issue #30318.
Change-Id: I31b5da600ed5091cd788cde42b72e2fb910fe03a
Reviewed-on: https://dart-review.googlesource.com/39843
Commit-Queue: Ben Konyi <bkonyi@google.com>
Reviewed-by: Zach Anderson <zra@google.com>

runtime/bin/platform_fuchsia.cc
@@ -150,6 +150,16 @@ void Platform::Exit(int exit_code) {
   exit(exit_code);
 }
 
+bool Platform::SaveConsoleConfiguration() {
+  UNIMPLEMENTED();
+  return false;
+}
+
+bool Platform::RestoreConsoleConfiguration() {
+  UNIMPLEMENTED();
+  return false;
+}
+
 }  // namespace bin
 }  // namespace dart
 

runtime/bin/platform.h
@@ -91,6 +91,9 @@ class Platform {
   static DART_NORETURN void Exit(int exit_code);
 
  private:
+  static void SaveConsoleConfiguration();
+  static void RestoreConsoleConfiguration();
+
   // The path to the executable.
   static const char* executable_name_;
   // The path to the resolved executable.

runtime/bin/platform_android.cc
@@ -7,13 +7,16 @@
 
 #include "bin/platform.h"
 
+#include <errno.h>        // NOLINT
 #include <signal.h>       // NOLINT
 #include <string.h>       // NOLINT
 #include <sys/utsname.h>  // NOLINT
+#include <termios.h>      // NOLINT
 #include <unistd.h>       // NOLINT
 
 #include "bin/fdutils.h"
 #include "bin/file.h"
+#include "platform/signal_blocker.h"
 
 namespace dart {
 namespace bin {
@@ -27,6 +30,53 @@ static void segv_handler(int signal, siginfo_t* siginfo, void* context) {
   Dart_DumpNativeStackTrace(context);
   abort();
 }
+
+class PlatformPosix {
+ public:
+  static void SaveConsoleConfiguration() {
+    SaveConsoleConfigurationHelper(STDOUT_FILENO, &stdout_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDERR_FILENO, &stderr_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDIN_FILENO, &stdin_initial_c_lflag_);
+  }
+
+  static void RestoreConsoleConfiguration() {
+    RestoreConsoleConfigurationHelper(STDOUT_FILENO, stdout_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDERR_FILENO, stderr_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDIN_FILENO, stdin_initial_c_lflag_);
+    stdout_initial_c_lflag_ = -1;
+    stderr_initial_c_lflag_ = -1;
+    stdin_initial_c_lflag_ = -1;
+  }
+
+ private:
+  static tcflag_t stdout_initial_c_lflag_;
+  static tcflag_t stderr_initial_c_lflag_;
+  static tcflag_t stdin_initial_c_lflag_;
+
+  static void SaveConsoleConfigurationHelper(intptr_t fd, tcflag_t* flag) {
+    ASSERT(flag != NULL);
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    *flag = term.c_lflag;
+  }
+
+  static void RestoreConsoleConfigurationHelper(intptr_t fd, tcflag_t flag) {
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    term.c_lflag = flag;
+    NO_RETRY_EXPECTED(tcsetattr(fd, TCSANOW, &term));
+  }
+};
+
+tcflag_t PlatformPosix::stdout_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stderr_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stdin_initial_c_lflag_ = 0;
 
 bool Platform::Initialize() {
   // Turn off the signal handler for SIGPIPE as it causes the process
@@ -62,7 +112,7 @@ bool Platform::Initialize() {
     perror("sigaction() failed.");
     return false;
   }
-
+  SaveConsoleConfiguration();
   return true;
 }
 
@@ -142,7 +192,16 @@ const char* Platform::ResolveExecutablePath() {
 }
 
 void Platform::Exit(int exit_code) {
+  RestoreConsoleConfiguration();
   exit(exit_code);
+}
+
+void Platform::SaveConsoleConfiguration() {
+  PlatformPosix::SaveConsoleConfiguration();
+}
+
+void Platform::RestoreConsoleConfiguration() {
+  PlatformPosix::RestoreConsoleConfiguration();
 }
 
 }  // namespace bin

runtime/bin/platform_linux.cc
@@ -7,13 +7,16 @@
 
 #include "bin/platform.h"
 
+#include <errno.h>        // NOLINT
 #include <signal.h>       // NOLINT
 #include <string.h>       // NOLINT
 #include <sys/utsname.h>  // NOLINT
+#include <termios.h>      // NOLINT
 #include <unistd.h>       // NOLINT
 
 #include "bin/fdutils.h"
 #include "bin/file.h"
+#include "platform/signal_blocker.h"
 
 namespace dart {
 namespace bin {
@@ -27,6 +30,53 @@ static void segv_handler(int signal, siginfo_t* siginfo, void* context) {
   Dart_DumpNativeStackTrace(context);
   abort();
 }
+
+class PlatformPosix {
+ public:
+  static void SaveConsoleConfiguration() {
+    SaveConsoleConfigurationHelper(STDOUT_FILENO, &stdout_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDERR_FILENO, &stderr_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDIN_FILENO, &stdin_initial_c_lflag_);
+  }
+
+  static void RestoreConsoleConfiguration() {
+    RestoreConsoleConfigurationHelper(STDOUT_FILENO, stdout_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDERR_FILENO, stderr_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDIN_FILENO, stdin_initial_c_lflag_);
+    stdout_initial_c_lflag_ = -1;
+    stderr_initial_c_lflag_ = -1;
+    stdin_initial_c_lflag_ = -1;
+  }
+
+ private:
+  static tcflag_t stdout_initial_c_lflag_;
+  static tcflag_t stderr_initial_c_lflag_;
+  static tcflag_t stdin_initial_c_lflag_;
+
+  static void SaveConsoleConfigurationHelper(intptr_t fd, tcflag_t* flag) {
+    ASSERT(flag != NULL);
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    *flag = term.c_lflag;
+  }
+
+  static void RestoreConsoleConfigurationHelper(intptr_t fd, tcflag_t flag) {
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    term.c_lflag = flag;
+    NO_RETRY_EXPECTED(tcsetattr(fd, TCSANOW, &term));
+  }
+};
+
+tcflag_t PlatformPosix::stdout_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stderr_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stdin_initial_c_lflag_ = 0;
 
 bool Platform::Initialize() {
   // Turn off the signal handler for SIGPIPE as it causes the process
@@ -62,6 +112,7 @@ bool Platform::Initialize() {
     perror("sigaction() failed.");
     return false;
   }
+  SaveConsoleConfiguration();
   return true;
 }
 
@@ -141,7 +192,16 @@ const char* Platform::ResolveExecutablePath() {
 }
 
 void Platform::Exit(int exit_code) {
+  RestoreConsoleConfiguration();
   exit(exit_code);
+}
+
+void Platform::SaveConsoleConfiguration() {
+  PlatformPosix::SaveConsoleConfiguration();
+}
+
+void Platform::RestoreConsoleConfiguration() {
+  PlatformPosix::RestoreConsoleConfiguration();
 }
 
 }  // namespace bin

runtime/bin/platform_macos.cc
@@ -12,16 +12,19 @@
 #if !HOST_OS_IOS
 #include <crt_externs.h>  // NOLINT
 #endif                    // !HOST_OS_IOS
+#include <errno.h>        // NOLINT
 #include <mach-o/dyld.h>
 #include <signal.h>       // NOLINT
 #include <string.h>       // NOLINT
 #include <sys/sysctl.h>   // NOLINT
 #include <sys/types.h>    // NOLINT
 #include <sys/utsname.h>  // NOLINT
+#include <termios.h>      // NOLINT
 #include <unistd.h>       // NOLINT
 
 #include "bin/fdutils.h"
 #include "bin/file.h"
+#include "platform/signal_blocker.h"
 
 namespace dart {
 namespace bin {
@@ -35,6 +38,53 @@ static void segv_handler(int signal, siginfo_t* siginfo, void* context) {
   Dart_DumpNativeStackTrace(context);
   abort();
 }
+
+class PlatformPosix {
+ public:
+  static void SaveConsoleConfiguration() {
+    SaveConsoleConfigurationHelper(STDOUT_FILENO, &stdout_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDERR_FILENO, &stderr_initial_c_lflag_);
+    SaveConsoleConfigurationHelper(STDIN_FILENO, &stdin_initial_c_lflag_);
+  }
+
+  static void RestoreConsoleConfiguration() {
+    RestoreConsoleConfigurationHelper(STDOUT_FILENO, stdout_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDERR_FILENO, stderr_initial_c_lflag_);
+    RestoreConsoleConfigurationHelper(STDIN_FILENO, stdin_initial_c_lflag_);
+    stdout_initial_c_lflag_ = -1;
+    stderr_initial_c_lflag_ = -1;
+    stdin_initial_c_lflag_ = -1;
+  }
+
+ private:
+  static tcflag_t stdout_initial_c_lflag_;
+  static tcflag_t stderr_initial_c_lflag_;
+  static tcflag_t stdin_initial_c_lflag_;
+
+  static void SaveConsoleConfigurationHelper(intptr_t fd, tcflag_t* flag) {
+    ASSERT(flag != NULL);
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    *flag = term.c_lflag;
+  }
+
+  static void RestoreConsoleConfigurationHelper(intptr_t fd, tcflag_t flag) {
+    struct termios term;
+    int status = NO_RETRY_EXPECTED(tcgetattr(fd, &term));
+    if (status != 0) {
+      return;
+    }
+    term.c_lflag = flag;
+    NO_RETRY_EXPECTED(tcsetattr(fd, TCSANOW, &term));
+  }
+};
+
+tcflag_t PlatformPosix::stdout_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stderr_initial_c_lflag_ = 0;
+tcflag_t PlatformPosix::stdin_initial_c_lflag_ = 0;
 
 bool Platform::Initialize() {
   // Turn off the signal handler for SIGPIPE as it causes the process
@@ -69,6 +119,7 @@ bool Platform::Initialize() {
     perror("sigaction() failed.");
     return false;
   }
+  SaveConsoleConfiguration();
   return true;
 }
 
@@ -229,7 +280,16 @@ const char* Platform::ResolveExecutablePath() {
 }
 
 void Platform::Exit(int exit_code) {
+  RestoreConsoleConfiguration();
   exit(exit_code);
+}
+
+void Platform::SaveConsoleConfiguration() {
+  PlatformPosix::SaveConsoleConfiguration();
+}
+
+void Platform::RestoreConsoleConfiguration() {
+  PlatformPosix::RestoreConsoleConfiguration();
 }
 
 }  // namespace bin

runtime/bin/platform_win.cc
@@ -47,6 +47,9 @@ class PlatformWin {
     platform_win_mutex_ = new Mutex();
     saved_output_cp_ = -1;
     saved_input_cp_ = -1;
+    saved_stdout_mode_ = -1;
+    saved_stderr_mode_ = -1;
+    saved_stdin_mode_ = -1;
     // Set up a no-op handler so that CRT functions return an error instead of
     // hitting an assertion failure.
     // See: https://msdn.microsoft.com/en-us/library/a9yf33zb.aspx
@@ -83,6 +86,9 @@ class PlatformWin {
     // Set both the input and output code pages to UTF8.
     ASSERT(saved_output_cp_ == -1);
     ASSERT(saved_input_cp_ == -1);
+    ASSERT(saved_stdout_mode_ == -1);
+    ASSERT(saved_stderr_mode_ == -1);
+    ASSERT(saved_stdin_mode_ == -1);
     const int output_cp = GetConsoleOutputCP();
     const int input_cp = GetConsoleCP();
     if (output_cp != CP_UTF8) {
@@ -95,12 +101,11 @@ class PlatformWin {
     }
 
     // Try to set the bits for ANSI support, but swallow any failures.
-    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    DWORD out_mode;
-    if ((out != INVALID_HANDLE_VALUE) && GetConsoleMode(out, &out_mode)) {
-      const DWORD request = out_mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-      SetConsoleMode(out, request);
-    }
+    saved_stdout_mode_ =
+        ModifyMode(STD_OUTPUT_HANDLE, ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    saved_stderr_mode_ = ModifyMode(STD_ERROR_HANDLE, 0);
+    saved_stdin_mode_ = ModifyMode(STD_INPUT_HANDLE, 0);
+
     // TODO(28984): Due to issue #29104, we cannot set
     // ENABLE_VIRTUAL_TERMINAL_INPUT here, as it causes ENABLE_PROCESSED_INPUT
     // to be ignored.
@@ -133,43 +138,54 @@ class PlatformWin {
   static Mutex* platform_win_mutex_;
   static int saved_output_cp_;
   static int saved_input_cp_;
+  static DWORD saved_stdout_mode_;
+  static DWORD saved_stderr_mode_;
+  static DWORD saved_stdin_mode_;
 
-  static void RestoreConsoleLocked() {
-    // STD_OUTPUT_HANDLE and STD_INPUT_HANDLE may have been closed or
-    // redirected. Therefore, we explicitly open the CONOUT$ and CONIN$
-    // devices, so that we can be sure that we are really unsetting
-    // ENABLE_VIRTUAL_TERMINAL_PROCESSING and ENABLE_VIRTUAL_TERMINAL_INPUT
-    // respectively.
+  static DWORD ModifyMode(DWORD handle, DWORD flags) {
+    HANDLE h = GetStdHandle(handle);
+    DWORD mode;
+    DWORD old_mode = 0;
+
+    if ((h != INVALID_HANDLE_VALUE) && GetConsoleMode(h, &mode)) {
+      old_mode = mode;
+      if (flags != 0) {
+        const DWORD request = mode | flags;
+        SetConsoleMode(h, request);
+      }
+    }
+    return old_mode;
+  }
+
+  static void CleanupDevices(const char* device,
+                             DWORD handle,
+                             DWORD orig_flags) {
     const intptr_t kWideBufLen = 64;
-    const char* conout = "CONOUT$";
     wchar_t widebuf[kWideBufLen];
     int result =
-        MultiByteToWideChar(CP_UTF8, 0, conout, -1, widebuf, kWideBufLen);
+        MultiByteToWideChar(CP_UTF8, 0, device, -1, widebuf, kWideBufLen);
     ASSERT(result != 0);
-    HANDLE out = CreateFileW(widebuf, GENERIC_READ | GENERIC_WRITE,
-                             FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
-    if (out != INVALID_HANDLE_VALUE) {
-      SetStdHandle(STD_OUTPUT_HANDLE, out);
+    HANDLE h = CreateFileW(widebuf, GENERIC_READ | GENERIC_WRITE,
+                           FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+    if (h != INVALID_HANDLE_VALUE) {
+      SetStdHandle(STD_OUTPUT_HANDLE, h);
+      if (orig_flags != -1) {
+        SetConsoleMode(h, orig_flags);
+      }
     }
-    DWORD out_mode;
-    if ((out != INVALID_HANDLE_VALUE) && GetConsoleMode(out, &out_mode)) {
-      DWORD request = out_mode & ~ENABLE_VIRTUAL_TERMINAL_INPUT;
-      SetConsoleMode(out, request);
-    }
+  }
 
-    const char* conin = "CONIN$";
-    result = MultiByteToWideChar(CP_UTF8, 0, conin, -1, widebuf, kWideBufLen);
-    ASSERT(result != 0);
-    HANDLE in = CreateFileW(widebuf, GENERIC_READ | GENERIC_WRITE,
-                            FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
-    if (in != INVALID_HANDLE_VALUE) {
-      SetStdHandle(STD_INPUT_HANDLE, in);
-    }
-    DWORD in_mode;
-    if ((in != INVALID_HANDLE_VALUE) && GetConsoleMode(in, &in_mode)) {
-      DWORD request = in_mode & ~ENABLE_VIRTUAL_TERMINAL_INPUT;
-      SetConsoleMode(in, request);
-    }
+  static void RestoreConsoleLocked() {
+    // STD_OUTPUT_HANDLE, STD_ERROR_HANDLE, and STD_INPUT_HANDLE may have been
+    // closed or redirected. Therefore, we explicitly open the CONOUT$, CONERR$
+    // and CONIN$ devices, so that we can be sure that we are really restoring
+    // the console to its original state.
+    CleanupDevices("CONOUT$", STD_OUTPUT_HANDLE, saved_stdout_mode_);
+    saved_stdout_mode_ = -1;
+    CleanupDevices("CONERR$", STD_ERROR_HANDLE, saved_stderr_mode_);
+    saved_stderr_mode_ = -1;
+    CleanupDevices("CONIN$", STD_INPUT_HANDLE, saved_stdin_mode_);
+    saved_stdin_mode_ = -1;
 
     if (saved_output_cp_ != -1) {
       SetConsoleOutputCP(saved_output_cp_);
@@ -196,11 +212,15 @@ class PlatformWin {
 
 int PlatformWin::saved_output_cp_ = -1;
 int PlatformWin::saved_input_cp_ = -1;
+DWORD PlatformWin::saved_stdout_mode_ = -1;
+DWORD PlatformWin::saved_stderr_mode_ = -1;
+DWORD PlatformWin::saved_stdin_mode_ = -1;
+
 Mutex* PlatformWin::platform_win_mutex_ = NULL;
 
 bool Platform::Initialize() {
   PlatformWin::InitOnce();
-  PlatformWin::SaveAndConfigureConsole();
+  SaveConsoleConfiguration();
   return true;
 }
 
@@ -385,10 +405,18 @@ void Platform::Exit(int exit_code) {
   // TODO(zra): Remove once VM shuts down cleanly.
   ::dart::private_flag_windows_run_tls_destructors = false;
   // Restore the console's output code page
-  PlatformWin::RestoreConsole();
+  RestoreConsoleConfiguration();
   // On Windows we use ExitProcess so that threads can't clobber the exit_code.
   // See: https://code.google.com/p/nativeclient/issues/detail?id=2870
   ::ExitProcess(exit_code);
+}
+
+void Platform::SaveConsoleConfiguration() {
+  PlatformWin::SaveAndConfigureConsole();
+}
+
+void Platform::RestoreConsoleConfiguration() {
+  PlatformWin::RestoreConsole();
 }
 
 }  // namespace bin

commit 365f7b5a8b6ef900a5ee23913b7203569b81b175

[vm] Fix Fuchsia's Platform::Save/RestoreConsoleConfiguration.
Broken in c9700040c344133f496e6451dffe20de3121acb6.

Change-Id: If85a3d29b7b5eb570c689858d5593dbcbf36026e
Reviewed-on: https://dart-review.googlesource.com/40473
Reviewed-by: Zach Anderson <zra@google.com>

runtime/bin/platform_fuchsia.cc
@@ -150,14 +150,12 @@ void Platform::Exit(int exit_code) {
   exit(exit_code);
 }
 
-bool Platform::SaveConsoleConfiguration() {
+void Platform::SaveConsoleConfiguration() {
   UNIMPLEMENTED();
-  return false;
 }
 
-bool Platform::RestoreConsoleConfiguration() {
+void Platform::RestoreConsoleConfiguration() {
   UNIMPLEMENTED();
-  return false;
 }
 
 }  // namespace bin