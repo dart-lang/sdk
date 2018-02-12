c12fd96ed357afdcb44b6400eace8df261c7110f
Phil Quitslund <pq@users.noreply.github.com>
2018-02-12 07:23:08 -0800

    Get current year dynamically. (dart-lang/linter#890)

--- tool/rule.dart
+++ tool/rule.dart
@@ -94,7 +94,7 @@ void updateRuleRegistry(String ruleName) {
 }
 
 String _generateClass(String ruleName, String className) => """
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c)  $_thisYear, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -141,12 +141,14 @@ class Visitor extends SimpleAstVisitor {
 """;
 
 String _generateTest(String libName, String className) => '''
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) $_thisYear, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 // test w/ `pub run test -N $libName`
 
 ''';
+
+String get _thisYear => new DateTime.now().year.toString();
 
 typedef String _Generator(String libName, String className);
