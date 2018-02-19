ded8dd2ad3a1ec9f8aa3721c915ecd8b34201342
Konstantin Shcheglov <scheglov@google.com>
2018-02-19 19:51:44 +0000

    Don't add explicit 'new' when previewDart2.
    R=brianwilkerson@google.com, devoncarew@google.com
    
    Bug: https://github.com/dart-lang/sdk/issues/32175
    Change-Id: I05c5845a6e511698833045f99716231974e57b61
    Reviewed-on: https://dart-review.googlesource.com/42320
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/lib/src/services/correction/assist_internal.dart
+++ pkg/analysis_server/lib/src/services/correction/assist_internal.dart
@@ -1646,7 +1646,9 @@ class AssistProcessor {
     DartChangeBuilder changeBuilder = new DartChangeBuilder(session);
     await changeBuilder.addFileEdit(file, (DartFileEditBuilder builder) {
       builder.addReplacement(range.node(widgetExpr), (DartEditBuilder builder) {
-        builder.write('new ');
+        if (!driver.analysisOptions.previewDart2) {
+          builder.write('new ');
+        }
         if (parentClassElement == null) {
           builder.addSimpleLinkedEdit('WIDGET', 'widget');
         } else {
@@ -1722,7 +1724,9 @@ class AssistProcessor {
       DartChangeBuilder changeBuilder = new DartChangeBuilder(session);
       await changeBuilder.addFileEdit(file, (DartFileEditBuilder builder) {
         builder.addReplacement(selectedRange, (DartEditBuilder builder) {
-          builder.write('new ');
+          if (!driver.analysisOptions.previewDart2) {
+            builder.write('new ');
+          }
           builder.writeType(parentClassElement.type);
           builder.write('(');
 
