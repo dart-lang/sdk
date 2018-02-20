1c7b7796bec9e3d272ecbfbad7d82e41617a5f11
Danny Tuppeny <danny@tuppeny.com>
2018-02-20 11:40:45 +0000

    Fix paths in imported_elements_computer tests for Windows
    See #32226.

--- pkg/analysis_server/test/src/computer/imported_elements_computer_test.dart
+++ pkg/analysis_server/test/src/computer/imported_elements_computer_test.dart
@@ -27,6 +27,10 @@ class ImportElementsComputerTest extends AbstractContextTest {
     sourcePath = resourceProvider.convertPath('/p/lib/source.dart');
   }
 
+  String convertPath(String path) {
+    return resourceProvider.convertPath(path);
+  }
+
   test_dartAsync_noPrefix() async {
     String selection = "Future<String> f = null;";
     String content = """
@@ -43,7 +47,7 @@ printer() {
     ImportedElements elements2 = elementsList[1];
     ImportedElements asyncElements;
     ImportedElements coreElements;
-    if (elements1.path == '/lib/core/core.dart') {
+    if (elements1.path == convertPath('/lib/core/core.dart')) {
       coreElements = elements1;
       asyncElements = elements2;
     } else {
@@ -51,12 +55,12 @@ printer() {
       asyncElements = elements1;
     }
     expect(coreElements, isNotNull);
-    expect(coreElements.path, '/lib/core/core.dart');
+    expect(coreElements.path, convertPath('/lib/core/core.dart'));
     expect(coreElements.prefix, '');
     expect(coreElements.elements, unorderedEquals(['String']));
 
     expect(asyncElements, isNotNull);
-    expect(asyncElements.path, '/lib/async/async.dart');
+    expect(asyncElements.path, convertPath('/lib/async/async.dart'));
     expect(asyncElements.prefix, '');
     expect(asyncElements.elements, unorderedEquals(['Future']));
   }
@@ -77,7 +81,7 @@ printer() {
     ImportedElements elements2 = elementsList[1];
     ImportedElements asyncElements;
     ImportedElements coreElements;
-    if (elements1.path == '/lib/core/core.dart') {
+    if (elements1.path == convertPath('/lib/core/core.dart')) {
       coreElements = elements1;
       asyncElements = elements2;
     } else {
@@ -85,12 +89,12 @@ printer() {
       asyncElements = elements1;
     }
     expect(coreElements, isNotNull);
-    expect(coreElements.path, '/lib/core/core.dart');
+    expect(coreElements.path, convertPath('/lib/core/core.dart'));
     expect(coreElements.prefix, '');
     expect(coreElements.elements, unorderedEquals(['String']));
 
     expect(asyncElements, isNotNull);
-    expect(asyncElements.path, '/lib/async/async.dart');
+    expect(asyncElements.path, convertPath('/lib/async/async.dart'));
     expect(asyncElements.prefix, 'a');
     expect(asyncElements.elements, unorderedEquals(['Future']));
   }
@@ -108,7 +112,7 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/lib/core/core.dart');
+    expect(elements.path, convertPath('/lib/core/core.dart'));
     expect(elements.prefix, '');
     expect(elements.elements, unorderedEquals(['String']));
   }
@@ -127,7 +131,7 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/lib/core/core.dart');
+    expect(elements.path, convertPath('/lib/core/core.dart'));
     expect(elements.prefix, 'core');
     expect(elements.elements, unorderedEquals(['String']));
   }
@@ -146,7 +150,7 @@ bool randomBool() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/lib/math/math.dart');
+    expect(elements.path, convertPath('/lib/math/math.dart'));
     expect(elements.prefix, '');
     expect(elements.elements, unorderedEquals(['Random']));
   }
@@ -170,13 +174,13 @@ $selection
 
     ImportedElements mathElements = elementsList[0];
     expect(mathElements, isNotNull);
-    expect(mathElements.path, '/lib/math/math.dart');
+    expect(mathElements.path, convertPath('/lib/math/math.dart'));
     expect(mathElements.prefix, '');
     expect(mathElements.elements, unorderedEquals(['Random']));
 
     ImportedElements coreElements = elementsList[1];
     expect(coreElements, isNotNull);
-    expect(coreElements.path, '/lib/core/core.dart');
+    expect(coreElements.path, convertPath('/lib/core/core.dart'));
     expect(coreElements.prefix, '');
     expect(coreElements.elements, unorderedEquals(['String', 'print']));
   }
@@ -256,7 +260,8 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/pubcache/foo/lib/foo.dart');
+    expect(elements.path,
+        resourceProvider.convertPath('/pubcache/foo/lib/foo.dart'));
     expect(elements.prefix, '');
     expect(elements.elements, unorderedEquals(['A', 'B']));
   }
@@ -279,7 +284,7 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/pubcache/foo/lib/foo.dart');
+    expect(elements.path, convertPath('/pubcache/foo/lib/foo.dart'));
     expect(elements.prefix, '');
     expect(elements.elements, unorderedEquals(['Foo']));
   }
@@ -302,7 +307,7 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/pubcache/foo/lib/foo.dart');
+    expect(elements.path, convertPath('/pubcache/foo/lib/foo.dart'));
     expect(elements.prefix, 'f');
     expect(elements.elements, unorderedEquals(['Foo']));
   }
@@ -325,7 +330,7 @@ blankLine() {
     expect(elementsList, hasLength(1));
     ImportedElements elements = elementsList[0];
     expect(elements, isNotNull);
-    expect(elements.path, '/pubcache/foo/lib/foo.dart');
+    expect(elements.path, convertPath('/pubcache/foo/lib/foo.dart'));
     expect(elements.prefix, '');
     expect(elements.elements, unorderedEquals(['Foo']));
   }
@@ -362,12 +367,12 @@ blankLine() {
     }
 
     expect(notPrefixedElements, isNotNull);
-    expect(notPrefixedElements.path, '/pubcache/foo/lib/foo.dart');
+    expect(notPrefixedElements.path, convertPath('/pubcache/foo/lib/foo.dart'));
     expect(notPrefixedElements.prefix, '');
     expect(notPrefixedElements.elements, unorderedEquals(['Foo']));
 
     expect(prefixedElements, isNotNull);
-    expect(prefixedElements.path, '/pubcache/foo/lib/foo.dart');
+    expect(prefixedElements.path, convertPath('/pubcache/foo/lib/foo.dart'));
     expect(prefixedElements.prefix, 'f');
     expect(prefixedElements.elements, unorderedEquals(['Foo']));
   }
