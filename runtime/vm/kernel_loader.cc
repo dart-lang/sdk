dart-lang/sdk

commit 8881ec77ec7aefd24447572370d6fa4c43d89292

[vm/kernel] Move work-around for #32087 into the loader.
While working on #32274 I noticed that #32087 also impacted debugging
experience in the external debuggers like VS Code - IDE attempts to
load non-existent local sources for core libraries, instead of
asking Service for their source.

To resolve this I moved work-around we placed in stack trace formatting
futher up the flow into kernel loader, which will now rewrite urls
for all core library scripts as classes are being created.

Bug: 32087
Change-Id: I2175a041e5fdcc9958bfccb2af724c37c2435f94
Reviewed-on: https://dart-review.googlesource.com/43021
Reviewed-by: Jens Johansen <jensj@google.com>
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>

runtime/vm/kernel_loader.cc
@@ -14,6 +14,7 @@
 #include "vm/longjump.h"
 #include "vm/object_store.h"
 #include "vm/parser.h"
+#include "vm/reusable_handles.h"
 #include "vm/service_isolate.h"
 #include "vm/symbols.h"
 
@@ -967,6 +968,44 @@ void KernelLoader::LoadPreliminaryClass(ClassHelper* class_helper,
   if (class_helper->is_abstract_) klass->set_is_abstract();
 }
 
+// Workaround for http://dartbug.com/32087: currently Kernel front-end
+// embeds absolute build-time paths to core library sources into Kernel
+// binaries this introduces discrepancy between how stack traces were
+// looked like in legacy pipeline and how they look in Dart 2 pipeline and
+// breaks users' code that attempts to pattern match and filter various
+// irrelevant frames (e.g. frames from dart:async).
+// This also breaks debugging experience in external debuggers because
+// debugger attempts to open files that don't exist in the local file
+// system.
+// To work around this issue we reformat urls of scripts belonging to
+// dart:-scheme libraries to look like they looked like in legacy pipeline:
+//
+//               dart:libname/filename.dart
+//
+void KernelLoader::FixCoreLibraryScriptUri(const Library& library,
+                                           const Script& script) {
+  if (library.is_dart_scheme()) {
+    REUSABLE_STRING_HANDLESCOPE(thread_);
+    String& url = thread_->StringHandle();
+    url = script.url();
+    if (!url.StartsWith(Symbols::DartScheme())) {
+      // Search backwards until '/' is found. That gives us the filename.
+      // Note: can't use reusable handle in the code below because
+      // concat also needs it.
+      String& new_url = String::Handle(zone_, url.raw());
+      intptr_t pos = new_url.Length() - 1;
+      while (pos >= 0 && new_url.CharAt(pos) != '/') {
+        pos--;
+      }
+
+      new_url = String::SubString(new_url, pos + 1);
+      new_url = String::Concat(Symbols::Slash(), new_url);
+      new_url = String::Concat(String::Handle(zone_, library.url()), new_url);
+      script.set_url(new_url);
+    }
+  }
+}
+
 Class& KernelLoader::LoadClass(const Library& library,
                                const Class& toplevel_class,
                                intptr_t class_end) {
@@ -987,6 +1026,7 @@ Class& KernelLoader::LoadClass(const Library& library,
     const Script& script =
         Script::Handle(Z, ScriptAt(class_helper.source_uri_index_));
     klass.set_script(script);
+    FixCoreLibraryScriptUri(library, script);
   }
   if (klass.token_pos() == TokenPosition::kNoSource) {
     class_helper.ReadUntilIncluding(ClassHelper::kPosition);
@@ -1345,6 +1385,7 @@ const Object& KernelLoader::ClassForScriptAt(const Class& klass,
     patch_class ^= patch_classes_.At(source_uri_index);
     if (patch_class.IsNull() || patch_class.origin_class() != klass.raw()) {
       ASSERT(!library_kernel_data_.IsNull());
+      FixCoreLibraryScriptUri(Library::Handle(klass.library()), correct_script);
       patch_class = PatchClass::New(klass, correct_script);
       patch_class.set_library_kernel_data(library_kernel_data_);
       patch_class.set_library_kernel_offset(library_kernel_offset_);

runtime/vm/kernel_loader.h
@@ -192,6 +192,8 @@ class KernelLoader {
 
   void CheckForInitializer(const Field& field);
 
+  void FixCoreLibraryScriptUri(const Library& library, const Script& script);
+
   Class& LoadClass(const Library& library,
                    const Class& toplevel_class,
                    intptr_t class_end);

runtime/vm/object.cc
@@ -22658,35 +22658,7 @@ static void PrintStackTraceFrame(Zone* zone,
     }
   }
 
-  // Workaround for http://dartbug.com/32087: currently Kernel front-end
-  // embeds absolute build-time paths to core library sources into Kernel
-  // binaries this introduces discrepancy between how stack traces were
-  // looked like in legacy pipeline and how they look in Dart 2 pipeline and
-  // breaks users' code that attempts to pattern match and filter various
-  // irrelevant frames (e.g. frames from dart:async).
-  // To work around this issue we reformat urls of scripts belonging to
-  // dart:-scheme libraries to look like they looked like in legacy pipeline:
-  //
-  //               dart:libname/filename.dart
-  //
   const char* url_string = url.ToCString();
-  if (script.kernel_program_info() != KernelProgramInfo::null()) {
-    const Class& owner = Class::Handle(function.Owner());
-    const Library& lib = Library::Handle(owner.library());
-    if (lib.is_dart_scheme()) {
-      // Search backwards until '/' is found. That gives us the filename.
-      intptr_t pos = strlen(url_string) - 1;
-      while (pos >= 0 && url_string[pos] != '/') {
-        pos--;
-      }
-      const char* filename = url_string + (pos + 1);
-
-      // Glue together canonic library url (e.g. dart:async) and filename.
-      url_string = zone->PrintToString(
-          "%s/%s", String::Handle(lib.url()).ToCString(), filename);
-    }
-  }
-
   if (column >= 0) {
     buffer->Printf("#%-6" Pd " %s (%s:%" Pd ":%" Pd ")\n", frame_index,
                    function_name.ToCString(), url_string, line, column);

commit e9ca5ea55f7936afb7a50f2dcaffc05641ef13c8

[vm/kernel] Unbreak debug build broken by 8881ec77ec7aefd24447572370d6fa4c43d89292
Change-Id: I34d30b53f39616c3181bdaee107928c2a93aada2
Reviewed-on: https://dart-review.googlesource.com/43220
Reviewed-by: Vyacheslav Egorov <vegorov@google.com>

runtime/vm/kernel_loader.cc
@@ -985,23 +985,20 @@ void KernelLoader::LoadPreliminaryClass(ClassHelper* class_helper,
 void KernelLoader::FixCoreLibraryScriptUri(const Library& library,
                                            const Script& script) {
   if (library.is_dart_scheme()) {
-    REUSABLE_STRING_HANDLESCOPE(thread_);
-    String& url = thread_->StringHandle();
-    url = script.url();
+    String& url = String::Handle(zone_, script.url());
     if (!url.StartsWith(Symbols::DartScheme())) {
       // Search backwards until '/' is found. That gives us the filename.
       // Note: can't use reusable handle in the code below because
       // concat also needs it.
-      String& new_url = String::Handle(zone_, url.raw());
-      intptr_t pos = new_url.Length() - 1;
-      while (pos >= 0 && new_url.CharAt(pos) != '/') {
+      intptr_t pos = url.Length() - 1;
+      while (pos >= 0 && url.CharAt(pos) != '/') {
         pos--;
       }
 
-      new_url = String::SubString(new_url, pos + 1);
-      new_url = String::Concat(Symbols::Slash(), new_url);
-      new_url = String::Concat(String::Handle(zone_, library.url()), new_url);
-      script.set_url(new_url);
+      url = String::SubString(url, pos + 1);
+      url = String::Concat(Symbols::Slash(), url);
+      url = String::Concat(String::Handle(zone_, library.url()), url);
+      script.set_url(url);
     }
   }
 }