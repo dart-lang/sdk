a96855a40f8171e4d6581e7beb8a53db7f6650d6
Peter von der Ahé <ahe@google.com>
2018-02-28 14:13:55 +0000

    Move exceptions to status file
    Change-Id: Iee0e98e5e1e9884f798db3e130d4fca6c8ac6477
    Reviewed-on: https://dart-review.googlesource.com/44064
    Reviewed-by: Dan Rubel <danrubel@google.com>
    Commit-Queue: Peter von der Ahé <ahe@google.com>

--- pkg/front_end/test/fasta/messages_test.dart
+++ pkg/front_end/test/fasta/messages_test.dart
@@ -15,14 +15,6 @@ import "package:testing/testing.dart"
 
 import "package:yaml/yaml.dart" show loadYaml;
 
-part "messages_exceptions.dart";
-
-final Set<String> messagesWithoutExamples =
-    new Set<String>.from(listOfMessagesWithoutExamples);
-
-final Set<String> messagesWithoutAnalyzerCode =
-    new Set<String>.from(listOfMessagesWithoutAnalyzerCode);
-
 class MessageTestDescription extends TestDescription {
   @override
   final Uri uri;
@@ -30,33 +22,171 @@ class MessageTestDescription extends TestDescription {
   @override
   final String shortName;
 
+  final String name;
+
   final Map data;
 
-  MessageTestDescription(this.uri, this.shortName, this.data);
+  final Example example;
+
+  final String problem;
+
+  MessageTestDescription(this.uri, this.shortName, this.name, this.data,
+      this.example, this.problem);
 }
 
 class MessageTestSuite extends ChainContext {
   final List<Step> steps = const <Step>[
-    const ValidateMessage(),
-    const CompileExamples(),
+    const Validate(),
+    const Compile(),
   ];
 
+  /// Convert all the examples found in `messages.yaml` to a test
+  /// description. In addition, for each problem found, create a test
+  /// description that has a problem. This problem will then be reported as a
+  /// failure by the [Validate] step that can be suppressed via the status
+  /// file.
   Stream<MessageTestDescription> list(Chain suite) async* {
     Uri uri = suite.uri.resolve("messages.yaml");
     File file = new File.fromUri(uri);
     Map yaml = loadYaml(await file.readAsString());
-    for (String key in yaml.keys) {
-      var data = yaml[key];
+    for (String name in yaml.keys) {
+      var data = yaml[name];
       if (data is String) continue;
-      yield new MessageTestDescription(uri.resolve("#$key"), key, data);
+
+      List<String> unknownKeys = <String>[];
+      List<Example> examples = <Example>[];
+      String analyzerCode;
+      String dart2jsCode;
+
+      for (String key in data.keys) {
+        var value = data[key];
+        switch (key) {
+          case "template":
+          case "tip":
+          case "severity":
+            break;
+
+          case "analyzerCode":
+            analyzerCode = value;
+            break;
+
+          case "dart2jsCode":
+            dart2jsCode = value;
+            break;
+
+          case "bytes":
+            if (value.first is List) {
+              for (List bytes in value) {
+                int i = 0;
+                examples.add(new BytesExample("bytes${++i}", name, bytes));
+              }
+            } else {
+              examples.add(new BytesExample("bytes", name, value));
+            }
+            break;
+
+          case "declaration":
+            if (value is List) {
+              int i = 0;
+              for (String declaration in value) {
+                examples.add(new DeclarationExample(
+                    "declaration${++i}", name, declaration));
+              }
+            } else {
+              examples.add(new DeclarationExample("declaration", name, value));
+            }
+            break;
+
+          case "expression":
+            if (value is List) {
+              int i = 0;
+              for (String expression in value) {
+                examples.add(new ExpressionExample(
+                    "expression${++i}", name, expression));
+              }
+            } else {
+              examples.add(new ExpressionExample("expression", name, value));
+            }
+            break;
+
+          case "script":
+            if (value is List) {
+              int i = 0;
+              for (String script in value) {
+                examples.add(new ScriptExample("script${++i}", name, script));
+              }
+            } else {
+              examples.add(new ScriptExample("script", name, value));
+            }
+            break;
+
+          case "statement":
+            if (value is List) {
+              int i = 0;
+              for (String statement in value) {
+                examples.add(
+                    new StatementExample("statement${++i}", name, statement));
+              }
+            } else {
+              examples.add(new StatementExample("statement", name, value));
+            }
+            break;
+
+          default:
+            unknownKeys.add(key);
+        }
+      }
+
+      MessageTestDescription createDescription(
+          String subName, Example example, String problem) {
+        String shortName = "$name/$subName";
+        if (problem != null) {
+          String base = "${Uri.base}";
+          String filename = "$uri";
+          if (filename.startsWith(base)) {
+            filename = filename.substring(base.length);
+          }
+          var location = data.span.start;
+          int line = location.line;
+          int column = location.column;
+          problem = "$filename:$line:$column: error:\n$problem";
+        }
+        return new MessageTestDescription(uri.resolve("#$shortName"), shortName,
+            name, data, example, problem);
+      }
+
+      for (Example example in examples) {
+        yield createDescription(example.name, example, null);
+      }
+
+      if (unknownKeys.isNotEmpty) {
+        yield createDescription(
+            "knownKeys", null, "Unknown keys: ${unknownKeys.join(' ')}");
+      }
+
+      if (examples.isEmpty) {
+        yield createDescription("example", null, "No example for $name");
+      }
+
+      if (analyzerCode == null) {
+        yield createDescription(
+            "analyzerCode", null, "No analyzer code for $name");
+      } else {
+        if (dart2jsCode == null) {
+          yield createDescription(
+              "dart2jsCode", null, "No dart2js code for $name");
+        }
+      }
     }
   }
 }
 
 abstract class Example {
+  final String name;
+
   final String expectedCode;
 
-  Example(this.expectedCode);
+  Example(this.name, this.expectedCode);
 
   Uint8List get bytes;
 }
@@ -65,15 +195,16 @@ class BytesExample extends Example {
   @override
   final Uint8List bytes;
 
-  BytesExample(String code, List bytes)
+  BytesExample(String name, String code, List bytes)
       : bytes = new Uint8List.fromList(bytes),
-        super(code);
+        super(name, code);
 }
 
 class DeclarationExample extends Example {
   final String declaration;
 
-  DeclarationExample(String code, this.declaration) : super(code);
+  DeclarationExample(String name, String code, this.declaration)
+      : super(name, code);
 
   @override
   Uint8List get bytes {
@@ -89,7 +220,8 @@ main() {
 class StatementExample extends Example {
   final String statement;
 
-  StatementExample(String code, this.statement) : super(code);
+  StatementExample(String name, String code, this.statement)
+      : super(name, code);
 
   @override
   Uint8List get bytes {
@@ -104,7 +236,8 @@ main() {
 class ExpressionExample extends Example {
   final String expression;
 
-  ExpressionExample(String code, this.expression) : super(code);
+  ExpressionExample(String name, String code, this.expression)
+      : super(name, code);
 
   @override
   Uint8List get bytes {
@@ -119,7 +252,7 @@ main() {
 class ScriptExample extends Example {
   final String script;
 
-  ScriptExample(String code, this.script) : super(code);
+  ScriptExample(String name, String code, this.script) : super(name, code);
 
   @override
   Uint8List get bytes {
@@ -127,140 +260,32 @@ class ScriptExample extends Example {
   }
 }
 
-class ValidateMessage
-    extends Step<MessageTestDescription, List<Example>, MessageTestSuite> {
-  const ValidateMessage();
+class Validate extends Step<MessageTestDescription, Example, MessageTestSuite> {
+  const Validate();
 
   String get name => "validate";
 
-  Future<Result<List<Example>>> run(
+  Future<Result<Example>> run(
       MessageTestDescription description, MessageTestSuite suite) async {
-    Map data = description.data;
-    String name = description.shortName;
-
-    List<String> unknownKeys = <String>[];
-    List<Example> examples = <Example>[];
-    String analyzerCode;
-    String dart2jsCode;
-
-    for (String key in data.keys) {
-      var value = data[key];
-      switch (key) {
-        case "template":
-        case "tip":
-        case "severity":
-          break;
-
-        case "analyzerCode":
-          analyzerCode = value;
-          break;
-
-        case "dart2jsCode":
-          dart2jsCode = value;
-          break;
-
-        case "bytes":
-          if (value.first is List) {
-            for (List bytes in value) {
-              examples.add(new BytesExample(name, bytes));
-            }
-          } else {
-            examples.add(new BytesExample(name, value));
-          }
-          break;
-
-        case "declaration":
-          if (value is List) {
-            for (String declaration in value) {
-              examples.add(new DeclarationExample(name, declaration));
-            }
-          } else {
-            examples.add(new DeclarationExample(name, value));
-          }
-          break;
-
-        case "expression":
-          if (value is List) {
-            for (String expression in value) {
-              examples.add(new ExpressionExample(name, expression));
-            }
-          } else {
-            examples.add(new ExpressionExample(name, value));
-          }
-          break;
-
-        case "script":
-          if (value is List) {
-            for (String script in value) {
-              examples.add(new ScriptExample(name, script));
-            }
-          } else {
-            examples.add(new ScriptExample(name, value));
-          }
-          break;
-
-        case "statement":
-          if (value is List) {
-            for (String statement in value) {
-              examples.add(new StatementExample(name, statement));
-            }
-          } else {
-            examples.add(new StatementExample(name, value));
-          }
-          break;
-
-        default:
-          unknownKeys.add(key);
-      }
-    }
-
-    if (unknownKeys.isNotEmpty) {
-      return fail(null, "Unknown keys: ${unknownKeys.join(' ')}");
-    }
-
-    if (examples.isEmpty) {
-      if (!messagesWithoutExamples.contains(name)) {
-        return fail(null, "No example for $name");
-      }
+    if (description.problem != null) {
+      return fail(null, description.problem);
     } else {
-      if (messagesWithoutExamples.contains(name)) {
-        return fail(
-            null, "$name can be removed from listOfMessagesWithoutExamples");
-      }
+      return pass(description.example);
     }
-
-    if (analyzerCode == null) {
-      if (!messagesWithoutAnalyzerCode.contains(name)) {
-        return fail(null, "No analyzer code for $name");
-      }
-    } else {
-      if (messagesWithoutAnalyzerCode.contains(name)) {
-        return fail(
-            null, "$name can be removed fro listOfMessagesWithoutAnalyzerCode");
-      }
-      if (dart2jsCode == null) {
-        return fail(null, "No dart2js code for $name");
-      }
-    }
-
-    return pass(examples);
   }
 }
 
-class CompileExamples extends Step<List<Example>, Null, MessageTestSuite> {
-  const CompileExamples();
+class Compile extends Step<Example, Null, MessageTestSuite> {
+  const Compile();
 
   String get name => "compile";
 
-  Future<Result<Null>> run(
-      List<Example> examples, MessageTestSuite suite) async {
-    for (Example example in examples) {
-      // TODO(ahe): This is where I should actually compile the examples and
-      // verify that only one message is reported, and it is the expected
-      // message.
-      if (example is! BytesExample) {
-        print(utf8.decode(example.bytes));
-      }
+  Future<Result<Null>> run(Example example, MessageTestSuite suite) async {
+    // TODO(ahe): This is where I should actually compile the example and
+    // verify that only one message is reported, and it is the expected
+    // message.
+    if (example is! BytesExample) {
+      print(utf8.decode(example.bytes));
     }
     return pass(null);
   }
