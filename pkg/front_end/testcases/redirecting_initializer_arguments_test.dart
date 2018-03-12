f820555d192450c5910f1a3e646da3129849337f
Dmitry Stefantsov <dmitryas@google.com>
2018-03-12 08:49:37 +0000

    Add test of type checks on redirecting initializer arguments
    Bug: http://dartbug.com/32343
    Change-Id: I863e370a3148f7a7da7a4c4599e005b381ceffa7
    Reviewed-on: https://dart-review.googlesource.com/44465
    Commit-Queue: Dmitry Stefantsov <dmitryas@google.com>
    Reviewed-by: Leaf Petersen <leafp@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>

---
+++ pkg/front_end/testcases/redirecting_initializer_arguments_test.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// The test checks that types of arguments of redirecting initializers are
+// checked against the corresponding formal parameter types of the redirection
+// targets, and the compile-time error is emitted in the case they are not
+// assignable.
+
+class Foo<T> {
+  T x;
+  Foo.from(String _init) : this._internal(x: _init);
+  Foo._internal({this.x});
+}
+
+void main() {}
