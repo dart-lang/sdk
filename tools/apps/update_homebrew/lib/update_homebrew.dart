970b03986da02173130d987b45fe72aa30339657
Kevin Moore <kevmoo@google.com>
2018-03-23 01:45:34 +0000

    Update homebrew formula to fail with dartium on dev releases
    Closes https://github.com/dart-lang/sdk/issues/32642
    
    Change-Id: I221ec3cca7c6965a0feef1dd380160221e1b5fb2
    Reviewed-on: https://dart-review.googlesource.com/48000
    Reviewed-by: Natalie Weizenbaum <nweiz@google.com>

--- tools/apps/update_homebrew/lib/update_homebrew.dart
+++ tools/apps/update_homebrew/lib/update_homebrew.dart
@@ -157,12 +157,18 @@ class Dart < Formula
     bin.write_exec_script Dir["#{libexec}/bin/{pub,dart?*}"]
 
     if build.with? "dartium"
+      if build.devel?
+        odie "dartium is no longer supported with --devel builds. Remove --with-dartium and try again."
+      end
       dartium_binary = "Chromium.app/Contents/MacOS/Chromium"
       prefix.install resource("dartium")
       (bin+"dartium").write shim_script dartium_binary
     end
 
     if build.with? "content-shell"
+      if build.devel?
+        odie "content-shell is no longer supported with --devel builds. Remove --with-content-shell and try again."
+      end
       content_shell_binary = "Content Shell.app/Contents/MacOS/Content Shell"
       prefix.install resource("content_shell")
       (bin+"content_shell").write shim_script content_shell_binary
