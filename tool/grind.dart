d46985419bec51ce3bad8c89d8f98a1ac97a1415
Phil Quitslund <pq@users.noreply.github.com>
2018-04-05 13:23:50 -0700

    Add a format grind task. (dart-lang/linter#962)

--- tool/grind.dart
+++ tool/grind.dart
@@ -9,6 +9,7 @@ import 'package:grinder/grinder.dart';
 import 'doc.dart';
 import 'rule.dart';
 
+//TODO(pq): add a validate task.
 main(args) => grind(args);
 
 @Task('Generate lint rule docs.')
@@ -23,4 +24,22 @@ rule() {
   TaskArgs args = context.invocation.arguments;
   String name = args.getOption('name');
   generateRule(name, outDir: Directory.current.path);
+}
+
+@Task('Format the linter sources.')
+format() {
+  var toFormat = existingSourceDirs.expand((dir) {
+    // Skip:
+    //   'test/rules'
+    //   'test/_data'
+    if (dir.path == 'test') {
+      return dir
+          .listSync(followLinks: false)
+          .where((dir) => dir.path != 'test/rules' && dir.path != 'test/_data');
+    }
+    return [dir];
+  });
+  Pub.run('dart_style',
+      script: 'format',
+      arguments: ['--overwrite']..addAll(toFormat.map((dir) => dir.path)));
 }
