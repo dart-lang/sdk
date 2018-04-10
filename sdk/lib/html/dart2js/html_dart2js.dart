6ab6d1eda95db34e91f355d0a599e9ddbc5893e8
Terry Lucas <terry@google.com>
2018-04-10 23:25:12 +0000

    Suppress fromRect its only available in DOMRect and not in ClientRect - will open bug to fix this issue.
    R=sra@google.com
    
    Change-Id: I09b653c7f3987dcf7ce5b235593e4f3140561d5c
    Reviewed-on: https://dart-review.googlesource.com/50581
    Reviewed-by: Stephen Adams <sra@google.com>
    Commit-Queue: Terry Lucas <terry@google.com>

--- sdk/lib/html/dart2js/html_dart2js.dart
+++ sdk/lib/html/dart2js/html_dart2js.dart
@@ -45145,28 +45145,6 @@ class _DomRect extends DomRectReadOnly implements Rectangle {
   set y(num value) {
     JS("void", "#.y = #", this, value);
   }
-
-  @DomName('DOMRect.fromRect')
-  @DocsEditable()
-  @Experimental() // untriaged
-  static Rectangle fromRect([Map other]) {
-    if (other != null) {
-      var other_1 = convertDartToNative_Dictionary(other);
-      return _fromRect_1(other_1);
-    }
-    return _fromRect_2();
-  }
-
-  @JSName('fromRect')
-  @DomName('DOMRect.fromRect')
-  @DocsEditable()
-  @Experimental() // untriaged
-  static Rectangle _fromRect_1(other) native;
-  @JSName('fromRect')
-  @DomName('DOMRect.fromRect')
-  @DocsEditable()
-  @Experimental() // untriaged
-  static Rectangle _fromRect_2() native;
 }
 
 /**
