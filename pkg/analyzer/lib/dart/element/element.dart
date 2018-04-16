a865531f57fa3f5257a9261363c4dddb6a1a7c98
Brian Wilkerson <brianwilkerson@google.com>
2018-04-16 19:15:29 +0000

    Remove two unused classes from the public API
    Change-Id: I4d27bc95d3a3e0ffc3ad379ffd823abf4fff0db1
    Reviewed-on: https://dart-review.googlesource.com/51244
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/lib/dart/element/element.dart
+++ pkg/analyzer/lib/dart/element/element.dart
@@ -35,7 +35,6 @@
  * represented by an element.
  */
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/resolution_base_classes.dart';
 import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/src/generated/engine.dart' show AnalysisContext;
@@ -562,7 +561,7 @@ abstract class ConstructorElement
  *
  * Clients may not extend, implement or mix-in this class.
  */
-abstract class Element implements AnalysisTarget, ResolutionTarget {
+abstract class Element implements AnalysisTarget {
   /**
    * A comparator that can be used to sort elements by their name offset.
    * Elements with a smaller offset will be sorted to be before elements with a
@@ -846,8 +845,7 @@ abstract class Element implements AnalysisTarget, ResolutionTarget {
  *
  * Clients may not extend, implement or mix-in this class.
  */
-abstract class ElementAnnotation
-    implements ConstantEvaluationTarget, ResolutionTarget {
+abstract class ElementAnnotation implements ConstantEvaluationTarget {
   /**
    * An empty list of annotations.
    */
