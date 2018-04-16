f85480b4dd910b247580c6c9d48b2036ec6e9670
Samir Jindel <sjindel@google.com>
2018-04-16 19:29:38 +0000

    Add tests for CFE implementation of expression compilation.
    Change-Id: Ib7af386876d831e84ca97c42bbe3084c96fcc114
    Reviewed-on: https://dart-review.googlesource.com/49906
    Commit-Queue: Samir Jindel <sjindel@google.com>
    Reviewed-by: Peter von der Ahé <ahe@google.com>

---
+++ pkg/front_end/testcases/expression/main.dart
@@ -0,0 +1,57 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library main;
+
+import 'dart:io' show File, Process, exit;
+
+int doitstat(int x) => x + 1;
+
+int globalVar = 6;
+
+class A<T> {
+  const A();
+  static int doit(int x) => x + 1;
+  static int staticVar = 3;
+  int doit_with_this(int x) => x + 1;
+}
+
+T id<T>(T x) => x;
+
+class B {
+  int x;
+  final int y;
+  String get z {
+    return "";
+  }
+
+  void set z(int _) {}
+}
+
+class Bound {}
+
+class C<T extends Bound> {}
+
+void hasBound<T extends Bound>() {}
+
+Object k;
+
+class D<T> {
+  Y id<Y>(Y x) => x;
+  m(List<T> l) {
+    assert(l is List<T>);
+  }
+
+  foo() {
+    List<T> s;
+  }
+}
+
+main() {
+  exit(0);
+}
