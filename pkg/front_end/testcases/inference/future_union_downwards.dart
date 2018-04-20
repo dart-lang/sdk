f50c86c419a62b2a72987db85d59dc53d6f133ed
Aske Simon Christensen <askesc@google.com>
2018-04-20 10:41:39 +0000

    Fixed noSuchMethod implementation in future subclass tests.
    Change-Id: Ia8f5eb36b2103b7312062167ae917e9c97b738b5
    Reviewed-on: https://dart-review.googlesource.com/51843
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Aske Simon Christensen <askesc@google.com>

--- pkg/front_end/testcases/inference/future_union_downwards.dart
+++ pkg/front_end/testcases/inference/future_union_downwards.dart
@@ -10,7 +10,7 @@ import 'dart:async';
 class MyFuture<T> implements Future<T> {
   MyFuture() {}
   MyFuture.value([x]) {}
-  dynamic noSuchMethod(/*@topType=Invocation*/ invocation);
+  dynamic noSuchMethod(/*@topType=Invocation*/ invocation) => null;
   MyFuture<S> then<S>(FutureOr<S> f(T x), {Function onError}) => null;
 }
 
