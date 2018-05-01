7fe8659613bfbf360d57d12327b7521cd869987a
Kevin Moore <kevmoo@google.com>
2018-05-01 16:47:26 +0000

    dump-info: Use relative paths for library canonicalUri...
    ...if they are within the working directory
    
    Makes output from dump-info more stable, especially with pkg:build
    
    Change-Id: I3e6c444a0aa7df55c10e1bdff4b59ef035d2acc3
    Reviewed-on: https://dart-review.googlesource.com/53168
    Reviewed-by: Harry Terkelsen <het@google.com>
    Commit-Queue: Kevin Moore <kevmoo@google.com>

--- pkg/compiler/lib/src/dump_info.dart
+++ pkg/compiler/lib/src/dump_info.dart
@@ -8,6 +8,7 @@ import 'dart:convert'
     show ChunkedConversionSink, JsonEncoder, StringConversionSink;
 
 import 'package:dart2js_info/info.dart';
+import 'package:path/path.dart' as p;
 
 import '../compiler_new.dart';
 import 'common/names.dart';
@@ -77,7 +78,17 @@ class ElementInfoCollector {
       libname = '<unnamed>';
     }
     int size = compiler.dumpInfoTask.sizeOf(lib);
-    LibraryInfo info = new LibraryInfo(libname, lib.canonicalUri, null, size);
+
+    var uri = lib.canonicalUri;
+    if (Uri.base.isScheme('file') && lib.canonicalUri.isScheme('file')) {
+      var basePath = p.fromUri(Uri.base);
+      var libPath = p.fromUri(lib.canonicalUri);
+      if (p.isWithin(basePath, libPath)) {
+        uri = p.toUri(p.relative(libPath, from: basePath));
+      }
+    }
+
+    LibraryInfo info = new LibraryInfo(libname, uri, null, size);
     _entityToInfo[lib] = info;
 
     environment.forEachLibraryMember(lib, (MemberEntity member) {
