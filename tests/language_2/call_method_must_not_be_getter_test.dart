58f41fd1b56bfc1ed0dd995277db0cf3e7cf0696
Jenny Messerly <jmesserly@google.com>
2018-05-03 21:25:07 +0000

    update issue21159_test for Dart 2, fixes #30701
    Change-Id: I170a253621f54ea89f79bd50003e22295377f4c4
    Reviewed-on: https://dart-review.googlesource.com/52864
    Reviewed-by: Leaf Petersen <leafp@google.com>

--- tests/language_2/call_method_must_not_be_getter_test.dart
+++ tests/language_2/call_method_must_not_be_getter_test.dart
@@ -9,6 +9,16 @@ class C {
   void Function() get call => () {};
 }
 
+class D {
+  get call => this;
+}
+
+// Recurs outside the try-block to avoid disabling inlining.
+callD() {
+  dynamic d = new D();
+  d();
+}
+
 main() {
   C c = new C();
   dynamic d = c;
@@ -20,6 +30,9 @@ main() {
   void Function() f = c; //# 02: compile-time error
   // Nor does it permit a dynamic invocation of `call`.
   Expect.throws(() => d()); //# 03: ok
+  // Same as previous line, but with a getter that can stack overflow if
+  // incorrect behavior is present. Merged from issue21159_test.
+  Expect.throwsNoSuchMethodError(() => callD()); //# 03: ok
 
   // However, all these things are possible if `call` is mentioned explicitly.
   c.call(); //# 04: ok
