dart-lang/sdk

commit fc6af81a7a0c840ae99009ff25e3fef36d55a166

[vm] Use LoadWordFromPool in AssertAssignable on ARMs instead of raw ldr
Pool offsets might be not encodable as immediates. However
 InstructionPattern::DecodeLoadWordFromPool can decode anything that
 LoadWordFromPool can emit.

Fixes https://github.com/flutter/flutter/issues/18332

Change-Id: I8873f523bb65844e7256c0c3030edea4cc591830
Reviewed-on: https://dart-review.googlesource.com/59640
Reviewed-by: Martin Kustermann <kustermann@google.com>
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>

runtime/vm/compiler/backend/flow_graph_compiler_arm64.cc
@@ -712,7 +712,7 @@ void FlowGraphCompiler::GenerateAssertAssignableViaTypeTestingStub(
   const intptr_t sub_type_cache_index = __ object_pool_wrapper().AddObject(
       Object::null_object(), Patchability::kPatchable);
   const intptr_t sub_type_cache_offset =
-      ObjectPool::element_offset(sub_type_cache_index);
+      ObjectPool::element_offset(sub_type_cache_index) - kHeapObjectTag;
   const intptr_t dst_name_index =
       __ object_pool_wrapper().AddObject(dst_name, Patchability::kPatchable);
   ASSERT((sub_type_cache_index + 1) == dst_name_index);
@@ -721,7 +721,7 @@ void FlowGraphCompiler::GenerateAssertAssignableViaTypeTestingStub(
   __ LoadField(R9,
                FieldAddress(kDstTypeReg,
                             AbstractType::type_test_stub_entry_point_offset()));
-  __ ldr(kSubtypeTestCacheReg, Address(PP, sub_type_cache_offset));
+  __ LoadWordFromPoolOffset(kSubtypeTestCacheReg, sub_type_cache_offset);
   __ blr(R9);
   EmitCallsiteMetadata(token_pos, deopt_id, RawPcDescriptors::kOther, locs);
   __ Bind(&done);

runtime/vm/compiler/assembler/assembler_arm.h
@@ -767,6 +767,13 @@ class Assembler : public ValueObject {
 
   void LoadIsolate(Register rd);
 
+  // Load word from pool from the given offset using encoding that
+  // InstructionPattern::DecodeLoadWordFromPool can decode.
+  void LoadWordFromPoolOffset(Register rd,
+                              int32_t offset,
+                              Register pp,
+                              Condition cond);
+
   void LoadObject(Register rd, const Object& object, Condition cond = AL);
   void LoadUniqueObject(Register rd, const Object& object, Condition cond = AL);
   void LoadFunctionFromCalleePool(Register dst,
@@ -1109,11 +1116,6 @@ class Assembler : public ValueObject {
 
   void BindARMv6(Label* label);
   void BindARMv7(Label* label);
-
-  void LoadWordFromPoolOffset(Register rd,
-                              int32_t offset,
-                              Register pp,
-                              Condition cond);
 
   void BranchLink(const ExternalLabel* label);
 

runtime/vm/compiler/assembler/assembler_arm64.h
@@ -1493,6 +1493,10 @@ class Assembler : public ValueObject {
   void LoadImmediate(Register reg, int64_t imm);
   void LoadDImmediate(VRegister reg, double immd);
 
+  // Load word from pool from the given offset using encoding that
+  // InstructionPattern::DecodeLoadWordFromPool can decode.
+  void LoadWordFromPoolOffset(Register dst, uint32_t offset, Register pp = PP);
+
   void PushObject(const Object& object) {
     LoadObject(TMP, object);
     Push(TMP);
@@ -1618,7 +1622,6 @@ class Assembler : public ValueObject {
 
   bool constant_pool_allowed_;
 
-  void LoadWordFromPoolOffset(Register dst, uint32_t offset, Register pp = PP);
   void LoadWordFromPoolOffsetFixed(Register dst, uint32_t offset);
 
   void LoadObjectHelper(Register dst, const Object& obj, bool is_unique);

runtime/vm/compiler/backend/flow_graph_compiler_arm.cc
@@ -743,7 +743,8 @@ void FlowGraphCompiler::GenerateAssertAssignableViaTypeTestingStub(
   __ LoadField(R9,
                FieldAddress(kDstTypeReg,
                             AbstractType::type_test_stub_entry_point_offset()));
-  __ ldr(kSubtypeTestCacheReg, Address(PP, sub_type_cache_offset));
+  __ LoadWordFromPoolOffset(kSubtypeTestCacheReg, sub_type_cache_offset, PP,
+                            AL);
   __ blx(R9);
   EmitCallsiteMetadata(token_pos, deopt_id, RawPcDescriptors::kOther, locs);
   __ Bind(&done);

runtime/vm/instructions_arm.h
@@ -42,6 +42,10 @@ class InstructionPattern : public AllStatic {
   // address of the first instruction in the sequence.  Returns the register
   // being loaded and the index in the pool being read from in the output
   // parameters 'reg' and 'index' respectively.
+  // IMPORANT: When generating code loading values from pool on ARM use
+  // LoadWordFromPool macro instruction instead of emitting direct load.
+  // The macro instruction takes care of pool offsets that can't be
+  // encoded as immediates.
   static uword DecodeLoadWordFromPool(uword end,
                                       Register* reg,
                                       intptr_t* index);

runtime/vm/instructions_arm64.h
@@ -42,6 +42,10 @@ class InstructionPattern : public AllStatic {
   // address of the first instruction in the sequence.  Returns the register
   // being loaded and the index in the pool being read from in the output
   // parameters 'reg' and 'index' respectively.
+  // IMPORANT: When generating code loading values from pool on ARM64 use
+  // LoadWordFromPool macro instruction instead of emitting direct load.
+  // The macro instruction takes care of pool offsets that can't be
+  // encoded as immediates.
   static uword DecodeLoadWordFromPool(uword end,
                                       Register* reg,
                                       intptr_t* index);

commit a63e9cebd5b0b16ae48099f2c66b3cd7181406d1

[vm] Fix ARM64 breakage caused by fc6af81a7a0c840ae99009ff25e3fef36d55a166
Pool pointer is untagged on ARM64.

Change-Id: I5c9c81d1b7377f8bace5243e196aafc778d5ba3d
Reviewed-on: https://dart-review.googlesource.com/59840
Reviewed-by: Vyacheslav Egorov <vegorov@google.com>
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>

runtime/vm/compiler/backend/flow_graph_compiler_arm64.cc
@@ -712,7 +712,7 @@ void FlowGraphCompiler::GenerateAssertAssignableViaTypeTestingStub(
   const intptr_t sub_type_cache_index = __ object_pool_wrapper().AddObject(
       Object::null_object(), Patchability::kPatchable);
   const intptr_t sub_type_cache_offset =
-      ObjectPool::element_offset(sub_type_cache_index) - kHeapObjectTag;
+      ObjectPool::element_offset(sub_type_cache_index);
   const intptr_t dst_name_index =
       __ object_pool_wrapper().AddObject(dst_name, Patchability::kPatchable);
   ASSERT((sub_type_cache_index + 1) == dst_name_index);