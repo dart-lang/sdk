dart-lang/sdk

commit 94a6a48edd838b0b6586e65073d5d742ddb54fb5

Don't encode strings up front; don't toString uris
Second try. First try was reverted because a previously empty URL (the
default 'always there' one) is now a null url instead, which wasn't
handled properly in the frontend_server.

See 47e90395128ed2744213786ba9fa26281cc92d64 for original details.

This reverts commit 4c9b71205262699764c5db74e95bc9e702451f4e.

Change-Id: I717eb927cbe29b6388e72f7e270428d234d44e4d
Reviewed-on: https://dart-review.googlesource.com/53580
Commit-Queue: Jens Johansen <jensj@google.com>
Reviewed-by: Peter von der Ahé <ahe@google.com>

pkg/vm/lib/frontend_server.dart
@@ -454,6 +454,7 @@ void _writeDepfile(Component component, String output, String depfile) async {
   file.write(_escapePath(output));
   file.write(':');
   for (Uri dep in component.uriToSource.keys) {
+    if (dep == null) continue;
     file.write(' ');
     file.write(_escapePath(dep.toFilePath()));
   }

pkg/front_end/test/summary_generator_test.dart
@@ -14,9 +14,9 @@ main() {
     var summary = await summarize(['a.dart'], allSources);
     var component = loadComponentFromBytes(summary);
 
-    // Note: the kernel representation always has an empty '' key in the map,
+    // Note: the kernel representation always has a null key in the map,
     // but otherwise no other data is included here.
-    expect(component.uriToSource.keys.single, Uri.parse(""));
+    expect(component.uriToSource.keys.single, null);
   });
 
   test('summary includes declarations, but no method bodies', () async {

pkg/kernel/lib/binary/ast_from_binary.dart
@@ -523,7 +523,9 @@ class BinaryBuilder {
     Map<Uri, Source> uriToSource = <Uri, Source>{};
     for (int i = 0; i < length; ++i) {
       List<int> uriBytes = readByteList();
-      Uri uri = Uri.parse(const Utf8Decoder().convert(uriBytes));
+      Uri uri = uriBytes.isEmpty
+          ? null
+          : Uri.parse(const Utf8Decoder().convert(uriBytes));
       _sourceUriTable[i] = uri;
       List<int> sourceCode = readByteList();
       int lineCount = readUInt();

pkg/kernel/lib/binary/ast_to_binary.dart
@@ -9,7 +9,6 @@ import '../ast.dart';
 import 'tag.dart';
 import 'dart:convert';
 import 'dart:typed_data';
-import 'dart:collection';
 
 /// Writes to a binary file.
 ///
@@ -22,7 +21,7 @@ class BinaryPrinter implements Visitor<void>, BinarySink {
   final TypeParameterIndexer _typeParameterIndexer = new TypeParameterIndexer();
   final StringIndexer stringIndexer;
   ConstantIndexer _constantIndexer;
-  final StringIndexer _sourceUriIndexer = new StringIndexer();
+  final UriIndexer _sourceUriIndexer = new UriIndexer();
   final Set<Uri> _knownSourceUri = new Set<Uri>();
   Map<LibraryDependency, int> _libraryDependencyIndex =
       <LibraryDependency, int>{};
@@ -103,15 +102,23 @@ class BinaryPrinter implements Visitor<void>, BinarySink {
     _binaryOffsetForStringTable = getBufferOffset();
 
     // Write the end offsets.
-    writeUInt30(indexer.numberOfStrings);
+    writeUInt30(indexer.index.length);
     int endOffset = 0;
-    for (var entry in indexer.entries) {
-      endOffset += entry.utf8Bytes.length;
+    List<List<int>> data =
+        new List<List<int>>.filled(indexer.index.length, null);
+    int i = 0;
+    Utf8Encoder utf8Encoder = const Utf8Encoder();
+    for (String key in indexer.index.keys) {
+      List<int> utf8Bytes = utf8Encoder.convert(key);
+      data[i] = utf8Bytes;
+      endOffset += utf8Bytes.length;
       writeUInt30(endOffset);
+      i++;
     }
+
     // Write the UTF-8 encoded strings.
-    for (var entry in indexer.entries) {
-      writeBytes(entry.utf8Bytes);
+    for (var entry in data) {
+      writeBytes(entry);
     }
   }
 
@@ -206,11 +213,12 @@ class BinaryPrinter implements Visitor<void>, BinarySink {
   // Returns the new active file uri.
   Uri writeUriReference(Uri uri) {
     if (_knownSourceUri.contains(uri)) {
-      final int index = _sourceUriIndexer.put(uri == null ? "" : "$uri");
+      final int index = _sourceUriIndexer.put(uri);
       writeUInt30(index);
       return uri;
     } else {
-      final int index = 0; // equivalent to index = _sourceUriIndexer[""];
+      // This is equivalent to `index = _sourceUriIndexer[null];`.
+      final int index = 0;
       writeUInt30(index);
       return null;
     }
@@ -487,19 +495,19 @@ class BinaryPrinter implements Visitor<void>, BinarySink {
   void writeUriToSource(Map<Uri, Source> uriToSource) {
     _binaryOffsetForSourceTable = getBufferOffset();
 
-    int length = _sourceUriIndexer.numberOfStrings;
+    int length = _sourceUriIndexer.index.length;
     writeUInt32(length);
-    List<int> index = new List<int>(_sourceUriIndexer.entries.length);
+    List<int> index = new List<int>(length);
 
     // Write data.
-    for (int i = 0; i < length; ++i) {
+    int i = 0;
+    Utf8Encoder utf8Encoder = const Utf8Encoder();
+    for (Uri uri in _sourceUriIndexer.index.keys) {
       index[i] = getBufferOffset();
 
-      StringTableEntry uri = _sourceUriIndexer.entries[i];
-      Source source = uriToSource[Uri.parse(uri.value)] ??
-          new Source(<int>[], const <int>[]);
+      Source source = uriToSource[uri] ?? new Source(<int>[], const <int>[]);
 
-      writeByteList(uri.utf8Bytes);
+      writeByteList(utf8Encoder.convert(uri == null ? "" : "$uri"));
       writeByteList(source.source);
       List<int> lineStarts = source.lineStarts;
       writeUInt30(lineStarts.length);
@@ -508,6 +516,7 @@ class BinaryPrinter implements Visitor<void>, BinarySink {
         writeUInt30(lineStart - previousLineStart);
         previousLineStart = lineStart;
       });
+      i++;
     }
 
     // Write index for random access.
@@ -2100,29 +2109,17 @@ class TypeParameterIndexer {
   int operator [](TypeParameter parameter) => index[parameter];
 }
 
-class StringTableEntry {
-  final String value;
-  final List<int> utf8Bytes;
-
-  StringTableEntry(String value)
-      : value = value,
-        utf8Bytes = const Utf8Encoder().convert(value);
-}
-
 class StringIndexer {
-  final List<StringTableEntry> entries = <StringTableEntry>[];
-  final LinkedHashMap<String, int> index = new LinkedHashMap<String, int>();
+  // Note that the iteration order is important.
+  final Map<String, int> index = new Map<String, int>();
 
   StringIndexer() {
     put('');
   }
 
-  int get numberOfStrings => index.length;
-
   int put(String string) {
     var result = index[string];
     if (result == null) {
-      entries.add(new StringTableEntry(string));
       result = index.length;
       index[string] = result;
     }
@@ -2130,6 +2127,24 @@ class StringIndexer {
   }
 
   int operator [](String string) => index[string];
+}
+
+class UriIndexer {
+  // Note that the iteration order is important.
+  final Map<Uri, int> index = new Map<Uri, int>();
+
+  UriIndexer() {
+    put(null);
+  }
+
+  int put(Uri uri) {
+    var result = index[uri];
+    if (result == null) {
+      result = index.length;
+      index[uri] = result;
+    }
+    return result;
+  }
 }
 
 /// Computes and stores the index of a library, class, or member within its

commit 385d82546907b1b0f7a3e101360912ea1757a2f0

Make frontend_server initialize from dill work again
It has probably been broken since
94a6a48edd838b0b6586e65073d5d742ddb54fb5.

Change-Id: I200b9ab6b2213a198d2fd4b396b0e501dc79273e
Reviewed-on: https://dart-review.googlesource.com/60241
Commit-Queue: Jens Johansen <jensj@google.com>
Reviewed-by: Peter von der Ahé <ahe@google.com>

pkg/vm/lib/frontend_server.dart
@@ -373,39 +373,39 @@ class FrontendCompiler implements CompilerInterface {
     // be invalidated by the normal approach anyway.
     if (_generator.initialized) return null;
 
+    final File f = new File(_initializeFromDill);
+    if (!f.existsSync()) return null;
+
+    Component component;
     try {
-      final File f = new File(_initializeFromDill);
-      if (!f.existsSync()) return null;
+      component = loadComponentSourceFromBytes(f.readAsBytesSync());
+    } catch (e) {
+      // If we cannot load the dill file we shouldn't initialize from it.
+      _generator = _createGenerator(null);
+      return null;
+    }
 
-      final Component component =
-          loadComponentSourceFromBytes(f.readAsBytesSync());
-      for (Uri uri in component.uriToSource.keys) {
-        if ('$uri' == '') continue;
+    for (Uri uri in component.uriToSource.keys) {
+      if (uri == null || '$uri' == '') continue;
 
-        final List<int> oldBytes = component.uriToSource[uri].source;
-        final FileSystemEntity entity =
-            _compilerOptions.fileSystem.entityForUri(uri);
-        if (!await entity.exists()) {
+      final List<int> oldBytes = component.uriToSource[uri].source;
+      final FileSystemEntity entity =
+          _compilerOptions.fileSystem.entityForUri(uri);
+      if (!await entity.exists()) {
+        _generator.invalidate(uri);
+        continue;
+      }
+      final List<int> newBytes = await entity.readAsBytes();
+      if (oldBytes.length != newBytes.length) {
+        _generator.invalidate(uri);
+        continue;
+      }
+      for (int i = 0; i < oldBytes.length; ++i) {
+        if (oldBytes[i] != newBytes[i]) {
           _generator.invalidate(uri);
           continue;
-        }
-        final List<int> newBytes = await entity.readAsBytes();
-        if (oldBytes.length != newBytes.length) {
-          _generator.invalidate(uri);
-          continue;
-        }
-        for (int i = 0; i < oldBytes.length; ++i) {
-          if (oldBytes[i] != newBytes[i]) {
-            _generator.invalidate(uri);
-            continue;
-          }
         }
       }
-    } catch (e) {
-      // If there's a failure in the above block we might not have invalidated
-      // correctly. Create a new generator that doesn't initialize from dill to
-      // avoid missing any changes.
-      _generator = _createGenerator(null);
     }
   }
 