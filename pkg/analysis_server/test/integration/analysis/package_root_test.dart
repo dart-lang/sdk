dart-lang/sdk

commit 591cf85c76e55af63f5aeee8f44398900c6fdb50

Re-enable the old analyzer/FE integration logic.
This gets the tests running again.  I'll follow this up with CLs that
reshape the ResolutionStorer API to match the Factory API we're
building, and then after that we can transition the code over to
calling the Factory API directly.

Fixes #33504
Fixes #32258

Change-Id: I105841ed5b2ed1198ec5e3e7387f694c61d36ca7
Reviewed-on: https://dart-review.googlesource.com/61101
Commit-Queue: Paul Berry <paulberry@google.com>
Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
Reviewed-by: Kevin Millikin <kmillikin@google.com>

pkg/analysis_server/test/integration/analysis/package_root_test.dart
@@ -13,9 +13,7 @@ import '../support/integration_tests.dart';
 main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(SetAnalysisRootsTest);
-    // TODO(scheglov): Restore similar test coverage when the front-end API
-    // allows it.  See https://github.com/dart-lang/sdk/issues/32258.
-    // defineReflectiveTests(SetAnalysisRootsTest_UseCFE);
+    defineReflectiveTests(SetAnalysisRootsTest_UseCFE);
   });
 }
 

commit 06a588233c0ffc006d132e8b86f577b8b2e23eb5

Fix package_root_test.dart on mac buildbot.
I broke this in 591cf85c76e55af63f5aeee8f44398900c6fdb50, but failed
to notice because the test is disabled on Linux (see
https://github.com/dart-lang/sdk/issues/33382)

Change-Id: Ida2bc1ec03db83bdb9c5cbd58cf4534d95fe7781
Reviewed-on: https://dart-review.googlesource.com/61307
Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
Commit-Queue: Paul Berry <paulberry@google.com>

pkg/analysis_server/test/integration/analysis/package_root_test.dart
@@ -83,4 +83,10 @@ f() {}
 class SetAnalysisRootsTest_UseCFE extends SetAnalysisRootsTest {
   @override
   bool get useCFE => true;
+
+  @override
+  @failingTest
+  test_package_root() {
+    return super.test_package_root();
+  }
 }