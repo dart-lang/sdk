d7f0907108e8d5b7a4712837c47da9d7c74ea8e1
Alex DeLorenzo <thismachinechills@users.noreply.github.com>
2018-08-08 17:45:08 -0400

    Use subString() instead of slice()
    As per https://dart-review.googlesource.com/c/sdk/+/63920, slice() is not part of the String API

--- sdk/lib/_http/http_headers.dart
+++ sdk/lib/_http/http_headers.dart
@@ -987,7 +987,7 @@ class _Cookie implements Cookie {
     }
     
     if (value[0] == '"' && value[value.length - 1] == '"')
-      value = value.slice(1, value.length - 1)
+      value = value.subString(1, value.length - 1)
     
     for (int i = 0; i < value.length; i++) {
       int codeUnit = value.codeUnits[i];
