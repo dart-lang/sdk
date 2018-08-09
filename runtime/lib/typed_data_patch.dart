5013a2ccc4284a7a1c23df9231b68cc24c368ac9
Samir Jindel <sjindel@google.com>
2018-08-09 16:29:15 +0000

    Remove spurious line.
    Change-Id: I7d23e6009029102892b66d84971014b21f7bbc76
    Reviewed-on: https://dart-review.googlesource.com/69163
    Commit-Queue: Samir Jindel <sjindel@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>

--- runtime/lib/typed_data_patch.dart
+++ runtime/lib/typed_data_patch.dart
@@ -1,5 +1,4 @@
 // Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
-@pragma("vm.entry-point")
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
