38b9a544df714906a7645594a8a54d0e92cda2c0
Nate Bosch <nbosch@google.com>
2018-08-28 22:51:02 +0000

    Call our error events in StreamTransform.bind doc
    Fixes #27739
    
    The doc already has guidelines on things like listening to and pausing
    the stream but did not mention whether or how error events should be
    considered. Since a Transformer may choose to do anything with errors
    metion that explicitly and encourage the behavior to be documented.
    
    Change-Id: I77e98c0967ae0041f5e959282369aa294aa9b18a
    Reviewed-on: https://dart-review.googlesource.com/71383
    Commit-Queue: Nate Bosch <nbosch@google.com>
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>

--- sdk/lib/async/stream.dart
+++ sdk/lib/async/stream.dart
@@ -2009,8 +2009,11 @@ abstract class StreamTransformer<S, T> {
    * Returns a new stream with events that are computed from events of the
    * provided [stream].
    *
-   * Implementors of the [StreamTransformer] interface should document
-   * differences from the following expected behavior:
+   * The [StreamTransformer] interface is completely generic,
+   * so it cannot say what subclasses do.
+   * Each [StreamTransformer] should document clearly how it transforms the
+   * stream (on the class or variable used to access the transformer),
+   * as well as any differences from the following typical behavior:
    *
    * * When the returned stream is listened to, it starts listening to the
    *   input [stream].
@@ -2023,6 +2026,11 @@ abstract class StreamTransformer<S, T> {
    * "Reasonable time" depends on the transformer and stream. Some transformers,
    * like a "timeout" transformer, might make these operations depend on a
    * duration. Others might not delay them at all, or just by a microtask.
+   *
+   * Transformers are free to handle errors in any way.
+   * A transformer implementation may choose to propagate errors,
+   * or convert them to other events, or ignore them completely,
+   * but if errors are ignored, it should be documented explicitly.
    */
   Stream<T> bind(Stream<S> stream);
 
