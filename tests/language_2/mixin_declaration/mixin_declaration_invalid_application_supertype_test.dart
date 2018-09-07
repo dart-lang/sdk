68b6bbc3ac08224dc2683a8a8d87754db0095902
Lasse Reichstein Holst Nielsen <lrn@google.com>
2018-09-07 06:54:49 +0000

    Add tests for new mixin declaration syntax.
    The tests are not exhaustive, but they can hopefully be a good start when implementing the feature.
    
    The test are not *checked* since there is currently no implementation of the feature, or even the syntax.
    They should be expected to contain some errors.
    
    Bug: https://github.com/dart-lang/language/issues/7
    Change-Id: I4a5364566aeba9de036e56ae188205337575154c
    Reviewed-on: https://dart-review.googlesource.com/70509
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Leaf Petersen <leafp@google.com>

---
+++ tests/language_2/mixin_declaration/mixin_declaration_invalid_application_supertype_test.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+// Test various invalid mixin applications where the supertype doesn't
+// implement the super-interfaces.
+
+abstract class UnaryNum {
+  num foo(num x) => x;
+}
+
+abstract class UnaryInt {
+  num foo(int x) => x;
+}
+
+mixin M1 on UnaryNum {}
+
+class _ = Object with M1; //# 01: compile-time error
+class _ = Null with M1; //# 02: compile-time error
+class _ = UnaryInt with M1;  //# 03: compile-time error.
+
+mixin M2 on UnaryNum, UnaryInt {}
+
+class _ = UnaryInt with M2;  //# 04: compile-time error.
+class _ = UnaryNum with M2;  //# 05: compile-time error.
+
+main() {
+  // M1 and M2 are valid types.
+  Expect.isNotType<M1>(null);
+  Expect.isNotType<M2>(null);
+}
---
+++ tests/language_2/mixin_declaration/mixin_declaration_subtype_test.dart
@@ -0,0 +1,63 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+// A mixin declaration introduces a type.
+
+// A mixin with multiple super-types and implemented types.
+class A {}
+class B {}
+class I {}
+class J {}
+
+mixin M on A, B implements I, J {}
+
+class C implements A, B {}
+
+class D = C with M;
+
+// Same, with generics.
+class GA<T> {}
+class GB<T> {}
+class GI<T> {}
+class GJ<T> {}
+
+mixin GM<T> on GA<T>, GB<List<T>> implements GI<Iterable<T>>, GJ<Set<T>> {}
+
+class GC<T> implements GA<T>, GB<List<T>> {}
+
+class GD<T> = GC<T> with GM<T>;
+
+bool expectSubtype<Sub, Super> {
+  if (<Sub>[] is! List<Super>) {
+    Expect.fail("$Sub is not a subtype of $Super");
+  }
+}
+
+bool expectNotSubtype<Sub, Super> {
+  if (<Sub>[] is List<Super>) {
+    Expect.fail("$Sub is a subtype of $Super");
+  }
+}
+
+main() {
+  expectSubtype<M, A>();
+  expectSubtype<M, B>();
+  expectSubtype<M, I>();
+  expectSubtype<M, J>();
+  expectSubtype<D, M>();
+  expectSubtype<D, C>();
+  expectNotSubtype<M, C>();
+  expectNotSubtype<C, M>();
+
+  expectSubtype<GM<int>, GA<int>>();
+  expectSubtype<GM<int>, GB<int>>();
+  expectSubtype<GM<int>, GI<int>>();
+  expectSubtype<GM<int>, GJ<int>>();
+  expectSubtype<GD<int>, GM<int>>();
+  expectSubtype<GD<int>, GC<int>>();
+  expectNotSubtype<GM<int>, GC<int>>();
+  expectNotSubtype<GC<int>, GM<int>>();
+}
