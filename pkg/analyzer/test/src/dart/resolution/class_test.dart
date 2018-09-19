80c1b886260a43aac7d19bf4577a3954c560269c
Konstantin Shcheglov <scheglov@google.com>
2018-09-19 16:58:07 +0000

    Add copyright comments in resolution tests.
    R=brianwilkerson@google.com, paulberry@google.com
    
    Change-Id: Iaac51f9d4cd0ef248e8a5bf88ff6388ac7ab83c5
    Reviewed-on: https://dart-review.googlesource.com/75560
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/dart/resolution/class_test.dart
+++ pkg/analyzer/test/src/dart/resolution/class_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:analyzer/src/error/codes.dart';
 import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
