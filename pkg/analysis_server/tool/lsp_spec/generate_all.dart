e4842a293b0946ade7908a39fe5e9fedbe5e3490
Danny Tuppeny <dantup@google.com>
2018-10-11 18:05:37 +0000

    Remove extra spaces in headers
    Change-Id: I401ad4537e7a8c3ea4bc3f693cf45b5f7ef31267
    Reviewed-on: https://dart-review.googlesource.com/c/79340
    Commit-Queue: Danny Tuppeny <dantup@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/tool/lsp_spec/generate_all.dart
+++ pkg/analysis_server/tool/lsp_spec/generate_all.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -13,11 +13,11 @@ import 'markdown.dart';
 import 'typescript.dart';
 
 const _generatedFileHeader = '''
-// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// This file has been automatically generated.  Please do not edit it manually.
+// This file has been automatically generated. Please do not edit it manually.
 // To regenerate the file, use the script
 // "pkg/analysis_server/tool/lsp_spec/generate_all.dart".
 
