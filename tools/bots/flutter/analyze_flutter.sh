7c788d0734f9aef95ccffc48212b79656b5b676e
Devon Carew <devoncarew@google.com>
2018-10-26 20:31:15 +0000

    Don't make a shallow clone of the flutter repo.
    Change-Id: Iddeec5f709b086fac3ab331fcffbe8e77201cbf7
    Reviewed-on: https://dart-review.googlesource.com/c/81720
    Reviewed-by: Paul Berry <paulberry@google.com>
    Reviewed-by: Alexander Thomas <athom@google.com>
    Commit-Queue: Devon Carew <devoncarew@google.com>

--- tools/bots/flutter/analyze_flutter.sh
+++ tools/bots/flutter/analyze_flutter.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2018, the Dart project authors. Please see the AUTHORS file
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
-#
-# Runs flutter's analyze tests with a locally built SDK.
+
+# Runs flutter's analyzer related tests with a locally built SDK.
 set -e
 
 dart=$(pwd)/tools/sdks/dart-sdk/bin/dart
@@ -15,8 +15,11 @@ cleanup() {
 trap cleanup EXIT HUP INT QUIT TERM PIPE
 cd "$tmpdir"
 
-git clone --depth 1 -vv https://chromium.googlesource.com/external/github.com/flutter/flutter
+git clone -vv https://chromium.googlesource.com/external/github.com/flutter/flutter
+
 cd flutter
+
 bin/flutter config --no-analytics
 bin/flutter update-packages
+
 $dart dev/bots/analyze.dart --dart-sdk $sdk
