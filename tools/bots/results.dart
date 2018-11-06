51f815f7f327df2b5672d57e4cc3a67ee90a9de0
Jonas Termansen <sortie@google.com>
2018-11-06 19:57:08 +0000

    [infra] Support configurations in compare_results and update_flakiness.
    Change-Id: I640486957a40ebd178a0d7d06a7a81af758536c8
    Reviewed-on: https://dart-review.googlesource.com/c/82980
    Commit-Queue: Jonas Termansen <sortie@google.com>
    Reviewed-by: William Hesse <whesse@google.com>

--- tools/bots/results.dart
+++ tools/bots/results.dart
@@ -30,8 +30,14 @@ Future<List<Map<String, dynamic>>> loadResults(String path) async {
 
 Map<String, Map<String, dynamic>> createResultsMap(
         List<Map<String, dynamic>> results) =>
-    new Map<String, Map<String, dynamic>>.fromIterable(results,
-        key: (dynamic result) => (result as Map<String, dynamic>)['name']);
+    new Map<String, Map<String, dynamic>>.fromIterable(
+        results
+            // TODO: Temporarily discard results in the old flaky.json format
+            // This can be removed once every bot has run once after this commit
+            // has landed, purging all old flakiness information.
+            .where((result) => result["configuration"] != null),
+        key: (dynamic result) =>
+            "${result["configuration"]}:${result["name"]}");
 
 Map<String, Map<String, dynamic>> parseResultsMap(String contents) =>
     createResultsMap(parseResults(contents));
