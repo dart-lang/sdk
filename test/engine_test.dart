592c81fe79285f795ce4bbde9584f1caf1f080df
Brian Wilkerson <brianwilkerson@google.com>
2018-11-06 15:56:50 -0800

    Clean up uses of previewDart2

--- test/engine_test.dart
+++ test/engine_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2015, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -112,8 +112,8 @@ void defineLinterEngineTests() {
     group('lint driver', () {
       test('pubspec', () {
         bool visited;
-        var options = new LinterOptions([new MockLinter((n) => visited = true)])
-          ..previewDart2 = true;
+        var options =
+            new LinterOptions([new MockLinter((n) => visited = true)]);
         new SourceLinter(options).lintPubspecSource(contents: 'name: foo_bar');
         expect(visited, isTrue);
       });
