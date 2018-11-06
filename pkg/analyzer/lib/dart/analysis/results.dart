a13c22f90a1a4dc3565d61e5124266db49b25f52
Konstantin Shcheglov <scheglov@google.com>
2018-11-06 20:11:11 +0000

    Backport Parsed/ResolvedLibraryResultImpl and ElementDeclarationResult into master.
    There are more Impl classes in src/dart/analysis/results.dart than
    required, I decided to keep them anyway - they don't hurt anything.
    
    R=brianwilkerson@google.com, paulberry@google.com
    
    Change-Id: I83aee3ed7423bbafcc91fba49339f5cfd9b778e9
    Reviewed-on: https://dart-review.googlesource.com/c/83080
    Reviewed-by: Paul Berry <paulberry@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/dart/analysis/results.dart
+++ pkg/analyzer/lib/dart/analysis/results.dart
@@ -49,6 +49,25 @@ abstract class AnalysisResultWithErrors implements FileResult {
   List<AnalysisError> get errors;
 }
 
+/// The declaration of an [Element].
+abstract class ElementDeclarationResult {
+  /// The [Element] that this object describes.
+  Element get element;
+
+  /// The node that declares the [element]. Depending on whether it is returned
+  /// from [ResolvedLibraryResult] or [ParsedLibraryResult] it might be resolved
+  /// or just parsed.
+  AstNode get node;
+
+  /// If this declaration is returned from [ParsedLibraryResult], the parsed
+  /// unit that contains the [node]. Otherwise `null`.
+  ParsedUnitResult get parsedUnit;
+
+  /// If this declaration is returned from [ResolvedLibraryResult], the
+  /// resolved unit that contains the [node]. Otherwise `null`.
+  ResolvedUnitResult get resolvedUnit;
+}
+
 /**
  * The result of computing all of the errors contained in a single file, both
  * syntactic and semantic.
@@ -75,6 +94,25 @@ abstract class FileResult implements AnalysisResult {
   LineInfo get lineInfo;
 }
 
+/// The result of building parsed AST(s) for the whole library.
+///
+/// Clients may not extend, implement or mix-in this class.
+abstract class ParsedLibraryResult implements AnalysisResult {
+  /// The parsed units of the library.
+  List<ParsedUnitResult> get units;
+
+  /// Return the declaration of the [element], or `null` if the [element]
+  /// is synthetic. Throw [ArgumentError] if the [element] is not defined in
+  /// this library.
+  ElementDeclarationResult getElementDeclaration(Element element);
+}
+
+/// The result of parsing of a single file. The errors returned include only
+/// those discovered during scanning and parsing.
+///
+/// Clients may not extend, implement or mix-in this class.
+abstract class ParsedUnitResult implements ParseResult {}
+
 /**
  * The result of parsing of a single file. The errors returned include only
  * those discovered during scanning and parsing.
@@ -92,6 +130,31 @@ abstract class ParseResult implements AnalysisResultWithErrors {
    */
   CompilationUnit get unit;
 }
+
+/// The result of building resolved AST(s) for the whole library.
+///
+/// Clients may not extend, implement or mix-in this class.
+abstract class ResolvedLibraryResult implements AnalysisResult {
+  /// The element representing this library.
+  LibraryElement get element;
+
+  /// The type provider used when resolving the library.
+  TypeProvider get typeProvider;
+
+  /// The resolved units of the library.
+  List<ResolvedUnitResult> get units;
+
+  /// Return the declaration of the [element], or `null` if the [element]
+  /// is synthetic. Throw [ArgumentError] if the [element] is not defined in
+  /// this library.
+  ElementDeclarationResult getElementDeclaration(Element element);
+}
+
+/// The result of building a resolved AST for a single file. The errors returned
+/// include both syntactic and semantic errors.
+///
+/// Clients may not extend, implement or mix-in this class.
+abstract class ResolvedUnitResult implements ResolveResult {}
 
 /**
  * The result of building a resolved AST for a single file. The errors returned
