fceae8524dd1eca550c37275e8f20c1cbea30ae5
Jenny Messerly <jmesserly@google.com>
2018-11-07 01:56:35 +0000

    [dartdevc] fix #34531, optimize covariance checks in the kernel backend
    This is similar to the analyzer backend's optimization for fields, but
    it also supports private methods and accessors.
    
    Also fixes #34769, checks were missing for explicit covariant fields
    and those are now generated (this is for fields of the form
    `covariant SomeType fieldName`--implicitly covariant fields in generic
    classes were already checked correctly).
    
    Change-Id: I5ce3ed7944bdc5a9799c731c5f95e199b461b079
    Reviewed-on: https://dart-review.googlesource.com/c/79432
    Commit-Queue: Jenny Messerly <jmesserly@google.com>
    Commit-Queue: Alan Knight <alanknight@google.com>
    Auto-Submit: Jenny Messerly <jmesserly@google.com>
    Reviewed-by: Alan Knight <alanknight@google.com>

--- pkg/dev_compiler/lib/src/kernel/compiler.dart
+++ pkg/dev_compiler/lib/src/kernel/compiler.dart
@@ -1370,12 +1370,12 @@ class ProgramCompiler extends Object
       return (member as Field).type;
     }
     FunctionType result;
-    if (!f.positionalParameters.any(isCovariant) &&
-        !f.namedParameters.any(isCovariant)) {
+    if (!f.positionalParameters.any(isCovariantParameter) &&
+        !f.namedParameters.any(isCovariantParameter)) {
       result = f.functionType;
     } else {
       reifyParameter(VariableDeclaration p) =>
-          isCovariant(p) ? coreTypes.objectClass.thisType : p.type;
+          isCovariantParameter(p) ? coreTypes.objectClass.thisType : p.type;
       reifyNamedParameter(VariableDeclaration p) =>
           NamedType(p.name, reifyParameter(p));
 
@@ -1772,9 +1772,10 @@ class ProgramCompiler extends Object
 
     var name = _declareMemberName(member);
     if (member.isSetter) {
-      if (superMember is Field && superMember.isGenericCovariantImpl ||
+      if (superMember is Field && isCovariantField(superMember) ||
           superMember is Procedure &&
-              isCovariant(superMember.function.positionalParameters[0])) {
+              isCovariantParameter(
+                  superMember.function.positionalParameters[0])) {
         return const [];
       }
       var setterType = substituteType(superMember.setterType);
@@ -1807,8 +1808,8 @@ class ProgramCompiler extends Object
       var jsParam = JS.Identifier(param.name);
       jsParams.add(jsParam);
 
-      if (isCovariant(param) &&
-          !isCovariant(superMember.function.positionalParameters[i])) {
+      if (isCovariantParameter(param) &&
+          !isCovariantParameter(superMember.function.positionalParameters[i])) {
         var check = _emitCast(jsParam, superMethodType.positionalParameters[i]);
         if (i >= function.requiredParameterCount) {
           body.add(js.statement('if (# !== void 0) #;', [jsParam, check]));
@@ -1819,8 +1820,8 @@ class ProgramCompiler extends Object
     }
     var namedParameters = function.namedParameters;
     for (var param in namedParameters) {
-      if (isCovariant(param) &&
-          !isCovariant(superMember.function.namedParameters
+      if (isCovariantParameter(param) &&
+          !isCovariantParameter(superMember.function.namedParameters
               .firstWhere((n) => n.name == param.name))) {
         var name = _propertyName(param.name);
         var paramType = superMethodType.namedParameters
@@ -1874,7 +1875,7 @@ class ProgramCompiler extends Object
     var args = field.isFinal ? [JS.Super(), name] : [JS.This(), virtualField];
 
     JS.Expression value = JS.Identifier('value');
-    if (!field.isFinal && field.isGenericCovariantImpl) {
+    if (!field.isFinal && isCovariantField(field)) {
       value = _emitCast(value, field.type);
     }
     args.add(value);
@@ -2884,7 +2885,7 @@ class ProgramCompiler extends Object
     _emitCovarianceBoundsCheck(f.typeParameters, body);
 
     initParameter(VariableDeclaration p, JS.Identifier jsParam) {
-      if (isCovariant(p)) {
+      if (isCovariantParameter(p)) {
         var castExpr = _emitCast(jsParam, p.type);
         if (!identical(castExpr, jsParam)) body.add(castExpr.toStatement());
       }
