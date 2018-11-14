4c683215fccd9da67fa3b72734c2f4a66e117028
Aart Bik <ajcbik@google.com>
2018-11-14 17:02:13 +0000

    [vm/compiler] induction variable analysis
    Rationale:
    The more one knows about loops, the better!
    This introduces a rigorous framework for detecting
    induction variables (more precise "sequence" variables).
    Even though this is already pretty general, in the
    future we can expand this on a "need to" base, recognizing
    more operators (shifts, negate, etc.) and more classes
    of induction (perhaps geometric, polynomial, etc.).
    
    Change-Id: I82a14515e8ae946d520ee470cd31046f6a58af7d
    Reviewed-on: https://dart-review.googlesource.com/c/81743
    Commit-Queue: Aart Bik <ajcbik@google.com>
    Reviewed-by: Vyacheslav Egorov <vegorov@google.com>

--- runtime/vm/compiler/backend/loops.h
+++ runtime/vm/compiler/backend/loops.h
@@ -10,6 +10,116 @@
 
 namespace dart {
 
+// Information on an induction variable in a particular loop.
+//
+// Invariant:
+//     offset + mult * def
+// Linear:
+//     initial + next * i, for invariant initial and next,
+//                       and a "normalized" loop index i
+// Wrap-around:
+//     initial then next, for invariant initial and any next
+// Periodic:
+//     alternate initial and next, for invariant initial and next
+//
+class InductionVar : public ZoneAllocated {
+ public:
+  enum Kind {
+    kInvariant,
+    kLinear,
+    kWrapAround,
+    kPeriodic,
+  };
+
+  // Constructor for an invariant.
+  InductionVar(int64_t offset, int64_t mult, Definition* def)
+      : kind_(kInvariant), offset_(offset), mult_(mult), def_(def) {}
+
+  // Constructor for a constant.
+  explicit InductionVar(int64_t offset) : InductionVar(offset, 0, nullptr) {}
+
+  // Constructor for induction.
+  InductionVar(Kind kind, InductionVar* initial, InductionVar* next)
+      : kind_(kind), initial_(initial), next_(next) {
+    ASSERT(IsInvariant(initial));
+    switch (kind) {
+      case kLinear:
+      case kPeriodic:
+        ASSERT(IsInvariant(next));
+        break;
+      case kWrapAround:
+        ASSERT(next != nullptr);
+        break;
+      default:
+        UNREACHABLE();
+    }
+  }
+
+  // Returns true if the other induction is structually equivalent.
+  bool IsEqual(InductionVar* other) const {
+    ASSERT(other != nullptr);
+    if (kind_ == other->kind_) {
+      switch (kind_) {
+        case kInvariant:
+          return offset_ == other->offset_ && mult_ == other->mult_ &&
+                 (mult_ == 0 || def_ == other->def_);
+        case kLinear:
+        case kWrapAround:
+        case kPeriodic:
+          return initial_->IsEqual(other->initial_) &&
+                 next_->IsEqual(other->next_);
+      }
+    }
+    return false;
+  }
+
+  // For debugging.
+  const char* ToCString() const;
+
+  // Returns true if x is invariant.
+  static bool IsInvariant(InductionVar* x) {
+    return x != nullptr && x->kind_ == kInvariant;
+  }
+
+  // Returns true if x is a constant (and invariant).
+  static bool IsConstant(InductionVar* x) {
+    return x != nullptr && x->kind_ == kInvariant && x->mult_ == 0;
+  }
+
+  // Returns true if x is linear.
+  static bool IsLinear(InductionVar* x) {
+    return x != nullptr && x->kind_ == kLinear;
+  }
+
+  // Returns true if x is wrap-around.
+  static bool IsWrapAround(InductionVar* x) {
+    return x != nullptr && x->kind_ == kWrapAround;
+  }
+
+  // Returns true if x is periodic.
+  static bool IsPeriodic(InductionVar* x) {
+    return x != nullptr && x->kind_ == kPeriodic;
+  }
+
+ private:
+  friend class InductionVarAnalysis;
+
+  const Kind kind_;
+  union {
+    struct {
+      int64_t offset_;
+      int64_t mult_;
+      Definition* def_;
+    };
+    struct {
+      InductionVar* initial_;
+      InductionVar* next_;
+    };
+  };
+
+  DISALLOW_COPY_AND_ASSIGN(InductionVar);
+};
+
 // Information on a "natural loop" in the flow graph.
 class LoopInfo : public ZoneAllocated {
  public:
@@ -24,11 +134,26 @@ class LoopInfo : public ZoneAllocated {
   // Returns true if given block is backedge of this loop.
   bool IsBackEdge(BlockEntryInstr* block) const;
 
-  // Returns true if this loop is nested inside other loop.
-  bool IsIn(LoopInfo* other) const;
+  // Returns true if given instruction is a header phi for this loop.
+  bool IsHeaderPhi(Instruction* instr) const;
+
+  // Returns true if this loop is nested inside given loop.
+  bool IsIn(LoopInfo* loop) const;
+
+  // Returns true if this loop contains given block.
+  bool Contains(BlockEntryInstr* block) const;
 
   // Returns the nesting depth of this loop.
   intptr_t NestingDepth() const;
+
+  // Resets induction.
+  void ResetInduction();
+
+  // Assigns induction to a definition.
+  void AddInduction(Definition* def, InductionVar* induc);
+
+  // Looks up induction.
+  InductionVar* LookupInduction(Definition* def) const;
 
   // Getters.
   intptr_t id() const { return id_; }
@@ -43,7 +168,10 @@ class LoopInfo : public ZoneAllocated {
   const char* ToCString() const;
 
  private:
+  friend class InductionVarAnalysis;
   friend class LoopHierarchy;
+
+  typedef RawPointerKeyValueTrait<Definition, InductionVar*> InductionKV;
 
   // Unique id of loop. We use its index in the
   // loop header array for this.
@@ -58,6 +186,9 @@ class LoopInfo : public ZoneAllocated {
 
   // Back edges of loop (usually one).
   GrowableArray<BlockEntryInstr*> back_edges_;
+
+  // Map instruction -> induction for this loop.
+  DirectChainedHashMap<InductionKV> induction_;
 
   // Loop hierarchy.
   LoopInfo* outer_;
@@ -81,6 +212,9 @@ class LoopHierarchy : public ZoneAllocated {
 
   // Returns total number of loops in the hierarchy.
   intptr_t num_loops() const { return headers_->length(); }
+
+  // Performs induction variable analysis on all loops.
+  void ComputeInduction() const;
 
  private:
   void Build();
