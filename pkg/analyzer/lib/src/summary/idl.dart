338b123002dffb801653b2a9892eef084ed443c2
Paul Berry <paulberry@google.com>
2018-11-15 20:15:15 +0000

    Change the order of enumerated values in UnlinkedExprOperation
    This causes the change in 7d84c2e4b0fa5c29559088838c0d481e22bc21bf to
    be backwards compatible with the previous summary format.
    
    Change-Id: If7f05f64846b84496bab1ca4afa03cf5f380dade
    Reviewed-on: https://dart-review.googlesource.com/c/84544
    Commit-Queue: Paul Berry <paulberry@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Auto-Submit: Paul Berry <paulberry@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/lib/src/summary/idl.dart
+++ pkg/analyzer/lib/src/summary/idl.dart
@@ -1690,12 +1690,6 @@ enum UnlinkedExprOperation {
   /// `dynamic`.
   makeUntypedList,
 
-  /// Pop the top n values from the stack (where n is obtained from
-  /// [UnlinkedExpr.ints]), place them in a [Set], and push the result back
-  /// onto the stack.  The type parameter for the [Set] is implicitly
-  /// `dynamic`.
-  makeUntypedSet,
-
   /// Pop the top 2*n values from the stack (where n is obtained from
   /// [UnlinkedExpr.ints]), interpret them as key/value pairs, place them in a
   /// [Map], and push the result back onto the stack.  The two type parameters
@@ -1707,12 +1701,6 @@ enum UnlinkedExprOperation {
   /// onto the stack.  The type parameter for the [List] is obtained from
   /// [UnlinkedExpr.references].
   makeTypedList,
-
-  /// Pop the top n values from the stack (where n is obtained from
-  /// [UnlinkedExpr.ints]), place them in a [Set], and push the result back
-  /// onto the stack.  The type parameter for the [Set] is obtained from
-  /// [UnlinkedExpr.references].
-  makeTypedSet,
 
   /// Pop the top 2*n values from the stack (where n is obtained from
   /// [UnlinkedExpr.ints]), interpret them as key/value pairs, place them in a
@@ -1966,6 +1954,18 @@ enum UnlinkedExprOperation {
 
   /// Push `super` expression onto the stack.
   pushSuper,
+
+  /// Pop the top n values from the stack (where n is obtained from
+  /// [UnlinkedExpr.ints]), place them in a [Set], and push the result back
+  /// onto the stack.  The type parameter for the [Set] is implicitly
+  /// `dynamic`.
+  makeUntypedSet,
+
+  /// Pop the top n values from the stack (where n is obtained from
+  /// [UnlinkedExpr.ints]), place them in a [Set], and push the result back
+  /// onto the stack.  The type parameter for the [Set] is obtained from
+  /// [UnlinkedExpr.references].
+  makeTypedSet,
 }
 
 /// Unlinked summary information about an import declaration.
