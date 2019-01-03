8499afe287002a17d2736230e7ef9c951ff052e6
Sam Rawlins <sam.rawlins@gmail.com>
2019-01-03 10:04:51 -0800

    Apply prefer_final_locals to for loop variables as well (dart-lang/linter#1337)
    Apply prefer_final_locals to for loop variables as well

--- test/rules/prefer_final_locals.dart
+++ test/rules/prefer_final_locals.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// test w/ `dart test/util/solo_test.dart prefer_final_locals`
+// test w/ `pub run test -N prefer_final_locals`
 
 void badMethod() {
   var label = 'hola mundo! badMethod'; // LINT
@@ -10,13 +10,33 @@ void badMethod() {
 }
 
 void goodMethod() {
-  final label = 'hola mundo! goodMethod';
+  final label = 'hola mundo! goodMethod'; // OK
   print(label);
 }
 
 void mutableCase() {
-  var label = 'hola mundo! mutableCase';
+  var label = 'hola mundo! mutableCase'; // OK
   print(label);
   label = 'hello world';
   print(label);
+}
+
+void fn() {
+  for (var i in [1, 2, 3]) { // LINT
+    print(i);
+  }
+
+  for (final i in [1, 2, 3]) { // OK
+    print(i);
+  }
+
+  for (var i in [1, 2, 3]) { // OK
+    i += 1;
+    print(i);
+  }
+
+  int j;
+  for (j in [1, 2, 3]) { // OK
+    print(j);
+  }
 }
