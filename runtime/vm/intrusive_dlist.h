2e5453ddb49d033f14bb8f7f2a271ea3db803e20
Ben Konyi <bkonyi@google.com>
2019-01-18 18:23:26 +0000

    [ VM ] Updated licenses to match other licenses in project
    Change-Id: I0848e49c63de5a6a6385fa926e7d5550d9a41c1d
    Reviewed-on: https://dart-review.googlesource.com/c/90301
    Reviewed-by: Siva Annamalai <asiva@google.com>

--- runtime/vm/intrusive_dlist.h
+++ runtime/vm/intrusive_dlist.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2019, the dart project authors. Please see the AUTHORS file
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE.md file.
+// BSD-style license that can be found in the LICENSE file.
 
 #ifndef RUNTIME_VM_INTRUSIVE_DLIST_H_
 #define RUNTIME_VM_INTRUSIVE_DLIST_H_
