1c2a47938f3007def09ae42ee29e9e2f8cc19c92
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-01-21 23:11:40 +0100

    add flutter_color_instantiation lint

--- lib/src/rules.dart
+++ lib/src/rules.dart
@@ -54,6 +54,7 @@ import 'package:linter/src/rules/empty_catches.dart';
 import 'package:linter/src/rules/empty_constructor_bodies.dart';
 import 'package:linter/src/rules/empty_statements.dart';
 import 'package:linter/src/rules/file_names.dart';
+import 'package:linter/src/rules/flutter_color_instantiation.dart';
 import 'package:linter/src/rules/flutter_style_todos.dart';
 import 'package:linter/src/rules/hash_and_equals.dart';
 import 'package:linter/src/rules/implementation_imports.dart';
@@ -192,6 +193,7 @@ void registerLintRules() {
     ..register(new EmptyConstructorBodies())
     ..register(new EmptyStatements())
     ..register(new FileNames())
+    ..register(new FlutterColorInstantiation())
     ..register(new FlutterStyleTodos())
     ..register(new HashAndEquals())
     ..register(new ImplementationImports())
