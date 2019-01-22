027d3d3c66445df0b5285712dee36c0ada476482
Dmitry Stefantsov <dmitryas@google.com>
2019-01-22 12:10:06 +0000

    [kernel] Create empty (De)SerializationState in the verifier
    Change-Id: Ie0af87d783a21b36949a288939b99ab45398543f
    Reviewed-on: https://dart-review.googlesource.com/c/90383
    Reviewed-by: Daniel Hillerström <hillerstrom@google.com>
    Commit-Queue: Dmitry Stefantsov <dmitryas@google.com>

--- pkg/kernel/lib/text/text_serialization_verifier.dart
+++ pkg/kernel/lib/text/text_serialization_verifier.dart
@@ -4,7 +4,8 @@
 
 import '../ast.dart';
 
-import '../text/serializer_combinators.dart' show TextSerializer;
+import '../text/serializer_combinators.dart'
+    show DeserializationState, SerializationState, TextSerializer;
 
 import '../text/text_reader.dart' show TextIterator;
 
@@ -80,7 +81,8 @@ class TextSerializationVerifier implements Visitor<void> {
     stream.moveNext();
     T result;
     try {
-      result = serializer.readFrom(stream, null);
+      result = serializer.readFrom(
+          stream, new DeserializationState(null, new CanonicalName.root()));
     } catch (exception) {
       failures.add(
           new TextDeserializationFailure(exception.toString(), uri, offset));
@@ -96,7 +98,7 @@ class TextSerializationVerifier implements Visitor<void> {
       T node, TextSerializer<T> serializer, Uri uri, int offset) {
     StringBuffer buffer = new StringBuffer();
     try {
-      serializer.writeTo(buffer, node, null);
+      serializer.writeTo(buffer, node, new SerializationState(null));
     } catch (exception) {
       failures
           .add(new TextSerializationFailure(exception.toString(), uri, offset));
