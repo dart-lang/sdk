8010fad320d3519af676fb52d6cbf5cb7dc40d6b
Martin Kustermann <kustermann@google.com>
2019-02-04 13:25:59 +0000

    [infra] Fix incorrect shell function syntax
    Change-Id: I493f5f322325f6d7c89439a38d3140628e137666
    Reviewed-on: https://dart-review.googlesource.com/c/91924
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- tools/patches/utils.sh
+++ tools/patches/utils.sh
@@ -4,14 +4,14 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-die {
+die() {
   echo "$0: error: "\
        "This script must be run from the root of a flutter engine checkout "\
        "(containing src/flutter/ and flutter/)" >&2
   exit 1
 }
 
-ensure_in_checkout_root {
+ensure_in_checkout_root() {
   set -e
   if [ ! -e src/third_party/dart ]; then
     die
@@ -22,13 +22,13 @@ ensure_in_checkout_root {
   fi
 }
 
-get_pinned_dart_version {
+get_pinned_dart_version() {
   pinned_dart_sdk=$(grep -E "'dart_revision':.*" src/flutter/DEPS |
                     sed -E "s/.*'([^']*)',/\1/")
   echo -n $pinned_dart_sdk
 }
 
-get_pinned_flutter_engine_version {
+get_pinned_flutter_engine_version() {
   pinned_engine_version=$(cat flutter/bin/internal/engine.version | sed 's/[[:space:]]//')
   echo -n $pinned_engine_version
 }
