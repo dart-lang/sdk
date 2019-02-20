9b10476092675d56259cbe0f98c4a818d9fa2fb2
Daco Harkes <dacoharkes@google.com>
2019-02-20 12:54:21 +0000

    [kernel/ffi] fix library paths
    Follow up of: https://dart-review.googlesource.com/c/sdk/+/93081
    
    Change-Id: Ie4a71d0aa723fc8f9b9ed0d244a921445a81b394
    Reviewed-on: https://dart-review.googlesource.com/c/93543
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Samir Jindel <sjindel@google.com>
    Commit-Queue: Samir Jindel <sjindel@google.com>

--- pkg/vm/lib/transformations/ffi_use_sites.dart
+++ pkg/vm/lib/transformations/ffi_use_sites.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library kernel.transformations.ffi_use_sites;
+library vm.transformations.ffi_use_sites;
 
 import 'package:front_end/src/api_unstable/vm.dart'
     show
