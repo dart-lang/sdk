0ad7caa5af5a4eb5a4aedcc5e0b02f7e21e855d1
Alexander Thomas <athom@google.com>
2019-02-22 19:44:21 +0000

    [testing] Improve multitest error messages
    Previously, messages would contain "Warning:" twice and failed to print
    which file the errors came from.
    
    Removed newlines from messages because the logger only prints the first
    line.
    
    Fixes #34405
    
    Change-Id: I34d3679a4d769d7c7e2488ca55ffd15f08af9b16
    Reviewed-on: https://dart-review.googlesource.com/c/93990
    Commit-Queue: Alexander Thomas <athom@google.com>
    Auto-Submit: Alexander Thomas <athom@google.com>
    Reviewed-by: Bob Nystrom <rnystrom@google.com>

--- tools/testing/dart/multitest.dart
+++ tools/testing/dart/multitest.dart
@@ -130,8 +130,8 @@ void extractTestsFromMultitest(Path filePath, Map<String, String> tests,
             outcomes[annotation.key].add(nextOutcome);
           } else {
             DebugLogger.warning(
-                "Warning: Invalid expectation '$nextOutcome' on line "
-                "$lineCount:\n${annotation.rest} ");
+                "${filePath.toNativePath()}: Invalid expectation "
+                "'$nextOutcome' on line $lineCount: $line");
           }
         }
       }
@@ -152,7 +152,8 @@ void extractTestsFromMultitest(Path filePath, Map<String, String> tests,
       .where((test) => test != 'none' && outcomes[test].isEmpty)
       .toList();
   for (var test in invalidTests) {
-    DebugLogger.warning("Warning: Test $test has no valid expectation.\n"
+    DebugLogger.warning(
+        "${filePath.toNativePath()}: Test $test has no valid expectation. "
         "Expected one of: ${_multitestOutcomes.toString()}");
 
     outcomes.remove(test);
@@ -301,7 +302,8 @@ Set<String> _findAllRelativeImports(Path topLibrary) {
         // This is just for safety reasons, we don't want to unintentionally
         // clobber files relative to the destination dir when copying them
         // over.
-        print("Relative import in multitest containing '..' is not allowed.");
+        DebugLogger.error("${filePath.toNativePath()}: "
+            "Relative import in multitest containing '..' is not allowed.");
         exit(1);
       }
 
