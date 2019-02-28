b186a270e49a18ecac1e0ac6297db3507459ce9c
Samir Jindel <sjindel@google.com>
2019-02-28 16:40:36 +0000

    [vm/ffi] Stress tests for FFI functions.
    Change-Id: I75251b92688b70b4d14eda7b1fddef0fe94c60fe
    Reviewed-on: https://dart-review.googlesource.com/c/94384
    Commit-Queue: Samir Jindel <sjindel@google.com>
    Reviewed-by: Daco Harkes <dacoharkes@google.com>
    Auto-Submit: Samir Jindel <sjindel@google.com>

--- runtime/bin/ffi_test_functions.cc
+++ runtime/bin/ffi_test_functions.cc
@@ -5,7 +5,11 @@
 // This file contains test functions for the dart:ffi test cases.
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <iostream>
+#include <limits>
 
 #include "include/dart_api.h"
 
@@ -380,5 +384,33 @@ DART_EXPORT float InventFloatValue() {
   std::cout << "returning " << retval << "\n";
   return retval;
 }
+
+// Functions for stress-testing GC by returning values that require boxing.
+
+DART_EXPORT int64_t MinInt64() {
+  return 0x8000000000000000;
+}
+
+DART_EXPORT int64_t MinInt32() {
+  return 0x80000000;
+}
+
+DART_EXPORT double SmallDouble() {
+  return 0x80000000 * -1.0;
+}
+
+DART_EXPORT void* SmallPointer() {
+  return reinterpret_cast<void*>(-0x80000000L);
+}
+
+#if !defined(_WIN32)
+DART_EXPORT int RedirectStderr() {
+  char filename[256];
+  snprintf(filename, sizeof(filename), "/tmp/captured_stderr_%d", getpid());
+  freopen(filename, "w", stderr);
+  printf("Got file %s\n", filename);
+  return getpid();
+}
+#endif
 
 }  // namespace dart
