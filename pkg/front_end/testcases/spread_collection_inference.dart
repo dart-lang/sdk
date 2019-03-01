2c8f70cde39e1567af1d6e65c10a7ea5373a4531
Dmitry Stefantsov <dmitryas@google.com>
2019-03-01 15:17:13 +0000

    [cfe] Add type inference for spreads in set literals
    Change-Id: I60250ec4ba7dba2381d84328095d38cf47b4ea8b
    Reviewed-on: https://dart-review.googlesource.com/c/94548
    Commit-Queue: Dmitry Stefantsov <dmitryas@google.com>
    Reviewed-by: Kevin Millikin <kmillikin@google.com>

--- pkg/front_end/testcases/spread_collection_inference.dart
+++ pkg/front_end/testcases/spread_collection_inference.dart
@@ -15,23 +15,43 @@ foo() {
   var /*@type=List<dynamic>*/ lhs10 = /*@typeArgs=dynamic*/ [...
     /*@typeArgs=dynamic*/ []];
 
+  var /*@type=Set<dynamic>*/ set10 = <dynamic>{... /*@typeArgs=dynamic*/ []};
+
   var /*@type=List<int>*/ lhs20 = /*@typeArgs=int*/ [...spread];
+
+  var /*@type=Set<int>*/ set20 = /*@typeArgs=int*/ {...spread, 42};
 
   var /*@type=List<dynamic>*/ lhs21 = /*@typeArgs=dynamic*/ [...(spread as
       dynamic)];
 
+  var /*@type=Set<dynamic>*/ set21 = /*@typeArgs=dynamic*/ {...(spread as
+      dynamic), 42};
+
   List<int> lhs22 = /*@typeArgs=int*/ [... /*@typeArgs=int*/ []];
+
+  Set<int> set22 = /*@typeArgs=int*/ {... /*@typeArgs=int*/ [], 42};
 
   List<List<int>> lhs23 = /*@typeArgs=List<int>*/ [... /*@typeArgs=List<int>*/
     [/*@typeArgs=int*/ []]];
 
+  Set<List<int>> set23 = /*@typeArgs=List<int>*/ {... /*@typeArgs=List<int>*/
+    [/*@typeArgs=int*/ []], <int>[42]};
+
   int lhs30 = /*@error=InvalidAssignment*/ /*@typeArgs=int*/ [...spread];
+
+  int set30 = /*@error=InvalidAssignment*/ /*@typeArgs=int*/ {...spread, 42};
 
   var /*@type=List<dynamic>*/ lhs40 = /*@typeArgs=dynamic*/ [...
     /*@error=SpreadTypeMismatch*/ notSpreadInt];
 
+  var /*@type=Set<dynamic>*/ set40 = /*@typeArgs=dynamic*/ {...
+    /*@error=SpreadTypeMismatch*/ notSpreadInt, 42};
+
   var /*@type=List<dynamic>*/ lhs50 = /*@typeArgs=dynamic*/ [...
     /*@error=SpreadTypeMismatch*/ notSpreadFunction];
+
+  var /*@type=Set<dynamic>*/ set50 = /*@typeArgs=dynamic*/ {...
+    /*@error=SpreadTypeMismatch*/ notSpreadFunction, 42};
 }
 
 main() {}
