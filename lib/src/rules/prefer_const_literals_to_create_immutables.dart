8353a57d8bea462fb8768027cc294547a9ed01c8
pq <pquitslund@gmail.com>
2019-03-08 13:37:11 -0800

    added set

--- lib/src/rules/prefer_const_literals_to_create_immutables.dart
+++ lib/src/rules/prefer_const_literals_to_create_immutables.dart
@@ -15,7 +15,7 @@ const desc =
 
 const details = '''
 
-**PREFER** using `const` for instantiating list and map literals used as
+**PREFER** using `const` for instantiating list, map and set literals used as
 parameters in immutable class instantiations.
 
 **BAD:**
