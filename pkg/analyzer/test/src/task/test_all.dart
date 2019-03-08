9a7679e9e1b675f093564eead2a5e17e4b6565e4
Brian Wilkerson <brianwilkerson@google.com>
2019-03-08 16:03:23 +0000

    Remove some of the tests of the task framework to reduce references to it
    Change-Id: I1ec466bfbbcb9279169f6d076db72cece38dcd6a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/96000
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/task/test_all.dart
+++ pkg/analyzer/test/src/task/test_all.dart
@@ -4,25 +4,12 @@
 
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
-import 'dart_work_manager_test.dart' as dart_work_manager_test;
-import 'general_test.dart' as general_test;
-import 'inputs_test.dart' as inputs_test;
-import 'manager_test.dart' as manager_test;
-import 'model_test.dart' as model_test;
 import 'options_test.dart' as options_test;
-import 'options_work_manager_test.dart' as options_work_manager_test;
 import 'strong/test_all.dart' as strong_mode_test_all;
 
-/// Utility for manually running all tests.
 main() {
   defineReflectiveSuite(() {
-    dart_work_manager_test.main();
-    general_test.main();
-    inputs_test.main();
-    manager_test.main();
-    model_test.main();
     options_test.main();
-    options_work_manager_test.main();
     strong_mode_test_all.main();
   }, name: 'task');
 }
