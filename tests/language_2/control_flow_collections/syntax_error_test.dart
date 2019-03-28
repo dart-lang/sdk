add5a27120b84f37d234608df716c3ac87fb4386
Nicholas Shahan <nshahan@google.com>
2019-03-28 01:35:53 +0000

    [infra] Add `constant-update-2018` flag to v2.3 lang features tests
    Patched changes from https://dart-review.googlesource.com/c/sdk/+/97939
    
    Removed the change to the language_2_kernel.status
    
    Kernel team can update as needed.
    
    Change-Id: Icc347081e6c00fb2ae7616afb452e2c652f9d6b4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/97942
    Reviewed-by: Vijay Menon <vsm@google.com>

--- tests/language_2/control_flow_collections/syntax_error_test.dart
+++ tests/language_2/control_flow_collections/syntax_error_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// SharedOptions=--enable-experiment=control-flow-collections,spread-collections
+// SharedOptions=--enable-experiment=control-flow-collections,spread-collections,constant-update-2018
 
 void main() {
   // No then element.
