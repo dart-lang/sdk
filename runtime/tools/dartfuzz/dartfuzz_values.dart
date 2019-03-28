f29e100f42f49cae1cb535d807955fc1503b0d80
Aart Bik <ajcbik@google.com>
2019-03-28 16:31:45 +0000

    [dart/fuzzer] Generate DartFuzz API tables automatically
    Rationale:
    Rather than manually constructing the API tables for DartFuzz,
    this introduces a utility to generate API tables based on
    the analyzer traversal of common libraries. The utility
    recognizes DartTypes that are currently understood by DartFuzz
    and constructs the tables organized by return type.
    
    TBD:
    DartFuzz's type system is rather simple; as this improves,
    more and more methods will be accepted by the utility,
    thereby increasing fuzzing coverage of our libraries.
    Change-Id: Idcc607a4a592dbbf80bd79cdfec429cb397b7f68
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/98041
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Commit-Queue: Aart Bik <ajcbik@google.com>

--- runtime/tools/dartfuzz/dartfuzz_values.dart
+++ runtime/tools/dartfuzz/dartfuzz_values.dart
@@ -17,10 +17,19 @@ class DartType {
   static const DOUBLE = const DartType._withName('double');
   static const STRING = const DartType._withName('String');
   static const INT_LIST = const DartType._withName('List<int>');
+  static const INT_SET = const DartType._withName('Set<int>');
   static const INT_STRING_MAP = const DartType._withName('Map<int, String>');
 
   // All value types.
-  static const allTypes = [BOOL, INT, DOUBLE, STRING, INT_LIST, INT_STRING_MAP];
+  static const allTypes = [
+    BOOL,
+    INT,
+    DOUBLE,
+    STRING,
+    INT_LIST,
+    INT_SET,
+    INT_STRING_MAP
+  ];
 }
 
 /// Class with interesting values for fuzzing.
@@ -34,15 +43,6 @@ class DartFuzzValues {
   // Regular characters.
   static const regularChars =
       'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#&()+- ';
-
-  // Interesting doubles.
-  static const interestingDoubles = [
-    'double.infinity',
-    'double.maxFinite',
-    'double.minPositive',
-    'double.nan',
-    'double.negativeInfinity',
-  ];
 
   // Interesting integer values.
   static const List<int> interestingIntegers = [
@@ -82,115 +82,5 @@ class DartFuzzValues {
     0xffffffff80000000,
     0xffffffff80000001,
     0xffffffffffffffff
-  ];
-}
-
-/// Class that represents Dart library methods.
-//
-/// The invididual lists are organized by return type.
-/// The proto string has the following format:
-///    +-------> receiver type (V denotes none)
-///    |+------> param1 type  (V denotes none, v denotes getter)
-///    ||+-----> param2 type
-///    |||+----> ....
-///    ||||
-///   "TTTT...."
-/// where:
-///   V void
-///   v void (special)
-///   B bool
-///   I int
-///   i int (small)
-///   D double
-///   S String
-///   L List<int>
-///   M Map<int, String>
-///
-/// TODO(ajcbik): generate these lists automatically
-///
-class DartLib {
-  final String name;
-  final String proto;
-  const DartLib(this.name, this.proto);
-
-  static const boolLibs = [
-    DartLib('isEven', "Iv"),
-    DartLib('isOdd', "Iv"),
-    DartLib('isEmpty', "Sv"),
-    DartLib('isEmpty', "Mv"),
-    DartLib('isNotEmpty', "Sv"),
-    DartLib('isNotEmpty', "Mv"),
-    DartLib('endsWith', "SS"),
-    DartLib('remove', "LI"),
-    DartLib('containsValue', "MS"),
-    DartLib('containsKey', "MI"),
-  ];
-
-  static const intLibs = [
-    DartLib('bitLength', "Iv"),
-    DartLib('sign', "Iv"),
-    DartLib('abs', "IV"),
-    DartLib('round', "IV"),
-    DartLib('round', "DV"),
-    DartLib('floor', "IV"),
-    DartLib('floor', "DV"),
-    DartLib('ceil', "IV"),
-    DartLib('ceil', "DV"),
-    DartLib('truncate', "IV"),
-    DartLib('truncate', "DV"),
-    DartLib('toInt', "DV"),
-    DartLib('toUnsigned', "II"),
-    DartLib('toSigned', "II"),
-    DartLib('modInverse', "II"),
-    DartLib('modPow', "III"),
-    DartLib('length', "Sv"),
-    DartLib('length', "Lv"),
-    DartLib('length', "Mv"),
-    DartLib('codeUnitAt', "SI"),
-    DartLib('compareTo', "SS"),
-    DartLib('removeLast', "LV"),
-    DartLib('removeAt', "LI"),
-    DartLib('indexOf', "LI"),
-    DartLib('lastIndexOf', "LI"),
-  ];
-
-  static const doubleLibs = [
-    DartLib('sign', "Dv"),
-    DartLib('abs', "DV"),
-    DartLib('toDouble', "IV"),
-    DartLib('roundToDouble', "IV"),
-    DartLib('roundToDouble', "DV"),
-    DartLib('floorToDouble', "IV"),
-    DartLib('floorToDouble', "DV"),
-    DartLib('ceilToDouble', "IV"),
-    DartLib('ceilToDouble', "DV"),
-    DartLib('truncateToDouble', "IV"),
-    DartLib('truncateToDouble', "DV"),
-    DartLib('remainder', "DD"),
-  ];
-
-  static const stringLibs = [
-    DartLib('toString', "BV"),
-    DartLib('toString', "IV"),
-    DartLib('toString', "DV"),
-    DartLib('toRadixString', "II"),
-    DartLib('trim', "SV"),
-    DartLib('trimLeft', "SV"),
-    DartLib('trimRight', "SV"),
-    DartLib('toLowerCase', "SV"),
-    DartLib('toUpperCase', "SV"),
-    DartLib('substring', "SI"),
-    DartLib('replaceRange', "SIIS"),
-    DartLib('remove', "MI"),
-    DartLib('padLeft', "Si"), // restrict!
-    DartLib('padRight', "Si"), // restrict!
-  ];
-
-  static const intListLibs = [
-    DartLib('sublist', "LI"),
-  ];
-
-  static const intStringMapLibs = [
-    DartLib('Map.from', "VM"),
   ];
 }
