79bb83a9278ba2e47b60e36bbc94611a64f2d25d
Alexander Thomas <athom@google.com>
2019-04-01 08:26:46 +0000

    [infra] Fix debianpackage build
    debian-updates was removed, that broke the build.
    
    Change-Id: Ib5856146cc60998861b59d2a89487312c6d898da
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/98320
    Auto-Submit: Alexander Thomas <athom@google.com>
    Reviewed-by: William Hesse <whesse@google.com>

--- tools/run_debian_build.sh
+++ tools/run_debian_build.sh
@@ -6,7 +6,8 @@ set -x
 
 ninja=$(which ninja)
 depot_tools=$(dirname $ninja)
-cmd="apt-get update && apt-get -y install build-essential debhelper git python\
+cmd="sed -i /jessie-updates/d /etc/apt/sources.list\
+    && apt-get update && apt-get -y install build-essential debhelper git python\
     && PATH=\"$depot_tools:\$PATH\"\
     python tools/bots/linux_distribution_support.py"
 image="launcher.gcr.io/google/debian8:latest"
