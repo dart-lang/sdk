35c4af445b8d14c7192ba58e71e6437b4b9b7584
Liam Appelbe <liama@google.com>
2019-04-04 16:55:03 +0000

    [vm] Allow running with the latest ABI dill files.
    To support this, we need to download the current version during gclient
    sync, and change the way the --use-abi-version flag is processed so that
    there's a distinction between the current version and the flag being
    unset (-1). Also bump the oldest supported ABI version.
    
    Bug: https://github.com/dart-lang/sdk/issues/36047
    Change-Id: If3fb5e1ba1ebddf11e9f0a9a7c42b7bb0b820ad8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/98609
    Commit-Queue: Liam Appelbe <liama@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>

--- tools/download_abi_dills.py
+++ tools/download_abi_dills.py
@@ -6,22 +6,50 @@ import sys
 import utils
 
 
-def main():
-  abi_version = int(utils.GetAbiVersion())
-  oldest_abi_version = int(utils.GetOldestSupportedAbiVersion())
+def procWait(p):
+  while p.returncode is None:
+    p.communicate()
+    p.poll()
+  return p.returncode
+
+
+def findAbiVersion(version):
+  cmd = ['cipd', 'instances', 'dart/abiversions/%d' % version]
+  p = subprocess.Popen(cmd,
+                       stdout = subprocess.PIPE,
+                       stderr = subprocess.PIPE,
+                       shell = utils.IsWindows(),
+                       cwd = utils.DART_DIR)
+  return procWait(p) == 0
+
+
+def downloadAbiDills(oldest_abi_version, abi_version):
   cmd = ['cipd', 'ensure', '-root', 'tools/abiversions', '-ensure-file', '-']
   ensure_file = ''
-  for i in xrange(oldest_abi_version, abi_version):
+  for i in xrange(oldest_abi_version, abi_version + 1):
     ensure_file += '@Subdir %d\ndart/abiversions/%d latest\n\n' % (i, i)
   if not ensure_file:
     return 0
   p = subprocess.Popen(cmd,
                        stdin = subprocess.PIPE,
+                       stdout = subprocess.PIPE,
+                       stderr = subprocess.PIPE,
                        shell = utils.IsWindows(),
                        cwd = utils.DART_DIR)
   p.communicate(ensure_file)
   p.stdin.close()
-  return p.wait()
+  return procWait(p)
+
+
+def main():
+  abi_version = int(utils.GetAbiVersion())
+  oldest_abi_version = int(utils.GetOldestSupportedAbiVersion())
+
+  # The latest abi version may not have an entry in CIPD yet, so check first.
+  if not findAbiVersion(abi_version):
+    abi_version -= 1
+
+  return downloadAbiDills(oldest_abi_version, abi_version)
 
 
 if __name__ == '__main__':
