f8c1cc7c2458969f193243f88378628bffa56ba6
Jaime Wren <jwren@google.com>
2019-04-15 23:05:56 +0000

    Update the output for the generated Outline.java to compile.  Outline.java has a special fromJson() method special cased in codegen_java_types.dart that was not updated when fields were added to the protocol.
    Change-Id: Ib78f8a81ea3b48d6ae821d89617d3c1b66e69fcd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/99375
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Jaime Wren <jwren@google.com>

--- pkg/analysis_server/tool/spec/generated/java/types/Outline.java
+++ pkg/analysis_server/tool/spec/generated/java/types/Outline.java
@@ -103,9 +103,11 @@ public class Outline {
     Element element = Element.fromJson(elementObject);
     int offset = outlineObject.get("offset").getAsInt();
     int length = outlineObject.get("length").getAsInt();
+    int codeOffset = outlineObject.get("codeOffset").getAsInt();
+    int codeLength = outlineObject.get("codeLength").getAsInt();
 
     // create outline object
-    Outline outline = new Outline(parent, element, offset, length);
+    Outline outline = new Outline(parent, element, offset, length, codeOffset, codeLength);
 
     // compute children recursively
     List<Outline> childrenList = Lists.newArrayList();
