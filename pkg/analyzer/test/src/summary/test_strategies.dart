9af057428766d8e4aa76c1bd3a292c4fa01f5e9b
Paul Berry <paulberry@google.com>
2019-04-24 01:17:15 +0000

    Remove unused import
    Change-Id: I6fc0c75afc22b31e101631dac366b58a2f2ba8a7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/100285
    Commit-Queue: Paul Berry <paulberry@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Auto-Submit: Paul Berry <paulberry@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/summary/test_strategies.dart
+++ pkg/analyzer/test/src/summary/test_strategies.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/analysis/declared_variables.dart';
-import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/error/listener.dart';
