6498a3e3578845b9c84241a9a9ea2ae34f2fff01
Brian Wilkerson <brianwilkerson@google.com>
2019-04-25 18:41:14 +0000

    Add some tests of the listTokenDetails request
    Change-Id: I5dc2ae390777454128f18bef95bb00dcbce18f66
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/100600
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/integration/completion/test_all.dart
+++ pkg/analysis_server/test/integration/completion/test_all.dart
@@ -4,13 +4,12 @@
 
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
-import 'get_suggestions_test.dart' as get_suggestions_test;
+import 'get_suggestions_test.dart' as get_suggestions;
+import 'list_token_details_test.dart' as list_token_details;
 
-/**
- * Utility for manually running all integration tests.
- */
 main() {
   defineReflectiveSuite(() {
-    get_suggestions_test.main();
+    get_suggestions.main();
+    list_token_details.main();
   }, name: 'completion');
 }
