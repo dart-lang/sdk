5fbf7720f8bcbf9723e50933f5b1c7d1bc940338
Ben Konyi <bkonyi@google.com>
2019-04-30 19:50:46 +0000

    [ VM ] Updated splay-tree licenses, added V8 project authors to AUTHORS
    Change-Id: I02e38f3b80617d0d4ddf398cb94e8ee027d68e0c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/100889
    Reviewed-by: Zach Anderson <zra@google.com>

--- runtime/platform/splay-tree-inl.h
+++ runtime/platform/splay-tree-inl.h
@@ -1,8 +1,6 @@
-// Copyright (c) 2019, the Dart project authors.
-// Copyright 2010 the V8 project authors.
-// Please see the AUTHORS file for details. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2010, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 //
 // The original file can be found at:
 // https://github.com/v8/v8/blob/master/src/splay-tree-inl.h
