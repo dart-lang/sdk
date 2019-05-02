07ca863ce56ab1c103f0f2d599d953f3b6be8cda
Alexander Thomas <athom@google.com>
2019-05-02 08:23:50 +0000

    Revert "[infra] Migrate Luci config to starlark"
    This reverts commit 20a1103538249ceb59abae269969cc30efcd2f87.
    
    Reason for revert: Maybe caused the troubles on the HHH flutter builder.
    
    Original change's description:
    > [infra] Migrate Luci config to starlark
    > 
    > * Fixes inconsistencies discovered while converting to starlark.
    > 
    > To regenerate the config, run: ./main.star
    > 
    > Documentation for lucicfg is here:
    > https://chromium.googlesource.com/infra/luci/luci-go/+/refs/heads/master/lucicfg/doc/README.md
    > 
    > Change-Id: I7f8f033162cb1117215d6763575c7e8956ea710c
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/99520
    > Reviewed-by: William Hesse <whesse@google.com>
    
    TBR=whesse@google.com,athom@google.com
    
    Change-Id: Icc41a1a580c8dedf22eff1a9c6ea246c7a29ff7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/100965
    Reviewed-by: Alexander Thomas <athom@google.com>
    Commit-Queue: Alexander Thomas <athom@google.com>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -2,20 +2,8 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-
 def CheckChangeOnUpload(input_api, output_api):
-    return _CommonChecks(input_api, output_api)
-
+  return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
 
 def CheckChangeOnCommit(input_api, output_api):
-    return _CommonChecks(input_api, output_api)
-
-
-def _CommonChecks(input_api, output_api):
-    tests = input_api.canned_checks.CheckLucicfgGenOutput(
-        input_api, output_api, "main.star")
-    results = []
-    results.extend(
-        input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api))
-    results.extend(input_api.RunTests(tests))
-    return results
+  return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
