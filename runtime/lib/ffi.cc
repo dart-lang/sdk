b2949d34366ddca3365966572470b1c336ae593f
Daco Harkes <dacoharkes@google.com>
2019-05-09 12:19:21 +0000

    fix Flutter build for "[vm/ffi] DBC support on x64 Linux/MacOS hosts"
    Change-Id: I119776682e53f70f621107f2159670cb3e75b310
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/101982
    Reviewed-by: Daco Harkes <dacoharkes@google.com>

--- runtime/lib/ffi.cc
+++ runtime/lib/ffi.cc
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#include "runtime/lib/ffi.h"
+#include "lib/ffi.h"
 
 #include "include/dart_api.h"
 #include "vm/bootstrap_natives.h"
