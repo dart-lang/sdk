a3a49c8b8e3ecc26b55cb59d089f9b51ef3698ed
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-05-14 16:41:15 +0200

    widen application of prefer_null_aware_operators

--- test/rules/prefer_null_aware_operators.dart
+++ test/rules/prefer_null_aware_operators.dart
@@ -6,28 +6,44 @@
 
 var v;
 
-m() {
+m(p) {
   var a, b;
   a == null ? null : a.b; // LINT
   a == null ? null : a.b(); // LINT
   a == null ? null : a.b.c; // LINT
   a == null ? null : a.b.c(); // LINT
+  a.b == null ? null : a.b.c; // LINT
+  a.b == null ? null : a.b.c(); // LINT
+  p == null ? null : p.b; // LINT
+  v == null ? null : v.b; // LINT
   null == a ? null : a.b; // LINT
   null == a ? null : a.b(); // LINT
   null == a ? null : a.b.c; // LINT
   null == a ? null : a.b.c(); // LINT
+  null == a.b ? null : a.b.c; // LINT
+  null == a.b ? null : a.b.c(); // LINT
+  null == p ? null : p.b; // LINT
+  null == v ? null : v.b; // LINT
   a != null ? a.b : null; // LINT
   a != null ? a.b() : null; // LINT
   a != null ? a.b.c : null; // LINT
   a != null ? a.b.c() : null; // LINT
+  a.b != null ? a.b.c : null; // LINT
+  a.b != null ? a.b.c() : null; // LINT
+  p != null ? p.b : null; // LINT
+  v != null ? v.b : null; // LINT
   null != a ? a.b : null; // LINT
   null != a ? a.b() : null; // LINT
   null != a ? a.b.c : null; // LINT
   null != a ? a.b.c() : null; // LINT
+  null != a.b ? a.b.c : null; // LINT
+  null != a.b ? a.b.c() : null; // LINT
+  null != p ? p.b : null; // LINT
+  null != v ? v.b : null; // LINT
+
   a == null ? b : a; // OK
   a == null ? b.c : null; // OK
   a.b != null ? a.b : null; // OK
 
-  // lint only for local vars
-  v == null ? null : v.b; // OK
+  a == null ? null : a.b + 10; // OK
 }
