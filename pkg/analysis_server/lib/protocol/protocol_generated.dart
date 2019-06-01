519ddc1ac40f25fe66588d8f612d202b031c1069
Konstantin Shcheglov <scheglov@google.com>
2019-06-01 17:27:03 +0000

    Document that AVAILABLE_SUGGESTION_SETS also turns on existingImports.
    R=brianwilkerson@google.com
    
    Change-Id: I053c68eb541358f0bbd891ca16f1c5628e701096
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/104342
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/lib/protocol/protocol_generated.dart
+++ pkg/analysis_server/lib/protocol/protocol_generated.dart
@@ -7092,11 +7092,13 @@ class CompletionResultsParams implements HasToJson {
  */
 class CompletionService implements Enum {
   /**
-   * The client will receive notifications once subscribed with completion
-   * suggestion sets from the libraries of interest. The client should keep an
-   * up-to-date record of these in memory so that it will be able to union
-   * these candidates with other completion suggestions when applicable at
-   * completion time.
+   * The client will receive availableSuggestions notifications once subscribed
+   * with completion suggestion sets from the libraries of interest. The client
+   * should keep an up-to-date record of these in memory so that it will be
+   * able to union these candidates with other completion suggestions when
+   * applicable at completion time.
+   *
+   * The client will also receive existingImports notifications.
    */
   static const CompletionService AVAILABLE_SUGGESTION_SETS =
       const CompletionService._("AVAILABLE_SUGGESTION_SETS");
