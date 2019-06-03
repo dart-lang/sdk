5b72293f496855b8457803390dd5342721c9d1db
Liam Appelbe <liama@google.com>
2019-06-03 22:14:16 +0000

    Reland "[vm] Create offsets_extractor tool."
    This reverts commit 224f82c21cb2966f36ab850eae7ef5c8697cc477.
    
    Reason for revert: Just need to split DBC section into 32 and 64 bit
    
    Original change's description:
    > Revert "[vm] Create offsets_extractor tool."
    >
    > This reverts commit 3015d79371a1cf6407e5e799354d42fc6a0dcaec.
    >
    > Reason for revert: Fails the Flutter build
    > /b/s/w/ir/cache/builder/mac_sdk -mmacosx-version-min=10.12 -m32  -fno-strict-aliasing -fstack-protector-all -fcolor-diagnostics -Wall -Wextra -Wendif-labels -Werror -Wno-missing-field-initializers -Wno-unused-parameter -Wunguarded-availability -fvisibility=hidden -stdlib=libc++ -Wheader-hygiene -Wstring-conversion -Wthread-safety -O2 -fno-ident -fdata-sections -ffunction-sections -g2 -Werror -Wall -Wextra -Wno-unused-parameter -Wno-unused-private-field -Wnon-virtual-dtor -Wvla -Wno-conversion-null -Woverloaded-virtual -Wno-comments -g3 -ggdb3 -fno-rtti -fno-exceptions -Wimplicit-fallthrough -O3 -fvisibility-inlines-hidden -std=c++14 -fno-rtti -fno-exceptions  -c ../../third_party/dart/runtime/vm/dart.cc -o clang_x86/obj/third_party/dart/runtime/vm/libdart_vm_nosnapshot_with_precompiler.dart.o
    > In file included from ../../third_party/dart/runtime/vm/dart.cc:9:
    > ../../third_party/dart/runtime/vm/compiler/runtime_offsets_extracted.h:958:50: error: implicit conversion from 'long long' to 'const dart::word' (aka 'const long') changes value from 576460752303423487 to -1 [-Werror,-Wconstant-conversion]
    > static constexpr dart::word Array_kMaxElements = 576460752303423487;
    >                             ~~~~~~~~~~~~~~~~~~   ^~~~~~~~~~~~~~~~~~
    > ../../third_party/dart/runtime/vm/compiler/runtime_offsets_extracted.h:965:51: error: implicit conversion from 'long long' to 'const dart::word' (aka 'const long') changes value from 2305843009213693951 to -1 [-Werror,-Wconstant-conversion]
    > static constexpr dart::word String_kMaxElements = 2305843009213693951;
    >                             ~~~~~~~~~~~~~~~~~~~   ^~~~~~~~~~~~~~~~~~~
    > 2 errors generated.
    >
    > Change-Id: Iaf509c6ee7a2ce75664935519ac02a933a9eb2bf
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/104402
    > Reviewed-by: Siva Annamalai <asiva@google.com>
    > Commit-Queue: Siva Annamalai <asiva@google.com>
    > Auto-Submit: Siva Annamalai <asiva@google.com>
    
    TBR=asiva@google.com
    
    Change-Id: Ibf749ceee274b03cdffa6d7ed46fcbe75d1a1e94
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/104620
    Reviewed-by: Liam Appelbe <liama@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Commit-Queue: Liam Appelbe <liama@google.com>

--- runtime/vm/globals.h
+++ runtime/vm/globals.h
@@ -73,6 +73,25 @@ const intptr_t kDefaultMaxOldGenHeapSize = (kWordSize <= 4) ? 1536 : 0;
 #define NOT_IN_PRECOMPILED(code) code
 #endif  // defined(DART_PRECOMPILED_RUNTIME)
 
+#if defined(TARGET_ARCH_DBC)
+#define NOT_IN_DBC(code)
+#else
+#define NOT_IN_DBC(code) code
+#endif  // defined(TARGET_ARCH_DBC)
+
+#if defined(TARGET_ARCH_ARM) || defined(TARGET_ARCH_ARM64) ||                  \
+    defined(TARGET_ARCH_X64)
+#define ONLY_IN_ARM_ARM64_X64(code) code
+#else
+#define ONLY_IN_ARM_ARM64_X64(code)
+#endif
+
+#if defined(DART_PRECOMPILED_RUNTIME)
+#define NOT_IN_PRECOMPILED_RUNTIME(code)
+#else
+#define NOT_IN_PRECOMPILED_RUNTIME(code) code
+#endif  // defined(DART_PRECOMPILED_RUNTIME)
+
 #if !defined(PRODUCT) || defined(HOST_OS_FUCHSIA) || defined(TARGET_OS_FUCHSIA)
 #define SUPPORT_TIMELINE 1
 #endif
