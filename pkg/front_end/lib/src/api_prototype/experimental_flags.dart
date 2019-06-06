4a27605fdc5bd5645b691f308eec97989f69e175
Dan Rubel <danrubel@google.com>
2019-06-06 14:03:29 +0000

    generate kernel experimental flags
    Change-Id: I9430d95c012b752bc4ca0530d79252b8b032a26b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/104841
    Reviewed-by: Kevin Millikin <kmillikin@google.com>
    Commit-Queue: Dan Rubel <danrubel@google.com>
    Auto-Submit: Dan Rubel <danrubel@google.com>

--- pkg/front_end/lib/src/api_prototype/experimental_flags.dart
+++ pkg/front_end/lib/src/api_prototype/experimental_flags.dart
@@ -2,7 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// TODO(askesc): Generate this file from a flag specification.
+// NOTE: THIS FILE IS GENERATED. DO NOT EDIT.
+//
+// Instead modify 'tools/experimental_features.yaml' and run
+// 'pkg/front_end/tool/fasta generate-experimental-flags' to update.
 
 enum ExperimentalFlag {
   constantUpdate2018,
