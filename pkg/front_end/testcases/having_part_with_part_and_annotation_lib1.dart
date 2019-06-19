a878514cf522e471cce82c5dbe1e00182401362c
Jens Johansen <jensj@google.com>
2019-06-19 10:56:55 +0000

    Add frontend testcases for part with part(s) in it
    Currently there's at least two issues:
    * We can crash
    * We can add metadata to things they weren't meant for
    
    Change-Id: I4e277ea614ce9faa25b2bf1c5a16a5e877df225b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/106640
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

---
+++ pkg/front_end/testcases/having_part_with_part_and_annotation_lib1.dart
@@ -0,0 +1,8 @@
+part of 'having_part_with_part_and_annotation.dart';
+
+@Bar
+part 'having_part_with_part_and_annotation_lib2.dart';
+
+const int Bar = 43;
+
+void fromLib1() {}
---
+++ pkg/front_end/testcases/having_part_with_parts_and_annotation_lib2.dart
@@ -0,0 +1,3 @@
+part of 'having_part_with_parts_and_annotation_lib1.dart';
+
+void fromLib2() {}
