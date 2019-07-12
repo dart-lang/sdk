e3663195c761c60c0add8b476cc8daaa7f5be0dd
Nicholas Shahan <nshahan@google.com>
2019-07-12 19:29:59 +0000

    [dartdevc] Add more subtype tests
    Change-Id: I5314a5b80efbdc544d901184b2536d2f10427c89
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/108369
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Vijay Menon <vsm@google.com>

--- tests/compiler/dartdevc_native/subtype_test.dart
+++ tests/compiler/dartdevc_native/subtype_test.dart
@@ -2,9 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "package:expect/expect.dart";
+import 'dart:async';
 import 'dart:_foreign_helper' show JS;
 import 'dart:_runtime' as dart;
+
+import 'package:expect/expect.dart';
 
 class A {}
 
@@ -22,25 +24,70 @@ Object generic1(Type sWrapped, Type tWrapped) {
   return dart.wrapType(JS('', '#(#)', sGeneric, t));
 }
 
+// Returns a function type of argWrapped -> returnWrapped as a wrapped type.
+Object function1(Type returnWrapped, Type argWrapped) {
+  var returnType = dart.unwrapType(returnWrapped);
+  var argType = dart.unwrapType(argWrapped);
+  var fun = dart.fnType(returnType, [argType]);
+  return dart.wrapType(fun);
+}
+
 void checkSubtype(Type sWrapped, Type tWrapped) {
   var s = dart.unwrapType(sWrapped);
   var t = dart.unwrapType(tWrapped);
-  Expect.isTrue(dart.isSubtypeOf(s, t));
+  Expect.isTrue(dart.isSubtypeOf(s, t), '$s should be subtype of $t.');
 }
 
 void checkProperSubtype(Type sWrapped, Type tWrapped) {
   var s = dart.unwrapType(sWrapped);
   var t = dart.unwrapType(tWrapped);
-  Expect.isTrue(dart.isSubtypeOf(s, t));
-  Expect.isFalse(dart.isSubtypeOf(t, s));
+  Expect.isTrue(dart.isSubtypeOf(s, t), '$s should be subtype of $t.');
+  Expect.isFalse(dart.isSubtypeOf(t, s), '$t should not be subtype of $s.');
 }
 
 void main() {
+  // A <: dynamic
+  checkProperSubtype(A, dynamic);
+  // A <: Object
+  checkProperSubtype(A, Object);
+  // TODO(nshahan) Test void as top? A <: void
+
+  // Null <: A
+  checkProperSubtype(Null, A);
+
+  // Future<B> <: FutureOr<A>
+  checkProperSubtype(generic1(Future, B), generic1(FutureOr, A));
+  // B <: <: FutureOr<A>
+  checkProperSubtype(B, generic1(FutureOr, A));
+  // Future<B> <: Future<A>
+  checkProperSubtype(generic1(Future, B), generic1(Future, A));
+  // B <: A
   checkProperSubtype(B, A);
+
+  // A <: A
+  checkSubtype(A, A);
+  // C <: B
   checkProperSubtype(C, B);
+  // C <: A
   checkProperSubtype(C, A);
 
+  // A -> B <: Function
+  checkProperSubtype(function1(B, A), Function);
+
+  // A -> B <: A -> B
+  checkSubtype(function1(B, A), function1(B, A));
+
+  // A -> B <: B -> B
+  checkProperSubtype(function1(B, A), function1(B, B));
+  // TODO(nshahan) Subtype check with covariant keyword?
+
+  // A -> B <: A -> A
+  checkSubtype(function1(B, A), function1(A, A));
+
+  // D <: D<B>
   checkSubtype(D, generic1(D, B));
+  // D<B> <: D
   checkSubtype(generic1(D, B), D);
+  // D<C> <: D<B>
   checkProperSubtype(generic1(D, C), generic1(D, B));
 }
