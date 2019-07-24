187c5b5bc9b3bcaab4f7b7cbc7a1143f2bcc1969
pq <pquitslund@google.com>
2019-07-24 15:46:53 +0000

    re-add test dep
    Change-Id: Ie1240eb63b62c4fcd24a9dfda709eb54deb08129
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/110300
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/dart/resolution/extension_method_test.dart
+++ pkg/analyzer/test/src/dart/resolution/extension_method_test.dart
@@ -5,6 +5,7 @@
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/src/error/codes.dart';
 import 'package:analyzer/src/generated/engine.dart';
+import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'driver_resolution.dart';
