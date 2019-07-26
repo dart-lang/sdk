5b5ddeda923eb00926d0b26a92835b59ba725e28
Konstantin Shcheglov <scheglov@google.com>
2019-07-26 21:46:11 +0000

    Switch EdgeBuilder to InheritanceManager3.
    R=brianwilkerson@google.com, paulberry@google.com
    
    Change-Id: Iea71517556d7dbd274aa36bc11ceb659e1af469e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/110742
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/nnbd_migration/lib/src/edge_builder.dart
+++ pkg/nnbd_migration/lib/src/edge_builder.dart
@@ -8,7 +8,7 @@ import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/src/dart/element/handle.dart';
-import 'package:analyzer/src/dart/element/inheritance_manager2.dart';
+import 'package:analyzer/src/dart/element/inheritance_manager3.dart';
 import 'package:analyzer/src/dart/element/member.dart';
 import 'package:analyzer/src/dart/element/type.dart';
 import 'package:analyzer/src/generated/resolver.dart';
@@ -31,7 +31,7 @@ import 'package:nnbd_migration/src/nullability_node.dart';
 /// variables that will determine its nullability.  For `visit...` methods that
 /// don't visit expressions, `null` will be returned.
 class EdgeBuilder extends GeneralizingAstVisitor<DecoratedType> {
-  final InheritanceManager2 _inheritanceManager;
+  final InheritanceManager3 _inheritanceManager;
 
   /// The repository of constraint variables and decorated types (from a
   /// previous pass over the source code).
@@ -88,7 +88,7 @@ class EdgeBuilder extends GeneralizingAstVisitor<DecoratedType> {
   EdgeBuilder(TypeProvider typeProvider, TypeSystem typeSystem, this._variables,
       this._graph, this._source, this.listener)
       : _decoratedClassHierarchy = DecoratedClassHierarchy(_variables, _graph),
-        _inheritanceManager = InheritanceManager2(typeSystem),
+        _inheritanceManager = InheritanceManager3(typeSystem),
         _notNullType = DecoratedType(typeProvider.objectType, _graph.never),
         _nonNullableBoolType =
             DecoratedType(typeProvider.boolType, _graph.never),
@@ -1128,12 +1128,14 @@ $stackTrace''');
       if (declaredElement is! ConstructorElement) {
         var classElement = declaredElement.enclosingElement as ClassElement;
         var origin = InheritanceOrigin(_source, node.offset);
-        for (var overridden in _inheritanceManager.getOverridden(
+        for (var overriddenElement in _inheritanceManager.getOverridden(
                 classElement.type,
                 Name(classElement.library.source.uri, declaredElement.name)) ??
-            const []) {
-          var overriddenElement = overridden.element as ExecutableElement;
-          assert(overriddenElement is! ExecutableMember);
+            const <ExecutableElement>[]) {
+          if (overriddenElement is ExecutableMember) {
+            var member = overriddenElement as ExecutableMember;
+            overriddenElement = member.baseElement;
+          }
           var overriddenClass =
               overriddenElement.enclosingElement as ClassElement;
           var decoratedOverriddenFunctionType =
