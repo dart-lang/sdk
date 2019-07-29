dart-lang/sdk

commit 92faca77d9ccdd9b953269d7fdce182f42d3e3ed

[vm/compiler] Use simpleInstanceOf for is checks if the type to test against is a rare type
For is checks where the type is a rare type we can use the faster _simpleInstanceOf implementation,
since there is no need to consider the type arguments.

By using the simpleInstanceOf implementation more optimizations get done
by our call specializer (e.g. simply doing a cid check).

This can improve type checks for code like `if (object is Future) {}`

Change-Id: I88497b9d2bdb4693ab79cd23cd19e727169eed63
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/110566
Commit-Queue: Martin Kustermann <kustermann@google.com>
Reviewed-by: Alexander Markov <alexmarkov@google.com>
Reviewed-by: Régis Crelier <regis@google.com>

runtime/vm/compiler/frontend/flow_graph_builder.cc
@@ -373,8 +373,18 @@ bool SimpleInstanceOfType(const AbstractType& type) {
 
   ASSERT(type.HasTypeClass());
   const Class& type_class = Class::Handle(type.type_class());
+
   // Bail if the type has any type parameters.
-  if (type_class.IsGeneric()) return false;
+  if (type_class.IsGeneric()) {
+    // If the interface type we check against is generic but has all-dynamic
+    // type arguments, then we can still use the _simpleInstanceOf
+    // implementation (see also runtime/lib/object.cc:Object_SimpleInstanceOf).
+    //
+    // NOTE: We rely on the all-dynamic vector to be represented as `null` here.
+    ASSERT(AbstractType::Handle(type_class.RareType()).arguments() ==
+           TypeArguments::null());
+    return type.arguments() == TypeArguments::null();
+  }
 
   // Finally a simple class for instance of checking.
   return true;

commit 8db1408ffdda4d20a0f8dbda63df284ee9c10139

[vm] Use RareType for fast is-checks optimization instead of TypeArguments::null() comparison
This fixes the ASSERT introduced in 92faca77d9ccdd9b953269d7fdce182f42d3e3ed

Change-Id: I6564a1b0436b15e2adf762ead111860f8dd5650c
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/110909
Commit-Queue: Martin Kustermann <kustermann@google.com>
Reviewed-by: Alexander Markov <alexmarkov@google.com>
Reviewed-by: Martin Kustermann <kustermann@google.com>

runtime/vm/compiler/frontend/flow_graph_builder.cc
@@ -379,11 +379,8 @@ bool SimpleInstanceOfType(const AbstractType& type) {
     // If the interface type we check against is generic but has all-dynamic
     // type arguments, then we can still use the _simpleInstanceOf
     // implementation (see also runtime/lib/object.cc:Object_SimpleInstanceOf).
-    //
-    // NOTE: We rely on the all-dynamic vector to be represented as `null` here.
-    ASSERT(AbstractType::Handle(type_class.RareType()).arguments() ==
-           TypeArguments::null());
-    return type.arguments() == TypeArguments::null();
+    const auto& rare_type = AbstractType::Handle(type_class.RareType());
+    return rare_type.Equals(type);
   }
 
   // Finally a simple class for instance of checking.