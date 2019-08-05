91f879b0a1af71c22a746925a2e93bddfe210b77
Konstantin Shcheglov <scheglov@google.com>
2019-08-05 14:28:01 +0000

    Remove unused import.
    R=brianwilkerson@google.com, paulberry@google.com
    
    Change-Id: I4896f11da73cc84d569bd28d32c5b4b23d5c4530
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/111964
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/diagnostics/access_static_extension_member_test.dart
+++ pkg/analyzer/test/src/diagnostics/access_static_extension_member_test.dart
@@ -5,7 +5,6 @@
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/src/error/codes.dart';
 import 'package:analyzer/src/generated/engine.dart';
-import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import '../dart/resolution/driver_resolution.dart';
