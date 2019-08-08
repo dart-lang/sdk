bb293ef8cf1ddf40f870aa3a1359d1e2662d0f2e
Sam Rawlins <srawlins@google.com>
2019-08-08 15:20:41 +0000

    Fix docs for Window.close() and Window.location=
    * `closed` is not a method.
    * `location` is not a method.
    * `location=` does _not_ take a String; only a Location object.
    
    Fixes #16893
    
    Change-Id: Ia5389ae80c39c5c7d9eebdd0cd04cb994a27fd7d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/111965
    Reviewed-by: Terry Lucas <terry@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- sdk/lib/html/dart2js/html_dart2js.dart
+++ sdk/lib/html/dart2js/html_dart2js.dart
@@ -30564,7 +30564,7 @@ class Window extends EventTarget
   // TODO: consider forcing users to do: window.location.assign('string').
   /**
    * Sets the window's location, which causes the browser to navigate to the new
-   * location. [value] may be a Location object or a String.
+   * location.
    */
   set location(value) {
     _location = value;
@@ -34587,12 +34587,14 @@ abstract class WindowBase implements EventTarget {
    *     var other = window.open('http://www.example.com', 'foo');
    *     // Closes other window, as it is script-closeable.
    *     other.close();
-   *     print(other.closed()); // 'true'
+   *     print(other.closed); // 'true'
    *
-   *     window.location('http://www.mysite.com', 'foo');
+   *     var newLocation = window.location
+   *         ..href = 'http://www.mysite.com';
+   *     window.location = newLocation;
    *     // Does not close this window, as the history has changed.
    *     window.close();
-   *     print(window.closed()); // 'false'
+   *     print(window.closed); // 'false'
    *
    * See also:
    *
