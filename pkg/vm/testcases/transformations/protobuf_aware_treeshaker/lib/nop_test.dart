32b70ce2a58a4dd8ab6e875b38a065a08b287385
Martin Kustermann <kustermann@google.com>
2019-08-12 13:33:45 +0000

    [protobuf] Remove constants transformation from protobuf-aware treeshaker, ensure we always clean metadata
    Change-Id: Ifb9e5bd3530ba6cfd78455f3ae380a36f60f31d2
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/112720
    Reviewed-by: Sigurd Meldgaard <sigurdm@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

---
+++ pkg/vm/testcases/transformations/protobuf_aware_treeshaker/lib/nop_test.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+main() {
+  // Ensures the protobuf-aware tree shaker can also transform non-protobuf
+  // using code.
+  print("hello world");
+}
