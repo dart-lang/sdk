2f02836576b427d2a2ade72f8c119162d04bfe48
Jonas Termansen <sortie@google.com>
2019-08-13 16:06:34 +0000

    [benchmark] The large tensorflow models are not needed for benchmarking.
    Change-Id: I1d1fdcdf5bddc0c263ae41086c9d9e682d9e6d49
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/112901
    Reviewed-by: William Hesse <whesse@google.com>

--- tools/bots/try_benchmarks.sh
+++ tools/bots/try_benchmarks.sh
@@ -84,6 +84,8 @@ for command; do
     tar -czf linux-ia32_profile.tar.gz \
       --exclude .git \
       --exclude .gitignore \
+      --exclude pkg/analysis_server/language_model \
+      --exclude out/ReleaseIA32/dart-sdk/model \
       -- \
       third_party/d8/linux/ia32/natives_blob.bin \
       third_party/d8/linux/ia32/snapshot_blob.bin \
@@ -167,6 +169,8 @@ for command; do
     tar -czf linux-ia32.tar.gz \
       --exclude .git \
       --exclude .gitignore \
+      --exclude pkg/analysis_server/language_model \
+      --exclude out/ReleaseIA32/dart-sdk/model \
       -- \
       third_party/d8/linux/ia32/natives_blob.bin \
       third_party/d8/linux/ia32/snapshot_blob.bin \
@@ -249,6 +253,8 @@ EOF
     tar -czf linux-x64_profile.tar.gz \
       --exclude .git \
       --exclude .gitignore \
+      --exclude pkg/analysis_server/language_model \
+      --exclude out/ReleaseX64/dart-sdk/model \
       -- \
       third_party/d8/linux/x64/natives_blob.bin \
       third_party/d8/linux/x64/snapshot_blob.bin \
@@ -352,6 +358,8 @@ EOF
     tar -czf linux-x64.tar.gz \
       --exclude .git \
       --exclude .gitignore \
+      --exclude pkg/analysis_server/language_model \
+      --exclude out/ReleaseX64/dart-sdk/model \
       -- \
       third_party/d8/linux/x64/natives_blob.bin \
       third_party/d8/linux/x64/snapshot_blob.bin \
