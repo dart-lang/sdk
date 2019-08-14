ec2d06d4b9f4f0accad2b4aa841499e8e93cd70b
Siva Annamalai <asiva@google.com>
2019-08-14 16:51:49 +0000

    Use MAP_JIT when doing an mmap for executable pages (needed for macOS Catalina).
    Please refer to https://developer.apple.com/documentation/bundleresources/entitlements/com_apple_security_cs_allow-jit for more details.
    
    This is the Dart VM part for https://github.com/flutter/flutter/issues/36714
    
    Change-Id: Ifbc0ce82921d5f05421e01ce2d19423a5f3e32f9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/112849
    Reviewed-by: Ryan Macnak <rmacnak@google.com>
    Commit-Queue: Siva Annamalai <asiva@google.com>

--- runtime/platform/utils_macos.cc
+++ runtime/platform/utils_macos.cc
@@ -7,6 +7,9 @@
 
 #include "platform/utils.h"
 
+#include <errno.h>        // NOLINT
+#include <sys/utsname.h>  // NOLINT
+
 namespace dart {
 
 char* Utils::StrNDup(const char* s, intptr_t n) {
@@ -63,6 +66,76 @@ int Utils::VSNPrint(char* str, size_t size, const char* format, va_list args) {
   }
   return retval;
 }
+
+namespace internal {
+
+// Returns the running system's Darwin major version. Don't call this, it's
+// an implementation detail and its result is meant to be cached by
+// MacOSXMinorVersion.
+int32_t DarwinMajorVersionInternal() {
+  // uname is implemented as a simple series of sysctl system calls to
+  // obtain the relevant data from the kernel. The data is
+  // compiled right into the kernel, so no threads or blocking or other
+  // funny business is necessary.
+
+  struct utsname uname_info;
+  if (uname(&uname_info) != 0) {
+    FATAL("Fatal error in DarwinMajorVersionInternal : invalid return uname");
+    return 0;
+  }
+
+  if (strcmp(uname_info.sysname, "Darwin") != 0) {
+    FATAL1(
+        "Fatal error in DarwinMajorVersionInternal : unexpected uname"
+        " sysname '%s'",
+        uname_info.sysname);
+    return 0;
+  }
+
+  int32_t darwin_major_version = 0;
+  char* dot = strchr(uname_info.release, '.');
+  if (dot) {
+    errno = 0;
+    char* end_ptr = NULL;
+    darwin_major_version = strtol(uname_info.release, &end_ptr, 10);
+    if (errno != 0 || (end_ptr == uname_info.release)) {
+      dot = NULL;
+    }
+  }
+
+  if (!dot) {
+    FATAL1(
+        "Fatal error in DarwinMajorVersionInternal :"
+        " could not parse uname release '%s'",
+        uname_info.release);
+    return 0;
+  }
+
+  return darwin_major_version;
+}
+
+// Returns the running system's Mac OS X minor version. This is the |y| value
+// in 10.y or 10.y.z. Don't call this, it's an implementation detail and the
+// result is meant to be cached by MacOSXMinorVersion.
+int32_t MacOSXMinorVersionInternal() {
+  int darwin_major_version = DarwinMajorVersionInternal();
+
+  // The Darwin major version is always 4 greater than the Mac OS X minor
+  // version for Darwin versions beginning with 6, corresponding to Mac OS X
+  // 10.2. Since this correspondence may change in the future, warn when
+  // encountering a version higher than anything seen before. Older Darwin
+  // versions, or versions that can't be determined, result in
+  // immediate death.
+  ASSERT(darwin_major_version >= 6);
+  return (darwin_major_version - 4);
+}
+
+int32_t MacOSXMinorVersion() {
+  static int mac_os_x_minor_version = MacOSXMinorVersionInternal();
+  return mac_os_x_minor_version;
+}
+
+}  // namespace internal
 
 }  // namespace dart
 
