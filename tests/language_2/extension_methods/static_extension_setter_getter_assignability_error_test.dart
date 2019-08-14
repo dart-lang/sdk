76c99bcd01c2679363adf385ea60dec8521fce35
Leaf Petersen <leafp@google.com>
2019-08-14 02:24:44 +0000

    Add tests for internal name conflicts in extension methods
    Change-Id: I7c2bed173f4d945ab83ab9157fcf650ddd4990ce
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/112645
    Reviewed-by: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language_2/extension_methods/static_extension_setter_getter_assignability_error_test.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=extension-methods
+
+// It is an error to have a setter and a getter in an extension where
+// the argument type of the setter is not assignable to the return type
+// of the getter
+extension E1 on int {
+  static int get property => 1;
+  //     ^^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+  static void set property(String value) {}
+  //                       ^^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+  int get property2 => 1;
+  //  ^^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+  void set property2(String x) {}
+  //                 ^^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+void main() {}
