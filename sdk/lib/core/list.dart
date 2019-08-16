43670101a4b63c7ca77dac2172242daeef3b6f52
Lasse R.H. Nielsen <lrn@google.com>
2019-08-16 15:19:13 +0200

    Remove trailing whitespace.

--- sdk/lib/core/list.dart
+++ sdk/lib/core/list.dart
@@ -718,11 +718,11 @@ abstract class List<E> implements EfficientLengthIterable<E> {
    */
   Map<int, E> asMap();
 
-  /** 
+  /**
   * Whether this list is equal to [other].
   *
   * Lists are, by default, only equal to themselves.
-  * Even if [other] is also a list, the equality comparison 
+  * Even if [other] is also a list, the equality comparison
   * does not compare the elements of the two lists.
   */
  bool operator ==(Object other);
