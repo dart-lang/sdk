208c8dd0c08caebc5cc5ddabb0f564dc0354bab9
Jonas Termansen <sortie@google.com>
2019-08-19 14:38:43 +0000

    [benchmark] Remove SIMDBC benchmark test coverage.
    This removes 1 minute of build time from the benchmark-linux-try CQ builder.
    
    Change-Id: I327b35cf50687bee26a3b12dedfdf5fc053c26ea
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/113323
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- tools/bots/try_benchmarks.sh
+++ tools/bots/try_benchmarks.sh
@@ -243,13 +243,8 @@ EOF
     ./tools/build.py --mode=release --arch=x64 runtime
     ./tools/build.py --mode=release --arch=x64 gen_snapshot
     ./tools/build.py --mode=release --arch=x64 dart_precompiled_runtime
-    ./tools/build.py --mode=release --arch=simdbc64 runtime
   elif [ "$command" = linux-x64-archive ] ||
        [ "$command" = linux-x64-bytecode-archive ]; then
-    simdbc_dart=out/ReleaseSIMDBC64/dart
-    if [ "$command" = linux-x64-bytecode-archive ]; then
-      simdbc_dart=
-    fi
     tar -czf linux-x64_profile.tar.gz \
       --exclude .git \
       --exclude .gitignore \
@@ -262,7 +257,6 @@ EOF
       out/ReleaseX64/vm_platform_strong.dill \
       out/ReleaseX64/gen/kernel_service.dill \
       out/ReleaseX64/dart-sdk \
-      $simdbc_dart \
       out/ReleaseX64/dart \
       out/ReleaseX64/gen_snapshot \
       out/ReleaseX64/gen_kernel_bytecode.dill \
@@ -367,7 +361,6 @@ EOF
       out/ReleaseX64/vm_platform_strong.dill \
       out/ReleaseX64/gen/kernel_service.dill \
       out/ReleaseX64/dart-sdk \
-      $simdbc_dart \
       out/ReleaseX64/dart \
       out/ReleaseX64/gen_snapshot \
       out/ReleaseX64/gen_kernel_bytecode.dill \
@@ -403,9 +396,6 @@ EOF
     out/ReleaseX64/dart --profile-period=10000 --packages=.packages --enable-interpreter --compilation-counter-threshold=-1 hello.dart
     out/ReleaseX64/dart --profile-period=10000 --packages=.packages --use-bytecode-compiler hello.dart
     out/ReleaseX64/dart --profile-period=10000 --packages=.packages --use-bytecode-compiler --optimization-counter-threshold=-1 hello.dart
-    if [ "$command" = linux-x64-benchmark ]; then
-      out/ReleaseSIMDBC64/dart --profile-period=10000 --packages=.packages hello.dart
-    fi
     out/ReleaseX64/dart pkg/front_end/tool/perf.dart parse hello.dart
     out/ReleaseX64/dart pkg/front_end/tool/perf.dart scan hello.dart
     out/ReleaseX64/dart pkg/front_end/tool/fasta_perf.dart --legacy kernel_gen_e2e hello.dart
