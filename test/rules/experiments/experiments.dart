8e1e14c28a2604239d1b9afcb05d900e27187b7e
pq <pquitslund@gmail.com>
2019-08-22 10:47:00 -0700

    extension support for avoid_positional_boolean_parameters

--- test/rules/experiments/experiments.dart
+++ test/rules/experiments/experiments.dart
@@ -5,6 +5,7 @@
 import 'package:linter/src/analyzer.dart';
 import 'package:linter/src/rules/always_declare_return_types.dart';
 import 'package:linter/src/rules/avoid_returning_this.dart';
+import 'package:linter/src/rules/avoid_positional_boolean_parameters.dart';
 import 'package:linter/src/rules/camel_case_extensions.dart';
 import 'package:linter/src/rules/prefer_constructors_over_static_methods.dart';
 import 'package:linter/src/rules/public_member_api_docs.dart';
@@ -13,6 +14,7 @@ import 'package:linter/src/rules/type_annotate_public_apis.dart';
 
 final experiments = <LintRule>[
   AlwaysDeclareReturnTypes(),
+  AvoidPositionalBooleanParameters(),
   AvoidReturningThis(),
   CamelCaseExtensions(),
   PreferConstructorsInsteadOfStaticMethods(),
