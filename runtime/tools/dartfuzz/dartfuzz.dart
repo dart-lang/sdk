f632df018a7faf03e50e7cfd0df6dd29c8623d36
Felicitas Hetzelt <felih@google.com>
2019-08-22 18:38:18 +0000

    [vm/fuzzer] Add interesting fuzzing values
    Rationale:
    AFL provides a list of integer values likely to trigger bugs in the
    target program. I added the afl list to the local list of interesting
    values.
    Change-Id: I4f5a29b395ab3bd16a9389a3c381d73eb1ad8fc4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/114051
    Reviewed-by: Aart Bik <ajcbik@google.com>
    Commit-Queue: Felicitas Hetzelt <felih@google.com>

--- runtime/tools/dartfuzz/dartfuzz.dart
+++ runtime/tools/dartfuzz/dartfuzz.dart
@@ -13,7 +13,7 @@ import 'dartfuzz_api_table.dart';
 // Version of DartFuzz. Increase this each time changes are made
 // to preserve the property that a given version of DartFuzz yields
 // the same fuzzed program for a deterministic random seed.
-const String version = '1.19';
+const String version = '1.20';
 
 // Restriction on statements and expressions.
 const int stmtLength = 2;
