f989605cc96bef6e34a3833dbbea522b86d313b2
Michael Thomsen <mit@google.com>
2019-09-06 07:47:49 +0000

    Fix dartdoc typo in ffi
    Closes #38233
    https://github.com/dart-lang/sdk/pull/38233
    
    GitOrigin-RevId: c3e017d8c340976aeabfded522886cd9cda0638a
    Change-Id: Ife1adb72db8887a91b2049316e8444a4d5f121ec
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/115905
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- sdk/lib/ffi/ffi.dart
+++ sdk/lib/ffi/ffi.dart
@@ -5,7 +5,7 @@
 /**
  * Foreign Function Interface for interoperability with the C programming language.
  *
- * ** NOTE**: Dart:FFI is in technical preview. The overall feature is incomplete,
+ * **NOTE**: Dart:FFI is in technical preview. The overall feature is incomplete,
  * may contain issues, and breaking API changes are still expected.
  *
  * For further details, please see: https://dart.dev/server/c-interop
