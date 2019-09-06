fd7676e0bcb1f3436e52a548d3631630fd7e3871
Mayank Patke <fishythefish@google.com>
2019-09-06 23:20:16 +0000

    Fix language_2/config_import_corelib_test
    Change-Id: Idc45ec34d12957f2ea097e27ba606344aec32ee5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/116057
    Auto-Submit: Mayank Patke <fishythefish@google.com>
    Reviewed-by: Leaf Petersen <leafp@google.com>
    Commit-Queue: Mayank Patke <fishythefish@google.com>

--- tests/language_2/config_import_corelib_io.dart
+++ tests/language_2/config_import_corelib_io.dart
@@ -6,9 +6,11 @@ import "dart:io";
 
 class Classy {
   String get name => "classy io";
+  String httpSpecific() => throw UnimplementedError();
   String ioSpecific() => "classy io";
 }
 
 bool general() => true;
+bool httpSpecific() => false;
 bool ioSpecific() => true;
 final String name = "io";
