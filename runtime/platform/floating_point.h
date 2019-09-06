ec7ec4ecf7fb388c8bc6d074acb65e7d5605ff05
Daco Harkes <dacoharkes@google.com>
2019-09-06 09:08:49 +0000

    [gardening] Fix import to fix Flutter build
    How cmath is included changes whether the symbols are in the std:: namespace or not. This changes it back to how we have it everywhere in the codebase: math.h
    
    Follow up of https://dart-review.googlesource.com/c/sdk/+/115707/
    
    Change-Id: I383c1e9de0434d1367dfdb6302a8fb6db2c3062c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/115920
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Daco Harkes <dacoharkes@google.com>

--- runtime/platform/floating_point.h
+++ runtime/platform/floating_point.h
@@ -5,7 +5,7 @@
 #ifndef RUNTIME_PLATFORM_FLOATING_POINT_H_
 #define RUNTIME_PLATFORM_FLOATING_POINT_H_
 
-#include <cmath>
+#include <math.h>
 
 inline double fmod_ieee(double x, double y) {
   return fmod(x, y);
