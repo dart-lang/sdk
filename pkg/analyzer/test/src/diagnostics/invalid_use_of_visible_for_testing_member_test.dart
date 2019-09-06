cf94c3a2fadf1183e4ffd9a0d341726de1d927c3
Sam Rawlins <srawlins@google.com>
2019-09-06 22:20:57 +0000

    Additional tests verifying interaction between mixins, static extensions, and visiblity annotations (protected, visibleForTemplate, visibleForTesting).
    Change-Id: I7dc09aa0d12ea917c4c8c8235efc4554b8287379
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/116048
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Auto-Submit: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/diagnostics/invalid_use_of_visible_for_testing_member_test.dart
+++ pkg/analyzer/test/src/diagnostics/invalid_use_of_visible_for_testing_member_test.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/src/error/codes.dart';
+import 'package:analyzer/src/generated/engine.dart';
 import 'package:analyzer/src/test_utilities/package_mixin.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
@@ -11,12 +13,18 @@ import '../dart/resolution/driver_resolution.dart';
 main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(InvalidUseOfVisibleForTestingMemberTest);
+    defineReflectiveTests(InvalidUseOfVisibleForTestingMember_InExtensionTest);
   });
 }
 
 @reflectiveTest
 class InvalidUseOfVisibleForTestingMemberTest extends DriverResolutionTest
     with PackageMixin {
+  @override
+  AnalysisOptionsImpl get analysisOptions => AnalysisOptionsImpl()
+    ..contextFeatures = new FeatureSet.forTesting(
+        sdkVersion: '2.3.0', additionalFeatures: [Feature.extension_methods]);
+
   test_constructor() async {
     addMetaPackage();
     newFile('/lib1.dart', content: r'''
@@ -210,6 +218,29 @@ void main() {
         [HintCode.INVALID_USE_OF_VISIBLE_FOR_TESTING_MEMBER]);
   }
 
+  test_mixin() async {
+    addMetaPackage();
+    newFile('/lib1.dart', content: r'''
+import 'package:meta/meta.dart';
+mixin M {
+  @visibleForTesting
+  int m() => 1;
+}
+class C with M {}
+''');
+    newFile('/lib2.dart', content: r'''
+import 'lib1.dart';
+void main() {
+  C().m();
+}
+''');
+
+    await _resolveTestFile('/lib1.dart');
+    await _resolveTestFile('/lib2.dart');
+    assertTestErrorsWithCodes(
+        [HintCode.INVALID_USE_OF_VISIBLE_FOR_TESTING_MEMBER]);
+  }
+
   test_topLevelFunction() async {
     addMetaPackage();
     newFile('/lib1.dart', content: r'''
@@ -235,5 +266,57 @@ void main() {
   /// Similar to ResolutionTest.resolveTestFile, but a custom path is supported.
   Future<void> _resolveTestFile(String path) async {
     result = await resolveFile(convertPath(path));
+  }
+}
+
+@reflectiveTest
+class InvalidUseOfVisibleForTestingMember_InExtensionTest
+    extends InvalidUseOfVisibleForTestingMemberTest {
+  @override
+  AnalysisOptionsImpl get analysisOptions => AnalysisOptionsImpl()
+    ..contextFeatures = new FeatureSet.forTesting(
+        sdkVersion: '2.3.0', additionalFeatures: [Feature.extension_methods]);
+
+  test_functionInExtension() async {
+    addMetaPackage();
+    newFile('/lib1.dart', content: r'''
+import 'package:meta/meta.dart';
+extension E on List {
+  @visibleForTesting
+  int m() => 1;
+}
+''');
+    newFile('/lib2.dart', content: r'''
+import 'lib1.dart';
+void main() {
+  E([]).m();
+}
+''');
+
+    await _resolveTestFile('/lib1.dart');
+    await _resolveTestFile('/lib2.dart');
+    assertTestErrorsWithCodes(
+        [HintCode.INVALID_USE_OF_VISIBLE_FOR_TESTING_MEMBER]);
+  }
+
+  test_functionInExtension_fromTestDirectory() async {
+    addMetaPackage();
+    newFile('/lib1.dart', content: r'''
+import 'package:meta/meta.dart';
+extension E on List {
+  @visibleForTesting
+  int m() => 1;
+}
+''');
+    newFile('/test/test.dart', content: r'''
+import '../lib1.dart';
+void main() {
+  E([]).m();
+}
+''');
+
+    await _resolveTestFile('/lib1.dart');
+    await _resolveTestFile('/test/test.dart');
+    assertNoTestErrors();
   }
 }
