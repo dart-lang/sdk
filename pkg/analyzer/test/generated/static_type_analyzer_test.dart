3d3bbc2dbf0ac329019c5f2f6ca248785b77c83f
Konstantin Shcheglov <scheglov@google.com>
2019-09-07 15:01:23 +0000

    Remove 'strongMode' flag in StaticTypeAnalyzerTest.
    R=brianwilkerson@google.com
    
    Change-Id: I85c0a0a5fd9ea2f0814906f44ce922c70f0c842e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/116088
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/generated/static_type_analyzer_test.dart
+++ pkg/analyzer/test/generated/static_type_analyzer_test.dart
@@ -14,7 +14,6 @@ import 'package:analyzer/src/dart/element/element.dart';
 import 'package:analyzer/src/dart/element/inheritance_manager3.dart';
 import 'package:analyzer/src/dart/element/type.dart';
 import 'package:analyzer/src/error/codes.dart';
-import 'package:analyzer/src/generated/engine.dart';
 import 'package:analyzer/src/generated/resolver.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/generated/static_type_analyzer.dart';
@@ -1016,7 +1015,6 @@ class StaticTypeAnalyzerTest extends EngineTestCase with ResourceProviderMixin {
   }
 
   void test_visitListLiteral_unresolved() {
-    _analyzer = _createAnalyzer(strongMode: true);
     // [a] // where 'a' is not resolved
     Identifier identifier = AstTestFactory.identifier3('a');
     Expression node = AstTestFactory.listLiteral([identifier]);
@@ -1029,7 +1027,6 @@ class StaticTypeAnalyzerTest extends EngineTestCase with ResourceProviderMixin {
   }
 
   void test_visitListLiteral_unresolved_multiple() {
-    _analyzer = _createAnalyzer(strongMode: true);
     // [0, a, 1] // where 'a' is not resolved
     Identifier identifier = AstTestFactory.identifier3('a');
     Expression node = AstTestFactory.listLiteral(
@@ -1380,14 +1377,8 @@ class StaticTypeAnalyzerTest extends EngineTestCase with ResourceProviderMixin {
   /**
    * Create the analyzer used by the tests.
    */
-  StaticTypeAnalyzer _createAnalyzer({bool strongMode: false}) {
-    AnalysisContext context;
-    if (strongMode) {
-      AnalysisOptionsImpl options = new AnalysisOptionsImpl();
-      context = TestAnalysisContext();
-    } else {
-      context = TestAnalysisContext();
-    }
+  StaticTypeAnalyzer _createAnalyzer() {
+    var context = TestAnalysisContext();
     var inheritance = new InheritanceManager3(context.typeSystem);
     Source source = new FileSource(getFile("/lib.dart"));
     CompilationUnitElementImpl definingCompilationUnit =
