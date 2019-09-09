ecb9a113d1c215471b0df8a426532d89662782cc
Mayank Patke <fishythefish@google.com>
2019-09-09 19:25:56 +0000

    Fix language_2/regress_22976_test
    Change-Id: I0ea4a1861d37e5eefa2e05b0aafcde95fe95f484
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/115902
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Mayank Patke <fishythefish@google.com>
    Auto-Submit: Mayank Patke <fishythefish@google.com>

--- tests/language_2/regress_22976_test.dart
+++ tests/language_2/regress_22976_test.dart
@@ -9,10 +9,16 @@ class A<T> {}
 class B<T> implements A<T> {}
 
 class C<S, T> implements B<S>, A<T> {}
+// [error line 11, column 1, length 38]
+// [analyzer] COMPILE_TIME_ERROR.CONFLICTING_GENERIC_INTERFACES
+//    ^
+// [cfe] 'C' can't implement both 'A<S>' and 'A<T>'
 
 main() {
   C<int, String> c1 = new C<int, String>();
   C<String, int> c2 = new C<String, int>();
-  A<int> a0 = c1; //# 01: ok
-  A<int> a1 = c2; //# 02: ok
+  A<int> a0 = c1;
+  A<int> a1 = c2;
+  //          ^
+  // [cfe] A value of type 'C<String, int>' can't be assigned to a variable of type 'A<int>'.
 }
