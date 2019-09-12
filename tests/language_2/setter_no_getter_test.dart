4331e7e5fac4a1123182ed24cfebc83cddbef513
Kallen Tu <kallentu@google.com>
2019-09-12 17:16:08 +0000

    Fix bad tests setter_no_getter_test and syntax_test.
    Changed void test expectation to ok. Using void as such is now valid.
    
    Change-Id: I2ea0208a182eb0f20eb9e1b79c938df899f96319
    
    Change setter_no_getter_test to a static error test - compile-time error.
    
    Change-Id: I232e4460397f88e4f9806adf73622903fd76d000
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/116825
    Reviewed-by: Leaf Petersen <leafp@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

--- tests/language_2/setter_no_getter_test.dart
+++ tests/language_2/setter_no_getter_test.dart
@@ -2,13 +2,21 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "package:expect/expect.dart";
-
-/* //   //# 01: runtime error
-get topLevel => 42;
-*/ //  //# 01: continued
 set topLevel(var value) {}
 
+class Example {
+  set foo(var value) {}
+}
+
 main() {
-  Expect.equals(42, topLevel++); //# 01: continued
+  print(topLevel++);
+  //    ^
+  // [analyzer] unspecified
+  // [cfe] Getter not found: 'topLevel'.
+
+  Example ex = new Example();
+  print(ex.foo++);
+  //       ^
+  // [analyzer] unspecified
+  // [cfe] The getter 'foo' isn't defined for the class 'Example'.
 }
