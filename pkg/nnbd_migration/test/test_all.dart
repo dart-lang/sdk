605384dbe738416cc67431b100bb6d7e43955725
Paul Berry <paulberry@google.com>
2019-09-19 16:02:16 +0000

    Migration: Add `nullability_migration_impl_test.dart` to test_all
    Change-Id: I8fb3d642744bf3a7c7889e2cfa71733db6c489ba
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/118060
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Paul Berry <paulberry@google.com>

--- pkg/nnbd_migration/test/test_all.dart
+++ pkg/nnbd_migration/test/test_all.dart
@@ -14,6 +14,8 @@ import 'edge_builder_flow_analysis_test.dart'
 import 'edge_builder_test.dart' as edge_builder_test;
 import 'instrumentation_test.dart' as instrumentation_test;
 import 'node_builder_test.dart' as node_builder_test;
+import 'nullability_migration_impl_test.dart'
+    as nullability_migration_impl_test;
 import 'nullability_node_test.dart' as nullability_node_test;
 import 'utilities/test_all.dart' as utilities;
 
@@ -27,6 +29,7 @@ main() {
     edge_builder_test.main();
     instrumentation_test.main();
     node_builder_test.main();
+    nullability_migration_impl_test.main();
     nullability_node_test.main();
     utilities.main();
   });
