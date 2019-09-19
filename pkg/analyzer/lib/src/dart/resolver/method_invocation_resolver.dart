342b48bd27a3b50b13e7ee34ffe1fefc2f28f2c9
Sam Rawlins <srawlins@google.com>
2019-09-19 17:13:50 +0000

    Remove unused import
    Change-Id: I962a0a93abb57e4f84602442df7bce4444a8aec8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/118101
    Auto-Submit: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Paul Berry <paulberry@google.com>

--- pkg/analyzer/lib/src/dart/resolver/method_invocation_resolver.dart
+++ pkg/analyzer/lib/src/dart/resolver/method_invocation_resolver.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/src/dart/ast/ast.dart';
