143aa4bdb4ab1037826b05d3ccb7932b06404cd0
Aart Bik <ajcbik@google.com>
2019-10-04 20:38:57 +0000

    [vm/fuzzer] don't print oom on oom
    Rationale:
    exit code suffices, don't make it more likely
    that the oom handler itself goes oom
    
    https://github.com/dart-lang/sdk/issues/38657
    
    Change-Id: I3037c8e781da06d05b74eddd8b1d57ff9fb00f29
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/120301
    Reviewed-by: Felicitas Hetzelt <felih@google.com>
    Commit-Queue: Aart Bik <ajcbik@google.com>

--- runtime/tools/dartfuzz/dartfuzz.dart
+++ runtime/tools/dartfuzz/dartfuzz.dart
@@ -14,7 +14,7 @@ import 'dartfuzz_type_table.dart';
 // Version of DartFuzz. Increase this each time changes are made
 // to preserve the property that a given version of DartFuzz yields
 // the same fuzzed program for a deterministic random seed.
-const String version = '1.58';
+const String version = '1.59';
 
 // Restriction on statements and expressions.
 const int stmtDepth = 1;
@@ -485,7 +485,6 @@ class DartFuzz {
     indent -= 2;
     emitLn('} on OutOfMemoryError {');
     indent += 2;
-    emitLn("print(\'oom\');");
     emitLn("exit(${oomExitCode});");
     indent -= 2;
     emitLn('} catch (e, st) {');
