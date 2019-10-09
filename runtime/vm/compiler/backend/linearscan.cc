f5ce3a6e7f0d5fc4a16d681bd3cd9a062914c0d5
Ben Konyi <bkonyi@google.com>
2019-10-09 17:06:00 +0000

    [ VM / Compiler ] Fixed issue where TMP was being used as source for store with large offset on ARM
    While emitting code for a ParallelMove on ARM32, the TMP register was
    being used as a dest/src. When the stack slot offset is large,
    StoreToOffset creates a new base in TMP in order to use a smaller
    offset. Since the ParallelMove implementation was hardcoding TMP as the
    dest/src register, the assert that the register != TMP failed with large
    offsets. Now we instead perform a check to see if the offset will fit to
    determine whether or not we can use TMP or if we need to fall back and
    use LR as our temporary (we can do this since we're in the middle of a
    ParallelMove).
    
    Fixes https://github.com/dart-lang/sdk/issues/38654
    
    Change-Id: I4d8cc8b54f6090d86992aabb8ff44d088bc02d3e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/119584
    Reviewed-by: Ryan Macnak <rmacnak@google.com>
    Commit-Queue: Ben Konyi <bkonyi@google.com>

--- runtime/vm/compiler/backend/linearscan.cc
+++ runtime/vm/compiler/backend/linearscan.cc
@@ -1151,6 +1151,8 @@ void FlowGraphAllocator::ProcessOneInput(BlockEntryInstr* block,
       live_registers->Add(*in_ref, range->representation());
     }
     MoveOperands* move = AddMoveAt(pos - 1, *in_ref, Location::Any());
+    ASSERT(!in_ref->IsRegister() ||
+           ((1 << in_ref->reg()) & kDartAvailableCpuRegs) != 0);
     BlockLocation(*in_ref, pos - 1, pos + 1);
     range->AddUseInterval(block->start_pos(), pos - 1);
     range->AddHintedUse(pos - 1, move->src_slot(), in_ref);
@@ -1217,6 +1219,8 @@ void FlowGraphAllocator::ProcessOneOutput(BlockEntryInstr* block,
     //    register        [--)
     //    output             [-------
     //
+    ASSERT(!out->IsRegister() ||
+           ((1 << out->reg()) & kDartAvailableCpuRegs) != 0);
     BlockLocation(*out, pos, pos + 1);
 
     if (range->vreg() == kTempVirtualRegister) return;
@@ -1420,6 +1424,8 @@ void FlowGraphAllocator::ProcessOneInstruction(BlockEntryInstr* block,
     // We do not support pair locations for temporaries.
     ASSERT(!temp.IsPairLocation());
     if (temp.IsMachineRegister()) {
+      ASSERT(!temp.IsRegister() ||
+             ((1 << temp.reg()) & kDartAvailableCpuRegs) != 0);
       BlockLocation(temp, pos, pos + 1);
     } else if (temp.IsUnallocated()) {
       LiveRange* range = MakeLiveRangeForTemporary();
