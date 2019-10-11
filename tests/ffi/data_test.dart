e1c159d5fe9a060d4bc49a1ea69fc15771a54662
Samir Jindel <sjindel@google.com>
2019-10-11 15:52:37 +0000

    [vm/ffi] Add missing sharded library dependency to ffi/data_test.dart.
    Change-Id: I399882d0911b3e5de86b752048a22e96d2415e3a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/121420
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Reviewed-by: Samir Jindel <sjindel@google.com>

--- tests/ffi/data_test.dart
+++ tests/ffi/data_test.dart
@@ -3,6 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 //
 // Dart test program for testing dart:ffi primitive data pointers.
+//
+// SharedObjects=ffi_test_functions
 
 library FfiTest;
 
