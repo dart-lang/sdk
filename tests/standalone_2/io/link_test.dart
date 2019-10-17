49d743f940696fa46786775bc77aa782b61198f2
Zichang Guo <zichangguo@google.com>
2019-10-17 21:05:56 +0000

    [io] exclude links from isDirectory()
    isDirectory() should not return true for links.
    
    Bug: https://github.com/dart-lang/sdk/issues/35102
    Change-Id: I737345a581d6a50d7bccab0dbc10f1a31a57e219
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/121900
    Commit-Queue: Zichang Guo <zichangguo@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>

--- tests/standalone_2/io/link_test.dart
+++ tests/standalone_2/io/link_test.dart
@@ -275,10 +275,33 @@ testRelativeLinksSync() {
   tempDirectory.deleteSync(recursive: true);
 }
 
+testIsDir() async {
+  Directory sandbox = Directory.systemTemp.createTempSync();
+  Directory dir = new Directory(sandbox.path + Platform.pathSeparator + "dir");
+  dir.createSync();
+  File target = new File(sandbox.path + Platform.pathSeparator + "target");
+  target.createSync();
+
+  final eventCompleter = new Completer<FileSystemEvent>();
+  var subscription;
+  subscription = dir.watch().listen((FileSystemEvent event) {
+    eventCompleter.complete(event);
+    subscription.cancel();
+  });
+  Link link = new Link(dir.path + Platform.pathSeparator + "link");
+  link.createSync(target.path);
+  final event = await eventCompleter.future;
+  // Link should not be marked as Dir.
+  Expect.isFalse(event.isDirectory);
+
+  sandbox.deleteSync(recursive: true);
+}
+
 main() {
   testCreateSync();
   testCreateLoopingLink();
   testRenameSync();
   testLinkErrorSync();
   testRelativeLinksSync();
+  testIsDir();
 }
