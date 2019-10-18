09b08f7b0ec8d16ecf8ca99af23aaddef9cff7ac
Sam Rawlins <srawlins@google.com>
2019-10-18 13:22:48 +0000

    Add comment on non-negativity of periodic Timer and Stream.
    Fixes #30122
    
    Change-Id: Idde67193fb671aa7fb0d277f8ec81193421d8c21
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/112276
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- sdk/lib/async/timer.dart
+++ sdk/lib/async/timer.dart
@@ -67,6 +67,8 @@ abstract class Timer {
    * a `duration` after either when the previous callback ended,
    * when the previous callback started, or when the previous callback was
    * scheduled for - even if the actual callback was delayed.
+   *
+   * [duration] must a non-negative [Duration].
    */
   factory Timer.periodic(Duration duration, void callback(Timer timer)) {
     if (Zone.current == Zone.root) {
