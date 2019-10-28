c3e21581712bd17997b378ba05f2232cf5c069a6
Konstantin Shcheglov <scheglov@google.com>
2019-10-28 15:39:21 +0000

    Remove CompilationUnitElementImpl.annotationMap
    Change-Id: I4a53887c210b380ffdbd0b6bc0a9128781e8ad8d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/123063
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/element/element.dart
+++ pkg/analyzer/lib/src/dart/element/element.dart
@@ -1245,11 +1245,6 @@ class CompilationUnitElementImpl extends UriReferencedElementImpl
   /// computed.
   Source librarySource;
 
-  /// A table mapping the offset of a directive to the annotations associated
-  /// with that directive, or `null` if none of the annotations in the
-  /// compilation unit have annotations.
-  Map<int, List<ElementAnnotation>> annotationMap;
-
   /// A list containing all of the top-level accessors (getters and setters)
   /// contained in this compilation unit.
   List<PropertyAccessorElement> _accessors;
@@ -1605,16 +1600,6 @@ class CompilationUnitElementImpl extends UriReferencedElementImpl
     }
   }
 
-  /// Return the annotations associated with the directive at the given
-  /// [offset], or an empty list if the directive has no annotations or if
-  /// there is no directive at the given offset.
-  List<ElementAnnotation> getAnnotations(int offset) {
-    if (annotationMap == null) {
-      return const <ElementAnnotation>[];
-    }
-    return annotationMap[offset] ?? const <ElementAnnotation>[];
-  }
-
   @override
   ElementImpl getChild(String identifier) {
     //
@@ -1681,13 +1666,6 @@ class CompilationUnitElementImpl extends UriReferencedElementImpl
       TopLevelVariableElement from, TopLevelVariableElement to) {
     int index = _variables.indexOf(from);
     _variables[index] = to;
-  }
-
-  /// Set the annotations associated with the directive at the given [offset] to
-  /// the given list of [annotations].
-  void setAnnotations(int offset, List<ElementAnnotation> annotations) {
-    annotationMap ??= new HashMap<int, List<ElementAnnotation>>();
-    annotationMap[offset] = annotations;
   }
 
   @override
