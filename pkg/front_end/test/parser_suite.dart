5b7c981086141d367077c9b9a8efdb7e9fc5d784
Jens Johansen <jensj@google.com>
2019-10-28 10:25:30 +0000

    [CFE] Parser test framework also outputs token stream
    This makes it much easier to see what the parser is actually working
    with in the face of token stream rewrites.
    
    Change-Id: Ic336f18fab7c8fd7a42020b32af4a480bbde25bc
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/122865
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/test/parser_suite.dart
+++ pkg/front_end/test/parser_suite.dart
@@ -68,6 +68,8 @@ class Context extends ChainContext with MatchContext {
   Context(this.updateExpectations, this.addTrace);
 
   final List<Step> steps = const <Step>[
+    const TokenStep(true, ".scanner.expect"),
+    const TokenStep(false, ".parser.expect"),
     const ListenerStep(),
     const IntertwinedStep(),
   ];
@@ -82,19 +84,11 @@ class ListenerStep extends Step<TestDescription, TestDescription, Context> {
   String get name => "listener";
 
   Future<Result<TestDescription>> run(
-      TestDescription description, Context context) async {
-    File f = new File.fromUri(description.uri);
-    List<int> rawBytes = f.readAsBytesSync();
-
-    Uint8List bytes = new Uint8List(rawBytes.length + 1);
-    bytes.setRange(0, rawBytes.length, rawBytes);
-
-    Utf8BytesScanner scanner = new Utf8BytesScanner(bytes,
-        includeComments: true, configuration: scannerConfiguration);
-    Token firstToken = scanner.tokenize();
+      TestDescription description, Context context) {
+    Token firstToken = scanUri(description.uri);
 
     if (firstToken == null) {
-      return crash(description, StackTrace.current);
+      return Future.value(crash(description, StackTrace.current));
     }
 
     ParserTestListener parserTestListener =
@@ -113,19 +107,11 @@ class IntertwinedStep extends Step<TestDescription, TestDescription, Context> {
   String get name => "intertwined";
 
   Future<Result<TestDescription>> run(
-      TestDescription description, Context context) async {
-    File f = new File.fromUri(description.uri);
-    List<int> rawBytes = f.readAsBytesSync();
-
-    Uint8List bytes = new Uint8List(rawBytes.length + 1);
-    bytes.setRange(0, rawBytes.length, rawBytes);
-
-    Utf8BytesScanner scanner = new Utf8BytesScanner(bytes,
-        includeComments: true, configuration: scannerConfiguration);
-    Token firstToken = scanner.tokenize();
+      TestDescription description, Context context) {
+    Token firstToken = scanUri(description.uri);
 
     if (firstToken == null) {
-      return crash(description, StackTrace.current);
+      return Future.value(crash(description, StackTrace.current));
     }
 
     ParserTestListener2 parserTestListener =
@@ -138,6 +124,128 @@ class IntertwinedStep extends Step<TestDescription, TestDescription, Context> {
     return context.match<TestDescription>(
         ".intertwined.expect", "${parser.sb}", description.uri, description);
   }
+}
+
+class TokenStep extends Step<TestDescription, TestDescription, Context> {
+  final bool onlyScanner;
+  final String suffix;
+
+  const TokenStep(this.onlyScanner, this.suffix);
+
+  String get name => "token";
+
+  Future<Result<TestDescription>> run(
+      TestDescription description, Context context) {
+    List<int> lineStarts = new List<int>();
+    Token firstToken = scanUri(description.uri, lineStarts: lineStarts);
+
+    if (firstToken == null) {
+      return Future.value(crash(description, StackTrace.current));
+    }
+
+    StringBuffer beforeParser = tokenStreamToString(firstToken, lineStarts);
+    StringBuffer beforeParserWithTypes =
+        tokenStreamToString(firstToken, lineStarts, addTypes: true);
+    if (onlyScanner) {
+      return context.match<TestDescription>(
+          suffix,
+          "${beforeParser}\n\n${beforeParserWithTypes}",
+          description.uri,
+          description);
+    }
+
+    ParserTestListener parserTestListener =
+        new ParserTestListener(context.addTrace);
+    Parser parser = new Parser(parserTestListener);
+    bool parserCrashed = false;
+    dynamic parserCrashedE;
+    StackTrace parserCrashedSt;
+    try {
+      parser.parseUnit(firstToken);
+    } catch (e, st) {
+      parserCrashed = true;
+      parserCrashedE = e;
+      parserCrashedSt = st;
+    }
+
+    StringBuffer afterParser = tokenStreamToString(firstToken, lineStarts);
+    StringBuffer afterParserWithTypes =
+        tokenStreamToString(firstToken, lineStarts, addTypes: true);
+
+    bool rewritten = beforeParser.toString() != afterParser.toString();
+    String rewrittenString =
+        rewritten ? "NOTICE: Stream was rewritten by parser!\n\n" : "";
+
+    Future<Result<TestDescription>> result = context.match<TestDescription>(
+        suffix,
+        "${rewrittenString}${afterParser}\n\n${afterParserWithTypes}",
+        description.uri,
+        description);
+    return result.then((result) {
+      if (parserCrashed) {
+        return crash("Parser crashed: $parserCrashedE", parserCrashedSt);
+      } else {
+        return result;
+      }
+    });
+  }
+
+  StringBuffer tokenStreamToString(Token firstToken, List<int> lineStarts,
+      {bool addTypes: false}) {
+    StringBuffer sb = new StringBuffer();
+    Token token = firstToken;
+    bool printed = false;
+    int endOfLast = -1;
+    int lineStartsIteratorLine = 1;
+    Iterator<int> lineStartsIterator = lineStarts.iterator;
+    lineStartsIterator.moveNext();
+    lineStartsIterator.moveNext();
+    lineStartsIteratorLine++;
+    while (token != null) {
+      int prevLine = lineStartsIteratorLine;
+      while (token.offset >= lineStartsIterator.current &&
+          lineStartsIterator.moveNext()) {
+        lineStartsIteratorLine++;
+      }
+      if (printed &&
+          (token.offset > endOfLast || prevLine < lineStartsIteratorLine)) {
+        if (prevLine < lineStartsIteratorLine) {
+          for (int i = prevLine; i < lineStartsIteratorLine; i++) {
+            sb.write("\n");
+          }
+        } else {
+          sb.write(" ");
+        }
+      }
+      if (token is! ErrorToken) {
+        sb.write(token.lexeme);
+      }
+      if (addTypes) {
+        sb.write("[${token.runtimeType}]");
+      }
+      printed = true;
+      endOfLast = token.end;
+      if (token == token.next) break;
+      token = token.next;
+    }
+    return sb;
+  }
+}
+
+Token scanUri(Uri uri, {List<int> lineStarts}) {
+  File f = new File.fromUri(uri);
+  List<int> rawBytes = f.readAsBytesSync();
+
+  Uint8List bytes = new Uint8List(rawBytes.length + 1);
+  bytes.setRange(0, rawBytes.length, rawBytes);
+
+  Utf8BytesScanner scanner = new Utf8BytesScanner(bytes,
+      includeComments: true, configuration: scannerConfiguration);
+  Token firstToken = scanner.tokenize();
+  if (lineStarts != null) {
+    lineStarts.addAll(scanner.lineStarts);
+  }
+  return firstToken;
 }
 
 class ParserTestListener2 extends ParserTestListener {
