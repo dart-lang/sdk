034ae89434f2d6e4b79eba2ecf759c916f3ac98d
pq <pquitslund@gmail.com>
2019-10-29 11:09:48 -0700

    tweaks

--- lib/src/rules/avoid_web_libraries_in_flutter.dart
+++ lib/src/rules/avoid_web_libraries_in_flutter.dart
@@ -15,8 +15,8 @@ const _desc =
 
 const _details = r'''Avoid using web libraries, `dart:html`, `dart:js` and 
 `dart:js_util` in Flutter packages that are not web plugins. These libraries are 
-not supported outside a web context and functionality that depends on them will
-fail at runtime in Flutter mobile and their use is generally discouraged in
+not supported outside a web context; functionality that depends on them will
+fail at runtime in Flutter mobile, and their use is generally discouraged in
 Flutter web.
 
 Web library access *is* allowed in:
