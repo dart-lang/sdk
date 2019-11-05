619ecbd1948743995f6e25261c3b29bfbee3e67b
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-11-05 16:54:44 -0700

    Modify typedef for catchError callback function

--- sdk/lib/async/future.dart
+++ sdk/lib/async/future.dart
@@ -635,7 +635,8 @@ abstract class Future<T> {
   // - (dynamic, StackTrace) -> FutureOr<T>
   // Given that there is a `test` function that is usually used to do an
   // `isCheck` we should also expect functions that take a specific argument.
-  Future<T> catchError(Function onError, {bool test(Object error)});
+  Future<T> catchError(void onError(Object error, StackTrace stackTrace),
+      {bool test(Object error)});
 
   /**
    * Registers a function to be called when this future completes.
