23770fcc6c1f2ea5a746554aed1d3031f580595e
Ryan Macnak <rmacnak@google.com>
2019-11-07 22:45:47 +0000

    [vm, gc] Increase HeapPage size to 512k; increase max heap size to 32GB.
    Change-Id: I0d5649d37a73caea39d3f703f429a4af7b4c29c5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/113950
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Vyacheslav Egorov <vegorov@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>

--- runtime/vm/globals.h
+++ runtime/vm/globals.h
@@ -37,10 +37,11 @@ const intptr_t kBytesPerBigIntDigit = 4;
 
 // The default old gen heap size in MB, where 0 == unlimited.
 // 32-bit: OS limit is 2 or 3 GB
-// 64-bit: OS limit is 2^16 page table entries * 256 KB HeapPages = 16 GB
+// 64-bit: Linux's limit is
+//   sysctl vm.max_map_count (default 2^16) * 512 KB HeapPages = 32 GB
 // Set the VM limit below the OS limit to increase the likelihood of failing
 // gracefully with a Dart OutOfMemory exception instead of SIGABORT.
-const intptr_t kDefaultMaxOldGenHeapSize = (kWordSize <= 4) ? 1536 : 15360;
+const intptr_t kDefaultMaxOldGenHeapSize = (kWordSize <= 4) ? 1536 : 30720;
 
 #define kPosInfinity bit_cast<double>(DART_UINT64_C(0x7ff0000000000000))
 #define kNegInfinity bit_cast<double>(DART_UINT64_C(0xfff0000000000000))
