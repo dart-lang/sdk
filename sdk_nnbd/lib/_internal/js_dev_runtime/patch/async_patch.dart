ed956daa3f75199a7df19494db2b1edf16e3a9fc
Robert Nystrom <rnystrom@google.com>
2019-11-09 00:57:18 +0000

    Update the DDC dart:async patch file version to match the migrated lib.
    Change-Id: Ia15e68e2124fe0af5ebcb36529cef943b3406362
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/124642
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>

--- sdk_nnbd/lib/_internal/js_dev_runtime/patch/async_patch.dart
+++ sdk_nnbd/lib/_internal/js_dev_runtime/patch/async_patch.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.5
-
 // Patch file for the dart:async library.
 
 import 'dart:_js_helper' show notNull, patch, ReifyFunctionTypes;
