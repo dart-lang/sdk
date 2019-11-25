629d4ce0950629cd3f4f5eb6f7bac918dadae6bc
Liam Appelbe <liama@google.com>
2019-11-25 17:21:15 +0000

    [vm] Test for late fields with complicated initializers
    This exposed a bug in late_var_init_transformer, where statements that
    came before the late var declaration weren't copied over. So I rewrote
    it a lot simpler.
    
    Bug: https://github.com/dart-lang/sdk/issues/38841
    Change-Id: I7f75448179f737fa1e3b90aefd9ea15a2c4be5d8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/126085
    Reviewed-by: Alexander Markov <alexmarkov@google.com>
    Commit-Queue: Liam Appelbe <liama@google.com>

--- tests/language_2/nnbd/syntax/late_modifier_edge_cases_test.dart
+++ tests/language_2/nnbd/syntax/late_modifier_edge_cases_test.dart
@@ -42,13 +42,105 @@ class A {
 lateFieldWithInitThatWritesIntermediateValue() {
   A a = A();
   Expect.equals(123, a.lateField);
-  Expect.throws(() => print(a.lateFinalField));
+  Expect.throws(() => a.lateFinalField);
   Expect.equals(5, a.lateFinalField);
   Expect.equals(123, A.staticLateField);
-  Expect.throws(() => print(A.staticLateFinalField));
+  Expect.throws(() => A.staticLateFinalField);
   Expect.equals(5, A.staticLateFinalField);
+}
+
+lateFieldWithComplicatedInitializers() {
+  late int count = 0;
+  int closure() {
+    ++count;
+    return 5;
+  }
+
+  late int nestedLateVar = closure();
+  late var lateVar = [for (int i = 0; i < nestedLateVar; ++i) i * i];
+  Expect.equals(0, count);
+  Expect.listEquals([0, 1, 4, 9, 16], lateVar);
+  Expect.equals(1, count);
+  Expect.listEquals([0, 1, 4, 9, 16], lateVar);
+  Expect.equals(1, count);
+  count = 0;
+
+  late var lateVarInIf = [++count, for (int i = 0; i < 3; ++i) i * i];
+  Expect.equals(0, count);
+  if (true) Expect.listEquals([1, 0, 1, 4], lateVarInIf);
+  Expect.equals(1, count);
+  Expect.listEquals([1, 0, 1, 4], lateVarInIf);
+  Expect.equals(1, count);
+  count = 0;
+
+  late var lateVarInFor = [++count, for (int i = 0; i < 3; ++i) i * i];
+  Expect.equals(0, count);
+  for (int i = 0; i < 3; ++i) Expect.listEquals([1, 0, 1, 4], lateVarInFor);
+  Expect.equals(1, count);
+  count = 0;
+
+  late var lateVarInClosure = [++count, for (int i = 0; i < 3; ++i) i * i];
+  void anotherClosure() {
+    Expect.listEquals([1, 0, 1, 4], lateVarInClosure);
+  }
+
+  Expect.equals(0, count);
+  anotherClosure();
+  anotherClosure();
+  anotherClosure();
+  Expect.equals(1, count);
+  count = 0;
+
+  late int lateVarClosureOuter = () {
+    Expect.equals(0, count);
+    ++count;
+    late int lateVarClosureInner = closure();
+    Expect.equals(1, count);
+    Expect.equals(5, lateVarClosureInner);
+    Expect.equals(2, count);
+    return lateVarClosureInner;
+  }();
+  Expect.equals(0, count);
+  Expect.equals(5, lateVarClosureOuter);
+  Expect.equals(2, count);
+}
+
+class B {
+  late int throwBeforeWrite = initThrowBeforeWrite();
+  int initThrowBeforeWrite() {
+    throw AssertionError();
+    return 123;
+  }
+
+  late int throwAfterWrite = initThrowAfterWrite();
+  int initThrowAfterWrite() {
+    throwAfterWrite = 456;
+    throw AssertionError();
+    return 123;
+  }
+}
+
+lateFieldWithThrowingInitializers() {
+  late int throwBeforeWrite = () {
+    throw AssertionError();
+    return 123;
+  }();
+  Expect.throwsAssertionError(() => throwBeforeWrite);
+  Expect.throwsAssertionError(() => throwBeforeWrite);
+  Expect.throwsAssertionError(() => throwBeforeWrite);
+
+  B b = B();
+  Expect.throwsAssertionError(() => b.throwBeforeWrite);
+  Expect.throwsAssertionError(() => b.throwBeforeWrite);
+  Expect.throwsAssertionError(() => b.throwBeforeWrite);
+
+  Expect.throwsAssertionError(() => b.throwAfterWrite);
+  Expect.equals(456, b.throwAfterWrite);
+  Expect.equals(456, b.throwAfterWrite);
 }
 
 main() {
   lateFieldWithInitThatWritesIntermediateValue();
+  lateFieldWithComplicatedInitializers();
+  lateFieldWithThrowingInitializers();
 }
