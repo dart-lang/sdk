1d4c45d2d0ca9e43958d7707390c328768fbe353
Samir Jindel <sjindel@google.com>
2019-12-05 14:06:08 +0000

    Re-land "[vm/cfe] Elaborate for-in statements during async transform"
    This removes the component of the change which updates the kernel binary
    format.
    
    Change-Id: I86dbfe0ea61453bbea6eee200743961d0d0c3814
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/127002
    Commit-Queue: Samir Jindel <sjindel@google.com>
    Reviewed-by: Teagan Strickland <sstrickl@google.com>

--- pkg/kernel/lib/core_types.dart
+++ pkg/kernel/lib/core_types.dart
@@ -65,6 +65,9 @@ class CoreTypes {
   Constructor _fallThroughErrorUrlAndLineConstructor;
   Procedure _objectEquals;
   Procedure _mapUnmodifiable;
+  Procedure _iterableGetIterator;
+  Procedure _iteratorMoveNext;
+  Procedure _iteratorGetCurrent;
 
   Class _internalSymbolClass;
 
@@ -323,8 +326,23 @@ class CoreTypes {
     return _iterableClass ??= index.getClass('dart:core', 'Iterable');
   }
 
+  Procedure get iterableGetIterator {
+    return _iterableGetIterator ??=
+        index.getMember('dart:core', 'Iterable', 'get:iterator');
+  }
+
   Class get iteratorClass {
     return _iteratorClass ??= index.getClass('dart:core', 'Iterator');
+  }
+
+  Procedure get iteratorMoveNext {
+    return _iteratorMoveNext ??=
+        index.getMember('dart:core', 'Iterator', 'moveNext');
+  }
+
+  Procedure get iteratorGetCurrent {
+    return _iteratorGetCurrent ??=
+        index.getMember('dart:core', 'Iterator', 'get:current');
   }
 
   Class get listClass {
