68f6e147305c2fb4b37c8b95a67abf83692badea
William Hesse <whesse@google.com>
2019-12-16 11:18:27 +0000

    [infra] Build status script now queries Firestore, using access token
    Change-Id: I417a65cd8dd0046e78b9c4dac1079e086e3547b9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/128408
    Commit-Queue: William Hesse <whesse@google.com>
    Reviewed-by: Alexander Thomas <athom@google.com>

--- tools/bots/get_builder_status.dart
+++ tools/bots/get_builder_status.dart
@@ -12,31 +12,115 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart';
+import 'package:args/args.dart';
+import 'package:http/http.dart' as http;
 
-const firebaseUrl =
-    'https://firestore.googleapis.com/v1/projects/dart-ci/databases/(default)/documents';
+const numAttempts = 20;
 
-void main(List<String> args) async {
-  final builder = args[0];
-  final buildNumber = args[1];
-  final client = Client();
-  return; // Temporarily make script return immediately, while we
-  // land recipe change that calls it.
-  for (int count = 0; count < 30; ++count) {
-    // TODO(whesse): Add code to fetch from "try_builds" for builders that
-    // end with "-try".
-    final response =
-        await client.get('$firebaseUrl/build_status/$builder:$buildNumber');
-    if (response.statusCode == HttpStatus.ok) {
-      final document = jsonDecode(response.body);
-      final status = (document['fields']['status'] ?? const {})['stringValue'];
-      if (status == 'success') exit(0);
-      if (status == 'failed') exit(1);
-    }
-    await Future.delayed(Duration(seconds: 10));
+void usage(ArgParser parser) {
+  print('''
+Usage: get_builder_status.dart [OPTIONS]
+Gets the builder status from the Firestore database.
+Polls until it gets a completed builder status, or times out.
+
+The options are as follows:
+
+${parser.usage}''');
+  exit(1);
+}
+
+main(List<String> args) async {
+  final parser = new ArgParser();
+  parser.addFlag('help', help: 'Show the program usage.', negatable: false);
+  parser.addOption('auth_token',
+      abbr: 'a', help: 'Authorization token with cloud-platform scope');
+  parser.addOption('builder', abbr: 'b', help: 'The builder name');
+  parser.addOption('build_number', abbr: 'n', help: 'The build number');
+
+  final options = parser.parse(args);
+  if (options['help']) {
+    usage(parser);
   }
-  print(
-      'No status received for $builder:$buildNumber after 30 attempts, with 10 second waits.');
+
+  const postUrl = 'https://firestore.googleapis.com/v1/'
+      'projects/dart-ci/databases/(default)/documents:runQuery';
+
+  final builder = options['builder'];
+  final buildNumber = options['build_number'];
+  final table = builder.endsWith('-try') ? 'try_builds' : 'builds';
+  final token = await File(options['auth_token']).readAsString();
+  final client = http.Client();
+  final query = {
+    "from": [
+      {"collectionId": table}
+    ],
+    "limit": 1,
+    "where": {
+      "compositeFilter": {
+        "op": "AND",
+        "filters": [
+          {
+            "fieldFilter": {
+              "field": {"fieldPath": "build_number"},
+              "op": "EQUAL",
+              "value": {"integerValue": buildNumber}
+            }
+          },
+          {
+            "fieldFilter": {
+              "field": {"fieldPath": "builder"},
+              "op": "EQUAL",
+              "value": {"stringValue": builder}
+            }
+          }
+        ]
+      }
+    }
+  };
+
+  final json = jsonEncode({"structuredQuery": query});
+  final headers = {
+    'Authorization': 'Bearer $token',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+  for (int count = 0; count < numAttempts; ++count) {
+    if (count > 0) {
+      await Future.delayed(Duration(seconds: 10));
+    }
+    final response = await client.post(postUrl, headers: headers, body: json);
+    if (response.statusCode == HttpStatus.ok) {
+      final documents = jsonDecode(response.body);
+      final document = documents.first['document'];
+      if (document != null) {
+        bool success =
+            (document['fields']['success'] ?? const {})['booleanValue'];
+        bool completed =
+            (document['fields']['completed'] ?? const {})['booleanValue'];
+        if (completed) {
+          exit(success ? 0 : 1);
+        }
+        String chunks =
+            (document['fields']['num_chunks'] ?? const {})['integerValue'];
+        String processedChunks = (document['fields']['processed_chunks'] ??
+            const {})['integerValue'];
+        if (processedChunks != null) {
+          print([
+            'Received',
+            processedChunks,
+            if (chunks != null) ...['out of', chunks],
+            'chunks.'
+          ].join(' '));
+        }
+      } else {
+        print("No results recieved for build $buildNumber of $builder");
+      }
+    } else {
+      print("HTTP status ${response.statuscCode} received "
+          "when fetching build data");
+    }
+  }
+  print('No status received for build $buildNumber of $builder '
+      'after $numAttempts attempts, with 10 second waits.');
   exit(2);
 }
