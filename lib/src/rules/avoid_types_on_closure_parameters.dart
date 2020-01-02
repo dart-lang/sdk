ff1c9c7719cd9fa3abc25c657b487b5801aa0953
Phil Quitslund <pq@users.noreply.github.com>
2020-01-02 11:52:00 -0800

    doc typo (dart-lang/linter#1922)

--- lib/src/rules/avoid_types_on_closure_parameters.dart
+++ lib/src/rules/avoid_types_on_closure_parameters.dart
@@ -11,7 +11,7 @@ const _desc = r'Avoid annotating types for function expression parameters.';
 
 const _details = r'''
 
-**AVOID** annotating types for function expression paremeters.
+**AVOID** annotating types for function expression parameters.
 
 Annotating types for function expression parameters is usually unnecessary
 because the parameter types can almost always be inferred from the context,
