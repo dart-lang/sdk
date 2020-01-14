12310ebcc062200aa6d346424aee5bc59790df1d
Leaf Petersen <leafp@google.com>
2020-01-14 19:06:06 +0000

    Convert late field syntax test to static error test
    Change-Id: I725cec70cb2a93d2fb2c92b5cb8d7efb156bae94
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/131040
    Reviewed-by: Erik Ernst <eernst@google.com>

--- tests/language/nnbd/syntax/class_member_declarations_test.dart
+++ tests/language/nnbd/syntax/class_member_declarations_test.dart
@@ -5,56 +5,25 @@
 // SharedOptions=--enable-experiment=non-nullable
 
 class A {
-  static late x1; //# 01: syntax error
   static late final x2;
   static late int x3;
   static late final A x4;
-  static late x5 = 0; //# 02: syntax error
   static late final x6 = 0;
   static late int x7 = 0;
   static late final A? x8 = null;
 
-  static final late x9; //# 03: syntax error
-  static final late A x10; //# 04: syntax error
-  static final late x11 = 0; //# 05: syntax error
-  static final late A x12 = null; //# 06: syntax error
-
   covariant late var x13;
   covariant late var x14 = '';
-  covariant late x15; //# 07: syntax error
-  covariant late x16 = ''; //# 08: syntax error
-
-  late covariant var x17; //# 09: syntax error
-  late covariant var x18 = '';  //# 10: syntax error
-  late covariant x19; //# 11: syntax error
-  late covariant x20 = ''; //# 12: syntax error
-
-  covariant var late x21; //# 13: syntax error
-  covariant var late x22 = '';  //# 14: syntax error
 
   covariant late double x23;
   covariant late String x24 = '';
 
-  covariant double late x23; //# 15: syntax error
-  covariant String late x24 = ''; //# 16: syntax error
-
-  late x25; //# 17: syntax error
   late final x26;
   late int x27;
   late final A x28;
-  late x29 = 0; //# 18: syntax error
   late final x30 = 0;
   late int x31 = 0;
   late final A? x32 = null;
-
-  final late x33; //# 19: syntax error
-  int late x34; //# 20: syntax error
-  var late x35; //# 21: syntax error
-  final late A x36; //# 22: syntax error
-  final late x37 = 0; //# 23: syntax error
-  int late x38 = 0; //# 24: syntax error
-  var late x39 = 0; //# 25: syntax error
-  final late A? x40 = null; //# 26: syntax error
 
   List foo() {
     final x41 = true;
@@ -65,16 +34,20 @@ class A {
 }
 
 abstract class B {
-  m1(int some, regular, covariant parameters, {
-      required p1,
-      // required p2 = null, // Likely intended to be an error.
-      required covariant p3,
-      required covariant int p4,
+  m1(
+    int some,
+    regular,
+    covariant parameters, {
+    required p1,
+    required covariant p3,
+    required covariant int p4,
   });
 }
 
 main() {
   A? a;
   String? s = '';
-  a?..foo().length..x27 = s!.toString().length;
+  a
+    ?..foo().length
+    ..x27 = s!.toString().length;
 }
