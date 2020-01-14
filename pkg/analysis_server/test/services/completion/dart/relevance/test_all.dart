82785b67628428bf3162ba55f775d4f0f6588779
Jaime Wren <jwren@google.com>
2020-01-14 21:11:46 +0000

    This is a failing test for http://dartbug.com/37907 using the new DartCompletionManagerTest
    Change-Id: Id852d5f2894f5e0ba12127a04fad2f9c91593cd7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/131702
    Commit-Queue: Jaime Wren <jwren@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/services/completion/dart/relevance/test_all.dart
+++ pkg/analysis_server/test/services/completion/dart/relevance/test_all.dart
@@ -4,10 +4,12 @@
 
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
+import 'bool_assignment_relevance_test.dart' as bool_assignment;
 import 'deprecated_member_relevance_test.dart' as deprecated_member;
 
 main() {
   defineReflectiveSuite(() {
+    bool_assignment.main();
     deprecated_member.main();
   });
 }
