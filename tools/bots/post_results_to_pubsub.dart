5c67dd71a1f6014b43697d93bcc4e43a57252172
William Hesse <whesse@google.com>
2020-01-21 18:48:17 +0000

    [infra] Send only 100 changed test results to Firestore at once
    Fixes: https://github.com/dart-lang/dart_ci/issues/46
    Change-Id: Ie6870c33db15f0e3d8a239dcca39af951ebf03aa
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/132665
    Commit-Queue: William Hesse <whesse@google.com>
    Reviewed-by: Alexander Thomas <athom@google.com>

--- tools/bots/post_results_to_pubsub.dart
+++ tools/bots/post_results_to_pubsub.dart
@@ -2,7 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Post results from Dart continuous integration testers to Cloud Pub/Sub.
+// Post results from Dart tryjobs and CI builders to Cloud Pub/Sub.
+//
+// Reads a results.json input file, sends only the changed results from
+// that file to the Pub/Sub channel 'results' in the 'dart-ci' project.
+// Multiple messages are sent if there are more than 100 changed results,
+// so the cloud function only needs to process 100 records within its time
+// limit of 60s. Because of this, we never approach the limit of 10 MB
+// base64-encoded data bytes per message.
+//
+// The function also scans results.json to find the configurations tested
+// by this builder, and writes them and a results feed URL filtered to
+// those configuration groups to a json file, used later in the recipe.
 
 import 'dart:convert';
 import 'dart:io';
@@ -13,7 +24,7 @@ import 'package:http/http.dart' as http;
 void usage(ArgParser parser) {
   print('''
 Usage: post_results_to_pubsub.dart [OPTIONS]
-Posts Dart CI results as messages to Google cloud pubsub
+Posts Dart CI results as messages to Google Cloud Pub/Sub
 
 The options are as follows:
 
@@ -21,12 +32,7 @@ ${parser.usage}''');
   exit(1);
 }
 
-// Pubsub messages must be < 10MB long.  Because the JSON we send is
-// Base64 encoded, and we add a final record after checking the size,
-// the limit must be less than 3/4 of 10MB.
-// Because we have some Firestore scalability issues when thousands of new
-// tests are added, we currently use less than half of the maximum size.
-const messageLengthLimit = 3000000;
+const resultsPerMessage = 100;
 const postUrl =
     'https://pubsub.googleapis.com/v1/projects/dart-ci/topics/results:publish';
 
@@ -50,39 +56,77 @@ main(List<String> args) async {
   final lines = await File(options['result_file']).readAsLines();
   final token = await File(options['auth_token']).readAsString();
   final buildbucketID = options['id'];
-  // Construct pubsub messages.
-  var line = 0;
-  var numMessages = 0;
-  while (line < lines.length) {
-    final message = StringBuffer();
-    message.write('[');
-    message.write(lines[line++]);
-    var messageLines = 1;
-    while (message.length < messageLengthLimit && line < lines.length) {
-      message.write(',\n');
-      message.write(lines[line++]);
-      messageLines++;
-    }
-    message.write(']');
-    numMessages++;
+  if (lines.isEmpty) {
+    print('No results in input file');
+    return;
+  }
+  // Record the configurations appearing in these results.
+  final configurationRegexp = RegExp('"configuration":"([^"]*)"');
+  final configurationsSet = {
+    for (final result in lines) configurationRegexp.firstMatch(result)?.group(1)
+  };
+  if (configurationsSet.contains(null)) {
+    print("Error: result without a configuration");
+    configurationsSet.remove(null);
+  }
+  final configurations = configurationsSet.toList()..sort();
+  final configurationGroups = {
+    for (final configuration in configurations) configuration.split('-').first
+  };
+  // Construct a link to the results feed that includes a filter showing
+  // unapproved failures for those configuration groups.
+  String fragment = [
+    'showLatestFailures=true',
+    'showUnapprovedOnly=true',
+    if (configurationGroups.isNotEmpty)
+      'configurationGroups=${configurationGroups.join(',')}'
+  ].join('&');
+  final resultsFeedUrl = 'https://dart-ci.firebaseapp.com/#$fragment';
+  // Save them to a file.
+  await File('builder_tested_configurations')
+      .writeAsString(JsonEncoder.withIndent('  ').convert({
+    'configurations': configurations,
+    'results_feed_link': resultsFeedUrl,
+  }));
+
+  final changedPattern = '"changed":true';
+  List<String> changedResults =
+      lines.where((change) => change.contains(changedPattern)).toList();
+  // We need to send at least one result, to save build metadata to Firestore.
+  // Send an unchanged result - the cloud function filters these out.
+  if (changedResults.isEmpty) changedResults = lines.sublist(0, 1);
+
+  final chunks = <List<String>>[];
+  final chunkLength = resultsPerMessage;
+  var position = 0;
+  final lastFullChunkStart = changedResults.length - resultsPerMessage;
+  while (position <= lastFullChunkStart) {
+    chunks.add(changedResults.sublist(position, position += resultsPerMessage));
+  }
+  if (position < changedResults.length)
+    chunks.add(changedResults.sublist(position));
+
+  // Send pubsub messages.
+  for (final chunk in chunks) {
+    final message = '[\n${chunk.join(",\n")}\n]';
+    final base64data = base64Encode(utf8.encode(message.toString()));
     final attributes = {
-      if (line == lines.length) 'num_chunks': numMessages.toString(),
+      if (chunk == chunks.last) 'num_chunks': chunks.length.toString(),
       if (buildbucketID != null) 'buildbucket_id': buildbucketID
     };
-    var base64data = base64Encode(utf8.encode(message.toString()));
-    var messageObject = {
+    final jsonMessage = jsonEncode({
       'messages': [
         {'attributes': attributes, 'data': base64data}
       ]
-    };
-    var jsonMessage = jsonEncode(messageObject);
-    var headers = {'Authorization': 'Bearer $token'};
-    var response =
+    });
+    final headers = {'Authorization': 'Bearer $token'};
+    final response =
         await client.post(postUrl, headers: headers, body: jsonMessage);
-    print('Sent pubsub message containing ${messageLines} results');
+
+    print('Sent pubsub message containing ${chunk.length} results');
     print('Status ${response.statusCode}');
     print('Response: ${response.body}');
   }
-  print('Number of Pub/Sub messages sent: $numMessages');
+  print('Number of Pub/Sub messages sent: ${chunks.length}');
   client.close();
 }
