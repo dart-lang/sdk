c352c6c0b8258b58591d89d2cbd1a03d613c7379
Jaime Wren <jwren@google.com>
2020-01-27 19:41:19 +0000

    Cleanup and refactoring in completion_metrics.dart
    Change-Id: I46dd8b9f05d44dcb1d3d2f1088b95845427005c2
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/133428
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Jaime Wren <jwren@google.com>

--- pkg/analysis_server/tool/completion_metrics/completion_metrics.dart
+++ pkg/analysis_server/tool/completion_metrics/completion_metrics.dart
@@ -14,7 +14,6 @@ import 'package:analysis_server/src/services/completion/dart/utilities.dart';
 import 'package:analysis_server/src/status/pages.dart';
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/src/dart/analysis/byte_store.dart';
 import 'package:analyzer/src/generated/engine.dart';
@@ -23,41 +22,47 @@ import 'package:analyzer/src/services/available_declarations.dart';
 import 'metrics_util.dart';
 import 'visitors.dart';
 
+// TODO(jwren) have the analysis root and verbose option be configurable via a
+//  command line UX
 main() async {
-  var analysisRoots = [''];
-  await _computeCompletionMetrics(
-      PhysicalResourceProvider.INSTANCE, analysisRoots);
+  await CompletionMetricsComputer('', true).computeCompletionMetrics();
 }
 
-/// When enabled, expected, but missing completion tokens will be printed to
-/// stdout.
-const bool _doPrintMissingCompletions = true;
+/// This is the main metrics computer class for code completions. After the
+/// object is constructed, [computeCompletionMetrics] is executed to do analysis
+/// and print a summary of the metrics gathered from the completion tests.
+class CompletionMetricsComputer {
+  /// The analysis root path that this CompletionMetrics class will be computed.
+  final String _rootPath;
 
-/// TODO(jwren) put the following methods into a class
-Future _computeCompletionMetrics(
-    ResourceProvider resourceProvider, List<String> analysisRoots) async {
-  int includedCount = 0;
-  int notIncludedCount = 0;
-  Counter completionKindCounter = Counter('completion kind counter');
-  Counter completionElementKindCounter =
-      Counter('completion element kind counter');
+  /// When enabled, expected, but missing completion tokens will be printed to
+  /// stdout.
+  final bool _verbose;
 
-  for (var root in analysisRoots) {
-    print('Analyzing root: \"$root\"');
+  CompletionMetricsComputer(this._rootPath, this._verbose);
 
-    if (!io.Directory(root).existsSync()) {
+  Future computeCompletionMetrics() async {
+    int includedCount = 0;
+    int notIncludedCount = 0;
+    var completionKindCounter = Counter('completion kind counter');
+    var completionElementKindCounter =
+        Counter('completion element kind counter');
+
+    print('Analyzing root: \"$_rootPath\"');
+
+    if (!io.Directory(_rootPath).existsSync()) {
       print('\tError: No such directory exists on this machine.\n');
-      continue;
+      return;
     }
 
     final collection = AnalysisContextCollection(
-      includedPaths: [root],
-      resourceProvider: resourceProvider,
+      includedPaths: [_rootPath],
+      resourceProvider: PhysicalResourceProvider.INSTANCE,
     );
 
     for (var context in collection.contexts) {
-      var declarationsTracker =
-          DeclarationsTracker(MemoryByteStore(), resourceProvider);
+      var declarationsTracker = DeclarationsTracker(
+          MemoryByteStore(), PhysicalResourceProvider.INSTANCE);
       declarationsTracker.addContext(context);
 
       while (declarationsTracker.hasWork) {
@@ -74,7 +79,7 @@ Future _computeCompletionMetrics(
             resolvedUnitResult.unit.accept(visitor);
 
             for (var expectedCompletion in visitor.expectedCompletions) {
-              var suggestions = await computeCompletionSuggestions(
+              var suggestions = await _computeCompletionSuggestions(
                   resolvedUnitResult,
                   expectedCompletion.offset,
                   declarationsTracker);
@@ -86,7 +91,7 @@ Future _computeCompletionMetrics(
                 includedCount++;
               } else {
                 notIncludedCount++;
-                if (_doPrintMissingCompletions) {
+                if (_verbose) {
                   // The format "/file/path/foo.dart:3:4" makes for easier input
                   // with the Files dialog in IntelliJ
                   print(
@@ -116,61 +121,68 @@ Future _computeCompletionMetrics(
 
     completionKindCounter.printCounterValues();
     completionElementKindCounter.printCounterValues();
-    print('Summary for $root:');
+    print('Summary for $_rootPath:');
     print('Total number of completion tests   = $totalCompletionCount');
     print(
         'Number of successful completions   = $includedCount (${printPercentage(percentIncluded)})');
     print(
         'Number of unsuccessful completions = $notIncludedCount (${printPercentage(percentNotIncluded)})');
-  }
-  includedCount = 0;
-  notIncludedCount = 0;
-  completionKindCounter.clear();
-  completionElementKindCounter.clear();
-}
 
-Place _placementInSuggestionList(List<CompletionSuggestion> suggestions,
-    ExpectedCompletion expectedCompletion) {
-  var placeCounter = 1;
-  for (var completionSuggestion in suggestions) {
-    if (expectedCompletion.matches(completionSuggestion)) {
-      return Place(placeCounter, suggestions.length);
+    includedCount = 0;
+    notIncludedCount = 0;
+    completionKindCounter.clear();
+    completionElementKindCounter.clear();
+  }
+
+  Future<List<CompletionSuggestion>> _computeCompletionSuggestions(
+      ResolvedUnitResult resolvedUnitResult, int offset,
+      [DeclarationsTracker declarationsTracker]) async {
+    var completionRequestImpl = CompletionRequestImpl(
+      resolvedUnitResult,
+      offset,
+      CompletionPerformance(),
+    );
+
+    // This gets all of the suggestions with relevances.
+    var suggestions =
+        await DartCompletionManager().computeSuggestions(completionRequestImpl);
+
+    // If a non-null declarationsTracker was passed, use it to call
+    // computeIncludedSetList, this current implementation just adds the set of
+    // included element names with relevance 0, future implementations should
+    // compute out the relevance that clients will set to each value.
+    if (declarationsTracker != null) {
+      var includedSuggestionSets = <IncludedSuggestionSet>[];
+      var includedElementNames = <String>{};
+
+      computeIncludedSetList(declarationsTracker, resolvedUnitResult,
+          includedSuggestionSets, includedElementNames);
+
+      for (var eltName in includedElementNames) {
+        suggestions.add(CompletionSuggestion(
+            CompletionSuggestionKind.INVOCATION,
+            0,
+            eltName,
+            0,
+            eltName.length,
+            false,
+            false));
+      }
     }
-    placeCounter++;
+
+    suggestions.sort(completionComparator);
+    return suggestions;
   }
-  return Place.none();
-}
 
-Future<List<CompletionSuggestion>> computeCompletionSuggestions(
-    ResolvedUnitResult resolvedUnitResult, int offset,
-    [DeclarationsTracker declarationsTracker]) async {
-  var completionRequestImpl = CompletionRequestImpl(
-    resolvedUnitResult,
-    offset,
-    CompletionPerformance(),
-  );
-
-  // This gets all of the suggestions with relevances.
-  var suggestions =
-      await DartCompletionManager().computeSuggestions(completionRequestImpl);
-
-  // If a non-null declarationsTracker was passed, use it to call
-  // computeIncludedSetList, this current implementation just adds the set of
-  // included element names with relevance 0, future implementations should
-  // compute out the relevance that clients will set to each value.
-  if (declarationsTracker != null) {
-    var includedSuggestionSets = <IncludedSuggestionSet>[];
-    var includedElementNames = <String>{};
-
-    computeIncludedSetList(declarationsTracker, resolvedUnitResult,
-        includedSuggestionSets, includedElementNames);
-
-    for (var eltName in includedElementNames) {
-      suggestions.add(CompletionSuggestion(CompletionSuggestionKind.INVOCATION,
-          0, eltName, 0, eltName.length, false, false));
+  Place _placementInSuggestionList(List<CompletionSuggestion> suggestions,
+      ExpectedCompletion expectedCompletion) {
+    var placeCounter = 1;
+    for (var completionSuggestion in suggestions) {
+      if (expectedCompletion.matches(completionSuggestion)) {
+        return Place(placeCounter, suggestions.length);
+      }
+      placeCounter++;
     }
+    return Place.none();
   }
-
-  suggestions.sort(completionComparator);
-  return suggestions;
 }
