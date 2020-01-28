61b4bbcb6b45b5d4eea2d81885f88478456f6f10
Liam Appelbe <liama@google.com>
2020-01-28 16:47:58 +0000

    [vm] Set non_nullable_flag() to true
    We'll leave the deletion and cleanup of this function until later, so
    that this CL is as easy to rollback as possible.
    
    Bug: https://github.com/dart-lang/sdk/issues/40179
    Change-Id: Ie583aaabd117ae06ea27a539c3bc904240a0e6bc
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/133429
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Commit-Queue: Liam Appelbe <liama@google.com>

--- runtime/vm/dart.h
+++ runtime/vm/dart.h
@@ -123,8 +123,10 @@ class Dart : public AllStatic {
   static Dart_EntropySource entropy_source_callback() {
     return entropy_source_callback_;
   }
+
+  // TODO(dartbug.com/40342): Delete these functions.
   static void set_non_nullable_flag(bool value) { non_nullable_flag_ = value; }
-  static bool non_nullable_flag() { return non_nullable_flag_; }
+  static bool non_nullable_flag() { return true; }
 
  private:
   static void WaitForIsolateShutdown();
