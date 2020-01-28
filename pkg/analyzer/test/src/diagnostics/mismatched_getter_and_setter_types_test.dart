efdd38b0eb2f4a58bcb3c884f280c1fd7ad69eb3
Konstantin Shcheglov <scheglov@google.com>
2020-01-28 19:12:08 +0000

    Move tests for MISMATCHED_GETTER_AND_SETTER_TYPES.
    Change-Id: I49f0ea4d30fb50be4ecb6b4226081470397d098d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/133543
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/diagnostics/mismatched_getter_and_setter_types_test.dart
+++ pkg/analyzer/test/src/diagnostics/mismatched_getter_and_setter_types_test.dart
@@ -20,24 +20,7 @@ main() {
 
 @reflectiveTest
 class MismatchedGetterAndSetterTypesTest extends DriverResolutionTest {
-  test_topLevel() async {
-    await assertErrorsInCode('''
-int get g { return 0; }
-set g(String v) {}''', [
-      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 0, 23),
-    ]);
-  }
-
-  test_instance_sameTypes() async {
-    await assertNoErrorsInCode(r'''
-class C {
-  int get x => 0;
-  set x(int v) {}
-}
-''');
-  }
-
-  test_instance_unspecifiedGetter() async {
+  test_class_instance_dynamicGetter() async {
     await assertNoErrorsInCode(r'''
 class C {
   get x => 0;
@@ -46,7 +29,7 @@ class C {
 ''');
   }
 
-  test_instance_unspecifiedSetter() async {
+  test_class_instance_dynamicSetter() async {
     await assertNoErrorsInCode(r'''
 class C {
   int get x => 0;
@@ -55,24 +38,188 @@ class C {
 ''');
   }
 
-  test_topLevel_sameTypes() async {
+  test_class_instance_interfaces() async {
+    await assertErrorsInCode(r'''
+class A {
+  int get foo => 0;
+}
+
+class B {
+  set foo(String _) {}
+}
+
+abstract class X implements A, B {}
+''', [
+      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 84, 1),
+    ]);
+  }
+
+  test_class_instance_private_getter() async {
+    newFile('/test/lib/a.dart', content: r'''
+class A {
+  int get _foo => 0;
+}
+''');
+    await assertErrorsInCode(r'''
+import 'a.dart';
+
+class B extends A {
+  set _foo(String _) {}
+}
+''', [
+      error(HintCode.UNUSED_ELEMENT, 44, 4),
+    ]);
+  }
+
+  test_class_instance_private_interfaces() async {
+    newFile('/test/lib/a.dart', content: r'''
+class A {
+  int get _foo => 0;
+}
+''');
+    newFile('/test/lib/b.dart', content: r'''
+class B {
+  set _foo(String _) {}
+}
+''');
     await assertNoErrorsInCode(r'''
-int get x => 0;
-set x(int v) {}
+import 'a.dart';
+import 'b.dart';
+
+class X implements A, B {}
 ''');
   }
 
-  test_topLevel_unspecifiedGetter() async {
+  test_class_instance_private_interfaces2() async {
+    newFile('/test/lib/a.dart', content: r'''
+class A {
+  int get _foo => 0;
+}
+
+class B {
+  set _foo(String _) {}
+}
+''');
+    await assertNoErrorsInCode(r'''
+import 'a.dart';
+
+class X implements A, B {}
+''');
+  }
+
+  test_class_instance_private_setter() async {
+    newFile('/test/lib/a.dart', content: r'''
+class A {
+  set _foo(String _) {}
+}
+''');
+    await assertErrorsInCode(r'''
+import 'a.dart';
+
+class B extends A {
+  int get _foo => 0;
+}
+''', [
+      error(HintCode.UNUSED_ELEMENT, 48, 4),
+    ]);
+  }
+
+  test_class_instance_sameClass() async {
+    await assertErrorsInCode(r'''
+class C {
+  int get foo => 0;
+  set foo(String _) {}
+}
+''', [
+      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 20, 3),
+    ]);
+  }
+
+  test_class_instance_sameTypes() async {
+    await assertNoErrorsInCode(r'''
+class C {
+  int get x => 0;
+  set x(int v) {}
+}
+''');
+  }
+
+  test_class_instance_setterParameter_0() async {
+    await assertErrorsInCode(r'''
+class C {
+  int get foo => 0;
+  set foo() {}
+}
+''', [
+      error(CompileTimeErrorCode.WRONG_NUMBER_OF_PARAMETERS_FOR_SETTER, 36, 3),
+    ]);
+  }
+
+  test_class_instance_setterParameter_2() async {
+    await assertErrorsInCode(r'''
+class C {
+  int get foo => 0;
+  set foo(String p1, String p2) {}
+}
+''', [
+      error(CompileTimeErrorCode.WRONG_NUMBER_OF_PARAMETERS_FOR_SETTER, 36, 3),
+    ]);
+  }
+
+  test_class_instance_superGetter() async {
+    await assertErrorsInCode(r'''
+class A {
+  int get foo => 0;
+}
+
+class B extends A {
+  set foo(String _) {}
+}
+''', [
+      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 59, 3),
+    ]);
+  }
+
+  test_class_instance_superSetter() async {
+    await assertErrorsInCode(r'''
+class A {
+  set foo(String _) {}
+}
+
+class B extends A {
+  int get foo => 0;
+}
+''', [
+      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 66, 3),
+    ]);
+  }
+
+  test_topLevel() async {
+    await assertErrorsInCode('''
+int get g { return 0; }
+set g(String v) {}''', [
+      error(StaticWarningCode.MISMATCHED_GETTER_AND_SETTER_TYPES, 0, 23),
+    ]);
+  }
+
+  test_topLevel_dynamicGetter() async {
     await assertNoErrorsInCode(r'''
 get x => 0;
 set x(String v) {}
 ''');
   }
 
-  test_topLevel_unspecifiedSetter() async {
+  test_topLevel_dynamicSetter() async {
     await assertNoErrorsInCode(r'''
 int get x => 0;
 set x(v) {}
+''');
+  }
+
+  test_topLevel_sameTypes() async {
+    await assertNoErrorsInCode(r'''
+int get x => 0;
+set x(int v) {}
 ''');
   }
 }
@@ -85,7 +232,7 @@ class MismatchedGetterAndSetterTypesWithExtensionMethodsTest
     ..contextFeatures = FeatureSet.forTesting(
         sdkVersion: '2.3.0', additionalFeatures: [Feature.extension_methods]);
 
-  test_extensionMembers_instance() async {
+  test_extension_instance() async {
     await assertErrorsInCode('''
 extension E on Object {
   int get g { return 0; }
@@ -96,7 +243,7 @@ extension E on Object {
     ]);
   }
 
-  test_extensionMembers_static() async {
+  test_extension_static() async {
     await assertErrorsInCode('''
 extension E on Object {
   static int get g { return 0; }
@@ -116,7 +263,7 @@ class MismatchedGetterAndSetterTypesWithNNBDTest
     ..contextFeatures = FeatureSet.forTesting(
         sdkVersion: '2.3.0', additionalFeatures: [Feature.non_nullable]);
 
-  test_classMembers_instance() async {
+  test_nullSafety_class_instance() async {
     await assertErrorsInCode('''
 class C {
   num get g { return 0; }
@@ -128,7 +275,7 @@ class C {
   }
 
   @failingTest
-  test_classMembers_static() async {
+  test_nullSafety_class_static() async {
     await assertErrorsInCode('''
 class C {
   static num get g { return 0; }
