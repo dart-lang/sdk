68e904e444dcfdce6acbce49e949df675f44674b
Robert Nystrom <rnystrom@google.com>
2020-02-04 18:57:26 +0000

    Migrate language_2/abstract to NNBD.
    Change-Id: I265933f36f68df6f8e542a3f85ce0b04e1dfd549
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/134205
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Erik Ernst <eernst@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language/abstract/syntax_runtime_test.dart
@@ -0,0 +1,23 @@
+// TODO(multitest): This was automatically migrated from a multitest and may
+// contain strange or dead code.
+
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+main() {
+  var b = new B();
+  Expect.equals(42, b.foo());
+}
+
+class A {
+
+
+}
+
+class B extends A {
+  foo() => 42;
+  bar() => 87;
+}
