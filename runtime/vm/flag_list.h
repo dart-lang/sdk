4ab9c71a6b44f29e4e3e0307a551a6ab7443d174
Aske Simon Christensen <askesc@google.com>
2020-02-05 16:07:09 +0000

    [vm/aot] Enable table dispatch by default.
    Most benchmarks see speed improvements, especially the ones doing many
    highly polymorphic calls. DeltaBlue on X64 is 35% faster.
    
    ARM64 instructions size reduction on Flutter benchmarks ranges from
    1.6% to 2.5% (0.7% to 1.2% on total size).
    
    In terms of absolute numbers, the most significant size changes in the
    Flutter Gallery snapshot are:
    
                     v7 before v7 after v7 diff  v8 before v8 after v8 diff
    Instructions       6034448  5964136  -70312    5880528  5740112 -140416
    ObjectPool          139175   102704  -36471     141186   104720  -36466
    UnlinkedCall          5795     3331   -2464       5795     3331   -2464
    DispatchTable            0    48802  +48802          0    48796  +48796
    CompressedStackMaps 410194   421222  +11028     461743   470841   +9098
    Total              6589612  6540195  -49417    6489252  6367800 -121452
    
    Change-Id: I78663b423c91ab105fc52a8e0bcb47eaf8cc57fb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/131833
    Commit-Queue: Aske Simon Christensen <askesc@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>

--- runtime/vm/flag_list.h
+++ runtime/vm/flag_list.h
@@ -178,7 +178,7 @@ constexpr bool kDartUseBackgroundCompilation = true;
   P(strong_non_nullable_type_checks, bool, false,                              \
     "Enable strong non-nullable type checking mode.")                          \
   P(use_bare_instructions, bool, true, "Enable bare instructions mode.")       \
-  P(use_table_dispatch, bool, false, "Enable dispatch table based calls.")     \
+  P(use_table_dispatch, bool, true, "Enable dispatch table based calls.")      \
   P(enable_isolate_groups, bool, false, "Enable isolate group support.")       \
   P(show_invisible_frames, bool, false,                                        \
     "Show invisible frames in stack traces.")                                  \
