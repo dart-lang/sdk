2edd3d5e4aed9153c6b145bf0eaf10d75d51bbda
Konstantin Shcheglov <scheglov@google.com>
2020-02-10 22:56:06 +0000

    Issue 40414. Test for invalid override checked even via legacy library.
    Bug: https://github.com/dart-lang/sdk/issues/40414
    Change-Id: I73ac38823d05c71985ea0f55f92ea390d9273a45
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/135187
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/diagnostics/invalid_override_test.dart
+++ pkg/analyzer/test/src/diagnostics/invalid_override_test.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/src/dart/analysis/experiments.dart';
 import 'package:analyzer/src/error/codes.dart';
+import 'package:analyzer/src/generated/engine.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import '../dart/resolution/driver_resolution.dart';
@@ -10,6 +12,7 @@ import '../dart/resolution/driver_resolution.dart';
 main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(InvalidOverrideTest);
+    defineReflectiveTests(InvalidOverrideWithNnbdTest);
   });
 }
 
@@ -485,6 +488,48 @@ class B implements I<int>, J<String> {
 ''', [
       error(CompileTimeErrorCode.INVALID_OVERRIDE, 125, 1),
       error(CompileTimeErrorCode.INVALID_OVERRIDE, 125, 1),
+    ]);
+  }
+}
+
+@reflectiveTest
+class InvalidOverrideWithNnbdTest extends DriverResolutionTest {
+  @override
+  AnalysisOptionsImpl get analysisOptions => AnalysisOptionsImpl()
+    ..enabledExperiments = [EnableString.non_nullable]
+    ..implicitCasts = false;
+
+  @override
+  bool get typeToStringWithNullability => true;
+
+  test_method_viaLegacy_returnType_notSubtype() async {
+    newFile('/test/lib/a.dart', content: r'''
+class Nullable {
+  int? foo() => 0;
+}
+
+class NonNullable {
+  int foo() => 0;
+}
+''');
+
+    newFile('/test/lib/b.dart', content: r'''
+// @dart=2.6
+import 'a.dart';
+
+class A extends Nullable implements NonNullable {}
+''');
+
+    await assertErrorsInCode('''
+import 'b.dart';
+
+class B extends A {}
+
+class C extends A {   
+  int? foo() => 0;
+}
+''', [
+      error(CompileTimeErrorCode.INVALID_OVERRIDE, 70, 3),
     ]);
   }
 }
