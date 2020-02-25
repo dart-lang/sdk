26acdfb31042db896996ddc5ce61e84d19d7def0
pq <pquitslund@google.com>
2020-02-25 23:46:14 +0000

    clone and update corpus repos
    Running this with `data/itsallwidgets_repos.txt` as an argument, should produce
    a directory `third_party/download` populated with cloned repos ready for
    analysis (`flutter pub get` will have been run).
    
    
    Change-Id: I5f5c553a4f880c9970e502b4efa9359f224a68aa
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/137242
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/analysis_server/tool/completion_metrics/corpus.dart
+++ pkg/analysis_server/tool/completion_metrics/corpus.dart
@@ -7,22 +7,75 @@ import 'dart:io';
 import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 
-Future<void> main() async {
-  // (Re) generate the list of github repos on itsallwidgets.com
-  final repos = await RepoList.fromItsAllWidgetsRssFeed();
-  File('itsallwidgets_repos.txt').writeAsStringSync(repos.join('\n'));
+/// Generate or update corpus data.
+/// Input is a list of directories, or a file containing such a list.
+/// Output is produced in a relative `third_party/download` directory.
+Future<void> main(List<String> args) async {
+  if (args.isEmpty) {
+    print('A file name or list of directories is required.');
+    exit(1);
+  }
+
+  final repos = [];
+  if (args.length == 1 && !Directory(args[0]).existsSync()) {
+    final contents = File(args[0]).readAsStringSync();
+    repos.addAll(contents.split('\n'));
+  } else {
+    repos.addAll(args);
+  }
+
+  if (!Directory(_downloadDir).existsSync()) {
+    print('Creating: $_downloadDir');
+    Directory(_downloadDir).createSync(recursive: true);
+  }
+
+  print('Cloning repositories...');
+  for (var repo in repos) {
+    final result = await _clone(repo);
+    if (result.processResult.exitCode != 0) {
+      print('Error cloning $repo: ${result.processResult.stderr}');
+    } else {
+      print('Updating pub dependencies...');
+      await _runPub(result.directory);
+    }
+  }
 }
 
-const itsallwidgetsRssFeed = 'https://itsallwidgets.com/app/feed';
-
+const _downloadDir = 'third_party/download';
 final _client = http.Client();
+
+Future<CloneResult> _clone(String repo) async {
+  final name = repo.split('https://github.com/').last.replaceAll('/', '_');
+  final cloneDir = '$_downloadDir/$name';
+  var result;
+  if (Directory(cloneDir).existsSync()) {
+    print('Repository "$name" exists -- pulling to update');
+    result = await Process.run('git', ['pull'], workingDirectory: cloneDir);
+  } else {
+    print('Cloning $repo to $cloneDir');
+    result = await Process.run('git', ['clone', '$repo.git', cloneDir]);
+  }
+  return CloneResult(result, cloneDir);
+}
 
 Future<String> _getBody(String url) async => (await _getResponse(url)).body;
 
 Future<http.Response> _getResponse(String url) async => _client
     .get(url, headers: const {'User-Agent': 'dart.pkg.completion_metrics'});
 
+Future<ProcessResult> _runPub(String dir) async =>
+    await Process.run('flutter', ['pub', 'get'], workingDirectory: dir);
+
+class CloneResult {
+  final String directory;
+  final ProcessResult processResult;
+  CloneResult(this.processResult, this.directory);
+}
+
 class RepoList {
+  static const itsallwidgetsRssFeed = 'https://itsallwidgets.com/app/feed';
+
+  // (Re) generate the list of github repos on itsallwidgets.com
   static Future<List<String>> fromItsAllWidgetsRssFeed() async {
     final repos = <String>{};
 
