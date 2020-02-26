c9884ccf525b78a48930b70fc2e2e95951ca66b5
Konstantin Shcheglov <scheglov@google.com>
2020-02-26 21:08:21 +0000

    Analyze SDK with non-nullable experiment enabled, update MockSdk.
    Initial:  https://dart-review.googlesource.com/c/sdk/+/134401
    Reverted: https://dart-review.googlesource.com/c/sdk/+/134802
    
    Internal presubmit is green.
    https://test.corp.google.com/ui#id=OCL:294345942:BASE:297377939:1582738004318:fb4ae9fe
    
    Change-Id: I25e6445bd0241a89850dff3d88ece0edd768053a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/135241
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/analysis/feature_set_provider.dart
+++ pkg/analyzer/lib/src/dart/analysis/feature_set_provider.dart
@@ -7,14 +7,11 @@ import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/src/context/packages.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:meta/meta.dart';
-import 'package:pub_semver/pub_semver.dart';
 
 class FeatureSetProvider {
   /// This flag will be turned to `true` and inlined when we un-fork SDK,
   /// so that the only SDK is the Null Safe SDK.
-  static const isNullSafetySdk = false;
-
-  static final _preNonNullableVersion = Version(2, 7, 0);
+  static const isNullSafetySdk = true;
 
   final FeatureSet _sdkFeatureSet;
   final Packages _packages;
@@ -58,11 +55,7 @@ class FeatureSetProvider {
     @required FeatureSet packageDefaultFeatureSet,
     @required FeatureSet nonPackageDefaultFeatureSet,
   }) {
-    var sdkFeatureSet = _determineSdkFeatureSet(
-      resourceProvider,
-      sourceFactory,
-      packageDefaultFeatureSet,
-    );
+    var sdkFeatureSet = FeatureSet.fromEnableFlags(['non-nullable']);
 
     return FeatureSetProvider._(
       sdkFeatureSet: sdkFeatureSet,
@@ -70,33 +63,5 @@ class FeatureSetProvider {
       packageDefaultFeatureSet: packageDefaultFeatureSet,
       nonPackageDefaultFeatureSet: nonPackageDefaultFeatureSet,
     );
-  }
-
-  /// Read `dart:core` file and determine if SDK in non-nullable by default.
-  ///
-  /// If it is, use the [defaultFeatureSet], which might have enabled
-  /// [Feature.non_nullable], so SDK will be parsed and resolved as
-  /// non-nullable.
-  ///
-  /// Otherwise, restrict the SDK language to the maximum known now.
-  static FeatureSet _determineSdkFeatureSet(
-    ResourceProvider resourceProvider,
-    SourceFactory sourceFactory,
-    FeatureSet defaultFeatureSet,
-  ) {
-    var objectSource = sourceFactory.forUri('dart:core/object.dart');
-    if (objectSource == null) {
-      objectSource = sourceFactory.forUri('dart:core');
-    }
-
-    try {
-      var objectFile = resourceProvider.getFile(objectSource.fullName);
-      var objectContent = objectFile.readAsStringSync();
-      if (!objectContent.contains('bool operator ==(Object other)')) {
-        return defaultFeatureSet.restrictToVersion(_preNonNullableVersion);
-      }
-    } catch (_) {}
-
-    return defaultFeatureSet;
   }
 }
--- pkg/nnbd_migration/lib/src/decorated_type.dart
+++ pkg/nnbd_migration/lib/src/decorated_type.dart
@@ -378,6 +378,15 @@ class DecoratedType implements DecoratedTypeInfo {
       namedParameters: namedParameters,
       typeArguments: typeArguments);
 
+  /// Creates a shallow copy of `this`, replacing the nullability node and the
+  /// type.
+  DecoratedType withNodeAndType(NullabilityNode node, DartType type) =>
+      DecoratedType(type, node,
+          returnType: returnType,
+          positionalParameters: positionalParameters,
+          namedParameters: namedParameters,
+          typeArguments: typeArguments);
+
   /// Internal implementation of [_substitute], used as a recursion target.
   DecoratedType _substitute(
       Map<TypeParameterElement, DecoratedType> substitution,
@@ -404,8 +413,8 @@ class DecoratedType implements DecoratedTypeInfo {
           var typeFormal = typeFormals[i];
           var oldDecoratedBound =
               DecoratedTypeParameterBounds.current.get(typeFormal);
-          var newDecoratedBound = oldDecoratedBound._substitute(
-              substitution, typeFormal.bound ?? oldDecoratedBound.type);
+          var newDecoratedBound = oldDecoratedBound._substitute(substitution,
+              undecoratedResult.typeFormals[i].bound ?? oldDecoratedBound.type);
           if (identical(typeFormal, undecoratedResult.typeFormals[i])) {
             assert(oldDecoratedBound == newDecoratedBound);
           } else {
@@ -428,8 +437,9 @@ class DecoratedType implements DecoratedTypeInfo {
       if (inner == null) {
         return this;
       } else {
-        return inner
-            .withNode(NullabilityNode.forSubstitution(inner.node, node));
+        return inner.withNodeAndType(
+            NullabilityNode.forSubstitution(inner.node, node),
+            undecoratedResult);
       }
     } else if (type.isVoid || type.isDynamic) {
       return this;
