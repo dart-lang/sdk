9ca77d42be4b3be37c583b68a4917ff8d7844474
Ben Konyi <bkonyi@google.com>
2020-02-28 20:40:10 +0000

    [ VM / DartDev ] Load and run dartdev snapshot if first argument to dart is not a valid file path
    Change-Id: I754ff3cd44af95364ce8095255a67621f256dbd3
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/137249
    Reviewed-by: Jaime Wren <jwren@google.com>
    Commit-Queue: Ben Konyi <bkonyi@google.com>

--- runtime/bin/dartdev_utils.cc
+++ runtime/bin/dartdev_utils.cc
@@ -14,47 +14,19 @@
 namespace dart {
 namespace bin {
 
-typedef struct {
-  const char* command;
-  const char* snapshot_name;
-} DartDevCommandMapping;
-
-static const DartDevCommandMapping dart_dev_commands[] = {
-    {"format", "dartfmt.dart.snapshot"},
-    {"pub", "pub.dart.snapshot"},
-};
-
-static const DartDevCommandMapping* FindCommandMapping(const char* command) {
-  intptr_t num_commands =
-      sizeof(dart_dev_commands) / sizeof(dart_dev_commands[0]);
-  for (intptr_t i = 0; i < num_commands; i++) {
-    const DartDevCommandMapping& command_mapping = dart_dev_commands[i];
-    if (strcmp(command, command_mapping.command) == 0) {
-      return &command_mapping;
-    }
-  }
-  return nullptr;
-}
-
 bool DartDevUtils::ShouldParseCommand(const char* script_uri) {
-  return !File::ExistsUri(nullptr, script_uri);
+  return (!File::ExistsUri(nullptr, script_uri) &&
+          (strncmp(script_uri, "http", 4) != 0));
 }
 
-bool DartDevUtils::TryParseCommandFromScriptName(char** script_name) {
-  const DartDevCommandMapping* command = FindCommandMapping(*script_name);
-
-  // Either the command doesn't exist or we've been given an HTTP resource.
-  if (command == nullptr) {
-    return true;
-  }
-
+bool DartDevUtils::TryResolveDartDevSnapshotPath(char** script_name) {
   // |dir_prefix| includes the last path seperator.
   auto dir_prefix = std::unique_ptr<char, void (*)(void*)>(
       EXEUtils::GetDirectoryPrefixFromExeName(), free);
 
   // First assume we're in dart-sdk/bin.
-  char* snapshot_path = Utils::SCreate("%s/snapshots/%s", dir_prefix.get(),
-                                       command->snapshot_name);
+  char* snapshot_path =
+      Utils::SCreate("%s/snapshots/dartdev.dart.snapshot", dir_prefix.get());
   if (File::Exists(nullptr, snapshot_path)) {
     free(*script_name);
     *script_name = snapshot_path;
@@ -64,16 +36,14 @@ bool DartDevUtils::TryParseCommandFromScriptName(char** script_name) {
 
   // If we're not in dart-sdk/bin, we might be in one of the $SDK/out/*
   // directories. Try to use a snapshot from a previously built SDK.
-  snapshot_path = Utils::SCreate("%s/dart-sdk/bin/snapshots/%s",
-                                 dir_prefix.get(), command->snapshot_name);
+  snapshot_path = Utils::SCreate(
+      "%s/dart-sdk/bin/snapshots/dartdev.dart.snapshot", dir_prefix.get());
   if (File::Exists(nullptr, snapshot_path)) {
     free(*script_name);
     *script_name = snapshot_path;
     return true;
   }
   free(snapshot_path);
-  Syslog::PrintErr("Could not find snapshot for command '%s': %s\n",
-                   *script_name, command->snapshot_name);
   return false;
 }
 
