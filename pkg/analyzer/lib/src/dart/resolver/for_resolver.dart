001db5d6697bc05cab349520a43cb57b775ac5dd
Konstantin Shcheglov <scheglov@google.com>
2020-02-28 15:30:12 +0000

    Extract shared code for resolving ForEachParts and ForParts.
    Change-Id: I3b38174aab90faaf3a563567e6e862dd38a3d186
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/137729
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/resolver/for_resolver.dart
+++ pkg/analyzer/lib/src/dart/resolver/for_resolver.dart
@@ -25,184 +25,22 @@ class ForResolver {
         _flowAnalysis = flowAnalysis;
 
   void resolveElement(ForElementImpl node) {
-    ForLoopParts forLoopParts = node.forLoopParts;
+    var forLoopParts = node.forLoopParts;
     if (forLoopParts is ForParts) {
-      if (forLoopParts is ForPartsWithDeclarations) {
-        forLoopParts.variables?.accept(_resolver);
-      } else if (forLoopParts is ForPartsWithExpression) {
-        forLoopParts.initialization?.accept(_resolver);
-      }
-
-      var condition = forLoopParts.condition;
-
-      _flowAnalysis?.for_conditionBegin(node);
-      if (condition != null) {
-        InferenceContext.setType(condition, _resolver.typeProvider.boolType);
-        condition.accept(_resolver);
-        condition = forLoopParts.condition;
-        _resolver.boolExpressionVerifier.checkForNonBoolCondition(condition);
-      }
-
-      _flowAnalysis?.for_bodyBegin(node, condition);
-      node.body?.accept(_resolver);
-
-      _flowAnalysis?.flow?.for_updaterBegin();
-      forLoopParts.updaters.accept(_resolver);
-
-      _flowAnalysis?.flow?.for_end();
+      _forParts(node, forLoopParts, node.body);
     } else if (forLoopParts is ForEachParts) {
-      Expression iterable = forLoopParts.iterable;
-      DeclaredIdentifier loopVariable;
-      DartType valueType;
-      Element identifierElement;
-      if (forLoopParts is ForEachPartsWithDeclaration) {
-        loopVariable = forLoopParts.loopVariable;
-        valueType = loopVariable?.type?.type ?? UnknownInferredType.instance;
-      } else if (forLoopParts is ForEachPartsWithIdentifier) {
-        SimpleIdentifier identifier = forLoopParts.identifier;
-        identifier?.accept(_resolver);
-        identifierElement = identifier?.staticElement;
-        if (identifierElement is VariableElement) {
-          valueType = identifierElement.type;
-        } else if (identifierElement is PropertyAccessorElement) {
-          var parameters = identifierElement.parameters;
-          if (parameters.isNotEmpty) {
-            valueType = parameters[0].type;
-          }
-        }
-      }
-
-      if (valueType != null) {
-        InterfaceType targetType = (node.awaitKeyword == null)
-            ? _resolver.typeProvider.iterableType2(valueType)
-            : _resolver.typeProvider.streamType2(valueType);
-        InferenceContext.setType(iterable, targetType);
-      }
-      //
-      // We visit the iterator before the loop variable because the loop
-      // variable cannot be in scope while visiting the iterator.
-      //
-      iterable?.accept(_resolver);
-      // Note: the iterable could have been rewritten so grab it from
-      // forLoopParts again.
-      iterable = forLoopParts.iterable;
-      loopVariable?.accept(_resolver);
-      var elementType =
-          _computeForEachElementType(iterable, node.awaitKeyword != null);
-      if (loopVariable != null &&
-          elementType != null &&
-          loopVariable.type == null) {
-        var loopVariableElement =
-            loopVariable.declaredElement as LocalVariableElementImpl;
-        loopVariableElement.type = elementType;
-      }
-      _flowAnalysis?.flow?.forEach_bodyBegin(
-          node,
-          identifierElement is VariableElement
-              ? identifierElement
-              : loopVariable?.declaredElement,
-          elementType ?? DynamicTypeImpl.instance);
-      node.body?.accept(_resolver);
-      _flowAnalysis?.flow?.forEach_end();
+      _forEachParts(node, node.awaitKeyword != null, forLoopParts, node.body);
     }
   }
 
   void resolveStatement(ForStatementImpl node) {
     _flowAnalysis?.checkUnreachableNode(node);
 
-    ForLoopParts forLoopParts = node.forLoopParts;
+    var forLoopParts = node.forLoopParts;
     if (forLoopParts is ForParts) {
-      if (forLoopParts is ForPartsWithDeclarations) {
-        forLoopParts.variables?.accept(_resolver);
-      } else if (forLoopParts is ForPartsWithExpression) {
-        forLoopParts.initialization?.accept(_resolver);
-      }
-
-      var condition = forLoopParts.condition;
-
-      _flowAnalysis?.for_conditionBegin(node);
-      if (condition != null) {
-        InferenceContext.setType(condition, _resolver.typeProvider.boolType);
-        condition.accept(_resolver);
-        condition = forLoopParts.condition;
-        _resolver.boolExpressionVerifier.checkForNonBoolCondition(condition);
-      }
-
-      _flowAnalysis?.for_bodyBegin(node, condition);
-      _resolver.visitStatementInScope(node.body);
-
-      _flowAnalysis?.flow?.for_updaterBegin();
-      forLoopParts.updaters.accept(_resolver);
-
-      _flowAnalysis?.flow?.for_end();
+      _forParts(node, forLoopParts, node.body);
     } else if (forLoopParts is ForEachParts) {
-      Expression iterable = forLoopParts.iterable;
-      DeclaredIdentifier loopVariable;
-      SimpleIdentifier identifier;
-      Element identifierElement;
-      if (forLoopParts is ForEachPartsWithDeclaration) {
-        loopVariable = forLoopParts.loopVariable;
-      } else if (forLoopParts is ForEachPartsWithIdentifier) {
-        identifier = forLoopParts.identifier;
-        identifier?.accept(_resolver);
-      }
-
-      DartType valueType;
-      if (loopVariable != null) {
-        TypeAnnotation typeAnnotation = loopVariable.type;
-        valueType = typeAnnotation?.type ?? UnknownInferredType.instance;
-      }
-      if (identifier != null) {
-        identifierElement = identifier.staticElement;
-        if (identifierElement is VariableElement) {
-          valueType = identifierElement.type;
-        } else if (identifierElement is PropertyAccessorElement) {
-          var parameters = identifierElement.parameters;
-          if (parameters.isNotEmpty) {
-            valueType = parameters[0].type;
-          }
-        }
-      }
-      if (valueType != null) {
-        InterfaceType targetType = (node.awaitKeyword == null)
-            ? _resolver.typeProvider.iterableType2(valueType)
-            : _resolver.typeProvider.streamType2(valueType);
-        InferenceContext.setType(iterable, targetType);
-      }
-      //
-      // We visit the iterator before the loop variable because the loop variable
-      // cannot be in scope while visiting the iterator.
-      //
-      iterable?.accept(_resolver);
-      // Note: the iterable could have been rewritten so grab it again.
-      iterable = forLoopParts.iterable;
-
-      _resolver.nullableDereferenceVerifier.expression(iterable);
-
-      loopVariable?.accept(_resolver);
-      var elementType =
-          _computeForEachElementType(iterable, node.awaitKeyword != null);
-      if (loopVariable != null &&
-          elementType != null &&
-          loopVariable.type == null) {
-        var loopVariableElement =
-            loopVariable.declaredElement as LocalVariableElementImpl;
-        loopVariableElement.type = elementType;
-      }
-
-      _flowAnalysis?.flow?.forEach_bodyBegin(
-          node,
-          identifierElement is VariableElement
-              ? identifierElement
-              : loopVariable?.declaredElement,
-          elementType ?? DynamicTypeImpl.instance);
-
-      Statement body = node.body;
-      if (body != null) {
-        _resolver.visitStatementInScope(body);
-      }
-
-      _flowAnalysis?.flow?.forEach_end();
+      _forEachParts(node, node.awaitKeyword != null, forLoopParts, node.body);
     }
   }
 
@@ -228,6 +66,108 @@ class ForResolver {
       return iteratedType.typeArguments.single;
     } else {
       return null;
+    }
+  }
+
+  void _forEachParts(
+    AstNode node,
+    bool isAsync,
+    ForEachParts forEachParts,
+    AstNode body,
+  ) {
+    Expression iterable = forEachParts.iterable;
+    DeclaredIdentifier loopVariable;
+    SimpleIdentifier identifier;
+    Element identifierElement;
+    if (forEachParts is ForEachPartsWithDeclaration) {
+      loopVariable = forEachParts.loopVariable;
+    } else if (forEachParts is ForEachPartsWithIdentifier) {
+      identifier = forEachParts.identifier;
+      identifier?.accept(_resolver);
+    }
+
+    DartType valueType;
+    if (loopVariable != null) {
+      TypeAnnotation typeAnnotation = loopVariable.type;
+      valueType = typeAnnotation?.type ?? UnknownInferredType.instance;
+    }
+    if (identifier != null) {
+      identifierElement = identifier.staticElement;
+      if (identifierElement is VariableElement) {
+        valueType = identifierElement.type;
+      } else if (identifierElement is PropertyAccessorElement) {
+        var parameters = identifierElement.parameters;
+        if (parameters.isNotEmpty) {
+          valueType = parameters[0].type;
+        }
+      }
+    }
+    if (valueType != null) {
+      InterfaceType targetType = isAsync
+          ? _resolver.typeProvider.streamType2(valueType)
+          : _resolver.typeProvider.iterableType2(valueType);
+      InferenceContext.setType(iterable, targetType);
+    }
+
+    iterable?.accept(_resolver);
+    iterable = forEachParts.iterable;
+
+    _resolver.nullableDereferenceVerifier.expression(iterable);
+
+    loopVariable?.accept(_resolver);
+    var elementType = _computeForEachElementType(iterable, isAsync);
+    if (loopVariable != null &&
+        elementType != null &&
+        loopVariable.type == null) {
+      var loopVariableElement =
+          loopVariable.declaredElement as LocalVariableElementImpl;
+      loopVariableElement.type = elementType;
+    }
+
+    _flowAnalysis?.flow?.forEach_bodyBegin(
+      node,
+      identifierElement is VariableElement
+          ? identifierElement
+          : loopVariable?.declaredElement,
+      elementType ?? DynamicTypeImpl.instance,
+    );
+
+    _resolveBody(body);
+
+    _flowAnalysis?.flow?.forEach_end();
+  }
+
+  void _forParts(AstNode node, ForParts forParts, AstNode body) {
+    if (forParts is ForPartsWithDeclarations) {
+      forParts.variables?.accept(_resolver);
+    } else if (forParts is ForPartsWithExpression) {
+      forParts.initialization?.accept(_resolver);
+    }
+
+    _flowAnalysis?.for_conditionBegin(node);
+
+    var condition = forParts.condition;
+    if (condition != null) {
+      InferenceContext.setType(condition, _resolver.typeProvider.boolType);
+      condition.accept(_resolver);
+      condition = forParts.condition;
+      _resolver.boolExpressionVerifier.checkForNonBoolCondition(condition);
+    }
+
+    _flowAnalysis?.for_bodyBegin(node, condition);
+    _resolveBody(body);
+
+    _flowAnalysis?.flow?.for_updaterBegin();
+    forParts.updaters.accept(_resolver);
+
+    _flowAnalysis?.flow?.for_end();
+  }
+
+  void _resolveBody(AstNode body) {
+    if (body is Statement) {
+      _resolver.visitStatementInScope(body);
+    } else {
+      body.accept(_resolver);
     }
   }
 }
