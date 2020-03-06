972e04564939032ca5afd808635c332c0b22066b
Mike Fairhurst <mfairhurst@google.com>
2020-03-06 18:29:13 +0000

    [analyzer] Fix windows paths again, and use ResourceProviderMixin.
    The paths in the SourceEdits I was constructing needed to be converted,
    and since it wasn't a "new failure," the windows trybot for the previous
    CL passed anyway (and didn't include output of what went wrong).
    
    Change-Id: I5bb2a52bf8cf112d6a2dae0b5749d1ae4e900e2a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/138575
    Auto-Submit: Mike Fairhurst <mfairhurst@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Mike Fairhurst <mfairhurst@google.com>

--- pkg/analysis_server/test/src/edit/preview/preview_site_test.dart
+++ pkg/analysis_server/test/src/edit/preview/preview_site_test.dart
@@ -7,11 +7,11 @@ import 'package:analysis_server/src/edit/nnbd_migration/migration_info.dart';
 import 'package:analysis_server/src/edit/nnbd_migration/migration_state.dart';
 import 'package:analysis_server/src/edit/nnbd_migration/path_mapper.dart';
 import 'package:analysis_server/src/edit/preview/preview_site.dart';
-import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/memory_file_system.dart';
 import 'package:analyzer_plugin/protocol/protocol_common.dart';
 import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
+import 'package:analyzer/src/test_utilities/resource_provider_mixin.dart';
 
 void main() {
   defineReflectiveSuite(() {
@@ -20,14 +20,10 @@ void main() {
 }
 
 @reflectiveTest
-class PreviewSiteTest {
+class PreviewSiteTest with ResourceProviderMixin {
   PreviewSite site;
-  MemoryResourceProvider resourceProvider;
   DartFixListener dartfixListener;
   MigrationState state;
-
-  File getFile(String path) =>
-      resourceProvider.getFile(resourceProvider.convertPath(path));
 
   void setUp() {
     dartfixListener = DartFixListener(null);
@@ -40,9 +36,10 @@ class PreviewSiteTest {
   }
 
   void test_applyChangesEmpty() {
-    getFile('/test.dart').writeAsStringSync('void main() {}');
+    final file = getFile('/test.dart');
+    file.writeAsStringSync('void main() {}');
     site.performApply();
-    expect(getFile('/test.dart').readAsStringSync(), 'void main() {}');
+    expect(file.readAsStringSync(), 'void main() {}');
     expect(state.hasBeenApplied, true);
   }
 
@@ -52,18 +49,20 @@ class PreviewSiteTest {
   }
 
   void test_applyMultipleChanges() {
-    getFile('/test.dart').writeAsStringSync('void main() {}');
+    final path = convertPath('/test.dart');
+    final file = getFile(path);
+    file.writeAsStringSync('void main() {}');
     dartfixListener.addSourceChange(
-        "test change",
-        Location('/test.dart', 10, 0, 1, 10),
-        SourceChange("test change", edits: [
-          SourceFileEdit('/test.dart', 0, edits: [
+        'test change',
+        Location(path, 10, 0, 1, 10),
+        SourceChange('test change', edits: [
+          SourceFileEdit(path, 0, edits: [
             SourceEdit(10, 0, 'List args'),
             SourceEdit(13, 0, '\n  print(args);\n')
           ])
         ]));
     site.performApply();
-    expect(getFile('/test.dart').readAsStringSync(), '''
+    expect(file.readAsStringSync(), '''
 void main(List args) {
   print(args);
 }''');
@@ -71,16 +70,17 @@ void main(List args) {
   }
 
   void test_applySingleChange() {
-    getFile('/test.dart').writeAsStringSync('void main() {}');
+    final path = convertPath('/test.dart');
+    final file = getFile(path);
+    file.writeAsStringSync('void main() {}');
     dartfixListener.addSourceChange(
-        "test change",
-        Location('/test.dart', 10, 0, 1, 10),
-        SourceChange("test change", edits: [
-          SourceFileEdit('/test.dart', 0,
-              edits: [SourceEdit(10, 0, 'List args')])
+        'test change',
+        Location(path, 10, 0, 1, 10),
+        SourceChange('test change', edits: [
+          SourceFileEdit(path, 0, edits: [SourceEdit(10, 0, 'List args')])
         ]));
     site.performApply();
-    expect(getFile('/test.dart').readAsStringSync(), 'void main(List args) {}');
+    expect(file.readAsStringSync(), 'void main(List args) {}');
     expect(state.hasBeenApplied, true);
   }
 }
