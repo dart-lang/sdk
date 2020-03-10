b311a0052e3abe22868fe9af1145e7618d6dbfa1
Konstantin Shcheglov <scheglov@google.com>
2020-03-10 15:50:00 +0000

    Fixes REMOVE_UNUSED_FIELD and optional field formal parameters.
    R=brianwilkerson@google.com
    
    Change-Id: I15587001262ed70fb6e4b740fee12542acb6b1a9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/138905
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/remove_unused.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/remove_unused.dart
@@ -5,6 +5,7 @@
 import 'package:analysis_server/src/services/correction/dart/abstract_producer.dart';
 import 'package:analysis_server/src/services/correction/fix.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/source/source_range.dart';
@@ -65,6 +66,7 @@ class RemoveUnusedField extends _RemoveUnused {
     if (declaration is! VariableDeclaration) {
       return;
     }
+
     final element = (declaration as VariableDeclaration).declaredElement;
     if (element is! FieldElement) {
       return;
@@ -82,32 +84,13 @@ class RemoveUnusedField extends _RemoveUnused {
       if (referenceNode == null) {
         return;
       }
-      var sourceRange;
+      SourceRange sourceRange;
       if (referenceNode is VariableDeclaration) {
-        VariableDeclarationList parent = referenceNode.parent;
-        if (parent.variables.length == 1) {
-          sourceRange = utils.getLinesRange(range.node(parent.parent));
-        } else {
-          sourceRange = range.nodeInList(parent.variables, referenceNode);
-        }
+        sourceRange = _forVariableDeclaration(referenceNode);
       } else if (referenceNode is ConstructorFieldInitializer) {
-        ConstructorDeclaration cons =
-            referenceNode.parent as ConstructorDeclaration;
-        // A() : _f = 0;
-        if (cons.initializers.length == 1) {
-          sourceRange = range.endEnd(cons.parameters, referenceNode);
-        } else {
-          sourceRange = range.nodeInList(cons.initializers, referenceNode);
-        }
+        sourceRange = _forConstructorFieldInitializer(referenceNode);
       } else if (referenceNode is FieldFormalParameter) {
-        FormalParameterList params =
-            referenceNode.parent as FormalParameterList;
-        if (params.parameters.length == 1) {
-          sourceRange =
-              range.endStart(params.leftParenthesis, params.rightParenthesis);
-        } else {
-          sourceRange = range.nodeInList(params.parameters, referenceNode);
-        }
+        sourceRange = _forFieldFormalParameter(referenceNode);
       } else {
         sourceRange = utils.getLinesRange(range.node(referenceNode));
       }
@@ -119,6 +102,64 @@ class RemoveUnusedField extends _RemoveUnused {
         builder.addDeletion(sourceRange);
       }
     });
+  }
+
+  SourceRange _forConstructorFieldInitializer(
+    ConstructorFieldInitializer node,
+  ) {
+    final constructor = node.parent as ConstructorDeclaration;
+    if (constructor.initializers.length == 1) {
+      return range.endEnd(constructor.parameters, node);
+    } else {
+      return range.nodeInList(constructor.initializers, node);
+    }
+  }
+
+  SourceRange _forFieldFormalParameter(FieldFormalParameter node) {
+    var parameter = node.parent is DefaultFormalParameter ? node.parent : node;
+    var parameterList = parameter.parent as FormalParameterList;
+
+    // (node) -> ()
+    if (parameterList.parameters.length == 1) {
+      return range.endStart(
+        parameterList.leftParenthesis,
+        parameterList.rightParenthesis,
+      );
+    }
+
+    var prevToken = parameter.beginToken.previous;
+    var nextToken = parameter.endToken.next;
+
+    // (node, tail) -> (tail)
+    if (nextToken.type == TokenType.COMMA) {
+      nextToken = nextToken.next;
+      return range.startStart(parameter.beginToken, nextToken);
+    }
+
+    // (head, node) -> (head)
+    // (head, node, tail) -> (head, tail)
+    var isFirstOptional = prevToken.type == TokenType.OPEN_CURLY_BRACKET ||
+        prevToken.type == TokenType.OPEN_SQUARE_BRACKET;
+    if (isFirstOptional) {
+      prevToken = prevToken.previous;
+    }
+    if (isFirstOptional) {
+      var isLastOptional = nextToken.type == TokenType.CLOSE_CURLY_BRACKET ||
+          nextToken.type == TokenType.CLOSE_SQUARE_BRACKET;
+      if (isLastOptional) {
+        nextToken = nextToken.next;
+      }
+    }
+    return range.endStart(prevToken.previous, nextToken);
+  }
+
+  SourceRange _forVariableDeclaration(VariableDeclaration node) {
+    VariableDeclarationList parent = node.parent;
+    if (parent.variables.length == 1) {
+      return utils.getLinesRange(range.node(parent.parent));
+    } else {
+      return range.nodeInList(parent.variables, node);
+    }
   }
 }
 
