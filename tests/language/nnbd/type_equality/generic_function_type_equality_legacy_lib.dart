9301c2f5933dd91d1a7a3890b8f7ff97d64184ed
Nicholas Shahan <nshahan@google.com>
2020-03-12 18:16:36 +0000

    [tests] Fix analysis errors in tests
    Non-nullable and optional method arguments need default values. Embedded
    the generic type argument into a List as an easy way to create a default
    value.
    
    Change-Id: Ib171c272c75b114447d356d423c45dc9aeff9bb4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/139160
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Erik Ernst <eernst@google.com>

--- tests/language/nnbd/type_equality/generic_function_type_equality_legacy_lib.dart
+++ tests/language/nnbd/type_equality/generic_function_type_equality_legacy_lib.dart
@@ -10,14 +10,14 @@ import 'generic_function_type_equality_null_safe_lib.dart';
 void fn<X>() => null;
 R voidToR<R>() => null as R;
 void positionalRToVoid<R>(R i) => null;
-void optionalRToVoid<R>([R i]) => null;
-void namedRToVoid<R>({R i}) => null;
-void hn<T, S>(T b, [S i]) => null;
-void kn<T, S>(T b, {S i}) => null;
+void optionalRToVoid<R>([List<R> i]) => null;
+void namedRToVoid<R>({List<R> i}) => null;
+void hn<T, S>(T b, [List<S> i]) => null;
+void kn<T, S>(T b, {List<S> i}) => null;
 
 void positionalTToVoidWithBound<T extends B>(T i) => null;
-void optionalTToVoidWithBound<T extends B>([T i]) => null;
-void namedTToVoidWithBound<T extends B>({T i}) => null;
+void optionalTToVoidWithBound<T extends B>([List<T> i]) => null;
+void namedTToVoidWithBound<T extends B>({List<T> i}) => null;
 
 class A<T extends B> {
   void fn(T i) => null;
--- tests/language/nnbd/type_equality/generic_function_type_equality_test.dart
+++ tests/language/nnbd/type_equality/generic_function_type_equality_test.dart
@@ -14,12 +14,12 @@ void positionalTToVoid<T>(T i) => null;
 void positionalSToVoid<S>(S i) => null;
 void positionalNullableTToVoid<T>(T? i) => null;
 void positionalNullableSToVoid<S>(S? i) => null;
-void optionalTToVoid<T>([T i]) => null;
-void optionalSToVoid<S>([S i]) => null;
+void optionalTToVoid<T>([List<T> i = const <Never>[]]) => null;
+void optionalSToVoid<S>([List<S> i = const <Never>[]]) => null;
 void optionalNullableTToVoid<T>([T? i]) => null;
 void optionalNullableSToVoid<S>([S? i]) => null;
-void namedTToVoid<T>({T i}) => null;
-void namedSToVoid<S>({S i}) => null;
+void namedTToVoid<T>({List<T> i = const <Never>[]}) => null;
+void namedSToVoid<S>({List<S> i = const <Never>[]}) => null;
 void namedNullableTToVoid<T>({T? i}) => null;
 void namedNullableSToVoid<S>({S? i}) => null;
 void requiredTToVoid<T>({required T i}) => null;
@@ -28,8 +28,9 @@ void requiredNullableTToVoid<T>({required T? i}) => null;
 void requiredNullableSToVoid<S>({required S? i}) => null;
 
 void positionalTToVoidWithBound<T extends B>(T i) => null;
-void optionalTToVoidWithBound<T extends B>([T i]) => null;
-void namedTToVoidWithBound<T extends B>({T i}) => null;
+void optionalTToVoidWithBound<T extends B>([List<T> i = const <Never>[]]) =>
+    null;
+void namedTToVoidWithBound<T extends B>({List<T> i = const <Never>[]}) => null;
 
 class A<T extends B> {
   void fn(T i) => null;
@@ -38,7 +39,7 @@ class A<T extends B> {
 main() {
   // Same functions with different names.
   Expect.equals(fn.runtimeType, fn2.runtimeType);
-  Expect.equals(voidToT.runtimeType, voidToT.runtimeType);
+  Expect.equals(voidToT.runtimeType, voidToS.runtimeType);
   Expect.equals(positionalTToVoid.runtimeType, positionalSToVoid.runtimeType);
   Expect.equals(positionalNullableTToVoid.runtimeType,
       positionalNullableSToVoid.runtimeType);
