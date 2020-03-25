f6d63899a5f1e0003ea56ddcec9759861b95e224
David Morgan <davidmorgan@google.com>
2020-03-25 11:54:50 +0000

    Revert "[dart:io] Making OSError implements Exception"
    This reverts commit ae392b8e007f2a2ad42fab96b7156c0fc68c49c7.
    
    Reason for revert: Breaks package:flutter_tools, dead code in icon_tree_shaker.dart, which blocks google3 roll.
    
    Original change's description:
    > [dart:io] Making OSError implements Exception
    > 
    > Bug: https://github.com/dart-lang/sdk/issues/40934
    > Change-Id: Icff457333bd570d81cdf9c3136ee4fe1cadfaae8
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/139406
    > Reviewed-by: Zach Anderson <zra@google.com>
    > Reviewed-by: Siva Annamalai <asiva@google.com>
    > Commit-Queue: Zichang Guo <zichangguo@google.com>
    
    TBR=zra@google.com,asiva@google.com,zichangguo@google.com
    
    Change-Id: Idc3a41173ba224d0a30fea3ef0f92d1211d9a63c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: https://github.com/dart-lang/sdk/issues/40934
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/140864
    Reviewed-by: David Morgan <davidmorgan@google.com>
    Commit-Queue: David Morgan <davidmorgan@google.com>

--- sdk/lib/io/common.dart
+++ sdk/lib/io/common.dart
@@ -52,7 +52,7 @@ abstract class IOException implements Exception {
   * operating system.
   */
 @pragma("vm:entry-point")
-class OSError implements Exception {
+class OSError {
   /** Constant used to indicate that no OS error code is available. */
   static const int noErrorCode = -1;
 
