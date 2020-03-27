232052da06095d59053f6733656835f8070b8504
Brian Wilkerson <brianwilkerson@google.com>
2020-03-27 22:51:34 +0000

    New relevance values for UriContributor
    Change-Id: I32a3151cb78a333ef7e23e89f2688acb4c74edb4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/141480
    Reviewed-by: Jaime Wren <jwren@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/lib/src/services/completion/dart/uri_contributor.dart
+++ pkg/analysis_server/lib/src/services/completion/dart/uri_contributor.dart
@@ -12,12 +12,10 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/src/generated/sdk.dart';
-import 'package:analyzer/src/generated/source.dart';
 import 'package:path/path.dart' show posix;
-import 'package:path/src/context.dart';
 
-/// A contributor for calculating uri suggestions for import and part
-/// directives.
+/// A contributor that produces suggestions based on the content of the file
+/// system to complete within URIs in import, export and part directives.
 class UriContributor extends DartCompletionContributor {
   /// A flag indicating whether file: and package: URI suggestions should
   /// be included in the list of completion suggestions.
@@ -26,21 +24,23 @@ class UriContributor extends DartCompletionContributor {
   // to only those paths within context roots.
   static bool suggestFilePaths = true;
 
-  _UriSuggestionBuilder builder;
-
   @override
   Future<List<CompletionSuggestion>> computeSuggestions(
       DartCompletionRequest request) async {
-    // TODO(brianwilkerson) Determine whether this await is necessary.
-    await null;
-    builder = _UriSuggestionBuilder(request);
+    var builder = _UriSuggestionBuilder(request);
     request.target.containingNode.accept(builder);
     return builder.suggestions;
   }
 }
 
 class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
+  // TODO(brianwilkerson) Consider whether to make these constants in
+  //  Relevance (after renaming them).
+  static const int dartCoreRelevance = 100;
+  static const int defaultRelevance = 900;
+
   final DartCompletionRequest request;
+
   final List<CompletionSuggestion> suggestions = <CompletionSuggestion>[];
 
   _UriSuggestionBuilder(this.request);
@@ -56,11 +56,11 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
   }
 
   void visitNamespaceDirective(NamespaceDirective node) {
-    StringLiteral uri = node.uri;
+    var uri = node.uri;
     if (uri is SimpleStringLiteral) {
-      int offset = request.offset;
-      int start = uri.offset;
-      int end = uri.end;
+      var offset = request.offset;
+      var start = uri.offset;
+      var end = uri.end;
       if (offset > start) {
         if (offset < end) {
           // Quoted non-empty string
@@ -70,9 +70,9 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
             // Quoted empty string
             visitSimpleStringLiteral(uri);
           } else {
-            String data = request.sourceContents;
+            var data = request.sourceContents;
             if (end == data.length) {
-              String ch = data[end - 1];
+              var ch = data[end - 1];
               if (ch != '"' && ch != "'") {
                 // Insertion point at end of file
                 // and missing closing quote on non-empty string
@@ -82,9 +82,9 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
           }
         }
       } else if (offset == start && offset == end) {
-        String data = request.sourceContents;
+        var data = request.sourceContents;
         if (end == data.length) {
-          String ch = data[end - 1];
+          var ch = data[end - 1];
           if (ch == '"' || ch == "'") {
             // Insertion point at end of file
             // and missing closing quote on empty string
@@ -97,9 +97,9 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
 
   @override
   void visitSimpleStringLiteral(SimpleStringLiteral node) {
-    AstNode parent = node.parent;
+    var parent = node.parent;
     if (parent is NamespaceDirective && parent.uri == node) {
-      String partialUri = _extractPartialUri(node);
+      var partialUri = _extractPartialUri(node);
       if (partialUri != null) {
         _addDartSuggestions();
         if (UriContributor.suggestFilePaths) {
@@ -108,7 +108,7 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
         }
       }
     } else if (parent is PartDirective && parent.uri == node) {
-      String partialUri = _extractPartialUri(node);
+      var partialUri = _extractPartialUri(node);
       if (partialUri != null) {
         if (UriContributor.suggestFilePaths) {
           _addFileSuggestions(partialUri);
@@ -119,23 +119,30 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
 
   void _addDartSuggestions() {
     _addSuggestion('dart:');
-    SourceFactory factory = request.sourceFactory;
+    var factory = request.sourceFactory;
     for (SdkLibrary lib in factory.dartSdk.sdkLibraries) {
       if (!lib.isInternal && !lib.isImplementation) {
         if (!lib.shortName.startsWith('dart:_')) {
-          _addSuggestion(lib.shortName,
-              relevance: lib.shortName == 'dart:core'
-                  ? DART_RELEVANCE_LOW
-                  : DART_RELEVANCE_DEFAULT);
+          int relevance;
+          if (request.useNewRelevance) {
+            relevance = lib.shortName == 'dart:core'
+                ? dartCoreRelevance
+                : defaultRelevance;
+          } else {
+            relevance = lib.shortName == 'dart:core'
+                ? DART_RELEVANCE_LOW
+                : DART_RELEVANCE_DEFAULT;
+          }
+          _addSuggestion(lib.shortName, relevance: relevance);
         }
       }
     }
   }
 
   void _addFileSuggestions(String partialUri) {
-    ResourceProvider resProvider = request.resourceProvider;
-    Context resContext = resProvider.pathContext;
-    Source source = request.source;
+    var resProvider = request.resourceProvider;
+    var resContext = resProvider.pathContext;
+    var source = request.source;
 
     String parentUri;
     if ((partialUri.endsWith('/'))) {
@@ -146,27 +153,27 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
         parentUri = '$parentUri/';
       }
     }
-    String uriPrefix = parentUri == '.' ? '' : parentUri;
+    var uriPrefix = parentUri == '.' ? '' : parentUri;
 
     // Only handle file uris in the format file:///xxx or /xxx
-    String parentUriScheme = Uri.parse(parentUri).scheme;
+    var parentUriScheme = Uri.parse(parentUri).scheme;
     if (!parentUri.startsWith('file://') && parentUriScheme != '') {
       return;
     }
 
-    String dirPath = resProvider.pathContext.fromUri(parentUri);
+    var dirPath = resProvider.pathContext.fromUri(parentUri);
     dirPath = resContext.normalize(dirPath);
 
     if (resContext.isRelative(dirPath)) {
-      String sourceDirPath = resContext.dirname(source.fullName);
+      var sourceDirPath = resContext.dirname(source.fullName);
       if (resContext.isAbsolute(sourceDirPath)) {
         dirPath = resContext.normalize(resContext.join(sourceDirPath, dirPath));
       } else {
         return;
       }
       // Do not suggest relative paths reaching outside the 'lib' directory.
-      bool srcInLib = resContext.split(sourceDirPath).contains('lib');
-      bool dstInLib = resContext.split(dirPath).contains('lib');
+      var srcInLib = resContext.split(sourceDirPath).contains('lib');
+      var dstInLib = resContext.split(dirPath).contains('lib');
       if (srcInLib && !dstInLib) {
         return;
       }
@@ -175,10 +182,10 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
       dirPath = dirPath.substring(0, dirPath.length - 1);
     }
 
-    Resource dir = resProvider.getResource(dirPath);
+    var dir = resProvider.getResource(dirPath);
     if (dir is Folder) {
       try {
-        for (Resource child in dir.getChildren()) {
+        for (var child in dir.getChildren()) {
           String completion;
           if (child is Folder) {
             if (!child.shortName.startsWith('.')) {
@@ -202,9 +209,9 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
   void _addPackageFolderSuggestions(
       String partial, String prefix, Folder folder) {
     try {
-      for (Resource child in folder.getChildren()) {
+      for (var child in folder.getChildren()) {
         if (child is Folder) {
-          String childPrefix = '$prefix${child.shortName}/';
+          var childPrefix = '$prefix${child.shortName}/';
           _addSuggestion(childPrefix);
           if (partial.startsWith(childPrefix)) {
             _addPackageFolderSuggestions(partial, childPrefix, child);
@@ -220,14 +227,14 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
   }
 
   void _addPackageSuggestions(String partial) {
-    SourceFactory factory = request.sourceFactory;
-    Map<String, List<Folder>> packageMap = factory.packageMap;
+    var factory = request.sourceFactory;
+    var packageMap = factory.packageMap;
     if (packageMap != null) {
       _addSuggestion('package:');
-      packageMap.forEach((String pkgName, List<Folder> folders) {
-        String prefix = 'package:$pkgName/';
+      packageMap.forEach((pkgName, folders) {
+        var prefix = 'package:$pkgName/';
         _addSuggestion(prefix);
-        for (Folder folder in folders) {
+        for (var folder in folders) {
           if (folder.exists) {
             _addPackageFolderSuggestions(partial, prefix, folder);
           }
@@ -236,8 +243,12 @@ class _UriSuggestionBuilder extends SimpleAstVisitor<void> {
     }
   }
 
-  void _addSuggestion(String completion,
-      {int relevance = DART_RELEVANCE_DEFAULT}) {
+  void _addSuggestion(String completion, {int relevance}) {
+    if (request.useNewRelevance) {
+      relevance ??= defaultRelevance;
+    } else {
+      relevance ??= DART_RELEVANCE_DEFAULT;
+    }
     suggestions.add(CompletionSuggestion(CompletionSuggestionKind.IMPORT,
         relevance, completion, completion.length, 0, false, false));
   }
