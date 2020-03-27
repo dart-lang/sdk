65a377b96732f9b23324f367dd2b101dcd9721b0
Mike Fairhurst <mfairhurst@google.com>
2020-03-27 18:54:47 +0000

    [analyzer] Remove deprecated language version API
    Change-Id: I10122c8860b7b4c7dfcea1d6f8829ad6be439e7e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/141300
    Commit-Queue: Mike Fairhurst <mfairhurst@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/ast/ast.dart
+++ pkg/analyzer/lib/src/dart/ast/ast.dart
@@ -7,7 +7,6 @@ import 'dart:math' as math;
 
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/language_version.dart';
 import 'package:analyzer/dart/ast/precedence.dart';
 import 'package:analyzer/dart/ast/syntactic_entity.dart';
 import 'package:analyzer/dart/ast/token.dart';
@@ -2024,12 +2023,6 @@ class CompilationUnitImpl extends AstNodeImpl implements CompilationUnit {
   /// The line information for this compilation unit.
   @override
   LineInfo lineInfo;
-
-  @override
-  LanguageVersion get languageVersion {
-    final token = languageVersionToken;
-    return LanguageVersion(token.major, token.minor);
-  }
 
   @override
   LanguageVersionToken get languageVersionToken {
