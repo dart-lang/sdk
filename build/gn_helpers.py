a441d28fbe8effe184c6c6385eca89b6e6c91979
Nemath Ahmed <nemath.ahmed14@gmail.com>
2020-04-08 00:15:46 +0530

    Update gn_helpers.py

--- build/gn_helpers.py
+++ build/gn_helpers.py
@@ -1,7 +1,7 @@
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Helper functions useful when writing scripts that are run from GN's
+"""Helper functions are useful when writing scripts that are run from GN's
 exec_script function."""
 
 
