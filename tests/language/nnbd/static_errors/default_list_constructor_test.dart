b135aab6d731bc8c78306c5225a17b63cb5b7670
Leaf Petersen <leafp@google.com>
2020-04-10 18:07:44 +0000

    Fix bad test expectation, convert to static error test
    Change-Id: I821dd3af8c1be346d983bd41bb80efefba176cd5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/143102
    Auto-Submit: Leaf Petersen <leafp@google.com>
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>

--- tests/language/nnbd/static_errors/default_list_constructor_test.dart
+++ tests/language/nnbd/static_errors/default_list_constructor_test.dart
@@ -2,22 +2,35 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// SharedOptions=--enable-experiment=non-nullable
-
-import 'opted_out_library.dart';
-
 // Test that it is an error to call the default List constructor.
 main() {
-  var a = new List<int>(3); //# 01: compile-time error
-  var b = new List<int?>(3); //# 02: compile-time-error
-  var c = new List<int>(); //# 03: compile-time error
-  var d = new List<int?>(); //# 04: compile-time error
-  List<C> c = new List(5); //# 05: compile-time error
-  consumeListOfStringStar(new List(3)); //# 06: compile-time error
+  var a = new List<int>(3);
+  //          ^^^^^^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
+  var b = new List<int?>(3);
+  //          ^^^^^^^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
+  var c = new List<int>();
+  //          ^^^^^^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
+  var d = new List<int?>();
+  //          ^^^^^^^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
+  List<C> e = new List(5);
+  //              ^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
 }
 
 class A<T> {
-  var l = new List<T>(3); //# 07: compile-time error
+  var l = new List<T>(3);
+  //          ^^^^^^^
+  // [analyzer] COMPILE_TIME_ERROR.DEFAULT_LIST_CONSTRUCTOR
+  // [cfe] Can't use the default List constructor.
 }
 
 class C {}
