e35120cfd14ed09302596e9fa20d8e845e8e2668
Zichang Guo <zichangguo@google.com>
2020-04-21 21:41:08 +0000

    [tests] rewrite and format several tests on http
    Resolve comments in previous cls.
    https://dart-review.googlesource.com/c/sdk/+/140564
    https://dart-review.googlesource.com/c/sdk/+/136322
    https://dart-review.googlesource.com/c/sdk/+/138860
    
    Change-Id: I9549019fe871f35187af5ff75df320748792498b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/141845
    Commit-Queue: Zichang Guo <zichangguo@google.com>
    Reviewed-by: Jonas Termansen <sortie@google.com>

--- tests/standalone/io/http_parser_connect_method_test.dart
+++ tests/standalone/io/http_parser_connect_method_test.dart
@@ -4,11 +4,13 @@
 
 import "dart:async";
 import "dart:io";
+
+import "package:async_helper/async_helper.dart";
 import "package:expect/expect.dart";
 
-test(String header, value) async {
+Future<void> test(String header, value) async {
   final connect = "CONNECT";
-  var server = await HttpServer.bind("127.0.0.1", 0);
+  final server = await HttpServer.bind("127.0.0.1", 0);
   server.listen((HttpRequest request) {
     Expect.equals(connect, request.method);
     request.response.statusCode = 200;
@@ -16,15 +18,25 @@ test(String header, value) async {
     request.response.close();
   });
 
-  var completer = Completer();
+  final completer = Completer<void>();
   HttpClient client = HttpClient();
   client
       .open(connect, "127.0.0.1", server.port, "/")
       .then((HttpClientRequest request) {
     return request.close();
   }).then((HttpClientResponse response) {
-    Expect.isTrue(response.statusCode == 200);
-    Expect.isNull(response.headers[header]);
+    Expect.equals(200, response.statusCode);
+    // Headers except Content-Length and Transfer-Encoding header will be read.
+    if (header == HttpHeaders.contentLengthHeader ||
+        header == HttpHeaders.transferEncodingHeader) {
+      Expect.isNull(response.headers[header]);
+    } else {
+      final list = response.headers[header];
+      Expect.isNotNull(list);
+      Expect.equals(1, list!.length);
+      Expect.equals(value, list[0]);
+    }
+
     client.close();
     server.close();
     completer.complete();
@@ -33,7 +45,12 @@ test(String header, value) async {
   await completer.future;
 }
 
-main() async {
+Future<void> runTests() async {
   await test(HttpHeaders.contentLengthHeader, 0);
   await test(HttpHeaders.transferEncodingHeader, 'chunked');
+  await test('testHeader', 'testValue');
+}
+
+main() {
+  asyncTest(runTests);
 }
--- tests/standalone_2/io/http_parser_connect_method_test.dart
+++ tests/standalone_2/io/http_parser_connect_method_test.dart
@@ -4,11 +4,13 @@
 
 import "dart:async";
 import "dart:io";
+
+import "package:async_helper/async_helper.dart";
 import "package:expect/expect.dart";
 
-test(String header, value) async {
+Future<void> test(String header, value) async {
   final connect = "CONNECT";
-  var server = await HttpServer.bind("127.0.0.1", 0);
+  final server = await HttpServer.bind("127.0.0.1", 0);
   server.listen((HttpRequest request) {
     Expect.equals(connect, request.method);
     request.response.statusCode = 200;
@@ -16,15 +18,24 @@ test(String header, value) async {
     request.response.close();
   });
 
-  var completer = Completer();
+  final completer = Completer<void>();
   HttpClient client = HttpClient();
   client
       .open(connect, "127.0.0.1", server.port, "/")
       .then((HttpClientRequest request) {
     return request.close();
   }).then((HttpClientResponse response) {
-    Expect.isTrue(response.statusCode == 200);
-    Expect.isNull(response.headers[header]);
+    Expect.equals(200, response.statusCode);
+    // Headers except Content-Length and Transfer-Encoding header will be read.
+    if (header == HttpHeaders.contentLengthHeader ||
+        header == HttpHeaders.transferEncodingHeader) {
+      Expect.isNull(response.headers[header]);
+    } else {
+      Expect.isNotNull(response.headers[header]);
+      Expect.equals(1, response.headers[header].length);
+      Expect.equals(value, response.headers[header][0]);
+    }
+
     client.close();
     server.close();
     completer.complete();
@@ -33,7 +44,12 @@ test(String header, value) async {
   await completer.future;
 }
 
-main() async {
+Future<void> runTests() async {
   await test(HttpHeaders.contentLengthHeader, 0);
   await test(HttpHeaders.transferEncodingHeader, 'chunked');
+  await test('testHeader', 'testValue');
+}
+
+main() {
+  asyncTest(runTests);
 }
