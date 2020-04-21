de6cfe7e1589d657cd6e797402ce53bbef8a26f9
Ben Konyi <bkonyi@google.com>
2020-04-21 00:09:04 +0000

    [ VM ] Fixed issue where resolving symlinks while trying to get the executable directory path would fail resulting in a "No such file" error
    As part of adding support for brew installations, a bug was introduced
    which resulted in the executable directory path not being resolved
    correctly if the process was launched via a symlink. This resulted in
    the script path passed to the CFE not being fully qualified relative to
    the executable location which would cause lookup failures for relative
    script paths.
    
    Fixes https://github.com/dart-lang/sdk/issues/41329
    
    Change-Id: I31813ccbc024eb0e4dd6dc265986ba3aefd9898e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/144005
    Commit-Queue: Ben Konyi <bkonyi@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>

--- tests/standalone_2/regress_41329_absolute_test.dart
+++ tests/standalone_2/regress_41329_absolute_test.dart
@@ -5,17 +5,18 @@
 import 'dart:io';
 
 import 'package:async_helper/async_helper.dart';
+import 'package:path/path.dart' as p;
 import 'package:expect/expect.dart';
 
 Future<void> main() async {
   final origDir = Directory.current;
   final exePath = Platform.resolvedExecutable;
-  var d = Directory.systemTemp.createTempSync('dart_symlink');
+  final d = Directory.systemTemp.createTempSync('dart_symlink');
 
   // Roughly emulate a Brew installation:
   //   - $BREW/bin/dart -> ../Cellar/dart/2.8.0-dev.20.0/bin/dart
   //   - $BREW/Cellar/dart/2.8.0-dev.20.0/bin/dart -> $DART_SDK/bin/dart
-  var a = Directory('${d.path}/usr/local/bin');
+  final a = Directory('${d.path}/usr/local/bin');
   a.createSync(recursive: true);
 
   // /usr/local/Cellar/dart/2.8.0-dev.20.0/bin/dart -> $DART_SDK/bin/dart
@@ -26,11 +27,10 @@ Future<void> main() async {
 
   // /usr/local/bin/dart -> /usr/local/Cellar/dart/2.8.0-dev.20/bin/dart
   final link2 = Link('dart')..createSync(linkLocation, recursive: true);
-  final path = link2.absolute.path;
-
+  final path = Uri.parse(link2.absolute.path).path;
   Directory.current = origDir;
   final result = await Process.run('${path}',
-      [Platform.script.resolve('regress_41329_helper.dart').toString()]);
+      [p.relative(Platform.script.resolve('regress_41329_helper.dart').path)]);
   Expect.equals(result.stdout, '');
   Expect.equals(result.stderr, '');
   Expect.equals(result.exitCode, 42);
