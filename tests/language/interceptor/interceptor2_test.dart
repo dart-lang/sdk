9c2075be49d0537cd4664922df021b6c3091caba
Robert Nystrom <rnystrom@google.com>
2020-05-26 16:04:15 +0000

    Migrate language_2/interceptor to NNBD.
    Change-Id: Ibbc89b68ef980e1ff9913c299c05d5117b249477
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/148949
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Erik Ernst <eernst@google.com>
    Reviewed-by: Erik Ernst <eernst@google.com>

---
+++ tests/language/interceptor/interceptor2_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+// Regression test for issue http://dartbug.com/6903: dart2js used to
+// not generate an interceptor forwarder when a getter call and a
+// method call on an intercepted method were both used.
+
+class A {
+  get iterator => () => 499;
+}
+
+main() {
+  var a = <dynamic>[
+    new A(),
+    [1, 1]
+  ];
+  Expect.equals(499, a[0].iterator());
+  Expect.equals(499, (a[0].iterator)());
+  for (var i in a[1]) {
+    Expect.equals(1, i);
+  }
+}
