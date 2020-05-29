89c7c0b6e048c1ea0a438c010ee40c74b683e018
Robert Nystrom <rnystrom@google.com>
2020-05-29 19:23:24 +0000

    Migrate language_2/lazy to NNBD.
    Change-Id: Icd7ea40455f207eca1dc817ff86823cfabed3de0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/149042
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Leaf Petersen <leafp@google.com>

---
+++ tests/language/lazy/static8_test.dart
@@ -0,0 +1,76 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+// Test re-entrant initializer - calls throw StackOverflowError.
+
+var trace;
+
+final foo = bar;
+
+var recursionDepth = 0;
+
+get bar {
+  if (recursionDepth > 3) throw "recursed";
+  recursionDepth++;
+
+  trace.add(recursionDepth);
+  try {
+    return foo ?? 1;
+  } catch (e) {
+    trace.add(e);
+  }
+
+  return 42;
+}
+
+void testTopLevel() {
+  trace = [];
+  recursionDepth = 0;
+  var result = foo;
+  Expect.equals(42, result);
+  Expect.equals('1,2,3,4,recursed', trace.join(','));
+  trace = [];
+  recursionDepth = 0;
+  result = foo;
+  Expect.equals(42, result);
+  Expect.equals('', trace.join(','));
+}
+
+class X {
+  static final foo = X.bar;
+
+  static get bar {
+    if (recursionDepth > 3) throw "recursed";
+    recursionDepth++;
+
+    trace.add(recursionDepth);
+    try {
+      return foo ?? 1;
+    } catch (e) {
+      trace.add(e);
+    }
+
+    return 49;
+  }
+}
+
+void testClassStatic() {
+  trace = [];
+  recursionDepth = 0;
+  var result = X.foo;
+  Expect.equals(49, result);
+  Expect.equals('1,2,3,4,recursed', trace.join(','));
+  trace = [];
+  recursionDepth = 0;
+  result = X.foo;
+  Expect.equals(49, result);
+  Expect.equals('', trace.join(','));
+}
+
+main() {
+  testTopLevel();
+  testClassStatic();
+}
