dart-lang/sdk

commit 72e559929f7c6db5f38a9b2ad52df67aec624dfc

[ddc] Add --sound-null-safety flag and pass to CFE
https://dart-review.googlesource.com/c/sdk/+/149080 is required to
get errors if you try use weak .dill files when passing the
--sound-null-safety flag.

Added a new build target to copy the sound SDK outline .dill to
dart-sdk/lib/_internal. This is only part of dartdevc_test right
now. We will decide in the future if we are going to include this
in the released SDK or if we will have the build systems create
them as needed. For now in testing scenarios it appears to be part
of the SDK.

Change-Id: I3b31a894841f59b437658104e7ee9443ee67db93
Fixes: https://github.com/dart-lang/sdk/issues/41694
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/146962
Commit-Queue: Nicholas Shahan <nshahan@google.com>
Reviewed-by: Sigmund Cherem <sigmund@google.com>
Reviewed-by: Bob Nystrom <rnystrom@google.com>
Reviewed-by: Johnni Winther <johnniwinther@google.com>

pkg/front_end/lib/src/api_unstable/modular_incremental_compilation.dart
@@ -14,6 +14,8 @@ import '../api_prototype/experimental_flags.dart' show ExperimentalFlag;
 
 import '../api_prototype/file_system.dart' show FileSystem;
 
+import '../base/nnbd_mode.dart' show NnbdMode;
+
 import '../base/processed_options.dart' show ProcessedOptions;
 
 import '../fasta/compiler_context.dart' show CompilerContext;
@@ -54,7 +56,8 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
     bool outlineOnly,
     bool omitPlatform: false,
     bool trackNeededDillLibraries: false,
-    bool verbose: false}) async {
+    bool verbose: false,
+    NnbdMode nnbdMode: NnbdMode.Weak}) async {
   bool isRetry = false;
   while (true) {
     try {
@@ -78,6 +81,7 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
           oldState.incrementalCompiler == null ||
           oldState.options.compileSdk != compileSdk ||
           oldState.incrementalCompiler.outlineOnly != outlineOnly ||
+          oldState.options.nnbdMode != options.nnbdMode ||
           !equalMaps(oldState.options.experimentalFlags, experimentalFlags) ||
           !equalMaps(oldState.options.environmentDefines, environmentDefines) ||
           !equalSets(oldState.tags, tags) ||
@@ -100,7 +104,8 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
           ..omitPlatform = omitPlatform
           ..environmentDefines = environmentDefines
           ..experimentalFlags = experimentalFlags
-          ..verbose = verbose;
+          ..verbose = verbose
+          ..nnbdMode = nnbdMode;
 
         processedOpts = new ProcessedOptions(options: options);
         cachedSdkInput = new WorkerInputComponent(

pkg/dev_compiler/lib/src/compiler/shared_command.dart
@@ -84,6 +84,8 @@ class SharedCompilerOptions {
   /// for more details.
   final Map<String, bool> experiments;
 
+  final bool soundNullSafety;
+
   /// The name of the module.
   ///
   /// This used when to support file concatenation. The JS module will contain
@@ -100,7 +102,8 @@ class SharedCompilerOptions {
       this.summaryModules = const {},
       this.moduleFormats = const [],
       this.experiments = const {},
-      this.moduleName});
+      this.moduleName,
+      this.soundNullSafety = false});
 
   SharedCompilerOptions.fromArguments(ArgResults args,
       [String moduleRoot, String summaryExtension])
@@ -115,7 +118,8 @@ class SharedCompilerOptions {
                 args['summary'] as List<String>, moduleRoot, summaryExtension),
             moduleFormats: parseModuleFormatOption(args),
             moduleName: _getModuleName(args, moduleRoot),
-            replCompile: args['repl-compile'] as bool);
+            replCompile: args['repl-compile'] as bool,
+            soundNullSafety: args['sound-null-safety'] as bool);
 
   static void addArguments(ArgParser parser, {bool hide = true}) {
     addModuleFormatOptions(parser, hide: hide);
@@ -144,7 +148,11 @@ class SharedCompilerOptions {
               ' to private members across library boundaries. This should'
               ' only be used by debugging tools.',
           defaultsTo: false,
-          hide: hide);
+          hide: hide)
+      ..addFlag('sound-null-safety',
+          help: 'Compile for sound null safety at runtime.',
+          negatable: true,
+          defaultsTo: false);
   }
 
   static String _getModuleName(ArgResults args, String moduleRoot) {

pkg/dev_compiler/lib/src/kernel/command.dart
@@ -204,7 +204,8 @@ Future<CompilerResult> _compile(List<String> args,
   var sdkSummaryPath = argResults['dart-sdk-summary'] as String;
   var librarySpecPath = argResults['libraries-file'] as String;
   if (sdkSummaryPath == null) {
-    sdkSummaryPath = defaultSdkSummaryPath;
+    sdkSummaryPath =
+        defaultSdkSummaryPath(soundNullSafety: options.soundNullSafety);
     librarySpecPath ??= defaultLibrarySpecPath;
   }
   var invalidSummary = summaryPaths.any((s) => !s.endsWith('.dill')) ||
@@ -281,7 +282,9 @@ Future<CompilerResult> _compile(List<String> args,
             enableNullSafety: options.enableNullSafety)),
         fileSystem: fileSystem,
         experiments: experiments,
-        environmentDefines: declaredVariables);
+        environmentDefines: declaredVariables,
+        nnbdMode:
+            options.soundNullSafety ? fe.NnbdMode.Strong : fe.NnbdMode.Weak);
   } else {
     // If digests weren't given and if not in worker mode, create fake data and
     // ensure we don't have a previous state (as that wouldn't be safe with
@@ -319,7 +322,9 @@ Future<CompilerResult> _compile(List<String> args,
         fileSystem: fileSystem,
         experiments: experiments,
         environmentDefines: declaredVariables,
-        trackNeededDillLibraries: recordUsedInputs);
+        trackNeededDillLibraries: recordUsedInputs,
+        nnbdMode:
+            options.soundNullSafety ? fe.NnbdMode.Strong : fe.NnbdMode.Weak);
     incrementalCompiler = compilerState.incrementalCompiler;
     cachedSdkInput =
         compilerState.workerInputCache[sourcePathToUri(sdkSummaryPath)];
@@ -692,9 +697,12 @@ Map<String, String> parseAndRemoveDeclaredVariables(List<String> args) {
   return declaredVariables;
 }
 
-/// The default path of the kernel summary for the Dart SDK.
-final defaultSdkSummaryPath =
-    p.join(getSdkPath(), 'lib', '_internal', 'ddc_sdk.dill');
+/// The default path of the kernel summary for the Dart SDK given the
+/// [soundNullSafety] mode.
+String defaultSdkSummaryPath({bool soundNullSafety}) {
+  var outlineDill = soundNullSafety ? 'ddc_outline_sound.dill' : 'ddc_sdk.dill';
+  return p.join(getSdkPath(), 'lib', '_internal', outlineDill);
+}
 
 final defaultLibrarySpecPath = p.join(getSdkPath(), 'lib', 'libraries.json');
 

pkg/dev_compiler/test/nullable_inference_test.dart
@@ -644,7 +644,8 @@ Future<CompileResult> kernelCompile(String code) async {
   var sdkUri = Uri.file('/memory/dart_sdk.dill');
   var sdkFile = _fileSystem.entityForUri(sdkUri);
   if (!await sdkFile.exists()) {
-    sdkFile.writeAsBytesSync(File(defaultSdkSummaryPath).readAsBytesSync());
+    sdkFile.writeAsBytesSync(
+        File(defaultSdkSummaryPath(soundNullSafety: false)).readAsBytesSync());
   }
   var packagesUri = Uri.file('/memory/.packages');
   var packagesFile = _fileSystem.entityForUri(packagesUri);

pkg/dev_compiler/test/sourcemap/sourcemaps_ddk_suite.dart
@@ -56,7 +56,7 @@ class DevCompilerRunner implements CompilerRunner {
     var sdkJsFile = findInOutDir('gen/utils/dartdevc/kernel/es6/dart_sdk.js');
     var jsSdkPath = sdkJsFile.uri;
 
-    var ddcSdkSummary = findInOutDir('ddc_sdk.dill');
+    var ddcSdkSummary = findInOutDir('ddc_outline.dill');
 
     var args = <String>[
       "--packages=${sdkRoot.uri.resolve(".packages").toFilePath()}",

pkg/dev_compiler/tool/ddb
@@ -207,18 +207,19 @@ void main(List<String> args) async {
       break;
   }
 
-  String sdkJsPath;
-  String requirePath;
   String ddcSdk;
   if (debug) {
     var sdkRoot = p.dirname(p.dirname(ddcPath));
     var buildDir = p.join(sdkRoot, Platform.isMacOS ? 'xcodebuild' : 'out');
     dartSdk = p.join(buildDir, 'ReleaseX64', 'dart-sdk');
+    ddcSdk = p.join(buildDir, 'ReleaseX64',
+        isNnbdStrong ? 'ddc_outline_sound.dill' : 'ddc_outline.dill');
+  } else {
+    ddcSdk = p.join(dartSdk, 'lib', '_internal', 'ddc_sdk.dill');
   }
   var suffix = p.join('kernel', mod);
-  sdkJsPath = p.join(dartSdk, 'lib', 'dev_compiler', suffix);
-  requirePath = sdkJsPath;
-  ddcSdk = p.join(dartSdk, 'lib', '_internal', 'ddc_sdk.dill');
+  var sdkJsPath = p.join(dartSdk, 'lib', 'dev_compiler', suffix);
+  var requirePath = sdkJsPath;
 
   // Print an initial empty line to separate the invocation from the output.
   print('');
@@ -231,6 +232,7 @@ void main(List<String> args) async {
       '--dart-sdk-summary=$ddcSdk',
       for (var summary in summaries) '--summary=$summary',
       for (var experiment in experiments) '--enable-experiment=$experiment',
+      if (isNnbdStrong) '--sound-null-safety',
       if (options['packages'] != null) '--packages=${options['packages']}',
       '-o',
       out,

pkg/front_end/lib/src/api_unstable/ddc.dart
@@ -25,6 +25,8 @@ import '../api_prototype/standard_file_system.dart' show StandardFileSystem;
 
 import '../base/processed_options.dart' show ProcessedOptions;
 
+import '../base/nnbd_mode.dart' show NnbdMode;
+
 import '../kernel_generator_impl.dart' show generateKernel;
 
 import 'compiler_state.dart' show InitializedCompilerState;
@@ -57,6 +59,8 @@ export '../api_prototype/terminal_color_support.dart'
     show printDiagnosticMessage;
 
 export '../base/processed_options.dart' show ProcessedOptions;
+
+export '../base/nnbd_mode.dart' show NnbdMode;
 
 export '../fasta/compiler_context.dart' show CompilerContext;
 
@@ -110,7 +114,8 @@ Future<InitializedCompilerState> initializeCompiler(
     Target target,
     {FileSystem fileSystem,
     Map<ExperimentalFlag, bool> experiments,
-    Map<String, String> environmentDefines}) async {
+    Map<String, String> environmentDefines,
+    NnbdMode nnbdMode}) async {
   additionalDills.sort((a, b) => a.toString().compareTo(b.toString()));
 
   if (oldState != null &&
@@ -118,6 +123,7 @@ Future<InitializedCompilerState> initializeCompiler(
       oldState.options.sdkSummary == sdkSummary &&
       oldState.options.packagesFileUri == packagesFile &&
       oldState.options.librariesSpecificationUri == librariesSpecificationUri &&
+      oldState.options.nnbdMode == nnbdMode &&
       equalLists(oldState.options.additionalDills, additionalDills) &&
       equalMaps(oldState.options.experimentalFlags, experiments) &&
       equalMaps(oldState.options.environmentDefines, environmentDefines)) {
@@ -134,7 +140,8 @@ Future<InitializedCompilerState> initializeCompiler(
     ..librariesSpecificationUri = librariesSpecificationUri
     ..target = target
     ..fileSystem = fileSystem ?? StandardFileSystem.instance
-    ..environmentDefines = environmentDefines;
+    ..environmentDefines = environmentDefines
+    ..nnbdMode = nnbdMode;
   if (experiments != null) options.experimentalFlags = experiments;
 
   ProcessedOptions processedOpts = new ProcessedOptions(options: options);
@@ -161,7 +168,8 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
     {FileSystem fileSystem,
     Map<ExperimentalFlag, bool> experiments,
     Map<String, String> environmentDefines,
-    bool trackNeededDillLibraries: false}) async {
+    bool trackNeededDillLibraries: false,
+    NnbdMode nnbdMode}) async {
   return modular.initializeIncrementalCompiler(
       oldState,
       tags,
@@ -180,7 +188,8 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
           environmentDefines ?? const <ExperimentalFlag, bool>{},
       outlineOnly: false,
       omitPlatform: false,
-      trackNeededDillLibraries: trackNeededDillLibraries);
+      trackNeededDillLibraries: trackNeededDillLibraries,
+      nnbdMode: nnbdMode);
 }
 
 Future<DdcResult> compile(InitializedCompilerState compilerState,

pkg/test_runner/lib/src/compiler_configuration.dart
@@ -508,6 +508,8 @@ class DevCompilerConfiguration extends CompilerConfiguration {
       ..._configuration.sharedOptions,
       ..._experimentsArgument(_configuration, testFile),
       ...testFile.ddcOptions,
+      if (_configuration.nnbdMode == NnbdMode.strong)
+        '--sound-null-safety',
       // The file being compiled is the last argument.
       args.last
     ];
@@ -521,7 +523,9 @@ class DevCompilerConfiguration extends CompilerConfiguration {
       //
       // For local development we don't have a built SDK yet, so point directly
       // at the built summary file location.
-      var sdkSummaryFile = 'ddc_sdk.dill';
+      var sdkSummaryFile = _configuration.nnbdMode == NnbdMode.strong
+          ? 'ddc_outline_sound.dill'
+          : 'ddc_outline.dill';
       var sdkSummary = Path(_configuration.buildDirectory)
           .append(sdkSummaryFile)
           .absolute
@@ -541,6 +545,8 @@ class DevCompilerConfiguration extends CompilerConfiguration {
 
     // Link to the summaries for the available packages, so that they don't
     // get recompiled into the test's own module.
+    var packageSummaryDir =
+        _configuration.nnbdMode == NnbdMode.strong ? 'pkg_sound' : 'pkg_kernel';
     for (var package in testPackages) {
       args.add("-s");
 
@@ -548,7 +554,7 @@ class DevCompilerConfiguration extends CompilerConfiguration {
       // dartdevc explicit module paths for each one. When the test is run, we
       // will tell require.js where to find each package's compiled JS.
       var summary = Path(_configuration.buildDirectory)
-          .append("/gen/utils/dartdevc/pkg_kernel/$package.dill")
+          .append("/gen/utils/dartdevc/$packageSummaryDir/$package.dill")
           .absolute
           .toNativePath();
       args.add("$summary=$package");

commit 1ad89b85a2ee82e42c3b522069208892f5e7a1e7

[gardening] Read nnbdMode parameter instead of field on null options.
Fixes crash introduced by https://github.com/dart-lang/sdk/commit/72e559929f7c6db5f38a9b2ad52df67aec624dfc.

Change-Id: Ie9e3cdbcd282a840dcd028661fa93e49d207d38f
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/149421
Commit-Queue: Aske Simon Christensen <askesc@google.com>
Commit-Queue: Johnni Winther <johnniwinther@google.com>
Auto-Submit: Aske Simon Christensen <askesc@google.com>
Reviewed-by: Johnni Winther <johnniwinther@google.com>

pkg/front_end/lib/src/api_unstable/modular_incremental_compilation.dart
@@ -81,7 +81,7 @@ Future<InitializedCompilerState> initializeIncrementalCompiler(
           oldState.incrementalCompiler == null ||
           oldState.options.compileSdk != compileSdk ||
           oldState.incrementalCompiler.outlineOnly != outlineOnly ||
-          oldState.options.nnbdMode != options.nnbdMode ||
+          oldState.options.nnbdMode != nnbdMode ||
           !equalMaps(oldState.options.experimentalFlags, experimentalFlags) ||
           !equalMaps(oldState.options.environmentDefines, environmentDefines) ||
           !equalSets(oldState.tags, tags) ||