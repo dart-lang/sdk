9d8087a399ebe62f6a93f21ec20f56d39dbcf1a2
Paul Berry <paulberry@google.com>
2020-06-01 20:53:58 +0000

    Combine all "verify sorted" tests.
    The tests in analyzer_cli and analyzer_plugin imported
    analysis_server, resulting in warnings when publishing
    analyzer_plugin.  combining these tests all into analysis_server
    shares more code and avoids the warning.
    
    Change-Id: I519f67872500e93e399c5c3d2aeddf781f07602c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/149610
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Paul Berry <paulberry@google.com>

--- pkg/analysis_server/test/verify_sorted_test.dart
+++ pkg/analysis_server/test/verify_sorted_test.dart
@@ -17,6 +17,33 @@ import 'utils/package_root.dart';
 void main() {
   var provider = PhysicalResourceProvider.INSTANCE;
   var normalizedRoot = provider.pathContext.normalize(packageRoot);
+  group('analysis_server',
+      () => buildTestsForAnalysisServer(provider, normalizedRoot));
+  group(
+      'analyzer_cli', () => buildTestsForAnalyzerCli(provider, normalizedRoot));
+  group('analyzer_plugin',
+      () => buildTestsForAnalyzerPlugin(provider, normalizedRoot));
+}
+
+void buildTests(String packagePath, String excludeFolderPath,
+    List<String> generatedFilePaths, PhysicalResourceProvider provider) {
+  var collection = AnalysisContextCollection(includedPaths: <String>[
+    packagePath
+  ], excludedPaths: [
+    if (excludeFolderPath != null) excludeFolderPath,
+    ...generatedFilePaths
+  ], resourceProvider: provider);
+  var contexts = collection.contexts;
+  if (contexts.length != 1) {
+    fail('The directory $packagePath contains multiple analysis contexts.');
+  }
+
+  buildTestsIn(contexts[0].currentSession, packagePath, generatedFilePaths,
+      excludeFolderPath, provider.getFolder(packagePath));
+}
+
+void buildTestsForAnalysisServer(
+    PhysicalResourceProvider provider, String normalizedRoot) {
   var packagePath =
       provider.pathContext.join(normalizedRoot, 'analysis_server');
   var mockPackagesPath =
@@ -48,37 +75,51 @@ void main() {
         packagePath, 'test', 'services', 'completion', 'dart', 'test_all.dart'),
   ];
 
-  var collection = AnalysisContextCollection(
-      includedPaths: <String>[packagePath],
-      excludedPaths: [mockPackagesPath, ...generatedFilePaths],
-      resourceProvider: provider);
-  var contexts = collection.contexts;
-  if (contexts.length != 1) {
-    fail('The directory $packagePath contains multiple analysis contexts.');
-  }
-
-  buildTestsIn(contexts[0].currentSession, packagePath, generatedFilePaths,
-      mockPackagesPath, provider.getFolder(packagePath));
+  buildTests(packagePath, mockPackagesPath, generatedFilePaths, provider);
 }
 
-void buildTestsIn(
-    AnalysisSession session,
-    String testDirPath,
-    List<String> generatedFilePaths,
-    String mockPackagesPath,
-    Folder directory) {
+void buildTestsForAnalyzerCli(
+    PhysicalResourceProvider provider, String normalizedRoot) {
+  var packagePath = provider.pathContext.join(normalizedRoot, 'analyzer_cli');
+  var testDataPath = provider.pathContext.join(packagePath, 'test', 'data');
+
+  buildTests(packagePath, testDataPath, [], provider);
+}
+
+void buildTestsForAnalyzerPlugin(
+    PhysicalResourceProvider provider, String normalizedRoot) {
+  var packagePath =
+      provider.pathContext.join(normalizedRoot, 'analyzer_plugin');
+  // TODO(brianwilkerson) Fix the generator to sort the generated files and
+  //  remove these exclusions.
+  var generatedFilePaths = [
+    provider.pathContext
+        .join(packagePath, 'lib', 'protocol', 'protocol_common.dart'),
+    provider.pathContext
+        .join(packagePath, 'lib', 'protocol', 'protocol_generated.dart'),
+    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
+        'integration_test_methods.dart'),
+    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
+        'protocol_matchers.dart'),
+  ];
+
+  buildTests(packagePath, null, generatedFilePaths, provider);
+}
+
+void buildTestsIn(AnalysisSession session, String testDirPath,
+    List<String> excludeFilePaths, String excludeFolderPath, Folder directory) {
   var pathContext = session.resourceProvider.pathContext;
   var children = directory.getChildren();
   children.sort((first, second) => first.shortName.compareTo(second.shortName));
   for (var child in children) {
     if (child is Folder) {
-      if (child.path != mockPackagesPath) {
+      if (child.path != excludeFolderPath) {
         buildTestsIn(
-            session, testDirPath, generatedFilePaths, mockPackagesPath, child);
+            session, testDirPath, excludeFilePaths, excludeFolderPath, child);
       }
     } else if (child is File && child.shortName.endsWith('.dart')) {
       var path = child.path;
-      if (generatedFilePaths.contains(path)) {
+      if (excludeFilePaths.contains(path)) {
         continue;
       }
       var relativePath = pathContext.relative(path, from: testDirPath);
