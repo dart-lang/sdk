c0560de8aeecf621d8c03e8935c09d6c454a14b6
Konstantin Shcheglov <scheglov@google.com>
2020-06-02 15:46:21 +0000

    Clean up verify_sorted_test.
    Change-Id: I0fe51c2ab2ca5edebb2625fb21c4fa7a2883bf94
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/149682
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/test/verify_sorted_test.dart
+++ pkg/analysis_server/test/verify_sorted_test.dart
@@ -10,116 +10,114 @@ import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/analysis/session.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
 import 'utils/package_root.dart';
 
 void main() {
-  var provider = PhysicalResourceProvider.INSTANCE;
-  var normalizedRoot = provider.pathContext.normalize(packageRoot);
-  group('analysis_server',
-      () => buildTestsForAnalysisServer(provider, normalizedRoot));
-  group(
-      'analyzer_cli', () => buildTestsForAnalyzerCli(provider, normalizedRoot));
-  group('analyzer_plugin',
-      () => buildTestsForAnalyzerPlugin(provider, normalizedRoot));
+  group('analysis_server', () {
+    buildTestsForAnalysisServer();
+  });
+
+  group('analyzer_cli', () {
+    buildTestsForAnalyzerCli();
+  });
+
+  group('analyzer_plugin', () {
+    buildTestsForAnalyzerPlugin();
+  });
 }
 
-void buildTests(String packagePath, String excludeFolderPath,
-    List<String> generatedFilePaths, PhysicalResourceProvider provider) {
-  var collection = AnalysisContextCollection(includedPaths: <String>[
-    packagePath
-  ], excludedPaths: [
-    if (excludeFolderPath != null) excludeFolderPath,
-    ...generatedFilePaths
-  ], resourceProvider: provider);
+void buildTests({
+  @required String packagePath,
+  @required List<String> excludedPaths,
+}) {
+  var provider = PhysicalResourceProvider.INSTANCE;
+  var pkgRootPath = provider.pathContext.normalize(packageRoot);
+
+  packagePath = _toPlatformPath(pkgRootPath, packagePath);
+  excludedPaths = excludedPaths.map((e) {
+    return _toPlatformPath(packagePath, e);
+  }).toList();
+
+  var collection = AnalysisContextCollection(
+    includedPaths: <String>[packagePath],
+    excludedPaths: excludedPaths,
+    resourceProvider: provider,
+  );
   var contexts = collection.contexts;
   if (contexts.length != 1) {
     fail('The directory $packagePath contains multiple analysis contexts.');
   }
 
-  buildTestsIn(contexts[0].currentSession, packagePath, generatedFilePaths,
-      excludeFolderPath, provider.getFolder(packagePath));
+  buildTestsIn(contexts[0].currentSession, packagePath, excludedPaths,
+      provider.getFolder(packagePath));
 }
 
-void buildTestsForAnalysisServer(
-    PhysicalResourceProvider provider, String normalizedRoot) {
-  var packagePath =
-      provider.pathContext.join(normalizedRoot, 'analysis_server');
-  var mockPackagesPath =
-      provider.pathContext.join(packagePath, 'test', 'mock_packages');
-  // TODO(brianwilkerson) Fix the generator to sort the generated files and
-  //  remove these exclusions.
-  var generatedFilePaths = <String>[
-    provider.pathContext.join(
-        packagePath, 'lib', 'lsp_protocol', 'protocol_custom_generated.dart'),
-    provider.pathContext
-        .join(packagePath, 'lib', 'lsp_protocol', 'protocol_generated.dart'),
-    provider.pathContext
-        .join(packagePath, 'lib', 'protocol', 'protocol_constants.dart'),
-    provider.pathContext
-        .join(packagePath, 'lib', 'protocol', 'protocol_generated.dart'),
-    provider.pathContext.join(packagePath, 'lib', 'src', 'edit',
-        'nnbd_migration', 'resources', 'resources.g.dart'),
-    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
-        'integration_test_methods.dart'),
-    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
-        'protocol_matchers.dart'),
+void buildTestsForAnalysisServer() {
+  var excludedPaths = <String>[
+    'test/mock_packages',
+    // TODO(brianwilkerson) Fix the generator to sort the generated files and
+    //  remove these exclusions.
+    'lib/lsp_protocol/protocol_custom_generated.dart',
+    'lib/lsp_protocol/protocol_generated.dart',
+    'lib/protocol/protocol_constants.dart',
+    'lib/protocol/protocol_generated.dart',
+    'lib/src/edit/nnbd_migration/resources/resources.g.dart',
+    'test/integration/support/integration_test_methods.dart',
+    'test/integration/support/protocol_matchers.dart',
     // The following are not generated, but can't be sorted because the contain
     // ignore comments in the directives, which sorting deletes.
-    provider.pathContext
-        .join(packagePath, 'lib', 'src', 'edit', 'edit_domain.dart'),
-    provider.pathContext
-        .join(packagePath, 'lib', 'src', 'services', 'kythe', 'schema.dart'),
-    provider.pathContext.join(
-        packagePath, 'test', 'services', 'completion', 'dart', 'test_all.dart'),
+    'lib/src/edit/edit_domain.dart',
+    'lib/src/services/kythe/schema.dart',
+    'test/services/completion/dart/test_all.dart',
   ];
 
-  buildTests(packagePath, mockPackagesPath, generatedFilePaths, provider);
+  buildTests(
+    packagePath: 'analysis_server',
+    excludedPaths: excludedPaths,
+  );
 }
 
-void buildTestsForAnalyzerCli(
-    PhysicalResourceProvider provider, String normalizedRoot) {
-  var packagePath = provider.pathContext.join(normalizedRoot, 'analyzer_cli');
-  var testDataPath = provider.pathContext.join(packagePath, 'test', 'data');
-
-  buildTests(packagePath, testDataPath, [], provider);
+void buildTestsForAnalyzerCli() {
+  buildTests(
+    packagePath: 'analyzer_cli',
+    excludedPaths: [
+      'test/data',
+    ],
+  );
 }
 
-void buildTestsForAnalyzerPlugin(
-    PhysicalResourceProvider provider, String normalizedRoot) {
-  var packagePath =
-      provider.pathContext.join(normalizedRoot, 'analyzer_plugin');
+void buildTestsForAnalyzerPlugin() {
   // TODO(brianwilkerson) Fix the generator to sort the generated files and
   //  remove these exclusions.
-  var generatedFilePaths = [
-    provider.pathContext
-        .join(packagePath, 'lib', 'protocol', 'protocol_common.dart'),
-    provider.pathContext
-        .join(packagePath, 'lib', 'protocol', 'protocol_generated.dart'),
-    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
-        'integration_test_methods.dart'),
-    provider.pathContext.join(packagePath, 'test', 'integration', 'support',
-        'protocol_matchers.dart'),
+  var excludedPaths = <String>[
+    'lib/protocol/protocol_common.dart',
+    'lib/protocol/protocol_generated.dart',
+    'test/integration/support/integration_test_methods.dart',
+    'test/integration/support/protocol_matchers.dart',
   ];
 
-  buildTests(packagePath, null, generatedFilePaths, provider);
+  buildTests(
+    packagePath: 'analyzer_plugin',
+    excludedPaths: excludedPaths,
+  );
 }
 
 void buildTestsIn(AnalysisSession session, String testDirPath,
-    List<String> excludeFilePaths, String excludeFolderPath, Folder directory) {
+    List<String> excludedPath, Folder directory) {
   var pathContext = session.resourceProvider.pathContext;
   var children = directory.getChildren();
   children.sort((first, second) => first.shortName.compareTo(second.shortName));
   for (var child in children) {
     if (child is Folder) {
-      if (child.path != excludeFolderPath) {
-        buildTestsIn(
-            session, testDirPath, excludeFilePaths, excludeFolderPath, child);
+      if (!excludedPath.contains(child.path)) {
+        buildTestsIn(session, testDirPath, excludedPath, child);
       }
     } else if (child is File && child.shortName.endsWith('.dart')) {
       var path = child.path;
-      if (excludeFilePaths.contains(path)) {
+      if (excludedPath.contains(path)) {
         continue;
       }
       var relativePath = pathContext.relative(path, from: testDirPath);
@@ -142,4 +140,12 @@ void buildTestsIn(AnalysisSession session, String testDirPath,
       });
     }
   }
+}
+
+String _toPlatformPath(String pathPath, String relativePosixPath) {
+  var pathContext = PhysicalResourceProvider.INSTANCE.pathContext;
+  return pathContext.joinAll([
+    pathPath,
+    ...relativePosixPath.split('/'),
+  ]);
 }
