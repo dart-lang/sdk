44914f5eba5695231d9c97f7a84ac59cc785b7b5
Robert Nystrom <rnystrom@google.com>
2020-06-03 01:01:08 +0000

    Migrate language_2/new to NNBD.
    Change-Id: Id3a078523127c19b525423d27923efab7c2a0139
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/149782
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Srujan Gaddam <srujzs@google.com>
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Srujan Gaddam <srujzs@google.com>

---
+++ tests/language/new/statement_test.dart
@@ -0,0 +1,32 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+// Test a new statement by itself.
+
+import "package:expect/expect.dart";
+
+class A {
+  int a;
+  int b;
+  static int c = -1;
+  static int d = -1;
+
+  A(int x, int y)
+      : a = x,
+        b = y {
+    A.c = x;
+    A.d = y;
+  }
+}
+
+class NewStatementTest {
+  static testMain() {
+    new A(10, 20);
+    Expect.equals(10, A.c);
+    Expect.equals(20, A.d);
+  }
+}
+
+main() {
+  NewStatementTest.testMain();
+}
