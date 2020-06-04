978342b7b12824befff45340d92cbd2026a5e472
Jens Johansen <jensj@google.com>
2020-06-04 08:46:11 +0000

    [CFE] Fix redirecting factory constructor usage in expression compilation
    Fixes: #41976
    Change-Id: I71d393e6503d7ad3ea3c433dda783ffb02f5e280
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/150020
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/lib/src/fasta/kernel/body_builder.dart
+++ pkg/front_end/lib/src/fasta/kernel/body_builder.dart
@@ -1306,6 +1306,7 @@ class BodyBuilder extends ScopeListener<JumpTarget>
   @override
   Expression parseSingleExpression(
       Parser parser, Token token, FunctionNode parameters) {
+    assert(redirectingFactoryInvocations.isEmpty);
     int fileOffset = offsetForToken(token);
     List<TypeVariableBuilder> typeParameterBuilders;
     for (TypeParameter typeParameter in parameters.typeParameters) {
@@ -1351,6 +1352,8 @@ class BodyBuilder extends ScopeListener<JumpTarget>
         fakeReturn == inferredStatement,
         "Previously implicit assumption about inferFunctionBody "
         "not returning anything different.");
+
+    resolveRedirectingFactoryTargets();
     libraryBuilder.loader.transformPostInference(fakeReturn,
         transformSetLiterals, transformCollections, libraryBuilder.library);
 
