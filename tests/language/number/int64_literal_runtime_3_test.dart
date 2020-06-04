062cd60ee2ea1ed1fb6ca5aa21c9aa03be420d4a
Robert Nystrom <rnystrom@google.com>
2020-06-04 23:07:53 +0000

    Migrate language_2/number to NNBD.
    Change-Id: Ie2bdcec491603f231a762ad211ba1d087a4c5c94
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/150000
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language/number/int64_literal_runtime_3_test.dart
@@ -0,0 +1,40 @@
+// TODO(multitest): This was automatically migrated from a multitest and may
+// contain strange or dead code.
+
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details.  All rights reserved.  Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:expect/expect.dart';
+
+const String realMaxInt64Value = '9223372036854775807';
+const String realMinInt64Value = '-9223372036854775808';
+
+const int i21 = 2097152;
+
+main() {
+  int minInt64Value = -1 * i21 * i21 * i21;
+
+
+
+  minInt64Value = -(0x8000000000000000);
+
+
+
+  Expect.equals('$minInt64Value', realMinInt64Value);
+  Expect.equals('${minInt64Value - 1}', realMaxInt64Value);
+
+  int maxInt64Value = 1 * i21 * i21 * i21 - 1;
+
+
+
+
+
+
+
+
+
+
+  Expect.equals('$maxInt64Value', realMaxInt64Value);
+  Expect.equals('${maxInt64Value + 1}', realMinInt64Value);
+}
