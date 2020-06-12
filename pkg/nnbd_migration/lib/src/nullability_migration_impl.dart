b1442a1f9a3d02456f284906afa1811321eb956d
Paul Berry <paulberry@google.com>
2020-06-12 20:25:39 +0000

    Remove unused import
    Change-Id: I6d25e5ea3f1fdf320ec85d5f405789e6e8c2ddf0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/151084
    Auto-Submit: Paul Berry <paulberry@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/nnbd_migration/lib/src/nullability_migration_impl.dart
+++ pkg/nnbd_migration/lib/src/nullability_migration_impl.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/src/dart/analysis/session.dart';
@@ -15,9 +14,9 @@ import 'package:nnbd_migration/src/decorated_class_hierarchy.dart';
 import 'package:nnbd_migration/src/decorated_type.dart';
 import 'package:nnbd_migration/src/edge_builder.dart';
 import 'package:nnbd_migration/src/edit_plan.dart';
+import 'package:nnbd_migration/src/exceptions.dart';
 import 'package:nnbd_migration/src/fix_aggregator.dart';
 import 'package:nnbd_migration/src/fix_builder.dart';
-import 'package:nnbd_migration/src/exceptions.dart';
 import 'package:nnbd_migration/src/node_builder.dart';
 import 'package:nnbd_migration/src/nullability_node.dart';
 import 'package:nnbd_migration/src/postmortem_file.dart';
