d25c369f3ee8c4ef08bdda30d55d2c056ed85d7a
Robert Nystrom <rnystrom@google.com>
2020-06-12 01:41:47 +0000

    Migrate language_2/script to NNBD.
    Change-Id: I1263ec9b1accbd7d092b27a15495bf1a4590a1af
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/150843
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language/script/lib.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// A perfectly legal dart library for use with script and library tests.
+
+library ScriptLib;
+
+const int scriptLib = 1;
