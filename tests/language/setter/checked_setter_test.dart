ce90061e27388431449c4d58744b2baf698a9df6
Robert Nystrom <rnystrom@google.com>
2020-06-12 20:47:19 +0000

    Migrate language_2/setter to NNBD.
    Change-Id: I5c234c68d35d83b0b1f1e299303dd29cf972fdb7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/150845
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Auto-Submit: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language/setter/checked_setter_test.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test that implicit setters do a runtime type check.
+
+import "package:expect/expect.dart";
+
+class A {
+  late C c;
+}
+
+class B extends A {}
+
+class C {}
+
+var array = <dynamic>[new B()];
+
+main() {
+  array[0].c = new C();
+  Expect.throwsTypeError(() => array[0].c = new B());
+}
---
+++ tests/language/setter/no_getter_test.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+set topLevel(var value) {}
+
+class Example {
+  set foo(var value) {}
+}
+
+main() {
+  print(topLevel++);
+  //    ^
+  // [analyzer] unspecified
+  // [cfe] Getter not found: 'topLevel'.
+
+  Example ex = new Example();
+  print(ex.foo++);
+  //       ^^^
+  // [analyzer] STATIC_TYPE_WARNING.UNDEFINED_GETTER
+  // [cfe] The getter 'foo' isn't defined for the class 'Example'.
+}
