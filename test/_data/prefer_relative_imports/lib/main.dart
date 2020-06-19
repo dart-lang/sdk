0fe6293636fdb309fbb03bec996e660b5be36603
berosoboy <andrelvsousa@gmail.com>
2020-06-19 19:02:23 -0300

    Add regression test

--- test/_data/prefer_relative_imports/lib/main.dart
+++ test/_data/prefer_relative_imports/lib/main.dart
@@ -2,4 +2,6 @@ import 'package:sample_project/dummy.dart'; //LINT
 
 import 'package:p6/p6_lib.dart'; //OK
 
+import 'package:internal_path_package/internal_lib.dart'; //OK
+
 import 'dummy.dart'; //OK
