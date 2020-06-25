28583a42b64f89af71793a065be9c4479ba50506
Robert Nystrom <rnystrom@google.com>
2020-06-25 18:57:32 +0000

    Mark legacy boolean tests as complete for migration.
    Change-Id: Id25f1e2bd0049ad1522bd5d2bc75101e6db00efe
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/152584
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Srujan Gaddam <srujzs@google.com>
    Reviewed-by: Srujan Gaddam <srujzs@google.com>

--- tests/language_2/bool/check_test.dart
+++ tests/language_2/bool/check_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// [NNBD non-migrated]: This file is migrated to check_strong_test.dart and
+// check_weak_test.dart.
 import "package:expect/expect.dart";
 
 main() {
