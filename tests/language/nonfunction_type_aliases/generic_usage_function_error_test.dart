1ab82dee7ef5aa183051799f7fb329dabfe57bb6
Robert Nystrom <rnystrom@google.com>
2020-06-26 18:28:55 +0000

    Migrate language_2/nonfunction_type_aliases to NNBD.
    Change-Id: I2cf8ebe11b86549488fa04f18d5e92c1bf9c3a15
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/152585
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Bob Nystrom <rnystrom@google.com>

---
+++ tests/language/nonfunction_type_aliases/generic_usage_function_error_test.dart
@@ -0,0 +1,52 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=nonfunction-type-aliases
+
+import 'dart:async';
+
+// Introduce an aliased type.
+
+typedef T<X> = Function;
+
+// Use the aliased type.
+
+abstract class C {
+  final T<Null> v7;
+
+  C(): v7 = T();
+  //        ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+X foo<X>(X x) => x;
+
+main() {
+  var v9 = <Set<T<T>>, Set<T<T>>>{{}: {}};
+  v9[{}] = {T<T>()};
+  //        ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  T<Null>();
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  T<Null>.named();
+//^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+  T<Object> v12 = foo<T<bool>>(T<bool>());
+  //                           ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  T<List<List<List<List>>>>.staticMethod<T<int>>();
+  //                        ^^^^^^^^^^^^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
