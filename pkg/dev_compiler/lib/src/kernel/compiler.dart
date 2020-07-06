9547628ecf9a40a688456151aea703d401c03e6b
Nicholas Shahan <nshahan@google.com>
2020-07-06 19:39:31 +0000

    [ddc] Compare types with new DartTypeEquivalence
    Avoids an assertion failure in the front end.
    
    Change-Id: I7994fa611f82011857750cc60af64a2c89c96bf7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/152923
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Nicholas Shahan <nshahan@google.com>

--- pkg/dev_compiler/lib/src/kernel/compiler.dart
+++ pkg/dev_compiler/lib/src/kernel/compiler.dart
@@ -12,6 +12,7 @@ import 'package:kernel/kernel.dart' hide MapEntry;
 import 'package:kernel/library_index.dart';
 import 'package:kernel/type_algebra.dart';
 import 'package:kernel/type_environment.dart';
+import 'package:kernel/src/dart_type_equivalence.dart';
 import 'package:source_span/source_span.dart' show SourceLocation;
 import 'package:path/path.dart' as p;
 
@@ -5331,7 +5332,8 @@ class ProgramCompiler extends ComputeOnceConstantVisitor<js_ast.Expression>
     }
 
     if (!isTypeError &&
-        from.withDeclaredNullability(Nullability.nonNullable) == to &&
+        DartTypeEquivalence(_coreTypes, ignoreTopLevelNullability: true)
+            .areEqual(from, to) &&
         _mustBeNonNullable(to)) {
       // If the underlying type is the same, we only need a null check.
       return runtimeCall('nullCast(#, #)', [jsFrom, _emitType(to)]);
