01ac35b84460165b38b6b768c3a63048860497e3
Jens Johansen <jensj@google.com>
2020-07-09 15:03:17 +0000

    [parser] Allow '..' as something following a nullable type
    Fixes https://github.com/dart-lang/sdk/issues/42621
    
    Change-Id: I56fd9475ffb3821592a6917949e858c6e48bdf20
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/153773
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

--- pkg/_fe_analyzer_shared/lib/src/parser/parser_impl.dart
+++ pkg/_fe_analyzer_shared/lib/src/parser/parser_impl.dart
@@ -5655,7 +5655,7 @@ class Parser {
     if (typeInfo.isNullable) {
       Token next = typeInfo.skipType(token).next;
       if (!isOneOfOrEof(
-          next, const [')', '?', '??', ',', ';', ':', 'is', 'as'])) {
+          next, const [')', '?', '??', ',', ';', ':', 'is', 'as', '..'])) {
         // TODO(danrubel): investigate other situations
         // where `?` should be considered part of the type info
         // rather than the start of a conditional expression.
