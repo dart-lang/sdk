fc8a6d9f9bdb7906fde5c268841afed4406e2e7f
Regis Crelier <regis@google.com>
2020-07-22 23:22:39 +0000

    [VM/compiler] Dereference TypeRef literal when propagating constants.
    This fixes https://github.com/dart-lang/sdk/issues/42753
    
    Change-Id: Ibb179c0cf45c77d5b1f29a75ca2dea88d449ed99
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/155503
    Reviewed-by: Alexander Markov <alexmarkov@google.com>
    Commit-Queue: Régis Crelier <regis@google.com>

--- runtime/vm/compiler/backend/constant_propagator.cc
+++ runtime/vm/compiler/backend/constant_propagator.cc
@@ -1024,6 +1024,9 @@ void ConstantPropagator::VisitInstantiateType(InstantiateTypeInstr* instr) {
   AbstractType& result = AbstractType::Handle(
       Z, instr->type().InstantiateFrom(
              instantiator_type_args, function_type_args, kAllFree, Heap::kOld));
+  if (result.IsTypeRef()) {
+    result = TypeRef::Cast(result).type();
+  }
   ASSERT(result.IsInstantiated());
   result = result.Canonicalize();
   SetValue(instr, result);
