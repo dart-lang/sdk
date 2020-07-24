9e1b6030d23ae6d7a43650803376b0f2a41ec6e4
Brian Wilkerson <brianwilkerson@google.com>
2020-07-24 23:36:23 +0000

    Fix a type cast exception in the create-constructor fix (issue 42830)
    Change-Id: I47b11000c2c4ec96856a58d43f9b7b6493cc7908
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/155825
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/create_constructor.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/create_constructor.dart
@@ -65,10 +65,10 @@ class CreateConstructor extends CorrectionProducer {
     // prepare target ClassDeclaration
     ClassElement targetElement = targetType.element;
     var targetResult = await sessionHelper.getElementDeclaration(targetElement);
-    if (targetResult.node is! ClassOrMixinDeclaration) {
+    var targetNode = targetResult.node;
+    if (targetNode is! ClassDeclaration) {
       return;
     }
-    ClassOrMixinDeclaration targetNode = targetResult.node;
 
     // prepare location
     var targetLocation = CorrectionUtils(targetResult.resolvedUnit)
@@ -108,10 +108,10 @@ class CreateConstructor extends CorrectionProducer {
     // prepare target ClassDeclaration
     var targetElement = constructorElement.enclosingElement;
     var targetResult = await sessionHelper.getElementDeclaration(targetElement);
-    if (targetResult.node is! ClassOrMixinDeclaration) {
+    var targetNode = targetResult.node;
+    if (targetNode is! ClassDeclaration) {
       return;
     }
-    ClassOrMixinDeclaration targetNode = targetResult.node;
 
     // prepare location
     var targetLocation = CorrectionUtils(targetResult.resolvedUnit)
