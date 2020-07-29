07aa9ec332600b585b0edc8d3805fb413e4370bd
Johnni Winther <johnniwinther@google.com>
2020-07-29 08:15:33 +0000

    [cfe] Add test for issue 42537
    This adds a language versioning test that shows that the language
    version for a package defined in a `.packages` will have version 2.7
    and whereas a package defined in a `.packages_config.json` will have
    the current language version (2.8 in the test). Therefore the
    former will not have the non-nullable experiment enabled even when
    the package is in allowed_experiments.json.
    
    Change-Id: Iabf5d6882e21538cfc1237d848bad03a37825dfb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/156000
    Reviewed-by: Jens Johansen <jensj@google.com>

---
+++ pkg/front_end/test/language_versioning/data/allowed_experiments_with_json/foo/test/test_file.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/*library: 
+ languageVersion=2.8,
+ packageUri=package:collection
+*/
+
+method3() {}
---
+++ pkg/front_end/test/language_versioning/data/allowed_experiments_with_packages/main.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/*library: languageVersion=2.8*/
+
+import 'foo/bin/bin_file.dart';
+import 'foo/test/test_file.dart';
+import 'package:collection/foo.dart';
+
+main() {}
