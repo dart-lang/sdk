e60a0315dd0ef0888505db30457e1f3dcef5cdc5
Konstantin Shcheglov <scheglov@google.com>
2020-07-29 22:01:59 +0000

    Fix for crash in RemoveEmptyConstructorBody.
    R=brianwilkerson@google.com
    
    Change-Id: I0fe17e9fe96028c5286f509c3bf9ec16248f4841
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/156401
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/remove_empty_constructor_body.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/remove_empty_constructor_body.dart
@@ -4,6 +4,7 @@
 
 import 'package:analysis_server/src/services/correction/dart/abstract_producer.dart';
 import 'package:analysis_server/src/services/correction/fix.dart';
+import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
 import 'package:analyzer_plugin/utilities/range_factory.dart';
@@ -15,8 +16,12 @@ class RemoveEmptyConstructorBody extends CorrectionProducer {
   @override
   Future<void> compute(ChangeBuilder builder) async {
     await builder.addDartFileEdit(file, (builder) {
-      builder.addSimpleReplacement(
-          utils.getLinesRange(range.node(node.parent)), ';');
+      if (node is Block && node.parent is BlockFunctionBody) {
+        builder.addSimpleReplacement(
+          utils.getLinesRange(range.node(node.parent)),
+          ';',
+        );
+      }
     });
   }
 
