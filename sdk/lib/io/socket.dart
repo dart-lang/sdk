336a733e446087db79cbb27070a7c99aca3630d0
Mehmet Fidanboylu <mehmetf@google.com>
2020-07-31 16:45:00 +0000

    [dart:io] Disable isInsecureConnectionAllowed check as it broken an internal test on Fuchsia.
    Change-Id: I1fc6124b7dbdd07a82d09daf5c870971e0c237de
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/156720
    Commit-Queue: Mehmet Fidanboylu <mehmetf@google.com>
    Reviewed-by: Jonas Termansen <sortie@google.com>

--- sdk/lib/io/socket.dart
+++ sdk/lib/io/socket.dart
@@ -801,10 +801,12 @@ abstract class Socket implements Stream<Uint8List>, IOSink {
       {sourceAddress, Duration? timeout}) {
     final IOOverrides? overrides = IOOverrides.current;
     if (overrides == null) {
-      if (!isInsecureConnectionAllowed(host)) {
-        throw new SocketException(
-            "Insecure socket connections are disallowed by platform: $host");
-      }
+      // TODO(b/162514236): This breaks internal device lab tests for Fuchsia.
+      //
+      // if (!isInsecureConnectionAllowed(host)) {
+      //   throw new SocketException(
+      //       "Insecure socket connections are disallowed by platform: $host");
+      // }
       return Socket._connect(host, port,
           sourceAddress: sourceAddress, timeout: timeout);
     }
@@ -819,10 +821,12 @@ abstract class Socket implements Stream<Uint8List>, IOSink {
       {sourceAddress}) {
     final IOOverrides? overrides = IOOverrides.current;
     if (overrides == null) {
-      if (!isInsecureConnectionAllowed(host)) {
-        throw new SocketException(
-            "Insecure socket connections are disallowed by platform: $host");
-      }
+      // TODO(b/162514236): This breaks internal device lab tests for Fuchsia.
+      //
+      // if (!isInsecureConnectionAllowed(host)) {
+      //   throw new SocketException(
+      //       "Insecure socket connections are disallowed by platform: $host");
+      // }
       return Socket._startConnect(host, port, sourceAddress: sourceAddress);
     }
     return overrides.socketStartConnect(host, port,
