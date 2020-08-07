fac552a45c3d9db7b5e75cfdb5eb9cf9338a926a
Régis Crelier <regis@google.com>
2020-08-07 02:15:52 +0000

    Revert "[VM/runtime] Fix computation of the hash of recursive types."
    This reverts commit a528b19d2c62eff86dc262409f9f873c78e23305.
    
    Reason for revert: Failures in precompiled mode.
    
    Original change's description:
    > [VM/runtime] Fix computation of the hash of recursive types.
    > 
    > When computing the hash of a type, only include the hashes of the type arguments corresponding to the type parameters of the type's class and not of the whole type argument vector, otherwise, the variable location of TypeRefs in the base class type arguments may result in different hashes.
    > 
    > Add a regression test (this test was not failing in all configurations, since the issue becomes visible depending on the order in which types are loaded and finalized).
    > 
    > This fixes issue https://github.com/dart-lang/sdk/issues/42954
    > 
    > Small improvement: pick hash value 1 for a vector of dynamic types, so it can be cached and not recomputed repeatedly, which was the case with value 0.
    > 
    > Change-Id: I3468952cbf61944a82cf8e00efbf4952bd831fbe
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/157563
    > Reviewed-by: Alexander Markov <alexmarkov@google.com>
    > Commit-Queue: Régis Crelier <regis@google.com>
    
    TBR=rmacnak@google.com,alexmarkov@google.com,asiva@google.com,regis@google.com
    
    Change-Id: I904e7043fa175d03bb9af56a9346b097b883c7a0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/157700
    Reviewed-by: Régis Crelier <regis@google.com>
    Commit-Queue: Régis Crelier <regis@google.com>

--- runtime/vm/object.h
+++ runtime/vm/object.h
@@ -7528,9 +7528,6 @@ class TypeArguments : public Instance {
   // architecture.
   static const intptr_t kHashBits = 30;
 
-  // Hash value for a type argument vector consisting solely of dynamic types.
-  static const intptr_t kAllDynamicHash = 1;
-
   intptr_t Length() const;
   AbstractTypePtr TypeAt(intptr_t index) const;
   AbstractTypePtr TypeAtNullSafe(intptr_t index) const;
@@ -7745,7 +7742,6 @@ class TypeArguments : public Instance {
     return 0;
   }
   intptr_t Hash() const;
-  intptr_t HashForRange(intptr_t from_index, intptr_t len) const;
 
   static TypeArgumentsPtr New(intptr_t len, Heap::Space space = Heap::kOld);
 
@@ -11330,7 +11326,7 @@ inline void TypeParameter::SetHash(intptr_t value) const {
 }
 
 inline intptr_t TypeArguments::Hash() const {
-  if (IsNull()) return kAllDynamicHash;
+  if (IsNull()) return 0;
   intptr_t result = Smi::Value(raw_ptr()->hash_);
   if (result != 0) {
     return result;
