13de7e7560ad8bcfa0e12fc437ff05cf543ee1df
Jens Johansen <jensj@google.com>
2020-08-11 09:01:29 +0000

    [CFE] Add first stab at weekly leak test
    Change-Id: Ib0307a4d82414e8387f65a9010a6f0f91827c3bb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/157962
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Alexander Thomas <athom@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/test/vm_service_heap_finder.dart
+++ pkg/front_end/test/vm_service_heap_finder.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import "dart:io";
 import "vm_service_heap_helper.dart";
 
