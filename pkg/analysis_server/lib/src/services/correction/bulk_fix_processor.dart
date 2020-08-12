ab7c1e59044986f2c7d8c55d744af002c99522f2
pq <pquitslund@google.com>
2020-08-12 19:27:48 +0000

    bulk fix for `prefer_generic_function_type_aliases`
    Change-Id: Iebe4c3a11a37a0dedb0dbaef5c001e848ae29a3e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/158340
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/analysis_server/lib/src/services/correction/bulk_fix_processor.dart
+++ pkg/analysis_server/lib/src/services/correction/bulk_fix_processor.dart
@@ -15,6 +15,7 @@ import 'package:analysis_server/src/services/correction/dart/convert_conditional
 import 'package:analysis_server/src/services/correction/dart/convert_documentation_into_line.dart';
 import 'package:analysis_server/src/services/correction/dart/convert_quotes.dart';
 import 'package:analysis_server/src/services/correction/dart/convert_to_contains.dart';
+import 'package:analysis_server/src/services/correction/dart/convert_to_generic_function_syntax.dart';
 import 'package:analysis_server/src/services/correction/dart/convert_to_if_null.dart';
 import 'package:analysis_server/src/services/correction/dart/create_method.dart';
 import 'package:analysis_server/src/services/correction/dart/make_final.dart';
@@ -82,6 +83,8 @@ class BulkFixProcessor {
     LintNames.prefer_equal_for_default_values:
         ReplaceColonWithEquals.newInstance,
     LintNames.prefer_final_fields: MakeFinal.newInstance,
+    LintNames.prefer_generic_function_type_aliases:
+        ConvertToGenericFunctionSyntax.newInstance,
     LintNames.prefer_if_elements_to_conditional_expressions:
         ConvertConditionalExpressionToIfElement.newInstance,
     LintNames.prefer_if_null_operators: ConvertToIfNull.newInstance,
--- pkg/analysis_server/test/src/services/correction/fix/bulk/test_all.dart
+++ pkg/analysis_server/test/src/services/correction/fix/bulk/test_all.dart
@@ -9,6 +9,8 @@ import 'add_override_test.dart' as add_override;
 import 'convert_documentation_into_line_test.dart'
     as convert_documentation_into_line;
 import 'convert_to_contains_test.dart' as convert_to_contains;
+import 'convert_to_generic_function_syntax_test.dart'
+    as convert_to_generic_function_syntax;
 import 'convert_to_if_element_test.dart' as convert_to_if_element;
 import 'convert_to_if_null_test.dart' as convert_to_if_null;
 import 'convert_to_single_quoted_strings_test.dart'
@@ -47,6 +49,7 @@ void main() {
     add_override.main();
     convert_documentation_into_line.main();
     convert_to_contains.main();
+    convert_to_generic_function_syntax.main();
     convert_to_if_element.main();
     convert_to_if_null.main();
     convert_to_single_quoted_strings.main();
