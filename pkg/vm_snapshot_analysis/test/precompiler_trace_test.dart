3471490bb27e51534a3012b04bcbd211b7cba480
Vyacheslav Egorov <vegorov@google.com>
2020-09-01 21:35:21 +0000

    [vm_snapshot_analysis] Fix CallGraph.collapse by package
    Take into account that not all libraries are grouped into packages
    avoid collapsing such libraries into the root node.
    
    Cq-Include-Trybots: luci.dart.try:pkg-linux-release-try,pkg-win-release-try,pkg-mac-release-try
    Change-Id: I73a14a2498c0399073be432d1e22badf99eca3eb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/161167
    Commit-Queue: Kenzie Schmoll <kenzieschmoll@google.com>
    Reviewed-by: Kenzie Schmoll <kenzieschmoll@google.com>

--- pkg/vm_snapshot_analysis/test/precompiler_trace_test.dart
+++ pkg/vm_snapshot_analysis/test/precompiler_trace_test.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 import 'package:vm_snapshot_analysis/precompiler_trace.dart';
+import 'package:vm_snapshot_analysis/program_info.dart';
 
 import 'utils.dart';
 
@@ -97,6 +98,19 @@ void main() async {
               'package:input/input.dart::B.[tear-off-extractor] get:tornOff',
               'package:input/input.dart::A.[tear-off-extractor] get:tornOff',
             ]));
+      });
+    });
+
+    test('collapse-by-package', () async {
+      await withFlag(
+          'collapse-by-package', testSource, '--trace_precompiler_to',
+          (json) async {
+        final jsonRaw = await loadJson(File(json));
+        final callGraph = loadTrace(jsonRaw).collapse(NodeType.packageNode);
+
+        // Collapsing by package should not collapse dart:* libraries into root
+        // node and create predecessors for the root node.
+        expect(callGraph.root.pred, isEmpty);
       });
     });
   });
