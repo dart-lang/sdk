000f3135a5ccebf254f58ba87dc36dfd64088da0
Mike Fairhurst <mfairhurst@google.com>
2020-09-02 17:14:22 +0000

    [analyzer] Remove unused dart:async imports
    Change-Id: If21c3a30f3bc521586ad01f8f9e241038e7aa7eb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/161448
    Auto-Submit: Mike Fairhurst <mfairhurst@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/file_system/file_system.dart
+++ pkg/analyzer/lib/file_system/file_system.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:analyzer/src/generated/source.dart';
 import 'package:path/path.dart';
 import 'package:watcher/watcher.dart';
