98c5fed2dc6d7856a8d04bf1a01c25beb2eb6627
Phil Quitslund <pq@users.noreply.github.com>
2020-09-03 09:35:27 -0700

    inline Flutter doc guidance (dart-lang/linter#2232)

--- lib/src/rules/flutter_style_todos.dart
+++ lib/src/rules/flutter_style_todos.dart
@@ -15,6 +15,10 @@ const _details = r'''
 
 **DO** Use Flutter TODO format.
 
+From the [Flutter docs](https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo#comments):
+
+> TODOs should include the string TODO in all caps, followed by the GitHub username of the person with the best context about the problem referenced by the TODO in parenthesis. A TODO is not a commitment that the person referenced will fix the problem, it is intended to be the person with enough context to explain the problem. Thus, when you create a TODO, it is almost always your username that is given.
+
 **GOOD:**
 ```
 // TODO(username): message.
