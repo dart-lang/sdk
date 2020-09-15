dart-lang/sdk

commit e2aadc7a075da3b52957c93dc39d3b7fc86a5d1b

Reland "[vm/fieldtable] Move current field values out of Field object into separate table."
This reverts commit c8f8c11b70b49f77564de6c4dc85d4de385442cf with the fix for https://github.com/flutter/flutter/issues/49008 in Patchset 2.

Change-Id: I85406b92a69ed950d0ba945f96997658be3cae64
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/132302
Reviewed-by: Ryan Macnak <rmacnak@google.com>
Commit-Queue: Alexander Aprelev <aam@google.com>

runtime/vm/isolate.cc
@@ -280,6 +280,7 @@ Thread* IsolateGroup::ScheduleThreadLocked(MonitorLocker* ml,
     // Set up other values and set the TLS value.
     thread->isolate_ = nullptr;
     thread->isolate_group_ = this;
+    thread->field_table_values_ = nullptr;
     thread->set_os_thread(os_thread);
     ASSERT(thread->execution_state() == Thread::kThreadInNative);
     thread->set_execution_state(Thread::kThreadInVM);
@@ -526,6 +527,10 @@ void Isolate::ValidateClassTable() {
   class_table()->Validate();
 }
 #endif  // DEBUG
+
+void Isolate::RegisterStaticField(const Field& field) {
+  field_table()->Register(field);
+}
 
 void Isolate::RehashConstants() {
   Thread* thread = Thread::Current();
@@ -1224,6 +1229,7 @@ Isolate::Isolate(IsolateGroup* isolate_group,
       ic_miss_code_(Code::null()),
       shared_class_table_(new SharedClassTable()),
       class_table_(shared_class_table_.get()),
+      field_table_(new FieldTable()),
       store_buffer_(new StoreBuffer()),
 #if !defined(DART_PRECOMPILED_RUNTIME)
       native_callback_trampolines_(),
@@ -1325,6 +1331,7 @@ Isolate::~Isolate() {
   delete heap_;
   ASSERT(marking_stack_ == nullptr);
   delete object_store_;
+  delete field_table_;
   delete api_state_;
 #if defined(USING_SIMULATOR)
   delete simulator_;
@@ -2307,6 +2314,9 @@ void Isolate::VisitObjectPointers(ObjectPointerVisitor* visitor,
   // Visit objects in the class table.
   class_table()->VisitObjectPointers(visitor);
 
+  // Visit objects in the field table.
+  field_table()->VisitObjectPointers(visitor);
+
   // Visit the dart api state for all local and persistent handles.
   if (api_state() != nullptr) {
     api_state()->VisitObjectPointers(visitor);
@@ -3277,6 +3287,7 @@ Thread* Isolate::ScheduleThread(bool is_mutator, bool bypass_safepoint) {
     scheduled_mutator_thread_ = thread;
   }
   thread->isolate_ = this;
+  thread->field_table_values_ = field_table_->table();
 
   ASSERT(heap() != nullptr);
   thread->heap_ = heap();
@@ -3312,6 +3323,7 @@ void Isolate::UnscheduleThread(Thread* thread,
     ASSERT(mutator_thread_ == scheduled_mutator_thread_);
     scheduled_mutator_thread_ = nullptr;
   }
+  thread->field_table_values_ = nullptr;
   group()->UnscheduleThreadLocked(&ml, thread, is_mutator, bypass_safepoint);
 }
 

runtime/observatory/tests/service/get_retaining_path_rpc_test.dart
@@ -48,9 +48,9 @@ var tests = <IsolateTest>[
       'limit': 100,
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
-    expect(result['gcRootType'], 'user global');
-    expect(result['elements'].length, equals(2));
-    expect(result['elements'][1]['value']['name'], equals('globalObject'));
+    expect(result['gcRootType'], 'static fields table');
+    expect(result['elements'].length, equals(1));
+    expect(result['elements'][0]['value']['type'], equals('@Instance'));
   },
 
   // missing limit.
@@ -81,10 +81,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'user global');
-    expect(result['elements'].length, equals(3));
+    expect(result['gcRootType'], 'static fields table');
+    expect(result['elements'].length, equals(2));
+    expect(result['elements'][0]['value']['type'], equals('@Instance'));
     expect(result['elements'][1]['parentField'], equals('x'));
-    expect(result['elements'][2]['value']['name'], equals('globalObject'));
   },
 
   (Isolate isolate) async {
@@ -96,10 +96,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'user global');
-    expect(result['elements'].length, equals(3));
+    expect(result['gcRootType'], 'static fields table');
+    expect(result['elements'].length, equals(2));
     expect(result['elements'][1]['parentField'], equals('y'));
-    expect(result['elements'][2]['value']['name'], equals('globalObject'));
+    expect(result['elements'][1]['value']['type'], equals('@Instance'));
   },
 
   (Isolate isolate) async {
@@ -111,10 +111,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'user global');
-    expect(result['elements'].length, equals(3));
+    expect(result['gcRootType'], 'static fields table');
+    expect(result['elements'].length, equals(2));
     expect(result['elements'][1]['parentListIndex'], equals(12));
-    expect(result['elements'][2]['value']['name'], equals('globalList'));
+    expect(result['elements'][1]['value']['type'], equals('@Instance'));
   },
 
   (Isolate isolate) async {
@@ -126,11 +126,11 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'user global');
-    expect(result['elements'].length, equals(3));
+    expect(result['gcRootType'], 'static fields table');
+    expect(result['elements'].length, equals(2));
     expect(
         result['elements'][1]['parentMapKey']['valueAsString'], equals('key'));
-    expect(result['elements'][2]['value']['name'], equals('globalMap1'));
+    expect(result['elements'][1]['value']['type'], equals('@Instance'));
   },
 
   (Isolate isolate) async {
@@ -142,10 +142,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(3));
+    expect(result['elements'].length, equals(2));
     expect(result['elements'][1]['parentMapKey']['class']['name'],
         equals('_TestClass'));
-    expect(result['elements'][2]['value']['name'], equals('globalMap2'));
+    expect(result['elements'][1]['value']['type'], equals('@Instance'));
   },
 
   // object store

runtime/observatory/tests/service/get_user_level_retaining_path_rpc_test.dart
@@ -46,10 +46,9 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(2));
+    expect(result['elements'].length, equals(1));
     expect(
         result['elements'][0]['value']['class']['name'], equals('_TestConst'));
-    expect(result['elements'][1]['value']['name'], equals('x'));
   },
 
   // Expect a simple path through variable fn instead of long path filled
@@ -62,9 +61,8 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(2));
+    expect(result['elements'].length, equals(1));
     expect(result['elements'][0]['value']['class']['name'], equals('_Closure'));
-    expect(result['elements'][1]['value']['name'], equals('fn'));
   }
 ];
 

runtime/observatory/tests/service/inbound_references_test.dart
@@ -46,8 +46,6 @@ var tests = <IsolateTest>[
         r['source'].clazz.name == 'Node');
     hasReferenceSuchThat(
         (r) => r['parentListIndex'] == 1 && r['source'].isList);
-    hasReferenceSuchThat(
-        (r) => r['source'] is Field && r['source'].name == 'e');
   }
 ];
 

runtime/vm/clustered_snapshot.cc
@@ -913,26 +913,11 @@ class FieldSerializationCluster : public SerializationCluster {
     s->Push(field->ptr()->name_);
     s->Push(field->ptr()->owner_);
     s->Push(field->ptr()->type_);
-    // Write out the initial static value or field offset.
-    if (Field::StaticBit::decode(field->ptr()->kind_bits_)) {
-      if (kind == Snapshot::kFullAOT) {
-        // For precompiled static fields, the value was already reset and
-        // initializer_ now contains a Function.
-        s->Push(field->ptr()->value_.static_value_);
-      } else if (Field::ConstBit::decode(field->ptr()->kind_bits_)) {
-        // Do not reset const fields.
-        s->Push(field->ptr()->value_.static_value_);
-      } else {
-        // Otherwise, for static fields we write out the initial static value.
-        s->Push(field->ptr()->saved_initial_value_);
-      }
-    } else {
-      s->Push(field->ptr()->value_.offset_);
-    }
     // Write out the initializer function
     s->Push(field->ptr()->initializer_function_);
+
     if (kind != Snapshot::kFullAOT) {
-      // Write out the saved initial value
+      // Write out the saved initial values
       s->Push(field->ptr()->saved_initial_value_);
     }
     if (kind != Snapshot::kFullAOT) {
@@ -941,6 +926,22 @@ class FieldSerializationCluster : public SerializationCluster {
     }
     if (kind == Snapshot::kFullJIT) {
       s->Push(field->ptr()->dependent_code_);
+    }
+    // Write out either static value, initial value or field offset.
+    if (Field::StaticBit::decode(field->ptr()->kind_bits_)) {
+      if (
+          // For precompiled static fields, the value was already reset and
+          // initializer_ now contains a Function.
+          kind == Snapshot::kFullAOT ||
+          // Do not reset const fields.
+          Field::ConstBit::decode(field->ptr()->kind_bits_)) {
+        s->Push(s->field_table()->At(field->ptr()->offset_or_field_id_));
+      } else {
+        // Otherwise, for static fields we write out the initial static value.
+        s->Push(field->ptr()->saved_initial_value_);
+      }
+    } else {
+      s->Push(Smi::New(field->ptr()->offset_or_field_id_));
     }
   }
 
@@ -964,22 +965,6 @@ class FieldSerializationCluster : public SerializationCluster {
       WriteField(field, name_);
       WriteField(field, owner_);
       WriteField(field, type_);
-      // Write out the initial static value or field offset.
-      if (Field::StaticBit::decode(field->ptr()->kind_bits_)) {
-        if (kind == Snapshot::kFullAOT) {
-          // For precompiled static fields, the value was already reset and
-          // initializer_ now contains a Function.
-          WriteField(field, value_.static_value_);
-        } else if (Field::ConstBit::decode(field->ptr()->kind_bits_)) {
-          // Do not reset const fields.
-          WriteField(field, value_.static_value_);
-        } else {
-          // Otherwise, for static fields we write out the initial static value.
-          WriteField(field, saved_initial_value_);
-        }
-      } else {
-        WriteField(field, value_.offset_);
-      }
       // Write out the initializer function and initial value if not in AOT.
       WriteField(field, initializer_function_);
       if (kind != Snapshot::kFullAOT) {
@@ -1004,6 +989,27 @@ class FieldSerializationCluster : public SerializationCluster {
 #endif
       }
       s->Write<uint16_t>(field->ptr()->kind_bits_);
+
+      // Write out the initial static value or field offset.
+      if (Field::StaticBit::decode(field->ptr()->kind_bits_)) {
+        if (
+            // For precompiled static fields, the value was already reset and
+            // initializer_ now contains a Function.
+            // WriteField(field, value_.static_value_);
+            kind == Snapshot::kFullAOT ||
+            // Do not reset const fields.
+            Field::ConstBit::decode(field->ptr()->kind_bits_)) {
+          WriteFieldValue(
+              "static value",
+              s->field_table()->At(field->ptr()->offset_or_field_id_));
+        } else {
+          // Otherwise, for static fields we write out the initial static value.
+          WriteFieldValue("static value", field->ptr()->saved_initial_value_);
+        }
+        s->WriteUnsigned(field->ptr()->offset_or_field_id_);
+      } else {
+        WriteFieldValue("offset", Smi::New(field->ptr()->offset_or_field_id_));
+      }
     }
   }
 
@@ -1045,6 +1051,17 @@ class FieldDeserializationCluster : public DeserializationCluster {
 #endif
       }
       field->ptr()->kind_bits_ = d->Read<uint16_t>();
+
+      RawObject* value_or_offset = d->ReadRef();
+      if (Field::StaticBit::decode(field->ptr()->kind_bits_)) {
+        intptr_t field_id = d->ReadUnsigned();
+        d->field_table()->SetAt(
+            field_id, reinterpret_cast<RawInstance*>(value_or_offset));
+        field->ptr()->offset_or_field_id_ = field_id;
+      } else {
+        field->ptr()->offset_or_field_id_ =
+            Smi::Value(Smi::RawCast(value_or_offset));
+      }
     }
   }
 
@@ -4344,6 +4361,7 @@ Serializer::Serializer(Thread* thread,
       num_base_objects_(0),
       num_written_objects_(0),
       next_ref_index_(1),
+      field_table_(thread->isolate()->field_table()),
       vm_(vm),
       profile_writer_(profile_writer)
 #if defined(SNAPSHOT_BACKTRACE)
@@ -4798,6 +4816,7 @@ void Serializer::Serialize() {
   WriteUnsigned(num_objects);
   WriteUnsigned(num_clusters);
   WriteUnsigned(code_order_length);
+  WriteUnsigned(field_table_->NumFieldIds());
 
   for (intptr_t cid = 1; cid < num_cids_; cid++) {
     SerializationCluster* cluster = clusters_by_cid_[cid];
@@ -5014,7 +5033,8 @@ Deserializer::Deserializer(Thread* thread,
       image_reader_(NULL),
       refs_(NULL),
       next_ref_index_(1),
-      clusters_(NULL) {
+      clusters_(NULL),
+      field_table_(thread->isolate()->field_table()) {
   if (Snapshot::IncludesCode(kind)) {
     ASSERT(instructions_buffer != NULL);
     ASSERT(data_buffer != NULL);
@@ -5282,9 +5302,14 @@ void Deserializer::Prepare() {
   num_objects_ = ReadUnsigned();
   num_clusters_ = ReadUnsigned();
   code_order_length_ = ReadUnsigned();
+  const intptr_t field_table_len = ReadUnsigned();
 
   clusters_ = new DeserializationCluster*[num_clusters_];
   refs_ = Array::New(num_objects_ + 1, Heap::kOld);
+  if (field_table_len > 0) {
+    field_table_->AllocateIndex(field_table_len - 1);
+  }
+  ASSERT(field_table_->NumFieldIds() == field_table_len);
 }
 
 void Deserializer::Deserialize() {

runtime/vm/clustered_snapshot.h
@@ -216,6 +216,9 @@ class Serializer : public ThreadStackResource {
   void WriteVersionAndFeatures(bool is_vm_snapshot);
 
   void Serialize();
+
+  FieldTable* field_table() { return field_table_; }
+
   WriteStream* stream() { return &stream_; }
   intptr_t bytes_written() { return stream_.bytes_written(); }
 
@@ -372,6 +375,7 @@ class Serializer : public ThreadStackResource {
   intptr_t num_written_objects_;
   intptr_t next_ref_index_;
   SmiObjectIdMap smi_ids_;
+  FieldTable* field_table_;
 
   // True if writing VM snapshot, false for Isolate snapshot.
   bool vm_;
@@ -556,6 +560,7 @@ class Deserializer : public ThreadStackResource {
   intptr_t next_index() const { return next_ref_index_; }
   Heap* heap() const { return heap_; }
   Snapshot::Kind kind() const { return kind_; }
+  FieldTable* field_table() const { return field_table_; }
 
   // The number of code objects which were relocated during AOT snapshot
   // writing.
@@ -585,6 +590,7 @@ class Deserializer : public ThreadStackResource {
   RawArray* refs_;
   intptr_t next_ref_index_;
   DeserializationCluster** clusters_;
+  FieldTable* field_table_;
 };
 
 #define ReadFromTo(obj, ...) d->ReadFromTo(obj, ##__VA_ARGS__);

runtime/vm/compiler/aot/precompiler.cc
@@ -166,6 +166,7 @@ Precompiler::Precompiler(Thread* thread)
       libraries_(GrowableObjectArray::Handle(I->object_store()->libraries())),
       pending_functions_(
           GrowableObjectArray::Handle(GrowableObjectArray::New())),
+      pending_static_fields_to_retain_(),
       sent_selectors_(),
       enqueued_functions_(
           HashTables::New<FunctionSet>(/*initial_capacity=*/1024)),
@@ -530,6 +531,13 @@ void Precompiler::AddRoots() {
   }
 }
 
+void Precompiler::AddRetainedStaticField(const Field& field) {
+  if (pending_static_fields_to_retain_.HasKey(&field)) {
+    return;
+  }
+  pending_static_fields_to_retain_.Insert(&Field::ZoneHandle(Z, field.raw()));
+}
+
 void Precompiler::Iterate() {
   Function& function = Function::Handle(Z);
 
@@ -539,6 +547,17 @@ void Precompiler::Iterate() {
     while (pending_functions_.Length() > 0) {
       function ^= pending_functions_.RemoveLast();
       ProcessFunction(function);
+    }
+
+    // AddField might retain more static fields.
+    while (!pending_static_fields_to_retain_.IsEmpty()) {
+      FieldSet copy = pending_static_fields_to_retain_;
+      pending_static_fields_to_retain_.Clear();
+      FieldSet::Iterator it = copy.GetIterator();
+      for (const Field** field = it.Next(); field != nullptr;
+           field = it.Next()) {
+        AddField(**field);
+      }
     }
 
     CheckForNewDynamicFunctions();
@@ -1551,6 +1570,12 @@ void Precompiler::DropFields() {
           if (FLAG_trace_precompiler) {
             THR_Print("Dropping field %s\n", field.ToCString());
           }
+
+          // This cleans up references to field current and initial values.
+          if (field.is_static()) {
+            field.SetStaticValue(Object::null_instance(),
+                                 /*save_initial_value=*/true);
+          }
         }
       }
 
@@ -1794,9 +1819,25 @@ void Precompiler::DropMetadata() {
   Array& dependencies = Array::Handle(Z);
   Namespace& ns = Namespace::Handle(Z);
   const Field& null_field = Field::Handle(Z);
+  GrowableObjectArray& metadata = GrowableObjectArray::Handle(Z);
+  Field& metadata_field = Field::Handle(Z);
 
   for (intptr_t i = 0; i < libraries_.Length(); i++) {
     lib ^= libraries_.At(i);
+    metadata ^= lib.metadata();
+    for (intptr_t j = 0; j < metadata.Length(); j++) {
+      metadata_field ^= metadata.At(j);
+      if (metadata_field.is_static()) {
+        // Although this field will become garbage after clearing the list
+        // below, we also need to clear its value from the field table.
+        // The value may be an instance of an otherwise dead class, and if
+        // it remains in the field table we can get an instance on the heap
+        // with a deleted class.
+        metadata_field.SetStaticValue(Object::null_instance(),
+                                      /*save_initial_value=*/true);
+      }
+    }
+
     lib.set_metadata(null_growable_list);
 
     dependencies = lib.imports();
@@ -2453,6 +2494,16 @@ bool PrecompileParsedFunctionHelper::Compile(CompilationPipeline* pipeline) {
         ASSERT(thread()->IsMutatorThread());
         FinalizeCompilation(&assembler, &graph_compiler, flow_graph,
                             function_stats);
+      }
+
+      for (intptr_t i = 0; i < graph_compiler.used_static_fields().length();
+           i++) {
+        // We can't use precompiler_->AddField() directly here because they
+        // need to be added later as part of Iterate(). If they are added
+        // before that, initializer functions will get their code cleared by
+        // Precompiler::ClearAllCode().
+        precompiler_->AddRetainedStaticField(
+            *graph_compiler.used_static_fields().At(i));
       }
 
       // In bare instructions mode try adding all entries from the object

commit 083ecee7f6a7042f2f2b073e3f5fba5426b1a529

[vm/fieldtable] Fix retaining paths for static fields.
This is a follow-up to https://dart.googlesource.com/sdk/+/e2aadc7a075da3b52957c93dc39d3b7fc86a5d1b which regressed retaining path reporting for static field values.

Fixes https://github.com/dart-lang/sdk/issues/43025

Change-Id: Iee9f289652e41fafd075a9e93b225099aac8e01c
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/162900
Reviewed-by: Ryan Macnak <rmacnak@google.com>
Commit-Queue: Alexander Aprelev <aam@google.com>

runtime/vm/isolate.cc
@@ -2736,7 +2736,9 @@ void Isolate::VisitObjectPointers(ObjectPointerVisitor* visitor,
   }
 
   // Visit objects in the field table.
-  field_table()->VisitObjectPointers(visitor);
+  if (!visitor->trace_values_through_fields()) {
+    field_table()->VisitObjectPointers(visitor);
+  }
 
   visitor->clear_gc_root_type();
   // Visit the objects directly referenced from the isolate structure.

runtime/vm/object_graph.cc
@@ -48,6 +48,8 @@ class ObjectGraph::Stack : public ObjectPointerVisitor {
     object_ids_ = nullptr;
   }
 
+  virtual bool trace_values_through_fields() const { return true; }
+
   // Marks and pushes. Used to initialize this stack with roots.
   // We can use ObjectIdTable normally used by serializers because it
   // won't be in use while handling a service request (ObjectGraph's only use).
@@ -472,6 +474,8 @@ class InboundReferencesVisitor : public ObjectVisitor,
     ASSERT(Thread::Current()->no_safepoint_scope_depth() != 0);
   }
 
+  virtual bool trace_values_through_fields() const { return true; }
+
   intptr_t length() const { return length_; }
 
   virtual void VisitObject(ObjectPtr raw_obj) {
@@ -765,6 +769,8 @@ class Pass1Visitor : public ObjectVisitor,
         HandleVisitor(Thread::Current()),
         writer_(writer) {}
 
+  virtual bool trace_values_through_fields() const { return true; }
+
   void VisitObject(ObjectPtr obj) {
     if (obj->IsPseudoObject()) return;
 
@@ -818,6 +824,8 @@ class Pass2Visitor : public ObjectVisitor,
         HandleVisitor(Thread::Current()),
         isolate_(thread()->isolate()),
         writer_(writer) {}
+
+  virtual bool trace_values_through_fields() const { return true; }
 
   void VisitObject(ObjectPtr obj) {
     if (obj->IsPseudoObject()) return;

runtime/vm/raw_object.cc
@@ -516,7 +516,6 @@ REGULAR_VISITOR(ClosureData)
 REGULAR_VISITOR(SignatureData)
 REGULAR_VISITOR(RedirectionData)
 REGULAR_VISITOR(FfiTrampolineData)
-REGULAR_VISITOR(Field)
 REGULAR_VISITOR(Script)
 REGULAR_VISITOR(Library)
 REGULAR_VISITOR(LibraryPrefix)
@@ -591,6 +590,26 @@ NULL_VISITOR(WeakSerializationReference)
 #else
 REGULAR_VISITOR(WeakSerializationReference)
 #endif
+
+intptr_t FieldLayout::VisitFieldPointers(FieldPtr raw_obj,
+                                         ObjectPointerVisitor* visitor) {
+  ASSERT(raw_obj->IsHeapObject());
+  ASSERT_UNCOMPRESSED(Field);
+  visitor->VisitPointers(raw_obj->ptr()->from(), raw_obj->ptr()->to());
+
+  if (visitor->trace_values_through_fields()) {
+    if (Field::StaticBit::decode(raw_obj->ptr()->kind_bits_)) {
+      visitor->isolate_group()->ForEachIsolate(
+          [&](Isolate* isolate) {
+            intptr_t index =
+                Smi::Value(raw_obj->ptr()->host_offset_or_field_id_);
+            visitor->VisitPointer(&isolate->field_table()->table()[index]);
+          },
+          /*at_safepoint=*/true);
+    }
+  }
+  return Field::InstanceSize();
+}
 
 bool CodeLayout::ContainsPC(const ObjectPtr raw_obj, uword pc) {
   if (!raw_obj->IsCode()) return false;

runtime/vm/visitor.h
@@ -52,6 +52,11 @@ class ObjectPointerVisitor {
 
   virtual bool visit_weak_persistent_handles() const { return false; }
 
+  // When visiting objects to build retaining paths, trace field values
+  // through fields.
+  // Otherwise trace field values through isolate's field_table.
+  virtual bool trace_values_through_fields() const { return false; }
+
   const SharedClassTable* shared_class_table() const {
     return shared_class_table_;
   }

runtime/observatory/tests/service/get_retaining_path_rpc_test.dart
@@ -21,9 +21,13 @@ var target2 = new _TestClass();
 var target3 = new _TestClass();
 var target4 = new _TestClass();
 var target5 = new _TestClass();
+@pragma("vm:entry-point") // Prevent obfuscation
 var globalObject = new _TestClass();
+@pragma("vm:entry-point") // Prevent obfuscation
 var globalList = new List(100);
+@pragma("vm:entry-point") // Prevent obfuscation
 var globalMap1 = new Map();
+@pragma("vm:entry-point") // Prevent obfuscation
 var globalMap2 = new Map();
 
 void warmup() {
@@ -93,9 +97,9 @@ var tests = <IsolateTest>[
       'limit': 100,
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
-    expect(result['gcRootType'], 'static fields table');
-    expect(result['elements'].length, equals(1));
-    expect(result['elements'][0]['value']['type'], equals('@Instance'));
+    expect(result['gcRootType'], 'user global');
+    expect(result['elements'].length, equals(2));
+    expect(result['elements'][1]['value']['name'], equals('globalObject'));
   },
 
   // missing limit.
@@ -125,10 +129,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'static fields table');
-    expect(result['elements'].length, equals(2));
-    expect(result['elements'][0]['value']['type'], equals('@Instance'));
+    expect(result['gcRootType'], 'user global');
+    expect(result['elements'].length, equals(3));
     expect(result['elements'][1]['parentField'], equals('x'));
+    expect(result['elements'][2]['value']['name'], equals('globalObject'));
   },
 
   (Isolate isolate) async {
@@ -139,10 +143,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'static fields table');
-    expect(result['elements'].length, equals(2));
+    expect(result['gcRootType'], 'user global');
+    expect(result['elements'].length, equals(3));
     expect(result['elements'][1]['parentField'], equals('y'));
-    expect(result['elements'][1]['value']['type'], equals('@Instance'));
+    expect(result['elements'][2]['value']['name'], equals('globalObject'));
   },
 
   (Isolate isolate) async {
@@ -153,10 +157,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'static fields table');
-    expect(result['elements'].length, equals(2));
+    expect(result['gcRootType'], 'user global');
+    expect(result['elements'].length, equals(3));
     expect(result['elements'][1]['parentListIndex'], equals(12));
-    expect(result['elements'][1]['value']['type'], equals('@Instance'));
+    expect(result['elements'][2]['value']['name'], equals('globalList'));
   },
 
   (Isolate isolate) async {
@@ -167,11 +171,11 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['gcRootType'], 'static fields table');
-    expect(result['elements'].length, equals(2));
+    expect(result['gcRootType'], 'user global');
+    expect(result['elements'].length, equals(3));
     expect(
         result['elements'][1]['parentMapKey']['valueAsString'], equals('key'));
-    expect(result['elements'][1]['value']['type'], equals('@Instance'));
+    expect(result['elements'][2]['value']['name'], equals('globalMap1'));
   },
 
   (Isolate isolate) async {
@@ -182,10 +186,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(2));
+    expect(result['elements'].length, equals(3));
     expect(result['elements'][1]['parentMapKey']['class']['name'],
         equals('_TestClass'));
-    expect(result['elements'][1]['value']['type'], equals('@Instance'));
+    expect(result['elements'][2]['value']['name'], equals('globalMap2'));
   },
 
   // object store

runtime/observatory/tests/service/get_user_level_retaining_path_rpc_test.dart
@@ -53,9 +53,10 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(1));
+    expect(result['elements'].length, equals(2));
     expect(
         result['elements'][0]['value']['class']['name'], equals('_TestConst'));
+    expect(result['elements'][1]['value']['name'], equals('x'));
   },
 
   // Expect a simple path through variable fn instead of long path filled
@@ -68,8 +69,9 @@ var tests = <IsolateTest>[
     };
     var result = await isolate.invokeRpcNoUpgrade('getRetainingPath', params);
     expect(result['type'], equals('RetainingPath'));
-    expect(result['elements'].length, equals(1));
+    expect(result['elements'].length, equals(2));
     expect(result['elements'][0]['value']['class']['name'], equals('_Closure'));
+    expect(result['elements'][1]['value']['name'], equals('fn'));
   }
 ];
 

runtime/observatory/tests/service/inbound_references_test.dart
@@ -48,6 +48,8 @@ var tests = <IsolateTest>[
         r['source'].clazz.name == 'Node');
     hasReferenceSuchThat(
         (r) => r['parentListIndex'] == 1 && r['source'].isList);
+    hasReferenceSuchThat(
+        (r) => r['source'] is Field && r['source'].name == 'e');
   }
 ];
 