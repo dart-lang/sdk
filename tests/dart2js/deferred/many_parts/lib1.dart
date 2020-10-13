d2b0e391447570ad7dc79a9553f6977874714405
Joshua Litt <joshualitt@google.com>
2020-10-13 17:55:58 +0000

    [dart2js] Add stressful unit / integation tests for deferred loading.
    Change-Id: I456af72961803bb24b089c6f3655e9d8ef2a7396
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/167044
    Commit-Queue: Joshua Litt <joshualitt@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Reviewed-by: Stephen Adams <sra@google.com>

---
+++ tests/dart2js/deferred/many_parts/lib1.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'lib_000_000_000_000_1.dart' deferred as b1;
+
+entryLib1() async {
+  await b1.loadLibrary();
+  b1.g_000_000_000_000_1();
+}
