22931c30e31945d95e9110471a937caca6a7959c
Phil Quitslund <pq@users.noreply.github.com>
2020-11-01 08:24:21 -0800

    fix failing lines_longer_than_80_chars integration test (dart-lang/linter#2316)
    * fix ignore format
    
    * fix

--- test/_data/lines_longer_than_80_chars/a.dart
+++ test/_data/lines_longer_than_80_chars/a.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // abcde abcde abcde abcde abcde abcde abcde abcde
 // abcde abcde abcde abcde abcde abcde abcde abcde http://url.com/abcde/abcde/abcde/abcde.dart
 // abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde abcde
