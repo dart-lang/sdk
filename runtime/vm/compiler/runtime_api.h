dart-lang/sdk

commit b97e8ecf72304012357adc2c6cb47c6ad0b4ed8a

[vm/snapshots] Write strings as ROData in root snapshot
This partially undoes changes from acbd51c08
because they are now known to increate RSS of AOT
compiled applications.

This CL also removes length adjustment code from *String::InstanceSize
which tried to ensure that external string fits into a non-external
string. This code is no longer necessary because APIs for
string externalization were removed.

TEST=covered by the existing tests

Cq-Include-Trybots: luci.dart.try:app-kernel-linux-release-x64-try,app-kernel-linux-debug-x64-try,vm-kernel-nnbd-linux-debug-x64-try,vm-kernel-linux-debug-x64-try,vm-kernel-precomp-linux-debug-x64-try,vm-kernel-precomp-nnbd-linux-debug-x64-try
Change-Id: Ife4f16d6939d98bb7966121b5899cef9f0121220
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/170080
Commit-Queue: Vyacheslav Egorov <vegorov@google.com>
Reviewed-by: Ryan Macnak <rmacnak@google.com>

runtime/vm/compiler/runtime_api.h
@@ -702,6 +702,7 @@ class String : public AllStatic {
   static word length_offset();
   static word InstanceSize();
   static word NextFieldOffset();
+  static word InstanceSize(intptr_t payload_size);
 };
 
 class OneByteString : public AllStatic {

runtime/vm/clustered_snapshot.cc
@@ -2380,21 +2380,43 @@ class RODataSerializationCluster : public SerializationCluster {
 
 class RODataDeserializationCluster : public DeserializationCluster {
  public:
-  RODataDeserializationCluster() : DeserializationCluster("ROData") {}
+  explicit RODataDeserializationCluster(intptr_t cid)
+      : DeserializationCluster("ROData"), cid_(cid) {}
   ~RODataDeserializationCluster() {}
 
   void ReadAlloc(Deserializer* d, bool is_canonical) {
+    start_index_ = d->next_index();
     intptr_t count = d->ReadUnsigned();
     uint32_t running_offset = 0;
     for (intptr_t i = 0; i < count; i++) {
       running_offset += d->ReadUnsigned() << kObjectAlignmentLog2;
       d->AssignRef(d->GetObjectAt(running_offset));
     }
+    stop_index_ = d->next_index();
   }
 
   void ReadFill(Deserializer* d, bool is_canonical) {
     // No-op.
   }
+
+  void PostLoad(Deserializer* d, const Array& refs, bool is_canonical) {
+    if (is_canonical && IsStringClassId(cid_) &&
+        (d->isolate() != Dart::vm_isolate())) {
+      CanonicalStringSet table(d->zone(),
+                               d->isolate()->object_store()->symbol_table());
+      String& str = String::Handle(d->zone());
+      for (intptr_t i = start_index_; i < stop_index_; i++) {
+        str ^= refs.At(i);
+        ASSERT(str.IsCanonical());
+        bool present = table.Insert(str);
+        ASSERT(!present);
+      }
+      d->isolate()->object_store()->set_symbol_table(table.Release());
+    }
+  }
+
+ private:
+  const intptr_t cid_;
 };
 
 #if !defined(DART_PRECOMPILED_RUNTIME)
@@ -5499,6 +5521,14 @@ const char* Serializer::ReadOnlyObjectType(intptr_t cid) {
       return "CodeSourceMap";
     case kCompressedStackMapsCid:
       return "CompressedStackMaps";
+    case kOneByteStringCid:
+      return current_loading_unit_id_ <= LoadingUnit::kRootId
+                 ? "OneByteStringCid"
+                 : nullptr;
+    case kTwoByteStringCid:
+      return current_loading_unit_id_ <= LoadingUnit::kRootId
+                 ? "TwoByteStringCid"
+                 : nullptr;
     default:
       return nullptr;
   }
@@ -5771,19 +5801,15 @@ void Serializer::Trace(ObjectPtr object) {
     is_canonical = object->ptr()->IsCanonical();
   }
 
-  SerializationCluster* cluster =
-      is_canonical ? canonical_clusters_by_cid_[cid] : clusters_by_cid_[cid];
-  if (cluster == nullptr) {
-    cluster = NewClusterForClass(cid);
-    if (cluster == nullptr) {
+  SerializationCluster** cluster_ref =
+      is_canonical ? &canonical_clusters_by_cid_[cid] : &clusters_by_cid_[cid];
+  if (*cluster_ref == nullptr) {
+    *cluster_ref = NewClusterForClass(cid);
+    if (*cluster_ref == nullptr) {
       UnexpectedObject(object, "No serialization cluster defined");
     }
-    if (is_canonical) {
-      canonical_clusters_by_cid_[cid] = cluster;
-    } else {
-      clusters_by_cid_[cid] = cluster;
-    }
   }
+  SerializationCluster* cluster = *cluster_ref;
   ASSERT(cluster != nullptr);
 
 #if defined(SNAPSHOT_BACKTRACE)
@@ -6171,6 +6197,7 @@ Deserializer::Deserializer(Thread* thread,
                            intptr_t size,
                            const uint8_t* data_buffer,
                            const uint8_t* instructions_buffer,
+                           bool is_non_root_unit,
                            intptr_t offset)
     : ThreadStackResource(thread),
       heap_(thread->isolate()->heap()),
@@ -6183,7 +6210,8 @@ Deserializer::Deserializer(Thread* thread,
       previous_text_offset_(0),
       canonical_clusters_(nullptr),
       clusters_(nullptr),
-      field_table_(thread->isolate()->field_table()) {
+      field_table_(thread->isolate()->field_table()),
+      is_non_root_unit_(is_non_root_unit) {
   if (Snapshot::IncludesCode(kind)) {
     ASSERT(instructions_buffer != nullptr);
     ASSERT(data_buffer != nullptr);
@@ -6218,7 +6246,13 @@ DeserializationCluster* Deserializer::ReadCluster() {
       case kPcDescriptorsCid:
       case kCodeSourceMapCid:
       case kCompressedStackMapsCid:
-        return new (Z) RODataDeserializationCluster();
+        return new (Z) RODataDeserializationCluster(cid);
+      case kOneByteStringCid:
+      case kTwoByteStringCid:
+        if (!is_non_root_unit_) {
+          return new (Z) RODataDeserializationCluster(cid);
+        }
+        break;
     }
   }
 
@@ -7076,7 +7110,8 @@ ApiErrorPtr FullSnapshotReader::ReadVMSnapshot() {
   }
 
   Deserializer deserializer(thread_, kind_, buffer_, size_, data_image_,
-                            instructions_image_, offset);
+                            instructions_image_, /*is_non_root_unit=*/false,
+                            offset);
   ApiErrorPtr api_error = deserializer.VerifyImageAlignment();
   if (api_error != ApiError::null()) {
     return api_error;
@@ -7116,7 +7151,8 @@ ApiErrorPtr FullSnapshotReader::ReadProgramSnapshot() {
   }
 
   Deserializer deserializer(thread_, kind_, buffer_, size_, data_image_,
-                            instructions_image_, offset);
+                            instructions_image_, /*is_non_root_unit=*/false,
+                            offset);
   ApiErrorPtr api_error = deserializer.VerifyImageAlignment();
   if (api_error != ApiError::null()) {
     return api_error;
@@ -7149,8 +7185,9 @@ ApiErrorPtr FullSnapshotReader::ReadUnitSnapshot(const LoadingUnit& unit) {
     return ConvertToApiError(error);
   }
 
-  Deserializer deserializer(thread_, kind_, buffer_, size_, data_image_,
-                            instructions_image_, offset);
+  Deserializer deserializer(
+      thread_, kind_, buffer_, size_, data_image_, instructions_image_,
+      /*is_non_root_unit=*/unit.id() != LoadingUnit::kRootId, offset);
   ApiErrorPtr api_error = deserializer.VerifyImageAlignment();
   if (api_error != ApiError::null()) {
     return api_error;

runtime/vm/clustered_snapshot.h
@@ -417,7 +417,7 @@ class Serializer : public ThreadStackResource {
   }
 
  private:
-  static const char* ReadOnlyObjectType(intptr_t cid);
+  const char* ReadOnlyObjectType(intptr_t cid);
 
   Heap* heap_;
   Zone* zone_;
@@ -548,6 +548,7 @@ class Deserializer : public ThreadStackResource {
                intptr_t size,
                const uint8_t* data_buffer,
                const uint8_t* instructions_buffer,
+               bool is_non_root_unit,
                intptr_t offset = 0);
   ~Deserializer();
 
@@ -658,6 +659,7 @@ class Deserializer : public ThreadStackResource {
   DeserializationCluster** canonical_clusters_;
   DeserializationCluster** clusters_;
   FieldTable* field_table_;
+  const bool is_non_root_unit_;
 };
 
 #define ReadFromTo(obj, ...) d->ReadFromTo(obj, ##__VA_ARGS__);

runtime/vm/compiler/asm_intrinsifier_arm64.cc
@@ -1986,17 +1986,11 @@ static void TryAllocateString(Assembler* assembler,
   __ mov(R6, length_reg);  // Save the length register.
   if (cid == kOneByteStringCid) {
     // Untag length.
-    __ adds(length_reg, ZR, Operand(length_reg, ASR, kSmiTagSize));
+    __ SmiUntag(length_reg, length_reg);
   } else {
     // Untag length and multiply by element size -> no-op.
-    __ adds(length_reg, ZR, Operand(length_reg));
+    ASSERT(kSmiTagSize == 1);
   }
-  // If the length is 0 then we have to make the allocated size a bit bigger,
-  // otherwise the string takes up less space than an ExternalOneByteString,
-  // and cannot be externalized.  TODO(erikcorry): We should probably just
-  // return a static zero length string here instead.
-  // length <- (length != 0) ? length : (ZR + 1).
-  __ csinc(length_reg, length_reg, ZR, NE);
   const intptr_t fixed_size_plus_alignment_padding =
       target::String::InstanceSize() +
       target::ObjectAlignment::kObjectAlignment - 1;

runtime/vm/compiler/asm_intrinsifier_x64.cc
@@ -1943,18 +1943,11 @@ static void TryAllocateString(Assembler* assembler,
   __ pushq(RDI);                          // Preserve length.
   if (cid == kOneByteStringCid) {
     // Untag length.
-    __ sarq(RDI, Immediate(kSmiTagShift));
+    __ SmiUntag(RDI);
   } else {
     // Untag length and multiply by element size -> no-op.
-    __ testq(RDI, RDI);
+    ASSERT(kSmiTagSize == 1);
   }
-  // If the length is 0 then we have to make the allocated size a bit bigger,
-  // otherwise the string takes up less space than an ExternalOneByteString,
-  // and cannot be externalized.  TODO(erikcorry): We should probably just
-  // return a static zero length string here instead.
-  __ j(NOT_ZERO, &not_zero_length);
-  __ addq(RDI, Immediate(1));
-  __ Bind(&not_zero_length);
   const intptr_t fixed_size_plus_alignment_padding =
       target::String::InstanceSize() +
       target::ObjectAlignment::kObjectAlignment - 1;

runtime/vm/compiler/runtime_api.cc
@@ -934,6 +934,10 @@ word String::NextFieldOffset() {
   return -kWordSize;
 }
 
+word String::InstanceSize(word payload_size) {
+  return RoundedAllocationSize(String::InstanceSize() + payload_size);
+}
+
 word OneByteString::NextFieldOffset() {
   return -kWordSize;
 }

runtime/vm/image_snapshot.cc
@@ -271,6 +271,16 @@ intptr_t ImageWriter::SizeInSnapshot(ObjectPtr raw_object) {
       return compiler::target::Instructions::InstanceSize(
           Instructions::Size(raw_insns));
     }
+    case kOneByteStringCid: {
+      auto raw_str = String::RawCast(raw_object);
+      return compiler::target::String::InstanceSize(
+          String::LengthOf(raw_str) * OneByteString::kBytesPerElement);
+    }
+    case kTwoByteStringCid: {
+      auto raw_str = String::RawCast(raw_object);
+      return compiler::target::String::InstanceSize(
+          String::LengthOf(raw_str) * TwoByteString::kBytesPerElement);
+    }
     default: {
       const Class& clazz = Class::Handle(Object::Handle(raw_object).clazz());
       FATAL("Unsupported class %s in rodata section.\n", clazz.ToCString());
@@ -520,6 +530,24 @@ void ImageWriter::WriteROData(NonStreamingWriteStream* stream, bool vm) {
       ASSERT_EQUAL(stream->Position() - object_start,
                    compiler::target::PcDescriptors::HeaderSize());
       stream->WriteBytes(desc.raw()->ptr()->data(), desc.Length());
+    } else if (obj.IsString()) {
+      const String& str = String::Cast(obj);
+      RELEASE_ASSERT(String::GetCachedHash(str.raw()) != 0);
+      RELEASE_ASSERT(str.IsOneByteString() || str.IsTwoByteString());
+
+      stream->WriteTargetWord(static_cast<uword>(str.raw()->ptr()->length_));
+#if !defined(HASH_IN_OBJECT_HEADER)
+      stream->WriteTargetWord(static_cast<uword>(str.raw()->ptr()->hash_));
+#endif
+      ASSERT_EQUAL(stream->Position() - object_start,
+                   compiler::target::String::InstanceSize());
+      stream->WriteBytes(
+          str.IsOneByteString()
+              ? static_cast<const void*>(OneByteString::DataStart(str))
+              : static_cast<const void*>(TwoByteString::DataStart(str)),
+          str.Length() * (str.IsOneByteString()
+                              ? OneByteString::kBytesPerElement
+                              : TwoByteString::kBytesPerElement));
     } else {
       const Class& clazz = Class::Handle(obj.clazz());
       FATAL("Unsupported class %s in rodata section.\n", clazz.ToCString());
@@ -535,7 +563,9 @@ static UNLESS_DEBUG(constexpr) const uword kReadOnlyGCBits =
     ObjectLayout::OldAndNotRememberedBit::encode(true) |
     ObjectLayout::NewBit::encode(false);
 
-uword ImageWriter::GetMarkedTags(classid_t cid, intptr_t size) {
+uword ImageWriter::GetMarkedTags(classid_t cid,
+                                 intptr_t size,
+                                 bool is_canonical /* = false */) {
   // ObjectLayout::SizeTag expects a size divisible by kObjectAlignment and
   // checks this in debug mode, but the size on the target machine may not be
   // divisible by the host machine's object alignment if they differ.
@@ -554,7 +584,8 @@ uword ImageWriter::GetMarkedTags(classid_t cid, intptr_t size) {
                compiler::target::ObjectAlignment::kObjectAlignmentLog2);
 
   return kReadOnlyGCBits | ObjectLayout::ClassIdTag::encode(cid) |
-         ObjectLayout::SizeTag::encode(adjusted_size);
+         ObjectLayout::SizeTag::encode(adjusted_size) |
+         ObjectLayout::CanonicalBit::encode(is_canonical);
 }
 
 uword ImageWriter::GetMarkedTags(const Object& obj) {
@@ -562,7 +593,8 @@ uword ImageWriter::GetMarkedTags(const Object& obj) {
 #if defined(HASH_IN_OBJECT_HEADER)
       static_cast<uword>(obj.raw()->ptr()->hash_) << kBitsPerInt32 |
 #endif
-      GetMarkedTags(obj.raw()->GetClassId(), SizeInSnapshot(obj));
+      GetMarkedTags(obj.raw()->GetClassId(), SizeInSnapshot(obj),
+                    obj.IsCanonical());
 }
 
 const char* ImageWriter::SectionSymbol(ProgramSection section, bool vm) const {

runtime/vm/image_snapshot.h
@@ -319,7 +319,9 @@ class ImageWriter : public ValueObject {
   // shared by both.
   const char* SectionSymbol(ProgramSection section, bool vm) const;
 
-  static uword GetMarkedTags(classid_t cid, intptr_t size);
+  static uword GetMarkedTags(classid_t cid,
+                             intptr_t size,
+                             bool is_canonical = false);
   static uword GetMarkedTags(const Object& obj);
 
   void DumpInstructionStats();

commit 11c62356ec9c8dd91da6482a8548480eda6579d9

[vm] Fix compilation issue on Mac
b97e8ecf72304012357adc2c6cb47c6ad0b4ed8a introduced an accidental type
mismatch which is only revealed on Mac

TBR=rmacnak@google.com

Change-Id: I6cb8d0cf14afe9961507ba15cc6342f0d0a3d1ae
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/170090
Reviewed-by: Vyacheslav Egorov <vegorov@google.com>

runtime/vm/compiler/runtime_api.h
@@ -702,7 +702,7 @@ class String : public AllStatic {
   static word length_offset();
   static word InstanceSize();
   static word NextFieldOffset();
-  static word InstanceSize(intptr_t payload_size);
+  static word InstanceSize(word payload_size);
 };
 
 class OneByteString : public AllStatic {