b1b5f82c528b9e8cda3cdbead1fbe4159551a454
Alexander Thomas <athom@google.com>
2020-11-13 14:52:24 +0000

    [infra] Use buildbucket.v2 API in find_base_commit
    * Migrate find_base_commit to null-safety.
    * Replace incorrect use of the term "bot" with "builder".
    
    See also https://crbug.com/1112975.
    
    Change-Id: Iee07e46a19a6304184e66f36560fba88d9b18971
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/171730
    Reviewed-by: Jonas Termansen <sortie@google.com>

--- tools/bots/find_base_commit.dart
+++ tools/bots/find_base_commit.dart
@@ -3,9 +3,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Find the newest commit that has a full set of results on the bots.
-
-// @dart = 2.9
+// Find the newest commit that has a full set of results on the builders.
 
 import 'dart:async';
 import 'dart:convert';
@@ -17,13 +15,13 @@ import 'package:glob/glob.dart';
 
 void main(List<String> args) async {
   final parser = new ArgParser();
-  parser.addMultiOption("bot",
+  parser.addMultiOption("builder",
       abbr: "b",
-      help: "Select the bots matching the glob pattern [option is repeatable]",
+      help: "Select the builders matching the glob [option is repeatable]",
       splitCommas: false);
   parser.addOption("branch",
       abbr: "B",
-      help: "Select the bots building this branch",
+      help: "Select the builders building this branch",
       defaultsTo: "master");
   parser.addOption("count",
       abbr: "c", help: "List this many commits", defaultsTo: "1");
@@ -33,7 +31,7 @@ void main(List<String> args) async {
   if (options["help"]) {
     print("""
 Usage: find_base_commit.dart [OPTION]...
-Find the newest commit that has a full set of results on the bots.
+Find the newest commit that has a full set of results on the builders.
 
 The options are as follows:
 
@@ -43,34 +41,50 @@ ${parser.usage}""");
 
   int count = int.parse(options["count"]);
   final globs = new List<Glob>.from(
-      options["bot"].map((String pattern) => new Glob(pattern)));
+      options["builder"].map((String pattern) => new Glob(pattern)));
 
   // Download the most recent builds from buildbucket.
-  int maxBuilds = 1000;
-  final url = Uri.parse(
-      "https://cr-buildbucket.appspot.com/_ah/api/buildbucket/v1/search"
-      "?bucket=luci.dart.ci.sandbox"
-      "&max_builds=$maxBuilds"
-      "&status=COMPLETED"
-      "&fields=builds(url%2Cparameters_json)");
+  const maxBuilds = 1000;
+  final url = Uri.parse("https://cr-buildbucket.appspot.com"
+      "/prpc/buildbucket.v2.Builds/SearchBuilds");
   const maxRetries = 3;
   const timeout = const Duration(seconds: 30);
-  Map<String, dynamic> object;
+  final query = jsonEncode({
+    "predicate": {
+      "builder": {"project": "dart", "bucket": "ci.sandbox"},
+      "status": "ENDED_MASK"
+    },
+    "pageSize": maxBuilds,
+    "fields": "builds.*.builder.builder,builds.*.input"
+  });
+  late Map<String, dynamic> searchResult;
   for (int i = 1; i <= maxRetries; i++) {
     try {
       final client = new HttpClient();
-      final request = await client.getUrl(url).timeout(timeout);
+      final request = await client.postUrl(url).timeout(timeout)
+        ..headers.contentType = ContentType.json
+        ..headers.add(HttpHeaders.acceptHeader, ContentType.json)
+        ..write(query);
       final response = await request.close().timeout(timeout);
-      object = await response
+      if (response.statusCode != 200) {
+        print("Failed to search for builds: "
+            "${response.statusCode}:${response.reasonPhrase}");
+        exit(1);
+      }
+      const prefix = ")]}'";
+      searchResult = await (response
           .cast<List<int>>()
           .transform(new Utf8Decoder())
+          .map((event) =>
+              event.startsWith(prefix) ? event.substring(prefix.length) : event)
           .transform(new JsonDecoder())
+          .cast<Map<String, dynamic>>()
           .first
-          .timeout(timeout);
+          .timeout(timeout));
       client.close();
       break;
     } on TimeoutException catch (e) {
-      final inSeconds = e.duration.inSeconds;
+      final inSeconds = e.duration?.inSeconds;
       stderr.writeln(
           "Attempt $i of $maxRetries timed out after $inSeconds seconds");
       if (i == maxRetries) {
@@ -81,57 +95,61 @@ ${parser.usage}""");
   }
 
   // Locate the builds we're interested in and map them to each commit. The
-  // builds returned by the API are sorted with the newest first. Since bots
+  // builds returned by the API are sorted with the newest first. Since builders
   // don't build back in time and always build the latest commit whenever they
   // can, the first time we see a commit, we know it's newer than all commits
-  // we haven't seen yet. The insertion order into the botsForCommits map will
-  // then sorted with the newest commit first.
-  final builds = object["builds"];
-  final botsForCommits = <String, Set<String>>{};
+  // we haven't seen yet. The insertion order into the buildersForCommits map
+  // will then sorted with the newest commit first.
+  final builds = searchResult["builds"];
+  if (builds == null) {
+    print("No builds found");
+    exit(1);
+  }
+  final buildersForCommits = <String, Set<String>>{};
   for (final build in builds) {
-    final parameters = jsonDecode(build["parameters_json"]);
-    final bot = parameters["builder_name"];
-    if (bot.endsWith("-beta") ||
-        bot.endsWith("-dev") ||
-        bot.endsWith("-stable")) {
+    final builder = build["builder"]?["builder"];
+    if (builder is! String ||
+        builder.endsWith("-beta") ||
+        builder.endsWith("-dev") ||
+        builder.endsWith("-stable")) {
       // Ignore the release builders. The -try builders aren't in the
       // bucket we're reading.
       continue;
     }
-    if (globs.isNotEmpty && !globs.any((glob) => glob.matches(bot))) {
-      // Filter way bots we're not interested in.
+    if (globs.isNotEmpty && !globs.any((glob) => glob.matches(builder))) {
+      // Filter way builders we're not interested in.
       continue;
     }
-    final properties = parameters["properties"];
-    final branch = properties["branch"];
-    if (branch != null && branch != "refs/heads/${options['branch']}") {
-      // Ignore bots that are building the wrong branch.
+    final input = build["input"]?["gitilesCommit"];
+    if (input == null) {
+      // Ignore builds not triggered by a commit, e.g. fuzz-linux.
       continue;
     }
-    final commit = properties["revision"];
-    if (commit == null) {
-      // Ignore bots that aren't commit based, e.g. fuzz-linux.
+    final ref = input["ref"];
+    if (ref != "refs/heads/${options['branch']}") {
+      // Ignore builds on the wrong branch.
       continue;
     }
-    final botsForCommit =
-        botsForCommits.putIfAbsent(commit, () => new Set<String>());
-    botsForCommit.add(bot);
+    final commit = input["id"] as String;
+    final buildersForCommit =
+        buildersForCommits.putIfAbsent(commit, () => new Set<String>());
+    buildersForCommit.add(builder);
   }
 
-  if (botsForCommits.isEmpty) {
-    print("Failed to locate any commits having run on the bots");
+  if (buildersForCommits.isEmpty) {
+    print("Failed to locate any commits having run on the builders");
     exitCode = 1;
     return;
   }
 
   int maxBots = 0;
-  for (final commit in botsForCommits.keys) {
-    maxBots = max(maxBots, botsForCommits[commit].length);
+  for (final builders in buildersForCommits.values) {
+    maxBots = max(maxBots, builders.length);
   }
 
-  // List commits run on the most bots.
-  for (final commit in botsForCommits.keys
-      .where((commit) => botsForCommits[commit].length == maxBots)
+  // List commits run on the most builders.
+  for (final commit in buildersForCommits.keys
+      .where((commit) => buildersForCommits[commit]!.length == maxBots)
       .take(count)) {
     print(commit);
   }
