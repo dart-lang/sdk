18b19db3006d6bf3d978cb6d13d1e0452dda6e80
Jacob MacDonald <jakemac@google.com>
2020-11-25 01:47:49 +0000

    update Glob in the sdk, also adds package:file to the sdk
    Change-Id: Ia8628c28bfaf6343c19eecfef391339968cd57e9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/172063
    Commit-Queue: Jake Macdonald <jakemac@google.com>
    Reviewed-by: Nate Bosch <nbosch@google.com>

--- pkg/test_runner/tool/update_static_error_tests.dart
+++ pkg/test_runner/tool/update_static_error_tests.dart
@@ -7,7 +7,9 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:file/file.dart' as pkg_file;
 import 'package:glob/glob.dart';
+import 'package:glob/list_local_fs.dart';
 import 'package:path/path.dart' as p;
 
 import 'package:test_runner/src/command_output.dart';
@@ -130,7 +132,7 @@ Future<void> main(List<String> args) async {
   for (var entry in glob.listSync(root: root)) {
     if (!entry.path.endsWith(".dart")) continue;
 
-    if (entry is File) {
+    if (entry is pkg_file.File) {
       await _processFile(entry,
           dryRun: dryRun,
           remove: removeSources,
