d518832a7c9f6f9925da6d7595803f485ab41917
Konstantin Shcheglov <scheglov@google.com>
2020-12-05 18:31:33 +0000

    Simplify type hierarchy for resynthesize tests.
    Change-Id: I82ba179c4f2756c63f3a30ea71f5f6332b902a6d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/175180
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/summary/test_strategies.dart
+++ pkg/analyzer/test/src/summary/test_strategies.dart
@@ -2,21 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/dart/analysis/declared_variables.dart';
 import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/error/listener.dart';
-import 'package:analyzer/file_system/memory_file_system.dart';
 import 'package:analyzer/src/dart/analysis/experiments.dart';
 import 'package:analyzer/src/dart/ast/ast.dart';
 import 'package:analyzer/src/dart/scanner/reader.dart';
 import 'package:analyzer/src/dart/scanner/scanner.dart';
 import 'package:analyzer/src/generated/parser.dart';
 import 'package:analyzer/src/generated/source.dart';
-
-import 'resynthesize_common.dart';
 
 CompilationUnit parseText(
   String text,
@@ -48,43 +44,4 @@ CompilationUnit parseText(
   );
 
   return unit;
-}
-
-/// Abstract base class for tests of summary resynthesis.
-///
-/// Test classes should not extend this class directly; they should extend a
-/// class that implements this class with methods that drive summary generation.
-/// The tests themselves can then be provided via mixin, allowing summaries to
-/// be tested in a variety of ways.
-abstract class ResynthesizeTestStrategy {
-  /// The set of features enabled in this test.
-  FeatureSet featureSet;
-
-  set allowMissingFiles(bool value);
-
-  set declaredVariables(DeclaredVariables declaredVariables);
-
-  MemoryResourceProvider get resourceProvider;
-
-  set testFile(String value);
-
-  Source get testSource;
-
-  void addLibrary(String uri);
-
-  Source addLibrarySource(String filePath, String contents);
-
-  Source addSource(String path, String contents);
-
-  Source addTestSource(String code, [Uri uri]);
-}
-
-/// Implementation of [ResynthesizeTestStrategy] that drives summary
-/// generation using the old two-phase API.
-class ResynthesizeTestStrategyTwoPhase extends AbstractResynthesizeTest
-    implements ResynthesizeTestStrategy {
-  @override
-  FeatureSet featureSet = FeatureSet.forTesting(sdkVersion: '2.7.0');
-
-  final Set<Source> serializedSources = <Source>{};
 }
