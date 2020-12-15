76dcd9e37d106e3d6680da8f1078b38f2c4d2994
Johnni Winther <johnniwinther@google.com>
2020-12-15 15:34:40 +0000

    [cfe] Ensure implicit-as and typedef reference are legacy erased
    TEST=existing expectation tests
    
    Change-Id: I48571347ef95b709a0804d284902ac6af7115afd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/176083
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>

--- pkg/kernel/lib/src/replacement_visitor.dart
+++ pkg/kernel/lib/src/replacement_visitor.dart
@@ -21,7 +21,11 @@ class ReplacementVisitor implements DartTypeVisitor<DartType> {
     List<TypeParameter> newTypeParameters;
     for (int i = 0; i < node.typeParameters.length; i++) {
       TypeParameter typeParameter = node.typeParameters[i];
-      DartType newBound = typeParameter.bound.accept(this);
+      // TODO(johnniwinther): Bounds should not be null, even in case of
+      // cyclic typedefs. Currently
+      //   instantiate_to_bound/non_simple_class_parametrized_typedef_cycle
+      // fails with this.
+      DartType newBound = typeParameter.bound?.accept(this);
       DartType newDefaultType = typeParameter.defaultType?.accept(this);
       if (newBound != null || newDefaultType != null) {
         newTypeParameters ??= node.typeParameters.toList(growable: false);
