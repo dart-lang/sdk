5645bd0e0674f2481cce5d15435d2eb4e5774638
Johnni Winther <johnniwinther@google.com>
2020-12-28 14:43:14 +0000

    [cfe] Don't replace dispatch target with interface target on super property set
    In response to b/176332835
    
    Change-Id: Id131ba44817aa55399ba3b44b9fe5db833847f75
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/177100
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

---
+++ pkg/front_end/testcases/general/super_set_covariant.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+class SuperClass {
+  void set setter(Object o) {}
+}
+
+abstract class Class extends SuperClass {
+  // TODO(johnniwinther): Should this introduce a forwarding super stub, and
+  // if so, should the target of the super set below be the forwarding super
+  // stub?
+  void set setter(covariant int o);
+}
+
+class SubClass extends Class {
+  void set setter(covariant int o) {
+    super.setter = '$o';
+  }
+}
+
+test() {
+  new SubClass().setter = 0;
+}
+
+main() {}
