fe7efd5606e3fd93c10e7fc6f0536b0e331364b7
Lasse R.H. Nielsen <lrn@google.com>
2021-01-12 13:06:41 +0000

    Update documentation in dart:core.
    Fix null safety issues and tweak documentation where it can be improved.
    
    Change-Id: Ia8cc20274ae151c454662bf5f137913787365a64
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/177960
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Nate Bosch <nbosch@google.com>

--- sdk/lib/core/expando.dart
+++ sdk/lib/core/expando.dart
@@ -4,59 +4,47 @@
 
 part of dart.core;
 
-/**
- * An [Expando] allows adding new properties to objects.
- *
- * Does not work on numbers, strings, booleans or `null`.
- *
- * An `Expando` does not hold on to the added property value after an object
- * becomes inaccessible.
- *
- * Since you can always create a new number that is identical to an existing
- * number, it means that an expando property on a number could never be
- * released. To avoid this, expando properties cannot be added to numbers.
- * The same argument applies to strings, booleans and `null`, which also have
- * literals that evaluate to identical values when they occur more than once.
- *
- * There is no restriction on other classes, even for compile time constant
- * objects. Be careful if adding expando properties to compile time constants,
- * since they will stay alive forever.
- */
+/// An [Expando] allows adding new properties to objects.
+///
+/// Does not work on numbers, strings, booleans or `null`.
+///
+/// An `Expando` does not hold on to the added property value after an object
+/// becomes inaccessible.
+///
+/// Since you can always create a new number that is identical to an existing
+/// number, it means that an expando property on a number could never be
+/// released. To avoid this, expando properties cannot be added to numbers.
+/// The same argument applies to strings, booleans and `null`, which also have
+/// literals that evaluate to identical values when they occur more than once.
+///
+/// There is no restriction on other classes, even for compile time constant
+/// objects. Be careful if adding expando properties to compile time constants,
+/// since they will stay alive forever.
 class Expando<T extends Object> {
-  /**
-   * The name of the this [Expando] as passed to the constructor. If
-   * no name was passed to the constructor, the name is `null`.
-   */
+  /// The name of the this [Expando] as passed to the constructor. If
+  /// no name was passed to the constructor, the name is `null`.
   final String? name;
 
-  /**
-   * Creates a new [Expando]. The optional name is only used for
-   * debugging purposes and creating two different [Expando]s with the
-   * same name yields two [Expando]s that work on different properties
-   * of the objects they are used on.
-   */
+  /// Creates a new [Expando]. The optional name is only used for
+  /// debugging purposes and creating two different [Expando]s with the
+  /// same name yields two [Expando]s that work on different properties
+  /// of the objects they are used on.
   external Expando([String? name]);
 
-  /**
-   * Expando toString method override.
-   */
-  String toString() => "Expando:${name.toString()}";
+  /// Expando toString method override.
+  String toString() => "Expando:$name";
 
-  /**
-   * Gets the value of this [Expando]'s property on the given
-   * object. If the object hasn't been expanded, the method returns
-   * `null`.
-   *
-   * The object must not be a number, a string, a boolean or null.
-   */
+  /// Gets the value of this [Expando]'s property on the given
+  /// object. If the object hasn't been expanded, the method returns
+  /// `null`.
+  ///
+  /// The object must not be a number, a string or a boolean.
   external T? operator [](Object object);
 
-  /**
-   * Sets the value of this [Expando]'s property on the given
-   * object. Properties can effectively be removed again by setting
-   * their value to null.
-   *
-   * The object must not be a number, a string, a boolean or null.
-   */
+  /// Sets the value of this [Expando]'s property on the given
+  /// object. Properties can effectively be removed again by setting
+  /// their value to `null`.
+  ///
+  /// The object must not be a number, a string or a boolean.
   external void operator []=(Object object, T? value);
 }
--- sdk/lib/core/string_buffer.dart
+++ sdk/lib/core/string_buffer.dart
@@ -4,47 +4,45 @@
 
 part of dart.core;
 
-/**
- * A class for concatenating strings efficiently.
- *
- * Allows for the incremental building of a string using write*() methods.
- * The strings are concatenated to a single string only when [toString] is
- * called.
- */
+/// A class for concatenating strings efficiently.
+///
+/// Allows for the incremental building of a string using `write*()` methods.
+/// The strings are concatenated to a single string only when [toString] is
+/// called.
 class StringBuffer implements StringSink {
-  /** Creates the string buffer with an initial content. */
+  /// Creates the string buffer with an initial content.
   external StringBuffer([Object content = ""]);
 
-  /**
-   * Returns the length of the content that has been accumulated so far.
-   * This is a constant-time operation.
-   */
+  /// Returns the length of the content that has been accumulated so far.
+  /// This is a constant-time operation.
   external int get length;
 
-  /** Returns whether the buffer is empty. This is a constant-time operation. */
+  /// Returns whether the buffer is empty. This is a constant-time operation.
   bool get isEmpty => length == 0;
 
-  /**
-   * Returns whether the buffer is not empty. This is a constant-time
-   * operation.
-   */
+  /// Returns whether the buffer is not empty. This is a constant-time
+  /// operation.
   bool get isNotEmpty => !isEmpty;
 
-  /// Adds the contents of [obj], converted to a string, to the buffer.
-  external void write(Object? obj);
+  /// Adds the string representatoon of [object] to the buffer.
+  external void write(Object? object);
 
   /// Adds the string representation of [charCode] to the buffer.
+  ///
+  /// Equivalent to `write(String.fromCharCode(charCode))`.
   external void writeCharCode(int charCode);
 
+  /// Writes all [objects] separated by [separator].
+  ///
+  /// Writes each individual object in [objects] in iteration order,
+  /// and writes [separtor] between any two objects.
   external void writeAll(Iterable<dynamic> objects, [String separator = ""]);
 
   external void writeln([Object? obj = ""]);
 
-  /**
-   * Clears the string buffer.
-   */
+  /// Clears the string buffer.
   external void clear();
 
-  /// Returns the contents of buffer as a concatenated string.
+  /// Returns the contents of buffer as a single string.
   external String toString();
 }
