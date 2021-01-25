91b0f166637e750966b52bc942487c4dafd32ccf
Alexander Markov <alexmarkov@google.com>
2021-01-25 20:06:43 +0000

    Migrate samples/sample_extension to null safety
    Closes https://github.com/dart-lang/sdk/issues/43711
    
    Change-Id: I7628e4a6192efb26a12f8051b4fa87dfafabab9c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/180960
    Reviewed-by: Régis Crelier <regis@google.com>
    Commit-Queue: Alexander Markov <alexmarkov@google.com>

--- samples/sample_extension/sample_synchronous_extension.dart
+++ samples/sample_extension/sample_synchronous_extension.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
-
 library sample_synchronous_extension;
 
 import 'dart-ext:sample_extension';
