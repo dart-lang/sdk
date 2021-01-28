4a484c88e13ef0f4847daba3c25fa22dbe933a08
Lasse R.H. Nielsen <lrn@google.com>
2021-01-28 17:19:35 +0000

    Make ListMixin.take check that the argument is not null.
    Fixes #41743, #30155
    BUG= http://dartbug.com/41743
    
    Change-Id: I223905a92b7353cdc50498d265a6dfe2f2f614d9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/160640
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Nate Bosch <nbosch@google.com>

--- tests/corelib_2/iterable_take_test.dart
+++ tests/corelib_2/iterable_take_test.dart
@@ -224,4 +224,15 @@ main() {
   Expect.throwsRangeError(() => list3.map((x) => x).skip(-1));
   Expect.throwsRangeError(() => set1.map((x) => x).skip(-1));
   Expect.throwsRangeError(() => set2.map((x) => x).skip(-1));
+
+  Expect.throws(() => list1.take(null));
+  Expect.throws(() => list2.take(null));
+  Expect.throws(() => list3.take(null));
+  Expect.throws(() => set1.take(null));
+  Expect.throws(() => set2.take(null));
+  Expect.throws(() => list1.map((x) => x).take(null));
+  Expect.throws(() => list2.map((x) => x).take(null));
+  Expect.throws(() => list3.map((x) => x).take(null));
+  Expect.throws(() => set1.map((x) => x).take(null));
+  Expect.throws(() => set2.map((x) => x).take(null));
 }
