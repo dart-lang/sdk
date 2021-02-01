2ecc9b76c829413813e6ac07e28f4c63d91b01ec
pq <pquitslund@google.com>
2021-02-01 18:47:59 +0000

    initial sdk API fix data
    Some simple SDK API changes codified in fix data w/ accompanying tests.
    
    Tests are in `tests/lib/fix_data` and run on the pkg bots.
    
    Sample run:
    
    https://ci.chromium.org/ui/p/dart/builders/try/pkg-linux-release-try/46526/steps?succeeded=true&debug=false
    https://logs.chromium.org/logs/dart/buildbucket/cr-buildbucket.appspot.com/8856735093298466576/+/steps/validate_SDK_API_data-driven_fixes/0/stdout
    
    
    Change-Id: Id0959d6cde4003f114056c21b9969f2792f21a6f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/180940
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

---
+++ tests/lib/fix_data_tests/file.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+void main() {
+  print(FileMode.APPEND);
+  print(FileMode.READ);
+  print(FileMode.WRITE);
+  print(FileMode.WRITE_ONLY);
+  print(FileMode.WRITE_ONLY_APPEND);
+}
