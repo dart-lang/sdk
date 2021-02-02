dfd5413952ee5232235adf460575a1a7c2674874
Clement Skau <cskau@google.com>
2021-02-02 06:59:54 +0000

    [VM] Adds Debugger break for Future.catchError
    TEST=Added new Future.catchError pause-on-unhandled-exceptions test.
    
    Bug: https://github.com/dart-lang/sdk/issues/37953
    Change-Id: Ieb3cf834eacad4f9b98ab2db0e5813e9bfa09747
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/180840
    Commit-Queue: Clement Skau <cskau@google.com>
    Reviewed-by: Daco Harkes <dacoharkes@google.com>

---
+++ runtime/observatory_2/tests/service_2/pause_on_unhandled_exceptions_catcherror_test.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Regression: https://github.com/dart-lang/sdk/issues/37953
+
+import 'test_helper.dart';
+import 'service_test_common.dart';
+
+Future<void> throwAsync() async {
+  await Future.delayed(const Duration(milliseconds: 100));
+  throw 'Throw from throwAsync!';
+}
+
+testeeMain() async {
+  await throwAsync().then((v) {
+    print('Hello from then()!');
+  }).catchError((e, st) {
+    print('Caught in catchError: $e!');
+  });
+}
+
+var tests = <IsolateTest>[
+  // We shouldn't get any debugger breaks before exit as all exceptions are
+  // caught (via `Future.catchError()`).
+  hasStoppedAtExit,
+];
+
+main(args) => runIsolateTests(args, tests,
+    pause_on_unhandled_exceptions: true,
+    pause_on_exit: true,
+    testeeConcurrent: testeeMain,
+    extraArgs: extraDebuggingArgs);
