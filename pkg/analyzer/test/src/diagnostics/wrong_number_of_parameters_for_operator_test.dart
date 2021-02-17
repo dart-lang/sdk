dbe2dee928a783eaebce667f78fa8d2b2af19d72
Sam Rawlins <srawlins@google.com>
2021-02-17 05:24:40 +0000

    analyzer: Add tests ensuring that duplicate operator parameter errors are not raised
    Change-Id: Icb9e68164d36010ea6c7b1f83356d592b2bc8416
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/185204
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer/test/src/diagnostics/wrong_number_of_parameters_for_operator_test.dart
+++ pkg/analyzer/test/src/diagnostics/wrong_number_of_parameters_for_operator_test.dart
@@ -80,6 +80,8 @@ class A {
   test_unaryTilde() async {
     await _check('~', 'a');
     await _check('~', 'a, b');
+    await _check('~', 'a, [b]');
+    await _check('~', 'a, {b}');
   }
 
   Future<void> _check(String name, String parameters) async {
@@ -108,5 +110,7 @@ class A {
   Future<void> _checkTooFewAndTooMany(String name) async {
     await _check(name, '');
     await _check(name, 'a, b');
+    await _check(name, 'a, [b]');
+    await _check(name, 'a, {b}');
   }
 }
