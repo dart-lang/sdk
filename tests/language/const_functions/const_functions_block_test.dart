3210fb43068fe405b628f1580108a3a2de6d9ae0
Kallen Tu <kallentu@google.com>
2021-03-10 15:23:18 +0000

    [cfe] Environment parents and block tests for const functions.
    Fixed up block environment and added tests for them.
    
    Change-Id: I97375c90c9244f6dcf8294dc11b0d4e70697d114
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/190320
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Reviewed-by: Jake Macdonald <jakemac@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/const_functions/const_functions_block_test.dart
@@ -0,0 +1,55 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Tests blocks and scope with const functions.
+
+// SharedOptions=--enable-experiment=const-functions
+
+import "package:expect/expect.dart";
+
+void blockTest() {
+  int x() => 1;
+  const i = x();
+  //        ^^^
+  // [analyzer] COMPILE_TIME_ERROR.CONST_INITIALIZED_WITH_NON_CONSTANT_VALUE
+  Expect.equals(i, 1);
+  {
+    int x() => 2;
+    const y = x();
+    //        ^^^
+    // [analyzer] COMPILE_TIME_ERROR.CONST_INITIALIZED_WITH_NON_CONSTANT_VALUE
+    Expect.equals(y, 2);
+    {
+      int x() => 3;
+      const y = x();
+      //        ^^^
+      // [analyzer] COMPILE_TIME_ERROR.CONST_INITIALIZED_WITH_NON_CONSTANT_VALUE
+      Expect.equals(y, 3);
+    }
+  }
+  const z = x();
+  //        ^^^
+  // [analyzer] COMPILE_TIME_ERROR.CONST_INITIALIZED_WITH_NON_CONSTANT_VALUE
+  Expect.equals(z, 1);
+}
+
+void blockTest1() {
+  int x() {
+    int z = 3;
+    {
+      int z = 4;
+    }
+    return z;
+  }
+
+  const i = x();
+  //        ^^^
+  // [analyzer] COMPILE_TIME_ERROR.CONST_INITIALIZED_WITH_NON_CONSTANT_VALUE
+  Expect.equals(i, 3);
+}
+
+void main() {
+  blockTest();
+  blockTest1();
+}
