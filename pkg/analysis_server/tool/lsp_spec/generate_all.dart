c5d01ec4f7078a75b038a2ed04312afb50154323
Brian Wilkerson <brianwilkerson@google.com>
2021-03-19 14:44:27 +0000

    Remove a couple of ignore-in-file comments and make one more unnecessary in generated code
    Change-Id: I8313fe2b4c3908dd5751f7eeca417adfcb2e5790
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/192040
    Reviewed-by: Danny Tuppeny <danny@tuppeny.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/tool/lsp_spec/generate_all.dart
+++ pkg/analysis_server/tool/lsp_spec/generate_all.dart
@@ -160,11 +160,7 @@ String generatedFileHeader(int year, {bool importCustom = false}) => '''
 // "pkg/analysis_server/tool/lsp_spec/generate_all.dart".
 
 // ignore_for_file: annotate_overrides
-// ignore_for_file: deprecated_member_use
-// ignore_for_file: deprecated_member_use_from_same_package
-// ignore_for_file: unnecessary_brace_in_string_interps
 // ignore_for_file: unnecessary_parenthesis
-// ignore_for_file: unused_import
 // ignore_for_file: unused_shown_name
 
 import 'dart:core' hide deprecated;
