6f6afa4dbaa10dd3fba39b1ac55811b64a0fff91
Joshua Litt <joshualitt@google.com>
2021-03-22 18:57:24 +0000

    [dart2js] Add component detection to deferred fragment merge.
    This is to avoid merging unrelated code into language files.
    
    Change-Id: Ief514d0761fbb471a0460c78db35c0a07bf78f6d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/191800
    Commit-Queue: Joshua Litt <joshualitt@google.com>
    Reviewed-by: Stephen Adams <sra@google.com>

---
+++ pkg/compiler/test/deferred_loading/data/components/libB.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "libBCDE.dart";
+
+/*member: component:member_unit=2{libB}*/
+String component() {
+  return componentBCDE();
+}
