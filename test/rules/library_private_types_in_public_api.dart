9099cfe18f4630910f051b06721745017154aa64
Phil Quitslund <pq@users.noreply.github.com>
2021-03-23 11:53:55 -0700

    => dart test (dart-lang/linter#2550)

--- test/rules/library_private_types_in_public_api.dart
+++ test/rules/library_private_types_in_public_api.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// test w/ `pub run test -N library_private_types_in_public_api`
+// test w/ `dart test -N library_private_types_in_public_api`
 
 // Top level functions
 
