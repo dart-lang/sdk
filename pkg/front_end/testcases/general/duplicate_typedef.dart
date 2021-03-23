0de315c14649fbe15eb2576fa2040dd5fe3fb234
Johnni Winther <johnniwinther@google.com>
2021-03-23 07:32:26 +0000

    [cfe] Compute variance for type parameters on duplicate typedefs
    Change-Id: I6e4a05911c77d26f1fb440ec8e90c13e783926e9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/192147
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

---
+++ pkg/front_end/testcases/general/duplicate_typedef.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+typedef F = void Function();
+typedef F = void Function();
+
+typedef G<T> = void Function(T);
+typedef G<T> = void Function(T);
+
+main() {}
