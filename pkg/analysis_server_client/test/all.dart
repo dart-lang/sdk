a4fd3f8ba3b3f362c57f93f6fae97c827d7b25ca
Konstantin Shcheglov <scheglov@google.com>
2021-04-05 16:10:16 +0000

    Remove remaining opt-outs to 2.9 from analysis_server_client.
    Change-Id: I4d971ef7cbc433894b4219f3e942eef40db24a60
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/194010
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server_client/test/all.dart
+++ pkg/analysis_server_client/test/all.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
-
 import 'live_test.dart' as live;
 import 'server_test.dart' as server;
 import 'verify_sorted_test.dart' as verify_sorted;
