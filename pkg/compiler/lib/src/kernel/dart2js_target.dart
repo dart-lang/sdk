853597a978b8c63109322a0e510909861646b301
Riley Porter <rileyporter@google.com>
2021-04-21 19:01:11 +0000

    Optimize js_util setProperty calls for non-function value types.
    No change in the generated JavaScript for dart2js base on a sample
    foo.dart file and tests/lib/js/js_util/properties_test.dart
    
    Bug: #44533
    Change-Id: Ib6866fa7fca1f13b03c75dd4f1eac904b18811e3
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/193838
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Commit-Queue: Riley Porter <rileyporter@google.com>

--- pkg/compiler/lib/src/kernel/dart2js_target.dart
+++ pkg/compiler/lib/src/kernel/dart2js_target.dart
@@ -110,6 +110,7 @@ class Dart2jsTarget extends Target {
         'dart:_interceptors',
         'dart:_js_helper',
         'dart:_late_helper',
+        'dart:js',
         'dart:js_util'
       ];
 
