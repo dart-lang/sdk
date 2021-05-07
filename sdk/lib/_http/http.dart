56243f5008ee59184c97e69ebaf820181f6351d1
Kleak <me@kleak.dev>
2021-05-07 12:52:55 +0200

    remove type

--- sdk/lib/_http/http.dart
+++ sdk/lib/_http/http.dart
@@ -391,17 +391,17 @@ abstract class HttpHeaders {
   static const acceptEncodingHeader = "accept-encoding";
   static const acceptLanguageHeader = "accept-language";
   static const acceptRangesHeader = "accept-ranges";
-  static const String accessControlAllowCredentialsHeader =
+  static const accessControlAllowCredentialsHeader =
       'access-control-allow-credentials';
   static const accessControlAllowHeadersHeader = 'access-control-allow-headers';
   static const accessControlAllowMethodsHeader = 'access-control-allow-methods';
   static const accessControlAllowOriginHeader = 'access-control-allow-origin';
-  static const String accessControlExposeHeadersHeader =
+  static const accessControlExposeHeadersHeader =
       'access-control-expose-headers';
-  static const String accessControlMaxAgeHeader = 'access-control-max-age';
-  static const String accessControlRequestHeadersHeader =
+  static const accessControlMaxAgeHeader = 'access-control-max-age';
+  static const accessControlRequestHeadersHeader =
       'access-control-request-headers';
-  static const String accessControlRequestMethodHeader =
+  static const accessControlRequestMethodHeader =
       'access-control-request-method';
   static const ageHeader = "age";
   static const allowHeader = "allow";
