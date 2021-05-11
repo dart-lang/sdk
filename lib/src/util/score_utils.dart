f5b3267dd41ace7af56afd50a464c24edc036f99
Phil Quitslund <pq@users.noreply.github.com>
2021-05-11 12:47:36 -0700

    label rulesets (dart-lang/linter#2635)

--- lib/src/util/score_utils.dart
+++ lib/src/util/score_utils.dart
@@ -5,15 +5,63 @@
 import 'package:analyzer/src/lint/config.dart'; // ignore: implementation_imports
 import 'package:http/http.dart' as http;
 
-final _pedanticOptionsRootUrl =
-    Uri.https('raw.githubusercontent.com', '/dart-lang/pedantic/master/lib/');
-final _pedanticOptionsUrl =
-    _pedanticOptionsRootUrl.resolve('analysis_options.yaml');
+Future<List<String>> _readPedanticLints() async {
+  var version = await pedanticLatestVersion;
+  return _fetchLints(
+      'https://raw.githubusercontent.com/dart-lang/pedantic/master/lib/analysis_options.$version.yaml');
+}
+
+Future<List<String>> _readCoreLints() async => _fetchLints(
+    'https://raw.githubusercontent.com/dart-lang/lints/main/lib/core.yaml');
+
+Future<String> get pedanticLatestVersion async {
+  var url =
+      'https://raw.githubusercontent.com/dart-lang/pedantic/master/lib/analysis_options.yaml';
+  var client = http.Client();
+  print('loading $url...');
+  var req = await client.get(Uri.parse(url));
+  var parts = req.body.split('package:pedantic/analysis_options.');
+  return parts[1].split('.yaml')[0];
+}
+
+Future<List<String>> _readFlutterLints() async => _fetchLints(
+// todo(pq):update when landed
+    'https://raw.githubusercontent.com/dart-lang/lints/400c60bf379bbad5b161148f52a29c0e4fd378f6/lib/flutter.yaml');
+
+Future<List<String>> _readRecommendedLints() async => _fetchLints(
+    'https://raw.githubusercontent.com/dart-lang/lints/main/lib/recommended.yaml');
+
+/// todo(pq): update `scorecard.dart` to reuse these fetch functions.
+Future<List<String>> _fetchLints(String url) async {
+  var client = http.Client();
+  var req = await client.get(Uri.parse(url));
+  return _readLints(req.body);
+}
+
+List<String> _readLints(String contents) {
+  var lintConfigs = processAnalysisOptionsFile(contents);
+  if (lintConfigs == null) {
+    return [];
+  }
+  return lintConfigs.ruleConfigs.map((c) => c.name ?? '<unknown>').toList();
+}
 
 List<String>? _pedanticRules;
+List<String>? _coreRules;
+List<String>? _recommendedRules;
+List<String>? _flutterRules;
 
 Future<List<String>> get pedanticRules async =>
-    _pedanticRules ??= await _fetchPedanticRules();
+    _pedanticRules ??= await _readPedanticLints();
+
+Future<List<String>> get coreRules async =>
+    _coreRules ??= await _readCoreLints();
+
+Future<List<String>> get recommendedRules async =>
+    _recommendedRules ??= await _readRecommendedLints();
+
+Future<List<String>> get flutterRules async =>
+    _flutterRules ??= await _readFlutterLints();
 
 Future<List<String>> fetchRules(Uri optionsUrl) async {
   var config = await _fetchConfig(optionsUrl);
@@ -35,11 +83,4 @@ Future<LintConfig?> _fetchConfig(Uri url) async {
   print('loading $url...');
   var req = await http.get(url);
   return processAnalysisOptionsFile(req.body);
-}
-
-Future<List<String>> _fetchPedanticRules() async {
-  print('loading $_pedanticOptionsUrl...');
-  var req = await http.get(_pedanticOptionsUrl);
-  var includedOptions = req.body.split('include: package:pedantic/')[1].trim();
-  return fetchRules(_pedanticOptionsRootUrl.resolve(includedOptions));
 }
