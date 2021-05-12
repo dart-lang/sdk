b0ac3711f245afe95a44ec9a7c99f0c48858ef87
Martin Kustermann <kustermann@google.com>
2021-05-12 14:09:35 +0000

    [vm/concurrency] Add "iso-stress-linux" builder running isolate stress test configurations
    Issue https://github.com/dart-lang/sdk/issues/36097
    
    TEST=runtime/tests/concurrency/run_stress_test_shards.dart
    
    Change-Id: I7c76260161e7257f0ff77d6fc221d019156f98a6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/199248
    Commit-Queue: Martin Kustermann <kustermann@google.com>
    Reviewed-by: Alexander Aprelev <aam@google.com>

---
+++ runtime/tests/concurrency/run_stress_test_shards.dart
@@ -0,0 +1,134 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:io' as io;
+import 'dart:isolate';
+import 'dart:math';
+
+import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
+
+import '../vm/dart/snapshot_test_helper.dart';
+
+void forwardStream(Stream<List<int>> input, IOSink output) {
+  // Print the information line-by-line.
+  input
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .listen((String line) {
+    output.writeln(line);
+  });
+}
+
+Future<bool> run(String executable, List<String> args) async {
+  print('Running "$executable ${args.join(' ')}"');
+  final Process process = await Process.start(executable, args);
+  forwardStream(process.stdout, stdout);
+  forwardStream(process.stderr, stderr);
+  final int exitCode = await process.exitCode;
+  if (exitCode != 0) {
+    print('=> Running "$executable ${args.join(' ')}" failed with $exitCode');
+    io.exitCode = 255; // Make this shard fail.
+    return false;
+  }
+  return true;
+}
+
+abstract class TestRunner {
+  Future runTest();
+}
+
+class JitTestRunner extends TestRunner {
+  final String buildDir;
+  final List<String> arguments;
+
+  JitTestRunner(this.buildDir, this.arguments);
+
+  Future runTest() async {
+    await run('$buildDir/dart', arguments);
+  }
+}
+
+class AotTestRunner extends TestRunner {
+  final String buildDir;
+  final List<String> arguments;
+  final List<String> aotArguments;
+
+  AotTestRunner(this.buildDir, this.arguments, this.aotArguments);
+
+  Future runTest() async {
+    await withTempDir((String dir) async {
+      final elfFile = path.join(dir, 'app.elf');
+
+      if (await run('$buildDir/gen_snapshot',
+          ['--snapshot-kind=app-aot-elf', '--elf=$elfFile', ...arguments])) {
+        await run(
+            '$buildDir/dart_precompiled_runtime', [...aotArguments, elfFile]);
+      }
+    });
+  }
+}
+
+final configurations = <TestRunner>[
+  JitTestRunner('out/DebugX64', [
+    '--disable-dart-dev',
+    '--no-sound-null-safety',
+    '--enable-isolate-groups',
+    '--experimental-enable-isolate-groups-jit',
+    'runtime/tests/concurrency/generated_stress_test.dart.jit.dill',
+  ]),
+  JitTestRunner('out/ReleaseX64', [
+    '--disable-dart-dev',
+    '--no-sound-null-safety',
+    '--enable-isolate-groups',
+    '--experimental-enable-isolate-groups-jit',
+    '--no-inline-alloc',
+    '--use-slow-path',
+    '--deoptimize-on-runtime-call-every=3',
+    'runtime/tests/concurrency/generated_stress_test.dart.jit.dill',
+  ]),
+  JitTestRunner('out/ReleaseTSANX64', [
+    '--disable-dart-dev',
+    '--no-sound-null-safety',
+    '--enable-isolate-groups',
+    '--experimental-enable-isolate-groups-jit',
+    'runtime/tests/concurrency/generated_stress_test.dart.jit.dill',
+  ]),
+  AotTestRunner('out/ReleaseX64', [
+    '--no-sound-null-safety',
+    'runtime/tests/concurrency/generated_stress_test.dart.aot.dill',
+  ], [
+    '--no-sound-null-safety',
+    '--enable-isolate-groups',
+  ]),
+  AotTestRunner('out/DebugX64', [
+    '--no-sound-null-safety',
+    'runtime/tests/concurrency/generated_stress_test.dart.aot.dill',
+  ], [
+    '--no-sound-null-safety',
+    '--enable-isolate-groups',
+  ]),
+];
+
+main(List<String> arguments) async {
+  final parser = ArgParser()
+    ..addOption('shards', help: 'number of shards used', defaultsTo: '1')
+    ..addOption('shard', help: 'shard id', defaultsTo: '1');
+
+  final options = parser.parse(arguments);
+  final shards = int.parse(options['shards']);
+  final shard = int.parse(options['shard']) - 1;
+
+  final thisShardsConfigurations = [];
+  for (int i = 0; i < configurations.length; i++) {
+    if ((i % shards) == shard) {
+      thisShardsConfigurations.add(configurations[i]);
+    }
+  }
+  for (final config in thisShardsConfigurations) {
+    await config.runTest();
+  }
+}
