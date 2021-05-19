b8f4b252efa3fe1766922833afa6b956e094f6e2
Devon Carew <devoncarew@google.com>
2021-05-19 16:26:42 +0000

    move pkg/telemetry to using package:lints
    Change-Id: I75130cc8d5964ef0f95a672858da8bbce8ffd78c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/200520
    Commit-Queue: Devon Carew <devoncarew@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>

--- pkg/telemetry/test/utils_test.dart
+++ pkg/telemetry/test/utils_test.dart
@@ -8,12 +8,12 @@ import 'package:test/test.dart';
 void main() {
   group('ThrottlingBucket', () {
     test('can send', () {
-      ThrottlingBucket bucket = new ThrottlingBucket(10, Duration(minutes: 1));
+      ThrottlingBucket bucket = ThrottlingBucket(10, Duration(minutes: 1));
       expect(bucket.removeDrop(), true);
     });
 
     test("doesn't send too many", () {
-      ThrottlingBucket bucket = new ThrottlingBucket(10, Duration(minutes: 1));
+      ThrottlingBucket bucket = ThrottlingBucket(10, Duration(minutes: 1));
       for (int i = 0; i < 10; i++) {
         expect(bucket.removeDrop(), true);
       }
