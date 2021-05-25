14e672a6dd20f4f1bd11468294fd42b92f19a740
Robert Nystrom <rnystrom@google.com>
2021-05-25 11:55:40 +0000

    Rename library6.dart to make it a test.
    Also update the comment since it used to be a negative test but is now
    a positive test.
    
    Change-Id: I8bc6fa9e112d2d84e61cd849f6f7cd37cebe9a47
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/201081
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Lasse R.H. Nielsen <lrn@google.com>
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>

--- tests/language_2/library/library6_test.dart
+++ tests/language_2/library/library6_test.dart
@@ -4,12 +4,9 @@
 
 // @dart = 2.9
 
-// This tests that it is a compile-time error to both import a library
+// This tests that it is not a compile-time error to both import a library
 // that defines a function type alias and to have a local definition for
 // another function type alias with the same name.
-// This name conflict is considered an error even if Fun is never referred to.
-
-library Library6NegativeTest.dart;
 
 import "library5a.dart"; // Defines function type alias Fun
 
