f08edad8ce111330988fa157f1cc6f9fe54a4e89
Riley Porter <rileyporter@google.com>
2021-05-25 19:28:01 +0000

    Optimize js_util setProperty calls for non-function values to _setPropertyUnchecked version that can be inlined.
    No change in the generated JavaScript for dart2js.
    
    Change-Id: Ie4a8e5a34826b6c9083d34656aaa27050635cb21
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/200933
    Reviewed-by: Srujan Gaddam <srujzs@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Commit-Queue: Riley Porter <rileyporter@google.com>

--- pkg/_js_interop_checks/lib/src/transformations/js_util_optimizer.dart
+++ pkg/_js_interop_checks/lib/src/transformations/js_util_optimizer.dart
@@ -11,21 +11,47 @@ import 'package:kernel/kernel.dart';
 class JsUtilOptimizer extends Transformer {
   final Procedure _jsTarget;
   final Procedure _getPropertyTarget;
+  final Procedure _setPropertyTarget;
+  final Procedure _setPropertyUncheckedTarget;
+
+  /// Dynamic members in js_util that interop allowed.
+  static final Iterable<String> _allowedInteropJsUtilMembers = <String>[
+    'callConstructor',
+    'callMethod',
+    'getProperty',
+    'jsify',
+    'newObject',
+    'setProperty'
+  ];
+  final Iterable<Procedure> _allowedInteropJsUtilTargets;
+  final Procedure _allowInteropTarget;
 
   JsUtilOptimizer(CoreTypes coreTypes)
       : _jsTarget =
             coreTypes.index.getTopLevelMember('dart:_foreign_helper', 'JS'),
         _getPropertyTarget =
-            coreTypes.index.getTopLevelMember('dart:js_util', 'getProperty') {}
+            coreTypes.index.getTopLevelMember('dart:js_util', 'getProperty'),
+        _setPropertyTarget =
+            coreTypes.index.getTopLevelMember('dart:js_util', 'setProperty'),
+        _setPropertyUncheckedTarget = coreTypes.index
+            .getTopLevelMember('dart:js_util', '_setPropertyUnchecked'),
+        _allowInteropTarget =
+            coreTypes.index.getTopLevelMember('dart:js', 'allowInterop'),
+        _allowedInteropJsUtilTargets = _allowedInteropJsUtilMembers.map(
+            (member) =>
+                coreTypes.index.getTopLevelMember('dart:js_util', member)) {}
 
-  /// Replaces js_util method calls with lowering straight to JS fragment call.
+  /// Replaces js_util method calls with optimization when possible.
   ///
-  /// Lowers the following types of js_util calls:
-  ///  - `getProperty` for any argument types
+  /// Lowers `getProperty` for any argument type straight to JS fragment call.
+  /// Lowers `setProperty` to `_setPropertyUnchecked` for values that are
+  /// not Function type and guaranteed to be interop allowed.
   @override
   visitStaticInvocation(StaticInvocation node) {
     if (node.target == _getPropertyTarget) {
       node = _lowerGetProperty(node);
+    } else if (node.target == _setPropertyTarget) {
+      node = _lowerSetProperty(node);
     }
     node.transformChildren(this);
     return node;
@@ -49,7 +75,60 @@ class JsUtilOptimizer extends Transformer {
           ],
           // TODO(rileyporter): Copy type from getProperty when it's generic.
           types: [DynamicType()],
-        )..fileOffset = node.arguments.fileOffset)
+        )..fileOffset = arguments.fileOffset)
       ..fileOffset = node.fileOffset;
+  }
+
+  /// Lowers the given js_util `setProperty` call to `_setPropertyUnchecked`
+  /// when the additional validation checks in `setProperty` can be elided.
+  /// Removing the checks allows further inlining by the compilers.
+  StaticInvocation _lowerSetProperty(StaticInvocation node) {
+    Arguments arguments = node.arguments;
+    assert(arguments.types.isEmpty);
+    assert(arguments.positional.length == 3);
+    assert(arguments.named.isEmpty);
+
+    if (!_allowedInterop(arguments.positional.last)) {
+      return node;
+    }
+
+    return StaticInvocation(_setPropertyUncheckedTarget, arguments)
+      ..fileOffset = node.fileOffset;
+  }
+
+  /// Returns whether the given TreeNode is guaranteed to be allowed to interop
+  /// with JS.
+  ///
+  /// Returns true when the node is guaranteed to be not a function:
+  ///    - has a DartType that is NullType or an InterfaceType that is not
+  ///      Function or Object
+  /// Also returns true for allowed method calls within the JavaScript domain:
+  ///        - dart:_foreign_helper JS
+  ///        - dart:js `allowInterop`
+  ///        - dart:js_util and any of the `_allowedInteropJsUtilMembers`
+  bool _allowedInterop(TreeNode node) {
+    // TODO(rileyporter): Detect functions that have been wrapped at some point
+    // with `allowInterop`
+    // TODO(rileyporter): Use staticTypeContext to generalize type checking and
+    // allow more non-function types. Currently, we skip all literal types.
+    var checkType;
+    if (node is VariableGet) {
+      checkType = node.variable.type;
+    }
+
+    if (node is StaticInvocation) {
+      if (node.target == _allowInteropTarget) return true;
+      if (node.target == _jsTarget) return true;
+      if (_allowedInteropJsUtilTargets.contains(node.target)) return true;
+      checkType = node.target.function.returnType;
+    }
+
+    if (checkType is InterfaceType) {
+      return checkType.classNode.name != 'Function' &&
+          checkType.classNode.name != 'Object';
+    } else {
+      // Only other DartType guaranteed to not be a function.
+      return checkType is NullType;
+    }
   }
 }
--- pkg/dev_compiler/lib/src/kernel/target.dart
+++ pkg/dev_compiler/lib/src/kernel/target.dart
@@ -93,6 +93,7 @@ class DevCompilerTarget extends Target {
         'dart:collection',
         'dart:html',
         'dart:indexed_db',
+        'dart:js',
         'dart:js_util',
         'dart:math',
         'dart:svg',
