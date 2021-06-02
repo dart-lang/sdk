7cdb6444f263d3266290b44ee9872c08b2731e98
Mayank Patke <fishythefish@google.com>
2021-06-02 23:21:24 +0000

    Add language tests for some late instance variable corner cases.
    Change-Id: Ic269846684201bebab74ae83d269753fa40626f1
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/200921
    Reviewed-by: Stephen Adams <sra@google.com>

--- tests/language/nnbd/late/chained_assignment_test.dart
+++ tests/language/nnbd/late/chained_assignment_test.dart
@@ -23,82 +23,82 @@ void main() {
   late int d;
 
   b = a = 1;
-  Expect.equals(a, 1);
-  Expect.equals(b, 1);
+  Expect.equals(1, a);
+  Expect.equals(1, b);
 
   a = b = 2;
-  Expect.equals(a, 2);
-  Expect.equals(b, 2);
+  Expect.equals(2, a);
+  Expect.equals(2, b);
 
   b = foo.x = 3;
-  Expect.equals(foo.x, 3);
-  Expect.equals(b, 3);
+  Expect.equals(3, foo.x);
+  Expect.equals(3, b);
 
   foo.x = b = 4;
-  Expect.equals(foo.x, 4);
-  Expect.equals(b, 4);
+  Expect.equals(4, foo.x);
+  Expect.equals(4, b);
 
   b = foo.y = 5;
-  Expect.equals(foo.y, 5);
-  Expect.equals(b, 5);
+  Expect.equals(5, foo.y);
+  Expect.equals(5, b);
 
   foo.y = b = 6;
-  Expect.equals(foo.y, 6);
-  Expect.equals(b, 6);
+  Expect.equals(6, foo.y);
+  Expect.equals(6, b);
 
   b = c = 7;
-  Expect.equals(b, 7);
-  Expect.equals(c, 7);
+  Expect.equals(7, b);
+  Expect.equals(7, c);
 
   d = b = 8;
-  Expect.equals(b, 8);
-  Expect.equals(d, 8);
+  Expect.equals(8, b);
+  Expect.equals(8, d);
 
   Foo.z = a = 9;
-  Expect.equals(a, 9);
-  Expect.equals(Foo.z, 9);
+  Expect.equals(9, a);
+  Expect.equals(9, Foo.z);
 
   a = Foo.z = 10;
-  Expect.equals(a, 10);
-  Expect.equals(Foo.z, 10);
+  Expect.equals(10, a);
+  Expect.equals(10, Foo.z);
 
   Foo.z = foo.x = 11;
-  Expect.equals(foo.x, 11);
-  Expect.equals(Foo.z, 11);
+  Expect.equals(11, foo.x);
+  Expect.equals(11, Foo.z);
 
   foo.x = Foo.z = 12;
-  Expect.equals(foo.x, 12);
-  Expect.equals(Foo.z, 12);
+  Expect.equals(12, foo.x);
+  Expect.equals(12, Foo.z);
 
   Foo.z = foo.y = 13;
-  Expect.equals(foo.y, 13);
-  Expect.equals(Foo.z, 13);
+  Expect.equals(13, foo.y);
+  Expect.equals(13, Foo.z);
 
   foo.y = Foo.z = 14;
-  Expect.equals(foo.y, 14);
-  Expect.equals(Foo.z, 14);
+  Expect.equals(14, foo.y);
+  Expect.equals(14, Foo.z);
 
   foo.w = a = 15;
-  Expect.equals(a, 15);
-  Expect.equals(foo.w, 15);
+  Expect.equals(15, a);
+  Expect.equals(15, foo.w);
 
   a = foo.w = 16;
-  Expect.equals(a, 16);
-  Expect.equals(foo.w, 16);
+  Expect.equals(16, a);
+  Expect.equals(16, foo.w);
 
   foo.w = foo.x = 17;
-  Expect.equals(foo.x, 17);
-  Expect.equals(foo.w, 17);
+  Expect.equals(17, foo.x);
+  Expect.equals(17, foo.w);
 
   foo.x = foo.w = 18;
-  Expect.equals(foo.x, 18);
-  Expect.equals(foo.w, 18);
+  Expect.equals(18, foo.x);
+  Expect.equals(18, foo.w);
 
   foo.w = foo.y = 19;
-  Expect.equals(foo.y, 19);
-  Expect.equals(foo.w, 19);
+  Expect.equals(19, foo.y);
+  Expect.equals(19, foo.w);
 
   foo.y = foo.w = 20;
-  Expect.equals(foo.y, 20);
-  Expect.equals(foo.w, 20);
+  Expect.equals(20, foo.y);
+  Expect.equals(20, foo.w);
 }
