1b70b94df94383c8da40c39ea1b7d55e3ee78a5b
Konstantin Shcheglov <scheglov@google.com>
2021-06-03 21:09:02 +0000

    Issue 46039. Restore FieldFormalParameterElement.hasImplicitType
    This reverts commit 33231b300510448876c3650985a78b7f428ac819.
    
    The necessary fix landed into linter, and pulled into SDK and google3.
    https://github.com/dart-lang/linter/pull/2674
    
    Bug: https://github.com/dart-lang/sdk/issues/46039
    Change-Id: I24a6fea41aa749c5fc7ffffc842d119d2a617658
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/202262
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/summary2/element_builder.dart
+++ pkg/analyzer/lib/src/summary2/element_builder.dart
@@ -345,11 +345,7 @@ class ElementBuilder extends ThrowingAstVisitor<void> {
       _linker.elementNodes[element] = node;
       _enclosingContext.addParameter(null, element);
     }
-
-    // TODO(scheglov) https://github.com/dart-lang/sdk/issues/46039
-    // element.hasImplicitType = node.type == null && node.parameters == null;
-    element.hasImplicitType = false;
-
+    element.hasImplicitType = node.type == null && node.parameters == null;
     element.isExplicitlyCovariant = node.covariantKeyword != null;
     element.isFinal = node.isFinal;
     element.metadata = _buildAnnotations(node.metadata);
