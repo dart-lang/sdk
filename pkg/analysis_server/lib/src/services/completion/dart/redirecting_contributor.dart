75c9f63da03f216503d059a56dfc6c7547b3eb41
Brian Wilkerson <brianwilkerson@google.com>
2021-06-04 20:16:47 +0000

    Fix completion for redirecting constructors
    Change-Id: Ia082b04ab90a9541b1eeec12c6c5f7f8379d3af9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/202541
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/analysis_server/lib/src/services/completion/dart/redirecting_contributor.dart
@@ -0,0 +1,81 @@
+// Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analysis_server/src/provisional/completion/dart/completion_dart.dart';
+import 'package:analysis_server/src/services/completion/dart/suggestion_builder.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+
+/// A contributor that produces suggestions for constructors that are being
+/// redirected to. More concretely, this class produces suggestions for
+/// expressions of the form `this.^` or `super.^` in a constructor's initializer
+/// list or after an `=` in a factory constructor.
+class RedirectingContributor extends DartCompletionContributor {
+  @override
+  Future<void> computeSuggestions(
+      DartCompletionRequest request, SuggestionBuilder builder) async {
+    var entity = request.target.entity;
+    if (entity is SimpleIdentifier) {
+      var parent = entity.parent;
+      if (parent is PropertyAccess &&
+          parent.parent is ConstructorFieldInitializer) {
+        // C() : this.^
+        var containingConstructor =
+            parent.thisOrAncestorOfType<ConstructorDeclaration>();
+        var constructorElement = containingConstructor?.declaredElement;
+        var containingClass = containingConstructor
+            ?.thisOrAncestorOfType<ClassOrMixinDeclaration>();
+        var classElement = containingClass?.declaredElement;
+        if (classElement != null) {
+          for (var constructor in classElement.constructors) {
+            if (constructor != constructorElement) {
+              builder.suggestConstructor(constructor, hasClassName: true);
+            }
+          }
+        }
+      } else if (parent is SuperConstructorInvocation) {
+        // C() : super.^
+        var containingClass =
+            parent.thisOrAncestorOfType<ClassOrMixinDeclaration>();
+        var superclassElement =
+            containingClass?.declaredElement?.supertype?.element;
+        if (superclassElement != null) {
+          for (var constructor in superclassElement.constructors) {
+            if (constructor.isAccessibleIn(request.libraryElement)) {
+              builder.suggestConstructor(constructor, hasClassName: true);
+            }
+          }
+        }
+      }
+    } else if (entity is ConstructorName) {
+      var parent = entity.parent;
+      if (parent is ConstructorDeclaration &&
+          parent.redirectedConstructor == entity) {
+        // factory C() = ^
+        var containingConstructor =
+            parent.thisOrAncestorOfType<ConstructorDeclaration>();
+        var constructorElement = containingConstructor?.declaredElement;
+        var containingClass =
+            parent.thisOrAncestorOfType<ClassOrMixinDeclaration>();
+        var classElement = containingClass?.declaredElement;
+        var libraryElement = request.libraryElement;
+        if (classElement == null || libraryElement == null) {
+          return;
+        }
+        var typeSystem = libraryElement.typeSystem;
+        for (var unit in libraryElement.units) {
+          for (var type in unit.types) {
+            if (typeSystem.isSubtypeOf(type.thisType, classElement.thisType)) {
+              for (var constructor in type.constructors) {
+                if (constructor != constructorElement &&
+                    constructor.isAccessibleIn(request.libraryElement)) {
+                  builder.suggestConstructor(constructor);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
