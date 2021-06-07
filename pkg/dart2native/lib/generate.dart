6862fa14081b176599f204baa15e5a3c94126673
Devon Carew <devoncarew@google.com>
2021-06-07 17:02:24 +0000

    Revert "[pkg/dart2native] upgrade dart2native to null safe"
    This reverts commit b6fad9c4f3599bc5cd5c52e22fbbe95665a87abb.
    
    Reason for revert: Seeing failures in google3.
    
    Original change's description:
    > [pkg/dart2native] upgrade dart2native to null safe
    >
    > Change-Id: Ieef99b982bab14425a4df9f84a40d8ae1ba2bd79
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/202402
    > Reviewed-by: Ben Konyi <bkonyi@google.com>
    > Commit-Queue: Devon Carew <devoncarew@google.com>
    
    TBR=devoncarew@google.com,bkonyi@google.com
    
    Change-Id: Idff233978847be421f7b59ab93d04da99c02cbf5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/202722
    Reviewed-by: Devon Carew <devoncarew@google.com>
    Commit-Queue: Devon Carew <devoncarew@google.com>

--- pkg/dart2native/lib/generate.dart
+++ pkg/dart2native/lib/generate.dart
@@ -3,32 +3,30 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:io';
-
 import 'package:path/path.dart' as path;
-
 import 'dart2native.dart';
 
 final Directory binDir = File(Platform.resolvedExecutable).parent;
 final String executableSuffix = Platform.isWindows ? '.exe' : '';
 final String dartaotruntime =
-    path.join(binDir.path, 'dartaotruntime$executableSuffix');
+    path.join(binDir.path, 'dartaotruntime${executableSuffix}');
 final String genKernel =
     path.join(binDir.path, 'snapshots', 'gen_kernel.dart.snapshot');
 final String genSnapshot =
-    path.join(binDir.path, 'utils', 'gen_snapshot$executableSuffix');
+    path.join(binDir.path, 'utils', 'gen_snapshot${executableSuffix}');
 final String productPlatformDill = path.join(
     binDir.parent.path, 'lib', '_internal', 'vm_platform_strong_product.dill');
 
 Future<void> generateNative({
   String kind = 'exe',
-  required String sourceFile,
-  String? outputFile,
-  String? debugFile,
-  String? packages,
-  required List<String> defines,
+  String sourceFile,
+  String outputFile,
+  String debugFile,
+  String packages,
+  List<String> defines,
   String enableExperiment = '',
   bool enableAsserts = false,
-  bool? soundNullSafety,
+  bool soundNullSafety,
   bool verbose = false,
   String verbosity = 'all',
   List<String> extraOptions = const [],
@@ -39,13 +37,17 @@ Future<void> generateNative({
     if (packages != null) {
       packages = path.canonicalize(path.normalize(packages));
     }
-    final Kind outputKind = {'aot': Kind.aot, 'exe': Kind.exe}[kind]!;
+    final Kind outputKind = {
+      'aot': Kind.aot,
+      'exe': Kind.exe,
+    }[kind];
     final sourceWithoutDart = sourcePath.replaceFirst(RegExp(r'\.dart$'), '');
-    final outputPath = path.canonicalize(path.normalize(outputFile ??
-        {
-          Kind.aot: '$sourceWithoutDart.aot',
-          Kind.exe: '$sourceWithoutDart.exe',
-        }[outputKind]!));
+    final outputPath = path.canonicalize(path.normalize(outputFile != null
+        ? outputFile
+        : {
+            Kind.aot: '${sourceWithoutDart}.aot',
+            Kind.exe: '${sourceWithoutDart}.exe',
+          }[outputKind]));
     final debugPath =
         debugFile != null ? path.canonicalize(path.normalize(debugFile)) : null;
 
@@ -88,14 +90,8 @@ Future<void> generateNative({
     final String snapshotFile = (outputKind == Kind.aot
         ? outputPath
         : path.join(tempDir.path, 'snapshot.aot'));
-    final snapshotResult = await generateAotSnapshot(
-      genSnapshot,
-      kernelFile,
-      snapshotFile,
-      enableAsserts,
-      extraOptions,
-      debugFile: debugPath,
-    );
+    final snapshotResult = await generateAotSnapshot(genSnapshot, kernelFile,
+        snapshotFile, debugPath, enableAsserts, extraOptions);
     if (snapshotResult.exitCode != 0) {
       stderr.writeln(snapshotResult.stdout);
       stderr.writeln(snapshotResult.stderr);
@@ -117,7 +113,7 @@ Future<void> generateNative({
       }
     }
 
-    print('Generated: $outputPath');
+    print('Generated: ${outputPath}');
   } finally {
     tempDir.deleteSync(recursive: true);
   }
