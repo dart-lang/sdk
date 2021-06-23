537703439053b64be0a04b4e6f46d4c7ec1a26de
Joshua Litt <joshualitt@google.com>
2021-06-23 22:44:27 +0000

    Revert "[dart2js] Use library as key for some holder properties."
    This reverts commit 889659e876aa93dfc5394d82401fe4fa6d83889a.
    
    Reason for revert: This CL is too limiting.
    
    Original change's description:
    > [dart2js] Use library as key for some holder properties.
    >
    > Change-Id: I24d21dd6239191ab0242387d97d6b47cdf9582b5
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/203826
    > Reviewed-by: Stephen Adams <sra@google.com>
    > Commit-Queue: Joshua Litt <joshualitt@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Id4d3d401a5b96a1c8e42afd87513520e52b725d9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/204746
    Reviewed-by: Joshua Litt <joshualitt@google.com>
    Commit-Queue: Joshua Litt <joshualitt@google.com>

--- pkg/compiler/lib/src/js_emitter/startup_emitter/fragment_emitter.dart
+++ pkg/compiler/lib/src/js_emitter/startup_emitter/fragment_emitter.dart
@@ -827,7 +827,7 @@ class FragmentEmitter {
 
   /// Adds code to a finalizer.
   void addCodeToFinalizer(void Function(js.Node) addCode, js.Node fragmentCode,
-      Map<Library, List<js.Property>> holderCode) {
+      Map<Entity, List<js.Property>> holderCode) {
     addCode(fragmentCode);
     for (var properties in holderCode.values) {
       for (var property in properties) {
@@ -839,7 +839,7 @@ class FragmentEmitter {
   /// Finalizes the code for a fragment, and optionally finalizes holders.
   /// Finalizing holders must be the last step of the emitter.
   void finalizeCode(String resourceName, js.Node code,
-      Map<Library, List<js.Property>> holderCode,
+      Map<Entity, List<js.Property>> holderCode,
       {bool finalizeHolders: false}) {
     StringReferenceFinalizer stringFinalizer =
         StringReferenceFinalizerImpl(_options.enableMinification);
@@ -871,21 +871,23 @@ class FragmentEmitter {
   /// into a map keyed by [Entity] because we don't yet know anything about the
   /// structure of the underlying holders and thus we cannot emit this code
   /// directly into the ast.
-  Map<Library, List<js.Property>> emitHolderCode(List<Library> libraries) {
-    Map<Library, List<js.Property>> holderCode = {};
+  Map<Entity, List<js.Property>> emitHolderCode(List<Library> libraries) {
+    Map<Entity, List<js.Property>> holderCode = {};
     for (Library library in libraries) {
       for (StaticMethod method in library.statics) {
         Map<js.Name, js.Expression> propertyMap = emitStaticMethod(method);
         propertyMap.forEach((js.Name key, js.Expression value) {
           var property = new js.Property(js.quoteName(key), value);
-          (holderCode[library] ??= []).add(property);
+          Entity holderKey =
+              method is StaticStubMethod ? method.library : method.element;
+          (holderCode[holderKey] ??= []).add(property);
           registerEntityAst(method.element, property, library: library.element);
         });
       }
       for (Class cls in library.classes) {
         js.Expression constructor = emitConstructor(cls);
         var property = new js.Property(js.quoteName(cls.name), constructor);
-        (holderCode[library] ??= []).add(property);
+        (holderCode[cls.element] ??= []).add(property);
         registerEntityAst(cls.element, property, library: library.element);
       }
     }
