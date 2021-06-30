63ac437e9934c6422c96e5195201e8968bc8dfc6
Riley Porter <rileyporter@google.com>
2021-06-30 20:11:44 +0000

    Optimize js_util callMethod calls for 0-4 arguments.
    Change completes initial set of js_util optimizations. Some usages
    of `getProperty`, `setProperty`, and `callMethod` with <= 4
    arguments will have unnecessary checks removed by the compilers.
    
    Change-Id: I94d5402ca9a64ad2818fd6d6a5f7f114f87348c5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/204562
    Commit-Queue: Riley Porter <rileyporter@google.com>
    Reviewed-by: Srujan Gaddam <srujzs@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

--- pkg/_js_interop_checks/lib/src/transformations/js_util_optimizer.dart
+++ pkg/_js_interop_checks/lib/src/transformations/js_util_optimizer.dart
@@ -12,6 +12,8 @@ import 'package:kernel/kernel.dart';
 /// emits the code as a JavaScript code fragment.
 class JsUtilOptimizer extends Transformer {
   final Procedure _jsTarget;
+  final Procedure _callMethodTarget;
+  final List<Procedure> _callMethodUncheckedTargets;
   final Procedure _getPropertyTarget;
   final Procedure _setPropertyTarget;
   final Procedure _setPropertyUncheckedTarget;
@@ -27,6 +29,7 @@ class JsUtilOptimizer extends Transformer {
   ];
   final Iterable<Procedure> _allowedInteropJsUtilTargets;
   final Procedure _allowInteropTarget;
+  final Procedure _listEmptyFactory;
 
   final CoreTypes _coreTypes;
   final StatefulStaticTypeContext _staticTypeContext;
@@ -34,6 +37,12 @@ class JsUtilOptimizer extends Transformer {
   JsUtilOptimizer(this._coreTypes, ClassHierarchy hierarchy)
       : _jsTarget =
             _coreTypes.index.getTopLevelMember('dart:_foreign_helper', 'JS'),
+        _callMethodTarget =
+            _coreTypes.index.getTopLevelMember('dart:js_util', 'callMethod'),
+        _callMethodUncheckedTargets = List<Procedure>.generate(
+            5,
+            (i) => _coreTypes.index
+                .getTopLevelMember('dart:js_util', '_callMethodUnchecked$i')),
         _getPropertyTarget =
             _coreTypes.index.getTopLevelMember('dart:js_util', 'getProperty'),
         _setPropertyTarget =
@@ -45,6 +54,8 @@ class JsUtilOptimizer extends Transformer {
         _allowedInteropJsUtilTargets = _allowedInteropJsUtilMembers.map(
             (member) =>
                 _coreTypes.index.getTopLevelMember('dart:js_util', member)),
+        _listEmptyFactory =
+            _coreTypes.index.getMember('dart:core', 'List', 'empty'),
         _staticTypeContext = StatefulStaticTypeContext.stacked(
             TypeEnvironment(_coreTypes, hierarchy)) {}
 
@@ -69,12 +80,16 @@ class JsUtilOptimizer extends Transformer {
   /// Lowers `getProperty` for any argument type straight to JS fragment call.
   /// Lowers `setProperty` to `_setPropertyUnchecked` for values that are
   /// not Function type and guaranteed to be interop allowed.
+  /// Lowers `callMethod` to `_callMethodUncheckedN` when the number of given
+  /// arguments is 0-4 and all arguments are guaranteed to be interop allowed.
   @override
   visitStaticInvocation(StaticInvocation node) {
     if (node.target == _getPropertyTarget) {
       node = _lowerGetProperty(node);
     } else if (node.target == _setPropertyTarget) {
       node = _lowerSetProperty(node);
+    } else if (node.target == _callMethodTarget) {
+      node = _lowerCallMethod(node);
     }
     node.transformChildren(this);
     return node;
@@ -104,6 +119,7 @@ class JsUtilOptimizer extends Transformer {
 
   /// Lowers the given js_util `setProperty` call to `_setPropertyUnchecked`
   /// when the additional validation checks in `setProperty` can be elided.
+  ///
   /// Removing the checks allows further inlining by the compilers.
   StaticInvocation _lowerSetProperty(StaticInvocation node) {
     Arguments arguments = node.arguments;
@@ -117,6 +133,85 @@ class JsUtilOptimizer extends Transformer {
 
     return StaticInvocation(_setPropertyUncheckedTarget, arguments)
       ..fileOffset = node.fileOffset;
+  }
+
+  /// Lowers the given js_util `callMethod` call to `_callMethodUncheckedN`
+  /// when the additional validation checks on the arguments can be elided.
+  ///
+  /// Calls will be lowered when using a List literal or constant list with 0-4
+  /// elements for the `callMethod` arguments, or the `List.empty()` factory.
+  /// Removing the checks allows further inlining by the compilers.
+  StaticInvocation _lowerCallMethod(StaticInvocation node) {
+    Arguments arguments = node.arguments;
+    assert(arguments.types.isEmpty);
+    assert(arguments.positional.length == 3);
+    assert(arguments.named.isEmpty);
+
+    // Lower List.empty factory call.
+    var argumentsList = arguments.positional.last;
+    if (argumentsList is StaticInvocation &&
+        argumentsList.target == _listEmptyFactory) {
+      return _createNewCallMethodNode([], arguments, node.fileOffset);
+    }
+
+    // Lower other kinds of Lists.
+    var callMethodArguments;
+    var entryType;
+    if (argumentsList is ListLiteral) {
+      if (argumentsList.expressions.length >=
+          _callMethodUncheckedTargets.length) {
+        return node;
+      }
+      callMethodArguments = argumentsList.expressions;
+      entryType = argumentsList.typeArgument;
+    } else if (argumentsList is ConstantExpression &&
+        argumentsList.constant is ListConstant) {
+      var argumentsListConstant = argumentsList.constant as ListConstant;
+      if (argumentsListConstant.entries.length >=
+          _callMethodUncheckedTargets.length) {
+        return node;
+      }
+      callMethodArguments = argumentsListConstant.entries
+          .map((constant) => ConstantExpression(
+              constant, constant.getType(_staticTypeContext)))
+          .toList();
+      entryType = argumentsListConstant.typeArgument;
+    } else {
+      // Skip lowering any other type of List.
+      return node;
+    }
+
+    // Check the overall List entry type, then verify each argument if needed.
+    if (!_allowedInteropType(entryType)) {
+      for (var argument in callMethodArguments) {
+        if (!_allowedInterop(argument)) {
+          return node;
+        }
+      }
+    }
+
+    return _createNewCallMethodNode(
+        callMethodArguments, arguments, node.fileOffset);
+  }
+
+  /// Creates a new StaticInvocation node for `_callMethodUncheckedN` with the
+  /// given 0-4 arguments.
+  StaticInvocation _createNewCallMethodNode(
+      List<Expression> callMethodArguments,
+      Arguments arguments,
+      int nodeFileOffset) {
+    assert(callMethodArguments.length <= 4);
+    return StaticInvocation(
+        _callMethodUncheckedTargets[callMethodArguments.length],
+        Arguments(
+          [
+            arguments.positional[0],
+            arguments.positional[1],
+            ...callMethodArguments
+          ],
+          types: [],
+        )..fileOffset = arguments.fileOffset)
+      ..fileOffset = nodeFileOffset;
   }
 
   /// Returns whether the given Expression is guaranteed to be allowed to
@@ -138,7 +233,12 @@ class JsUtilOptimizer extends Transformer {
       if (_allowedInteropJsUtilTargets.contains(node.target)) return true;
     }
 
-    var type = node.getStaticType(_staticTypeContext);
+    return _allowedInteropType(node.getStaticType(_staticTypeContext));
+  }
+
+  /// Returns whether the given DartType is guaranteed to be not a function
+  /// and therefore allowed to interop with JS.
+  bool _allowedInteropType(DartType type) {
     if (type is InterfaceType) {
       return type.classNode != _coreTypes.functionClass &&
           type.classNode != _coreTypes.objectClass;
