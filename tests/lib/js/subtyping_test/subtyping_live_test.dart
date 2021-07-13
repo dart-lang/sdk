0fc2d929e3893632d8133207801d231fbc6b5a4c
Srujan Gaddam <srujzs@google.com>
2021-07-13 18:51:34 +0000

    [pkg:js] Add subtyping tests with liveness
    Adds tests for subtyping between pkg:js types. Also runs tests
    for both when types are made live and for when they are not.
    
    Also adds these tests to lib_2.
    
    Change-Id: I25fdc293e422eb21fa8a80dea66ae5d492c5aafd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/205601
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Reviewed-by: Riley Porter <rileyporter@google.com>
    Commit-Queue: Srujan Gaddam <srujzs@google.com>

---
+++ tests/lib/js/subtyping_test/subtyping_live_test.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Tests subtyping relationships after making package:js types live.
+
+@JS()
+library subtyping_live_test;
+
+import 'package:js/js.dart';
+import 'subtyping_test_util.dart';
+
+@JS()
+external dynamic get externalGetter;
+
+void main() {
+  // Call to foreign function should trigger dart2js to assume types are live.
+  externalGetter;
+  testSubtyping();
+}
