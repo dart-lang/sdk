e681abace97eba2632474474cb9f0deadcdacd22
Sam Rawlins <srawlins@google.com>
2021-07-15 16:57:21 +0000

    Remove unnecessary imports
    TEST=N/A
    
    Change-Id: I7440c1165a7403aad023f002a97c90572cb66c59
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/205820
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Ben Konyi <bkonyi@google.com>

--- pkg/vm_service/test/library_dependency_test.dart
+++ pkg/vm_service/test/library_dependency_test.dart
@@ -8,7 +8,6 @@ import 'dart:isolate' show Isolate, SendPort hide Capability;
 import 'package:test/test.dart';
 import 'package:vm_service/vm_service.dart';
 
-import 'common/service_test_common.dart';
 import 'common/test_helper.dart';
 
 export 'dart:io' show Socket hide SecureSocket;
