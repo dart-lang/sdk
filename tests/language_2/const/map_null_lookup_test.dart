a5d4e010afb5a846b48cbf927371065a241634dd
Daco Harkes <dacoharkes@google.com>
2021-07-26 12:42:20 +0000

    [test] const types and null in const maps and sets
    We were lacking tests for doing a `null` lookup in const maps and sets.
    
    Also, we were lacking tests for having type literals with user-defined
    classes in const maps and sets.
    
    Bug: https://github.com/dart-lang/sdk/issues/45908
    
    Change-Id: I1665420ddcc71df7b47a87ee9e16d7ebbe9e13f5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/208080
    Commit-Queue: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Erik Ernst <eernst@google.com>
    Reviewed-by: Lasse R.H. Nielsen <lrn@google.com>

--- tests/language_2/const/map_null_lookup_test.dart
+++ tests/language_2/const/map_null_lookup_test.dart
@@ -2,9 +2,20 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Tests lookup of null in const maps.
+
 // @dart = 2.9
 
 import 'package:expect/expect.dart';
+
+main() {
+  final map1 = const {1: 42, null: 2, 'asdf': 'fdsa'};
+  Expect.equals(2, map1[getValueNonOptimized(null)]);
+  Expect.isTrue(map1.containsKey(null));
+  final map2 = const {1: 42, 2: 2, 'asdf': 'fdsa'};
+  Expect.equals(null, map2[getValueNonOptimized(null)]);
+  Expect.isFalse(map2.containsKey(null));
+}
 
 /// Returns its argument.
 ///
@@ -17,24 +28,4 @@ dynamic getValueNonOptimized(dynamic x) {
     return getValueNonOptimized(2);
   }
   return x;
-}
-
-class Nasty {
-  final int n;
-
-  const Nasty(this.n);
-
-  int get hashCode {
-    while (true) {}
-  }
-}
-
-main() {
-  final map = const {1: 42, 'foo': 499, 2: 'bar', Nasty(1): 'baz'};
-  Expect.equals(42, map[getValueNonOptimized(1.0)]);
-  Expect.equals(
-      499, map[getValueNonOptimized(new String.fromCharCodes('foo'.runes))]);
-  Expect.equals('bar', map[getValueNonOptimized(2)]);
-  Expect.isNull(map[getValueNonOptimized(Nasty(1))]);
-  Expect.equals('baz', map[getValueNonOptimized(const Nasty(1))]);
 }
