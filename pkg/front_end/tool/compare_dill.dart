e6e25ac7b08d6d71abb38a17b3889ea16a2238c3
Johnni Winther <johnniwinther@google.com>
2021-08-04 08:49:51 +0000

    [cfe] Migrate remaining front_end tools
    Change-Id: I75f936734a89ec5858390d96be7a5cdc4a300891
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/208402
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>

--- pkg/front_end/tool/compare_dill.dart
+++ pkg/front_end/tool/compare_dill.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.9
-
 import 'dart:io' show File;
 
 import '../test/binary_md_dill_reader.dart' show DillComparer;
