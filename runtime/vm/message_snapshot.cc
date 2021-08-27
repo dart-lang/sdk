bc387837c4f1162d23a1434a8715ec4175fed9be
asiva <asiva@google.com>
2021-08-27 17:45:17 +0000

    [VM/Runtime] Reland : Fix 'File' object leak in async file open operation
    The 'File' object created in the async file open method is leaked
    if the operation is not completed when the isolate shuts down
    with an unhandled exception.
    This change adds a finalizable state for the 'File' object so that
    the message deletion that happens when ports are closed could run
    the callback to clean up the 'File' object.
    
    
    TEST=new tests added
    
    Change-Id: I64c18a7905261b0fc7bf9f220086791478232d0d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/211340
    Reviewed-by: Alexander Aprelev <aam@google.com>
    Commit-Queue: Siva Annamalai <asiva@google.com>

--- runtime/vm/message_snapshot.cc
+++ runtime/vm/message_snapshot.cc
@@ -1818,6 +1818,65 @@ class ExternalTypedDataMessageDeserializationCluster
   const intptr_t cid_;
 };
 
+class NativePointerMessageSerializationCluster
+    : public MessageSerializationCluster {
+ public:
+  explicit NativePointerMessageSerializationCluster(Zone* zone)
+      : MessageSerializationCluster("NativePointer",
+                                    MessagePhase::kNonCanonicalInstances,
+                                    kNativePointer),
+        objects_(zone, 0) {}
+  ~NativePointerMessageSerializationCluster() {}
+
+  void Trace(MessageSerializer* s, Object* object) { UNREACHABLE(); }
+
+  void WriteNodes(MessageSerializer* s) { UNREACHABLE(); }
+
+  void TraceApi(ApiMessageSerializer* s, Dart_CObject* object) {
+    objects_.Add(object);
+  }
+
+  void WriteNodesApi(ApiMessageSerializer* s) {
+    intptr_t count = objects_.length();
+    s->WriteUnsigned(count);
+    for (intptr_t i = 0; i < count; i++) {
+      Dart_CObject* data = objects_[i];
+      s->AssignRef(data);
+
+      s->finalizable_data()->Put(
+          data->value.as_native_pointer.size,
+          reinterpret_cast<void*>(data->value.as_native_pointer.ptr),
+          reinterpret_cast<void*>(data->value.as_native_pointer.ptr),
+          data->value.as_native_pointer.callback);
+    }
+  }
+
+ private:
+  GrowableArray<Dart_CObject*> objects_;
+};
+
+class NativePointerMessageDeserializationCluster
+    : public MessageDeserializationCluster {
+ public:
+  NativePointerMessageDeserializationCluster()
+      : MessageDeserializationCluster("NativePointer"), cid_(kNativePointer) {}
+  ~NativePointerMessageDeserializationCluster() {}
+
+  void ReadNodes(MessageDeserializer* d) {
+    intptr_t count = d->ReadUnsigned();
+    for (intptr_t i = 0; i < count; i++) {
+      FinalizableData finalizable_data = d->finalizable_data()->Take();
+      intptr_t ptr = reinterpret_cast<intptr_t>(finalizable_data.data);
+      d->AssignRef(Integer::New(ptr));
+    }
+  }
+
+  void ReadNodesApi(ApiMessageDeserializer* d) { UNREACHABLE(); }
+
+ private:
+  const intptr_t cid_;
+};
+
 class TypedDataViewMessageSerializationCluster
     : public MessageSerializationCluster {
  public:
@@ -3189,6 +3248,9 @@ bool ApiMessageSerializer::Trace(Dart_CObject* object) {
     case Dart_CObject_kCapability:
       cid = kCapabilityCid;
       break;
+    case Dart_CObject_kNativePointer:
+      cid = kNativePointer;
+      break;
     default:
       return Fail("invalid Dart_CObject type");
   }
@@ -3242,6 +3304,8 @@ MessageSerializationCluster* BaseSerializer::NewClusterForClass(
   }
 
   switch (cid) {
+    case kNativePointer:
+      return new (Z) NativePointerMessageSerializationCluster(Z);
     case kClassCid:
       return new (Z) ClassMessageSerializationCluster();
     case kTypeArgumentsCid:
@@ -3326,6 +3390,9 @@ MessageDeserializationCluster* BaseDeserializer::ReadCluster() {
   }
 
   switch (cid) {
+    case kNativePointer:
+      ASSERT(!is_canonical);
+      return new (Z) NativePointerMessageDeserializationCluster();
     case kClassCid:
       ASSERT(!is_canonical);
       return new (Z) ClassMessageDeserializationCluster();
