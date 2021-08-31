dart-lang/sdk

commit 1e24fe7d699a1c36be142afa21859c6a9c82d035

[vm, compiler] Specialize unoptimized monomorphic and megamorphic calls.
dart-bytecode, arm64:            +4.742% geomean
dart-bytecode-jit-unopt, arm64: +12.73% geomean
dart2js-compile, x64:            +3.635% geomean

In the polymorphic and unlinked cases, call to a stub the does a linear scan against an ICData.

In the monomorphic case, call to a prologue of the expected target function that checks the expected receiver class. There is additional indirection in the JIT version compared to the AOT version to also tick a usage counter so the inliner can make good decisions.

In the megamorphic case, call to a stub that does a hash table lookup against a MegamorphicCache.

Megamorphic call sites face a loss of precision in usage counts. The call site count is not recorded and the usage counter of the target function is used as an approximation.

Monomorphic and megamorphic calls sites are reset to the polymorphic/unlinked state on hot reload.

Monomorphic and megamorphic calls sites do not check the stepping state, so they are reset to the polymorphic/unlinked state when stepping begins and disabled.

Back-edges now increment the usage counter in addition to checking it. This ensures function with loops containing monomorphic calls will eventually cross the optimization threshold.

Fixed backwards use of kMonomorphicEntryOffset and kPolymorphicEntryOffset.

Fixed C stack overflow when bouncing between the KBC interpreter and a simulator.

Bug: https://github.com/dart-lang/sdk/issues/26780
Bug: https://github.com/dart-lang/sdk/issues/36409
Bug: https://github.com/dart-lang/sdk/issues/36731
Change-Id: I78a49cccd962703a459288e71ce246ed845df474
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/102820
Commit-Queue: Ryan Macnak <rmacnak@google.com>
Reviewed-by: Alexander Markov <alexmarkov@google.com>

runtime/vm/compiler/stub_code_compiler_x64.cc
@@ -571,6 +571,10 @@ void StubCodeCompiler::GenerateCallStaticFunctionStub(Assembler* assembler) {
 // (invalid because its function was optimized or deoptimized).
 // R10: arguments descriptor array.
 void StubCodeCompiler::GenerateFixCallersTargetStub(Assembler* assembler) {
+  Label monomorphic;
+  __ BranchOnMonomorphicCheckedEntryJIT(&monomorphic);
+
+  // This was a static call.
   // Load code pointer to this stub from the thread:
   // The one that is passed in, is not correct - it points to the code object
   // that needs to be replaced.
@@ -584,6 +588,27 @@ void StubCodeCompiler::GenerateFixCallersTargetStub(Assembler* assembler) {
   __ popq(CODE_REG);  // Get Code object.
   __ popq(R10);       // Restore arguments descriptor array.
   __ movq(RAX, FieldAddress(CODE_REG, target::Code::entry_point_offset()));
+  __ LeaveStubFrame();
+  __ jmp(RAX);
+  __ int3();
+
+  __ Bind(&monomorphic);
+  // This was a switchable call.
+  // Load code pointer to this stub from the thread:
+  // The one that is passed in, is not correct - it points to the code object
+  // that needs to be replaced.
+  __ movq(CODE_REG,
+          Address(THR, target::Thread::fix_callers_target_code_offset()));
+  __ EnterStubFrame();
+  __ pushq(RBX);           // Preserve cache (guarded CID as Smi).
+  __ pushq(RDX);           // Preserve receiver.
+  __ pushq(Immediate(0));  // Result slot.
+  __ CallRuntime(kFixCallersTargetMonomorphicRuntimeEntry, 0);
+  __ popq(CODE_REG);  // Get Code object.
+  __ popq(RDX);       // Restore receiver.
+  __ popq(RBX);       // Restore cache (guarded CID as Smi).
+  __ movq(RAX, FieldAddress(CODE_REG, target::Code::entry_point_offset(
+                                          CodeEntryKind::kMonomorphic)));
   __ LeaveStubFrame();
   __ jmp(RAX);
   __ int3();
@@ -2500,6 +2525,26 @@ void StubCodeCompiler::GenerateICCallBreakpointStub(Assembler* assembler) {
 #endif  // defined(PRODUCT)
 }
 
+void StubCodeCompiler::GenerateUnoptStaticCallBreakpointStub(
+    Assembler* assembler) {
+#if defined(PRODUCT)
+  __ Stop("No debugging in PRODUCT mode");
+#else
+  __ EnterStubFrame();
+  __ pushq(RDX);           // Preserve receiver.
+  __ pushq(RBX);           // Preserve IC data.
+  __ pushq(Immediate(0));  // Result slot.
+  __ CallRuntime(kBreakpointRuntimeHandlerRuntimeEntry, 0);
+  __ popq(CODE_REG);  // Original stub.
+  __ popq(RBX);       // Restore IC data.
+  __ popq(RDX);       // Restore receiver.
+  __ LeaveStubFrame();
+
+  __ movq(RAX, FieldAddress(CODE_REG, target::Code::entry_point_offset()));
+  __ jmp(RAX);  // Jump to original stub.
+#endif  // defined(PRODUCT)
+}
+
 //  TOS(0): return address (Dart code).
 void StubCodeCompiler::GenerateRuntimeCallBreakpointStub(Assembler* assembler) {
 #if defined(PRODUCT)
@@ -3284,18 +3329,18 @@ void StubCodeCompiler::GenerateUnlinkedCallStub(Assembler* assembler) {
   __ pushq(RDX);  // Preserve receiver.
 
   __ pushq(Immediate(0));  // Result slot.
-  __ pushq(RDX);           // Arg0: Receiver
-  __ pushq(RBX);           // Arg1: UnlinkedCall
-  __ CallRuntime(kUnlinkedCallRuntimeEntry, 2);
+  __ pushq(Immediate(0));  // Arg0: stub out.
+  __ pushq(RDX);           // Arg1: Receiver
+  __ pushq(RBX);           // Arg2: UnlinkedCall
+  __ CallRuntime(kUnlinkedCallRuntimeEntry, 3);
   __ popq(RBX);
   __ popq(RBX);
+  __ popq(CODE_REG);  // result = stub
   __ popq(RBX);  // result = IC
 
   __ popq(RDX);  // Restore receiver.
   __ LeaveStubFrame();
 
-  __ movq(CODE_REG,
-          Address(THR, target::Thread::ic_lookup_through_code_stub_offset()));
   __ movq(RCX, FieldAddress(CODE_REG, target::Code::entry_point_offset(
                                           CodeEntryKind::kMonomorphic)));
   __ jmp(RCX);
@@ -3328,16 +3373,16 @@ void StubCodeCompiler::GenerateSingleTargetCallStub(Assembler* assembler) {
   __ pushq(RDX);  // Preserve receiver.
 
   __ pushq(Immediate(0));  // Result slot.
-  __ pushq(RDX);           // Arg0: Receiver
-  __ CallRuntime(kSingleTargetMissRuntimeEntry, 1);
+  __ pushq(Immediate(0));  // Arg0: stub out
+  __ pushq(RDX);           // Arg1: Receiver
+  __ CallRuntime(kSingleTargetMissRuntimeEntry, 2);
   __ popq(RBX);
+  __ popq(CODE_REG);  // result = stub
   __ popq(RBX);  // result = IC
 
   __ popq(RDX);  // Restore receiver.
   __ LeaveStubFrame();
 
-  __ movq(CODE_REG,
-          Address(THR, target::Thread::ic_lookup_through_code_stub_offset()));
   __ movq(RCX, FieldAddress(CODE_REG, target::Code::entry_point_offset(
                                           CodeEntryKind::kMonomorphic)));
   __ jmp(RCX);
@@ -3352,16 +3397,16 @@ void StubCodeCompiler::GenerateMonomorphicMissStub(Assembler* assembler) {
   __ pushq(RDX);  // Preserve receiver.
 
   __ pushq(Immediate(0));  // Result slot.
-  __ pushq(RDX);           // Arg0: Receiver
-  __ CallRuntime(kMonomorphicMissRuntimeEntry, 1);
+  __ pushq(Immediate(0));  // Arg0: stub out.
+  __ pushq(RDX);           // Arg1: Receiver
+  __ CallRuntime(kMonomorphicMissRuntimeEntry, 2);
   __ popq(RBX);
+  __ popq(CODE_REG);  // result = stub
   __ popq(RBX);  // result = IC
 
   __ popq(RDX);  // Restore receiver.
   __ LeaveStubFrame();
 
-  __ movq(CODE_REG,
-          Address(THR, target::Thread::ic_lookup_through_code_stub_offset()));
   __ movq(RCX, FieldAddress(CODE_REG, target::Code::entry_point_offset(
                                           CodeEntryKind::kMonomorphic)));
   __ jmp(RCX);

commit baa1ebb377e1bdd2a9f09b1cddfd6c45e8416cc9

[vm, compiler] Fix incorrect register save in breakpoint stub.
Bug introduced in 1e24fe7d699a1c36be142afa21859c6a9c82d035.

Symbolize stub frames.

TEST=ci
Bug: https://github.com/dart-lang/sdk/issues/47039
Change-Id: Ida3535a01923085cd48a2801ed9b868bfef7418b
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/212026
Commit-Queue: Ryan Macnak <rmacnak@google.com>
Reviewed-by: Siva Annamalai <asiva@google.com>

runtime/vm/compiler/stub_code_compiler_x64.cc
@@ -2726,13 +2726,11 @@ void StubCodeCompiler::GenerateUnoptStaticCallBreakpointStub(
   __ Stop("No debugging in PRODUCT mode");
 #else
   __ EnterStubFrame();
-  __ pushq(RDX);           // Preserve receiver.
   __ pushq(RBX);           // Preserve IC data.
   __ pushq(Immediate(0));  // Result slot.
   __ CallRuntime(kBreakpointRuntimeHandlerRuntimeEntry, 0);
   __ popq(CODE_REG);  // Original stub.
   __ popq(RBX);       // Restore IC data.
-  __ popq(RDX);       // Restore receiver.
   __ LeaveStubFrame();
 
   __ movq(RAX, FieldAddress(CODE_REG, target::Code::entry_point_offset()));

runtime/vm/stack_frame.cc
@@ -162,24 +162,12 @@ bool StackFrame::IsStubFrame() const {
 const char* StackFrame::ToCString() const {
   ASSERT(thread_ == Thread::Current());
   Zone* zone = Thread::Current()->zone();
-  if (IsDartFrame()) {
-    const Code& code = Code::Handle(zone, LookupDartCode());
-    ASSERT(!code.IsNull());
-    const auto& owner = Object::Handle(
-        zone, WeakSerializationReference::UnwrapIfTarget(code.owner()));
-    ASSERT(!owner.IsNull());
-    auto const opt = code.IsFunctionCode() && code.is_optimized() ? "*" : "";
-    auto const owner_name =
-        owner.IsFunction() ? Function::Cast(owner).ToFullyQualifiedCString()
-                           : owner.ToCString();
-    return zone->PrintToString("[%-8s : sp(%#" Px ") fp(%#" Px ") pc(%#" Px
-                               ") %s%s ]",
-                               GetName(), sp(), fp(), pc(), opt, owner_name);
-  } else {
-    return zone->PrintToString("[%-8s : sp(%#" Px ") fp(%#" Px ") pc(%#" Px
-                               ")]",
-                               GetName(), sp(), fp(), pc());
-  }
+  const Code& code = Code::Handle(zone, GetCodeObject());
+  ASSERT(!code.IsNull());
+  const char* name =
+      code.QualifiedName(NameFormattingParams(Object::kInternalName));
+  return zone->PrintToString("  pc 0x%" Pp " fp 0x%" Pp " sp 0x%" Pp " %s",
+                             pc(), fp(), sp(), name);
 }
 
 void ExitFrame::VisitObjectPointers(ObjectPointerVisitor* visitor) {

runtime/observatory/tests/service/breakpoint_gc_test.dart
@@ -1,0 +1,53 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'test_helper.dart';
+import 'service_test_common.dart';
+
+const int LINE_A = 16;
+const int LINE_B = 19;
+const int LINE_C = 22;
+const String file = "breakpoint_gc_test.dart";
+
+foo() => 42;
+
+testeeMain() {
+  foo(); // static call
+
+  dynamic list = [1, 2, 3];
+  list.clear(); // instance call
+  print(list);
+
+  dynamic local = list; // debug step check = runtime call
+  return local;
+}
+
+Future forceGC(isolate) async {
+  await isolate.invokeRpcNoUpgrade("_collectAllGarbage", {});
+}
+
+var tests = <IsolateTest>[
+  hasPausedAtStart,
+  setBreakpointAtUriAndLine(file, LINE_A), // at `foo()`
+  setBreakpointAtUriAndLine(file, LINE_B), // at `list.clear()`
+  setBreakpointAtUriAndLine(file, LINE_C), // at `local = list`
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_A),
+  forceGC, // Should not crash
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_B),
+  forceGC, // Should not crash
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_C),
+  forceGC, // Should not crash
+  resumeIsolate,
+];
+
+main(args) {
+  runIsolateTestsSynchronous(args, tests,
+      testeeConcurrent: testeeMain, pause_on_start: true);
+}

runtime/observatory/tests/service/service_kernel.status
@@ -52,6 +52,7 @@ break_on_function_child_isolate_test: SkipByDesign # Debugger is disabled in AOT
 break_on_function_many_child_isolates_test: SkipByDesign # Debugger is disabled in AOT mode.
 break_on_function_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_async_break_test: SkipByDesign # Debugger is disabled in AOT mode.
+breakpoint_gc_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_package_parts_class_file_uri_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_package_parts_class_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_parts_class_test: SkipByDesign # Debugger is disabled in AOT mode.

runtime/observatory_2/tests/service_2/breakpoint_gc_test.dart
@@ -1,0 +1,53 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'test_helper.dart';
+import 'service_test_common.dart';
+
+const int LINE_A = 16;
+const int LINE_B = 19;
+const int LINE_C = 22;
+const String file = "breakpoint_gc_test.dart";
+
+foo() => 42;
+
+testeeMain() {
+  foo(); // static call
+
+  dynamic list = [1, 2, 3];
+  list.clear(); // instance call
+  print(list);
+
+  dynamic local = list; // debug step check = runtime call
+  return local;
+}
+
+Future forceGC(isolate) async {
+  await isolate.invokeRpcNoUpgrade("_collectAllGarbage", {});
+}
+
+var tests = <IsolateTest>[
+  hasPausedAtStart,
+  setBreakpointAtUriAndLine(file, LINE_A), // at `foo()`
+  setBreakpointAtUriAndLine(file, LINE_B), // at `list.clear()`
+  setBreakpointAtUriAndLine(file, LINE_C), // at `local = list`
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_A),
+  forceGC, // Should not crash
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_B),
+  forceGC, // Should not crash
+  resumeIsolate,
+  hasStoppedAtBreakpoint,
+  stoppedAtLine(LINE_C),
+  forceGC, // Should not crash
+  resumeIsolate,
+];
+
+main(args) {
+  runIsolateTestsSynchronous(args, tests,
+      testeeConcurrent: testeeMain, pause_on_start: true);
+}

runtime/observatory_2/tests/service_2/service_2_kernel.status
@@ -52,6 +52,7 @@ break_on_function_child_isolate_test: SkipByDesign # Debugger is disabled in AOT
 break_on_function_many_child_isolates_test: SkipByDesign # Debugger is disabled in AOT mode.
 break_on_function_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_async_break_test: SkipByDesign # Debugger is disabled in AOT mode.
+breakpoint_gc_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_package_parts_class_file_uri_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_package_parts_class_test: SkipByDesign # Debugger is disabled in AOT mode.
 breakpoint_in_parts_class_test: SkipByDesign # Debugger is disabled in AOT mode.