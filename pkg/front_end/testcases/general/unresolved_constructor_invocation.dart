5952a4811938f8838b34550f75b33a9153902ddd
Johnni Winther <johnniwinther@google.com>
2021-09-02 15:16:13 +0000

    [cfe] Add language version to pre constructor tearoff tests
    Change-Id: I5c340277f30c6290365087b7d5498fed9803fc65
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/212288
    Reviewed-by: Dmitry Stefantsov <dmitryas@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/testcases/general/unresolved_constructor_invocation.dart
+++ pkg/front_end/testcases/general/unresolved_constructor_invocation.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=2.14
+
 import 'unresolved_constructor_invocation.dart' as resolved_prefix;
 
 class Super {
