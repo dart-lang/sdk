21298ee88433572bc0f483635d1d5e127bfd1d3c
Alexander Aprelev <aam@google.com>
2021-09-03 23:29:08 +0000

    Reland "[vm/concurrency] Enable isolate groups by-default in all modes"
    This reverts commit d0a3daffb778797cbc46dee2480abd907434f21c that
    reverted the initial land.
    
    The fix for the problem that required the revert landed in
    https://dart.googlesource.com/sdk/+/5ccf97a5a950ef70f41d84f186dad550d4acda01
    
    Issue https://github.com/dart-lang/sdk/issues/36097
    
    TEST=ci
    
    Change-Id: Ie92074059319e046809169d9e16d459ce1c2848f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/212481
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Alexander Aprelev <aam@google.com>

--- runtime/tests/vm/dart_2/isolates/dart_api_create_lightweight_isolate_test.dart
+++ runtime/tests/vm/dart_2/isolates/dart_api_create_lightweight_isolate_test.dart
@@ -5,7 +5,7 @@
 // @dart = 2.9
 
 // SharedObjects=ffi_test_functions
-// VMOptions=
+// VMOptions=--no-enable-isolate-groups
 // VMOptions=--enable-isolate-groups --disable-heap-verification
 
 import 'dart:async';
