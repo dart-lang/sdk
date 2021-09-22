4d82adb91f71d665b9ce7c39f2d6de83ff140dd2
Kevin Moore <kevmoo@google.com>
2021-09-22 20:47:17 +0000

    dart:html: use Object.hash
    Drop custom hash class
    
    Related to https://github.com/dart-lang/sdk/issues/27698
    
    Change-Id: I7f5fb3a642e5ae461a00ed1b5d307adb4ef448df
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/214181
    Auto-Submit: Kevin Moore <kevmoo@google.com>
    Commit-Queue: Kevin Moore <kevmoo@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>
    Reviewed-by: Stephen Adams <sra@google.com>

--- sdk/lib/html/dart2js/html_dart2js.dart
+++ sdk/lib/html/dart2js/html_dart2js.dart
@@ -11447,8 +11447,7 @@ class DomRectReadOnly extends Interceptor implements Rectangle {
       width == other.width &&
       height == other.height;
 
-  int get hashCode => _JenkinsSmiHash.hash4(
-      left.hashCode, top.hashCode, width.hashCode, height.hashCode);
+  int get hashCode => Object.hash(left, top, width, height);
 
   /**
    * Computes the intersection of `this` and [other].
@@ -34578,8 +34577,7 @@ class _DomRect extends DomRectReadOnly implements Rectangle {
       width == other.width &&
       height == other.height;
 
-  int get hashCode => _JenkinsSmiHash.hash4(
-      left.hashCode, top.hashCode, width.hashCode, height.hashCode);
+  int get hashCode => Object.hash(left, top, width, height);
 
   /**
    * Computes the intersection of `this` and [other].
@@ -34717,43 +34715,6 @@ class _DomRect extends DomRectReadOnly implements Rectangle {
   num? get y native;
 
   set y(num? value) native;
-}
-
-/**
- * This is the [Jenkins hash function][1] but using masking to keep
- * values in SMI range.
- *
- * [1]: http://en.wikipedia.org/wiki/Jenkins_hash_function
- *
- * Use:
- * Hash each value with the hash of the previous value, then get the final
- * hash by calling finish.
- *
- *     var hash = 0;
- *     for (var value in values) {
- *       hash = JenkinsSmiHash.combine(hash, value.hashCode);
- *     }
- *     hash = JenkinsSmiHash.finish(hash);
- */
-class _JenkinsSmiHash {
-  // TODO(11617): This class should be optimized and standardized elsewhere.
-
-  static int combine(int hash, int value) {
-    hash = 0x1fffffff & (hash + value);
-    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
-    return hash ^ (hash >> 6);
-  }
-
-  static int finish(int hash) {
-    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
-    hash = hash ^ (hash >> 11);
-    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
-  }
-
-  static int hash2(a, b) => finish(combine(combine(0, a), b));
-
-  static int hash4(a, b, c, d) =>
-      finish(combine(combine(combine(combine(0, a), b), c), d));
 }
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
@@ -36633,8 +36594,7 @@ abstract class CssRect implements Rectangle<num> {
       right == other.right &&
       bottom == other.bottom;
 
-  int get hashCode => _JenkinsSmiHash.hash4(
-      left.hashCode, top.hashCode, right.hashCode, bottom.hashCode);
+  int get hashCode => Object.hash(left, top, right, bottom);
 
   /**
    * Computes the intersection of `this` and [other].
