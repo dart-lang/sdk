5521b19af597c1e5688a4b22f037b5b8ce9bdb1a
Konstantin Shcheglov <scheglov@google.com>
2021-09-30 16:52:38 +0000

    Add assertHasLintFix() to help with testing g3 specific lint fixes.
    Bug: https://buganizer.corp.google.com/issues/200632192
    Change-Id: I5ea9d73ba440485ffeb72a189b3e77d24cb6bea0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/215000
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/src/test_all.dart
+++ pkg/analysis_server/test/src/test_all.dart
@@ -9,6 +9,7 @@ import 'computer/test_all.dart' as computer;
 import 'domain_abstract_test.dart' as domain_abstract;
 import 'domains/test_all.dart' as domains;
 import 'flutter/test_all.dart' as flutter;
+import 'g3/test_all.dart' as g3;
 import 'lsp/test_all.dart' as lsp;
 import 'plugin/test_all.dart' as plugin;
 import 'server/test_all.dart' as server;
@@ -22,6 +23,7 @@ void main() {
     domain_abstract.main();
     domains.main();
     flutter.main();
+    g3.main();
     lsp.main();
     plugin.main();
     server.main();
