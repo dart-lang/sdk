67be6634909a8806276c1bc68cc1f8190b2da557
Srujan Gaddam <srujzs@google.com>
2021-10-06 19:04:02 +0000

    [pkg:js] Add @staticInterop annotation to package:js
    Adds @staticInterop annotation to allow users to declare static interop
    classes. Also adds errors for erroneous usage of static interop
    classes, like including instance members or using a non-static
    supertype.
    
    Change-Id: I21abafbf6ea6c2eb7cd0425f0a54c1ba35d6ec6c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/215010
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

--- tests/lib/js/native_as_js_classes_static_test/global_library_namespace_test.dart
+++ tests/lib/js/native_as_js_classes_static_test/global_library_namespace_test.dart
@@ -12,27 +12,27 @@ import 'package:js/js.dart';
 @JS()
 class HTMLDocument {}
 //    ^
-// [web] JS interop class 'HTMLDocument' conflicts with natively supported class 'HtmlDocument' in 'dart:html'.
+// [web] Non-static JS interop class 'HTMLDocument' conflicts with natively supported class 'HtmlDocument' in 'dart:html'.
 
 @JS('HTMLDocument')
 class HtmlDocument {}
 //    ^
-// [web] JS interop class 'HtmlDocument' conflicts with natively supported class 'HtmlDocument' in 'dart:html'.
+// [web] Non-static JS interop class 'HtmlDocument' conflicts with natively supported class 'HtmlDocument' in 'dart:html'.
 
 @JS('self.Window')
 class WindowWithSelf {}
 //    ^
-// [web] JS interop class 'WindowWithSelf' conflicts with natively supported class 'Window' in 'dart:html'.
+// [web] Non-static JS interop class 'WindowWithSelf' conflicts with natively supported class 'Window' in 'dart:html'.
 
 @JS('window.Window')
 class WindowWithWindow {}
 //    ^
-// [web] JS interop class 'WindowWithWindow' conflicts with natively supported class 'Window' in 'dart:html'.
+// [web] Non-static JS interop class 'WindowWithWindow' conflicts with natively supported class 'Window' in 'dart:html'.
 
 @JS('self.window.self.window.self.Window')
 class WindowWithMultipleSelfsAndWindows {}
 //    ^
-// [web] JS interop class 'WindowWithMultipleSelfsAndWindows' conflicts with natively supported class 'Window' in 'dart:html'.
+// [web] Non-static JS interop class 'WindowWithMultipleSelfsAndWindows' conflicts with natively supported class 'Window' in 'dart:html'.
 
 @JS('foo.Window')
 class WindowWithDifferentPrefix {}
@@ -40,12 +40,12 @@ class WindowWithDifferentPrefix {}
 @JS()
 class DOMWindow {}
 //    ^
-// [web] JS interop class 'DOMWindow' conflicts with natively supported class 'Window' in 'dart:html'.
+// [web] Non-static JS interop class 'DOMWindow' conflicts with natively supported class 'Window' in 'dart:html'.
 
 @JS('DOMWindow')
 class DomWindow {}
 //    ^
-// [web] JS interop class 'DomWindow' conflicts with natively supported class 'Window' in 'dart:html'.
+// [web] Non-static JS interop class 'DomWindow' conflicts with natively supported class 'Window' in 'dart:html'.
 
 @JS('Foo')
 class Window {}
