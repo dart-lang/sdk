c5f432668c6ea275d406a4bc622971dc1b86bdcb
Konstantin Shcheglov <scheglov@google.com>
2021-10-12 02:41:54 +0000

    Remove unused MethodInvocationRewriteTag.
    Change-Id: I64c5e28705ec69a908e838f8f7165f81aa8dfdf4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/216276
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/summary2/ast_binary_tag.dart
+++ pkg/analyzer/lib/src/summary2/ast_binary_tag.dart
@@ -7,16 +7,6 @@ class AliasedElementTag {
   static const int genericFunctionElement = 1;
 }
 
-/// A `MethodInvocation` in unresolved AST might be rewritten later as
-/// another kinds of AST node. We store this rewrite with resolution data.
-class MethodInvocationRewriteTag {
-  static const int extensionOverride = 1;
-  static const int functionExpressionInvocation = 2;
-  static const int instanceCreationExpression_withName = 3;
-  static const int instanceCreationExpression_withoutName = 4;
-  static const int none = 5;
-}
-
 class Tag {
   static const int Nothing = 0;
   static const int Something = 1;
