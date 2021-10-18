60535b95de1a855f1762656fa9f86ea49bc8dc67
Konstantin Shcheglov <scheglov@google.com>
2021-10-18 16:45:59 +0000

    Issue 47073. Inherit covariant-by-declaration from any candidate.
    Bug: https://github.com/dart-lang/sdk/issues/47073
    Change-Id: I1ae779d50f1e4ee63f005098f9a22615c744480a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/217081
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/lib/src/dart/element/element.dart
+++ pkg/analyzer/lib/src/dart/element/element.dart
@@ -4188,12 +4188,17 @@ class MethodElementImpl extends ExecutableElementImpl implements MethodElement {
   /// this variable is not a subject of type inference, or there was no error.
   TopLevelInferenceError? typeInferenceError;
 
+  /// If this method is a synthetic element which is based on another method
+  /// with some modifications (such as making some parameters covariant),
+  /// this field contains the base method.
+  MethodElement? prototype;
+
   /// Initialize a newly created method element to have the given [name] at the
   /// given [offset].
   MethodElementImpl(String name, int offset) : super(name, offset);
 
   @override
-  MethodElement get declaration => this;
+  MethodElement get declaration => prototype ?? this;
 
   @override
   String get displayName {
@@ -4988,6 +4993,11 @@ class PropertyAccessorElementImpl extends ExecutableElementImpl
   @override
   late PropertyInducingElement variable;
 
+  /// If this method is a synthetic element which is based on another method
+  /// with some modifications (such as making some parameters covariant),
+  /// this field contains the base method.
+  PropertyAccessorElement? prototype;
+
   /// Initialize a newly created property accessor element to have the given
   /// [name] and [offset].
   PropertyAccessorElementImpl(String name, int offset) : super(name, offset);
@@ -5020,7 +5030,7 @@ class PropertyAccessorElementImpl extends ExecutableElementImpl
   }
 
   @override
-  PropertyAccessorElement get declaration => this;
+  PropertyAccessorElement get declaration => prototype ?? this;
 
   @override
   String get identifier {
