29e0c5e2bc1a17923d8b0b731eb39c9af24fafc7
Chloe Stefantsova <dmitryas@google.com>
2021-10-21 16:50:15 +0000

    Add "super-parameters" experiment flag
    Part of https://github.com/dart-lang/sdk/issues/47525.
    
    Change-Id: If7bf6f39b9b3ade1769f158ea1ca45515f859a2a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/217640
    Auto-Submit: Chloe Stefantsova <dmitryas@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- pkg/analyzer/lib/src/dart/analysis/driver.dart
+++ pkg/analyzer/lib/src/dart/analysis/driver.dart
@@ -80,7 +80,7 @@ import 'package:meta/meta.dart';
 /// TODO(scheglov) Clean up the list of implicitly analyzed files.
 class AnalysisDriver implements AnalysisDriverGeneric {
   /// The version of data format, should be incremented on every format change.
-  static const int DATA_VERSION = 189;
+  static const int DATA_VERSION = 190;
 
   /// The number of exception contexts allowed to write. Once this field is
   /// zero, we stop writing any new exception contexts in this process.
