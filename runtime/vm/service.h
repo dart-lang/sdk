711fdcc0de4226a86dbed3532036d467c6797573
Siva Annamalai <asiva@google.com>
2021-10-27 23:48:49 +0000

    Revert "Reland "[ VM / Service ] Add lookupResolvedPackageUri and lookupPackageUri RPCs""
    This reverts commit 6c6dd13b145aa737a939e475ad1901c3f59ca861.
    
    Reason for revert: Still seeing test failures in some configurations
    
    Original change's description:
    > Reland "[ VM / Service ] Add lookupResolvedPackageUri and lookupPackageUri RPCs"
    >
    > This reverts commit aa25975644e614c9fba5a000a7dcb13b595a0d92.
    >
    > TEST=uri_mappings_lookup_test.dart
    >
    > Change-Id: I311e3130bc14a392cfc4b5d81afb137505c5c202
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/218324
    > Commit-Queue: Ben Konyi <bkonyi@google.com>
    > Reviewed-by: Siva Annamalai <asiva@google.com>
    
    TBR=bkonyi@google.com,asiva@google.com
    
    Change-Id: Ia69d9cfd7b395f692679fdfd185ef757550104bf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/218360
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Commit-Queue: Siva Annamalai <asiva@google.com>

--- runtime/vm/service.h
+++ runtime/vm/service.h
@@ -15,7 +15,7 @@
 namespace dart {
 
 #define SERVICE_PROTOCOL_MAJOR_VERSION 3
-#define SERVICE_PROTOCOL_MINOR_VERSION 52
+#define SERVICE_PROTOCOL_MINOR_VERSION 51
 
 class Array;
 class EmbedderServiceHandler;
