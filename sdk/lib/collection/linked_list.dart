d4fece91cf05b145b02152dc95a070780518b7dc
Jukka-Pekka Siitonen <jukka-pekka.siitonen@codemate.com>
2021-11-02 09:20:30 +0200

    LinkedList example

--- sdk/lib/collection/linked_list.dart
+++ sdk/lib/collection/linked_list.dart
@@ -39,7 +39,7 @@ part of dart.collection;
 ///
 ///   @override
 ///   String toString() {
-///     return '$id $text';
+///     return 'id: $id text: $text';
 ///   }
 /// }
 ///
@@ -47,30 +47,30 @@ part of dart.collection;
 ///   final LinkedList linkedList = LinkedList<EntryItem>();
 ///   linkedList.addAll(
 ///       [EntryItem(1, 'A'), EntryItem(2, 'B'), EntryItem(3, 'C')].toList());
-///   print(linkedList.first); // 1 A
-///   print(linkedList.last); // 3 C
+///   print(linkedList.first); // 1 : A
+///   print(linkedList.last); // 3 : C
 ///   // Add new item after first item
 ///   linkedList.first.insertAfter(EntryItem(15, 'E'));
 ///   // Add new item before last item
 ///   linkedList.last.insertBefore(EntryItem(10, 'D'));
 ///   // Iterate items using forEach
 ///   linkedList.forEach((entry) {
-///     print(entry.toString()); // 3 C
-///     // 1 A
-///     // 15 E
-///     // 2 B
-///     // 10 D
-///     // 3 C
+///     print(entry.toString());
+///     // 1 : A
+///     // 15 : E
+///     // 2 : B
+///     // 10 : D
+///     // 3 : C
 ///   });
 ///   // Remove item using index from list
 ///   linkedList.elementAt(2).unlink();
-///   print(linkedList); // (1 A, 15 E, 10 D, 3 C)
+///   print(linkedList); // (1 : A, 15 : E, 10 : D, 3 : C)
 ///   // Remove first item
 ///   linkedList.first.unlink();
-///   print(linkedList); // (15 E, 10 D, 3 C)
+///   print(linkedList); // (15 : E, 10 : D, 3 : C)
 ///   // Remove last item from list
 ///   linkedList.remove(linkedList.last);
-///   print(linkedList); // (15 E, 10 D)
+///   print(linkedList); // (15 : E, 10 : D)
 ///   // Remove all items
 ///   linkedList.clear();
 ///   print(linkedList.length); // 0
