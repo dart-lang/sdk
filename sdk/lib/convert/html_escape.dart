78c620456de40af08865b034ac4bca18eca1b6f1
stepthomp <92918404+stepthomp@users.noreply.github.com>
2021-11-02 11:05:44 +0200

    Minor corrections

--- sdk/lib/convert/html_escape.dart
+++ sdk/lib/convert/html_escape.dart
@@ -26,7 +26,7 @@ const HtmlEscape htmlEscape = HtmlEscape();
 
 /// HTML escape modes.
 ///
-/// Allows specifying a mode for HTML escaping that depend on the context
+/// Allows specifying a mode for HTML escaping that depends on the context
 /// where the escaped result is going to be used.
 /// The relevant contexts are:
 ///
@@ -47,7 +47,7 @@ const HtmlEscape htmlEscape = HtmlEscape();
 ///   escapeQuot: false,
 ///   escapeApos: false,
 ///   escapeSlash: false,
-/// );
+///  );
 ///
 /// const HtmlEscape htmlEscape = HtmlEscape(htmlEscapeMode);
 /// String unescaped = 'Text & subject';
@@ -88,13 +88,13 @@ class HtmlEscapeMode {
   /// [the Open Web Application Security Project](https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content)
   final bool escapeSlash;
 
-  /// Default escaping mode which escape all characters.
+  /// Default escaping mode, which escapes all characters.
   ///
   /// The result of such an escaping is usable both in element content and
   /// in any attribute value.
   ///
   /// The escaping only works for elements with normal HTML content,
-  /// and not for, for example, script or style element content,
+  /// and not, for example, for script or style element content,
   /// which require escapes matching their particular content syntax.
   static const HtmlEscapeMode unknown =
       HtmlEscapeMode._('unknown', true, true, true, true);
@@ -117,14 +117,14 @@ class HtmlEscapeMode {
   ///
   /// Escapes single quotes (`'`) but not double quotes (`"`),
   /// and escapes `<` and `>` characters because they are not allowed
-  /// in strict XHTML attributes
+  /// in strict XHTML attributes.
   static const HtmlEscapeMode sqAttribute =
       HtmlEscapeMode._('attribute', true, false, true, false);
 
   /// Escaping mode for text going into HTML element content.
   ///
   /// The escaping only works for elements with normal HTML content,
-  /// and not for, for example, script or style element content,
+  /// and not, for example, for script or style element content,
   /// which require escapes matching their particular content syntax.
   ///
   /// Escapes `<` and `>` characters.
@@ -152,12 +152,12 @@ class HtmlEscapeMode {
 
 /// Converter which escapes characters with special meaning in HTML.
 ///
-/// The converter finds characters that are significant in HTML source and
+/// The converter finds characters that are significant in the HTML source and
 /// replaces them with corresponding HTML entities.
 ///
 /// The characters that need escaping in HTML are:
 ///
-/// * `&` (ampersand) always need to be escaped.
+/// * `&` (ampersand) always needs to be escaped.
 /// * `<` (less than) and `>` (greater than) when inside an element.
 /// * `"` (quote) when inside a double-quoted attribute value.
 /// * `'` (apostrophe) when inside a single-quoted attribute value.
@@ -201,7 +201,7 @@ class HtmlEscape extends Converter<String, String> {
   ///
   /// If [mode] is provided as either [HtmlEscapeMode.attribute] or
   /// [HtmlEscapeMode.element], only the corresponding subset of HTML
-  /// characters are escaped.
+  /// characters is escaped.
   /// The default is to escape all HTML characters.
   const HtmlEscape([this.mode = HtmlEscapeMode.unknown]);
 
