8dcc1dcf5e781c6acb94fe0021f34d84ac8012d5
Johnni Winther <johnniwinther@google.com>
2021-11-18 14:58:26 +0000

    [cfe] Migrate (more) CFE tests (part 7)
    Change-Id: I11f667e9e0f6d96c8b57708da1485414e3b9f8da
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/220743
    Reviewed-by: Jens Johansen <jensj@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/test/fasta/outline_suite.dart
+++ pkg/front_end/test/fasta/outline_suite.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
-// @dart = 2.9
-
 library fasta.test.outline_test;
 
 import 'testing/suite.dart';
