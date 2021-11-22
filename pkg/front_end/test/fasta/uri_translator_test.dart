f5af678467753f6a1a0d585bb7699712520ff38e
Johnni Winther <johnniwinther@google.com>
2021-11-22 09:04:30 +0000

    [cfe] Migrate (more) CFE tests (part 8)
    Change-Id: I39c493168b847eb14ebc9ff649e2552e8d00b23c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/220764
    Reviewed-by: Jens Johansen <jensj@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/test/fasta/uri_translator_test.dart
+++ pkg/front_end/test/fasta/uri_translator_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
-
 import 'package:_fe_analyzer_shared/src/util/libraries_specification.dart';
 import 'package:front_end/src/fasta/uri_translator.dart';
 import 'package:package_config/package_config.dart';
