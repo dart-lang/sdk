e0c7fdf004fb43900001ce430271881d106abca7
Jukka-Pekka Siitonen <jukka-pekka.siitonen@codemate.com>
2021-11-22 18:11:56 +0200

    code fixes based on review

--- sdk/lib/collection/linked_list.dart
+++ sdk/lib/collection/linked_list.dart
@@ -45,36 +45,37 @@ part of dart.collection;
 /// void main(){
 ///   final linkedList = LinkedList<EntryItem>();
 ///   linkedList.addAll(
-///       [EntryItem(1, 'A'), EntryItem(2, 'B'), EntryItem(3, 'C')].toList());
+///       [EntryItem(1, 'A'), EntryItem(2, 'B'), EntryItem(3, 'C')]);
 ///   print(linkedList.first); // 1 : A
 ///   print(linkedList.last); // 3 : C
 ///
-///   // Add new item after first item
+///   // Add new item after first item.
 ///   linkedList.first.insertAfter(EntryItem(15, 'E'));
-///   // Add new item before last item
+///   // Add new item before last item.
 ///   linkedList.last.insertBefore(EntryItem(10, 'D'));
-///   // Iterate items using forEach
-///   linkedList.forEach((entry) {
-///     print(entry.toString());
+///   // Iterate items.
+///   for (var entry in linkedList) {
+///     print(entry);
 ///     // 1 : A
 ///     // 15 : E
 ///     // 2 : B
 ///     // 10 : D
 ///     // 3 : C
-///   });
-///   
-///   // Remove item using index from list
+///   }
+///
+///   // Remove item using index from list.
 ///   linkedList.elementAt(2).unlink();
 ///   print(linkedList); // (1 : A, 15 : E, 10 : D, 3 : C)
-///   // Remove first item
+///   // Remove first item.
 ///   linkedList.first.unlink();
 ///   print(linkedList); // (15 : E, 10 : D, 3 : C)
-///   // Remove last item from list
+///   // Remove last item from list.
 ///   linkedList.remove(linkedList.last);
 ///   print(linkedList); // (15 : E, 10 : D)
-///   // Remove all items
+///   // Remove all items.
 ///   linkedList.clear();
 ///   print(linkedList.length); // 0
+///   print(linkedList.isEmpty); // true
 /// }
 /// ```
 class LinkedList<E extends LinkedListEntry<E>> extends Iterable<E> {
