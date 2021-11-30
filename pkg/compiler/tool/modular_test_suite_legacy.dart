b735151bbccdcbe216f45eb511ab01d1650b0eb2
Joshua Litt <joshualitt@google.com>
2021-11-30 01:02:34 +0000

    [dart2js] Restore test coverage for modular analysis.
    Change-Id: Iedfc6c1779064db469c209545c361e047ee0318c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/221026
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Commit-Queue: Joshua Litt <joshualitt@google.com>

--- pkg/compiler/tool/modular_test_suite_legacy.dart
+++ pkg/compiler/tool/modular_test_suite_legacy.dart
@@ -22,8 +22,7 @@ main(List<String> args) async {
         IOPipeline([
           OutlineDillCompilationStep(),
           FullDillCompilationStep(),
-          ModularAnalysisStep(),
-          ComputeClosedWorldStep(useModularAnalysis: true),
+          ComputeClosedWorldStep(useModularAnalysis: false),
           GlobalAnalysisStep(),
           Dart2jsCodegenStep(codeId0),
           Dart2jsCodegenStep(codeId1),
