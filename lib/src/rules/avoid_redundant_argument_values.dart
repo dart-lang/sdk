e66f1d6c67931e39a55454df8000cd1f6b474b42
Phil Quitslund <pq@users.noreply.github.com>
2021-12-03 09:14:09 -0800

    Tidy param check (dart-lang/linter#3086)
    * tidy up param check
    
    * + hasRequired

--- lib/src/rules/avoid_redundant_argument_values.dart
+++ lib/src/rules/avoid_redundant_argument_values.dart
@@ -71,10 +71,7 @@ class _Visitor extends SimpleAstVisitor {
     for (var i = arguments.length - 1; i >= 0; --i) {
       var arg = arguments[i];
       var param = arg.staticParameterElement;
-      if (param == null ||
-          param.hasRequired ||
-          param.isRequiredNamed ||
-          param.isRequiredPositional) {
+      if (param == null || param.hasRequired || !param.isOptional) {
         continue;
       }
       var value = param.computeConstantValue();
