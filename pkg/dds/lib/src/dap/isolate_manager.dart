6cde2fb662ab2ee4040038855cde3b39e53c4f04
Sam Rawlins <srawlins@google.com>
2021-12-16 18:43:09 +0000

    DDS: Add missing returns to nullably-typed functions
    Bug: https://github.com/dart-lang/sdk/issues/46656
    Change-Id: I130903d6c009d60bcf3c1fee2378abb96c28714f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/224381
    Reviewed-by: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Ben Konyi <bkonyi@google.com>

--- pkg/dds/lib/src/dap/isolate_manager.dart
+++ pkg/dds/lib/src/dap/isolate_manager.dart
@@ -375,6 +375,7 @@ class IsolateManager {
         'Debugger failed to evaluate breakpoint $type "$expression": $e\n',
       );
     }
+    return null;
   }
 
   void _handleExit(vm.Event event) {
