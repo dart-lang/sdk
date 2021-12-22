819a809a9edee0eb4b0b1769c1bdfa6ea701dbf9
Jukka-Pekka Siitonen <jukka-pekka.siitonen@codemate.com>
2021-12-22 11:11:16 +0200

    improvements to documentation after review

--- sdk/lib/math/random.dart
+++ sdk/lib/math/random.dart
@@ -12,20 +12,20 @@ part of dart.math;
 /// Use the [Random.secure]() constructor for cryptographic purposes.
 ///
 /// To create a non-negative random integer uniformly distributed in the range
-/// from 0, inclusive, to [max], exclusive, use [nextInt(int max)].
+/// from 0, inclusive, to max, exclusive, use [nextInt(int max)].
 /// ```dart
-/// var intValue = Random().nextInt(10); // Value between 0 - 9
-/// intValue = Random().nextInt(100) + 50; // Value between 50 - 149
+/// var intValue = Random().nextInt(10); // Value is >= 0.0 and < 1.0.
+/// intValue = Random().nextInt(100) + 50; // Value is >= 50 and < 150.
 /// ```
 /// To create a non-negative random floating point value uniformly distributed
 /// in the range from 0.0, inclusive, to 1.0, exclusive, use [nextDouble].
 /// ```dart
-/// var doubleValue = Random().nextDouble(); // Value is >= 0.0 and < 1.0
-/// doubleValue = Random().nextDouble() + 10; // Value is >= 10.0 and < 11.0
+/// var doubleValue = Random().nextDouble(); // Value is >= 0.0 and < 1.0.
+/// doubleValue = Random().nextDouble() * 256; // Value is >= 0.0 and < 256.0.
 /// ```
 /// To create a random boolean value, use [nextBool].
 /// ```dart
-/// var boolValue = Random().nextBool();
+/// var boolValue = Random().nextBool(); // true or false, with equal chance.
 /// ```
 ///
 abstract class Random {
@@ -50,8 +50,8 @@ abstract class Random {
   ///
   /// Example:
   /// ```dart
-  /// var intValue = Random().nextInt(10); // Value between 0 - 9
-  /// intValue = Random().nextInt(100) + 50; // Value between 50 - 149
+  /// var intValue = Random().nextInt(10); // Value is >= 0.0 and < 1.0.
+  /// intValue = Random().nextInt(100) + 50; // Value is >= 50 and < 150.
   /// ```
   int nextInt(int max);
 
@@ -60,8 +60,8 @@ abstract class Random {
   ///
   /// Example:
   /// ```dart
-  /// var doubleValue = Random().nextDouble(); // Value is >= 0.0 and < 1.0
-  /// doubleValue = Random().nextDouble() + 10; // Value is >= 10.0 and < 11.0
+  /// var doubleValue = Random().nextDouble(); // Value is >= 0.0 and < 1.0.
+  /// doubleValue = Random().nextDouble() * 256; // Value is >= 0.0 and < 256.0.
   /// ```
   double nextDouble();
 
@@ -69,7 +69,7 @@ abstract class Random {
   ///
   /// Example:
   /// ```dart
-  /// var boolValue = Random().nextBool();
+  /// var boolValue = Random().nextBool(); // true or false, with equal chance.
   /// ```
   bool nextBool();
 }
