d095700f13cc0fa2683d62251793f26f211a4854
Lasse R.H. Nielsen <lrn@google.com>
2022-01-07 13:20:06 +0000

    Add `WeakReference` and `Finalizer` to `dart:core`.
    This adds the abstract classes with external factory constructors,
    and adds throwing constructors to all backed patch files.
    
    Further implementation can add the functionality for the backends.
    
    This addresses part of #47773, leaving the FFI class to the
    FFI implementors.
    
    Bug: https://github.com/47773
    Change-Id: Id7ee0006b6ea5ed789867a143ee6266f6dbbef66
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/221502
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Lasse Nielsen <lrn@google.com>

--- sdk/lib/_internal/vm/lib/expando_patch.dart
+++ sdk/lib/_internal/vm/lib/expando_patch.dart
@@ -166,3 +166,19 @@ class Expando<T> {
   List<_WeakProperty?> _data;
   int _used; // Number of used (active and deleted) slots.
 }
+
+@patch
+class WeakReference<T extends Object> {
+  @patch
+  factory WeakReference(T object) {
+    throw UnimplementedError("WeakReference");
+  }
+}
+
+@patch
+class Finalizer<T> {
+  @patch
+  factory Finalizer(void Function(T) object) {
+    throw UnimplementedError("Finalizer");
+  }
+}
