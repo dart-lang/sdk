411acab4e93a90aeba13cc985834e3f70ccb6af8
Jukka-Pekka Siitonen <jukka-pekka.siitonen@codemate.com>
2022-01-11 15:53:32 +0200

    Updated based on review comments.

--- sdk/lib/collection/collection.dart
+++ sdk/lib/collection/collection.dart
@@ -10,38 +10,42 @@
 /// ```
 ///
 /// ## Map
-/// A collection of key/value pairs, from which to retrieve a value
-/// using the associated key. [Map] is the general interface of key/value pair
-/// collections.
+/// A finite mapping from unique keys to their associated values.
+/// Allows efficient lookup of the value associated with a key, if any,
+/// and iterating through the individual keys and values of the map.
+/// The [Map] interface has a number of implementations, including the following:
 /// * [HashMap] is unordered, the order of iteration is not guaranteed.
 /// * [LinkedHashMap] iterates in key insertion order.
 /// * [SplayTreeMap] iterates the keys in sorted order.
 /// * [UnmodifiableMapView] is a wrapper, an unmodifiable [Map] view of another
-/// Map.
+/// `Map`.
 ///
 /// ## Set
 /// A collection of objects in which each object can occur only once.
-/// [Set] is the general interface of collection where each object can occur
-/// only once.
+/// The [Set] interface has a number of implementations, including the following:
 /// * [HashSet] the order of the objects in the iterations is not guaranteed.
 /// * [LinkedHashSet] iterates the objects in insertion order.
 /// * [SplayTreeSet] iterates the objects in sorted order.
 /// * [UnmodifiableSetView] is a wrapper, an unmodifiable [Set] view of another
-/// Set.
+/// `Set`.
 ///
 /// ## Queue
-/// A queue is a collection that can be processed at both ends.
-/// No access to object data through the index, access to first and last object.
-/// * [Queue] is a base class for queue.
-/// * [ListQueue] is a queue-based list. Default implementation for [Queue].
+/// A queue is a sequence of elements that is intended to be modified,
+/// by adding or removing elements, only at its ends.
+/// Dart queues are *double ended* queues, which means that they can be
+/// accessed equally from either end, and can therefore be used
+/// to implement both stack and queue behavior.
+/// * [Queue] is the general interface for queues.
+/// * [ListQueue] is a list-based queue. Default implementation for [Queue].
 /// * [DoubleLinkedQueue] is a queue implementation based on a double-linked
 /// list.
 ///
 /// ## List
-/// An indexable collection of objects, objects can be accessed through index
-/// of list. [List] is also called an "array" in other programming languages.
+/// An indexable Sequence of objects. Objects can be accessed using their
+/// position, index, in the sequence. [List] is also called an "array" in other
+/// programming languages.
 /// * [UnmodifiableListView] is a wrapper, an unmodifiable [List] view of
-/// another List.
+/// another `List`.
 ///
 /// ## LinkedList
 /// [LinkedList] is a specialized double-linked list of elements that extends
