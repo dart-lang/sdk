127f6518fce461a0edabddb2d91e381cc5069e74
Alex Lindroos <alex.lindroos@codemate.com>
2022-01-12 11:57:39 +0200

    Modified examples in Identical

--- sdk/lib/core/identical.dart
+++ sdk/lib/core/identical.dart
@@ -5,24 +5,17 @@
 part of dart.core;
 
 /// Check whether two references are to the same object.
+///
+/// Example:
 /// ```dart
-/// void main() {
-///  Car micro = Car(type: 'Micro', color: 'Red');
-///  Car suv = Car(type: 'SUV', color: 'Black');
-///
-///  var areObjectsIdentical = identical(micro, micro);
-///  print(areObjectsIdentical); // true
-///
-///  areObjectsIdentical = identical(micro, suv);
-///  print(areObjectsIdentical); // false
-/// }
-///
-/// class Car {
-///  late final type;
-///  late final color;
-///
-///  Car({this.type, this.color});
-/// }
+/// var o = new Object();
+/// var isIdentical = identical(o, new Object()); // false, different objects.
+/// isIdentical = identical(o, o); // true, same object
+/// isIdentical = identical(const Object(), const Object()); // true, const canonicalizes
+/// isIdentical = identical([1], [1]); // false
+/// isIdentical = identical(const [1], const [1]); // true
+/// isIdentical = identical(const [1], const [2]); // false
+/// isIdentical = identical(2, 1 + 1); // true, integers canonicalizes
 /// ```
 external bool identical(Object? a, Object? b);
 
@@ -33,18 +26,28 @@ external bool identical(Object? a, Object? b);
 ///
 /// This hash code is compatible with [identical],
 /// which just means that it's guaranteed to be stable over time.
+/// ```dart import dart:collection
+///   var identitySet = HashSet(equals: identical, hashCode: identityHashCode);
+///   var dt1 = DateTime.now();
+///   var dt2 = DateTime.fromMicrosecondsSinceEpoch(dt1.microsecondsSinceEpoch);
+///   assert(dt1 == dt2);
+///   identitySet.add(dt1);
+///   print(identitySet.contains(dt1)); // true
+///   print(identitySet.contains(dt2)); // false
+/// ```
+/// Another example:
 /// ```dart
 /// void main() {
-///  Car micro = Car(type: 'Micro', color: 'Red');
-///  int identityHashCode = identityHashCode(micro);
-///  print(identityHashCode); // Hash code of the object
+///   Planet mars = Planet(name: 'Mars', color: 'Red');
+///   int identityHash = identityHashCode(mars);
+///   print(identityHash); // Hash code of the object
 /// }
 ///
-/// class Car {
-///  late final type;
-///  late final color;
+/// class Planet {
+///   late final name;
+///   late final color;
 ///
-///  Car({this.type, this.color});
+///   Planet({this.name, this.color});
 /// }
 /// ```
 @pragma("vm:entry-point")
