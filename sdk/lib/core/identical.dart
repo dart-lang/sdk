c1e68cb55cb1ff81d3bfb4dd86eb53cf761ad1f1
Alex Lindroos <alex_lindroos@hotmail.com>
2022-01-12 14:30:52 +0000

    Docs update Identical
    Examples added
    
    Closes https://github.com/dart-lang/sdk/pull/48120
    https://github.com/dart-lang/sdk/pull/48120
    
    GitOrigin-RevId: 0e73845c815492e0e64defe814e7867803d192a3
    Change-Id: Ie5232093a27e2df9f9c679490d84b2848a8fbedc
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/227480
    Reviewed-by: Lasse Nielsen <lrn@google.com>
    Commit-Queue: Lasse Nielsen <lrn@google.com>

--- sdk/lib/core/identical.dart
+++ sdk/lib/core/identical.dart
@@ -5,6 +5,18 @@
 part of dart.core;
 
 /// Check whether two references are to the same object.
+///
+/// Example:
+/// ```dart
+/// var o = new Object();
+/// var isIdentical = identical(o, new Object()); // false, different objects.
+/// isIdentical = identical(o, o); // true, same object
+/// isIdentical = identical(const Object(), const Object()); // true, const canonicalizes
+/// isIdentical = identical([1], [1]); // false
+/// isIdentical = identical(const [1], const [1]); // true
+/// isIdentical = identical(const [1], const [2]); // false
+/// isIdentical = identical(2, 1 + 1); // true, integers canonicalizes
+/// ```
 external bool identical(Object? a, Object? b);
 
 /// The identity hash code of [object].
@@ -14,5 +26,14 @@ external bool identical(Object? a, Object? b);
 ///
 /// This hash code is compatible with [identical],
 /// which just means that it's guaranteed to be stable over time.
+/// ```dart import:dart:collection
+/// var identitySet = HashSet(equals: identical, hashCode: identityHashCode);
+/// var dt1 = DateTime.now();
+/// var dt2 = DateTime.fromMicrosecondsSinceEpoch(dt1.microsecondsSinceEpoch);
+/// assert(dt1 == dt2);
+/// identitySet.add(dt1);
+/// print(identitySet.contains(dt1)); // true
+/// print(identitySet.contains(dt2)); // false
+/// ```
 @pragma("vm:entry-point")
 external int identityHashCode(Object? object);
