95c9a115d166474ab835d851f47c001bb02bcf63
Jukka-Pekka Siitonen <jukka-pekka.siitonen@codemate.com>
2022-01-13 12:02:54 +0200

    RegExpMatch example added

--- sdk/lib/core/regexp.dart
+++ sdk/lib/core/regexp.dart
@@ -208,6 +208,41 @@ abstract class RegExp implements Pattern {
 /// Regular expression matches are [Match]es, but also include the ability
 /// to retrieve the names for any named capture groups and to retrieve
 /// matches for named capture groups by name instead of their index.
+///
+/// Example:
+/// ```dart
+/// const pattern =
+///     r'^\[(?<Time>\s*((?<hour>\d+)):((?<minute>\d+))\.((?<second>\d+)))\]'
+///     r'\s(?<Message>\s*(.*)$)';
+///
+/// final regExp = RegExp(
+///   pattern,
+///   multiLine: true,
+/// );
+///
+/// const multilineText = '[00:13.37] This is a first message.\n'
+///     '[01:15.57] This is a second message.\n';
+///
+/// RegExpMatch regExpMatch = regExp.firstMatch(multilineText)!;
+/// print(regExpMatch.groupNames.join('-')); // hour-minute-second-Time-Message.
+/// final time = regExpMatch.namedGroup('Time'); // 00:13.37
+/// final hour = regExpMatch.namedGroup('hour'); // 00
+/// final minute = regExpMatch.namedGroup('minute'); // 13
+/// final second = regExpMatch.namedGroup('second'); // 37
+/// final message =
+///     regExpMatch.namedGroup('Message'); // This is a first message.
+/// final date = regExpMatch.namedGroup('Date'); // Undefined `Date`, throws.
+///
+/// Iterable<RegExpMatch> matches = regExp.allMatches(multilineText);
+/// for (final m in matches) {
+///   print(m.namedGroup('Time'));
+///   print(m.namedGroup('Message'));
+///   // 00:13.37
+///   // This is a first message.
+///   // 01:15.57
+///   // This is a second message.
+/// }
+/// ```
 @Since("2.3")
 abstract class RegExpMatch implements Match {
   /// The string matched by the group named [name].
