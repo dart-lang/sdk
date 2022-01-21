e04aa10aa03f2a90f299038dfc9cf7ef6d5d4522
Alex Lindroos <alex_lindroos@hotmail.com>
2022-01-21 18:10:40 +0000

    Docs update RegExp and RegExpMatch
    Examples added
    
    Closes https://github.com/dart-lang/sdk/pull/48157
    https://github.com/dart-lang/sdk/pull/48157
    
    GitOrigin-RevId: f95f8ca1b6ed23892035bbdebf1aeb1e0f2e978d
    Change-Id: I0714c3f636314fbbd97c91e2f6a1ebd032c9ca26
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/228441
    Reviewed-by: Lasse Nielsen <lrn@google.com>
    Commit-Queue: Lasse Nielsen <lrn@google.com>

--- sdk/lib/core/regexp.dart
+++ sdk/lib/core/regexp.dart
@@ -19,15 +19,32 @@ part of dart.core;
 /// and returns the first [RegExpMatch].
 /// All other methods in [RegExp] can be build from that.
 ///
+/// The following example finds the first match of a regular expression in
+/// a string.
+/// ```dart
+/// RegExp exp = RegExp(r'(\w+)');
+/// String str = 'Parse my string';
+/// RegExpMatch? match = exp.firstMatch(str);
+/// print(match![0]); // "Parse"
+/// ```
 /// Use [allMatches] to look for all matches of a regular expression in
 /// a string.
 ///
 /// The following example finds all matches of a regular expression in
 /// a string.
 /// ```dart
-/// RegExp exp = RegExp(r"(\w+)");
-/// String str = "Parse my string";
+/// RegExp exp = RegExp(r'(\w+)');
+/// String str = 'Parse my string';
 /// Iterable<RegExpMatch> matches = exp.allMatches(str);
+/// for (final m in matches) {
+///   print(m[0]);
+/// }
+/// ```
+/// The output of the example is:
+/// ```
+/// Parse
+/// my
+/// string
 /// ```
 ///
 /// Note the use of a _raw string_ (a string prefixed with `r`)
@@ -54,8 +71,8 @@ abstract class RegExp implements Pattern {
   /// Example:
   ///
   /// ```dart
-  /// var wordPattern = RegExp(r"(\w+)");
-  /// var bracketedNumberValue = RegExp("$key: \\[\\d+\\]");
+  /// final wordPattern = RegExp(r'(\w+)');
+  /// final digitPattern = RegExp(r'(\d+)');
   /// ```
   ///
   /// Notice the use of a _raw string_ in the first example, and a regular
@@ -82,22 +99,55 @@ abstract class RegExp implements Pattern {
   /// larger regular expression. Since a [String] is itself a [Pattern]
   /// which matches itself, converting the string to a regular expression
   /// isn't needed in order to search for just that string.
+  /// ```dart
+  /// print(RegExp.escape('dash@example.com')); // dash@example\.com
+  /// print(RegExp.escape('a+b')); // a\+b
+  /// print(RegExp.escape('a*b')); // a\*b
+  /// print(RegExp.escape('{a-b}')); // \{a-b\}
+  /// print(RegExp.escape('a?')); // a\?
+  /// ```
   external static String escape(String text);
 
   /// Finds the first match of the regular expression in the string [input].
   ///
   /// Returns `null` if there is no match.
+  /// ```dart
+  /// final string = '[00:13.37] This is a chat message.';
+  /// final regExp = RegExp(r'c\w*');
+  /// final match = regExp.firstMatch(string)!;
+  /// print(match[0]); // chat
+  /// ```
   RegExpMatch? firstMatch(String input);
 
   Iterable<RegExpMatch> allMatches(String input, [int start = 0]);
 
   /// Whether the regular expression has a match in the string [input].
+  /// ```dart
+  /// var string = 'Dash is a bird';
+  /// var regExp = RegExp(r'(humming)?bird');
+  /// var match = regExp.hasMatch(string); // true
+  ///
+  /// regExp = RegExp(r'dog');
+  /// match = regExp.hasMatch(string); // false
+  /// ```
   bool hasMatch(String input);
 
   /// The substring of the first match of this regular expression in [input].
+  /// ```dart
+  /// var string = 'Dash is a bird';
+  /// var regExp = RegExp(r'(humming)?bird');
+  /// var match = regExp.stringMatch(string); // Match
+  ///
+  /// regExp = RegExp(r'dog');
+  /// match = regExp.stringMatch(string); // No match
+  /// ```
   String? stringMatch(String input);
 
   /// The source regular expression string used to create this `RegExp`.
+  /// ```dart
+  /// final regExp = RegExp(r'\p{L}');
+  /// print(regExp.pattern); // \p{L}
+  /// ```
   String get pattern;
 
   /// Whether this regular expression matches multiple lines.
@@ -112,6 +162,16 @@ abstract class RegExp implements Pattern {
   /// If the regular expression is not case sensitive, it will match an input
   /// letter with a pattern letter even if the two letters are different case
   /// versions of the same letter.
+  /// ```dart
+  /// final str = 'Parse my string';
+  /// var regExp = RegExp(r'STRING', caseSensitive: false);
+  /// final hasMatch = regExp.hasMatch(str); // Has matches.
+  /// print(regExp.isCaseSensitive); // false
+  ///
+  /// regExp = RegExp(r'STRING', caseSensitive: true);
+  /// final hasCaseSensitiveMatch = regExp.hasMatch(str); // No matches.
+  /// print(regExp.isCaseSensitive); // true
+  /// ```
   bool get isCaseSensitive;
 
   /// Whether this regular expression is in Unicode mode.
@@ -124,6 +184,19 @@ abstract class RegExp implements Pattern {
   /// In Unicode mode, the syntax of the RegExp pattern is more restricted, but
   /// some pattern features, like Unicode property escapes, are only available in
   /// this mode.
+  /// ```dart
+  /// var regExp = RegExp(r'^\p{L}$', unicode: true);
+  /// print(regExp.hasMatch('a')); // true
+  /// print(regExp.hasMatch('b')); // true
+  /// print(regExp.hasMatch('?')); // false
+  /// print(regExp.hasMatch(r'p{L}')); // false
+  ///
+  /// regExp = RegExp(r'^\p{L}$', unicode: false);
+  /// print(regExp.hasMatch('a')); // false
+  /// print(regExp.hasMatch('b')); // false
+  /// print(regExp.hasMatch('?')); // false
+  /// print(regExp.hasMatch(r'p{L}')); // true
+  /// ```
   @Since("2.4")
   bool get isUnicode;
 
@@ -145,6 +218,41 @@ abstract class RegExp implements Pattern {
 /// Regular expression matches are [Match]es, but also include the ability
 /// to retrieve the names for any named capture groups and to retrieve
 /// matches for named capture groups by name instead of their index.
+///
+/// Example:
+/// ```dart
+/// const pattern =
+///     r'^\[(?<Time>\s*((?<hour>\d+)):((?<minute>\d+))\.((?<second>\d+)))\]'
+///     r'\s(?<Message>\s*(.*)$)';
+///
+/// final regExp = RegExp(
+///   pattern,
+///   multiLine: true,
+/// );
+///
+/// const multilineText = '[00:13.37] This is a first message.\n'
+///     '[01:15.57] This is a second message.\n';
+///
+/// RegExpMatch regExpMatch = regExp.firstMatch(multilineText)!;
+/// print(regExpMatch.groupNames.join('-')); // hour-minute-second-Time-Message.
+/// final time = regExpMatch.namedGroup('Time'); // 00:13.37
+/// final hour = regExpMatch.namedGroup('hour'); // 00
+/// final minute = regExpMatch.namedGroup('minute'); // 13
+/// final second = regExpMatch.namedGroup('second'); // 37
+/// final message =
+///     regExpMatch.namedGroup('Message'); // This is a first message.
+/// final date = regExpMatch.namedGroup('Date'); // Undefined `Date`, throws.
+///
+/// Iterable<RegExpMatch> matches = regExp.allMatches(multilineText);
+/// for (final m in matches) {
+///   print(m.namedGroup('Time'));
+///   print(m.namedGroup('Message'));
+///   // 00:13.37
+///   // This is a first message.
+///   // 01:15.57
+///   // This is a second message.
+/// }
+/// ```
 @Since("2.3")
 abstract class RegExpMatch implements Match {
   /// The string matched by the group named [name].
