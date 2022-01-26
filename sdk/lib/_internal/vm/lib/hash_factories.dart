c05fe0bfd193653225f0374ba2e9cf2badb55c2d
Aske Simon Christensen <askesc@google.com>
2022-01-26 15:33:38 +0000

    [vm,dart2wasm] Add a placeholder patch file for map/set factories
    The relevant code will be moved to the new file after the Flutter patch
    file lists have been updated.
    
    TEST=ci
    Change-Id: I37bd1f7124029e35b81dc4913962f2707bc02781
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/229905
    Reviewed-by: Daco Harkes <dacoharkes@google.com>
    Commit-Queue: Aske Simon Christensen <askesc@google.com>

---
+++ sdk/lib/_internal/vm/lib/hash_factories.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This is a placeholder file which will shortly contain the LinkedhashMap and
+// LinkedHashSet patches from collection_patch.dart. The change is done in two
+// steps to ease rolling it into Flutter.
