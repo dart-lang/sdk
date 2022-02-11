da66e1f39aa2aad64a1c376e05d39d707de4346f
Joshua Litt <joshualitt@google.com>
2022-02-11 19:41:26 +0000

    [dart2js] Make info_visitor_test cleanup created test data.
    Change-Id: I8f70c5d751b670bde2d2b6f3fcc2c163722edac8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/232661
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Joshua Litt <joshualitt@google.com>

--- pkg/compiler/tool/kernel_visitor/test/info_visitor_test.dart
+++ pkg/compiler/tool/kernel_visitor/test/info_visitor_test.dart
@@ -1,45 +1,17 @@
 // Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import "package:kernel/kernel.dart";
-import "package:expect/expect.dart";
-import "package:expect/minitest.dart";
-import "../dart_html_metrics_visitor.dart";
-import "package:cli_util/cli_util.dart";
-import "package:path/path.dart" as path;
+
 import "dart:io";
 
-main() async {
-  // Compile Dill
-  var sdkPath = getSdkPath();
-  if (!sdkPath.contains("ReleaseX64"))
-    sdkPath = path.join(sdkPath, "ReleaseX64", "dart-sdk");
-  var scriptPath = Platform.script.path;
-  var pkgPath = path.dirname(
-      path.dirname(path.dirname(path.dirname(path.dirname(scriptPath)))));
-  var compilePath = path.canonicalize(
-      path.join(pkgPath, "front_end", "tool", "_fasta", "compile.dart"));
-  var testClassesPath = path
-      .canonicalize(path.join(path.dirname(scriptPath), "test_classes.dart"));
-  var ddcOutlinePath =
-      path.canonicalize(path.join(sdkPath, "lib", "_internal", "ddc_sdk.dill"));
-  var dillPath = path
-      .canonicalize(path.join(path.dirname(scriptPath), "test_classes.dill"));
+import "package:expect/expect.dart";
+import "package:expect/minitest.dart";
+import "package:kernel/kernel.dart";
+import "package:path/path.dart" as path;
 
-  await Process.run(path.join(sdkPath, "bin", "dart"), [
-    compilePath,
-    "--target=dartdevc",
-    "--platform=${ddcOutlinePath}",
-    "-o=${dillPath}",
-    testClassesPath
-  ]);
+import "../dart_html_metrics_visitor.dart";
 
-  // Dill compiled from test_classes.dart using ddc.
-  var component = loadComponentFromBinary(dillPath);
-  var visitor = MetricsVisitor(["file:${testClassesPath}"]);
-
-  component.accept(visitor);
-
+void runTests(MetricsVisitor visitor) {
   test("Class A does not call super", () {
     Expect.equals(visitor.classInfo["A"].invokesSuper, false);
   });
@@ -94,4 +66,43 @@ main() async {
     Expect.equals(
         visitor.classInfo["G"].notOverriddenMethods.contains("getValue"), true);
   });
+}
+
+void main() async {
+  // Compile Dill
+  var sdkPath = path.dirname(path.dirname(Platform.resolvedExecutable));
+  if (!sdkPath.contains("ReleaseX64"))
+    sdkPath = path.join(sdkPath, "ReleaseX64", "dart-sdk");
+  var scriptPath = Platform.script.path;
+  var pkgPath = path.dirname(
+      path.dirname(path.dirname(path.dirname(path.dirname(scriptPath)))));
+  var compilePath = path.canonicalize(
+      path.join(pkgPath, "front_end", "tool", "_fasta", "compile.dart"));
+  var testClassesPath = path
+      .canonicalize(path.join(path.dirname(scriptPath), "test_classes.dart"));
+  var ddcOutlinePath =
+      path.canonicalize(path.join(sdkPath, "lib", "_internal", "ddc_sdk.dill"));
+  var dillPath = path
+      .canonicalize(path.join(path.dirname(scriptPath), "test_classes.dill"));
+
+  await Process.run(path.join(sdkPath, "bin", "dart"), [
+    compilePath,
+    "--target=dartdevc",
+    "--platform=${ddcOutlinePath}",
+    "-o=${dillPath}",
+    testClassesPath
+  ]);
+
+  // Dill compiled from test_classes.dart using ddc.
+  var component = loadComponentFromBinary(dillPath);
+  var visitor = MetricsVisitor(["file:${testClassesPath}"]);
+
+  component.accept(visitor);
+
+  try {
+    runTests(visitor);
+  } finally {
+    // Cleanup.
+    File(dillPath).deleteSync();
+  }
 }
