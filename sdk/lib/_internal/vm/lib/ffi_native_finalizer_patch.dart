7c04f35043d52b348061a4af110e18928916c955
Daco Harkes <dacoharkes@google.com>
2022-02-18 12:35:01 +0000

    [vm] Placeholder patch files for `Finalizer` and `NativeFinalizer`
    Follow up CLs recognize classes in these patches in the VM.
    If the patch files are not already in Flutter's libraries.json and
    libraries.yaml, the G3 build will fail.
    
    We land empty patch files first, so that we can update Flutter's
    libraries files, before landing the VM referencing to things in these
    patch files.
    
    Change-Id: Ib1ed3bdbe5e69ab42b45114ac97d2e4ee423d996
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/233561
    Reviewed-by: Aske Simon Christensen <askesc@google.com>
    Commit-Queue: Daco Harkes <dacoharkes@google.com>

---
+++ sdk/lib/_internal/vm/lib/ffi_native_finalizer_patch.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// All imports must be in all FFI patch files to not depend on the order
+// the patches are applied.
+import 'dart:_internal';
+import 'dart:isolate';
+import 'dart:typed_data';
+
+// This is a placeholder file which will shortly contain a NativeFinalizer
+// implementation.
---
+++ sdk/lib/_internal/vm/lib/finalizer_patch.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// part of "core_patch.dart";
+
+// This is a placeholder file which will shortly contain a Finalizer
+// implementation.
