9db0718440d11d841fca92008c829abae2c3b509
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-02-23 18:38:48 -0500

    Improve description & example for avoid_print (dart-lang/linter#3241)
    * Improve description & example for avoid_print
    The existing message gave no hint on what to do *instead* of using
    print calls, so this gives an explanation and a few examples.
    
    * Update extended description
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>
    
    * Add Kristen to authors
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>

--- lib/src/rules/avoid_print.dart
+++ lib/src/rules/avoid_print.dart
@@ -14,10 +14,43 @@ const _desc = r'Avoid `print` calls in production code.';
 const _details = r'''
 **DO** avoid `print` calls in production code.
 
+For production code, consider using a logging framework.
+If you are using Flutter, you can use `debugPrint`
+or surround `print` calls with a check for `kDebugMode`
+
 **BAD:**
 ```dart
 void f(int x) {
   print('debug: $x');
+  ...
+}
+```
+
+
+**GOOD:**
+```dart
+void f(int x) {
+  debugPrint('debug: $x');
+  ...
+}
+```
+
+
+**GOOD:**
+```dart
+void f(int x) {
+  log('log: $x');
+  ...
+}
+```
+
+
+**GOOD:**
+```dart
+void f(int x) {
+  if (kDebugMode) {
+      print('debug: $x');
+  }
   ...
 }
 ```
