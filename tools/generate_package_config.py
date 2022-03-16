f2c623a98732d22be643a05cf1ccefd0b0590295
Devon Carew <devoncarew@google.com>
2022-03-16 08:22:33 +0000

    Turn the tools/generate_package_config.py script into a no-op.
    Change-Id: Idd1bdd9f0d9613b9898d81afd00f76273634ac26
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/237441
    Auto-Submit: Devon Carew <devoncarew@google.com>
    Reviewed-by: Alexander Thomas <athom@google.com>
    Commit-Queue: Alexander Thomas <athom@google.com>

--- tools/generate_package_config.py
+++ tools/generate_package_config.py
@@ -5,43 +5,17 @@
 
 # Invoke the `tools/generate_package_config.dart` script.
 
-import os
-import os.path
-import platform
-import subprocess
-import sys
+# NOTE(devoncarew): This script is currently a no-op in order to facilitate us
+# landing the package_config generation changes through flutter/engine. Once
+# generate_package_config.py exists in flutter/engine, we'll update that repo's
+# DEPS file to call this script, and then land the full package_config changes
+# in dart-lang/sdk.
 
 USE_PYTHON3 = True
 
 
-def is_windows():
-    os_id = platform.system()
-    return os_id == 'Windows'
-
-
-def checked_in_sdk_path():
-    tools_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(tools_dir, 'sdks', 'dart-sdk')
-
-
-def checked_in_sdk_executable():
-    name = 'dart'
-    if is_windows():
-        name = 'dart.exe'
-    return os.path.join(checked_in_sdk_path(), 'bin', name)
-
-
-def generate_package_config():
-    tools_dir = os.path.dirname(os.path.realpath(__file__))
-    process = subprocess.run([
-        checked_in_sdk_executable(),
-        os.path.join(tools_dir, 'generate_package_config.dart')
-    ])
-    return process.returncode
-
-
 def Main():
-    sys.exit(generate_package_config())
+    print('generate_package_config.py called')
 
 
 if __name__ == '__main__':
