59b7b8c8185d527f91fd42ba43895bc6e0090854
Ahmed Ashour <asashour@yahoo.com>
2022-03-25 16:58:51 +0000

    `create_constructor_super` to use super initializers
    Fixes #47979
    
    Change-Id: Ic6131d43b74a5a48ad901e56c0de474a03e2d552
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/238560
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/src/services/correction/fix/create_constructor_super_test.dart
+++ pkg/analysis_server/test/src/services/correction/fix/create_constructor_super_test.dart
@@ -12,6 +12,7 @@ import 'fix_processor.dart';
 void main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(CreateConstructorSuperTest);
+    defineReflectiveTests(CreateConstructorSuperWithoutSuperParametersTest);
   });
 }
 
@@ -42,18 +43,271 @@ class A {
 class B extends A {
   int existingField = 0;
 
-  B(int field) : super(field);
+  B(super.field);
+
+  void existingMethod() {}
+}
+''', matchFixMessage: 'Create constructor to call super.');
+  }
+
+  Future<void> test_importType() async {
+    addSource('$testPackageLibPath/a.dart', '''
+class A {}
+''');
+    addSource('$testPackageLibPath/b.dart', '''
+import 'package:test/a.dart';
+
+class B {
+  B(A a);
+}
+''');
+    await resolveTestCode('''
+import 'package:test/b.dart';
+
+class C extends B {
+}
+''');
+    await assertHasFix('''
+import 'package:test/b.dart';
+
+class C extends B {
+  C(super.a);
+}
+''');
+  }
+
+  Future<void> test_lint_sortConstructorsFirst() async {
+    createAnalysisOptionsFile(lints: [LintNames.sort_constructors_first]);
+    await resolveTestCode('''
+class A {
+  A(this.field);
+
+  int field;
+}
+class B extends A {
+  int existingField = 0;
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  A(this.field);
+
+  int field;
+}
+class B extends A {
+  B(super.field);
+
+  int existingField = 0;
+  void existingMethod() {}
+}
+''');
+  }
+
+  Future<void> test_namedConstructor() async {
+    await resolveTestCode('''
+class A {
+  A.named(p1, int p2);
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  A.named(p1, int p2);
+}
+class B extends A {
+  int existingField = 0;
+
+  B.named(super.p1, super.p2) : super.named();
+
+  void existingMethod() {}
+}
+''', matchFixMessage: 'Create constructor to call super.named()');
+  }
+
+  Future<void> test_namedOptionalParams() async {
+    await resolveTestCode('''
+class A {
+  A(p1, int p2, List<String> p3, {int? p4});
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  A(p1, int p2, List<String> p3, {int? p4});
+}
+class B extends A {
+  int existingField = 0;
+
+  B(super.p1, super.p2, super.p3);
 
   void existingMethod() {}
 }
 ''');
   }
 
+  Future<void> test_namedRequiredParams() async {
+    await resolveTestCode('''
+class A {
+  A(p1, int p2, List<String> p3, {required int p4, required int p5});
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  A(p1, int p2, List<String> p3, {required int p4, required int p5});
+}
+class B extends A {
+  int existingField = 0;
+
+  B(super.p1, super.p2, super.p3, {required super.p4, required super.p5});
+
+  void existingMethod() {}
+}
+''');
+  }
+
+  Future<void> test_optional() async {
+    await resolveTestCode('''
+class A {
+  A(p1, int p2, List<String> p3, [int p4 = 0]);
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  A(p1, int p2, List<String> p3, [int p4 = 0]);
+}
+class B extends A {
+  int existingField = 0;
+
+  B(super.p1, super.p2, super.p3);
+
+  void existingMethod() {}
+}
+''');
+  }
+
+  Future<void> test_private() async {
+    await resolveTestCode('''
+class A {
+  A._named(p);
+}
+class B extends A {
+}
+''');
+    await assertNoFix();
+  }
+
+  Future<void> test_typeArgument() async {
+    await resolveTestCode('''
+class C<T> {
+  final T x;
+  C(this.x);
+}
+class D extends C<int> {
+}
+''');
+    await assertHasFix('''
+class C<T> {
+  final T x;
+  C(this.x);
+}
+class D extends C<int> {
+  D(super.x);
+}
+''');
+  }
+
+  Future<void> test_underscore() async {
+    await resolveTestCode('''
+class A {
+  int _;
+  A(this._);
+  int get field => _;
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  int _;
+  A(this._);
+  int get field => _;
+}
+class B extends A {
+  int existingField = 0;
+
+  B(super._);
+
+  void existingMethod() {}
+}
+''');
+  }
+}
+
+@reflectiveTest
+class CreateConstructorSuperWithoutSuperParametersTest
+    extends FixProcessorTest {
+  @override
+  FixKind get kind => DartFixKind.CREATE_CONSTRUCTOR_SUPER;
+
+  @override
+  String get testPackageLanguageVersion => '2.16';
+
+  Future<void> test_fieldInitializer() async {
+    await resolveTestCode('''
+class A {
+  int _field;
+  A(this._field);
+  int get field => _field;
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  int _field;
+  A(this._field);
+  int get field => _field;
+}
+class B extends A {
+  int existingField = 0;
+
+  B(int field) : super(field);
+
+  void existingMethod() {}
+}
+''', matchFixMessage: 'Create constructor to call super(...)');
+  }
+
   Future<void> test_importType() async {
-    addSource('$testPackageLibPath/a.dart', r'''
+    addSource('$testPackageLibPath/a.dart', '''
 class A {}
 ''');
-    addSource('$testPackageLibPath/b.dart', r'''
+    addSource('$testPackageLibPath/b.dart', '''
 import 'package:test/a.dart';
 
 class B {
@@ -126,7 +380,7 @@ class B extends A {
 
   void existingMethod() {}
 }
-''');
+''', matchFixMessage: 'Create constructor to call super.named(...)');
   }
 
   Future<void> test_namedOptionalParams() async {
@@ -222,7 +476,8 @@ class C<T> {
   C(this.x);
 }
 class D extends C<int> {
-}''');
+}
+''');
     await assertHasFix('''
 class C<T> {
   final T x;
@@ -230,6 +485,36 @@ class C<T> {
 }
 class D extends C<int> {
   D(int x) : super(x);
-}''');
+}
+''');
+  }
+
+  Future<void> test_underscore() async {
+    await resolveTestCode('''
+class A {
+  int _;
+  A(this._);
+  int get field => _;
+}
+class B extends A {
+  int existingField = 0;
+
+  void existingMethod() {}
+}
+''');
+    await assertHasFix('''
+class A {
+  int _;
+  A(this._);
+  int get field => _;
+}
+class B extends A {
+  int existingField = 0;
+
+  B(int _) : super(_);
+
+  void existingMethod() {}
+}
+''');
   }
 }
