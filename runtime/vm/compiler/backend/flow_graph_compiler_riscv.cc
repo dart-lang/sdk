95d1ae911d5272f5911f75d8c80a74aaffe7863b
Tess Strickland <sstrickl@google.com>
2022-03-29 08:03:35 +0000

    [vm] Reduce class id reloads in TTSes when possible.
    Now GenerateCidRangesCheck in the flow graph compiler returns
    whether the class id register was clobbered, so users that
    make multiple calls to it for the same cid know whether the cid
    needs reloading after the generated check.
    
    Also unifies FlowGraphCompiler::EmitTestAndCheckCid across
    architectures, since the non-Intel versions would have
    always forced reloading.
    
    TEST=vm/cc/TTS
    
    Change-Id: I08835dc97ba457b7dc8dfa51206b4741a607db22
    Cq-Include-Trybots: luci.dart.try:vm-kernel-linux-release-simarm-try,vm-kernel-linux-release-simarm64-try,vm-kernel-mac-release-arm64-try,vm-kernel-precomp-linux-release-simarm_x64-try,vm-kernel-nnbd-mac-release-arm64-try,vm-kernel-nnbd-linux-release-simarm64-try,vm-kernel-nnbd-linux-release-simarm-try,vm-kernel-precomp-nnbd-linux-release-simarm_x64-try,vm-precomp-ffi-qemu-linux-release-arm-try,vm-kernel-linux-debug-simriscv64-try,vm-kernel-precomp-linux-debug-simriscv64-try
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/238583
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Tess Strickland <sstrickl@google.com>

--- runtime/vm/compiler/backend/flow_graph_compiler_riscv.cc
+++ runtime/vm/compiler/backend/flow_graph_compiler_riscv.cc
@@ -771,36 +771,6 @@ void FlowGraphCompiler::EmitTestAndCallLoadCid(Register class_id_reg) {
   __ LoadClassId(class_id_reg, A0);
 }
 
-#undef __
-#define __ assembler->
-
-int FlowGraphCompiler::EmitTestAndCallCheckCid(compiler::Assembler* assembler,
-                                               compiler::Label* label,
-                                               Register class_id_reg,
-                                               const CidRangeValue& range,
-                                               int bias,
-                                               bool jump_on_miss) {
-  const intptr_t cid_start = range.cid_start;
-  if (range.IsSingleCid()) {
-    __ AddImmediate(class_id_reg, class_id_reg, bias - cid_start);
-    if (jump_on_miss) {
-      __ bnez(class_id_reg, label);
-    } else {
-      __ beqz(class_id_reg, label);
-    }
-    bias = cid_start;
-  } else {
-    __ AddImmediate(class_id_reg, class_id_reg, bias - cid_start);
-    bias = cid_start;
-    __ CompareImmediate(class_id_reg, range.Extent());
-    __ BranchIf(jump_on_miss ? UNSIGNED_GREATER : UNSIGNED_LESS_EQUAL, label);
-  }
-  return bias;
-}
-
-#undef __
-#define __ assembler()->
-
 void FlowGraphCompiler::EmitMove(Location destination,
                                  Location source,
                                  TemporaryRegisterAllocator* allocator) {
