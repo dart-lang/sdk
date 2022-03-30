d1e23b856fe0fee7f4a30c3cc70aeffb2ee1d704
Stephen Adams <sra@google.com>
2022-03-30 23:38:28 +0000

    [js_ast] Clamp unmigrated files to @dart = 2.10
    Change-Id: Ic1d152cc2f86ede6de419b52a12a9991376912c6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/239582
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- pkg/js_ast/test/printer_callback_test.dart
+++ pkg/js_ast/test/printer_callback_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.10
+
 // Note: This test relies on LF line endings in the source file.
 
 // Test that JS printer callbacks occur when expected.
