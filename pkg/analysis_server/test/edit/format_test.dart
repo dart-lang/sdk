6207471a83146a893a8346f7342729071837b52b
Brian Wilkerson <brianwilkerson@google.com>
2022-03-30 16:08:00 +0000

    Convert two sync handlers from the edit domain
    Change-Id: I61e741530cb7702b2e0961464e6d13cf82043387
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/239366
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/edit/format_test.dart
+++ pkg/analysis_server/test/edit/format_test.dart
@@ -4,7 +4,6 @@
 
 import 'package:analysis_server/protocol/protocol_generated.dart';
 import 'package:analysis_server/src/edit/edit_domain.dart';
-import 'package:analysis_server/src/utilities/progress.dart';
 import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
@@ -26,44 +25,42 @@ class FormatTest extends AbstractAnalysisTest {
     handler = EditDomainHandler(server);
   }
 
-  Future test_format_longLine() {
+  Future<void> test_format_longLine() async {
     var content = '''
 fun(firstParam, secondParam, thirdParam, fourthParam) {
   if (firstParam.noNull && secondParam.noNull && thirdParam.noNull && fourthParam.noNull) {}
 }
 ''';
     addTestFile(content);
-    return waitForTasksFinished().then((_) {
-      var formatResult = _formatAt(0, 3, lineLength: 100);
+    await waitForTasksFinished();
+    var formatResult = await _formatAt(0, 3, lineLength: 100);
 
-      expect(formatResult.edits, isNotNull);
-      expect(formatResult.edits, hasLength(0));
+    expect(formatResult.edits, isNotNull);
+    expect(formatResult.edits, hasLength(0));
 
-      expect(formatResult.selectionOffset, equals(0));
-      expect(formatResult.selectionLength, equals(3));
-    });
+    expect(formatResult.selectionOffset, equals(0));
+    expect(formatResult.selectionLength, equals(3));
   }
 
-  Future test_format_noOp() {
+  Future<void> test_format_noOp() async {
     // Already formatted source
     addTestFile('''
 main() {
   int x = 3;
 }
 ''');
-    return waitForTasksFinished().then((_) {
-      var formatResult = _formatAt(0, 3);
-      expect(formatResult.edits, isNotNull);
-      expect(formatResult.edits, hasLength(0));
-    });
+    await waitForTasksFinished();
+    var formatResult = await _formatAt(0, 3);
+    expect(formatResult.edits, isNotNull);
+    expect(formatResult.edits, hasLength(0));
   }
 
-  Future test_format_noSelection() async {
+  Future<void> test_format_noSelection() async {
     addTestFile('''
 main() { int x = 3; }
 ''');
     await waitForTasksFinished();
-    var formatResult = _formatAt(0, 0);
+    var formatResult = await _formatAt(0, 0);
 
     expect(formatResult.edits, isNotNull);
     expect(formatResult.edits, hasLength(1));
@@ -78,44 +75,42 @@ main() {
     expect(formatResult.selectionLength, equals(0));
   }
 
-  Future test_format_simple() {
+  Future<void> test_format_simple() async {
     addTestFile('''
 main() { int x = 3; }
 ''');
-    return waitForTasksFinished().then((_) {
-      var formatResult = _formatAt(0, 3);
+    await waitForTasksFinished();
+    var formatResult = await _formatAt(0, 3);
 
-      expect(formatResult.edits, isNotNull);
-      expect(formatResult.edits, hasLength(1));
+    expect(formatResult.edits, isNotNull);
+    expect(formatResult.edits, hasLength(1));
 
-      var edit = formatResult.edits[0];
-      expect(edit.replacement, equals('''
+    var edit = formatResult.edits[0];
+    expect(edit.replacement, equals('''
 main() {
   int x = 3;
 }
 '''));
-      expect(formatResult.selectionOffset, equals(0));
-      expect(formatResult.selectionLength, equals(3));
-    });
+    expect(formatResult.selectionOffset, equals(0));
+    expect(formatResult.selectionLength, equals(3));
   }
 
-  Future test_format_withErrors() {
+  Future<void> test_format_withErrors() async {
     addTestFile('''
 main() { int x =
 ''');
-    return waitForTasksFinished().then((_) {
-      var request = EditFormatParams(testFile, 0, 3).toRequest('0');
-      var response = handler.handleRequest(request, NotCancelableToken());
-      expect(response, isResponseFailure('0'));
-    });
+    await waitForTasksFinished();
+    var request = EditFormatParams(testFile, 0, 3).toRequest('0');
+    var response = await waitResponse(request);
+    expect(response, isResponseFailure('0'));
   }
 
-  EditFormatResult _formatAt(int selectionOffset, int selectionLength,
-      {int? lineLength}) {
+  Future<EditFormatResult> _formatAt(int selectionOffset, int selectionLength,
+      {int? lineLength}) async {
     var request = EditFormatParams(testFile, selectionOffset, selectionLength,
             lineLength: lineLength)
         .toRequest('0');
-    var response = handleSuccessfulRequest(request);
+    var response = await waitResponse(request);
     return EditFormatResult.fromResponse(response);
   }
 }
