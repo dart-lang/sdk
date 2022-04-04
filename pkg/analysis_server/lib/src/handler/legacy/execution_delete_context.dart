6d543de174a229597184ee2404b7d53e328d731c
Brian Wilkerson <brianwilkerson@google.com>
2022-04-04 19:53:02 +0000

    Split out handlers for the execution domain
    The handlers need to share data, which I extracted from the domain
    class. Most of the remaining code that needs to be extracted into
    handlers has a similar requirement, so I'm interested in your thoughts
    about the general approach.
    
    Change-Id: Ib88b1453d0ca6db0f289ebd7e861385ed95bfcc8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/239842
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/analysis_server/lib/src/handler/legacy/execution_delete_context.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:analysis_server/protocol/protocol.dart';
+import 'package:analysis_server/protocol/protocol_generated.dart';
+import 'package:analysis_server/src/analysis_server.dart';
+import 'package:analysis_server/src/handler/legacy/legacy_handler.dart';
+import 'package:analysis_server/src/services/execution/execution_context.dart';
+import 'package:analysis_server/src/utilities/progress.dart';
+
+/// The handler for the `execution.deleteContext` request.
+class ExecutionDeleteContextHandler extends LegacyHandler {
+  /// The context used by the execution domain handlers.
+  final ExecutionContext executionContext;
+
+  /// Initialize a newly created handler to be able to service requests for the
+  /// [server].
+  ExecutionDeleteContextHandler(AnalysisServer server, Request request,
+      CancellationToken cancellationToken, this.executionContext)
+      : super(server, request, cancellationToken);
+
+  @override
+  Future<void> handle() async {
+    var contextId = ExecutionDeleteContextParams.fromRequest(request).id;
+    executionContext.contextMap.remove(contextId);
+    sendResult(ExecutionDeleteContextResult());
+  }
+}
---
+++ pkg/analysis_server/lib/src/handler/legacy/execution_get_suggestions.dart
@@ -0,0 +1,48 @@
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:analysis_server/protocol/protocol.dart';
+import 'package:analysis_server/protocol/protocol_generated.dart';
+import 'package:analysis_server/src/analysis_server.dart';
+import 'package:analysis_server/src/handler/legacy/legacy_handler.dart';
+import 'package:analysis_server/src/utilities/progress.dart';
+import 'package:analyzer_plugin/protocol/protocol_common.dart';
+
+/// The handler for the `execution.getSuggestions` request.
+class ExecutionGetSuggestionsHandler extends LegacyHandler {
+  /// Initialize a newly created handler to be able to service requests for the
+  /// [server].
+  ExecutionGetSuggestionsHandler(AnalysisServer server, Request request,
+      CancellationToken cancellationToken)
+      : super(server, request, cancellationToken);
+
+  @override
+  Future<void> handle() async {
+//    var params = new ExecutionGetSuggestionsParams.fromRequest(request);
+//    var computer = new RuntimeCompletionComputer(
+//        server.resourceProvider,
+//        server.fileContentOverlay,
+//        server.getAnalysisDriver(params.contextFile),
+//        params.code,
+//        params.offset,
+//        params.contextFile,
+//        params.contextOffset,
+//        params.variables,
+//        params.expressions);
+//    RuntimeCompletionResult completionResult = await computer.compute();
+//
+//    // Send the response.
+//    var result = new ExecutionGetSuggestionsResult(
+//        suggestions: completionResult.suggestions,
+//        expressions: completionResult.expressions);
+    // TODO(brianwilkerson) Re-enable this functionality after implementing a
+    // way of computing suggestions that is compatible with AnalysisSession.
+    var result = ExecutionGetSuggestionsResult(
+        suggestions: <CompletionSuggestion>[],
+        expressions: <RuntimeCompletionExpression>[]);
+    sendResponse(result.toResponse(request.id));
+  }
+}
