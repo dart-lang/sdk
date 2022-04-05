9ac50f35fe6ffe1b70a41e228b8fce5524499f44
Stephen Adams <sra@google.com>
2022-04-05 01:38:03 +0000

    [js_ast] language version 2.16
    Change-Id: I421fe264e7318bf774d25cce463e858e9323872a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/240145
    Reviewed-by: Nicholas Shahan <nshahan@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- pkg/js_ast/lib/src/printer.dart
+++ pkg/js_ast/lib/src/printer.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.15
-
 library js_ast.printer;
 
 import 'characters.dart' as char_codes;
--- pkg/js_ast/lib/src/strings.dart
+++ pkg/js_ast/lib/src/strings.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.15
-
 // Utilities for converting between JavaScript source-code Strings and the
 // String value they represent.
 
