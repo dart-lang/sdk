30413a1c5fb4ea399abda158123342002faa89d6
Stephen Adams <sra@google.com>
2022-04-09 04:15:20 +0000

    [dart2js] Require SDK 2.16
    Change-Id: I1594cb85c40feae1508ec31a5b4740eb6ca72816
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/240600
    Reviewed-by: Joshua Litt <joshualitt@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- pkg/compiler/lib/src/util/testing.dart
+++ pkg/compiler/lib/src/util/testing.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.15
-
 /// Marks its argument as live and prevents tree-shaking.
 ///
 /// This is more hermetic than using `package:expect` or `print`. This function
