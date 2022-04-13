46bc05240d826aad3423678b0ea19719d50970ef
Konstantin Shcheglov <scheglov@google.com>
2022-04-13 20:51:33 +0000

    Require '@dart = 2.15' for binary_protocol.dart
    Otherwise it fails internally, because the package is not null safe
    in google3, and cannot be made because it has dependencies
    (compiler and dev_compiler) that are not null safe. But the binary
    protocol only talks to null safe code, so can be upgraded to the
    newer language version.
    
    Bug: https://buganizer.corp.google.com/issues/229124206
    Change-Id: I215799de7d40a673c4c11d4d3a9e1c936b979df0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/241143
    Reviewed-by: Jake Macdonald <jakemac@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/frontend_server/lib/src/binary_protocol.dart
+++ pkg/frontend_server/lib/src/binary_protocol.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.15
 import 'dart:async';
 import 'dart:io' as io;
 import 'dart:typed_data';
