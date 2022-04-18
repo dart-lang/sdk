26043c95cf6bb8c509ba670111426dfc87e4f6c7
Devon Carew <devoncarew@google.com>
2022-04-18 22:09:50 +0000

    Move pkg/scrape to package:lints.
    Change-Id: I56eb8d0410ab529a434c540a67028f8ebe441c4d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/240913
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Devon Carew <devoncarew@google.com>

--- pkg/scrape/lib/scrape.dart
+++ pkg/scrape/lib/scrape.dart
@@ -1,6 +1,9 @@
 // Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
+// ignore_for_file: implementation_imports
+
 import 'dart:io';
 import 'dart:math' as math;
 
