34ebfc6594bf6efb187c0ffd9badc9e5c4bdbfb1
Mayank Patke <fishythefish@google.com>
2022-05-10 22:56:23 +0000

    [dart2js] Add regression test for https://dart-review.googlesource.com/c/sdk/+/225320
    Change-Id: Iff348b9ce13ea249b8c63f8c0952cd446ad5cb7d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/243528
    Commit-Queue: Mayank Patke <fishythefish@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

---
+++ pkg/compiler/test/inference/data/late_field.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:compiler/src/util/testing.dart';
+
+/*member: Foo.:[exact=Foo]*/
+class Foo {
+  /*member: Foo._#Foo#x:[sentinel|exact=JSUInt31]*/
+  /*member: Foo.x:[exact=JSUInt31]*/
+  late int /*[exact=Foo]*/ /*update: [exact=Foo]*/ x = 42;
+}
+
+/*member: main:[null]*/
+void main() {
+  makeLive(test(Foo()));
+}
+
+@pragma('dart2js:noInline')
+/*member: test:[exact=JSUInt31]*/
+int test(Foo /*[exact=Foo]*/ foo) => foo. /*[exact=Foo]*/ x;
