f5f26c9c61ae2f86a2b27d3ab387cdf60c757fab
Mark Zhou <markzipan@google.com>
2022-05-10 02:54:34 +0000

    Revert "[dart2js] Exporting as package: to allow non-SDK usage"
    This reverts commit 8aeeffb84825fa91c8b270afe2cdf77fad1cf1d5.
    
    Reason for revert: Bad fix. Doesn't resolve underlying bug
    
    Original change's description:
    > [dart2js] Exporting as package: to allow non-SDK usage
    >
    > Change-Id: I0235f524904a1a4c54c83161eb51ab0277473d1e
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/244180
    > Reviewed-by: Joshua Litt <joshualitt@google.com>
    > Commit-Queue: Mark Zhou <markzipan@google.com>
    
    TBR=markzipan@google.com,joshualitt@google.com
    
    Change-Id: I0e2f8ff70c86ff1e69608f715fa5545419e63314
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/244182
    Reviewed-by: Mark Zhou <markzipan@google.com>
    Commit-Queue: Sigmund Cherem <sigmund@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Auto-Submit: Mark Zhou <markzipan@google.com>

--- pkg/dart2js_runtime_metrics/lib/_startup_metrics_dart2js.dart
+++ pkg/dart2js_runtime_metrics/lib/_startup_metrics_dart2js.dart
@@ -2,5 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'package:dart2js_runtime_metrics/startup_metrics.dart'
-    show startupMetrics;
+export 'dart:_dart2js_runtime_metrics' show startupMetrics;
