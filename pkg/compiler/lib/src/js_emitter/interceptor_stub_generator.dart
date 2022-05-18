e9ba00e518bfb9b0586537cbf5540b31c757c6c1
Stephen Adams <sra@google.com>
2022-05-18 16:57:13 +0000

    [dart2js] Migrate js_emitter/constant_ordering.dart
    Change-Id: I2270e470ca03b7abb750f56fe319b35412b68551
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/245085
    Commit-Queue: Stephen Adams <sra@google.com>
    Reviewed-by: Joshua Litt <joshualitt@google.com>

--- pkg/compiler/lib/src/js_emitter/interceptor_stub_generator.dart
+++ pkg/compiler/lib/src/js_emitter/interceptor_stub_generator.dart
@@ -420,7 +420,7 @@ class InterceptorStubGenerator {
     CustomElementsCodegenAnalysis analysis = _customElementsCodegenAnalysis;
     if (!analysis.needsTable) return null;
 
-    List<jsAst.Expression> elements = [];
+    List<jsAst.Expression /*!*/ > elements = [];
     Iterable<ConstantValue> constants =
         _codegenWorld.getConstantsForEmission(_emitter.compareConstants);
     for (ConstantValue constant in constants) {
