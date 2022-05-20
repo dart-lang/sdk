b83b89a9005f2ae1abab21ad2fa8ad3dab23875d
Martin Kustermann <kustermann@google.com>
2022-05-20 08:16:41 +0000

    [vm/concurrency] Add number of objects / bytes copied to timeline events for inter-isolate messages
    Closes https://github.com/dart-lang/sdk/issues/48591
    
    TEST=vm/dart{,_2}/isolates/fast_object_copy_timeline_test
    
    Change-Id: I1de3a6f0d8a31450e45f689e0d67358285204a71
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/245167
    Commit-Queue: Martin Kustermann <kustermann@google.com>
    Reviewed-by: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Alexander Aprelev <aam@google.com>

--- runtime/tests/vm/dart/timeline_recorder_file_test.dart
+++ runtime/tests/vm/dart/timeline_recorder_file_test.dart
@@ -2,13 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "dart:io";
-import "dart:convert";
 import "dart:developer";
 
-import "package:path/path.dart" as path;
-
-import "snapshot_test_helper.dart";
+import "timeline_utils.dart";
 
 main(List<String> args) async {
   if (const bool.fromEnvironment("dart.vm.product")) {
@@ -21,48 +17,20 @@ main(List<String> args) async {
     return;
   }
 
-  await withTempDir((String tmp) async {
-    final String timelinePath = path.join(tmp, "timeline.json");
-    final p = await Process.run(Platform.executable, [
-      ...Platform.executableArguments,
-      "--trace_timeline",
-      "--timeline_recorder=file:$timelinePath",
-      "--timeline_streams=VM,Isolate,GC,Compiler",
-      Platform.script.toFilePath(),
-      "--child"
-    ]);
-    print(p.stdout);
-    print(p.stderr);
-    if (p.exitCode != 0) {
-      throw "Child process failed: ${p.exitCode}";
-    }
-    // On Android, --trace_timeline goes to syslog instead of stderr.
-    if (!Platform.isAndroid) {
-      if (!p.stderr.contains("Using the File timeline recorder")) {
-        throw "Failed to select file recorder";
-      }
-    }
+  final timelineEvents =
+      await runAndCollectTimeline('VM,Isolate,GC,Compiler', ['--child']);
 
-    final timeline = jsonDecode(await new File(timelinePath).readAsString());
-    if (timeline is! List) throw "Timeline should be a JSON list";
-    print("${timeline.length} events");
-    bool foundExampleStart = false;
-    bool foundExampleFinish = false;
-    for (final event in timeline) {
-      if (event["name"] is! String) throw "Event missing name";
-      if (event["cat"] is! String) throw "Event missing category";
-      if (event["tid"] is! int) throw "Event missing thread";
-      if (event["pid"] is! int) throw "Event missing process";
-      if (event["ph"] is! String) throw "Event missing type";
-      if ((event["name"] == "TestEvent") && (event["ph"] == "B")) {
-        foundExampleStart = true;
-      }
-      if ((event["name"] == "TestEvent") && (event["ph"] == "E")) {
-        foundExampleFinish = true;
-      }
+  bool foundExampleStart = false;
+  bool foundExampleFinish = false;
+  for (final event in timelineEvents) {
+    if (event.name == "TestEvent" && event.ph == "B") {
+      foundExampleStart = true;
     }
+    if (event.name == "TestEvent" && event.ph == "E") {
+      foundExampleFinish = true;
+    }
+  }
 
-    if (foundExampleStart) throw "Missing test start event";
-    if (foundExampleFinish) throw "Missing test finish event";
-  });
+  if (foundExampleStart) throw "Missing test start event";
+  if (foundExampleFinish) throw "Missing test finish event";
 }
