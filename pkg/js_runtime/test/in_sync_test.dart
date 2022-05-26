4fc7567c22bc07439d4bf3ab942a06e23a6c5544
Nicholas Shahan <nshahan@google.com>
2022-05-26 20:19:09 +0000

    [web] Cleanup redundant language version comments
    - Files in the SDK are already opted into null safety.
    - Files in the packages are already opted into null safety via
      the pubspecs.
    - in_sync_test.dart was opted out but required no changes to
      migrate.
    
    Change-Id: Ic9334824de1b22c879e962f488e1fa8021017710
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/246058
    Reviewed-by: Mayank Patke <fishythefish@google.com>
    Commit-Queue: Nicholas Shahan <nshahan@google.com>

--- pkg/js_runtime/test/in_sync_test.dart
+++ pkg/js_runtime/test/in_sync_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart = 2.9
-
 /// Test to verify that this package is in-sync with dart2js runtime libraries.
 import 'dart:io';
 
