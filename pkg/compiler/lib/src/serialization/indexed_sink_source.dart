cb2d5c972f8c9fe3b7f722b7f55f6191896a91bf
Nate Biggs <natebiggs@google.com>
2022-06-06 19:11:21 +0000

    [dart2js] Refactor Indexed(Sink/Source) to OrderedIndexed(Sink/Source)
    - Rename to OrderedIndexed(Sink/Source)
    - Add an interface to be shared with UnorderedIndexed(Sink/Source).
    - Move 'reshape' logic into subclass.
    
    Change-Id: I1f891aab961bde6685d77af342729ed6b5f31260
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/247400
    Reviewed-by: Joshua Litt <joshualitt@google.com>
    Commit-Queue: Nate Biggs <natebiggs@google.com>

--- pkg/compiler/lib/src/serialization/indexed_sink_source.dart
+++ pkg/compiler/lib/src/serialization/indexed_sink_source.dart
@@ -5,23 +5,43 @@
 import 'data_sink.dart';
 import 'data_source.dart';
 
+abstract class IndexedSource<E> {
+  E? read(E readValue());
+
+  /// Reshapes the cache to a [Map<E, int>] using [_getValue] if provided or
+  /// leaving the cache entry as is otherwise.
+  Map<T, int> reshape<T>([T Function(E? value)? getValue]);
+}
+
+abstract class IndexedSink<E> {
+  void write(E value, void writeValue(E value));
+}
+
 /// Data sink helper that canonicalizes [E?] values using indices.
-class IndexedSink<E extends Object> {
+///
+/// Writes a list index in place of already indexed values. This list index
+/// is the order in which we discover the indexable elements. On deserialization
+/// the indexable elements but be visited in the same order they were seen here
+/// so that the indices are maintained. Since the read order is assumed to be
+/// consistent, the actual data is written at the first occurrence of the
+/// indexable element.
+class OrderedIndexedSink<E extends Object> implements IndexedSink<E> {
   final DataSink _sink;
   final Map<E?, int> cache;
 
-  IndexedSink._(this._sink, this.cache);
+  OrderedIndexedSink._(this._sink, this.cache);
 
-  factory IndexedSink(DataSink sink, {Map<E?, int>? cache}) {
+  factory OrderedIndexedSink(DataSink sink, {Map<E?, int>? cache}) {
     // [cache] slot 0 is pre-allocated to `null`.
     cache ??= {null: 0};
-    return IndexedSink._(sink, cache);
+    return OrderedIndexedSink._(sink, cache);
   }
 
   /// Write a reference to [value] to the data sink.
   ///
   /// If [value] has not been canonicalized yet, [writeValue] is called to
   /// serialize the [value] itself.
+  @override
   void write(E? value, void writeValue(E value)) {
     const int pending = -1;
     int? index = cache[value];
@@ -40,22 +60,29 @@ class IndexedSink<E extends Object> {
 }
 
 /// Data source helper reads canonicalized [E?] values through indices.
-class IndexedSource<E extends Object> {
+///
+/// Reads indexable elements treating their read order as their index. Since the
+/// read order is consistent with the write order, when a new index is
+/// discovered we assume the data is written immediately after. Subsequent
+/// occurrences of that index then refer to the same value. Indices will appear
+/// in ascending order.
+class OrderedIndexedSource<E extends Object> implements IndexedSource<E> {
   final DataSource _source;
   final List<E?> cache;
 
-  IndexedSource._(this._source, this.cache);
+  OrderedIndexedSource._(this._source, this.cache);
 
-  factory IndexedSource(DataSource source, {List<E?>? cache}) {
+  factory OrderedIndexedSource(DataSource source, {List<E?>? cache}) {
     // [cache] slot 0 is pre-allocated to `null`.
     cache ??= [null];
-    return IndexedSource._(source, cache);
+    return OrderedIndexedSource._(source, cache);
   }
 
   /// Reads a reference to an [E] value from the data source.
   ///
   /// If the value hasn't yet been read, [readValue] is called to deserialize
   /// the value itself.
+  @override
   E? read(E readValue()) {
     int index = _source.readInt();
     if (index >= cache.length) {
@@ -71,5 +98,15 @@ class IndexedSource<E extends Object> {
       }
       return value;
     }
+  }
+
+  @override
+  Map<T, int> reshape<T>([T Function(E? value)? getValue]) {
+    var newCache = <T, int>{};
+    for (int i = 0; i < cache.length; i++) {
+      final newKey = getValue == null ? cache[i] as T : getValue(cache[i]);
+      newCache[newKey] = i;
+    }
+    return newCache;
   }
 }
