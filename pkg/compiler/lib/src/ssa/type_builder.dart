1c442cf9f0830f84e0cf0437b883f53767c5bca6
Stephen Adams <sra@google.com>
2022-06-13 16:53:57 +0000

    [dart2js] Move ClassTypeVariableAccess to own file
    Moving this enum to its own file breaks a dependency of the element
    maps on SSA.
    
    Change-Id: If66d1ddf0af6ccdc5380b4f65d5e447e3cebd8de
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/248081
    Reviewed-by: Nate Biggs <natebiggs@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- pkg/compiler/lib/src/ssa/type_builder.dart
+++ pkg/compiler/lib/src/ssa/type_builder.dart
@@ -9,33 +9,12 @@ import 'nodes.dart';
 import '../elements/entities.dart';
 import '../elements/types.dart';
 import '../inferrer/abstract_value_domain.dart';
+import '../js_model/class_type_variable_access.dart';
 import '../js_model/type_recipe.dart';
 import '../io/source_information.dart';
 import '../options.dart';
 import '../universe/use.dart' show TypeUse;
 import '../world.dart';
-
-/// Enum that defines how a member has access to the current type variables.
-enum ClassTypeVariableAccess {
-  /// The member has no access to type variables.
-  none,
-
-  /// Type variables are accessible as a property on `this`.
-  property,
-
-  /// Type variables are accessible as parameters in the current context.
-  parameter,
-
-  /// If the current context is a generative constructor, type variables are
-  /// accessible as parameters, otherwise type variables are accessible as
-  /// a property on `this`.
-  ///
-  /// This is used for instance fields whose initializers are executed in the
-  /// constructors.
-  // TODO(johnniwinther): Avoid the need for this by adding a field-setter
-  // to the J-model.
-  instanceField,
-}
 
 /// Functions to insert type checking, coercion, and instruction insertion
 /// depending on the environment for dart code.
