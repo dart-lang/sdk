93a4247af3e740f6cec9b9ace28b892f1514a11a
Ryan Macnak <rmacnak@google.com>
2022-06-27 18:56:12 +0000

    Reapply "Account for @pragma("vm:entry-point") creating additional "root" libraries when partitioning the program into loading units."
    Weaken assertion in gen_snapshot requiring all libraries to have a loading unit as there can still be unreachable libraries:
      - Google3 and Fuchsia will compile all the sources in a package to a single dill file, then present multiple input dill files to the AOT compilation. Since the set of libraries was derived from package membership instead of imports, many can be unreachable.
      - When the root library's main comes from an export, the frontend's representation will incorrectly report the library containing main as the root library and the true root library may be unreachable from it.
    
    Instead, assert only that surviving compiled code is assigned a loading unit.
    
    TEST=gallery
    Bug: https://github.com/flutter/gallery/issues/545
    Bug: https://github.com/dart-lang/sdk/issues/49325
    Bug: https://github.com/dart-lang/sdk/issues/41974
    Bug: b/237016312
    Change-Id: Ia52563a6f517308d041368be11dcc85270f19acc
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/249724
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>

---
+++ tests/language/deferred/exported_main_test.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+export "exported_main_lib.dart";
+
+@pragma("vm:entry-point")
+foo() {
+  // The frontend will not recognize that this is the root library. Though this
+  // library is not reachable from what the frontend considers the root library,
+  // the entry point pragma will still cause this function to compiled by
+  // gen_snaption, so it is important that this library is assigned to a
+  // loading unit.
+}
