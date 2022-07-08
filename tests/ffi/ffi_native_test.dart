aa6abbf464f740f06ba97ad08068a3806cf49b36
Daco Harkes <dacoharkes@google.com>
2022-07-08 12:36:09 +0000

    [cfe/ffi] Transform `FfiNative`s in a single pass
    Before this CL, `FfiNative`s were first transformed to `asFunction`
    calls, which were then immediately transformed to `_asFunctionInternal`
    calls.
    
    This caused the the static checks to be done in two steps, the second
    step happening after the first transform. It is cleaner to first do all
    checks.
    
    This refactoring enables implementing `_asFunctionInternal` variants
    for `FfiNative`s that don't use a `Pointer` for the address.
    
    Besides the transform change, this CL
    - moves shared logic over to pkg/vm/lib/transformations/ffi/common.dart,
    - splits up the ffi-native tests in to positive and negative tests, and
    - adds negative tests for mismatches between Dart and native types.
    
    These new tests do _not yet_ pass on the analyzer. This is tracked in:
    https://github.com/dart-lang/sdk/issues/49412
    
    TEST=tests/ffi/ffi_native_test.dart
    TEST=tests/ffi/vmspecific_static_checks_ffinative_test.dart
    
    Closes: https://github.com/dart-lang/sdk/issues/49413
    Change-Id: I5baded43eab7ff1dc1ffb16550b2a638e4b7a34e
    Cq-Include-Trybots: luci.dart.try:analyzer-linux-release-try,analyzer-mac-release-try,vm-ffi-android-debug-arm-try,vm-ffi-android-debug-arm64c-try
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/250843
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Daco Harkes <dacoharkes@google.com>

--- tests/ffi/ffi_native_test.dart
+++ tests/ffi/ffi_native_test.dart
@@ -8,28 +8,9 @@
 import 'dart:ffi';
 import 'dart:nativewrappers';
 
-// Error: FFI leaf call must not have Handle return type.
-@FfiNative<Handle Function()>("foo", isLeaf: true) //# 01: compile-time error
-external Object foo(); //# 01: compile-time error
-
-// Error: FFI leaf call must not have Handle argument types.
-@FfiNative<Void Function(Handle)>("bar", //# 02: compile-time error
-    isLeaf: true) //# 02: compile-time error
-external void bar(Object); //# 02: compile-time error
-
 class Classy {
   @FfiNative<IntPtr Function(IntPtr)>('ReturnIntPtr')
   external static int returnIntPtrStatic(int x);
-
-  // Error: Missing receiver in FfiNative annotation.
-  @FfiNative<Void Function(IntPtr)>('doesntmatter') //# 03: compile-time error
-  external void badMissingReceiver(int v); //# 03: compile-time error
-
-  // Error: Class doesn't extend NativeFieldWrapperClass1 - can't be converted
-  // to Pointer.
-  @FfiNative<Void Function(Pointer<Void>, IntPtr)>(//# 04: compile-time error
-      'doesntmatter') //# 04: compile-time error
-  external void badHasReceiverPointer(int v); //# 04: compile-time error
 
   @FfiNative<Void Function(Handle, IntPtr)>('doesntmatter')
   external void goodHasReceiverHandle(int v);
@@ -39,29 +20,12 @@ class NativeClassy extends NativeFieldWrapperClass1 {
   @FfiNative<IntPtr Function(IntPtr)>('ReturnIntPtr')
   external static int returnIntPtrStatic(int x);
 
-  // Error: Missing receiver in FfiNative annotation.
-  @FfiNative<Void Function(IntPtr)>('doesntmatter') //# 05: compile-time error
-  external void badMissingReceiver(int v); //# 05: compile-time error
-
   @FfiNative<Void Function(Pointer<Void>, IntPtr)>('doesntmatter')
   external void goodHasReceiverPointer(int v);
 
   @FfiNative<Void Function(Handle, IntPtr)>('doesntmatter')
   external void goodHasReceiverHandle(int v);
 }
-
-// Error: Too many FfiNative parameters.
-@FfiNative<Handle Function(IntPtr, IntPtr)>(//# 06: compile-time error
-    'doesntmatter') //# 06: compile-time error
-external Object badTooManyFfiParameter(int v); //# 06: compile-time error
-
-// Error: Too few FfiNative parameters.
-@FfiNative<Handle Function(IntPtr)>('doesntmatter') //# 07: compile-time error
-external Object badTooFewFfiParameter(int v, int v2); //# 07: compile-time error
-
-// Error: FfiNatives must be marked external (and by extension have no body).
-@FfiNative<Void Function()>('doesntmatter') //# 08: compile-time error
-void mustBeMarkedExternal() {} //# 08: compile-time error
 
 // Regression test: Ensure same-name FfiNative functions don't collide in the
 // top-level namespace, but instead live under their parent (Library, Class).
@@ -75,32 +39,6 @@ class B {
   external static void foo();
 }
 
-class DoesNotExtend implements NativeFieldWrapperClass1 {
-  // Error: Receiver type can't be converted to Pointer since it doesn't extend
-  // NativeFieldWrapperClass1.
-  @FfiNative<IntPtr Function(Pointer<Void>, Handle)>(//# 09: compile-time error
-      'doesntmatter') //# 09: compile-time error
-  external int bad1(DoesNotExtend obj); //# 09: compile-time error
-
-  // Error: Parameter type can't be converted to Pointer since it doesn't extend
-  // NativeFieldWrapperClass1.
-  @FfiNative<IntPtr Function(Handle, Pointer<Void>)>(//# 10: compile-time error
-      'doesntmatter') //# 10: compile-time error
-  external int bad2(DoesNotExtend obj); //# 10: compile-time error
-
-  // Error: Parameter type can't be converted to Pointer since it doesn't extend
-  // NativeFieldWrapperClass1.
-  @FfiNative<IntPtr Function(Pointer<Void>)>(//# 11: compile-time error
-      'doesntmatter') //# 11: compile-time error
-  external static int bad3(DoesNotExtend obj); //# 11: compile-time error
+void main() {
+  /* Intentionally empty: Checks that the transform succeeds. */
 }
-
-// Error: 'FfiNative' can't be declared with optional parameters.
-@FfiNative<Void Function([Double])>('doesntmatter') //# 12: compile-time error
-external static int badOptParam(); //# 12: compile-time error
-
-// Error: 'FfiNative' can't be declared with named parameters.
-@FfiNative<Void Function({Double})>('doesntmatter') //# 13: compile-time error
-external static int badNamedParam(); //# 13: compile-time error
-
-void main() {/* Intentionally empty: Compile-time error tests. */}
