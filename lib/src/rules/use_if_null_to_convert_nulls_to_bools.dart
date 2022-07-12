e80b3877462bbe4d085e929ada821ca85ae526c9
Phil Quitslund <pq@users.noreply.github.com>
2022-07-12 13:01:16 -0700

    + effective dart link (dart-lang/linter#3515)

--- lib/src/rules/use_if_null_to_convert_nulls_to_bools.dart
+++ lib/src/rules/use_if_null_to_convert_nulls_to_bools.dart
@@ -14,6 +14,8 @@ const _desc = r'Use if-null operators to convert nulls to bools.';
 
 const _details = r'''
 
+From [effective dart](https://dart.dev/guides/language/effective-dart/usage#prefer-using--to-convert-null-to-a-boolean-value):
+
 Use if-null operators to convert nulls to bools.
 
 **BAD:**
