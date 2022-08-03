c26b16cc5753891a425b210955759b87578429ea
Brett Morgan <brett.morgan@gmail.com>
2022-08-03 23:09:56 +1000

    Insert reasoning for `avoid_function_literals_in_foreach_calls` lint (dart-lang/linter#3545)
    * Update avoid_function_literals_in_foreach_calls.dart
    
    * Second attempt
    
    * Add a the to make for betterer english

--- lib/src/rules/avoid_function_literals_in_foreach_calls.dart
+++ lib/src/rules/avoid_function_literals_in_foreach_calls.dart
@@ -16,6 +16,12 @@ const _details = r'''
 
 **AVOID** using `forEach` with a function literal.
 
+The `for` loop enables a developer to be clear and explicit as to their intent.
+A return in the body of the `for` loop returns from the body of the function, 
+where as a return in the body of the `forEach` closure only returns a value 
+for that iteration of the `forEach`. The body of a `for` loop can contain 
+`await`s, while the closure body of a `forEach` cannot.
+
 **BAD:**
 ```dart
 people.forEach((person) {
@@ -28,8 +34,6 @@ people.forEach((person) {
 for (var person in people) {
   ...
 }
-
-people.forEach(print);
 ```
 ''';
 
