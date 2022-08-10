94d797ebee87e7bd02521c465e52056fdc9b4d01
Brian Wilkerson <brianwilkerson@google.com>
2022-08-10 22:52:41 +0000

    Add a refactor command
    The command name is a bit unfortunate in that it isn't very readable for
    debugging purposes, but it has the advantage that there isn't any
    opportunity for it to get out of sync. That might be the wrong thing to
    optimize for. Let me know what you think.
    
    Change-Id: Iec5c2ad9703f51c7702d4d5094f0548b48fbaafd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/254321
    Reviewed-by: Danny Tuppeny <danny@tuppeny.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/lib/src/services/refactoring/framework/refactoring_context.dart
+++ pkg/analysis_server/lib/src/services/refactoring/framework/refactoring_context.dart
@@ -3,9 +3,12 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analysis_server/src/lsp/lsp_analysis_server.dart';
+import 'package:analysis_server/src/services/correction/change_workspace.dart';
+import 'package:analysis_server/src/services/correction/util.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/analysis/session.dart';
 import 'package:analyzer/src/dart/analysis/session_helper.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_workspace.dart';
 
 /// The context in which a refactoring was requested.
 class RefactoringContext {
@@ -21,9 +24,15 @@ class RefactoringContext {
   /// The number of selected characters.
   final int selectionLength;
 
+  /// Utilities available to be used in the process of computing the edits.
+  late final CorrectionUtils utils = CorrectionUtils(resolvedResult);
+
   /// The helper used to efficiently access resolved units.
   late final AnalysisSessionHelper sessionHelper =
       AnalysisSessionHelper(session);
+
+  /// The change workspace associated with this refactoring.
+  late final ChangeWorkspace workspace = DartChangeWorkspace([session]);
 
   /// Initialize a newly created context based on the [resolvedResult].
   RefactoringContext({
