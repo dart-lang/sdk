4a0a3aaf85574958e5fe3927dabfa8667eecbbed
Phil Quitslund <pq@users.noreply.github.com>
2022-08-29 13:29:34 -0700

    + tests for record fields and types (dart-lang/linter#3623)

--- test/rules/non_constant_identifier_names_test.dart
+++ test/rules/non_constant_identifier_names_test.dart
@@ -9,7 +9,45 @@ import '../rule_test_support.dart';
 main() {
   defineReflectiveSuite(() {
     defineReflectiveTests(NonConstantIdentifierNamesTest);
+    defineReflectiveTests(NonConstantIdentifierNamesRecordsTest);
   });
+}
+
+@reflectiveTest
+class NonConstantIdentifierNamesRecordsTest extends LintRuleTest {
+  @override
+  List<String> get experiments => ['records'];
+
+  @override
+  String get lintRule => 'non_constant_identifier_names';
+
+  @FailingTest(issue: 'https://github.com/dart-lang/linter/issues/3621')
+  test_recordFields() async {
+    await assertDiagnostics(r'''
+var a = (x: 1);
+var b = (X: 1);
+''', [
+      lint('non_constant_identifier_names', 25, 1),
+    ]);
+  }
+
+  @FailingTest(issue: 'https://github.com/dart-lang/linter/issues/3622')
+  test_recordTypeDeclarations() async {
+    await assertDiagnostics(r'''
+var A = (x: 1);
+const B = (x: 1);
+''', [
+      lint('non_constant_identifier_names', 5, 1),
+    ]);
+  }
+
+  @FailingTest(issue: 'https://github.com/dart-lang/linter/issues/3621')
+  test_test_recordFields_underscores() async {
+    // This will produce a compile-time error and we don't want to over-report.
+    await assertNoDiagnostics(r'''
+var a = (_x: 1);
+''');
+  }
 }
 
 @reflectiveTest
