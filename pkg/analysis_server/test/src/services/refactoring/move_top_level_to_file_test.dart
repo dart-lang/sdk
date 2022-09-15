1857d5d7ec87015d1cc5d07fa9119b6d99bc4de6
Danny Tuppeny <danny@tuppeny.com>
2022-09-15 16:43:29 +0000

    [analysis_server] Enable new refactors in LSP behind a flag + additional tests
    Change-Id: Iec717f9522efaa51e8b087a022c0bca1bbd2da14
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/259420
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/src/services/refactoring/move_top_level_to_file_test.dart
+++ pkg/analysis_server/test/src/services/refactoring/move_top_level_to_file_test.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analysis_server/lsp_protocol/protocol.dart';
 import 'package:analysis_server/src/services/refactoring/move_top_level_to_file.dart';
+import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'refactoring_test_support.dart';
@@ -15,22 +17,122 @@ void main() {
 
 @reflectiveTest
 class MoveTopLevelToFileTest extends RefactoringTest {
+  /// Simple file content with a single class named 'A'.
+  static const simpleClassContent = 'class ^A {}';
+
+  /// The title of the refactor when using [simpleClassContent].
+  static const simpleClassRefactorTitle = "Move 'A' to file";
+
   @override
   String get refactoringName => MoveTopLevelToFile.commandName;
+
+  /// Replaces the filename argument in [action].
+  void replaceFilenameArgument(CodeAction action, String newFilePath) {
+    final arguments = getRefactorCommandArguments(action);
+    // The filename is the first item we prompt for so is first in the
+    // arguments.
+    arguments[0] = newFilePath;
+  }
+
+  Future<void> test_available() async {
+    addTestSource(simpleClassContent);
+    await initializeServer();
+    await expectCodeAction(simpleClassRefactorTitle);
+  }
+
+  Future<void> test_available_withoutClientCommandParameterSupport() async {
+    addTestSource(simpleClassContent);
+    await initializeServer(commandParameterSupport: false);
+    // This refactor is available without command parameter support because
+    // it has defaults.
+    await expectCodeAction(simpleClassRefactorTitle);
+  }
 
   Future<void> test_class() async {
     addTestSource('''
 class ClassToStay {}
 
 class ClassToMove^ {}
+
+class OtherClassToStay {}
 ''');
-    await assertRefactoring({
-      mainFilePath: '''
+
+    /// Expected main content after refactor.
+    const expectedMainContent = '''
 class ClassToStay {}
-''',
-      join(projectFolderPath, 'lib', 'class_to_move.dart'): '''
+
+class OtherClassToStay {}
+''';
+
+    /// Expected new file path/content.
+    final expectedNewFilePath =
+        join(projectFolderPath, 'lib', 'class_to_move.dart');
+    const expectedNewFileContent = '''
 class ClassToMove {}
-''',
-    });
+''';
+
+    await initializeServer();
+    final action = await expectCodeAction("Move 'ClassToMove' to file");
+    await executeRefactor(action);
+
+    expect(content[mainFilePath], expectedMainContent);
+    // Check the new file was added to `content`. If no CreateFile resource
+    // was sent, the executeRefactor helper would've thrown when trying to
+    // apply the changes.
+    expect(content[expectedNewFilePath], expectedNewFileContent);
+  }
+
+  Future<void> test_clientModifiedValues() async {
+    addTestSource(simpleClassContent);
+
+    /// Filename to inject to replace default.
+    final newFilePath = join(projectFolderPath, 'lib', 'my_new_class.dart');
+
+    /// Expected new file content.
+    const expectedNewFileContent = '''
+class A {}
+''';
+
+    await initializeServer();
+    final action = await expectCodeAction(simpleClassRefactorTitle);
+    // Replace the filename argument with our custom path.
+    replaceFilenameArgument(action, newFilePath);
+    await executeRefactor(action);
+
+    expect(content[newFilePath], expectedNewFileContent);
+  }
+
+  Future<void> test_existingFile() async {
+    addTestSource(simpleClassContent);
+
+    /// Existing new file contents where 'ClassToMove' will be moved to.
+    final newFilePath = join(projectFolderPath, 'lib', 'a.dart');
+    addSource(newFilePath, '''
+int? a;
+''');
+
+    /// Expected updated new file contents.
+    const expectedNewFileContent = '''
+class A {}
+int? a;
+''';
+
+    await initializeServer();
+    final action = await expectCodeAction(simpleClassRefactorTitle);
+    await executeRefactor(action);
+
+    expect(content[newFilePath], expectedNewFileContent);
+  }
+
+  Future<void> test_unavailable_withoutExperimentalOptIn() async {
+    addTestSource(simpleClassContent);
+    await initializeServer(experimentalOptInFlag: false);
+    await expectNoCodeAction(simpleClassRefactorTitle);
+  }
+
+  Future<void> test_unavailable_withoutFileCreateSupport() async {
+    addTestSource(simpleClassContent);
+    await initializeServer(fileCreateSupport: false);
+    await expectNoCodeAction(simpleClassRefactorTitle);
   }
 }
