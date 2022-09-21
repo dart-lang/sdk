3e6050a158db77e4c070008adf9158ee456edf32
Johnni Winther <johnniwinther@google.com>
2022-09-21 14:19:16 +0000

    [cfe] Handle structurally constant record literals
    Closes #50022
    
    Change-Id: I608ce2cd66a5d88c0c69636350b779c32569b056
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/260362
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Chloe Stefantsova <cstefantsova@google.com>

--- pkg/front_end/lib/src/fasta/kernel/constant_evaluator.dart
+++ pkg/front_end/lib/src/fasta/kernel/constant_evaluator.dart
@@ -803,8 +803,42 @@ class ConstantsTransformer extends RemovingTransformer {
   TreeNode visitRecordLiteral(RecordLiteral node, TreeNode? removalSentinel) {
     if (node.isConst) {
       return evaluateAndTransformWithContext(node, node);
+    } else {
+      // A record literal is a compile-time constant expression if and only
+      // if all its field expressions are compile-time constant expressions.
+
+      bool allConstant = true;
+
+      List<Constant> positional = [];
+
+      for (int i = 0; i < node.positional.length; i++) {
+        Expression result = transform(node.positional[i]);
+        node.positional[i] = result..parent = node;
+        if (allConstant && result is ConstantExpression) {
+          positional.add(result.constant);
+        } else {
+          allConstant = false;
+        }
+      }
+
+      Map<String, Constant> named = {};
+      for (NamedExpression expression in node.named) {
+        Expression result = transform(expression.value);
+        expression.value = result..parent = expression;
+        if (allConstant && result is ConstantExpression) {
+          named[expression.name] = result.constant;
+        } else {
+          allConstant = false;
+        }
+      }
+
+      if (allConstant) {
+        Constant constant = constantEvaluator.canonicalize(
+            new RecordConstant(positional, named, node.recordType));
+        return makeConstantExpression(constant, node);
+      }
+      return node;
     }
-    return super.visitRecordLiteral(node, removalSentinel);
   }
 
   @override
@@ -1505,12 +1539,12 @@ class ConstantEvaluator implements ExpressionVisitor<Constant> {
 
   @override
   Constant visitRecordLiteral(RecordLiteral node) {
-    if (!node.isConst) {
-      return createExpressionErrorConstant(
-          node,
-          templateNotConstantExpression
-              .withArguments('Non-constant record literal'));
-    }
+    // A record literal is a compile-time constant expression if and only
+    // if all its field expressions are compile-time constant expressions.
+    //
+    // This visitor is called when the context requires the literal to be
+    // constant, so we report an error on the expressions when these are not
+    // constants.
 
     List<Constant>? positional = _evaluatePositionalArguments(node.positional);
     if (positional == null) {
