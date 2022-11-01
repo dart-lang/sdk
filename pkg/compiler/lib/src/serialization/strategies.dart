9493c5d5b31770073899e6ce6f86d2a95ed5b814
Sigmund Cherem <sigmund@google.com>
2022-11-01 17:51:18 +0000

    [dart2js] prepare serialization/strategies for migration
    Change-Id: Ic479799af4cc0ff55a6b5124eaeb52658606a2ca
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/266724
    Reviewed-by: Mayank Patke <fishythefish@google.com>

--- pkg/compiler/lib/src/serialization/strategies.dart
+++ pkg/compiler/lib/src/serialization/strategies.dart
@@ -21,7 +21,7 @@ import '../options.dart';
 import '../source_file_provider.dart';
 import '../util/sink_adapter.dart';
 import 'serialization.dart';
-import 'task.dart';
+import 'task_migrated.dart';
 
 abstract class SerializationStrategy<T> {
   const SerializationStrategy();
---
+++ pkg/compiler/lib/src/serialization/task_migrated.dart
@@ -0,0 +1,70 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:kernel/ast.dart' as ir;
+
+import '../diagnostics/diagnostic_listener.dart';
+import '../inferrer/abstract_value_strategy.dart';
+import '../inferrer/types.dart';
+import '../js_backend/inferred_data.dart';
+import '../js_model/element_map_impl.dart';
+import '../js_model/js_world.dart';
+import '../js_model/locals.dart';
+import '../options.dart';
+import '../environment.dart';
+import 'serialization.dart';
+
+/// A data class holding some data [T] and the associated [DataSourceIndices].
+class DataAndIndices<T> {
+  final T data;
+  final DataSourceIndices indices;
+
+  DataAndIndices(this.data, this.indices);
+}
+
+void serializeGlobalTypeInferenceResultsToSink(
+    GlobalTypeInferenceResults results, DataSinkWriter sink) {
+  final closedWorld = results.closedWorld as JsClosedWorld;
+  GlobalLocalsMap globalLocalsMap = results.globalLocalsMap;
+  InferredData inferredData = results.inferredData;
+  globalLocalsMap.writeToDataSink(sink);
+  inferredData.writeToDataSink(sink);
+  results.writeToDataSink(sink, closedWorld.elementMap);
+  sink.close();
+}
+
+GlobalTypeInferenceResults deserializeGlobalTypeInferenceResultsFromSource(
+    CompilerOptions options,
+    DiagnosticReporter reporter,
+    Environment environment,
+    AbstractValueStrategy abstractValueStrategy,
+    ir.Component component,
+    JsClosedWorld closedWorld,
+    DataSourceReader source) {
+  source.registerComponentLookup(ComponentLookup(component));
+  source.registerEntityLookup(ClosedEntityLookup(closedWorld.elementMap));
+  GlobalLocalsMap globalLocalsMap = GlobalLocalsMap.readFromDataSource(
+      closedWorld.closureDataLookup.getEnclosingMember, source);
+  InferredData inferredData =
+      InferredData.readFromDataSource(source, closedWorld);
+  return GlobalTypeInferenceResults.readFromDataSource(source,
+      closedWorld.elementMap, closedWorld, globalLocalsMap, inferredData);
+}
+
+void serializeClosedWorldToSink(
+    JsClosedWorld closedWorld, DataSinkWriter sink) {
+  closedWorld.writeToDataSink(sink);
+  sink.close();
+}
+
+JsClosedWorld deserializeClosedWorldFromSource(
+    CompilerOptions options,
+    DiagnosticReporter reporter,
+    Environment environment,
+    AbstractValueStrategy abstractValueStrategy,
+    ir.Component component,
+    DataSourceReader source) {
+  return JsClosedWorld.readFromDataSource(
+      options, reporter, environment, abstractValueStrategy, component, source);
+}
