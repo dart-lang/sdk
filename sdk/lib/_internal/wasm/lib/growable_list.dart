6cc1c3c23c7b8ee0ca8291fc9d090cae9166d6f2
Ömer Sinan Ağacan <omersa@google.com>
2022-11-04 13:24:57 +0000

    [dart2wasm] Tweak exception behavior of List.addAll
    New passing test: corelib/regress_42011_test
    
    Change-Id: I944858bbeeb828aa01231de2a21e73182cf89989
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/267823
    Reviewed-by: Aske Simon Christensen <askesc@google.com>
    Commit-Queue: Ömer Ağacan <omersa@google.com>

--- sdk/lib/_internal/wasm/lib/growable_list.dart
+++ sdk/lib/_internal/wasm/lib/growable_list.dart
@@ -195,15 +195,15 @@ class _GrowableList<E> extends _ModifiableList<E> {
   void addAll(Iterable<E> iterable) {
     var len = length;
     if (iterable is EfficientLengthIterable) {
-      if (identical(iterable, this)) {
-        throw new ConcurrentModificationError(this);
-      }
-      var cap = _capacity;
       // Pregrow if we know iterable.length.
       var iterLen = iterable.length;
       if (iterLen == 0) {
         return;
       }
+      if (identical(iterable, this)) {
+        throw new ConcurrentModificationError(this);
+      }
+      var cap = _capacity;
       var newLen = len + iterLen;
       if (newLen > cap) {
         do {
