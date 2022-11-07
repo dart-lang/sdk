e1ed5e42bb9400d08ed331ffc8fdd7810107f721
Johnni Winther <johnniwinther@google.com>
2022-11-07 13:10:29 +0000

    [cfe] Remove distinction between top-level and local inference
    This ensures that the same inference is performed regardless of
    whether it has been triggered by top level inference or by
    local inference.
    
    Change-Id: I12c853773211fdb144a3795fe103e5628a7439c4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/267322
    Reviewed-by: Chloe Stefantsova <cstefantsova@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/testcases/general/top_level_binary.dart
+++ pkg/front_end/testcases/general/top_level_binary.dart
@@ -2,8 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library() {
-  print("Hello, World!");
-}
+var a = () {
+  b = c + 1;
+};
 
-main() => library();
+var b;
+var c = 1;
