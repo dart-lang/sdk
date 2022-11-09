c66667e75e3e2b0b68d38ba985f41dfd71656691
Stephen Adams <sra@google.com>
2022-11-09 21:13:47 +0000

    [dart2js] Stable sort for entities with same location and name
    Sometimes entities have the same name and location. For example, the
    synthesized getter and setter for a late instance field have the name
    of the field and the location of the original field declaration.  Some
    transforms inject multiple closures with the same location and name
    ('xxx_closure').
    
    We used to 'break ties' by sorting on `hashCode`.  The `hashCode` was
    somewhat deterministic as it relied on the `index` of Indexed
    entities. The same program unchanged, or under minor changes, tended
    to process the entities in the same order and, as the index-derived
    hashCode was monotonic in the index, the relative visit order was
    invariant when the program was recompiled without changes, and mostly
    stable for small changes.
    
    When the deterministic `hashCode` was removed and replaced by the
    default `Object.hashCode`, this lead to nondeterministic output, since
    the hashCode is different each run.
    
    This could mainly be seen in the sourcemaps where the name might
    change between 'foo' and 'foo=' as the relative order of the late
    instance field getter and setter changed.
    
    
    This change removes the comparison on `hashCode`.  Instead, we use a
    stable sort, and rely on the entities being sorted having been
    collected in some stable order.
    
    Change-Id: I7c8749fea6d2f43bde84bc3991501572f35b1ffa
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/268660
    Reviewed-by: Mayank Patke <fishythefish@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- pkg/compiler/lib/src/elements/entity_utils.dart
+++ pkg/compiler/lib/src/elements/entity_utils.dart
@@ -7,7 +7,7 @@ library entity_utils;
 import 'package:front_end/src/api_unstable/dart2js.dart'
     show isUserDefinableOperator, isMinusOperator;
 
-import 'entities.dart' show Entity, FunctionEntity;
+import 'entities.dart' show FunctionEntity;
 
 // Somewhat stable ordering for libraries using [Uri]s
 int compareLibrariesUris(Uri a, Uri b) {
@@ -61,27 +61,6 @@ int compareSourceUris(Uri uri1, Uri uri2) {
   // the Uris to usually be clustered together with a common scheme and path
   // prefix.
   return '${uri1}'.compareTo('${uri2}');
-}
-
-/// Compare entities within the same compilation unit.
-int compareEntities(Entity element1, int? line1, int? column1, Entity element2,
-    int? line2, int? column2) {
-  line1 ??= -1;
-  line2 ??= -1;
-  int r = line1.compareTo(line2);
-  if (r != 0) return r;
-
-  column1 ??= -1;
-  column2 ??= -1;
-  r = column1.compareTo(column2);
-  if (r != 0) return r;
-
-  r = element1.name!.compareTo(element2.name!);
-  if (r != 0) return r;
-
-  // Same file, position and name.  If this happens, we should find out why
-  // and make the order total and independent of hashCode.
-  return element1.hashCode.compareTo(element2.hashCode);
 }
 
 String reconstructConstructorName(FunctionEntity element) {
