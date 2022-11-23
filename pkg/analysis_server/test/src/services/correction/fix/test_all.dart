01fba618eddd38f41bc324159009cce7acc99911
Sam Rawlins <srawlins@google.com>
2022-11-23 20:24:00 +0000

    New fix: RemoveNameFromDeclarationClause
    Change-Id: I5a877e9dcc54b4c42b37bca5de89ea7f94a1fe6b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/271563
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/src/services/correction/fix/test_all.dart
+++ pkg/analysis_server/test/src/services/correction/fix/test_all.dart
@@ -162,6 +162,8 @@ import 'remove_interpolation_braces_test.dart' as remove_interpolation_braces;
 import 'remove_leading_underscore_test.dart' as remove_leading_underscore;
 import 'remove_method_declaration_test.dart' as remove_method_declaration;
 import 'remove_name_from_combinator_test.dart' as remove_name_from_combinator;
+import 'remove_name_from_declaration_clause_test.dart'
+    as remove_name_from_declaration_clause;
 import 'remove_non_null_assertion_test.dart' as remove_non_null_assertion_test;
 import 'remove_operator_test.dart' as remove_operator;
 import 'remove_parameters_in_getter_declaration_test.dart'
@@ -389,6 +391,7 @@ void main() {
     remove_leading_underscore.main();
     remove_method_declaration.main();
     remove_name_from_combinator.main();
+    remove_name_from_declaration_clause.main();
     remove_non_null_assertion_test.main();
     remove_operator.main();
     remove_parameters_in_getter_declaration.main();
