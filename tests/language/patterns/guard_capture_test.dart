b9b30b5883cc911b48f57ff16391026f5c398403
Robert Nystrom <rnystrom@google.com>
2022-11-23 00:05:47 +0000

    Add language tests for pattern variable scoping.
    This doesn't comprehensively cover everything we should test around
    pattern variable scopes, but I wanted to cover most of the tricky
    behavior around variables in cases that share a body, along with some
    other edge cases that I thought of while speccing that out.
    
    Change-Id: I64fff7b4a9a30b00801fbf37b52f07799ef0fd35
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/268370
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>
    Reviewed-by: Kallen Tu <kallentu@google.com>

---
+++ tests/language/patterns/guard_capture_test.dart
@@ -0,0 +1,91 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=patterns
+
+import "package:expect/expect.dart";
+
+main() {
+  switchSingleCase();
+  ifCase();
+  sharedCaseUseInBody();
+  sharedCaseOnlyGuards();
+}
+
+/// Guard and body use same variables when cases are not shared.
+void switchSingleCase() {
+  _closures.clear();
+  switch (['one', 'two']) {
+    case [var a, var b] when _capture(() => a) && _capture(() => b):
+      Expect.equals('one two', _runClosures());
+      Expect.equals('one', a);
+      Expect.equals('two', b);
+
+      a = 'after';
+      b = 'then';
+      Expect.equals('after', a);
+      Expect.equals('then', b);
+
+      // Guard closures see update.
+      Expect.equals('after then', _runClosures());
+  }
+}
+
+/// Guard and body use same variables in if-case statements.
+void ifCase() {
+  _closures.clear();
+  if (['one', 'two']
+      case [var a, var b] when _capture(() => a) && _capture(() => b)) {
+    Expect.equals('one two', _runClosures());
+    Expect.equals('one', a);
+    Expect.equals('two', b);
+
+    a = 'after';
+    b = 'then';
+    Expect.equals('after', a);
+    Expect.equals('then', b);
+
+    // Guard closures see update.
+    Expect.equals('after then', _runClosures());
+  }
+}
+
+/// Guards and body get separate variable when cases are shared.
+void sharedCaseUseInBody() {
+  _closures.clear();
+  switch (['one', 'two', 'three']) {
+    case [var a, _, _] when _capture(() => a, false):
+    case [_, var a, _] when _capture(() => a, false):
+    case [_, _, var a] when _capture(() => a, true):
+      Expect.equals('three', a);
+      Expect.equals('one two three', _runClosures());
+
+      a = 'after';
+      Expect.equals('after', a);
+
+      // Guard closures are unaffected.
+      Expect.equals('one two three', _runClosures());
+  }
+}
+
+/// Guards each have their own separate variables even when the variable isn't
+/// used in the body.
+void sharedCaseOnlyGuards() {
+  _closures.clear();
+  switch (['one', 'two', 'three']) {
+    case [var a, _, _] when _capture(() => a, false):
+    case [_, var a, _] when _capture(() => a, false):
+    case [_, _, var a] when _capture(() => a, true):
+      Expect.equals('one two three', _runClosures());
+  }
+}
+
+final _closures = <Object Function()>[];
+
+bool _capture(Object Function() closure, [bool result = true]) {
+  _closures.add(closure);
+  return result;
+}
+
+String _runClosures() => _closures.map((closure) => closure()).join(' ');
