8e22dd6c957258d5b5388d4bdee07075a8a49ee9
Brian Wilkerson <brianwilkerson@google.com>
2022-12-12 18:38:12 +0000

    Remove some unnecessary ignore comments in dart2wasm
    Change-Id: I5c7a8c9a4533e2fed5c5a35acf4b461bed912d36
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/274725
    Reviewed-by: Aske Simon Christensen <askesc@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/dart2wasm/lib/translator.dart
+++ pkg/dart2wasm/lib/translator.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: non_constant_identifier_names
-
 import 'dart:typed_data';
 
 import 'package:dart2wasm/class_info.dart';
