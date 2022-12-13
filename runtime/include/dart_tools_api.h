1703ccbe00bd1257b85af38c11113ecec8f5ecce
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-12 22:57:23 -0500

    spelling: execution
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- runtime/include/dart_tools_api.h
+++ runtime/include/dart_tools_api.h
@@ -364,7 +364,7 @@ DART_EXPORT bool Dart_IsReloading();
  *   "Embedder" - Execution of Dart embedder code
  *   "GC" - Execution of Dart Garbage Collector
  *   "Isolate" - Dart Isolate lifecycle execution
- *   "VM" - Excution in Dart VM runtime code
+ *   "VM" - Execution in Dart VM runtime code
  *   "" - None
  *
  *  When "all" is specified all the categories are enabled.
