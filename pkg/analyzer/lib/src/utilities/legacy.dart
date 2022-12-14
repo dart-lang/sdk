ce99386077cc615d657f8994a44a489b3a2d510e
Brian Wilkerson <brianwilkerson@google.com>
2022-12-14 19:13:28 +0000

    Update the noSoundNullSafety flag to allow for testing
    Change-Id: Ibf79675c758ffefaa8af7488dd1965f0fabbfc4b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/275741
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/lib/src/utilities/legacy.dart
+++ pkg/analyzer/lib/src/utilities/legacy.dart
@@ -3,8 +3,13 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// A flag indicating whether code that opts back to before 2.12 (null safety)
-/// is supported.
+/// is unsupported. That is, when the flag is `true` all code is analyzed
+/// assuming that null-safety is required, and when the flag is `false` all code
+/// is analyzed assuming that null-safety is optional.
 ///
-/// This flag will be removed in Dart 3.0, so it should not be used outside the
-/// analyzer package.
-const bool noSoundNullSafety = true;
+/// This flag is only writable in order to allow us to continue to test the
+/// pre-3.0 behavior while developing 3.0.
+///
+/// This flag will be removed in Dart 3.0 (with an implied value of `true`), so
+/// it should not be used outside the analyzer package.
+bool noSoundNullSafety = true;
