c5f6d585bf4b927a2804ef3864c3d0b9cfa94ca3
Sam Rawlins <srawlins@google.com>
2022-12-29 11:36:03 -0700

    Move avoid_final_parameters tests to reflective tests (dart-lang/linter#3949)

--- test/rules/avoid_final_parameters_test.dart
+++ test/rules/avoid_final_parameters_test.dart
@@ -17,6 +17,165 @@ class AvoidFinalParametersTest extends LintRuleTest {
   @override
   String get lintRule => 'avoid_final_parameters';
 
+  // TODO(srawlins): Test function-typed parameter like `void f(final p())`.
+
+  test_constructorFieldFormal_final() async {
+    await assertDiagnostics(r'''
+class C {
+  int p;
+  C(final this.p);
+}
+''', [
+      // TODO(srawlins): Do not report this lint rule here, as it is redundant
+      // with the Hint.
+      error(HintCode.UNNECESSARY_FINAL, 23, 5),
+      lint(23, 12),
+    ]);
+  }
+
+  test_constructorFieldFormal_noFinal() async {
+    await assertNoDiagnostics(r'''
+class C {
+  int p;
+  C(this.p);
+}
+''');
+  }
+
+  test_constructorSimple_final() async {
+    await assertDiagnostics(r'''
+class C {
+  C(final int p);
+}
+''', [
+      lint(14, 11),
+    ]);
+  }
+
+  test_constructorSimple_noFinal() async {
+    await assertNoDiagnostics(r'''
+class C {
+  C(int p);
+}
+''');
+  }
+
+  test_functionExpression_final() async {
+    await assertDiagnostics(r'''
+var f = (final int value) {};
+''', [
+      lint(9, 15),
+    ]);
+  }
+
+  test_functionExpression_noFinal() async {
+    await assertNoDiagnostics(r'''
+var f = (int value) {};
+''');
+  }
+
+  test_operator_final() async {
+    await assertDiagnostics(r'''
+class C {
+  int operator +(final int other) => 0;
+}
+''', [
+      lint(27, 15),
+    ]);
+  }
+
+  test_operator_noFinal() async {
+    await assertNoDiagnostics(r'''
+class C {
+  int operator +(int other) => 0;
+}
+''');
+  }
+
+  test_optionalNamed_final() async {
+    await assertDiagnostics(r'''
+void f({final int? p}) {}
+''', [
+      lint(8, 12),
+    ]);
+  }
+
+  test_optionalNamed_noFinal() async {
+    await assertNoDiagnostics(r'''
+void f({int? p}) {}
+''');
+  }
+
+  test_optionalPositional_final() async {
+    await assertDiagnostics(r'''
+void f([final int? p]) {}
+''', [
+      lint(8, 12),
+    ]);
+  }
+
+  test_optionalPositional_noFinal() async {
+    await assertNoDiagnostics(r'''
+void f([int? p]) {}
+''');
+  }
+
+  test_optionalPositionalWithDefault_final() async {
+    await assertDiagnostics(r'''
+void f([final int p = 0]) {}
+''', [
+      lint(8, 15),
+    ]);
+  }
+
+  test_optionalPositionalWithDefault_noFinal() async {
+    await assertNoDiagnostics(r'''
+void f([int p = 0]) {}
+''');
+  }
+
+  test_requiredNamed_final() async {
+    await assertDiagnostics(r'''
+void f({required final int? p}) {}
+''', [
+      lint(8, 21),
+    ]);
+  }
+
+  test_requiredNamed_noFinal() async {
+    await assertNoDiagnostics(r'''
+void f({required int p}) {}
+''');
+  }
+
+  test_requiredPositional_final() async {
+    await assertDiagnostics(r'''
+void f(final int p) {}
+''', [
+      lint(7, 11),
+    ]);
+  }
+
+  test_requiredPositional_noFinal() async {
+    await assertNoDiagnostics(r'''
+void f(int p) {}
+''');
+  }
+
+  test_setter_final() async {
+    await assertDiagnostics(r'''
+set f(final int value) {}
+''', [
+      lint(6, 15),
+    ]);
+  }
+
+  test_setter_noFinal() async {
+    await assertNoDiagnostics(r'''
+set f(int value) {}
+''');
+  }
+
   test_super() async {
     await assertDiagnostics(r'''
 class A {
@@ -28,6 +187,8 @@ class B extends A {
   B(final super.a, final super.b);
 }
 ''', [
+      // TODO(srawlins): Do not report this lint rule here, as it is redundant
+      // with the Hint.
       error(HintCode.UNNECESSARY_FINAL, 83, 5),
       error(HintCode.UNNECESSARY_FINAL, 98, 5),
       lint(83, 13),
