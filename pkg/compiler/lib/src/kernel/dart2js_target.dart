0a8521f2daddab4a1f95a3440fd7d6cd2f3c2169
Nate Biggs <natebiggs@google.com>
2023-01-04 18:36:38 +0000

    [dart2js] Migrate more files outside of the main dart2js app.
    Change-Id: I9f717f70e7166f449196d71b1c06967553dbb0cf
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/278243
    Reviewed-by: Mayank Patke <fishythefish@google.com>

--- pkg/compiler/lib/src/kernel/dart2js_target.dart
+++ pkg/compiler/lib/src/kernel/dart2js_target.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @dart=2.12
-
 // TODO(johnniwinther): Add a test that ensure that this library doesn't depend
 // on the dart2js internals.
 library compiler.src.kernel.dart2js_target;
