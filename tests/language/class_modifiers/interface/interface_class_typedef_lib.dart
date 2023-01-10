f3c774c67fd5425d5889ccb5b7b11cfcf1e9ac16
Kallen Tu <kallentu@google.com>
2023-01-10 00:30:15 +0000

    [tests] Language tests for 'interface' class modifier.
    Change-Id: Ia80f64e61aea46ba1f5fa5223c399f03c458fbec
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/276840
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/class_modifiers/interface/interface_class_typedef_lib.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=class-modifiers
+
+interface class InterfaceClass {
+  int foo = 0;
+}
+
+typedef InterfaceClassTypeDef = InterfaceClass;
+
+class A extends InterfaceClassTypeDef {}
+
+class B implements InterfaceClassTypeDef {
+  @override
+  int foo = 1;
+}
---
+++ tests/language/class_modifiers/interface/interface_class_typedef_outside_of_library_lib2.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=class-modifiers
+
+import "interface_class_typedef_outside_of_library_lib.dart";
+
+typedef ATypeDef = InterfaceClass;
