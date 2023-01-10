2b7454006fcc71c267195835d196d175071b7f9c
Phil Quitslund <pq@users.noreply.github.com>
2023-01-09 19:28:04 -0800

    Revert `dangling_library_doc_comments` (dart-lang/linter#3982)
    * Revert "enable `dangling_library_doc_comments` (dart-lang/linter#3980)"
    
    This reverts commit ef94221bfebac93f4b36ded3d54f570acd90b43a.
    
    * Revert "Revert "enable `dangling_library_doc_comments` (dart-lang/linter#3980)""
    
    This reverts commit eff442972d49dde5a9fb59340e63e7130b9b0843.
    
    * Revert "enable `dangling_library_doc_comments` (dart-lang/linter#3980)"
    
    This reverts commit ef94221bfebac93f4b36ded3d54f570acd90b43a.

--- lib/src/ast.dart
+++ lib/src/ast.dart
@@ -3,8 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// Common AST helpers.
-library;
-
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/syntactic_entity.dart';
 import 'package:analyzer/dart/ast/token.dart';
