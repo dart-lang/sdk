a3ba0d88a087b9a23a6c53c24569813b81cf1c8d
Ahmed Ashour <asashour@yahoo.com>
2023-01-11 23:12:48 +0000

    [analyzer] add test for `INCLUDED_FILE_WARNING`
    Fixes #50978
    
    Change-Id: I7ca22a09f7677ebe1e0dd5324d8c01a9d852622e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/278906
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer/test/src/diagnostics/analysis_options/analysis_options_test_support.dart
+++ pkg/analyzer/test/src/diagnostics/analysis_options/analysis_options_test_support.dart
@@ -3,19 +3,28 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:_fe_analyzer_shared/src/base/errors.dart';
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/src/context/source.dart';
 import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/task/options.dart';
+import 'package:analyzer/src/test_utilities/resource_provider_mixin.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 import '../../../generated/test_support.dart';
 
-abstract class AbstractAnalysisOptionsTest {
+abstract class AbstractAnalysisOptionsTest with ResourceProviderMixin {
+  late SourceFactory sourceFactory;
+
   VersionConstraint? get sdkVersionConstraint => null;
 
   Future<void> assertErrorsInCode(
       String code, List<ExpectedError> expectedErrors) async {
     var diagnostics = analyzeAnalysisOptions(
-        TestSource(), code, SourceFactory([]), '/', sdkVersionConstraint);
+        TestSource(convertPath('/analysis_options.yaml')),
+        code,
+        sourceFactory,
+        '/',
+        sdkVersionConstraint);
     var errorListener = GatheringErrorListener();
     errorListener.addAll(diagnostics);
     errorListener.assertErrors(expectedErrors);
@@ -32,4 +41,9 @@ abstract class AbstractAnalysisOptionsTest {
           message: text,
           messageContains: messageContains,
           expectedContextMessages: contextMessages);
+
+  void setUp() {
+    var resolvers = [ResourceUriResolver(resourceProvider)];
+    sourceFactory = SourceFactoryImpl(resolvers);
+  }
 }
