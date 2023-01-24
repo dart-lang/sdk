45b01a6fe7d25a8015fab1a9e141af42636f4a07
Brian Wilkerson <brianwilkerson@google.com>
2023-01-24 21:33:18 +0000

    Add a deprecation message to the remaining contributor classes
    Change-Id: I100c4fdbfdfd650dac0b40eb8f93c273bfad7d1d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/279464
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/services/completion/dart/type_member_contributor_test.dart
+++ pkg/analysis_server/test/services/completion/dart/type_member_contributor_test.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// This file contains tests written in a deprecated way. Please do not add any
+/// tests to this file. Instead, add tests to the files in `declaration`,
+/// `location`, or `relevance`.
+
 import 'package:analysis_server/src/provisional/completion/dart/completion_dart.dart';
 import 'package:analysis_server/src/services/completion/dart/completion_manager.dart';
 import 'package:analysis_server/src/services/completion/dart/suggestion_builder.dart';
