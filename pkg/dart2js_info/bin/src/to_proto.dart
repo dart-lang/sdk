fdfab9a1e58fa93a25379f990006c3befbfa3829
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-26 10:12:14 -0500

    spelling: outputted
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/dart2js_info/bin/src/to_proto.dart
+++ pkg/dart2js_info/bin/src/to_proto.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Command-line tool to convert an info.json file ouputted by dart2js to the
+/// Command-line tool to convert an info.json file outputted by dart2js to the
 /// alternative protobuf format.
 
 import 'dart:io';
