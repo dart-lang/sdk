2967b148ecbdf11399bb315bd91048e56c95c9cd
Tess Strickland <sstrickl@google.com>
2023-01-26 13:42:29 +0000

    [gardening] Fix genSnapshot detection in use_flag_test_helper.dart.
    Previously handled cases:
    - No cross compilation, gen_snapshot in same `buildDir` as rest.
    - X64->SIMARM cross compilation, gen_snapshot in `${buildDir}_X64`.
    
    Newly handled case:
    - X64->ARM64 cross compilation, gen_snapshot in `${buildDir}/clang_x64`.
    
    TEST=vm/dart{,_2}/analyze_snapshot_binary_test
    
    Change-Id: I3d3a23be445496ab068926f7d76b56299e06d950
    Cq-Include-Trybots: luci.dart.try:vm-kernel-precomp-linux-release-simarm_x64-try,vm-kernel-nnbd-linux-release-simarm64-try,vm-kernel-precomp-linux-release-x64-try,vm-kernel-precomp-android-release-arm64c-try
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/279880
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Reviewed-by: Daco Harkes <dacoharkes@google.com>
    Commit-Queue: Tess Strickland <sstrickl@google.com>

--- runtime/tests/vm/dart/use_flag_test_helper.dart
+++ runtime/tests/vm/dart/use_flag_test_helper.dart
@@ -16,12 +16,24 @@ final platformDill = path.join(buildDir, 'vm_platform_strong.dill');
 final genKernel = path.join(sdkDir, 'pkg', 'vm', 'tool',
     'gen_kernel' + (Platform.isWindows ? '.bat' : ''));
 final _genSnapshotBase = 'gen_snapshot' + (Platform.isWindows ? '.exe' : '');
-// Slight hack to work around issue that gen_snapshot for simarm_x64 is not
-// in the same subdirectory as dart_precompiled_runtime (${MODE}SIMARM), but
-// instead it's in ${MODE}SIMARM_X64.
-final genSnapshot = File(path.join(buildDir, _genSnapshotBase)).existsSync()
-    ? path.join(buildDir, _genSnapshotBase)
-    : path.join(buildDir + '_X64', _genSnapshotBase);
+// Lazily initialize `genSnapshot` so that tests that don't use it on platforms
+// that don't have a `gen_snapshot` don't fail.
+late final genSnapshot = () {
+  final possiblePaths = [
+    // No cross compilation.
+    path.join(buildDir, _genSnapshotBase),
+    // ${MODE}SIMARM_X64 for X64->SIMARM cross compilation.
+    path.join('${buildDir}_X64', _genSnapshotBase),
+    // ${MODE}XARM64/clang_x64 for X64->ARM64 cross compilation.
+    path.join(buildDir, 'clang_x64', _genSnapshotBase),
+  ];
+  for (final path in possiblePaths) {
+    if (File(path).existsSync()) {
+      return path;
+    }
+  }
+  throw 'Could not find gen_snapshot for build directory $buildDir';
+}();
 final aotRuntime = path.join(
     buildDir, 'dart_precompiled_runtime' + (Platform.isWindows ? '.exe' : ''));
 final isSimulator = path.basename(buildDir).contains('SIM');
