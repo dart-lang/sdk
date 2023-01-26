41f57cd3f85b9d73751551df62443cf1131ca673
Nicholas Shahan <nshahan@google.com>
2023-01-26 20:18:39 +0000

    [ddc] Cleanup names of modular tests
    Fix names in test matrix to match what is actually running on
    each bot.
    
    Updated file names:
    - Runs with sound null safety
      modular_suite_nnbd.dart -> modular_suite.dart
    
    - Runs without sound null safety
      modular_suite.dart -> modular_suite_unsound.dart
    
    Updated configs to record status:
    - Sound
      dartdevk-strong-linux-release-chrome
    - Unsound
      dartdevk-checked-linux-release-chrome
      dartdevk-checked-mac-release-chrome
      dartdevk-checked-win-release-chrome
      dartdevc-canary-weak-linux-release-chrome-x64
    
    Change-Id: I21d1df41a174824b26154584315cb300a37103f8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/279471
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

--- pkg/dev_compiler/test/modular_suite.dart
+++ pkg/dev_compiler/test/modular_suite.dart
@@ -110,7 +110,7 @@ class SourceToSummaryDillStep implements IOModularStep {
       '--multi-root-scheme',
       rootScheme,
       ...extraArgs,
-      '--no-sound-null-safety',
+      '--sound-null-safety',
       '--output',
       '${toUri(module, dillId)}',
       if (!module.isSdk) ...[
@@ -199,12 +199,11 @@ class DDCStep implements IOModularStep {
       '--modules=es6',
       '--no-summarize',
       '--no-source-map',
-      '--experimental-output-compiled-kernel',
       '--multi-root-scheme',
       rootScheme,
       ...sources,
       ...extraArgs,
-      '--no-sound-null-safety',
+      '--sound-null-safety',
       for (String flag in flags) '--enable-experiment=$flag',
       ...transitiveDependencies
           .where((m) => !m.isSdk)
