1631284945f9c9c50f4b258e018821e6511acd36
Ahmed Ashour <asashour@yahoo.com>
2023-01-31 18:20:56 +0000

    [analysis_server] update header of Flutter mock files.
    Bug #51184
    
    Change-Id: Ic8eda3e07858b3533f65bef4e2133d0da74e5209
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/280117
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/mock_packages/flutter/lib/widgets.dart
+++ pkg/analysis_server/test/mock_packages/flutter/lib/widgets.dart
@@ -1,4 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2015 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,5 +9,5 @@ export 'src/widgets/framework.dart';
 export 'src/widgets/gesture_detector.dart';
 export 'src/widgets/icon.dart';
 export 'src/widgets/placeholder.dart';
-export 'src/widgets/ticker_provider.dart';
 export 'src/widgets/text.dart';
+export 'src/widgets/ticker_provider.dart';
