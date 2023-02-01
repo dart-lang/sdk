1f7ac27eecdbf714abeadb2045028d7eb9dc3816
Ryan Macnak <rmacnak@google.com>
2023-02-01 23:25:47 +0000

    Revert "Suppress clang warning for musl libc inline"
    This reverts commit ddc19f7f92ff0aaa5ba17ff5e5fab9c82e8339fb.
    
    Reason for revert: Breaks gcc build
    ../../runtime/bin/socket_base_posix.cc:161: error: ignoring #pragma clang diagnostic [-Werror=unknown-pragmas]
      161 |     cmsg = CMSG_NEXTHDR(&msg, cmsg);
          |
    cc1plus: all warnings being treated as errors
    
    Original change's description:
    > Suppress clang warning for musl libc inline
    >
    > Closes https://github.com/dart-lang/sdk/pull/51191
    >
    > GitOrigin-RevId: d7064c43adce0b115c79e818800e44ba4f60739e
    > Change-Id: Ie73e96c83294d43baf3be6772cb84705ba2d2712
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/280136
    > Reviewed-by: Slava Egorov <vegorov@google.com>
    > Commit-Queue: Slava Egorov <vegorov@google.com>
    
    TBR=vegorov@google.com,dart-scoped@luci-project-accounts.iam.gserviceaccount.com,copybara-worker-blackhole@google.com
    
    Change-Id: I8b726d616ba1b5434b92c239c3b8897a942fad5b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/280300
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Reviewed-by: Ryan Macnak <rmacnak@google.com>

--- runtime/bin/socket_base_posix.cc
+++ runtime/bin/socket_base_posix.cc
@@ -21,13 +21,6 @@
 #include "bin/socket_base_macos.h"
 #include "platform/signal_blocker.h"
 
-// We wrap CMSG_NXTHDR to suppress sign-compare warnings which occur on musl.
-#define CMSG_NEXTHDR(mhdr, cmsg)                         \
-  _Pragma("clang diagnostic push")                       \
-  _Pragma("clang diagnostic ignored \"-Wsign-compare\"") \
-  CMSG_NXTHDR(mhdr, cmsg)                                \
-  _Pragma("clang diagnostic pop")
-
 namespace dart {
 namespace bin {
 
@@ -158,13 +151,13 @@ intptr_t SocketBase::ReceiveMessage(intptr_t fd,
   size_t num_messages = 0;
   while (cmsg != nullptr) {
     num_messages++;
-    cmsg = CMSG_NEXTHDR(&msg, cmsg);
+    cmsg = CMSG_NXTHDR(&msg, cmsg);
   }
   (*p_messages) = reinterpret_cast<SocketControlMessage*>(
       Dart_ScopeAllocate(sizeof(SocketControlMessage) * num_messages));
   SocketControlMessage* control_message = *p_messages;
   for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != nullptr;
-       cmsg = CMSG_NEXTHDR(&msg, cmsg), control_message++) {
+       cmsg = CMSG_NXTHDR(&msg, cmsg), control_message++) {
     void* data = CMSG_DATA(cmsg);
     size_t data_length = cmsg->cmsg_len - (reinterpret_cast<uint8_t*>(data) -
                                            reinterpret_cast<uint8_t*>(cmsg));
@@ -267,7 +260,7 @@ intptr_t SocketBase::SendMessage(intptr_t fd,
     struct cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
     message = messages;
     for (intptr_t i = 0; i < num_messages;
-         i++, message++, cmsg = CMSG_NEXTHDR(&msg, cmsg)) {
+         i++, message++, cmsg = CMSG_NXTHDR(&msg, cmsg)) {
       ASSERT(message->is_file_descriptors_control_message());
       cmsg->cmsg_level = SOL_SOCKET;
       cmsg->cmsg_type = SCM_RIGHTS;
