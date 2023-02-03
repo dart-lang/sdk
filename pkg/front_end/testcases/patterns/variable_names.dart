10953e26ca6b64d1c46ed5c136c3ba61d0537821
Johnni Winther <johnniwinther@google.com>
2023-02-03 12:47:31 +0000

    [cfe] Add test for parenthesized variable names
    Change-Id: I2d3964edde49c22d9916fe266ec32e1516dd4f6f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/280721
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Chloe Stefantsova <cstefantsova@google.com>

--- pkg/front_end/testcases/patterns/variable_names.dart
+++ pkg/front_end/testcases/patterns/variable_names.dart
@@ -9,14 +9,17 @@ abstract class A {
   num get boz;
   bool get qux;
   String get quux;
+  int get foobar;
 }
 
 test(x, y, z) {
   var qux;
   var quux;
-  if (x case A(:var foo!, :var bar, :var baz?, :var boz as double)) {
+  if (x case A(:var foo!, :var bar, :var baz?, :var boz as double,
+      :(var foobar))) {
     return 0;
-  } else if (x case (:var foo!, :var bar, :var baz?, var boz as double)) {
+  } else if (x case (:var foo!, :var bar, :var baz?, var boz as double,
+      :(var foobar))) {
     return 1;
   } else if (x case A(: [1, 2])) { // Error
     return 2;
