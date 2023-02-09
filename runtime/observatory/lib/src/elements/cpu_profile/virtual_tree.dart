1873228ebb43dbe258dc8ee9f195b4aa2b896ac5
Ryan Macnak <rmacnak@google.com>
2023-02-09 17:29:20 +0000

    [observatory] Fix NNBD damage to malloc profiler page.
    TEST=--profiler-native-memory
    Change-Id: I8333359e516d9cb30d57d85d32c4328a450c3579
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/281864
    Reviewed-by: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Ryan Macnak <rmacnak@google.com>

--- runtime/observatory/lib/src/elements/cpu_profile/virtual_tree.dart
+++ runtime/observatory/lib/src/elements/cpu_profile/virtual_tree.dart
@@ -27,14 +27,14 @@ class CpuProfileVirtualTreeElement extends CustomElement implements Renderable {
   late M.ProfileTreeDirection _direction;
   late ProfileTreeMode _mode;
   late M.SampleProfileType _type;
-  late M.IsolateRef _isolate;
+  M.IsolateRef? _isolate;
   late M.SampleProfile _profile;
   Iterable<M.CallTreeNodeFilter>? _filters;
 
   M.ProfileTreeDirection get direction => _direction;
   ProfileTreeMode get mode => _mode;
   M.SampleProfileType get type => _type;
-  M.IsolateRef get isolate => _isolate;
+  M.IsolateRef? get isolate => _isolate;
   M.SampleProfile get profile => _profile;
   Iterable<M.CallTreeNodeFilter>? get filters => _filters;
 
@@ -46,7 +46,8 @@ class CpuProfileVirtualTreeElement extends CustomElement implements Renderable {
     _r.dirty();
   }
 
-  factory CpuProfileVirtualTreeElement(Object? owner, M.SampleProfile profile,
+  factory CpuProfileVirtualTreeElement(
+      M.IsolateRef? isolate, M.SampleProfile profile,
       {ProfileTreeMode mode = ProfileTreeMode.function,
       M.SampleProfileType type = M.SampleProfileType.cpu,
       M.ProfileTreeDirection direction = M.ProfileTreeDirection.exclusive,
@@ -57,7 +58,7 @@ class CpuProfileVirtualTreeElement extends CustomElement implements Renderable {
     CpuProfileVirtualTreeElement e = new CpuProfileVirtualTreeElement.created();
     e._r =
         new RenderingScheduler<CpuProfileVirtualTreeElement>(e, queue: queue);
-    e._isolate = owner as M.Isolate;
+    e._isolate = isolate;
     e._profile = profile;
     e._mode = mode;
     e._type = type;
