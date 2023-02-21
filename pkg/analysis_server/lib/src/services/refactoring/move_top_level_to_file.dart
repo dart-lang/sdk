bc91549aeadbb30ddf2b3b1731e0a9cab0d21ef9
Danny Tuppeny <danny@tuppeny.com>
2023-02-21 17:27:27 +0000

    [analysis_server] Use the more specific "refactor.move" for Move Class to File
    VS Code refactors based on kind and shows some well-known kinds in a specific order. One new group is "refactor.move".
    
    It's not yet in the LSP spec/code-gen, so is added to our custom enum in the meantime.
    
    Fixes https://github.com/Dart-Code/Dart-Code/issues/4354.
    
    Change-Id: I5577ef67a81667652258b873125dc491d96ce597
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/284144
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/lib/src/services/refactoring/move_top_level_to_file.dart
+++ pkg/analysis_server/lib/src/services/refactoring/move_top_level_to_file.dart
@@ -4,6 +4,8 @@
 
 import 'package:analysis_server/lsp_protocol/protocol_custom_generated.dart'
     show CommandParameter, SaveUriCommandParameter;
+import 'package:analysis_server/lsp_protocol/protocol_generated.dart';
+import 'package:analysis_server/src/lsp/constants.dart';
 import 'package:analysis_server/src/services/refactoring/framework/refactoring_producer.dart';
 import 'package:analysis_server/src/utilities/extensions/ast.dart';
 import 'package:analysis_server/src/utilities/import_analyzer.dart';
@@ -33,6 +35,9 @@ class MoveTopLevelToFile extends RefactoringProducer {
   /// Initialize a newly created refactoring producer to use the given
   /// [context].
   MoveTopLevelToFile(super.context);
+
+  @override
+  CodeActionKind get kind => DartCodeActionKind.RefactorMove;
 
   @override
   List<CommandParameter> get parameters => [
