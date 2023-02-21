6548bf12ede9e07bb9602bf8a52e670a997e37da
Nate Biggs <natebiggs@google.com>
2023-02-21 23:25:23 +0000

    [dart2js] Implement a bucketed work queue for the inferrer.
    The bucket indexing can be made more complex but for now this simple change of processing CallSiteTypeInformations separately has shown an improvement in inference execution time (~5% on a big program).
    
    Change-Id: If029906ca0c51d67939776732e0a25dcab7f6af1
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/284400
    Reviewed-by: Stephen Adams <sra@google.com>
    Commit-Queue: Nate Biggs <natebiggs@google.com>

--- pkg/compiler/lib/src/inferrer/work_queue.dart
+++ pkg/compiler/lib/src/inferrer/work_queue.dart
@@ -5,18 +5,40 @@
 import 'dart:collection' show Queue;
 import 'type_graph_nodes.dart';
 
+const _numBuckets = 2;
+
+/// This function applies a bucket index to each type information.
+///
+/// Current strategy:
+/// Process call sites together effectively splitting intraprocedural type
+/// refinement and local type refinement.
+int _bucketForInfo(TypeInformation info) {
+  if (info is! CallSiteTypeInformation) return 0;
+  return 1;
+}
+
 /// A work queue for the inferrer. It filters out nodes that are tagged as
 /// [TypeInformation.doNotEnqueue], as well as ensures through
 /// [TypeInformation.inQueue] that a node is in the queue only once at
 /// a time.
+///
+/// The queue uses a bucketed approach to allow the inferrer to make progress
+/// on certain categories of types while also ensuring no category is starved
+/// of resources. The queue draws work items from a bucket until it is empty
+/// and then proceeds onto the next bucket with work remaining. This allows
+/// related work items to be processed closer to each other.
 class WorkQueue {
-  final Queue<TypeInformation> queue = Queue<TypeInformation>();
+  final List<Queue<TypeInformation>> buckets =
+      List.generate(_numBuckets, (_) => Queue());
+  int _length = 0;
+  int _activeBucket = 0;
 
   void add(TypeInformation element) {
     if (element.doNotEnqueue) return;
     if (element.inQueue) return;
-    queue.addLast(element);
+    buckets[_bucketForInfo(element)].addLast(element);
     element.inQueue = true;
+    _length++;
   }
 
   void addAll(Iterable<TypeInformation> all) {
@@ -24,12 +46,18 @@ class WorkQueue {
   }
 
   TypeInformation remove() {
-    TypeInformation element = queue.removeFirst();
+    var bucket = buckets[_activeBucket];
+    while (bucket.isEmpty) {
+      if (++_activeBucket == buckets.length) _activeBucket = 0;
+      bucket = buckets[_activeBucket];
+    }
+    TypeInformation element = bucket.removeFirst();
     element.inQueue = false;
+    _length--;
     return element;
   }
 
-  bool get isEmpty => queue.isEmpty;
+  bool get isEmpty => _length == 0;
 
-  int get length => queue.length;
+  int get length => _length;
 }
