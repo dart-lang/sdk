efc9ebb215237ed3518af9c0b48cdeeace81b76f
Kallen Tu <kallentu@google.com>
2023-02-28 16:46:29 +0000

    [cfe] Change location of mixin class constructor errors.
    Change-Id: Ic9484ca8ccadba8f13588e4e7f5fa781344c27b0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/285881
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

--- tests/language/class_modifiers/mixin/mixin_class_generative_constructor_error_test.dart
+++ tests/language/class_modifiers/mixin/mixin_class_generative_constructor_error_test.dart
@@ -8,61 +8,56 @@
 // constructor
 
 mixin class MixinClass {
-//          ^
-// [cfe] Can't use 'MixinClass' as a mixin because it has constructors.
   final int foo;
 
   MixinClass(this.foo);
 //^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClass' as a mixin because it has constructors.
 }
 
 mixin class MixinClassNamed {
-//          ^
-// [cfe] Can't use 'MixinClassNamed' as a mixin because it has constructors.
   final int foo;
 
   MixinClassNamed.named(this.foo);
 //^^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassNamed' as a mixin because it has constructors.
 }
 
 mixin class MixinClassRedirect {
-//          ^
-// [cfe] Can't use 'MixinClassRedirect' as a mixin because it has constructors.
   int foo = 0;
 
   MixinClassRedirect.named(int f) { this.foo = f; }
 //^^^^^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassRedirect' as a mixin because it has constructors.
 
   MixinClassRedirect.x(int f) : this.named(f);
 //^^^^^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassRedirect' as a mixin because it has constructors.
 }
 
 mixin class MixinClassExternal {
-//          ^
-// [cfe] Can't use 'MixinClassExternal' as a mixin because it has constructors.
   external MixinClassExternal();
 //         ^^^^^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassExternal' as a mixin because it has constructors.
 }
 
 mixin class MixinClassSuper {
-//          ^
-// [cfe] Can't use 'MixinClassSuper' as a mixin because it has constructors.
   MixinClassSuper(): super();
 //^^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassSuper' as a mixin because it has constructors.
 }
 
 mixin class MixinClassBody {
-//          ^
-// [cfe] Can't use 'MixinClassBody' as a mixin because it has constructors.
   MixinClassBody() {}
 //^^^^^^^^^^^^^^
 // [analyzer] COMPILE_TIME_ERROR.MIXIN_CLASS_DECLARES_CONSTRUCTOR
+// [cfe] Can't use 'MixinClassBody' as a mixin because it has constructors.
 }
 
 class GenerativeConstructor with MixinClass {}
