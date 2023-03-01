08b57e0457dce32606fa82af96863488b1e43804
Daco Harkes <dacoharkes@google.com>
2023-03-01 10:24:24 +0000

    [test/ffi] Fix test use `RTLD_GLOBAL` on Android
    Android arm has a different const for `RTLD_GLOBAL` which we take care
    of in the helper function `dlopenGlobalPlatformSpecific`. So use that
    instead.
    
    Change-Id: Icb5d0b84f7d76ec1f6e24d43a644cffe8f46fd42
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/286140
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Daco Harkes <dacoharkes@google.com>
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- tests/ffi/regress_51504_test.dart
+++ tests/ffi/regress_51504_test.dart
@@ -16,26 +16,8 @@ import 'dylib_utils.dart';
 
 final ffiTestFunctions = dlopenPlatformSpecific('ffi_test_functions');
 
-@Native<Pointer<Void> Function(Pointer<Char>, Int)>()
-external Pointer<Void> dlopen(Pointer<Char> file, int mode);
-
-const RTLD_LAZY = 0x00001;
-const RTLD_GLOBAL = 0x00100;
-
 void main() {
-  // Force dlopen so @Native lookups in DynamicLibrary.process() succeed.
-  print(ffiTestFunctions);
-  if (Platform.isLinux || Platform.isAndroid) {
-    // TODO(https://dartbug.com/50105): enable dlopen global via package:ffi.
-    using((arena) {
-      final dylibHandle = dlopen(
-          platformPath('ffi_test_functions')
-              .toNativeUtf8(allocator: arena)
-              .cast(),
-          RTLD_LAZY | RTLD_GLOBAL);
-      print(dylibHandle);
-    });
-  }
+  dlopenGlobalPlatformSpecific('ffi_test_functions');
 
   testVariadicAt1Int64x5NativeLeaf();
 }
