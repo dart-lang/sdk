da7ef9a66169b899d116a1f00608a399c03df4aa
Leaf Petersen <leafp@google.com>
2023-03-21 20:31:00 +0000

    Tests for transitivity of base.
    Test that all subtypes of classes marked base/final are required to be
    base/final/sealed, and that base/final classes may not be implemented
    outside of their own library.  Also test restrictions on sealed and
    interface classes.
    
    Change-Id: I15314a34b684bbec5a16b8ae228ca7de08f8498a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/290100
    Reviewed-by: Erik Ernst <eernst@google.com>

---
+++ tests/language/class_modifiers/base_transitivity/final_class_different_library_error_test.dart
@@ -0,0 +1,197 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=class-modifiers
+
+/// Test the invalid uses of a final class defined in a different library
+
+import "shared_library_definitions.dart" show FinalClass, SimpleClass;
+
+mixin _MixinOnObject {}
+
+/// It is an error if FinalClass is extended.
+
+// Simple extension.
+class SimpleExtend extends FinalClass {}
+//                         ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+base class BaseExtend extends FinalClass {}
+//                            ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+interface class InterfaceExtend extends FinalClass {}
+//                                      ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+final class FinalExtend extends FinalClass {}
+//                              ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+sealed class SealedExtend extends FinalClass {}
+//                                ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+// Extending via an anonymous mixin class.
+class SimpleExtendWith extends FinalClass with _MixinOnObject {}
+//                             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+base class BaseExtendWith extends FinalClass with _MixinOnObject {}
+//                                ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+interface class InterfaceExtendWith extends FinalClass with _MixinOnObject {}
+//                                          ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+final class FinalExtendWith extends FinalClass with _MixinOnObject {}
+//                                  ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+sealed class SealedExtendWith extends FinalClass with _MixinOnObject {}
+//                                    ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+// Extending via an anonymous mixin application class.
+class SimpleExtendApplication = FinalClass with _MixinOnObject;
+//                              ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+base class BaseExtendApplication = FinalClass with _MixinOnObject;
+//                                 ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+interface class InterfaceExtendApplication = FinalClass with _MixinOnObject;
+//                                           ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+final class FinalExtendApplication = FinalClass with _MixinOnObject;
+//                                   ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+sealed class SealedExtendApplication = FinalClass with _MixinOnObject;
+//                                     ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be extended outside of its library because it's a final class.
+
+/// It is an error if FinalClass is implemented by something which is not base,
+/// final or sealed.
+
+// Simple implementation.
+class SimpleImplement implements FinalClass {}
+//                               ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+base class BaseImplement implements FinalClass {}
+//                                  ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+interface class InterfaceImplement implements FinalClass {}
+//                                            ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+final class FinalImplement implements FinalClass {}
+//                                    ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+sealed class SealedImplement implements FinalClass {}
+//                                      ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+// Implementing with a mixin class.
+mixin class SimpleMixinClassImplement implements FinalClass {}
+//                                               ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+base mixin class BaseMixinClassImplement implements FinalClass {}
+//                                                  ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+// Implementing with a mixin application class.
+class SimpleImplementApplication = Object
+    with _MixinOnObject
+    implements FinalClass;
+//             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+base class BaseImplementApplication = Object
+    with _MixinOnObject
+    implements FinalClass;
+//             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+interface class InterfaceImplementApplication = Object
+    with _MixinOnObject
+    implements FinalClass;
+//             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+final class FinalImplementApplication = Object
+    with _MixinOnObject
+    implements FinalClass;
+//             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+sealed class SealedImplementApplication = Object
+    with _MixinOnObject
+    implements FinalClass;
+//             ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+// Implementing with a mixin.
+mixin SimpleMixinImplement implements FinalClass {}
+//                                    ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+base mixin BaseMixinImplement implements FinalClass {}
+//                                       ^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.INVALID_USE_OF_TYPE_OUTSIDE_LIBRARY
+// [cfe] The class 'FinalClass' can't be implemented outside of its library because it's a final class.
+
+/// It is an error if FinalClass is the `on` type of a mixin which is not marked base.
+
+mixin SimpleMixinOn on FinalClass {}
+//    ^^^^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.SUBTYPE_OF_BASE_OR_FINAL_IS_NOT_BASE_FINAL_OR_SEALED
+// [cfe] The type 'SimpleMixinOn' must be 'base', 'final' or 'sealed' because the supertype 'FinalClass' is 'final'.
+
+mixin SimpleMixinOnFinalSimple on FinalClass, SimpleClass {}
+//    ^^^^^^^^^^^^^^^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.SUBTYPE_OF_BASE_OR_FINAL_IS_NOT_BASE_FINAL_OR_SEALED
+// [cfe] The type 'SimpleMixinOnFinalSimple' must be 'base', 'final' or 'sealed' because the supertype 'FinalClass' is 'final'.
+
+mixin SimpleMixinOnSimpleFinal on SimpleClass, FinalClass {}
+//    ^^^^^^^^^^^^^^^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.SUBTYPE_OF_BASE_OR_FINAL_IS_NOT_BASE_FINAL_OR_SEALED
+// [cfe] The type 'SimpleMixinOnSimpleFinal' must be 'base', 'final' or 'sealed' because the supertype 'FinalClass' is 'final'.
+
+main() {}
