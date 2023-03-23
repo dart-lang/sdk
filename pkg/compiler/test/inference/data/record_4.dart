32c5b81f9c94a2f99fe76f04c2d0653ae109414a
Nate Biggs <natebiggs@google.com>
2023-03-23 22:56:15 +0000

    [dart2js] Add unit tests for records.
    The expectations set here are updated as fixes/improvements are made in the next 2 CLs.
    
    Change-Id: I0ac251ce09b980cde9e01afc1fa81b987a619747
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/290980
    Reviewed-by: Stephen Adams <sra@google.com>

---
+++ pkg/compiler/test/inference/data/record_4.dart
@@ -0,0 +1,27 @@
+/*member: main:[null]*/
+void main() {
+  testList();
+  testClosure1();
+}
+
+/*member: testList:Container([exact=JSExtendableArray], element: [null|subclass=Object], length: null)*/
+testList() {
+  dynamic list = [];
+  final rec = (list, 3);
+  final myList = rec
+      . /*[Record(RecordShape(2), [Container([exact=JSExtendableArray], element: [null|subclass=Object], length: null), [exact=JSUInt31]])]*/ $1;
+  myList
+      . /*invoke: Container([exact=JSExtendableArray], element: [null|subclass=Object], length: null)*/ add(
+          1);
+  return list;
+}
+
+/*member: testClosure1:[empty]*/
+testClosure1() {
+  return getRecord(). /*[exact=JSUInt31]*/ $1;
+}
+
+/*member: getRecord:[exact=JSUInt31]*/
+getRecord() {
+  return ([1, 2], 3);
+}
