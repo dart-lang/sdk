2427527612df1ecb8b0e3b086fdac904b6b6a1e7
Konstantin Shcheglov <scheglov@google.com>
2023-04-05 20:48:24 +0000

    Suggest type names during completion in CaseClause and SwitchExpression body.
    Change-Id: Iea7c2c3d0879191265c368b5e79fbf2cfaab0dd0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/293821
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/test/services/completion/dart/location/switch_expression_test.dart
+++ pkg/analysis_server/test/services/completion/dart/location/switch_expression_test.dart
@@ -54,9 +54,18 @@ int f(Object p01) {
     ^
   };
 }
+
+class A1 {}
 ''');
+    // TODO(scheglov) This is wrong.
     assertResponse(r'''
 suggestions
+  A1
+    kind: class
+  A1
+    kind: constructorInvocation
+  p01
+    kind: parameter
 ''');
   }
 
@@ -67,10 +76,167 @@ int f(Object p01) {
     ^
   };
 }
+
+class A1 {
+  A1.named();
+}
+
+const c01 = 0;
+
+final v01 = 0;
+
+int f01() => 0;
 ''');
+    // TODO(scheglov) This is wrong.
     assertResponse(r'''
 suggestions
+  A1
+    kind: class
+  c01
+    kind: topLevelVariable
+  f01
+    kind: functionInvocation
+  p01
+    kind: parameter
+  v01
+    kind: topLevelVariable
 ''');
+  }
+
+  Future<void> test_body_partial() async {
+    await computeSuggestions('''
+int f(Object p01) {
+  return switch (p01) {
+    A^
+  };
+}
+
+class A01 {}
+class B01 {}
+
+const A02 = 0;
+const B02 = 0;
+
+final A03 = 0;
+final B03 = 0;
+
+int A04() => 0;
+int B04() => 0;
+''');
+    if (isProtocolVersion2) {
+      // TODO(scheglov) This is wrong.
+      assertResponse(r'''
+replacement
+  left: 1
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: topLevelVariable
+  A03
+    kind: topLevelVariable
+  A04
+    kind: functionInvocation
+''');
+    } else {
+      assertResponse(r'''
+replacement
+  left: 1
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: topLevelVariable
+  A03
+    kind: topLevelVariable
+  A04
+    kind: functionInvocation
+  B01
+    kind: class
+  B01
+    kind: constructorInvocation
+  B02
+    kind: topLevelVariable
+  B03
+    kind: topLevelVariable
+  B04
+    kind: functionInvocation
+  p01
+    kind: parameter
+''');
+    }
+  }
+
+  Future<void> test_body_partial2() async {
+    await computeSuggestions('''
+void f(Object p01) {
+  (switch (p01) {
+    A^
+  });
+}
+
+class A01 {}
+class B01 {}
+
+const A02 = 0;
+const B02 = 0;
+
+final A03 = 0;
+final B03 = 0;
+
+int A04() => 0;
+int B04() => 0;
+''');
+    if (isProtocolVersion2) {
+      // TODO(scheglov) This is wrong.
+      assertResponse(r'''
+replacement
+  left: 1
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: topLevelVariable
+  A03
+    kind: topLevelVariable
+  A04
+    kind: functionInvocation
+''');
+    } else {
+      assertResponse(r'''
+replacement
+  left: 1
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: topLevelVariable
+  A03
+    kind: topLevelVariable
+  A04
+    kind: functionInvocation
+  B01
+    kind: class
+  B01
+    kind: constructorInvocation
+  B02
+    kind: topLevelVariable
+  B03
+    kind: topLevelVariable
+  B04
+    kind: functionInvocation
+  p01
+    kind: parameter
+''');
+    }
   }
 
   Future<void> test_expression() async {
