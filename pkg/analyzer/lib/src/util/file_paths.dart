ccb02fa8fc2ccbda33f3b91341e88481ac576eda
Danny Tuppeny <danny@tuppeny.com>
2023-04-21 18:50:50 +0000

    [analysis_server] Use fixDataYamlFolder constant instead of string literals
    Follow-up from https://dart-review.googlesource.com/c/sdk/+/296801.
    
    Change-Id: I72ea8a42ec64c4bd6c6cee021a1464167ec368e6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/296862
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/lib/src/util/file_paths.dart
+++ pkg/analyzer/lib/src/util/file_paths.dart
@@ -72,7 +72,7 @@ bool isDart(p.Context pathContext, String path) {
 /// Such files specify data-driven fixes.
 bool isFixDataYaml(p.Context pathContext, String path) {
   return pathContext.basename(path) == fixDataYaml ||
-      (pathContext.split(path).contains('fix_data') &&
+      (pathContext.split(path).contains(fixDataYamlFolder) &&
           pathContext.extension(path) == '.yaml');
 }
 
