614de8dddfa142e93ce79c5b4e4708c1e2ce2169
Brian Wilkerson <brianwilkerson@google.com>
2023-05-17 00:31:43 +0000

    Convert the named constructor completion tests
    Change-Id: I095571f486feead2ee4dad0a700bac46901e16a9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/303903
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/services/completion/dart/declaration/constructor_test.dart
+++ pkg/analysis_server/test/services/completion/dart/declaration/constructor_test.dart
@@ -15,14 +15,14 @@ void main() {
 
 @reflectiveTest
 class ConstructorTest1 extends AbstractCompletionDriverTest
-    with ConstructorTestCases {
+    with ConstructorTestCases, NamedConstructorTestCases {
   @override
   TestingCompletionProtocol get protocol => TestingCompletionProtocol.version1;
 }
 
 @reflectiveTest
 class ConstructorTest2 extends AbstractCompletionDriverTest
-    with ConstructorTestCases {
+    with ConstructorTestCases, NamedConstructorTestCases {
   @override
   TestingCompletionProtocol get protocol => TestingCompletionProtocol.version2;
 }
@@ -131,6 +131,383 @@ void f() {
 
     assertResponse(r'''
 suggestions
+''');
+  }
+}
+
+mixin NamedConstructorTestCases on AbstractCompletionDriverTest {
+  @override
+  Future<void> setUp() async {
+    await super.setUp();
+    allowedIdentifiers = {'new', 'named', 'c', '_d', 'fromCharCodes'};
+  }
+
+  Future<void> test_functionTypeContext_matchingReturnType() async {
+    await computeSuggestions('''
+class A {
+  A();
+  A.named();
+}
+
+void f() {
+  A Function() v = A.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_functionTypeContext_matchingReturnType_partial() async {
+    await computeSuggestions('''
+class A {
+  A();
+  A.named();
+}
+
+void f() {
+  A Function() v = A.na^;
+}
+''');
+    if (isProtocolVersion2) {
+      assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  named
+    kind: constructorInvocation
+''');
+    } else {
+      assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+    }
+  }
+
+  Future<void>
+      test_functionTypeContext_matchingReturnType_withTypeArguments() async {
+    await computeSuggestions('''
+class A<T> {
+  A.named();
+  A.new();
+}
+
+void f() {
+  A<int> Function() v = A<int>.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void>
+      test_functionTypeContext_matchingReturnType_withTypeArguments2() async {
+    await computeSuggestions('''
+class A {}
+
+class B<T> extends A {
+  B.named();
+  B.new();
+}
+
+void f() {
+  A Function() v = B<int>.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void>
+      test_functionTypeContext_matchingReturnType_withTypeArguments_partial() async {
+    await computeSuggestions('''
+class A<T> {
+  A.named();
+  A.new();
+}
+
+void f() {
+  A<int> Function() v = A<int>.na^;
+}
+''');
+    if (isProtocolVersion2) {
+      assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  named
+    kind: constructorInvocation
+''');
+    } else {
+      assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+    }
+  }
+
+  Future<void>
+      test_functionTypeContext_matchingReturnType_withTypeArguments_withPrefix() async {
+    newFile('$testPackageLibPath/a.dart', '''
+class A<T> {
+  A.named();
+  A.new();
+}
+''');
+    await computeSuggestions('''
+import 'a.dart' as prefix;
+
+void f() {
+  A<int> Function() v = prefix.A<int>.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+  new
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void>
+      test_functionTypeContext_notMatchingReturnType_withTypeArguments() async {
+    await computeSuggestions('''
+class A<T> {
+  A.named();
+}
+
+void f() {
+  List<int> Function() v = A<int>.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_importedClass() async {
+    newFile('$testPackageLibPath/b.dart', '''
+library B;
+int T0 = 0;
+F0() {}
+class X {
+  X.c();
+  X._d0();
+  z0() {
+    X._d0();
+  }
+}
+''');
+    await computeSuggestions('''
+import 'b.dart';
+var m0;
+void f() {
+  new X.^
+}
+''');
+    assertResponse(r'''
+suggestions
+  c
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_importedClass_unresolved() async {
+    newFile('$testPackageLibPath/b.dart', '''
+library B;
+int T0 = 0;
+F0() {}
+class X {
+  X.c();
+  X._d0();
+  z0() {
+    X._d0();
+  }
+}
+''');
+    await computeSuggestions('''
+import 'b.dart';
+var m0;
+void f() {
+  new X.^
+}
+''');
+    assertResponse(r'''
+suggestions
+  c
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_importedFactory() async {
+    newFile('$testPackageLibPath/b.dart', '''
+library B;
+int T0 = 0;
+F0() {}
+class X {
+  factory X.c() {
+    return X._d0();
+  }
+  factory X._d0() {
+    return X.c();
+  }
+  z0() {}
+}
+''');
+    await computeSuggestions('''
+import 'b.dart';
+var m0;
+void f() {
+  new X.^
+}
+''');
+    assertResponse(r'''
+suggestions
+  c
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_importedFactory2() async {
+    await computeSuggestions('''
+void f() {
+  new String.fr^omCharCodes([]);
+}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+  right: 11
+suggestions
+  fromCharCodes
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_interfaceContextType_withTypeArguments() async {
+    await computeSuggestions('''
+class A<T> {
+  A.named();
+}
+
+void f() {
+  A<int> v = A<int>.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_interfaceTypeContext() async {
+    await computeSuggestions('''
+class A {
+  A();
+  A.named();
+}
+
+void f() {
+  int v = A.^;
+}
+''');
+    assertResponse(r'''
+suggestions
+  named
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_interfaceTypeContext_partial() async {
+    await computeSuggestions('''
+class A {
+  A();
+  A.named();
+}
+
+void f() {
+  int v = A.na^;
+}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  named
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_localClass() async {
+    await computeSuggestions('''
+int T0 = 0;
+F0() {}
+class X {
+  X.c();
+  X._d();
+  z0() {}
+}
+void f() {
+  new X.^
+}
+''');
+    assertResponse(r'''
+suggestions
+  _d
+    kind: constructorInvocation
+  c
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_localFactory() async {
+    await computeSuggestions('''
+int T0;
+F0() {}
+class X {
+  factory X.c();
+  factory X._d();
+  z0() {}
+}
+void f() {
+  new X.^
+}
+''');
+    assertResponse(r'''
+suggestions
+  _d
+    kind: constructorInvocation
+  c
+    kind: constructorInvocation
 ''');
   }
 }
