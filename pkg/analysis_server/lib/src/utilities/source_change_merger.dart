9a455c809bb877a37a0363bb45bd35741afa4cb6
Danny Tuppeny <danny@tuppeny.com>
2023-05-19 15:30:46 +0000

    [analysis_server] Add a utility to merge SourceFileEdits from multiple passes of edits
    This is work towards https://github.com/dart-lang/sdk/issues/47968. The fix processor may need to be called multiple times to fix all issues because some fixes may produce code that can be fixed by another (for example one inserting "final" and then another replacing it with "const").
    
    In order to be able to provide multiple passes of edits to LSP, we need to merge them together because LSP's edits are not applied sequentially but must all represent valid locations in the original source file.
    
    This class is currently unused (besides tests).
    
    Change-Id: Ia006ff8044505c6d109f6480f7bcce4da4669cdb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/304180
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/analysis_server/lib/src/utilities/source_change_merger.dart
@@ -0,0 +1,171 @@
+// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:math' as math;
+
+import 'package:analyzer_plugin/protocol/protocol_common.dart';
+import 'package:collection/collection.dart';
+
+/// A helper to merge [SourceFileEdit]s that may have been made in multiple
+/// steps.
+///
+/// Edits to be merged must meet some criteria:
+///
+/// - Multiple [SourceFileEdit]s for the same file are assumed to be in order
+///   and each one assumes any earlier [SourceFileEdit] has been applied.
+/// - All edits within a [SourceFileEdit] are sorted from highest to lowest
+///   offsets so that there is no ambiguity about how one edit may affect the
+///   range of another.
+class SourceChangeMerger {
+  /// A buffer that contains debug information about the re-ordering and merging
+  /// of edits.
+  ///
+  /// This can be used in tests to provide more details about failures.
+  final StringBuffer? debugBuffer;
+
+  SourceChangeMerger({this.debugBuffer});
+
+  /// Merges a set of edits in-place.
+  List<SourceFileEdit> merge(List<SourceFileEdit> edits) {
+    final results = <SourceFileEdit>[];
+
+    for (final entry in edits.groupListsBy((edit) => edit.file).entries) {
+      final file = entry.key;
+      final editLists = entry.value;
+
+      // If this file only had a single set of edits, we don't need to do
+      // anything.
+      if (editLists.length == 1) {
+        results.add(editLists.single);
+        continue;
+      }
+
+      // Flatten all sets into a single set of edits. Because we know all
+      // lists and edits can be applied sequentially this is safe, however it
+      // can lose the property of all edits being ordered last-to-first which is
+      // something we will fix as part of sorting/merging.
+      var edits = editLists.expand((edits) => edits.edits).toList();
+
+      debugBuffer?.writeln(file);
+      _debugEdits('Original', edits);
+
+      _reorder(edits);
+      _debugEdits('Reordered', edits);
+
+      _merge(edits);
+      _debugEdits('Merged', edits);
+
+      results
+          .add(SourceFileEdit(file, editLists.first.fileStamp, edits: edits));
+    }
+
+    return results;
+  }
+
+  /// Writes [edits] into [debugBuffer] for debugging.
+  void _debugEdits(String editKind, List<SourceEdit> edits) {
+    final debugBuffer = this.debugBuffer;
+    if (debugBuffer == null) {
+      return;
+    }
+
+    debugBuffer.writeln('$editKind edits:');
+    for (final edit in edits) {
+      debugBuffer.writeln('   $edit');
+    }
+    debugBuffer.writeln('');
+  }
+
+  /// Merges (in-place) any sequential edits that are overlapping or touching.
+  ///
+  /// Overlapping/touching edits will be replaced with new edits that have the
+  /// same effect as applying the original edits sequentially to the source
+  /// string.
+  void _merge(List<SourceEdit> edits) {
+    for (var i = 0; i < edits.length - 1; i++) {
+      // "first" refers to position in the list (and order they were intended to
+      // be sequentially applied) and not necessarily offset/source order. Most
+      // edits will be in reverse order in the list.
+      final first = edits[i];
+      final second = edits[i + 1];
+
+      if (second.end < first.offset) {
+        // Since we know non-intersecting/touching edits are ordered correctly,
+        // the second one ending before the start of the first one means it does
+        // not require merging.
+        continue;
+      }
+
+      // Replace the first edit with a merged version and remove the second.
+      edits[i] = _mergeEdits(first, second);
+      edits.removeAt(i + 1);
+      i--; // Process this one again in case it also overlaps the next one.
+    }
+  }
+
+  /// Merges [first] and [second] into a new [SourceEdit] that has the same
+  /// effect as applying [first] then [second] sequentially to the source
+  /// string.
+  SourceEdit _mergeEdits(SourceEdit first, SourceEdit second) {
+    // "first" refers to position in the list (and order they were intended to
+    // be sequentially applied) and not necessarily offset/source order. Most
+    // edits will be in reverse order in the list.
+
+    final actualStart = math.min(first.offset, second.offset);
+    final actualEnd = math.max(first.end, second.end - first.delta);
+    final length = actualEnd - actualStart;
+
+    // The new replacement text is made up of three possible parts:
+    // 1. The start of first that is not replaced by second (prefix)
+    // 2. The text from second (middle)
+    // 3. The end of first that is not replaced by second (suffix)
+    final prefix = second.offset > first.offset
+        ? first.replacement.substring(0, second.offset - first.offset)
+        : '';
+    final middle = second.replacement;
+    final suffix = second.end < first.offset + first.replacement.length
+        ? first.replacement.substring(second.end - first.offset)
+        : '';
+
+    return SourceEdit(actualStart, length, '$prefix$middle$suffix');
+  }
+
+  /// Re-orders edits (in-place) so that they are from latest offset to earliest
+  /// offset except in the case where they touch or intersect.
+  ///
+  /// Edits that are moved will be replaced by new edits with updated offsets
+  /// to preserve the same behaviour when applying edits sequentially.
+  ///
+  /// Edits that touch or intersect will not be reordered, but will end up
+  /// adjacent because other edits will be moved around them.
+  void _reorder(List<SourceEdit> edits) {
+    // This is essentially an in-place insertion sort, but the edits are mutated
+    // as they are swapped to preserve behaviour.
+    for (var i = 1; i < edits.length; i++) {
+      var current = edits[i];
+      var j = i - 1;
+      // If the current edit starts after the j edit, we should swap
+      // it to bring it earlier.
+      while (j >= 0 && current.offset >= edits[j].end + edits[j].delta) {
+        // Because edits[j] will no longer be applied before us and we know it
+        // would have been applied earlier in the file, we need to adjust our
+        // offset by its delta.
+        // If we need to change the offset, we must create a new edit and not
+        // mutate the original.
+        current = SourceEdit(
+          current.offset - edits[j].delta,
+          current.length,
+          current.replacement,
+        );
+        edits[j + 1] = edits[j];
+        j--;
+      }
+      edits[j + 1] = current;
+    }
+  }
+}
+
+extension on SourceEdit {
+  int get delta => replacement.length - length;
+}
---
+++ pkg/analysis_server/test/src/utilities/source_change_merger_test.dart
@@ -0,0 +1,492 @@
+// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:analysis_server/src/utilities/source_change_merger.dart';
+import 'package:analyzer_plugin/protocol/protocol_common.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(SourceChangeMergerTest);
+  });
+}
+
+@reflectiveTest
+class SourceChangeMergerTest {
+  void test_multipleFiles_mergeIndividually() {
+    final original = [
+      SourceFileEdit('fileA', -1, edits: [SourceEdit(4, 1, '4')]),
+      SourceFileEdit('fileB', -1, edits: [SourceEdit(3, 1, '3')]),
+      SourceFileEdit('fileA', -1, edits: [SourceEdit(3, 1, '3')]),
+      SourceFileEdit('fileB', -1, edits: [SourceEdit(2, 1, '2')]),
+    ];
+    final expected = [
+      SourceFileEdit('fileA', -1, edits: [SourceEdit(3, 2, '34')]),
+      SourceFileEdit('fileB', -1, edits: [SourceEdit(2, 2, '23')]),
+    ];
+    final merged = SourceChangeMerger().merge(original);
+    expect(merged, expected);
+  }
+
+  void test_multipleFiles_noMerge() {
+    final original = [
+      SourceFileEdit('fileA', -1, edits: [SourceEdit(2, 1, '2')]),
+      SourceFileEdit('fileB', -1, edits: [SourceEdit(1, 1, '1')]),
+    ];
+    final merged = SourceChangeMerger().merge(original);
+    // The resulting edits should be the same with no merging because they
+    // are different files.
+    expect(merged, original);
+  }
+
+  void test_notTouching_singlePass() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0-12-34-56789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'),
+          SourceEdit(3, 1, '-3'),
+          SourceEdit(1, 1, '-1'),
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_andLeading() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01236789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(4, 3, ''), // Delete 4-5
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_andLeadingAndTrailing() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0123789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(4, 4, ''), // Delete 4-56
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_andTrailing() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 3, ''), // Delete -56
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_end() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234-6789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 1, ''), // Delete 5
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_exactly() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '012346789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 2, ''), // Delete -5
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_inner() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234556789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '5-5'), // Replace 5 with 5-5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 1, ''), // Delete -
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondDeletesFirst_start() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0123456789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, ''), // Delete -
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondInsertsIntoFirst() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234-056789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 0, '0'), // Insert 0 between - and 5
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondInsertsIntoFirst2() {
+    verifyMerge(
+      start: 'AAAAAAAAAABBBBBBBBBB',
+      expected:
+          'AAAAAAAAAACCCCCCCCCCEEEEEEEEEEGGGGGGGGGGHHHHHHHHHHFFFFFFFFFFDDDDDDDDDDBBBBBBBBBB',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          // Insert CD between AB
+          SourceEdit(10, 0, 'CCCCCCCCCCDDDDDDDDDD'),
+        ]),
+        SourceFileEdit('', -1, edits: [
+          // Insert EF between CD
+          SourceEdit(20, 0, 'EEEEEEEEEEFFFFFFFFFF'),
+        ]),
+        SourceFileEdit('', -1, edits: [
+          // Insert GH between EF
+          SourceEdit(30, 0, 'GGGGGGGGGGHHHHHHHHHH'),
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_andLeading() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '012306789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(4, 3, '0'), // Replace 4-5 with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_andLeadingAndTrailing() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01230789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(4, 4, '0'), // Replace 4-56 with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_andTrailing() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '012340789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 3, '0'), // Replace -56 with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_end() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234-06789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 1, '0'), // Replace 5 with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_exactly() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0123406789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 2, '0'), // Replace -5 with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_inner() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '012345056789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '5-5'), // Replace 5 with 5-5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 1, '0'), // Replace - with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_overlap_secondReplacesFirst_start() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234056789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '0'), // Replace - with 0
+        ]),
+      ],
+    );
+  }
+
+  void test_touching_deletes() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '06789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          // Delete 5, 3, 1
+          SourceEdit(5, 1, ''),
+          SourceEdit(3, 1, ''),
+          SourceEdit(1, 1, ''),
+        ]),
+        SourceFileEdit('', -1, edits: [
+          // Delete 4, 2
+          SourceEdit(2, 1, ''),
+          SourceEdit(1, 1, ''),
+        ]),
+      ],
+    );
+  }
+
+  void test_touching_inserts() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0-1-2-3-4-56789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          // Insert - before 5, 3, 1
+          SourceEdit(5, 0, '-'),
+          SourceEdit(3, 0, '-'),
+          SourceEdit(1, 0, '-'),
+        ]),
+        SourceFileEdit('', -1, edits: [
+          // Insert - before 4, 2
+          SourceEdit(6, 0, '-'),
+          SourceEdit(3, 0, '-'),
+        ]),
+      ],
+    );
+  }
+
+  void test_touching_mixed() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '01234-60789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, ''), // Delete 5
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-6'), // Change 6 to -6
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(7, 0, '0'), // Insert 0 between -6 and 7
+        ]),
+      ],
+    );
+  }
+
+  void test_touching_replacements() {
+    verifyMerge(
+      start: '0123456789',
+      expected: '0-1-2-3-4-56789',
+      edits: [
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(5, 1, '-5'), // Replace 5 with -5
+          SourceEdit(3, 1, '-3'),
+          SourceEdit(1, 1, '-1'),
+        ]),
+        SourceFileEdit('', -1, edits: [
+          SourceEdit(6, 1, '-4'), // 4 is at 6 because of two minuses before it
+          SourceEdit(3, 1, '-2'), // 2 is at 3...
+        ]),
+      ],
+    );
+  }
+
+  /// Verifies merged edits for a single file.
+  void verifyMerge({
+    required String start,
+    required String expected,
+    required List<SourceFileEdit> edits,
+  }) {
+    // First, apply edits sequentially to ensure the test is configured
+    // correctly.
+    _validateEdits(edits);
+    _verifyAppliedEdits(
+      start,
+      edits,
+      expected,
+      'Applying edits sequentially did not produce expected results. '
+      'This indicates a bug in the test.',
+    );
+
+    // Take a copy of the edits before merging to ensure they are not mutated.
+    final originalJson = jsonEncode(edits);
+
+    final debugBuffer = StringBuffer();
+    final merged = SourceChangeMerger(debugBuffer: debugBuffer).merge(edits);
+
+    // Ensure the merger didn't mutate the originals.
+    expect(jsonEncode(edits), originalJson);
+
+    try {
+      _validateMergedEdits(merged);
+      _verifyAppliedEdits(
+        start,
+        merged,
+        expected,
+        'Applying merged edits did not produce expected results. '
+        'This indicates a bug in the merger.',
+      );
+    } catch (_) {
+      print(debugBuffer);
+      rethrow;
+    }
+  }
+
+  /// Verifies that a [SourceFileEdit] meets some expected criteria:
+  ///
+  /// - Edits are all for the same file
+  /// - Edits are ordered from latest offset to earliest
+  /// - No edits intersect
+  void _validateEdits(List<SourceFileEdit> edits) {
+    expect(
+      edits.map((edit) => edit.file).toSet(),
+      hasLength(1),
+      reason: 'All edits should be from the same file',
+    );
+
+    for (final fileEdit in edits) {
+      var lastOffset = fileEdit.edits.first.offset;
+      for (final edit in fileEdit.edits.skip(1)) {
+        expect(
+          edit.end,
+          lessThanOrEqualTo(lastOffset),
+          reason: 'Edits within a SourceFileEdit should be ordered from '
+              'last (highest offset) to first (lowest offset) and not overlap',
+        );
+        lastOffset = edit.offset;
+      }
+    }
+  }
+
+  /// Verifies that merged edits meet some expected criteria:
+  ///
+  /// - Each file appears only once.
+  /// - Edits are ordered from latest offset to earliest
+  /// - No edits intersect
+  /// - No edits touch
+  void _validateMergedEdits(List<SourceFileEdit> edits) {
+    expect(
+      edits.map((edit) => edit.file).toSet(),
+      hasLength(edits.length),
+      reason: 'Merged edits should only contain one SourceFileEdit per file',
+    );
+
+    for (final fileEdit in edits) {
+      var lastOffset = fileEdit.edits.first.offset;
+      for (final edit in fileEdit.edits.skip(1)) {
+        expect(
+          edit.end,
+          lessThan(lastOffset),
+          reason: 'Edits within a SourceFileEdit should be ordered from '
+              'last (highest offset) to first (lowest offset) and not touch or '
+              'overlap',
+        );
+        lastOffset = edit.offset;
+      }
+    }
+  }
+
+  /// Verifies that [edits] applied to [content] produce [expected].
+  void _verifyAppliedEdits(
+    String content,
+    List<SourceFileEdit> edits,
+    String expected,
+    String reason,
+  ) {
+    final result = edits.fold(
+      content,
+      (content, edit) => SourceEdit.applySequence(content, edit.edits),
+    );
+
+    expect(result, expected, reason: reason);
+  }
+}
