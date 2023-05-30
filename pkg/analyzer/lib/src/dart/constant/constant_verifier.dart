b9302286cdbae1ef82f7ec3bbd0927be7e34bc6a
Konstantin Shcheglov <scheglov@google.com>
2023-05-30 20:30:14 +0000

    Issue 52548. When checking constant pattern matching, union 'T?' bound with Null.
    Bug: https://github.com/dart-lang/sdk/issues/52548
    Change-Id: I103134c48ea30df95f8e710518b92360b6f13d98
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/306309
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/constant/constant_verifier.dart
+++ pkg/analyzer/lib/src/dart/constant/constant_verifier.dart
@@ -14,6 +14,7 @@ import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/element/type_provider.dart';
 import 'package:analyzer/error/error.dart';
@@ -479,7 +480,11 @@ class ConstantVerifier extends RecursiveAstVisitor<void> {
       } else if (valueType is TypeParameterTypeImpl) {
         final bound = valueType.promotedBound ?? valueType.element.bound;
         if (bound != null && !hasTypeParameterReference(bound)) {
-          return _canBeEqual(constantType, bound);
+          final lowestBound =
+              valueType.nullabilitySuffix == NullabilitySuffix.question
+                  ? _typeSystem.makeNullable(bound)
+                  : bound;
+          return _canBeEqual(constantType, lowestBound);
         }
       } else if (valueType is FunctionType) {
         if (constantType.isDartCoreNull) {
