6ca0693c2862b9608bc092b1815c08cab996d202
Derek Xu <derekx@google.com>
2023-06-07 21:28:00 +0000

    Revert "[VM/Service] Add code for working with Perfetto protos to package:vm_service"
    This reverts commit 6c3c34560e6fdac211e362078557fdda254bafdd.
    
    Reason for revert: We will put the proto bindings in a separate package to prevent introducing new transitive dependencies into package:flutter.
    
    Original change's description:
    > [VM/Service] Add code for working with Perfetto protos to package:vm_service
    >
    > TEST=get_perfetto_vm_timeline_rpc_test and
    > get_perfetto_cpu_samples_rpc_test
    >
    > Change-Id: I23a30629f1d7a8cd7e2c9daad2d02fae4decb338
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/302421
    > Reviewed-by: Ben Konyi <bkonyi@google.com>
    > Commit-Queue: Derek Xu <derekx@google.com>
    
    Change-Id: I3bd9ce8a09e131271f76fe46ca92288f888d6147
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/307968
    Reviewed-by: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Derek Xu <derekx@google.com>

--- runtime/vm/protos/tools/compile_perfetto_protos.dart
+++ runtime/vm/protos/tools/compile_perfetto_protos.dart
@@ -8,13 +8,7 @@ import 'dart:io';
 Future<void> compilePerfettoProtos() async {
   final processResult = await Process.run(
     './tools/build.py',
-    [
-      '-mdebug',
-      '-ax64',
-      '--no-goma',
-      'runtime/vm:perfetto_protos_protozero',
-      'runtime/vm:perfetto_protos_dart'
-    ],
+    ['-mdebug', '-ax64', '--no-goma', 'runtime/vm:perfetto_protos'],
   );
 
   final int exitCode = processResult.exitCode;
@@ -29,21 +23,19 @@ Future<void> compilePerfettoProtos() async {
   }
 }
 
-const noticesToPrepend = r'''
-// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+Future<void> copyPerfettoProtoHeaders() async {
+  final copySource = Directory('./out/DebugX64/gen/runtime/vm/protos').path;
+  final copyDestination = Directory('./runtime/vm').path;
 
-// IMPORTANT: This file should only ever be modified by modifying the
-// corresponding .proto file and then running
-// `dart runtime/vm/protos/tools/compile_perfetto_protos.dart` from the SDK root
-// directory.
-''';
-
-Future<void> copyGeneratedFiles(
-    {required Directory source, required Directory destination}) async {
-  final executable = 'cp';
-  final args = ['-R', source.path, destination.path];
+  late final executable;
+  late final args;
+  if (Platform.operatingSystem == 'windows') {
+    executable = 'xcopy';
+    args = [copySource, copyDestination, '/e', '/i'];
+  } else {
+    executable = 'cp';
+    args = ['-R', copySource, copyDestination];
+  }
   final processResult = await Process.run(executable, args);
 
   final int exitCode = processResult.exitCode;
@@ -58,46 +50,22 @@ Future<void> copyGeneratedFiles(
   }
 
   for (final file
-      in Directory('${destination.path}/protos').listSync(recursive: true)) {
-    if (!(file is File &&
-        (file.path.endsWith('.pbzero.h') ||
-            file.path.endsWith('.pb.dart') ||
-            file.path.endsWith('.pbenum.dart') ||
-            file.path.endsWith('.pbjson.dart') ||
-            file.path.endsWith('.pbserver.dart')))) {
+      in Directory('./runtime/vm/protos').listSync(recursive: true)) {
+    if (!(file is File) || !file.path.endsWith('.pbzero.h')) {
       continue;
     }
-    if (file.path.endsWith('.pbenum.dart') &&
-        file.readAsStringSync().indexOf('class') == -1) {
-      // Sometimes .pbenum.dart files that are effictively empty get generated,
-      // so we delete them.
-      file.deleteSync();
-      continue;
-    }
+    final contentsIncludingPrependedNotices = r'''
+// Copyright (c) 2023, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
-    final contentsIncludingPrependedNotices =
-        noticesToPrepend + file.readAsStringSync();
+// IMPORTANT: This file should only ever be modified by modifying the
+// corresponding .proto file and then running
+// `dart runtime/vm/protos/tools/compile_perfetto_protos.dart` from the SDK root
+// directory.
+''' +
+        file.readAsStringSync();
     file.writeAsStringSync(contentsIncludingPrependedNotices, flush: true);
-  }
-}
-
-void createFileThatExportsAllGeneratedDartCode() {
-  final file = File('./pkg/vm_service/lib/vm_service_protos.dart');
-  if (!file.existsSync()) {
-    file.createSync();
-  }
-  file.writeAsStringSync(noticesToPrepend + '\n');
-
-  final generatedDartFilePaths =
-      Directory('./pkg/vm_service/lib/src/protos/perfetto')
-          .listSync(recursive: true)
-          .where((entity) => entity is File)
-          .map((file) => file.path)
-          .toList();
-  generatedDartFilePaths.sort();
-  for (final path in generatedDartFilePaths) {
-    final pathToExport = path.replaceAll('./pkg/vm_service/lib/', '');
-    file.writeAsStringSync("export '$pathToExport';\n", mode: FileMode.append);
   }
 }
 
@@ -106,23 +74,6 @@ main(List<String> files) async {
     print('Error: this tool must be run from the root directory of the SDK.');
     return;
   }
-  if (!Platform.isLinux) {
-    // TODO(derekx): The compilation of protoc fails on MacOS due to the error
-    // "'sprintf' is deprecated". When bumping protobuf, check if this problem
-    // has been resolved.
-    print('Error: this tool can only be run on Linux because protoc has '
-        'compatibility issues on other platforms.');
-    return;
-  }
-
   await compilePerfettoProtos();
-  await copyGeneratedFiles(
-    destination: Directory('./runtime/vm'),
-    source: Directory('./out/DebugX64/gen/runtime/vm/protos'),
-  );
-  await copyGeneratedFiles(
-    destination: Directory('./pkg/vm_service/lib/src'),
-    source: Directory('./out/DebugX64/gen/pkg/vm_service/lib/src/protos'),
-  );
-  createFileThatExportsAllGeneratedDartCode();
+  await copyPerfettoProtoHeaders();
 }
