12d0d34b00686777ec9287864daaa191bd7b2463
Jens Johansen <jensj@google.com>
2023-06-19 11:32:27 +0000

    [VM] Add token position to type argument AssertAssignable call
    This CL adds the token position of the checked parameter when doing the
    AssertAssignable for better stacktraces when it fails.
    
    E.g.
    
    ```
    $ cat t.dart
    class A {
      void takesA(A a) {
        print("Hello A");
      }
    }
    class B extends A {
      void takesA(covariant B b) {
        print("Hello B");
      }
    }
    
    void main() {
      A a = new A();
      A b = new B();
      b.takesA(b);
      b.takesA(a);
    }
    ```
    
    Before fix:
    
    ```
    $ out/ReleaseX64/dart t.dart
    Hello B
    Unhandled exception:
    type 'A' is not a subtype of type 'B' of 'b'
    #0      B.takesA (file:///path/to/t.dart)
    #1      main (file:///path/to/t.dart:16:5)
    [...]
    ```
    
    With fix:
    
    ```
    $ out/ReleaseX64/dart t.dart
    Hello B
    Unhandled exception:
    type 'A' is not a subtype of type 'B' of 'b'
    #0      B.takesA (file:///path/to/t.dart:7:27)
    #1      main (file:///path/to/t.dart:16:5)
    [...]
    ```
    
    TEST=runtime/tests/vm/dart/checked_parameter_assert_assignable_stacktrace_test.dart
    
    Change-Id: I6409f6d9c41e9825391957f0df2b92b48ed0eb7a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/302203
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>

---
+++ runtime/tests/vm/dart_2/checked_parameter_assert_assignable_stacktrace_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// @dart = 2.9
+
+import 'causal_stacks/utils.dart' show assertStack, IGNORE_REMAINING_STACK;
+
+class A {
+  void takesA(A a) {
+    print("Hello A");
+  }
+}
+
+class B extends A {
+  void takesA(covariant B b) {
+    print("Hello B");
+  }
+}
+
+StackTrace trace = null;
+
+void main() {
+  A a = new A();
+  A b = new B();
+  try {
+    b.takesA(a);
+  } catch (e, st) {
+    trace = st;
+  }
+  assertStack(const <String>[
+    r'^#0      B.takesA \(.*/checked_parameter_assert_assignable_stacktrace_test.dart:14(:27)?\)$',
+    r'^#1      main \(.*/checked_parameter_assert_assignable_stacktrace_test.dart:25(:7)?\)$',
+    IGNORE_REMAINING_STACK,
+  ], trace);
+}
