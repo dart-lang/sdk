7ecc57ac9e10ed1d43ba30a2015c4d35cb8f3885
Konstantin Shcheglov <scheglov@google.com>
2023-06-22 01:19:08 +0000

    [CMSR] Split into separate files.
    Change-Id: I45f1b13a2a025887ee7eb05179934f219097f67c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/310776
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/test/src/services/refactoring/convert_selected_formal_parameters_to_named_test.dart
+++ pkg/analysis_server/test/src/services/refactoring/convert_selected_formal_parameters_to_named_test.dart
@@ -2,226 +2,19 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analysis_server/src/services/refactoring/convert_formal_parameters.dart';
-import 'package:test/test.dart';
+import 'package:analysis_server/src/services/refactoring/convert_selected_formal_parameters_to_named.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'refactoring_test_support.dart';
 
 void main() {
   defineReflectiveSuite(() {
-    defineReflectiveTests(ConvertAllFormalParametersToNamedTest);
     defineReflectiveTests(ConvertSelectedFormalParametersToNamedTest);
   });
 }
 
 @reflectiveTest
-class ConvertAllFormalParametersToNamedTest
-    extends _ConvertFormalParametersToNamed {
-  @override
-  String get refactoringName => ConvertAllFormalParametersToNamed.commandName;
-
-  Future<void> test_formalParameters_optionalNamed() async {
-    addTestSource(r'''
-void ^test({int? a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int? a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> test_formalParameters_optionalPositional() async {
-    addTestSource(r'''
-void ^test([int a]) {}
-
-void f() {
-  test(0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> test_formalParameters_requiredNamed() async {
-    addTestSource(r'''
-void ^test({required int? a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int? a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> test_formalParameters_requiredPositional() async {
-    addTestSource(r'''
-void ^test(int a) {}
-
-void f() {
-  test(0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> test_multiple_files() async {
-    // TODO(scheglov) Unify behind `testPackageLibPath`
-    final a = getFile('$projectFolderPath/lib/a.dart');
-    addSource(a.path, r'''
-import 'main.dart';
-
-void f2() {
-  test(1);
-}
-''');
-
-    addTestSource(r'''
-void ^test(int a) {}
-
-void f() {
-  test(0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-
-    // TODO(scheglov) Ask me, if you want more of this opinion.
-    // This is bad code.
-    // I don't like using content for verifying refactoring results.
-    // We need to check all changes, without a way to check only some portion.
-    // See how _writeSourceChangeToBuffer is done.
-    //
-    // And addSource() above is another hack that we rely on to support these
-    // checks here.
-    // I don't like these too.
-    // We have newFile() already, this should be enough.
-    // Don't invent more way to add files.
-    // I worked hard in DAS legacy tests to get away from it.
-    // Don't add them back.
-    _assertTextExpectation(content[a.path]!, r'''
-import 'main.dart';
-
-void f2() {
-  test(a: 1);
-}
-''');
-  }
-
-  Future<void> test_noTarget_argument() async {
-    addTestSource(r'''
-void test(int a) {}
-
-void f() {
-  test(42^);
-}
-''');
-
-    await _assertNoRefactoring();
-  }
-
-  Future<void> test_noTarget_returnType() async {
-    addTestSource(r'''
-^void test(int a) {}
-''');
-
-    await _assertNoRefactoring();
-  }
-
-  Future<void> test_target_methodInvocation_name() async {
-    addTestSource(r'''
-void test(int a) {}
-
-void f() {
-  ^test(0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> test_target_topFunctionDeclaration_name() async {
-    addTestSource(r'''
-void ^test(int a) {}
-
-void f() {
-  test(0);
-}
-''');
-
-    await _executeRefactoring(r'''
-void test({required int a}) {}
-
-void f() {
-  test(a: 0);
-}
-''');
-  }
-
-  Future<void> _assertNoRefactoring() async {
-    await initializeServer();
-
-    await expectNoCodeAction(
-      ConvertAllFormalParametersToNamed.constTitle,
-    );
-  }
-
-  Future<void> _executeRefactoring(String expected) async {
-    await initializeServer();
-
-    final codeAction = await expectCodeAction(
-      ConvertAllFormalParametersToNamed.constTitle,
-    );
-
-    await executeRefactor(codeAction);
-
-    _assertTextExpectation(content[mainFilePath]!, expected);
-  }
-}
-
-@reflectiveTest
-class ConvertSelectedFormalParametersToNamedTest
-    extends _ConvertFormalParametersToNamed {
+class ConvertSelectedFormalParametersToNamedTest extends RefactoringTest {
   @override
   String get refactoringName =>
       ConvertSelectedFormalParametersToNamed.commandName;
@@ -493,18 +286,6 @@ void f() {
 
     await executeRefactor(codeAction);
 
-    _assertTextExpectation(content[mainFilePath]!, expected);
-  }
-}
-
-abstract class _ConvertFormalParametersToNamed extends RefactoringTest {
-  /// TODO(scheglov) This is duplicate.
-  void _assertTextExpectation(String actual, String expected) {
-    if (actual != expected) {
-      print('-' * 64);
-      print(actual.trimRight());
-      print('-' * 64);
-    }
-    expect(actual, expected);
+    assertTextExpectation(content[mainFilePath]!, expected);
   }
 }
--- pkg/analysis_server/test/src/services/refactoring/test_all.dart
+++ pkg/analysis_server/test/src/services/refactoring/test_all.dart
@@ -4,12 +4,16 @@
 
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
-import 'convert_formal_parameters_test.dart' as convert_formal_parameters;
+import 'convert_all_formal_parameters_to_named_test.dart'
+    as convert_all_formal_parameters_to_named;
+import 'convert_selected_formal_parameters_to_named_test.dart'
+    as convert_selected_formal_parameters_to_named;
 import 'move_top_level_to_file_test.dart' as move_top_level_to_file;
 
 void main() {
   defineReflectiveSuite(() {
-    convert_formal_parameters.main();
+    convert_all_formal_parameters_to_named.main();
+    convert_selected_formal_parameters_to_named.main();
     move_top_level_to_file.main();
   }, name: 'refactoring');
 }
