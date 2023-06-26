d2f5b3bfab485631606e2305f23ef187dc226235
Anna Gringauze <annagrin@google.com>
2023-06-26 22:55:07 +0000

    [ddc] Add --canary flag to expression compiler worker
    Closes: https://github.com/dart-lang/sdk/issues/52776
    Change-Id: I639fe7fbadf3e938cc686aa8dbd2320a4cc7af7d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/311151
    Reviewed-by: Mark Zhou <markzipan@google.com>
    Commit-Queue: Anna Gringauze <annagrin@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>

---
+++ pkg/dev_compiler/test/expression_compiler/expression_compiler_worker_amd_canary_test.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:dev_compiler/dev_compiler.dart';
+import 'package:test/test.dart';
+
+import 'expression_compiler_worker_shared.dart';
+
+void main() async {
+  // Set to true to enable debug output
+  var debug = false;
+
+  group('canary -', () {
+    group('amd module format -', () {
+      for (var soundNullSafety in [true, false]) {
+        group('${soundNullSafety ? "sound" : "unsound"} null safety -', () {
+          runTests(
+            moduleFormat: ModuleFormat.amd,
+            soundNullSafety: soundNullSafety,
+            canaryFeatures: true,
+            verbose: debug,
+          );
+        });
+      }
+    });
+  });
+}
