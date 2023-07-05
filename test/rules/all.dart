2b7c8a587d3ff38660253c8e6f7caadb5bcb9576
Sam Rawlins <srawlins@google.com>
2023-07-05 10:14:22 -0700

    Move prefer_single_quotes tests (dart-lang/linter#4532)

--- test/rules/all.dart
+++ test/rules/all.dart
@@ -112,6 +112,7 @@ import 'prefer_interpolation_to_compose_strings_test.dart'
     as prefer_interpolation_to_compose_strings;
 import 'prefer_mixin_test.dart' as prefer_mixin;
 import 'prefer_relative_imports_test.dart' as prefer_relative_imports;
+import 'prefer_single_quotes_test.dart' as prefer_single_quotes;
 import 'prefer_spread_collections_test.dart' as prefer_spread_collections;
 import 'prefer_void_to_null_test.dart' as prefer_void_to_null;
 import 'provide_deprecation_message_test.dart' as provide_deprecation_message;
@@ -254,6 +255,7 @@ void main() {
   prefer_interpolation_to_compose_strings.main();
   prefer_mixin.main();
   prefer_relative_imports.main();
+  prefer_single_quotes.main();
   prefer_spread_collections.main();
   prefer_void_to_null.main();
   provide_deprecation_message.main();
