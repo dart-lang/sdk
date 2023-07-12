4333288aaa3de000ee38fcaa0d051cef41c300e4
Daco Harkes <dacoharkes@google.com>
2023-07-12 14:02:41 +0000

    [gardening] Declare resources in test
    And give test more time in tsan.
    
    Bug: https://github.com/dart-lang/sdk/issues/52917
    Change-Id: I1342612470d63233b265dc9063b0335ec8bef760
    Cq-Include-Trybots: luci.dart.try:vm-tsan-linux-release-x64-try,vm-aot-tsan-linux-release-x64-try
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/313383
    Commit-Queue: Daco Harkes <dacoharkes@google.com>
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- tests/standalone/io/https_client_certificate_test.dart
+++ tests/standalone/io/https_client_certificate_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// OtherResources=certificates/client_authority.pem
+// OtherResources=certificates/client1_key.pem
+// OtherResources=certificates/client1.pem
 // OtherResources=certificates/server_chain.pem
 // OtherResources=certificates/server_key.pem
 // OtherResources=certificates/trusted_certs.pem
