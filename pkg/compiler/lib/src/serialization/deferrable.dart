25a1d01084fe3a6667692a7caa663c1f32ea4120
Nate Biggs <natebiggs@google.com>
2023-07-14 01:29:36 +0000

    [dart2js] Remove need for individual closures in Deferrable serialized data.
    This new approach uses static tear-offs where possible to avoid allocating a new Closure for each Deferrable.
    
    Note: This doesn't scale to every case but as of today the one case not covered is a singleton and so only 1 closure should be allocated there regardless.
    
    Data from a fairly large app:
    Before:
    Total => 3.8GB
    Closures => 85.6MB
    Closure Context => 71.0MB
    
    After:
    Total => 3.7GB
    Closures => 28.9MB
    Closure Context => 49.8MB
    
    Diff:
    Closures => 56.7MB
    Closure Context => 21.2MB
    
    Change-Id: If233f958df2822708b51c0d14c7439b5d3a5a07b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/313340
    Reviewed-by: Stephen Adams <sra@google.com>
    Commit-Queue: Nate Biggs <natebiggs@google.com>

--- pkg/compiler/lib/src/serialization/deferrable.dart
+++ pkg/compiler/lib/src/serialization/deferrable.dart
@@ -19,6 +19,10 @@ import 'package:compiler/src/serialization/serialization.dart';
 ///  `Deferrable<E>` to initialize `_m1`. This internal constructor should be
 ///  called from the readFromSource method/factory to create the instance
 ///  of `C`. `d` should be obtained using [DataSourceReader.readDeferrable].
+///  Note: [DataSourceReader.readDeferrable] passes the correct
+///  [DataSourceReader] to the reader function so that the [DataSourceReader]
+///  does not have to be closed over. If necessary a static tear-off should be
+///  used as the argument so a closure isn't created for every read.
 /// 3) Any existing constructors of `C` should maintain the same signature
 ///  and initialize `_m1` passing `m` to [Deferrable.eager] where m is the value
 ///  previously used to initialize `m0`.
@@ -31,11 +35,14 @@ import 'package:compiler/src/serialization/serialization.dart';
 ///
 /// class Foo {
 ///   final Bar bar;
+///   final String name;
 ///
-///   Foo(this.bar);
+///   Foo(this.bar, this.name);
 ///
 ///   factory Foo.readFromSource(DataSourceReader reader) {
-///     return Foo(Bar.readFromSource(reader));
+///     final bar = Bar.readFromSource(reader);
+///     final name = reader.readString();
+///     return Foo(bar, name);
 ///   }
 /// }
 ///
@@ -45,22 +52,36 @@ import 'package:compiler/src/serialization/serialization.dart';
 ///   Bar get bar => _bar.loaded();
 ///   final Deferrable<Bar> _bar;
 ///
-///   Foo(Bar bar) : _bar = Deferrable.eager(bar);
-///   Foo._deserialized(this._bar);
+///   String get name => _name.loaded();
+///   final Deferrable<String> _name;
+///
+///   Foo(Bar bar, String name) :
+///       _bar = Deferrable.eager(bar), _name = Deferrable.eager(name);
+///   Foo._deserialized(this._bar, this._name);
+///
+///   static String readName(DataSourceReader reader) => reader.readString();
 ///
 ///   factory Foo.readFromSource(DataSourceReader reader) {
-///     return Foo._deserialized(
-///         reader.readDeferrable(() => Bar.readFromSource(reader)));
+///     final bar = reader.readDeferrable(Bar.readFromSource);
+///     final name = reader.readDeferrable(readName);
+///     return Foo._deserialized(bar, name);
 ///   }
 /// }
 abstract class Deferrable<E> {
   E loaded();
 
-  factory Deferrable.deferred(DataSourceReader reader, E f(), int offset,
+  factory Deferrable.deferred(
+          DataSourceReader reader, E f(DataSourceReader source), int offset,
           {bool cacheData = true}) =>
       cacheData
           ? _DeferredCache(reader, f, offset)
           : _Deferred(reader, f, offset);
+  static Deferrable<E> deferredWithArg<E, A>(DataSourceReader reader,
+          E f(DataSourceReader source, A arg), A arg, int offset,
+          {bool cacheData = true}) =>
+      cacheData
+          ? _DeferredCacheWithArg(reader, f, arg, offset)
+          : _DeferredWithArg(reader, f, arg, offset);
   const factory Deferrable.eager(E data) = _Eager;
 
   const Deferrable();
@@ -73,12 +94,48 @@ class _Eager<E> extends Deferrable<E> {
   const _Eager(this._data);
 }
 
-class _Deferred<E> extends Deferrable<E> {
+class _DeferredWithArg<E, A> extends Deferrable<E> {
   final DataSourceReader _reader;
-  final E Function() _dataLoader;
+  final E Function(DataSourceReader source, A arg) _dataLoader;
+  final A _arg;
   final int _dataOffset;
   @override
-  E loaded() => _reader.readWithOffset(_dataOffset, _dataLoader);
+  E loaded() =>
+      _reader.readWithOffset(_dataOffset, () => _dataLoader(_reader, _arg));
+  _DeferredWithArg(this._reader, this._dataLoader, this._arg, this._dataOffset);
+}
+
+class _DeferredCacheWithArg<E, A> extends Deferrable<E> {
+  final int _dataOffset;
+  // Below fields are nullable so they can be cleared after loading.
+  DataSourceReader? _reader;
+  E Function(DataSourceReader source, A arg)? _dataLoader;
+  A? _arg;
+  late final E _data = _loadData();
+
+  @override
+  E loaded() => _data;
+
+  E _loadData() {
+    final reader = _reader!;
+    final dataLoader = _dataLoader!;
+    final arg = _arg!;
+    _reader = null;
+    _dataLoader = null;
+    _arg = null;
+    return reader.readWithOffset(_dataOffset, () => dataLoader(reader, arg));
+  }
+
+  _DeferredCacheWithArg(
+      this._reader, this._dataLoader, this._arg, this._dataOffset);
+}
+
+class _Deferred<E> extends Deferrable<E> {
+  final DataSourceReader _reader;
+  final E Function(DataSourceReader source) _dataLoader;
+  final int _dataOffset;
+  @override
+  E loaded() => _reader.readWithOffset(_dataOffset, () => _dataLoader(_reader));
   _Deferred(this._reader, this._dataLoader, this._dataOffset);
 }
 
@@ -86,7 +143,7 @@ class _DeferredCache<E> extends Deferrable<E> {
   final int _dataOffset;
   // Below fields are nullable so they can be cleared after loading.
   DataSourceReader? _reader;
-  E Function()? _dataLoader;
+  E Function(DataSourceReader source)? _dataLoader;
   late final E _data = _loadData();
 
   @override
@@ -97,7 +154,7 @@ class _DeferredCache<E> extends Deferrable<E> {
     final dataLoader = _dataLoader!;
     _reader = null;
     _dataLoader = null;
-    return reader.readWithOffset(_dataOffset, dataLoader);
+    return reader.readWithOffset(_dataOffset, () => dataLoader(reader));
   }
 
   _DeferredCache(this._reader, this._dataLoader, this._dataOffset);
