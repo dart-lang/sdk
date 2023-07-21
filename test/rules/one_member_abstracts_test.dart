1a9e3fd276ff68beea0f4ede21f08e9b84fb0220
Sam Rawlins <srawlins@google.com>
2023-07-21 11:30:10 -0700

    Move one_member_abstracts tests (dart-lang/linter#4598)

--- test/rules/one_member_abstracts_test.dart
+++ test/rules/one_member_abstracts_test.dart
@@ -17,6 +17,69 @@ class OneMemberAbstractsTest extends LintRuleTest {
   @override
   String get lintRule => 'one_member_abstracts';
 
+  test_oneMember_abstract() async {
+    await assertDiagnostics(r'''
+abstract class C {
+  void m();
+}
+''', [
+      lint(15, 1),
+    ]);
+  }
+
+  test_oneMember_abstractGetter() async {
+    await assertNoDiagnostics(r'''
+abstract class C {
+  String get s;
+}
+''');
+  }
+
+  test_oneMember_appliedMixin() async {
+    await assertNoDiagnostics(r'''
+abstract class C with M {
+  void m2();
+}
+
+mixin M {
+  void m1();
+}
+''');
+  }
+
+  test_oneMember_extendedType() async {
+    await assertNoDiagnostics(r'''
+abstract class D extends C {
+  int m2();
+}
+
+abstract class C {
+  int m2() => 42;
+}
+''');
+  }
+
+  test_oneMember_implementedInterface() async {
+    await assertNoDiagnostics(r'''
+abstract class D implements C {
+  void m3();
+}
+
+abstract class C {
+  void m1();
+  void m2();
+}
+''');
+  }
+
+  test_oneMember_nonAbstract() async {
+    await assertNoDiagnostics(r'''
+abstract class C {
+  int f() => 42;
+}
+''');
+  }
+
   test_sealed_concreteMethod_noDiagnostic() async {
     await assertNoDiagnostics(r'''
 sealed class C {
@@ -31,5 +94,26 @@ sealed class C {
   void f();
 }
 ''');
+  }
+
+  test_twoMembers() async {
+    await assertNoDiagnostics(r'''
+abstract class C {
+  int x = 0;
+  int f();
+}
+''');
+  }
+
+  test_zeroMember_extendedTypeHasOneMember() async {
+    await assertDiagnostics(r'''
+abstract class D extends C {}
+
+abstract class C {
+  void m();
+}
+''', [
+      lint(46, 1),
+    ]);
   }
 }
