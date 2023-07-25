fb9db32ac88a658d0e52b596bc2004f44bd4f053
Sam Rawlins <srawlins@google.com>
2023-07-25 10:59:25 -0700

    Move use_rethrow_when_possible tests (dart-lang/linter#4606)
    * Move use_rethrow_when_possible tests
    
    * remove LINT

--- test/rules/all.dart
+++ test/rules/all.dart
@@ -195,6 +195,7 @@ import 'use_key_in_widget_constructors_test.dart'
 import 'use_late_for_private_fields_and_variables_test.dart'
     as use_late_for_private_fields_and_variables;
 import 'use_named_constants_test.dart' as use_named_constants;
+import 'use_rethrow_when_possible_test.dart' as use_rethrow_when_possible;
 import 'use_setters_to_change_properties_test.dart'
     as use_setters_to_change_properties;
 import 'use_string_buffers_test.dart' as use_string_buffers;
@@ -344,6 +345,7 @@ void main() {
   use_key_in_widget_constructors.main();
   use_late_for_private_fields_and_variables.main();
   use_named_constants.main();
+  use_rethrow_when_possible.main();
   use_setters_to_change_properties.main();
   use_string_buffers.main();
   use_super_parameters.main();
