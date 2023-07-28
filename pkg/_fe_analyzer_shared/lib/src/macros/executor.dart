91f389ed6a385e229dee15d2bd971cdb7a968759
Jake Macdonald <jakemac@google.com>
2023-07-28 22:36:44 +0000

    Drop use of package:_dart_internal in _fe_analyzer_shared
    This is largely a copy of https://github.com/leafpetersen/cast, with some extra magic.
    
    Change-Id: Iff92ac9a57b1cfbaa67fe15647aabb8858442b60
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/316880
    Reviewed-by: Leaf Petersen <leafp@google.com>
    Commit-Queue: Jake Macdonald <jakemac@google.com>

--- pkg/_fe_analyzer_shared/lib/src/macros/executor.dart
+++ pkg/_fe_analyzer_shared/lib/src/macros/executor.dart
@@ -3,12 +3,12 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:_fe_analyzer_shared/src/macros/executor/serialization_extensions.dart';
-import 'package:dart_internal/extract_type_arguments.dart';
 import 'package:meta/meta.dart';
 
 import 'api.dart';
 // ignore: unused_import
 import 'bootstrap.dart'; // For doc comments only.
+import 'executor/cast.dart';
 import 'executor/introspection_impls.dart';
 import 'executor/serialization.dart';
 
