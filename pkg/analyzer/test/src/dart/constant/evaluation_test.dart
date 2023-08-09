3e93bc89d4d483d3ea93a5fdf2336dca88c80ff9
Konstantin Shcheglov <scheglov@google.com>
2023-08-09 17:24:14 +0000

    Don't sort context messages when verify diagnostics.
    Change-Id: I03d9020db1d155798752c253d67ecee2bb36aa5e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/319383
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/dart/constant/evaluation_test.dart
+++ pkg/analyzer/test/src/dart/constant/evaluation_test.dart
@@ -3533,12 +3533,12 @@ main() {
         124,
         10,
         contextMessages: [
-          ExpectedContextMessage(testFile.path, 31, 14,
-              text:
-                  "The exception is 'The assertion in this constant expression failed.' and occurs here."),
           ExpectedContextMessage(testFile.path, 84, 1,
               text:
                   "The evaluated constructor 'A' is called by 'B' and 'B' is defined here."),
+          ExpectedContextMessage(testFile.path, 31, 14,
+              text:
+                  "The exception is 'The assertion in this constant expression failed.' and occurs here."),
         ],
       ),
     ]);
@@ -3599,12 +3599,12 @@ const b = const B();
         101,
         9,
         contextMessages: [
-          ExpectedContextMessage(testFile.path, 23, 19,
-              text:
-                  "The exception is 'The assertion in this constant expression failed.' and occurs here."),
           ExpectedContextMessage(testFile.path, 74, 1,
               text:
                   "The evaluated constructor 'A' is called by 'B' and 'B' is defined here."),
+          ExpectedContextMessage(testFile.path, 23, 19,
+              text:
+                  "The exception is 'The assertion in this constant expression failed.' and occurs here."),
         ],
       ),
     ]);
