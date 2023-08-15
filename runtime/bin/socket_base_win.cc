305153bcf938bb1291d68be5cc6e86e8577841f9
Niklas Schulze <me@jns.io>
2023-08-15 12:23:54 +0200

    Add assertions

--- runtime/bin/socket_base_win.cc
+++ runtime/bin/socket_base_win.cc
@@ -380,6 +380,12 @@ AddressList<InterfaceSocketAddress>* SocketBase::ListInterfaces(
   for (IP_ADAPTER_ADDRESSES* a = addrs; a != nullptr; a = a->Next) {
     for (IP_ADAPTER_UNICAST_ADDRESS* u = a->FirstUnicastAddress; u != nullptr;
          u = u->Next) {
+      ASSERT((a->Flags & IP_ADAPTER_IPV4_ENABLED && a->IfIndex != 0) ||
+             (a->Flags & IP_ADAPTER_IPV6_ENABLED && a->Ipv6IfIndex != 0));
+      ASSERT(a->IfIndex == a->Ipv6IfIndex ||
+             !!(a->Flags & IP_ADAPTER_IPV4_ENABLED) !=
+                 !!(a->Flags & IP_ADAPTER_IPV6_ENABLED));
+
       addresses->SetAt(i,
                        new InterfaceSocketAddress(
                            u->Address.lpSockaddr,
