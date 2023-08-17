51ac43644d153a778097fe4f82b8e3a1902750f4
Johnni Winther <johnniwinther@google.com>
2023-08-17 11:16:20 +0000

    [cfe] Support extension type declarations in additional exports
    Closes #53118
    
    Change-Id: I614c449dc1148fad40e9a99ff035efcf09593027
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/321161
    Commit-Queue: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Jens Johansen <jensj@google.com>

---
+++ pkg/front_end/testcases/inline_class/extension_types/export/main.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by b
+// BSD-style license that can be found in the LICENSE file.
+
+import 'main_lib1.dart';
+
+main() {
+  ExtensionType(0);
+}
