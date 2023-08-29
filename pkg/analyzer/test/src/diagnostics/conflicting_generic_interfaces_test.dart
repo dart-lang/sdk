d1e0879f87c025b867c63fdb50371670221715c7
Konstantin Shcheglov <scheglov@google.com>
2023-08-29 15:52:28 +0000

    Issue 53314. Merge Object and Object? in extension type interfaces.
    Bug: https://github.com/dart-lang/sdk/issues/53314
    Change-Id: Ib9cf8a5ba1b0873cbb6802934993abc80b989875
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/323081
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>

--- pkg/analyzer/test/src/diagnostics/conflicting_generic_interfaces_test.dart
+++ pkg/analyzer/test/src/diagnostics/conflicting_generic_interfaces_test.dart
@@ -47,6 +47,17 @@ class C extends A implements B {}
     ]);
   }
 
+  test_class_extends_implements_object_objectQuestion() async {
+    await assertErrorsInCode('''
+class A<T> {}
+class B implements A<Object> {}
+class C implements A<Object?> {}
+class D extends B implements C {}
+''', [
+      error(CompileTimeErrorCode.CONFLICTING_GENERIC_INTERFACES, 85, 1),
+    ]);
+  }
+
   test_class_extends_implements_optOut() async {
     noSoundNullSafety = false;
     newFile('$testPackageLibPath/a.dart', r'''
@@ -214,6 +225,17 @@ extension type C(Never it) implements A, B {}
 ''', [
       error(CompileTimeErrorCode.CONFLICTING_GENERIC_INTERFACES, 87, 1),
     ]);
+  }
+
+  test_extensionType_objectNone_objectQuestion() async {
+    await assertNoErrorsInCode('''
+extension type A(int? it) {}
+extension type B(int it) implements A, num {}
+''');
+    assertElementTypes(
+      findElement.extensionType('B').allSupertypes,
+      ['A', 'Object', 'num', 'Comparable<num>'],
+    );
   }
 
   test_mixin_on_implements() async {
