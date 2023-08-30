6a2c9cb826bb94f7c700e145a30d40d0a0e08d86
pq <pquitslund@google.com>
2023-08-30 17:35:43 +0000

    migrate `tighten_type_of_initializing_formals` nnbd experiment tests
    Fixes: https://github.com/dart-lang/linter/issues/4732
    
    Change-Id: Ia6c0c5c28ddd7dc35dfc4c45dbc28c77a3a70a68
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/323441
    Commit-Queue: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/linter/test/rules/tighten_type_of_initializing_formals_test.dart
+++ pkg/linter/test/rules/tighten_type_of_initializing_formals_test.dart
@@ -35,4 +35,66 @@ class C extends A {
       lint(107, 7),
     ]);
   }
+
+  test_thisInit_asserts() async {
+    await assertDiagnostics(r'''
+class A {
+  String? p;
+  A(this.p) : assert(p != null);
+  A.a(this.p) : assert(null != p);
+}
+''', [
+      lint(27, 6),
+      lint(62, 6),
+    ]);
+  }
+
+  test_thisInit_asserts_positionalParams() async {
+    await assertDiagnostics(r'''
+class A {
+  A(
+    this.p1,
+    String? p2,
+    this.p3, {
+    this.p4,
+    this.p5,
+  }) : assert(p1 != null),
+       assert(p2 != null),
+       assert(p4 != null);
+
+  String? p;
+  String? p1;
+  String? p2;
+  String? p3;
+  String? p4;
+  String? p5;
+}
+''', [
+      lint(19, 7),
+      lint(63, 7),
+    ]);
+  }
+
+  test_thisInit_noAssert() async {
+    await assertNoDiagnostics(r'''
+class A {
+  String? p;
+  A(this.p);
+}
+''');
+  }
+
+  test_thisInit_tightens() async {
+    await assertDiagnostics(r'''
+class A {
+  String? p;
+  A(String this.p) : assert(p != null);
+  A.a(String this.p) : assert(null != p); 
+}
+''', [
+      // No lint
+      error(WarningCode.UNNECESSARY_NULL_COMPARISON_TRUE, 53, 7),
+      error(WarningCode.UNNECESSARY_NULL_COMPARISON_TRUE, 93, 7),
+    ]);
+  }
 }
