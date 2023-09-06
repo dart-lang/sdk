b4309860f91ff1c3eaf5008946431aefc26c01b3
Sam Rawlins <srawlins@google.com>
2023-09-06 16:11:32 +0000

    [linter] Move null_closures tests
    Change-Id: I071bb39750c08f05137ba244298b3d13309b5585
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/324540
    Auto-Submit: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/test/rules/null_closures_test.dart
+++ pkg/linter/test/rules/null_closures_test.dart
@@ -77,6 +77,92 @@ class NullClosuresTest extends LintRuleTest {
   @override
   String get lintRule => 'null_closures';
 
+  test_futureWait_cleanUp_closure() async {
+    await assertNoDiagnostics(r'''
+void f() {
+  Future.wait([], cleanUp: (_) => print('clean'));
+}
+''');
+  }
+
+  test_futureWait_cleanUp_null() async {
+    await assertDiagnostics(r'''
+void f() {
+  Future.wait([], cleanUp: null);
+}
+''', [
+      lint(29, 13),
+    ]);
+  }
+
+  test_iterableFirstWhere_orElse_null() async {
+    await assertDiagnostics(r'''
+void f(List<int> list) {
+  list.firstWhere((e) => e.isEven, orElse: null);
+}
+''', [
+      lint(60, 12),
+    ]);
+  }
+
+  test_iterableSingleWhere_orElse_closure() async {
+    await assertNoDiagnostics(r'''
+void f(List<int?> list) {
+  list.singleWhere((e) => e?.isEven ?? false, orElse: () => null);
+}
+''');
+  }
+
+  test_iterableSingleWhere_orElse_null() async {
+    await assertDiagnostics(r'''
+void f(Set<int> set) {
+  set.singleWhere((e) => e.isEven, orElse: null);
+}
+''', [
+      lint(58, 12),
+    ]);
+  }
+
+  test_iterableWhere_noOrElse() async {
+    await assertNoDiagnostics(r'''
+void f(List<int> list) {
+  list.where((e) => e.isEven);
+}
+''');
+  }
+
+  test_listGenerate_closure() async {
+    await assertNoDiagnostics(r'''
+void f() {
+  new List.generate(3, (_) => null);
+}
+''');
+  }
+
+  test_mapKeys() async {
+    await assertNoDiagnostics(r'''
+void f(Map<int, int> map) {
+  map.keys;
+}
+''');
+  }
+
+  test_mapOtherMethod() async {
+    await assertNoDiagnostics(r'''
+void f(Map<int, int> map) {
+  map.addAll({});
+}
+''');
+  }
+
+  test_mapPutIfAbsent_closure() async {
+    await assertNoDiagnostics(r'''
+void f(Map<int, int?> map) {
+  map.putIfAbsent(7, () => null);
+}
+''');
+  }
+
   ///https://github.com/dart-lang/linter/issues/1414
   test_recursiveInterfaceInheritance() async {
     await assertDiagnostics(r'''
