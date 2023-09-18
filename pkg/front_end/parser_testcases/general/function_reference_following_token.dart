9394b30b779296c07971d3692db703c023b2b2a0
Alexander Thomas <athom@google.com>
2023-09-18 06:44:08 +0000

    Fix incorrect file reference in license headers
    Bug: b/286184681
    Change-Id: I903528c4adfbc576644aec7541903df6b9633e26
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/325523
    Reviewed-by: Jonas Termansen <sortie@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Alexander Thomas <athom@google.com>

--- pkg/front_end/parser_testcases/general/function_reference_following_token.dart
+++ pkg/front_end/parser_testcases/general/function_reference_following_token.dart
@@ -1,6 +1,6 @@
 // Copyright (c) 2021, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE.md file.
+// BSD-style license that can be found in the LICENSE file.
 
 // The test cases called `typeArgs_...` verify that `<` and `>` are treated as
 // delimiting type arguments when the `>` is followed by one of the tokens:
--- pkg/frontend_server/test/frontend_server_flutter_suite.dart
+++ pkg/frontend_server/test/frontend_server_flutter_suite.dart
@@ -1,6 +1,6 @@
 // Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE.md file.
+// BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async' show StreamSubscription, Timer;
 import 'dart:convert' show jsonEncode;
