dart-lang/sdk

commit ef1c3098a17c0f8db567187f446482d992c8ca66

[vm, compiler] Implement unboxed SIMD for RISC-V via lowering.
This relands commit 460bd7a03a2ef87663ad14a28db52d7ad93be8b5.

TEST=ci
Change-Id: I0b5e91a3b8b6e8afa3f889afa21afed7c1a7e62c
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/327821
Reviewed-by: Slava Egorov <vegorov@google.com>
Reviewed-by: Alexander Markov <alexmarkov@google.com>
Commit-Queue: Ryan Macnak <rmacnak@google.com>

runtime/vm/compiler/backend/il_x64.cc
@@ -648,7 +648,12 @@ void ConstantInstr::EmitMoveToLocation(FlowGraphCompiler* compiler,
       __ LoadObject(destination.reg(), value_);
     }
   } else if (destination.IsFpuRegister()) {
-    __ LoadDImmediate(destination.fpu_reg(), Double::Cast(value_).value());
+    if (representation() == kUnboxedFloat) {
+      __ LoadSImmediate(destination.fpu_reg(), Double::Cast(value_).value());
+    } else {
+      ASSERT(representation() == kUnboxedDouble);
+      __ LoadDImmediate(destination.fpu_reg(), Double::Cast(value_).value());
+    }
   } else if (destination.IsDoubleStackSlot()) {
     __ LoadDImmediate(FpuTMP, Double::Cast(value_).value());
     __ movsd(LocationToStackSlotAddress(destination), FpuTMP);
@@ -4639,35 +4644,6 @@ void SimdOpInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
 
 #undef DEFINE_EMIT
 
-LocationSummary* MathUnaryInstr::MakeLocationSummary(Zone* zone,
-                                                     bool opt) const {
-  ASSERT((kind() == MathUnaryInstr::kSqrt) ||
-         (kind() == MathUnaryInstr::kDoubleSquare));
-  const intptr_t kNumInputs = 1;
-  const intptr_t kNumTemps = 0;
-  LocationSummary* summary = new (zone)
-      LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
-  summary->set_in(0, Location::RequiresFpuRegister());
-  if (kind() == MathUnaryInstr::kDoubleSquare) {
-    summary->set_out(0, Location::SameAsFirstInput());
-  } else {
-    summary->set_out(0, Location::RequiresFpuRegister());
-  }
-  return summary;
-}
-
-void MathUnaryInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
-  if (kind() == MathUnaryInstr::kSqrt) {
-    __ sqrtsd(locs()->out(0).fpu_reg(), locs()->in(0).fpu_reg());
-  } else if (kind() == MathUnaryInstr::kDoubleSquare) {
-    XmmRegister value_reg = locs()->in(0).fpu_reg();
-    __ mulsd(value_reg, value_reg);
-    ASSERT(value_reg == locs()->out(0).fpu_reg());
-  } else {
-    UNREACHABLE();
-  }
-}
-
 LocationSummary* CaseInsensitiveCompareInstr::MakeLocationSummary(
     Zone* zone,
     bool opt) const {
@@ -4725,14 +4701,39 @@ LocationSummary* UnaryDoubleOpInstr::MakeLocationSummary(Zone* zone,
   LocationSummary* summary = new (zone)
       LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
   summary->set_in(0, Location::RequiresFpuRegister());
-  summary->set_out(0, Location::SameAsFirstInput());
+  summary->set_out(0, Location::RequiresFpuRegister());
   return summary;
 }
 
 void UnaryDoubleOpInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  ASSERT(representation() == kUnboxedDouble);
+  XmmRegister result = locs()->out(0).fpu_reg();
   XmmRegister value = locs()->in(0).fpu_reg();
-  ASSERT(locs()->out(0).fpu_reg() == value);
-  __ DoubleNegate(value, value);
+  switch (op_kind()) {
+    case Token::kNEGATE:
+      __ DoubleNegate(result, value);
+      break;
+    case Token::kSQRT:
+      __ sqrtsd(result, value);
+      break;
+    case Token::kSQUARE:
+      if (result != value) {
+        __ movsd(result, value);
+      }
+      __ mulsd(result, value);
+      break;
+    case Token::kTRUNCATE:
+      __ roundsd(result, value, compiler::Assembler::kRoundToZero);
+      break;
+    case Token::kFLOOR:
+      __ roundsd(result, value, compiler::Assembler::kRoundDown);
+      break;
+    case Token::kCEILING:
+      __ roundsd(result, value, compiler::Assembler::kRoundUp);
+      break;
+    default:
+      UNREACHABLE();
+  }
 }
 
 LocationSummary* MathMinMaxInstr::MakeLocationSummary(Zone* zone,
@@ -4957,40 +4958,6 @@ void DoubleToSmiInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
   __ SmiTag(result);
 }
 
-LocationSummary* DoubleToDoubleInstr::MakeLocationSummary(Zone* zone,
-                                                          bool opt) const {
-  const intptr_t kNumInputs = 1;
-  const intptr_t kNumTemps = 0;
-  LocationSummary* result = new (zone)
-      LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
-  result->set_in(0, Location::RequiresFpuRegister());
-  result->set_out(0, Location::RequiresFpuRegister());
-  return result;
-}
-
-void DoubleToDoubleInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
-  XmmRegister value = locs()->in(0).fpu_reg();
-  XmmRegister result = locs()->out(0).fpu_reg();
-  if (value != result) {
-    // Clear full register to avoid false dependency due to
-    // a partial access to XMM register in roundsd instruction.
-    __ xorps(result, result);
-  }
-  switch (recognized_kind()) {
-    case MethodRecognizer::kDoubleTruncateToDouble:
-      __ roundsd(result, value, compiler::Assembler::kRoundToZero);
-      break;
-    case MethodRecognizer::kDoubleFloorToDouble:
-      __ roundsd(result, value, compiler::Assembler::kRoundDown);
-      break;
-    case MethodRecognizer::kDoubleCeilToDouble:
-      __ roundsd(result, value, compiler::Assembler::kRoundUp);
-      break;
-    default:
-      UNREACHABLE();
-  }
-}
-
 LocationSummary* DoubleToFloatInstr::MakeLocationSummary(Zone* zone,
                                                          bool opt) const {
   const intptr_t kNumInputs = 1;
@@ -5019,6 +4986,16 @@ LocationSummary* FloatToDoubleInstr::MakeLocationSummary(Zone* zone,
 
 void FloatToDoubleInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
   __ cvtss2sd(locs()->out(0).fpu_reg(), locs()->in(0).fpu_reg());
+}
+
+LocationSummary* FloatCompareInstr::MakeLocationSummary(Zone* zone,
+                                                        bool opt) const {
+  UNREACHABLE();
+  return NULL;
+}
+
+void FloatCompareInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  UNREACHABLE();
 }
 
 LocationSummary* InvokeMathCFunctionInstr::MakeLocationSummary(Zone* zone,
@@ -5244,6 +5221,26 @@ void ExtractNthOutputInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
     Register in = in_loc.reg();
     __ movq(out, in);
   }
+}
+
+LocationSummary* UnboxLaneInstr::MakeLocationSummary(Zone* zone,
+                                                     bool opt) const {
+  UNREACHABLE();
+  return NULL;
+}
+
+void UnboxLaneInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  UNREACHABLE();
+}
+
+LocationSummary* BoxLanesInstr::MakeLocationSummary(Zone* zone,
+                                                    bool opt) const {
+  UNREACHABLE();
+  return NULL;
+}
+
+void BoxLanesInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  UNREACHABLE();
 }
 
 LocationSummary* TruncDivModInstr::MakeLocationSummary(Zone* zone,
@@ -6690,6 +6687,26 @@ void BooleanNegateInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
   ASSERT(input == result);
   __ xorq(result, compiler::Immediate(
                       compiler::target::ObjectAlignment::kBoolValueMask));
+}
+
+LocationSummary* BoolToIntInstr::MakeLocationSummary(Zone* zone,
+                                                     bool opt) const {
+  UNREACHABLE();
+  return NULL;
+}
+
+void BoolToIntInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  UNREACHABLE();
+}
+
+LocationSummary* IntToBoolInstr::MakeLocationSummary(Zone* zone,
+                                                     bool opt) const {
+  UNREACHABLE();
+  return NULL;
+}
+
+void IntToBoolInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
+  UNREACHABLE();
 }
 
 LocationSummary* AllocateObjectInstr::MakeLocationSummary(Zone* zone,

runtime/vm/compiler/assembler/assembler_arm64.cc
@@ -662,6 +662,20 @@ void Assembler::LoadImmediate(Register reg, int64_t imm) {
   }
 }
 
+void Assembler::LoadSImmediate(VRegister vd, float imms) {
+  int32_t imm32 = bit_cast<int32_t, float>(imms);
+  if (imm32 == 0) {
+    veor(vd, vd, vd);
+  } else if (constant_pool_allowed()) {
+    intptr_t index = object_pool_builder().FindImmediate(imm32);
+    intptr_t offset = target::ObjectPool::element_offset(index);
+    LoadSFromOffset(vd, PP, offset);
+  } else {
+    LoadImmediate(TMP, imm32);
+    fmovsr(vd, TMP);
+  }
+}
+
 void Assembler::LoadDImmediate(VRegister vd, double immd) {
   if (fmovdi(vd, immd)) return;
 

runtime/vm/compiler/assembler/assembler_arm64.h
@@ -2144,6 +2144,7 @@ class Assembler : public AssemblerBase {
     LoadImmediate(reg, imm.value());
   }
 
+  void LoadSImmediate(VRegister reg, float immd);
   void LoadDImmediate(VRegister reg, double immd);
   void LoadQImmediate(VRegister reg, simd128_value_t immq);
 

runtime/vm/compiler/assembler/assembler_ia32.cc
@@ -2293,6 +2293,13 @@ void Assembler::IncrementSmiField(const Address& dest, int32_t increment) {
   addl(dest, inc_imm);
 }
 
+void Assembler::LoadSImmediate(XmmRegister dst, float value) {
+  int32_t constant = bit_cast<int32_t, float>(value);
+  pushl(Immediate(constant));
+  movss(dst, Address(ESP, 0));
+  addl(ESP, Immediate(target::kWordSize));
+}
+
 void Assembler::LoadDImmediate(XmmRegister dst, double value) {
   // TODO(5410843): Need to have a code constants table.
   int64_t constant = bit_cast<int64_t, double>(value);

runtime/vm/compiler/assembler/assembler_ia32.h
@@ -824,6 +824,7 @@ class Assembler : public AssemblerBase {
     LoadImmediate(reg, immediate.value());
   }
 
+  void LoadSImmediate(XmmRegister dst, float value);
   void LoadDImmediate(XmmRegister dst, double value);
 
   void Drop(intptr_t stack_elements);

runtime/vm/compiler/assembler/assembler_riscv.cc
@@ -3657,6 +3657,18 @@ void Assembler::LoadImmediate(Register reg, intx_t imm) {
   }
 }
 
+void Assembler::LoadSImmediate(FRegister reg, float imms) {
+  int32_t imm = bit_cast<int32_t, float>(imms);
+  if (imm == 0) {
+    fmvwx(reg, ZR);  // bit_cast uint32_t -> float
+  } else {
+    ASSERT(constant_pool_allowed());
+    intptr_t index = object_pool_builder().FindImmediate(imm);
+    intptr_t offset = target::ObjectPool::element_offset(index);
+    LoadSFromOffset(reg, PP, offset);
+  }
+}
+
 void Assembler::LoadDImmediate(FRegister reg, double immd) {
   int64_t imm = bit_cast<int64_t, double>(immd);
   if (imm == 0) {

runtime/vm/compiler/assembler/assembler_riscv.h
@@ -432,6 +432,8 @@ class MicroAssembler : public AssemblerBase {
   void feqs(Register rd, FRegister rs1, FRegister rs2);
   void flts(Register rd, FRegister rs1, FRegister rs2);
   void fles(Register rd, FRegister rs1, FRegister rs2);
+  void fgts(Register rd, FRegister rs1, FRegister rs2) { flts(rd, rs2, rs1); }
+  void fges(Register rd, FRegister rs1, FRegister rs2) { fles(rd, rs2, rs1); }
   void fclasss(Register rd, FRegister rs1);
   // int32_t <- float
   void fcvtws(Register rd, FRegister rs1, RoundingMode rounding = RNE);
@@ -517,6 +519,8 @@ class MicroAssembler : public AssemblerBase {
   void feqd(Register rd, FRegister rs1, FRegister rs2);
   void fltd(Register rd, FRegister rs1, FRegister rs2);
   void fled(Register rd, FRegister rs1, FRegister rs2);
+  void fgtd(Register rd, FRegister rs1, FRegister rs2) { fltd(rd, rs2, rs1); }
+  void fged(Register rd, FRegister rs1, FRegister rs2) { fled(rd, rs2, rs1); }
   void fclassd(Register rd, FRegister rs1);
   // int32_t <- double
   void fcvtwd(Register rd, FRegister rs1, RoundingMode rounding = RNE);
@@ -1162,6 +1166,9 @@ class Assembler : public MicroAssembler {
                              Register index);
   void LoadSFromOffset(FRegister dest, Register base, int32_t offset);
   void LoadDFromOffset(FRegister dest, Register base, int32_t offset);
+  void LoadSFieldFromOffset(FRegister dest, Register base, int32_t offset) {
+    LoadSFromOffset(dest, base, offset - kHeapObjectTag);
+  }
   void LoadDFieldFromOffset(FRegister dest, Register base, int32_t offset) {
     LoadDFromOffset(dest, base, offset - kHeapObjectTag);
   }
@@ -1189,6 +1196,9 @@ class Assembler : public MicroAssembler {
     StoreToOffset(ZR, address);
   }
   void StoreSToOffset(FRegister src, Register base, int32_t offset);
+  void StoreSFieldToOffset(FRegister src, Register base, int32_t offset) {
+    StoreSToOffset(src, base, offset - kHeapObjectTag);
+  }
   void StoreDToOffset(FRegister src, Register base, int32_t offset);
   void StoreDFieldToOffset(FRegister src, Register base, int32_t offset) {
     StoreDToOffset(src, base, offset - kHeapObjectTag);
@@ -1348,6 +1358,7 @@ class Assembler : public MicroAssembler {
   // Note: the function never clobbers TMP, TMP2 scratch registers.
   void LoadImmediate(Register reg, intx_t imm);
 
+  void LoadSImmediate(FRegister reg, float imms);
   void LoadDImmediate(FRegister reg, double immd);
   void LoadQImmediate(FRegister reg, simd128_value_t immq);
 

runtime/vm/compiler/assembler/assembler_x64.cc
@@ -1463,6 +1463,17 @@ void Assembler::MoveImmediate(const Address& dst, const Immediate& imm) {
   }
 }
 
+void Assembler::LoadSImmediate(FpuRegister dst, float immediate) {
+  int32_t bits = bit_cast<int32_t>(immediate);
+  if (bits == 0) {
+    xorps(dst, dst);
+  } else {
+    intptr_t index = object_pool_builder().FindImmediate(bits);
+    LoadUnboxedSingle(
+        dst, PP, target::ObjectPool::element_offset(index) - kHeapObjectTag);
+  }
+}
+
 void Assembler::LoadDImmediate(FpuRegister dst, double immediate) {
   int64_t bits = bit_cast<int64_t>(immediate);
   if (bits == 0) {

commit 77de6783ead141e8a1efa3da4992e4be426162ce

[vm, compiler] Use SameAsFirstInput for squaring doubles.
This causes a movaps to be used instead of a movsd, which makes a big difference in NBody. Fixes a performance regression in ef1c3098a17c0f8db567187f446482d992c8ca66.

TEST=ci
Change-Id: I92239b4f82b6409b27cb7b7ecfad6c8ce242ca47
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/328743
Reviewed-by: Alexander Markov <alexmarkov@google.com>
Commit-Queue: Ryan Macnak <rmacnak@google.com>

runtime/vm/compiler/backend/il_x64.cc
@@ -4701,7 +4701,11 @@ LocationSummary* UnaryDoubleOpInstr::MakeLocationSummary(Zone* zone,
   LocationSummary* summary = new (zone)
       LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
   summary->set_in(0, Location::RequiresFpuRegister());
-  summary->set_out(0, Location::RequiresFpuRegister());
+  if (op_kind() == Token::kSQUARE) {
+    summary->set_out(0, Location::SameAsFirstInput());
+  } else {
+    summary->set_out(0, Location::RequiresFpuRegister());
+  }
   return summary;
 }
 
@@ -4717,9 +4721,7 @@ void UnaryDoubleOpInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
       __ sqrtsd(result, value);
       break;
     case Token::kSQUARE:
-      if (result != value) {
-        __ movsd(result, value);
-      }
+      ASSERT(result == value);
       __ mulsd(result, value);
       break;
     case Token::kTRUNCATE: