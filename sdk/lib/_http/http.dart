0fa13d4627c4c5874c679512fc2520ea1ac9557a
Brian Quinlan <bquinlan@google.com>
2023-10-13 17:59:08 +0000

    [doc/io] Add documentation for HttpSession
    Bug:https://github.com/dart-lang/sdk/issues/16228
    Change-Id: I4e70c0b15af7325d9bfaba6dcee6b3a4c68639c1
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/330169
    Reviewed-by: Alexander Aprelev <aam@google.com>
    Commit-Queue: Brian Quinlan <bquinlan@google.com>

--- sdk/lib/_http/http.dart
+++ sdk/lib/_http/http.dart
@@ -641,6 +641,33 @@ abstract interface class HeaderValue {
 }
 
 /// The [session][HttpRequest.session] of an [HttpRequest].
+///
+/// Stores arbitrary information about a browser session on the server. This
+/// information is stored in memory so it will be lost when the server exits.
+///
+/// A cookie named "DARTSESSID" is stored on the browser to associate the
+/// `HttpSession` with a particular client.
+///
+/// ```dart
+/// import 'dart:io';
+///
+/// void main() async {
+///   HttpServer.bind("localhost", 8080).then((server) {
+///     server.listen((request) {
+///       final session = request.session;
+///       if (session.isNew) {
+///         session["cart"] = [];
+///       }
+///       if (request.uri.queryParameters['buy'] != null) {
+///         final item = request.uri.queryParameters['buy'];
+///         session["cart"].add(item);
+///       }
+///       session["cart"].cast<String>().forEach(request.response.writeln);
+///       request.response.close();
+///     });
+///   });
+/// }
+/// ```
 abstract interface class HttpSession implements Map {
   /// The id of the current session.
   String get id;
