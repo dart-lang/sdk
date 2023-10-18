b2a158cc431a1fb0e96c3038e5441fa4de9205e7
Alexander Thomas <athom@google.com>
2023-10-18 06:59:38 +0000

    [frontend_server] Cleanup license headers
    Bug: b/286184681
    Change-Id: Ia203b87fecf17903b46bf929b22d3667b6ec2d59
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/330785
    Auto-Submit: Alexander Thomas <athom@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Commit-Queue: Johnni Winther <johnniwinther@google.com>

--- pkg/frontend_server/test/src/strong_components_test.dart
+++ pkg/frontend_server/test/src/strong_components_test.dart
@@ -1,6 +1,6 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'package:frontend_server/src/strong_components.dart';
 import 'package:kernel/ast.dart';
