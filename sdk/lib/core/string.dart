0ecb9ba545293e6a7322df2a0743e64be0d42341
Lasse R.H. Nielsen <lrn@google.com>
2023-10-20 07:57:18 +0000

    Update documentation on `fromEnvironment` constructors.
    CoreLibraryReviewExempt: Comments only.
    Change-Id: If2e080764bc98ee6cb7fe31dffe12c66144c7352
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/331182
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Slava Egorov <vegorov@google.com>

--- sdk/lib/core/string.dart
+++ sdk/lib/core/string.dart
@@ -138,20 +138,27 @@ abstract final class String implements Comparable<String>, Pattern {
   /// Creating a [String] with one half of a surrogate pair is allowed.
   external factory String.fromCharCode(int charCode);
 
-  /// The string value of the environment declaration [name].
+  /// Value for [name] in the compilation configuration environment declaration.
   ///
-  /// Environment declarations are provided by the surrounding system compiling
-  /// or running the Dart program. Declarations map a string key to a string
-  /// value.
+  /// The compilation configuration environment is provided by the
+  /// surrounding tools which are compiling or running the Dart program.
+  /// The environment is a mapping from a set of string keys to their associated
+  /// string value.
+  /// The string value, or lack of a value, associated with a [name]
+  /// must be consistent across all calls to `String.fromEnvironment`,
+  /// [int.fromEnvironment], [bool.fromEnvironment] and [bool.hasEnvironment]
+  /// in a single program.
   ///
-  /// If [name] is not declared in the environment, the result is instead
-  /// [defaultValue].
+  /// The result of invoking this constructor is the string associated with
+  /// the key [name]. If no value is associated with [name],
+  /// the result is instead the [defaultValue] string, which defaults to
+  /// the empty string.
   ///
-  /// Example of getting a value:
+  /// Example of looking up a value:
   /// ```dart
   /// const String.fromEnvironment("defaultFloo", defaultValue: "no floo")
   /// ```
-  /// In order to check whether a declaration is there at all, use
+  /// In order to check whether a value is there at all, use
   /// [bool.hasEnvironment]. Example:
   /// ```dart
   /// const maybeDeclared = bool.hasEnvironment("maybeDeclared")
@@ -168,12 +175,10 @@ abstract final class String implements Comparable<String>, Pattern {
   /// It may work as a non-constant invocation on some platforms which
   /// have access to compiler options at run-time, but most ahead-of-time
   /// compiled platforms will not have this information.
-  // The .fromEnvironment() constructors are special in that we do not want
-  // users to call them using "new". We prohibit that by giving them bodies
-  // that throw, even though const constructors are not allowed to have bodies.
-  // Disable those static errors.
-  //ignore: const_constructor_with_body
-  //ignore: const_factory
+  ///
+  /// The compilation configuration environment is not the same as the
+  /// environment variables of a POSIX process. Those can be accessed on
+  /// native platforms using `Platform.environment` from the `dart:io` library.
   external const factory String.fromEnvironment(String name,
       {String defaultValue = ""});
 
