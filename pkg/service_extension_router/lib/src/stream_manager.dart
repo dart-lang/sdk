c7f122ee9ebb5b168c5bdda195878080e68887c7
Dan Chevalier <danchevalier@google.com>
2023-10-24 21:58:21 +0000

    Initial Implementation for Service Extension Router
    This instance of service_extension_router has been hooked into DDS seperately. The vm_service and DDS tests still worked with the integration.
    
    There will still be some iteration and discussion to be had, but I'd like to get this up as the initial version.
    
    Bug: https://github.com/dart-lang/sdk/issues/53300
    Change-Id: Ic72b770a61b2fc61fbd7c4b47ff8996a90f78665
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/331820
    Reviewed-by: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Dan Chevalier <danchevalier@google.com>

--- pkg/service_extension_router/lib/src/stream_manager.dart
+++ pkg/service_extension_router/lib/src/stream_manager.dart
@@ -2,4 +2,85 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-class StreamManager {}
+import 'client.dart';
+
+// TODO(danchevalier): Add documentation before major release
+class StreamManager {
+  StreamManager({this.onStreamCancel});
+  Future<void> Function(Client client, String stream)? onStreamCancel;
+
+  final _streamListeners =
+      <String, List<Client>>{}; // TODO should this be set of StreamClient?
+  List<Client>? getListenersFor({required String stream}) =>
+      _streamListeners[stream];
+
+  /// Returns true if [client] is subscribed to [stream]
+  bool isSubscribed(Client client, String stream) {
+    return _streamListeners[stream]?.contains(client) ?? false;
+  }
+
+  /// Returns true if [stream] has any [ServiceExtensionClients] subscribed.
+  bool hasSubscriptions(String stream) {
+    return _streamListeners[stream]?.isNotEmpty ?? false;
+  }
+
+  /// Triggers [Client.streamNotify] for all clients subscribed
+  /// to [stream].
+  void postEvent(
+    String stream,
+    Map<String, Object?> data, {
+    Client? excludedClient,
+  }) {
+    final listeners = _streamListeners[stream] ?? const <Client>[];
+    for (final listener in listeners) {
+      if (listener == excludedClient) continue;
+      listener.streamNotify(stream, data);
+    }
+  }
+
+  /// Subscribes `client` to a stream.
+  ///
+  /// If `client` is the first client to listen to `stream`, DDS will send a
+  /// `streamListen` request for `stream` to the VM service.
+  void streamListen(
+    Client client,
+    String stream,
+  ) async {
+    _streamListeners.putIfAbsent(stream, () => <Client>[]);
+    if (_streamListeners[stream]!.contains(client)) {
+      throw StreamAlreadyListeningException(stream, client);
+    }
+    _streamListeners[stream]!.add(client);
+  }
+
+  /// Unsubscribes [client] from [stream].
+  Future<void> streamCancel(
+    Client client,
+    String stream,
+  ) async {
+    if (!_streamListeners.containsKey(stream)) return;
+
+    _streamListeners[stream]!.remove(client);
+    if (onStreamCancel != null) {
+      await onStreamCancel!(client, stream);
+    }
+  }
+
+  Future<void> onClientDisconnect(Client client) async {
+    await Future.wait([
+      for (final stream in _streamListeners.keys)
+        if (_streamListeners[stream]!.contains(client))
+          streamCancel(client, stream),
+    ]);
+  }
+}
+
+class StreamAlreadyListeningException implements Exception {
+  const StreamAlreadyListeningException(this.stream, this.client);
+  final String stream;
+  final Client client;
+
+  @override
+  String toString() =>
+      "Client, with hashCode ${client.hashCode}, is already subscribed to stream $stream";
+}
