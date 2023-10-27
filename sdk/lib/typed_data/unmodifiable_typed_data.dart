c87fec8663ee4869101f556df45a2c7d787405da
Stephen Adams <sra@google.com>
2023-10-27 15:22:54 +0000

    Revert "[typed_data] Deprecate unmodifiable views"
    This reverts commit b4bae467d4b62d07ba8f0d006a335029c8bac0cc.
    
    Reason for revert: Flutter plugin contains use of deprecated method.
    
    Original change's description:
    > [typed_data] Deprecate unmodifiable views
    >
    > CoreLibraryReviewExempt: Reviewed in https://dart-review.googlesource.com/c/sdk/+/321922
    > Bug: #53785
    > Change-Id: I23a064e76a4b359e804f41676b3b775dd02ea183
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/331723
    > Commit-Queue: Stephen Adams <sra@google.com>
    > Reviewed-by: Lasse Nielsen <lrn@google.com>
    
    Bug: #53785
    Change-Id: I01998a3d74681ef2d8c804f59a82d51a2e4290e7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/332580
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: William Hesse <whesse@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- sdk/lib/typed_data/unmodifiable_typed_data.dart
+++ sdk/lib/typed_data/unmodifiable_typed_data.dart
@@ -8,7 +8,7 @@ part of dart.typed_data;
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableByteBufferView.
-@Deprecated('No replacement')
+// TODO(53785): @Deprecated('No replacement')
 abstract final class UnmodifiableByteBufferView implements ByteBuffer {
   external factory UnmodifiableByteBufferView(ByteBuffer data);
 }
@@ -17,7 +17,7 @@ abstract final class UnmodifiableByteBufferView implements ByteBuffer {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableByteDataView.
-@Deprecated('Use ByteData.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use ByteData.asUnmodifiableView() instead')
 abstract final class UnmodifiableByteDataView implements ByteData {
   external factory UnmodifiableByteDataView(ByteData data);
 }
@@ -26,7 +26,7 @@ abstract final class UnmodifiableByteDataView implements ByteData {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableUint8ListView.
-@Deprecated('Use Uint8List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Uint8List.asUnmodifiableView() instead')
 abstract final class UnmodifiableUint8ListView implements Uint8List {
   external factory UnmodifiableUint8ListView(Uint8List list);
 }
@@ -35,7 +35,7 @@ abstract final class UnmodifiableUint8ListView implements Uint8List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableInt8ListView.
-@Deprecated('Use Int8List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Int8List.asUnmodifiableView() instead')
 abstract final class UnmodifiableInt8ListView implements Int8List {
   external factory UnmodifiableInt8ListView(Int8List list);
 }
@@ -44,7 +44,7 @@ abstract final class UnmodifiableInt8ListView implements Int8List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableUint8ClampedListView.
-@Deprecated('Use Uint8ClampedList.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Uint8ClampedList.asUnmodifiableView() instead')
 abstract final class UnmodifiableUint8ClampedListView
     implements Uint8ClampedList {
   external factory UnmodifiableUint8ClampedListView(Uint8ClampedList list);
@@ -54,7 +54,7 @@ abstract final class UnmodifiableUint8ClampedListView
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableUint16ListView.
-@Deprecated('Use Uint16List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Uint16List.asUnmodifiableView() instead')
 abstract final class UnmodifiableUint16ListView implements Uint16List {
   external factory UnmodifiableUint16ListView(Uint16List list);
 }
@@ -63,7 +63,7 @@ abstract final class UnmodifiableUint16ListView implements Uint16List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableInt16ListView.
-@Deprecated('Use Int16List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Int16List.asUnmodifiableView() instead')
 abstract final class UnmodifiableInt16ListView implements Int16List {
   external factory UnmodifiableInt16ListView(Int16List list);
 }
@@ -72,7 +72,7 @@ abstract final class UnmodifiableInt16ListView implements Int16List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableUint32ListView.
-@Deprecated('Use Uint32List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Uint32List.asUnmodifiableView() instead')
 abstract final class UnmodifiableUint32ListView implements Uint32List {
   external factory UnmodifiableUint32ListView(Uint32List list);
 }
@@ -81,7 +81,7 @@ abstract final class UnmodifiableUint32ListView implements Uint32List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableInt32ListView.
-@Deprecated('Use Int32List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Int32List.asUnmodifiableView() instead')
 abstract final class UnmodifiableInt32ListView implements Int32List {
   external factory UnmodifiableInt32ListView(Int32List list);
 }
@@ -90,7 +90,7 @@ abstract final class UnmodifiableInt32ListView implements Int32List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableUint64ListView.
-@Deprecated('Use Uint64List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Uint64List.asUnmodifiableView() instead')
 abstract final class UnmodifiableUint64ListView implements Uint64List {
   external factory UnmodifiableUint64ListView(Uint64List list);
 }
@@ -99,7 +99,7 @@ abstract final class UnmodifiableUint64ListView implements Uint64List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableInt64ListView.
-@Deprecated('Use Int64List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Int64List.asUnmodifiableView() instead')
 abstract final class UnmodifiableInt64ListView implements Int64List {
   external factory UnmodifiableInt64ListView(Int64List list);
 }
@@ -108,7 +108,7 @@ abstract final class UnmodifiableInt64ListView implements Int64List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableInt32x4ListView.
-@Deprecated('Use Int32x4List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Int32x4List.asUnmodifiableView() instead')
 abstract final class UnmodifiableInt32x4ListView implements Int32x4List {
   external factory UnmodifiableInt32x4ListView(Int32x4List list);
 }
@@ -117,7 +117,7 @@ abstract final class UnmodifiableInt32x4ListView implements Int32x4List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableFloat32x4ListView.
-@Deprecated('Use Float32x4List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Float32x4List.asUnmodifiableView() instead')
 abstract final class UnmodifiableFloat32x4ListView implements Float32x4List {
   external factory UnmodifiableFloat32x4ListView(Float32x4List list);
 }
@@ -126,7 +126,7 @@ abstract final class UnmodifiableFloat32x4ListView implements Float32x4List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableFloat64x2ListView.
-@Deprecated('Use Float64x2List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Float64x2List.asUnmodifiableView() instead')
 abstract final class UnmodifiableFloat64x2ListView implements Float64x2List {
   external factory UnmodifiableFloat64x2ListView(Float64x2List list);
 }
@@ -135,7 +135,7 @@ abstract final class UnmodifiableFloat64x2ListView implements Float64x2List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableFloat32ListView.
-@Deprecated('Use Float32List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Float32List.asUnmodifiableView() instead')
 abstract final class UnmodifiableFloat32ListView implements Float32List {
   external factory UnmodifiableFloat32ListView(Float32List list);
 }
@@ -144,7 +144,7 @@ abstract final class UnmodifiableFloat32ListView implements Float32List {
 ///
 /// It is a compile-time error for a class to attempt to extend or implement
 /// UnmodifiableFloat64ListView.
-@Deprecated('Use Float64List.asUnmodifiableView() instead')
+// TODO(53785): @Deprecated('Use Float64List.asUnmodifiableView() instead')
 abstract final class UnmodifiableFloat64ListView implements Float64List {
   external factory UnmodifiableFloat64ListView(Float64List list);
 }
