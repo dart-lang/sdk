f5983e4214bf2ee3d16171750a1442ce3a11b840
Daco Harkes <dacoharkes@google.com>
2023-10-30 18:43:14 +0000

    [ffi] Update some `FfiNative` docs to `Native`
    The old FfiNatives have been deprecated for a while.
    
    TEST=Only comments updated.
    
    Change-Id: Ideb6e904841e5162d30df479e83b463b94682afe
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/332780
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>
    Commit-Queue: Alexander Markov <alexmarkov@google.com>
    Commit-Queue: Daco Harkes <dacoharkes@google.com>

--- pkg/vm/lib/target/vm.dart
+++ pkg/vm/lib/target/vm.dart
@@ -172,7 +172,7 @@ class VmTarget extends Target {
           transitiveImportingDartFfi, diagnosticReporter, referenceFromIndex);
       logger?.call("Transformed ffi annotations");
 
-      // Transform @FfiNative(..) functions into FFI native call functions.
+      // Transform @Native(..) functions into FFI native call functions.
       // Pass instance method receivers as implicit first argument to the static
       // native function.
       // Transform arguments that extend NativeFieldWrapperClass1 to Pointer if
