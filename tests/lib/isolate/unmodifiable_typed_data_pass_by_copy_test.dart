28a4b55c0a7abc6e1ed8856e799f4a15007bb4e5
Stephen Adams <sra@google.com>
2023-11-02 12:45:48 +0000

    [tests/isolate] Avoid use of deprecated unmodifiable typed data view constructor
    Issue: #53785
    Change-Id: I205492adcec6432f5503cbf9f015a6ac2e9d6f5c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/332165
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- tests/lib/isolate/unmodifiable_typed_data_pass_by_copy_test.dart
+++ tests/lib/isolate/unmodifiable_typed_data_pass_by_copy_test.dart
@@ -10,7 +10,7 @@ import "package:expect/expect.dart";
 
 main() {
   final backing = new Uint8List(1);
-  final original = new UnmodifiableUint8ListView(backing);
+  final original = backing.asUnmodifiableView();
   var port;
   port = new RawReceivePort((copy) {
     port.close();
