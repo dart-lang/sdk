2a898edcc2ea98b4330ce890d7af418f6b923a79
Bastian Schmalbach <bastian.schmalbach@web.de>
2023-11-13 17:06:55 +0100

    fix: incomplete http redirect for POST method on 307 and 308

--- sdk/lib/_http/http_impl.dart
+++ sdk/lib/_http/http_impl.dart
@@ -355,7 +355,9 @@ class _HttpClientResponse extends _HttpInboundMessageListInt
           statusCode == HttpStatus.seeOther ||
           statusCode == HttpStatus.temporaryRedirect;
     } else if (_httpRequest.method == "POST") {
-      return statusCode == HttpStatus.seeOther;
+      return statusCode == HttpStatus.seeOther || 
+          statusCode == HttpStatus.temporaryRedirect || 
+          statusCode == HttpStatus.permanentRedirect;
     }
     return false;
   }
