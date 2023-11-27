d032598d26da3a6d1734604336b604a177ecd29d
Keerti Parthasarathy <keertip@google.com>
2023-11-27 23:23:26 +0000

    Support multiple packages in a PubWorkspace.
    A PubWorkspace is now
    - rooted at package config, or the outermost pubspec if there is no package config
    - packages are identified by pubspec
    - all packages are considered to be package build packages
    
    Change-Id: Id1552ab584ccedf60a482b93b375d1c4c7da6796
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/337607
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer/test/src/dart/analysis/context_builder_test.dart
+++ pkg/analyzer/test/src/dart/analysis/context_builder_test.dart
@@ -14,7 +14,6 @@ import 'package:analyzer/src/dart/analysis/context_root.dart';
 import 'package:analyzer/src/dart/analysis/driver_based_analysis_context.dart';
 import 'package:analyzer/src/generated/engine.dart' show AnalysisOptionsImpl;
 import 'package:analyzer/src/generated/source.dart';
-import 'package:analyzer/src/source/package_map_resolver.dart';
 import 'package:analyzer/src/test_utilities/mock_sdk.dart';
 import 'package:analyzer/src/test_utilities/resource_provider_mixin.dart';
 import 'package:analyzer/src/util/file_paths.dart' as file_paths;
@@ -198,8 +197,9 @@ environment:
       analysisContext.uriResolvers,
       unorderedEquals([
         isA<DartUriResolver>(),
-        isA<PackageMapUriResolver>(),
+        isA<PackageBuildPackageUriResolver>(),
         isA<ResourceUriResolver>(),
+        isA<PackageBuildFileUriResolver>(),
       ]),
     );
   }
