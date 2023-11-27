6e771a1ce965113b757f1c0aabc9a84f0efc144c
Martin Kustermann <kustermann@google.com>
2023-11-27 15:51:55 +0000

    [dart2wasm] Make tools/test.py -n dart2wasm-linux-jsshell ... work
    Change-Id: I0a2678a5b141ab52597860380df7d44fd1f1db74
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/338381
    Commit-Queue: Martin Kustermann <kustermann@google.com>
    Reviewed-by: Ömer Ağacan <omersa@google.com>

--- pkg/test_runner/lib/src/compiler_configuration.dart
+++ pkg/test_runner/lib/src/compiler_configuration.dart
@@ -601,10 +601,11 @@ class Dart2WasmCompilerConfiguration extends CompilerConfiguration {
       CommandArtifact? artifact) {
     final filename = artifact!.filename;
     final args = testFile.dartOptions;
+    final isD8 = runtimeConfiguration is D8RuntimeConfiguration;
     return [
-      '--turboshaft-wasm',
+      if (isD8) '--turboshaft-wasm',
       'pkg/dart2wasm/bin/run_wasm.js',
-      '--',
+      if (isD8) '--',
       '${filename.substring(0, filename.lastIndexOf('.'))}.mjs',
       filename,
       ...testFile.sharedObjects
