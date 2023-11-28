92fba81cce8cdf223f5e2de0143946ad3cb46bce
Konstantin Shcheglov <scheglov@google.com>
2023-11-28 21:23:30 +0000

    Nucleus for adding AnalysisContextCollection tests.
    Feel free to contact me if you want to discuss it.
    
    Change-Id: I4e46c2d78677d59d9a3a4c56dc826e3e50645b5e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/336260
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/dart/analysis/analysis_context_collection_test.dart
+++ pkg/analyzer/test/src/dart/analysis/analysis_context_collection_test.dart
@@ -2,24 +2,34 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/dart/analysis/analysis_context.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/src/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/src/test_utilities/mock_sdk.dart';
 import 'package:analyzer/src/test_utilities/resource_provider_mixin.dart';
+import 'package:analyzer/src/util/file_paths.dart' as file_paths;
+import 'package:analyzer/src/utilities/extensions/file_system.dart';
+import 'package:analyzer/src/workspace/basic.dart';
+import 'package:analyzer/src/workspace/pub.dart';
+import 'package:analyzer/src/workspace/workspace.dart';
 import 'package:linter/src/rules.dart';
 import 'package:test/test.dart';
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
+import '../../../util/tree_string_sink.dart';
 import '../resolution/context_collection_resolution.dart';
+import '../resolution/node_text_expectations.dart';
 
 main() {
   defineReflectiveSuite(() {
+    defineReflectiveTests(AnalysisContextCollectionLowTest);
     defineReflectiveTests(AnalysisContextCollectionTest);
+    defineReflectiveTests(UpdateNodeTextExpectations);
   });
 }
 
 @reflectiveTest
-class AnalysisContextCollectionTest with ResourceProviderMixin {
+class AnalysisContextCollectionLowTest with ResourceProviderMixin {
   Folder get sdkRoot => newFolder('/sdk');
 
   void setUp() {
@@ -179,4 +189,219 @@ linter:
       sdkPath: sdkRoot.path,
     );
   }
+}
+
+@reflectiveTest
+class AnalysisContextCollectionTest with ResourceProviderMixin {
+  final _AnalysisContextCollectionPrinterConfiguration configuration =
+      _AnalysisContextCollectionPrinterConfiguration();
+
+  Folder get sdkRoot => newFolder('/sdk');
+
+  File newPackageConfigJsonFileFromBuilder(
+    String directoryPath,
+    PackageConfigFileBuilder builder,
+  ) {
+    final content = builder.toContent(toUriStr: toUriStr);
+    return newPackageConfigJsonFile(directoryPath, content);
+  }
+
+  void newSinglePackageConfigJsonFile({
+    required String packagePath,
+    required String name,
+  }) {
+    final builder = PackageConfigFileBuilder()
+      ..add(name: name, rootPath: packagePath);
+    newPackageConfigJsonFileFromBuilder(packagePath, builder);
+  }
+
+  void setUp() {
+    createMockSdk(
+      resourceProvider: resourceProvider,
+      root: sdkRoot,
+    );
+    registerLintRules();
+  }
+
+  test_pubWorkspace_multipleAnalysisOptions() async {
+    final workspaceRootPath = '/home';
+    final testPackageRootPath = '$workspaceRootPath/test';
+    final testPackageLibPath = '$testPackageRootPath/lib';
+
+    newPubspecYamlFile(testPackageRootPath, r'''
+name: test
+''');
+
+    newSinglePackageConfigJsonFile(
+      packagePath: testPackageRootPath,
+      name: 'test',
+    );
+
+    newAnalysisOptionsYamlFile(testPackageRootPath, '');
+    newFile('$testPackageLibPath/a.dart', '');
+
+    final nestedPath = '$testPackageLibPath/nested';
+    newAnalysisOptionsYamlFile(nestedPath, '');
+    newFile('$nestedPath/b.dart', '');
+
+    final contextCollection = AnalysisContextCollectionImpl(
+      resourceProvider: resourceProvider,
+      sdkPath: sdkRoot.path,
+      includedPaths: [
+        workspaceRootPath,
+      ],
+    );
+
+    _assertContextCollectionText(contextCollection, r'''
+/home/test
+  optionsFile: /home/test/analysis_options.yaml
+  packagesFile: /home/test/.dart_tool/package_config.json
+  workspace: PubWorkspace
+    root: /home/test
+  analyzedFiles
+    /home/test/lib/a.dart
+/home/test/lib/nested
+  optionsFile: /home/test/lib/nested/analysis_options.yaml
+  packagesFile: /home/test/.dart_tool/package_config.json
+  workspace: PubWorkspace
+    root: /home/test
+  analyzedFiles
+    /home/test/lib/nested/b.dart
+''');
+  }
+
+  test_pubWorkspace_singleAnalysisOptions() async {
+    final workspaceRootPath = '/home';
+    final testPackageRootPath = '$workspaceRootPath/test';
+    final testPackageLibPath = '$testPackageRootPath/lib';
+
+    newPubspecYamlFile(testPackageRootPath, r'''
+name: test
+''');
+
+    newSinglePackageConfigJsonFile(
+      packagePath: testPackageRootPath,
+      name: 'test',
+    );
+
+    newAnalysisOptionsYamlFile(testPackageRootPath, '');
+
+    newFile('$testPackageLibPath/a.dart', '');
+
+    final contextCollection = AnalysisContextCollectionImpl(
+      resourceProvider: resourceProvider,
+      sdkPath: sdkRoot.path,
+      includedPaths: [
+        workspaceRootPath,
+      ],
+    );
+
+    _assertContextCollectionText(contextCollection, r'''
+/home/test
+  optionsFile: /home/test/analysis_options.yaml
+  packagesFile: /home/test/.dart_tool/package_config.json
+  workspace: PubWorkspace
+    root: /home/test
+  analyzedFiles
+    /home/test/lib/a.dart
+''');
+  }
+
+  void _assertContextCollectionText(
+    AnalysisContextCollectionImpl contextCollection,
+    String expected,
+  ) {
+    final actual = _getContextCollectionText(contextCollection);
+    if (actual != expected) {
+      print('-------- Actual --------');
+      print('$actual------------------------');
+      NodeTextExpectationsCollector.add(actual);
+    }
+    expect(actual, expected);
+  }
+
+  String _getContextCollectionText(
+    AnalysisContextCollectionImpl contextCollection,
+  ) {
+    final buffer = StringBuffer();
+    _AnalysisContextCollectionPrinter(
+      configuration: configuration,
+      resourceProvider: resourceProvider,
+      sink: TreeStringSink(sink: buffer, indent: ''),
+    ).write(contextCollection);
+    return buffer.toString();
+  }
+}
+
+class _AnalysisContextCollectionPrinter {
+  final _AnalysisContextCollectionPrinterConfiguration configuration;
+  final ResourceProvider resourceProvider;
+  final TreeStringSink sink;
+
+  _AnalysisContextCollectionPrinter({
+    required this.configuration,
+    required this.resourceProvider,
+    required this.sink,
+  });
+
+  void write(AnalysisContextCollectionImpl contextCollection) {
+    for (final analysisContext in contextCollection.contexts) {
+      _writeAnalysisContext(analysisContext);
+    }
+  }
+
+  bool _isDartFile(File file) {
+    return file_paths.isDart(resourceProvider.pathContext, file.path);
+  }
+
+  void _writeAnalysisContext(AnalysisContext analysisContext) {
+    final contextRoot = analysisContext.contextRoot;
+
+    final analyzedFiles = contextRoot.analyzedFiles().toList();
+    if (!configuration.withEmptyContextRoots && analyzedFiles.isEmpty) {
+      return;
+    }
+
+    sink.writelnWithIndent(contextRoot.root.posixPath);
+    sink.withIndent(() {
+      _writeNamedFile('optionsFile', contextRoot.optionsFile);
+      _writeNamedFile('packagesFile', contextRoot.packagesFile);
+      _writeWorkspace(contextRoot.workspace);
+      sink.writeElements('analyzedFiles', analyzedFiles, (path) {
+        final file = resourceProvider.getFile(path);
+        if (_isDartFile(file)) {
+          sink.writelnWithIndent(file.posixPath);
+        }
+      });
+    });
+  }
+
+  void _writeNamedFile(String name, File? file) {
+    if (file != null) {
+      sink.writelnWithIndent('$name: ${file.posixPath}');
+    }
+  }
+
+  void _writeWorkspace(Workspace workspace) {
+    switch (workspace) {
+      case BasicWorkspace():
+        sink.writelnWithIndent('workspace: BasicWorkspace');
+        sink.withIndent(() {
+          final root = resourceProvider.getFolder(workspace.root);
+          sink.writelnWithIndent('root: ${root.posixPath}');
+        });
+      case PubWorkspace():
+        sink.writelnWithIndent('workspace: PubWorkspace');
+        sink.withIndent(() {
+          final root = resourceProvider.getFolder(workspace.root);
+          sink.writelnWithIndent('root: ${root.posixPath}');
+        });
+      default:
+        throw UnimplementedError('${workspace.runtimeType}');
+    }
+  }
+}
+
+class _AnalysisContextCollectionPrinterConfiguration {
+  bool withEmptyContextRoots = false;
 }
