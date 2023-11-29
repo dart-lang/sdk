bf9767ec5ad2ca20c6c8bd0c0d7a40a288a63aa2
Jens Johansen <jensj@google.com>
2023-11-29 12:33:18 +0000

    [CFE] Fix nested ifs that doesn't make sense
    E.g.
    
    ```
      final bool foo = (1+1==2) ? true : false;
      if (foo) {
        if (foo) {
          print("always true");
        }
        if (!foo) {
          print("always false");
        }
      }
    ```
    
    Inside the first if we know `foo` to be true, so there's no reason to do
    more if's on it.
    
    Change-Id: I87d0827c47ce0e7277d951c0ec28f19dff09767a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/338780
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

---
+++ pkg/front_end/test/unreachable_if_finder_git_test.dart
@@ -0,0 +1,90 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:_fe_analyzer_shared/src/messages/severity.dart';
+import 'package:front_end/src/api_prototype/compiler_options.dart' as api;
+import 'package:front_end/src/api_prototype/incremental_kernel_generator.dart'
+    show IncrementalCompilerResult;
+import 'package:front_end/src/base/processed_options.dart';
+import 'package:front_end/src/compute_platform_binaries_location.dart'
+    show computePlatformBinariesLocation;
+import 'package:front_end/src/fasta/compiler_context.dart';
+import 'package:front_end/src/fasta/incremental_compiler.dart';
+import 'package:kernel/kernel.dart';
+import 'package:kernel/target/targets.dart';
+
+import '../tool/unreachable_if_finder.dart';
+import 'testing_utils.dart' show getGitFiles;
+import "utils/io_utils.dart";
+
+final Uri repoDir = computeRepoDirUri();
+
+Set<Uri> libUris = {};
+
+Future<void> main(List<String> args) async {
+  api.CompilerOptions compilerOptions = getOptions();
+
+  Uri packageConfigUri = repoDir.resolve(".dart_tool/package_config.json");
+  if (!new File.fromUri(packageConfigUri).existsSync()) {
+    throw "Couldn't find .dart_tool/package_config.json";
+  }
+  compilerOptions.packagesFileUri = packageConfigUri;
+
+  ProcessedOptions options = new ProcessedOptions(options: compilerOptions);
+
+  libUris.add(repoDir.resolve("pkg/front_end/lib/"));
+  libUris.add(repoDir.resolve("pkg/front_end/test/fasta/"));
+  libUris.add(repoDir.resolve("pkg/front_end/tool/"));
+
+  for (Uri uri in libUris) {
+    Set<Uri> gitFiles = await getGitFiles(uri);
+    List<FileSystemEntity> entities =
+        new Directory.fromUri(uri).listSync(recursive: true);
+    for (FileSystemEntity entity in entities) {
+      if (entity is File &&
+          entity.path.endsWith(".dart") &&
+          gitFiles.contains(entity.uri)) {
+        options.inputs.add(entity.uri);
+      }
+    }
+  }
+
+  Stopwatch stopwatch = new Stopwatch()..start();
+
+  IncrementalCompiler compiler =
+      new IncrementalCompiler(new CompilerContext(options));
+  IncrementalCompilerResult compilerResult = await compiler.computeDelta();
+  Component component = compilerResult.component;
+  List<Warning> warnings = UnreachableIfFinder.find(component);
+
+  print("Done in ${stopwatch.elapsedMilliseconds} ms. "
+      "Found ${warnings.length} warnings.");
+  if (warnings.length > 0) {
+    for (Warning warning in warnings) {
+      print(warning);
+      print("");
+    }
+    throw "Found ${warnings.length} warnings.";
+  }
+}
+
+api.CompilerOptions getOptions() {
+  Uri sdkRoot = computePlatformBinariesLocation(forceBuildDir: true);
+  api.CompilerOptions options = new api.CompilerOptions()
+    ..sdkRoot = sdkRoot
+    ..compileSdk = false
+    ..target = new NoneTarget(new TargetFlags())
+    ..librariesSpecificationUri = repoDir.resolve("sdk/lib/libraries.json")
+    ..omitPlatform = true
+    ..onDiagnostic = (api.DiagnosticMessage message) {
+      if (message.severity == Severity.error) {
+        print(message.plainTextFormatted.join('\n'));
+        exitCode = 1;
+      }
+    }
+    ..environmentDefines = const {};
+  return options;
+}
---
+++ pkg/front_end/tool/unreachable_if_finder.dart
@@ -0,0 +1,148 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:kernel/kernel.dart';
+
+void main(List<String> args) {
+  for (String arg in args) {
+    File f = new File(arg);
+    if (!f.existsSync()) {
+      print("Skipping '$arg', not a file.");
+      continue;
+    }
+    Component c;
+    try {
+      c = loadComponentFromBytes(f.readAsBytesSync());
+    } catch (e) {
+      print("Skipping '$arg', couldn't load as kernel: '$e'");
+      continue;
+    }
+    try {
+      List<Warning> warnings = UnreachableIfFinder.find(c);
+      if (warnings.isEmpty) {
+        print("No warnings found for '$arg'");
+      } else {
+        for (Warning warning in warnings) {
+          print(warning);
+          print("");
+        }
+      }
+    } catch (e, st) {
+      print("Failure '$e' on '$arg':");
+      print(st);
+    }
+  }
+}
+
+class UnreachableIfFinder extends RecursiveVisitor<void> {
+  static List<Warning> find(Component c) {
+    EffectivelyFinal effectivelyFinal = new EffectivelyFinal._();
+    c.accept(effectivelyFinal);
+    UnreachableIfFinder unreachableIfFinder =
+        new UnreachableIfFinder._(effectivelyFinal.unwritten);
+    c.accept(unreachableIfFinder);
+    return unreachableIfFinder.warnings;
+  }
+
+  final Set<VariableDeclaration> unwritten;
+
+  UnreachableIfFinder._(this.unwritten);
+
+  List<Warning> warnings = [];
+
+  Map<VariableDeclaration, bool> knownValues = {};
+
+  @override
+  void visitIfStatement(IfStatement node) {
+    helperForIfLikeStructure(node.condition, node.then, node.otherwise, node);
+  }
+
+  @override
+  void visitConditionalExpression(ConditionalExpression node) {
+    helperForIfLikeStructure(node.condition, node.then, node.otherwise, node);
+  }
+
+  void helperForIfLikeStructure(
+    Expression condition,
+    TreeNode then,
+    TreeNode? otherwise,
+    TreeNode originNode,
+  ) {
+    // TODO(jensj): We could make the visit return a bool? instead and use that
+    // from the condition instead of doing special casing on `Not` and
+    // `VariableGet`.
+    VariableDeclaration? newKnownValueHere;
+    bool conditionNegated = false;
+
+    if (condition is Not) {
+      condition = condition.operand;
+      conditionNegated = true;
+    }
+
+    if (condition is VariableGet) {
+      bool? knownValue = knownValues[condition.variable];
+      if (knownValue != null) {
+        if (conditionNegated) knownValue = !knownValue;
+        String? hint;
+
+        if (knownValue && otherwise != null) {
+          hint = "The else branch will never execute.";
+        } else if (!knownValue) {
+          hint = "The then branch will never execute.";
+        }
+        warnings.add(new Warning(
+            originNode.location, "Condition is always $knownValue", hint));
+      } else {
+        if (condition.variable.isFinal ||
+            unwritten.contains(condition.variable)) {
+          newKnownValueHere = condition.variable;
+        }
+      }
+    }
+    if (newKnownValueHere != null) {
+      knownValues[newKnownValueHere] = conditionNegated ? false : true;
+    }
+    then.accept(this);
+    if (newKnownValueHere != null) {
+      knownValues[newKnownValueHere] = conditionNegated ? true : false;
+    }
+    otherwise?.accept(this);
+    if (newKnownValueHere != null) {
+      knownValues.remove(newKnownValueHere);
+    }
+  }
+}
+
+class EffectivelyFinal extends RecursiveVisitor<void> {
+  final Set<VariableDeclaration> unwritten = {};
+
+  EffectivelyFinal._();
+
+  @override
+  void visitVariableDeclaration(VariableDeclaration node) {
+    unwritten.add(node);
+    super.visitVariableDeclaration(node);
+  }
+
+  @override
+  void visitVariableSet(VariableSet node) {
+    unwritten.remove(node.variable);
+    super.visitVariableSet(node);
+  }
+}
+
+class Warning {
+  final Location? location;
+  final String message;
+  final String? hint;
+
+  Warning(this.location, this.message, this.hint);
+
+  @override
+  String toString() {
+    return "Warning: $message @ $location.${hint != null ? "\n$hint." : ""}";
+  }
+}
