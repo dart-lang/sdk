2d0e056672f5f430ae7494257dbeceb03ee3b7a1
Chloe Stefantsova <cstefantsova@google.com>
2023-12-06 16:11:42 +0000

    [cfe] Check for cyclic typedefs at declare site instead of use site
    Change-Id: I3738cd483b6436c630669edd6975b0fbb078d058
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/339941
    Reviewed-by: Erik Ernst <eernst@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Commit-Queue: Chloe Stefantsova <cstefantsova@google.com>

--- tests/language/nonfunction_type_aliases/cyclic_bound_error_test.dart
+++ tests/language/nonfunction_type_aliases/cyclic_bound_error_test.dart
@@ -7,51 +7,51 @@
 // error, when the cycle occurs via the bound.
 
 typedef T1<X extends T1<Never>> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T1' has a reference to itself.
 
 // Note: when the cycle involves two typedefs, the CFE only reports an error for
 // one of them; that's ok.
 typedef T2<X extends T3> = List<X>;
-//      ^
-// [analyzer] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T2' has a reference to itself.
 
 typedef T3 = T2<Never>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
 
 typedef T4<X extends List<T4<Never>>> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T4' has a reference to itself.
 
 typedef T5<X extends T5<Never> Function()> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T5' has a reference to itself.
 
 typedef T6<X extends void Function(T6<Never>)> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T6' has a reference to itself.
 
 // Note: not an error because T7 is the name of the parameter.
 typedef T7<X extends void Function(int T7)> = List<X>;
 
 typedef T8<X extends void Function([T8<Never>])> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T8' has a reference to itself.
 
 // Note: not an error because T9 is the name of the parameter.
 typedef T9<X extends void Function([int T9])> = List<X>;
 
 typedef T10<X extends void Function({T10<Never> x})> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T10' has a reference to itself.
 
 // Note: not an error because T11 is the name of the parameter.
 typedef T11<X extends void Function({int T11})> = List<X>;
@@ -59,6 +59,6 @@ typedef T11<X extends void Function({int T11})> = List<X>;
 // Note: we have to use `void Function<...>() Function()` because a generic
 // function can't directly be used as a bound.
 typedef T12<X extends void Function<Y extends T12<Never>>() Function()> = List<X>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T12' has a reference to itself.
--- tests/language/nonfunction_type_aliases/cyclic_bound_unused_error_test.dart
+++ tests/language/nonfunction_type_aliases/cyclic_bound_unused_error_test.dart
@@ -8,51 +8,51 @@
 // used in the expansion.
 
 typedef T1<X extends T1<Never>> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T1' has a reference to itself.
 
 // Note: when the cycle involves two typedefs, the CFE only reports an error for
 // one of them; that's ok.
 typedef T2<X extends T3> = int;
-//      ^
-// [analyzer] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T2' has a reference to itself.
 
 typedef T3 = T2<Never>;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
 
 typedef T4<X extends List<T4<Never>>> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T4' has a reference to itself.
 
 typedef T5<X extends T5<Never> Function()> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T5' has a reference to itself.
 
 typedef T6<X extends void Function(T6<Never>)> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T6' has a reference to itself.
 
 // Note: not an error because T7 is the name of the parameter.
 typedef T7<X extends void Function(int T7)> = int;
 
 typedef T8<X extends void Function([T8<Never>])> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T8' has a reference to itself.
 
 // Note: not an error because T9 is the name of the parameter.
 typedef T9<X extends void Function([int T9])> = int;
 
 typedef T10<X extends void Function({T10<Never> x})> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T10' has a reference to itself.
 
 // Note: not an error because T11 is the name of the parameter.
 typedef T11<X extends void Function({int T11})> = int;
@@ -60,6 +60,6 @@ typedef T11<X extends void Function({int T11})> = int;
 // Note: we have to use `void Function<...>() Function()` because a generic
 // function can't directly be used as a bound.
 typedef T12<X extends void Function<Y extends T12<Never>>() Function()> = int;
-//      ^
-// [analyzer] unspecified
-// [cfe] unspecified
+//      ^^^
+// [analyzer] COMPILE_TIME_ERROR.TYPE_ALIAS_CANNOT_REFERENCE_ITSELF
+// [cfe] The typedef 'T12' has a reference to itself.
