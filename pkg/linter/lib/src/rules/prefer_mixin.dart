89381ea4dbec007403708b7b32ff95a142b7f303
Parker Lougheed <parlough@gmail.com>
2023-12-18 18:00:42 +0000

    [linter] Simplify 'prefer_mixin' implementation
    The allow-listed classes have all been migrated to be typedefs to mixin classes.
    
    Keeps the tests for them there to ensure this is the case.
    
    Change-Id: I322707fc6d6ff61a98f43824b5ca70f197064b22
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/342301
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/linter/lib/src/rules/prefer_mixin.dart
+++ pkg/linter/lib/src/rules/prefer_mixin.dart
@@ -8,10 +8,6 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 
 import '../analyzer.dart';
-import '../extensions.dart';
-
-const _dartCollectionUri = 'dart.collection';
-const _dartConvertUri = 'dart.convert';
 
 const _desc = r'Prefer using mixins.';
 
@@ -34,12 +30,6 @@ class C with M {}
 ```
 
 ''';
-
-const _iterableMixinName = 'IterableMixin';
-const _listMixinName = 'ListMixin';
-const _mapMixinName = 'MapMixin';
-const _setMixinName = 'SetMixin';
-const _stringConversionSinkName = 'StringConversionSinkMixin';
 
 class PreferMixin extends LintRule {
   static const LintCode code = LintCode(
@@ -76,24 +66,10 @@ class _Visitor extends SimpleAstVisitor<void> {
       if (type is InterfaceType) {
         var element = type.element;
         if (element is MixinElement) continue;
-        if ((element is ClassElement && !element.isMixinClass) &&
-            !isAllowed(element)) {
+        if (element is ClassElement && !element.isMixinClass) {
           rule.reportLint(mixinNode, arguments: [mixinNode.name2.lexeme]);
         }
       }
     }
   }
-
-  /// Check for "legacy" classes that cannot easily be made `mixin`s for
-  /// compatibility reasons.
-  /// (See: https://github.com/dart-lang/linter/issues/2082)
-  static bool isAllowed(InterfaceElement element) =>
-      // TODO(pq): remove allowlist once legacy mixins are otherwise annotated.
-      // see: https://github.com/dart-lang/sdk/issues/45343
-
-      element.isClass(_iterableMixinName, _dartCollectionUri) ||
-      element.isClass(_listMixinName, _dartCollectionUri) ||
-      element.isClass(_mapMixinName, _dartCollectionUri) ||
-      element.isClass(_setMixinName, _dartCollectionUri) ||
-      element.isClass(_stringConversionSinkName, _dartConvertUri);
 }
