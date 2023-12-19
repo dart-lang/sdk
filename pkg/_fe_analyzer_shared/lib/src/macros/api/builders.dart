298988c972115c50846bdf9d355b9a4844079245
Jake Macdonald <jakemac@google.com>
2023-12-19 17:31:04 +0000

    clarify a few macro docs
    Change-Id: I160c3695bbe0e489170b9c2206b6bb7a67792a9e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/342140
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Auto-Submit: Jake Macdonald <jakemac@google.com>

--- pkg/_fe_analyzer_shared/lib/src/macros/api/builders.dart
+++ pkg/_fe_analyzer_shared/lib/src/macros/api/builders.dart
@@ -72,6 +72,9 @@ abstract interface class DeclarationPhaseIntrospector
     implements TypePhaseIntrospector {
   /// Instantiates a new [StaticType] for a given [type] annotation.
   ///
+  /// Throws if [type] is a [RawTypeAnnotationCode], more specific subtypes must
+  /// be used, as raw [Identifier]s are not allowed.
+  ///
   /// Throws an error if the [type] object contains [Identifier]s which cannot
   /// be resolved. This should only happen in the case of incomplete or invalid
   /// programs, but macros may be asked to run in this state during the
