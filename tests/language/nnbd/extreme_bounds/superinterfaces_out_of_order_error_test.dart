6a9216a46fcf82fdffe3568fe80ee32be583dfcf
Paul Berry <paulberry@google.com>
2023-12-19 00:08:19 +0000

    Remove Requirements=nnbd-weak from superinterfaces_out_of_order_error_test.dart.
    The annotation `Requirements=nnbd-weak` controls prevents a test from
    being run on a platform that doesn't have weak mode runtime semantics
    (e.g. a program running with sound null safety).
    
    In a previous CL
    (https://dart-review.googlesource.com/c/sdk/+/341020), I removed
    several tests from `tests/language` that contained legacy code, in
    order to unblock removal of legacy support from the analyzer; those
    tests all contained this annotation. However, after discussion with
    Siggi, we've decided that it would be better to keep those tests
    around for a while longer, because they exercise important runtime
    behaviors of unsound null safety mode on the web and VM platforms, and
    not all google3 code has been migrated off of unsound null safety mode
    yet.
    
    So, I intend to take a different approach to unblocking removal of
    legacy support from the analyzer: stop running tests with the
    annotation `Requirements=nnbd-weak` through the analyzer. This will
    allow the tests removed in
    https://dart-review.googlesource.com/c/sdk/+/341020 to be restored,
    but it will have a side effect of causing a small number of additional
    analyzer tests to be skipped. All those tests are benign to skip on
    the analyzer (because they are purely concerned with runtime
    semantics) except for one:
    `superinterfaces_out_of_order_error_test.dart`.
    
    This test doesn't have any runtime semantics at all; it merely checks
    that the analyzer and CFE produce the correct errors for certain
    compile-time type checks that have nothing to do with weak mode
    runtime semantics.
    
    So, to prevent a gap in test coverage when the analyzer begins
    skipping tests annotated with `Requirements=nnbd-weak`, we need to
    remove this annotation from
    `superinterfaces_out_of_order_error_test.dart`.
    
    Change-Id: I7932a0200be750116c41a303b48aaef50bc952ec
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/341980
    Reviewed-by: Sigmund Cherem <sigmund@google.com>
    Commit-Queue: Paul Berry <paulberry@google.com>

--- tests/language/nnbd/extreme_bounds/superinterfaces_out_of_order_error_test.dart
+++ tests/language/nnbd/extreme_bounds/superinterfaces_out_of_order_error_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Requirements=nnbd-weak
-
 // Test that upper bound computations which rely on the computations of the
 // cardinality of the superinterface sets define those sets without erasing or
 // modifying the superinterfaces.
