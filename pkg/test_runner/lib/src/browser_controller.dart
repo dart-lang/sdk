435cee17b744fd8bf4f20b1e9e9cd878616fdd7e
David Morgan <davidmorgan@google.com>
2024-01-15 13:01:54 +0000

    [test] Skip chrome Gnome keyring, KDE wallet.
    Prevents a popup on chrome start on my cloudtop; see:
    https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md#chromium-annoyances
    
    R=sortie@google.com
    
    Change-Id: I3f1def07bb940dd8930f5b2eed81053c24775197
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/346320
    Auto-Submit: Morgan :) <davidmorgan@google.com>
    Commit-Queue: Morgan :) <davidmorgan@google.com>
    Reviewed-by: Jonas Termansen <sortie@google.com>
    Commit-Queue: Jonas Termansen <sortie@google.com>

--- pkg/test_runner/lib/src/browser_controller.dart
+++ pkg/test_runner/lib/src/browser_controller.dart
@@ -408,6 +408,7 @@ class Chrome extends Browser {
         "--disable-blink-features=TimerThrottlingForBackgroundTabs",
         "--no-first-run",
         "--use-mock-keychain",
+        "--password-store=basic",
         "--user-data-dir=${userDir.path}",
         "--js-flags=--experimental-wasm-imported-strings",
         url,
