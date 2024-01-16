dart-lang/sdk

commit 1827fcbf68bf74b2a7d13b6a189c65197bef0dd8

[vm/compiler] Fix uses of constant indexes in LoadIndexed/StoreIndexed.
Previously, CanBeImmediateIndex calculated the scaling factor for the
index using Instance::ElementSizeFor. However, the LoadIndexed and
StoreIndexed instructions which use this function have an index_scale_
field that is not necessarily the same as the element size for the
class id. This means the displacement calculated within
CanBeImmediateIndex can differ from the actual displacement calculated
within ElementAddressForIntIndex, causing the bug seen by the fuzzer.

This CL moves the check in CanBeImmediateIndex to a new static
method Assembler::AddressCanHoldConstantIndex. In addition to the
original arguments to CanBeImmediateIndex, the new static method takes
an index_scale argument, so the field value can be passed appropriately.

It also adds an is_external argument on X64 and IA32 to match the other
architectures, since assuming a non-external typed data object could
cause a register to be used in cases where it isn't necessary.

TEST=vm/dart/regress_54486

Fixes: https://github.com/dart-lang/sdk/issues/54486
Change-Id: Ia553fb7da0500113b35f8d9af91a52df55437a3c
Cq-Include-Trybots: luci.dart.try:vm-aot-linux-debug-x64-try,vm-linux-debug-ia32-try,vm-aot-linux-debug-simarm_x64-try,vm-aot-linux-debug-simriscv64-try,vm-aot-mac-release-arm64-try
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/345002
Reviewed-by: Daco Harkes <dacoharkes@google.com>

runtime/vm/compiler/assembler/assembler_arm.cc
@@ -3732,6 +3732,44 @@ void Assembler::GenerateUnRelocatedPcRelativeTailCall(
   pattern.set_distance(offset_into_target);
 }
 
+bool Assembler::AddressCanHoldConstantIndex(const Object& constant,
+                                            bool is_load,
+                                            bool is_external,
+                                            intptr_t cid,
+                                            intptr_t index_scale,
+                                            bool* needs_base) {
+  if ((cid == kTypedDataInt32x4ArrayCid) ||
+      (cid == kTypedDataFloat32x4ArrayCid) ||
+      (cid == kTypedDataFloat64x2ArrayCid)) {
+    // We are using vldmd/vstmd which do not support offset.
+    return false;
+  }
+
+  if (!IsSafeSmi(constant)) return false;
+  const int64_t index = target::SmiValue(constant);
+  const intptr_t offset_base =
+      (is_external ? 0
+                   : (target::Instance::DataOffsetFor(cid) - kHeapObjectTag));
+  const int64_t offset = index * index_scale + offset_base;
+  if (!Utils::MagnitudeIsUint(12, offset)) {
+    return false;
+  }
+  if (Address::CanHoldImmediateOffset(is_load, cid, offset)) {
+    if (needs_base != nullptr) {
+      *needs_base = false;
+    }
+    return true;
+  }
+
+  if (needs_base != nullptr &&
+      Address::CanHoldImmediateOffset(is_load, cid, offset - offset_base)) {
+    *needs_base = true;
+    return true;
+  }
+
+  return false;
+}
+
 Address Assembler::ElementAddressForIntIndex(bool is_load,
                                              bool is_external,
                                              intptr_t cid,

runtime/vm/compiler/assembler/assembler_arm.h
@@ -1494,6 +1494,13 @@ class Assembler : public AssemblerBase {
   // nearby the load of the table address.
   void LoadAllocationTracingStateAddress(Register dest, intptr_t cid);
 
+  static bool AddressCanHoldConstantIndex(const Object& constant,
+                                          bool is_load,
+                                          bool is_external,
+                                          intptr_t cid,
+                                          intptr_t index_scale,
+                                          bool* needs_base = nullptr);
+
   Address ElementAddressForIntIndex(bool is_load,
                                     bool is_external,
                                     intptr_t cid,

runtime/vm/compiler/backend/il_arm.cc
@@ -2102,45 +2102,6 @@ void Utf8ScanInstr::EmitNativeCode(FlowGraphCompiler* compiler) {
   __ StoreFieldToOffset(flags_temp_reg, decoder_reg, scan_flags_field_offset);
 }
 
-static bool CanBeImmediateIndex(Value* value,
-                                intptr_t cid,
-                                bool is_external,
-                                bool is_load,
-                                bool* needs_base) {
-  if ((cid == kTypedDataInt32x4ArrayCid) ||
-      (cid == kTypedDataFloat32x4ArrayCid) ||
-      (cid == kTypedDataFloat64x2ArrayCid)) {
-    // We are using vldmd/vstmd which do not support offset.
-    return false;
-  }
-
-  ConstantInstr* constant = value->definition()->AsConstant();
-  if ((constant == nullptr) ||
-      !compiler::Assembler::IsSafeSmi(constant->value())) {
-    return false;
-  }
-  const int64_t index = compiler::target::SmiValue(constant->value());
-  const intptr_t scale = compiler::target::Instance::ElementSizeFor(cid);
-  const intptr_t base_offset =
-      (is_external ? 0 : (Instance::DataOffsetFor(cid) - kHeapObjectTag));
-  const int64_t offset = index * scale + base_offset;
-  if (!Utils::MagnitudeIsUint(12, offset)) {
-    return false;
-  }
-  if (compiler::Address::CanHoldImmediateOffset(is_load, cid, offset)) {
-    *needs_base = false;
-    return true;
-  }
-
-  if (compiler::Address::CanHoldImmediateOffset(is_load, cid,
-                                                offset - base_offset)) {
-    *needs_base = true;
-    return true;
-  }
-
-  return false;
-}
-
 LocationSummary* LoadIndexedInstr::MakeLocationSummary(Zone* zone,
                                                        bool opt) const {
   const bool directly_addressable =
@@ -2158,15 +2119,13 @@ LocationSummary* LoadIndexedInstr::MakeLocationSummary(Zone* zone,
   LocationSummary* locs = new (zone)
       LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
   locs->set_in(0, Location::RequiresRegister());
-  bool needs_base = false;
-  if (CanBeImmediateIndex(index(), class_id(), IsExternal(),
-                          true,  // Load.
-                          &needs_base)) {
-    // CanBeImmediateIndex must return false for unsafe smis.
-    locs->set_in(1, Location::Constant(index()->definition()->AsConstant()));
-  } else {
-    locs->set_in(1, Location::RequiresRegister());
-  }
+  const bool can_be_constant = index()->BindsToConstant() &&
+                               compiler::Assembler::AddressCanHoldConstantIndex(
+                                   index()->BoundConstant(), /*load=*/true,
+                                   IsExternal(), class_id(), index_scale());
+  locs->set_in(1, can_be_constant
+                      ? Location::Constant(index()->definition()->AsConstant())
+                      : Location::RequiresRegister());
   if ((representation() == kUnboxedFloat) ||
       (representation() == kUnboxedDouble) ||
       (representation() == kUnboxedFloat32x4) ||
@@ -2382,11 +2341,14 @@ LocationSummary* StoreIndexedInstr::MakeLocationSummary(Zone* zone,
   const intptr_t kNumInputs = 3;
   LocationSummary* locs;
 
-  bool needs_base = false;
   intptr_t kNumTemps = 0;
-  if (CanBeImmediateIndex(index(), class_id(), IsExternal(),
-                          false,  // Store.
-                          &needs_base)) {
+  bool needs_base = false;
+  const bool can_be_constant =
+      index()->BindsToConstant() &&
+      compiler::Assembler::AddressCanHoldConstantIndex(
+          index()->BoundConstant(), /*load=*/false, IsExternal(), class_id(),
+          index_scale(), &needs_base);
+  if (can_be_constant) {
     if (!directly_addressable) {
       kNumTemps += 2;
     } else if (needs_base) {
@@ -2396,7 +2358,6 @@ LocationSummary* StoreIndexedInstr::MakeLocationSummary(Zone* zone,
     locs = new (zone)
         LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
 
-    // CanBeImmediateIndex must return false for unsafe smis.
     locs->set_in(1, Location::Constant(index()->definition()->AsConstant()));
   } else {
     if (!directly_addressable) {

runtime/tests/vm/dart/regress_54486_test.dart
@@ -1,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Regression test for https://github.com/dart-lang/sdk/issues/54486.
+// Verifies that VM doesn't crash with an assertion failure in debug mode when
+// generating a LoadIndexed instruction with a too-large constant index.
+
+// Trimmed down a program generated by the Dart Project Fuzz Tester (1.101):
+//   dart dartfuzz.dart --seed 1258834069 --no-fp --no-ffi --flat
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:expect/expect.dart';
+
+Int32x4List array = Int32x4List(22);
+
+void foo0() => print(array[-(-33 >>> 8)]);
+void foo1() => array[49] ^= Int32x4.bool(false, false, false, true);
+
+main() {
+  Expect.throws(foo0);
+  Expect.throws(foo1);
+}

runtime/vm/compiler/assembler/assembler_arm64.cc
@@ -2174,6 +2174,20 @@ void Assembler::GenerateUnRelocatedPcRelativeTailCall(
   pattern.set_distance(offset_into_target);
 }
 
+bool Assembler::AddressCanHoldConstantIndex(const Object& constant,
+                                            bool is_external,
+                                            intptr_t cid,
+                                            intptr_t index_scale) {
+  if (!IsSafeSmi(constant)) return false;
+  const int64_t index = target::SmiValue(constant);
+  const int64_t offset = index * index_scale + HeapDataOffset(is_external, cid);
+  if (!Utils::IsInt(32, offset)) {
+    return false;
+  }
+  return Address::CanHoldOffset(static_cast<int32_t>(offset), Address::Offset,
+                                Address::OperandSizeFor(cid));
+}
+
 Address Assembler::ElementAddressForIntIndex(bool is_external,
                                              intptr_t cid,
                                              intptr_t index_scale,

runtime/vm/compiler/assembler/assembler_arm64.h
@@ -2360,6 +2360,11 @@ class Assembler : public AssemblerBase {
   // See also above for the pc-relative call.
   void GenerateUnRelocatedPcRelativeTailCall(intptr_t offset_into_target = 0);
 
+  static bool AddressCanHoldConstantIndex(const Object& constant,
+                                          bool is_external,
+                                          intptr_t cid,
+                                          intptr_t index_scale);
+
   Address ElementAddressForIntIndex(bool is_external,
                                     intptr_t cid,
                                     intptr_t index_scale,

runtime/vm/compiler/assembler/assembler_ia32.cc
@@ -3129,6 +3129,18 @@ void Assembler::EnsureHasClassIdInDEBUG(intptr_t cid,
 #endif
 }
 
+bool Assembler::AddressCanHoldConstantIndex(const Object& constant,
+                                            bool is_external,
+                                            intptr_t cid,
+                                            intptr_t index_scale) {
+  if (!IsSafeSmi(constant)) return false;
+  const int64_t index = target::SmiValue(constant);
+  const int64_t offset =
+      is_external ? 0 : (target::Instance::DataOffsetFor(cid) - kHeapObjectTag);
+  const int64_t disp = index * index_scale + offset;
+  return Utils::IsInt(32, disp);
+}
+
 Address Assembler::ElementAddressForIntIndex(bool is_external,
                                              intptr_t cid,
                                              intptr_t index_scale,

runtime/vm/compiler/assembler/assembler_ia32.h
@@ -1018,6 +1018,11 @@ class Assembler : public AssemblerBase {
                             Register scratch,
                             Label* is_smi);
 
+  static bool AddressCanHoldConstantIndex(const Object& constant,
+                                          bool is_external,
+                                          intptr_t cid,
+                                          intptr_t index_scale);
+
   static Address ElementAddressForIntIndex(bool is_external,
                                            intptr_t cid,
                                            intptr_t index_scale,

commit 3feab3655c93b8cda3ea09e173fa2a21eec35f39

[vm/compiler] Fix ARM7 regression after 1827fcbf68bf.
The refactoring in https://dart-review.googlesource.com/c/sdk/+/345002
erroneously assumed that if the caller to
Assembler::AddressCanHoldConstantIndex didn't pass an out parameter to
detect whether a base register was needed, that that case shouldn't be
considered.

While it's true that LoadIndexedInstr::MakeLocationSummary originally
ignored the value stored in the out parameter, it did that because it
can use TMP to store the array base, unlike the code for
StoreIndexedInstr which must have a temporary register allocated during
register allocation.

Thus, fix Assembler::AddressCanHoldConstantIndex to always require
the out parameter, and add a comment in LoadIndexedInstr as to why
we ignore the out parameter in this case instead of allocating an
additional temporary register.

TEST=vm/cc/Assembler_Regress54621

Fixes: https://github.com/dart-lang/sdk/issues/54621
Change-Id: I0af557565faf657a87641457884334446e9b7cc5
Cq-Include-Trybots: luci.dart.try:vm-ffi-qemu-linux-release-arm-try
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/346201
Reviewed-by: Martin Kustermann <kustermann@google.com>
Commit-Queue: Tess Strickland <sstrickl@google.com>

runtime/vm/compiler/assembler/assembler_arm.cc
@@ -3738,6 +3738,7 @@ bool Assembler::AddressCanHoldConstantIndex(const Object& constant,
                                             intptr_t cid,
                                             intptr_t index_scale,
                                             bool* needs_base) {
+  ASSERT(needs_base != nullptr);
   if ((cid == kTypedDataInt32x4ArrayCid) ||
       (cid == kTypedDataFloat32x4ArrayCid) ||
       (cid == kTypedDataFloat64x2ArrayCid)) {
@@ -3751,18 +3752,12 @@ bool Assembler::AddressCanHoldConstantIndex(const Object& constant,
       (is_external ? 0
                    : (target::Instance::DataOffsetFor(cid) - kHeapObjectTag));
   const int64_t offset = index * index_scale + offset_base;
-  if (!Utils::MagnitudeIsUint(12, offset)) {
-    return false;
-  }
+  ASSERT(Utils::IsInt(32, offset));
   if (Address::CanHoldImmediateOffset(is_load, cid, offset)) {
-    if (needs_base != nullptr) {
-      *needs_base = false;
-    }
+    *needs_base = false;
     return true;
   }
-
-  if (needs_base != nullptr &&
-      Address::CanHoldImmediateOffset(is_load, cid, offset - offset_base)) {
+  if (Address::CanHoldImmediateOffset(is_load, cid, offset - offset_base)) {
     *needs_base = true;
     return true;
   }

runtime/vm/compiler/assembler/assembler_arm.h
@@ -1494,6 +1494,9 @@ class Assembler : public AssemblerBase {
   // nearby the load of the table address.
   void LoadAllocationTracingStateAddress(Register dest, intptr_t cid);
 
+  // If true is returned, then the out parameter [need_base] signifies whether
+  // a register is needed for storing the array base (which should be passed
+  // as the [temp] parameter to ElementAddressForIntIndex).
   static bool AddressCanHoldConstantIndex(const Object& constant,
                                           bool is_load,
                                           bool is_external,

runtime/vm/compiler/backend/il_arm.cc
@@ -2119,10 +2119,14 @@ LocationSummary* LoadIndexedInstr::MakeLocationSummary(Zone* zone,
   LocationSummary* locs = new (zone)
       LocationSummary(zone, kNumInputs, kNumTemps, LocationSummary::kNoCall);
   locs->set_in(0, Location::RequiresRegister());
-  const bool can_be_constant = index()->BindsToConstant() &&
-                               compiler::Assembler::AddressCanHoldConstantIndex(
-                                   index()->BoundConstant(), /*load=*/true,
-                                   IsExternal(), class_id(), index_scale());
+  bool needs_base;
+  const bool can_be_constant =
+      index()->BindsToConstant() &&
+      compiler::Assembler::AddressCanHoldConstantIndex(
+          index()->BoundConstant(), /*load=*/true, IsExternal(), class_id(),
+          index_scale(), &needs_base);
+  // We don't need to check if [needs_base] is true, since we use TMP as the
+  // temp register in this case and so don't need to allocate a temp register.
   locs->set_in(1, can_be_constant
                       ? Location::Constant(index()->definition()->AsConstant())
                       : Location::RequiresRegister());

runtime/vm/compiler/assembler/assembler_test.cc
@@ -216,4 +216,31 @@ LOAD_FROM_BOX_TEST(Int64, int64_t, 0xFFFFFFFFFFFFFFFF, true)
 LOAD_FROM_BOX_TEST(Int64, int64_t, 0xFFFFFFFFFFFFFFFF, false)
 #endif
 
+#if defined(TARGET_ARCH_ARM)
+ISOLATE_UNIT_TEST_CASE(Assembler_Regress54621) {
+  auto zone = thread->zone();
+  const classid_t cid = kTypedDataInt32ArrayCid;
+  const intptr_t index_scale = 1;
+  const intptr_t kMaxAllowedOffsetForExternal = (2 << 11) - 1;
+  auto& smi = Smi::Handle(zone, Smi::New(kMaxAllowedOffsetForExternal));
+  bool needs_base;
+  EXPECT(compiler::Assembler::AddressCanHoldConstantIndex(
+      smi, /*is_load=*/true,
+      /*is_external=*/true, cid, index_scale, &needs_base));
+  EXPECT(!needs_base);
+  EXPECT(compiler::Assembler::AddressCanHoldConstantIndex(
+      smi, /*is_load=*/true,
+      /*is_external=*/false, cid, index_scale, &needs_base));
+  EXPECT(needs_base);
+  // Double-checking we're on a boundary of what's allowed.
+  smi = Smi::New(kMaxAllowedOffsetForExternal + 1);
+  EXPECT(!compiler::Assembler::AddressCanHoldConstantIndex(
+      smi, /*is_load=*/true,
+      /*is_external=*/false, cid, index_scale));
+  EXPECT(!compiler::Assembler::AddressCanHoldConstantIndex(
+      smi, /*is_load=*/true,
+      /*is_external=*/true, cid, index_scale));
+}
+#endif
+
 }  // namespace dart