e00ab8c3ce64a0c0dfbcdfda768469502ad67e12
asiva <asiva@google.com>
2024-01-25 00:54:07 +0000

    [pkg/dartdev] Use isolates for running dart2js, analysis server and dtd instead of VmInterop (bouncing back to the JIT VM).
    This is a first step towards turning dartdev into an aot module.
    
    TEST=ci
    
    Change-Id: I2d963d2afb82f1cb486612ff6412b165cace3aa7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/346901
    Commit-Queue: Siva Annamalai <asiva@google.com>
    Reviewed-by: Ben Konyi <bkonyi@google.com>

--- pkg/dartdev/lib/src/commands/tooling_daemon.dart
+++ pkg/dartdev/lib/src/commands/tooling_daemon.dart
@@ -2,7 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io' as io;
+import 'dart:async';
+import 'dart:isolate';
 
 import 'package:args/args.dart';
 import 'package:dtd_impl/dart_tooling_daemon.dart' as dtd
@@ -11,7 +12,6 @@ import 'package:dtd_impl/dart_tooling_daemon.dart' as dtd
 import '../core.dart';
 import '../sdk.dart';
 import '../utils.dart';
-import '../vm_interop_handler.dart';
 
 class ToolingDaemonCommand extends DartdevCommand {
   static const String commandName = 'tooling-daemon';
@@ -37,20 +37,37 @@ class ToolingDaemonCommand extends DartdevCommand {
 
   @override
   Future<int> run() async {
-    final args = argResults!.arguments;
+    // Need to make a copy as argResults!.arguments is an
+    // UnmodifiableListView object which cannot be passed as
+    // the args for spawnUri.
+    final args = [...argResults!.arguments];
 
     if (!Sdk.checkArtifactExists(sdk.dtdSnapshot)) return 255;
 
-    VmInteropHandler.run(
-      sdk.dtdSnapshot,
-      args,
-      packageConfigOverride: null,
-    );
-
-    // The daemon will continue to run past the return from this method.
-    //
-    // On an error on startup, the daemon will set the dart:io exitCode value
-    // (or, call exit() directly).
-    return io.exitCode;
+    var retval = 0;
+    final result = Completer<int>();
+    final exitPort = ReceivePort()
+      ..listen((msg) {
+        result.complete(0);
+      });
+    final errorPort = ReceivePort()
+      ..listen((error) {
+        log.stderr(error.toString());
+        result.complete(255);
+      });
+    try {
+      await Isolate.spawnUri(Uri.file(sdk.dtdSnapshot), args, null,
+          onExit: exitPort.sendPort, onError: errorPort.sendPort);
+      retval = await result.future;
+    } catch (e, st) {
+      log.stderr(e.toString());
+      if (verbose) {
+        log.stderr(st.toString());
+      }
+      retval = 255;
+    }
+    errorPort.close();
+    exitPort.close();
+    return retval;
   }
 }
