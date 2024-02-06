f4b1d59461635e75cbfb420ce8527fbbe73d11f8
Ben Konyi <bkonyi@google.com>
2024-02-06 17:23:32 +0000

    [ Gardening ] Fix pkg/vm_service/test/get_supported_protocols_with_dds on AOT
    Fixes https://github.com/dart-lang/sdk/issues/54835
    
    Change-Id: I1dc0c585214e8001dfb6f2f0d8f3f07252bf56f2
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/350660
    Reviewed-by: Derek Xu <derekx@google.com>
    Commit-Queue: Derek Xu <derekx@google.com>
    Auto-Submit: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Ben Konyi <bkonyi@google.com>

--- pkg/vm_service/test/get_supported_protocols_with_dds_test.dart
+++ pkg/vm_service/test/get_supported_protocols_with_dds_test.dart
@@ -2,14 +2,21 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'common/test_helper.dart';
 import 'get_supported_protocols_common.dart';
 
 final tests = <VMTest>[
-  expectedProtocolTest(<String>[
-    'VM Service',
-    'DDS',
-  ]),
+  expectedProtocolTest(
+    <String>[
+      'VM Service',
+      'DDS',
+    ],
+    // See https://github.com/dart-lang/sdk/issues/54841
+    expectMissingProtocol:
+        Platform.script.toString().endsWith('out.aotsnapshot'),
+  ),
 ];
 
 void main([args = const <String>[]]) => runVMTests(
