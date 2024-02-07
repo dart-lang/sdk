0e9cdfbc063f94b608e218158c75d25302e0db70
Oleh Prypin <oprypin@google.com>
2024-02-07 21:50:09 +0000

    Reland "analyzer: Remove unnecessary FixContributor and DartFixContributor classes"
    This is a reland of commit c69f4321e62344ab1a8b4dc6ad903a3766f0daef.
    
    Bug: b/318016861
    Change-Id: Ic47203839e453b9fd156c8df89117c642b4a235d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/350863
    Auto-Submit: Oleh Prypin <oprypin@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/lib/src/handler/legacy/edit_get_fixes.dart
+++ pkg/analysis_server/lib/src/handler/legacy/edit_get_fixes.dart
@@ -178,7 +178,7 @@ class EditGetFixesHandler extends LegacyHandler
 
           List<Fix> fixes;
           try {
-            fixes = await DartFixContributor().computeFixes(context);
+            fixes = await computeFixes(context);
           } on InconsistentAnalysisException {
             fixes = [];
           } catch (exception, stackTrace) {
