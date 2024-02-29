3a901f50ef85acea194c8342f9256c1b28b7d19c
pq <pquitslund@google.com>
2024-02-29 21:19:20 +0000

    test that `camel_case_extensions` isn't reported on augmentations
    See: https://github.com/dart-lang/linter/issues/4898
    
    Change-Id: I136b9743f3c3285cdbb3e1eff662f8e25a04d6f7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/355281
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/test/rules/camel_case_extensions_test.dart
+++ pkg/linter/test/rules/camel_case_extensions_test.dart
@@ -17,6 +17,24 @@ class CamelCaseExtensionsTest extends LintRuleTest {
   @override
   String get lintRule => 'camel_case_extensions';
 
+  @FailingTest(
+      issue: 'https://github.com/dart-lang/linter/issues/4898',
+      reason:
+          "ParserErrorCode.EXTRANEOUS_MODIFIER [27, 7, Can't have modifier 'augment' here.]")
+  test_augmentationExtension_lowerCase() async {
+    newFile('$testPackageLibPath/a.dart', r'''
+import augment 'test.dart';
+
+extension e on Object { }
+''');
+
+    await assertNoDiagnostics(r'''
+library augment 'a.dart';
+
+augment extension e { }
+''');
+  }
+
   test_lowerCase() async {
     await assertDiagnostics(r'''
 extension fooBar on Object {}
