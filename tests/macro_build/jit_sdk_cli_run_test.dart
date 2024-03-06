c2004efd47634abb37b976f64c13ff65953fc551
David Morgan <davidmorgan@google.com>
2024-03-06 12:31:37 +0000

    [macros] Add macro build tests.
    These are a new type of test that runs with the SDK under test against what
    looks like an external package; they start by running "pub get" then run SDK
    build commands to build the package, and check that macros applied correctly.
    
    Add tests for various builds that already pass, plus one that doesn't:
    cfe_sdk_cli_test fails because it was switched to run from an AOT snapshot
    and that doesn't support macros yet.
    
    Change-Id: Ic801cb61bd414d4876566452e01dd8c8203e9013
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/353100
    Reviewed-by: Jake Macdonald <jakemac@google.com>
    Reviewed-by: William Hesse <whesse@google.com>
    Commit-Queue: Morgan :) <davidmorgan@google.com>

---
+++ tests/macro_build/jit_sdk_cli_run_test.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'tester/tester.dart';
+
+void main() {
+  testMacroBuild([
+    r'$DART pub get',
+    r'$DART '
+        '--enable-experiment=macros '
+        'run '
+        'test/main.dart',
+  ]);
+}
