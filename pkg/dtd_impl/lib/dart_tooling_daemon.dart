68c74e2a97129ef3b1d0ba269aecfe615d224ba7
Dan Chevalier <danchevalier@google.com>
2024-03-07 19:33:19 +0000

    Add obfuscation token to DTD uri
    Adding this token adds a small layer of security since the caller needs to have gathered the uri in some way rather than just stumbling on the DTD port.
    
    Change-Id: I0665f4718d162daf94cb49fa1c6f4206c83d77cd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/355220
    Reviewed-by: Kenzie Davisson <kenzieschmoll@google.com>
    Commit-Queue: Dan Chevalier <danchevalier@google.com>
    Reviewed-by: Ben Konyi <bkonyi@google.com>

--- pkg/dtd_impl/lib/dart_tooling_daemon.dart
+++ pkg/dtd_impl/lib/dart_tooling_daemon.dart
@@ -101,6 +101,13 @@ class DartToolingDaemon {
 
   final String secret;
 
+  /// Any requests to DTD must have this token as the first element of the
+  /// uri path.
+  ///
+  /// This provides an obfuscation step to prevent bad actors from stumbling
+  /// onto the dtd address.
+  final String _uriToken = _generateSecret();
+
   /// The uri of the current [DartToolingDaemon] service.
   Uri? get uri => _uri;
   Uri? _uri;
@@ -118,7 +125,7 @@ class DartToolingDaemon {
       () async {
         Future<HttpServer?> startServer() async {
           try {
-            return await io.serve(_handlers().handler, host, port);
+            return await io.serve(_handlers(), host, port);
           } on SocketException catch (e) {
             errorMessage = e.message;
             if (e.osError != null) {
@@ -146,7 +153,7 @@ class DartToolingDaemon {
       scheme: 'ws',
       host: host,
       port: _server.port,
-      path: '/',
+      path: '/$_uriToken',
     );
   }
 
@@ -203,9 +210,25 @@ class DartToolingDaemon {
   // Attempt to upgrade HTTP requests to a websocket before processing them as
   // standard HTTP requests. The websocket handler will fail quickly if the
   // request doesn't appear to be a websocket upgrade request.
-  Cascade _handlers() {
-    return Cascade().add(_webSocketHandler()).add(_sseHandler());
+  Handler _handlers() {
+    return Pipeline().addMiddleware(_uriTokenHandler).addHandler(
+          Cascade().add(_webSocketHandler()).add(_sseHandler()).handler,
+        );
   }
+
+  Handler _uriTokenHandler(Handler innerHandler) => (Request request) {
+        final forbidden =
+            Response.forbidden('missing or invalid authentication code');
+        final pathSegments = request.url.pathSegments;
+        if (pathSegments.isEmpty) {
+          return forbidden;
+        }
+        final token = pathSegments[0];
+        if (token != _uriToken) {
+          return forbidden;
+        }
+        return innerHandler(request);
+      };
 
   Handler _webSocketHandler() => webSocketHandler((WebSocketChannel ws) {
         final client = DTDClient.fromWebSocket(
