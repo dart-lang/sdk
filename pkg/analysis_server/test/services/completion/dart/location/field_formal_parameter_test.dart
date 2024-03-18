28210d7793682638859e5b1336e73c2f035c84f0
Konstantin Shcheglov <scheglov@google.com>
2024-03-18 18:08:50 +0000

    Completion. Issue 55186. Suggest field formal parameter names even when starts with keyword.
    Bug: https://github.com/dart-lang/sdk/issues/55186
    Change-Id: I6f17e6fad9fe135737d75fb08200dae190c95ea1
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/357943
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/services/completion/dart/location/field_formal_parameter_test.dart
+++ pkg/analysis_server/test/services/completion/dart/location/field_formal_parameter_test.dart
@@ -15,9 +15,9 @@ void main() {
 
 @reflectiveTest
 class FieldFormalParameterTest extends AbstractCompletionDriverTest
-    with SuperFormalParameterTestCases {}
+    with FieldFormalParameterTestCases {}
 
-mixin SuperFormalParameterTestCases on AbstractCompletionDriverTest {
+mixin FieldFormalParameterTestCases on AbstractCompletionDriverTest {
   @override
   Future<void> setUp() async {
     await super.setUp();
@@ -156,6 +156,44 @@ suggestions
   second
     kind: field
     returnType: double
+''');
+  }
+
+  Future<void> test_requiredNamed_keywordName_in() async {
+    allowedIdentifiers = {'in01', 'out01'};
+    await computeSuggestions('''
+class A {
+  final int in01;
+  final int out01;
+  A({required this.in^});
+}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  in01
+    kind: field
+    returnType: int
+''');
+  }
+
+  Future<void> test_requiredPositional_keywordName_in() async {
+    allowedIdentifiers = {'in01', 'out01'};
+    await computeSuggestions('''
+class A {
+  final int in01;
+  final int out01;
+  A(this.in^);
+}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  in01
+    kind: field
+    returnType: int
 ''');
   }
 
