d45e3d1bac60449c368261c27cde2c8daf4e524c
David Morgan <davidmorgan@google.com>
2024-03-21 12:01:14 +0000

    [macros] Really find platform for Flutter desktop and web builds.
    The released layout is different from the "building Flutter to work on
    it" layout, this should work for both. Of course, an end to end test
    is needed--work in progress.
    
    R=jakemac@google.com, johnniwinther@google.com
    
    Change-Id: Ida664b6da62d8fd3b66b877f0441e771269d4fe5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/358443
    Commit-Queue: Morgan :) <davidmorgan@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Jake Macdonald <jakemac@google.com>
    Auto-Submit: Morgan :) <davidmorgan@google.com>

--- pkg/front_end/lib/src/macros/macro_target.dart
+++ pkg/front_end/lib/src/macros/macro_target.dart
@@ -2,37 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:kernel/target/targets.dart' show Target, TargetFlags;
+import 'package:kernel/target/targets.dart' show Target;
 
-import 'unsupported_vm_target.dart'
-    if (dart.library.io) 'package:vm/modular/target/vm.dart' show VmTarget;
+import 'macro_target_unsupported.dart'
+    if (dart.library.io) 'macro_target_io.dart' as impl;
 
-MacroConfiguration computeMacroConfiguration({Uri? targetSdkSummary}) {
-  // Force the SDK summary to "vm_platform_strong.dill".
-  // TODO(54404): make this sufficiently correct for all use cases.
-
-  Uri? sdkSummary;
-  if ((targetSdkSummary == null ||
-      targetSdkSummary.path == 'virtual_platform_kernel.dill' ||
-      targetSdkSummary.path.contains('vm_platform_strong.dill'))) {
-    sdkSummary = targetSdkSummary;
-  } else if (targetSdkSummary.path.contains('/platform_strong.dill')) {
-    // Flutter. Expecting the dill to be
-    // bin/cache/artifacts/engine/common/flutter_patched_sdk/platform_strong.dill
-    // and the Dart SDK to be under bin/cache/dart-sdk.
-    sdkSummary = targetSdkSummary.resolve(
-        './../../../../dart-sdk/lib/_internal/vm_platform_strong.dill');
-  } else {
-    // Non-VM target platform, not Flutter. Find the platform dill next to the
-    // non-VM platform dill.
-    sdkSummary = targetSdkSummary.resolve('./vm_platform_strong.dill');
-  }
-
-  return new MacroConfiguration(
-      target: new VmTarget(
-          new TargetFlags(soundNullSafety: true, supportMirrors: false)),
-      sdkSummary: sdkSummary);
-}
+MacroConfiguration computeMacroConfiguration({Uri? targetSdkSummary}) =>
+    impl.computeMacroConfiguration(targetSdkSummary: targetSdkSummary);
 
 class MacroConfiguration {
   final Target target;
