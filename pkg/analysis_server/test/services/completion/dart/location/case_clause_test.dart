219f8a90135000ca61f9f83b556f965f1d79de02
Konstantin Shcheglov <scheglov@google.com>
2024-03-22 15:48:49 +0000

    Completion. Issue 55167. Suggest type names for DeclaredVariablePattern.
    Bug: https://github.com/dart-lang/sdk/issues/55167
    Change-Id: I3fad2f2559bf8677afbe893d8d147a0d443e441a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/359121
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analysis_server/test/services/completion/dart/location/case_clause_test.dart
+++ pkg/analysis_server/test/services/completion/dart/location/case_clause_test.dart
@@ -61,6 +61,82 @@ suggestions
 ''');
   }
 
+  Future<void> test_afterCase_nameX_includeClass_imported() async {
+    newFile('$testPackageLibPath/a.dart', r'''
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    await computeSuggestions('''
+import 'a.dart';
+
+void f(Object? x) {
+  if (x case A0^) {}
+}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: class
+  A02
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_afterCase_nameX_includeClass_local() async {
+    await computeSuggestions('''
+void f(Object? x) {
+  if (x case A0^) {}
+}
+
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  A01
+    kind: class
+  A01
+    kind: constructorInvocation
+  A02
+    kind: class
+  A02
+    kind: constructorInvocation
+''');
+  }
+
+  Future<void> test_afterCase_nameX_includeClass_notImported() async {
+    newFile('$testPackageLibPath/a.dart', r'''
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    await computeSuggestions('''
+void f(Object? x) {
+  if (x case A0^) {}
+}
+''');
+    // TODO(scheglov): this is wrong, include only const constructors.
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  A01
+    kind: class
+  A02
+    kind: class
+''');
+  }
+
   Future<void> test_afterCaseClause_inIfElement() async {
     await computeSuggestions('''
 var v = [ if (o case != '' ^) ];
@@ -290,6 +366,69 @@ void f(Object o) {
 ''');
     assertResponse(r'''
 suggestions
+''');
+  }
+
+  Future<void> test_case_final_x_name() async {
+    await computeSuggestions('''
+void f(Object? x) {
+  if (x case final ^ y) {}
+}
+
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    assertResponse(r'''
+suggestions
+  A01
+    kind: class
+  A02
+    kind: class
+  B01
+    kind: class
+''');
+  }
+
+  Future<void> test_case_nothing_x_name() async {
+    await computeSuggestions('''
+void f(Object? x) {
+  if (x case ^ y) {}
+}
+
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    assertResponse(r'''
+suggestions
+  A01
+    kind: class
+  A02
+    kind: class
+  B01
+    kind: class
+''');
+  }
+
+  Future<void> test_declaredVariablePattern_typeX_name() async {
+    await computeSuggestions('''
+void f(Object? x) {
+  if (x case A0^ y) {}
+}
+
+class A01 {}
+class A02 {}
+class B01 {}
+''');
+    assertResponse(r'''
+replacement
+  left: 2
+suggestions
+  A01
+    kind: class
+  A02
+    kind: class
 ''');
   }
 
