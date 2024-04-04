eda91b407da71a6ab561c639ce938a0f4a51d146
Nicholas Shahan <nshahan@google.com>
2024-04-04 00:42:24 +0000

    [tests] Add implicit downcast requirements
    Some expectations in these tests rely on implicit downcasts throwing
    errors at runtime.
    
    Fixes: https://github.com/dart-lang/sdk/issues/55353
    Change-Id: Ie1923e26552b95ddb3d4199e0f8f5a2a05e2c124
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/361004
    Reviewed-by: Srujan Gaddam <srujzs@google.com>
    Commit-Queue: Nicholas Shahan <nshahan@google.com>

--- tests/lib/js/static_interop_test/call_method_test.dart
+++ tests/lib/js/static_interop_test/call_method_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Requirements=checked-implicit-downcasts
+
 /// WARNING
 ///
 /// Not all of the expectations in this test match the language specification.
