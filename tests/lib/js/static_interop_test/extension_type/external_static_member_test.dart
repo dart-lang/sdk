2631319edf4530ab0236e69a662630d482d11079
Srujan Gaddam <srujzs@google.com>
2024-04-05 17:43:48 +0000

    [dart2wasm] Use node's enclosing library annotation for lowerings
    Closes https://github.com/dart-lang/sdk/issues/55359
    
    The current library's annotation is used for the interop lowerings
    in dart2wasm. For most members, this is okay because we emit the
    equivalent JS code for the member when we visit the procedure and
    not when we visit the invocation. However, for methods, the invocation
    determines the resulting JS call due to the existence of optional
    parameters. In that case, if the invocation was not in the same
    library as the interop member declaration, it results in using the
    wrong library's annotation value.
    
    Adds tests for this case and does some cleanup of existing tests.
    
    Specifically:
    - Adds a consistent naming scheme for test libraries that are
    namespaced.
    - Adds code to delete the non-namespaced declarations so that the
    namespaced interop methods don't accidentally call those declarations.
    - Removes differentArgsMethod which was already tested in
    js_default_test.
    - Removes use of js_util in favor of js_interop_unsafe.
    
    Change-Id: Iac2abe7f11b17b149fb6726c007e1ffa434c3c5c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/361241
    Reviewed-by: Ömer Ağacan <omersa@google.com>
    Commit-Queue: Srujan Gaddam <srujzs@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

--- tests/lib/js/static_interop_test/extension_type/external_static_member_test.dart
+++ tests/lib/js/static_interop_test/extension_type/external_static_member_test.dart
@@ -2,23 +2,28 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// TODO(srujzs): There's a decent amount of code duplication in this test. We
+// should combine this with
+// tests/lib/js/static_interop_test/external_static_member_lowerings_test.dart.
+
 @JS()
 library external_static_member_test;
 
 import 'dart:js_interop';
-import 'dart:js_util' as js_util;
+import 'dart:js_interop_unsafe';
 
 import 'package:expect/minitest.dart';
+
+import 'external_static_member_with_namespaces.dart' as namespace;
 
 @JS()
 external void eval(String code);
 
 @JS()
-extension type ExternalStatic._(JSObject obj) implements Object {
+extension type ExternalStatic._(JSObject obj) implements JSObject {
   external ExternalStatic();
   external factory ExternalStatic.factory();
   external ExternalStatic.multipleArgs(double a, String b);
-  external ExternalStatic.differentArgs(double a, [String b = '']);
   ExternalStatic.nonExternal() : this.obj = ExternalStatic() as JSObject;
 
   external static String field;
@@ -34,39 +39,20 @@ extension type ExternalStatic._(JSObject obj) implements Object {
   external static set renamedGetSet(String val);
 
   external static String method();
-  external static String differentArgsMethod(String a, [String b = '']);
   @JS('method')
   external static String renamedMethod();
 }
 
-void main() {
-  eval('''
-    globalThis.ExternalStatic = function ExternalStatic(a, b) {
-      var len = arguments.length;
-      this.a = len < 1 ? 0 : a;
-      this.b = len < 2 ? '' : b;
-    }
-    globalThis.ExternalStatic.method = function() {
-      return 'method';
-    }
-    globalThis.ExternalStatic.differentArgsMethod = function(a, b) {
-      return a + b;
-    }
-    globalThis.ExternalStatic.field = 'field';
-    globalThis.ExternalStatic.finalField = 'finalField';
-    globalThis.ExternalStatic.getSet = 'getSet';
-  ''');
-
+void testStaticMembers() {
   // Constructors.
   void testExternalConstructorCall(ExternalStatic externalStatic) {
-    expect(js_util.getProperty(externalStatic, 'a'), 0);
-    expect(js_util.getProperty(externalStatic, 'b'), '');
+    expect((externalStatic['a'] as JSNumber).toDartInt, 0);
+    expect((externalStatic['b'] as JSString).toDart, '');
   }
 
   testExternalConstructorCall(ExternalStatic());
   testExternalConstructorCall(ExternalStatic.factory());
   testExternalConstructorCall(ExternalStatic.multipleArgs(0, ''));
-  testExternalConstructorCall(ExternalStatic.differentArgs(0));
   testExternalConstructorCall(ExternalStatic.nonExternal());
 
   // Fields.
@@ -88,6 +74,69 @@ void main() {
 
   // Methods.
   expect(ExternalStatic.method(), 'method');
-  expect(ExternalStatic.differentArgsMethod('method'), 'methodundefined');
   expect(ExternalStatic.renamedMethod(), 'method');
+}
+
+void testNamespacedStaticMembers() {
+  // Constructors.
+  void testExternalConstructorCall(namespace.ExternalStatic externalStatic) {
+    expect((externalStatic['a'] as JSNumber).toDartInt, 0);
+    expect((externalStatic['b'] as JSString).toDart, '');
+  }
+
+  testExternalConstructorCall(namespace.ExternalStatic());
+  testExternalConstructorCall(namespace.ExternalStatic.factory());
+  testExternalConstructorCall(namespace.ExternalStatic.multipleArgs(0, ''));
+  testExternalConstructorCall(namespace.ExternalStatic.nonExternal());
+
+  // Fields.
+  expect(namespace.ExternalStatic.field, 'field');
+  namespace.ExternalStatic.field = 'modified';
+  expect(namespace.ExternalStatic.field, 'modified');
+  expect(namespace.ExternalStatic.renamedField, 'modified');
+  namespace.ExternalStatic.renamedField = 'renamedField';
+  expect(namespace.ExternalStatic.renamedField, 'renamedField');
+  expect(namespace.ExternalStatic.finalField, 'finalField');
+
+  // Getters and setters.
+  expect(namespace.ExternalStatic.getSet, 'getSet');
+  namespace.ExternalStatic.getSet = 'modified';
+  expect(namespace.ExternalStatic.getSet, 'modified');
+  expect(namespace.ExternalStatic.renamedGetSet, 'modified');
+  namespace.ExternalStatic.renamedGetSet = 'renamedGetSet';
+  expect(namespace.ExternalStatic.renamedGetSet, 'renamedGetSet');
+
+  // Methods.
+  expect(namespace.ExternalStatic.method(), 'method');
+  expect(namespace.ExternalStatic.renamedMethod(), 'method');
+}
+
+void main() {
+  eval('''
+    globalThis.ExternalStatic = function ExternalStatic(a, b) {
+      var len = arguments.length;
+      this.a = len < 1 ? 0 : a;
+      this.b = len < 2 ? '' : b;
+    }
+    globalThis.ExternalStatic.method = function() {
+      return 'method';
+    }
+    globalThis.ExternalStatic.field = 'field';
+    globalThis.ExternalStatic.finalField = 'finalField';
+    globalThis.ExternalStatic.getSet = 'getSet';
+  ''');
+  testStaticMembers();
+  eval('''
+    var library3 = {};
+    var library2 = {library3: library3};
+    var library1 = {library2: library2};
+    globalThis.library1 = library1;
+
+    library3.ExternalStatic = globalThis.ExternalStatic;
+    library3.ExternalStatic.field = 'field';
+    library3.ExternalStatic.finalField = 'finalField';
+    library3.ExternalStatic.getSet = 'getSet';
+    delete globalThis.ExternalStatic;
+  ''');
+  testNamespacedStaticMembers();
 }
