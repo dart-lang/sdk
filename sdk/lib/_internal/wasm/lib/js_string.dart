7f275c01b65ef9177584fb3847889667e1f74616
Ömer Sinan Ağacan <omersa@google.com>
2024-04-15 08:46:58 +0000

    [dart2wasm] Escape replacement patterns when calling JS replace
    When calling `String.p.replace` we need to escape `$` characters in
    replacement argument as `$$` as Dart doesn't have the `$` replacement
    syntax.
    
    Fix extracted from https://dart-review.googlesource.com/c/sdk/+/361362
    to keep the CL small.
    
    Change-Id: I23a26dda48ed0d5ccb7fea5353f16350a2b046f3
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/362387
    Commit-Queue: Ömer Ağacan <omersa@google.com>
    Reviewed-by: Slava Egorov <vegorov@google.com>

--- sdk/lib/_internal/wasm/lib/js_string.dart
+++ sdk/lib/_internal/wasm/lib/js_string.dart
@@ -131,7 +131,7 @@ final class JSStringImpl implements String {
         return split(from).join(to);
       }
     } else if (from is js.JSSyntaxRegExp) {
-      return _replaceJS(js.regExpGetGlobalNative(from), to);
+      return _replaceJS(js.regExpGetGlobalNative(from), _escapeReplacement(to));
     } else {
       int startIndex = 0;
       StringBuffer result = StringBuffer();
@@ -236,7 +236,7 @@ final class JSStringImpl implements String {
     }
     if (from is js.JSSyntaxRegExp) {
       return startIndex == 0
-          ? _replaceJS(js.regExpGetNative(from), to)
+          ? _replaceJS(js.regExpGetNative(from), _escapeReplacement(to))
           : _replaceFirstRE(from, to, startIndex);
     }
     Iterator<Match> matches = from.allMatches(this, startIndex).iterator;
@@ -700,6 +700,14 @@ final class JSStringImpl implements String {
 String _matchString(Match match) => match[0]!;
 
 String _stringIdentity(String string) => string;
+
+String _escapeReplacement(String replacement) {
+  // The JavaScript `String.prototype.replace` method recognizes replacement
+  // patterns in the replacement string. Dart does not have that behavior, so
+  // the replacement patterns need to be escaped.
+  return JSStringImpl(js.JS<WasmExternRef>(
+      r'(s) => s.replace(/\$/g, "$$$$")', replacement.toJS.toExternRef));
+}
 
 @pragma("wasm:export", "\$jsStringToJSStringImpl")
 JSStringImpl _jsStringToJSStringImpl(WasmExternRef? string) =>
