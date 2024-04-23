0b069f1f253f0d82d4ff32f18fd5463ae7ef2736
pq <pquitslund@google.com>
2024-04-23 18:52:50 +0000

    augmentation support for `avoid_annotating_with_dynamic`
    Fixes: https://github.com/dart-lang/linter/issues/4953
    
    Change-Id: I637682c1b6ef45668259354cafe3c6d8e4bcafda
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/363945
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/lib/src/rules/avoid_annotating_with_dynamic.dart
+++ pkg/linter/lib/src/rules/avoid_annotating_with_dynamic.dart
@@ -7,6 +7,7 @@ import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/type.dart';
 
 import '../analyzer.dart';
+import '../extensions.dart';
 
 const _desc = r'Avoid annotating with dynamic when not required.';
 
@@ -82,8 +83,23 @@ class _Visitor extends SimpleAstVisitor<void> {
   }
 
   void _checkNode(NormalFormalParameter node, TypeAnnotation? type) {
+    if (node.inAugmentation) return;
+
     if (type is NamedType && type.type is DynamicType) {
       rule.reportLint(node);
     }
+  }
+}
+
+extension on AstNode {
+  bool get inAugmentation {
+    AstNode? target = this;
+    while (target != null) {
+      if (target.isAugmentation) return true;
+      if (target is Block) return false;
+      if (target is Declaration) return false;
+      target = target.parent;
+    }
+    return false;
   }
 }
