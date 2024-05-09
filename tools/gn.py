ebcf5fee57de62c4d1d435c69a2f86ba3fe761d9
Jonny Wang <jonnywang@google.com>
2024-05-09 18:33:33 +0000

    [fuchsia] Update Fuchsia API level to 18
    Bug: b/339619418
    Change-Id: Id8cc841ca1efd4356d54ed5513e958dc2c8c0cc6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/365981
    Reviewed-by: Ryan Macnak <rmacnak@google.com>

--- tools/gn.py
+++ tools/gn.py
@@ -235,7 +235,7 @@ def ToGnArgs(args, mode, arch, target_os, sanitizer, verify_sdk_hash,
             gn_args['arm_float_abi'] = floatabi
             gn_args['arm_use_neon'] = True
     if gn_args['target_os'] == 'fuchsia':
-        gn_args['fuchsia_target_api_level'] = 16
+        gn_args['fuchsia_target_api_level'] = 18
 
     gn_args['is_debug'] = mode == 'debug'
     gn_args['is_release'] = mode == 'release'
