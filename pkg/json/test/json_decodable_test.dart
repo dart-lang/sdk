6bdc615126581994f87063f9c29aa3fb5011da32
Jake Macdonald <jakemac@google.com>
2024-05-10 16:12:19 +0000

    add JsonEncodable and JsonDecodable
    Change-Id: Iedda1eaca3c8958a67093d998f71c528abee9bda
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/366000
    Reviewed-by: Bob Nystrom <rnystrom@google.com>
    Auto-Submit: Jake Macdonald <jakemac@google.com>
    Commit-Queue: Jake Macdonald <jakemac@google.com>

---
+++ pkg/json/test/json_decodable_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+// SharedOptions=--enable-experiment=macros
+
+import 'package:json/json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('generates a fromJson constructor', () {
+    expect(A.fromJson({'b': 5}), predicate<A>((a) => a.b == 5));
+  });
+
+  test('does not generate a toJson method', () {
+    expect(() => (A(5) as dynamic).toJson(), throwsA(isA<NoSuchMethodError>()));
+  });
+}
+
+@JsonDecodable()
+class A {
+  final int b;
+
+  A(this.b);
+}
