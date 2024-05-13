a9c2f1b6aa7cf1c64f83e9301c23483dda41047b
Sam Rawlins <srawlins@google.com>
2024-05-13 18:51:41 +0000

    linter: move cast_nullable_to_non_nullable tests
    Change-Id: I5bd5482108b9c0bb29936819ef4151d6b947a4b5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/366080
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/linter/test/rules/cast_nullable_to_non_nullable_test.dart
+++ pkg/linter/test/rules/cast_nullable_to_non_nullable_test.dart
@@ -17,13 +17,40 @@ class CastNullableToNonNullableTest extends LintRuleTest {
   @override
   String get lintRule => 'cast_nullable_to_non_nullable';
 
-  test_castNullable() async {
+  test_castDynamic_toNonNullable() async {
+    await assertNoDiagnostics(r'''
+void f(dynamic a) {
+  a as String;
+}
+''');
+  }
+
+  test_castDynamic_toNullable() async {
+    await assertNoDiagnostics(r'''
+void f(dynamic a) {
+  a as String?;
+}
+''');
+  }
+
+  test_castNullable_toNonNullable() async {
     await assertDiagnostics(r'''
 String? s;
 var a = s as String;
 ''', [
       lint(19, 11),
     ]);
+  }
+
+  test_castNullable_toNullable() async {
+    await assertNoDiagnostics(r'''
+class A {}
+class B extends A {}
+
+void f(A? a) {
+  a as B?;
+}
+''');
   }
 
   test_castNullable_unresolvedType() async {
