2fa0501985ecdfd97a765768fd83f47fe7f8510e
Konstantin Shcheglov <scheglov@google.com>
2024-05-17 18:07:34 +0000

    Macro. Issue 55756. Include the name of the unresolved identifier.
    Bug: https://github.com/dart-lang/sdk/issues/55756
    Change-Id: Ia1eab2bdf8900843151386f7dc31423e5f62805a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/366966
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/summary2/macro_declarations.dart
+++ pkg/analyzer/lib/src/summary2/macro_declarations.dart
@@ -304,6 +304,8 @@ class DeclarationBuilder {
           uri: null,
           staticScope: null,
         );
+      case null:
+        throw ArgumentError('Unresolved identifier: ${identifier.name}');
       default:
         throw UnimplementedError('${element.runtimeType}');
     }
