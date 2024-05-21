bd8f87d477cc515caf2d2a2f4a6b5f540486e4f2
Kallen Tu <kallentu@google.com>
2024-05-21 22:15:11 +0000

    [wildcard-variables] Test non-binding behaviour with top-level declarations.
    Added a little section for late wildcard variables too.
    
    This CL tests mixing top-level wildcard declarations with local wildcard declarations and the non-shadowing, non-binding behaviour.
    
    Bug: https://github.com/dart-lang/sdk/issues/55652
    Change-Id: I72e7cfb1b2d80a3934af355579c36252881cf3fb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/367241
    Reviewed-by: Lasse Nielsen <lrn@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/wildcard_variables/non_binding/top_level_typedef_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test the interactions between a wildcard typedef, which is binding, with
+// local non-binding wildcard variables.
+
+// SharedOptions=--enable-experiment=wildcard-variables
+
+import 'package:expect/expect.dart';
+
+typedef _ = Type;
+
+_ function<_ extends _>([_ _ = _]) => _;
+
+void main<_ extends _>() {
+  _ _ = _;
+  final _ _ = _;
+  const _ _ = _;
+  _ foo<_ extends _>([_ _ = int]) => _;
+  _ bar<_ extends _>([_ _ = _]) => int;
+  Expect.type<Type>(foo());
+  Expect.type<int>(bar());
+}
+
+class CConst {
+  static const _ = 42;
+  void member<_>() {
+    var _ = _;
+    final _ = _;
+    const _ = _;
+    int _() => 1;
+    int foo<_>([String _ = "$_"]) => _;
+    _ = foo();
+  }
+}
