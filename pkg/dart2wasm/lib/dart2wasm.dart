7b61a7c888225def6909dc43836b7c6a2d94a28a
Martin Kustermann <kustermann@google.com>
2024-05-28 12:35:57 +0000

    [dart2wasm] Remove deprecated --omit-type-checks/--dart-sdk options from dart2wasm
    Flutter no longer uses the --dart-sdk option (see [0]).
    g3 usages will be removed in [1]
    
    
    [0] https://github.com/flutter/flutter/pull/149129
    [1] cl/637819163
    
    Change-Id: Ic06bb30da45428367ddcbfaf165c8f046dcbaaa0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/368306
    Reviewed-by: Ömer Ağacan <omersa@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- pkg/dart2wasm/lib/dart2wasm.dart
+++ pkg/dart2wasm/lib/dart2wasm.dart
@@ -47,13 +47,6 @@ final List<Option> options = [
   Flag("omit-implicit-checks",
       (o, value) => o.translatorOptions.omitImplicitTypeChecks = value,
       defaultsTo: _d.translatorOptions.omitImplicitTypeChecks),
-  // TODO(http://dartbug.com/54675): Deprecate & Remove this one.
-  Flag("omit-type-checks", (o, value) {
-    o.translatorOptions.omitImplicitTypeChecks = value;
-    o.translatorOptions.omitExplicitTypeChecks = value;
-  },
-      defaultsTo: _d.translatorOptions.omitImplicitTypeChecks &&
-          _d.translatorOptions.omitExplicitTypeChecks),
   Flag("omit-bounds-checks", (o, value) {
     o.translatorOptions.omitBoundsChecks = value;
   }, defaultsTo: _d.translatorOptions.omitBoundsChecks),
@@ -67,9 +60,6 @@ final List<Option> options = [
       defaultsTo: "${_d.translatorOptions.inliningLimit}"),
   IntOption("shared-memory-max-pages",
       (o, value) => o.translatorOptions.sharedMemoryMaxPages = value),
-  StringOption("dart-sdk", (o, value) {
-    /* ignored: Remove when flutter no longer passes this. */
-  }, defaultsTo: null, hide: true),
   UriOption("packages", (o, value) => o.packagesPath = value),
   UriOption("libraries-spec", (o, value) => o.librariesSpecPath = value),
   UriOption("platform", (o, value) => o.platformPath = value),
