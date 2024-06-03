feea19e3767790764ac01e10a22156cce32f58fd
Sam Rawlins <srawlins@google.com>
2024-06-03 23:18:18 +0000

    Remove old UpdateSdkConstraints fixes
    Cq-Include-Trybots: luci.dart.try:flutter-analyze-try,analyzer-win-release-try,pkg-win-release-try
    Change-Id: I01acce254064c667ec641114ffd8d7247d11a0e8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/369542
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Auto-Submit: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/update_sdk_constraints.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/update_sdk_constraints.dart
@@ -15,30 +15,10 @@ class UpdateSdkConstraints extends ResolvedCorrectionProducer {
   /// The minimum version to which the SDK constraints should be updated.
   final String _minimumVersion;
 
-  /// Initialize a newly created instance that will update the SDK constraints
+  /// Initializes a newly created instance that will update the SDK constraints
   /// to '2.14.0'.
   UpdateSdkConstraints.version_2_14_0({required super.context})
       : _minimumVersion = '2.14.0';
-
-  /// Initialize a newly created instance that will update the SDK constraints
-  /// to '2.1.0'.
-  UpdateSdkConstraints.version_2_1_0({required super.context})
-      : _minimumVersion = '2.1.0';
-
-  /// Initialize a newly created instance that will update the SDK constraints
-  /// to '2.2.0'.
-  UpdateSdkConstraints.version_2_2_0({required super.context})
-      : _minimumVersion = '2.2.0';
-
-  /// Initialize a newly created instance that will update the SDK constraints
-  /// to '2.2.0'.
-  UpdateSdkConstraints.version_2_2_2({required super.context})
-      : _minimumVersion = '2.2.2';
-
-  /// Initialize a newly created instance that will update the SDK constraints
-  /// to '2.2.0'.
-  UpdateSdkConstraints.version_2_6_0({required super.context})
-      : _minimumVersion = '2.6.0';
 
   @override
   // Too nuanced to do unattended to apply in bulk.
