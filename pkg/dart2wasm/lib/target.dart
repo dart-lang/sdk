4796b25a308ea80d045b4c659ebf6281d532b6e7
Ömer Sinan Ağacan <omersa@google.com>
2024-06-11 13:12:27 +0000

    Reland "[dart2wasm] Check import/export pragmas in user code"
    This is a reland of commit 35bc17a0fa3a347089dfcd7a1c4f781b37c32db7
    
    Changes from the original CL is that we now allow packages in
    `allowedInteropLibrariesInDart2WasmPackages`.
    
    Original change's description:
    > [dart2wasm] Check import/export pragmas in user code
    >
    > Change-Id: I926d108a4571d685c67d3a174a8e506910cce8f7
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/369020
    > Commit-Queue: Ömer Ağacan <omersa@google.com>
    > Reviewed-by: Martin Kustermann <kustermann@google.com>
    
    Change-Id: I69f61ecf246dfdbfcab372c6c2adb2a64f2d0b36
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/370900
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Ömer Ağacan <omersa@google.com>

--- pkg/dart2wasm/lib/target.dart
+++ pkg/dart2wasm/lib/target.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:_fe_analyzer_shared/src/messages/codes.dart'
-    show Message, LocatedMessage;
+    show Message, LocatedMessage, messageWasmImportOrExportInUserCode;
 import 'package:_js_interop_checks/js_interop_checks.dart';
 import 'package:_js_interop_checks/src/js_interop.dart' as jsInteropHelper;
 import 'package:_js_interop_checks/src/transformations/shared_interop_transformer.dart';
@@ -247,6 +247,11 @@ class WasmTarget extends Target {
       ReferenceFromIndex? referenceFromIndex,
       {void Function(String msg)? logger,
       ChangedStructureNotifier? changedStructureNotifier}) {
+    // Check `wasm:import` and `wasm:export` pragmas before FFI transforms as
+    // FFI transforms convert JS interop annotations to these pragmas.
+    _checkWasmImportExportPragmas(libraries, coreTypes,
+        diagnosticReporter as DiagnosticReporter<Message, LocatedMessage>);
+
     Set<Library> transitiveImportingJSInterop = {
       ...jsInteropHelper.calculateTransitiveImportsOfJsInteropIfUsed(
           component, Uri.parse("package:js/js.dart")),
@@ -525,5 +530,54 @@ class WasmVerification extends Verification {
           node is AsExpression;
     }
     return false;
+  }
+}
+
+final _dartCoreUri = Uri.parse('dart:core');
+
+/// Check that `wasm:import` and `wasm:export` pragmas are only used in `dart:`
+/// libraries and in tests, with the exception of
+/// `reject_import_export_pragmas` test.
+void _checkWasmImportExportPragmas(List<Library> libraries, CoreTypes coreTypes,
+    DiagnosticReporter<Message, LocatedMessage> diagnosticReporter) {
+  for (Library library in libraries) {
+    final importUri = library.importUri;
+    if (importUri.isScheme('dart') ||
+        (importUri.isScheme('package') &&
+            JsInteropChecks.allowedInteropLibrariesInDart2WasmPackages
+                .any((pkg) => importUri.pathSegments.first == pkg)) ||
+        (importUri.path.contains('tests/web/wasm') &&
+            !importUri.path.contains('reject_import_export_pragmas'))) {
+      continue;
+    }
+
+    for (Member member in library.members) {
+      for (Expression annotation in member.annotations) {
+        if (annotation is! ConstantExpression) {
+          continue;
+        }
+        final annotationConstant = annotation.constant;
+        if (annotationConstant is! InstanceConstant) {
+          continue;
+        }
+        final cls = annotationConstant.classNode;
+        if (cls.name == 'pragma' &&
+            cls.enclosingLibrary.importUri == _dartCoreUri) {
+          final pragmaName = annotationConstant
+              .fieldValues[coreTypes.pragmaName.fieldReference];
+          if (pragmaName is StringConstant) {
+            if (pragmaName.value == 'wasm:import' ||
+                pragmaName.value == 'wasm:export') {
+              diagnosticReporter.report(
+                messageWasmImportOrExportInUserCode,
+                annotation.fileOffset,
+                0,
+                library.fileUri,
+              );
+            }
+          }
+        }
+      }
+    }
   }
 }
