1d3b1154307113083e6f3722205f061835a537ef
Konstantin Shcheglov <scheglov@google.com>
2024-07-01 18:42:20 +0000

    Augment. A few tests added during investigation of a co19/ failing test.
    Change-Id: Ie52049196fb2382ee1aa539adfce2c7ccc90f0a6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/373728
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/diagnostics/inconsistent_inheritance_test.dart
+++ pkg/analyzer/test/src/diagnostics/inconsistent_inheritance_test.dart
@@ -15,6 +15,76 @@ main() {
 
 @reflectiveTest
 class InconsistentInheritanceTest extends PubPackageResolutionTest {
+  test_class_augmentWithInterface_augmentWithMixin() async {
+    var a = newFile('$testPackageLibPath/a.dart', r'''
+import augment 'b.dart';
+import augment 'c.dart';
+
+mixin A {
+  void foo(int _);
+}
+
+abstract class B {
+  void foo(String _);
+}
+
+abstract class C extends Object {}
+''');
+
+    var b = newFile('$testPackageLibPath/b.dart', r'''
+augment library 'a.dart';
+
+augment abstract class C implements B {}
+''');
+
+    var c = newFile('$testPackageLibPath/c.dart', r'''
+augment library 'a.dart';
+
+augment abstract class C with A {}
+''');
+
+    await assertErrorsInFile2(a, [
+      error(CompileTimeErrorCode.INCONSISTENT_INHERITANCE, 142, 1),
+    ]);
+    await assertErrorsInFile2(b, []);
+    await assertErrorsInFile2(c, []);
+  }
+
+  test_class_augmentWithMixin_augmentWithInterface() async {
+    var a = newFile('$testPackageLibPath/a.dart', r'''
+import augment 'b.dart';
+import augment 'c.dart';
+
+mixin A {
+  void foo(int _);
+}
+
+abstract class B {
+  void foo(String _);
+}
+
+abstract class C extends Object {}
+''');
+
+    var b = newFile('$testPackageLibPath/b.dart', r'''
+augment library 'a.dart';
+
+augment abstract class C with A {}
+''');
+
+    var c = newFile('$testPackageLibPath/c.dart', r'''
+augment library 'a.dart';
+
+augment abstract class C implements B {}
+''');
+
+    await assertErrorsInFile2(a, [
+      error(CompileTimeErrorCode.INCONSISTENT_INHERITANCE, 142, 1),
+    ]);
+    await assertErrorsInFile2(b, []);
+    await assertErrorsInFile2(c, []);
+  }
+
   /// https://github.com/dart-lang/sdk/issues/47026
   test_class_covariantInSuper_withTwoUnrelated() async {
     await assertErrorsInCode('''
