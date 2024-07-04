6fae002f59c547bc03c56142a57ca620fbbacf1d
Martin Kustermann <kustermann@google.com>
2024-07-04 10:32:30 +0000

    [sdk] Use `{List,Set}.of()` instead of `{List,Set}.from()`
    The `{List,Set}.from` take an `Iterable` and perform a `is Iterable<T>`
    type check inside.
    
    The `{List,Set}.of` in comparison take a `Iterable<T>` and do not
    perform a type check.
    
    TEST=ci
    
    CoreLibraryReviewExempt: Optimizes implementation, no API changes.
    Change-Id: I99f138bfbdedac15752b01bfdf0bbec258bb9107
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/374561
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- sdk/lib/_internal/wasm/lib/js_typed_array.dart
+++ sdk/lib/_internal/wasm/lib/js_typed_array.dart
@@ -473,11 +473,11 @@ mixin _IntListMixin implements List<int> {
   }
 
   List<int> toList({bool growable = true}) {
-    return List<int>.from(this, growable: growable);
+    return List<int>.of(this, growable: growable);
   }
 
   Set<int> toSet() {
-    return Set<int>.from(this);
+    return Set<int>.of(this);
   }
 
   void forEach(void f(int element)) {
@@ -1687,11 +1687,11 @@ mixin _DoubleListMixin implements List<double> {
   }
 
   List<double> toList({bool growable = true}) {
-    return List<double>.from(this, growable: growable);
+    return List<double>.of(this, growable: growable);
   }
 
   Set<double> toSet() {
-    return Set<double>.from(this);
+    return Set<double>.of(this);
   }
 
   void forEach(void f(double element)) {
