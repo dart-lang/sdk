48599f56bfd085b67192ada304f402c8e54065bd
Martin Kustermann <kustermann@google.com>
2024-07-11 14:38:21 +0000

    Reland "[dart2wasm] Do not assign class ids to anonymous mixin classes."
    No changes from original CL. Refactoring in [0] will make us
    no longer trigger a bug with this CL where a target that is
    only reachable via super calls got the wrong representation.
    
    [0] https://dart-review.googlesource.com/c/sdk/+/375280
    
    Issue https://github.com/flutter/flutter/issues/151473
    
    Original CL description:
    
      Anonymous mixin application classes get created as a desguaring of mixin
      applications.
    
      For example this
    
         mixin M {
           <M-body>
         }
         class Base {}
         class A extends Base with M {}
    
      gets desugared into
    
         class Base {}
         abstract class A&Base&M1 extends Base implements M /*isAnonymousMixin*/ {
           <copy-of-M-body>
         }
         class A extends A&Base&M1 {}
    
      Those anonymous mixin application classes are not real classes: They cannot
      be named in code, cannot be used as type arguments, type literals, type
      checks etc,
    
      => This CL avoids assigning class ids to them.
      => This will shrink the type name array.
      => This will shrink RTT information.
      => This makes other classes get lower ids assigned
    
      Size changes of `...opt.wasm` file:
      * Hello: -5%
      * FluteComplex: -1.5%
    
      Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/374320
    Change-Id: I09fdc66ba9c2b5deb998988093e859361124f517
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/375103
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>

--- pkg/dart2wasm/lib/types.dart
+++ pkg/dart2wasm/lib/types.dart
@@ -914,6 +914,7 @@ class RuntimeTypeInformation {
         continue;
       }
       Class superclass = superclassInfo.cls!;
+      assert(!superclass.isAnonymousMixin);
 
       // TODO(joshualitt): This includes abstract types that can't be
       // instantiated, but might be needed for subtype checks. The majority of
@@ -925,6 +926,8 @@ class RuntimeTypeInformation {
       Iterable<InterfaceType> subtypes = subclasses.map(
           (Class cls) => cls.getThisType(coreTypes, Nullability.nonNullable));
       for (InterfaceType subtype in subtypes) {
+        if (subtype.classNode.isAnonymousMixin) continue;
+
         types.interfaceTypeEnvironment._add(subtype);
 
         final List<DartType>? typeArguments = translator.hierarchy
