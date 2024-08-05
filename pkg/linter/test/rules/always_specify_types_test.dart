967849a65d15f236f6cbbfc8b6c02b3f1a738210
Sam Rawlins <srawlins@google.com>
2024-08-05 20:26:48 +0000

    linter: Migrate 4 legacy tests
    * always_specify_types
    * avoid_double_and_int_checks
    * avoid_field_initializers_in_const_classes
    * avoid_null_checks_in_equality_operators
    
    Cq-Include-Trybots: luci.dart.try:flutter-analyze-try,analyzer-win-release-try,pkg-win-release-try
    Change-Id: I7829f46251ac94497bb8d577dd8c3a5f42f343e8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/378575
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/test/rules/always_specify_types_test.dart
+++ pkg/linter/test/rules/always_specify_types_test.dart
@@ -20,6 +20,76 @@ class AlwaysSpecifyTypesTest extends LintRuleTest {
   @override
   String get lintRule => 'always_specify_types';
 
+  test_0() async {
+    await assertNoDiagnostics(r'''
+/// https://github.com/dart-lang/linter/issues/3275
+typedef Foo1 = Map<String, Object>;
+final Foo1 foo = Foo1();
+''');
+  }
+
+  test_34() async {
+    // https://github.com/dart-lang/linter/issues/851
+    await assertNoDiagnostics(r'''
+import 'package:meta/meta.dart';
+void f() {
+  g<dynamic>();
+  g();
+}
+
+@optionalTypeArgs
+void g<T>() {}
+''');
+  }
+
+  test_catchVariable_omitted() async {
+    // https://codereview.chromium.org/1427223002/
+    await assertNoDiagnostics(r'''
+void f() {
+  try {
+  } catch (e) {
+    print(e);
+  }
+}
+''');
+  }
+
+  test_constructorTearoff_keptGeneric() async {
+    await assertNoDiagnostics(r'''
+void f() {
+  List<E> Function<E>(int, E) filledList = List.filled;
+}
+''');
+  }
+
+  test_constructorTearoff_typeArgument() async {
+    await assertDiagnostics(r'''
+void f() {
+  List<List>.filled;
+}
+''', [
+      lint(18, 4),
+    ]);
+  }
+
+  test_declaredVariable_genericTypeAlias() async {
+    await assertDiagnostics(r'''
+typedef StringMap<V> = Map<String, V>;
+StringMap? x;
+''', [
+      lint(39, 10),
+    ]);
+  }
+
+  test_declaredVariable_genericTypeAlias_inferredTypeArguments() async {
+    await assertDiagnostics(r'''
+typedef StringMap<V> = Map<String, V>;
+StringMap x = StringMap<String>();
+''', [
+      lint(39, 9),
+    ]);
+  }
+
   test_extensionType_optionalTypeArgs() async {
     await assertNoDiagnostics(r'''
 import 'package:meta/meta.dart';
@@ -43,6 +113,130 @@ void f() {
 ''', [
       lint(45, 1),
       lint(51, 1),
+    ]);
+  }
+
+  test_field_var() async {
+    await assertDiagnostics(r'''
+class C {
+  var x;
+}
+''', [
+      lint(12, 3),
+    ]);
+  }
+
+  test_forLoopVariableDeclaration_var() async {
+    await assertDiagnostics(r'''
+void f() {
+  for (var i = 0; i < 10; ++i) {
+    print(i);
+  }
+}
+''', [
+      lint(18, 3),
+    ]);
+  }
+
+  test_function_parameterType_explicit() async {
+    await assertNoDiagnostics(r'''
+void f(int x) {}
+''');
+  }
+
+  test_function_parameterType_final() async {
+    await assertDiagnostics(r'''
+void f(final x) {}
+''', [
+      lint(7, 5),
+    ]);
+  }
+
+  test_function_parameterType_omitted() async {
+    await assertDiagnostics(r'''
+void f(p) {}
+''', [
+      lint(7, 1),
+    ]);
+  }
+
+  test_function_parameterType_var() async {
+    await assertDiagnostics(r'''
+void f(var p) {}
+''', [
+      lint(7, 3),
+    ]);
+  }
+
+  test_functionExpression_parameterType_omitted() async {
+    await assertDiagnostics(r'''
+void f(List<String> p) {
+  p.forEach((s) => print(s));
+}
+''', [
+      lint(38, 1),
+    ]);
+  }
+
+  test_functionExpression_parameterType_omitted_wildcard() async {
+    await assertNoDiagnostics(r'''
+void f(List<String> p) {
+  p.forEach((_) {});
+}
+''');
+  }
+
+  test_functionExpression_parameterType_var() async {
+    await assertDiagnostics(r'''
+void f(List<String> p) {
+  p.forEach((s) => print(s));
+}
+''', [
+      lint(38, 1),
+    ]);
+  }
+
+  test_genericFunctionTypedVariable_invocation_instantiated() async {
+    await assertNoDiagnostics(r'''
+void f() {
+  List<E> Function<E>(int, E) filledList = List.filled;
+  filledList<int>(3, 3);
+}
+''');
+  }
+
+  test_genericFunctionTypedVariable_invocation_uninstantiated() async {
+    // See #2914.
+    await assertNoDiagnostics(r'''
+void f() {
+  List<E> Function<E>(int, E) filledList = List.filled;
+  filledList(3, 3);
+}
+''');
+  }
+
+  test_instanceCreation_genericTypeAlias_implicitTypeArgument() async {
+    await assertDiagnostics(r'''
+typedef StringMap<V> = Map<String, V>;
+StringMap<String> x = StringMap();
+''', [
+      lint(61, 9),
+    ]);
+  }
+
+  test_isExpression_typeArgument_implicit() async {
+    await assertNoDiagnostics(r'''
+void f(Object p) {
+  p is Map;
+}
+''');
+  }
+
+  test_listLiteral_inferredTypeArgument() async {
+    await assertDiagnostics(r'''
+List<String> x = [];
+''', [
+      lint(17, 1),
     ]);
   }
 
@@ -72,6 +266,34 @@ f() {
   var [int a] = <int>[1];
 }
 ''');
+  }
+
+  test_localVariableDeclaration_var() async {
+    await assertDiagnostics(r'''
+void f() {
+  var x = '';
+}
+''', [
+      lint(13, 3),
+    ]);
+  }
+
+  test_localVariableDeclaration_var_multiple() async {
+    await assertDiagnostics(r'''
+void f() {
+  var x = '', y = 1.2;
+}
+''', [
+      lint(13, 3),
+    ]);
+  }
+
+  test_mapLiteral_inferredTypeArguments() async {
+    await assertDiagnostics(r'''
+Map<String, String> x = {};
+''', [
+      lint(24, 1),
+    ]);
   }
 
   test_mapPattern_destructured() async {
@@ -162,5 +384,114 @@ f() {
   }
 }
 ''');
+  }
+
+  test_setLiteral_inferredTypeArgument() async {
+    await assertDiagnostics(r'''
+Set<String> set = {};
+''', [
+      lint(18, 1),
+    ]);
+  }
+
+  test_staticField_var() async {
+    await assertDiagnostics(r'''
+class C {
+  var x;
+}
+''', [
+      lint(12, 3),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_explicitType() async {
+    await assertNoDiagnostics(r'''
+final int x = 3;
+''');
+  }
+
+  test_topLevelVariableDeclaration_implicitTypeArgument() async {
+    await assertDiagnostics(r'''
+List? x;
+''', [
+      lint(0, 5),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_missingType_const() async {
+    await assertDiagnostics(r'''
+const x = 2;
+''', [
+      lint(0, 5),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_missingType_final() async {
+    await assertDiagnostics(r'''
+final x = 1;
+''', [
+      lint(0, 5),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_missingType_final_multiple() async {
+    await assertDiagnostics(r'''
+final x = 1, y = '', z = 1.2;
+''', [
+      lint(0, 5),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_missingType_multiple() async {
+    await assertDiagnostics(r'''
+var x = '', y = '';
+''', [
+      lint(0, 3),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_typeArgument_implicitTypeArgument() async {
+    await assertDiagnostics(r'''
+List<List>? x;
+''', [
+      lint(5, 4),
+    ]);
+  }
+
+  test_topLevelVariableDeclaration_var() async {
+    await assertDiagnostics(r'''
+var x;
+''', [
+      lint(0, 3),
+    ]);
+  }
+
+  test_typedef_aliased_typeArgument_withImplicitTypeArgument() async {
+    await assertDiagnostics(r'''
+typedef StringMap<V> = Map<String, V>;
+typedef MapList = List<StringMap>;
+''', [
+      lint(62, 9),
+    ]);
+  }
+
+  test_typedef_typeArgument_withExplicitTypeArgument() async {
+    await assertNoDiagnostics(r'''
+typedef JsonMap = Map<String, dynamic>;
+''');
+  }
+
+  test_typedef_typeArgument_withExplicitTypeArgument_typeVariable() async {
+    await assertNoDiagnostics(r'''
+typedef StringMap<V> = Map<String, V>;
+''');
+  }
+
+  test_typedef_withImplicitTypeArgument() async {
+    await assertDiagnostics(r'''
+typedef RawList = List;
+''', [
+      lint(18, 4),
+    ]);
   }
 }
---
+++ pkg/linter/test/rules/avoid_double_and_int_checks_test.dart
@@ -0,0 +1,69 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+import '../rule_test_support.dart';
+
+main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(AvoidDoubleAndIntChecksTest);
+  });
+}
+
+@reflectiveTest
+class AvoidDoubleAndIntChecksTest extends LintRuleTest {
+  @override
+  String get lintRule => 'avoid_double_and_int_checks';
+
+  test_checkingForDouble() async {
+    await assertNoDiagnostics(r'''
+void f(m) {
+  if (m is double) {}
+}
+''');
+  }
+
+  test_checkingForDoubleAfterInt() async {
+    await assertNoDiagnostics(r'''
+void f(m) {
+  if (m is int) {}
+  else if (m is double) {}
+}
+''');
+  }
+
+  test_checkingForIntAfterDouble() async {
+    await assertDiagnostics(r'''
+void f(m) {
+  if (m is double) {}
+  else if (m is int) {}
+}
+''', [
+      lint(45, 8),
+    ]);
+  }
+
+  test_checkingForIntAfterDouble_getter() async {
+    await assertNoDiagnostics(r'''
+get g => null;
+void f() {
+  if (g is double) {}
+  else if (g is int) {}
+}
+''');
+  }
+
+  test_checkingForIntAfterDouble_localVariable() async {
+    await assertDiagnostics(r'''
+void f() {
+  var m;
+  if (m is double) {}
+  else if (m is int) {}
+}
+''', [
+      lint(53, 8),
+    ]);
+  }
+}
