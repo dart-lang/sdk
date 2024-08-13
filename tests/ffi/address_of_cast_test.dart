c328011a6e7a4cad8363b7d59c7be3aec6e09866
Aravind <codesculptures@gmail.com>
2024-08-13 23:09:46 +0530

    Adding SharedObjects comment to make the test pass on Android

--- tests/ffi/address_of_cast_test.dart
+++ tests/ffi/address_of_cast_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// SharedObjects=ffi_test_functions
+
 // @dart=3.5
 
 import 'dart:ffi';
