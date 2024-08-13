51eb1888d8523680abb554e8203cd6386029543d
Sam Rawlins <srawlins@google.com>
2024-08-13 18:48:08 +0000

    linter: move 3 rule tests:
    * avoid_shadowing_type_parameters
    * avoid_type_to_string
    * avoid_types_as_parameter_names
    
    Cq-Include-Trybots: luci.dart.try:flutter-analyze-try,analyzer-win-release-try,pkg-win-release-try
    Change-Id: Id2ddef557475a76244cf4c75f5720c469b34cf3b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/380142
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Auto-Submit: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/test/rules/avoid_type_to_string_test.dart
+++ pkg/linter/test/rules/avoid_type_to_string_test.dart
@@ -17,6 +17,18 @@ class AvoidTypeToStringTest extends LintRuleTest {
   @override
   String get lintRule => 'avoid_type_to_string';
 
+  test_extensionOnType_implicitThis() async {
+    await assertDiagnostics(r'''
+extension E on Type {
+  void f() {
+    toString();
+  }
+}
+''', [
+      lint(39, 8),
+    ]);
+  }
+
   test_extensionType_implicitThis() async {
     await assertDiagnostics(r'''
 extension type E(int i) {
@@ -38,6 +50,144 @@ extension type E(int i) {
 }
 ''', [
       lint(55, 8),
+    ]);
+  }
+
+  test_mixinOnType_explicitThis() async {
+    await assertDiagnostics(r'''
+mixin M on Type {
+  late var x = this.toString();
+}
+''', [
+      lint(38, 8),
+    ]);
+  }
+
+  test_mixinOnType_implicitThis() async {
+    await assertDiagnostics(r'''
+mixin M on Type {
+  late var x = toString();
+}
+''', [
+      lint(33, 8),
+    ]);
+  }
+
+  test_runtimeType() async {
+    await assertDiagnostics(r'''
+var x = 7.runtimeType.toString();
+''', [
+      lint(22, 8),
+    ]);
+  }
+
+  test_type_tearoff() async {
+    await assertDiagnostics(r'''
+void f() {
+  foo(7.runtimeType.toString);
+}
+void foo(String Function() p) {}
+''', [
+      lint(31, 8),
+    ]);
+  }
+
+  test_typeExtendingType_withToStringOverride() async {
+    await assertNoDiagnostics(r'''
+mixin M {
+  @override
+  String toString() => '';
+}
+class Type2 extends Type with M {
+  String get x => toString();
+}
+''');
+  }
+
+  test_typeThatExtendsType() async {
+    await assertDiagnostics(r'''
+var x = Type2().toString();
+class Type2 extends Type {}
+''', [
+      lint(16, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_explicitThis() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {
+  late var x = this.toString();
+}
+''', [
+      lint(47, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_implicitThis() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {
+  late var x = toString();
+}
+''', [
+      lint(42, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_super() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {
+  late var x = super.toString();
+}
+''', [
+      lint(48, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_tearoff() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {}
+void f(Type2 t) {
+  foo(t.toString);
+}
+void foo(String Function() p) {}
+''', [
+      lint(54, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_tearoff_explicitThis() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {
+  void f() {
+    foo(this.toString);
+  }
+  void foo(String Function() p) {}
+}
+''', [
+      lint(53, 8),
+    ]);
+  }
+
+  test_typeThatExtendsType_tearoff_implicitThis() async {
+    await assertDiagnostics(r'''
+class Type2 extends Type {
+  void f() {
+    foo(toString);
+  }
+  void foo(String Function() p) {}
+}
+''', [
+      lint(48, 8),
+    ]);
+  }
+
+  test_typeThatExtendsTypeThatExtendsType() async {
+    await assertDiagnostics(r'''
+var x = Type3().toString();
+class Type2 extends Type {}
+class Type3 extends Type2 {}
+''', [
+      lint(16, 8),
     ]);
   }
 }
