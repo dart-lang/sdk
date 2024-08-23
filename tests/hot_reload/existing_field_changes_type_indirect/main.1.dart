87f7758b88814eebc24a58d9f9699e07d293a240
MarkZ <markzipan@google.com>
2024-08-23 00:09:08 +0000

    [reload_test] Porting more field modification VM tests hot reload suite
    Change-Id: I79d8c405295779ad09f55af514a74f7884ea52a0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/381940
    Commit-Queue: Mark Zhou <markzipan@google.com>
    Reviewed-by: Nicholas Shahan <nshahan@google.com>

---
+++ tests/hot_reload/existing_field_changes_type_indirect/main.1.dart
@@ -0,0 +1,53 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:expect/expect.dart';
+import 'package:reload_test/reload_test_utils.dart';
+
+// Adapted from:
+// https://github.com/dart-lang/sdk/blob/a70adce28e53ff8bb3445fe96f3f1be951d8a417/runtime/vm/isolate_reload_test.cc#L5757
+
+class A {}
+
+class B {}
+
+A value = init();
+
+init() => A();
+
+helper() {
+  try {
+    return value.toString();
+  } catch (e) {
+    return e.toString();
+  }
+}
+
+Future<void> main() async {
+  Expect.equals("Instance of 'B'", helper());
+  Expect.equals(0, hotReloadGeneration);
+
+  await hotReload();
+
+  Expect.contains(
+      "type 'B' is not a subtype of type 'A' of 'function result'", helper());
+  Expect.equals(1, hotReloadGeneration);
+}
+/** DIFF **/
+/*
+@@ -10,11 +10,11 @@
+ 
+ class A {}
+ 
+-class B extends A {}
++class B {}
+ 
+ A value = init();
+ 
+-init() => B();
++init() => A();
+ 
+ helper() {
+   try {
+*/
