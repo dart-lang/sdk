932526e9dc581b46591e4a4a73e36111b230315e
Aravind <codesculptures@gmail.com>
2024-08-27 16:15:54 +0530

    Removed ARGUMENT_TYPE_NOT_ASSIGNABLE tests as seems here (in ffi_tests) doesn't necessary (suggested by daco charkes)

--- pkg/analyzer/test/src/diagnostics/ffi_address_of_cast_test.dart
+++ pkg/analyzer/test/src/diagnostics/ffi_address_of_cast_test.dart
@@ -20,15 +20,11 @@ class FfiAddressOfCast extends PubPackageResolutionTest {
     await assertErrorsInCode('''
 import 'dart:ffi';
 
-@Native<Void Function(Pointer<Void>)>(isLeaf: true)
-external void myNative(Pointer<Void> buffer);
-
 @Native<Void Function(Pointer<Void>)>()
 external void myNonLeafNative(Pointer<Void> buffer);
 
 main() {
   final myStruct = Struct.create<MyStruct>();
-  myNative(myStruct.arr.address.cast<Int8>());
   myNonLeafNative(myStruct.arr.address.cast());
 }
 
@@ -38,25 +34,18 @@ final class MyStruct extends Struct {
   @Array(2)
   external Array<Int8> arr;
 }
-''', [
-      error(CompileTimeErrorCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, 279, 33),
-      error(FfiCode.ADDRESS_POSITION, 346, 7)
-    ]);
+''', [error(FfiCode.ADDRESS_POSITION, 200, 7)]);
   }
 
   test_struct_error_2() async {
     await assertErrorsInCode('''
 import 'dart:ffi';
 
-@Native<Void Function(Pointer<Void>)>(isLeaf: true)
-external void myNative(Pointer<Void> buffer);
-
 @Native<Void Function(Pointer<Void>)>()
 external void myNonLeafNative(Pointer<Void> buffer);
 
 main() {
   final myStruct = Struct.create<MyStruct>();
-  myNative(myStruct.value.address.cast<Int8>());
   myNonLeafNative(myStruct.value.address.cast());
 }
 
@@ -66,10 +55,7 @@ final class MyStruct extends Struct {
   @Array(2)
   external Array<Int8> arr;
 }
-''', [
-      error(CompileTimeErrorCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, 279, 35),
-      error(FfiCode.ADDRESS_POSITION, 350, 7)
-    ]);
+''', [error(FfiCode.ADDRESS_POSITION, 202, 7)]);
   }
 
   test_struct_no_error() async {
@@ -99,21 +85,14 @@ final class MyStruct extends Struct {
 import 'dart:ffi';
 import 'dart:typed_data';
 
-@Native<Void Function(Pointer<Void>)>(isLeaf: true)
-external void myNative(Pointer<Void> buffer);
-
 @Native<Void Function(Pointer<Void>)>()
 external void myNonLeafNative(Pointer<Void> buffer);
 
 main() {
   final buffer = Int8List(2);
-  myNative(buffer.address.cast<Int8>());
   myNonLeafNative(buffer.address.cast());
 }
-''', [
-      error(CompileTimeErrorCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, 289, 27),
-      error(FfiCode.ADDRESS_POSITION, 344, 7)
-    ]);
+''', [error(FfiCode.ADDRESS_POSITION, 204, 7)]);
   }
 
   test_typed_data_no_error() async {
@@ -135,15 +114,11 @@ main() {
     await assertErrorsInCode('''
 import 'dart:ffi';
 
-@Native<Void Function(Pointer<Void>)>(isLeaf: true)
-external void myNative(Pointer<Void> buffer);
-
 @Native<Void Function(Pointer<Void>)>()
 external void myNonLeafNative(Pointer<Void> buffer);
 
 main() {
   final myUnion = Union.create<MyUnion>();
-  myNative(myUnion.arr.address.cast<Int8>());
   myNonLeafNative(myUnion.arr.address.cast());
 }
 
@@ -153,25 +128,18 @@ final class MyUnion extends Union {
   @Array(2)
   external Array<Int8> arr;
 }
-''', [
-      error(CompileTimeErrorCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, 276, 32),
-      error(FfiCode.ADDRESS_POSITION, 341, 7)
-    ]);
+''', [error(FfiCode.ADDRESS_POSITION, 196, 7)]);
   }
 
   test_union_error_2() async {
     await assertErrorsInCode('''
 import 'dart:ffi';
 
-@Native<Void Function(Pointer<Void>)>(isLeaf: true)
-external void myNative(Pointer<Void> buffer);
-
 @Native<Void Function(Pointer<Void>)>()
 external void myNonLeafNative(Pointer<Void> buffer);
 
 main() {
   final myUnion = Union.create<MyUnion>();
-  myNative(myUnion.value.address.cast<Int8>());
   myNonLeafNative(myUnion.value.address.cast());
 }
 final class MyUnion extends Union {
@@ -180,10 +148,7 @@ final class MyUnion extends Union {
   @Array(2)
   external Array<Int8> arr;
       }
-''', [
-      error(CompileTimeErrorCode.ARGUMENT_TYPE_NOT_ASSIGNABLE, 276, 34),
-      error(FfiCode.ADDRESS_POSITION, 345, 7)
-    ]);
+''', [error(FfiCode.ADDRESS_POSITION, 198, 7)]);
   }
 
   test_union_no_error() async {
