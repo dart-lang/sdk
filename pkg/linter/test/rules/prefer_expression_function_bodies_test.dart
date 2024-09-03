e4631a75236f7b1ca66cccb69b16947d9a97bc30
Sam Rawlins <srawlins@google.com>
2024-09-03 16:06:09 +0000

    linter: Move 3 more linter rule tests
    * literal_only_bool_expressions
    * prefer_expression_function_bodies
    * unnecessary_overrides
    
    Change-Id: Ic5ede5a8b2749109440a4ee6a8d659fed1c51afe
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/383280
    Auto-Submit: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/test/rules/prefer_expression_function_bodies_test.dart
+++ pkg/linter/test/rules/prefer_expression_function_bodies_test.dart
@@ -17,6 +17,90 @@ class PreferExpressionFunctionBodiesTest extends LintRuleTest {
   @override
   String get lintRule => 'prefer_expression_function_bodies';
 
+  test_function_multipleStatements() async {
+    await assertNoDiagnostics(r'''
+int f() {
+  int a = 2 + 3;
+  return a;
+}
+''');
+  }
+
+  test_function_returnStatement() async {
+    await assertDiagnostics(r'''
+int f() {
+  return 1;
+}
+''', [
+      lint(8, 15),
+    ]);
+  }
+
+  test_getter_expressionFunctionBody() async {
+    await assertNoDiagnostics(r'''
+class A {
+  int get f => 0;
+}
+''');
+  }
+
+  test_getter_returnStatement() async {
+    await assertDiagnostics(r'''
+class A {
+  get f {
+    return 7 - 6;
+  }
+}
+''', [
+      lint(18, 23),
+    ]);
+  }
+
+  test_method_multipleStatements() async {
+    await assertNoDiagnostics(r'''
+class A {
+  int m() {
+    if (1 == 2) {
+      return 1;
+    }
+    return 0;
+  }
+}
+''');
+  }
+
+  test_method_returnStatement() async {
+    await assertDiagnostics(r'''
+class A {
+  int m() {
+    return 1;
+  }
+}
+''', [
+      lint(20, 19),
+    ]);
+  }
+
+  test_setter_expressionFunctionBody() async {
+    await assertNoDiagnostics(r'''
+class A {
+  int x = 0;
+  set f(int p) => x = p;
+}
+''');
+  }
+
+  test_setter_noReturn() async {
+    await assertNoDiagnostics(r'''
+class A {
+  int x = 0;
+  set f(int p) {
+    x = p;
+  }
+}
+''');
+  }
+
   /// https://github.com/dart-lang/linter/issues/4221
   test_voidReturn() async {
     await assertNoDiagnostics(r'''
