a4e9775a994003f79566782a2646d67ccaceb96c
Jason Simmons <jsimmons@google.com>
2024-09-09 20:10:06 +0000

    Revert "[dart2wasm] Allocate boxed bools once" and "[dart2wasm] Box `Pointer` values obtained from FFI calls"
    This reverts commit d7a283de2620720120b58d4a71ba636089c8d2cf and commit d7a39788d4fc69f64a23593242a542cbaa4bc08d
    
    The "[dart2wasm] Box `Pointer` values obtained from FFI calls" change causes a Flutter Web test to hang (https://github.com/flutter/engine/blob/main/lib/web_ui/test/fallbacks/fallbacks_test.dart)
    
    Change-Id: Ia44bcf68cd6edc72697a0c2b05e05b4f8e1408fd
    TEST=reproduced the hang locally and confirmed that it does not happen with this revert
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/384321
    Reviewed-by: Siva Annamalai <asiva@google.com>
    Reviewed-by: Ömer Ağacan <omersa@google.com>
    Commit-Queue: Jason Simmons <jsimmons@google.com>

--- pkg/dart2wasm/lib/kernel_nodes.dart
+++ pkg/dart2wasm/lib/kernel_nodes.dart
@@ -149,6 +149,7 @@ mixin KernelNodes {
       index.getTopLevelProcedure("dart:async", "_newAsyncSuspendState");
 
   // dart:ffi classes
+  late final Class ffiCompoundClass = index.getClass("dart:ffi", "_Compound");
   late final Class ffiPointerClass = index.getClass("dart:ffi", "Pointer");
 
   // dart:_wasm classes
