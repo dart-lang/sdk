b2eb394c3ec2292c7a03fd7da15193f7f23f1900
FMorschel <52160996+FMorschel@users.noreply.github.com>
2024-09-20 13:13:24 -0300

    new assist to add/edit `hide` at import for ambiguous import

--- pkg/analysis_server/lib/src/services/correction/fix.dart
+++ pkg/analysis_server/lib/src/services/correction/fix.dart
@@ -849,6 +849,11 @@ abstract final class DartFixKind {
     DartFixKindPriority.standard + 5,
     "Update library '{0}' import",
   );
+  static const IMPORT_LIBRARY_HIDE = FixKind(
+    'dart.fix.import.libraryHide',
+    DartFixKindPriority.standard,
+    "Add 'hide {0}' to library '{1}'{2} import",
+  );
   static const INLINE_INVOCATION = FixKind(
     'dart.fix.inlineInvocation',
     DartFixKindPriority.standard - 20,
