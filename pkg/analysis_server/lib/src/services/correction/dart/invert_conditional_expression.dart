3781ec01af6d00212cbcc9325ee78d71f0a8c56d
FMorschel <52160996+FMorschel@users.noreply.github.com>
2024-09-23 14:14:05 -0300

    moving conditional exp finding node to new method

--- pkg/analysis_server/lib/src/services/correction/dart/invert_conditional_expression.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/invert_conditional_expression.dart
@@ -5,7 +5,6 @@
 import 'package:analysis_server/src/services/correction/assist.dart';
 import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer_plugin/utilities/assist/assist.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:analyzer_plugin/utilities/range_factory.dart';
@@ -23,34 +22,7 @@ class InvertConditionalExpression extends ResolvedCorrectionProducer {
 
   @override
   Future<void> compute(ChangeBuilder builder) async {
-    var node = this.node;
-    MethodInvocation? methodInvocation = thisOrParentOfType(node);
-
-    node = methodInvocation ?? node;
-    AwaitExpression? awaitExp1 = thisOrParentOfType(node);
-
-    node = awaitExp1 ?? node;
-    Expression? booleanExpression;
-    if (node case IsExpression() || BinaryExpression() || BooleanLiteral()) {
-      booleanExpression = node as Expression;
-    } else if (node.parent
-        case IsExpression() || BinaryExpression() || BooleanLiteral()) {
-      booleanExpression = node.parent as Expression;
-    } else if (thisOrParentOfType<PrefixExpression>(node)
-        case PrefixExpression exp) {
-      if (exp.operator.type == TokenType.BANG) {
-        booleanExpression = exp;
-      }
-    }
-
-    node = booleanExpression ?? node;
-    ParenthesizedExpression? parenthesizedExpression = thisOrParentOfType(node);
-
-    node = parenthesizedExpression ?? node;
-    AwaitExpression? awaitExp2 = thisOrParentOfType(node);
-
-    node = awaitExp2 ?? node;
-    ConditionalExpression? conditionalExpression = thisOrParentOfType(node);
+    var conditionalExpression = _getConditionalExpressionAncestor();
     if (conditionalExpression == null) {
       return;
     }
@@ -70,7 +42,56 @@ class InvertConditionalExpression extends ResolvedCorrectionProducer {
     });
   }
 
-  T? thisOrParentOfType<T extends AstNode>(AstNode node) {
+  /// Finds the ConditionalExpression that is the ancestor of the node if it is
+  /// close enough to the ConditionalExpression.
+  ///
+  /// This is not exhaustive, but should cover the most common cases.
+  /// The intention is to make this assist callable from more places than the
+  /// expression itself, while also keeping the assist close enough to the
+  /// expression to be intuitive.
+  ///
+  /// E.g.:
+  ///   - Calling the assist on any part of
+  ///                                 `(await futureBool()) ? value() : (1 + 2)`
+  ///
+  /// This should prevent cases like calling the assist on `method` in
+  /// `condition ? outer(method(args)) : null` since that is not a "close
+  /// enough" child of the ConditionalExpression. This could be revisited if
+  /// there is a need for it.
+  ///
+  /// It is also deliberately not allowing the assist to be called on inner
+  /// lambdas since that would be unintuitive and not very useful.
+  ConditionalExpression? _getConditionalExpressionAncestor() {
+    var node = this.node;
+    MethodInvocation? methodInvocation = _thisOrParentOfType(node);
+
+    node = methodInvocation ?? node;
+    AwaitExpression? awaitExp1 = _thisOrParentOfType(node);
+
+    node = awaitExp1 ?? node;
+    Expression? booleanExpression;
+    if (node case IsExpression() || BinaryExpression() || BooleanLiteral()) {
+      booleanExpression = node as Expression;
+    } else if (node.parent
+        case IsExpression() || BinaryExpression() || BooleanLiteral()) {
+      booleanExpression = node.parent as Expression;
+    } else if (_thisOrParentOfType<PrefixExpression>(node)
+        case Expression exp) {
+      booleanExpression = exp;
+    }
+
+    node = booleanExpression ?? node;
+    ParenthesizedExpression? parenthesizedExpression =
+        _thisOrParentOfType(node);
+
+    node = parenthesizedExpression ?? node;
+    AwaitExpression? awaitExp2 = _thisOrParentOfType(node);
+
+    node = awaitExp2 ?? node;
+    return _thisOrParentOfType(node);
+  }
+
+  T? _thisOrParentOfType<T extends AstNode>(AstNode node) {
     T? result;
     if (node is T) {
       result = node;
