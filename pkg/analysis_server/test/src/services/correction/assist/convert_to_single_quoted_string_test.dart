dbf87d8644f859833d4a98018106dcc3f2a035c8
FMorschel <52160996+FMorschel@users.noreply.github.com>
2024-09-25 21:11:17 +0000

    Improving string conversion
    Closes https://github.com/dart-lang/sdk/pull/56723
    
    GitOrigin-RevId: 802fda7efeace66f33a82fa939a3efac11064390
    Change-Id: I6d4d3931e62b89265eb1d324856bed5b9febfe8e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/384962
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/test/src/services/correction/assist/convert_to_single_quoted_string_test.dart
+++ pkg/analysis_server/test/src/services/correction/assist/convert_to_single_quoted_string_test.dart
@@ -20,13 +20,30 @@ class ConvertToSingleQuotedStringTest extends AssistProcessorTest {
   @override
   AssistKind get kind => DartAssistKind.CONVERT_TO_SINGLE_QUOTED_STRING;
 
+  Future<void> test_one_backslash() async {
+    await resolveTestCode(r'''
+void f() {
+  print("a\"b\"c");
+}
+''');
+    await assertHasAssistAt('"a', r"""
+void f() {
+  print('a"b"c');
+}
+""");
+  }
+
   Future<void> test_one_embeddedTarget() async {
     await resolveTestCode('''
 void f() {
   print("a'b'c");
 }
 ''');
-    await assertNoAssistAt('"a');
+    await assertHasAssistAt('"a', r'''
+void f() {
+  print('a\'b\'c');
+}
+''');
   }
 
   Future<void> test_one_enclosingTarget() async {
@@ -118,7 +135,11 @@ void f() {
   print("""a''\'bc""");
 }
 ''');
-    await assertNoAssistAt('"a');
+    await assertHasAssistAt('"a', r"""
+void f() {
+  print('''a\'\'\'bc''');
+}
+""");
   }
 
   Future<void> test_three_enclosingTarget() async {
