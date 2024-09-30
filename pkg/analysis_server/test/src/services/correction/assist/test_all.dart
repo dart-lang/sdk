65743b1e3b578f1516fb30a5f33876b68b8a5dc7
FMorschel <52160996+FMorschel@users.noreply.github.com>
2024-09-30 13:31:31 -0300

    fixing failing format test

--- pkg/analysis_server/test/src/services/correction/assist/test_all.dart
+++ pkg/analysis_server/test/src/services/correction/assist/test_all.dart
@@ -73,7 +73,8 @@ import 'flutter_wrap_sized_box_test.dart' as flutter_wrap_sized_box;
 import 'flutter_wrap_stream_builder_test.dart' as flutter_wrap_stream_builder;
 import 'import_add_show_test.dart' as import_add_show;
 import 'inline_invocation_test.dart' as inline_invocation;
-import 'invert_conditional_expression_test.dart' as invert_conditional_expression;
+import 'invert_conditional_expression_test.dart'
+    as invert_conditional_expression;
 import 'invert_if_statement_test.dart' as invert_if_statement;
 import 'join_if_with_inner_test.dart' as join_if_with_inner;
 import 'join_if_with_outer_test.dart' as join_if_with_outer;
