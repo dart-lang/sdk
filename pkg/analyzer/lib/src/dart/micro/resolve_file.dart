bbb72e6eedefa097cac64b160e4929f4c130aefe
Konstantin Shcheglov <scheglov@google.com>
2024-10-02 14:21:49 +0000

    Cider. Clear parsed unit cache after analysis operations.
    Change-Id: I047112d8e9de19c45c10cdd82df8c9357ceedba4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/387860
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer/lib/src/dart/micro/resolve_file.dart
+++ pkg/analyzer/lib/src/dart/micro/resolve_file.dart
@@ -294,6 +294,7 @@ class FileResolver {
         for (var filePath in result) {
           await collectReferences2(filePath, performance!);
         }
+        _clearFileSystemStateParsedCache();
       }
       return references;
     });
@@ -419,6 +420,7 @@ class FileResolver {
         targetLibrary: kind,
         performance: performance,
       );
+      _clearFileSystemStateParsedCache();
     });
 
     return libraryContext!.elementFactory.libraryOfUri2(uri);
@@ -532,6 +534,7 @@ class FileResolver {
         path: libraryFile.path,
         performance: performance,
       );
+      _clearFileSystemStateParsedCache();
       var unit = libraryResult.units.firstWhereOrNull(
         (unitResult) => unitResult.path == path,
       );
@@ -593,6 +596,7 @@ class FileResolver {
           libraryKind,
           typeSystemOperations: typeSystemOperations,
         );
+        _clearFileSystemStateParsedCache();
 
         var analysisResult = performance!.run('analyze', (performance) {
           return libraryAnalyzer.analyzeForCompletion(
@@ -696,6 +700,10 @@ class FileResolver {
     });
   }
 
+  void _clearFileSystemStateParsedCache() {
+    fsState?.parsedFileStateCache.clear();
+  }
+
   /// Make sure that [fsState], [contextObjects], and [libraryContext] are
   /// created and configured with the given [fileAnalysisOptions].
   ///
@@ -709,6 +717,10 @@ class FileResolver {
   /// system. And there are lints that are enabled for one package, but not
   /// for another.
   void _createContext(String path, AnalysisOptionsImpl fileAnalysisOptions) {
+    // Clear it here too, so that even if we miss the invocation somewhere,
+    // we still eventually do it, and so limit the number of cached items.
+    _clearFileSystemStateParsedCache();
+
     if (contextObjects != null) {
       libraryContext!.analysisContext.analysisOptions = fileAnalysisOptions;
       return;
