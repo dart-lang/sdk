867391edc238d058c442542f057c88d8ea1ecaf4
pq <pquitslund@google.com>
2024-10-10 17:30:20 +0000

    [element model] migrate `prefer_const_constructors`
    Bug: https://github.com/dart-lang/linter/issues/5099
    Change-Id: I1298112a1b565724b651fe107e4f573fe2f795e4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/389404
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Phil Quitslund <pquitslund@google.com>

--- pkg/linter/lib/src/rules/prefer_const_constructors.dart
+++ pkg/linter/lib/src/rules/prefer_const_constructors.dart
@@ -4,7 +4,7 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/src/lint/linter.dart'; // ignore: implementation_imports
 
@@ -41,24 +41,25 @@ class _Visitor extends SimpleAstVisitor<void> {
     if (node.isConst) return;
     if (node.constructorName.type.isDeferred) return;
 
-    var element = node.constructorName.staticElement;
+    var element = node.constructorName.element;
     if (element == null) return;
     if (!element.isConst) return;
 
     // Handled by an analyzer warning.
     if (element.hasLiteral) return;
 
-    var enclosingElement = element.enclosingElement3;
-    if (enclosingElement is ClassElement && enclosingElement.isDartCoreObject) {
+    var enclosingElement = element.enclosingElement2;
+    if (enclosingElement is ClassElement2 &&
+        enclosingElement.isDartCoreObject) {
       // Skip lint for `new Object()`, because it can be used for ID creation.
       return;
     }
 
-    if (enclosingElement.typeParameters.isNotEmpty &&
+    if (enclosingElement.typeParameters2.isNotEmpty &&
         node.constructorName.type.typeArguments == null) {
       var approximateContextType = node.approximateContextType;
       var contextTypeAsInstanceOfEnclosing =
-          approximateContextType?.asInstanceOf(enclosingElement);
+          approximateContextType?.asInstanceOf2(enclosingElement);
       if (contextTypeAsInstanceOfEnclosing != null) {
         if (contextTypeAsInstanceOfEnclosing.typeArguments
             .any((e) => e is TypeParameterType)) {
