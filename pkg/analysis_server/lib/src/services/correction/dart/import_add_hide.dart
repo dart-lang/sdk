56ae18f62551e46be9c59a910586f74bf9eeb0f9
FMorschel <52160996+FMorschel@users.noreply.github.com>
2024-10-10 15:46:27 -0300

    refactor to simplify user choice and automation
    - Removed verbose process for user.
    - Refactored to allow user to specify desired elements explicitly.
    - The fix now automatically hide everything else

--- pkg/analysis_server/lib/src/services/correction/dart/import_add_hide.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/import_add_hide.dart
@@ -9,6 +9,7 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/source/source_range.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
+import 'package:collection/collection.dart';
 
 class ImportAddHide extends MultiCorrectionProducer {
   ImportAddHide({required super.context});
@@ -26,37 +27,66 @@ class ImportAddHide extends MultiCorrectionProducer {
       return const [];
     }
     var elements = element.conflictingElements;
-    var pairs = <ImportDirective, Element>{};
+    var records = <({Element element, String uri, String? prefix}),
+        List<ImportDirective>>{};
     for (var element in elements) {
       var library = element.enclosingElement3?.library;
+      if (library == null) {
+        continue;
+      }
+
+      var directives = <ImportDirective>[];
       // find all ImportDirective that import this library in this unit
       for (var directive in unit.directives.whereType<ImportDirective>()) {
+        // Get import directive that
         var imported = directive.element?.importedLibrary;
         if (imported == null) {
           continue;
         }
         if (imported == library) {
-          pairs[directive] = element;
+          directives.add(directive);
         }
         // If the directive exports the library, then the library is also
         // imported.
         if (imported.exportedLibraries.contains(library)) {
-          pairs[directive] = element;
+          directives.add(directive);
+        }
+      }
+      for (var directive in directives) {
+        var uri = directive.uri.stringValue;
+        var prefix = directive.prefix?.name;
+        if (uri != null) {
+          records[(element: element, uri: uri, prefix: prefix)] = directives;
         }
       }
     }
-    return [
-      for (var MapEntry(key: import, value: element) in pairs.entries)
-        _ImportAddHide(import, element, context: context),
-    ];
+    if (records.entries.isEmpty) {
+      return const [];
+    }
+
+    var producers = <ResolvedCorrectionProducer>[];
+    for (var MapEntry(key: key) in records.entries) {
+      var directives = records.entries
+          .whereNot((e) => e.key == key)
+          .expand((e) => e.value)
+          .whereNot((d) =>
+              (d.prefix?.name == key.prefix) && (d.uri.stringValue == key.uri))
+          .toSet();
+      producers.add(_ImportAddHide(key.element, key.uri, key.prefix, directives,
+          context: context));
+    }
+    return producers;
   }
 }
 
 class _ImportAddHide extends ResolvedCorrectionProducer {
-  final ImportDirective importDirective;
+  final Set<ImportDirective> importDirectives;
   final Element element;
+  final String uri;
+  final String? prefix;
 
-  _ImportAddHide(this.importDirective, this.element, {required super.context});
+  _ImportAddHide(this.element, this.uri, this.prefix, this.importDirectives,
+      {required super.context});
 
   @override
   CorrectionApplicability get applicability =>
@@ -65,48 +95,76 @@ class _ImportAddHide extends ResolvedCorrectionProducer {
 
   @override
   List<String> get fixArguments {
-    var aliasStr = importDirective.prefix?.name;
-    var alias = '';
-    if (aliasStr != null) {
-      alias = " as '$aliasStr'";
+    // Any import directive that imports the element is directly showing it?
+    var removeShow = false;
+    for (var directives in importDirectives
+        .map((d) => d.combinators.whereType<ShowCombinator>())
+        .where((d) => d.isNotEmpty)) {
+      for (var name in directives.first.shownNames) {
+        if (name.name == _elementName) {
+          removeShow = true;
+          break;
+        }
+      }
     }
-    return [_elementName, _importStr, alias];
+    // If there is a show combinator explicitly showing the element, then
+    // we should remove it.
+    var removeShowStr = removeShow ? ' (removing \'show\')' : '';
+    var prefix = '';
+    if ((this.prefix ?? '') != '') {
+      prefix = ' as ${this.prefix}';
+    }
+    return [_elementName, uri, prefix, removeShowStr];
   }
 
   @override
   FixKind get fixKind => DartFixKind.IMPORT_LIBRARY_HIDE;
 
   String get _elementName => element.name ?? '';
-  String get _importStr => importDirective.uri.stringValue ?? '';
 
   @override
   Future<void> compute(ChangeBuilder builder) async {
-    if (_elementName.isEmpty || _importStr.isEmpty) {
+    if (_elementName.isEmpty || uri.isEmpty) {
       return;
     }
 
-    if (importDirective.combinators.whereType<ShowCombinator>().isNotEmpty) {
-      return;
-    }
-
-    var hide = importDirective.combinators.whereType<HideCombinator>();
-    if (hide.isNotEmpty) {
-      return await builder.addDartFileEdit(file, (builder) {
-        var hideCombinator = hide.first;
-        var allNames = <String>[_elementName];
-        for (var name in hideCombinator.hiddenNames) {
-          allNames.add(name.name);
+    for (var directive in importDirectives) {
+      var show = directive.combinators.whereType<ShowCombinator>();
+      var hide = directive.combinators.whereType<HideCombinator>();
+      await builder.addDartFileEdit(file, (builder) {
+        if (show.isNotEmpty) {
+          var showCombinator = show.first;
+          var allNames = <String>[];
+          for (var show in showCombinator.shownNames) {
+            if (show.name == _elementName) continue;
+            allNames.add(show.name);
+          }
+          allNames.sort();
+          if (allNames.isEmpty) {
+            builder.addDeletion(SourceRange(
+                showCombinator.offset - 1, showCombinator.length + 1));
+          } else {
+            var combinator = 'show ${allNames.join(', ')}';
+            var range =
+                SourceRange(showCombinator.offset, showCombinator.length);
+            builder.addSimpleReplacement(range, combinator);
+          }
         }
-        allNames.sort();
-        var combinator = 'hide ${allNames.join(', ')}';
-        var range = SourceRange(hideCombinator.offset, hideCombinator.length);
-        builder.addSimpleReplacement(range, combinator);
+        if (hide.isNotEmpty) {
+          var hideCombinator = hide.first;
+          var allNames = <String>[_elementName];
+          for (var name in hideCombinator.hiddenNames) {
+            allNames.add(name.name);
+          }
+          allNames.sort();
+          var combinator = 'hide ${allNames.join(', ')}';
+          var range = SourceRange(hideCombinator.offset, hideCombinator.length);
+          builder.addSimpleReplacement(range, combinator);
+        } else {
+          var hideCombinator = ' hide $_elementName';
+          builder.addSimpleInsertion(directive.end - 1, hideCombinator);
+        }
       });
     }
-
-    await builder.addDartFileEdit(file, (builder) {
-      var hideCombinator = ' hide $_elementName';
-      builder.addSimpleInsertion(importDirective.end - 1, hideCombinator);
-    });
   }
 }
