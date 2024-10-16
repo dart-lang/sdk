1e624b81222e9bf11018d6be7988feea1684db08
Siva Annamalai <asiva@google.com>
2024-10-16 20:05:10 +0000

    Reland "[SDK] Reland : Switch dart2js to an AOT snapshot."
    This reverts commit ceba7a5e65df4ac733d98163cfd18dc2f07a688f.
    
    Reason for revert: Flutter tools has been fixed to use 'compile js' option instead of directly naming the snapshot.
    
    Original change's description:
    > Revert "[SDK] Reland : Switch dart2js to an AOT snapshot."
    >
    > This reverts commit 9fec00aeedea94ac96d6b97f9ae36910591c826f.
    >
    > Reason for revert: flutter tools needs an update to use the 'compile js' command instead of reaching into the dart-sdk and using snapshot names (see https://github.com/flutter/flutter/issues/156654)
    >
    > Original change's description:
    > > [SDK] Reland : Switch dart2js to an AOT snapshot.
    > >
    > > TESTS=ci
    > >
    > > Change-Id: I8c9f9d01cb462d1027c7a6a6521f40946fc7638b
    > > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/388071
    > > Reviewed-by: Ben Konyi <bkonyi@google.com>
    > > Commit-Queue: Siva Annamalai <asiva@google.com>
    >
    > Change-Id: I72bd9ad731b2b60d154c1e6477cce913d72acec4
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/390020
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Alexander Aprelev <aam@google.com>
    > Commit-Queue: Siva Annamalai <asiva@google.com>
    
    Change-Id: I15a9dc3092e436cdcb0fa84c8e7ddd1dac195074
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/390620
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ben Konyi <bkonyi@google.com>
    Commit-Queue: Siva Annamalai <asiva@google.com>

--- pkg/dartdev/lib/src/commands/compile.dart
+++ pkg/dartdev/lib/src/commands/compile.dart
@@ -4,7 +4,6 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'dart:isolate';
 
 import 'package:args/args.dart';
 import 'package:dart2native/generate.dart';
@@ -88,48 +87,41 @@ class CompileJSCommand extends CompileSubcommandCommand {
 
   @override
   FutureOr<int> run() async {
-    if (!Sdk.checkArtifactExists(sdk.dart2jsSnapshot) ||
-        !Sdk.checkArtifactExists(sdk.librariesJson)) {
-      return 255;
+    if (!Sdk.checkArtifactExists(sdk.librariesJson)) {
+      return genericErrorExitCode;
     }
-
     final args = argResults!;
-
-    // Build arguments.
-    final buildArgs = <String>[
+    var snapshot = sdk.dart2jsAotSnapshot;
+    var runtime = sdk.dartAotRuntime;
+    if (!Sdk.checkArtifactExists(snapshot, logError: false)) {
+      // AOT snapshots cannot be generated on IA32, so we need this fallback
+      // branch until support for IA32 is dropped (https://dartbug.com/49969).
+      snapshot = sdk.dart2jsSnapshot;
+      runtime = sdk.dart;
+      if (!Sdk.checkArtifactExists(snapshot)) {
+        return genericErrorExitCode;
+      }
+    }
+    final dart2jsCommand = [
+      runtime,
+      snapshot,
       '--libraries-spec=${sdk.librariesJson}',
       '--cfe-invocation-modes=compile',
       '--invoker=dart_cli',
       // Add the remaining arguments.
       if (args.rest.isNotEmpty) ...args.rest.sublist(0),
     ];
-
-    var retval = 0;
-    final result = Completer<int>();
-    final exitPort = ReceivePort()
-      ..listen((msg) {
-        result.complete(0);
-      });
-    final errorPort = ReceivePort()
-      ..listen((error) {
-        log.stderr(error.toString());
-        result.complete(255);
-      });
     try {
-      await Isolate.spawnUri(Uri.file(sdk.dart2jsSnapshot), buildArgs, null,
-          onExit: exitPort.sendPort, onError: errorPort.sendPort);
-      retval = await result.future;
+      final exitCode = await runProcessInheritStdio(dart2jsCommand);
+      return exitCode;
     } catch (e, st) {
       log.stderr('Error: JS compilation failed');
       log.stderr(e.toString());
       if (verbose) {
         log.stderr(st.toString());
       }
-      retval = compileErrorExitCode;
+      return compileErrorExitCode;
     }
-    errorPort.close();
-    exitPort.close();
-    return retval;
   }
 }
 
