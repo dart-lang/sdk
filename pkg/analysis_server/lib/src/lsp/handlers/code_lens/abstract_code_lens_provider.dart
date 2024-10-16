286bb78d3480c0b371c53e95d3b7dc02251d7db4
Danny Tuppeny <danny@tuppeny.com>
2024-10-16 17:21:03 +0000

    [analysis_server] Migrate LSP CodeLens handlers to new element model
    + update migrate tool to produce correct output on Windows
    
    Change-Id: I5c75a6310a1f4fdaf4b49062d8a46b663e4fe810
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/389961
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/lib/src/lsp/handlers/code_lens/abstract_code_lens_provider.dart
+++ pkg/analysis_server/lib/src/lsp/handlers/code_lens/abstract_code_lens_provider.dart
@@ -10,8 +10,7 @@ import 'package:analysis_server/src/lsp/constants.dart';
 import 'package:analysis_server/src/lsp/error_or.dart';
 import 'package:analysis_server/src/lsp/handlers/handlers.dart';
 import 'package:analysis_server/src/lsp/mapping.dart';
-import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/source/line_info.dart';
+import 'package:analyzer/dart/element/element2.dart';
 
 /// A base class for providers that can contribute CodeLenses.
 ///
@@ -29,47 +28,44 @@ abstract class AbstractCodeLensProvider
   bool clientSupportsGoToLocationCommand(LspClientCapabilities capabilities) =>
       capabilities.supportedCommands.contains(ClientCommands.goToLocation);
 
-  /// Attempt to compute a [Location] to the declaration of [element].
+  /// Attempt to compute a [Location] to [fragment].
   ///
   /// If for any reason the location cannot be computed, returns `null`.
-  Location? getLocation(Element element, Map<String, LineInfo?> lineInfoCache) {
-    var source = element.source;
-    if (source == null) {
+  Location? getLocation(Fragment fragment) {
+    // We can't produce a location to a name if there isn't one.
+    var nameOffset = fragment.nameOffset;
+    var nameLength = fragment.element.displayName.length;
+    if (nameOffset == null || nameOffset == -1) {
       return null;
     }
 
     // Map the source onto a URI and only return this item if the client
     // can handle the URI.
+    var source = fragment.libraryFragment.source;
     var uri = server.uriConverter.toClientUri(source.fullName);
     if (!server.uriConverter.supportedSchemes.contains(uri.scheme)) {
       return null;
     }
 
-    var lineInfo = lineInfoCache.putIfAbsent(
-        source.fullName, () => server.getLineInfo(source.fullName));
-    if (lineInfo == null) {
-      return null;
-    }
-
+    var lineInfo = fragment.libraryFragment.lineInfo;
     return Location(
       uri: uri,
-      range: toRange(lineInfo, element.nameOffset, element.nameLength),
+      range: toRange(lineInfo, nameOffset, nameLength),
     );
   }
 
   /// Builds a [Command] that with the text [title] that navigate to the
-  /// declaration of [element].
+  /// [fragment].
   ///
   /// If for any reason the location cannot be computed, returns `null`.
   Command? getNavigationCommand(
     LspClientCapabilities clientCapabilities,
     String title,
-    Element element,
-    Map<String, LineInfo?> lineInfoCache,
+    Fragment fragment,
   ) {
     assert(clientSupportsGoToLocationCommand(clientCapabilities));
 
-    var location = getLocation(element, lineInfoCache);
+    var location = getLocation(fragment);
     if (location == null) {
       return null;
     }
@@ -85,7 +81,6 @@ abstract class AbstractCodeLensProvider
     CodeLensParams params,
     MessageInfo message,
     CancellationToken token,
-    Map<String, LineInfo?> lineInfoCache,
   );
 
   bool isAvailable(
--- pkg/analysis_server/lib/src/lsp/handlers/code_lens/augmentations.dart
+++ pkg/analysis_server/lib/src/lsp/handlers/code_lens/augmentations.dart
@@ -11,11 +11,8 @@ import 'package:analysis_server/src/lsp/handlers/code_lens/abstract_code_lens_pr
 import 'package:analysis_server/src/lsp/handlers/handlers.dart';
 import 'package:analysis_server/src/lsp/mapping.dart';
 import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/visitor.dart';
-import 'package:analyzer/source/line_info.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/src/util/performance/operation_performance.dart';
-import 'package:analyzer/src/utilities/extensions/element.dart';
 
 class AugmentationCodeLensProvider extends AbstractCodeLensProvider {
   AugmentationCodeLensProvider(super.server);
@@ -28,7 +25,6 @@ class AugmentationCodeLensProvider extends AbstractCodeLensProvider {
     CodeLensParams params,
     MessageInfo message,
     CancellationToken token,
-    Map<String, LineInfo?> lineInfoCache,
   ) async {
     var clientCapabilities = message.clientCapabilities;
     if (clientCapabilities == null) {
@@ -44,8 +40,8 @@ class AugmentationCodeLensProvider extends AbstractCodeLensProvider {
     return await unit.mapResult((result) {
       return performance.runAsync(
         '_getCodeLenses',
-        (performance) => _getCodeLenses(
-            clientCapabilities, result, token, performance, lineInfoCache),
+        (performance) =>
+            _getCodeLenses(clientCapabilities, result, token, performance),
       );
     });
   }
@@ -64,94 +60,50 @@ class AugmentationCodeLensProvider extends AbstractCodeLensProvider {
     ResolvedUnitResult result,
     CancellationToken token,
     OperationPerformanceImpl performance,
-    Map<String, LineInfo?> lineInfoCache,
   ) async {
-    var lineInfo = result.lineInfo;
     var codeLenses = <CodeLens>[];
 
-    /// Helper to add a CodeLens at [declaration] to [target] for [title].
-    void addCodeLens(String title, Element declaration, Element target) {
-      var command = getNavigationCommand(
-          clientCapabilities, title, target, lineInfoCache);
-      if (command != null && declaration.nameOffset != -1) {
+    /// Helper to add a CodeLens at [thisFragment] to [targetFragment] with
+    /// the text [title].
+    void addCodeLens(
+      String title,
+      Fragment thisFragment,
+      Fragment targetFragment,
+    ) {
+      var command =
+          getNavigationCommand(clientCapabilities, title, targetFragment);
+      var nameOffset = thisFragment.nameOffset;
+      var nameLength = thisFragment.element.displayName.length;
+      if (command != null && nameOffset != null && nameOffset != -1) {
         var range = toRange(
-          lineInfo,
-          declaration.nameOffset,
-          declaration.nameLength,
+          result.lineInfo,
+          nameOffset,
+          nameLength,
         );
         codeLenses.add(CodeLens(range: range, command: command));
       }
     }
 
-    var computer = _AugmentationComputer(result);
-    if (codeLens.augmented) {
-      for (var MapEntry(key: declaration, value: augmentated)
-          in computer.augmenteds.entries) {
-        addCodeLens('Go to Augmented', declaration, augmentated);
+    // Helper to add all CodeLenses for a [fragment] and child fragments
+    // recursively.
+    void addCodeLenses(Fragment fragment) {
+      var previousFragment = fragment.previousFragment;
+      var nextFragment = fragment.nextFragment;
+      if (codeLens.augmented && previousFragment != null) {
+        addCodeLens('Go to Augmented', fragment, previousFragment);
+      }
+      if (codeLens.augmentation && nextFragment != null) {
+        addCodeLens('Go to Augmentation', fragment, nextFragment);
+      }
+
+      for (var fragment in fragment.children3) {
+        addCodeLenses(fragment);
       }
     }
-    if (codeLens.augmentation) {
-      for (var MapEntry(key: declaration, value: augmentation)
-          in computer.augmentations.entries) {
-        addCodeLens('Go to Augmentation', declaration, augmentation);
-      }
-    }
+
+    // Add fragments starting at the library.
+    addCodeLenses(result.libraryFragment);
 
     return success(codeLenses);
-  }
-}
-
-class _AugmentationComputer {
-  /// A mapping of declarations to their augmentations.
-  final Map<Element, Element> augmentations = {};
-
-  /// A mapping of augmentations to their declarations.
-  final Map<Element, Element> augmenteds = {};
-
-  final ResolvedUnitResult result;
-
-  _AugmentationComputer(this.result) {
-    result.unit.declaredElement?.accept(_AugmentationVisitor(this));
-  }
-
-  void recordAugmentation(Element declaration, Element augmentation) {
-    augmentations[declaration] = augmentation;
-  }
-
-  void recordAugmented(Element declaration, Element augmented) {
-    augmenteds[declaration] = augmented;
-  }
-}
-
-/// Visits an AST and records mappings from augmentations to the declarations
-/// they augment, and from declarations to their augmentations.
-class _AugmentationVisitor extends GeneralizingElementVisitor<void> {
-  final _AugmentationComputer _computer;
-
-  _AugmentationVisitor(this._computer);
-
-  @override
-  void visitElement(Element element) {
-    assert(element.source?.fullName == _computer.result.path);
-    assert(element == element.declaration);
-
-    if (!element.isSynthetic) {
-      var agumentation = element.augmentation;
-      if (agumentation != null) {
-        _computer.recordAugmentation(element, agumentation);
-      }
-
-      var augmented = switch (element) {
-        ExecutableElement element => element.augmentationTarget,
-        InstanceElement element => element.augmentationTarget,
-        PropertyInducingElement element => element.augmentationTarget,
-        _ => null,
-      };
-      if (augmented != null) {
-        _computer.recordAugmented(element, augmented);
-      }
-    }
-
-    super.visitElement(element);
   }
 }
