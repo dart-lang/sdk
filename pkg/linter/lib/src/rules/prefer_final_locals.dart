c92cb890a874cec9c5e72223fcc120ea6d440da0
pq <pquitslund@google.com>
2024-10-17 18:30:48 +0000

    [element model] migrate `prefer_final_locals`
    Bug: https://github.com/dart-lang/linter/issues/5099
    Change-Id: I0b56b0da3c2ecfe3391d06430d634808c99ab0e8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/390780
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Auto-Submit: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/linter/lib/src/rules/prefer_final_locals.dart
+++ pkg/linter/lib/src/rules/prefer_final_locals.dart
@@ -4,7 +4,7 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/src/dart/element/extensions.dart'; // ignore: implementation_imports
 
 import '../analyzer.dart';
@@ -37,11 +37,11 @@ class PreferFinalLocals extends LintRule {
 }
 
 class _DeclaredVariableVisitor extends RecursiveAstVisitor<void> {
-  final List<BindPatternVariableElement> declaredElements = [];
+  final List<BindPatternVariableElement2> declaredElements = [];
 
   @override
   void visitDeclaredVariablePattern(DeclaredVariablePattern node) {
-    var element = node.declaredElement;
+    var element = node.declaredElement2;
     if (element != null) {
       declaredElements.add(element);
     }
@@ -129,10 +129,10 @@ class _Visitor extends SimpleAstVisitor<void> {
       if (variable.equals == null || variable.initializer == null) {
         return;
       }
-      var declaredElement = variable.declaredElement;
+      var declaredElement = variable.declaredElement2;
       if (declaredElement != null &&
           (declaredElement.isWildcardVariable ||
-              function.isPotentiallyMutatedInScope(declaredElement))) {
+              function.isPotentiallyMutatedInScope2(declaredElement))) {
         return;
       }
     }
@@ -179,7 +179,7 @@ extension on AstNode {
     accept(declaredVariableVisitor);
     var declaredElements = declaredVariableVisitor.declaredElements;
     for (var element in declaredElements) {
-      if (function.isPotentiallyMutatedInScope(element)) {
+      if (function.isPotentiallyMutatedInScope2(element)) {
         return true;
       }
     }
