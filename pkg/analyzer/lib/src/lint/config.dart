03d7cc4ed6e8dd391e8704df34087036af16d379
Sam Rawlins <srawlins@google.com>
2024-10-23 21:23:50 +0000

    analyzer: Remove internal LintConfig class; static elements can be top-level.
    This just simplifies the internal parsing code that parses out lint rule
    configurations from YAML.
    
    Change-Id: I7eb83835cff25f99b79dad9446db5215dce126fa
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/391700
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer/lib/src/lint/config.dart
+++ pkg/analyzer/lib/src/lint/config.dart
@@ -12,96 +12,89 @@ List<RuleConfig>? parseLintRuleConfigs(YamlMap optionsMap) {
   var options = optionsMap.valueAt('linter');
   // Quick check of basic contract.
   if (options is YamlMap) {
-    return LintConfig.parseMap(options).ruleConfigs;
+    var ruleConfigs = <RuleConfig>[];
+    var rulesNode = options.valueAt(AnalyzerOptions.rules);
+    if (rulesNode != null) {
+      ruleConfigs.addAll(_ruleConfigs(rulesNode));
+    }
+
+    return ruleConfigs;
   }
 
   return null;
 }
 
-/// The configuration of lint rules within an analysis options file.
-class LintConfig {
-  final List<RuleConfig> ruleConfigs;
+/// Returns [scalar] as a [bool], if it can be parsed as one.
+bool? _asBool(YamlNode scalar) {
+  var value = scalar is YamlScalar ? scalar.valueOrThrow : scalar;
+  return switch (value) {
+    bool() => value,
+    'true' => true,
+    'false' => false,
+    _ => null,
+  };
+}
 
-  factory LintConfig.parseMap(YamlMap yaml) {
+/// Returns [scalar] as a [String], if it can be parsed as one.
+String? _asString(Object scalar) {
+  var value = scalar is YamlScalar ? scalar.value : scalar;
+  return value is String ? value : null;
+}
+
+Map<String, bool> _parseArgs(YamlNode args) {
+  var enabled = _asBool(args);
+  if (enabled != null) {
+    return {'enabled': enabled};
+  }
+  return {};
+}
+
+/// Returns the [RuleConfig]s that are parsed from [value], which can be either
+/// a YAML list or a YAML map.
+List<RuleConfig> _ruleConfigs(YamlNode value) {
+  // For example:
+  //
+  // ```yaml
+  // - unnecessary_getters
+  // - camel_case_types
+  // ```
+  if (value is YamlList) {
+    return [
+      for (var rule in value.nodes)
+        RuleConfig(name: _asString(rule), args: {'enabled': true}),
+    ];
+  }
+
+  // style_guide: {unnecessary_getters: false, camel_case_types: true}
+  if (value is YamlMap) {
     var ruleConfigs = <RuleConfig>[];
-    var rulesNode = yaml.valueAt(AnalyzerOptions.rules);
-    if (rulesNode != null) {
-      ruleConfigs.addAll(_ruleConfigs(rulesNode));
-    }
+    value.nodes.cast<Object, YamlNode>().forEach((key, value) {
+      // For example: `{unnecessary_getters: false}`.
+      var valueAsBool = _asBool(value);
+      if (valueAsBool != null) {
+        ruleConfigs.add(RuleConfig(
+          name: _asString(key),
+          args: {'enabled': valueAsBool},
+        ));
+      }
 
-    return LintConfig._(ruleConfigs);
-  }
-
-  LintConfig._(this.ruleConfigs);
-
-  static bool? _asBool(YamlNode scalar) {
-    var value = scalar is YamlScalar ? scalar.valueOrThrow : scalar;
-    return switch (value) {
-      bool() => value,
-      'true' => true,
-      'false' => false,
-      _ => null,
-    };
-  }
-
-  static String? _asString(Object scalar) {
-    var value = scalar is YamlScalar ? scalar.value : scalar;
-    return value is String ? value : null;
-  }
-
-  static Map<String, bool> _parseArgs(YamlNode args) {
-    var enabled = _asBool(args);
-    if (enabled != null) {
-      return {'enabled': enabled};
-    }
-    return {};
-  }
-
-  static List<RuleConfig> _ruleConfigs(YamlNode value) {
-    // For example:
-    //
-    // ```yaml
-    // - unnecessary_getters
-    // - camel_case_types
-    // ```
-    if (value is YamlList) {
-      return [
-        for (var rule in value.nodes)
-          RuleConfig(name: _asString(rule), args: {'enabled': true}),
-      ];
-    }
-
-    // style_guide: {unnecessary_getters: false, camel_case_types: true}
-    if (value is YamlMap) {
-      var ruleConfigs = <RuleConfig>[];
-      value.nodes.cast<Object, YamlNode>().forEach((key, value) {
-        // For example: `{unnecessary_getters: false}`.
-        var valueAsBool = _asBool(value);
-        if (valueAsBool != null) {
+      // style_guide: {unnecessary_getters: false, camel_case_types: true}
+      if (value is YamlMap) {
+        value.nodes.cast<Object, YamlNode>().forEach((rule, args) {
+          // TODO(brianwilkerson): verify format.
+          // For example: `unnecessary_getters: false`.
           ruleConfigs.add(RuleConfig(
-            name: _asString(key),
-            args: {'enabled': valueAsBool},
+            name: _asString(rule),
+            args: _parseArgs(args),
+            group: _asString(key),
           ));
-        }
-
-        // style_guide: {unnecessary_getters: false, camel_case_types: true}
-        if (value is YamlMap) {
-          value.nodes.cast<Object, YamlNode>().forEach((rule, args) {
-            // TODO(brianwilkerson): verify format.
-            // For example: `unnecessary_getters: false`.
-            ruleConfigs.add(RuleConfig(
-              name: _asString(rule),
-              args: _parseArgs(args),
-              group: _asString(key),
-            ));
-          });
-        }
-      });
-      return ruleConfigs;
-    }
-
-    return const [];
+        });
+      }
+    });
+    return ruleConfigs;
   }
+
+  return const [];
 }
 
 /// The configuration of a single lint rule within an analysis options file.
