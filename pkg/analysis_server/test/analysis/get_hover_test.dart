5fcbb3206032199249e2684d4d925042e4ee6271
Danny Tuppeny <danny@tuppeny.com>
2024-11-12 17:47:13 +0000

    [analysis_server] Migrate hovers to new element model
    (also fixes https://github.com/dart-lang/sdk/issues/56933)
    
    Change-Id: I0ae66313d9c11012f42093631158c2f2df00d938
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/391644
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/test/analysis/get_hover_test.dart
+++ pkg/analysis_server/test/analysis/get_hover_test.dart
@@ -526,7 +526,7 @@ void f(A<String> a) {
     expect(hover.containingLibraryPath, testFile.path);
     expect(hover.containingClassDescription, 'A');
     expect(hover.dartdoc, '''doc aaa\ndoc bbb''');
-    expect(hover.elementDescription, 'T get fff');
+    expect(hover.elementDescription, 'String get fff');
     expect(hover.elementKind, 'getter');
     // types
     expect(hover.staticType, 'String');
@@ -549,8 +549,11 @@ void f(A a) {
     expect(hover.containingLibraryPath, testFile.path);
     expect(hover.containingClassDescription, 'A');
     expect(hover.dartdoc, '''doc aaa\ndoc bbb''');
-    expect(hover.elementDescription, 'String fff');
-    expect(hover.elementKind, 'field');
+    // Synthetic getters are now shown as getters to avoid confusing
+    // users by hiding them.
+    // https://github.com/dart-lang/sdk/issues/55956
+    expect(hover.elementDescription, 'String get fff');
+    expect(hover.elementKind, 'getter');
     // types
     expect(hover.staticType, 'String');
     expect(hover.propagatedType, isNull);
@@ -701,7 +704,7 @@ void f(A<String> a) {
     expect(hover.containingLibraryPath, testFile.path);
     expect(hover.containingClassDescription, 'A');
     expect(hover.dartdoc, '''doc aaa\ndoc bbb''');
-    expect(hover.elementDescription, 'set fff(T value)');
+    expect(hover.elementDescription, 'set fff(String value)');
     expect(hover.elementKind, 'setter');
     // types
     expect(hover.staticType, 'String');
@@ -828,8 +831,11 @@ void f(A a) {
     expect(hover.containingLibraryPath, testFile.path);
     expect(hover.containingClassDescription, 'A');
     expect(hover.dartdoc, '''doc aaa\ndoc bbb''');
-    expect(hover.elementDescription, 'String fff');
-    expect(hover.elementKind, 'field');
+    // Synthetic getters are now shown as getters to avoid confusing
+    // users by hiding them.
+    // https://github.com/dart-lang/sdk/issues/55956
+    expect(hover.elementDescription, 'set fff(String _fff)');
+    expect(hover.elementKind, 'setter');
     // types
     expect(hover.staticType, 'String');
     expect(hover.propagatedType, isNull);
@@ -1053,8 +1059,8 @@ void f(E e) {
     expect(hover.containingLibraryPath, testFile.path);
     expect(hover.containingClassDescription, 'E');
     expect(hover.dartdoc, '''doc aaa\ndoc bbb''');
-    expect(hover.elementDescription, 'String fff');
-    expect(hover.elementKind, 'field');
+    expect(hover.elementDescription, 'String get fff');
+    expect(hover.elementKind, 'getter');
     // types
     expect(hover.staticType, 'String');
     expect(hover.propagatedType, isNull);
@@ -1715,7 +1721,7 @@ typedef void A(int a);
 /// doc bbb
 set fff(String value) {}
 
-void f(A a) {
+void f() {
   fff = '';
 }
 ''');
@@ -1730,6 +1736,30 @@ void f(A a) {
     // types
     expect(hover.staticType, 'String');
     expect(hover.propagatedType, isNull);
+    // don't show parameter for setters
+    expect(hover.parameter, isNull);
+  }
+
+  Future<void> test_topLevel_setter_parameter() async {
+    newFile(testFilePath, '''
+set fff(String value) {}
+
+void f(String x) {
+  fff = x;
+}
+''');
+    var hover = await prepareHover('x;');
+    // element
+    expect(hover.containingLibraryName, isNull);
+    expect(hover.containingLibraryPath, isNull);
+    expect(hover.containingClassDescription, isNull);
+    expect(hover.elementDescription, 'String x');
+    expect(hover.elementKind, 'parameter');
+    // types
+    expect(hover.staticType, 'String');
+    expect(hover.propagatedType, isNull);
+    // don't show parameter information for setters
+    expect(hover.parameter, isNull);
   }
 
   void _assertHover(
--- pkg/analysis_server/test/lsp/hover_test.dart
+++ pkg/analysis_server/test/lsp/hover_test.dart
@@ -174,7 +174,7 @@ void f() {
 ''';
     var expected = '''
 ```dart
-MyEnum one
+MyEnum get one
 ```
 Type: `MyEnum`
 
@@ -209,7 +209,7 @@ class A {
 ''';
     var expected = '''
 ```dart
-int _
+int get _
 ```
 Type: `int`
 
@@ -729,7 +729,7 @@ final data = A<String?>().[!myF^ield!];
     var expectedHoverContent =
         '''
 ```dart
-T? myField
+String? get myField
 ```
 Type: `String?`
 
@@ -774,7 +774,7 @@ final data = A<String?>().[!myG^etter!];
     var expectedHoverContent =
         '''
 ```dart
-T? get myGetter
+String? get myGetter
 ```
 Type: `String?`
 
@@ -822,7 +822,7 @@ void f() {
     var expectedHoverContent =
         '''
 ```dart
-set mySetter(T value)
+set mySetter(String value)
 ```
 Type: `String`
 
@@ -914,7 +914,7 @@ int f() => [!^_!];
 ''';
     var expected = '''
 ```dart
-int _
+int get _
 ```
 Type: `int`
 
