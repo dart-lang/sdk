6129842d748ead7d81e1ae3863388bfe7b6ed651
Robert Nystrom <rnystrom@google.com>
2024-11-19 00:57:42 +0000

    Opt multitests out of formatting in standalone/.
    The new formatter supports opting a region of code out from being
    formatted. I'm applying this marker to all of the multitests since
    those tests are often very sensitive to formatting and easily broken.
    This way, anyone touching a multitest (including me when I reformat
    the tests) doesn't have to remember to not run the formatter on it.
    
    Change-Id: I06928e08530ad658affd23e594702cff658d8035
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/396181
    Commit-Queue: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Brian Quinlan <bquinlan@google.com>
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Reviewed-by: Brian Quinlan <bquinlan@google.com>

--- tests/standalone/package/scenarios/packages_file_strange_formatting/empty_package_dir_test.dart
+++ tests/standalone/package/scenarios/packages_file_strange_formatting/empty_package_dir_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Formatting can break multitests, so don't format them.
+// dart format off
+
 // Packages=empty_package_dir.packages
 
 // This test verifies handling of legacy .packages file and cannot be run in
