dcee4f2d4d507365794cd4691567270de122dc47
Kallen Tu <kallentu@google.com>
2024-11-19 18:52:33 +0000

    [tests] Enum shorthand language tests - simple identifier and == only.
    This CL adds language tests for the simple identifier variation of enum shorthands. In these tests, we are testing prefix identifiers with a single simple identifier.
    
    For example, `Decoration(fit: BoxFit.cover)` where `fit` has the type `BoxFit?`, we would be able to turn this into `Decoration(fit: .cover)` with enum shorthands.
    
    The tests also test erroneous ways that someone can use the `==` operator like the shorthand not being on the RHS.
    
    Bug: https://github.com/dart-lang/sdk/issues/57038
    Change-Id: I03610db2a683e9c2cdf8c12a99c7808171377e3a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/393606
    Commit-Queue: Kallen Tu <kallentu@google.com>
    Reviewed-by: Erik Ernst <eernst@google.com>

---
+++ tests/language/enum_shorthands/equality/equality_error_test.dart
@@ -0,0 +1,358 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Testing erroneous ways of using shorthands with the `==` and `!=` operators.
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+import '../enum_shorthand_helper.dart';
+
+class ConstConstructorAssert {
+  const ConstConstructorAssert.blue(Color color)
+      : assert(.blue == color);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+
+  const ConstConstructorAssert.notBlue(Color color)
+      : assert(.blue != color);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+
+  const ConstConstructorAssert.one(Integer integer)
+      : assert(.constOne == integer);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+
+  const ConstConstructorAssert.notOne(Integer integer)
+      : assert(.constOne != integer);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+
+  const ConstConstructorAssert.oneExt(IntegerExt integer)
+      : assert(.constOne == integer);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+
+  const ConstConstructorAssert.notOneExt(IntegerExt integer)
+      : assert(.constOne != integer);
+        //     ^
+        // [analyzer] unspecified
+        // [cfe] unspecified
+}
+
+void notSymmetrical(Color color, Integer integer, IntegerExt integerExt) {
+  const bool symBlueEq = .blue == color;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool symBlueNeq = .blue != color;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool symOneEq = .one == integer;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool symOneNeq = .one != integer;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool symOneExtEq = .one == integerExt;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool symOneExtNeq = .one != integerExt;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.blue == color) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.blue != color) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.one == integer) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.one != integer) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.one == integerExt) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (.one != integerExt) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+void rhsNeedsToBeShorthand(Color color, Integer integer, IntegerExt integerExt, bool condition) {
+  const Color constColor = Color.red;
+  const Object obj = true;
+  const bool constCondition = obj as bool;
+
+  const bool rhsColorEq = constColor == (constCondition ? .red : .green);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool rhsColorNeq = constColor != (constCondition ? .red : .green);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (color == (condition ? .red : .green)) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (color != (condition ? .red : .green)) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (color case == (constCondition ? .red : .green)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (color case != (constCondition ? .red : .green)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  const Integer constInteger = Integer.constOne;
+  const bool rhsIntegerEq = constInteger == (constCondition ? .constOne : .constTwo);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool rhsIntegerNeq = constInteger != (constCondition ? .constOne : .constTwo);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (integer == (condition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integer != (condition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integer case == (constCondition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integer case != (constCondition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  const IntegerExt constIntegerExt = IntegerExt.constOne;
+  const bool rhsIntegerExtEq = constIntegerExt == (constCondition ? .constOne : .constTwo);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool rhsIntegerExtNeq = constIntegerExt != (constCondition ? .constOne : .constTwo);
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if (integerExt == (condition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integerExt != (condition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integerExt case == (constCondition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+
+  if (integerExt case != (constCondition ? .constOne : .constTwo)) {
+    // ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+    print('not ok');
+  }
+}
+
+void objectContextType(Color color, Integer integer, IntegerExt integerExt) {
+  const Color constColor = Color.red;
+  const bool contextTypeColorEq = (constColor as Object) == .blue;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool contextTypeColorNeq = (constColor as Object) != .blue;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((color as Object) == .blue) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((color as Object) case == .blue) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((color as Object) != .blue) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((color as Object) case != .blue) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const Integer constInteger = Integer.constOne;
+  const bool contextTypeIntegerEq = (constInteger as Object) == .constTwo;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool contextTypeIntegerNeq = (constInteger as Object) != .constTwo;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integer as Object) == .one) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integer as Object) case == .constOne) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integer as Object) != .one) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integer as Object) case != .constOne) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const IntegerExt constIntegerExt = IntegerExt.constOne;
+  const bool contextTypeIntegerExtEq = (constIntegerExt as Object) == .constTwo;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  const bool contextTypeIntegerExtNeq = (constIntegerExt as Object) != .constTwo;
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integerExt as Object) == .one) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integerExt as Object) case == .constOne) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integerExt as Object) != .one) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  if ((integerExt as Object) case != .constOne) print('not ok');
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+void typeParameterContext<C extends Color, T extends Object>(C color, T value) {
+  if (color == .red) print('not ok');
+  //        ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+  if (value is Color) {
+    if (value == .red) print('not ok');
+    //        ^
+    // [analyzer] unspecified
+    // [cfe] unspecified
+  }
+}
+
+void main() {
+  Color color = .blue;
+  Integer integer = .one;
+  IntegerExt integerExt = .one;
+
+  notSymmetrical(color, integer, integerExt);
+  rhsNeedsToBeShorthand(color, integer, integerExt, true);
+  rhsNeedsToBeShorthand(color, integer, integerExt, false);
+  objectContextType(color, integer, integerExt);
+
+  typeParameterContext(color, integer);
+  typeParameterContext(color, Color.red);
+
+  // Test the constant evaluation for enum shorthands in const constructor
+  // asserts.
+  const ConstConstructorAssert.blue(Color.blue);
+  const ConstConstructorAssert.notBlue(Color.red);
+  const ConstConstructorAssert.one(Integer.constOne);
+  const ConstConstructorAssert.notOne(Integer.constTwo);
+  const ConstConstructorAssert.oneExt(IntegerExt.constOne);
+  const ConstConstructorAssert.notOneExt(IntegerExt.constTwo);
+}
