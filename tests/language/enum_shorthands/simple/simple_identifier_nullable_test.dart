76879d4555de3c08265820df1e6728ad14a1cecf
Kallen Tu <kallentu@google.com>
2024-11-26 17:20:31 +0000

    [tests] Enum shorthand tests for `T?` and language-specified bool contexts.
    This CL adds tests for the enum shorthands behavior where the namespace for `T?` should be the same as `T`. We also test enum shorthands in declarations where the language has specified a `bool` context type.
    
    Follow up to Erik's comment: https://dart-review.googlesource.com/c/sdk/+/393606/comments/16e214b1_4bbfad8f
    
    Bug: https://github.com/dart-lang/sdk/issues/57038
    Change-Id: I7962492a0459e987110e688d4159517e6c58f6ea
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/397182
    Reviewed-by: Erik Ernst <eernst@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/enum_shorthands/simple/simple_identifier_nullable_test.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// `T?` denotes the same namespace as `T` for enum shorthands.
+
+// SharedOptions=--enable-experiment=enum-shorthands
+
+import '../enum_shorthand_helper.dart';
+
+void main() {
+  // Enum
+  Color? color = .blue;
+  const Color? constColor = .blue;
+  switch (color) {
+    case .blue:
+      print('blue');
+    case .red:
+      print('red');
+    case .green:
+      print('green');
+  }
+  var colorList = <Color?>[.blue, .green, .red];
+
+  // Class
+  Integer? integer = .one;
+  const Integer? constInteger = .constOne;
+  var integerList = <Integer?>[.one, .two, .one];
+
+  // Extension type
+  IntegerExt? integerExt = .one;
+  const IntegerExt? constIntegerExt = .constOne;
+  var integerExtList = <Integer?>[.one, .two, .one];
+
+  // Mixin
+  IntegerMixin? integerMixin = .mixinOne;
+  const IntegerMixin? constIntegerMixin = .mixinConstOne;
+  var integerMixinList = <IntegerMixin?>[.one, .two, .one];
+}
+
