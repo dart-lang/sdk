0224206aaa613d66ef9ed126be2d36086dab1d06
Nicholas Shahan <nshahan@google.com>
2024-11-28 01:23:24 +0000

    [tests] Port const tests from vm hot reload suite
    Change-Id: I8fedb4afe8f117e54aa17ff85e0415e818b2c62f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/389583
    Reviewed-by: Nate Biggs <natebiggs@google.com>
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Mark Zhou <markzipan@google.com>

---
+++ tests/hot_reload/const_to_non_const_class/main.1.reject.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:reload_test/reload_test_utils.dart';
+
+// Adapted from:
+// https://github.com/dart-lang/sdk/blob/368cb645e5ff5baa1d1ed86bfd2e7d818471a652/runtime/vm/isolate_reload_test.cc#L4569
+
+void helper() {
+  throw Exception('This should never run.');
+}
+
+class A {
+  dynamic x;
+  A(this.x);
+}
+
+dynamic a;
+
+Future<void> main() async {
+  helper();
+  await hotReload(expectRejection: true);
+  helper();
+}
+/** DIFF **/
+/*
+@@ -8,12 +8,12 @@
+ // https://github.com/dart-lang/sdk/blob/368cb645e5ff5baa1d1ed86bfd2e7d818471a652/runtime/vm/isolate_reload_test.cc#L4569
+ 
+ void helper() {
+-  a = const A(1);
++  throw Exception('This should never run.');
+ }
+ 
+ class A {
+-  final dynamic x;
+-  const A(this.x);
++  dynamic x;
++  A(this.x);
+ }
+ 
+ dynamic a;
+*/
