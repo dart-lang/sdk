dart-lang/sdk

commit 9b06e26620a081c89931e5618336ff23dc6083be

Reland "[vm] Enforce that entry points must be annotated by default."
This is a reland of commit cb9ecbc3636aaa4e8c8301caa4bab2d903825bf3

This reland only turns on the entry point verification flag by
default in AOT mode. After Flutter tests that use native access
in JIT mode have been appropriately updated, a followup CL will
turn this flag on by default in JIT mode as well.

Original change's description:
> [vm] Enforce that entry points must be annotated by default.
>
> Changes the default value of the --verify-entry-points flag
> to true.
>
> Changes the default value for the check_is_entrypoint argument to
> to the Invoke/InvokeGetter/InvokeSetter flags to true. The mirrors
> library implementation and calls via vm-service explicitly pass
> false for this argument now.
>
> Add annotations as needed, such as annotating classes with
> annotated generative constructors. In some cases, the annotations
> were more general than needed (e.g., annotating with a no-argument
> entry point annotation when only the setter is needed), so make
> those annotations more specific.
>
> As this pattern is already common in downstream code, allow
> Dart_Invoke on fields as long as the field is annotated for getter
> access. (That is, calling Dart_Invoke for a field is equivalent to
> retrieving the closure value via Dart_GetField and then calling
> Dart_InvokeClosure.)
>
> TEST=vm/cc/DartAPI_MissingEntryPoints
>      vm/dart/entrypoints_verification_test
>
> Issue: https://github.com/dart-lang/sdk/issues/50649
> Issue: https://github.com/flutter/flutter/issues/118608
>
> Change-Id: Ibb3bf15632ab2958d8791b449af8651d47f871a5
> Cq-Include-Trybots: luci.dart.try:vm-aot-linux-product-x64-try,vm-aot-linux-debug-x64-try,vm-aot-mac-release-arm64-try,vm-aot-mac-product-arm64-try,vm-aot-dwarf-linux-product-x64-try
> CoreLibraryReviewExempt: adding/editing vm-only pragma annotations
> Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/363566

runtime/vm/compiler/backend/il_test_helper.cc
@@ -81,19 +81,6 @@ TypeParameterPtr GetFunctionTypeParameter(const Function& fun, intptr_t index) {
   return param.ptr();
 }
 
-ObjectPtr Invoke(const Library& lib, const char* name) {
-  Thread* thread = Thread::Current();
-  Dart_Handle api_lib = Api::NewHandle(thread, lib.ptr());
-  Dart_Handle result;
-  {
-    TransitionVMToNative transition(thread);
-    result =
-        Dart_Invoke(api_lib, NewString(name), /*argc=*/0, /*argv=*/nullptr);
-    EXPECT_VALID(result);
-  }
-  return Api::UnwrapHandle(result);
-}
-
 InstructionsPtr BuildInstructions(
     std::function<void(compiler::Assembler* assembler)> fun) {
   auto thread = Thread::Current();

runtime/vm/compiler/backend/il_test_helper.h
@@ -65,8 +65,6 @@ ClassPtr GetClass(const Library& lib, const char* name);
 TypeParameterPtr GetClassTypeParameter(const Class& klass, intptr_t index);
 TypeParameterPtr GetFunctionTypeParameter(const Function& fun, intptr_t index);
 
-ObjectPtr Invoke(const Library& lib, const char* name);
-
 InstructionsPtr BuildInstructions(
     std::function<void(compiler::Assembler* assembler)> fun);
 

runtime/bin/entrypoints_verification_test.cc
@@ -2,9 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 // TODO(dartbug.com/40579): This requires static linking to either link
 // dart.exe or dartaotruntime.exe on Windows.
@@ -12,98 +12,178 @@
 #include "include/dart_api.h"
 #include "include/dart_native_api.h"
 
+static bool is_dartaotruntime = true;
+
+bool IsTreeShaken(const char* name, Dart_Handle handle, const char* error) {
+  // No tree shaking in the JIT runtime.
+  if (!is_dartaotruntime) return false;
+  if (Dart_IsApiError(handle)) {
+    // All tree-shaking related API errors should include the expected name.
+    if (strstr(error, name) == nullptr) return false;
+    // Node was tree shaken (e.g., 'Class C not found in library...').
+    if (strstr(error, " not found in ") != nullptr) return true;
+    // Constructor was tree shaken.
+    if (strstr(error, "Dart_New: could not find ") != nullptr) return true;
+  } else if (Dart_IsUnhandledExceptionError(handle)) {
+    // TFA replaces operations with a throw in some cases. If obfuscation
+    // is turned on and the member has no entry point annotation, then its
+    // name may be obfuscated in the result and so cannot be depended on.
+    if (strstr(error, "Attempt to execute code removed by ") != nullptr) {
+      return true;
+    }
+    // All other tree-shaking related unhandled exceptions are NSM errors
+    // that should include the expected name.
+    if (strstr(error, name) == nullptr) return false;
+    if (strstr(error, "NoSuchMethodError: ") == nullptr) {
+      return false;
+    }
+    if (strstr(error, "No top-level method '") != nullptr) return true;
+    if (strstr(error, "No top-level getter '") != nullptr) return true;
+    if (strstr(error, "No top-level setter '") != nullptr) return true;
+    if (strstr(error, "No static method '") != nullptr) return true;
+    if (strstr(error, "No static getter '") != nullptr) return true;
+    if (strstr(error, "No static setter '") != nullptr) return true;
+    if (strstr(error, "' has no instance method '") != nullptr) return true;
+    if (strstr(error, "' has no instance getter '") != nullptr) return true;
+    if (strstr(error, "' has no instance setter '") != nullptr) return true;
+  }
+  // Not an tree shaking-related error.
+  return false;
+}
+
+#if defined(_MSC_VER)
+#define FATAL(fmt, ...)                                                        \
+  do {                                                                         \
+    fprintf(stderr, "Failed at %s:%d: " fmt "!\n", __FILE__, __LINE__,         \
+            __VA_ARGS__);                                                      \
+    abort();                                                                   \
+  } while (false)
+#else
+#define FATAL(fmt, ...)                                                        \
+  do {                                                                         \
+    fprintf(stderr, "Failed at %s:%d: " fmt "!\n", __FILE__, __LINE__,         \
+            ##__VA_ARGS__);                                                    \
+    abort();                                                                   \
+  } while (false)
+#endif
+
 #define CHECK(H)                                                               \
   do {                                                                         \
+    fprintf(stderr, "Checking %s...\n", #H);                                   \
     Dart_Handle __handle__ = H;                                                \
     if (Dart_IsError(__handle__)) {                                            \
       const char* message = Dart_GetError(__handle__);                         \
-      fprintf(stderr, "Check \"" #H "\" failed: %s", message);                 \
-      abort();                                                                 \
+      FATAL("\n%s", message);                                                  \
+    } else {                                                                   \
+      fprintf(stderr, "  Check passed.\n\n");                                  \
     }                                                                          \
   } while (false)
 
-#define ASSERT(E)                                                              \
-  if (!(E)) {                                                                  \
-    fprintf(stderr, "Assertion \"" #E "\" failed at %s:%d!\n", __FILE__,       \
-            __LINE__);                                                         \
-    abort();                                                                   \
-  }
+#define ASSERT_SUBSTRING(needle, haystack)                                     \
+  do {                                                                         \
+    if (strstr(haystack, needle) == nullptr) {                                 \
+      FATAL("expected '%s' within:\n%s\n", needle, haystack);                  \
+    }                                                                          \
+  } while (false)
 
-static bool is_dartaotruntime = true;
+#define FAIL(name, result)                                                     \
+  do {                                                                         \
+    fprintf(stderr, "Expect failure to access '%s'\n", name);                  \
+    if (!Dart_IsError(result)) {                                               \
+      FATAL("No error for accessing %s", name);                                \
+    }                                                                          \
+    const char* error = Dart_GetError(result);                                 \
+    if (IsTreeShaken(name, result, error)) {                                   \
+      fprintf(stderr, "  Received error due to tree shaking: %s\n\n", error);  \
+    } else if (!Dart_IsApiError(result)) {                                     \
+      FATAL("Not an API error for accessing %s: %s", name, error);             \
+    } else {                                                                   \
+      ASSERT_SUBSTRING(name, error);                                           \
+      ASSERT_SUBSTRING("ERROR: ", error);                                      \
+      ASSERT_SUBSTRING("' from native code, it must be annotated.", error);    \
+      fprintf(stderr, "  Received expected error: %s\n\n", error);             \
+    }                                                                          \
+  } while (false)
 
 // Some invalid accesses are allowed in AOT since we don't retain @pragma
-// annotations. Therefore we skip the negative tests in AOT.
-#define FAIL(name, result)                                                     \
-  if (!is_dartaotruntime) {                                                    \
-    Fail(name, result);                                                        \
-  }
+// annotations. Only use this if there's no other way to detect missing
+// annotations, e.g., a function with no code signals that the precompiler
+// did not preserve the code object because the function is not annotated
+// as a "call" entry point.
+#define FAIL_UNLESS_PRECOMPILED(name, result)                                  \
+  do {                                                                         \
+    if (!is_dartaotruntime) {                                                  \
+      FAIL(name, result);                                                      \
+    } else {                                                                   \
+      CHECK(result);                                                           \
+    }                                                                          \
+  } while (false);
 
-void Fail(const char* name, Dart_Handle result) {
-  ASSERT(Dart_IsApiError(result));
-  const char* error = Dart_GetError(result);
-  ASSERT(strstr(error, name));
-  ASSERT(strstr(error, "It is illegal to access"));
-}
+#define FAIL_CLOSURIZE_CONSTRUCTOR(name, result)                               \
+  do {                                                                         \
+    fprintf(stderr, "Expect failure to closurize constructor '%s'\n", name);   \
+    if (!Dart_IsError(result)) {                                               \
+      FATAL("No error for closurizing %s", name);                              \
+    }                                                                          \
+    const char* error = Dart_GetError(result);                                 \
+    if (!Dart_IsUnhandledExceptionError(result)) {                             \
+      FATAL("Not an unhandled exception error for closurizing %s: %s", name,   \
+            error);                                                            \
+    } else {                                                                   \
+      ASSERT_SUBSTRING(name, error);                                           \
+      ASSERT_SUBSTRING("No static getter", error);                             \
+      fprintf(stderr, "  Received expected error: %s\n\n", error);             \
+    }                                                                          \
+  } while (false)
 
-#define FAIL_INVOKE_FIELD(name, result)                                        \
-  if (!is_dartaotruntime) {                                                    \
-    FailInvokeField(name, result);                                             \
-  }
-
-static void FailInvokeField(const char* name, Dart_Handle result) {
-  ASSERT(Dart_IsApiError(result));
-  const char* error = Dart_GetError(result);
-  ASSERT(strstr(error, name));
-  ASSERT(strstr(error, "Entry-points do not allow invoking fields"));
-}
-
-static void FailClosurizeConstructor(const char* name, Dart_Handle result) {
-  ASSERT(Dart_IsUnhandledExceptionError(result));
-  const char* error = Dart_GetError(result);
-  ASSERT(strstr(error, name));
-  ASSERT(strstr(error, "No static getter"));
-}
-
-static void TestFields(Dart_Handle target) {
-  FAIL("fld0", Dart_GetField(target, Dart_NewStringFromCString("fld0")));
-  FAIL("fld0",
-       Dart_SetField(target, Dart_NewStringFromCString("fld0"), Dart_Null()));
-
-  FAIL_INVOKE_FIELD(
-      "fld0",
-      Dart_Invoke(target, Dart_NewStringFromCString("fld0"), 0, nullptr));
-
-  CHECK(Dart_GetField(target, Dart_NewStringFromCString("fld1")));
-  CHECK(Dart_SetField(target, Dart_NewStringFromCString("fld1"), Dart_Null()));
-  FAIL_INVOKE_FIELD(
-      "fld1",
-      Dart_Invoke(target, Dart_NewStringFromCString("fld1"), 0, nullptr));
-
-  CHECK(Dart_GetField(target, Dart_NewStringFromCString("fld2")));
-  FAIL("fld2",
-       Dart_SetField(target, Dart_NewStringFromCString("fld2"), Dart_Null()));
-  FAIL_INVOKE_FIELD(
-      "fld2",
-      Dart_Invoke(target, Dart_NewStringFromCString("fld2"), 0, nullptr));
-
-  FAIL("fld3", Dart_GetField(target, Dart_NewStringFromCString("fld3")));
-  CHECK(Dart_SetField(target, Dart_NewStringFromCString("fld3"), Dart_Null()));
-  FAIL_INVOKE_FIELD(
-      "fld3",
-      Dart_Invoke(target, Dart_NewStringFromCString("fld3"), 0, nullptr));
-}
+#define TEST_FIELDS(target)                                                    \
+  do {                                                                         \
+    /* Since the fields start off initialized to a non-null value and then */  \
+    /* are updated with a null value, check invocation prior to set.       */  \
+    FAIL("fld0", Dart_GetField(target, Dart_NewStringFromCString("fld0")));    \
+    FAIL("fld0",                                                               \
+         Dart_Invoke(target, Dart_NewStringFromCString("fld0"), 0, nullptr));  \
+    FAIL("fld0", Dart_SetField(target, Dart_NewStringFromCString("fld0"),      \
+                               Dart_Null()));                                  \
+    CHECK(Dart_GetField(target, Dart_NewStringFromCString("fld1")));           \
+    CHECK(Dart_Invoke(target, Dart_NewStringFromCString("fld1"), 0, nullptr)); \
+    CHECK(Dart_SetField(target, Dart_NewStringFromCString("fld1"),             \
+                        Dart_Null()));                                         \
+    CHECK(Dart_GetField(target, Dart_NewStringFromCString("fld2")));           \
+    CHECK(Dart_Invoke(target, Dart_NewStringFromCString("fld2"), 0, nullptr)); \
+    if (Dart_IsLibrary(target) || Dart_IsType(target)) {                       \
+      /* There are no implicit setters for static fields, so the pragma     */ \
+      /* must be checked; in precompiled mode, that means a false positive. */ \
+      FAIL_UNLESS_PRECOMPILED(                                                 \
+          "fld2", Dart_SetField(target, Dart_NewStringFromCString("fld2"),     \
+                                Dart_Null()));                                 \
+    } else {                                                                   \
+      FAIL("fld2", Dart_SetField(target, Dart_NewStringFromCString("fld2"),    \
+                                 Dart_Null()));                                \
+    }                                                                          \
+    FAIL("fld3", Dart_GetField(target, Dart_NewStringFromCString("fld3")));    \
+    FAIL("fld3",                                                               \
+         Dart_Invoke(target, Dart_NewStringFromCString("fld3"), 0, nullptr));  \
+    CHECK(Dart_SetField(target, Dart_NewStringFromCString("fld3"),             \
+                        Dart_Null()));                                         \
+  } while (false)
 
 DART_EXPORT void RunTests() {
   is_dartaotruntime = Dart_IsPrecompiledRuntime();
 
   Dart_Handle lib = Dart_RootLibrary();
 
-  //////// Test allocation and constructor invocation.
+  //////// Test class access.
 
   FAIL("C", Dart_GetClass(lib, Dart_NewStringFromCString("C")));
 
   Dart_Handle D_class = Dart_GetClass(lib, Dart_NewStringFromCString("D"));
   CHECK(D_class);
+
+  Dart_Handle F_class = Dart_GetClass(lib, Dart_NewStringFromCString("F"));
+  CHECK(F_class);
+
+  //////// Test allocation and constructor invocation.
 
   CHECK(Dart_Allocate(D_class));
 
@@ -116,22 +196,31 @@ DART_EXPORT void RunTests() {
 
   //////// Test actions against methods
 
-  FailClosurizeConstructor(
+  fprintf(stderr, "\n\nTesting methods with library target\n\n\n");
+
+  FAIL("noop", Dart_Invoke(lib, Dart_NewStringFromCString("noop"), 0, nullptr));
+
+  FAIL("fn0", Dart_Invoke(lib, Dart_NewStringFromCString("fn0"), 0, nullptr));
+
+  CHECK(Dart_Invoke(lib, Dart_NewStringFromCString("fn1"), 0, nullptr));
+  FAIL("fn1_get",
+       Dart_Invoke(lib, Dart_NewStringFromCString("fn1_get"), 0, nullptr));
+  CHECK(Dart_Invoke(lib, Dart_NewStringFromCString("fn1_call"), 0, nullptr));
+
+  FAIL("fn0", Dart_GetField(lib, Dart_NewStringFromCString("fn0")));
+
+  CHECK(Dart_GetField(lib, Dart_NewStringFromCString("fn1")));
+  CHECK(Dart_GetField(lib, Dart_NewStringFromCString("fn1_get")));
+  FAIL("fn1_call", Dart_GetField(lib, Dart_NewStringFromCString("fn1_call")));
+
+  fprintf(stderr, "\n\nTesting methods with class target\n\n\n");
+
+  FAIL_CLOSURIZE_CONSTRUCTOR(
       "defined", Dart_GetField(D_class, Dart_NewStringFromCString("defined")));
-  FailClosurizeConstructor(
+  FAIL_CLOSURIZE_CONSTRUCTOR(
       "fact", Dart_GetField(D_class, Dart_NewStringFromCString("fact")));
 
   FAIL("fn0", Dart_Invoke(D, Dart_NewStringFromCString("fn0"), 0, nullptr));
-
-  CHECK(Dart_Invoke(D, Dart_NewStringFromCString("fn1"), 0, nullptr));
-  FAIL("fn1", Dart_Invoke(D, Dart_NewStringFromCString("fn1_get"), 0, nullptr));
-  CHECK(Dart_Invoke(D, Dart_NewStringFromCString("fn1_call"), 0, nullptr));
-
-  FAIL("fn0", Dart_GetField(D, Dart_NewStringFromCString("fn0")));
-
-  CHECK(Dart_GetField(D, Dart_NewStringFromCString("fn1")));
-  CHECK(Dart_GetField(D, Dart_NewStringFromCString("fn1_get")));
-  FAIL("fn1", Dart_GetField(D, Dart_NewStringFromCString("fn1_call")));
 
   FAIL("fn2",
        Dart_Invoke(D_class, Dart_NewStringFromCString("fn2"), 0, nullptr));
@@ -139,7 +228,7 @@ DART_EXPORT void RunTests() {
   CHECK(Dart_Invoke(D_class, Dart_NewStringFromCString("fn3"), 0, nullptr));
   CHECK(
       Dart_Invoke(D_class, Dart_NewStringFromCString("fn3_call"), 0, nullptr));
-  FAIL("fn3",
+  FAIL("fn3_get",
        Dart_Invoke(D_class, Dart_NewStringFromCString("fn3_get"), 0, nullptr));
 
   FAIL("fn2", Dart_GetField(D_class, Dart_NewStringFromCString("fn2")));
@@ -149,25 +238,27 @@ DART_EXPORT void RunTests() {
        Dart_GetField(D_class, Dart_NewStringFromCString("fn3_call")));
   CHECK(Dart_GetField(D_class, Dart_NewStringFromCString("fn3_get")));
 
-  FAIL("fn0", Dart_Invoke(lib, Dart_NewStringFromCString("fn0"), 0, nullptr));
+  fprintf(stderr, "\n\nTesting methods with instance target\n\n\n");
 
-  CHECK(Dart_Invoke(lib, Dart_NewStringFromCString("fn1"), 0, nullptr));
-  FAIL("fn1",
-       Dart_Invoke(lib, Dart_NewStringFromCString("fn1_get"), 0, nullptr));
-  CHECK(Dart_Invoke(lib, Dart_NewStringFromCString("fn1_call"), 0, nullptr));
+  CHECK(Dart_Invoke(D, Dart_NewStringFromCString("fn1"), 0, nullptr));
+  FAIL("fn1_get",
+       Dart_Invoke(D, Dart_NewStringFromCString("fn1_get"), 0, nullptr));
+  CHECK(Dart_Invoke(D, Dart_NewStringFromCString("fn1_call"), 0, nullptr));
 
-  FAIL("fn0", Dart_GetField(lib, Dart_NewStringFromCString("fn0")));
+  FAIL("fn0", Dart_GetField(D, Dart_NewStringFromCString("fn0")));
 
-  CHECK(Dart_GetField(lib, Dart_NewStringFromCString("fn1")));
-  CHECK(Dart_GetField(lib, Dart_NewStringFromCString("fn1_get")));
-  FAIL("fn1", Dart_GetField(lib, Dart_NewStringFromCString("fn1_call")));
+  CHECK(Dart_GetField(D, Dart_NewStringFromCString("fn1")));
+  CHECK(Dart_GetField(D, Dart_NewStringFromCString("fn1_get")));
+  FAIL("fn1_call", Dart_GetField(D, Dart_NewStringFromCString("fn1_call")));
 
   //////// Test actions against fields
 
-  TestFields(D);
+  fprintf(stderr, "\n\nTesting fields with library target\n\n\n");
+  TEST_FIELDS(lib);
 
-  Dart_Handle F_class = Dart_GetClass(lib, Dart_NewStringFromCString("F"));
-  TestFields(F_class);
+  fprintf(stderr, "\n\nTesting fields with class target\n\n\n");
+  TEST_FIELDS(F_class);
 
-  TestFields(lib);
+  fprintf(stderr, "\n\nTesting fields with instance target\n\n\n");
+  TEST_FIELDS(D);
 }

runtime/lib/isolate.cc
@@ -710,7 +710,7 @@ ObjectPtr IsolateSpawnState::ResolveFunction() {
   // Check whether the root library defines a main function.
   const Library& lib =
       Library::Handle(zone, IG->object_store()->root_library());
-  const String& main = String::Handle(zone, String::New("main"));
+  const String& main = Symbols::main();
   Function& func = Function::Handle(zone, lib.LookupFunctionAllowPrivate(main));
   if (func.IsNull()) {
     // Check whether main is reexported from the root library.

runtime/lib/mirrors.cc
@@ -1221,6 +1221,8 @@ DEFINE_NATIVE_ENTRY(TypeVariableMirror_upper_bound, 0, 1) {
   return param.bound();
 }
 
+static constexpr bool kNoStrictEntryPointChecks = false;
+
 DEFINE_NATIVE_ENTRY(InstanceMirror_invoke, 0, 5) {
   // Argument 0 is the mirror, which is unused by the native. It exists
   // because this native is an instance method in order to be polymorphic
@@ -1230,7 +1232,8 @@ DEFINE_NATIVE_ENTRY(InstanceMirror_invoke, 0, 5) {
                                arguments->NativeArgAt(2));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, args, arguments->NativeArgAt(3));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, arg_names, arguments->NativeArgAt(4));
-  RETURN_OR_PROPAGATE(reflectee.Invoke(function_name, args, arg_names));
+  RETURN_OR_PROPAGATE(reflectee.Invoke(function_name, args, arg_names,
+                                       kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(InstanceMirror_invokeGetter, 0, 3) {
@@ -1239,7 +1242,8 @@ DEFINE_NATIVE_ENTRY(InstanceMirror_invokeGetter, 0, 3) {
   // with its cousins.
   GET_NATIVE_ARGUMENT(Instance, reflectee, arguments->NativeArgAt(1));
   GET_NON_NULL_NATIVE_ARGUMENT(String, getter_name, arguments->NativeArgAt(2));
-  RETURN_OR_PROPAGATE(reflectee.InvokeGetter(getter_name));
+  RETURN_OR_PROPAGATE(
+      reflectee.InvokeGetter(getter_name, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(InstanceMirror_invokeSetter, 0, 4) {
@@ -1249,7 +1253,8 @@ DEFINE_NATIVE_ENTRY(InstanceMirror_invokeSetter, 0, 4) {
   GET_NATIVE_ARGUMENT(Instance, reflectee, arguments->NativeArgAt(1));
   GET_NON_NULL_NATIVE_ARGUMENT(String, setter_name, arguments->NativeArgAt(2));
   GET_NATIVE_ARGUMENT(Instance, value, arguments->NativeArgAt(3));
-  RETURN_OR_PROPAGATE(reflectee.InvokeSetter(setter_name, value));
+  RETURN_OR_PROPAGATE(
+      reflectee.InvokeSetter(setter_name, value, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(InstanceMirror_computeType, 0, 1) {
@@ -1309,7 +1314,8 @@ DEFINE_NATIVE_ENTRY(ClassMirror_invoke, 0, 5) {
                                arguments->NativeArgAt(2));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, args, arguments->NativeArgAt(3));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, arg_names, arguments->NativeArgAt(4));
-  RETURN_OR_PROPAGATE(klass.Invoke(function_name, args, arg_names));
+  RETURN_OR_PROPAGATE(
+      klass.Invoke(function_name, args, arg_names, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(ClassMirror_invokeGetter, 0, 3) {
@@ -1324,7 +1330,8 @@ DEFINE_NATIVE_ENTRY(ClassMirror_invokeGetter, 0, 3) {
     UNREACHABLE();
   }
   GET_NON_NULL_NATIVE_ARGUMENT(String, getter_name, arguments->NativeArgAt(2));
-  RETURN_OR_PROPAGATE(klass.InvokeGetter(getter_name, true));
+  RETURN_OR_PROPAGATE(
+      klass.InvokeGetter(getter_name, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(ClassMirror_invokeSetter, 0, 4) {
@@ -1335,7 +1342,8 @@ DEFINE_NATIVE_ENTRY(ClassMirror_invokeSetter, 0, 4) {
   const Class& klass = Class::Handle(ref.GetClassReferent());
   GET_NON_NULL_NATIVE_ARGUMENT(String, setter_name, arguments->NativeArgAt(2));
   GET_NATIVE_ARGUMENT(Instance, value, arguments->NativeArgAt(3));
-  RETURN_OR_PROPAGATE(klass.InvokeSetter(setter_name, value));
+  RETURN_OR_PROPAGATE(
+      klass.InvokeSetter(setter_name, value, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(ClassMirror_invokeConstructor, 0, 5) {
@@ -1488,7 +1496,8 @@ DEFINE_NATIVE_ENTRY(LibraryMirror_invoke, 0, 5) {
                                arguments->NativeArgAt(2));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, args, arguments->NativeArgAt(3));
   GET_NON_NULL_NATIVE_ARGUMENT(Array, arg_names, arguments->NativeArgAt(4));
-  RETURN_OR_PROPAGATE(library.Invoke(function_name, args, arg_names));
+  RETURN_OR_PROPAGATE(library.Invoke(function_name, args, arg_names,
+                                     kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(LibraryMirror_invokeGetter, 0, 3) {
@@ -1498,7 +1507,8 @@ DEFINE_NATIVE_ENTRY(LibraryMirror_invokeGetter, 0, 3) {
   GET_NON_NULL_NATIVE_ARGUMENT(MirrorReference, ref, arguments->NativeArgAt(1));
   const Library& library = Library::Handle(ref.GetLibraryReferent());
   GET_NON_NULL_NATIVE_ARGUMENT(String, getter_name, arguments->NativeArgAt(2));
-  RETURN_OR_PROPAGATE(library.InvokeGetter(getter_name, true));
+  RETURN_OR_PROPAGATE(
+      library.InvokeGetter(getter_name, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(LibraryMirror_invokeSetter, 0, 4) {
@@ -1509,7 +1519,8 @@ DEFINE_NATIVE_ENTRY(LibraryMirror_invokeSetter, 0, 4) {
   const Library& library = Library::Handle(ref.GetLibraryReferent());
   GET_NON_NULL_NATIVE_ARGUMENT(String, setter_name, arguments->NativeArgAt(2));
   GET_NATIVE_ARGUMENT(Instance, value, arguments->NativeArgAt(3));
-  RETURN_OR_PROPAGATE(library.InvokeSetter(setter_name, value));
+  RETURN_OR_PROPAGATE(
+      library.InvokeSetter(setter_name, value, kNoStrictEntryPointChecks));
 }
 
 DEFINE_NATIVE_ENTRY(MethodMirror_owner, 0, 2) {

commit 16baa339272ee8b3516f272036a1f32d7a27895b

[vm/compiler] Fix build on ARM64 Windows.
Fixes the ARM64 Windows build error introduced by 9b06e26 due to
allocating an array of size 0 when the varargs argument to the
template is empty.

TEST=ci

Change-Id: Iccd714de5643da647db8de2a5bea9ef7074610c0
Cq-Include-Trybots: luci.dart.try:vm-win-release-arm64-try,vm-aot-win-release-arm64-try
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/398886
Auto-Submit: Tess Strickland <sstrickl@google.com>
Reviewed-by: Derek Xu <derekx@google.com>

runtime/vm/compiler/backend/il_test_helper.cc
@@ -81,6 +81,19 @@ TypeParameterPtr GetFunctionTypeParameter(const Function& fun, intptr_t index) {
   return param.ptr();
 }
 
+ObjectPtr Invoke(const Library& lib, const char* name) {
+  Thread* thread = Thread::Current();
+  Dart_Handle api_lib = Api::NewHandle(thread, lib.ptr());
+  Dart_Handle result;
+  {
+    TransitionVMToNative transition(thread);
+    result =
+        Dart_Invoke(api_lib, NewString(name), /*argc=*/0, /*argv=*/nullptr);
+    EXPECT_VALID(result);
+  }
+  return Api::UnwrapHandle(result);
+}
+
 InstructionsPtr BuildInstructions(
     std::function<void(compiler::Assembler* assembler)> fun) {
   auto thread = Thread::Current();

runtime/vm/compiler/backend/il_test_helper.h
@@ -65,6 +65,8 @@ ClassPtr GetClass(const Library& lib, const char* name);
 TypeParameterPtr GetClassTypeParameter(const Class& klass, intptr_t index);
 TypeParameterPtr GetFunctionTypeParameter(const Function& fun, intptr_t index);
 
+ObjectPtr Invoke(const Library& lib, const char* name);
+
 InstructionsPtr BuildInstructions(
     std::function<void(compiler::Assembler* assembler)> fun);
 