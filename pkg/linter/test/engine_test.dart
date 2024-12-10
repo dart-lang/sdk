f11466be9991be7d20f8e2228ebede36f79cb9ed
Sam Rawlins <srawlins@google.com>
2024-12-10 00:12:59 +0000

    linter: Remove infra for removed integration-style tests
    Change-Id: I0e8d4db0914fb72d451f7fbea389e0bf8cd36886
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/399625
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/linter/test/engine_test.dart
+++ pkg/linter/test/engine_test.dart
@@ -2,12 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:analyzer/error/error.dart';
-import 'package:analyzer/src/lint/linter.dart';
-import 'package:analyzer/src/lint/pub.dart';
-import 'package:analyzer/src/string_source.dart' show StringSource;
-import 'package:linter/src/test_utilities/linter_options.dart';
-import 'package:linter/src/test_utilities/test_linter.dart';
 import 'package:linter/src/utils.dart';
 import 'package:test/test.dart';
 
@@ -36,47 +30,6 @@ void defineLinterEngineTests() {
       });
     });
 
-    group('lint driver', () {
-      test('pubspec', () {
-        var visited = false;
-        var options =
-            LinterOptions(enabledRules: [MockLintRule((n) => visited = true)]);
-        TestLinter(options).lintPubspecSource(contents: 'name: foo_bar');
-        expect(visited, isTrue);
-      });
-      test('error collecting', () {
-        var error = AnalysisError.tmp(
-            source: StringSource('foo', ''),
-            offset: 0,
-            length: 0,
-            errorCode: LintCode('MockLint', 'This is a test...'));
-        var linter = TestLinter(LinterOptions(enabledRules: []))
-          ..onError(error);
-        expect(linter.errors.contains(error), isTrue);
-      });
-    });
-
     group('dtos', () {});
   });
-}
-
-typedef NodeVisitor = void Function(Object node);
-
-class MockLintRule extends LintRule {
-  final NodeVisitor nodeVisitor;
-  MockLintRule(this.nodeVisitor) : super(name: 'MockLint', description: 'Desc');
-
-  @override
-  PubspecVisitor<void> getPubspecVisitor() => MockPubspecVisitor(nodeVisitor);
-}
-
-class MockPubspecVisitor extends PubspecVisitor<void> {
-  final NodeVisitor? nodeVisitor;
-
-  MockPubspecVisitor(this.nodeVisitor);
-
-  @override
-  void visitPackageName(PSEntry node) {
-    nodeVisitor?.call(node);
-  }
 }
