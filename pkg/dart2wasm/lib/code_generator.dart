fb5fbf1dec8823937c61f3735c90993a5d80e75d
Nate Biggs <natebiggs@google.com>
2025-01-06 10:32:33 -0800

    [dart2wasm] Fix null checks being added on non-nullable value types.
    Bug: https://github.com/dart-lang/sdk/issues/59840
    Change-Id: Ifaac6430eabfee21e4971b3dd864e22f65c80adf
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/402980
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Nate Biggs <natebiggs@google.com>

--- pkg/dart2wasm/lib/code_generator.dart
+++ pkg/dart2wasm/lib/code_generator.dart
@@ -2561,20 +2561,25 @@ abstract class AstCodeGenerator
 
   @override
   w.ValueType visitNullCheck(NullCheck node, w.ValueType expectedType) {
-    return _nullCheck(node.operand, translator.throwNullCheckError);
-  }
-
-  w.ValueType _nullCheck(Expression operand, Procedure errorProcedure) {
-    w.ValueType operandType = translator.translateType(dartTypeOf(operand));
+    w.ValueType operandType =
+        translator.translateType(dartTypeOf(node.operand));
     w.ValueType nonNullOperandType = operandType.withNullability(false);
-    w.Label nullCheckBlock = b.block(const [], [nonNullOperandType]);
-    translateExpression(operand, operandType);
 
-    // We lower a null check to a br_on_non_null, throwing a [TypeError] in the
-    // null case.
+    // In rare cases the operand is non-nullable but TFA doesn't optimize away
+    // the null check. If the operand is an unboxed type, the br_on_non_null
+    // would fail to compile.
+    if (!operandType.nullable) {
+      translateExpression(node.operand, operandType);
+      return nonNullOperandType;
+    }
+    w.Label nullCheckBlock = b.block(const [], [nonNullOperandType]);
+    translateExpression(node.operand, operandType);
+
+    // We lower a null check to a br_on_non_null, throwing a [TypeError] in
+    // the null case.
     b.br_on_non_null(nullCheckBlock);
     call(translator.stackTraceCurrent.reference);
-    call(errorProcedure.reference);
+    call(translator.throwNullCheckError.reference);
     b.unreachable();
     b.end();
     return nonNullOperandType;
