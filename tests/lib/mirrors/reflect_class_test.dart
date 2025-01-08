3a73f3282476e70d1b905fa830df7be3328815cd
Alexander Markov <alexmarkov@google.com>
2025-01-08 09:25:09 -0800

    [vm,mirrors] reflectClass: throw ArgumentError for function and record types
    TEST=tests/lib/mirrors/reflect_class_test.dart
    Fixes https://github.com/dart-lang/sdk/issues/59863
    
    Change-Id: Ic9ab394da9abeb7f793556a4827a496c372576a0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/403620
    Reviewed-by: Alexander Aprelev <aam@google.com>
    Commit-Queue: Alexander Markov <alexmarkov@google.com>

--- tests/lib/mirrors/reflect_class_test.dart
+++ tests/lib/mirrors/reflect_class_test.dart
@@ -2,18 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Formatting can break multitests, so don't format them.
-// dart format off
-
 import "dart:mirrors";
 
 import "package:expect/expect.dart";
 
 typedef void FooFunction(int a, double b);
+typedef Rec = (int,);
+typedef Void = void;
 
 main() {
   Expect.throwsArgumentError(() => reflectClass(dynamic));
-  Expect.throwsArgumentError(() => reflectClass(1)); //# 01: compile-time error
-  Expect.throwsArgumentError(() => reflectClass("string")); //# 02: compile-time error
+  Expect.throwsArgumentError(() => reflectClass(Void));
+  Expect.throwsArgumentError(() => reflectClass(Never));
   Expect.throwsArgumentError(() => reflectClass(FooFunction));
+  Expect.throwsArgumentError(() => reflectClass(Rec));
 }
