337e07fab6b415e96d3554bc23a8da69507d7745
Derek Xu <derekx@google.com>
2025-01-08 14:51:12 -0800

    [VM/Service] Use the resident frontend server for hot reload when it's available
    TEST=pkg/vm_service/test/reload_sources_with_resident_compiler_test.dart
    and pkg/vm_service/test/breakpoint_resolution_after_reloading_with_resident_compiler_test.dart
    
    CoreLibraryReviewExempt: This CL does not include any core library API
    changes, only VM Service implementation changes within
    sdk/lib/vmservice/.
    Change-Id: Ibc99cd37439ddd8aca97fa7e18a5112cbfc3b4cb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/401646
    Reviewed-by: Ben Konyi <bkonyi@google.com>

--- pkg/vm_service/test/reload_sources_with_resident_compiler_test.dart
+++ pkg/vm_service/test/reload_sources_with_resident_compiler_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -8,6 +8,7 @@ import 'reload_sources_test_common.dart' show reloadSourcesTests, testeeMain;
 void main([args = const <String>[]]) => runIsolateTests(
       args,
       reloadSourcesTests,
-      'reload_sources_test.dart',
+      'reload_sources_with_resident_compiler_test.dart',
       testeeConcurrent: testeeMain,
+      shouldTesteeBeLaunchedWithDartRunResident: true,
     );
