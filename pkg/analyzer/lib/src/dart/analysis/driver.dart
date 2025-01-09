2b1f956207c7ceac3723ff0b088503bffa4fe490
pq <pquitslund@google.com>
2025-01-09 08:03:08 -0800

    [element model] migrate `driver`
    Change-Id: Ie4d18ec4188cb3a5d2fbb1059f8988f4e4310ea8
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/403705
    Commit-Queue: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/analysis/driver.dart
+++ pkg/analyzer/lib/src/dart/analysis/driver.dart
@@ -597,7 +597,7 @@ class AnalysisDriver {
       var uriStr = uri.toString();
       var libraryResult = await getLibraryByUri(uriStr);
       if (libraryResult is LibraryElementResult) {
-        var libraryElement = libraryResult.element as LibraryElementImpl;
+        var libraryElement = libraryResult.element2 as LibraryElementImpl;
         bundleWriter.writeLibraryElement(libraryElement);
 
         packageBundleBuilder.addLibrary(
@@ -894,7 +894,7 @@ class AnalysisDriver {
     if (_pendingFileChanges.isEmpty) {
       var rootReference = libraryContext.elementFactory.rootReference;
       var reference = rootReference.getChild('$uriObj');
-      var element = reference.element;
+      var element = reference.element2;
       if (element is LibraryElementImpl) {
         return LibraryElementResultImpl(element);
       }
