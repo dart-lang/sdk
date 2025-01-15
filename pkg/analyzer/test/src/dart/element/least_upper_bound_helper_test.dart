f157bdcf4b6524768c791e6cba7f3faf9711d47c
pq <pquitslund@google.com>
2025-01-14 16:32:19 -0800

    [element model] remove unnecessary `analyzer_use_new_elements` ignore
    Change-Id: Id2a50c825ce94408a99f91602c3e9e05ef22b93c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/404342
    Auto-Submit: Phil Quitslund <pquitslund@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analyzer/test/src/dart/element/least_upper_bound_helper_test.dart
+++ pkg/analyzer/test/src/dart/element/least_upper_bound_helper_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: analyzer_use_new_elements
-
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/src/dart/element/element.dart';
 import 'package:analyzer/src/dart/element/least_upper_bound.dart';
