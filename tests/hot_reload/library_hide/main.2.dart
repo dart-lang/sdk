5ec1002eff41778ee46a91048f3b131106992215
Nicholas Shahan <nshahan@google.com>
2025-01-21 16:36:01 -0800

    [ddc] Port library show/hide hot reload tests
    Change-Id: Iaa7ef742f1204c73e96738cdba774f1263b32797
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/405061
    Commit-Queue: Nicholas Shahan <nshahan@google.com>
    Reviewed-by: Nate Biggs <natebiggs@google.com>
    Reviewed-by: Mark Zhou <markzipan@google.com>

---
+++ tests/hot_reload/library_hide/main.2.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:expect/expect.dart';
+import 'package:reload_test/reload_test_utils.dart';
+
+import 'lib1.dart';
+
+// Adapted from:
+// https://github.com/dart-lang/sdk/blob/9f465e5b6eab0dc3af96140189d4f0190e0ff925/runtime/vm/isolate_reload_test.cc#L1123
+
+void helper() {
+  Expect.equals('a', importedFunc());
+}
+
+Future<void> main() async {
+  helper();
+  await hotReload(expectRejection: true);
+  helper();
+  await hotReload();
+  helper();
+}
+
+/** DIFF **/
+/*
+@@ -5,7 +5,7 @@
+ import 'package:expect/expect.dart';
+ import 'package:reload_test/reload_test_utils.dart';
+ 
+-import 'lib1.dart' hide importedFunc;
++import 'lib1.dart';
+ 
+ // Adapted from:
+ // https://github.com/dart-lang/sdk/blob/9f465e5b6eab0dc3af96140189d4f0190e0ff925/runtime/vm/isolate_reload_test.cc#L1123
+*/
