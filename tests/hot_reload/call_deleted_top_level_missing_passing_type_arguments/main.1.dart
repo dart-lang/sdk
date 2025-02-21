a647f0eed0f98ae8896e0ace7411e56f90759efc
MarkZ <markzipan@google.com>
2025-02-20 16:29:25 -0800

    [tests] Porting top level deleted member tests to the hot reload framework
    Change-Id: Ic3561614b75b63556ea91b5a4dd75421cc5d3d5e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/411084
    Reviewed-by: Nate Biggs <natebiggs@google.com>
    Commit-Queue: Mark Zhou <markzipan@google.com>

--- tests/hot_reload/call_deleted_top_level_missing_passing_type_arguments/main.1.dart
+++ tests/hot_reload/call_deleted_top_level_missing_passing_type_arguments/main.1.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -6,11 +6,7 @@ import 'package:expect/expect.dart';
 import 'package:reload_test/reload_test_utils.dart';
 
 // Adapted from:
-// https://github.com/dart-lang/sdk/blob/bf2fba78e006ce4feac43e514c0b8f3ea9e9fbb8/runtime/vm/isolate_reload_test.cc#L2627
-
-deleted(newParameter) {
-  return 'hello';
-}
+// https://github.com/dart-lang/sdk/blob/13f5fc6b168d8b6e5843d17fb9ba77f1343a7dfe/runtime/vm/isolate_reload_test.cc#L2764
 
 var retained;
 
@@ -30,18 +26,15 @@ Future<void> main() async {
 
 /** DIFF **/
 /*
- // Adapted from:
- // https://github.com/dart-lang/sdk/blob/bf2fba78e006ce4feac43e514c0b8f3ea9e9fbb8/runtime/vm/isolate_reload_test.cc#L2627
- 
--deleted() {
-+deleted(newParameter) {
-   return 'hello';
- }
+ // https://github.com/dart-lang/sdk/blob/13f5fc6b168d8b6e5843d17fb9ba77f1343a7dfe/runtime/vm/isolate_reload_test.cc#L2764
  
  var retained;
+-deleted<A, B, C>() {
+-  return 'hello';
+-}
  
  helper() {
--  retained = () => deleted();
+-  retained = () => deleted<int, int, int>();
    return retained();
  }
  
--- tests/hot_reload/call_deleted_top_level_remove_type_arguments/main.0.dart
+++ tests/hot_reload/call_deleted_top_level_remove_type_arguments/main.0.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -6,16 +6,15 @@ import 'package:expect/expect.dart';
 import 'package:reload_test/reload_test_utils.dart';
 
 // Adapted from:
-// https://github.com/dart-lang/sdk/blob/bf2fba78e006ce4feac43e514c0b8f3ea9e9fbb8/runtime/vm/isolate_reload_test.cc#L2597
+// https://github.com/dart-lang/sdk/blob/13f5fc6b168d8b6e5843d17fb9ba77f1343a7dfe/runtime/vm/isolate_reload_test.cc#L2733
 
-deleted() {
+var retained;
+deleted<A, B, C>() {
   return 'hello';
 }
 
-var retained;
-
 helper() {
-  retained = () => deleted();
+  retained = () => deleted<int, int, int>();
   return retained();
 }
 
