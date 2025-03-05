4ee4acc93cbd4905b0115f5c5a8cc735ad0a8cd6
Sam Rawlins <srawlins@google.com>
2025-03-05 10:42:30 -0800

    DAS: Tidy docs and API in import_library.dart
    Change-Id: I2e18a9113ec96ec33fbf71e656ab0045496599c2
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/413721
    Reviewed-by: Phil Quitslund <pquitslund@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analyzer_plugin/lib/src/utilities/library.dart
+++ pkg/analyzer_plugin/lib/src/utilities/library.dart
@@ -2,11 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Checks whether importing the library with URI [library2] into the
-/// library with URI [library1] could be a relative import.
+/// Returns whether importing the library with URI [library2] into the library
+/// with URI [library1] could be a relative import.
 ///
-/// Both URIs must be package: URIs and belong to the same package for this to
-/// be true.
+/// In other words, returns whether each is a 'package:' URI referencing the
+/// same package name.
 bool canBeRelativeImport(Uri library1, Uri library2) {
   return library1.isScheme('package') &&
       library2.isScheme('package') &&
