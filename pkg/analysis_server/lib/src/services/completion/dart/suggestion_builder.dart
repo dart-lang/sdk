086576297b9003873b2c89518340603f43daccfa
Keerti Parthasarathy <keertip@google.com>
2025-03-05 07:40:15 -0800

    Cleanup after element model migration
    Change-Id: I4a0ac8ed2554d1f5ad11fb5093e5cd587b87cdd4
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/413523
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/lib/src/services/completion/dart/suggestion_builder.dart
+++ pkg/analysis_server/lib/src/services/completion/dart/suggestion_builder.dart
@@ -1068,35 +1068,6 @@ class SuggestionBuilder {
     }
   }
 
-  /// Add a suggestion for a top-level [function]. If a [kind] is provided it
-  /// will be used as the kind for the suggestion. If the function can only be
-  /// referenced using a prefix, then the [prefix] should be provided.
-  void suggestTopLevelFunction2(
-    TopLevelFunctionElement function, {
-    CompletionSuggestionKind kind = CompletionSuggestionKind.INVOCATION,
-    String? prefix,
-    int? relevance,
-  }) {
-    var completion = _getCompletionString2(function);
-    if (completion == null) return;
-    if (_couldMatch(completion, prefix)) {
-      relevance ??= relevanceComputer.computeTopLevelRelevance2(
-        function,
-        elementType: function.returnType,
-        isNotImportedLibrary: isNotImportedLibrary,
-      );
-      _addBuilder(
-        _createCompletionSuggestionBuilder2(
-          function,
-          kind: kind,
-          prefix: prefix,
-          relevance: relevance,
-          isNotImported: isNotImportedLibrary,
-        ),
-      );
-    }
-  }
-
   /// Add a suggestion for a top-level property [getter]. If the accessor can
   /// only be referenced using a prefix, then the [prefix] should be provided.
   void suggestTopLevelGetter(
@@ -1424,43 +1395,6 @@ class SuggestionBuilder {
     );
   }
 
-  /// Returns a [CompletionSuggestionBuilder] based on the [element], or `null`
-  /// if the element cannot be suggested.
-  ///
-  /// If the completion should be something different than the name of the
-  /// element, then the [completion] should be supplied. If a [kind] is
-  /// provided, then it will be used rather than the kind normally used for the
-  /// element. If a [prefix] is provided, then the element name (or completion)
-  /// will be prefixed. The [relevance] is the relevance of the suggestion.
-  CompletionSuggestionBuilder? _createCompletionSuggestionBuilder2(
-    Element2 element, {
-    String? completion,
-    required CompletionSuggestionKind kind,
-    required int relevance,
-    required bool isNotImported,
-    String? prefix,
-  }) {
-    completion ??= _getCompletionString2(element);
-    if (completion == null) {
-      return null;
-    }
-
-    if (prefix != null) {
-      completion = '$prefix.$completion';
-    }
-
-    return _CompletionSuggestionBuilderImpl(
-      orgElement: element,
-      suggestionBuilder: this,
-      kind: kind,
-      completion: completion,
-      relevance: relevance,
-      libraryUriStr: libraryUriStr,
-      requiredImports: requiredImports,
-      isNotImported: isNotImported,
-    );
-  }
-
   _ElementCompletionData _createElementCompletionData(Element2 element) {
     var documentation = _getDocumentation(element);
 
@@ -1549,14 +1483,6 @@ class SuggestionBuilder {
   }
 
   String? _getCompletionString(Element2 element) {
-    if (element is MethodElement2 && element.isOperator) {
-      return null;
-    }
-
-    return element.displayName;
-  }
-
-  String? _getCompletionString2(Element2 element) {
     if (element is MethodElement2 && element.isOperator) {
       return null;
     }
