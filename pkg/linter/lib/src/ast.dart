f702c93b1ae96ad5c92caa483f33960c5df62f6f
Jens Johansen <jensj@google.com>
2025-03-04 22:56:39 -0800

    [analysis server] Sort members more consistant
    I ran into an issue (in a child CL) where I couldn't satisfy the sort
    test because it kept changing the ordering of "p" and "P" because the
    sorting does `.toLowerCase` before comparing.
    In this CL - when the result of comparing with a `.toLowerCase` is 0 it
    compares without the `.toLowerCase` call.
    
    Change-Id: Ieed0f199cb61ea45d475c5271a127fbf4463edb7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/413420
    Commit-Queue: Jens Johansen <jensj@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/linter/lib/src/ast.dart
+++ pkg/linter/lib/src/ast.dart
@@ -173,13 +173,13 @@ bool isInPublicDir(CompilationUnit node, WorkspacePackage? package) {
       cuPath == linkHookFile;
 }
 
+/// Returns `true` if the given [id] is a Dart keyword.
+bool isKeyWord(String id) => Keyword.keywords.containsKey(id);
+
 /// Returns `true` if the keyword associated with the given [token] matches
 /// [keyword].
 bool isKeyword(Token token, Keyword keyword) =>
     token is KeywordToken && token.keyword == keyword;
-
-/// Returns `true` if the given [id] is a Dart keyword.
-bool isKeyWord(String id) => Keyword.keywords.containsKey(id);
 
 /// Returns `true` if the given [ClassMember] is a method.
 bool isMethod(ClassMember m) => m is MethodDeclaration;
