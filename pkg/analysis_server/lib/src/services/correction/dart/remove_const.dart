b222933af6aadfd89f585569c3b086d6ccbced99
FMorschel <git@fmorschel.dev>
2025-03-06 10:42:46 -0800

    [DAS] Adds remove const fix error to most common const errors
    Following the work done at https://dart-review.googlesource.com/c/sdk/+/276941. But trying to merge this time.
    
    Fixes: https://github.com/dart-lang/sdk/issues/49818
    Fixes: https://github.com/dart-lang/sdk/issues/53599
    Change-Id: I7ed6890ec3d526b855dd71b45e0493cbaf419d6d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/410862
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Auto-Submit: Felipe Morschel <git@fmorschel.dev>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/remove_const.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/remove_const.dart
@@ -4,6 +4,7 @@
 
 import 'package:analysis_server/src/services/correction/fix.dart';
 import 'package:analysis_server_plugin/edit/dart/correction_producer.dart';
+import 'package:analysis_server_plugin/src/correction/fix_generators.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/error/error.dart';
@@ -14,15 +15,15 @@ import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dar
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_dart.dart';
 import 'package:analyzer_plugin/utilities/fixes/fixes.dart';
 import 'package:analyzer_plugin/utilities/range_factory.dart';
+import 'package:collection/collection.dart';
 
 class RemoveConst extends _RemoveConst {
   RemoveConst({required super.context});
 
   @override
   CorrectionApplicability get applicability =>
-          // Not predictably the correct action.
-          CorrectionApplicability
-          .singleLocation;
+      // Not predictably the correct action.
+      CorrectionApplicability.singleLocation;
 
   @override
   FixKind get fixKind => DartFixKind.REMOVE_CONST;
@@ -42,9 +43,34 @@ class RemoveUnnecessaryConst extends _RemoveConst {
   FixKind get multiFixKind => DartFixKind.REMOVE_UNNECESSARY_CONST_MULTI;
 }
 
+class _ChildrenVisitor extends GeneralizingAstVisitor<AstNode?> {
+  AstNode? _selectedNode;
+
+  final int offset;
+  final int end;
+
+  _ChildrenVisitor(this.offset, this.end);
+
+  AstNode get selectedNode => _selectedNode!;
+
+  @override
+  AstNode? visitNode(AstNode node) {
+    if (node.offset > offset || node.end < end) {
+      return null;
+    }
+    if (node.offset <= offset && node.end >= end) {
+      var result = super.visitNode(node);
+      if (result != null) {
+        return _selectedNode ??= result;
+      }
+    }
+    return _selectedNode ??= node;
+  }
+}
+
 class _PushConstVisitor extends GeneralizingAstVisitor<void> {
   final DartFileEditBuilder builder;
-  final AstNode excluded;
+  final List<AstNode> excluded;
 
   _PushConstVisitor(this.builder, this.excluded);
 
@@ -56,13 +82,13 @@ class _PushConstVisitor extends GeneralizingAstVisitor<void> {
 
   @override
   void visitInstanceCreationExpression(InstanceCreationExpression node) {
-    if (node == excluded) {
-      // Don't speculate whether arguments can be const.
-    } else if (_containsExcluded(node)) {
+    if (_containsExcluded(node)) {
       node.argumentList.visitChildren(this);
+    } else if (excluded.any(node.contains)) {
+      // Don't speculate whether arguments can be const.
     } else {
       if (node.keyword == null) {
-        builder.addSimpleInsertion(node.offset, 'const ');
+        builder.addSimpleInsertion(node.offset, '${Keyword.CONST.lexeme} ');
       }
     }
   }
@@ -71,9 +97,6 @@ class _PushConstVisitor extends GeneralizingAstVisitor<void> {
   void visitNamedExpression(NamedExpression node) {
     node.expression.accept(this);
   }
-
-  @override
-  void visitNode(AstNode node) {}
 
   @override
   void visitSpreadElement(SpreadElement node) {
@@ -86,7 +109,7 @@ class _PushConstVisitor extends GeneralizingAstVisitor<void> {
       node.visitChildren(this);
     } else {
       if (node.constKeyword == null) {
-        builder.addSimpleInsertion(node.offset, 'const ');
+        builder.addSimpleInsertion(node.offset, '${Keyword.CONST.lexeme} ');
       }
     }
   }
@@ -102,15 +125,31 @@ class _PushConstVisitor extends GeneralizingAstVisitor<void> {
   }
 
   bool _containsExcluded(AstNode node) {
-    return excluded.withParents.contains(node);
+    return {for (var e in excluded) ...e.withParents}.contains(node);
   }
 }
 
 abstract class _RemoveConst extends ParsedCorrectionProducer {
   _RemoveConst({required super.context});
 
+  /// A map of all the error codes that this fix can be applied to and the
+  /// generators that can be used to apply the fix.
+  Map<ErrorCode, List<ProducerGenerator>> get _errorCodesWhereThisIsValid {
+    var constructors = [RemoveUnnecessaryConst.new, RemoveConst.new];
+    var nonLintMultiProducers = registeredFixGenerators.nonLintProducers;
+    return {
+      for (var MapEntry(:key, :value) in nonLintMultiProducers.entries)
+        if (value.containsAny(constructors)) key: value,
+    };
+  }
+
   @override
   Future<void> compute(ChangeBuilder builder) async {
+    var diagnostic = this.diagnostic;
+    if (diagnostic is! AnalysisError) {
+      return;
+    }
+
     switch (node) {
       case ClassDeclaration declaration:
         var first = declaration.firstTokenAfterCommentAndMetadata;
@@ -132,28 +171,44 @@ abstract class _RemoveConst extends ParsedCorrectionProducer {
         if (constantContext != null) {
           var constKeyword = constantContext.$2;
           if (constKeyword != null) {
+            var validErrors = _errorCodesWhereThisIsValid.keys.toList();
+            var validDiagnostics = unitResult.errors.whereErrorCodeIn(
+              validErrors,
+            );
             switch (constantContext.$1) {
               case InstanceCreationExpression contextNode:
-                await builder.addDartFileEdit(file, (builder) {
+                var (:constNodes, :nodesWithDiagnostic) = contextNode
+                    .argumentList
+                    .arguments
+                    .withErrorCodeIn(validDiagnostics);
+                await builder.addDartFileEdit(file, (builder) async {
                   _deleteToken(builder, constKeyword);
-                  contextNode.accept(_PushConstVisitor(builder, expression));
+                  contextNode.accept(
+                    _PushConstVisitor(builder, nodesWithDiagnostic),
+                  );
                 });
               case TypedLiteral contextNode:
-                await builder.addDartFileEdit(file, (builder) {
+                var (:constNodes, :nodesWithDiagnostic) = switch (contextNode) {
+                  ListLiteral list => list.elements,
+                  SetOrMapLiteral set => set.elements,
+                }.withErrorCodeIn(validDiagnostics);
+                await builder.addDartFileEdit(file, (builder) async {
                   _deleteToken(builder, constKeyword);
-                  contextNode.accept(_PushConstVisitor(builder, expression));
+                  contextNode.accept(
+                    _PushConstVisitor(builder, nodesWithDiagnostic),
+                  );
                 });
               case VariableDeclarationList contextNode:
+                var (:constNodes, :nodesWithDiagnostic) = contextNode.variables
+                    .withErrorCodeIn(validDiagnostics);
                 await builder.addDartFileEdit(file, (builder) {
-                  if (contextNode.type != null) {
-                    _deleteToken(builder, constKeyword);
-                  } else {
-                    builder.addSimpleReplacement(
-                      range.token(constKeyword),
-                      'var',
-                    );
-                  }
-                  contextNode.accept(_PushConstVisitor(builder, expression));
+                  builder.addSimpleReplacement(
+                    range.token(constKeyword),
+                    Keyword.FINAL.lexeme,
+                  );
+                  contextNode.accept(
+                    _PushConstVisitor(builder, nodesWithDiagnostic),
+                  );
                 });
             }
           }
@@ -186,5 +241,55 @@ abstract class _RemoveConst extends ParsedCorrectionProducer {
 
   void _deleteToken(DartFileEditBuilder builder, Token token) {
     builder.addDeletion(range.startStart(token, token.next!));
+  }
+}
+
+extension on AnalysisError {
+  int get end => offset + length;
+
+  bool isWithin(AstNode node) {
+    return node.offset <= offset && node.end >= end;
+  }
+}
+
+extension on AstNode {
+  bool contains(AstNode node) {
+    return offset <= node.offset && end >= node.end;
+  }
+}
+
+extension on List<AstNode> {
+  ({List<AstNode> nodesWithDiagnostic, List<AstNode> constNodes})
+  withErrorCodeIn(List<AnalysisError> diagnostics) {
+    if (diagnostics.isEmpty) {
+      return (constNodes: toList(), nodesWithDiagnostic: const []);
+    }
+    var invalidNodes = <AstNode>[];
+    var constNodes = <AstNode>[];
+    for (var node in this) {
+      // If no error spans this node, it is valid.
+      if (diagnostics.none((error) => error.isWithin(node))) {
+        constNodes.add(node);
+        continue;
+      }
+      var error = diagnostics.firstWhere((error) => error.isWithin(node));
+      var visitor = _ChildrenVisitor(error.offset, error.end);
+      node.accept(visitor);
+      invalidNodes.add(visitor.selectedNode);
+    }
+    return (nodesWithDiagnostic: invalidNodes, constNodes: constNodes);
+  }
+}
+
+extension on List<AnalysisError> {
+  List<AnalysisError> whereErrorCodeIn(List<ErrorCode> errors) =>
+      where((error) {
+        return errors.contains(error.errorCode);
+      }).toList();
+}
+
+extension<T> on Iterable<T> {
+  bool containsAny(Iterable<T> values) {
+    return values.any((v) => contains(v));
   }
 }
