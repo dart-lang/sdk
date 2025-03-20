acd16c6aa7d5614fa113f0b8698c003517e6ea9d
Tess Strickland <sstrickl@google.com>
2025-03-20 03:53:10 -0700

    [pkg/vm] Add tool for generating DartEngine shims from entry points.
    This is an initial cut of a tool for automatically creating shims for
    C/C++ programs using the entry point annotations in Dart code. The tool
    has two required arguments:
    
    * the .dill file containing the kernel representation of the Dart code
    * the base path of the header and implementation files to create
    
    With a base path of 'dir/name', the header file is created as
    'dir/name.h' and the implementation file is created as 'dir/name.cc'.
    In addition, 'dir/name.h' is used as a basis for creating a
    #ifndef/#define/#endif header guard around the header contents.
    
    
    The created shims are specific to a single package, either
    
    * a user-specified package, provided via '-p'/'--package', or
    * the package of the main method
    
    If the user does not specify a package and there is no main method
    in the .dill file, the tool fails.
    
    
    Each shim takes the following arguments in order when applicable:
    
    * the isolate in which to perform the requested operation,
    * the instantiated type of the generic class (for invoking, setting, or
      getting constructors, static methods, and static fields)
    * the instance (for invoking, getting, or setting instance methods and
      fields)
    * the type arguments (for retrieving nullable or non-nullable
      instantiated types of a generic class)
    * the arguments (for invoking, setting, or getting constructors,
      methods, and fields)
    
    
    The generated shims:
    
    * cache the package library, types for non-generic classes, and
      types for generic classes instantiated with default type arguments.
      The cached persistent handles are cleared if any of the methods
      are called with a different isolate from the one used to populate
      the cache.
    * automatically handle conversions between C int64_t <=> Dart int
      and C double <=> Dart double.
    
    
    By default, shims are not created for allocation or initializing
    uninitialized instances. To create such shims, use the '-u' command
    line argument.
    
    
    Currently, shims are not created for methods that take optional
    or named arguments. To report an error if a shim cannot be created
    for any entry points, use the '-e' command line argument.
    
    TEST=tests/standalone/embedder_samples_test
    
    Change-Id: Ibdf3b52d900ba98038528178485f295c5868ac9d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/410500
    Reviewed-by: Ivan Inozemtsev <iinozemtsev@google.com>
    Commit-Queue: Tess Strickland <sstrickl@google.com>

---
+++ pkg/vm/lib/embedder/shims.dart
@@ -0,0 +1,662 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:kernel/ast.dart';
+import 'package:kernel/core_types.dart' show CoreTypes;
+
+void _convertDartTypeToC(
+  StringBuffer buffer,
+  CoreTypes? coreTypes,
+  DartType type,
+) {
+  if (coreTypes == null) {
+    buffer.write('Dart_Handle');
+  } else if (type is VoidType) {
+    buffer.write('void');
+  } else if (type == coreTypes.intNonNullableRawType) {
+    buffer.write('int64_t');
+  } else if (type == coreTypes.doubleNonNullableRawType) {
+    buffer.write('double');
+  } else {
+    buffer.write('Dart_Handle');
+  }
+}
+
+void _convertCValueToDart(
+  StringBuffer buffer,
+  CoreTypes coreTypes,
+  DartType type,
+  void Function(StringBuffer) bodyWriter,
+) {
+  assert(type is! VoidType);
+  if (type == coreTypes.intNonNullableRawType) {
+    buffer.write('Dart_NewInteger(');
+    bodyWriter(buffer);
+    buffer.write(')');
+  } else if (type == coreTypes.doubleNonNullableRawType) {
+    buffer.write('Dart_NewDouble(');
+    bodyWriter(buffer);
+    buffer.write(')');
+  } else {
+    bodyWriter(buffer);
+  }
+}
+
+void _convertDartValueToC(
+  StringBuffer buffer,
+  CoreTypes coreTypes,
+  DartType type,
+  void Function(StringBuffer) bodyWriter,
+) {
+  if (type is VoidType) {
+    buffer.write('CheckError(');
+    bodyWriter(buffer);
+    buffer.write(')');
+  } else if (type == coreTypes.intNonNullableRawType) {
+    buffer.write('IntFromHandle(');
+    bodyWriter(buffer);
+    buffer.write(')');
+  } else if (type == coreTypes.doubleNonNullableRawType) {
+    buffer.write('DoubleFromHandle(');
+    bodyWriter(buffer);
+    buffer.write(')');
+  } else {
+    bodyWriter(buffer);
+  }
+}
+
+class EntryPointShimParameter {
+  final String name;
+  final DartType type;
+
+  EntryPointShimParameter._(this.name, {DartType? type})
+    : type = type ?? DynamicType();
+
+  void writeAsParameter(StringBuffer buffer, [CoreTypes? coreTypes]) {
+    assert(type is! VoidType);
+    _convertDartTypeToC(buffer, coreTypes, type);
+    buffer.write(' $name');
+  }
+
+  void writeAsArgument(StringBuffer buffer, [CoreTypes? coreTypes]) {
+    if (coreTypes != null) {
+      _convertCValueToDart(buffer, coreTypes, type, (b) => b.write(name));
+    } else {
+      buffer.write(name);
+    }
+  }
+
+  @override
+  String toString() {
+    final b = StringBuffer();
+    writeAsArgument(b);
+    return b.toString();
+  }
+}
+
+class EntryPointShimTypeParameter extends EntryPointShimParameter {
+  static int counter = 0;
+  static String get gensym => 'tp${counter++}';
+
+  EntryPointShimTypeParameter._(String name) : super._('t_' + name);
+
+  factory EntryPointShimTypeParameter.fromTypeParameter(TypeParameter p) {
+    return EntryPointShimTypeParameter._(p.name ?? gensym);
+  }
+}
+
+class EntryPointShimValueParameter extends EntryPointShimParameter {
+  static int counter = 0;
+  static String get gensym => '${counter++}';
+
+  EntryPointShimValueParameter._(String name, {super.type})
+    : super._('v_' + name);
+
+  factory EntryPointShimValueParameter.fromDeclaration(VariableDeclaration v) {
+    return EntryPointShimValueParameter._(v.name ?? gensym, type: v.type);
+  }
+}
+
+abstract class EntryPointFunctionShim {
+  final NamedNode node;
+  final CoreTypes coreTypes;
+  final List<EntryPointShimParameter> parameters;
+  // The target may be the instance for instance members or a type if the
+  // enclosing class is generic and thus needs instantiation.
+  final EntryPointShimParameter? target;
+
+  static EntryPointShimParameter? targetOf(
+    NamedNode node,
+    CoreTypes coreTypes,
+  ) {
+    if (node is! Member) return null;
+
+    if (node.isInstanceMember) {
+      // For instance methods, the instance contains the instantiation of the
+      // class type parameters.
+      return EntryPointShimValueParameter._(
+        "_inst",
+        type: node.enclosingClass!.getThisType(
+          coreTypes,
+          Nullability.nonNullable,
+        ),
+      );
+    }
+    final cls = node.enclosingClass;
+    if (cls != null && cls.typeParameters.isNotEmpty) {
+      // Need to handle type parameters on the enclosing class. Defer this
+      // by requiring the user provide the appropriate instantiated
+      // non-nullable type.
+      return EntryPointShimTypeParameter._("_T");
+    }
+    return null;
+  }
+
+  static List<EntryPointShimParameter> parametersOf(NamedNode node) {
+    if (node is Procedure || node is Constructor) {
+      final function = (node as dynamic).function as FunctionNode;
+      assert(function.namedParameters.isEmpty);
+      assert(
+        function.requiredParameterCount == function.positionalParameters.length,
+      );
+      return function.positionalParameters
+          .map(EntryPointShimValueParameter.fromDeclaration)
+          .toList();
+    }
+    if (node is Class) {
+      return node.typeParameters
+          .map(EntryPointShimTypeParameter.fromTypeParameter)
+          .toList();
+    }
+    return const [];
+  }
+
+  EntryPointFunctionShim._({
+    required this.node,
+    required this.coreTypes,
+    List<EntryPointShimParameter>? parameters,
+    EntryPointShimParameter? target,
+  }) : parameters = parameters ?? parametersOf(node),
+       target = target ?? targetOf(node, coreTypes);
+
+  static final _cachedBaseNames = <Reference, String>{};
+  static final _cachedPrefixedNames = <Reference, String>{};
+
+  static String _baseName(NamedNode node) {
+    if (!_cachedBaseNames.containsKey(node.reference)) {
+      var name = (node as dynamic).name;
+      if (name is Name) {
+        assert(name.isPrivate == name.text.startsWith('_'));
+        name = name.text;
+      }
+      _cachedBaseNames[node.reference] = name as String;
+    }
+    return _cachedBaseNames[node.reference]!;
+  }
+
+  static String _prefixedName(NamedNode node) {
+    if (!_cachedPrefixedNames.containsKey(node.reference)) {
+      String prefixedName = _baseName(node);
+      if (node is Member) {
+        final cls = node.enclosingClass;
+        if (cls != null) {
+          final prefix = _prefixedName(cls);
+          if (prefixedName.isEmpty || prefixedName == '_') {
+            prefixedName = '$prefix$prefixedName';
+          } else {
+            prefixedName = '${prefix}_$prefixedName';
+          }
+        }
+      }
+      _cachedPrefixedNames[node.reference] = prefixedName;
+    }
+    return _cachedPrefixedNames[node.reference]!;
+  }
+
+  String get baseName => _baseName(node);
+  String get prefixedName => _prefixedName(node);
+
+  String get functionName;
+  DartType get returnType;
+
+  void _writeTarget(StringBuffer buffer) {
+    if (target != null) {
+      target!.writeAsArgument(buffer, coreTypes);
+      return;
+    }
+    final n = node;
+    Class? cls;
+    if (n is Member) {
+      cls = n.enclosingClass;
+      if (cls != null && cls.typeParameters.isNotEmpty) {
+        // The shim should have had a target parameter created.
+        throw "Unexpected attempt to write target";
+      }
+    } else if (n is Class) {
+      cls = n;
+    }
+    if (cls == null) {
+      buffer.write('PackageState::instance()->PackageLibrary()');
+    } else if (cls.typeParameters.isEmpty) {
+      buffer.write(
+        'PackageState::instance()->TypeWithDefaults("${_baseName(cls)}")',
+      );
+    } else {
+      assert(node is Class);
+      assert(parameters.isNotEmpty);
+      buffer.write(
+        'Dart_GetNonNullableType(PackageState::instance()->PackageLibrary(), Dart_NewStringFromCString("$baseName"), ',
+      );
+      _writeArgumentsListCountAndPointer(buffer);
+      buffer.write(')');
+    }
+  }
+
+  void _writeArguments(StringBuffer buffer) {
+    bool first = true;
+    for (final p in parameters) {
+      if (!first) buffer.write(', ');
+      p.writeAsArgument(buffer, coreTypes);
+      first = false;
+    }
+  }
+
+  void _writeArgumentsListCountAndPointer(StringBuffer buffer) {
+    buffer.write('${parameters.length}, ');
+    if (parameters.isEmpty) {
+      // No arguments list created.
+      buffer.write('nullptr');
+    } else {
+      buffer.write('parameter_list.data()');
+    }
+  }
+
+  void _writeReturnType(StringBuffer buffer) {
+    _convertDartTypeToC(buffer, coreTypes, returnType);
+  }
+
+  String get isolateName => 'dart_isolate';
+
+  void _writeFunctionHeader(StringBuffer buffer) {
+    buffer.write('PACKAGE_EXPORT ');
+    _writeReturnType(buffer);
+    buffer.write(' $functionName(Dart_Isolate $isolateName');
+    if (target != null) {
+      buffer.write(', ');
+      target!.writeAsParameter(buffer, coreTypes);
+    }
+    for (final p in parameters) {
+      buffer.write(', ');
+      p.writeAsParameter(buffer, coreTypes);
+    }
+    buffer.write(')');
+  }
+
+  void writeDeclaration(StringBuffer buffer) {
+    _writeFunctionHeader(buffer);
+    buffer.writeln(';');
+  }
+
+  void _writeReturnBody(StringBuffer buffer);
+
+  void writeDefinition(StringBuffer buffer) {
+    _writeFunctionHeader(buffer);
+    buffer.writeln(''' {
+  IsolateScope isolate_scope($isolateName);
+  DartScope scope;''');
+    if (parameters.isNotEmpty) {
+      buffer.write('''
+  std::vector parameter_list{''');
+      _writeArguments(buffer);
+      buffer.writeln('};');
+    }
+    buffer.write('  ');
+    if (returnType is! VoidType) {
+      buffer.write('return ');
+    }
+    _convertDartValueToC(buffer, coreTypes, returnType, _writeReturnBody);
+    buffer
+      ..writeln(';')
+      ..writeln('}')
+      ..writeln('');
+  }
+
+  void write(StringBuffer declaration, StringBuffer definition) {
+    writeDeclaration(declaration);
+    writeDefinition(definition);
+  }
+
+  @override
+  String toString() {
+    final b = StringBuffer();
+    writeDefinition(b);
+    return b.toString();
+  }
+}
+
+class EntryPointGetterShim extends EntryPointFunctionShim {
+  @override
+  final Member node;
+
+  EntryPointGetterShim._({required this.node, required super.coreTypes})
+    : super._(node: node, parameters: const []);
+
+  factory EntryPointGetterShim.fromMember(Member node, CoreTypes coreTypes) {
+    assert(node is Field || (node is Procedure && node.isGetter));
+    return EntryPointGetterShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Get_$prefixedName';
+
+  @override
+  DartType get returnType => node.getterType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_GetField(');
+    _writeTarget(buffer);
+    buffer.write(', Dart_NewStringFromCString("$baseName"))');
+  }
+}
+
+class EntryPointClosureShim extends EntryPointGetterShim {
+  @override
+  final Procedure node;
+
+  EntryPointClosureShim._({required this.node, required super.coreTypes})
+    : super._(node: node);
+
+  factory EntryPointClosureShim.fromProcedure(
+    Procedure node,
+    CoreTypes coreTypes,
+  ) {
+    assert(!node.isGetter && !node.isFactory);
+    return EntryPointClosureShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => '${super.functionName}_Closure';
+}
+
+class EntryPointSetterShim extends EntryPointFunctionShim {
+  @override
+  final Member node;
+
+  EntryPointSetterShim._({required this.node, required super.coreTypes})
+    : super._(
+        node: node,
+        parameters: [
+          EntryPointShimValueParameter._("value", type: node.setterType),
+        ],
+      );
+
+  factory EntryPointSetterShim.fromMember(Member node, CoreTypes coreTypes) {
+    assert(node is Field || (node is Procedure && node.isSetter));
+    return EntryPointSetterShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Set_$prefixedName';
+
+  @override
+  DartType get returnType => VoidType();
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_SetField(');
+    _writeTarget(buffer);
+    buffer.write(', Dart_NewStringFromCString("$baseName"), ');
+    _writeArguments(buffer);
+    buffer.write(')');
+  }
+}
+
+class EntryPointClassShim extends EntryPointFunctionShim {
+  @override
+  final Class node;
+
+  EntryPointClassShim._({required this.node, required super.coreTypes})
+    : super._(node: node, parameters: const []);
+
+  factory EntryPointClassShim.fromClass(Class node, CoreTypes coreTypes) {
+    return EntryPointClassShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Get_${prefixedName}_Class';
+
+  @override
+  DartType get returnType => coreTypes.typeNonNullableRawType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) =>
+  // Don't use _writeTarget, since we want the default type arguments for
+  // generic classes.
+  buffer.write('PackageState::instance()->TypeWithDefaults("$baseName")');
+}
+
+class EntryPointNullableTypeShim extends EntryPointFunctionShim {
+  @override
+  final Class node;
+
+  EntryPointNullableTypeShim._({required this.node, required super.coreTypes})
+    : super._(node: node);
+
+  factory EntryPointNullableTypeShim.fromClass(
+    Class node,
+    CoreTypes coreTypes,
+  ) {
+    return EntryPointNullableTypeShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Get_${prefixedName}_NullableType';
+
+  @override
+  DartType get returnType => coreTypes.typeNonNullableRawType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_TypeToNullableType(');
+    _writeTarget(buffer);
+    buffer.write(')');
+  }
+}
+
+class EntryPointNonNullableTypeShim extends EntryPointFunctionShim {
+  @override
+  final Class node;
+
+  EntryPointNonNullableTypeShim._({
+    required this.node,
+    required super.coreTypes,
+  }) : super._(node: node);
+
+  factory EntryPointNonNullableTypeShim.fromClass(
+    Class node,
+    CoreTypes coreTypes,
+  ) {
+    return EntryPointNonNullableTypeShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Get_${prefixedName}_NonNullableType';
+
+  @override
+  DartType get returnType => coreTypes.typeNonNullableRawType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) => _writeTarget(buffer);
+}
+
+class EntryPointAllocationShim extends EntryPointFunctionShim {
+  @override
+  final Class node;
+
+  EntryPointAllocationShim._({required this.node, required super.coreTypes})
+    : super._(node: node);
+
+  factory EntryPointAllocationShim.fromClass(Class node, CoreTypes coreTypes) {
+    return EntryPointAllocationShim._(node: node, coreTypes: coreTypes);
+  }
+
+  @override
+  String get functionName => 'Allocate_${prefixedName}';
+
+  @override
+  DartType get returnType => DynamicType(); // to avoid C conversions
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_Allocate(');
+    _writeTarget(buffer);
+    buffer.write(')');
+  }
+}
+
+class EntryPointNewShim extends EntryPointFunctionShim {
+  @override
+  final Constructor node;
+
+  EntryPointNewShim._({required this.node, required super.coreTypes})
+    : super._(node: node);
+
+  factory EntryPointNewShim.fromConstructor(
+    Constructor node,
+    CoreTypes coreTypes,
+  ) {
+    return EntryPointNewShim._(node: node, coreTypes: coreTypes);
+  }
+
+  static FunctionType functionType(Constructor node) =>
+      node.function.computeThisFunctionType(Nullability.nonNullable);
+
+  @override
+  String get functionName => 'New_$prefixedName';
+
+  @override
+  DartType get returnType => node.function.returnType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_New(');
+    _writeTarget(buffer);
+    buffer.write(', Dart_NewStringFromCString("$baseName"), ');
+    _writeArgumentsListCountAndPointer(buffer);
+    buffer.write(')');
+  }
+}
+
+class EntryPointInitializationShim extends EntryPointFunctionShim {
+  @override
+  final Constructor node;
+
+  EntryPointInitializationShim._({required this.node, required super.coreTypes})
+    : super._(
+        node: node,
+        target: EntryPointShimValueParameter._(
+          "_instance",
+          type: node.enclosingClass.getThisType(
+            coreTypes,
+            Nullability.nonNullable,
+          ),
+        ),
+      );
+
+  factory EntryPointInitializationShim.fromConstructor(
+    Constructor node,
+    CoreTypes coreTypes,
+  ) {
+    return EntryPointInitializationShim._(node: node, coreTypes: coreTypes);
+  }
+
+  static FunctionType functionType(Constructor node) {
+    final type = node.function.computeThisFunctionType(Nullability.nonNullable);
+    return FunctionType(
+      type.positionalParameters,
+      VoidType(),
+      type.declaredNullability,
+      namedParameters: type.namedParameters,
+      typeParameters: type.typeParameters,
+      requiredParameterCount: type.requiredParameterCount,
+    );
+  }
+
+  @override
+  String get functionName => 'Initialize_$prefixedName';
+
+  @override
+  DartType get returnType => VoidType();
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_InvokeConstructor(');
+    _writeTarget(buffer);
+    buffer.write(', Dart_NewStringFromCString("$baseName"), ');
+    _writeArgumentsListCountAndPointer(buffer);
+    buffer.write(')');
+  }
+}
+
+class EntryPointCallShim extends EntryPointFunctionShim {
+  @override
+  final Member node;
+
+  EntryPointCallShim._({
+    required this.node,
+    required super.coreTypes,
+    super.parameters,
+  }) : super._(node: node);
+
+  factory EntryPointCallShim.fromMember(Member node, CoreTypes coreTypes) {
+    final FunctionType type = functionType(node);
+    assert(type.typeParameters.isEmpty);
+    assert(type.namedParameters.isEmpty);
+    assert(
+      type.requiredPositionalParameterCount == type.positionalParameters.length,
+    );
+    List<EntryPointShimParameter>? parameters;
+    if (_isGetter(node)) {
+      parameters = <EntryPointShimParameter>[];
+      for (int i = 0; i < type.positionalParameters.length; ++i) {
+        parameters.add(
+          EntryPointShimValueParameter._(
+            i.toString(),
+            type: type.positionalParameters[i],
+          ),
+        );
+      }
+    }
+
+    return EntryPointCallShim._(
+      node: node,
+      coreTypes: coreTypes,
+      parameters: parameters,
+    );
+  }
+
+  static bool _isGetter(Member node) =>
+      node is Field || (node is Procedure && node.isGetter);
+
+  static FunctionType functionType(Member node) =>
+      _isGetter(node)
+          ? node.getterType as FunctionType
+          : (node as Procedure).computeSignatureOrFunctionType();
+
+  @override
+  String get functionName => 'Call_$prefixedName';
+
+  @override
+  DartType get returnType => functionType(node).returnType;
+
+  @override
+  void _writeReturnBody(StringBuffer buffer) {
+    buffer.write('Dart_Invoke(');
+    _writeTarget(buffer);
+    buffer.write(', Dart_NewStringFromCString("$baseName"), ');
+    _writeArgumentsListCountAndPointer(buffer);
+    buffer.write(')');
+  }
+}
---
+++ samples/embedder/timer.cc
@@ -0,0 +1,201 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+//
+// Generated with:
+//   dart pkg/vm/tools/generate_entry_point_shims.dart \
+//       out/ReleaseX64/gen/samples/embedder/timer_aot.dart.dill \
+//       samples/embedder/timer
+
+#include "timer.h"
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+namespace {
+
+Dart_Handle CheckError(Dart_Handle handle, std::string_view context = "") {
+  if (Dart_IsError(handle)) {
+    std::cerr << "Error " << context << ": " << Dart_GetError(handle)
+              << std::endl;
+    std::exit(1);
+  }
+  return handle;
+}
+
+int64_t IntFromHandle(Dart_Handle handle) {
+  CheckError(handle, "IntFromHandle received an error");
+
+  if (!Dart_IsInteger(handle)) {
+    std::cerr << "IntFromHandle handle is not an int" << std::endl;
+    std::exit(1);
+  }
+
+  int64_t result;
+  Dart_Handle to_int64_result = Dart_IntegerToInt64(handle, &result);
+  CheckError(to_int64_result, "Dart_IntegerToInt64");
+  return result;
+}
+
+double DoubleFromHandle(Dart_Handle handle) {
+  CheckError(handle, "DoubleFromHandle received an error");
+
+  if (!Dart_IsDouble(handle)) {
+    std::cerr << "DoubleFromHandle handle is not an double" << std::endl;
+    std::exit(1);
+  }
+
+  double result;
+  Dart_Handle to_double_result = Dart_DoubleValue(handle, &result);
+  CheckError(to_double_result, "Dart_DoubleValue");
+  return result;
+}
+
+class PackageState {
+ public:
+  static PackageState* instance() {
+    static PackageState* instance = new PackageState();
+    return instance;
+  }
+
+  Dart_Handle PackageLibrary() {
+    ValidateCurrentIsolate();
+    Dart_Handle result = package_library_;
+    if (package_library_ == nullptr) {
+      result = Dart_RootLibrary();
+      if (!Dart_IsError(result)) {
+        package_library_ = Dart_NewPersistentHandle(result);
+      }
+    }
+    return result;
+  }
+
+  Dart_Handle TypeWithDefaults(std::string name) {
+    ValidateCurrentIsolate();
+    auto search = default_types_.find(name);
+    Dart_Handle result;
+    if (search == default_types_.end()) {
+      Dart_Handle lib = PackageLibrary();
+      result = Dart_GetClass(lib, Dart_NewStringFromCString(name.c_str()));
+      if (!Dart_IsError(result)) {
+        Dart_PersistentHandle handle = Dart_NewPersistentHandle(result);
+        default_types_.emplace(std::make_pair(name, handle));
+      }
+    } else {
+      result = search->second;
+    }
+    return result;
+  }
+
+ private:
+  PackageState() : default_types_() {}
+
+  void ValidateCurrentIsolate() {
+    Dart_Isolate current = Dart_CurrentIsolate();
+    if (isolate_ != current) {
+      isolate_ = current;
+      if (package_library_ != nullptr) {
+        Dart_DeletePersistentHandle(package_library_);
+        package_library_ = nullptr;
+      }
+      for (const auto& [str, handle] : default_types_) {
+        Dart_DeletePersistentHandle(handle);
+      }
+      default_types_.clear();
+    }
+  }
+
+  // Ensure the helper methods defined above won't trigger unused
+  // function warnings.
+  template <typename T>
+  static inline void USE(T&&) {}
+
+  void UseHelperFunctions() {
+    USE(IntFromHandle);
+    USE(DoubleFromHandle);
+  }
+
+  Dart_Isolate isolate_ = nullptr;
+  Dart_PersistentHandle package_library_ = nullptr;
+  std::unordered_map<std::string, Dart_PersistentHandle> default_types_;
+};
+
+class DartScope {
+ public:
+  DartScope() { Dart_EnterScope(); }
+  virtual ~DartScope() { Dart_ExitScope(); }
+
+ private:
+  DartScope(const DartScope&) = delete;
+  void operator=(const DartScope&) = delete;
+};
+
+class IsolateScope {
+ public:
+  explicit IsolateScope(Dart_Isolate isolate) {
+    DartEngine_AcquireIsolate(isolate);
+  }
+  virtual ~IsolateScope() { DartEngine_ReleaseIsolate(); }
+
+ private:
+  IsolateScope(const IsolateScope&) = delete;
+  void operator=(const IsolateScope&) = delete;
+};
+
+}  // namespace
+
+#ifdef __cplusplus
+#define PACKAGE_EXTERN_C extern "C"
+#else
+#define PACKAGE_EXTERN_C extern
+#endif
+
+#if defined(__CYGWIN__)
+#error Tool chain and platform not supported.
+#elif defined(_WIN32)
+#define PACKAGE_EXPORT PACKAGE_EXTERN_C __declspec(dllexport)
+#else
+#if __GNUC__ >= 4
+#define PACKAGE_EXPORT                                                         \
+  PACKAGE_EXTERN_C __attribute__((visibility("default"))) __attribute((used))
+#else
+#error Tool chain not supported.
+#endif
+#endif
+
+PACKAGE_EXPORT void Call_startTimer(Dart_Isolate dart_isolate,
+                                    int64_t v_millis) {
+  IsolateScope isolate_scope(dart_isolate);
+  DartScope scope;
+  std::vector parameter_list{Dart_NewInteger(v_millis)};
+  CheckError(Dart_Invoke(PackageState::instance()->PackageLibrary(),
+                         Dart_NewStringFromCString("startTimer"), 1,
+                         parameter_list.data()));
+}
+
+PACKAGE_EXPORT void Call_stopTimer(Dart_Isolate dart_isolate) {
+  IsolateScope isolate_scope(dart_isolate);
+  DartScope scope;
+  CheckError(Dart_Invoke(PackageState::instance()->PackageLibrary(),
+                         Dart_NewStringFromCString("stopTimer"), 0, nullptr));
+}
+
+PACKAGE_EXPORT void Call_resetTimer(Dart_Isolate dart_isolate) {
+  IsolateScope isolate_scope(dart_isolate);
+  DartScope scope;
+  CheckError(Dart_Invoke(PackageState::instance()->PackageLibrary(),
+                         Dart_NewStringFromCString("resetTimer"), 0, nullptr));
+}
+
+PACKAGE_EXPORT int64_t Get_ticks(Dart_Isolate dart_isolate) {
+  IsolateScope isolate_scope(dart_isolate);
+  DartScope scope;
+  return IntFromHandle(Dart_GetField(PackageState::instance()->PackageLibrary(),
+                                     Dart_NewStringFromCString("ticks")));
+}
+
+#undef PACKAGE_EXPORT
+#undef PACKAGE_EXTERN_C
