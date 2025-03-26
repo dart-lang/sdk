c5133720b6ec9b56f03ab91bbf36a30028d12804
Kallen Tu <kallentu@google.com>
2025-03-26 10:19:51 -0700

    [tests] Dot Shorthands: .call() tests.
    Adding tests for calling .call() on the dot shorthand getters/fields.
    
    Bug: https://github.com/dart-lang/sdk/issues/57038
    Change-Id: Ibdacd2bb9feba617f96996dd467328dbdb55c7b6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/417904
    Reviewed-by: Erik Ernst <eernst@google.com>

---
+++ tests/language/dot_shorthands/simple/call_error_test.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// `id()` calls the `call()` method of the type of `id` if `id` is a getter or
+// field. It's a compile-time error if that `call` method does not exist.
+
+// SharedOptions=--enable-experiment=dot-shorthands
+
+class C {
+  const C();
+  static C get id1 => const C();
+  static C id2 = const C();
+}
+
+void main() {
+  C c1 = .id1();
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  C c2 = .id2();
+  // ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
