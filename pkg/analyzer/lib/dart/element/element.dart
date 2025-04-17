76606cad270f390f4166031f3e4ba0578037bd62
Konstantin Shcheglov <scheglov@google.com>
2025-04-16 18:00:20 -0700

    Elements. Restore InstanceElement.augmented
    This API was removed in 7.4.0 under the assumption that its removal would not be a breaking change, because it was marked as `@experimental`. But it turns out that some clients had been published to pub that relied on it (for example `drift_dev`).
    
    To avoid breakages in those packages, the API has been restored (by pulling `AugmentedXyz` APIs from the published version `7.3.0`, and re-implementing on top of new `XyzElement2`). However the restored API is still slated for removal in analyzer 8.0.0, so it's been marked as `@deprecated`. Clients relying on this API should migrate away from it as soon as possible.
    
    Bug: https://github.com/dart-lang/sdk/issues/60545
    Bug: https://github.com/dart-lang/sdk/issues/60553
    
    Change-Id: If12c1fa37cafa63d0eef121a39350ce6d41ab68f
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/422846
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/dart/element/element.dart
+++ pkg/analyzer/lib/dart/element/element.dart
@@ -60,6 +60,186 @@ import 'package:analyzer/src/task/api/model.dart' show AnalysisTarget;
 import 'package:meta/meta.dart';
 import 'package:pub_semver/pub_semver.dart';
 
+/// The result of applying augmentations to a [ClassElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedClassElement implements AugmentedInterfaceElement {
+  @override
+  ClassElement get firstFragment;
+}
+
+/// The result of applying augmentations to an [EnumElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedEnumElement implements AugmentedInterfaceElement {
+  /// The enum constants declared in this element.
+  List<FieldElement> get constants;
+
+  @override
+  EnumElement get firstFragment;
+}
+
+/// The result of applying augmentations to an [ExtensionElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedExtensionElement implements AugmentedInstanceElement {
+  /// The type that is extended by this extension.
+  DartType get extendedType;
+}
+
+/// The result of applying augmentations to an [ExtensionTypeElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedExtensionTypeElement
+    implements AugmentedInterfaceElement {
+  @override
+  ExtensionTypeElement get firstFragment;
+
+  /// The primary constructor of this extension.
+  ConstructorElement get primaryConstructor;
+
+  /// The representation of this extension.
+  FieldElement get representation;
+
+  /// The extension type erasure, obtained by recursively replacing every
+  /// subterm which is an extension type by the corresponding representation
+  /// type.
+  DartType get typeErasure;
+}
+
+/// The result of applying augmentations to a [InstanceElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedInstanceElement {
+  /// The accessors (getters and setters) declared in this element.
+  ///
+  /// [PropertyAccessorElement]s replace corresponding elements,
+  /// other [PropertyAccessorElement]s are appended.
+  List<PropertyAccessorElement> get accessors;
+
+  /// The fields declared in this element.
+  ///
+  /// `FieldAugmentationElement`s replace corresponding elements, other
+  /// [FieldElement]s are appended.
+  List<FieldElement> get fields;
+
+  /// The declaration (not augmentation) that owns this result.
+  InstanceElement get firstFragment;
+
+  /// The metadata associated with this element.
+  ///
+  /// This is a union of annotations associated with the class declaration and
+  /// all its augmentations.
+  List<ElementAnnotation> get metadata;
+
+  /// The methods declared in this element.
+  ///
+  /// `MethodAugmentationElement`s replace corresponding elements, other
+  /// [MethodElement]s are appended.
+  List<MethodElement> get methods;
+
+  /// The type of `this` expression.
+  DartType get thisType;
+
+  /// Returns the field from [fields] that has the given [name].
+  FieldElement? getField(String name);
+
+  /// Returns the getter from [accessors] that has the given [name].
+  PropertyAccessorElement? getGetter(String name);
+
+  /// Returns the method from [methods] that has the given [name].
+  MethodElement? getMethod(String name);
+
+  /// Returns the setter from [accessors] that has the given [name].
+  PropertyAccessorElement? getSetter(String name);
+
+  /// Returns the element representing the getter that results from looking up
+  /// the given [name] in this class with respect to the given [library],
+  /// or `null` if the look up fails.
+  ///
+  /// The behavior of this method is defined by the Dart Language Specification
+  /// in section 17.18 Lookup.
+  PropertyAccessorElement? lookUpGetter({
+    required String name,
+    required LibraryElement library,
+  });
+
+  /// Returns the element representing the method that results from looking up
+  /// the given [name] in this class with respect to the given [library],
+  /// or `null` if the look up fails.
+  ///
+  /// The behavior of this method is defined by the Dart Language Specification
+  /// in section 17.18 Lookup.
+  MethodElement? lookUpMethod({
+    required String name,
+    required LibraryElement library,
+  });
+
+  /// Returns the element representing the setter that results from looking up
+  /// the given [name] in this class with respect to the given [library],
+  /// or `null` if the look up fails.
+  ///
+  /// The behavior of this method is defined by the Dart Language Specification
+  /// in section 17.18 Lookup.
+  PropertyAccessorElement? lookUpSetter({
+    required String name,
+    required LibraryElement library,
+  });
+}
+
+/// The result of applying augmentations to a [InterfaceElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedInterfaceElement implements AugmentedInstanceElement {
+  /// The constructors declared in this element.
+  ///
+  /// `ConstructorAugmentationElement`s replace corresponding elements,
+  /// other [ConstructorElement]s are appended.
+  List<ConstructorElement> get constructors;
+
+  @override
+  InterfaceElement get firstFragment;
+
+  /// The interfaces implemented by this element.
+  ///
+  /// This is a union of interfaces declared by the class declaration and
+  /// all its augmentations.
+  List<InterfaceType> get interfaces;
+
+  /// The mixins applied by this class or in its augmentations.
+  ///
+  /// This is a union of mixins applied by the class declaration and all its
+  /// augmentations.
+  List<InterfaceType> get mixins;
+
+  @override
+  InterfaceType get thisType;
+
+  /// The unnamed constructor from [constructors].
+  ConstructorElement? get unnamedConstructor;
+
+  /// Returns the constructor from [constructors] that has the given [name].
+  ConstructorElement? getNamedConstructor(String name);
+}
+
+/// The result of applying augmentations to a [MixinElement].
+///
+/// Clients may not extend, implement or mix-in this class.
+@Deprecated(elementModelDeprecationMsg)
+abstract class AugmentedMixinElement extends AugmentedInterfaceElement {
+  /// The superclass constraints of this element.
+  ///
+  /// This is a union of constraints declared by the class declaration and
+  /// all its augmentations.
+  List<InterfaceType> get superclassConstraints;
+}
+
 /// A pattern variable that is explicitly declared.
 ///
 /// Clients may not extend, implement or mix-in this class.
@@ -74,6 +254,10 @@ abstract class BindPatternVariableElement implements PatternVariableElement {}
 /// Clients may not extend, implement or mix-in this class.
 @Deprecated('Use ClassElement2 instead')
 abstract class ClassElement implements InterfaceElement {
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedClassElement get augmented;
+
   /// Whether the class or its superclass declares a non-final instance field.
   bool get hasNonFinalField;
 
@@ -1079,7 +1263,11 @@ abstract class ElementVisitor<R> {
 ///
 /// Clients may not extend, implement or mix-in this class.
 @Deprecated('Use EnumElement2 instead')
-abstract class EnumElement implements InterfaceElement {}
+abstract class EnumElement implements InterfaceElement {
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedEnumElement get augmented;
+}
 
 /// An element representing an executable object, including functions, methods,
 /// constructors, getters, and setters.
@@ -1150,6 +1338,10 @@ abstract class ExecutableElement implements FunctionTypedElement {
 /// Clients may not extend, implement or mix-in this class.
 @Deprecated('Use ExtensionElement2 instead')
 abstract class ExtensionElement implements InstanceElement {
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedExtensionElement get augmented;
+
   /// The type that is extended by this extension.
   DartType get extendedType;
 
@@ -1179,6 +1371,10 @@ abstract class ExtensionElement implements InstanceElement {
 /// Clients may not extend, implement or mix-in this class.
 @Deprecated('Use ExtensionTypeElement2 instead')
 abstract class ExtensionTypeElement implements InterfaceElement {
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedExtensionTypeElement get augmented;
+
   /// The primary constructor of this extension.
   @Deprecated(elementModelDeprecationMsg)
   ConstructorElement get primaryConstructor;
@@ -1320,6 +1516,13 @@ abstract class InstanceElement
   /// The declared accessors (getters and setters).
   List<PropertyAccessorElement> get accessors;
 
+  /// The result of merging augmentations.
+  ///
+  /// It includes the members of the base element and its augmentations as
+  /// specified by the merge operations.
+  @Deprecated(elementModelDeprecationMsg)
+  AugmentedInstanceElement get augmented;
+
   @Deprecated(elementModelDeprecationMsg)
   @override
   CompilationUnitElement get enclosingElement3;
@@ -1350,6 +1553,10 @@ abstract class InterfaceElement implements InstanceElement {
   ///
   /// This includes superclasses, mixins, interfaces, and superclass constraints.
   List<InterfaceType> get allSupertypes;
+
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedInterfaceElement get augmented;
 
   /// The declared constructors.
   ///
@@ -1838,6 +2045,10 @@ abstract class MethodElement implements ClassMemberElement, ExecutableElement {
 /// Clients may not extend, implement or mix-in this class.
 @Deprecated('Use MixinElement2 instead')
 abstract class MixinElement implements InterfaceElement {
+  @Deprecated(elementModelDeprecationMsg)
+  @override
+  AugmentedMixinElement get augmented;
+
   /// Whether the mixin is a base mixin.
   ///
   /// A mixin is a base mixin if it has an explicit `base` modifier.
