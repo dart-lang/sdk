36117c443c9212b65d694083dd58ff9c359ecc1b
Robert Nystrom <rnystrom@google.com>
2025-05-05 02:24:20 -0700

    Format tests/lib/typed_data/ using 3.8 style.
    Change-Id: I85d53883d8f976246cae6f3caece157f906cf829
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/426320
    Auto-Submit: Bob Nystrom <rnystrom@google.com>
    Commit-Queue: Lasse Nielsen <lrn@google.com>
    Reviewed-by: Lasse Nielsen <lrn@google.com>

--- tests/lib/typed_data/simd_type_null_params_weak_test.dart
+++ tests/lib/typed_data/simd_type_null_params_weak_test.dart
@@ -29,23 +29,37 @@ main() {
   Expect.equals(true, throwsSomeError(() => float32x4 / dynamicNull));
   Expect.equals(true, throwsSomeError(() => float32x4.lessThan(dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => float32x4.lessThanOrEqual(dynamicNull)));
+    true,
+    throwsSomeError(() => float32x4.lessThanOrEqual(dynamicNull)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => float32x4.greaterThan(dynamicNull)));
+    true,
+    throwsSomeError(() => float32x4.greaterThan(dynamicNull)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => float32x4.greaterThanOrEqual(dynamicNull)));
+    true,
+    throwsSomeError(() => float32x4.greaterThanOrEqual(dynamicNull)),
+  );
   Expect.equals(true, throwsSomeError(() => float32x4.equal(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float32x4.notEqual(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float32x4.scale(dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => float32x4.clamp(dynamicNull, float32x4)));
+    true,
+    throwsSomeError(() => float32x4.clamp(dynamicNull, float32x4)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => float32x4.clamp(float32x4, dynamicNull)));
+    true,
+    throwsSomeError(() => float32x4.clamp(float32x4, dynamicNull)),
+  );
   Expect.equals(true, throwsSomeError(() => float32x4.shuffle(dynamicNull)));
-  Expect.equals(true,
-      throwsSomeError(() => float32x4.shuffleMix(float32x4, dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => float32x4.shuffleMix(dynamicNull, 0)));
+    true,
+    throwsSomeError(() => float32x4.shuffleMix(float32x4, dynamicNull)),
+  );
+  Expect.equals(
+    true,
+    throwsSomeError(() => float32x4.shuffleMix(dynamicNull, 0)),
+  );
   Expect.equals(true, throwsSomeError(() => float32x4.withX(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float32x4.withY(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float32x4.withZ(dynamicNull)));
@@ -53,13 +67,21 @@ main() {
   Expect.equals(true, throwsSomeError(() => float32x4.min(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float32x4.max(dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => Float32x4(dynamicNull, 0.0, 0.0, 0.0)));
+    true,
+    throwsSomeError(() => Float32x4(dynamicNull, 0.0, 0.0, 0.0)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => Float32x4(0.0, dynamicNull, 0.0, 0.0)));
+    true,
+    throwsSomeError(() => Float32x4(0.0, dynamicNull, 0.0, 0.0)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => Float32x4(0.0, 0.0, dynamicNull, 0.0)));
+    true,
+    throwsSomeError(() => Float32x4(0.0, 0.0, dynamicNull, 0.0)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => Float32x4(0.0, 0.0, 0.0, dynamicNull)));
+    true,
+    throwsSomeError(() => Float32x4(0.0, 0.0, 0.0, dynamicNull)),
+  );
 
   // Float32x4.splat
   Expect.equals(true, throwsSomeError(() => Float32x4.splat(dynamicNull)));
@@ -72,9 +94,13 @@ main() {
   Expect.equals(true, throwsSomeError(() => float64x2 / dynamicNull));
   Expect.equals(true, throwsSomeError(() => float64x2.scale(dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => float64x2.clamp(dynamicNull, float64x2)));
+    true,
+    throwsSomeError(() => float64x2.clamp(dynamicNull, float64x2)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => float64x2.clamp(float64x2, dynamicNull)));
+    true,
+    throwsSomeError(() => float64x2.clamp(float64x2, dynamicNull)),
+  );
   Expect.equals(true, throwsSomeError(() => float64x2.withX(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => float64x2.withY(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => Float64x2(dynamicNull, 0.0)));
@@ -94,9 +120,13 @@ main() {
   Expect.equals(true, throwsSomeError(() => int32x4 | dynamicNull));
   Expect.equals(true, throwsSomeError(() => int32x4.shuffle(dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => int32x4.shuffleMix(int32x4, dynamicNull)));
+    true,
+    throwsSomeError(() => int32x4.shuffleMix(int32x4, dynamicNull)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => int32x4.shuffleMix(dynamicNull, 0)));
+    true,
+    throwsSomeError(() => int32x4.shuffleMix(dynamicNull, 0)),
+  );
   Expect.equals(true, throwsSomeError(() => int32x4.withX(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => int32x4.withY(dynamicNull)));
   Expect.equals(true, throwsSomeError(() => int32x4.withZ(dynamicNull)));
@@ -111,17 +141,29 @@ main() {
   Expect.equals(true, throwsSomeError(() => Int32x4(0, 0, dynamicNull, 0)));
   Expect.equals(true, throwsSomeError(() => Int32x4(0, 0, 0, dynamicNull)));
   Expect.equals(
-      true, throwsSomeError(() => int32x4.select(dynamicNull, float32x4)));
+    true,
+    throwsSomeError(() => int32x4.select(dynamicNull, float32x4)),
+  );
   Expect.equals(
-      true, throwsSomeError(() => int32x4.select(float32x4, dynamicNull)));
+    true,
+    throwsSomeError(() => int32x4.select(float32x4, dynamicNull)),
+  );
 
   // Int32x4.bool
-  Expect.equals(true,
-      throwsSomeError(() => Int32x4.bool(dynamicNull, false, false, false)));
-  Expect.equals(true,
-      throwsSomeError(() => Int32x4.bool(false, dynamicNull, false, false)));
-  Expect.equals(true,
-      throwsSomeError(() => Int32x4.bool(false, false, dynamicNull, false)));
-  Expect.equals(true,
-      throwsSomeError(() => Int32x4.bool(false, false, false, dynamicNull)));
+  Expect.equals(
+    true,
+    throwsSomeError(() => Int32x4.bool(dynamicNull, false, false, false)),
+  );
+  Expect.equals(
+    true,
+    throwsSomeError(() => Int32x4.bool(false, dynamicNull, false, false)),
+  );
+  Expect.equals(
+    true,
+    throwsSomeError(() => Int32x4.bool(false, false, dynamicNull, false)),
+  );
+  Expect.equals(
+    true,
+    throwsSomeError(() => Int32x4.bool(false, false, false, dynamicNull)),
+  );
 }
--- tests/lib/typed_data/typed_data_interface_test.dart
+++ tests/lib/typed_data/typed_data_interface_test.dart
@@ -19,13 +19,19 @@ import 'package:expect/variations.dart';
 void test(TypedData data, int bytesPerElement, int lengthInBytes) {
   var type = data.runtimeType.toString();
   Expect.equals(
-      bytesPerElement, data.elementSizeInBytes, "$type.elementSizeInBytes");
+    bytesPerElement,
+    data.elementSizeInBytes,
+    "$type.elementSizeInBytes",
+  );
   Expect.equals(lengthInBytes, data.lengthInBytes, "$type.lengthInBytes");
   // Always aligned.
   Expect.equals(0, data.offsetInBytes % bytesPerElement, "$type alignment");
   if (data is TypedDataList) {
     Expect.equals(
-        lengthInBytes ~/ bytesPerElement, data.length, "$type.length");
+      lengthInBytes ~/ bytesPerElement,
+      data.length,
+      "$type.length",
+    );
   } else {
     // Only `ByteData` is not a `TypedDataList`.
     Expect.type<ByteData>(data);
@@ -61,87 +67,123 @@ void main() {
     Expect.equals(1, size);
     test(Uint8ClampedList(bytes), size, bytes);
     test(Uint8ClampedList.view(buffer, 32, bytes), size, bytes);
-    test(Uint8ClampedList.sublistView(typedList, 32, 32 + bytes * size), size,
-        bytes);
+    test(
+      Uint8ClampedList.sublistView(typedList, 32, 32 + bytes * size),
+      size,
+      bytes,
+    );
 
     size = Uint16List.bytesPerElement;
     Expect.equals(2, size);
     test(Uint16List(bytes ~/ 2), size, bytes);
     test(Uint16List.view(buffer, 32, bytes ~/ 2), size, bytes);
-    test(Uint16List.sublistView(typedList, 32, 32 + bytes * size ~/ 2), size,
-        bytes);
+    test(
+      Uint16List.sublistView(typedList, 32, 32 + bytes * size ~/ 2),
+      size,
+      bytes,
+    );
 
     size = Int16List.bytesPerElement;
     Expect.equals(2, size);
     test(Int16List(bytes ~/ 2), size, bytes);
     test(Int16List.view(buffer, 32, bytes ~/ 2), size, bytes);
-    test(Int16List.sublistView(typedList, 32, 32 + bytes * size ~/ 2), size,
-        bytes);
+    test(
+      Int16List.sublistView(typedList, 32, 32 + bytes * size ~/ 2),
+      size,
+      bytes,
+    );
 
     size = Uint32List.bytesPerElement;
     Expect.equals(4, size);
     test(Uint32List(bytes ~/ 4), size, bytes);
     test(Uint32List.view(buffer, 32, bytes ~/ 4), size, bytes);
-    test(Uint32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4), size,
-        bytes);
+    test(
+      Uint32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4),
+      size,
+      bytes,
+    );
 
     size = Int32List.bytesPerElement;
     Expect.equals(4, size);
     test(Int32List(bytes ~/ 4), size, bytes);
     test(Int32List.view(buffer, 32, bytes ~/ 4), size, bytes);
-    test(Int32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4), size,
-        bytes);
+    test(
+      Int32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4),
+      size,
+      bytes,
+    );
 
     if (!jsNumbers) {
       size = Uint64List.bytesPerElement;
       Expect.equals(8, size);
       test(Uint64List(bytes ~/ 8), size, bytes);
       test(Uint64List.view(buffer, 32, bytes ~/ 8), size, bytes);
-      test(Uint64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8), size,
-          bytes);
+      test(
+        Uint64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8),
+        size,
+        bytes,
+      );
 
       size = Int64List.bytesPerElement;
       Expect.equals(8, size);
       test(Int64List(bytes ~/ 8), size, bytes);
       test(Int64List.view(buffer, 32, bytes ~/ 8), size, bytes);
-      test(Int64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8), size,
-          bytes);
+      test(
+        Int64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8),
+        size,
+        bytes,
+      );
     }
 
     size = Float32List.bytesPerElement;
     Expect.equals(4, size);
     test(Float32List(bytes ~/ 4), size, bytes);
     test(Float32List.view(buffer, 32, bytes ~/ 4), size, bytes);
-    test(Float32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4), size,
-        bytes);
+    test(
+      Float32List.sublistView(typedList, 32, 32 + bytes * size ~/ 4),
+      size,
+      bytes,
+    );
 
     size = Float64List.bytesPerElement;
     Expect.equals(8, size);
     test(Float64List(bytes ~/ 8), size, bytes);
     test(Float64List.view(buffer, 32, bytes ~/ 8), size, bytes);
-    test(Float64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8), size,
-        bytes);
+    test(
+      Float64List.sublistView(typedList, 32, 32 + bytes * size ~/ 8),
+      size,
+      bytes,
+    );
 
     size = Int32x4List.bytesPerElement;
     Expect.equals(16, size);
     test(Int32x4List(bytes ~/ 16), size, bytes);
     test(Int32x4List.view(buffer, 32, bytes ~/ 16), size, bytes);
-    test(Int32x4List.sublistView(typedList, 32, 32 + bytes * size ~/ 16), size,
-        bytes);
+    test(
+      Int32x4List.sublistView(typedList, 32, 32 + bytes * size ~/ 16),
+      size,
+      bytes,
+    );
 
     size = Float32x4List.bytesPerElement;
     Expect.equals(16, size);
     test(Float32x4List(bytes ~/ 16), size, bytes);
     test(Float32x4List.view(buffer, 32, bytes ~/ 16), size, bytes);
-    test(Float32x4List.sublistView(typedList, 32, 32 + bytes * size ~/ 16),
-        size, bytes);
+    test(
+      Float32x4List.sublistView(typedList, 32, 32 + bytes * size ~/ 16),
+      size,
+      bytes,
+    );
 
     size = Float64x2List.bytesPerElement;
     Expect.equals(16, size);
     test(Float64x2List(bytes ~/ 16), size, bytes);
     test(Float64x2List.view(buffer, 32, bytes ~/ 16), size, bytes);
-    test(Float64x2List.sublistView(typedList, 32, 32 + bytes * size ~/ 16),
-        size, bytes);
+    test(
+      Float64x2List.sublistView(typedList, 32, 32 + bytes * size ~/ 16),
+      size,
+      bytes,
+    );
   }
 
   var oddSize = 64 + 7;
@@ -150,34 +192,62 @@ void main() {
   // View truncates to multiple of `lengthInBytes`.
   int roundTo(int num, int mod) => num - (num % mod);
   Expect.equals(oddSize, ByteData.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Uint8List.bytesPerElement),
-      Uint8List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Int8List.bytesPerElement),
-      Int8List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Uint8ClampedList.bytesPerElement),
-      Uint8ClampedList.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Uint16List.bytesPerElement),
-      Uint16List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Int16List.bytesPerElement),
-      Int16List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Uint32List.bytesPerElement),
-      Uint32List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Int32List.bytesPerElement),
-      Int32List.view(oddBuffer).lengthInBytes);
+  Expect.equals(
+    roundTo(oddSize, Uint8List.bytesPerElement),
+    Uint8List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Int8List.bytesPerElement),
+    Int8List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Uint8ClampedList.bytesPerElement),
+    Uint8ClampedList.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Uint16List.bytesPerElement),
+    Uint16List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Int16List.bytesPerElement),
+    Int16List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Uint32List.bytesPerElement),
+    Uint32List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Int32List.bytesPerElement),
+    Int32List.view(oddBuffer).lengthInBytes,
+  );
   if (!jsNumbers) {
-    Expect.equals(roundTo(oddSize, Uint64List.bytesPerElement),
-        Uint64List.view(oddBuffer).lengthInBytes);
-    Expect.equals(roundTo(oddSize, Int64List.bytesPerElement),
-        Int64List.view(oddBuffer).lengthInBytes);
+    Expect.equals(
+      roundTo(oddSize, Uint64List.bytesPerElement),
+      Uint64List.view(oddBuffer).lengthInBytes,
+    );
+    Expect.equals(
+      roundTo(oddSize, Int64List.bytesPerElement),
+      Int64List.view(oddBuffer).lengthInBytes,
+    );
   }
-  Expect.equals(roundTo(oddSize, Float32List.bytesPerElement),
-      Float32List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Float64List.bytesPerElement),
-      Float64List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Int32x4List.bytesPerElement),
-      Int32x4List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Float32x4List.bytesPerElement),
-      Float32x4List.view(oddBuffer).lengthInBytes);
-  Expect.equals(roundTo(oddSize, Float64x2List.bytesPerElement),
-      Float64x2List.view(oddBuffer).lengthInBytes);
+  Expect.equals(
+    roundTo(oddSize, Float32List.bytesPerElement),
+    Float32List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Float64List.bytesPerElement),
+    Float64List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Int32x4List.bytesPerElement),
+    Int32x4List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Float32x4List.bytesPerElement),
+    Float32x4List.view(oddBuffer).lengthInBytes,
+  );
+  Expect.equals(
+    roundTo(oddSize, Float64x2List.bytesPerElement),
+    Float64x2List.view(oddBuffer).lengthInBytes,
+  );
 }
