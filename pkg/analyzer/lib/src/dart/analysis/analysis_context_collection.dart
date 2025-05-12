0048e78e38491d050f01e29e0d1a3d3aff0ef920
Sam Rawlins <srawlins@google.com>
2025-05-12 14:57:49 -0700

    analyzer: Remove deprecated AnalysisContextCollectionImpl.updateAnalysisOptions2 parameter
    This parameter was deprecated in analyzer 7.4.0. It's also never been public API.
    
    Change-Id: I3372c751ac1808bd6806b4312cfdcff0dd51faa7
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/428061
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/analysis/analysis_context_collection.dart
+++ pkg/analyzer/lib/src/dart/analysis/analysis_context_collection.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
-import 'package:analyzer/dart/analysis/context_root.dart';
 import 'package:analyzer/dart/analysis/declared_variables.dart';
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
@@ -56,13 +55,6 @@ class AnalysisContextCollectionImpl implements AnalysisContextCollection {
     FileContentCache? fileContentCache,
     UnlinkedUnitStore? unlinkedUnitStore,
     InfoDeclarationStore? infoDeclarationStore,
-    @Deprecated('Use updateAnalysisOptions3 instead')
-    void Function({
-      required AnalysisOptionsImpl analysisOptions,
-      required ContextRoot contextRoot,
-      required DartSdk sdk,
-    })?
-    updateAnalysisOptions2,
     void Function({
       required AnalysisOptionsImpl analysisOptions,
       required DartSdk sdk,
@@ -74,13 +66,6 @@ class AnalysisContextCollectionImpl implements AnalysisContextCollection {
     sdkPath ??= getSdkPath();
 
     performanceLog ??= PerformanceLog(null);
-
-    if (updateAnalysisOptions2 != null && updateAnalysisOptions3 != null) {
-      throw ArgumentError(
-        'Only one of updateAnalysisOptions2 and updateAnalysisOptions3 may be '
-        'given',
-      );
-    }
 
     if (scheduler == null) {
       scheduler = AnalysisDriverScheduler(performanceLog);
@@ -112,20 +97,7 @@ class AnalysisContextCollectionImpl implements AnalysisContextCollection {
     var contextBuilder = ContextBuilderImpl(
       resourceProvider: this.resourceProvider,
     );
-    // While users can use the deprecated `updateAnalysisOptions2` and the new
-    // `updateAnalysisOptions3` parameter, prefer `updateAnalysisOptions3`, but
-    // create a new closure with the signature of the old.
-    var updateAnalysisOptions =
-        updateAnalysisOptions3 != null
-            ? ({
-              required AnalysisOptionsImpl analysisOptions,
-              required ContextRoot? contextRoot,
-              required DartSdk sdk,
-            }) => updateAnalysisOptions3(
-              analysisOptions: analysisOptions,
-              sdk: sdk,
-            )
-            : updateAnalysisOptions2;
+
     for (var root in roots) {
       var context = contextBuilder.createContext(
         byteStore: byteStore,
@@ -141,7 +113,7 @@ class AnalysisContextCollectionImpl implements AnalysisContextCollection {
         sdkPath: sdkPath,
         sdkSummaryPath: sdkSummaryPath,
         scheduler: scheduler,
-        updateAnalysisOptions2: updateAnalysisOptions,
+        updateAnalysisOptions3: updateAnalysisOptions3,
         fileContentCache: fileContentCache,
         unlinkedUnitStore: unlinkedUnitStore ?? UnlinkedUnitStoreImpl(),
         infoDeclarationStore: infoDeclarationStore,
