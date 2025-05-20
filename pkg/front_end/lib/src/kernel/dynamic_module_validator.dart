0f42d66b6f60e744cbb895027904a0ec80fff6c3
Alexander Markov <alexmarkov@google.com>
2025-05-20 06:25:00 -0700

    [dyn_modules] Fix overriding of a method declared in a mixin
    Mixin transformation copies members of a mixin into a mixin
    application class. Copied members should still behave similarly to
    original members in terms of overriding.
    
    When validating a dynamic module, overriding a copy of mixin
    member should be allowed if original member can be overridden.
    
    When annotating AST with dynamic interface pragmas, can-be-overridden
    pragmas should apply to all copies of mixin members.
    
    TEST=pkg/dynamic_modules/test/data/override_mixin_method
    Fixes b/418681054
    
    Change-Id: I038ff2133ef6f0c288a47cec7300c6ac4b23cbab
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/429423
    Reviewed-by: Slava Egorov <vegorov@google.com>
    Commit-Queue: Alexander Markov <alexmarkov@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>

--- pkg/front_end/lib/src/kernel/dynamic_module_validator.dart
+++ pkg/front_end/lib/src/kernel/dynamic_module_validator.dart
@@ -616,6 +616,9 @@ class _DynamicModuleValidator extends RecursiveVisitor {
     if (target is Procedure) {
       target = _unwrapMixinStubs(target);
     }
+    if (target is Member) {
+      target = _unwrapMixinCopy(target);
+    }
     if (!_isFromDynamicModule(target) &&
         !_isSpecified(target, spec.callable) &&
         !languageImplPragmas.isCallable(target)) {
@@ -692,6 +695,22 @@ class _DynamicModuleValidator extends RecursiveVisitor {
     return member;
   }
 
+  // Unwrap copied method from an eliminated mixin to get actual
+  // interface member.
+  Member _unwrapMixinCopy(Member member) {
+    if (!member.isInstanceMember) {
+      return member;
+    }
+    Class enclosingClass = member.enclosingClass!;
+    if (!enclosingClass.isEliminatedMixin) {
+      return member;
+    }
+    // Coverage-ignore-block(suite): Not run.
+    Class origin = enclosingClass.implementedTypes.last.classNode;
+    return hierarchy.getInterfaceMember(origin, member.name,
+        setter: member is Procedure && member.isSetter)!;
+  }
+
   void _verifyOverrides(
       List<Member> implementationMembers, List<Member> interfaceMembers) {
     int i = 0, j = 0;
@@ -716,6 +735,7 @@ class _DynamicModuleValidator extends RecursiveVisitor {
   }
 
   void _verifyOverride(Member ownMember, Member superMember) {
+    superMember = _unwrapMixinCopy(superMember);
     if (!_isFromDynamicModule(superMember) &&
         !_isSpecified(superMember, spec.canBeOverridden) &&
         !languageImplPragmas.canBeOverridden(superMember)) {
