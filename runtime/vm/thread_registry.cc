6497b412e218ed7df79fb9981614b7cfdfe6e6d7
Alexander Aprelev <aam@google.com>
2025-05-23 07:23:22 -0700

    [vm/shared] Ensure dart mutator thread stack is walked during gc.
    Fix asynchronous aspect of the test.
    
    Fixes https://github.com/dart-lang/sdk/issues/60756
    TEST=ci
    
    Change-Id: Ife311d5663665e7fa6e8dd3977fc31f5ed22cc17
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/430580
    Commit-Queue: Alexander Aprelev <aam@google.com>
    Reviewed-by: Liam Appelbe <liama@google.com>

--- runtime/vm/thread_registry.cc
+++ runtime/vm/thread_registry.cc
@@ -49,9 +49,10 @@ void ThreadRegistry::VisitObjectPointers(
   Thread* thread = active_list_;
   while (thread != nullptr) {
     if (thread->isolate_group() == isolate_group_of_interest) {
-      // The mutator thread is visited by the isolate itself (see
+      // The mutator thread with isolate is visited by the isolate itself (see
       // [IsolateGroup::VisitStackPointers]).
-      if (!thread->HasDartMutatorStack()) {
+      // All other threads are visited here.
+      if (thread->scheduled_dart_mutator_isolate() == nullptr) {
         thread->VisitObjectPointers(visitor, validate_frames);
       }
     }
