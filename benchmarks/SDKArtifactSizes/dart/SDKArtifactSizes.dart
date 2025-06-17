76997e66f0f813152892cd6ffb5b706b928c1f38
Ryan Macnak <rmacnak@google.com>
2025-06-17 10:59:09 -0700

    [benchmarks] Report VM platform file sizes under their old names for continuity of benchmarking data.
    Cf. ab4f9351b18ebc35f1f127ab91cfc4186ebed78e
    
    Change-Id: Iac3773c953c59d0a9ec60f069866a97499bc0494
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/435280
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Alexander Aprelev <aam@google.com>

--- benchmarks/SDKArtifactSizes/dart/SDKArtifactSizes.dart
+++ benchmarks/SDKArtifactSizes/dart/SDKArtifactSizes.dart
@@ -62,10 +62,14 @@ void main() {
     reportFileSize(executablePath, executable);
   }
 
-  for (final lib in libs) {
-    final libPath = '$rootDir/dart-sdk/lib/_internal/$lib';
-    reportFileSize(libPath, lib);
+  void reportLib(String lib, String name) {
+    reportFileSize('$rootDir/dart-sdk/lib/_internal/$lib', name);
   }
+
+  // Continue reporting the file size under the old name for continuity of
+  // benchmarking data.
+  reportLib('vm_platform.dill', 'vm_platform_strong.dill');
+  reportLib('vm_platform_product.dill', 'vm_platform_strong_product.dill');
 
   for (final snapshot in snapshots) {
     final snapshotPath =
