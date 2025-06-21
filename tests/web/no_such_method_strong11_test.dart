d3539374ffb66e625837d9da6c68b9b92a578c0e
Nate Biggs <natebiggs@google.com>
2025-06-20 20:11:25 -0700

    Cleanup extra --strong flags being passed in test/web.
    Change-Id: Ia00ced1c31ccd49dc883d4dfdc4fe644d8a5e363
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/435600
    Reviewed-by: Mayank Patke <fishythefish@google.com>
    Commit-Queue: Nate Biggs <natebiggs@google.com>

--- tests/web/no_such_method_strong11_test.dart
+++ tests/web/no_such_method_strong11_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// dart2jsOptions=--strong
-
 // Regression test checking that nsm-forwarders do not get installed for private
 // members of other libraries. See http://dartbug.com/33665
 
--- tests/web/regress/issue/28749_test.dart
+++ tests/web/regress/issue/28749_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// dart2jsOptions=--strong
-
 // Regression test for http://dartbug.com/28749.
 //
 // This would crash at compile time because inner typedefs remain after calling
