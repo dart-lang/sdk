40a5bd0e08f9995184e15b67f61a4f1e6d119ade
Daco Harkes <dacoharkes@google.com>
2025-07-16 02:21:02 -0700

    [pkg] Fix `dart_data_home` typo
    Change-Id: Iffdcbfe8cabcde8042544fa279d3ffbd91d936ce
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/440620
    Commit-Queue: Daco Harkes <dacoharkes@google.com>
    Auto-Submit: Daco Harkes <dacoharkes@google.com>
    Reviewed-by: Michael Goderbauer <goderbauer@google.com>

--- pkg/dart_data_home/lib/dart_data_home.dart
+++ pkg/dart_data_home/lib/dart_data_home.dart
@@ -6,7 +6,7 @@
 
 /// A package providing [getDartDataHome], a standardized way to access a
 /// user-specific data directory for Dart and Flutter tooling, defaulting to
-/// OS-conventions and configurable via the `DART_USER_HOME` environment
+/// OS-conventions and configurable via the `DART_DATA_HOME` environment
 /// variable.
 library;
 
