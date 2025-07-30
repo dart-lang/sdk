4564d148b329f93fcdb6327716f0f125183a19f8
FMorschel <git@fmorschel.dev>
2025-07-30 14:51:42 -0700

    [DAS] Fixes Go to import on extension getter/setter
    Fixes: https://github.com/dart-lang/sdk/issues/61200
    Change-Id: I10c64d4aff472c0f219e2b1af79520fbacbb2ea2
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/442460
    Auto-Submit: Felipe Morschel <git@fmorschel.dev>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/lib/src/lsp/handlers/custom/handler_imports.dart
+++ pkg/analysis_server/lib/src/lsp/handlers/custom/handler_imports.dart
@@ -72,13 +72,14 @@ class ImportsHandler
       if (node is NamedType) {
         prefixName = node.importPrefix?.name.lexeme;
       } else if (node.thisOrAncestorOfType<PrefixedIdentifier>()
-          case PrefixedIdentifier(:var prefix) when prefix != node) {
+          case PrefixedIdentifier(:var prefix)
+          when prefix != node && prefix.element is PrefixElement) {
         prefixName = prefix.name;
       } else if (node is SimpleIdentifier) {
         if (node.parent case MethodInvocation(
           target: SimpleIdentifier target?,
-        )) {
-          prefixName = target.toString();
+        ) when target.element is PrefixElement) {
+          prefixName = target.name;
         }
       }
 
