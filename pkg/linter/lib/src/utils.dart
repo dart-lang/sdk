6fc1427b2a1ca50a67c895a38ba19a33f3d9f2c4
Sam Rawlins <srawlins@google.com>
2025-08-06 14:36:14 -0700

    linter: Remove unused utils
    `_reservedWords` and `isReservedWord` were only used in `utils.dart`,
    so are moved over there.
    
    The following were unused (except in their own unit tests), and are
    removed: `_lowerCaseUnderScore`, `_pubspec`, `hasLeadingUnderscore`,
    `isIdentifier`, `isLowerCaseUnderScore`, `isPubspecFileName`,
    `CamelCaseString`.
    
    I touched up doc comments as I went.
    
    Change-Id: Ie03f9328a25e3147bd9c320cf80841ba56da7abd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/444161
    Commit-Queue: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/linter/lib/src/utils.dart
+++ pkg/linter/lib/src/utils.dart
@@ -2,10 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/src/lint/io.dart'; // ignore: implementation_imports
 
-import 'ast.dart';
-import 'util/ascii_utils.dart';
+// A camel case string here is defined as:
+// * An arbitrary number of optional leading `_`s or `$`s,
+// * followed by an upper-case letter, `$` or `?`,
+// * followed by any number of letters, digits, `?` or `$`s.
+//
+// This ensures that the text contains a `$`, `?` or upper-case letter
+// before any lower-case letter or digit, and no letters or `?`s before an
+// `_`.
+final _camelCasePattern = RegExp(r'^_*(?:\$+_+)*[$?A-Z][$?a-zA-Z\d]*$');
 
 // An identifier here is defined as:
 // * A sequence of `_`, `$`, letters or digits,
@@ -24,13 +32,6 @@ final _lowerCamelCase = RegExp(
   r'^_*[?$a-z][a-z\d?$]*(?:(?:[A-Z]|_\d)[a-z\d?$]*)*_?$',
 );
 
-// A lower-case underscore (snake-case) is here defined as:
-// * A sequence of lower-case letters, digits and underscores,
-// * starting with a lower-case letter, and
-// * with no two adjacent underscores,
-// * and not ending in an underscore.
-final _lowerCaseUnderScore = RegExp(r'^[a-z](?:_?[a-z\d])*$');
-
 @Deprecated('Prefer: ascii_utils.isValidFileName')
 final _lowerCaseUnderScoreWithDots = RegExp(
   r'^_?[_a-z\d]*(?:\.[a-z][_a-z\d]*)*$',
@@ -45,7 +46,10 @@ final _lowerCaseUnderScoreWithLeadingUnderscores = RegExp(
   r'^_*[a-z](?:_?[a-z\d])*$',
 );
 
-final _pubspec = RegExp(r'^_?pubspec\.yaml$');
+final Set<String> _reservedWords = {
+  for (var entry in Keyword.keywords.entries)
+    if (entry.value.isReservedWord) entry.key,
+};
 
 // A library prefix here is defined as:
 // * An optional leading `?`,
@@ -54,48 +58,35 @@ final _pubspec = RegExp(r'^_?pubspec\.yaml$');
 // * followed by any number of lower-case letters, digits and underscores.
 final _validLibraryPrefix = RegExp(r'^\$?_*[a-z][_a-z\d]*$');
 
-/// Returns `true` if the given [name] has a leading `_`.
-@Deprecated('Prefer: ascii_utils String extension `hasLeadingUnderscore`')
-bool hasLeadingUnderscore(String name) => name.hasLeadingUnderscore;
+/// Whether this [string] is formatted in `CamelCase`.
+bool isCamelCase(String string) => _isCamelCase(string);
 
-/// Check if this [string] is formatted in `CamelCase`.
-bool isCamelCase(String string) => CamelCaseString.isCamelCase(string);
-
-/// Returns `true` if this [fileName] is a Dart file.
+/// Whether this [fileName] is a Dart file.
 bool isDartFileName(String fileName) => fileName.endsWith('.dart');
 
-/// Returns `true` if this [name] is a legal Dart identifier.
-bool isIdentifier(String name) => _identifier.hasMatch(name);
-
-/// Returns `true` if this [id] is `lowerCamelCase`.
+/// Whether [id] is `lowerCamelCase`.
 bool isLowerCamelCase(String id) =>
     id.length == 1 && isUpperCase(id.codeUnitAt(0)) ||
     id == '_' ||
     _lowerCamelCase.hasMatch(id);
 
-/// Returns `true` if this [id] is `lower_camel_case_with_underscores`.
-bool isLowerCaseUnderScore(String id) => _lowerCaseUnderScore.hasMatch(id);
-
-/// Returns `true` if this [id] is `lower_camel_case_with_underscores_or.dots`.
+/// Whether this [id] is `lower_camel_case_with_underscores_or.dots`.
 bool isLowerCaseUnderScoreWithDots(String id) =>
 // ignore: deprecated_member_use_from_same_package
 _lowerCaseUnderScoreWithDots.hasMatch(id);
 
-/// Returns `true` if this [fileName] is a Pubspec file.
-bool isPubspecFileName(String fileName) => _pubspec.hasMatch(fileName);
-
-/// Returns `true` if the given code unit [c] is upper case.
+/// Whether the given code unit [c] is upper case.
 bool isUpperCase(int c) => c >= 0x40 && c <= 0x5A;
 
-/// Returns true if this [libraryPrefix] is valid.
+/// Whether this [libraryPrefix] is valid.
 bool isValidLibraryPrefix(String libraryPrefix) =>
     _validLibraryPrefix.hasMatch(libraryPrefix);
 
-/// Returns true if this [id] is a valid package name.
+/// Whether this [id] is a valid package name.
 bool isValidPackageName(String id) =>
     _lowerCaseUnderScoreWithLeadingUnderscores.hasMatch(id) &&
-    isIdentifier(id) &&
-    !isReservedWord(id);
+    _isIdentifier(id) &&
+    !_isReservedWord(id);
 
 /// Write the given [object] to the console.
 /// Uses the shared [outSink] for redirecting in tests.
@@ -103,33 +94,10 @@ void printToConsole(Object? object) {
   outSink.writeln(object);
 }
 
-class CamelCaseString {
-  static final _camelCaseMatcher = RegExp(r'[A-Z][a-z]*');
+bool _isCamelCase(String name) => _camelCasePattern.hasMatch(name);
 
-  // A camel case string here is defined as:
-  // * An arbitrary number of optional leading `_`s or `$`s,
-  // * followed by an upper-case letter, `$` or `?`,
-  // * followed by any number of letters, digits, `?` or `$`s.
-  //
-  // This ensures that the text contains a `$`, `?` or upper-case letter
-  // before any lower-case letter or digit, and no letters or `?`s before an
-  // `_`.
-  static final _camelCaseTester = RegExp(r'^_*(?:\$+_+)*[$?A-Z][$?a-zA-Z\d]*$');
+/// Whether this [name] is a legal Dart identifier.
+bool _isIdentifier(String name) => _identifier.hasMatch(name);
 
-  final String value;
-  CamelCaseString(this.value) {
-    if (!isCamelCase(value)) {
-      throw ArgumentError.value(value, 'value', '$value is not CamelCase');
-    }
-  }
-
-  String get humanized => _humanize(value);
-
-  @override
-  String toString() => value;
-
-  static bool isCamelCase(String name) => _camelCaseTester.hasMatch(name);
-
-  static String _humanize(String camelCase) =>
-      _camelCaseMatcher.allMatches(camelCase).map((m) => m[0]).join(' ');
-}
+/// Whether the given word is a Dart reserved word.
+bool _isReservedWord(String word) => _reservedWords.contains(word);
