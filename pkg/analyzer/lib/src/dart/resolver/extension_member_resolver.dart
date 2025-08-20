8e3aa277cb0916546117c6c96ce0d216acf0ccff
Paul Berry <paulberry@google.com>
2025-08-20 14:06:24 -0700

    [analyzer] Move DiagnosticReporter into src/.
    In a follow-up CL, I plan to add an extension on `DiagnosticReporter`
    that's not exposed through the analyzer public API, so that we can try
    out the analyzer's new literate API for diagnostic reporting without
    exposing it to analyzer clients yet. The extension will need to be in
    the same library as `DiagnosticReporter` (so that it can access a
    private method), so in order to avoid exposing the extension through
    the analyzer public API, that library will need to be in `src/`.
    
    From the point of view of analyzer clients, this change is a no-op;
    the `DiagnosticReporter` class is still available from
    `package:analyzer/error/listener.dart` by way of an export directive.
    
    Change-Id: I6a6a696426f43ee57789b8a7ec1c36bc0b8680ef
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/446100
    Commit-Queue: Paul Berry <paulberry@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/lib/src/dart/resolver/extension_member_resolver.dart
+++ pkg/analyzer/lib/src/dart/resolver/extension_member_resolver.dart
@@ -6,7 +6,6 @@ import 'package:analyzer/dart/analysis/features.dart';
 import 'package:analyzer/dart/ast/syntactic_entity.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
-import 'package:analyzer/error/listener.dart';
 import 'package:analyzer/src/dart/ast/ast.dart';
 import 'package:analyzer/src/dart/ast/extensions.dart';
 import 'package:analyzer/src/dart/element/element.dart';
