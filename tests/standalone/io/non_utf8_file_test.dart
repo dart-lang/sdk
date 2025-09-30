c850242ef99554b7351d6a4e9a3a8a6a716c16e1
Ryan Macnak <rmacnak@google.com>
2025-09-30 15:43:42 -0700

    [test] Remove use of package:test from standalone tests.
    Bug: https://github.com/dart-lang/sdk/issues/61548
    Change-Id: I7a4d562a88d5d2f4ca2245e924411042ec463c37
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/452544
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Brian Quinlan <bquinlan@google.com>

--- tests/standalone/io/non_utf8_file_test.dart
+++ tests/standalone/io/non_utf8_file_test.dart
@@ -2,98 +2,97 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io';
-import 'dart:typed_data';
+import "dart:async";
+import "dart:io";
+import "dart:typed_data";
 
-import 'package:test/test.dart';
+import "package:expect/expect.dart";
 
-Future main() async {
-  var asyncFile;
-  var syncFile;
+testAsync() async {
+  final tmp = await Directory.systemTemp.createTemp("non_utf8_file_test_async");
 
-  test('Non-UTF8 Filename', () async {
-    final tmp = await Directory.systemTemp.createTemp(
-      'non_utf8_file_test_async',
-    );
-    try {
-      final rawPath = new Uint8List.fromList([182]);
-      asyncFile = new File.fromRawPath(rawPath);
-      if (Platform.isMacOS || Platform.isIOS) {
-        try {
-          await asyncFile.create();
-        } on FileSystemException catch (e) {
-          // Macos doesn't support non-UTF-8 paths.
-          await tmp.delete(recursive: true);
-          return;
-        }
-      } else {
+  try {
+    final rawPath = new Uint8List.fromList([182]);
+    final asyncFile = new File.fromRawPath(rawPath);
+    if (Platform.isMacOS || Platform.isIOS) {
+      try {
         await asyncFile.create();
+      } on FileSystemException catch (e) {
+        // Macos doesn"t support non-UTF-8 paths.
+        await tmp.delete(recursive: true);
+        return;
       }
-      expect(await asyncFile.exists(), isTrue);
-
-      for (final file in tmp.listSync()) {
-        // FIXME(bkonyi): reenable when rawPath is exposed.
-        /*
-        if (Platform.isWindows) {
-          // Windows replaces invalid characters with � when creating file system
-          // entities.
-          final raw = file.rawPath;
-          expect(raw.sublist(raw.length - 3), [239, 191, 189]);
-        } else {
-          expect(file.rawPath.last, 182);
-        }
-        */
-        // FIXME(bkonyi): this isn't true on some versions of MacOS. Why?
-        if (!Platform.isMacOS && !Platform.isIOS) {
-          expect(file.path.endsWith('�'), isTrue);
-        }
-      }
-      await asyncFile.delete();
-    } finally {
-      await tmp.delete(recursive: true);
+    } else {
+      await asyncFile.create();
     }
-  });
+    Expect.isTrue(await asyncFile.exists());
 
-  test('Non-UTF8 Filename Sync', () {
-    final tmp = Directory.systemTemp.createTempSync('non_utf8_file_test_sync');
-    try {
-      final rawPath = new Uint8List.fromList([182]);
-      syncFile = new File.fromRawPath(rawPath);
-
-      if (Platform.isMacOS || Platform.isIOS) {
-        try {
-          syncFile.createSync();
-        } on FileSystemException catch (e) {
-          // Macos doesn't support non-UTF-8 paths.
-          tmp.deleteSync(recursive: true);
-          return;
-        }
+    for (final file in tmp.listSync()) {
+      // FIXME(bkonyi): reenable when rawPath is exposed.
+      /*
+      if (Platform.isWindows) {
+        // Windows replaces invalid characters with � when creating file system
+        // entities.
+        final raw = file.rawPath;
+        Expect.listEquals(raw.sublist(raw.length - 3), [239, 191, 189]);
       } else {
-        syncFile.createSync();
+        Expect.equals(file.rawPath.last, 182);
       }
-      expect(syncFile.existsSync(), isTrue);
-
-      for (final file in tmp.listSync()) {
-        // FIXME(bkonyi): reenable when rawPath is exposed.
-        /*
-        if (Platform.isWindows) {
-          // Windows replaces invalid characters with � when creating file system
-          // entities.
-          final raw = file.rawPath;
-          expect(raw.sublist(raw.length - 3), [239, 191, 189]);
-        } else {
-          expect(file.rawPath.last, 182);
-        }
-        */
-        // FIXME(bkonyi): this isn't true on some versions of MacOS. Why?
-        if (!Platform.isMacOS && !Platform.isIOS) {
-          expect(file.path.endsWith('�'), isTrue);
-        }
+      */
+      // FIXME(bkonyi): this isn"t true on some versions of MacOS. Why?
+      if (!Platform.isMacOS && !Platform.isIOS) {
+        Expect.isTrue(file.path.endsWith("�"));
       }
-      syncFile.deleteSync();
-    } finally {
-      tmp.deleteSync(recursive: true);
     }
-  });
+    await asyncFile.delete();
+  } finally {
+    await tmp.delete(recursive: true);
+  }
+}
+
+testSync() {
+  final tmp = Directory.systemTemp.createTempSync("non_utf8_file_test_sync");
+  try {
+    final rawPath = new Uint8List.fromList([182]);
+    final syncFile = new File.fromRawPath(rawPath);
+
+    if (Platform.isMacOS || Platform.isIOS) {
+      try {
+        syncFile.createSync();
+      } on FileSystemException catch (e) {
+        // Macos doesn"t support non-UTF-8 paths.
+        tmp.deleteSync(recursive: true);
+        return;
+      }
+    } else {
+      syncFile.createSync();
+    }
+    Expect.isTrue(syncFile.existsSync());
+
+    for (final file in tmp.listSync()) {
+      // FIXME(bkonyi): reenable when rawPath is exposed.
+      /*
+      if (Platform.isWindows) {
+        // Windows replaces invalid characters with � when creating file system
+        // entities.
+        final raw = file.rawPath;
+        Expect.listEquals(raw.sublist(raw.length - 3), [239, 191, 189]);
+      } else {
+        Expect.equals(file.rawPath.last, 182);
+      }
+      */
+      // FIXME(bkonyi): this isn"t true on some versions of MacOS. Why?
+      if (!Platform.isMacOS && !Platform.isIOS) {
+        Expect.isTrue(file.path.endsWith("�"));
+      }
+    }
+    syncFile.deleteSync();
+  } finally {
+    tmp.deleteSync(recursive: true);
+  }
+}
+
+main() async {
+  await testAsync();
+  testSync();
 }
