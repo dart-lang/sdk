9ee6b090a4afee0d50a378114e0acd06b8c45130
Alexander Markov <alexmarkov@google.com>
2025-10-01 11:36:12 -0700

    [dyn_modules] Allow dynamic modules to use and override public members of _Enum
    Private class _Enum is an implementation detail of Dart enums, so
    we should not require users to explicitly expose it through dynamic
    interface in order to use enums in the dynamic modules.
    
    This change adds pragmas to make all public members of _Enum callable
    and allows _Enum.toString() to be overridden in dynamic modules.
    
    TEST=pkg/dynamic_modules/test/data/enum
    
    CoreLibraryReviewExempt: no API changes, only pragmas.
    Bug: b/448593948
    Change-Id: I37040d4912d6da38fbf568c11ae0834a110d3a46
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/452880
    Commit-Queue: Alexander Markov <alexmarkov@google.com>
    Reviewed-by: Sigmund Cherem <sigmund@google.com>

--- pkg/dynamic_modules/test/data/enum/main.dart
+++ pkg/dynamic_modules/test/data/enum/main.dart
@@ -6,8 +6,18 @@ import '../../common/testing.dart' as helper;
 import 'package:expect/expect.dart';
 
 void main() async {
-  final result = await helper.load('entry1.dart') as Enum;
-  Expect.equals('e2', result.name);
-  Expect.equals('Foo.e2', result.toString());
+  final result = await helper.load('entry1.dart') as List;
+  final r0 = result[0] as Enum;
+  final r1 = result[1] as int;
+  final r2 = result[2] as String;
+  final r3 = result[3] as Enum;
+  Expect.equals('e2', r0.name);
+  Expect.equals('Foo.e2', r0.toString());
+  Expect.equals(1, r0.index);
+  Expect.equals(0, r1);
+  Expect.equals('Foo.e3', r2);
+  Expect.equals('e1', r3.name);
+  Expect.equals('E1', r3.toString());
+  Expect.equals(0, r3.index);
   helper.done();
 }
