9bcd33c50bf9bf27b3d063ebe39c9eba93ebbc53
Ryan Macnak <rmacnak@google.com>
2025-10-08 08:16:42 -0700

    Reapply "[standalone] Use fewer syscalls to get entropy on Linux and Mac."
    Use SecRandomCopyBytes on Mac/iOS.
    
    TEST=ci, local engine build
    Change-Id: I48e1fc0186e4a65419fd9bf02be4d91983f8d8d9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/453581
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Slava Egorov <vegorov@google.com>

--- runtime/bin/crypto_linux.cc
+++ runtime/bin/crypto_linux.cc
@@ -5,17 +5,19 @@
 #include "platform/globals.h"
 #if defined(DART_HOST_OS_LINUX) || defined(DART_HOST_OS_ANDROID)
 
-#include <errno.h>  // NOLINT
-#include <fcntl.h>  // NOLINT
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
 
 #include "bin/crypto.h"
 #include "bin/fdutils.h"
+#include "platform/memory_sanitizer.h"
 #include "platform/signal_blocker.h"
 
 namespace dart {
 namespace bin {
 
-bool Crypto::GetRandomBytes(intptr_t count, uint8_t* buffer) {
+static bool GetRandomFromDev(intptr_t count, uint8_t* buffer) {
   ThreadSignalBlocker signal_blocker(SIGPROF);
   intptr_t fd = TEMP_FAILURE_RETRY_NO_SIGNAL_BLOCKER(
       open("/dev/urandom", O_RDONLY | O_CLOEXEC));
@@ -35,6 +37,28 @@ bool Crypto::GetRandomBytes(intptr_t count, uint8_t* buffer) {
     bytes_read += res;
   } while (bytes_read < count);
   close(fd);
+  return true;
+}
+
+bool Crypto::GetRandomBytes(intptr_t count, uint8_t* buffer) {
+  intptr_t bytes_read = 0;
+  do {
+    ssize_t res;
+    do {
+      res = syscall(__NR_getrandom, buffer + bytes_read, count - bytes_read,
+                    /*flags=*/0);
+    } while (res == -1 && errno == EINTR);
+    if (res == -1) {
+      if (errno == ENOSYS) {
+        return GetRandomFromDev(count, buffer);
+      }
+      return false;
+    }
+    bytes_read += res;
+  } while (bytes_read < count);
+  // Not using the libc wrapper `getrandom`, which MSAN is missing an
+  // interceptor for anyway.
+  MSAN_UNPOISON(buffer, count);
   return true;
 }
 
