0954307d8513ef5e2f7938b6dd27bf9042cb01f9
Konstantin Shcheglov <scheglov@google.com>
2025-10-20 16:46:42 -0700

    Fine. Fix for NPE when re-exporting dart:core with its in-nowhere dynamic and Never.
    Change-Id: Ida13cd9774b3eedb5f9ace8c83aca2c7a3d7d400
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/456120
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Paul Berry <paulberry@google.com>

--- pkg/analyzer/test/src/dart/analysis/driver_test.dart
+++ pkg/analyzer/test/src/dart/analysis/driver_test.dart
@@ -22665,6 +22665,94 @@ class A2 {}
     );
   }
 
+  test_dependency_export_dynamic() async {
+    configuration
+      ..withGetErrorsEvents = false
+      ..withStreamResolvedUnitResults = false;
+
+    await _runChangeScenarioTA(
+      initialA: r'''
+class A {}
+''',
+      testCode: r'''
+import 'a.dart';
+export 'dart:core' show int, dynamic, Never;
+''',
+      operation: _FineOperationTestFileGetErrors(),
+      expectedInitialEvents: r'''
+[status] working
+[operation] linkLibraryCycle SDK
+[operation] linkLibraryCycle
+  package:test/a.dart
+    hashForRequirements: #H0
+    declaredClasses
+      A: #M0
+        interface: #M1
+    exportMapId: #M2
+    exportMap
+      A: #M0
+  requirements
+[operation] linkLibraryCycle
+  package:test/test.dart
+    hashForRequirements: #H1
+    exportMapId: #M3
+    exportMap
+      int: #M4
+    reExportMap
+      int: #M4
+    exportedLibraryUris: dart:core
+  requirements
+    exportRequirements
+      package:test/test.dart
+        exports
+          dart:core
+            combinators
+              show Never, dynamic, int
+            int: #M4
+[operation] analyzeFile
+  file: /home/test/lib/test.dart
+  library: /home/test/lib/test.dart
+[operation] analyzedLibrary
+  file: /home/test/lib/test.dart
+  requirements
+    libraries
+      package:test/a.dart
+        libraryMetadataId: #M5
+        exportMapId: #M2
+        exportMap
+          Never: <null>
+          Never=: <null>
+          dynamic: <null>
+          dynamic=: <null>
+          int: <null>
+          int=: <null>
+[status] idle
+''',
+      updatedA: r'''
+class B {}
+''',
+      expectedUpdatedEvents: r'''
+[status] working
+[operation] linkLibraryCycle
+  package:test/a.dart
+    hashForRequirements: #H2
+    declaredClasses
+      B: #M6
+        interface: #M7
+    exportMapId: #M8
+    exportMap
+      B: #M6
+  requirements
+[operation] reuseLinkedBundle
+  package:test/test.dart
+[operation] getErrorsFromBytes
+  file: /home/test/lib/test.dart
+  library: /home/test/lib/test.dart
+[status] idle
+''',
+    );
+  }
+
   test_dependency_export_enum() async {
     var a = newFile('$testPackageLibPath/a.dart', r'''
 enum E {v}
