456efb510d3fc76766bfbbb03b03531935e3e790
Stephen Adams <sra@google.com>
2025-10-23 11:21:40 -0700

    Make wasm/uri_base_test run on browsers
    Change-Id: I6bc542bc6b2f68e7043d4c840da26dda30faad2d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/456141
    Reviewed-by: Ömer Ağacan <omersa@google.com>
    Commit-Queue: Stephen Adams <sra@google.com>

--- tests/web/wasm/uri_base_test.dart
+++ tests/web/wasm/uri_base_test.dart
@@ -2,6 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:expect/expect.dart';
+
 void main() {
-  print(Uri.base.toFilePath()); // should not crash
+  final base = Uri.base; // should not crash
+
+  // Tests are compiled to generated files in a directory that uses the test
+  // file name.
+  Expect.contains('uri_base_test', '$base');
 }
