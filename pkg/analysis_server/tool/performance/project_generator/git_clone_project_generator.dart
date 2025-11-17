5436fcde7dbb3fefad122184518dcba3115b31ba
Jake Macdonald <jakemac@google.com>
2025-11-17 09:01:58 -0800

    add analysis server scenarios, project generators, and a simple runner script
    Currently supports a "git worktree" mode for running on local directories, as well as a "git clone" mode for cloning remote directories.
    
    You can execute the script, but it will fail when it gets to replaying the logs due to the empty Json file, but this is a good start.
    
    I think the next step here is to have some way of normalizing the logs so they are portable across machines/directories.
    
    Change-Id: I8de5defe81c76f18c3bf3220e2a517a4247d4641
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/462260
    Commit-Queue: Jake Macdonald <jakemac@google.com>
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

---
+++ pkg/analysis_server/tool/performance/project_generator/git_clone_project_generator.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import '../utilities/git.dart';
+import 'project_generator.dart';
+
+/// A [ProjectGenerator] that clones a git [repo] and checks out a specific
+/// [ref] (commit sha, tag, or branch name).
+class GitCloneProjectGenerator implements ProjectGenerator {
+  /// The URI of the git repo to clone.
+  final String repo;
+
+  /// The ref (commit sha, tag, or branch) to check out.
+  final String ref;
+
+  GitCloneProjectGenerator(this.repo, this.ref);
+
+  @override
+  String get description => 'Cloning git repo "$repo" at ref "$ref"';
+
+  @override
+  Future<Directory> setUp() async {
+    var outputDir = await Directory.systemTemp.createTemp('as_git_clone');
+    await runGitCommand(['clone', repo, '.'], outputDir);
+    await runGitCommand(['fetch', 'origin', ref], outputDir);
+    await runGitCommand(['checkout', ref], outputDir);
+    return outputDir;
+  }
+
+  @override
+  Future<void> tearDown(Directory projectDir) async {
+    await projectDir.delete(recursive: true);
+  }
+}
---
+++ pkg/analysis_server/tool/performance/project_generator/git_worktree_project_generator.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import '../utilities/git.dart';
+import 'project_generator.dart';
+
+/// A [ProjectGenerator] that creates a new git working tree for an already
+/// cloned local repo, checked out at a specific [ref] (commit sha, tag, or branch name).
+class GitWorktreeProjectGenerator implements ProjectGenerator {
+  /// The Directory containing the local git repo.
+  final Directory originalRepo;
+
+  /// The ref (commit sha, tag, or branch) to check out into a new working tree.
+  final String ref;
+
+  GitWorktreeProjectGenerator(this.originalRepo, this.ref);
+
+  @override
+  String get description =>
+      'Creating git worktree for "${originalRepo.path}" at ref "$ref"';
+
+  @override
+  Future<Directory> setUp() async {
+    var projectDir = await Directory.systemTemp.createTemp('as_git_worktree');
+    await runGitCommand(['worktree', 'add', projectDir.path], originalRepo);
+    return projectDir;
+  }
+
+  @override
+  Future<void> tearDown(Directory projectDir) async {
+    await runGitCommand(['worktree', 'remove', projectDir.path], originalRepo);
+  }
+}
