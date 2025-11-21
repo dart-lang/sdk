58fdefcd9e04c5e1f0485b21d34ee4f5578357a0
Alexander Markov <alexmarkov@google.com>
2025-11-21 13:42:21 -0800

    AST to CFG IR translation, IR tests
    Issue: https://github.com/dart-lang/sdk/issues/61635
    Change-Id: Ib64610138ee104cdcf55fa1cbb617e653e5fda0a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/462601
    Commit-Queue: Alexander Markov <alexmarkov@google.com>
    Reviewed-by: Slava Egorov <vegorov@google.com>

---
+++ pkg/cfg/test/ir_test.dart
@@ -0,0 +1,190 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:cfg/ir/global_context.dart';
+import 'package:front_end/src/api_unstable/vm.dart'
+    show
+        CompilerOptions,
+        computePlatformBinariesLocation,
+        CfeDiagnosticMessage,
+        kernelForProgram;
+import 'package:kernel/ast.dart';
+import 'package:kernel/class_hierarchy.dart';
+import 'package:kernel/core_types.dart' show CoreTypes;
+import 'package:kernel/kernel.dart';
+import 'package:kernel/target/targets.dart';
+import 'package:cfg/front_end/ast_to_ir.dart';
+import 'package:cfg/front_end/recognized_methods.dart';
+import 'package:cfg/ir/functions.dart';
+import 'package:cfg/ir/ir_to_text.dart';
+import 'package:kernel/type_environment.dart';
+import 'package:test/test.dart';
+import 'package:vm/modular/target/vm.dart';
+
+/// Environment define to update expectation files on failures.
+const kUpdateExpectations = 'updateExpectations';
+
+final String dartSdkPkgDir = Platform.script.resolve('../..').toFilePath();
+
+Future<void> runTestCase(Uri source) async {
+  final target = VmTarget(TargetFlags());
+  Component component = await compileTestCaseToKernelProgram(
+    source,
+    target: target,
+  );
+  final coreTypes = CoreTypes(component);
+  final hierarchy = ClassHierarchy(component, coreTypes);
+  final typeEnvironment = TypeEnvironment(coreTypes, hierarchy);
+
+  String actual = GlobalContext.withContext(
+    GlobalContext(typeEnvironment: typeEnvironment),
+    () {
+      final compileAndDump = CompileAndDumpIr();
+
+      final mainLibrary = component.mainMethod!.enclosingLibrary;
+      mainLibrary.accept(compileAndDump);
+
+      return compileAndDump.buffer.toString();
+    },
+  );
+
+  // Remove absolute library URIs.
+  actual = actual.replaceAll(
+    new Uri.file(dartSdkPkgDir).toString(),
+    'DART_SDK/pkg/',
+  );
+
+  compareResultWithExpectationsFile(source, actual);
+}
+
+Future<Component> compileTestCaseToKernelProgram(
+  Uri sourceUri, {
+  required Target target,
+}) async {
+  final platformKernel = computePlatformBinariesLocation().resolve(
+    'vm_platform.dill',
+  );
+  final options = CompilerOptions()
+    ..target = target
+    ..additionalDills = <Uri>[platformKernel]
+    ..environmentDefines = {}
+    ..onDiagnostic = (CfeDiagnosticMessage message) {
+      fail("Compilation error: ${message.plainTextFormatted.join('\n')}");
+    };
+
+  final Component component = (await kernelForProgram(
+    sourceUri,
+    options,
+  ))!.component!;
+
+  // Make sure the library name is the same and does not depend on the order
+  // of test cases.
+  component.mainMethod!.enclosingLibrary.name = '#lib';
+  return component;
+}
+
+class CompileAndDumpIr extends RecursiveVisitor {
+  final FunctionRegistry functionRegistry = FunctionRegistry();
+  final RecognizedMethods recognizedMethods = CommonRecognizedMethods();
+  final buffer = StringBuffer();
+
+  @override
+  void visitProcedure(Procedure node) {
+    final function = functionRegistry.getFunction(
+      node,
+      isGetter: node.isGetter,
+      isSetter: node.isSetter,
+    );
+    // TODO: enable testing of other functions
+    if (function is! RegularFunction) {
+      return;
+    }
+    final graph = AstToIr(
+      function,
+      functionRegistry,
+      recognizedMethods,
+      enableAsserts: true,
+    ).buildFlowGraph();
+    buffer.writeln('--- ${node.name}');
+    buffer.writeln(
+      IrToText(graph, printDominators: true, printLoops: true).toString(),
+    );
+  }
+}
+
+class Difference {
+  final int line;
+  final String actual;
+  final String expected;
+
+  Difference(this.line, this.actual, this.expected);
+}
+
+Difference findFirstDifference(String actual, String expected) {
+  final actualLines = actual.split('\n');
+  final expectedLines = expected.split('\n');
+  int i = 0;
+  for (; i < actualLines.length && i < expectedLines.length; ++i) {
+    if (actualLines[i] != expectedLines[i]) {
+      return Difference(i + 1, actualLines[i], expectedLines[i]);
+    }
+  }
+  return Difference(
+    i + 1,
+    i < actualLines.length ? actualLines[i] : '<END>',
+    i < expectedLines.length ? expectedLines[i] : '<END>',
+  );
+}
+
+void compareResultWithExpectationsFile(
+  Uri source,
+  String actual, {
+  String expectFilePostfix = '',
+}) {
+  final baseFilename = '${source.toFilePath()}$expectFilePostfix';
+  final expectFile = new File('$baseFilename.expect');
+  final expected = expectFile.existsSync() ? expectFile.readAsStringSync() : '';
+
+  if (actual != expected) {
+    if (bool.fromEnvironment(kUpdateExpectations)) {
+      expectFile.writeAsStringSync(actual);
+      print("  Updated $expectFile");
+    } else {
+      Difference diff = findFirstDifference(actual, expected);
+      fail("""
+
+Result is different for the test case $source
+
+The first difference is at line ${diff.line}.
+Actual:   ${diff.actual}
+Expected: ${diff.expected}
+
+This failure can be caused by changes in the front-end if it starts generating
+different kernel AST for the same Dart programs.
+
+In order to re-generate expectations run test with -D$kUpdateExpectations=true VM option:
+
+  dart -DupdateExpectations=true pkg/cfg/test/ir_test.dart
+
+""");
+    }
+  }
+}
+
+void main() {
+  group('ir-test', () {
+    final testCasesDir = new Directory(dartSdkPkgDir + 'cfg/testcases');
+
+    for (var entry in testCasesDir.listSync(
+      recursive: true,
+      followLinks: false,
+    )) {
+      if (entry.path.endsWith(".dart")) {
+        test(entry.path, () => runTestCase(entry.uri));
+      }
+    }
+  });
+}
---
+++ pkg/cfg/testcases/unreachable_ast.dart
@@ -0,0 +1,60 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// ignore_for_file: dead_code
+
+class A {
+  int foo(int a, int b, int c) => a + b + c;
+}
+
+void bar(int a, int b, int c) {}
+
+void unreachable1(A obj) {
+  obj.foo(10, (throw 'Bye'), int.parse('11'));
+  print(1);
+}
+
+void unreachable2(A obj) {
+  bar(
+    int.parse('12'),
+    obj.foo(10, (throw 'Bye'), int.parse('13')),
+    int.parse('14'),
+  );
+  print(2);
+}
+
+void unreachableBreak1(int n) {
+  for (int i = 0; i < n; ++i) {
+    if (i > 2) {
+      throw 'Bye';
+      break;
+    }
+  }
+}
+
+void unreachableBreak2(int n) {
+  for (int i = 0; i < n; ++i) {
+    if (i > 2) {
+      throw 'Bye';
+      break;
+    }
+    return;
+  }
+}
+
+void unreachableFinally() {
+  try {
+    try {} finally {
+      print(1);
+      throw 'Bye';
+      print(2);
+    }
+  } finally {
+    print(3);
+    throw 'Bye-bye';
+    print(4);
+  }
+}
+
+void main() {}
