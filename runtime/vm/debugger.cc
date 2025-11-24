2c804fa25134aa14c85c6d05410902661e2c373e
Tess Strickland <sstrickl@google.com>
2025-11-24 04:39:04 -0800

    [vm] Handle the context of implicit instance closures in the debugger.
    Since the only captured variable in the context for an implicit
    instance closure is this, the allocation of a context object is
    elided and instead the instance is stored directly as the "context".
    
    In the debugger, create the elided context object for implicit instance
    closures when retrieving the saved context from an implicit instance
    closure activation frame.
    
    Without this, any attempt to GetSavedCurrentContext() on an
    ActivationFrame for an implicit instance closure (e.g., when
    --trace-debugger-stacktrace is enabled) breaks due to the assert
    in debug mode, and returns a null context (which isn't correct either)
    in non-debug modes.
    
    TEST=vm/dart/regress_cl_463021
    
    Change-Id: I2ba22407695f432a30d23d342f33dfe2a00ba308
    Cq-Include-Trybots: luci.dart.try:vm-dyn-linux-debug-x64-try
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/463021
    Commit-Queue: Tess Strickland <sstrickl@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>

--- runtime/vm/debugger.cc
+++ runtime/vm/debugger.cc
@@ -790,7 +790,14 @@ const Context& ActivationFrame::GetSavedCurrentContext() {
       }
       const auto variable_index = VariableIndex(var_info.index());
       obj = GetStackVar(variable_index);
-      if (obj.IsClosure()) {
+      if (function_.IsImplicitInstanceClosureFunction()) {
+        // The context of an implicit instance closure contains only one slot
+        // for "this", so the instance is stored as the context and accessed
+        // directly by generated code to avoid allocating a separate context.
+        // Here, we create the elided context object for debugging purposes.
+        ctx_ = Context::New(1);
+        ctx_.SetAt(0, obj);
+      } else if (obj.IsClosure()) {
         ASSERT(function().IsClosureCallDispatcher());
         // Closure.call frames.
         ctx_ = Closure::Cast(obj).GetContext();
