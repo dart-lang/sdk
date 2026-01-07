89717eae4652d49ee914f886a7c05cdffa025e81
Alexander Aprelev <aam@google.com>
2026-01-07 14:01:31 -0800

    [vm/ubsan] Fix NO_SANITIZE_UNDEFINED definition.
    This is a follow-up to 61c3fbc220778f7c5160f4bf3710731f2fae8a42 to make it accomodate android builds.
    
    Fixes https://ci.chromium.org/ui/p/dart/builders/ci.sandbox/vm-ffi-android-product-arm64c/10387 breakage.
    TEST=ci
    
    Change-Id: I0d18446014516b7de94ca1ed7336c0b98d699ee6
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/471324
    Commit-Queue: Alexander Aprelev <aam@google.com>
    Reviewed-by: Ryan Macnak <rmacnak@google.com>

--- runtime/platform/undefined_behavior_sanitizer.h
+++ runtime/platform/undefined_behavior_sanitizer.h
@@ -5,6 +5,10 @@
 #ifndef RUNTIME_PLATFORM_UNDEFINED_BEHAVIOR_SANITIZER_H_
 #define RUNTIME_PLATFORM_UNDEFINED_BEHAVIOR_SANITIZER_H_
 
+#ifdef __clang__
+#define NO_SANITIZE_UNDEFINED(check) [[clang::no_sanitize(check)]]
+#else
 #define NO_SANITIZE_UNDEFINED(check) [[gnu::no_sanitize(check)]]
+#endif
 
 #endif  // RUNTIME_PLATFORM_UNDEFINED_BEHAVIOR_SANITIZER_H_
