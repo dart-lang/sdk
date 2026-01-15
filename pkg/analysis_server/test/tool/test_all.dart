f904d1ebb2adc6d66c2f9c60ba7a4d46a65cebe0
Jake Macdonald <jakemac@google.com>
2026-01-15 09:20:12 -0800

    Add custom message equality class to support ignoring certain map keys, skipping id checks, and supporting future customization.
    Change-Id: I8da7c1ec4e6150ca699e5916b1d4ed9308c00bbb
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/472720
    Auto-Submit: Jake Macdonald <jakemac@google.com>
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>

--- pkg/analysis_server/test/tool/test_all.dart
+++ pkg/analysis_server/test/tool/test_all.dart
@@ -5,11 +5,13 @@
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import 'completion_metrics/test_all.dart' as completion_metrics;
+import 'log_player/test_all.dart' as log_player;
 import 'lsp_spec/test_all.dart' as lsp_spec;
 
 void main() {
   defineReflectiveSuite(() {
     completion_metrics.main();
+    log_player.main();
     lsp_spec.main();
   }, name: 'tool');
 }
