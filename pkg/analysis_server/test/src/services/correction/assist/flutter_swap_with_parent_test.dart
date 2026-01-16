49f2a5e1cb42c667f1a1bb0bd26edebdf53df323
Bogdan Ene-Doghitoiu <edb.bogdan@gmail.com>
2026-01-16 18:09:58 +0200

    [analysis_server] Fix duplication in swap with parent assist
    Update the "Swap with parent" assist to detect the named parameter (e.g.
    'body', 'child') holding the child widget, rather than defaulting to
    'child'.
    
    Fixes a bug where swapping a Scaffold's body would cause the swapped
    Widget to duplicate.
    
    Fixes issue #62350.

--- pkg/analysis_server/test/src/services/correction/assist/flutter_swap_with_parent_test.dart
+++ pkg/analysis_server/test/src/services/correction/assist/flutter_swap_with_parent_test.dart
@@ -219,4 +219,35 @@ startResize() {}
 ''');
     await assertNoAssist();
   }
+
+  Future<void> test_bodyWidgetNotDuplicated() async {
+    await resolveTestCode('''
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ^SizedBox(child: Placeholder()));
+  }
+}
+''');
+    await assertHasAssist('''
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Scaffold(
+        body: Placeholder(),
+      ),
+    );
+  }
+}
+''');
+  }
 }
