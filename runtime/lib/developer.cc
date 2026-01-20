dart-lang/sdk

commit 0fee95455c8b725991dfd5b3a5d6d017c8895065

[vm] Cleanup Profiler lifecycle
Move away from setting command line flags and calling Cleanup or Init
to reinitialize the profiler. Split starting and stopping the profiler
into separate methods and keep only one-time initialization/cleanup
in Init and Cleanup methods. Create a separate method for reconfiguring
the profiler Profiler::SetConfig.

This removes data races which were plaguing older code and makes it
simpler to reason about.

Fixes https://github.com/dart-lang/sdk/issues/62038
Fixes https://github.com/dart-lang/sdk/issues/61828

TEST=ci

Cq-Include-Trybots: luci.dart.try:vm-tsan-mac-release-arm64-try,vm-tsan-linux-release-x64-try
Change-Id: I679aa0a8724e39624e76abfad6814fe06a6a6964
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/472862
Commit-Queue: Slava Egorov <vegorov@google.com>
Reviewed-by: Martin Kustermann <kustermann@google.com>

runtime/lib/developer.cc
@@ -245,10 +245,10 @@ DEFINE_NATIVE_ENTRY(Developer_NativeRuntime_streamTimelineTo, 0, 5) {
       Integer::CheckedHandle(zone, arguments->NativeArgAt(4));
 
   if (enable_profiler.value()) {
-    FLAG_profiler = true;
-    FLAG_profile_period = sampling_interval.Value();
-    Profiler::UpdateSamplePeriod();
-    Profiler::UpdateRunningState();
+    Profiler::SetConfig({
+        .enabled = true,
+        .period_us = sampling_interval.Value(),
+    });
   }
 #endif
 
@@ -268,10 +268,9 @@ DEFINE_NATIVE_ENTRY(Developer_NativeRuntime_streamTimelineTo, 0, 5) {
 DEFINE_NATIVE_ENTRY(Developer_NativeRuntime_stopStreamingTimeline, 0, 0) {
 #if defined(SUPPORT_TIMELINE)
 #if defined(DART_INCLUDE_PROFILER)
-  FLAG_profiler = false;
-  FLAG_profile_period = 1000;
-  Profiler::UpdateSamplePeriod();
-  Profiler::UpdateRunningState();
+  Profiler::SetConfig({
+      .enabled = false,
+  });
 #endif
   Timeline::StopStreaming();
 #else

runtime/vm/dart_api_impl.cc
@@ -1563,21 +1563,15 @@ DART_EXPORT void Dart_EnterIsolate(Dart_Isolate isolate) {
 }
 
 DART_EXPORT void Dart_StartProfiling() {
-#if !defined(PRODUCT)
-  if (!FLAG_profiler) {
-    FLAG_profiler = true;
-    Profiler::Init();
-  }
-#endif  // !defined(PRODUCT)
+#if defined(DART_INCLUDE_PROFILER)
+  Profiler::SetConfig({.enabled = true});
+#endif  // defined(DART_INCLUDE_PROFILER)
 }
 
 DART_EXPORT void Dart_StopProfiling() {
-#if !defined(PRODUCT)
-  if (FLAG_profiler) {
-    Profiler::Cleanup();
-    FLAG_profiler = false;
-  }
-#endif  // !defined(PRODUCT)
+#if defined(DART_INCLUDE_PROFILER)
+  Profiler::SetConfig({.enabled = false});
+#endif  // defined(DART_INCLUDE_PROFILER)
 }
 
 DART_EXPORT void Dart_ThreadDisableProfiling() {
@@ -1611,7 +1605,7 @@ DART_EXPORT bool Dart_WriteProfileToTimeline(Dart_Port main_port,
 #if defined(PRODUCT)
   return false;
 #else
-  if (!FLAG_profiler) {
+  if (!Profiler::IsRunning()) {
     if (error != nullptr) {
       *error = Utils::StrDup("The profiler is not running.");
     }

runtime/vm/isolate.cc
@@ -3260,7 +3260,7 @@ void Isolate::PrintJSON(JSONStream* stream, bool ref) {
     jsobj.AddProperty("rootLib", lib);
   }
 
-  if (FLAG_profiler) {
+  if (Profiler::IsRunning()) {
     JSONObject tagCounters(&jsobj, "_tagCounters");
     vm_tag_counters()->PrintToJSONObject(&tagCounters);
   }

runtime/vm/os_thread.cc
@@ -10,6 +10,7 @@
 #include "platform/thread_sanitizer.h"
 #include "vm/lockers.h"
 #include "vm/log.h"
+#include "vm/profiler.h"
 #include "vm/thread_interrupter.h"
 #include "vm/timeline.h"
 
@@ -153,7 +154,7 @@ void OSThread::DisableThreadInterrupts() {
 void OSThread::EnableThreadInterrupts() {
   ASSERT(OSThread::Current() == this);
   uintptr_t old = thread_interrupt_disabled_.fetch_sub(1u);
-  if (FLAG_profiler && (old == 1)) {
+  if (Profiler::IsRunning() && (old == 1)) {
     ThreadInterrupter::WakeUp();
   }
   if (old == 0) {

runtime/vm/profiler.cc
@@ -160,7 +160,7 @@ class ProfilerStackWalker : public ValueObject {
       return true;
     }
 #if defined(DART_INCLUDE_PROFILER)
-    if (total_frames_ >= FLAG_max_profile_depth) {
+    if (total_frames_ >= Profiler::CurrentConfig().max_depth) {
       sample_->set_truncated_trace(true);
       return false;
     }
@@ -610,10 +610,15 @@ void Profiler::DumpStackTrace(uword sp, uword fp, uword pc, bool for_crash) {
 
 #if defined(DART_INCLUDE_PROFILER)
 
-RelaxedAtomic<bool> Profiler::initialized_ = false;
+Monitor* Profiler::monitor_ = nullptr;
+Profiler::Config Profiler::config_ = {.enabled = false,
+                                      .period_us = 0,
+                                      .max_depth = 0};
+RelaxedAtomic<bool> Profiler::running_ = false;
 SampleBlockBuffer* Profiler::sample_block_buffer_ = nullptr;
 Profiler::ProfileProcessorCallback Profiler::process_profile_callback_ =
     nullptr;
+
 bool SampleBlockProcessor::initialized_ = false;
 bool SampleBlockProcessor::shutdown_ = false;
 bool SampleBlockProcessor::drain_ = false;
@@ -623,24 +628,85 @@ ThreadJoinId SampleBlockProcessor::processor_thread_id_ =
 Monitor* SampleBlockProcessor::monitor_ = nullptr;
 
 void Profiler::Init() {
-  // Place some sane restrictions on user controlled flags.
-  SetSampleDepth(FLAG_max_profile_depth);
-  if (!FLAG_profiler) {
-    return;
-  }
-  ASSERT(!initialized_);
-  // The profiler may have been shutdown previously, in which case the sample
-  // buffer will have already been initialized.
-  if (sample_block_buffer_ == nullptr) {
-    intptr_t num_blocks = CalculateSampleBufferCapacity();
-    sample_block_buffer_ = new SampleBlockBuffer(num_blocks);
-  }
-  UpdateFlagProfilePeriod(FLAG_profile_period);
-  ThreadInterrupter::Init(FLAG_profile_period);
-  ThreadInterrupter::Startup();
+  monitor_ = new Monitor();
+  ThreadInterrupter::Init();
   SampleBlockProcessor::Init();
+  SetConfig({});
+}
+
+void Profiler::Cleanup() {
+  {
+    SafepointMonitorLocker lock(monitor_);
+    StopLocked();
+  }
+
+  SampleBlockProcessor::Cleanup();
+  ThreadInterrupter::Cleanup();
+  delete monitor_;
+}
+
+namespace {
+Profiler::Config NormalizeConfig(const Profiler::Config& config) {
+  const intptr_t kMinimumDepth = 2;
+  const intptr_t kMaximumDepth = 255;
+  const intptr_t kMinimumProfilePeriodUs = 50;
+  return {
+      .enabled = config.enabled,
+      .period_us = Utils::Maximum(kMinimumProfilePeriodUs, config.period_us),
+      .max_depth = Utils::Minimum(
+          kMaximumDepth,
+          Utils::Maximum(kMinimumDepth, config.max_depth.load())),
+  };
+}
+}  // namespace
+
+void Profiler::SetConfig(const Profiler::Config& config) {
+  SafepointMonitorLocker lock(monitor_);
+
+  const auto new_config = NormalizeConfig(config);
+  const auto old_config = config_;
+  config_ = new_config;
+
+  if (new_config.enabled != old_config.enabled) {
+    // Update running state.
+    if (new_config.enabled) {
+      StartLocked();
+    } else {
+      StopLocked();
+    }
+  } else if (old_config.enabled) {
+    // Check if we need to reconfigure a running profiler.
+    //
+    // Note: this will not resize the sampling buffer, you
+    // need to stop and restart the profiler to resize it.
+    if (new_config.period_us != old_config.period_us) {
+      ThreadInterrupter::SetInterruptPeriod(new_config.period_us);
+    }
+
+    // Profiling thread will automatically pickup a change in
+    // config_.max_depth, but to resize underlying buffer
+    // you need to start and stop the profiler.
+  }
+}
+
+void Profiler::StartLocked() {
+  RELEASE_ASSERT(!running_);
+
+  // The profiler may have been shutdown previously, in which case the sample
+  // buffer will have already been initialized. However it might be too small.
+  const intptr_t sample_buffer_capacity = CalculateSampleBufferCapacity();
+  if (sample_block_buffer_ != nullptr &&
+      sample_buffer_capacity > sample_block_buffer_->Capacity()) {
+    delete sample_block_buffer_;
+    sample_block_buffer_ = nullptr;
+  }
+  if (sample_block_buffer_ == nullptr) {
+    sample_block_buffer_ = new SampleBlockBuffer(sample_buffer_capacity);
+  }
+  ThreadInterrupter::SetInterruptPeriod(config_.period_us);
+  ThreadInterrupter::Startup();
   SampleBlockProcessor::Startup();
-  initialized_ = true;
+  running_ = true;
 }
 
 class SampleBlockCleanupVisitor : public IsolateVisitor {
@@ -654,45 +720,20 @@ class SampleBlockCleanupVisitor : public IsolateVisitor {
   }
 };
 
-void Profiler::Cleanup() {
-  if (!FLAG_profiler && !initialized_) {
+void Profiler::StopLocked() {
+  if (!running_) {
     return;
   }
-  ASSERT(initialized_);
-  ThreadInterrupter::Cleanup();
+
+  ThreadInterrupter::Shutdown();
 
   const bool should_drain = process_profile_callback_ != nullptr;
-  SampleBlockProcessor::Cleanup(should_drain);
+  SampleBlockProcessor::Shutdown(should_drain);
 
   SampleBlockCleanupVisitor visitor;
   Isolate::VisitIsolates(&visitor);
 
-  initialized_ = false;
-}
-
-void Profiler::UpdateRunningState() {
-  if (!FLAG_profiler && initialized_) {
-    Cleanup();
-  } else if (FLAG_profiler && !initialized_) {
-    Init();
-  }
-}
-
-void Profiler::SetSampleDepth(intptr_t depth) {
-  const int kMinimumDepth = 2;
-  const int kMaximumDepth = 255;
-  if (depth < kMinimumDepth) {
-    FLAG_max_profile_depth = kMinimumDepth;
-  } else if (depth > kMaximumDepth) {
-    FLAG_max_profile_depth = kMaximumDepth;
-  } else {
-    FLAG_max_profile_depth = depth;
-  }
-}
-
-static intptr_t SamplesPerSecond() {
-  const intptr_t kMicrosPerSec = 1000000;
-  return kMicrosPerSec / FLAG_profile_period;
+  running_ = false;
 }
 
 intptr_t Profiler::CalculateSampleBufferCapacity() {
@@ -706,25 +747,13 @@ intptr_t Profiler::CalculateSampleBufferCapacity() {
   // We use the fact that `ceil((float)a / (float)b) == (a + b - 1) / b` when
   // `a` and `b` are positive integers below.
   const intptr_t max_sample_chain_length =
-      (FLAG_max_profile_depth + Sample::kPCArraySizeInWords - 1) /
+      (config_.max_depth + Sample::kPCArraySizeInWords - 1) /
       Sample::kPCArraySizeInWords;
+  const intptr_t kMicrosPerSec = 1000000;
+  const intptr_t samples_per_second = kMicrosPerSec / config_.period_us;
   const intptr_t sample_count = FLAG_sample_buffer_duration *
-                                SamplesPerSecond() * max_sample_chain_length;
+                                samples_per_second * max_sample_chain_length;
   return (sample_count / SampleBlock::kSamplesPerBlock) + 1;
-}
-
-void Profiler::UpdateFlagProfilePeriod(intptr_t period) {
-  const int kMinimumProfilePeriod = 50;
-  if (period < kMinimumProfilePeriod) {
-    FLAG_profile_period = kMinimumProfilePeriod;
-  } else {
-    FLAG_profile_period = period;
-  }
-}
-
-void Profiler::UpdateSamplePeriod() {
-  UpdateFlagProfilePeriod(FLAG_profile_period);
-  ThreadInterrupter::SetInterruptPeriod(FLAG_profile_period);
 }
 
 SampleBlockBuffer::SampleBlockBuffer(intptr_t blocks,
@@ -1880,19 +1909,24 @@ ProcessedSampleBuffer::ProcessedSampleBuffer()
 
 void SampleBlockProcessor::Init() {
   ASSERT(!initialized_);
-  if (monitor_ == nullptr) {
-    monitor_ = new Monitor();
-  }
-  ASSERT(monitor_ != nullptr);
+  monitor_ = new Monitor();
   initialized_ = true;
-  shutdown_ = false;
+  shutdown_ = true;
   drain_ = false;
+}
+
+void SampleBlockProcessor::Cleanup() {
+  Shutdown();
+  initialized_ = false;
+  delete monitor_;
 }
 
 void SampleBlockProcessor::Startup() {
   ASSERT(initialized_);
   ASSERT(processor_thread_id_ == OSThread::kInvalidThreadJoinId);
   SafepointMonitorLocker startup_ml(monitor_);
+  shutdown_ = false;
+  drain_ = false;
   OSThread::Start("Dart Profiler SampleBlockProcessor", ThreadMain, 0);
   while (!thread_running_) {
     startup_ml.Wait();
@@ -1900,7 +1934,7 @@ void SampleBlockProcessor::Startup() {
   ASSERT(processor_thread_id_ != OSThread::kInvalidThreadJoinId);
 }
 
-void SampleBlockProcessor::Cleanup(bool drain /* = false */) {
+void SampleBlockProcessor::Shutdown(bool drain /* = false */) {
   {
     SafepointMonitorLocker shutdown_ml(monitor_);
     if (shutdown_) {
@@ -1924,7 +1958,6 @@ void SampleBlockProcessor::Cleanup(bool drain /* = false */) {
     OSThread::Join(processor_thread_id_);
   }
   processor_thread_id_ = OSThread::kInvalidThreadJoinId;
-  initialized_ = false;
   ASSERT(!thread_running_);
 }
 

runtime/vm/profiler.h
@@ -51,20 +51,43 @@ struct ProfilerCounters {
 #undef DECLARE_PROFILER_COUNTER
 };
 
+DECLARE_FLAG(int, profile_period);
+DECLARE_FLAG(int, max_profile_depth);
+
 class Profiler : public AllStatic {
  public:
+  // Initialize profiler's state on VM startup.
+  //
+  // To reconfigure profiler (e.g. to start or stop) use |SetConfig|.
   static void Init();
+
+  // Cleanup profiler's state on VM shutdown.
   static void Cleanup();
 
-  static void SetSampleDepth(intptr_t depth);
-  // Sets |FLAG_profile_period| to |max(period, 50)|.
-  static void UpdateFlagProfilePeriod(intptr_t period);
-  // Restarts sampling with a given profile period. This is called after the
-  // profile period is changed via the service protocol.
-  static void UpdateSamplePeriod();
-  // Starts or shuts down the profiler after --profiler is changed via the
-  // service protocol.
-  static void UpdateRunningState();
+  struct Config {
+    bool enabled = FLAG_profiler;
+    intptr_t period_us = FLAG_profile_period;
+    RelaxedAtomic<intptr_t> max_depth = FLAG_max_profile_depth;
+  };
+
+  // Configure the profiler.
+  //
+  // Can be used to start/stop the profiler (by toggling |Config::enabled|) or
+  // to reconfigure profiler (e.g. change sampling period or depth).
+  //
+  // Note: changing |Config::max_depth| or |Config::period_us| while profiler
+  // is running will not resize the underlying sample buffer. To resize the
+  // buffer stop and then restart the profiler.
+  static void SetConfig(const Config& config);
+  static const Config& CurrentConfig() { return config_; }
+
+  DART_FORCE_INLINE static bool IsRunning() {
+#if defined(DART_INCLUDE_PROFILER)
+    return running_;
+#else
+    return false;
+#endif  // defined(DART_INCLUDE_PROFILER)
+  }
 
   typedef void (*ProfileProcessorCallback)(Profile&);
 
@@ -109,6 +132,16 @@ class Profiler : public AllStatic {
   static void IsolateShutdown(Thread* thread);
 
  private:
+  // Start the profiler.
+  //
+  // Must be called by a thread holding |monitor_|.
+  static void StartLocked();
+
+  // Stop the profiler.
+  //
+  // Must be called by a thread holding |monitor_|.
+  static void StopLocked();
+
   static void DumpStackTrace(uword sp, uword fp, uword pc, bool for_crash);
 
   // Calculates the sample buffer capacity. Returns
@@ -122,7 +155,12 @@ class Profiler : public AllStatic {
   static void SampleThreadSingleFrame(Thread* thread,
                                       Sample* sample,
                                       uintptr_t pc);
-  static RelaxedAtomic<bool> initialized_;
+
+  static Monitor* monitor_;
+
+  static Profiler::Config config_;
+
+  static RelaxedAtomic<bool> running_;
 
   static SampleBlockBuffer* sample_block_buffer_;
 
@@ -783,6 +821,8 @@ class SampleBlockBuffer {
 
   intptr_t Size() const { return memory_->size(); }
 
+  intptr_t Capacity() const { return capacity_; }
+
   ProcessedSampleBuffer* BuildProcessedSampleBuffer(
       Isolate* isolate,
       SampleFilter* filter,
@@ -926,10 +966,17 @@ class ProcessedSampleBuffer : public ZoneAllocated {
 
 class SampleBlockProcessor : public AllStatic {
  public:
+  // Initialize the state on VM startup.
   static void Init();
 
+  // Cleanup the state on VM shutdown.
+  static void Cleanup();
+
+  // Start the worker thread.
   static void Startup();
-  static void Cleanup(bool drain = false);
+
+  // Shutdown the worker thread.
+  static void Shutdown(bool drain = false);
 
  private:
   static constexpr intptr_t kMaxThreads = 4096;

runtime/vm/profiler_service.cc
@@ -1587,9 +1587,8 @@ ProfileCode* Profile::GetCodeFromPC(uword pc, int64_t timestamp) {
 void Profile::PrintHeaderJSON(JSONObject* obj) {
   intptr_t pid = OS::ProcessId();
 
-  obj->AddProperty("samplePeriod", static_cast<intptr_t>(FLAG_profile_period));
-  obj->AddProperty("maxStackDepth",
-                   static_cast<intptr_t>(FLAG_max_profile_depth));
+  obj->AddProperty("samplePeriod", Profiler::CurrentConfig().period_us);
+  obj->AddProperty("maxStackDepth", Profiler::CurrentConfig().max_depth.load());
   obj->AddProperty("sampleCount", sample_count());
   obj->AddPropertyTimeMicros("timeOriginMicros", min_time());
   obj->AddPropertyTimeMicros("timeExtentMicros", GetTimeSpan());

runtime/vm/profiler_test.cc
@@ -62,14 +62,16 @@ class DisableNativeProfileScope : public ValueObject {
 class MaxProfileDepthScope : public ValueObject {
  public:
   explicit MaxProfileDepthScope(intptr_t new_max_depth)
-      : FLAG_max_profile_depth_(FLAG_max_profile_depth) {
-    Profiler::SetSampleDepth(new_max_depth);
+      : saved_config_(Profiler::CurrentConfig()) {
+    auto new_config = saved_config_;
+    new_config.max_depth = new_max_depth;
+    Profiler::SetConfig(new_config);
   }
 
-  ~MaxProfileDepthScope() { Profiler::SetSampleDepth(FLAG_max_profile_depth_); }
+  ~MaxProfileDepthScope() { Profiler::SetConfig(saved_config_); }
 
  private:
-  const intptr_t FLAG_max_profile_depth_;
+  const Profiler::Config saved_config_;
 };
 
 class ProfileSampleBufferTestHelper : public SampleVisitor {
@@ -287,13 +289,6 @@ class AllocationFilter : public SampleFilter {
   bool enable_vm_ticks_;
 };
 
-static void EnableProfiler() {
-  if (!FLAG_profiler) {
-    FLAG_profiler = true;
-    Profiler::Init();
-  }
-}
-
 class ProfileStackWalker {
  public:
   explicit ProfileStackWalker(Profile* profile, bool as_func = false)
@@ -469,7 +464,7 @@ class ProfileStackWalker {
 };
 
 ISOLATE_UNIT_TEST_CASE(Profiler_TrivialRecordAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -552,7 +547,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_NullSampleBuffer) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_ToggleRecordAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
 
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
@@ -635,7 +630,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ToggleRecordAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_CodeTicks) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -708,7 +703,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_CodeTicks) {
   }
 }
 ISOLATE_UNIT_TEST_CASE(Profiler_FunctionTicks) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -782,7 +777,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_FunctionTicks) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_IntrinsicAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript = "double foo(double a, double b) => a + b;";
@@ -846,7 +841,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_IntrinsicAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_ArrayAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -927,7 +922,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ArrayAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_ContextAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -986,7 +981,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ContextAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_ClosureAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1049,7 +1044,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ClosureAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_TypedArrayAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1123,7 +1118,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_TypedArrayAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_StringAllocation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript = "String foo(String a, String b) => a + b;";
@@ -1197,7 +1192,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_StringAllocation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_StringInterpolation) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript = "String foo(String a, String b) => '$a | $b';";
@@ -1277,7 +1272,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_StringInterpolation) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_FunctionInline) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   SetFlagScope<int> sfs(&FLAG_optimization_counter_threshold, 30000);
@@ -1401,7 +1396,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_InliningIntervalBoundary) {
   // This test checks the profiler service takes this into account; see
   // ProfileBuilder::ProcessFrame.
 
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   SetFlagScope<int> sfs(&FLAG_optimization_counter_threshold, 30000);
@@ -1509,7 +1504,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_InliningIntervalBoundary) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_ChainedSamples) {
-  EnableProfiler();
+  Dart_StartProfiling();
   MaxProfileDepthScope mpds(32);
   DisableNativeProfileScope dnps;
 
@@ -1617,7 +1612,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ChainedSamples) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_BasicSourcePosition) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1681,7 +1676,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_BasicSourcePosition) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_BasicSourcePositionOptimized) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1763,7 +1758,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_BasicSourcePositionOptimized) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_SourcePosition) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1854,7 +1849,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_SourcePosition) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_SourcePositionOptimized) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -1962,7 +1957,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_SourcePositionOptimized) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_BinaryOperatorSourcePosition) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
   const char* kScript =
@@ -2061,7 +2056,7 @@ ISOLATE_UNIT_TEST_CASE(Profiler_BinaryOperatorSourcePosition) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_BinaryOperatorSourcePositionOptimized) {
-  EnableProfiler();
+  Dart_StartProfiling();
   DisableNativeProfileScope dnps;
   DisableBackgroundCompilationScope dbcs;
 
@@ -2217,7 +2212,7 @@ static uword FindPCForTokenPosition(const Code& code, TokenPosition tp) {
 }
 
 ISOLATE_UNIT_TEST_CASE(Profiler_GetSourceReport) {
-  EnableProfiler();
+  Dart_StartProfiling();
   const char* kScript =
       "int doWork(i) => i * i;\n"
       "int main() {\n"
@@ -2446,9 +2441,10 @@ ISOLATE_UNIT_TEST_CASE(Profiler_ProfileCodeTableTest) {
 // Try to hit any races in related to setting TLS and Isolate::mutator_thread_.
 // https://github.com/flutter/flutter/issues/134548
 ISOLATE_UNIT_TEST_CASE(Profiler_EnterExitIsolate) {
-  EnableProfiler();
-  Profiler::UpdateFlagProfilePeriod(50);  // Microseconds.
-  Profiler::UpdateSamplePeriod();
+  Profiler::SetConfig({
+      .enabled = true,
+      .period_us = 50,
+  });
 
   const char* kScript = "main() => null;\n";
   const Library& root_library = Library::Handle(LoadTestScript(kScript));
@@ -2465,9 +2461,10 @@ ISOLATE_UNIT_TEST_CASE(Profiler_EnterExitIsolate) {
 // sampling thread.
 // https://github.com/dart-lang/sdk/issues/62332
 ISOLATE_UNIT_TEST_CASE(Profiler_ThreadEnableDisableProfiler) {
-  EnableProfiler();
-  Profiler::UpdateFlagProfilePeriod(50);  // Microseconds.
-  Profiler::UpdateSamplePeriod();
+  Profiler::SetConfig({
+      .enabled = true,
+      .period_us = 50,
+  });
 
   for (intptr_t i = 0; i < 100000; i++) {
     Dart_ThreadDisableProfiling();
@@ -2479,9 +2476,10 @@ ISOLATE_UNIT_TEST_CASE(Profiler_UpdateRunningState) {
   Isolate* isolate = Isolate::Current();
   SampleFilter filter(isolate->main_port(), Thread::kMutatorTask, -1, -1);
 
-  EnableProfiler();
-  Profiler::UpdateFlagProfilePeriod(50);  // Microseconds.
-  Profiler::UpdateSamplePeriod();
+  Profiler::SetConfig({
+      .enabled = true,
+      .period_us = 50,
+  });
 
   const char* kScript = R"(
 void main() {
@@ -2503,8 +2501,7 @@ void main() {
 
   // Switch profiler off and clear the sample block buffer to discard all
   // collected samples.
-  FLAG_profiler = false;
-  Profiler::UpdateRunningState();
+  Dart_StopProfiling();
   delete Profiler::sample_block_buffer();
   Profiler::set_sample_block_buffer(new SampleBlockBuffer());
 
@@ -2518,8 +2515,7 @@ void main() {
   }
 
   // Enable profiler again.
-  FLAG_profiler = true;
-  Profiler::UpdateRunningState();
+  Dart_StartProfiling();
 
   // Test again that samples are collected.
   Invoke(root_library, "main");

commit 5cf77e660a2c3e6e1c722ca09ba042b32039fa42

[vm] Add an intptr_t cast to fix 32-bit builds
Compilation issue introduced by
commit 0fee95455c8b725991dfd5b3a5d6d017c8895065.

TEST=ci

Cq-Include-Trybots: luci.dart.try:vm-ffi-android-release-arm-try
Change-Id: I087b9b6db438d77f9cb147afa7ce00256a6a6964
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/474401
Commit-Queue: Slava Egorov <vegorov@google.com>
Reviewed-by: Daco Harkes <dacoharkes@google.com>

runtime/lib/developer.cc
@@ -247,7 +247,7 @@ DEFINE_NATIVE_ENTRY(Developer_NativeRuntime_streamTimelineTo, 0, 5) {
   if (enable_profiler.value()) {
     Profiler::SetConfig({
         .enabled = true,
-        .period_us = sampling_interval.Value(),
+        .period_us = static_cast<intptr_t>(sampling_interval.Value()),
     });
   }
 #endif