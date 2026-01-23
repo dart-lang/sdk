9c81de5338f7c68f727011703d2b2f6ada6d05ad
Lasse R.H. Nielsen <lrn@google.com>
2026-01-23 01:59:13 -0800

    Update to primary constructor tests for extension types.
    Checks that there is no inference from super-interface
    members for the representation type/getter.
    
    Check that object-instance-member-names cannot be
    used as extension member declarations,
    both before and after primary constructors.
    
    Adds a few more tests for good measure.
    (Could be redundant.)
    
    Change-Id: Idb75cc0bcbc9718ffec5967ed6ed1463a1c3a149
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/475061
    Commit-Queue: Lasse Nielsen <lrn@google.com>
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Kallen Tu <kallentu@google.com>

--- tests/language/primary_constructors/header/extension_type_error_test.dart
+++ tests/language/primary_constructors/header/extension_type_error_test.dart
@@ -3,10 +3,15 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // It is a compile-time error if an extension type does not contain a declaring
-// constructor that has exactly one declaring parameter which is final. This is
-// the test for in-header constructors
+// constructor that has exactly one declaring parameter which is final.
+// With primary constructors, the extension type representation type/value name
+// declaration must still have exactly one parameter, which must be declaring.
+// The `final` can be omitted, but a `var` cannot be used.
 
 // SharedOptions=--enable-experiment=primary-constructors
+
+// -----------------
+// No `var` allowed.
 
 extension type ET1(var int i);
 //                 ^
@@ -18,19 +23,97 @@ extension type ET2(var i);
 // [analyzer] unspecified
 // [cfe] unspecified
 
+// --------------------------------
+// Must have exactly one parameter.
+
 extension type ET3(final i, final x);
-//                 ^
+//                          ^
 // [analyzer] unspecified
 // [cfe] unspecified
 
-// Two `final` declaring parameters are inferred.
 extension type ET4(int i, int x);
-//                 ^
+//                        ^
 // [analyzer] unspecified
 // [cfe] unspecified
 
-// A final modifier on the first parameter is inferred.
 extension type ET5(int i, final x);
-//                 ^
+//                        ^
 // [analyzer] unspecified
 // [cfe] unspecified
+
+// --------------------
+// The extension type representation parameter cannot be covariant.
+// (Declaring parameters of a class can.)
+// Neither can have any other modifier that could otherwise apply to
+// a parameter or instance variable.
+
+extension type ET6(covariant int i);
+//                 ^^^^^^^^^
+// [analyzer] SYNTACTIC_ERROR.INVALID_COVARIANT_MODIFIER_IN_PRIMARY_CONSTRUCTOR
+// [cfe] The 'covariant' modifier can only be used on non-final declaring parameters.
+
+extension type ET7(late int i);
+//                 ^^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'late' here.
+
+extension type ET8(static int i);
+//                 ^^^^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'static' here.
+
+extension type ET9(external final int i);
+//                 ^^^^^^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'external' here.
+
+extension type ET10(abstract final int i);
+//                  ^^^^^^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'abstract' here.
+
+extension type ET11(const int i);
+//                  ^^^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'const' here.
+
+// --------------------------------------------------------------
+// Still cannot declare member with same name as `Object` instance members,
+// which includes the representation variable.
+
+extension type ET12({required final int hashCode});
+//                                      ^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'hashCode'.
+
+extension type ET13({required final dynamic Function(Invocation) noSuchMethod});
+//                                                               ^^^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'noSuchMethod'.
+
+extension type ET14({required final Type runtimeType});
+//                                       ^^^^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'runtimeType'.
+
+extension type ET15({required final String Function() toString});
+//                                                    ^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'toString'.
+
+// Doesn't matter how the parameter list is written.
+
+extension type ET16(final int hashCode);
+//                            ^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'hashCode'.
+
+extension type ET17({final int hashCode = 0});
+//                             ^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'hashCode'.
+
+extension type ET18([final int hashCode = 0]);
+//                             ^^^^^^^^
+// [analyzer] COMPILE_TIME_ERROR.EXTENSION_TYPE_DECLARES_MEMBER_OF_OBJECT
+// [cfe] This extension member conflicts with Object member 'hashCode'.
