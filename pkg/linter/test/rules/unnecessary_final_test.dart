a6d78c53df5068c075b92ff8df186c1b44e05642
Sam Rawlins <srawlins@google.com>
2026-01-28 19:42:39 -0800

    linter: Support primary constructors in unnecessary_final
    Fixes https://github.com/dart-lang/sdk/issues/62527
    
    Change-Id: I203d18eae897dd3016b8a36e8132b0be7121e240
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/476145
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/linter/test/rules/unnecessary_final_test.dart
+++ pkg/linter/test/rules/unnecessary_final_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:analyzer/src/diagnostic/diagnostic.dart' as diag;
 import 'package:test_reflective_loader/test_reflective_loader.dart';
 
 import '../rule_test_support.dart';
@@ -193,6 +194,26 @@ f() {
 }
 ''',
       [lint(79, 5)],
+    );
+  }
+
+  test_parameter_declaring() async {
+    await assertNoDiagnostics(r'''
+class C(final int x);
+''');
+  }
+
+  test_parameter_declaring_primaryConstructorsDisabled() async {
+    await assertDiagnostics(
+      r'''
+// @dart = 3.10
+class C(final int x);
+''',
+      [
+        error(diag.experimentNotEnabled, 23, 1),
+        lint(24, 5),
+        error(diag.experimentNotEnabledOffByDefault, 36, 1),
+      ],
     );
   }
 
