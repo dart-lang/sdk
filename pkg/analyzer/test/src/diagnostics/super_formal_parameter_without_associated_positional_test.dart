16139e48397cec664ca93002377b9c14ac4b66bb
Konstantin Shcheglov <scheglov@google.com>
2026-01-31 10:40:39 -0800

    DeCo. Stop reporting superFormalParameterWithoutAssociatedPositional if the super constructor is unresolved.
    Change-Id: I42dd992b2edd4a50050df1c2266dee9129c6013c
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/476870
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>

--- pkg/analyzer/test/src/diagnostics/super_formal_parameter_without_associated_positional_test.dart
+++ pkg/analyzer/test/src/diagnostics/super_formal_parameter_without_associated_positional_test.dart
@@ -16,33 +16,29 @@ main() {
 @reflectiveTest
 class SuperFormalParameterWithoutAssociatedPositionalTest
     extends PubPackageResolutionTest {
-  test_explicit_optional() async {
+  test_class_primary_optionalPositional_01() async {
     await assertErrorsInCode(
       r'''
 class A {}
 
-class B extends A {
-  B([super.a]) : super();
-}
+class B([super.a]) extends A {}
 ''',
-      [error(diag.superFormalParameterWithoutAssociatedPositional, 43, 1)],
+      [error(diag.superFormalParameterWithoutAssociatedPositional, 27, 1)],
     );
   }
 
-  test_explicit_required() async {
+  test_class_primary_requiredPositional_01() async {
     await assertErrorsInCode(
       r'''
 class A {}
 
-class B extends A {
-  B(super.a) : super();
-}
+class B(super.a) extends A {}
 ''',
-      [error(diag.superFormalParameterWithoutAssociatedPositional, 42, 1)],
+      [error(diag.superFormalParameterWithoutAssociatedPositional, 26, 1)],
     );
   }
 
-  test_implicit_optional() async {
+  test_class_secondary_optionalPositional_01() async {
     await assertErrorsInCode(
       r'''
 class A {}
@@ -55,7 +51,7 @@ class B extends A {
     );
   }
 
-  test_implicit_required() async {
+  test_class_secondary_requiredPositional_01() async {
     await assertErrorsInCode(
       r'''
 class A {}
@@ -65,6 +61,102 @@ class B extends A {
 }
 ''',
       [error(diag.superFormalParameterWithoutAssociatedPositional, 42, 1)],
+    );
+  }
+
+  test_class_secondary_requiredPositional_12() async {
+    await assertErrorsInCode(
+      r'''
+class A {
+  A(int a);
+}
+
+class B extends A {
+  B(super.a, super.b);
+}
+''',
+      [error(diag.superFormalParameterWithoutAssociatedPositional, 64, 1)],
+    );
+  }
+
+  test_enum_primary_requiredPositional_01() async {
+    await assertErrorsInCode(
+      r'''
+enum E(super.a) {
+  v(0);
+}
+''',
+      [error(diag.superFormalParameterWithoutAssociatedPositional, 13, 1)],
+    );
+  }
+
+  test_enum_secondary_requiredPositional_01() async {
+    await assertErrorsInCode(
+      r'''
+enum E {
+  v(0);
+  const E(super.x);
+}
+''',
+      [error(diag.superFormalParameterWithoutAssociatedPositional, 33, 1)],
+    );
+  }
+
+  test_recovery_hasSuperClass_noSuperConstructor_primary() async {
+    await assertErrorsInCode(
+      r'''
+class A {
+  A(int x);
+}
+
+class B(super.a) extends A {
+  this : super.named();
+}
+''',
+      [error(diag.undefinedConstructorInInitializer, 63, 13)],
+    );
+  }
+
+  test_recovery_hasSuperClass_noSuperConstructor_secondary() async {
+    await assertErrorsInCode(
+      r'''
+class A {
+  A(int x);
+}
+
+class B extends A {
+  B(super.x) : super.named();
+}
+''',
+      [error(diag.undefinedConstructorInInitializer, 60, 13)],
+    );
+  }
+
+  test_recovery_noSuperClass_primary() async {
+    await assertErrorsInCode(
+      r'''
+class B(super.a) extends A {
+  this : super.named();
+}
+''',
+      [
+        error(diag.extendsNonClass, 25, 1),
+        error(diag.undefinedConstructorInInitializer, 38, 13),
+      ],
+    );
+  }
+
+  test_recovery_noSuperClass_secondary() async {
+    await assertErrorsInCode(
+      r'''
+class B extends A {
+  B(super.x) : super.named();
+}
+''',
+      [
+        error(diag.extendsNonClass, 16, 1),
+        error(diag.undefinedConstructorInInitializer, 35, 13),
+      ],
     );
   }
 }
