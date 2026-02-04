eebcb046b95554052a9d6f5e3e11dbd1820faf05
Sam Rawlins <srawlins@google.com>
2026-02-04 13:50:39 -0800

    analyzer: Refactor @doNotSubmit warnings to use ElementUsageReporter
    This wasn't as much of a slam dunk as I was hoping for, but I think this
    change still successfully simplifies reporting uses of `@doNotSubmit`-
    annotated elements.
    
    I can summarize the change as three steps:
    
    1. Create subclasses of ElementUsageReporter and ElementUsageSet for
       tracking `@doNotSubmit`-annotated elements, and use it in
       BestPracticesVerifier. Then remove all of the custom logic for
       `@doNotSubmit`-annotated elements from BestPracticesVerifier.
    
    2. We also change the behavior of the `@doNotSubmit`-reporting code to
       also report on usage of a `@doNotSubmit`-annotated element, even if
       that element is not a valid target for the annotation. I believe this
       change is good, and not a regression.
    
    3. The third step comes about from testing. Flutter has a local function
       which is annotated with `@useResult`, which is allowed on
       `TargetKind.function`. We had no tests of annotating local functions.
       So I add a new test case for this.
    
    Change-Id: I561475914ceac80d2917cc56a415c531a46bfe7e
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/478081
    Reviewed-by: Paul Berry <paulberry@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

---
+++ pkg/analyzer/lib/src/error/do_not_submit_member_use_verifier.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/ast/syntactic_entity.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/src/diagnostic/diagnostic.dart' as diag;
+import 'package:analyzer/src/error/element_usage_detector.dart';
+import 'package:analyzer/src/error/listener.dart';
+
+/// Instance of [ElementUsageReporter] for reporting uses of elements annotated
+/// with `@doNotSubmit`.
+class DoNotSubmitElementUsageReporter implements ElementUsageReporter<()> {
+  final DiagnosticReporter _diagnosticReporter;
+
+  DoNotSubmitElementUsageReporter({
+    required DiagnosticReporter diagnosticReporter,
+  }) : _diagnosticReporter = diagnosticReporter;
+
+  @override
+  void report(
+    SyntacticEntity errorEntity,
+    String displayName,
+    () tagInfo, {
+    required bool isInSamePackage,
+  }) {
+    _diagnosticReporter.report(
+      diag.invalidUseOfDoNotSubmitMember
+          .withArguments(name: displayName)
+          .at(errorEntity),
+    );
+  }
+}
+
+/// Instance of [ElementUsageSet] for elements annotated with `@doNotSubmit`.
+class DoNotSubmitElementUsageSet implements ElementUsageSet<()> {
+  const DoNotSubmitElementUsageSet();
+
+  @override
+  ()? getTagInfo(Element element) =>
+      element.metadata.hasDoNotSubmit ? () : null;
+}
