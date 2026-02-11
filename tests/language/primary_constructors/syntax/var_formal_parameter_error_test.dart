1f534ed52d08d264fb3b81c116bde96442159882
Kallen Tu <kallentu@google.com>
2026-02-11 15:20:52 -0800

    [tests] Primary constructors - var tests, update final tests, primary initializer scope.
    Tests for the following:
    - Declaration cycles.
    - A compile-time error occurs if an assignment to a primary parameter
    occurs in the initializing expression of a non-late instance variable.
    - A compile-time error occurs if an assignment to a primary parameter
    occurs in the initializer list of the body part of a primary
    constructor.
    - Late variables cannot access primary constructor parameters.
    - Declarations without primary constructors don't have a primary
    initializer scope.
    - The primary initializer scope is the current scope for the
    initializing expression, if any, of each non-late instance variable
    declaration. It is also the current scope for the initializer list in the body part of the primary constructor, if any.
    - The primary parameter scope is the current scope for the body of the
    body part of the primary constructor, if any. Every primary parameter
    which is not declaring, not initializing, and not a super parameter is
    introduced into this scope.
    - It's a compile-time error to write a formal non-declaring parameter with a `var` keyword and no type annotation.
    
    Bug: https://github.com/dart-lang/sdk/issues/61687
    Change-Id: If4dde05ac61fb69ef06a85df3bc36a47b1df57b5
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/479420
    Reviewed-by: Erik Ernst <eernst@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/primary_constructors/syntax/var_formal_parameter_error_test.dart
@@ -0,0 +1,65 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// It's a compile-time error to write a formal non-declaring parameter with a
+// `var` keyword and no type annotation.
+
+// SharedOptions=--enable-experiment=primary-constructors
+
+void fn(var x, var y) {}
+//      ^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'var' here.
+//             ^^^
+// [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+// [cfe] Can't have modifier 'var' here.
+
+class C {
+  C.ctor(var x) {}
+  //     ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+
+  void method1(var x) {}
+  //           ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+
+  void method2([var y = 1]) {}
+  //            ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+}
+
+enum E(final int x) {
+  e(1);
+  void method1(var x) {}
+  //           ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+
+  void method2({required var y}) {}
+  //                     ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+}
+
+extension type ET(final int x) {
+  void method1(var x) {}
+  //           ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+
+  void method2({var y = 1}) {}
+  //            ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+}
+
+void main() {
+  [1, 4, 6, 8].forEach((var value) => print(value + 2));
+  //                    ^^^
+  // [analyzer] SYNTACTIC_ERROR.EXTRANEOUS_MODIFIER
+  // [cfe] Can't have modifier 'var' here.
+}
