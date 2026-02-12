32b6f24e4d465a7511dbf7717a749f31f331e046
Johnni Winther <johnniwinther@google.com>
2026-02-12 00:38:54 -0800

    [cfe][PrimaryConstructors] Enable features in tests
    This enables primary-constructors and private-named-parameters by default in CFE expectation tests.
    
    Test are update accordingly; some are annotated with `@dart=3.10` to opt out of the features, some cloned to support both opt-in and opt-out, and some are modified to use opt-in before only.
    
    Part of #61700
    
    Change-Id: Iae9e5d188d9b49d4264d17dc989dbaeee7321845
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/479620
    Reviewed-by: Konstantin Shcheglov <scheglov@google.com>

--- pkg/front_end/testcases/general/qualified.dart
+++ pkg/front_end/testcases/general/qualified.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=3.10
+
 library test.qualified.main;
 
 import "qualified_lib.dart" as lib;
