6005ca0cf7f96251f4f433f12396ea0da00665f4
Kallen Tu <kallentu@google.com>
2026-02-20 16:03:44 -0800

    [tests] Primary constructors - Wildcards, assignment, initializing formals.
    Tests the following:
    - A compile-time error occurs if an assignment to a primary parameter
    occurs in the initializing expression of a non-late instance variable.
    - A compile-time error occurs if a declaration has a primary constructor
    with an initializing formal and no instance variable of the same name.
    - Declaring parameters named `_` cannot be declared multiple times in a primary constructor.
    - Wildcard variables cannot be referenced in the initializing
    expressions of non-late instance variables or in the initializer list of
    the body part of the primary constructor.
    - Declaring parameters named `_` are allowed. Accessing `_` is valid in
    the body part of a primary constructor if there's an instance variable
    with the name `_`.
    
    Bug: https://github.com/dart-lang/language/issues/4634, https://github.com/dart-lang/sdk/issues/61687
    Change-Id: Id2cb98bd78e31292894610b595174fa682bdc1ae
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/481321
    Reviewed-by: Erik Ernst <eernst@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

---
+++ tests/language/primary_constructors/wildcard_declaring_parameters_error_test.dart
@@ -0,0 +1,90 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// SharedOptions=--enable-experiment=primary-constructors
+
+import 'package:expect/expect.dart';
+
+// --------------------
+// Declaring parameters named `_` cannot be declared multiple times in a primary
+// constructor.
+
+class CMultiple(var int _, var int _);
+//                         ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+class DMultiple(final int _, final int _);
+//                                     ^
+// [analyzer] unspecified
+// [cfe] unspecified
+
+enum EnumMultiple(final int _, final int _) {
+  //                                     ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+  e1(1, 2);
+}
+
+// --------------------
+// Wildcard variables cannot be referenced in the initializing expressions of
+// non-late instance variables or in the initializer list of the body part of
+// the primary constructor.
+
+class C(var int _) {
+  int x = _;
+  //      ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  this : assert(_ > 0);
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+class D(final int _) {
+  int x = _;
+  //      ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  this : assert(_ > 0);
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+class E(int _, int _) {
+  int x = _;
+  //      ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  this : assert(_ > 0);
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+enum E1(var int _) {
+  e(1);
+
+  final int x = _;
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+
+  this : assert(_ > 0);
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
+
+extension type Ext(int _) {
+  this : assert(_ > 0);
+  //            ^
+  // [analyzer] unspecified
+  // [cfe] unspecified
+}
