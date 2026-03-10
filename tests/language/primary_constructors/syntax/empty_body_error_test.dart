d87e8f866ca58577e19c2771a2ec220bf9a32a6d
Kallen Tu <kallentu@google.com>
2026-03-10 14:58:45 -0700

    [tests] Add test for explicit class body on mixin applications.
    Adding in an extra test just to make sure this isn't
    possible. Don't know if we already have a test for this,
    but I couldn't find anything.
    
    Change-Id: Id2ce089e070cc139389e805f1b8b7f987a438bfd
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/486525
    Reviewed-by: Lasse Nielsen <lrn@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

--- tests/language/primary_constructors/syntax/empty_body_error_test.dart
+++ tests/language/primary_constructors/syntax/empty_body_error_test.dart
@@ -2,10 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// SharedOptions=--enable-experiment=primary-constructors
+
 // Enums that have an empty body (i.e. `;`) can be parsed, but will cause a
 // compile-time error when there's no enum constant declared.
-
-// SharedOptions=--enable-experiment=primary-constructors
 
 enum E1;
 //   ^
@@ -26,3 +26,24 @@ enum const E4(final int x);
 //   ^
 // [analyzer] unspecified
 // [cfe] unspecified
+
+// Mixin application classes cannot have an explicit (empty) class body.
+
+class S;
+mixin M on S;
+class C = S with M {}
+//               ^
+// [analyzer] SYNTACTIC_ERROR.EXPECTED_TOKEN
+// [cfe] Expected ';' after this.
+//                 ^
+// [analyzer] SYNTACTIC_ERROR.EXPECTED_EXECUTABLE
+// [cfe] Expected a declaration, but got '{'.
+
+class I;
+class C2 = S with M implements I {}
+//                             ^
+// [analyzer] SYNTACTIC_ERROR.EXPECTED_TOKEN
+// [cfe] Expected ';' after this.
+//                               ^
+// [analyzer] SYNTACTIC_ERROR.EXPECTED_EXECUTABLE
+// [cfe] Expected a declaration, but got '{'.
