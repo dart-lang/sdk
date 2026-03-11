6ecc8bee8fbd1191d7102fd10dde8083b3280f9b
Jens Johansen <jensj@google.com>
2026-03-11 02:18:58 -0700

    [analyzer] Find TODOs mainly without regex
    The RegEx engine in the VM was updated in
    e443b89f238cf97b7d33eb54ca34fc19ef44f52f which caused the analyzer
    analyzing the CFE to use ~150 mio instructions more.
    
    Part of this was an increased cost in TODO processing which relied on
    regex.
    The regex was long, complicated (and buggy) and the method scraping todo
    comments (including children) when analyzing the CFE cost ~265 mio
    instructions (up from ~191 mio instructions before the regex engine update) (numbers from `valgrind --tool=callgrind`).
    
    This CL:
    * Adds a test that didn't pass before.
    * Removes the usage of the complicated regex with "normal code" that -
      while longer - is likely easier to debug in practice.
    * Reduces the cost of scraping to ~92 mio (numbers from
      `valgrind --tool=callgrind`).
    
    `perf stat` on this change with GC disabled goes from
    
    ```
        40,938,677,452      instructions:u
        40,935,421,892      instructions:u
        40,935,153,915      instructions:u
    ```
    
    to
    
    ```
        40,756,218,191      instructions:u
        40,756,538,467      instructions:u
        40,756,528,402      instructions:u
    ```
    
    A ~179 mio savings.
    
    Change-Id: Id02c42d8be2ddbd24b96e33cc89320342efe7f4b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/486800
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

--- pkg/analyzer/lib/src/dart/error/todo_codes.dart
+++ pkg/analyzer/lib/src/dart/error/todo_codes.dart
@@ -20,35 +20,6 @@ class Todo {
     'UNDONE': diag.undone,
   };
 
-  /// This matches the two common Dart task styles
-  ///
-  /// * `TODO`:
-  /// * `TODO`(username):
-  ///
-  /// As well as
-  /// * `TODO`
-  ///
-  /// But not
-  // * `todo`
-  /// * `TODOS`
-  ///
-  /// It also supports wrapped TODOs where the next line is indented by a space:
-  ///
-  ///   /**
-  ///    * `TODO`(username): This line is
-  ///    *  wrapped onto the next line
-  ///    */
-  ///
-  /// The matched kind of the `TODO` (`TODO`, `FIXME`, etc.) is returned in named
-  /// captures of "kind1", "kind2" (since it is not possible to reuse a name
-  /// across different parts of the regex).
-  static RegExp TODO_REGEX = RegExp(
-    '([\\s/\\*])(((?<kind1>$_TODO_KIND_PATTERN)[^\\w\\d][^\\r\\n]*(?:\\n\\s*\\*  [^\\r\\n]*)*)'
-    '|((?<kind2>$_TODO_KIND_PATTERN):?\$))',
-  );
-
-  static final _TODO_KIND_PATTERN = _codes.keys.join('|');
-
   Todo._() {
     throw UnimplementedError('Do not construct');
   }
