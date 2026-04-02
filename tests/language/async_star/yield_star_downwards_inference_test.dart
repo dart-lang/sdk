6cc1bdb1ad8d41a90ca1464cfe7b7121749b14d2
Paul Berry <paulberry@google.com>
2026-04-02 11:15:32 -0700

    Add language tests of yield* downwards inference.
    These tests cover the behaviors mentioned in
    https://github.com/dart-lang/language/issues/4672.
    
    Long term, I'm not certain whether these are the behaviors we
    want. But considering that these are the behaviors we have, it seems
    reasonable to test them to avoid regressions.
    
    Paves the way for a follow-up CL that will refactor some of the
    analyzer and front end logic that handles yield statements.
    
    Change-Id: I6af8eb67dac588d90ed95e2536184c4a6a6a6964
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/491706
    Reviewed-by: Erik Ernst <eernst@google.com>
    Commit-Queue: Paul Berry <paulberry@google.com>

---
+++ tests/language/async_star/yield_star_downwards_inference_test.dart
@@ -0,0 +1,57 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../static_type_helper.dart';
+
+class A implements Stream<dynamic> {
+  final Stream<dynamic> _stream = (() async* {})();
+
+  /// This do-nothing method is only defined for the class `A`, so calling it
+  /// serves as a runtime check that `this` is `A`.
+  void checkThisIsA() {}
+
+  @override
+  listen(onData, {onError, onDone, cancelOnError}) => _stream.listen(
+    onData,
+    onError: onError,
+    onDone: onDone,
+    cancelOnError: cancelOnError,
+  );
+
+  @override
+  noSuchMethod(invocation) => super.noSuchMethod(invocation);
+}
+
+dynamic returnTypeIsDynamic() async* {
+  T f<T>() => A() as T;
+
+  // Downward inference context for the operand of `yield*` is `_`, so `f` is
+  // instantiated as `f<dynamic>`, hence the call to `..checkThisIsA()` is
+  // allowed (it's a dynamic invocation).
+  yield* f()..checkThisIsA();
+}
+
+Stream<dynamic> returnTypeIsStreamDynamic() async* {
+  // Downward inference context for the operand of `yield*` is
+  // `Stream<dynamic>`.
+  yield* contextType(A())..expectStaticType<Exactly<Stream<dynamic>>>();
+}
+
+dynamic returnContextIsUnknown() {
+  T f<T>() => A() as T;
+
+  var g = () async* {
+    // Downward inference context for the operand of `yield*` is `_`, so `f` is
+    // instantiated as `f<dynamic>`, hence the call to `..checkThisIsA()` is
+    // allowed (it's a dynamic invocation).
+    yield* f()..checkThisIsA();
+  };
+  return g();
+}
+
+main() async {
+  await for (var _ in returnTypeIsDynamic()) {}
+  await for (var _ in returnTypeIsStreamDynamic()) {}
+  await for (var _ in returnContextIsUnknown()) {}
+}
