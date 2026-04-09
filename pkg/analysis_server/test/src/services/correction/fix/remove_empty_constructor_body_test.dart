9fee4af3021c2e79c07126db6157626133da13da
Kallen Tu <kallentu@google.com>
2026-04-09 13:40:14 -0700

    [analysis_server] Primary constructors: Update RemoveEmptyConstructorBody.
    Added support to the `RemoveEmptyConstructorBody` fix to handle primary constructor bodies.
    
    ```dart
    class C(int x) {
      this : assert(x > 0) {}
    }
    ```
    
    becomes
    
    ```dart
    class C(int x) {
      this : assert(x > 0);
    }
    ```
    
    Fixes: https://github.com/dart-lang/sdk/issues/63075
    Change-Id: I00bc80711ee436c0132a3ae5313763073b030aac
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/492883
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Kallen Tu <kallentu@google.com>

--- pkg/analysis_server/test/src/services/correction/fix/remove_empty_constructor_body_test.dart
+++ pkg/analysis_server/test/src/services/correction/fix/remove_empty_constructor_body_test.dart
@@ -72,6 +72,118 @@ class A {
     await assertNoFix(filter: _isInterestingError);
   }
 
+  Future<void> test_noPrimaryConstructor() async {
+    await resolveTestCode('''
+class C {
+  this {}
+}
+''');
+    await assertHasFix('''
+class C {
+  this;
+}
+''', filter: _isInterestingError);
+  }
+
+  Future<void> test_primaryConstructor_class() async {
+    await resolveTestCode('''
+class C(int x) {
+  this {}
+}
+''');
+    await assertHasFix('''
+class C(int x) {
+  this;
+}
+''');
+  }
+
+  Future<void> test_primaryConstructor_enum() async {
+    await resolveTestCode('''
+enum E(int x) {
+  c(1);
+
+  this {}
+}
+''');
+    await assertHasFix('''
+enum E(int x) {
+  c(1);
+
+  this;
+}
+''', filter: _isInterestingError);
+  }
+
+  Future<void> test_primaryConstructor_enum_withInitializer() async {
+    await resolveTestCode('''
+enum E(int x) {
+  c(1);
+
+  this : assert(x > 0) {}
+}
+''');
+    await assertHasFix('''
+enum E(int x) {
+  c(1);
+
+  this : assert(x > 0);
+}
+''', filter: _isInterestingError);
+  }
+
+  Future<void> test_primaryConstructor_extensionType() async {
+    await resolveTestCode('''
+extension type E(int x) {
+  this {}
+}
+''');
+    await assertHasFix('''
+extension type E(int x) {
+  this;
+}
+''');
+  }
+
+  Future<void> test_primaryConstructor_extensionType_withInitializer() async {
+    await resolveTestCode('''
+extension type E(int x) {
+  this : assert(x > 0) {}
+}
+''');
+    await assertHasFix('''
+extension type E(int x) {
+  this : assert(x > 0);
+}
+''');
+  }
+
+  Future<void> test_primaryConstructor_mixinClass() async {
+    await resolveTestCode('''
+mixin class M(int x) {
+  this {}
+}
+''');
+    await assertHasFix('''
+mixin class M(int x) {
+  this;
+}
+''', filter: _isInterestingError);
+  }
+
+  Future<void> test_primaryConstructor_mixinClass_withInitializer() async {
+    await resolveTestCode('''
+mixin class M(int x) {
+  this : assert(x > 0) {}
+}
+''');
+    await assertHasFix('''
+mixin class M(int x) {
+  this : assert(x > 0);
+}
+''', filter: _isInterestingError);
+  }
+
   static bool _isInterestingError(Diagnostic e) {
     return e.diagnosticCode.lowerCaseName == LintNames.empty_constructor_bodies;
   }
