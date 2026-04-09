4c9e441b4d7cf20d7d61b1a35174b22e8bddafe7
Sam Rawlins <srawlins@google.com>
2026-04-09 08:01:50 -0700

    DAS: lazily normalize paths in the session logger
    Fixes https://github.com/dart-lang/sdk/issues/63009
    
    Change-Id: Ifdf4b4dc7216edc5294c7faaff614ba0a4f4ecc9
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/493965
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

---
+++ pkg/analysis_server/test/src/session_logger/session_logger_sink_test.dart
@@ -0,0 +1,81 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analysis_server/src/session_logger/log_normalizer.dart';
+import 'package:analysis_server/src/session_logger/session_logger_sink.dart';
+import 'package:analyzer/file_system/memory_file_system.dart';
+import 'package:test/test.dart';
+import 'package:test_reflective_loader/test_reflective_loader.dart';
+
+void main() {
+  defineReflectiveSuite(() {
+    defineReflectiveTests(SessionLoggerFileSinkTest);
+    defineReflectiveTests(SessionLoggerInMemorySinkTest);
+  });
+}
+
+@reflectiveTest
+class SessionLoggerFileSinkTest {
+  late LogNormalizer normalizer;
+
+  void setUp() {
+    normalizer = LogNormalizer();
+  }
+
+  Future<void> test_normalized() async {
+    var x = MemoryResourceProvider();
+    var logFile = x.getFile('/foo.txt');
+    var fileSink = SessionLoggerFileSink(logFile, normalizer: normalizer);
+    normalizer.addPathReplacement('/path/to/normalize', '{{normalized}}');
+    fileSink.writeLogEntry({
+      'kind': 'message',
+      'message': {
+        'method': 'textDocument/didOpen',
+        'params': {
+          'textDocument': {'uri': 'file:///path/to/normalize'},
+        },
+      },
+    });
+
+    var content = logFile.readAsStringSync();
+    expect(
+      content,
+      '{"kind":"message",'
+      '"message":{"method":"textDocument/didOpen",'
+      '"params":{"textDocument":{"uri":"{{normalized}}"}}}}\n',
+    );
+  }
+}
+
+@reflectiveTest
+class SessionLoggerInMemorySinkTest {
+  late LogNormalizer normalizer;
+  late SessionLoggerInMemorySink sink;
+
+  void setUp() {
+    normalizer = LogNormalizer();
+    sink = SessionLoggerInMemorySink(
+      maxBufferLength: 10,
+      normalizer: normalizer,
+    );
+  }
+
+  void test_capturedEntries_normalized() {
+    normalizer.addPathReplacement('/path/to/normalize', '{{normalized}}');
+    sink.startCapture();
+    sink.writeLogEntry({
+      'kind': 'message',
+      'message': {
+        'method': 'textDocument/didOpen',
+        'params': {
+          'textDocument': {'uri': 'file:///path/to/normalize'},
+        },
+      },
+    });
+
+    var entries = sink.capturedEntries;
+    expect(entries, hasLength(1));
+    expect(entries[0].message.textDocument, '{{normalized}}');
+  }
+}
