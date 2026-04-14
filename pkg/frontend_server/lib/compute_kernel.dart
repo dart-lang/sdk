16132dff5d8c57d3e6bafb7d736d675e9442a47e
Kilian Schulte <netlight@schultek.dev>
2026-04-14 21:46:21 +0200

    remove second call to performOutlineTransformations in incremental compiler

--- pkg/frontend_server/lib/compute_kernel.dart
+++ pkg/frontend_server/lib/compute_kernel.dart
@@ -490,7 +490,6 @@ Future<ComputeKernelResult> computeKernel(
         incrementalComponent.uriToSource.clear();
         incrementalComponent.problemsAsJson = null;
         incrementalComponent.setMainMethodAndMode(null, true);
-        target.performOutlineTransformations(incrementalComponent);
         makeStable(incrementalComponent);
         return new Future.value(
           fe.serializeComponent(
