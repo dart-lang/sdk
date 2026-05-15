98c3699f32aa4192bfb5714b7ac44a0da0ab5b50
Keerti Parthasarathy <keertip@google.com>
2026-05-15 09:28:17 -0700

    Add test for inlining a getter.
    This test confirms that https://github.com/dart-lang/sdk/issues/45997 is fixed.
    
    Change-Id: Iafdd8be922a770d2bc11260a94237845df336d4b
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/503660
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/test/services/refactoring/legacy/inline_method_test.dart
+++ pkg/analysis_server/test/services/refactoring/legacy/inline_method_test.dart
@@ -1601,6 +1601,27 @@ void f(A a) {
 ''');
   }
 
+  Future<void> test_getter_classMember_instance_propertyAccess_target() async {
+    await indexTestUnit(r'''
+class A {
+  int f = 0;
+  int get ^result => f + 1;
+}
+void f(A a) {
+  print(a.result.isEven);
+}
+''');
+    _createRefactoring();
+    return _assertSuccessfulRefactoring(r'''
+class A {
+  int f = 0;
+}
+void f(A a) {
+  print((a.f + 1).isEven);
+}
+''');
+  }
+
   Future<void> test_getter_classMember_static() async {
     await indexTestUnit(r'''
 class A {
