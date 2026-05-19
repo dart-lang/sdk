ddeb964d80c2f03f68df5fe90284e0bb39422e5a
Danny Tuppeny <danny@tuppeny.com>
2026-05-19 12:12:02 -0700

    [analysis_server] Handle some reserved names ("default", "new") in LSP codegen
    Both of these names are used in the JSON in the Go-spec'd refactoring support, so we need to map them to new names.
    
    We had some existing code for mapping some kinds of names (like enums), but not for JSON fields in interface classes.
    
    See https://github.com/dart-lang/sdk/issues/63371
    
    Change-Id: Ic223a783be9170774444844bb1934a3733ddf446
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/504261
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>

---
+++ pkg/analysis_server/test/tool/lsp_spec/codegen_test.dart
@@ -0,0 +1,59 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:test/test.dart';
+
+import '../../../tool/lsp_spec/codegen_dart.dart';
+import '../../../tool/lsp_spec/meta_model.dart';
+
+void main() {
+  group('names', () {
+    test('handles fields named "default"', () {
+      var generatedCode = generateDartForTypes([
+        Interface(
+          name: 'x',
+          members: [
+            Field(
+              name: 'default',
+              type: TypeReference.string,
+              allowsNull: false,
+              allowsUndefined: true,
+            ),
+          ],
+        ),
+      ]);
+
+      // Verify the generated code parses with no errors.
+      parseString(content: generatedCode);
+
+      // Verify some expected code.
+      expect(generatedCode, contains('final String? defaultValue'));
+      expect(generatedCode, contains('this.defaultValue'));
+      expect(generatedCode, contains('defaultValue.hashCode'));
+
+      // JSON still uses the original protocol name.
+      expect(generatedCode, contains("result['default'] = defaultValue"));
+      expect(generatedCode, contains("defaultValueJson = json['default']"));
+    });
+
+    test('handles enum members named "new"', () {
+      var generatedCode = generateDartForTypes([
+        LspEnum(
+          name: 'x',
+          typeOfValues: TypeReference.int,
+          members: [
+            Constant(name: 'new', type: TypeReference.string, value: '1'),
+          ],
+        ),
+      ]);
+
+      // Verify the generated code parses with no errors.
+      parseString(content: generatedCode);
+
+      // Verify some expected code.
+      expect(generatedCode, contains('static const new_ = x(1)'));
+    });
+  });
+}
