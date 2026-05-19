efd30a2de0d2c7d3c4af4f62d9c5a7203404847b
Kallen Tu <kallentu@google.com>
2026-05-19 14:11:50 -0700

    [migrate] Initial implementation of SDK constraint bumping.
    Uses the pubspec util `computeVersionBumpEdit` that bumps the pubspec
    file's `sdk:` one minor version up.
    
    Very basic iteration through the given pubspec files to bump all of
    them. We'll very likely have to refactor this as we get the pre/post
    migrations involved, but I wanted to get there in small (reviewable)
    steps.
    
    Bug: https://github.com/dart-lang/sdk/issues/63268
    Change-Id: I2a7abeacdc76b114dfef7888cd6db6bc8973d01a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/504381
    Commit-Queue: Kallen Tu <kallentu@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/lsp/migrate_test.dart
+++ pkg/analysis_server/test/lsp/migrate_test.dart
@@ -18,6 +18,103 @@ void main() {
 
 @reflectiveTest
 class MigrateTest extends AbstractLspAnalysisServerTest {
+  Future<void> test_bumpSdkConstraint() async {
+    await _setupProject(
+      pubspecContent: '''
+name: test_project
+environment:
+  sdk: '^3.0.0'
+''',
+    );
+    await _assertMigrationResult(
+      expectedSummary: '''
+Bumped SDK constraints in 1 package(s):
+- test_project: ^3.0.0 -> ^3.1.0''',
+      expectedPubspecContent: '''
+name: test_project
+environment:
+  sdk: '^3.1.0'
+''',
+    );
+  }
+
+  Future<void> test_bumpSdkConstraint_emptyPubspec() async {
+    await _setupProject(pubspecContent: '');
+    await _assertMigrationResult(
+      expectedSummary: 'No SDK constraints were bumped.',
+    );
+  }
+
+  Future<void> test_bumpSdkConstraint_multiplePackages() async {
+    await initialize();
+
+    var project1Path = pathContext.join(projectFolderPath, 'project1');
+    var project2Path = pathContext.join(projectFolderPath, 'project2');
+
+    newFile(pathContext.join(project1Path, 'pubspec.yaml'), '''
+name: project1
+environment:
+  sdk: '^3.0.0'
+''');
+
+    newFile(pathContext.join(project2Path, 'pubspec.yaml'), '''
+name: project2
+environment:
+  sdk: '^3.2.0'
+''');
+
+    await _assertMigrationResult(
+      uris: [Uri.file(project1Path), Uri.file(project2Path)],
+      expectedSummary: '''
+Bumped SDK constraints in 2 package(s):
+- project1: ^3.0.0 -> ^3.1.0
+- project2: ^3.2.0 -> ^3.3.0''',
+    );
+  }
+
+  Future<void> test_bumpSdkConstraint_noneBumped() async {
+    await _setupProject(
+      pubspecContent: '''
+name: test_project
+''',
+    );
+    await _assertMigrationResult(
+      expectedSummary: 'No SDK constraints were bumped.',
+    );
+  }
+
+  Future<void> test_bumpSdkConstraint_range() async {
+    await _setupProject(
+      pubspecContent: '''
+name: test_project
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+''',
+    );
+    await _assertMigrationResult(
+      expectedSummary: contains('>=3.0.0 <4.0.0 -> >=3.1.0'),
+      expectedPubspecContent: '''
+name: test_project
+environment:
+  sdk: '>=3.1.0 <4.0.0'
+''',
+    );
+  }
+
+  Future<void> test_bumpSdkConstraint_skipped() async {
+    var otherDirPath = convertPath('/other_project');
+    var otherPubspecPath = pathContext.join(otherDirPath, 'pubspec.yaml');
+
+    await _setupProject(
+      pubspecContent: 'name: other_project',
+      customPubspecFilePath: otherPubspecPath,
+    );
+    await _assertMigrationResult(
+      uris: [Uri.file(otherDirPath)],
+      expectedSummary: contains('- other_project: Skipped (not analyzed)'),
+    );
+  }
+
   Future<void> test_error_directoryWithoutPubspec() async {
     await initialize();
 
@@ -153,16 +250,48 @@ resolution: workspace
   }
 
   Future<void> test_validDirectory() async {
-    await initialize();
+    await _setupProject(pubspecContent: 'name: test_project');
+    await _assertMigrationResult();
+  }
 
-    newFile(pubspecFilePath, 'name: test_project');
-
+  Future<void> _assertMigrationResult({
+    List<Uri>? uris,
+    Object? expectedSummary,
+    String? expectedPubspecContent,
+    String expectedPubspecPath = 'pubspec.yaml',
+  }) async {
     var request = makeRequest(
       CustomMethods.migrate,
-      DartMigrateParams(uris: [projectFolderUri]),
+      DartMigrateParams(uris: uris ?? [projectFolderUri]),
     );
     var response = await sendRequestToServer(request);
 
     expect(response.error, isNull);
+
+    var result = DartMigrateResult.fromJson(
+      response.result as Map<String, Object?>,
+    );
+    if (expectedSummary != null) {
+      expect(result.summary, expectedSummary);
+    }
+    if (expectedPubspecContent != null) {
+      var workspaceEdit = result.edit!;
+      var expectedContent =
+          '''
+>>>>>>>>>> $expectedPubspecPath
+$expectedPubspecContent''';
+
+      verifyEdit(workspaceEdit, expectedContent);
+    }
+  }
+
+  Future<void> _setupProject({
+    required String pubspecContent,
+    String? customPubspecFilePath,
+  }) async {
+    await initialize();
+
+    var pubspecPath = customPubspecFilePath ?? pubspecFilePath;
+    newFile(pubspecPath, pubspecContent);
   }
 }
