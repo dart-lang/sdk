07ea2ec9a91c231ce7a563febc8ad4988aae0949
Ravant Vignesh <messiahravant@gmail.com>
2026-05-28 00:01:39 +0530

    fix(build): Improve error handling in num_cpus.py

--- build/dart/num_cpus.py
+++ build/dart/num_cpus.py
@@ -3,6 +3,26 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import multiprocessing
+import sys
 
-print(multiprocessing.cpu_count())
+_logger = logging.getLogger(__name__)
+
+def get_num_cpus():
+    try:
+        return multiprocessing.cpu_count()
+    except NotImplementedError:
+        _logger.error(
+            "multiprocessing.cpu_count() is not implemented on this system."
+        )
+        sys.exit(1)
+    except Exception as e:
+        _logger.exception(
+            "An unexpected error occurred while getting the number of CPUs."
+        )
+        sys.exit(1)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    print(get_num_cpus())
