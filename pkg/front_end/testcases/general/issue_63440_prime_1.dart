d441e28656418da265ae19c46fdb2bf66a3e85e1
Jens Johansen <jensj@google.com>
2026-05-28 05:15:07 -0700

    [CFE/VM] Fix mixin deleting procedure-setter because of final field
    Fixes https://github.com/dart-lang/sdk/issues/63440
    Tested: Tests added.
    Change-Id: I0bdfd71c48ecae6094557fa4f46b8fc4b50afa73
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/506980
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Jens Johansen <jensj@google.com>

---
+++ pkg/front_end/testcases/general/issue_63440_prime_1.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This has historically crashed with target vm.
+
+mixin M {
+  late final int c;
+  void set c(int _) {}
+}
+
+class MA = Object with M;
+
+void hello() {
+  MA().c = 42;
+}
