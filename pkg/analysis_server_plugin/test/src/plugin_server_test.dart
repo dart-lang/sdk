dart-lang/sdk

commit d0f27293cea39c8dcacf1d82f77cee7d3e2b4cc4

DAS plugins: Respect the analysis roots set by "dart analyze"
Work towards https://github.com/dart-lang/sdk/issues/62686

`dart analyze` uses an 'analysis.setAnalysisRoots' "command" to let
the analysis server know that the user requested, for example,
`dart analyze foo` or `dart analyze foo/bar.dart`. This was not conveyed
to analyzer plugins, so analyzer plugins will always analyze the entire
context collection.

(For some reason, only files in the "analysis roots" were reported in
stdout; maybe DAS or dartdev does its own filtering on diagnostics.)

This change forwards the `setAnalysisRoots` request to DAS plugins using
a new protocol message that mirrors the one for the server.

We should not change the behavior of `analysis.setContextRoots` for
legacy plugins, so we keep sending that request as per usual. Then
we send `analysis.setAnalysisRoots` at the same time.

If we receive an `analysis.setAnalysisRoots` message, dispose and
delete references to the existing context collection, and instantiate
a new one, with the specified `included` and `excluded` paths.

Change-Id: I53627da1c30351a22b5e5410a557bf486620a7aa
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/505041
Commit-Queue: Samuel Rawlins <srawlins@google.com>
Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

pkg/analysis_server_plugin/lib/src/plugin_server.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// @docImport 'package:analyzer/src/dart/analysis/driver.dart';
+library;
+
 import 'dart:async';
 
 import 'package:analysis_server_plugin/edit/assist/assist.dart';
@@ -97,6 +100,11 @@ class PluginServer {
 
   /// The map of registered features for each plugin, for namespacing purposes.
   static final registries = <String, PluginRegistryImpl>{};
+
+  /// The current subscription of [_contextCollection]'s
+  /// [AnalysisDriverScheduler.events] Stream. This must be cancelled when
+  /// [_contextCollection] is disposed.
+  StreamSubscription<Object>? _eventsSubscription;
 
   PluginServer({
     required ResourceProvider resourceProvider,
@@ -618,10 +626,15 @@ class PluginServer {
         result = await _handleAnalysisWatchEvents(params);
 
       case protocol.ANALYSIS_REQUEST_SET_CONTEXT_ROOTS:
-        var params = protocol.AnalysisSetContextRootsParams.fromRequest(
+        // This request is for legacy plugins. New plugins use
+        // `protocol.ANALYSIS_REQUEST_SET_ANALYSIS_ROOTS`.
+        result = null;
+
+      case protocol.ANALYSIS_REQUEST_SET_ANALYSIS_ROOTS:
+        var params = protocol.AnalysisSetAnalysisRootsParams.fromRequest(
           request,
         );
-        result = await _handleAnalysisSetContextRoots(params);
+        result = await _handleAnalysisSetAnalysisRoots(params);
 
       case protocol.ANALYSIS_REQUEST_SET_PRIORITY_FILES:
         var params = protocol.AnalysisSetPriorityFilesParams.fromRequest(
@@ -693,20 +706,24 @@ class PluginServer {
     return result.toResponse(request.id, requestTime);
   }
 
-  /// Handles an 'analysis.setContextRoots' request.
-  Future<protocol.AnalysisSetContextRootsResult> _handleAnalysisSetContextRoots(
-    protocol.AnalysisSetContextRootsParams parameters,
+  /// Handles an 'analysis.setAnalysisRoots' request.
+  Future<protocol.AnalysisSetAnalysisRootsResult>
+  _handleAnalysisSetAnalysisRoots(
+    protocol.AnalysisSetAnalysisRootsParams parameters,
   ) async {
-    var currentContextCollection = _contextCollection;
-    if (currentContextCollection != null) {
+    if (_contextCollection case var contextCollection?) {
       _contextCollection = null;
-      await currentContextCollection.dispose();
+      await contextCollection.dispose();
+    }
+    if (_eventsSubscription case var eventsSubscription?) {
+      _eventsSubscription = null;
+      await eventsSubscription.cancel();
     }
 
-    var includedPaths = parameters.roots.map((e) => e.root).toList();
     var contextCollection = AnalysisContextCollectionImpl(
       resourceProvider: _resourceProvider,
-      includedPaths: includedPaths,
+      includedPaths: parameters.included,
+      excludedPaths: parameters.excluded,
       byteStore: _byteStore,
       sdkPath: _sdkPath,
       fileContentCache: FileContentCache(_resourceProvider),
@@ -722,7 +739,7 @@ class PluginServer {
     );
     _contextCollection = contextCollection;
     _updatePriorityFiles();
-    contextCollection.scheduler.events.listen((event) {
+    _eventsSubscription = contextCollection.scheduler.events.listen((event) {
       if (event is ResolvedUnitResult) {
         _handleResolvedUnit(event);
       } else if (event is AnalysisStatus) {
@@ -734,7 +751,8 @@ class PluginServer {
     await _analyzeAllFilesInContextCollection(
       contextCollection: contextCollection,
     );
-    return protocol.AnalysisSetContextRootsResult();
+
+    return protocol.AnalysisSetAnalysisRootsResult();
   }
 
   void _handleAnalysisStatus(AnalysisStatus status) {
@@ -843,11 +861,6 @@ class PluginServer {
     if (_contextCollection case var contextCollection?) {
       _filesBeingAnalyzed.clear();
       _filesBeingResolved.clear();
-      _channel.sendNotification(
-        protocol.PluginStatusParams(
-          analysis: protocol.AnalysisStatus(true),
-        ).toNotification(),
-      );
       await _forAnalysisContexts(contextCollection, (analysisContext) async {
         final driver = analysisContext.driver;
         for (var path in modifiedPaths) {
@@ -865,11 +878,6 @@ class PluginServer {
           protocol.AnalysisErrorsParams(path, []).toNotification(),
         );
       }
-      _channel.sendNotification(
-        protocol.PluginStatusParams(
-          analysis: protocol.AnalysisStatus(false),
-        ).toNotification(),
-      );
     }
   }
 

pkg/analysis_server_plugin/test/src/plugin_server_error_test.dart
@@ -56,9 +56,7 @@ plugins:
     newFile(filePath, 'bool b = false;');
     var contextRoot = protocol.ContextRoot(packagePath, []);
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots([contextRoot]);
 
     var pluginErrorParamsQueue = StreamQueue(
       channel.notifications
@@ -96,9 +94,7 @@ plugins:
           .map((n) => protocol.PluginErrorParams.fromNotification(n)),
     );
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots([contextRoot]);
 
     var analysisErrorsParams = await analysisErrorsParamsQueue.next;
     expect(analysisErrorsParams.errors, isEmpty);
@@ -126,9 +122,7 @@ plugins:
     newFile(filePath, 'bool b = false;');
     var contextRoot = protocol.ContextRoot(packagePath, []);
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots([contextRoot]);
 
     var pluginErrorParamsQueue = StreamQueue(
       channel.notifications
@@ -170,9 +164,7 @@ plugins:
           .map((n) => protocol.PluginErrorParams.fromNotification(n)),
     );
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots([contextRoot]);
 
     // Wait for initial analysis to complete and populate `_recentState`.
     var analysisErrorsParams = await analysisErrorsParamsQueue.next;
@@ -212,9 +204,7 @@ plugins:
           .where((p) => p.file == filePath),
     );
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots([contextRoot]);
 
     // Wait for initial analysis to complete and populate `_recentState`.
     await analysisErrorsParamsQueue.next;
@@ -247,6 +237,17 @@ plugins:
         plugins: [_PluginWithFixWithNoFixKind()],
       ),
       throwsArgumentError,
+    );
+  }
+
+  Future<void> _setRoots(List<protocol.ContextRoot> contextRoots) async {
+    await channel.sendRequest(
+      protocol.AnalysisSetContextRootsParams(contextRoots),
+    );
+    await channel.sendRequest(
+      protocol.AnalysisSetAnalysisRootsParams([
+        for (var contextRoot in contextRoots) contextRoot.root,
+      ], []),
     );
   }
 }

pkg/analysis_server_plugin/test/src/plugin_server_test.dart
@@ -57,9 +57,7 @@ class PluginServerMapTest extends PluginServerTestBase
 // ignore: no_literals/no_bools
 bool b = false;
 ''');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -96,9 +94,7 @@ analyzer:
     var fileContent = 'bool b = false;';
     newFile(filePath, fileContent);
     newFile(file2Path, fileContent);
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
@@ -179,9 +175,7 @@ class PluginServerTest extends PluginServerTestBase with PluginServerTestMixin {
 // ignore: no_literals/no_bools
 bool b = false;
 ''');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -194,9 +188,7 @@ bool b = false;
 
 // ignore_for_file: no_literals/no_bools
 ''');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -205,9 +197,7 @@ bool b = false;
   Future<void> test_handleAnalysisSetContextRoots() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -323,9 +313,7 @@ bool b = false;'''),
   Future<void> test_handleEditGetFixes_afterLine() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;\n\n');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var result = await pluginServer.handleEditGetFixes(
       protocol.EditGetFixesParams(filePath, 'bool b = false;\n'.length),
@@ -397,9 +385,7 @@ var n = ^10;
   Future<void> test_lintCodesCanHaveConfigurableSeverity() async {
     writeAnalysisOptionsWithPlugin({'no_doubles_custom_severity': 'error'});
     newFile(filePath, 'double x = 3.14;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -413,9 +399,7 @@ var n = ^10;
   Future<void> test_lintCodesCanHaveCustomSeverity() async {
     writeAnalysisOptionsWithPlugin({'no_doubles_custom_severity': 'enable'});
     newFile(filePath, 'double x = 3.14;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -429,9 +413,7 @@ var n = ^10;
   Future<void> test_lintRulesAreDisabledByDefault() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'double x = 3.14;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -440,9 +422,7 @@ var n = ^10;
   Future<void> test_lintRulesCanBeEnabled() async {
     writeAnalysisOptionsWithPlugin({'no_doubles': 'enable'});
     newFile(filePath, 'double x = 3.14;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -462,9 +442,7 @@ part 'test2.dart';
 C? c;
 ''');
     newFile(filePath, code.code);
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1), reason: 'Expected one diagnostic.');
@@ -516,9 +494,7 @@ C? c;
     writeAnalysisOptionsWithPlugin({'needs_package': 'enable'});
     newFile(filePath, 'var x = 1;');
     newFile(testFilePath, 'var x = 1;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.file, filePath);
@@ -540,9 +516,7 @@ int a = 0;
     newFile(file2Path, '''
 int b = 1;
 ''');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
@@ -557,9 +531,7 @@ int b = 1;
 
     expect(pluginServer.priorityPaths, {file2Path});
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     params = await paramsQueue.next;
     expect(params.file, file2Path);
@@ -578,9 +550,7 @@ int b = 1;
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;');
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     // This request is unsupported.
     var response = await channel.sendRequest(
@@ -593,9 +563,7 @@ int b = 1;
   Future<void> test_updateContent_addOverlay() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'int b = 7;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
@@ -625,9 +593,7 @@ int b = 1;
     var notifications = <protocol.Notification>[];
     var subscription = channel.notifications.listen(notifications.add);
 
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     // Wait for initial analysis result.
     await pluginServer.waitForIdle();
@@ -668,9 +634,7 @@ void f() {
 }
 ''');
     var paramsQueue = _analysisErrorsParams;
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var params = await paramsQueue.next; // test.dart
     expect(params.errors, isEmpty);
@@ -707,9 +671,7 @@ void f() {
   print(s);
 }
 ''');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next; // test.dart
@@ -743,9 +705,7 @@ String s = "hello";
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'int b = 7;');
     var paramsQueue = _analysisErrorsParams;
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -776,9 +736,7 @@ String s = "hello";
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;');
     var paramsQueue = _analysisErrorsParams;
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -807,9 +765,7 @@ String s = "hello";
   Future<void> test_warningRulesAreEnabledByDefault() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
@@ -819,9 +775,7 @@ String s = "hello";
   Future<void> test_warningRulesCanBeDisabled() async {
     writeAnalysisOptionsWithPlugin({'no_bools': 'disable'});
     newFile(filePath, 'bool b = false;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
     expect(params.errors, isEmpty);
@@ -829,9 +783,7 @@ String s = "hello";
 
   Future<void> test_watchEvent_add() async {
     writeAnalysisOptionsWithPlugin();
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
 
@@ -851,9 +803,7 @@ String s = "hello";
   Future<void> test_watchEvent_modify() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'int b = 7;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
@@ -875,9 +825,7 @@ String s = "hello";
   Future<void> test_watchEvent_remove() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'int b = 7;');
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     var paramsQueue = _analysisErrorsParams;
     var params = await paramsQueue.next;
@@ -936,9 +884,7 @@ mixin PluginServerTestMixin on PluginServerTestBase {
 
   Future<void> _setContextRootsAndReadFirstErrors() async {
     var paramsQueue = _analysisErrorsParams;
-    await channel.sendRequest(
-      protocol.AnalysisSetContextRootsParams([contextRoot]),
-    );
+    await _setRoots();
 
     // Read the analysis errors.
     await paramsQueue.next;
@@ -963,6 +909,15 @@ mixin PluginServerTestMixin on PluginServerTestBase {
                 p.file == file2Path ||
                 p.file == testFilePath,
           ),
+    );
+  }
+
+  Future<void> _setRoots() async {
+    await channel.sendRequest(
+      protocol.AnalysisSetContextRootsParams([contextRoot]),
+    );
+    await channel.sendRequest(
+      protocol.AnalysisSetAnalysisRootsParams([contextRoot.root], []),
     );
   }
 }

commit 4c636d3bddc2fa58f248895359161149a4780df8

DAS plugins: Fix issue with Dart 3.12
Work towards https://github.com/dart-lang/sdk/issues/63538

This bug was introduced in d0f27293cea39c8dcacf1d82f77cee7d3e2b4cc4. I
thought I had handled gracefully the case where `analysis.setAnalysisRoots` was sent (DAS at HEAD) and where it wasn't
(DAS <= 3.12.x), but no. So the plugins isolate never sends an analysis
state saying "not analyzing." (We should probably have some kind of timeout which is shorter than, for example, someone's CI timeout of like
3 hours. Something even liberal like 300 seconds or 180 seconds would
be better than indefinite.)

With this change, we respond to `analysis.setContextRoots` the same as
`setAnalysisRoots`, considering whether both have been received.

And in the tests I add more assertions about the analysis status, to
prevent a regression.

Change-Id: Ie85e7515b34442b8ac6563e422a9e7c12f21e88c
Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/518540
Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
Commit-Queue: Samuel Rawlins <srawlins@google.com>

pkg/analysis_server_plugin/lib/src/plugin_server.dart
@@ -106,6 +106,11 @@ class PluginServer {
   /// [_contextCollection] is disposed.
   StreamSubscription<Object>? _eventsSubscription;
 
+  /// Whether we have received an 'analysis.setAnalysisRoots' request.
+  ///
+  /// If we have, we can ignore 'analysis.setContextRoots' requests.
+  bool _receivedAnalysisRoots = false;
+
   /// Whether to fall back to the global [Registry.ruleRegistry] if a plugin
   /// registry is not found by name.
   ///
@@ -615,6 +620,48 @@ class PluginServer {
     return severity;
   }
 
+  Future<void> _createContextCollection(List<String> includedPaths) async {
+    if (_contextCollection case var contextCollection?) {
+      _contextCollection = null;
+      await contextCollection.dispose();
+    }
+    if (_eventsSubscription case var eventsSubscription?) {
+      _eventsSubscription = null;
+      await eventsSubscription.cancel();
+    }
+
+    var contextCollection = AnalysisContextCollectionImpl(
+      resourceProvider: _resourceProvider,
+      includedPaths: includedPaths,
+      byteStore: _byteStore,
+      sdkPath: _sdkPath,
+      fileContentCache: FileContentCache(_resourceProvider),
+      configureAnalysisOptionsBuilder:
+          // Disable extra warning computation and lint computation, because
+          // these are reported in the main analysis server isolate, not in the
+          // plugins isolate.
+          ({required analysisOptionsBuilder}) => analysisOptionsBuilder
+            ..warning = false
+            ..lint = false,
+      withFineDependencies: true,
+      drainStreams: false,
+    );
+    _contextCollection = contextCollection;
+    _updatePriorityFiles();
+    _eventsSubscription = contextCollection.scheduler.events.listen((event) {
+      if (event is ResolvedUnitResult) {
+        _handleResolvedUnit(event);
+      } else if (event is AnalysisStatus) {
+        _handleAnalysisStatus(event);
+      } else if (event is ErrorsResult) {
+        _handleErrorsResult(event);
+      }
+    });
+    await _analyzeAllFilesInContextCollection(
+      contextCollection: contextCollection,
+    );
+  }
+
   /// Invokes [fn] first for priority analysis contexts, then for the rest.
   Future<void> _forAnalysisContexts(
     AnalysisContextCollectionImpl contextCollection,
@@ -645,9 +692,10 @@ class PluginServer {
         result = await _handleAnalysisWatchEvents(params);
 
       case protocol.ANALYSIS_REQUEST_SET_CONTEXT_ROOTS:
-        // This request is for legacy plugins. New plugins use
-        // `protocol.ANALYSIS_REQUEST_SET_ANALYSIS_ROOTS`.
-        result = null;
+        var params = protocol.AnalysisSetContextRootsParams.fromRequest(
+          request,
+        );
+        result = await _handleAnalysisSetContextRoots(params);
 
       case protocol.ANALYSIS_REQUEST_SET_ANALYSIS_ROOTS:
         var params = protocol.AnalysisSetAnalysisRootsParams.fromRequest(
@@ -730,47 +778,24 @@ class PluginServer {
   _handleAnalysisSetAnalysisRoots(
     protocol.AnalysisSetAnalysisRootsParams parameters,
   ) async {
-    if (_contextCollection case var contextCollection?) {
-      _contextCollection = null;
-      await contextCollection.dispose();
-    }
-    if (_eventsSubscription case var eventsSubscription?) {
-      _eventsSubscription = null;
-      await eventsSubscription.cancel();
-    }
-
-    var contextCollection = AnalysisContextCollectionImpl(
-      resourceProvider: _resourceProvider,
-      includedPaths: parameters.included,
-      byteStore: _byteStore,
-      sdkPath: _sdkPath,
-      fileContentCache: FileContentCache(_resourceProvider),
-      configureAnalysisOptionsBuilder:
-          // Disable extra warning computation and lint computation, because
-          // these are reported in the main analysis server isolate, not in the
-          // plugins isolate.
-          ({required analysisOptionsBuilder}) => analysisOptionsBuilder
-            ..warning = false
-            ..lint = false,
-      withFineDependencies: true,
-      drainStreams: false,
-    );
-    _contextCollection = contextCollection;
-    _updatePriorityFiles();
-    _eventsSubscription = contextCollection.scheduler.events.listen((event) {
-      if (event is ResolvedUnitResult) {
-        _handleResolvedUnit(event);
-      } else if (event is AnalysisStatus) {
-        _handleAnalysisStatus(event);
-      } else if (event is ErrorsResult) {
-        _handleErrorsResult(event);
-      }
-    });
-    await _analyzeAllFilesInContextCollection(
-      contextCollection: contextCollection,
-    );
-
+    _receivedAnalysisRoots = true;
+    await _createContextCollection(parameters.included);
     return protocol.AnalysisSetAnalysisRootsResult();
+  }
+
+  /// Handles an 'analysis.setContextRoots' request.
+  Future<protocol.AnalysisSetContextRootsResult> _handleAnalysisSetContextRoots(
+    protocol.AnalysisSetContextRootsParams parameters,
+  ) async {
+    // Allow any "simultaneous" requests to be processed.
+    await Future<void>.delayed(Duration.zero);
+    if (_receivedAnalysisRoots) {
+      return protocol.AnalysisSetContextRootsResult();
+    }
+
+    var includedPaths = parameters.roots.map((e) => e.root).toList();
+    await _createContextCollection(includedPaths);
+    return protocol.AnalysisSetContextRootsResult();
   }
 
   void _handleAnalysisStatus(AnalysisStatus status) {

pkg/analysis_server_plugin/test/src/plugin_server_error_test.dart
@@ -285,14 +285,15 @@ plugins:
   }
 
   Future<void> _setRoots(List<protocol.ContextRoot> contextRoots) async {
-    await channel.sendRequest(
+    var future1 = channel.sendRequest(
       protocol.AnalysisSetContextRootsParams(contextRoots),
     );
-    await channel.sendRequest(
+    var future2 = channel.sendRequest(
       protocol.AnalysisSetAnalysisRootsParams([
         for (var contextRoot in contextRoots) contextRoot.root,
       ], []),
     );
+    await Future.wait([future1, future2]);
   }
 }
 

pkg/analysis_server_plugin/test/src/plugin_server_test.dart
@@ -224,11 +224,68 @@ bool b = false;
   Future<void> test_handleAnalysisSetContextRoots() async {
     writeAnalysisOptionsWithPlugin();
     newFile(filePath, 'bool b = false;');
-    await _setRoots();
-    var paramsQueue = _analysisErrorsParams;
+
+    var notifications = channel.notifications.asBroadcastStream();
+    var statusQueue = StreamQueue(
+      notifications
+          .where((n) => n.event == protocol.PLUGIN_NOTIFICATION_STATUS)
+          .map((n) => protocol.PluginStatusParams.fromNotification(n)),
+    );
+    var paramsQueue = StreamQueue(
+      notifications
+          .where((n) => n.event == protocol.ANALYSIS_NOTIFICATION_ERRORS)
+          .map((n) => protocol.AnalysisErrorsParams.fromNotification(n)),
+    );
+
+    var setRootsFuture = _setRoots();
+
+    var status1 = await statusQueue.next;
+    expect(status1.analysis!.isAnalyzing, isTrue);
+
     var params = await paramsQueue.next;
     expect(params.errors, hasLength(1));
     _expectAnalysisError(params.errors.single, message: 'No bools message');
+
+    var status2 = await statusQueue.next;
+    expect(status2.analysis!.isAnalyzing, isFalse);
+
+    await setRootsFuture;
+  }
+
+  Future<void> test_handleAnalysisSetContextRoots_only() async {
+    writeAnalysisOptionsWithPlugin();
+    newFile(filePath, 'bool b = false;');
+
+    var notifications = channel.notifications.asBroadcastStream();
+    var statusQueue = StreamQueue(
+      notifications
+          .where((n) => n.event == protocol.PLUGIN_NOTIFICATION_STATUS)
+          .map((n) => protocol.PluginStatusParams.fromNotification(n)),
+    );
+    var paramsQueue = StreamQueue(
+      notifications
+          .where((n) => n.event == protocol.ANALYSIS_NOTIFICATION_ERRORS)
+          .map((n) => protocol.AnalysisErrorsParams.fromNotification(n)),
+    );
+
+    // Rather than using `_setRoots`, this test emulates Dart Analysis Server
+    // <= 3.12.0, where only `protocol.ANALYSIS_REQUEST_SET_CONTEXT_ROOTS` is
+    // sent.
+    var requestFuture = channel.sendRequest(
+      protocol.AnalysisSetContextRootsParams([contextRoot]),
+    );
+
+    var status1 = await statusQueue.next;
+    expect(status1.analysis!.isAnalyzing, isTrue);
+
+    var params = await paramsQueue.next;
+    expect(params.errors, hasLength(1));
+    _expectAnalysisError(params.errors.single, message: 'No bools message');
+
+    var status2 = await statusQueue.next;
+    expect(status2.analysis!.isAnalyzing, isFalse);
+
+    await requestFuture;
   }
 
   Future<void> test_handleEditGetAssists() async {
@@ -976,12 +1033,13 @@ mixin PluginServerTestMixin on PluginServerTestBase {
   }
 
   Future<void> _setRoots() async {
-    await channel.sendRequest(
+    var future1 = channel.sendRequest(
       protocol.AnalysisSetContextRootsParams([contextRoot]),
     );
-    await channel.sendRequest(
+    var future2 = channel.sendRequest(
       protocol.AnalysisSetAnalysisRootsParams([contextRoot.root], []),
     );
+    await Future.wait([future1, future2]);
   }
 }
 