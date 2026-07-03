86d62960b0b163e61c29fa042940662be067cc23
Lasse R.H. Nielsen <lrn@google.com>
2026-07-03 02:23:38 -0700

    Make `Duration` not rely on `other._duration`.
    Also makes `abs` not create a new duration if already positive.
    Tweak some doc wording.
    
    Bug: https://dartbug.com/63692
    Change-Id: I05617ae871743447768b9ab19e5d7fa566fff65a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/518080
    Reviewed-by: Stephen Adams <sra@google.com>
    Reviewed-by: Johnni Winther <johnniwinther@google.com>
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Lasse Nielsen <lrn@google.com>

--- sdk/lib/core/duration.dart
+++ sdk/lib/core/duration.dart
@@ -4,15 +4,32 @@
 
 part of "dart:core";
 
-/// A span of time, such as 27 days, 4 hours, 12 minutes, and 3 seconds.
+/// A difference between points in time.
 ///
-/// A `Duration` represents a difference from one point in time to another. The
-/// duration may be "negative" if the difference is from a later time to an
-/// earlier.
+/// A `Duration` is a signed number of microseconds, which represents
+/// an offset from a point in time to another.
+/// You can add a `Duration` to a [DateTime], which gives the
+/// point in time that is offset by that duration from the original.
 ///
-/// Durations are context independent. For example, a duration of 2 days is
-/// always 48 hours, even when it is added to a `DateTime` just when the
+/// Most durations are _positive_ and represent an offset
+/// to a later time. A positive duration is useful for representing
+/// a delay, a point in the future relative to now (as used by
+/// [Future.delayed]),
+/// or as a measure of time that has passed from an earlier
+/// time until now (as reported by [Stopwatch.elapsed]).
+///
+/// A negative duration is the difference from a later time to an
+/// earlier, and adding it to a time gives an earlier time.
+/// You cannot wait for a negative amount of time using
+/// [Future.delayed].
+///
+/// Durations are independent of calendars. For example, a duration of 2 days
+/// is always 48 hours, even when it is added to a `DateTime` just when the
 /// time zone is about to make a daylight-savings switch. (See [DateTime.add]).
+///
+/// A duration is a (positive or negative) number of microseconds.
+/// Durations are ordered by this number of microseconds when compared
+/// using [operator <] or [compareTo].
 ///
 /// Despite the same name, a `Duration` object does not implement "Durations"
 /// as specified by ISO 8601. In particular, a duration object does not keep
@@ -25,13 +42,14 @@ part of "dart:core";
 /// ```dart
 /// const fastestMarathon = Duration(hours: 2, minutes: 3, seconds: 2);
 /// ```
-/// The [Duration] represents a single number of microseconds,
-/// which is the sum of all the individual arguments to the constructor.
+/// The created [Duration] is defined by the number of microseconds
+/// that is the sum of all the individual arguments to the constructor.
 ///
 /// Properties can access that single number in different ways.
-/// For example the [inMinutes] gives the number of whole minutes
-/// in the total duration, which includes the minutes that were provided
-/// as "hours" to the constructor, and can be larger than 59.
+/// The [inMicroseconds] is the entire value, and, for example,
+/// [inMinutes] gives the number of whole minutes in the total duration,
+/// which includes any minutes that were provided as "hours" to the constructor
+/// or provided as "seconds" with a value of 60 or above.
 ///
 /// ```dart
 /// const fastestMarathon = Duration(hours: 2, minutes: 0, seconds: 35);
@@ -41,25 +59,30 @@ part of "dart:core";
 /// print(fastestMarathon.inSeconds); // 7235
 /// print(fastestMarathon.inMilliseconds); // 7235000
 /// ```
-/// The duration can be negative, in which case
-/// all the properties derived from the duration are also non-positive.
+/// If a duration is negative, all the properties derived from the duration are
+/// non-positive (negative unless they are zero).
 /// ```dart
 /// const overDayAgo = Duration(days: -1, hours: -10);
 /// print(overDayAgo.inDays); // -1
 /// print(overDayAgo.inHours); // -34
 /// print(overDayAgo.inMinutes); // -2040
 /// ```
+///
 /// Use one of the properties, such as [inDays],
 /// to retrieve the integer value of the `Duration` in the specified time unit.
-/// Note that the returned value is rounded down.
+/// Note that the returned value is rounded towards zero.
 /// For example,
 /// ```dart
 /// const aLongWeekend = Duration(hours: 88);
 /// print(aLongWeekend.inDays); // 3
+/// const aLongWeekendAgo = Duration(hours: -88);
+/// print(aLongWeekendAgo.inDays); // -3
 /// ```
-/// This class provides a collection of arithmetic
-/// and comparison operators,
-/// plus a set of constants useful for converting time units.
+///
+/// A duration also provides arithmetic and comparison operators,
+/// all based on the microsecond offset of the duration.
+/// and the class provides a set of constants useful for converting
+/// between time units.
 /// ```dart
 /// const firstHalf = Duration(minutes: 45); // 00:45:00.000000
 /// const secondHalf = Duration(minutes: 45); // 00:45:00.000000
@@ -103,9 +126,9 @@ class Duration implements Comparable<Duration> {
 
   /// The number of hours per day.
   ///
-  /// Notice that some days may differ in length because
+  /// Notice that some calendar days may differ in length because
   /// of time zone changes due to daylight saving.
-  /// The [Duration] class is time zone agnostic and
+  /// The [Duration] class is calendar and time zone agnostic and
   /// considers all days to have 24 hours.
   static const int hoursPerDay = 24;
 
@@ -145,8 +168,17 @@ class Duration implements Comparable<Duration> {
   /// An empty duration, representing zero time.
   static const Duration zero = Duration(seconds: 0);
 
-  /// The total microseconds of this [Duration] object.
-  final int _duration;
+  /// The time offset of this duration in microseconds.
+  ///
+  /// The value can be greater than 999999.
+  /// For example, a duration of nine seconds, 123 milliseconds and
+  /// 678 microseconds has 9123678 microseconds.
+  /// ```dart
+  /// const duration = Duration(seconds: 9, milliseconds: 123,
+  ///     microseconds: 567);
+  /// print(duration.inMicroseconds); // 9123678
+  /// ```
+  final int inMicroseconds;
 
   /// Creates a new [Duration] object whose value
   /// is the sum of all individual parts.
@@ -190,51 +222,67 @@ class Duration implements Comparable<Duration> {
   // Fast path internal direct constructor to avoids the optional arguments
   // and [_microseconds] recomputation.
   // The `+ 0` prevents -0.0 on the web, if the incoming duration happens to be -0.0.
-  const Duration._microseconds(int duration) : _duration = duration + 0;
+  const Duration._microseconds(int duration) : inMicroseconds = duration + 0;
 
-  /// Adds this Duration and [other] and
-  /// returns the sum as a new Duration object.
+  /// Adds this `Duration` and [other] and
+  /// returns the sum as a new `Duration` object.
   Duration operator +(Duration other) {
-    return Duration._microseconds(_duration + other._duration);
+    return Duration._microseconds(inMicroseconds + other.inMicroseconds);
   }
 
-  /// Subtracts [other] from this Duration and
-  /// returns the difference as a new Duration object.
+  /// The offset of this duration minus the offset of [other].
   Duration operator -(Duration other) {
-    return Duration._microseconds(_duration - other._duration);
+    return Duration._microseconds(inMicroseconds - other.inMicroseconds);
   }
 
-  /// Multiplies this Duration by the given [factor] and returns the result
-  /// as a new Duration object.
+  /// This [Duration] scaled by [factor].
   ///
   /// Note that when [factor] is a double, and the duration is greater than
   /// 53 bits, precision is lost because of double-precision arithmetic.
   Duration operator *(num factor) {
-    return Duration._microseconds((_duration * factor).round());
+    return Duration._microseconds((inMicroseconds * factor).round());
   }
 
-  /// Divides this Duration by the given [quotient] and returns the truncated
-  /// result as a new Duration object.
+  /// One-[quotient]th of this duration.
   ///
-  /// The [quotient] must not be `0`.
+  /// Divides the offset in microseconds of this duration by [quotient]
+  /// rounding towards zero, and provides a new `Duration` with that
+  /// as its [inMicroseconds].
+  ///
+  /// The [quotient] must not be `0`. If it is negative, the result
+  /// is equivalent to dividing by the absolute value and then negating.
   Duration operator ~/(int quotient) {
     // By doing the check here instead of relying on "~/" below we get the
     // exception even with dart2js.
     if (quotient == 0) throw IntegerDivisionByZeroException();
-    return Duration._microseconds(_duration ~/ quotient);
+    return Duration._microseconds(inMicroseconds ~/ quotient);
   }
 
-  /// Whether this [Duration] is shorter than [other].
-  bool operator <(Duration other) => this._duration < other._duration;
+  /// Whether this duration's offset is smaller than that of [other].
+  ///
+  /// A negative duration's offset is always smaller than a positive one's.
+  /// Equivalent to comparing [inMicroseconds]
+  bool operator <(Duration other) => this.inMicroseconds < other.inMicroseconds;
 
-  /// Whether this [Duration] is longer than [other].
-  bool operator >(Duration other) => this._duration > other._duration;
+  /// Whether this duration's offset is greater than that of [other].
+  ///
+  /// A negative duration's offset is always smaller than a positive one's.
+  /// Equivalent to comparing [inMicroseconds]
+  bool operator >(Duration other) => this.inMicroseconds > other.inMicroseconds;
 
-  /// Whether this [Duration] is shorter than or equal to [other].
-  bool operator <=(Duration other) => this._duration <= other._duration;
+  /// Whether this duration's offset is not greater than that of [other].
+  ///
+  /// A negative duration's offset is always smaller than a positive one's.
+  /// Equivalent to comparing [inMicroseconds]
+  bool operator <=(Duration other) =>
+      this.inMicroseconds <= other.inMicroseconds;
 
-  /// Whether this [Duration] is longer than or equal to [other].
-  bool operator >=(Duration other) => this._duration >= other._duration;
+  /// Whether this duration's offset is not smaller than that of [other].
+  ///
+  /// A negative duration's offset is always smaller than a positive one's.
+  /// Equivalent to comparing [inMicroseconds]
+  bool operator >=(Duration other) =>
+      this.inMicroseconds >= other.inMicroseconds;
 
   /// The number of entire days spanned by this [Duration].
   ///
@@ -244,7 +292,9 @@ class Duration implements Comparable<Duration> {
   /// const duration = Duration(days: 4, hours: 3);
   /// print(duration.inDays); // 4
   /// ```
-  int get inDays => _duration ~/ Duration.microsecondsPerDay;
+  /// A negative duration has a negative number of whole days,
+  /// so that `(-duration).inDays` = `-(duration.inDays)`.
+  int get inDays => inMicroseconds ~/ Duration.microsecondsPerDay;
 
   /// The number of entire hours spanned by this [Duration].
   ///
@@ -253,9 +303,11 @@ class Duration implements Comparable<Duration> {
   /// has 99 entire hours.
   /// ```dart
   /// const duration = Duration(days: 4, hours: 3);
-  /// print(duration.inHours); // 99
+  /// print(duration.inHours); // 99 = 4 * 24 + 3
   /// ```
-  int get inHours => _duration ~/ Duration.microsecondsPerHour;
+  /// A negative duration has a negative number of whole hours,
+  /// so that `(-duration).inHours` = `-(duration.inHours)`.
+  int get inHours => inMicroseconds ~/ Duration.microsecondsPerHour;
 
   /// The number of whole minutes spanned by this [Duration].
   ///
@@ -264,9 +316,11 @@ class Duration implements Comparable<Duration> {
   /// has 192 minutes.
   /// ```dart
   /// const duration = Duration(hours: 3, minutes: 12);
-  /// print(duration.inMinutes); // 192
+  /// print(duration.inMinutes); // 192 = 3 * 60 + 12
   /// ```
-  int get inMinutes => _duration ~/ Duration.microsecondsPerMinute;
+  /// A negative duration has a negative number of whole minutes,
+  /// so that `(-duration).inMinutes` = `-(duration.inMinutes)`.
+  int get inMinutes => inMicroseconds ~/ Duration.microsecondsPerMinute;
 
   /// The number of whole seconds spanned by this [Duration].
   ///
@@ -275,56 +329,54 @@ class Duration implements Comparable<Duration> {
   /// has 192 seconds.
   /// ```dart
   /// const duration = Duration(minutes: 3, seconds: 12);
-  /// print(duration.inSeconds); // 192
+  /// print(duration.inSeconds); // 192 = 3 * 60 + 12
   /// ```
-  int get inSeconds => _duration ~/ Duration.microsecondsPerSecond;
+  /// A negative duration has a negative number of whole seconds,
+  /// so that `(-duration).inSeconds` = `-(duration.inSeconds)`.
+  int get inSeconds => inMicroseconds ~/ Duration.microsecondsPerSecond;
 
   /// The number of whole milliseconds spanned by this [Duration].
   ///
   /// The returned value can be greater than 999.
-  /// For example, a duration of three seconds and 125 milliseconds
-  /// has 3125 milliseconds.
+  /// For example, a duration of nine seconds and 123 milliseconds
+  /// has 9125 milliseconds.
   /// ```dart
-  /// const duration = Duration(seconds: 3, milliseconds: 125);
-  /// print(duration.inMilliseconds); // 3125
+  /// const duration = Duration(seconds: 9, milliseconds: 123);
+  /// print(duration.inMilliseconds); // 9123 = 9 * 1000 + 123
   /// ```
-  int get inMilliseconds => _duration ~/ Duration.microsecondsPerMillisecond;
+  int get inMilliseconds =>
+      inMicroseconds ~/ Duration.microsecondsPerMillisecond;
 
-  /// The number of whole microseconds spanned by this [Duration].
+  /// Whether this [Duration] is equivalent to [other].
   ///
-  /// The returned value can be greater than 999999.
-  /// For example, a duration of three seconds, 125 milliseconds and
-  /// 369 microseconds has 3125369 microseconds.
-  /// ```dart
-  /// const duration = Duration(seconds: 3, milliseconds: 125,
-  ///     microseconds: 369);
-  /// print(duration.inMicroseconds); // 3125369
-  /// ```
-  int get inMicroseconds => _duration;
-
-  /// Whether this [Duration] has the same length as [other].
-  ///
-  /// Durations have the same length if they have the same number
+  /// Durations are equivalent if they have the same number
   /// of microseconds, as reported by [inMicroseconds].
   bool operator ==(Object other) =>
-      other is Duration && _duration == other.inMicroseconds;
+      other is Duration && inMicroseconds == other.inMicroseconds;
 
-  int get hashCode => _duration.hashCode;
+  /// The hash code of a duration is the hash code of its [inMicroseconds].
+  int get hashCode => inMicroseconds.hashCode;
 
-  /// Compares this [Duration] to [other], returning zero if the values are equal.
+  /// Compares this [Duration] to [other].
   ///
-  /// Returns a negative integer if this [Duration] is shorter than
-  /// [other], or a positive integer if it is longer.
+  /// Returns a negative integer if this [Duration]'s offset is smaller
+  /// than that of [other], a positive integer if this duration's offset
+  /// is greater than that of [other], and 0 if they are equivalent.
   ///
-  /// A negative [Duration] is always considered shorter than a positive one.
+  /// A longer positive duration ends after a shorter positive duration,
+  /// a positive duration ends after a negative duration,
+  /// and a shorter negative duration ends after a longer negative duration.
+  /// Equivalent to comparing the [inMicroseconds] of the duration).
   ///
-  /// It is always the case that `duration1.compareTo(duration2) < 0` iff
+  /// It is always the case that `duration1.compareTo(duration2) < 0`
+  /// if and only if
   /// `(someDate + duration1).compareTo(someDate + duration2) < 0`.
-  int compareTo(Duration other) => _duration.compareTo(other._duration);
+  int compareTo(Duration other) =>
+      inMicroseconds.compareTo(other.inMicroseconds);
 
-  /// Returns a string representation of this [Duration].
+  /// A string representation of this [Duration].
   ///
-  /// Returns a string with hours, minutes, seconds, and microseconds, in the
+  /// A string with hours, minutes, seconds, and microseconds, in the
   /// following format: `H:MM:SS.mmmmmm`. For example,
   /// ```dart
   /// var d = const Duration(days: 1, hours: 1, minutes: 33, microseconds: 500);
@@ -372,19 +424,19 @@ class Duration implements Comparable<Duration> {
   ///
   /// A negative [Duration] represents the difference from a later time to an
   /// earlier time.
-  bool get isNegative => _duration < 0;
+  bool get isNegative => inMicroseconds < 0;
 
-  /// Creates a new [Duration] representing the absolute length of this
-  /// [Duration].
+  /// A positive [Duration] with the same absolute length as this [Duration].
   ///
   /// The returned [Duration] has the same length as this one, but is always
   /// positive where possible.
-  Duration abs() => Duration._microseconds(_duration.abs());
+  Duration abs() =>
+      inMicroseconds >= 0 ? this : Duration._microseconds(0 - inMicroseconds);
 
-  /// Creates a new [Duration] with the opposite direction of this [Duration].
+  /// Creates a new [Duration] with the opposite  sign of this [Duration].
   ///
-  /// The returned [Duration] has the same length as this one, but will have the
-  /// opposite sign (as reported by [isNegative]) as this one where possible.
+  /// The returned [Duration] has the absolute length as this one, but has the
+  /// opposite sign (as reported by [isNegative]) where possible.
   // Using subtraction helps dart2js avoid negative zeros.
-  Duration operator -() => Duration._microseconds(0 - _duration);
+  Duration operator -() => Duration._microseconds(0 - inMicroseconds);
 }
