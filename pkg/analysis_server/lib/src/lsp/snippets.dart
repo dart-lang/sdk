f43f2c04090a46949307a3a7604221d8405b4e3c
Danny Tuppeny <danny@tuppeny.com>
2026-08-05 10:51:27 -0700

    [analysis_server] Update comments explaining placeholders outside of LSP snippet text edits
    This improves a comment that explains what was found while investigating https://github.com/dart-lang/sdk/issues/63824, and also removes a TODO comment that was answered in a review (and concluded there's nothing to do).
    
    Change-Id: I7061d6364086c504916746a8fbbcebebc7abd5a3
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/531620
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>

--- pkg/analysis_server/lib/src/lsp/snippets.dart
+++ pkg/analysis_server/lib/src/lsp/snippets.dart
@@ -124,8 +124,23 @@ String _buildSnippetString(
     ...editGroups.expandIndexed(convertEditGroup),
   ];
 
-  // Remove any groups outside of the range (it's possible the edit groups apply
-  // to a different edit in the collection).
+  // Remove any groups outside of the range of this edit.
+  //
+  // We have some fixes that produced linked edit groups (and selections) that
+  // might be outside of the edit. For example:
+  //
+  // ```
+  // void f() {
+  //   x(); // invoke "Create function 'x' here"
+  // }
+  // ```
+  //
+  // This inserts a new function that tries to link the invocation `x` to the
+  // name of the new function, so that renaming the new function also updates
+  // the invocation. However, because the invocation is not part of the edit, it
+  // cannot be marked up in the snippet text.
+  //
+  // See https://github.com/dart-lang/sdk/issues/63824
   placeholders.removeWhere(
     (placeholder) =>
         placeholder.offset < 0 ||
