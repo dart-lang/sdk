9be9ffc86d1ad75f8a040712839f90afd74e97ef
Konstantin Shcheglov <scheglov@google.com>
2026-08-13 13:13:35 -0700

    DAS. In ReplaceWithIsEmpty keep comments between the target and 'length'.
    Modernize a little.
    
    Change-Id: I661747b8d51308ae3b5ec6edbade924a0ed91a0d
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/535621
    Commit-Queue: Konstantin Shcheglov <scheglov@google.com>
    Reviewed-by: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/lib/src/services/correction/dart/replace_with_is_empty.dart
+++ pkg/analysis_server/lib/src/services/correction/dart/replace_with_is_empty.dart
@@ -74,15 +74,20 @@ class ReplaceWithIsEmpty extends ResolvedCorrectionProducer {
     }
 
     // Skip nullable targets.
-    if (replacement.lengthTarget.staticType?.nullabilitySuffix ==
+    var lengthAccess = replacement.lengthAccess;
+    if (lengthAccess.target.staticType?.nullabilitySuffix ==
         NullabilitySuffix.question) {
       return;
     }
 
-    var target = utils.getNodeText(replacement.lengthTarget);
+    // Keep everything through the period so that comments between the target
+    // and `length` remain attached to the replacement getter.
+    var target = utils.getRangeText(
+      range.startStart(lengthAccess.target, lengthAccess.length),
+    );
     var getter = replacement.getter;
     await builder.addDartFileEdit(file, (builder) {
-      builder.addSimpleReplacement(range.node(binary), '$target.$getter');
+      builder.addSimpleReplacement(range.node(binary), '$target$getter');
     });
   }
 
@@ -92,46 +97,46 @@ class ReplaceWithIsEmpty extends ResolvedCorrectionProducer {
     var operator = binary.operator.type;
     var rightValue = _getIntValue(binary.rightOperand);
     if (rightValue != null) {
-      var lengthTarget = _getLengthTarget(binary.leftOperand);
-      if (lengthTarget == null) {
+      var lengthAccess = _getLengthAccess(binary.leftOperand);
+      if (lengthAccess == null) {
         return null;
       }
       if (rightValue == 0) {
         if (operator == TokenType.EQ_EQ || operator == TokenType.LT_EQ) {
-          return _Replacement.isEmpty(lengthTarget);
+          return _Replacement.isEmpty(lengthAccess);
         } else if (operator == TokenType.GT || operator == TokenType.BANG_EQ) {
-          return _Replacement.isNotEmpty(lengthTarget);
+          return _Replacement.isNotEmpty(lengthAccess);
         }
       } else if (rightValue == 1) {
         // 'length >= 1' is same as 'isNotEmpty',
         // and 'length < 1' is same as 'isEmpty'
         if (operator == TokenType.GT_EQ) {
-          return _Replacement.isNotEmpty(lengthTarget);
+          return _Replacement.isNotEmpty(lengthAccess);
         } else if (operator == TokenType.LT) {
-          return _Replacement.isEmpty(lengthTarget);
+          return _Replacement.isEmpty(lengthAccess);
         }
       }
     } else {
       var leftValue = _getIntValue(binary.leftOperand);
       if (leftValue != null) {
-        var lengthTarget = _getLengthTarget(binary.rightOperand);
-        if (lengthTarget == null) {
+        var lengthAccess = _getLengthAccess(binary.rightOperand);
+        if (lengthAccess == null) {
           return null;
         }
         if (leftValue == 0) {
           if (operator == TokenType.EQ_EQ || operator == TokenType.GT_EQ) {
-            return _Replacement.isEmpty(lengthTarget);
+            return _Replacement.isEmpty(lengthAccess);
           } else if (operator == TokenType.LT ||
               operator == TokenType.BANG_EQ) {
-            return _Replacement.isNotEmpty(lengthTarget);
+            return _Replacement.isNotEmpty(lengthAccess);
           }
         } else if (leftValue == 1) {
           // '1 <= length' is same as 'isNotEmpty',
           // and '1 > length' is same as 'isEmpty'
           if (operator == TokenType.LT_EQ) {
-            return _Replacement.isNotEmpty(lengthTarget);
+            return _Replacement.isNotEmpty(lengthAccess);
           } else if (operator == TokenType.GT) {
-            return _Replacement.isEmpty(lengthTarget);
+            return _Replacement.isEmpty(lengthAccess);
           }
         }
       }
@@ -158,46 +163,43 @@ class ReplaceWithIsEmpty extends ResolvedCorrectionProducer {
     return null;
   }
 
-  /// Return the expression producing the object on which `length` is being
-  /// invoked, or `null` if there is no such expression.
-  static Expression? _getLengthTarget(Expression expression) {
-    if (expression is PropertyAccess &&
-        expression.propertyName.name == 'length') {
-      return expression.target;
-    } else if (expression is PrefixedIdentifier &&
-        expression.identifier.name == 'length') {
-      return expression.prefix;
+  /// Returns the target and `length` identifier of a `length` access.
+  static ({Expression target, SimpleIdentifier length})? _getLengthAccess(
+    Expression expression,
+  ) {
+    switch (expression) {
+      case PropertyAccess(target: var target?, propertyName: var length):
+        if (length.name == 'length') {
+          return (target: target, length: length);
+        }
+      case PrefixedIdentifier(:var prefix, identifier: var length):
+        if (length.name == 'length') {
+          return (target: prefix, length: length);
+        }
     }
     return null;
   }
 }
 
-class _Replacement {
-  final FixKind fixKind;
-  final FixKind multiFixKind;
-  final String getter;
-  final Expression lengthTarget;
-
-  new isEmpty(Expression lengthTarget)
-    : this._(
+class _Replacement({
+  required final FixKind fixKind,
+  required final FixKind multiFixKind,
+  required final String getter,
+  required final ({Expression target, SimpleIdentifier length}) lengthAccess,
+}) {
+  new isEmpty(({Expression target, SimpleIdentifier length}) lengthAccess)
+    : this(
         fixKind: DartFixKind.replaceWithIsEmpty,
         multiFixKind: DartFixKind.replaceWithIsEmptyMulti,
         getter: 'isEmpty',
-        lengthTarget: lengthTarget,
+        lengthAccess: lengthAccess,
       );
 
-  new isNotEmpty(Expression lengthTarget)
-    : this._(
+  new isNotEmpty(({Expression target, SimpleIdentifier length}) lengthAccess)
+    : this(
         fixKind: DartFixKind.replaceWithIsNotEmpty,
         multiFixKind: DartFixKind.replaceWithIsNotEmptyMulti,
         getter: 'isNotEmpty',
-        lengthTarget: lengthTarget,
+        lengthAccess: lengthAccess,
       );
-
-  new _({
-    required this.fixKind,
-    required this.multiFixKind,
-    required this.getter,
-    required this.lengthTarget,
-  });
 }
