b6d40f74f96d9b85b703c1c4b6856463c7fb721c
Modestas Valauskas <valauskasmodestas@gmail.com>
2026-08-17 04:37:08 -0700

    [typed_data] Add blank line after license header in typed_data tests
    Separates the copyright header from the file contents across the
    typed_data test suite, matching the convention used by the rest of the
    suite.
    
    Change-Id: I451ce300549ffa47d253e8c120a21003d05ef9b3
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/534540
    Reviewed-by: Martin Kustermann <kustermann@google.com>
    Commit-Queue: Martin Kustermann <kustermann@google.com>
    Auto-Submit: Modestas Valauskas <valauskasmodestas@gmail.com>
    Reviewed-by: Lasse Nielsen <lrn@google.com>

--- tests/lib/typed_data/float64x2_list_test.dart
+++ tests/lib/typed_data/float64x2_list_test.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
 // VMOptions=--max_deoptimization_counter_threshold=1000 --optimization-counter-threshold=10 --no-background_compilation
 
 import 'dart:typed_data';
