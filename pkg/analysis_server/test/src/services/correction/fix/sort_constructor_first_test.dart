ce43f85b028d2ff348076ab8ea4750838198777d
hkarmoush <hkarmoush@gmail.com>
2026-08-22 12:23:51 +0300

    [analysis_server] Sort members in sort_constructor_first_test.dart
    Reorder the new test methods added to fix blank-line placement so the
    file matches the repo's enforced member-sort convention; this was the
    cause of the CQ presubmit failure ("Unsorted file").

--- pkg/analysis_server/test/src/services/correction/fix/sort_constructor_first_test.dart
+++ pkg/analysis_server/test/src/services/correction/fix/sort_constructor_first_test.dart
@@ -158,6 +158,74 @@ class A() {
 ''');
   }
 
+  Future<void> test_crlfLineEndings() async {
+    await resolveTestCode(
+      '''
+class A {
+  final String foo;
+
+  const A({required this.foo});
+}
+'''
+          .replaceAll('\n', '\r\n'),
+    );
+    await assertHasFix(
+      '''
+class A {
+  const A({required this.foo});
+
+  final String foo;
+}
+'''
+          .replaceAll('\n', '\r\n'),
+    );
+  }
+
+  Future<void> test_enum_blankLineAddedAroundRelocatedConstructor() async {
+    await resolveTestCode('''
+enum A {
+  bar('Open'),
+  baz('Accepted Provisionally');
+  final String value;
+
+  const A(this.value);
+}
+''');
+    await assertHasFix('''
+enum A {
+  bar('Open'),
+  baz('Accepted Provisionally');
+
+  const A(this.value);
+
+  final String value;
+}
+''');
+  }
+
+  Future<void> test_enum_blankLineAfterSemicolon() async {
+    await resolveTestCode('''
+enum A {
+  bar('Open'),
+  baz('Accepted Provisionally');
+
+  final String value;
+
+  const A(this.value);
+}
+''');
+    await assertHasFix('''
+enum A {
+  bar('Open'),
+  baz('Accepted Provisionally');
+
+  const A(this.value);
+
+  final String value;
+}
+''');
+  }
+
   Future<void> test_enum_constHead() async {
     await resolveTestCode('''
 enum E {
@@ -259,91 +327,6 @@ enum E {
   a;
   E();
   void m() {}
-}
-''');
-  }
-
-  Future<void> test_noBlankLineAfterOpeningBrace() async {
-    await resolveTestCode('''
-class A {
-  final String foo;
-
-  const A({required this.foo});
-}
-''');
-    await assertHasFix('''
-class A {
-  const A({required this.foo});
-
-  final String foo;
-}
-''');
-  }
-
-  Future<void> test_crlfLineEndings() async {
-    await resolveTestCode(
-      '''
-class A {
-  final String foo;
-
-  const A({required this.foo});
-}
-'''
-          .replaceAll('\n', '\r\n'),
-    );
-    await assertHasFix(
-      '''
-class A {
-  const A({required this.foo});
-
-  final String foo;
-}
-'''
-          .replaceAll('\n', '\r\n'),
-    );
-  }
-
-  Future<void> test_enum_blankLineAfterSemicolon() async {
-    await resolveTestCode('''
-enum A {
-  bar('Open'),
-  baz('Accepted Provisionally');
-
-  final String value;
-
-  const A(this.value);
-}
-''');
-    await assertHasFix('''
-enum A {
-  bar('Open'),
-  baz('Accepted Provisionally');
-
-  const A(this.value);
-
-  final String value;
-}
-''');
-  }
-
-  Future<void> test_enum_blankLineAddedAroundRelocatedConstructor() async {
-    await resolveTestCode('''
-enum A {
-  bar('Open'),
-  baz('Accepted Provisionally');
-  final String value;
-
-  const A(this.value);
-}
-''');
-    await assertHasFix('''
-enum A {
-  bar('Open'),
-  baz('Accepted Provisionally');
-
-  const A(this.value);
-
-  final String value;
 }
 ''');
   }
@@ -452,6 +435,23 @@ class A {
 class A {
   new();
   void m() {}
+}
+''');
+  }
+
+  Future<void> test_noBlankLineAfterOpeningBrace() async {
+    await resolveTestCode('''
+class A {
+  final String foo;
+
+  const A({required this.foo});
+}
+''');
+    await assertHasFix('''
+class A {
+  const A({required this.foo});
+
+  final String foo;
 }
 ''');
   }
