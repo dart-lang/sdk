fce778266227930c4133ef435bb1d583d66aafc7
Danny Tuppeny <danny@tuppeny.com>
2026-08-27 09:52:21 -0700

    [analysis_server] Handle Color.withXxx() methods for Flutter colors in LSP
    Before this change, `Color.foo.withValues(red:100)` would show a misleading preview/picker for `Color.foo` ignoring the call to `withValues()`. With this, we handle all of the built-in (non-deprecated) `withXxx()` methods.
    
    Fixes https://github.com/Dart-Code/Dart-Code/issues/6143
    
    Change-Id: I5f4be30a85d124d226fe98a93ab9a4ea2e3da310
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/542060
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server/test/src/computer/color_computer_test.dart
+++ pkg/analysis_server/test/src/computer/color_computer_test.dart
@@ -84,6 +84,20 @@ class ColorComputerTest extends AbstractContextTest {
     'CupertinoColors.activeBlue.darkHighContrastColor': 0xFF409CFF,
     'CupertinoColors.activeBlue.elevatedColor': 0xFF007AFF,
     'CupertinoColors.activeBlue.darkElevatedColor': 0xFF0A84FF,
+    // withX() helpers.
+    'Colors.white.withAlpha(100)': 0x64FFFFFF,
+    'Colors.white.withRed(100)': 0xFF64FFFF,
+    'Colors.white.withGreen(100)': 0xFFFF64FF,
+    'Colors.white.withBlue(100)': 0xFFFFFF64,
+    'Colors.white.withValues(alpha: 0.5, red: 0.6, green: 0.7, blue: 0.8)':
+        0x8099B3CC,
+    // withX calls chained on self and constructors.
+    'Colors.red.shade100.withRed(0)': 0xFF00CDD2,
+    'Color(0xFFFFFFFF).withRed(100).withBlue(100)': 0xFF64FF64,
+    'Colors.white.withRed(100).withBlue(100)': 0xFF64FF64,
+    // Invalid values get clamped.
+    'Colors.white.withValues(alpha: -2, red: -2, green: 2, blue: 2).withRed(-100).withGreen(300)':
+        0x0000FFFF,
   };
 
   /// A map of Dart source code that creates multiple nested color references.
@@ -370,6 +384,23 @@ void f() {
 }
 ''';
     await checkAllColors(testCode);
+  }
+
+  Future<void> test_noStackOverflow() async {
+    // While manual testing, this code previously triggered a StackOverflow so
+    // this test just verifies it doesn't happen.
+    // We don't compute a color for .background because ColorScheme is not
+    // const here.
+    const testCode = '''
+class ColorScheme {
+    final Color background;
+    ColorScheme(this.background);
+}
+
+var x = ColorScheme(Colors.white).background;
+''';
+
+    await expectColors(testCode, {'Colors.white': 0xFFFFFFFF});
   }
 
   Future<void> test_nullAwareElement_inList_const() async {
