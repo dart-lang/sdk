46025b3d2689c35da93c12032f4f2a22244b41c1
Ryan Macnak <rmacnak@google.com>
2026-08-31 10:24:30 -0700

    Add dart:ffi Abi value for arm64e.
    TEST=ci, local machine with mac 26.
    CoreLibraryReviewExempt: VM-only library
    Bug: https://github.com/dart-lang/sdk/issues/63420
    Change-Id: I1a0648b55b28862500d48e41f3f83a7edfe7e12a
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/538020
    Reviewed-by: Daco Harkes <dacoharkes@google.com>
    Commit-Queue: Ryan Macnak <rmacnak@google.com>

--- tests/ffi/native_assets/helpers.dart
+++ tests/ffi/native_assets/helpers.dart
@@ -272,9 +272,9 @@ Future<void> runGenSnapshot({
       // `computeAssembleCommand`.
       if (Platform.isMacOS) {
         await runProcess(
-          executable: Abi.current() == Abi.macosArm64
-              ? 'buildtools/mac-arm64/clang/bin/clang'
-              : 'buildtools/mac-x64/clang/bin/clang',
+          executable: Abi.current() == Abi.macosX64
+              ? 'buildtools/mac-x64/clang/bin/clang'
+              : 'buildtools/mac-arm64/clang/bin/clang',
           arguments: [
             '-Wl,-undefined,error',
             '-Wl,-no_compact_unwind',
