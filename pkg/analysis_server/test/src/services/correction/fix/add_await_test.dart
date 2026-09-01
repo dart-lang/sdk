2d8d2cfae4d60d9e769bd3e13c2056e8f70d6676
FMorschel <git@fmorschel.dev>
2026-09-01 13:09:55 -0700

    Reland "[linter] Makes `unawaited_futures` report for `FutureOr`"
    This reverts commit 5c630788056fba98e715acad97a405718e0f2a3d.
    
    Reason for revert: The issue is still valid, we just found failures in g3.
    
    Original change's description:
    > Revert "[linter] Makes `unawaited_futures` report for `FutureOr`"
    >
    > This reverts commit 6f00c0695c7c2cdddf16776b9a1c272bba70045a.
    >
    > Reason for revert: Many failures in google3
    >
    > Failure Link: https://fusion2.corp.google.com/presubmit/947562707/OCL:947562707:BASE:948100983:1784092383598:6c2699f3/targets
    >
    > Original change's description:
    > > [linter] Makes `unawaited_futures` report for `FutureOr`
    > >
    > > Fixes: https://github.com/dart-lang/sdk/issues/59105
    > > Change-Id: I8fcfd5efc2ff6b29cb9cf94353f8e11ea02c8864
    > > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/520882
    > > Commit-Queue: Samuel Rawlins <srawlins@google.com>
    > > Auto-Submit: FMorschel <git@fmorschel.dev>
    > > Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    > > Reviewed-by: Samuel Rawlins <srawlins@google.com>
    >
    > Change-Id: I56c341fbd650fd152e7c8694bb87d82025a80958
    > Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/524720
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Liam Appelbe <liama@google.com>
    > Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    > Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    
    Change-Id: Ib6d78bfc626bb817414d150645f722416504a876
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/525320
    Reviewed-by: Samuel Rawlins <srawlins@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Brian Wilkerson <brianwilkerson@google.com>
    Commit-Queue: Samuel Rawlins <srawlins@google.com>

--- pkg/analysis_server/test/src/services/correction/fix/add_await_test.dart
+++ pkg/analysis_server/test/src/services/correction/fix/add_await_test.dart
@@ -147,6 +147,25 @@ void f() async {
 ''');
   }
 
+  Future<void> test_methodInvocation_futureOr() async {
+    await resolveTestCode('''
+import 'dart:async';
+FutureOr doSomething() => Future.value('');
+
+void f() async {
+  doSomething();
+}
+''');
+    await assertHasFix('''
+import 'dart:async';
+FutureOr doSomething() => Future.value('');
+
+void f() async {
+  await doSomething();
+}
+''');
+  }
+
   Future<void> test_methodInvocationWithParserError() async {
     await resolveTestCode('''
 Future doSomething() => Future.value('');
