d109b74bef96a0b46a989fe8a561102ea1316542
Danny Tuppeny <danny@tuppeny.com>
2026-09-02 12:22:17 -0700

    [analysis_server] Remove inTestMode from bulk fix API
    This flag appears to be unused in the server. It's only used inside the `dart fix` command itself.
    
    Since I haven't implemented an equivalent in LSP, I'd like to remove it first to verify it's definitely not being used by any code, and to avoid it looking like a change in an upcoming LSP migration CL.
    
    Change-Id: I38ae374e08b1972b7db2fd50f2f2714e610d2cd0
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/544740
    Reviewed-by: Keerti Parthasarathy <keertip@google.com>
    Commit-Queue: Keerti Parthasarathy <keertip@google.com>
    Reviewed-by: Brian Wilkerson <brianwilkerson@google.com>

--- pkg/analysis_server_client/lib/src/protocol/protocol_generated.dart
+++ pkg/analysis_server_client/lib/src/protocol/protocol_generated.dart
@@ -5175,7 +5175,6 @@ class DiagnosticGetServerPortResult implements ResponseResult {
 ///
 ///     {
 ///       "included": List<FilePath>
-///       "inTestMode": optional bool
 ///       "updatePubspec": optional bool
 ///       "codes": optional List<String>
 ///     }
@@ -5192,14 +5191,6 @@ class EditBulkFixesParams implements RequestParams {
   /// of type `FILE_NOT_ANALYZED` will be generated.
   List<String> included;
 
-  /// A flag indicating whether the bulk fixes are being run in test mode. The
-  /// only difference is that in test mode the fix processor will look for a
-  /// configuration file that can modify the content of the data file used to
-  /// compute the fixes when data-driven fixes are being considered.
-  ///
-  /// If this field is omitted the flag defaults to `false`.
-  bool? inTestMode;
-
   /// A flag indicating whether to validate that the dependencies used by the
   /// included files are listed in the pubspec file. If specified, the fix
   /// processor will compute the set of packages imported in the source and
@@ -5212,12 +5203,7 @@ class EditBulkFixesParams implements RequestParams {
   /// A list of diagnostic codes to be fixed.
   List<String>? codes;
 
-  EditBulkFixesParams(
-    this.included, {
-    this.inTestMode,
-    this.updatePubspec,
-    this.codes,
-  });
+  EditBulkFixesParams(this.included, {this.updatePubspec, this.codes});
 
   factory EditBulkFixesParams.fromJson(
     JsonDecoder jsonDecoder,
@@ -5236,13 +5222,6 @@ class EditBulkFixesParams implements RequestParams {
       } else {
         throw jsonDecoder.mismatch(jsonPath, 'included', json);
       }
-      bool? inTestMode;
-      if (json.containsKey('inTestMode')) {
-        inTestMode = jsonDecoder.decodeBool(
-          '$jsonPath.inTestMode',
-          json['inTestMode'],
-        );
-      }
       bool? updatePubspec;
       if (json.containsKey('updatePubspec')) {
         updatePubspec = jsonDecoder.decodeBool(
@@ -5260,7 +5239,6 @@ class EditBulkFixesParams implements RequestParams {
       }
       return EditBulkFixesParams(
         included,
-        inTestMode: inTestMode,
         updatePubspec: updatePubspec,
         codes: codes,
       );
@@ -5281,10 +5259,6 @@ class EditBulkFixesParams implements RequestParams {
   Map<String, Object> toJson() {
     var result = <String, Object>{};
     result['included'] = included;
-    var inTestMode = this.inTestMode;
-    if (inTestMode != null) {
-      result['inTestMode'] = inTestMode;
-    }
     var updatePubspec = this.updatePubspec;
     if (updatePubspec != null) {
       result['updatePubspec'] = updatePubspec;
@@ -5308,14 +5282,12 @@ class EditBulkFixesParams implements RequestParams {
   bool operator ==(Object other) =>
       other is EditBulkFixesParams &&
       listEqual(included, other.included, (String a, String b) => a == b) &&
-      inTestMode == other.inTestMode &&
       updatePubspec == other.updatePubspec &&
       listEqual(codes, other.codes, (String a, String b) => a == b);
 
   @override
   int get hashCode => Object.hash(
     Object.hashAll(included),
-    inTestMode,
     updatePubspec,
     Object.hashAll(codes ?? []),
   );
