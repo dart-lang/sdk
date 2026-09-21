0e88be1077024b293733640e963233c443602fb8
Ryan Macnak <rmacnak@google.com>
2026-09-21 12:04:53 -0700

    [gardening] More avoid running multiple children at once in tests.
    Greatly reduces peak memory consumption, especially under TSAN.
    
    Compare e64edefde7f2cdf4a6375f5bb72470c72391e5b8
    
    Change-Id: Ic7e4bd11fb52d5ea24097ee4e57bdb77ca170531
    Reviewed-on: https://dart-review.googlesource.com/c/sdk/+/544300
    Commit-Queue: Ryan Macnak <rmacnak@google.com>
    Reviewed-by: Alexander Markov <alexmarkov@google.com>

--- tests/standalone/io/stdin_sync_test.dart
+++ tests/standalone/io/stdin_sync_test.dart
@@ -10,45 +10,45 @@ import "dart:io";
 import "package:path/path.dart";
 import "package:expect/expect.dart";
 
-void testReadByte() {
-  void test(String line, List<String> expected) {
+Future<void> testReadByte() async {
+  test(String line, List<String> expected) async {
     var script = Platform.script.resolve("stdin_sync_script.dart").toFilePath();
-    Process.start(
+    var process = await Process.start(
       Platform.executable,
       []
         ..addAll(Platform.executableArguments)
         ..add('--verbosity=warning')
         ..add(script)
         ..addAll(expected.map(json.encode)),
-    ).then((process) {
-      process.stdin.write(line);
-      process.stdin.flush().then((_) => process.stdin.close());
-      process.stderr
-          .transform(utf8.decoder)
-          .transform(new LineSplitter())
-          .fold<StringBuffer>(new StringBuffer(), (b, d) => b..write(d))
-          .then((data) {
-            if (data.toString() != '') throw "Bad output: '$data'";
-          });
-      process.stdout
-          .transform(utf8.decoder)
-          .transform(new LineSplitter())
-          .fold<StringBuffer>(new StringBuffer(), (b, d) => b..write(d))
-          .then((data) {
-            if (data.toString() != 'true') throw "Bad output: '$data'";
-          });
-    });
+    );
+    process.stdin.write(line);
+    process.stdin.flush().then((_) => process.stdin.close());
+    process.stderr
+        .transform(utf8.decoder)
+        .transform(new LineSplitter())
+        .fold<StringBuffer>(new StringBuffer(), (b, d) => b..write(d))
+        .then((data) {
+          if (data.toString() != '') throw "Bad output: '$data'";
+        });
+    process.stdout
+        .transform(utf8.decoder)
+        .transform(new LineSplitter())
+        .fold<StringBuffer>(new StringBuffer(), (b, d) => b..write(d))
+        .then((data) {
+          if (data.toString() != 'true') throw "Bad output: '$data'";
+        });
+    await process.exitCode;
   }
 
-  test("hej\x01\x00\x0d\x0a\x0a4\x0a", ['hej\x01\x00', '', '4']);
+  await test("hej\x01\x00\x0d\x0a\x0a4\x0a", ['hej\x01\x00', '', '4']);
 
-  test("hej\u0187", ['hej\u0187']);
+  await test("hej\u0187", ['hej\u0187']);
 
-  test("hej\rhej\nhej\r", ['hej\rhej', 'hej\r']);
+  await test("hej\rhej\nhej\r", ['hej\rhej', 'hej\r']);
 
-  test("hej\r\r\nhej\r\nhej\r", ['hej\r', 'hej', 'hej\r']);
+  await test("hej\r\r\nhej\r\nhej\r", ['hej\r', 'hej', 'hej\r']);
 
-  test("hej", ['hej']);
+  await test("hej", ['hej']);
 }
 
 void testEchoMode() {
@@ -73,8 +73,8 @@ void testLineMode() {
   }
 }
 
-void main() {
-  testReadByte();
+main() async {
+  await testReadByte();
 
   // testEchoMode and testLineMode is developer-interactive tests, thus not
   // enabled.
